0e6a81a79324ca9392ac3c759f8a106745a28437
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-13 22:34:14 +0000

    Fix archive view.

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ccache g++-4.7 -o main.o -Wall -Werror -Wextra -pedantic-errors -std=c++0x -pthread -I/usr/local/include main.cpp
+g++ -o main.o -Wall -Werror -Wextra -pedantic-errors -std=c++0x -pthread -I/usr/local/include main.cpp
