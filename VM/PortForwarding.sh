f8791254bc66db4e377f65ff75023e928a439c30
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-14 19:54:54 +0000

    Rename server.

--- VM/PortForwarding.sh
+++ VM/PortForwarding.sh
@@ -1,5 +1,5 @@
-VBoxManage modifyvm "coliru-server" --natpf1 delete guestssh
-VBoxManage modifyvm "coliru-server" --natpf1 "guestssh,tcp,,2222,,22"
+VBoxManage modifyvm "coliru" --natpf1 delete guestssh
+VBoxManage modifyvm "coliru" --natpf1 "guestssh,tcp,,2222,,22"
 
-VBoxManage modifyvm "coliru-server" --natpf1 delete guesthttp
-VBoxManage modifyvm "coliru-server" --natpf1 "guesthttp,tcp,,8080,,80"
+VBoxManage modifyvm "coliru" --natpf1 delete guesthttp
+VBoxManage modifyvm "coliru" --natpf1 "guesthttp,tcp,,8080,,80"
