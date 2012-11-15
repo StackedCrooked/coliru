ca6e3643948ddf7a20d4cbbc0391243bf56e7995
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-15 20:02:29 +0000

    Remove PortForwarding script.

--- trunk/VM/bin/PortForwarding.sh
+++
@@ -1,5 +0,0 @@
-VBoxManage modifyvm "coliru" --natpf1 delete guestssh
-VBoxManage modifyvm "coliru" --natpf1 "guestssh,tcp,,2222,,22"
-
-VBoxManage modifyvm "coliru" --natpf1 delete guesthttp
-VBoxManage modifyvm "coliru" --natpf1 "guesthttp,tcp,,8080,10.0.2.15,80"
