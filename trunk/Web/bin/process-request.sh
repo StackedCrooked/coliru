6080c1dd0dd0691138dcd6377e86e0ed90355559
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-17 12:45:19 +0000

    Fix processing script. Todo: sanitation.

--- trunk/Web/bin/process-request.sh
+++ trunk/Web/bin/process-request.sh
@@ -5,7 +5,7 @@ if [ $# -ne 1 ] ; then
     exit 1
 fi
 
-cat request.txt | head -n 1 | sed 's/\\n/\n/g' > main.user.cpp
-cat request.txt | head -n 2 | tail -n 1 > compiler-flags.user.txt
+cat request.txt | sed -n 1p | sed 's/\\n/\n/g' > main.user.cpp
+cat request.txt | sed -n 2p > compiler-flags.user.txt
 g++ -o main.user.binary `cat compiler-flags.user.txt | head -n 1` main.user.cpp >response.txt 2>&1
 ./main.user.binary >>response.txt 2>&1
