9cd574266cedc9519bf663d5da6cf389c0f89f43
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-17 17:25:30 +0000

    Improve script.

--- trunk/Web/bin/process-request.sh
+++ trunk/Web/bin/process-request.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
 set -x
-if [ $# -ne 1 ] ; then
-    echo "Usage: $0 request-file" 1>&2
-    exit 1
+touch compiler-flags.txt
+GCC_OPTIONS=`cat main.cpp | head -n1`
+echo "GCC_OPTIONS: ${GCC_OPTIONS}"
+if grep -q "^// g++" <<< ${GCC_OPTIONS} ; then
+    echo ${GCC_OPTIONS} >> compiler-flags.txt
 fi
-
-cat request.txt | sed -n 1p | sed 's/\\n/\n/g' > main.user.cpp
-cat request.txt | sed -n 2p > compiler-flags.user.txt
-g++ -o main.user.binary `cat compiler-flags.user.txt | head -n 1` main.user.cpp >response.txt 2>&1
-./main.user.binary >>response.txt 2>&1
+g++ `cat compiler-flags.txt | head -n 1` -o test main.cpp >response.txt 2>&1
+./test >>response.txt 2>&1
+cat response.txt
