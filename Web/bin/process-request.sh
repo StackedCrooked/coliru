d3b210f6d6a3abd6562184daf37c2526cf314af6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-17 23:34:21 +0000

    Fix fork stopping.

--- Web/bin/process-request.sh
+++ Web/bin/process-request.sh
@@ -1,1 +1,7 @@
-sudo -u sandboxer bin/process-request-sandboxed.sh
+set -x
+sudo -u sandboxer bin/process-request-sandboxed.sh &
+echo "Timeout after 2 second."
+sleep 2
+echo "Time's up!"
+ps -ef | grep "bin/process-request-sandboxed.sh" | awk '{print $3}' | xargs -I PID kill -9 PID
+echo "Timed out!"
