bcb62c647be634854a8b72e4b471e0e24c618fad
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-18 16:19:20 +0000

    Working code.

--- Web/process.sh
+++ Web/process.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set +x
+#set -x
 set -e 
-ulimit -t 10
+ulimit -t 5
 
 
 (file request.txt 2>/dev/null 1>&2) || { echo "No request file found. Exiting." >&2 ; exit 1 ; }
 
 { { sleep 3 && echo "Timeout" 1>&2 ; /bin/bash -c 'killall test' ; } & }
-{ trap "kill $!" INT ERR EXIT && { sudo -u sandbox ./sandbox.sh ./request.txt ; } ; }
+{ trap "kill $!" INT ERR EXIT && { sudo -u sandbox ./sandbox.sh ./request.txt ; } || { echo "Compilation was terminated before it could finish." 1>&2  ; } ; }
 exit 0
