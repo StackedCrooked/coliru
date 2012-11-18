e0cff8e873fb708b30ff3f77a84a3cb870199607
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-18 05:42:24 +0000

    Cleanup.

--- trunk/Web/process.sh
+++ trunk/Web/process.sh
@@ -4,5 +4,5 @@ set -e
 (file request.txt 2>/dev/null 1>&2) || { echo "No request file found. Exiting." >&2 ; exit 1 ; }
 
 { { sleep 3 && echo "Timeout" 1>&2 ; /bin/bash -c 'killall test' ; } & }
-{ trap "kill $!" INT ERR EXIT && { sudo -u sandboxer bin/process-request-sandboxed.sh ./request.txt ; } ; }
+{ trap "kill $!" INT ERR EXIT && { sudo -u sandboxer ./sandbox.sh ./request.txt ; } ; }
 exit 0
