9ff5e3e61f3bb8f2c3f41e6fce34f75641d60dd4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-18 05:43:04 +0000

    Remove obsolete bin dir.

--- Web/bin/checksum.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ `uname` == "Linux" ]] ; then
-    md5sum $1 | cut -d ' ' -f 1
-elif [[ `uname` == "Darwin" ]] ; then
-    md5 $1 | cut -d ' ' -f 4
-else
-    echo "Invalid platform!" 1>&2
-fi
