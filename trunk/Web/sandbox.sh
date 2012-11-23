786cb311b309520171ef962eccaf67b31d6a4c10
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-23 19:23:42 +0000

    Update sandbox.

--- trunk/Web/sandbox.sh
+++ trunk/Web/sandbox.sh
@@ -1,8 +1,3 @@
-set -x
-MYPID=$$
-echo "MYPID: $MYPID"
-cp main.cpp /chroot/user/main.cpp
-cp build.sh /chroot/user/build.sh
-
-# Schedule timeout
-{ { sleep 5 && echo "Timeout" 1>&2 ; /bin/bash -c "kill -9 ${MYPID}" ; } & } && { trap "kill $!" INT ERR EXIT && { chroot /chroot bash coliru >output; } || { echo "User command was terminated." 1>&2 ; } ; }
+cp main.cpp /var/chroot/home/lol/main.cpp
+echo "cd /home/lol && bash main.cpp" >/var/chroot/home/lol/build.sh
+chroot /var/chroot bash /home/lol/build.sh
