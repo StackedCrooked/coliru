1732c902570ff60496d1810956aeae70f98c82b6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-24 00:48:31 +0000

    Improve checks.

--- trunk/Web/sandbox.sh
+++ trunk/Web/sandbox.sh
@@ -1,7 +1,11 @@
 NPROC_LIMIT=`ulimit -a 2>&1 | grep "\-u" | awk '{print $5}'`
-[ ${NPROC_LIMIT} -lt 200 ] || { echo "NPROC limit is too high: ${NPROC_LIMIT}. Exiting." >&2 ; exit 1 ; }
+[ ${NPROC_LIMIT} -lt 301 ] || { echo "NPROC limit is too high: ${NPROC_LIMIT}. Exiting." >&2 ; exit 1 ; }
 ulimit -t 1
 ulimit -n 20
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+[ -f main.cpp ] || { echo "No main.cpp found in ${DIR}." >&2 ; echo "Exiting." >&2 ; exit 1 ; }
 cp main.cpp /var/chroot/home/lol/main.cpp
 echo "cd /home/lol && bash main.cpp" >/var/chroot/home/lol/build.sh
 chroot /var/chroot bash /home/lol/build.sh
+
