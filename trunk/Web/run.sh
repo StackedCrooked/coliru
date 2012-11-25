ada26acdbbaa27abd6fcd2ed320c4e315f57d92a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-25 01:35:00 +0000

    Update scripts.

--- trunk/Web/run.sh
+++ trunk/Web/run.sh
@@ -1,1 +1,6 @@
-nohup ruby webserver.rb "10.0.2.15" 8080 &
+#!/bin/bash
+if [ "$(whoami)" != "webserver" ]; then
+	echo "For security reasons this script must be run by a user named \"webserver\"."
+	exit 1
+fi
+ruby webserver.rb "10.0.2.15" 8080
