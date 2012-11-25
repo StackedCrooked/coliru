a308bc9d6af26e2cd0860b0804e016d8b628b3df
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-25 01:35:00 +0000

    Update scripts.

--- Web/sandbox.sh
+++ Web/sandbox.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+if [ "$(whoami)" != "sandbox" ]; then
+	echo "For security reasons this script must be run by a user named \"webserver\"."
+	exit 1
+fi
 NPROC_LIMIT=`ulimit -a 2>&1 | grep "\-u" | awk '{print $5}'`
-[ ${NPROC_LIMIT} -lt 301 ] || { echo "NPROC limit is too high: ${NPROC_LIMIT}. Exiting." >&2 ; exit 1 ; }
+[ ${NPROC_LIMIT} -lt 20 ] || { echo "NPROC limit is too high: ${NPROC_LIMIT}. Exiting." >&2 ; exit 1 ; }
 ulimit -t 1
 ulimit -n 20
 
