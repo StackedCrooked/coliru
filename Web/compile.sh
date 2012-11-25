0c8e7c852ef8f09e8472c4020fc90a25559821e6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-25 03:38:33 +0000

    Fix smaller issues.

--- Web/compile.sh
+++ Web/compile.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+set -x
+ulimit -a
 ulimit -u 20
 ulimit -t 5
+ulimit -f 100
 cd /tmp
-g++-4.7 -o test -std=c++0x -Wall -Werror -Wextra -pedantic-errors -O2 -I/usr/include -I/usr/include -L/usr/lib -pthread main.cpp && ./test
+g++-4.7 -o test -std=c++0x -Wall -Werror -Wextra -pedantic-errors -O2 -I/usr/include -I/usr/include -L/usr/lib -pthread main.cpp && ./test >output
+cat output
--- Web/sandbox.sh
+++ Web/sandbox.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+ulimit -f 100
 set -e
 [ -f main.cpp ] || { echo "Webserver could not find main.cpp. Exiting." >2 ; exit 1; }
 cp compile.sh main.cpp /var/chroot/tmp
 
 # Run the chroot
-sudo -u sandbox chroot /var/chroot /tmp/compile.sh
+{ { sleep 1 && echo "Timeout!" && pkill -9 -u sandbox && exit 1 ; } & } && { trap "kill $!" INT ERR EXIT; ./chroot.sh ; exit 0 ; }
