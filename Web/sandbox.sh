21f1eeceab7440fb9bb22083fd4af974b2b4bae7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-25 02:35:54 +0000

    Run chroot as sandbox.

--- Web/sandbox.sh
+++ Web/sandbox.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-set -x
 set -e
 [ -f main.cpp ] || { echo "Webserver could not find main.cpp. Exiting." >2 ; exit 1; }
 cp compile.sh main.cpp /var/chroot/tmp
 
 # Run the chroot
-SANDBOX_UID=`id sandbox | cut -d ' ' -f1 | cut -d '=' -f2 | cut -d '(' -f1`
-SANDBOX_GID=`id sandbox | cut -d ' ' -f2 | cut -d '=' -f2 | cut -d '(' -f1`
-chroot --userspec=${SANDBOX_UID}:${SANDBOX_GID} /var/chroot /tmp/compile.sh
+sudo -u sandbox chroot /var/chroot /tmp/compile.sh
