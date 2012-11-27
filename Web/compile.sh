8945dbe599c6fa4e803946199c7371e05630d91f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-27 22:09:05 +0000

    Update sample program.

--- Web/compile.sh
+++ Web/compile.sh
@@ -4,5 +4,6 @@ ulimit -u 20
 ulimit -t 5
 ulimit -f 100
 cd /tmp
-g++-4.7 -o test -std=c++0x -Wall -Werror -Wextra -pedantic-errors -O2 -I/usr/include -I/usr/include -L/usr/lib -pthread main.cpp && ./test >output
-cat output
+rm -f .stdout
+g++-4.7 -o test -std=c++0x -Wall -Werror -Wextra -pedantic-errors -O2 -I/usr/include -I/usr/include -L/usr/lib -pthread main.cpp && ./test >.stdout
+[ ! -f .stdout ] || cat .stdout
