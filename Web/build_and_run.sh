e31dfd571d1709e4fb0852855c4d4bb199dde6d4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-28 22:04:24 +0000

    Fix scripts.

--- Web/build_and_run.sh
+++ Web/build_and_run.sh
@@ -7,5 +7,5 @@ set -e
 cp compile.sh main.cpp /var/chroot/tmp
 
 # Run the chroot
-{ { sleep 10 && echo "Timeout!" && { pkill -9 -u sandbox >/dev/null 2>&1  ; } && exit 1 ; } & } && { trap "kill $! >/dev/null 2>&1" INT ERR EXIT; { ./chroot.sh >"${DIR}/output" 2>&1 ; } ; echo ${ID} ;  exit 0 ; }
+{ { sleep 10 && echo "Timeout!" && { pkill -9 -u sandbox >/dev/null 2>&1  ; } && exit 1 ; } & } && { trap "kill $! >/dev/null 2>&1" INT ERR EXIT; ./chroot.sh ; exit 0 ; }
 
