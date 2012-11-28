e10f38aaa24da5d4a622077aebb35f6a102f89a6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-28 20:59:31 +0000

    Add share script.

---
+++ Web/share.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+mkdir -p Archive
+if [ "$(uname)" == "Darwin" ] ; then 
+    ID="$(md5 main.cpp | cut -d '=' -f 2 | sed -e 's/ //g')"
+else
+    ID="id_is_md5"
+fi
+
+if [ -d "Archive/${ID}" ] ; then
+    rm -rf "Archive/${ID}"
+fi
+
+DIR="Archive/${ID}"
+mkdir "${DIR}"
+
+
+if [ "$(uname)" == "Darwin" ] ; then 
+    cat main.cpp > "${DIR}/main.cpp"
+    CMD="g++ -std=c++0x -o test -pthread main.cpp"
+    ${CMD} > "${DIR}/output"
+    ./test  >> "${DIR}/output"
+    echo ${ID}
+    exit 0
+fi
