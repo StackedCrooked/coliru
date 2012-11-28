f7013c941f85d3d4261be2fabdb91c973e8a5045
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-28 21:42:39 +0000

    Update scripts.

--- Web/sandbox.sh
+++ Web/sandbox.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 if [ "$(uname)" == "Darwin" ] ; then 
+    ID="$(md5 main.cpp | cut -d '=' -f 2 | sed -e 's/ //g')"
+else
+    ID="$(md5sum main.cpp | cut -d ' ' -f 1)"
+fi
+
+if [ -f "Archive/${ID}/output" ] ; then
+    cat "Archive/${ID}/output"
+    exit
+fi
+
+
+if [ "$(uname)" == "Darwin" ] ; then 
     g++ -std=c++0x -o test -pthread main.cpp && ./test
     exit 0
 fi
