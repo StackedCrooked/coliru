650760f5c3860ce709478158a7fb0a2e7246a660
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-28 01:38:57 +0000

    Compiler also looks in /usr/local/include and /usr/local/lib for headers and libraries.

--- trunk/Web/compile.sh
+++ trunk/Web/compile.sh
@@ -3,5 +3,5 @@ ulimit -u 20
 ulimit -t 5
 cd /tmp
 rm -f .stdout
-g++-4.7 -o test -std=c++0x -Wall -Werror -Wextra -pedantic-errors -Wno-unused-variable -O2 -I/usr/include -I/usr/include -L/usr/lib -pthread main.cpp && ./test >.stdout
+g++-4.7 -o test -std=c++0x -Wall -Werror -Wextra -pedantic-errors -Wno-unused-variable -O2 -I/usr/include -I/usr/local/include -L/usr/lib -L/usr/local -pthread main.cpp && ./test >.stdout
 [ ! -f .stdout ] || cat .stdout
