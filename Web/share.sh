8ce30e588dd80c72e1dd30202205269306a3f471
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-28 21:08:10 +0000

    Update share.sh.

--- Web/share.sh
+++ Web/share.sh
@@ -3,7 +3,7 @@ mkdir -p Archive
 if [ "$(uname)" == "Darwin" ] ; then 
     ID="$(md5 main.cpp | cut -d '=' -f 2 | sed -e 's/ //g')"
 else
-    ID="id_is_md5"
+    ID="$(md5sum main.cpp | cut -d ' ' -f 1)"
 fi
 
 if [ -d "Archive/${ID}" ] ; then
@@ -14,12 +14,6 @@ fi
 DIR="Archive/${ID}"
 mkdir "${DIR}"
 
-
-if [ "$(uname)" == "Darwin" ] ; then 
-    cat main.cpp > "${DIR}/main.cpp"
-    CMD="g++ -std=c++0x -o test -pthread main.cpp"
-    ${CMD} > "${DIR}/output"
-    ./test  >> "${DIR}/output"
-    echo ${ID}
-    exit 0
-fi
+cat main.cpp > "${DIR}/main.cpp"
+./sandbox.sh >${DIR}/output 2>&1
+echo ${ID}
