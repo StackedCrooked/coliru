972f60b7cc80d3d23e83338717651124190fc9bd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-29 03:14:51 +0000

    Put chroot in env var.

--- trunk/Web/build_and_run.sh
+++ trunk/Web/build_and_run.sh
@@ -1,10 +1,13 @@
-
+#!/bin/bash
+if [ -z "${CHROOT}" ] ; then
+    echo "CHROOT environment variable is not set. Exiting." 1>&2
+fi
 
 ulimit -f 1000
-rm -rf /var/chroot/tmp/*
+rm -rf "${CHROOT}/*"
 set -e
 [ -f main.cpp ] || { echo "Webserver could not find main.cpp. Exiting." >2 ; exit 1; }
-cp compile.sh main.cpp /var/chroot/tmp
+cp compile.sh main.cpp "${CHROOT}/"
 
 # Run the chroot
 { { sleep 10 && echo "Timeout!" && { pkill -9 -u sandbox >/dev/null 2>&1  ; } && exit 1 ; } & } && { trap "kill $! >/dev/null 2>&1" INT ERR EXIT; ./chroot.sh ; exit 0 ; }
