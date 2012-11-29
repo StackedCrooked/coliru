6083978a161e7c8333fe4b9dcf855afbae51a750
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-29 22:36:46 +0000

    Update.

--- Web/run.sh
+++ Web/run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+pkill -9 -u webserver
+chown -R webserver:coliru /var/chroot/tmp/main.cpp 
+chown -R webserver:coliru /var/chroot/tmp/compile.sh 
 sudo -u webserver ruby webserver.rb stacked-crooked.com 80
