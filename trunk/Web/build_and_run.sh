b304f7d2efa4be41d225a31809dd6647b9d9ff63
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-11-29 17:57:38 +0000

    Fix redirect error.

--- trunk/Web/build_and_run.sh
+++ trunk/Web/build_and_run.sh
@@ -6,7 +6,7 @@ fi
 ulimit -f 1000
 rm -rf "${CHROOT}/*"
 set -e
-[ -f main.cpp ] || { echo "Webserver could not find main.cpp. Exiting." >2 ; exit 1; }
+[ -f main.cpp ] || { echo "Webserver could not find main.cpp. Exiting." 1>&2 ; exit 1; }
 cp compile.sh main.cpp "${CHROOT}/"
 
 # Run the chroot
