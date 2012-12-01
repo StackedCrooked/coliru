dabe85ea14a1883d8c0a38ae9de04d6411c5df6b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-12-01 02:01:37 +0000

    Add simple program that contains backslashes.

---
+++ TestSuite/Escaping/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Tab: \t" << std::endl;
+    std::cout << "Backslash: \\" << std::endl;
+    std::cout << "Backslash followed by t: \\t" << std::endl;
+}
