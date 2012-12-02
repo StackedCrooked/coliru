6d9dda8190401ff401a0043b5d5d78f42b1ce410
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-12-02 04:36:54 +0000

    Restore old code.

--- Web/build_and_run.sh
+++ Web/build_and_run.sh
@@ -2,10 +2,11 @@
 export CHROOT="/var/chroot"
 
 ulimit -f 1000
-rm -rf "${CHROOT}/*"
+rm -rf "${CHROOT}/tmp"
+mkdir -p "${CHROOT}/tmp"
 set -e
 [ -f main.cpp ] || { echo "Webserver could not find main.cpp. Exiting." 1>&2 ; exit 1; }
-cp compile.sh main.cpp "${CHROOT}/tmp"
+cp compile.sh main.cpp "${CHROOT}/tmp/"
 
 # Run the chroot
 { { sleep 10 && echo "Timeout!" && { pkill -9 -u sandbox >/dev/null 2>&1  ; } && exit 1 ; } & } && { trap "kill $! >/dev/null 2>&1" INT ERR EXIT; ./chroot.sh ; exit 0 ; }
