7bb6db4faa38eaccb2ba264bc6dbe32069e11628
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-12-02 10:09:48 +0000

    Ensure that compile.sh is executable in the chroot.

--- trunk/Web/build_and_run.sh
+++ trunk/Web/build_and_run.sh
@@ -6,6 +6,7 @@ set -e
 ulimit -f 1000
 [ -f main.cpp ] || { echo "Webserver could not find main.cpp. Exiting." 1>&2 ; exit 1; }
 cp compile.sh main.cpp "${CHROOT}/tmp/"
+chmod 755 "${CHROOT}/tmp/compile.sh"
 
 # Run the chroot
 { { sleep 10 && echo "Timeout!" && { pkill -9 -u sandbox >/dev/null 2>&1  ; } && exit 1 ; } & } && { trap "kill $! >/dev/null 2>&1" INT ERR EXIT; ./chroot.sh ; exit 0 ; }
