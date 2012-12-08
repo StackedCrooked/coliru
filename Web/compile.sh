f0715081cab42debd594a57e687fc3e2b68f05e3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-12-08 17:49:37 +0000

    Link with poco and tbb.

--- Web/compile.sh
+++ Web/compile.sh
@@ -3,5 +3,5 @@ ulimit -u 20
 ulimit -t 5
 cd /tmp
 rm -f .stdout
-g++-4.7 -o test -std=c++0x -Wall -Wextra -pedantic-errors -Wno-unused-variable -O2 -I/usr/include -I/usr/local/include -L/usr/lib -L/usr/local -pthread main.cpp && ./test >.stdout
+g++-4.7 -o test -std=c++0x -Wall -Wextra -pedantic-errors -Wno-unused-variable -O2 -I/usr/include -I/usr/local/include -L/usr/lib -L/usr/local main.cpp -pthread -lPocoFoundation -ltbb && ./test >.stdout
 [ ! -f .stdout ] || cat .stdout
