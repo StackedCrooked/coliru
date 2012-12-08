843d4b51e98d7a2c5689b8823c5dfcab4ab04440
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-12-08 20:38:21 +0000

    Update Archive.

--- trunk/Web/Archive/committer.sh
+++ trunk/Web/Archive/committer.sh
@@ -1,1 +1,1 @@
-while true ; do svn add * 2>/dev/null ; svn ci -m "Update Archive. ${RANDOM}" ; sleep 10 ; done
+while true ; do svn add * 2>/dev/null ; svn ci -m "Update Archive. ${RANDOM}" ; sleep 60 ; done
