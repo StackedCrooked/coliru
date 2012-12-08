968f4c0ddf1b8647b63d0332c3ec372047f81a43
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-12-08 21:59:56 +0000

    Update Archive.

---
+++ Web/Archive/12abbdca093f9edab76e925fa4030ef7/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <memory>
+#include <tuple>
+ 
+ 
+struct Dynamic
+{
+    virtual ~Dynamic() {}
+};
+ 
+template<typename A, typename B>
+struct Generic : Dynamic
+{
+    Generic()
+    {
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
+};
+ 
+ 
+template<typename A, typename B>
+std::unique_ptr<Dynamic> Make()
+{
+    return std::unique_ptr<Dynamic>(new Generic<A, B>());
+}
+ 
+ 
+template<typename A>
+std::unique_ptr<Dynamic> Make(char b)
+{
+    switch (b)
+    {
+        case 'i':
+        {
+            return Make<A, int>();
+        }
+        case 'u':
+        {
+            return Make<A, unsigned>();
+        }
+        default: throw 0;
+    }
+}
+ 
+ 
+std::unique_ptr<Dynamic> Make(char a, char b)
+{
+    switch (a)
+    {
+        case 'i':
+        {
+            return Make<int>(b);
+        }
+        case 'u':
+        {
+            return Make<unsigned>(b);
+        }
+        default: throw 0;
+    }
+}
+ 
+ 
+int main()
+{
+    Make('i', 'i');
+    Make('i', 'u');
+    Make('u', 'i');
+    Make('i', 'i');
+}
