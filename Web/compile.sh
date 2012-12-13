95a86d6ec898c9238976732845acdd8073207379
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-12-13 21:13:11 +0000

    Timeout is 20 seconds now.

--- Web/compile.sh
+++ Web/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ulimit -u 20
-ulimit -t 10
+ulimit -t 20
 cd /tmp
 rm -f .stdout
 g++-4.7 -o test -std=c++0x -Wall -Wextra -pedantic-errors -Wno-unused-variable -O2 -I/usr/include -I/usr/local/include -L/usr/lib -L/usr/local main.cpp -pthread -lPocoFoundation -ltbb && ./test >.stdout
