f6d8a2eec92dbbc540bed7349da279fe21dd25ff
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-12-13 21:09:58 +0000

    Webserver is restarted on timeout. Cleanup all defunct processes.

--- trunk/Web/repair-permissions.sh
+++ trunk/Web/repair-permissions.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-echo "$(whoami)"
+echo "$0 executed by $(whoami)"
 
-chown webserver:coliru .
+chown -R webserver:coliru .
+chmod a+rw .
+
+chown -R webserver:coliru .
 chmod a+rw .
 
 find /var/chroot/tmp -type f | xargs -I {} rm -f {} || sudo -u sandbox find /var/chroot/tmp -type f | xargs -I {} rm -f {}
 chmod -R a+rw /var/chroot/tmp >/dev/null 2>&1 || sudo -u sandbox chmod -R a+rw /var/chroot/tmp >/dev/null 2>&1
 
 echo "Owner of /var/chroot/tmp is $(ls -alt /var/chroot/tmp | head -n2 | tail -n1)"
-sudo -u sandbox rm -rf /var/chroot/tmp
+rm -rf /var/chroot/tmp
 mkdir -p /var/chroot/tmp
+chown -R webserver:coliru /var/chroot/tmp
+chmod a+rw /var/chroot/tmp
 
 touch ./output
 chown webserver:coliru ./output
