1be5f655443fd3c201ee4631857f267d11f414de
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-12-13 19:29:31 +0000

    Don't show cursor in view mode. File limit increased to 10000.

--- Web/build_and_run.sh
+++ Web/build_and_run.sh
@@ -3,7 +3,7 @@ export CHROOT="/var/chroot"
 
 rm -rf "${CHROOT}/tmp/*"
 set -e
-ulimit -f 1000
+ulimit -f 10000
 [ -f main.cpp ] || { echo "Webserver could not find main.cpp. Exiting." 1>&2 ; exit 1; }
 cp compile.sh main.cpp "${CHROOT}/tmp/"
 chmod 755 "${CHROOT}/tmp/compile.sh"
