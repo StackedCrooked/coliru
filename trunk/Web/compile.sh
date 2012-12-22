e1149656748669d56028aa19ddabf8f7856faa22
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-12-22 00:33:50 +0000

    Add Google analytics.

--- trunk/Web/compile.sh
+++ trunk/Web/compile.sh
@@ -3,5 +3,5 @@ ulimit -u 20
 ulimit -t 20
 cd /tmp
 rm -f .stdout
-g++-4.7 -o test -std=c++0x -Wall -Wextra -pedantic-errors -Wno-unused-variable -O2 -I/usr/include -I/usr/local/include -L/usr/lib -L/usr/local main.cpp -pthread -lPocoFoundation -ltbb && ./test >.stdout
+g++-4.7 -o test -std=c++0x -Wall -Wextra -pedantic-errors -Wno-unused-variable -O2 -g3 -I/usr/include -I/usr/local/include -L/usr/lib -L/usr/local main.cpp -pthread -lPocoFoundation -ltbb && ./test >.stdout
 [ ! -f .stdout ] || cat .stdout
