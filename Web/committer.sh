41bbd7c64b972e69b30ef8e4818d6409cf31ff66
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-12-22 00:33:50 +0000

    Add Google analytics.

--- Web/committer.sh
+++ Web/committer.sh
@@ -1,1 +1,1 @@
-while true ; do ls -tc1 Archive | xargs -I {} svn add {} ; svn ci Archive -m "Update archive" ; sleep 3600 ; done
+while true ; do ls -tc1 Archive | xargs -I {} svn add Archive/{} ; svn ci Archive -m "Update archive" ; sleep 3600 ; done
