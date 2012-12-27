852b524022762164f0a005fd036833298e0bedf3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-12-27 20:35:02 +0000

    Update archive

---
+++ trunk/Web/Archive/00e3fbf1cc6d56a1cc887095af313e73/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <unistd.h>
+
+constexpr int something_expensive(int x) { return x; }
+
+int process(int x)
+{
+    switch (x)
+    {
+        // hard-coded for common cases
+        case 0: return something_expensive(0);
+        case 1: return something_expensive(2);
+        case 2: return something_expensive(2);
+        
+        // default is non-optimized
+        default: return something_expensive(x);
+    };
+}
+
+int main()
+{
+    std::cout << process(0) << std::endl;
+}
