10c05c61ce84aa6887528ba59116f944992a2652
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-12-28 19:40:11 +0000

    Update archive

---
+++ trunk/Web/Archive/bbc8e0af173435186cf3697f217b2892/main.cpp
@@ -0,0 +1,34 @@
+/**
+ * Problem 9
+ * ---------
+ * A Pythagorean triplet is a set of three natural numbers, a  b  c, for which:
+ *   a^2 + b^2 = c^2
+ *
+ * For example: 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+ *
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ */
+
+
+#include <iostream>
+
+
+int main()
+{
+    for (int a = 1; a <= 1000; ++a)
+    {
+        for (int b = 1; b <= (1000 - a); ++b)
+        {
+            int c = 1000 - a - b;
+            if (a*a + b*b == c*c)
+            {
+                std::cout << (a*b*c) << std::endl;
+                return 0;
+            }
+        }
+    }
+
+    std::cout << "Not found." << std::endl;
+    return 1;
+}
