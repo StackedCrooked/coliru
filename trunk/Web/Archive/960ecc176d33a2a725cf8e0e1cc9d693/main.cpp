7e937d56eb412ff38ccc290cfc0fe17db020d32b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-12-29 02:43:08 +0000

    Update archive

---
+++ trunk/Web/Archive/960ecc176d33a2a725cf8e0e1cc9d693/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <stdint.h>
+
+bool is_prime(uint64_t n, const std::vector<uint64_t> & preceding)
+{
+    // precondition: "preceding" contains all primes < n
+
+    for (auto p : preceding)
+    {
+        if (n % p == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+uint64_t next_prime(const std::vector<uint64_t> & preceding)
+{
+    if (preceding.empty())
+    {
+        return 2;
+    }
+
+    if (preceding.back() == 2)
+    {
+        return 3;
+    }
+
+    for (uint64_t n = preceding.back() + 2; ; n += 2)
+    {
+        if (is_prime(n, preceding))
+        {
+            return n;
+        }
+    }
+}
+
+int main()
+{
+    std::vector<uint64_t> primes;
+    while (primes.size() < 10001)
+    {
+        primes.push_back(next_prime(primes));
+    }
+    std::cout << primes.back() << std::endl;
+}
