185bef53a187e308eedbb6cf00b786bd9f96d973
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2012-12-29 03:11:09 +0000

    Compile with -std=c++11 instead of -std=c++0x.

--- trunk/Web/compile.sh
+++ trunk/Web/compile.sh
@@ -3,5 +3,5 @@ ulimit -u 20
 ulimit -t 20
 cd /tmp
 rm -f .stdout
-time g++-4.7 -o test -std=c++0x -Wall -Wextra -pedantic-errors -Wno-unused-variable -O2 -g3 -I/usr/include -I/usr/local/include -L/usr/lib -L/usr/local main.cpp -pthread -lPocoFoundation -ltbb && ./test >.stdout
+g++-4.7 -o test -std=c++11 -Wall -Wextra -pedantic-errors -Wno-unused-variable -O2 -g3 -I/usr/include -I/usr/local/include -L/usr/lib -L/usr/local main.cpp -pthread -lPocoFoundation -ltbb && time ./test >.stdout
 [ ! -f .stdout ] || cat .stdout
