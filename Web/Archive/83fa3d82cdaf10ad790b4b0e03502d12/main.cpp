3f99e30218b15ee97f8d6dc5604d286a56b2126e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-01-02 04:13:51 +0000

    Update archive

---
+++ Web/Archive/83fa3d82cdaf10ad790b4b0e03502d12/main.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <initializer_list>
+#include <iostream>
+
+unsigned product(std::initializer_list<unsigned> factors);
+
+template<typename ...T> unsigned product(T ...t)
+{
+    return product( { t... });
+}
+
+int main()
+{
+    std::cout << product(2u) << std::endl;
+    std::cout << product(2u, 3u) << std::endl;
+    std::cout << product(2u, 3u, 5u) << std::endl;
+    std::cout << product(2u, 3u, 5u, 7u) << std::endl;
+}
