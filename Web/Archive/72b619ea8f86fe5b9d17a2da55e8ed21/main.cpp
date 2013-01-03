5a0abafb3dbc6265fea15fb21cf57125233cde10
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-01-03 13:26:40 +0000

    Update archive

---
+++ Web/Archive/72b619ea8f86fe5b9d17a2da55e8ed21/main.cpp
@@ -0,0 +1,101 @@
+#include <boost/algorithm/string.hpp>
+#include <boost/variant.hpp>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <vector>
+
+
+template<typename R, typename A>
+struct Visitor : boost::static_visitor<>
+{
+    Visitor(const A & a) : a(a) {}
+    
+    template<typename F>
+    void operator()(const F & f)
+    {
+        r = std::move(f(a));
+    }
+    
+    const A & a;
+    R r;
+};
+
+
+template<typename R, typename Variant, typename A>
+R visit(Variant & var, const A & a)
+{
+    Visitor<R, A> visitor{a};
+    var.apply_visitor(visitor);
+    return visitor.r;
+}
+
+
+struct Void {}; // for Void return type
+
+
+//
+// Example
+//
+struct uppercase
+{    
+    std::string operator()(std::string s) const
+    {
+        boost::to_upper(s);
+        return s;
+    }
+};
+
+
+struct lowercase
+{    
+    std::string operator()(std::string s) const
+    {
+        boost::to_lower(s);
+        return s;
+    }
+};
+
+
+struct decorate_with_stars
+{
+    std::string operator()(std::string s) const
+    {
+        return "*** " + s + " ***";
+    }
+};
+
+
+struct decorate_not
+{
+    const std::string & operator()(const std::string & s) const
+    {
+        return s;
+    }
+};
+
+
+struct Formatter 
+{
+    template<typename Casing, typename Decoration>
+    Formatter(const Casing & casing, const Decoration & dec) :
+        casing(casing),
+        decoration(dec)
+    {
+    }
+    
+    std::string format(const std::string & s)
+    {
+        return visit<std::string>(decoration, visit<std::string>(casing, s));
+    }
+    
+    boost::variant<uppercase, lowercase> casing;
+    boost::variant<decorate_with_stars, decorate_not> decoration;
+};
+
+int main()
+{
+    Formatter formatter(uppercase{}, decorate_with_stars{});
+    std::cout << formatter.format("Hello World!") << std::endl;
+}
