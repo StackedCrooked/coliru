b6c662cb69d6fa804cc14354d1a1963bf4717d87
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-01-08 21:10:17 +0000

    Update archive

---
+++ trunk/Web/Archive/bf664afb00d14260d7e2f1f1cd364e27/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+
+int main()
+{
+        int numArray[] ={1,4,5,10,11,12,13,14,15,16,17,18,19,20,35,26,43,15,48,69,32,45,57,98,100};
+        const int N = sizeof(numArray)/ sizeof(*numArray);
+        
+        int key = 7;
+        auto cmp = [&](int a, int b) { return std::abs(key-a) < std::abs(key-b); };
+        int nearest_value = *std::min_element(numArray, numArray + N, cmp);
+        std::cout << nearest_value << std::endl;
+}
