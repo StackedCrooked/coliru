5635c1bc9ca40cf41b786b40a4a6c1cacf171622
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-01-08 15:08:04 +0000

    Update archive

---
+++ trunk/Web/Archive/2e873bde821d1e06beec6e131c9e0b7b/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+template<typename T>
+struct wrap
+{
+    wrap(const T & t) : t(t) {}
+    const T & t;
+};
+
+int main()
+{
+    typedef char c17[17];
+    const c17 & ref = c17{};
+    std::cout << sizeof(ref) << std::endl;
+    std::cout << sizeof(wrap<c17>(ref)) << std::endl;
+}
