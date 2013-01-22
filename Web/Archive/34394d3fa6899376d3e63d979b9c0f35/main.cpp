be07c2ccea69e6b0bd3615f41e6637a4415218af
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-01-22 09:11:52 +0000

    Update archive

---
+++ Web/Archive/34394d3fa6899376d3e63d979b9c0f35/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+namespace simple
+{
+    struct Proba_t
+    {
+        int a;
+        double b;
+    };
+
+    inline void function( Proba_t pt )
+    {
+        using std::cout;
+        using std::endl;
+        cout << "Proba_t.a : " << pt.a << endl <<
+            "Proba_t.b : " << pt.b << endl;
+    }
+}
+
+
+int main()
+{
+    simple::Proba_t ppt = { 5,4.4 };
+
+    // Nota bene : Typical call this functions is similar simple::function( ppt );
+    function( ppt );
+
+    return EXIT_SUCCESS;
+}
