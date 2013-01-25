b397e5dbf41ae88cf71ad0bb63e9fbd22095646c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-01-25 12:45:41 +0000

    Update archive

---
+++ Web/Archive/a37e6d6e79dd6fb14ac42ed1b823d630/main.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+
+class base
+{
+public:
+    virtual void f1()
+    {
+        std::cout << "base::f1()" << std::endl;
+    }
+};
+
+class child : public base {};
+
+void show_obj( base * pb )
+{
+    child * pc = (child*)pb;
+    pc->f1();
+}
+
+
+int main()
+{
+    child c(5);
+    show_obj( &c );
+    return EXIT_SUCCESS;
+}
+
+class child : public base
+{
+public:
+    child( int a )
+        : a_(a)
+    {
+    }
+
+    virtual void f1()
+    {
+        std::cout << "child::f1()" << std::endl;
+        std::cout << "a: " << a_ << std::endl;
+    }
+private:
+    int a_;
+};
