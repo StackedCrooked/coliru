30bafa0d05f583467a0e47a2d99df85727fd242c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-01-26 11:04:47 +0000

    Update archive

---
+++ Web/Archive/98f6b1254319f79eceb7f0d648679251/main.cpp
@@ -0,0 +1,61 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+std::string extract(const std::string & input, char delim, char escape)
+{
+    enum class State
+	{
+		parsing,
+		escaped
+	};
+
+	State state = State::parsing;
+
+	std::string found = "";
+	std::string parsed = "";
+
+	for (auto && c : input)
+	{
+		if (state == State::parsing)
+		{
+			if (c == delim)
+			{
+				found.append(parsed);
+				parsed.clear();
+			}
+			else if (c == escape)
+			{
+				state = State::escaped;
+			}
+			else
+			{
+				parsed += c;
+			}
+		}
+		else
+		{
+			assert(state == State::escaped);
+			parsed += c;
+			state = State::parsing;
+		}
+	}
+
+	if (!parsed.empty())
+	{
+		found.append(parsed);
+	}
+
+	return found;
+}
+
+
+int main()
+{
+	std::cout << extract("tab	bat", '	', '\\') << std::endl;
+	std::cout << extract("tab\tbat", '	', '\\') << std::endl;
+	std::cout << extract("tab\\	bat", '	', '\\') << std::endl;
+}
+
