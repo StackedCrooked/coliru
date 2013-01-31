e614c407966d70d768aaa5971bb5752c8c6ec9a2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-01-31 12:59:32 +0000

    Update archive

---
+++ Web/Archive/c01e1d63331173ecdc00a43d522f10fa/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+template<typename T>
+struct lowercase_char_traits : std::char_traits<T>
+{
+    static T* copy(T* dest, const T* src, std::size_t count )
+    {
+         for(size_t i = 0 ; i < count ; ++i)
+              dest[i] = std::tolower(src[i]);
+         return dest;
+    }
+};
+
+typedef std::basic_string<char, lowercase_char_traits<char>> lowercase;
+
+int main() 
+{
+    lowercase s = "Hello World";
+    std::cout << s.c_str() << std::endl;
+}
