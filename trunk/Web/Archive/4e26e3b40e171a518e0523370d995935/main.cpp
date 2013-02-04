c2428d43e9adac2233e94c580fb478a1bc6fa79e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-02-04 09:44:27 +0000

    Update archive

---
+++ trunk/Web/Archive/4e26e3b40e171a518e0523370d995935/main.cpp
@@ -0,0 +1,42 @@
+#include <limits>
+#include <iostream>
+#include <memory>
+#include <functional>
+
+using std::cout;
+using std::endl;
+
+//I need to generate a callback to the appropriate function
+//paramaters are passed either by value or by const&
+
+void OverloadedFunction(float f) { cout<<f<<endl; }
+void OverloadedFunction(const std::string& s) { cout<<s<<endl; }
+void OverloadedFunction(float f, const float& g) { cout<<f<<' '<<g<<endl; } //bonus points if this works too
+
+template<typename... Args>
+std::function<void()> GenerateCallback(Args... args)
+{
+    return std::bind((void(*)(Args...))(&OverloadedFunction), args...);
+}
+
+int main()
+{    
+	auto f1 = GenerateCallback(42.f);
+    auto f2 = GenerateCallback(std::string("bibi"));
+    
+    float g = 43.f;
+    std::string m("mimi");
+    
+    auto f3 = GenerateCallback(g);
+    auto f4 = GenerateCallback(m);
+    
+    float h = 44.f;
+    
+    auto f5 = GenerateCallback(g, h);
+    
+    f1();
+    f2();
+    f3();
+    f4();
+    f5();
+}
