dd6b240fcf31620c9e9595db3958ccb025e001c1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-02-05 00:53:55 +0000

    Update archive

---
+++ Web/Archive/7238c0c5d231702990facd2594ba82a0/main.cpp
@@ -0,0 +1,34 @@
+#include <boost/phoenix/core.hpp>
+#include <boost/phoenix/operator.hpp>
+#include <boost/phoenix/function.hpp>
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+
+struct flip_impl
+{
+    typedef std::string result_type;
+    
+    std::string operator()(std::string arg) const
+    {
+        using std::begin;
+        using std::end;
+        std::reverse(begin(arg), end(arg));
+        return arg;
+    }
+};
+
+
+boost::phoenix::function<flip_impl> flip;
+
+boost::phoenix::expression::argument<1>::type arg1;
+auto flip2 = flip(flip(arg1));
+
+
+int main()
+{
+    std::string abc = "abc";
+    std::cout << flip(abc)() << std::endl;
+    std::cout << flip2(abc) << std::endl;
+}
---
+++ Web/Archive/eaf12ef76db724aedaf45fc6edf57257/main.cpp
@@ -0,0 +1,33 @@
+#include <boost/phoenix/core.hpp>
+#include <boost/phoenix/operator.hpp>
+#include <boost/phoenix/function.hpp>
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+
+boost::phoenix::expression::argument<1>::type arg1;
+struct flip_impl
+{
+    typedef std::string result_type;
+    
+    std::string operator()(std::string arg) const
+    {
+        using std::begin;
+        using std::end;
+        std::reverse(begin(arg), end(arg));
+        return arg;
+    }
+};
+
+
+boost::phoenix::function<flip_impl> flip;
+auto flip2 = flip(flip);
+
+
+int main()
+{
+    std::string abc = "abc";
+    std::cout << flip(abc)() << std::endl;
+    std::cout << flip2(abc)() << std::endl;
+}
