87b42f03addcc28872c00cbc270508523edb1881
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-02-05 22:01:10 +0000

    Update archive

---
+++ Web/Archive/be3f625fd1dae40c8073de8ddf9aaf0d/main.cpp
@@ -0,0 +1,12 @@
+#include <cassert>
+#include <iostream>
+
+class final { };
+
+struct countdown {
+    countdown(int i = 10) { assert(i); std::cout << i << std::endl; new countdown(i - 1); }
+};
+
+int main() {
+    final countdown();
+}
