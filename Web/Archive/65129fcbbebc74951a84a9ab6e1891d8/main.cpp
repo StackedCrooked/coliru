6a182ec0804c374cfb7059beba5b8cb33400549a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-02-05 11:01:07 +0000

    Update archive

---
+++ Web/Archive/65129fcbbebc74951a84a9ab6e1891d8/main.cpp
@@ -0,0 +1,33 @@
+#include <boost/coroutine/all.hpp>
+#include <string>
+#include <vector>
+
+
+typedef boost::coroutines::coroutine<int(char)> Coroutine;
+
+
+void f(Coroutine::caller_type & ca)
+{
+    std::vector<int> vec = {1, 2, 3};
+    for (int i : vec)
+    {
+        char c = ca.get();
+        std::cout << "c: " << c << std::endl;
+        ca(i);
+    }
+}
+
+int main()
+{
+    Coroutine theCoroutine(f);
+    std::string str("abc");
+    for (char c : str)
+    {
+        std::cout << c << std::flush;
+        theCoroutine(c);
+        int n = theCoroutine.get();
+        std::cout << n << std::endl;        
+    }
+}
+
+
