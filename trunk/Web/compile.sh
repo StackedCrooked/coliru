00695c7c6609960819b0b61bee59eb2f626ce29d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-02-06 06:25:47 +0000

    - Silent committer. - Link with boost_thread. - LD_LIBRARY_PATH is set by compile.sh (ldconfig is not available in chroot).

--- trunk/Web/compile.sh
+++ trunk/Web/compile.sh
@@ -3,5 +3,6 @@ ulimit -u 20
 ulimit -t 20
 cd /tmp
 rm -f .stdout
-g++-4.7 -o test -std=c++11 -Wall -Wextra -pedantic-errors -O2 -g -I/usr/include -I/usr/local/include -L/usr/lib -L/usr/local/lib main.cpp -pthread -lPocoFoundation -ltbb && ./test >.stdout
+export LD_LIBRARY_PATH="/usr/local/lib:${LD_LIBRARY_PATH}"
+g++-4.7 -o test -std=c++11 -Wall -Wextra -pedantic-errors -O2 -g -I/usr/include -I/usr/local/include -L/usr/lib -L/usr/local/lib main.cpp -pthread -lPocoFoundation -ltbb -lboost_thread -lboost_system && ./test >.stdout
 [ ! -f .stdout ] || cat .stdout
