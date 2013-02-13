aebb31b466511970ec669ab6dd033cb7300d3ad2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-02-13 16:01:09 +0000

    Update archive

---
+++ trunk/Web/Archive/83a76cc41177793c701bab9677855ede/main.cpp
@@ -0,0 +1,15 @@
+
+# include <iostream>
+# include <boost/multiprecision/cpp_dec_float.hpp>
+# include <boost/math/constants/constants.hpp>
+
+using namespace std;
+using namespace boost::math::constants;
+using namespace boost::multiprecision;
+
+int main()
+{
+    cout << setprecision(1001)
+		 << pi<number<cpp_dec_float<1000>>>()
+		 << '\n';
+}
