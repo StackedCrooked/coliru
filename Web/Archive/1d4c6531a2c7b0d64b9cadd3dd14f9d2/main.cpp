dfd631568abf7620ec0c2ffad9725b6a35a132a2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-02-14 16:15:37 +0000

    Update archive

---
+++ Web/Archive/1d4c6531a2c7b0d64b9cadd3dd14f9d2/main.cpp
@@ -0,0 +1,22 @@
+#include <boost/optional.hpp>
+#include <cstddef>
+#include <utility>
+
+template<class T>
+struct nullable_pointer{
+  nullable_pointer() : value(boost::none) {}
+    
+  nullable_pointer(std::nullptr_t) : value(boost::none) {}
+    
+  template<class U>
+  nullable_pointer(U&& u) : value(std::forward<U>(u)) {}
+  
+  nullable_pointer& operator=(std::nullptr_t){ value = boost::none; return *this; }
+    
+  bool operator==(nullable_pointer const& other) const{ return value == other.value; }
+  bool operator!=(nullable_pointer const& other) const{ return value != other.value; }
+  
+  explicit operator bool() const{ return value; }
+    
+  boost::optional<T> value;  
+};
