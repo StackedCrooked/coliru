278cced0f58fb474bce04c8960602191ebcec67a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-02-15 20:51:07 +0000

    Introduce environment variables for path.

--- trunk/Web/_run.sh
+++ trunk/Web/_run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+COLIRU=${COLIRU:-${HOME}/coliru}
+COLIRU_ARCHIVE=${COLIRU_ARCHIVE:-${COLIRU}/Archive}
+echo "Overridable variables related to COLIRU:"
+set | grep COLIRU
+
 set -x
 cd "$(dirname $0)"
 pkill -9 -u sandbox
--- trunk/Web/committer.sh
+++ trunk/Web/committer.sh
@@ -1,1 +1,1 @@
-while true ; do ls -tc1 Archive | xargs -I {} svn add -q Archive/{} ; svn ci Archive -m "Update archive" ; sleep 3600 ; done
+while true ; do ls -tc1 ${COLIRU_ARCHIVE} | xargs -I {} svn add -q ${COLIRU_ARCHIVE}/{} ; svn ci ${COLIRU_ARCHIVE} -m "Update archive" ; sleep 3600 ; done
