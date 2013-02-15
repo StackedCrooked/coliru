93b0d5767fc1a9c2da02f6d760e73df8a4f414ba
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-02-15 23:58:14 +0000

    Update archive

---
+++ trunk/Archive/0b8f1d4956e089dc552e360b45b8a5aa/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+bool add(std::string& animal, int number)
+{std::cout << "add\n"; return true;}
+bool del(std::string& animal)
+{std::cout << "del\n"; return true;}
+
+int main() {
+    char command;
+    std::string animal;
+    int number;
+    bool valid = true;
+    while(std::cin >> command && valid) { 
+        switch(command) {
+        case 'A': 
+            if (std::cin >> animal >> number)
+                valid = add(animal, number);
+            break;
+        case 'D':
+            if (std::cin >> animal)
+                valid = del(animal);
+            break;
+        default:
+            std::cout << "invalid command\n";
+            valid = false;
+        }
+    }
+    return 0;
+}
