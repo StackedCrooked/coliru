c02c668a25a427275f444e5a6d1be8c550561eeb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-02-15 21:28:10 +0000

    Kill kill-all.sh.

--- Web/kill-all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in `seq 4` ; do 
-    echo "Iteration $i"
-    ps -ef | grep ^200[12] | grep -v grep |  awk '{print $2}' | xargs -I {} bash -c "echo Killing {} && kill -9 {}"
-    ps -ef | grep run.sh   | grep -v grep |  awk '{print $2}' | xargs -I {} bash -c "echo Killing {} && kill -9 {}"
-done
