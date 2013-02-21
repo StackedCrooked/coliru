635657642dadcb1ec597f9dce4281bf2e45fa205
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-02-21 08:19:25 +0000

    Update archive

---
+++ trunk/Archive/496d205f17417ef3cbbcc1d5ce6d1cad/main.cpp
@@ -0,0 +1,19 @@
+#include <string>
+
+template<typename T, unsigned>
+struct Wrap
+{
+    template<typename ...Args>
+    explicit Wrap(Args && ...args) : obj_(std::forward<Args>(args)...) {}
+
+    operator T() const { return obj_; }
+
+    T obj_;
+};
+
+int main()
+{
+    Wrap<std::string, 0u> first = "John";
+    Wrap<std::string, 1u> last = "Smith";
+    last = first;
+}
