6a46f1fc3c6d1b4b73d0c425e9656ab4e50153b5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-02-25 02:57:21 +0000

    Update archive

---
+++ trunk/Archive/bde0a10d24e45fa830fb143ad13d407a/main.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include <tuple>
+using namespace std;
+
+class A
+{
+public:
+    template <class First, class... Funcs>
+    auto operator()(First first, Funcs... funcs) -> decltype((make_tuple(first(),funcs()...)))
+    {
+        return (make_tuple(first(),funcs()...)); //{ first(),funcs() ... } ;
+    }
+};
+
+int main(){
+    auto func1 = [](){ cout << 1 << endl; return 1;};
+    auto func2 = [](){ cout << 2 << endl; return 2;};
+
+    A a;
+    auto x = a(func1, func2);
+    cout << "ans : " << get<0>(x) << get<1>(x) << endl; // I expect ans : 12
+}
