4e8df47f56c6e01c956b8a2b51c9487b14567a05
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-02-28 20:20:56 +0000

    Update archive

---
+++ Archive/50bcab47a5b513ecf9a461f8c0b692ea/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <thread>
+
+int main()
+{
+    std::cout << "Hello GCC " << __VERSION__ << "!" << std::endl;
+    std::thread t([](){ std::cout << "Hello Thread!"; });
+    t.join();
+}asdfasdf
+
+
+asdf
+
+
+asdfasfaasdfasdfa
+asdf
