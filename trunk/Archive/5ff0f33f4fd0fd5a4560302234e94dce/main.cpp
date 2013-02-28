6fdc71ec589fb05f845bbc7681c91dfcb6fb55f8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-02-28 08:48:00 +0000

    Update archive

---
+++ trunk/Archive/5ff0f33f4fd0fd5a4560302234e94dce/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+
+#define ASSERT_TWO(a, b)  { constexpr int x = 1 / (reinterpret_cast<std::ptrdiff_t>(&a) - reinterpret_cast<std::ptrdiff_t>(&b)); (void)x; }
+
+int *global;
+
+int main()
+{
+    int *local;
+    
+    ASSERT_TWO(global, global);
+    ASSERT_TWO(local, local);
+}
