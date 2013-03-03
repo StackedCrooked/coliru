bfdff0a6abf1976e69e3599305d3265b3c28d7c4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-03-03 00:52:20 +0000

    Update archive

---
+++ Archive/ea78fb10152f2fae7d819ae161d8ecc3-18aa934a8d82d638dde2147aa94cac94/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> v;
+    for (auto i = 0; i != 10000000; ++i)
+    {
+        v.push_back(i);
+    }
+    return v.size();
+}//aaaaaaaaaaa
