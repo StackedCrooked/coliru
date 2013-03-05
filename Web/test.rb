f0da5f8ac83d3b9ca69c4a3c8b6bab794e249155
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-03-05 19:59:34 +0000

    add test.rb safe timeout experiment.

---
+++ Web/test.rb
@@ -0,0 +1,24 @@
+require 'timeout'
+require 'pp'
+
+def safe_popen(cmd)
+  begin
+    Timeout.timeout(3) do
+      @my_pipe = IO.popen(cmd)
+      until @my_pipe.eof?
+        line = @my_pipe.readline
+        yield line
+      end
+      Process.wait @my_pipe.pid
+    end
+  rescue Timeout::Error => e
+    Process.kill 9, @my_pipe.pid
+    yield e.to_s
+  end
+end
+
+safe_popen('for i in `seq 5` ; do echo $i ; sleep 1 ; done') { |line|
+  puts line
+}
+
+puts "result: #{result}"
