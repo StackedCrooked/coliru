c0e61b1cc35d1eb77b4879d89b47783de7fd119c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-03-06 21:02:53 +0000

    Sinatra script seems to work well.

--- Web/webserver-sinatra.rb
+++ Web/webserver-sinatra.rb
@@ -1,26 +1,31 @@
+require 'rubygems' # must be first!
 require 'fileutils'
 require 'json'
 require 'popen4'
 require 'pp'
-require 'rubygems'
 require 'sinatra'
+
+
+set :port, ENV['COLIRU_PORT']
 
 
 # @param [String] cmd Command to be executed.
 def safe_popen(cmd)
   begin
     Timeout.timeout(20) do
-      @stdout = IO.popen(cmd)
+      @stdout = IO.popen("#{cmd} 2>&1 ")
       until @stdout.eof?
         line = @stdout.readline
         yield line
       end
-      Process.wait @stdout.pid
     end
   rescue Timeout::Error => e
     Process.kill 9, @stdout.pid
-    Process.wait @stdout.pid
     yield e.to_s
+  rescue Exception => e
+    e.to_s
+  ensure
+    Process.wait @stdout.pid
   end
 end
 
@@ -37,14 +42,25 @@ end
 
 post '/compile' do
   json_obj = JSON.parse(request.body.read)
-  File.open('cmd.sh') { |f| f << json_obj['cmd'] }
-  File.open('main.cpp') { |f| f << json_obj['src'] }
-  safe_popen('./sandbox.sh') { |line| out << line }
+  File.open('cmd.sh', 'w') { |f| f << json_obj['cmd'] }
+  File.open('main.cpp', 'w') { |f| f << json_obj['src'] }
+  stream do |out|
+    safe_popen('./sandbox.sh') { |line| out << line }
+  end
 end
 
 
 post '/share' do
-  safe_popen('./share.sh') { |line| out << line }
+  json_obj = JSON.parse(request.body.read)
+  File.open('cmd.sh', 'w') { |f| f << json_obj['cmd'] }
+  File.open('main.cpp', 'w') { |f| f << json_obj['src'] }
+
+  result = nil
+  safe_popen('./share.sh') do |line|
+    result = result || line
+    next # we want to wait for the process to completely finish
+  end
+  result
 end
 
 
@@ -53,10 +69,11 @@ get '/view' do
 end
 
 
-get '/*' do
+get '/archive' do
   get_contents = Proc.new do |name|
     begin
-      File.read(name)
+      pp ENV
+      File.read("#{ENV['COLIRU_ARCHIVE']}/#{params[:id]}/#{name}")
     rescue Exception => e
       e.to_s
     end
@@ -67,21 +84,4 @@ get '/*' do
       :src => get_contents.call('main.cpp'),
       :output => get_contents.call('output')
   }.to_json
-end
-
-
-def log
-  Logger.new(STDOUT)
-end
-
-
-before do
-  @path_info = request.path_info
-  @request_start = Time.now.to_f
-  log.info("before #{@path_info}")
-end
-
-
-after do
-  log.info("after  #{@path_info}")
 end
