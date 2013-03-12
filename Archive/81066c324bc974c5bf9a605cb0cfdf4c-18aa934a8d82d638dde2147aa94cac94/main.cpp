b8042cee0a7177cc163c5ccaaaf7957951549461
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-03-12 19:21:27 +0000

    Update archive

---
+++ Archive/81066c324bc974c5bf9a605cb0cfdf4c-18aa934a8d82d638dde2147aa94cac94/main.cpp
@@ -0,0 +1,25 @@
+#include <memory>
+
+template <class T>
+inline
+T*
+to_raw_pointer(T* p) noexcept
+{
+    return p;
+}
+
+template <class Pointer>
+inline
+typename std::pointer_traits<Pointer>::pointer  //why can't this be ::pointer, which after all is T*???
+to_raw_pointer(Pointer p) noexcept
+{
+    return ::to_raw_pointer(p.operator->());
+}
+
+int main()
+{
+    std::shared_ptr<int> p1(new int(1));
+    int* r1 = to_raw_pointer(p1); 
+    int* p2 = new int(2);
+    int* r2 = to_raw_pointer(p2);
+}
