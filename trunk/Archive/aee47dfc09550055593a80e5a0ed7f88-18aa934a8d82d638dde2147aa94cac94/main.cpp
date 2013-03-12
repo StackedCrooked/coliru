dd20a06abc5d7f6227615f083792737ee977b05a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-03-12 23:25:06 +0000

    Update archive

---
+++ trunk/Archive/aee47dfc09550055593a80e5a0ed7f88-18aa934a8d82d638dde2147aa94cac94/main.cpp
@@ -0,0 +1,35 @@
+#include <memory>
+
+template<class allocator>
+struct alloc_aware {
+    typedef typename allocator::size_type size_type;
+    allocator* a;    
+    size_type s;
+    alloc_aware(allocator& alloc, size_type size):a(&alloc), s(size) {}
+    template<class T> void operator()(T* ptr) {a->destroy(ptr); a->deallocate(ptr, s);}
+};
+
+template<class allocator>
+struct only_deallocate {
+    typedef typename allocator::size_type size_type;
+    allocator* a;    
+    size_type s;
+    only_deallocate(allocator& alloc, size_type size):a(&alloc), s(size) {}
+    template<class T> void operator()(T* ptr) {a->deallocate(ptr, s);}
+    operator alloc_aware<allocator>() const {return alloc_aware<allocator>(*a, s);}
+};
+template<class node, class allocator, class value_type>
+std::unique_ptr<node,alloc_aware<allocator>> allocate_new_node(allocator& alloc, const value_type& value) {
+    std::unique_ptr<node, only_deallocate<allocator>> buf(alloc.allocate(sizeof(node)),{alloc, sizeof(node)});//allocate memory
+    alloc.construct(buf.get(), value);
+    return std::unique_ptr<node,alloc_aware<allocator>>(std::move(buf));
+}
+
+struct node {
+    node(int) {}
+};
+
+int main() {
+    std::allocator<node> alloc;
+    auto r = allocate_new_node<node>(alloc, 3);
+}
