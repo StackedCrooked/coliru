9b48ae2fe7321e7a793b6e11a5b30eae7623f20b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-03-15 16:08:36 +0000

    Update archive

---
+++ trunk/Archive/8498a069754d6f3567b1b695efe16d30-61c3814520a8d4318f681038dc4b4da7/main.cpp
@@ -0,0 +1,62 @@
+#include <boost/exception/all.hpp>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+
+// root exception type
+struct ExceptionBase : virtual std::exception, virtual boost::exception { };
+
+// error tag for suggesting probable cause or solution
+typedef boost::error_info<struct Suggestion, std::string> SuggestionTag;
+
+
+namespace FileSystem {        
+    
+struct FileSystemError : virtual ExceptionBase {};
+struct FileNotFound : virtual FileSystemError {};
+
+typedef boost::error_info<struct FileName, std::string> FileNameTag;
+
+bool FileExists(const std::string &)
+{
+    return false;
+}
+
+void MoveFile(const std::string & from, const std::string & to)
+{
+    BOOST_THROW_EXCEPTION(FileNotFound() << FileNameTag(from));
+}
+
+} // namespace FileSystem
+
+namespace Distribution {
+
+void PublishFile(const std::string & file)
+{
+    std::string remote_location = "network://path/to/destination";
+    try
+    {
+        FileSystem::MoveFile(remote_location, file);
+    }    
+    catch (ExceptionBase & exc)
+    {
+        exc << SuggestionTag("Check if the network drive is available.");
+        throw;
+    }
+}
+
+} // namespace Distribution
+
+
+int main()
+{
+    try
+    {
+        Distribution::PublishFile("report.odt");
+    }
+    catch (...)
+    {
+        std::cerr << boost::current_exception_diagnostic_information() << std::endl;
+    }
+}
