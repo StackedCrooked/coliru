e0b0d9a79968bcb890849ea449dcc41c5f788bb2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-03-15 02:55:12 +0000

    Update archive

---
+++ trunk/Archive/20279a9fab3371b6c29d2922890b8c32-cec58fd375b703538cfb777580e03dd3/main.cpp
@@ -0,0 +1,43 @@
+#include <fstream>
+
+struct file : std::ofstream {
+    template<typename String>
+    file(const std::string & name, String && str) : std::ofstream(name) {
+        *this << std::forward<String>(str);
+    }
+};
+
+
+file foo_hpp("foo.hpp", R"(
+    template<typename T>
+    struct Foo {
+        T get();
+    };
+    
+)");
+
+file foo_cpp("foo.cpp", R"(    
+    #include "foo.hpp"
+    
+    template<typename T>
+    T Foo<T>::get() {
+        return T();
+    }
+    
+)");
+
+file test_cpp("test.cpp", R"(
+    #include "foo.cpp"
+    #include <iostream>
+    
+    int main() {
+        std::cout << Foo<int>().get() << std::endl;
+        std::cout << Foo<long>().get() << std::endl;
+        std::cout << "Done." << std::endl;
+    }
+    
+)");
+
+
+
+int main() { }
