fe67efa82f66a4d7928a5545c0ef7ecc69204d17
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-03-15 00:51:24 +0000

    Remove _run file. Use function instead.

--- trunk/Web/_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source ./coliru_env.source
-cd "$(dirname $0)"
-./kill-current.sh
-./repair-permissions.sh 
-echo "Starting web server."
-sudo -E -u webserver ruby webserver-sinatra.rb >coliru.log 2>&1
--- trunk/Web/run.sh
+++ trunk/Web/run.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 source "./coliru_env.source"
 [ -d ${COLIRU_ARCHIVE} ] || { echo "${COLIRU_ARCHIVE} does not exist. Exiting." && exit 1 ; }
+
+restart_webserver() {
+  source ./coliru_env.source
+  cd "$(dirname $0)"
+  ./kill-current.sh
+  ./repair-permissions.sh 
+  echo "Starting web server."
+  sudo -E -u webserver ruby webserver-sinatra.rb >coliru.log 2>&1
+}
+
 while true ; do
-    ./_run.sh
+    restart_webserver
     sleep 2
 done
+
