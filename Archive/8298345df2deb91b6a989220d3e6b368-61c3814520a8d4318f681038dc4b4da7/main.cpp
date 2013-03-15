7ab1afa921a6a048ad4ea5c58d47fa235ee76682
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-03-15 13:05:52 +0000

    Update archive

---
+++ Archive/8298345df2deb91b6a989220d3e6b368-61c3814520a8d4318f681038dc4b4da7/main.cpp
@@ -0,0 +1,32 @@
+#include <boost/algorithm/string.hpp>
+#include <boost/preprocessor.hpp>
+#include <iostream>
+ 
+#define ELEMENT(n, product) \
+    BOOST_PP_TUPLE_ELEM(BOOST_PP_SEQ_SIZE(product), n, BOOST_PP_SEQ_TO_TUPLE(product))
+ 
+#define COMBINER(r, product) \
+    ELEMENT(2, product) \
+    ELEMENT(1, product) \
+    ELEMENT(0, product);
+ 
+#define TEST(args) \
+    BOOST_PP_SEQ_FOR_EACH_PRODUCT(COMBINER, args)
+ 
+#define TEST_STRING(args) \
+    BOOST_PP_STRINGIZE(TEST(args))
+
+#define EMPTY
+ 
+int main()
+{
+    std::cout << "Types:" << std::endl;
+    std::string s = TEST_STRING(
+        ((her)(him)(nobody))
+        ((marries)(divorces))
+        ((he)(nobody)(she))
+        );
+    boost::replace_all(s, "; ", "\n");
+    boost::replace_all(s, ";", "\n"); // last one has no space
+    std::cout << s;
+}
