a4dfcf1b6c7b71ba39f0222ec87a9875463ff06c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-03-17 15:50:58 +0000

    Update archive

---
+++ Archive/865faf8df6e67a809ef908505743dde0-ae0f5612d8833bcae7780f6181436e36/main.cpp
@@ -0,0 +1,88 @@
+#include <tuple>
+#include <functional>
+#include <utility>
+#include <type_traits>
+
+struct tie_it{
+    template<class... Ts>
+    std::tuple<Ts&...> operator()(Ts&... vs){
+        return std::tie(vs...);
+    }
+};
+
+struct less{
+    template<class L, class R>
+    bool operator()(L&& lhs, R&& rhs){
+        return std::forward<L>(lhs) < std::forward<R>(rhs);
+    }
+};
+
+template<unsigned...> struct seq{};
+
+template<unsigned I, unsigned... Is>
+struct gen_seq : gen_seq<I-1, I-1, Is...>{};
+template<unsigned... Is>
+struct gen_seq<0, Is...> : seq<Is...>{};
+
+template<class Comp, class... Keys>
+struct compare_by_{
+    Comp comp;
+    std::tuple<Keys...> keys;
+    
+    template<class L, class R>
+    bool operator()(L&& lhs, R&& rhs){
+        auto access = make_access(gen_seq<sizeof...(Keys)>{});
+        return access(std::forward<L>(lhs)) < access(std::forward<R>(rhs));
+    }
+    
+
+    template<unsigned... Is>
+    auto make_access(seq<Is...>)
+      -> decltype(std::bind(tie_it{}, std::bind(std::get<Is>(keys), std::placeholders::_1)...))
+    {
+        return std::bind(tie_it{}, std::bind(std::get<Is>(keys), std::placeholders::_1)...);
+    }
+    
+    
+};
+
+template<class Comp = less, class... Keys>
+compare_by_<Comp, Keys...> compare_by(Keys... ks){ return {{}, std::tuple<Keys...>{ks...}}; }
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+struct identity{
+    template<class T>
+    T&& operator()(T&& v){
+        return std::forward<T>(v);
+    }
+};
+
+struct user{
+  int id;
+  std::string name;
+};
+
+int const& get_id(user const& u){ return u.id; }
+std::string const& get_name(user const& u){ return u.name; }
+
+int main(){
+    std::vector<int> v1{ 3, 5, 1, 0, 4, 8, 6, 2, 7, 9};
+    std::vector<user> users{ {4, "Bjarne"}, {1, "Herb"}, {3, "Scott"}, {0, "STL"}, {2, "Andrei"} };
+    
+    std::sort(v1.begin(), v1.end(), compare_by(identity{}));
+    for(int i : v1)
+        std::cout << i << " ";
+ 
+    std::cout << "\n";
+    std::sort(users.begin(), users.end(), compare_by(get_id));
+    for(auto& u : users)
+        std::cout << "(" << u.id << ", " << u.name << ") ";
+        
+    std::cout << "\n";
+    std::sort(users.begin(), users.end(), compare_by(get_name));
+    for(auto& u : users)
+        std::cout << "(" << u.id << ", " << u.name << ") ";
+}
