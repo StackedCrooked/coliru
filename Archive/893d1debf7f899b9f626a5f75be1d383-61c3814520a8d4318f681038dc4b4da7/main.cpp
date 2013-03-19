2e44aa7c951d69ac0f045eab8bc8de89a9df43b3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-03-19 06:25:15 +0000

    Update archive

---
+++ Archive/893d1debf7f899b9f626a5f75be1d383-61c3814520a8d4318f681038dc4b4da7/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <functional>
+
+
+class Test {
+public:
+    Test() { std::cout << "Test()" << std::endl; }
+    Test(const Test& rhs) {
+        std::cout << "Test(const Test&)" << std::endl;
+        std::cout << "   this: " << this << std::endl;
+        std::cout << "   &rhs: " << &rhs << std::endl;
+    }
+    Test(Test&&) { std::cout << "Test(Test&&)" << std::endl; }
+    
+    Test& operator = (const Test&) {
+        std::cout << "Test::operator=(const Test&)" << std::endl;
+        return *this;
+    }
+    
+    Test& operator = (Test&&) {
+        std::cout << "Test::operator=(Test&&)" << std::endl; 
+        return *this;
+    }
+    
+    Test& operator()() {
+        std::cout << "Test::operator()" << std::endl << std::endl;
+        return *this;
+    }
+};
+
+
+Test& DoSomething(std::function<Test&()> func) {
+    return func();
+}
+
+
+Test& Func(Test& t) {
+    std::cout << &t << std::endl;;
+    return t;   
+}
+
+
+int main() {
+    Test bar = Func(bar());
+    std::cout << &bar << std::endl << std::endl;
+    
+    Test foo = DoSomething([&foo]() -> Test& {
+        std::cout << "In lamba: " << &foo << std::endl;
+        return foo;
+    });
+    
+    std::cout << "In main: " << &foo << std::endl;
+}
