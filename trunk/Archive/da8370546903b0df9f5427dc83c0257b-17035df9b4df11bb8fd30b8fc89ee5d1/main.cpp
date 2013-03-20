ddf44b4c3a455107f8fa9f583f5cd45c8acaa21f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-03-20 23:02:52 +0000

    Update archive

---
+++ trunk/Archive/da8370546903b0df9f5427dc83c0257b-17035df9b4df11bb8fd30b8fc89ee5d1/main.cpp
@@ -0,0 +1,29 @@
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+int main (int argc, char *argv[])
+{
+    po::options_description config("Configuration");
+    config.add_options()
+                ("IPAddress,i", po::value<std::string>()->required(), "IP Address")
+                ("Port,p", po::value<std::string>()->required(), "Port")
+                ;
+
+    try {
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, config),vm);
+        po::notify(vm);
+        std::cout << "Values" << std::endl;
+
+        const std::string address = vm["IPAddress"].as<std::string>();
+        const std::string port = vm["Port"].as<std::string>();
+
+        std::cout << "address: " << address << std::endl;
+        std::cout << "port: " << port << std::endl;
+    } catch ( const std::exception& e ) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
