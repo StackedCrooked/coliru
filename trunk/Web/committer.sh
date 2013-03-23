91a2b1d65f30366f75df6d5add8460191fbf3888
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-03-23 17:37:05 +0000

    Update the commiter. Change default build command to use the gcc-4.8 compiler.

--- trunk/Web/committer.sh
+++ trunk/Web/committer.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 [ -z ${COLIRU_ARCHIVE} ] && { echo "COLIRU_ARCHIVE variable must be set." && exit 1; }
 [ -d ${COLIRU_ARCHIVE} ] || { echo "${COLIRU_ARCHIVE} does not exist. Exiting." && exit 1 ; }
-while true ; do ls -tc1 ${COLIRU_ARCHIVE} | xargs -I {} svn add -q ${COLIRU_ARCHIVE}/{} ; svn ci ${COLIRU_ARCHIVE} -m "Update archive" ; sleep 3600 ; done
+
+while true ; do
+  # Copy the archive to the chroot
+  rsync -az --exclude=.svn ${COLIRU_ARCHIVE} /var/chroot/ && chmod -R a+rx /var/chroot/Archive
+
+  # Add new files to subversion.
+  ls -tc1 ${COLIRU_ARCHIVE} | xargs -I {} svn add -q ${COLIRU_ARCHIVE}/{}
+
+  # And commit them.
+  svn ci ${COLIRU_ARCHIVE} -m "Update archive"
+
+  # Repeat later. 
+  sleep 3600
+done
