6a41cd938320c2c3ece695980814987c862bedb4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-03-28 21:59:13 +0000

    Update archive

---
+++ Archive/ab32365bb073deab7a8b661f687edf52-50d9cfc8a1d350e7409e81e87c2653ba/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct the_api_thing {
+    int x;
+}
+
+void f(the_api_thing* thing, void(*callback)(the_api_thing*)) {
+    callback(thing);
+}
+
+int main()
+{
+    struct with_my_stuffs : the_api_thing {
+        int y;
+    } object;
+    object.y = 42;
+    
+    f(&object, [](the_api_thing* x) {
+        auto p = static_cast<with_my_stuffs*>(x);
+        std::cout << p->y;
+    });
+}
