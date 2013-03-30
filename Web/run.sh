cfaf392f25bb10946198c38d47d589c5268c2eb2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-03-30 12:29:22 +0000

    No longer store the stdout to log.

--- Web/run.sh
+++ Web/run.sh
@@ -8,7 +8,7 @@ restart_webserver() {
   ./kill-current.sh
   ./repair-permissions.sh 
   echo "Starting web server."
-  sudo -E -u webserver ruby webserver-sinatra.rb >coliru.log 2>&1
+  sudo -E -u webserver ruby webserver-sinatra.rb >/dev/null 2>&1
 }
 
 while true ; do
