6d089834e4e0dc5e83f6401d0897acef1a9d0f44
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-03-30 06:13:38 +0000

    Update archive

---
+++ Archive/2492ef1ee130a80750ba99d0dd9c7290-50d9cfc8a1d350e7409e81e87c2653ba/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+#define TRACE std::cout << __PRETTY_FUNCTION__ << std::endl;
+
+
+namespace foo_default
+{    
+    void a() { TRACE }
+    void b() { TRACE }
+}
+
+    
+namespace foo_special
+{    
+    void a() { TRACE }    
+    void b() { TRACE }
+}
+
+
+namespace foo
+{
+    using namespace foo_default;
+    using namespace foo_special;
+    
+    // resolve ambiguities
+    using foo_default::a; 
+    using foo_special::b;
+}
+
+
+int main()
+{
+    foo::a();
+    foo::b();
+}
