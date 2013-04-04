116ea38aba34202427ce965afbc20f85793fd753
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-04-04 22:01:08 +0000

    Update archive

---
+++ Archive/5f71efde77dd4f7433516b6a46f409da-50d9cfc8a1d350e7409e81e87c2653ba/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <typeinfo>
+
+struct A
+{
+    virtual ~A() {}
+};
+
+struct B : A {};
+
+
+int main()
+{
+    std::cout << typeid(A).name() << std::endl;
+    std::cout << typeid(B).name() << std::endl;
+    
+    B b;
+    A & a = b;
+    std::cout << typeid(a).name() << std::endl;
+    std::cout << typeid(b).name() << std::endl;
+}//b
