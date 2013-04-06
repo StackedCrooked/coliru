d14841e260fa65582c7bf51790218b3900905673
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-04-06 22:53:01 +0000

    Restore external uri behavior.

--- Web/webserver-sinatra.rb
+++ Web/webserver-sinatra.rb
@@ -1,6 +1,7 @@
 require 'rubygems' # must be first!
 require 'fileutils'
 require 'json'
+require 'net/http'
 require 'popen4'
 require 'pp'
 require 'sinatra'
@@ -17,87 +18,96 @@ $semaphore = Mutex.new
 
 # @param [String] cmd Command to be executed.
 def safe_popen(cmd)
-  begin
-    Timeout.timeout(20) do
-      @stdout = IO.popen("#{cmd} 2>&1 ")
-      until @stdout.eof?
-        yield @stdout.readline
-      end
-      Process.wait @stdout.pid
+    begin
+        Timeout.timeout(20) do
+            @stdout = IO.popen("#{cmd} 2>&1 ")
+            until @stdout.eof?
+                yield @stdout.readline
+            end
+            Process.wait @stdout.pid
+        end
+    rescue Timeout::Error => e
+        Process.kill 9, @stdout.pid
+        Process.wait @stdout.pid
+        yield e.to_s
+    rescue Exception => e
+        e.to_s
     end
-  rescue Timeout::Error => e
-    Process.kill 9, @stdout.pid
-    Process.wait @stdout.pid
-    yield e.to_s
-  rescue Exception => e
-    e.to_s
-  end
 end
 
 
 get '/' do
-  File.read('index.html')
+    File.read('index.html')
 end
 
 
 get '/*.html' do |file|
-  File.read("#{file}.html")
+    File.read("#{file}.html")
 end
 
 
 get '/favicon.ico' do
-  File.read('favicon.ico')
+    File.read('favicon.ico')
 end
 
 
 post '/compile' do
-  $semaphore.synchronize do
-    json_obj = JSON.parse(request.body.read)
-    File.open('cmd.sh', 'w') { |f| f << json_obj['cmd'] }
-    File.open('main.cpp', 'w') { |f| f << json_obj['src'] }
-    stream do |out|
-      safe_popen('./sandbox.sh') { |line| out << line }
+    $semaphore.synchronize do
+        json_obj = JSON.parse(request.body.read)
+        File.open('cmd.sh', 'w') { |f| f << json_obj['cmd'] }
+        File.open('main.cpp', 'w') { |f| f << json_obj['src'] }
+        stream do |out|
+            safe_popen('./sandbox.sh') { |line| out << line }
+        end
     end
-  end
 end
 
 
 post '/share' do
-  $semaphore.synchronize do
-    result = nil
-    json_obj = JSON.parse(request.body.read)
-    File.open('cmd.sh', 'w') { |f| f << json_obj['cmd'] }
-    File.open('main.cpp', 'w') { |f| f << json_obj['src'] }
-    
-    safe_popen('./share.sh') do |line|
-      result = result || line
-      next # we want to wait for the process to completely finish
+    $semaphore.synchronize do
+        result = nil
+        json_obj = JSON.parse(request.body.read)
+        File.open('cmd.sh', 'w') { |f| f << json_obj['cmd'] }
+        File.open('main.cpp', 'w') { |f| f << json_obj['src'] }
+
+        safe_popen('./share.sh') do |line|
+            result = result || line
+            next # we want to wait for the process to completely finish
+        end
+        stream { |out| out << result }
     end
-    stream { |out| out << result }
-  end
 end
 
 
 get '/view' do
-  File.read('view.html')
+    File.read('view.html')
+end
+
+
+get '/external' do
+    stream do |out|
+        uri = URI.parse(params[:url])
+        http_get_request = Net::HTTP::Get.new(uri.path)
+        result = Net::HTTP.start(uri.host, uri.port) { |http| http.request(http_get_request) }
+        out << result.body
+    end
 end
 
 
 get '/archive' do
-  get_contents = Proc.new do |name|
-    begin
-      pp ENV
-      File.read("#{ENV['COLIRU_ARCHIVE']}/#{params[:id]}/#{name}")
-    rescue Exception => e
-      ""
+    get_contents = Proc.new do |name|
+        begin
+            File.read("#{ENV['COLIRU_ARCHIVE']}/#{params[:id]}/#{name}")
+        rescue Exception => e
+            ""
+        end
     end
-  end
 
-  $semaphore.synchronize do
-    {
-        :cmd => get_contents.call('cmd.sh'),
-        :src => get_contents.call('main.cpp'),
-        :output => get_contents.call('output')
-    }.to_json
-  end
+    $semaphore.synchronize do
+        {
+            :cmd => get_contents.call('cmd.sh'),
+            :src => get_contents.call('main.cpp'),
+            :output => get_contents.call('output')
+        }.to_json
+    end
 end
