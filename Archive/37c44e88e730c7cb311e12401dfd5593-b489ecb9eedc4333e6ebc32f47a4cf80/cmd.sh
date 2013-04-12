3d8b346d28ec50acef085a1c0d22c061283dc27b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-04-12 15:19:32 +0000

    Update archive

---
+++ Archive/37c44e88e730c7cb311e12401dfd5593-b489ecb9eedc4333e6ebc32f47a4cf80/cmd.sh
@@ -0,0 +1,8 @@
+g++-4.8 -O3 -DUSE_ITERATOR -S main.cpp -o - > iterator
+g++-4.8 -O3                -S main.cpp -o - > pointer
+echo "  * pointer *                                                  * iterator *"
+diff -y pointer iterator 
+
+
+
+
