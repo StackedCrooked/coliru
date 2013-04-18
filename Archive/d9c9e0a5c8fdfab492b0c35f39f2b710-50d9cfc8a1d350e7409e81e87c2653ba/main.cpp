bff8be5712d5c1e9c7eb56e90b62a57f24cb5faf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-04-18 20:14:14 +0000

    Update archive

---
+++ Archive/d9c9e0a5c8fdfab492b0c35f39f2b710-50d9cfc8a1d350e7409e81e87c2653ba/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <iomanip>
+#include <stdint.h>
+
+enum class Binary : bool { No, Yes };
+enum class Text : bool { No, Yes };
+
+template<typename T, Binary, Text>
+struct Wrap
+{
+    const T & get() const { return obj_; }
+    T obj_;    
+};
+
+template<Binary b, Text t>
+using Octet = Wrap<unsigned char, b, t>;
+
+using Byte  = Octet<Binary::Yes, Text::No >;
+using Ascii = Octet<Binary::No , Text::Yes>;
+using Mixed = Octet<Binary::Yes, Text::Yes>;
+
+
+std::ostream& operator<<(std::ostream& os, Byte b)
+{
+    return os << std::hex << "0x" << std::setw(2) << std::setfill('0') << static_cast<int>(b.get()) << std::dec;
+}
+
+std::ostream& operator<<(std::ostream& os, Ascii b)
+{
+    return os << std::hex << "  " << b.get() << ' ' << std::dec;
+}
+
+std::ostream& operator<<(std::ostream& os, Mixed b)
+{
+    enum { first = 36, last = 127 };
+    if (b.get() >= first && b.get() <= last)
+    {
+        return os << Ascii{b.get()};
+    }
+    else
+    {
+        return os << Byte{b.get()};
+    }
+}
+
+
+int main()
+{
+    for (int i = 0; i != 256; ++i)
+    {
+        if (i%16 == 0) std::cout << std::endl;
+        unsigned char c = i;
+        std::cout << Mixed{c} << " ";
+    }
+}
