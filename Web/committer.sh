d85857c7532eb9be993f1a6c0ac4687c4a28f05c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-04-27 03:42:21 +0000

    Update the committer.

--- Web/committer.sh
+++ Web/committer.sh
@@ -1,19 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 [ -z ${COLIRU_ARCHIVE} ] && { echo "COLIRU_ARCHIVE variable must be set." && exit 1; }
 [ -d ${COLIRU_ARCHIVE} ] || { echo "${COLIRU_ARCHIVE} does not exist. Exiting." && exit 1 ; }
 
 while true ; do
-  # Copy the archive to the chroot
-  rsync -az --exclude=.svn ${COLIRU_ARCHIVE} /var/chroot/ && chmod -R a+rx /var/chroot/Archive
-
   # Add new files to subversion.
-  ls -tc1 ${COLIRU_ARCHIVE} | xargs -I {} svn add -q ${COLIRU_ARCHIVE}/{}
+  (cd ${COLIRU_ARCHIVE} && svn st --no-ignore | grep -e ^[I?] | sed 's_^[?I][ ]*__' | xargs -I {} svn add -q ${COLIRU_ARCHIVE}/{})
 
   # And commit them.
   svn ci ${COLIRU_ARCHIVE} -m "Update archive"
 
   # Also commit the feedback."
   svn ci feedback.txt -m "Update feedback."
+
+  # Copy the archive to the chroot
+  { rsync -az --exclude=.svn ${COLIRU_ARCHIVE} /var/chroot/ && chmod -R a+rx /var/chroot/Archive ; } & disown
 
   # Repeat later. 
   sleep 1200
