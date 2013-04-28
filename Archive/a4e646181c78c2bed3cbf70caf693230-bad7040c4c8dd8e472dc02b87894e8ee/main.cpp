9c5ca57859d23f3da538e6a9f17aa60489942f5c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-04-28 17:18:57 +0000

    Update archive

---
+++ Archive/a4e646181c78c2bed3cbf70caf693230-bad7040c4c8dd8e472dc02b87894e8ee/main.cpp
@@ -0,0 +1,25 @@
+#include <thread>
+#include <iostream>
+#include <pthread.h>
+ 
+using namespace std;
+
+void foo()
+{
+    string s = "thread1";
+    size_t len = 0;
+    char name[16];
+    
+     pthread_setname_np(pthread_self(), s.c_str()); // set the name (pthread_self() returns the pthread_t of the current thread)    
+     pthread_getname_np(pthread_self(),name, len);   
+     cout << static_cast<const void*>(name) << endl;
+
+}
+
+int main() 
+{
+    thread t1(foo);
+ 
+    t1.join();
+    return 0;
+}
