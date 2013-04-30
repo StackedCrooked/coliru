88d0d6bdb8e6edfa45b35e651c8681ad226f93fa
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-04-30 10:14:48 +0000

    Update archive

---
+++ Archive/c18bef74821d7de8a55b4ada3500ab6c-50d9cfc8a1d350e7409e81e87c2653ba/main.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include <set>
+#include <list>
+
+
+
+namespace tesla {
+    
+    template<typename T, size_t first, size_t... rest>
+    struct multi_array {
+        enum { dims = sizeof...(rest) };
+        typedef std::array< typename multi_array<T, rest...>::type, first > type;
+    };
+    
+    template<typename T, size_t first>
+    struct multi_array<T,first>{
+        enum { dims = 1 };
+        typedef std::array<T,first> type;
+    };
+    
+    template<class T, class U>
+    struct is_same {
+        enum {value = false };
+    };
+     
+    template<class T>
+    struct is_same<T, T> {
+        enum {value = true };  
+    };
+    
+    template<class T>
+    struct is_integer {
+        enum {value = is_same<T,int>::value };  
+    };
+    
+    template<class T>
+    struct is_bool {
+        enum {value = is_same<T,bool>::value };  
+    };
+    
+};
+
+template<typename T, size_t N>
+std::ostream& operator<<(std::ostream& os, std::array<T, N> arr) {
+
+    os << "{";
+    for(int i = 0; i < N - 1; i++)
+        os << arr[i] << ", ";
+    os << arr[N - 1];
+    os << "}";
+    return os;
+}
+
+
+
+
+
+
+
+template<typename T, size_t N>
+void init(std::array<T,N>& arr) {
+    for(int i = 0; i < arr.size(); i++)
+        for(int j = 0; j < arr[i].size(); j++)
+            for(int k = 0; k < arr[i][j].size(); k++)
+                arr[i][j][k] = i + j + k;
+}
+
+
+
+int main()
+{
+    
+using tesla::multi_array;
+using std::endl;
+using std::cout;
+    
+    multi_array<int,2,3,4>::type arr1;
+    multi_array<int,3,4>::type arr2 = {};
+        
+    init(arr1);
+    arr2[0] = arr1[0][0];
+    arr1[1][2][0] = 9;
+    std::cout << arr2 << std::endl;
+    std::cout << arr1 << std::endl;
+    
+    // std::cout <<  arr1.dims << std::endl;
+    
+    std::cout << tesla::is_same<int*,int>::value;
+    std::cout << tesla::is_bool<bool>::value;
+    
+    std::vector<int> v;
+std::set<int> v2;
+std::vector<int>::iterator iter;
+std::set<int>::iterator iter2;
+
+for (iter = v.begin(), iter2 = v2.begin(); 
+           iter != v.end(), iter2 != v2.end();
+           ++iter, ++iter2)
+  cout << (*iter) << (*iter2) << endl;
+        
+}
+
+
