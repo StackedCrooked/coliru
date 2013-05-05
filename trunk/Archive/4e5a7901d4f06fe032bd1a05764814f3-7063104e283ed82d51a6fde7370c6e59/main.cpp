c94059db347c0afd3ea03ce9aa11b2b348c5caa6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-05 09:56:44 +0000

    Update archive

---
+++ trunk/Archive/4e5a7901d4f06fe032bd1a05764814f3-7063104e283ed82d51a6fde7370c6e59/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+   double v1;
+   
+   for (int i=0; i<= 100; i++){
+     cout << "rand 1:"<< rand() % 100 << endl;
+   }
+}
