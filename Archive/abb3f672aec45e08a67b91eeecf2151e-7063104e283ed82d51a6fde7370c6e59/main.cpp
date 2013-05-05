2d0a80e90ef59bfb72dc28235e915f32c0337b64
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-05 15:59:16 +0000

    Update archive

---
+++ Archive/abb3f672aec45e08a67b91eeecf2151e-7063104e283ed82d51a6fde7370c6e59/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+template <class deleter_t> class id {
+    public:
+		id(deleter_t deleter) : d(deleter) {
+		};
+		~id() {
+			d();
+		}
+		~id() {
+			d(1);
+		}
+	private:
+		deleter_t d;
+};
+
+void deleter_one() {
+	std::cout << "Delete without size was called!\n";
+}
+
+void deleter_two(int size) {
+	std::cout << "Delete with size=" << size << "was called!\n";
+}
+
+int main() {
+	auto one = id(deleter_one);
+	auto two = id(deleter_two);
+}
