d77831c1ab507b79fcec3192724a6b449ddc9367
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-06 05:20:45 +0000

    Update archive

---
+++ trunk/Archive/c749fa886e7d7f9ebc1b53fd7d5cc5e4-7063104e283ed82d51a6fde7370c6e59/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int userinput = 0;
+int hp = 1000;
+int mp = 500;
+string weapon "axe";
+
+int main()
+{
+  cout << "DARK SOULS II\n";
+  cout << "1: new game\n2: load game\n3: quit\n";
+  cin >> userinput;
+ 
+  cout << "you are walking in a castle with no one in it and then a knight jumps out and attacks you\n";
+  cout << "battle begin\n";
+  cout << "1: slash\n2: block\n";
+  cin >> userinput;
+  
+  cout << "LORDRAN CITY\n";
+  cout << "1 fight\n 2:buy gear\n 3: talk to the lord\n";
+  cin << userinput;
+  
+  
+}
