e2b0098734fce6c710aa76e593a00ba9a06f96f0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-08 18:46:01 +0000

    Allow custom timeout.wqa

--- trunk/Web/build_and_run.sh
+++ trunk/Web/build_and_run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 export CHROOT="/var/chroot"
+export COLIRU_TIMEOUT="$(cat timeout.txt || echo 20)"
+echo "COLIRU_TIMEOUT: ${COLIRU_TIMEOUT}"
+
 
 rm -rf "${CHROOT}/tmp/*"
 set -e
 ulimit -f 10000
 [ -f main.cpp ] || { echo "Webserver could not find main.cpp. Exiting." 1>&2 ; exit 1; }
 
-cp compile.sh cmd.sh main.cpp "${CHROOT}/tmp/"
+cp cmd.sh main.cpp "${CHROOT}/tmp/"
+cat compile.sh | sed -e "s/TIMEOUT/${COLIRU_TIMEOUT}/" > "${CHROOT}/tmp/compile.sh"
+
 chmod 755 "${CHROOT}/tmp/cmd.sh"
 chmod 755 "${CHROOT}/tmp/compile.sh"
 
