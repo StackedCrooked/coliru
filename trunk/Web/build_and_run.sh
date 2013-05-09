f80f1816962fe0837d7ca8e8879df058ba3ff1ee
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-09 06:28:18 +0000

    Fix <>.

--- trunk/Web/build_and_run.sh
+++ trunk/Web/build_and_run.sh
@@ -15,9 +15,11 @@ ulimit -f 10000
 [ -f main.cpp ] || { echo "Webserver could not find main.cpp. Exiting." 1>&2 ; exit 1; }
 
 cp cmd.sh main.cpp "${CHROOT}/tmp/"
-cat compile.sh | sed -e "s/TIMEOUT/${COLIRU_TIMEOUT}/" > "${CHROOT}/tmp/compile.sh"
 
-chmod 755 "${CHROOT}/tmp/cmd.sh"
-chmod 755 "${CHROOT}/tmp/compile.sh"
+# copy cmd.sh to chroot but prepend 'set -x' first. And make it executable as well.
+echo 'set -x' >  "${CHROOT}/tmp/cmd.sh" && cat cmd.sh | sed -e "s/TIMEOUT/${COLIRU_TIMEOUT}/" >> "${CHROOT}/tmp/cmd.sh" && chmod 755 "${CHROOT}/tmp/cmd.sh"
+
+# copy compile.sh to chroot but replace TIMEOUT with actual timeout. And also make it executable.
+cat compile.sh | sed -e "s/TIMEOUT/${COLIRU_TIMEOUT}/" > "${CHROOT}/tmp/compile.sh" && chmod 755 "${CHROOT}/tmp/compile.sh"
 
 ./chroot.sh
