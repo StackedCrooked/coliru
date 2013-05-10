713acd736e34d68b354aaf8def5821e9dcbbe831
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-10 18:21:40 +0000

    More refactoring.

--- trunk/Web/build_and_run.sh
+++ trunk/Web/build_and_run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 export CHROOT="/var/chroot"
 
 TIMEOUT=$(cat timeout.txt 2>/dev/null || echo 20)
@@ -10,22 +9,13 @@ else
 fi
 
 
-TMP_DIR="/tmp/$(date '+%s')"
-[ ! -d ${TMP_DIR} ] || { echo "The temp directory already exists: ${TMP_DIR}." 1>&2 ; exit 1 ; }
+[ ! -d ${CHROOT}/${TMP_DIR} ] || { echo "The temp directory already exists: ${TMP_DIR}." 1>&2 ; exit 1 ; }
 #sudo -u sandbox chroot ${CHROOT} mkdir -p ${TMP_DIR}
-mkdir -p ${CHROOT}/${TMP_DIR}
-chmod -R a+w ${CHROOT}/${TMP_DIR}
+DIR=$(basename ${TMP_DIR})
+chmod 755 ${TMP_DIR}/cmd.sh
+mv ${TMP_DIR} ${CHROOT}/tmp/
+export DST=${CHROOT}/tmp/${DIR}
+chmod -R a+w ${CHROOT}/tmp/${DIR}
 
-rm -rf "${CHROOT}/${TMP_DIR}/*"
-set -e
-[ -f main.cpp ] || { echo "Webserver could not find main.cpp. Exiting." 1>&2 ; exit 1; }
-
-mv main.cpp "${CHROOT}/${TMP_DIR}/"
-
-# copy cmd.sh to chroot but prepend 'set -x' first. And make it executable as well.
-echo 'set -x' >  "${CHROOT}/${TMP_DIR}/cmd.sh" && cat cmd.sh | sed -e "s/TIMEOUT/${COLIRU_TIMEOUT}/" >> "${CHROOT}/${TMP_DIR}/cmd.sh" && chmod 755 "${CHROOT}/${TMP_DIR}/cmd.sh"
-
-# copy compile.sh to chroot but replace TIMEOUT with actual timeout. And also make it executable.
-cat compile.sh | sed -e "s/TIMEOUT/${COLIRU_TIMEOUT}/" > "${CHROOT}/${TMP_DIR}/compile.sh" && chmod 755 "${CHROOT}/${TMP_DIR}/compile.sh"
-
-./chroot.sh ${TMP_DIR}
+cp compile.sh /var/chroot/tmp/$(basename ${DIR}).sh
+./chroot.sh
--- trunk/Web/sandbox.sh
+++ trunk/Web/sandbox.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+set -e
 source coliru_env.source
+
+
+[ "$TMP_DIR" == "" ] && { echo "TMP_DIR not set." 1>&2 ; exit 1 ; }
+
 if [ "$(uname)" == "Darwin" ] ; then 
-    ID="$(md5 main.cpp | cut -d '=' -f 2 | sed -e 's/ //g')-$(md5 cmd.sh | cut -d '=' -f 2 | sed -e 's/ //g')"
+    ID="$(md5 ${TMP_DIR}/main.cpp | cut -d '=' -f 2 | sed -e 's/ //g')-$(md5 ${TMP_DIR}/cmd.sh | cut -d '=' -f 2 | sed -e 's/ //g')"
 else
-    ID="$(md5sum main.cpp | cut -d ' ' -f 1)-$(md5sum cmd.sh | cut -d ' ' -f 1)"
+    ID="$(md5sum ${TMP_DIR}/main.cpp | cut -d ' ' -f 1)-$(md5sum ${TMP_DIR}/cmd.sh | cut -d ' ' -f 1)"
 fi
 
 if [ -f "${COLIRU_ARCHIVE}/${ID}/output" ] ; then
@@ -13,6 +18,7 @@ fi
 
 trap "rm -f timeout.txt; exit" INT TERM EXIT
 
-pkill -9 -u sandbox
-./repair-permissions.sh >/dev/null 2>&1
+#pkill -9 -u sandbox
+
+#{ ./repair-permissions.sh >/dev/null 2>&1 ; } || true
 ./build_and_run.sh
