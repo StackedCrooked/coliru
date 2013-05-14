1445d48dee4feae4d73642096cde2250886b9dc4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-14 11:01:57 +0000

    Add to archive.

---
+++ Archive/0d75a871d4931eac9638c36860a9628e-7063104e283ed82d51a6fde7370c6e59/main.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <iostream>
+
+struct Tit {
+public:
+    template <typename TMilkContainer, typename TMilk = typename TMilkContainer::value_type>
+    Tit ( TMilkContainer&& milkcontainer ) {
+        std::cout << "Milky" << std::endl;
+    }
+    
+    Tit () {
+        
+    }
+    
+    Tit ( Tit&& mov ) {
+        std::cout << "Moving" << std::endl;    
+    }
+ 
+};
+
+struct ManBreast : public Tit {
+public:
+    struct DryAsADesert {
+        
+    };
+    
+    using value_type = int;
+    
+    DryAsADesert d;
+    
+    ManBreast () {
+        
+    }
+    
+    // Invokes templated `Tit` constructor rather than mov constructor
+    // on MSVC
+    ManBreast ( ManBreast&& mov ) : Tit( static_cast<Tit&&>( std::move( mov ) ) ), d( mov.d ) {
+        
+    }
+    
+};
+
+int main ( int argc, char** argv ) {
+	
+    ManBreast mb = std::move ( ManBreast() );
+    
+}
