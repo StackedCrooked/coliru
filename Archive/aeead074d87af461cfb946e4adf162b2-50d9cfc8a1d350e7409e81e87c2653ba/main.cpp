46ba80693d77845b09f3988ac727898bd3e251e6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-14 10:58:05 +0000

    Add to archive.

---
+++ Archive/aeead074d87af461cfb946e4adf162b2-50d9cfc8a1d350e7409e81e87c2653ba/main.cpp
@@ -0,0 +1,65 @@
+template <class ItSource, class ItPredicate>
+class ConditionalIterator
+{
+    ItSource _srcBegin;
+    ItSource _srcEnd;
+
+    ItPredicate _predBegin;
+    ItPredicate _predEnd;
+public:
+    typedef ConditionalIterator & Reference;
+    typedef typename ItSource::value_type ValueType;
+
+    ConditionalIterator(ItSource srcBegin, ItSource srcEnd,
+                        ItPredicate predBegin, ItPredicate predEnd)
+                        : _srcBegin(srcBegin)
+                        , _srcEnd(srcEnd)
+                        , _predBegin(predBegin)
+                        , _predEnd(predEnd)
+    {
+    }
+
+    Reference operator++()
+    {
+        do 
+        {
+            ++_predBegin;
+            ++_srcBegin;
+        }
+        while (_predBegin != _predEnd &&
+               _srcBegin != _srcEnd &&
+               !*_predBegin);
+        return (*this);
+    }
+
+    operator bool() const
+    {
+        return (_srcBegin != _srcEnd &&
+                _predBegin != _predEnd);
+    }
+
+    ValueType operator*()
+    {
+        return (*_srcBegin);
+    }
+};
+
+template <class ItSource, class ItPredicate>
+ConditionalIterator<ItSource, ItPredicate> 
+make_conditional_iterator(ItSource fs, ItSource es, ItPredicate fp, ItPredicate ep) 
+{return ConditionalIterator<ItSource, ItPredicate>(fs, es, fp, ep);}
+
+#include <array>
+#include <iostream>
+
+int main() {
+    std::array<int,5>  element={ 1   , 2   , 4    , 8    , 10   };
+    std::array<bool,5> deleted={ true, true, false, false, true };
+    
+    auto cit = make_conditional_iterator(element.begin(), element.end(), deleted.begin(), deleted.end());
+    while (cit)
+    {
+        std::cout << *cit << std::endl;
+        cit++;
+    }
+}
