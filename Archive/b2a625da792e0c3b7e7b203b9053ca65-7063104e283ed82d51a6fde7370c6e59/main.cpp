de4a4b654dc2e4bcaacc7868c0970aa5119de847
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-14 12:27:56 +0000

    Add to archive.

---
+++ Archive/b2a625da792e0c3b7e7b203b9053ca65-7063104e283ed82d51a6fde7370c6e59/main.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <map>
+#include <string>
+#include <vector>
+
+#include <boost/bind.hpp>
+
+using namespace std;
+
+class A{
+public:
+    A( const std::string& key )
+    : key( key ) {}
+
+    std::string getKey(){ return key; }
+private:
+    std::string key;
+};
+
+int main()
+{
+    map<string, string> mp;
+    vector<A> vec;
+    find_if( vec.begin(), vec.end(), boost::bind( &map<string, string>::count, &mp, boost::bind( &A::getKey, _1 ) )) != 0);
+    return 0;
+}
