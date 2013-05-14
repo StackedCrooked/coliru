fb0ab6ef9ddcd05fe3f49b7dd52b841192a18f8d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-14 10:57:44 +0000

    Add to archive.

---
+++ trunk/Archive/3cc8a6319c886987ce18b3aa33a5a666-7063104e283ed82d51a6fde7370c6e59/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <tuple>
+#include <utility>
+#include <type_traits>
+#include <functional>
+
+/*template <typename T>
+struct xrange {
+    
+    
+};*/
+
+int main ( int argc, char** argv ) {
+	
+    auto results = ;
+    std::cout << "[ ";
+    for ( auto& result : results ) {
+        std::cout << result << ' ';
+    }
+    std::cout << "]";
+    
+}
