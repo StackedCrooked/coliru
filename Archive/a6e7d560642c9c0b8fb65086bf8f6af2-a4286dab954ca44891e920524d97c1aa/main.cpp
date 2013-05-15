2d00e907ec7637b4dbd2963ad03842d104f46291
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-15 15:29:16 +0000

    Add to archive.

---
+++ Archive/a6e7d560642c9c0b8fb65086bf8f6af2-a4286dab954ca44891e920524d97c1aa/main.cpp
@@ -0,0 +1,11 @@
+class Program {
+    Uniform& operator[](std:string name) {
+        auto found = uniforms.find(name);
+    	if(found == uniforms.end()) {
+    		found.insert(Uniform(*this, name);
+    		return uniforms.find(name)
+    	} else {
+    		return *found;
+    	}
+    }
+}
