1de1e8f7722291d47f21b6b3c4b06b3dbdcbaca0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-15 05:06:16 +0000

    Add to archive.

---
+++ trunk/Archive/0928b7e4c71dac6d38c73e3c2b7dc05d-a2574bcf90a5184e7a985df72132c580/main.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <type_traits>
+
+struct sum_t {};
+
+template<typename Pred>
+using EnableIf = typename std::enable_if<Pred::value>::type;
+
+template<typename Container, EnableIf<std::is_class<Container>>>
+typename std::decay<Container>::type::value_type operator|(const sum_t& sum_f, Container&& container) {
+    return std::accumulate(std::begin(container),std::end(container),0);
+}
+
+int main() {
+    std::vector<int> r = {1,2,3,4,5,6,7,8,9,10};
+    auto result = sum_t{} | r;
+    std::cout << result;
+}
