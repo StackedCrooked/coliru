ef534471af047074892f5dd9ecc9340ec7198e88
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-15 18:36:22 +0000

    Update run script.

--- trunk/Web/run.sh
+++ trunk/Web/run.sh
@@ -3,13 +3,9 @@
 source "./coliru_env.source"
 [ -d ${COLIRU_ARCHIVE} ] || { echo "${COLIRU_ARCHIVE} does not exist. Exiting." 1>&2 && exit 1 ; }
 
-restart_webserver() {
-  source ./coliru_env.source
-  cd "$(dirname $0)"
-  ./kill-current.sh
-  ./repair-permissions.sh 
-  echo "$(date): Starting web server."
-  sudo -u webserver -E shotgun webserver-sinatra.rb -p 80 -o stacked-crooked.com
-}
-
-restart_webserver
+source ./coliru_env.source
+cd "$(dirname $0)"
+./kill-current.sh
+./repair-permissions.sh 
+echo "$(date): Starting web server."
+sudo -u webserver -E ruby webserver-sinatra.rb -o stacked-crooked.com
