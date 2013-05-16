57d493f3e5be972925fe6e21e931f07458c9e24e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-16 19:24:38 +0000

    Cleaner set -x insertion.

--- Web/build_and_run.sh
+++ Web/build_and_run.sh
@@ -16,6 +16,20 @@ fi
 DIR=$(basename ${TMP_DIR})
 chmod 755 ${TMP_DIR}/cmd.sh
 mv ${TMP_DIR} ${CHROOT}/tmp/
+
+
+#
+# Insert set -x and ulimit
+#
+HLP=${CHROOT}/tmp/$(basename ${TMP_DIR})
+CMD=${HLP}/cmd.sh
+mv ${CMD} ${CMD}_
+echo 'set -x' >> ${CMD}
+echo 'ulimit -u 20' >> ${CMD}
+cat ${CMD}_ >> ${CMD}
+rm ${CMD}_
+chmod a+rx ${CMD}
+
 export DST=${CHROOT}/tmp/${DIR}
 chmod -R a+w ${CHROOT}/tmp/${DIR}
 
