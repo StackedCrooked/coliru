dcbaddf0ef0da60dcb57c91a0b05d863bc830782
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-16 19:48:18 +0000

    More fixes.

--- trunk/Web/chroot.sh
+++ trunk/Web/chroot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export CHROOT=/var/chroot
 ID=$(basename ${CHROOT_TARGET_PATH})
-sudo -u sandbox chroot "${CHROOT}" /tmp/${ID}.sh /tmp/${ID}
+sudo -u sandbox chroot "${CHROOT}" /tmp/${ID}/cmd.sh
