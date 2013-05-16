36c6974afc9ca53861a8a1d790f52285635dfdc2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-16 19:28:37 +0000

    Fix directories.

--- trunk/Web/sandbox.sh
+++ trunk/Web/sandbox.sh
@@ -3,12 +3,12 @@ set -e
 source coliru_env.source
 
 
-[ "$TMP_DIR" == "" ] && { echo "TMP_DIR not set." 1>&2 ; exit 1 ; }
+[ "$INPUT_FILES_DIR" == "" ] && { echo "INPUT_FILES_DIR not set." 1>&2 ; exit 1 ; }
 
-if [ -f "${TMP_DIR}/main.cpp" ] ; then
-    ID="$(md5sum ${TMP_DIR}/main.cpp | cut -d ' ' -f 1)-$(md5sum ${TMP_DIR}/cmd.sh | cut -d ' ' -f 1)"
+if [ -f "${INPUT_FILES_DIR}/main.cpp" ] ; then
+    ID="$(md5sum ${INPUT_FILES_DIR}/main.cpp | cut -d ' ' -f 1)-$(md5sum ${INPUT_FILES_DIR}/cmd.sh | cut -d ' ' -f 1)"
 else
-    ID="$(md5sum ${TMP_DIR}/cmd.sh | cut -d ' ' -f 1)"
+    ID="$(md5sum ${INPUT_FILES_DIR}/cmd.sh | cut -d ' ' -f 1)"
 fi
 
 if [ -f "${COLIRU_ARCHIVE}/${ID}/output" ] ; then
