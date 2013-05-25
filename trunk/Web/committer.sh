7161a279d753dc9a558e2a531b1193a0710835b2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-25 21:24:57 +0000

    Update the committer.sh.

--- trunk/Web/committer.sh
+++ trunk/Web/committer.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -x
-exec 1> >(logger -t $0)
-exec 2> >(logger -t $0)
+exec 1> >(logger -t "$0 stdout")
+exec 2> >(logger -t "$0 stderr")
+echo "Starting the committer."
+echo "Loading the environment variables."
 source coliru_env.source
 [ -z ${COLIRU_ARCHIVE} ] && { echo "COLIRU_ARCHIVE variable must be set." && exit 1; }
 [ -d ${COLIRU_ARCHIVE} ] || { echo "${COLIRU_ARCHIVE} does not exist. Exiting." && exit 1 ; }
@@ -9,14 +11,15 @@ source coliru_env.source
 SLEEP_DURATION=3600
 
 while true ; do
-  # Add new files to subversion.
-  (cd ${COLIRU_ARCHIVE} && svn st --no-ignore | grep -e ^[I?] | sed 's_^[?I][ ]*__' && xargs svn add && svn ci -m "Update Archive.")
+    # Add new files to subversion.
+    echo "Commit any new archives."
+    (cd ${COLIRU_ARCHIVE} && svn st --no-ignore | grep -e ^[I?] | sed 's_^[?I][ ]*__' | xargs svn add && svn ci -m "Update Archive.")
 
-  # Also commit the feedback."
-  echo "Committing any new feedback..."
-  svn ci feedback.txt -m "Update feedback." || echo "...but there is no new feedback."
+    # Also commit the feedback."
+    echo "Committing any new feedback..."
+    svn ci feedback.txt -m "Update feedback." || echo "...but there is no new feedback."
 
-  # Repeat later. 
-  echo "Sleeping for ${SLEEP_DURATION} seconds until retry."
-  sleep ${SLEEP_DURATION}
+    # Repeat later. 
+    echo "Sleeping for ${SLEEP_DURATION} seconds until retry."
+    sleep ${SLEEP_DURATION}
 done
