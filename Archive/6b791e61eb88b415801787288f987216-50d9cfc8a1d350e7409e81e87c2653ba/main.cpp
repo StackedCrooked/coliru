cf6efdfb74341bb3e594495c46f4ce43f0bdba2e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-30 10:07:23 +0000

    Update Archive.

---
+++ Archive/6b791e61eb88b415801787288f987216-50d9cfc8a1d350e7409e81e87c2653ba/main.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+int main() {
+    std::vector<int> x;
+    
+    const std::vector<int>& r = x;
+    std::cout << r.size() << '\n';
+
+    x.push_back(17);
+    std::cout << r.size() << '\n'; // oops, it changed!
+}
