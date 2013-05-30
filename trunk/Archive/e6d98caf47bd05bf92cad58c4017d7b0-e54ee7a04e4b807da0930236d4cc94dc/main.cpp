5ea28f581d15b49bd920ecc2b5735268df3062d2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-30 21:09:25 +0000

    Update Archive.

---
+++ trunk/Archive/e6d98caf47bd05bf92cad58c4017d7b0-e54ee7a04e4b807da0930236d4cc94dc/main.cpp
@@ -0,0 +1,22 @@
+
+// Source: http://msdn.microsoft.com/en-us/library/dd412070.aspx
+
+// Defines a predicate delegate to use 
+// for the SortedSet.RemoveWhere method. 
+private static bool isDoc(string s)
+{
+    if (s.ToLower().EndsWith(".txt") ||
+        s.ToLower().EndsWith(".doc") ||
+        s.ToLower().EndsWith(".xls") ||
+        s.ToLower().EndsWith(".xlsx") ||
+        s.ToLower().EndsWith(".pdf") ||
+        s.ToLower().EndsWith(".doc") ||
+        s.ToLower().EndsWith(".docx"))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
