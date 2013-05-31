9ae14f4513ce53bbc844dbb5019b11d370da0f33
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-05-31 21:18:45 +0000

    Update Archive.

---
+++ trunk/Archive/635bc33dd68507435a205a6b6338f85a-c056299b5afab84f40636e0111d56983/main.cpp
@@ -0,0 +1,36 @@
+#include <boost/thread.hpp>
+#include <boost/asio.hpp>
+
+#include <string>
+
+class CConnection {
+public:
+    CConnection(void) { }
+    boost::asio::io_service io_service;
+    boost::shared_ptr<boost::asio::ip::tcp::socket> socket;
+    boost::asio::ip::tcp::endpoint endpoint;
+    boost::shared_ptr<boost::thread> thread;
+    boost::asio::io_service *master_io_service;
+    bool close;
+};
+
+void worker(boost::shared_ptr<CConnection> connection) 
+{
+    boost::asio::ip::tcp::socket &socket = *(connection->socket);
+    boost::asio::socket_base::non_blocking_io make_non_blocking(true);
+    socket.io_control(make_non_blocking);
+
+    char acBuffer[1024];
+    std::string line("");
+
+    while ( connection->close == false ) {
+
+    } // while connection not to be closed
+}
+
+int
+main()
+{
+    boost::shared_ptr<CConnection> connection( new CConnection );
+    new boost::thread(worker, connection);
+}
