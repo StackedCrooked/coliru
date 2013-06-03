3cdbca529bee8151185f8c99dd4a826d1cc5611d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-06-03 22:37:47 +0000

    Update Archive.

---
+++ Archive/e7363295aa68fe41b3a1b2fbe6ae6f78-e54ee7a04e4b807da0930236d4cc94dc/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <functional>
+#include <memory>
+#include <cstdint>
+
+using namespace std;
+
+function<uint64_t ()> makeFibonacci()
+{
+    shared_ptr<uint64_t> cur = make_shared<uint64_t>(1);
+    shared_ptr<uint64_t> prev = make_shared<uint64_t>(1);
+    return [=]
+    {
+        const auto next = *cur + *prev;
+        const auto result = *prev;
+        *prev = *cur;
+        *cur = next;
+        return result;
+    };
+}
+
+void repeat(const int times, const function<void ()>& f)
+{
+    for(auto i = 0; i < times; ++i)
+        f();
+}
+
+int main()
+{
+    const auto fibonacci = makeFibonacci();
+    repeat(16, [&] { cout << fibonacci() << endl; });
+}
---
+++ Archive/fa43f8c293f76bdf3fdc11ef541e9e8c-e54ee7a04e4b807da0930236d4cc94dc/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <functional>
+#include <fstream>
+#include <cstdint>
+
+using namespace std;
+
+struct FileHelper
+{
+    static void write(const string& filePath, const function<void (ostream&)>& f)
+    {
+        ofstream file(filePath);
+        f(file);
+    }
+    
+    static void write(const string& filePath, const string& contents)
+    {
+        write(filePath, [&](ostream& s) { s.write(contents.c_str(), contents.size()); });
+    }
+};
+
+struct StringHelper
+{
+    static string stream(const function<void (ostream&)>& f)
+    {
+        stringstream ss;
+        f(ss);
+        return ss.str();
+    }
+};
+
+int main()
+{
+    int x = 42;
+    
+    FileHelper::write(
+        "ninjas.txt",
+        StringHelper::stream([&](ostream& s) { s << x; }));
+}
