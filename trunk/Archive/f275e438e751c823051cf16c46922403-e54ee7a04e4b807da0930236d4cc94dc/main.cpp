7627684977074d371510d6233b028d01ddc6eac2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-06-04 18:41:16 +0000

    Update Archive.

---
+++ trunk/Archive/f275e438e751c823051cf16c46922403-e54ee7a04e4b807da0930236d4cc94dc/main.cpp
@@ -0,0 +1,11 @@
+#include <boost/config.hpp>
+#include <boost/version.hpp>
+#include <iostream>
+
+int main()
+{
+  std::cout << "Boost: " << BOOST_LIB_VERSION << std::endl;
+  std::cout << "Compiler: " << BOOST_COMPILER << std::endl;
+  std::cout << "Standard Library: " << BOOST_STDLIB << std::endl;
+  return 0;
+}
