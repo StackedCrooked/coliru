1a6a128c5a7f38f167aba363f55874864304602d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-06-04 22:43:51 +0000

    Update Archive.

---
+++ Archive/5000407cace71b71e46e85e2724d5aa5-e54ee7a04e4b807da0930236d4cc94dc/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int poly = 0x4ba8;
+int data = 0xbba3;
+/*
+int random(){
+    int number = 0;
+    for(int bit = 0; i < 16)
+    for(int i = 0; i < 8; i++){
+    }
+};*/
+
+int main(){
+    /*
+    for(int i = 1; i <= 0x0008; i=i<<1){
+        std::cout<<i<<std::endl;
+    }*/
+    
+    /*
+    for(long int i = 0x0008; i <= 0x00000008; i=i<<1){
+        std::cout<<i<<std::endl;
+    }*/
+    
+    long int a = 0x00000008;
+    std::cout<<"->"<<a;
+}
