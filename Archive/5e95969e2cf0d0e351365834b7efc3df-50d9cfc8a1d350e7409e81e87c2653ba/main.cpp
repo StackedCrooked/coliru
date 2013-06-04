5a3846fe05510e543ddbf6afa5d055c572d85943
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-06-04 08:40:27 +0000

    Update Archive.

---
+++ Archive/5e95969e2cf0d0e351365834b7efc3df-50d9cfc8a1d350e7409e81e87c2653ba/main.cpp
@@ -0,0 +1,14 @@
+  #include     <iostream>
+    using std::cout;
+    using std::endl;
+
+    int main () 
+   {
+
+       const char* pointer = nullptr;
+
+       cout << "This the value of pointer   " << (void*)pointer << "." << endl;
+
+       return 0;
+
+   }
