26c26e69665c43c29901f4b0651a914e256d8038
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-06-07 08:30:38 +0000

    Update Archive.

---
+++ Archive/e5f361f2498bdeea92c8f4a533aee240-e54ee7a04e4b807da0930236d4cc94dc/main.cpp
@@ -0,0 +1,55 @@
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+#include <iostream>
+
+class Layer
+{
+public:
+    explicit Layer(std::string n, Layer* p = nullptr): name_(n), parent_(p) {}
+    std::string name() { return name_; }
+    Layer* parent() { return parent_; }
+private:
+    std::string name_;
+    Layer* parent_;
+};
+
+class City:     public Layer {     using Layer::Layer; };
+class Country:     public Layer {     using Layer::Layer; };
+class Planet:     public Layer {     using Layer::Layer; };
+
+typedef std::multimap<Layer*, Layer*> Atlas;
+
+int main()
+{
+    Planet Earth("Earth");
+    
+    Country USA("United States of America", &Earth);
+    Country FRA("France", &Earth);
+    
+    std::vector<City> USA_cities = { City("Los Angeles", &USA), City("New York", &USA) };
+    std::vector<City> FRA_cities = { City("Bordeaux", &FRA), City("Paris", &FRA) };
+    
+    Atlas a;
+    
+    a.insert(std::make_pair(&Earth, &USA));
+    a.insert(std::make_pair(&Earth, &FRA));
+    
+    for (auto& city: USA_cities)
+        a.insert(std::make_pair(&USA, &city));
+    
+    for (auto& city: FRA_cities)
+        a.insert(std::make_pair(&FRA, &city));
+        
+    auto countries = a.equal_range(&Earth);
+    std::cout << "Countries on planet " << countries.first->first->name() << " :\n";
+    for (auto it = countries.first; it != countries.second; ++it) {
+         auto country = it->second;
+         std::cout << country->name() << " (located on planet " << country->parent()->name() << "), with cities:\n";
+         auto cities = a.equal_range(country);
+         for (auto jt = cities.first; jt != cities.second; ++jt) 
+              std::cout << "     " << jt->second->name() << " (located in country " << jt->second->parent()->name() << ")\n";
+         std::cout << "\n";
+    }
+}
