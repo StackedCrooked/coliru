2b5020b4617d56022572947cdca860e543af3c71
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-06-07 07:30:30 +0000

    Update Archive.

---
+++ trunk/Archive/1dc94c991c3d6f8fcfbd87674f882de9-50d9cfc8a1d350e7409e81e87c2653ba/main.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+int main()
+{
+    std::vector<int> vector;
+    vector.push_back(0);
+
+    std::vector<int>::const_iterator vectorItr = vector.begin();
+    vector.erase(vectorItr);
+}
