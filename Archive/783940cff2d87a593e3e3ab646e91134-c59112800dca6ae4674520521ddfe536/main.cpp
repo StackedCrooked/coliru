6fa32b886c0ed0aba9feaa9ef91b16b139edc363
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-06-09 03:45:47 +0000

    Update Archive.

---
+++ Archive/783940cff2d87a593e3e3ab646e91134-c59112800dca6ae4674520521ddfe536/main.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <limits.h>
+ 
+int idiv_ceil ( int n, int d )
+{
+    return (n + d + (d>0)?-1:1) / d;
+}
+ 
+void test( int n, int d )
+{
+    int using_float = (int) std::ceil( (double)n / d );
+    int using_fn = idiv_ceil(n, d);
+    std::cout << std::setw(15) << n
+              << std::setw( 5) << d
+              << std::setw(15) << using_float
+              << std::setw(15) << using_fn
+              << std::setw( 8) << ((using_float == using_fn)? "ok" : "ERROR") << std::endl;
+}
+ 
+int main(void)
+{
+    test(INT_MAX, 1);
+    test(-INT_MAX, 1);
+    test(INT_MIN, 1);
+    for( int n = -5; n < 5; n++ ) {
+        for ( int d = 1; d < 5; d++ ) {
+            test(n, d);
+            test(n, -d);
+        }
+    }
+}
