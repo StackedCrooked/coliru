7ea21e5548857bca8aef2a669541046084b9cb55
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-06-09 19:49:43 +0000

    Update Archive.

---
+++ Archive/ebc320be7ac743754fda4d9a46a5cf48-ff683aff19d685e086e79e4ef634f9fb/main.cpp
@@ -0,0 +1,11 @@
+#include <unordered_set>
+
+class lol
+{
+    const std::unordered_set<double>& har;
+public:
+    lol(const std::unordered_set<double>& yodel = {}): har(yodel) {}
+};
+
+int main()
+{}
