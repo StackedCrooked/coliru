e9f8b36fe55afe8925e4017cb7f84c9b8755aeb8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-06-20 08:36:04 +0000

    Update Archive.

---
+++ trunk/Archive/65d1f0d97a3b2ef38b88025dc639a248-f674c1a6d04c632b71a62362c0ccfc51/main.cpp
@@ -0,0 +1,36 @@
+#include <functional>
+#include <iostream>
+
+
+using namespace std::placeholders;
+
+template <typename T>
+struct holder{
+    holder(int hold): hold_this(hold){}
+    
+    ~holder(){
+        call_on_delete(hold_this);
+    }
+    T call_on_delete;
+    int hold_this;
+};
+
+void style_a_x(int id) {
+    std::cout << "style_a_x(" << id << ")" << std::endl;
+}
+
+void style_a_y(int id) {
+    std::cout << "style_a_y(" << id << ")" << std::endl;
+}
+
+void style_b_x(int i, int* id) {
+    std::cout << "style_b_x(" << i << ", " << *id << ")" << std::endl;
+}
+
+void style_b_y(int i, int* id) {
+    std::cout << "style_b_y(" << i << ", " << *id << ")" << std::endl;
+}
+
+int main() {
+    holder<[](int a){style_a_x(a))>(69);
+}
