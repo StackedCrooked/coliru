d9cc9582fca01ba5c16ec1a77c9a56d1755771b1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-06-21 10:50:26 +0000

    Update Archive.

---
+++ trunk/Archive/e084917e1318bcdcb72a6cdae2537036-7f4bc2c90970fdf19c48ebf8cc2bae3f/main.cpp
@@ -0,0 +1,28 @@
+#include <cassert>
+#include <algorithm>
+#include <iostream>
+
+template <typename T, int N> class SafeArray
+{
+public:
+    SafeArray()
+    {
+        std::fill(m_data, m_data + N, T());
+    }
+    T &operator[](int index)
+    {
+        assert(0 <= index && index < N);
+        return m_data[index];
+    }
+private:
+    T m_data[N];
+};
+
+int main()
+{
+    SafeArray<bool, 3> b;
+    for (int i = 0; i < 3; i++)
+    {
+        std::cout << b[i] << " ";
+    }
+}
