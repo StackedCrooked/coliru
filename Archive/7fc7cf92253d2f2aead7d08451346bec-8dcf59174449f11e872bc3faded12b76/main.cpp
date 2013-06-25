25bca3cf04fee0dde825e3901034d6100d49b41a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-06-25 14:05:05 +0000

    Update Archive.

---
+++ Archive/7fc7cf92253d2f2aead7d08451346bec-8dcf59174449f11e872bc3faded12b76/main.cpp
@@ -0,0 +1,8 @@
+#include <array>
+//#include <initializer_list>
+// Note how it works without bring std::initializer_list into scope
+
+int main() {
+    std::array<int, 3> a = { 1, 2, 3 };
+    return a[0];
+}
