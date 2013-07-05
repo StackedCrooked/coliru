60a61ea73880f958151d1588756c65b944e91ed9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-07-05 16:01:22 +0000

    Update Archive.

---
+++ Archive/5fa639df0baf12c5ebc61cc89b1107b7-70854cb7d61c5b883318b4d11e427bb9/main.cpp
@@ -0,0 +1,23 @@
+#include <thread>
+#include <future>
+
+template<typename T>
+struct X
+{
+    X() : t() { }
+
+    X(X&& x) : X(std::unique_lock<std::mutex>(x.m), std::move(x)) { }
+
+    X(std::unique_lock<std::mutex> const& l, X&& x) :
+        X(std::move(x))
+    {
+    }
+
+private:
+    std::mutex m;
+    T t;
+};
+
+int main()
+{
+}
