c55f8ce407d03e2014d0ffcdbbad99ce2c0c4bf9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-07-11 23:16:46 +0000

    Update Archive.

---
+++ trunk/Archive/833325984c405364fcb7293bb4c6366c-f674c1a6d04c632b71a62362c0ccfc51/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <functional>
+
+namespace you {
+    class suck {
+        unsigned long long big_balls;
+        struct k{
+          int a;
+          int b;
+          int c;
+        };
+        
+        k k_is_now_a_member;
+    };
+}
+
+
+int main ( int argc, char* argv[] ) {
+    const static std::size_t sz = sizeof( you::suck );
+    
+    std::cout << sz << std::endl;
+    
+}
