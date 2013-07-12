dc31a327dcf2bff56c8b08720445cc1d63de1d2b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-07-12 23:47:11 +0000

    Fix defunct processes caused by not killing.

--- trunk/Web/webserver-sinatra.rb
+++ trunk/Web/webserver-sinatra.rb
@@ -252,7 +252,6 @@ def safe_popen(cmd)
         end
     rescue Timeout::Error => e
         # Kill the process group that started the sandbox
-        IO.popen("./ps.sh | grep 2002 | grep -v grep | awk '{print $1}' | sort | uniq | xargs -I {} kill -9 -{}") {||}
         Process.kill(9, Process.getpgid(@stdout.pid))
         Process.wait @stdout.pid
         yield e.to_s
