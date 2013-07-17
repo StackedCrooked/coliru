2ba7bf666ff856fc97fbce5fa6570781611e4d4f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-07-17 09:50:46 +0000

    Update Archive.

---
+++ trunk/Archive/88f67c32fdbada299685f0274aa7a254-f674c1a6d04c632b71a62362c0ccfc51/main.cpp
@@ -0,0 +1,64 @@
+#include <boost/algorithm/string.hpp>
+#include <cstddef>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+namespace detail {
+    template<typename T>
+    class map_tie_t {
+    public:
+        map_tie_t(std::vector<T*> variables_, std::vector<std::string> names_)
+            : variables(std::move(variables_))
+            , names(std::move(names_)) {
+
+        }
+        
+        map_tie_t& operator=(std::unordered_map<std::string, T>&& map) {
+            for (std::size_t i = 0; i < names.size(); ++i) {
+                *variables[i] = map.at(names[i]);
+            }
+        }
+    
+    private:
+        std::vector<T*> variables;
+        std::vector<std::string> names;
+    };
+    
+    template<typename T>
+    T* to_pointer(T& variable) {
+        return std::addressof(variable);
+    }
+    
+    template<typename... Ts>
+    map_tie_t<typename std::common_type<Ts...>::type> map_tie(Ts&... variables, std::string const& csnames) {
+        std::vector<std::string> names;
+        boost::split(names, csnames, ',');
+        for (auto& name : names) {
+            boost::trim(name);
+        }
+
+        return map_tie_t<typename std::common_type<Ts...>::type>({ to_pointer(variables)... }, std::move(names));
+    }
+}
+
+#define MAP_TIE(...) ::detail::map_tie(__VA_ARGS__, ::std::string(#__VA_ARGS__))
+
+int main() {
+    std::unordered_map<std::string, int> map{
+        {"a", 42},
+        {"b", 314},
+        {"c", 1337},
+    };
+
+    int a = 0, b = 0, c = 0;
+    MAP_TIE(b, c) = map;
+    
+    std::cout << a << ", " << b << ", " << c << '\n';
+    
+    return 0;
+}
