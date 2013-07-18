997d8a425e6884743ca38cbd99c9af4cb0f1fb18
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-07-18 16:29:29 +0000

    Update Archive.

---
+++ Archive/a148b29be11fe60a77d4265c9e23cadd-f674c1a6d04c632b71a62362c0ccfc51/main.cpp
@@ -0,0 +1,25 @@
+class SomeClass
+{
+public:
+    // Constructors and destructors.
+    SomeClass();
+    SomeClass(const SomeClass & other);
+    SomeClass operator =(const SomeClass & other);
+    SomeClass(const OtherClass & other); 
+    ~SomeClass();
+      
+    // Functions that "do stuff" with this class.
+    int SomeFunc();
+    int OtherFunc();
+    
+    // Public member variables.
+    int value; 
+
+private:
+    // Internal use functions. 
+    int SomePrivateFunc();
+    int OtherPrivateFunc();
+
+    // Private member variables.
+    int x, y; 
+};
