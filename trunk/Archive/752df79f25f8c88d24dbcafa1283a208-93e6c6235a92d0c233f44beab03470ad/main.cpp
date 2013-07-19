ad96fba1d9038a009eecd2897454b0db266598e7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-07-19 21:03:25 +0000

    Update Archive.

---
+++ trunk/Archive/752df79f25f8c88d24dbcafa1283a208-93e6c6235a92d0c233f44beab03470ad/main.cpp
@@ -0,0 +1,66 @@
+#include <type_traits>
+#include <algorithm>
+
+/* Helpers */
+template<typename T>
+using Type = typename T::type;
+
+template<typename T>
+using Unqualified = Type<std::remove_reference<Type<std::remove_cv<T>>>>;
+
+template<typename Specialization, typename Target>
+struct rebind {};
+
+/* Sensible default: assume first parameter is for the target */
+template<template<typename...> class Cont, typename T, typename... Ts, typename Target>
+struct rebind<Cont<T, Ts...>, Target> {
+    using type = Cont<Target, Ts...>;
+};
+
+/* Special-case */
+template<typename Old, std::size_t N, typename Target>
+struct rebind<std::array<Old, N>, Target> {
+    using type = std::array<Target, N>;
+};
+
+template<typename Specialization, typename Target>
+using Rebind = Type<rebind<Specialization, Target>>;
+
+#include <tuple>
+
+template<typename T>
+struct function_traits : public function_traits<decltype(&T::operator())> {};
+
+template<typename T, typename R, typename... Args>
+struct function_traits<R(T::*)(Args...) const> {
+    
+    static constexpr size_t args = sizeof...(Args);
+
+    using result_type = R;
+    template<size_t i>
+    struct arg {
+        using type = typename std::tuple_element<i,std::tuple<Args...>>::type;
+    };
+};
+
+template<typename T>
+using Resultant = typename function_traits<T>::result_type;
+
+template<class Cont, typename Map>
+auto map(const Cont& cont, Map&& mapped) -> Rebind<Cont, Resultant<Unqualified<Map>>> {
+    Rebind<Cont, Resultant<Unqualified<Map>>> result;
+    auto result_iterator = std::back_inserter(result);
+    for(const auto& elem : cont) {
+        *result_iterator = mapped(elem);
+    }
+    return result;
+}
+
+#include <iostream>
+
+int main() {
+    auto i = map(std::vector<int>{1,2,3,4,5,6}, [](int x) { return x % 2 == 0; });
+    for(auto&& j : i) {
+        std::cout << j << ' ';
+    }
+}
---
+++ trunk/Archive/f6a46a6d660e386c53208bb9da5da78f-f674c1a6d04c632b71a62362c0ccfc51/main.cpp
@@ -0,0 +1,59 @@
+#include <boost/mpl/for_each.hpp>
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/transform_view.hpp>
+#include <boost/phoenix/phoenix.hpp>
+#include <iostream>
+
+namespace phx = boost::phoenix;
+namespace mpl = boost::mpl;
+
+/////////////////////////////////////////////////////////
+// plumbing added just to make it compile...
+// this is "your shit" :)
+namespace c_funs { namespace basic {
+    template <typename> struct void_unary { };
+    template <typename> struct void_binary_same { };
+    template <typename, typename> struct void_binary_diff { };
+} }
+
+struct lua_t 
+{ 
+    static constexpr struct Nil {} nil = {};
+    template <typename CF> void register_function(std::string const& n, CF&) const 
+    {
+        std::cout << "ACK: lua_t::register_function(\"" << n << "\", " << typeid(CF).name() << ")\n";
+    }
+};
+
+struct nil {};
+// end shit
+/////////////////////////////////////////////////////////
+
+struct wrap_ref_func {
+    
+    wrap_ref_func(lua_t& state) : state(state) {}
+    lua_t& state;
+
+    template <typename> struct result { typedef void type; };
+    template <typename CFunction>
+    void operator()(CFunction& f) const 
+    { 
+        state.register_function("foo", f);
+    }
+};
+
+typedef mpl::vector<signed int, double, bool, std::string, nil> basic_types;
+
+int main()
+{
+    using namespace boost::phoenix::arg_names;
+
+    lua_t lua;
+    phx::function<wrap_ref_func> register_function(lua);
+
+    typedef mpl::transform_view<basic_types, c_funs::basic::void_binary_same<mpl::_> > bin_same_functions;
+    mpl::for_each<bin_same_functions>(register_function(arg1));
+
+    typedef mpl::transform_view<basic_types, c_funs::basic::void_unary<mpl::_> > unary_functions;;
+    mpl::for_each<unary_functions>(register_function(arg1));
+}
