82830e97be23cd86f6ee8b4989e9d5c8b72248a3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-07-20 09:20:26 +0000

    Update Archive.

---
+++ trunk/Archive/f4dcd50cc3967df552395c0f4f5f963e-93e6c6235a92d0c233f44beab03470ad/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+struct A {
+    int a;
+    A(int b): a(b) { std::cout << "A()\n"; }
+    A(A&& a): a(a.a) { std::cout << "A(A&&)\n"; }
+    A(const A&) = delete;
+    A& operator=(const A&) = delete;
+    A& operator=(A&&) = delete;
+};
+
+#include <vector>
+
+int main() {
+    std::vector<A> a;
+    a.push_back(A(20));
+    a.push_back(A(30));
+}
