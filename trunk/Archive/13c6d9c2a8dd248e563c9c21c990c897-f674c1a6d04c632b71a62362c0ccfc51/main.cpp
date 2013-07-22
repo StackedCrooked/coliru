d6cb6cab7887085002eb980cff5483f94a9d9da3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-07-22 05:20:39 +0000

    Update Archive.

---
+++ trunk/Archive/13c6d9c2a8dd248e563c9c21c990c897-f674c1a6d04c632b71a62362c0ccfc51/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+
+int main()
+{
+   cout<<"\nHello Kartik";
+}
