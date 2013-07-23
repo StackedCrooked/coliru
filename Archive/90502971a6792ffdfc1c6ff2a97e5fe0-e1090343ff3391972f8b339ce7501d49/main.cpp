e4e04523d9505e859e8abcc30e08441487f355c0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-07-23 17:22:21 +0000

    Update Archive.

---
+++ Archive/90502971a6792ffdfc1c6ff2a97e5fe0-e1090343ff3391972f8b339ce7501d49/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+//define.hpp
+
+class A{
+public:
+
+int getA() const;
+int getAa() const;
+
+};
+
+ostream& operator<<(ostream& out, const A& obj); // defined outside of the class
+
+
+
+//implement.cpp
+
+ostream& operator<<(ostream& out, const A& obj){
+    return out << obj.getA() 
+                << obj.getAa()
+                << endl;
+};
+
+int A::getA() const{ return 0; }
+int A::getAa() const{ return 0; }
+
+int main(){
+    return 0;
+}
