fee49451e2e4b41047e27382b548f78bce7a0451
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-07-23 15:20:47 +0000

    Update Archive.

---
+++ Archive/5fbc22e56bfeee69e2061492d4c827b2-e1204655eaff68246b392dc70c5a32c9/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+void fun ( map<string, map<string, int> > &m1, map<string, map<string, int> > &) {
+    m1["abc"]["xyz"] = 1;
+}
+
+int main() {
+    map<string, map<string, int> > m[10];
+    fun(m[0], m[1]);
+    cout << m[0]["abc"]["xyz"] << endl;
+}
