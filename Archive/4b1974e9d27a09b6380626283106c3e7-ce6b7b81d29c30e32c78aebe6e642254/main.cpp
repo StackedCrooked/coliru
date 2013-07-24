4d3965b2a1ddd67ce43edc3dc2181792c8966fad
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-07-24 09:55:31 +0000

    Update Archive.

---
+++ Archive/4b1974e9d27a09b6380626283106c3e7-ce6b7b81d29c30e32c78aebe6e642254/main.cpp
@@ -0,0 +1,66 @@
+#include <string>
+#include <set>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+class Base
+{ protected:
+    string STR__Description;  // Pardon my non-standard style
+  public:
+    virtual
+    string GetDescription() const
+    { return STR__Description;
+    }
+    void   SetDescription( string str__Description )
+    { STR__Description = str__Description;
+    }
+};
+
+class ArmyBase: public Base
+{ public:
+    string GetDescription() const
+    { return STR__Description + " (Army)";
+    }
+};
+
+class NavyBase: public Base
+{ public:
+    string GetDescription() const
+    { return STR__Description + " (Navy)";
+    }
+};
+
+class Cmp
+{
+public:
+    bool operator()(Base const* lhs, Base const* rhs) const
+    { return lhs->GetDescription() < rhs->GetDescription(); };
+};
+
+int main()
+{
+
+    Base                         base;
+    ArmyBase                     base_Army;
+    set<Base*, Cmp>           set__ArmyBases;
+    map<string, Base*>      map__Bases;
+    map<string, Base*>::iterator         iter_Bases;
+    NavyBase                     base_Navy;
+    set<Base*, Cmp>          set__NavyBases;
+    
+    
+    base_Army = ArmyBase(); 
+    base_Navy = NavyBase();
+    
+    set__ArmyBases.insert(                   &base_Army   );
+    map__Bases.insert(     pair<string, Base*>( "Boca Raton",    &base_Army ) );
+    
+    
+    set__NavyBases.insert(                   &base_Navy   );
+    map__Bases.insert(     pair<string, Base*>( "NAS Pensacola", &base_Navy ) );
+    
+    for (auto b: map__Bases)
+        std::cout << b.first << "..." << b.second->GetDescription() << std::endl;
+}
