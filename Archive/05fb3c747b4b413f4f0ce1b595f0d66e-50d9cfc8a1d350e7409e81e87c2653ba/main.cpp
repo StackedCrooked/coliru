2284644a70f1e7aba1dd98d9ac8e7df6f5761ab6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-07-24 01:39:48 +0000

    Update Archive.

---
+++ Archive/05fb3c747b4b413f4f0ce1b595f0d66e-50d9cfc8a1d350e7409e81e87c2653ba/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+#define let \
+  if(bool _done_ = false){} else \
+  for(auto&&
+  
+#define in \
+  ; !_done_; _done_ = true)
+  
+std::vector<int> g(){ return {1,2,3,4,5}; }
+
+int main(){
+    let v = g() in std::cout << std::accumulate(v.begin(), v.end(), 0) << "\n";
+}
