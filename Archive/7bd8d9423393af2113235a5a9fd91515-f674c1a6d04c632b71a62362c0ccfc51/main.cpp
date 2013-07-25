17cd4adee7336f95a59446e3b3bd1aba337a425d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-07-25 18:35:22 +0000

    Update Archive.

---
+++ Archive/7bd8d9423393af2113235a5a9fd91515-f674c1a6d04c632b71a62362c0ccfc51/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   char myName;
+    
+   cout << "Hello World" << endl; 
+   cout<< endl; cout<<endl;
+   cout << "I'm here" << endl; 
+   cout << "My name is "; 
+   cin >> myName;
+   cout << myName << endl;
+   
+   return 0;
+}
---
+++ Archive/82b59bb2fe2c56a6e21856f11bcf6c24-d41d8cd98f00b204e9800998ecf8427e/main.cpp
@@ -0,0 +1,29 @@
+
+
+//
+// wrapper function which has a name that more closely matches the user's intent
+//
+int bounded(int x, int lower, int upper = x)
+{
+    return std::max(x, y);    
+}
+
+
+//
+// simple type wrapper
+//
+auto result = bounded<int, 0, 9>{};
+result = x;
+
+
+//
+// goofy functor
+//
+auto result = at_least(10)(x);
+
+
+//
+// silly expression template
+//
+auto result = (0 <= boost::convolutions2::bondage::clamp{} <= 10);
+result = x;
