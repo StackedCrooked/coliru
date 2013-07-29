de51e2983d2c3b689264c7bf4ac287a41e5e1ac4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-07-29 20:59:29 +0000

    Update Archive.

---
+++ Archive/b7e94bcb859577f782e9e34557127bc2-7f4bc2c90970fdf19c48ebf8cc2bae3f/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+  while(1);  // http://stackoverflow.com/questions/3592557/optimizing-away-a-while1-in-c0x
+  std::cout << "hi" << std::endl;
+}
