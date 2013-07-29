c23c6f0bcc90d221a55b517980c6414a256d3543
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-07-29 15:51:59 +0000

    Update Archive.

---
+++ trunk/Archive/3ae0c6e8fb2d091f64563c8e14a14fd5-b24e7def60aba11ee8094350eb73164e/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class factorial {
+public:
+  int operator()(int n) {
+    if (n == 0)
+      return 1;
+    return n * (*this)(n-1);
+  }
+};
+
+int main()
+{
+    std::cout << factorial()(5) << std::endl;
+}
