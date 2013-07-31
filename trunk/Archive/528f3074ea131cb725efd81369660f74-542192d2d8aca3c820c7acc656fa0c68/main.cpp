cdda57c5f0f1118ef26826c8e66ffcc237dda6af
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-07-31 21:06:46 +0000

    Update Archive.

---
+++ trunk/Archive/528f3074ea131cb725efd81369660f74-542192d2d8aca3c820c7acc656fa0c68/main.cpp
@@ -0,0 +1,5 @@
+  A B C D 
+A 0 0 1 0 A referred 2 (A referred C, C referred B)
+B 0 0 1 0 B referred 2 (B referred C, C referred A) 
+C 1 1 0 0 C referred 2 (C referred A and B)
+D 0 1 1 0 D referred 4 (D referred B and C, C referred A)
