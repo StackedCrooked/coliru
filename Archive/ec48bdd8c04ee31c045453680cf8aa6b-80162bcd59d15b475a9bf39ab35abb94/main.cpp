af5c76d9e6a0abf1e3b011638e0a578ef580b47e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-08-01 11:34:25 +0000

    Update Archive.

---
+++ Archive/ec48bdd8c04ee31c045453680cf8aa6b-80162bcd59d15b475a9bf39ab35abb94/main.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <string>
+#include <wctype.h>
+#include <iostream>
+int main() {
+    std::wstring data = L"Straße";
+    std::wcout << data << '\n';
+    std::wcout << data.size() << '\n';
+    std::transform(data.begin(), data.end(), data.begin(), ::towupper);
+    std::wcout << data << '\n';
+    std::wcout << data.size() << '\n';
+}
