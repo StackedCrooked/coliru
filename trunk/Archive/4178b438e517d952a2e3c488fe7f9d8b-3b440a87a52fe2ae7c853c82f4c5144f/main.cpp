4754fe339a7f8fe9df7efbaaa60f43fadc438872
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-08-02 08:08:55 +0000

    Update Archive.

---
+++ trunk/Archive/4178b438e517d952a2e3c488fe7f9d8b-3b440a87a52fe2ae7c853c82f4c5144f/main.cpp
@@ -0,0 +1,14 @@
+struct S;
+
+template<typename T>
+void call(T *, void (T::*)()){}
+
+struct S {
+    void foo(){}
+    
+    void bar() const {
+        call(this, &S::foo);
+    }
+};
+
+int main() {}
