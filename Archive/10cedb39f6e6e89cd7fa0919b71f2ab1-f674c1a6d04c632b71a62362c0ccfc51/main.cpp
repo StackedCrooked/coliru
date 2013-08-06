f77ad4db6715af976084f3c5fe521e0fcfebf7f2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-08-06 08:33:17 +0000

    Update Archive.

---
+++ Archive/10cedb39f6e6e89cd7fa0919b71f2ab1-f674c1a6d04c632b71a62362c0ccfc51/main.cpp
@@ -0,0 +1,44 @@
+namespace w {
+    template<class T>
+    struct wrapper { T t; };
+    
+    template<class T> 
+    T const& unwrap(T const& t) {
+        return t;
+    }
+    
+    template<class T>
+    T const& unwrap(wrapper<T> const& w) {
+        return w.t;
+    }
+    
+    template<class T1,class T2>
+    auto operator +(T1 t1, T2 t2) -> decltype(unwrap(t1)+unwrap(t2)) {
+        return unwrap(t1)+unwrap(t2);
+    }
+    template<class T1,class T2>
+    auto operator -(T1 t1, T2 t2) -> decltype(unwrap(t1)-unwrap(t2)) {
+        return unwrap(t1)-unwrap(t2);
+    }
+
+    template<class T1,class T2>
+    auto operator *(T1 t1, T2 t2) -> decltype(unwrap(t1)*unwrap(t2)) {
+        return unwrap(t1)*unwrap(t2);
+    }
+}    
+
+// Test case
+struct test {};
+
+test operator+(test const&, test const&) { return test(); }
+test operator-(test const&, test const&) { return test(); }
+
+int main() {
+    test() + test();
+    w::wrapper<test>() + w::wrapper<test>();
+    
+    w::wrapper<test>() + test();
+    test() - w::wrapper<test>();
+
+    return 0;
+}
