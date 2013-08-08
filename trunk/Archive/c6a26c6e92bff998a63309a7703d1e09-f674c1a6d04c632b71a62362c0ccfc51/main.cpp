6511b461834c0ac286af90f0d213850cf29beaef
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-08-08 20:46:28 +0000

    Update Archive.

---
+++ trunk/Archive/c6a26c6e92bff998a63309a7703d1e09-f674c1a6d04c632b71a62362c0ccfc51/main.cpp
@@ -0,0 +1,8 @@
+        if(CadastroAlunos[k][2] == NumeroCursoPesquisa){
+            if(SexoPesquisa == CadastroAlunos[k][1]){
+                if(CadastroAlunos[k][3] > NotaAuxiliar){
+                    NotaAuxiliar = CadastroAlunos[k][3];
+
+                }
+            }
+        }
