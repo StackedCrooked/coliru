995d8ee5f757837bbea7524f4c8564a5aa0fb375
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-08-09 21:34:18 +0000

    Update Archive.

---
+++ Archive/6a21d5531a6e1849dcccb1e3b5a4b0d2-cc73e281b3b6abc9bf6cf4f153b944a6/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+
+#include "lundi.hpp"
+
+
+//I use object.get(name) and object.set(name, value) on the assumption
+//implementation is easier.  But if possible, object["name"] = value would be better.
+
+void exceptionErrorReporter(std::string const& s) {
+    throw lua::exception(s);
+}
+
+class person {
+    int age;
+public:
+    person(std::string name_, int age_) :name(name_), age(age_) {}
+    std::string name;
+    int print_age() const {std::cout << age;}
+};
+
+//lua types only loosly mirror cpp types
+lua::table assign_to_lua(lua::state& lua, const person& src, lua::variant& dst) {
+    lua::table t = dst.assign_new_table(); //I'm not sure I like this at all.  
+    t.set("name", name); //t and dst refer to the same lua variable here
+    t.set("age", age);
+    t.set("print_age", person::get_age);
+    return t;
+}
+
+person create_from_lua(lua::state& lua, lua::variant& src) {
+    return person(src.get("name"),src.get("age"));
+}
+
+//potentially comes with a default based on create_from_lua
+bool assign_from_lua(lua::state& lua, lua::variant& src, const person& dst) {
+    dst = person(src.get("name"),src.get("age"));
+    return true;
+}
+
+
+
+
+
+void f_cpp(const person& p) {std::cout << p.print_age() << '\n';}
+
+int main() {
+    lua::state lua(&exceptionErrorReporter);
+    
+    // cpp -> lua conversion
+    person p1("STEVE", 20);    
+    lua::variant p2 = assign_to_lua(lua, p1, lua.global().get("v"));
+    lua.eval("print(v.name, \" \", v.age)");
+    lua.eval("v.print_age()");
+    
+    // lua -> cpp conversion
+    person p2 = create_from_lua(lua, lua.global().get("v"));
+    std::cout << p2.name << " " << p2.get_age() << '\n';
+    p2 = assign_from_lua(lua, lua.global().get("v"));
+    std::cout << p2.name << " " << p2.get_age() << '\n';
+    
+    // pass C++ object to lua function
+    lua.eval("f = function(p) p.print_age() end");
+    lua.global().call("f")(p1);
+    
+    //pass lua object to C++ function
+    lua.global().set("f", f_cpp);
+    lua.global().call("f")(lua.global().get("v")); 
+}
+
