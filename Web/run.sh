4c961a55a023c1fc219849395d400fba420f7e61
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-08-13 19:45:52 +0000

    Fix start scripts.

--- Web/run.sh
+++ Web/run.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
+set -x
 if [ "$(whoami)" != "root" ] ; then
     echo "$(basename $0) must be run with root permissions." 1>&2
     exit 1
 fi
 
-exec > >(logger -t $0) 2>&1
+source coliru_env.source
 ./repair-permissions.sh
-if [ "$(hostname)" == "stacked-crooked" ] ; then
-    URL="stacked-crooked.com"
-else
-    IP="$(ifconfig | grep 'inet addr:' | head -n1 | perl -pe 's,.*addr:(\d+\.\d+\.\d+\.\d+).*,\1,')"
-    URL="${IP}"
-fi
-echo "Binding to: ${URL}"
-
-
-sudo -u webserver ruby webserver-sinatra.rb -o ${URL}
+setsid sudo -u webserver -E bash -c "
+    exec > >(logger -t $0) 2>&1 
+    ruby webserver-sinatra.rb -o stacked-crooked.com
+    "
