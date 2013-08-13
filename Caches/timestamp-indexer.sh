e1bff4e675238c4b00e0fab563b1ac9b128ec192
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-08-13 20:34:55 +0000

    Add run script.

--- Caches/timestamp-indexer.sh
+++ Caches/timestamp-indexer.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-set -x
+if [ "$(whoami)" != "webserver" ] ; then
+    echo "The indexer must be run by the webserver." 1>&2
+    exit 1
+fi
 cd $(dirname $0)
 
 find ../Archive -name timestamp >_timestamps
