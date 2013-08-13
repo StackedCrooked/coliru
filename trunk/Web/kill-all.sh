640448421caf31b93a86393a4940bf3f9683b0ee
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-08-13 18:28:51 +0000

    Improve kill-all.

--- trunk/Web/kill-all.sh
+++ trunk/Web/kill-all.sh
@@ -1,1 +1,7 @@
-ps -eopgid,ppid,pid,comm | grep 'restart.sh' | awk '{print $1}'
+PGID="$(ps -eopgid,ppid,pid,comm | grep 'restart.sh' | awk '{print $1}')"
+if [ "${PGID}" != "" ] ; then
+    echo "Killing pgid ${PGID}"
+    kill -9 -${PGID}
+else
+    echo "restart.sh does not seem to be running." 1>&2 ; exit 1
+fi
