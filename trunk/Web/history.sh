1659c3bd70a27cdeb40560e6ac510d0bc2769432
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-08-13 20:36:23 +0000

    Update history.sh.

--- trunk/Web/history.sh
+++ trunk/Web/history.sh
@@ -11,7 +11,7 @@ get_entries_by_date() {
         for entry in $(get_timestamp_files) ; do
             printf "$(cat $entry)_$(dirname $entry)\n"
         done
-    } | sort -n -r | head -n 100
+    } | sort -n -r | head -n 500
 }
 
 convert_epoch_to_readable_format() {
@@ -22,15 +22,15 @@ print_file() {
     CHARACTERS=$(cat $1 | wc -c)
     [ $CHARACTERS -lt 8000 ] || { printf "$(basename $1): too big ($CHARACTERS characters)\n\n" ; return ; }
     printf "$(basename $1):\n"
-    cat $1 | sed -e 's,\(.*\),       \1,'
+    cat $1 | sed -e 's,\(.*\),  \1,'
     printf "\n\n"
 }
 
 print_entry() {
     printf "=== $2 $(basename $1) ===\n" | sed -e 's,DATE_SEP, ,g'
-    print_file "$1/cmd.sh"
-    print_file "$1/output"
     print_file "$1/main.cpp"
+    print_file "$1/output"
+    print_file "$1/cmd.sh"
     printf "\n\n"
 }
 
@@ -63,29 +63,19 @@ PART_FILE="${HISTORY_FILE}.part"
 }
 
 
-print_partial_results() {
-    [ -f $PART_FILE ] && {
-        echo "Note: partial results. refresh to see more."
-        cat $PART_FILE
-    }
-}
-
-
-
 export PREVIOUS_RESULTS="$({ ls /tmp/history* -t1 | grep -v part |  head -n1 ; } 2>/dev/null)"
-if [[ "${PREVIOUS_RESULTS}" != "" ]] ; then
-    echo "Printing previous results while new results are getting generated." 
+if [ "${PREVIOUS_RESULTS}" != "" ] ; then
     cat ${PREVIOUS_RESULTS}
-else
-    echo "No previous results found. Looking for partial results."
-    print_partial_results
 fi
 
 # Presence of the part file indicates that another process is already working on it.
 [ -f $PART_FILE ] && {
-    echo "Another process is already generating the new results. Exiting."
+    echo "Partial results. Refresh later to see more."
+    cat $PART_FILE
     exit
 }
+
+echo "Generating history for the first time. This may take a few minutes. Refresh a little bit later to see the results."
 
 {
     echo "Note: generating results. refresh to see more."
