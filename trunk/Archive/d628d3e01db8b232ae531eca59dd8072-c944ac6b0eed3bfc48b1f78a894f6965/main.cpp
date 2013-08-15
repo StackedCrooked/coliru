a0145f2435b234ab278b43366e95976582cb8182
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-08-15 23:15:23 +0000

    Update Archive.

---
+++ trunk/Archive/d628d3e01db8b232ae531eca59dd8072-c944ac6b0eed3bfc48b1f78a894f6965/main.cpp
@@ -0,0 +1,25 @@
+#include <array>
+#include <tuple>
+#include <string>
+
+template <typename ArrayType, std::size_t ArraySize, typename... Fields>
+struct ArrayTuple : ArrayTuple<ArrayType, ArraySize-1, ArrayType, Fields...> {};
+template <typename ArrayType, typename... Fields>
+struct ArrayTuple<ArrayType, 0, Fields...> {
+    typedef std::tuple<Fields...> type;
+};
+
+template <typename, typename, typename> class Test;
+
+template <typename Scalar, typename ArrayType, std::size_t ArraySize, typename... Fields>
+class Test<Scalar, std::array<ArrayType, ArraySize>, std::tuple<Fields...>> {
+public:
+    // Modified tuple_cat code from Xeo's comment:
+    using Tuple = std::tuple<Fields...>;
+    using Types = decltype(std::tuple_cat(std::tuple<Scalar>(), typename ArrayTuple<ArrayType, ArraySize>::type(), std::declval<Tuple>()));
+};
+
+int main() {
+    using expected = std::tuple<int, double, double, double, char, float, std::string>;
+    static_assert(typename std::is_same<Test<int, std::array<double, 3>, std::tuple<char, float, std::string>>::Types, expected>(), "...");
+}
