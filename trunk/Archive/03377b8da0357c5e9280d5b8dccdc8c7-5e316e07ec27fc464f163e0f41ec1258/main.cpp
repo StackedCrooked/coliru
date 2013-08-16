498830ec6034df00c233a004996fa497fc3743e3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-08-16 09:33:14 +0000

    Update Archive.

---
+++ trunk/Archive/03377b8da0357c5e9280d5b8dccdc8c7-5e316e07ec27fc464f163e0f41ec1258/main.cpp
@@ -0,0 +1,24 @@
+#include <boost/range/any_range.hpp>
+#include <boost/range/adaptors.hpp>
+ #include <boost/type_traits.hpp>
+#include <memory>
+#include <vector>
+
+struct IFooBar {
+    virtual void func() = 0;
+};
+
+typedef boost::any_range<IFooBar,
+                         boost::random_access_traversal_tag,
+                         const IFooBar&,
+                         std::ptrdiff_t> FooBarRange;
+
+void func( const FooBarRange& range ) {
+}
+
+int main() {
+    std::vector<std::unique_ptr<IFooBar>> vec;
+    func( vec | boost::adaptors::indirected );
+
+    return 0;
+}
