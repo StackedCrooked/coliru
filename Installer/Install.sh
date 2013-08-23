d28c10da1fcf6d295a4c4fe639c482b47373275e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-08-23 03:15:11 +0000

    Update Install.sh with todo item for installing locales.

--- Installer/Install.sh
+++ Installer/Install.sh
@@ -33,7 +33,7 @@ apt-get install -y dchroot debootstrap
 
 
 apt-get install -y libcap2-bin ruby-dev rubygems lsof rsync subversion
-gem install sinatra shotgun popen4 json --no-ri --no-rdoc
+gem install sinatra shotgun popen4 json sinatra-cross_origin --no-ri --no-rdoc
 
 
 # Allow ruby to bind to port 80
@@ -90,3 +90,27 @@ sudo service whoopsie stop || true
 #apt-get install mercurial
 #hg clone https://bitbucket.org/martinhofernandes/wheels
 
+ # TODO: Install locales
+ #1984  for i in $(echo en_US.utf8 ja_JP.utf8 sv_SE.utf8 ru_RU.utf8 vi_VN.utf8) ; do locale-gen $i ; locale-gen ${i}.UTF8 ; done ; update-locale
+ #1985  locale-gen vi_VN.UTF8
+ #1986  echo 'for i in $(echo en_US.utf8 ja_JP.utf8 sv_SE.utf8 ru_RU.utf8 vi_VN.utf8) ; do locale-gen $i ; locale-gen ${i}.UTF8 ; done ; update-locale' | perl -pe 's,\.utf8,,g'
+ #1987  for i in $(echo en_US ja_JP sv_SE ru_RU vi_VN) ; do locale-gen $i.utf8 ; locale-gen ${i}.UTF8 ; done ; update-locale
+ #1988  locale-gen vi.utf8
+ #1989  locale-gen vi.UTF8
+ #1990  locale-gen vi_VN.utf8
+ #1991  locale-gen vi_VN.UTF8
+ #1992  locale-update
+ #1993  update-locale
+ #1994  locale-gen vi_VN
+ #1995  locale-gen vi_VN.utf8
+ #1996* locale-gen cs_CZ.utf8
+ #1997  update-locale
+ #1998  locale-gen vi_VN.utf8
+ #1999  locale-gen cs_CZ.utf8
+ #2000  locale-gen cs_CZ.UTF8
+ #2001  locale-gen zh_CN.gb18030
+ #2002  update-locale
+ #2003  locale-gen zh_CN.gb18030
+ #2004  update-locale
+ #2005  history | grep locale
+ #2006  history | grep locale >>~/coliru/Installer/Install.sh 
