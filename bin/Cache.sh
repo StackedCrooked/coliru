ee5a5a6c2bfed5e7e3d8ed44a54db9e68853c7ce
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-08-29 18:53:46 +0000

    Remove Cache.sh.

--- bin/Cache.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cache_dir="${HOME}/.Cache.sh"
-mkdir -p "${cache_dir}"
-cache_file="${cache_dir}/$(echo $@ | base64)"
-[ -f "${cache_file}" ] || {
-    echo "$($@)" >"$cache_file"
-}
-cat "${cache_file}"
