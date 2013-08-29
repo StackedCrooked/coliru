d012276ebdac1f303fc532c57b3373cf86300467
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-08-29 21:38:01 +0000

    Add hash.sh.

---
+++ branches/wip/Web/hash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+[ "$INPUT_FILES_DIR" == "" ] && { echo "INPUT_FILES_DIR is not set." 1>&2 ; exit 1 ; }
+[ "$INPUT_FILES_DIR" == "." ] && { echo "INPUT_FILES_DIR is jsut a dot." 1>&2 ; exit 1 ; }
+sum="$(shasum ${INPUT_FILES_DIR}/cmd.sh | cut -d ' ' -f1)"
+[ -f main.cpp ] && {
+    sum="$(shasum ${INPUT_FILES_DIR}/main.cpp | cut -d ' ' -f1)${sum}"
+    sum="$(echo ${sum} | shasum | cut -d ' ' -f1 | cut -c -10)"
+}
+echo "${sum}"
