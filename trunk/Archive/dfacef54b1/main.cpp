7bb2c25cd2dc7ee7621f0c71c1ecb5295db44302
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-08-30 17:39:08 +0000

    Update Archive.

---
+++ trunk/Archive/dfacef54b1/main.cpp
@@ -0,0 +1,17 @@
+// Option 1
+typedef unsigned int uf_uint;
+typedef std::vector<uf_uint>::size_type uf_size;
+
+namespace uf
+{
+    // My classes go here
+}
+
+// Option 2
+namespace uf
+{
+    typedef unsigned int uint;
+    typedef std::vector<uf_uint>::size_type size;
+    
+    // Classes are here
+}
