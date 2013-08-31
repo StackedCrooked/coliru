79c1f454d4aaca05aac88836fb87f5d1075fdcf2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-08-31 21:51:56 +0000

    Add id2path.sh

---
+++ Web/id2path.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+COLIRU_ARCHIVE="Archive"
+COLIRU_ARCHIVE2="Archive2"
+
+# search in old archive
+old="${COLIRU_ARCHIVE}/${1}"
+echo "old: ${old}"
+[ -d ${old} ] && { echo "${old}" ; exit ; }
+
+# search in new archive
+id="$(./pathify-id.sh $1)"
+new="${COLIRU_ARCHIVE2}/${id}"
+echo "new: ${new}"
+[ -d "${new}" ] && { echo "${new}" ; exit ; }
+
