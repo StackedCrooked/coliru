de9737c73d0c7865459df88cf3dea9c362f2789e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-08-31 21:59:36 +0000

    Update id2path.

--- Web/id2path.sh
+++ Web/id2path.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-COLIRU_ARCHIVE="Archive"
-COLIRU_ARCHIVE2="Archive2"
+COLIRU_ARCHIVE="../Archive"
+COLIRU_ARCHIVE2="../Archive2"
 
 # search in old archive
 old="${COLIRU_ARCHIVE}/${1}"
-echo "old: ${old}"
+#echo "old: ${old}"
 [ -d ${old} ] && { echo "${old}" ; exit ; }
 
 # search in new archive
 id="$(./pathify-id.sh $1)"
 new="${COLIRU_ARCHIVE2}/${id}"
-echo "new: ${new}"
+#echo "new: ${new}"
 [ -d "${new}" ] && { echo "${new}" ; exit ; }
 
