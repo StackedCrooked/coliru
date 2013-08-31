ccf120145471804a352aaf984d4b470dcff44d82
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-08-31 19:26:00 +0000

    Merge changes back from trunk.

--- Web/run.sh
+++ Web/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#set -x
+set -x
 if [ "$(whoami)" != "root" ] ; then
     echo "$(basename $0) must be run with root permissions." 1>&2
     exit 1
 fi
 
 source coliru_env.source
-./repair-permissions.sh
+./repair-permissions.sh & disown
 setsid sudo -u webserver -E bash -c "
     exec > >(logger -t $0) 2>&1 
     ruby webserver-sinatra.rb -o ${COLIRU_URL}
