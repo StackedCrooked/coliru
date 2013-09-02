18865363bac2f0cdc04832d4c403306fed27397a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-02 08:11:22 +0000

    Update archive.

---
+++ trunk/Archive2/9e/c6954f46883ea5/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+#define MAXCHANNELS 20000
+#define N_USER_SCAN_LIST 200
+
+int main()
+{
+    short iIndex = 173;
+    short iOffset = 4;
+    
+    short iScanlist=(iIndex+(iOffset*MAXCHANNELS))/N_USER_SCAN_LIST;
+    short iElement=(iIndex+(iOffset*MAXCHANNELS))%N_USER_SCAN_LIST;
+
+    std::cout << iScanlist << '\n' << iElement << '\n';
+}
