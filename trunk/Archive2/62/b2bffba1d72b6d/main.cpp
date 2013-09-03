8586bba8a54bd428c8c91098321725bef152c884
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-03 01:21:29 +0000

    Update archive.

---
+++ trunk/Archive2/62/b2bffba1d72b6d/main.cpp
@@ -0,0 +1,24 @@
+#include <type_traits>
+
+template<typename T>
+struct CopyConstructible {
+    static constexpr bool check() {
+        static_assert(std::is_copy_constructible<T>(), "Concept Violation: Not CopyConstructible");
+        return true;
+    }
+};
+
+struct f {
+    f(const f&) = delete;
+    f() = default;
+};
+
+template<typename T>
+void g(T&& t) {
+    CopyConstructible<T>::check(); // why?
+}
+
+int main() {
+    f lol;
+    g(lol);
+}
