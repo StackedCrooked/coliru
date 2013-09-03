c29e6429e887bbc5a2932c6fb5a1a16c616f4385
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-03 17:47:08 +0000

    Update archive.

---
+++ trunk/Archive2/d7/1a9d076ca60f2a/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <limits>
+
+int main()
+{
+    std::cout << std::numeric_limits<int>::max() << std::endl;
+    std::cout << std::numeric_limits<long int>::max() << std::endl;
+
+}
