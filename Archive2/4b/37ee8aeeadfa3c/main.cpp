2ca00a374f99a9d07b84e5ca226133e180d587c9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-04 08:52:37 +0000

    Update archive.

---
+++ Archive2/4b/37ee8aeeadfa3c/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+//file.h
+class c
+{
+    int a;
+    int b;
+    //other variables
+  public:
+    void setValues();
+    //other variables
+};
+
+void c::setValues()
+{
+    a=1;
+    b=2;
+    //other variables
+}
+
+int main() {return 0;}
