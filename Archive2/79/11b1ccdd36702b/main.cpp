a90b21ef1ef0692ab2f82a69fe5f0c1d15a43d72
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-04 09:53:09 +0000

    Update archive.

---
+++ Archive2/79/11b1ccdd36702b/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <memory>
+ 
+struct Foo {
+    Foo() { std::cout << "Foo\n"; }
+    ~Foo() { std::cout << "~Foo\n"; }
+};
+ 
+int main() 
+{
+    std::unique_ptr<Foo> p1;
+ 
+    {
+        std::cout << "Creating new Foo...\n";
+        std::unique_ptr<Foo> p2(new Foo);
+ 
+        p1 = std::move(p2);
+        std::cout << "About to leave inner block...\n";
+ 
+        // Foo instance will continue to live, 
+        // despite p2 going out of scope
+    }
+ 
+    std::cout << "About to leave program...\n";
+}
