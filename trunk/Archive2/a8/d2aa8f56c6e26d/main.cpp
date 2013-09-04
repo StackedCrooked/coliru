dfb053832e4efd5b2be445f1a7fcbf0e89700f28
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-04 19:38:45 +0000

    Update archive.

---
+++ trunk/Archive2/a8/d2aa8f56c6e26d/main.cpp
@@ -0,0 +1,48 @@
+#include <thread>
+#include <future>
+#include <iostream>
+#include <chrono>
+
+
+template< class Function, class... Args>
+std::future<typename std::result_of<Function(Args...)>::type> my_async(Function&& f, Args && ... args)
+{
+    typedef typename std::result_of<Function(Args...)>::type ret_type;
+
+	std::promise<ret_type> p;
+
+	auto fut = p.get_future();
+
+	auto l = [](Function && f, Args && ... args, std::promise<ret_type>&& p)
+	{
+		p.set_value(f(args...));
+	};
+
+	std::thread th(l, f, args..., std::move(p));
+
+
+	th.detach();
+
+	return std::move(fut);
+
+}
+
+int wannaRunAsync(int i)
+{
+	return i;
+};
+
+int main()
+{
+
+	auto fut = my_async(&wannaRunAsync, 42);
+
+	auto fut2 = my_async([](int i) -> int { return i; } , 42);
+
+	std::cout << fut.get() << std::endl;
+	std::cout << fut2.get() << std::endl;
+
+	std::cin.get();
+
+	return 0;
+}
