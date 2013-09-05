ad4a8ac1326a47d24de97ef2c20f94beb50c1292
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-05 23:28:15 +0000

    Update archive.

---
+++ trunk/Archive2/71/fe693ed8a53dc1/main.cpp
@@ -0,0 +1,27 @@
+#include <type_traits>
+#include <string>
+#include <iostream>
+
+struct A {
+    A(const A&) {std::cout << "cctr\n";}
+    A(A&&) {std::cout << "mctr\n";}
+    A&operator=(const A&) {std::cout << "casn\n"; return*this;}
+    A&operator=(A&&) {std::cout << "masn\n"; return*this;}
+};
+
+template <typename T>
+constexpr typename std::enable_if<
+    !std::is_const<
+        typename std::remove_reference<T>::type>::value,
+    typename std::remove_reference<T>::type&&
+>::type
+strict_move(T&& x)
+{
+    return std::move(x);
+}
+
+int main() {
+    std::string src = "HI";
+    std::string dest;
+    dest = strict_move(src);
+}
