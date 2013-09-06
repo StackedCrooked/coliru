b49cf22a656027034ce8ae58e66926541c7fcdd6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-06 05:30:46 +0000

    Update archive.

---
+++ Archive2/54/696c4c8caa5e53/main.cpp
@@ -0,0 +1,12 @@
+ // Fig. 2.1: fig02_01.cpp
+ // Programa para imprimir texto.
+ #include <iostream> // permite al programa imprimir datos en la pantalla
+
+ // la función main comienza la ejecución del programa
+ int main()
+ {
+ std::cout << "Bienvenido a C++!\n"; // muestra un mensaje
+
+ return 0; // indica que el programa terminó con éxito
+
+ } // fin de la función main
