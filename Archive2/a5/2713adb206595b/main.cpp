676d4460e0661386aae692614f7bf8193a24045f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-08 19:23:00 +0000

    Update archive.

---
+++ Archive2/a5/2713adb206595b/main.cpp
@@ -0,0 +1,42 @@
+#include <string>
+
+using namespace std;
+
+size_t BruteForce(const string& line, const string& pattern) {
+    const size_t n = line.length();
+    const size_t m = pattern.length();
+
+    if (n<m) return -1;
+
+    for(size_t i = 0u; i <= (n - m); i++) {
+        for(size_t j=0; j < m  && (line[i + j] == pattern[j]); ++j) {
+            if(j == m-1) {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+int main() {
+    string text, algo_type, pattern, fname, line;
+    getline(cin ,text);
+    istringstream iss(text);
+    if(iss >>  algo_type  >>  pattern  >> fname) {
+        cout << " " << algo_type  << " " << pattern <<  " " <<fname << "\n";
+    }
+
+    int i = 1;
+    ifstream ifs;
+    ifs.open(fname.c_str());
+    while(getline(ifs, line) && fname != "") {
+        if(algo_type == "bf") {
+            cout << "Line " << i++ << ":" << BruteForce(line,pattern) << " '" << line << "'" << endl;
+        }
+    }
+    return 0;
+}
