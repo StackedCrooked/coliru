ea5076bcf473b8911bc991f1ec0e4cddaa111385
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-08 19:23:16 +0000

    Update archive.

---
+++ Archive2/fb/54acd63349e1ff/main.cpp
@@ -0,0 +1,36 @@
+#include <utility>
+
+/*
+// illegal context for `...`
+template<class Cont, class... Args>
+void emplace_all(Cont& c, Args&&... args)
+{
+    c.emplace_back(std::forward<Args>(args))...;
+}
+*/
+
+template<class Cont, class Arg>
+int emplace_back(Cont& c, Arg&& arg)
+{
+    c.emplace_back(std::forward<Arg>(arg));
+    return 42;
+}
+
+template<class Cont, class... Args>
+void emplace_all2(Cont& c, Args&&... args)
+{
+    int unused[] = {(emplace_back(c, std::forward<Args>(args)), 0)...};
+}
+
+#include <vector>
+#include <iostream>
+
+int main()
+{
+    std::vector<int> v;
+    emplace_all2(v, 1,2,3,4);
+    for(auto const& e : v)
+    {
+        std::cout << e << ',';
+    }
+}
