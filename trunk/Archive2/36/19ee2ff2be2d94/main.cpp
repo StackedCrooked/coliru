cdd533e3e4a9fe9815d68d0cb9a51c3fa906c503
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-08 16:23:48 +0000

    Update archive.

---
+++ trunk/Archive2/36/19ee2ff2be2d94/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+#include <thread>
+#include <future>
+#include <functional>
+ 
+void task_lambda()
+{
+    std::packaged_task<int(int,int)> task([](int a, int b) {
+        return std::pow(a, b); 
+    });
+    std::future<int> result = task.get_future();
+ 
+    task(2, 9);
+ 
+    std::cout << "task_lambda:\t" << result.get() << '\n';
+}
+ 
+void task_bind()
+{
+    std::packaged_task<int()> task(std::bind(std::pow<int, int>, 2, 11));
+    std::future<int> result = task.get_future();
+ 
+    task();
+ 
+    std::cout << "task_bind:\t" << result.get() << '\n';
+}
+ 
+void task_thread()
+{
+    std::packaged_task<int(int,int)> task(std::pow<int, int>);
+    std::future<int> result = task.get_future();
+ 
+    std::thread task_td(std::move(task), 2, 10);
+    task_td.join();
+ 
+    std::cout << "task_thread:\t" << result.get() << '\n';
+}
+ 
+int main()
+{
+    task_lambda();
+    task_bind();
+    task_thread();
+}
