60f841f557288eebd536399c0ae3eb9e7a4b8efb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-08 21:23:34 +0000

    Update archive.

---
+++ trunk/Archive2/36/2bb803911c0a7d/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+void test(char c)
+{
+    std::cout << "char\n";
+}
+
+void test(signed char c)
+{
+    std::cout << "signed char\n";
+}
+
+void test(unsigned char c)
+{
+    std::cout << "unsigned char\n";
+}
+
+int main(int argc, char **argv){
+    
+    char a;
+    signed char b;
+    unsigned char c;
+    test(a);
+    test(b);
+    test(c);
+    
+
+    return 0;
+}
