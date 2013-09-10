8c264a445281f472d8f031f601aec47e6c318911
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-10 11:48:49 +0000

    Update archive.

---
+++ trunk/Archive2/d2/3ff08d6f3a0848/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+// uncomment to disable assert()
+// #define NDEBUG
+#include <cassert>
+ 
+int main()
+{
+    int *p;
+    assert(p);
+    assert(2+2==4);
+    std::cout << "Execution continues past the first assert\n";
+    assert(2+2==5);
+    std::cout << "Execution continues past the second assert\n";
+}
