f0863c2c4d7ec13d4830a781eb2b5fd5d76d01c5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-10 12:49:07 +0000

    Update archive.

---
+++ Archive2/4f/04978c66584075/main.cpp
@@ -0,0 +1,20 @@
+
+
+// shared header for both server and client
+struct Increment
+{
+    using result_type = int;
+    using argument_type = int;
+};
+
+
+int main()
+{
+    // server side
+    rpc_server server(9000);
+    server.registerMethod<Increment>([](Increment::argument_type n) { return n + 1; });
+    
+    // client side
+    rpc_client client("localhost", 9000);
+    client.send<Increment>(2); // returns 3
+}
