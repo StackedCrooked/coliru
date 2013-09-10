126599cbffd7f66b4aefcbd2ea80a9080d11e2b4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-10 11:48:24 +0000

    Update archive.

---
+++ trunk/Archive2/41/8ee60fa11ddc2e/main.cpp
@@ -0,0 +1,44 @@
+#include <thread>
+#include <mutex>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <memory>
+
+
+/// synchronize console output
+std::mutex print_mutex;
+
+void print()
+{
+    std::cout << std::endl;
+}
+
+template<typename T,typename... Args>
+void print(T t,Args... args)
+{
+	std::cout << t;
+	if (sizeof...(args))
+		print(args...);
+	else {
+		std::cout << std::endl;
+	}
+}
+
+template<typename T,typename... Args>
+void sync_print(T t,Args... args)
+{
+	std::unique_lock<std::mutex> lock(print_mutex);
+	print(t,args...);
+}
+
+int main() {
+	std::vector<std::thread> threads;
+	for (int i=0; i<50; i++)
+		threads.push_back(std::thread(
+			[=]{ sync_print("thread: ",i); }
+		));
+		
+	for (auto& t : threads)
+		t.join();
+}
