e5a71d590d8d0ddd930570ca7dfbc94de14fc8e4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-11 23:15:12 +0000

    Update archive.

---
+++ Archive2/b5/11fe046acec0be/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+
+#define TRACE std::cout << __PRETTY_FUNCTION__ << std::endl;
+
+
+template<class F, class... Fs>
+struct overloaded{
+    
+    
+    
+    F f;
+    
+    template<class T>
+    auto operator()(T v) -> decltype(f(v)){ return f(v); }
+};
+
+template<class F, class Fx, class... Fs>
+struct overloaded<F, Fx, Fs...> : overloaded<Fx, Fs...>{
+    //overloaded() { TRACE }
+    F f;
+    
+    overloaded(F f_, Fx fx, Fs... fs)
+      : overloaded<Fx, Fs...>{fx, fs...}
+      , f(f_) {}
+      
+    using overloaded<Fx, Fs...>::operator();
+    
+    template<class T>
+    auto operator()(T&& v) -> decltype(f(v)){ return f(v); }
+};
+
+template<class... Fs>
+overloaded<Fs...> overload(Fs... fs){ return {fs...}; }
+
+#include <string>
+
+int main()
+{
+    auto o = overload([](int){}, [](std::string){});
+    
+    o(42);
+    o("yay");
+}
