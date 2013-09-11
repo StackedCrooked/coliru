ffa4f18fd3703583951196abac3e8028c02c293c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-11 21:41:31 +0000

    Check for existence of main.cpp in INPUT_FILES_DIR.

--- Web/hash.sh
+++ Web/hash.sh
@@ -2,7 +2,7 @@
 [ "$INPUT_FILES_DIR" == "" ] && { echo "INPUT_FILES_DIR is not set." 1>&2 ; exit 1 ; }
 [ "$INPUT_FILES_DIR" == "." ] && { echo "INPUT_FILES_DIR is jsut a dot." 1>&2 ; exit 1 ; }
 sum="$(shasum ${INPUT_FILES_DIR}/cmd.sh | cut -d ' ' -f1)"
-[ -f main.cpp ] && {
+[ -f "${INPUT_FILES_DIR}/main.cpp" ] && {
     sum="$(shasum ${INPUT_FILES_DIR}/main.cpp | cut -d ' ' -f1)${sum}"
     sum="$(g++ --version | shasum | cut -d' ' -f1)${sum}"
     sum="$(clang++ --version | shasum | cut -d' ' -f1)${sum}"
