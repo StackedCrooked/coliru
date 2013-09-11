2f5b1ee5eaa8cdacbc0f0c4b41c4d1777c78cdd2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-11 21:46:20 +0000

    Merge from trunk.

--- branches/wip/Web/hash.sh
+++ branches/wip/Web/hash.sh
@@ -2,8 +2,12 @@
 [ "$INPUT_FILES_DIR" == "" ] && { echo "INPUT_FILES_DIR is not set." 1>&2 ; exit 1 ; }
 [ "$INPUT_FILES_DIR" == "." ] && { echo "INPUT_FILES_DIR is jsut a dot." 1>&2 ; exit 1 ; }
 sum="$(shasum ${INPUT_FILES_DIR}/cmd.sh | cut -d ' ' -f1)"
-[ -f main.cpp ] && {
+[ -f "${INPUT_FILES_DIR}/main.cpp" ] && {
     sum="$(shasum ${INPUT_FILES_DIR}/main.cpp | cut -d ' ' -f1)${sum}"
+    sum="$(g++ --version | shasum | cut -d' ' -f1)${sum}"
+    sum="$(clang++ --version | shasum | cut -d' ' -f1)${sum}"
+
+    # we now have a long string which must be hashed again before shortening it
     sum="$(echo ${sum} | shasum | cut -d ' ' -f1)"
 }
 HASH=$(echo $sum | cut -c -16)
