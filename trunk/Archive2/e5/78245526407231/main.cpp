e564b3e8e6cb22a9e2fb25749ba0b7fcaff9527e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-11 23:15:22 +0000

    Update archive.

---
+++ trunk/Archive2/e5/78245526407231/main.cpp
@@ -0,0 +1,30 @@
+template<class> struct empty{};
+
+template<class F>
+struct overloaded_base{
+    F f;
+    
+    template<class T>
+    //using T = int;
+    auto operator()(T v) -> decltype(f(v)){ return f(v); }
+};
+
+template<class F>
+struct overloaded : overloaded_base<F>{
+    
+    overloaded(F f)
+      : overloaded_base<F>{f} {}
+    
+    using overloaded_base<F>::operator();
+      
+    //template<class T, class = typename empty<T>::type>
+    //void operator()(T){}
+};
+
+template<class F>
+overloaded<F> overload(F f){ return {f}; }
+
+int main()
+{
+    overload([](int){})(42);
+}
