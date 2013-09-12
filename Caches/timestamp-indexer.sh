b5fb8f9afbd295ab1b274e6d31d1d0ef01105493
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-12 18:57:58 +0000

    Update timestamp-indexer script.

--- Caches/timestamp-indexer.sh
+++ Caches/timestamp-indexer.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-if [ "$(whoami)" != "webserver" ] ; then
-    echo "The indexer must be run by the webserver." 1>&2
+
+# Check arguments
+[ "$#" == "1" ] || {
+    echo "Usage: $(basename $0) ArchivePath" 1>&2
     exit 1
+}
+
+if [ "$(whoami)" != "webserver" ] ; then
+    echo "Restarting $(basename $0) as webserver user." 1>&2
+    sudo -u webserver bash $0 $1
+    exit
 fi
-cd $(dirname $0)
 
-for ts in $(find ../Archive -name timestamp) ; do
-    printf "$(cat $ts) $(dirname $ts)\n" >>_timestamp_to_directory
-done
-
+(
+    cd $(dirname $0)
+    {
+        while read ts ; do
+            printf "$(cat ${ts}) $(dirname ${ts})\n"
+        done < <(find "${1}" -name timestamp) 
+    } >_timestamp_to_directory
+)
