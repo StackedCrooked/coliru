91c7dab67b3b4838b2cce61ce38da6db0f22d71b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-13 01:10:45 +0000

    Restore hash.

--- Web/hash.sh
+++ Web/hash.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
-dir="$INPUT_FILES_DIR}"
-[ "${dir}" == "" ] && {
-    echo "INPUT_FILES_DIR not set." 1>&2 
-    exit 1
+[ "$INPUT_FILES_DIR" == "" ] && { echo "INPUT_FILES_DIR is not set." 1>&2 ; exit 1 ; }
+[ "$INPUT_FILES_DIR" == "." ] && { echo "INPUT_FILES_DIR is jsut a dot." 1>&2 ; exit 1 ; }
+sum="$(shasum ${INPUT_FILES_DIR}/cmd.sh | cut -d ' ' -f1)"
+[ -f "${INPUT_FILES_DIR}/main.cpp" ] && {
+    sum="$(shasum ${INPUT_FILES_DIR}/main.cpp | cut -d ' ' -f1)${sum}"
+    sum="$(g++ --version | shasum | cut -d' ' -f1)${sum}"
+    sum="$(clang++ --version | shasum | cut -d' ' -f1)${sum}"
+
+    # we now have a long string which must be hashed again before shortening it
+    sum="$(echo ${sum} | shasum | cut -d ' ' -f1)"
 }
-
-get_text() {
-    cat cmd.sh
-    [ -f "${dir}/main.cpp" ] && {
-        cat main.cpp
-        g++ --version
-        clang++ --version
-    }
-}
-
-
-get_hash() {
-    get_text | shasum | cut -d ' ' -f1 | cut -c -16
-}
-
-get_hash
+HASH=$(echo $sum | cut -c -16)
+echo $(echo "${HASH}" | cut -c -2)/$(echo "${HASH:2}")
