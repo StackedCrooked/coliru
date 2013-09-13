16c202af2a9f6cacc7cb94fc743339bd16abeeaf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-13 01:42:11 +0000

    Merge from wip.

--- trunk/Web/hash.sh
+++ trunk/Web/hash.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-[ "$INPUT_FILES_DIR" == "" ] && { echo "INPUT_FILES_DIR is not set." 1>&2 ; exit 1 ; }
-[ "$INPUT_FILES_DIR" == "." ] && { echo "INPUT_FILES_DIR is jsut a dot." 1>&2 ; exit 1 ; }
-sum="$(shasum ${INPUT_FILES_DIR}/cmd.sh | cut -d ' ' -f1)"
-[ -f "${INPUT_FILES_DIR}/main.cpp" ] && {
-    sum="$(shasum ${INPUT_FILES_DIR}/main.cpp | cut -d ' ' -f1)${sum}"
-    sum="$(g++ --version | shasum | cut -d' ' -f1)${sum}"
-    sum="$(clang++ --version | shasum | cut -d' ' -f1)${sum}"
-
-    # we now have a long string which must be hashed again before shortening it
-    sum="$(echo ${sum} | shasum | cut -d ' ' -f1)"
+[ "$INPUT_FILES_DIR" == "" ] && {
+    echo "INPUT_FILES_DIR is not set." 1>&2
+    exit 1
 }
-HASH=$(echo $sum | cut -c -16)
-echo $(echo "${HASH}" | cut -c -2)/$(echo "${HASH:2}")
+
+# start a subshell in order to restore
+# the current directory when it exits
+(
+    cd ${INPUT_FILES_DIR}
+    cat *
+    type g++ && g++ --version
+    type clang++ && clang++ --version
+) 2>&1 | shasum | awk '{print $1}' | cut -c -16
