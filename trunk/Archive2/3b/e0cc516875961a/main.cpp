99cb9dd0b17c0c5e48ca9571dcd65dd1cb5179c6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-17 11:20:05 +0000

    Committing 3b.

---
+++ trunk/Archive2/3b/e0cc516875961a/main.cpp
@@ -0,0 +1,8 @@
+(
+    echo "een twee drie vier vijf zes zeven acht negen tien"
+    echo "one two three four five six seven eight nine ten"
+    echo "un deux trois quatre cinq six sept huit neuf dix"
+    echo "いち に さん ご ろく なな はち きゅう じゅう"
+    echo "uno due tre quattro cinque sei sette otto nove dieci"
+    echo "ein zwei drei vier fünf sech sieben acht neun zehn"
+) | column -t
