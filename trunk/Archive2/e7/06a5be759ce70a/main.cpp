fc907849961a7e86976c17135e38daa231e662fc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-17 11:28:10 +0000

    Committing e7.

---
+++ trunk/Archive2/e7/06a5be759ce70a/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+template <class T> struct rcg {
+    
+  template <class Arg1>
+  T operator()() const {
+    auto arg1 = Arg1{5};
+    return T{arg1}; 
+  }
+
+};
+
+class A {
+  int a_;
+  public:
+  A(int a) : a_{a} {}
+  int a() const { return a_; } 
+};
+
+template <class Arg1>
+Arg1 foo() {
+    return Arg1{5};
+}
+
+int main() {
+  auto a1 = foo<A>();
+  auto my_a_generator = rcg<A>{};
+  auto my_a = my_a_generator<int>(); 
+  std::cout << my_a.a() << std::endl;
+}
---
+++ trunk/Archive2/e7/3be962b91be1af/main.cpp
@@ -0,0 +1,18 @@
+struct A
+{
+    explicit A(){}
+};
+
+void f(A){}
+
+const struct default_t
+{
+     template<typename T>
+     operator T() const {   return T{}; }
+}default_;
+
+int main()
+{
+    //f({}); does NOT work
+      f(default_);
+}
