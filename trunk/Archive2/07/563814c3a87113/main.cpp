e03a3cebd2db8049d8153a43091b534975d7618a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-17 11:17:14 +0000

    Committing 07.

---
+++ trunk/Archive2/07/563814c3a87113/main.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+
+const struct default_t
+{
+     template<typename T>
+     operator T() const {   return T{}; }
+}default_{};
+
+struct VeryVeryVeryLongTypeName { int value = 100; };
+struct WithExplicitConstructor_VeryVeryVeryLongTypeName { int value;  explicit WithExplicitConstructor_VeryVeryVeryLongTypeName() : value(1000) {}  };
+
+VeryVeryVeryLongTypeName f()
+{
+     //...
+     return default_;
+}
+
+WithExplicitConstructor_VeryVeryVeryLongTypeName g()
+{
+     //...
+     return default_;
+}
+int main() 
+{
+    std::cout << f().value << std::endl;
+    std::cout << g().value << std::endl;
+}
