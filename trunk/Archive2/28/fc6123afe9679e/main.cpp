8e7c2aa0a16a1a139f8fc0daa76d1a437048f08b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-17 11:18:53 +0000

    Committing 28.

---
+++ trunk/Archive2/28/fc6123afe9679e/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+class A {
+    public: 
+    A() {
+        std::cout << "Hello A" << std::endl;
+    }
+};
+
+class B {
+    public:
+    B() { 
+        std::cout << "Hello B" << std::endl;
+    }
+};
+
+class C : public B {
+    public:
+    C() {
+        std::cout << "Hello C" << std::endl;
+    }
+    private:
+    static A MyA;
+};
+
+int main() {
+    C ThisC;
+    return 0;
+}
