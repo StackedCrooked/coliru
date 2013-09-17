6774adb95eda7974f16f500f0b2b08ef7ed40422
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-17 11:20:22 +0000

    Committing 44.

---
+++ Archive2/44/87435cfc90c012/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+class test {
+public:
+    union {
+        int arr[3]{0,1,2};
+        struct {int a, b, c;};
+    };
+};
+
+int main() {
+    test a;
+    std::cout << a.c << std::endl;;
+}
