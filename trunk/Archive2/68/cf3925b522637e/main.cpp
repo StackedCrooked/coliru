a6b3f543c7438461567d269607176767f4040d81
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-17 11:22:00 +0000

    Committing 68.

---
+++ trunk/Archive2/68/cf3925b522637e/main.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+int countWords( char texto[], int pos )
+    {
+     int cont;
+
+     cont = 0;
+
+     if( texto[ pos ] != '.' )
+        {
+         cont = countWords( texto, pos + 1 );
+
+         if( texto[ pos ] == ' ' )
+            cont++;
+        }
+
+     return ( cont );
+    }
+
+
+int enxii()
+    {
+     char frase[] = "hola mundo.";
+
+     int words;
+
+     words = ( frase[ 0 ] != '.' )? ( 1 + countWords( frase, 0 ) ) : ( 0 );
+
+     printf("\nLa cantidad de palabras es: %i\n\n", words );
+
+     printf( "Chime puto.\n\n" );
+
+     return ( 0 );
+    }
+
+int main()
+{
+    enxii();
+}
