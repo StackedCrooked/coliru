e6ead048597e8d667770ef2cc88798d088d5f8b5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-17 11:24:51 +0000

    Committing 99.

---
+++ trunk/Archive2/99/5504a5dbaec781/main.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <vector>
+
+template <typename C, typename V/* = typename C::value_type*/>
+   size_t index_of(C const& c, V const v)
+{
+    const auto f(begin(c)), l(end(c));
+
+    auto match = std::find(f, l, v);
+    return (l==match) ? -1 : std::distance(f, match);
+}
+
+template <typename C, typename Pred>
+   size_t index_if(C const& c, Pred&& pred)
+{
+    const auto f(begin(c)), l(end(c));
+
+    auto match = std::find_if(f, l, std::move(pred));
+    return (l==match) ? -1 : std::distance(f, match);
+}
+
+int main()
+{
+    struct X { int ID; };
+    const std::vector<X> v { {1},{2},{3},{25},{4},{5},{6},{42} };
+    return index_if(v, [](X const& x) { return x.ID == 25; });
+}
---
+++ trunk/Archive2/99/da6655cd21e540/main.cpp
@@ -0,0 +1,15 @@
+#include <cassert>
+#include <climits>
+
+int type_id_seq;
+
+template<typename T>
+struct type_id
+{
+    static int const value;
+};
+
+template<typename T>
+int const type_id<T>::value = (assert(type_id_seq < INT_MAX), ++type_id_seq);
+
+template struct type_id<int>;
