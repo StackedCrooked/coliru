d0fd9cb186d3f1b03fb75e217713b3fbe71d56b9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-17 11:25:21 +0000

    Committing a1.

---
+++ trunk/Archive2/a1/6c78d59c855b82/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    puts("een twee drie vier vijf zes zeven acht negen tien");
+    puts("one two three four five six seven eight nine ten");
+    puts("un deux trois quatre cinq six sept huit neuf dix");
+    puts("いち に さん ご ろく なな はち きゅう じゅう");
+    puts("uno due tre quattro cinque sei sette otto nove dieci");
+    puts("ein zwei drei vier fünf sech sieben acht neun zehn");
+}
