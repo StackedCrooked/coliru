6f9bcc370102a5027a8e6d63e4fc8775ebee5427
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-17 11:23:16 +0000

    Committing 7f.

---
+++ Archive2/7f/9f6ffbb687de4e/main.cpp
@@ -0,0 +1,29 @@
+
+struct args{};
+
+struct A {
+    A(A* next_=nullptr) :next(next_) {}
+    void B() {C(this);}
+protected:
+    A* next;
+
+    static args C(A* self, args a=args()) {
+        return self->C_impl(a);
+    }
+private:
+    virtual args C_impl(args a) =0;
+};
+
+struct D : public A {   
+    virtual args C_impl(args a) {
+        if (next)
+            C(next, a);
+        return a;
+    }
+};
+
+int main() {
+    D one;
+    D two(one);    
+    two.B();
+}
