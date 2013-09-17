49acc2fd1cf2ae04fbbb53b01c9e2a8c80082ade
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-17 11:17:37 +0000

    Committing 10.

---
+++ Archive2/10/0b945b9129857f/main.cpp
@@ -0,0 +1,29 @@
+
+struct args{};
+
+struct A {
+    A(A* next_=nullptr) :next(next_) {}
+    void B() {C();}
+protected:
+    A* next;
+
+    static args C(A& self, args a=args()) {
+        self.C_impl(std::move(a));
+    }
+private:
+    virtual args C_impl(args a) =0;
+};
+
+struct D : public A {   
+    virtual args C_impl(args a) {
+        if (next)
+            C(next, a);
+        return a;
+    }
+};
+
+int main() {
+    D one;
+    D two(one);    
+    two.B();
+}
