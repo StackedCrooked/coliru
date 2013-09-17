8830d53c523ccdd0c628f34633811efa5a5b0738
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-17 11:22:15 +0000

    Committing 6d.

---
+++ trunk/Archive2/6d/ece4b21df18a95/main.cpp
@@ -0,0 +1,13 @@
+#include <type_traits>
+
+void F1(int) {    
+}
+
+template <typename T = float>
+void F2(int) {
+    static_assert(std::is_integral<T>::value, "Don't call F2!");
+}
+
+int main() {
+ F1(1);  
+}
