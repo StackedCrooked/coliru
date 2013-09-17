a237c0b9c3de01c9044ce47cd8931c6e04bab7d4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-17 11:28:07 +0000

    Committing e5.

---
+++ Archive2/e5/42cc71ccf7c531/main.cpp
@@ -0,0 +1,19 @@
+set -x
+git init .
+
+# create 26 commits
+for a in {a..z}; do touch $a && git add $a && git commit -m "added $a"; done
+
+# tag (commit 'p') one as our previous RELEASE_A
+git tag RELEASE_A HEAD~10
+
+# now we do a release without patches h, t, p, and w:
+culprits=$(git rev-list --grep='[htpw]' RELEASE_A..HEAD)
+git revert --no-edit $culprits
+git tag RELEASE_B
+
+# re-apply the culprits:
+git cherry-pick $culprits
+
+# end result:
+git log --decorate --oneline
