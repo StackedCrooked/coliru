61a479bb5f1a6d1d10297b555cc11d4b3f37ab96
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-24 20:27:46 +0000

    Cleanup restart.sh.

--- trunk/Web/restart.sh
+++ trunk/Web/restart.sh
@@ -1,40 +1,33 @@
 #!/bin/bash
 
-let comments=
-(
-    Restart Procedure
-    -----------------
-    - kills webserver and any user scripts
-    - starts webserver
-
-    Issues
-    ------
-    - repair-permissions.sh
-        - permissions errors are a frequent problem
-        - construction == reconstruction
-        - convenient
-        - sloppy
-        - repair archive permissions is slow
-    - committer.sh
-        - kinda sloppy
-    - run.sh
-)
-
 source coliru_env.source
+source logger.source
+
 if [ "$(whoami)" != "root" ] ; then
     echo "$(basename $0) must be run with root permissions." 1>&2
     exit 1
 fi
+
+
 cd $(dirname $0)
+
+
 echo "Killing any previously running instances..."
 ./kill-all.sh >/dev/null 2>&1
+
+
 echo "Starting new instance..."
 ./run.sh & disown
 
+
+
+echo "Starting committer.sh if not already started."
 COMMITTER_PGID=$(ps -eopgid,comm | grep 'committer.sh' | sort | uniq | awk '{ print $1 }')
 if [ "$COMMITTER_PGID" == "" ] ; then 
-    echo "Starting committer.sh"
+    echo "committer.sh not yet running => start it"
     ./committer.sh & disown
+else
+    echo "committer.sh already running => don't start it."
 fi
 
 
