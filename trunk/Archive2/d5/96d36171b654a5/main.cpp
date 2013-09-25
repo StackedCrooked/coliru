ced5f502dc3b2580b4c338848aa779623b373af7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-25 19:47:52 +0000

    Updating archive.

---
+++ trunk/Archive2/d5/96d36171b654a5/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>                                                                 
+#define FOO 0xAB'CD'EF                                                              
+#define BAR(x,y) A ## x ## y                                                        
+#define BAR2(x) BAR(x,)                                                             
+#define BAR3(x) #x                                                                  
+#define BAR4(x) BAR3(x)                                                             
+#define BAR5(x) BAR4(BAR2(x))                                                       
+#define A0xAB                                                                
+int main() {                                                                        
+    std::cout << BAR5(FOO) << std::endl;                                            
+}                                                                                   
