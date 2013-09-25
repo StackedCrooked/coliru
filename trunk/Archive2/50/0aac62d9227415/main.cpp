1f682c26bbf3e968dad9e205f19c3694bcc34e61
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-25 20:47:58 +0000

    Updating archive.

---
+++ trunk/Archive2/50/0aac62d9227415/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+template <typename T> struct C2;
+
+template <typename T> 
+struct C1
+{
+  template <typename Type,
+        template <typename Ti> class Container = C2>
+  void m() {}
+};
+
+template <typename T> 
+struct C2
+{
+  template <typename Type = int,
+        template <typename Ti> class Container = ::C2> 
+                                              // ^^ <-- here is the solution!
+  void m() {}
+
+};
+
+int main() 
+{
+    std::cout << "Hello World!\n";    
+}
