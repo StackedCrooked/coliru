e7f1a7a8e150996d214bd8999f896368b2e76c1f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-25 19:46:54 +0000

    Updating archive.

---
+++ trunk/Archive2/17/deffe7667e68ec/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <map>
+#include <cstdio>
+
+int main()
+{
+    int A = 4;
+    
+    int x = 7;
+    
+    x &= ~4;
+    
+    std::cout << x;
+    
+}
