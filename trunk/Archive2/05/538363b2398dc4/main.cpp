01eb1b21d176aabb8b196b359109e66085c1d273
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-26 09:52:54 +0000

    Updating archive.

---
+++ trunk/Archive2/05/538363b2398dc4/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iterator>
+#include <array>
+#include <utility>
+
+class Foo
+{
+private:
+    int a_ = 0;
+    int b_ = 0;
+    int c_ = 0;
+    int d_ = 0;
+    
+public:
+    constexpr Foo() = default;
+   
+    constexpr Foo(int a, int b, int c, int d)
+    : 
+        a_{a}, b_{b}, c_{c}, d_{d}
+    {}
+          
+    constexpr Foo& SetA(int i) { a_ = i; return *this; }
+    constexpr Foo& SetB(int i) { b_ = i; return *this; }
+    constexpr Foo& SetC(int i) { c_ = i; return *this; }
+    constexpr Foo& SetD(int i) { d_ = i; return *this; }
+
+    friend std::ostream& operator<<(std::ostream& os, const Foo& f)
+    {
+        return os << f.a_ << " " << f.b_ << " " << f.c_ << " " << f.d_ << " ";    
+    }
+};
+
+int main() 
+{
+    constexpr Foo f = Foo{}.SetC(2).SetA(-1);
+    std::cout << f;
+}
