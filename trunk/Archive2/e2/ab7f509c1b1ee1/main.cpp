3b1c02cbfbca4627cf6cf87bd8f9680ab8a0dbfa
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-26 03:49:38 +0000

    Updating archive.

---
+++ trunk/Archive2/e2/ab7f509c1b1ee1/main.cpp
@@ -0,0 +1,23 @@
+#include <thread>
+#include <mutex>
+ 
+int g_i = 0;
+std::mutex g_i_mutex;  // protects g_i
+ 
+void safe_increment()
+{
+    std::lock_guard<std::mutex> lock(g_i_mutex);
+    ++g_i;
+ 
+    // g_i_mutex is automatically released when lock
+    // goes out of scope
+}
+ 
+int main()
+{
+    std::thread t1(safe_increment);
+    std::thread t2(safe_increment);
+ 
+    t1.join();
+    t2.join();
+}
