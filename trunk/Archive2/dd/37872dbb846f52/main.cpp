0fb0abb3bfe8b8f931b95f4e0c597f82519f2572
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-26 00:49:06 +0000

    Updating archive.

---
+++ trunk/Archive2/dd/37872dbb846f52/main.cpp
@@ -0,0 +1,11 @@
+/* Hello world example
+ 
+   This comment spans several lines
+*/
+#include <iostream>
+ 
+int main() /* This comment spans part of a line */
+{
+    std::cout << "Hello World!\n"; // till the end of the line
+    return 0;
+}
