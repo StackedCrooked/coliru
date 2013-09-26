912ed37e829ef0c1d7e0a26418503fab94d22210
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-26 10:53:53 +0000

    Updating archive.

---
+++ trunk/Archive2/e1/e71cfe19d27852/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+ 
+std::string xfrm(std::string const& input)
+{
+std::string result(1+std::strxfrm(nullptr, input.c_str(), 0), '\0');
+std::strxfrm(&result[0], input.c_str(), result.size());
+ 
+return result;
+}
+ 
+int main()
+{
+using namespace std;
+setlocale(LC_ALL, "es_ES.UTF-8");
+ 
+const string aba = "aba";
+const string rabano = "rábano";
+ 
+cout << "Without xfrm: " << aba << " in " << rabano << " == " <<
+boolalpha << (string::npos != rabano.find(aba)) << "\n";
+ 
+cout << "Using xfrm: " << aba << " in " << rabano << " == " <<
+boolalpha << (string::npos != xfrm(rabano).find(xfrm(aba))) << "\n";
+
+cout << "Using strcoll: " << aba << " <=> " << "ába" << " == " <<
+std::strcoll(aba.c_str(), rabano.c_str()) << "\n";
+}
