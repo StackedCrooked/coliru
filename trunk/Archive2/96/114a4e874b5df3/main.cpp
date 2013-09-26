3724e032a0789094e56ec7077cd328e8e0d242ba
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-26 08:52:49 +0000

    Updating archive.

---
+++ trunk/Archive2/96/114a4e874b5df3/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iterator>
+#include <array>
+#include <utility>
+
+// these templates will be in <utility> for C++14 
+// namespace std
+
+template<unsigned... Indices> 
+struct integer_sequence {};
+
+template<unsigned N, unsigned... Indices>
+struct make_integer_sequence 
+: 
+    make_integer_sequence<N-1, N-1, Indices...>
+{};
+
+template<unsigned... Indices>
+struct make_integer_sequence<0, Indices...> 
+: 
+    integer_sequence<Indices...>
+{};
+
+// } // namespace std
+
+constexpr double fun(double x) { return x * x; }
+
+template<class Function, unsigned... Indices>
+constexpr auto generator(Function f, integer_sequence<Indices...>) 
+-> std::array<typename std::result_of<Function(unsigned)>::type, sizeof...(Indices)> 
+{
+    return {{ f(Indices)... }};
+}
+
+int main() 
+{
+    auto constexpr N = 10;
+    constexpr auto a = generator(fun, make_integer_sequence<N>{});
+    
+    std::copy(std::begin(a), std::end(a), std::ostream_iterator<double>(std::cout, ", ")); 
+}
