d5433c47c7e7bc1f49cb4f530a8e8929fdc3e822
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-26 12:55:14 +0000

    Updating archive.

---
+++ Archive2/b3/5f4cc8b3e94591/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+
+class Class {
+public:
+  void fun() {
+    std::cout << __func__ << std::endl;   
+  }
+};
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
+    std::ostream_iterator<T> out(os, " ");
+    std::copy(std::begin(vec), std::end(vec), out);
+    
+    Class c;
+    c.fun();
+    
+    std::cout << __func__ << std::endl;
+    return os;
+}
+
+
+int main() {
+    std::vector<std::string> words = {
+        "Hello", "from", "GCC", __VERSION__, "!"
+    };
+    std::cout << words << std::endl;
+}
