eaaee79a2cbc189173ac3f2e4c40ae9c14564bc1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-27 08:00:05 +0000

    Updating archive.

---
+++ trunk/Archive2/53/4a61a0f3974af7/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class haus
+{
+    haus()
+    {
+       std::cout << "rofl" << std::endl; 
+    }
+};
+
+int main()
+{
+    haus house();
+    
+}
