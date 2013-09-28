74c22cc669284edd3a2faf46433bc8515e8e566e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-28 12:07:57 +0000

    Updating archive.

---
+++ trunk/Archive2/e1/bed8984dafae6e/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+template<class T>
+void func(std::initializer_list<T> a_args)
+{
+    if (a_args.begin() != a_args.end())
+    {
+        auto last = prev(a_args.end());
+        copy(a_args.begin(), last, ostream_iterator<int>(cout, ","));
+        cout << *last;
+    }
+    cout << endl;
+}
+
+template<class T, class ...Args>
+struct first_of
+{
+    typedef T type;
+};
+
+template<class ...Args>
+initializer_list<typename first_of<Args...>::type> foo(Args&&... args)
+{
+    return { forward<Args>(args)... };
+}
+
+int main()
+{
+    auto x = foo(1,2);
+    func(x);
+}
