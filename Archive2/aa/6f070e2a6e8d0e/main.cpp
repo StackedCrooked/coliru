47fc85291208f3cfba030dc0bd979894de41027e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-09-30 07:24:12 +0000

    Updating archive.

---
+++ Archive2/aa/6f070e2a6e8d0e/main.cpp
@@ -0,0 +1,29 @@
+#include <future>
+
+struct Message {
+    std::string text;
+};
+enum errType { success, failure };
+
+struct MyClass
+{
+    std::future<errType> async_sendMessage(Message msg) {
+        auto fut = std::async(&MyClass::sendMessage, this, msg);
+        return fut;
+    }
+  private:
+    errType sendMessage(Message msg) {
+        return {};
+    }
+};
+
+int main()
+{
+    MyClass object;
+    Message msg { "say hi for me" };
+    auto fut = object.async_sendMessage(msg);
+
+    errType result = fut.get();
+
+    return result == success;
+}
