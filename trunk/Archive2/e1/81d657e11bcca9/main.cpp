291dc78df89fb935b0c1bc5c57edd4d9bbd360ca
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-02 05:17:42 +0000

    Updating archive.

---
+++ trunk/Archive2/e1/81d657e11bcca9/main.cpp
@@ -0,0 +1,11 @@
+#include <cstring>
+#include <cstdio>
+ 
+int main() 
+{
+    char str[50] = "Hello ";
+    char str2[50] = "World!";
+    std::strcat(str, str2);
+    std::strcat(str, " Goodbye World!");
+    std::puts(str);
+}
