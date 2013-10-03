e652b5fbe1aa8d41123119dcf72afcb50114c5f1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-03 22:10:55 +0000

    Updating archive.

---
+++ trunk/Archive2/0f/8412fdfaec4b71/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+int grid_size = 5;
+
+int x = 3;
+std::cout <<  (x/grid_size)*grid_size << std::endl;
+
+x = 5;
+std::cout <<   (x/grid_size)*grid_size << std::endl;
+
+x = 12;
+std::cout <<   (x/grid_size)*grid_size << std::endl;
+
+return 0;
+}
