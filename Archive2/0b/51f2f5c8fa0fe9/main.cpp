6c3605b38f8a720f4182861b5af52302e955f920
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-04 19:23:53 +0000

    Updating archive.

---
+++ Archive2/0b/51f2f5c8fa0fe9/main.cpp
@@ -0,0 +1,12 @@
+//Failed program
+#include <stdio.h>
+#include <math.h>
+
+int main (void)
+{
+    double input, sine_A;
+    input = 6.2830;
+    sine_A = sin(input);
+    printf("sine=%f\n",sine_A);
+    return 0;
+}
