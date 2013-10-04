57ac66233c72fa7293cf770c24cc1ba461d943ee
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-04 09:18:10 +0000

    Updating archive.

---
+++ Archive2/af/b3e00484688910/cmd.sh
@@ -0,0 +1,1 @@
+clang++ -std=c++98 -O3 -pthread main.cpp && ./a.out
---
+++ Archive2/af/b3e00484688910/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <bitset>
+#include <climits>
+
+std::bitset<UINT_MAX> justAVar;
+
+int main()
+{
+   std::cout << UINT_MAX << std::endl;
+   return 0;
+}
