647010289e69e3c42d469800e2347c06cecfff79
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-04 10:20:24 +0000

    Updating archive.

---
+++ Archive2/f7/bb3d104c6ebbdb/main.cpp
@@ -0,0 +1,25 @@
+    
+
+    template <class T> int bit_sizeof(T unused) {
+            long long a,b,c;
+            T x;
+            x^=x;
+            x|=1;
+            a=b=c=x;
+            b<<=8;
+            c<<=3;
+            x=b;
+            while (x) {
+                    a<<=1;
+                    b<<=c;
+                    c<<=1;
+                    x=b;
+            }
+            return a;
+    }
+
+
+int main() {
+  int* pInt;
+  auto i = bit_sizeof(pInt);
+}
