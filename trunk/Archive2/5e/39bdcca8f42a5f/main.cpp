8eb79d74b0777a451060a22a36e4b8dde70cb28e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-05 08:32:04 +0000

    Updating archive.

---
+++ trunk/Archive2/5e/39bdcca8f42a5f/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+#include <thread>
+#include <future>
+#include <functional>
+ 
+ //wrapping to avoid 'unresolved overloaded function type' error
+ int ipow(int a, int b)
+ {
+     return std::pow(a, b);
+ }
+ 
+void task_lambda()
+{
+    std::packaged_task<int(int,int)> task([](int a, int b) {
+        return std::pow(a, b); 
+    });
+    std::future<int> result = task.get_future();
+ 
+    task(2, 9);
+ 
+    std::cout << "task_lambda:\t" << result.get() << '\n';
+}
+ 
+void task_bind()
+{
+    std::packaged_task<int()> task(std::bind(ipow, 2, 11));
+    std::future<int> result = task.get_future();
+ 
+    task();
+ 
+    std::cout << "task_bind:\t" << result.get() << '\n';
+}
+ 
+void task_thread()
+{
+    std::packaged_task<int(int,int)> task(ipow);
+    std::future<int> result = task.get_future();
+ 
+    std::thread task_td(std::move(task), 2, 10);
+    task_td.join();
+ 
+    std::cout << "task_thread:\t" << result.get() << '\n';
+}
+ 
+int main()
+{
+    task_lambda();
+    task_bind();
+    task_thread();
+}
