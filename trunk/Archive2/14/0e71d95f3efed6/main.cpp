fcc9c9a47142aed8906254a63aab3bc4e62fb360
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-05 21:40:00 +0000

    Updating archive.

---
+++ trunk/Archive2/14/0e71d95f3efed6/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <locale>
+#include <iomanip>
+
+// method one; wotks for integer types
+void print_with_commas( unsigned int n )
+{
+    if( n < 1000 )
+    {
+        std::cout << n ;
+    }
+    else
+    {
+        int remainder = n % 1000 ;
+        print_with_commas( n / 1000 ) ;
+        std::cout << ',' << remainder ;
+    }
+}
+
+int main()
+{
+    const int n = 123456789 ;
+
+    print_with_commas(n) ;
+    std::cout << '\n' ;
+
+    {
+        // method two; works for all numeric types
+
+        // see: http://en.cppreference.com/w/cpp/locale/numpunct/grouping
+        struct with_commas : std::numpunct<char>
+        {
+            protected:
+
+               // group every three digits before the decimal point
+               virtual std::string do_grouping() const override { return "\3"; }
+
+               // use ',' as the group seperator
+               virtual char do_thousands_sep() const override { return ',' ; }
+        };
+
+        // see: http://en.cppreference.com/w/cpp/io/basic_ios/imbue
+        // http://en.cppreference.com/w/cpp/locale/locale/locale overload (7)
+        // the locale calls delete on the facet when it is destroyed.
+        std::cout.imbue( std::locale( std::cout.getloc(), new with_commas ) );
+
+        std::cout << n << '\n' ;
+
+        const double d = 1234567890.987654321 ;
+        std::cout << std::fixed << std::setprecision(2) << d << '\n' ;
+
+    }
+}
