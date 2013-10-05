d84549465219fbf73c6215c591191745319bd48d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-05 22:40:53 +0000

    Updating archive.

---
+++ trunk/Archive2/a3/a75f88a478c0d3/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+// works for integer types
+void print_with_commas( unsigned long long n )
+{
+    const unsigned int THOUSAND = 1000 ;
+
+    if( n < THOUSAND )
+    {
+        std::cout << n ;
+    }
+    else
+    {
+        int remainder = n % THOUSAND ;
+        print_with_commas( n / THOUSAND ) ;
+        std::cout << ',' << remainder ;
+    }
+}
+
+int main()
+{
+    const int n = 123456789 ;
+    print_with_commas(n) ; // 123,456,789
+    std::cout << '\n' ;
+
+    int m = -1234567 ;
+    if( m < 0 )
+    {
+        std::cout << '-' ;
+        m = -m ;
+    }
+    print_with_commas(m) ; // -1,234,567
+    std::cout << '\n' ;
+}
