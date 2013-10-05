cb1ca10e88f0f256747bfca3987d325aa5bd6575
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-05 09:32:41 +0000

    Updating archive.

---
+++ Archive2/98/fc41b5c19caaf4/main.cpp
@@ -0,0 +1,19 @@
+#include <limits>
+#include <iostream>
+ 
+int main() 
+{
+    std::cout << "type\tlowest\thighest\n";
+    std::cout << "int\t"
+              << std::numeric_limits<int>::lowest() << '\t'
+              << std::numeric_limits<int>::max() << '\n';
+    std::cout << "float\t"
+              << std::numeric_limits<float>::lowest() << '\t'
+              << std::numeric_limits<float>::max() << '\n';
+    std::cout << "double\t"
+              << std::numeric_limits<double>::lowest() << '\t'
+              << std::numeric_limits<double>::max() << '\n';
+    std::cout << "unsigned\t"
+              << std::numeric_limits<uint32_t>::min() << '\t'
+              << std::numeric_limits<uint32_t>::max() << '\n';
+}
