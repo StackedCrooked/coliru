71d0053018cde88dbffba1db9615f9b7bca4e90f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-05 12:33:55 +0000

    Updating archive.

---
+++ Archive2/f8/2a20c24a402a53/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <algorithm>
+#include <omp.h>
+#include <boost/bind.hpp>
+
+typedef std::list<std::string> strlist;
+
+// STL find in 2 lines of code
+void find(strlist *lst, std::string search)
+{
+   strlist::iterator it = std::find_if(lst->begin(), lst->end(), boost::bind(std::equal_to<std::string>(), _1, search));
+   std::cout << "'" << search << (it != lst->end() ? "' found!" : "' not found!") << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+   strlist strings;
+   
+   strings.push_back("this");
+   strings.push_back("is");
+   strings.push_back("a list");
+   strings.push_back("of strings");
+   
+   double start = omp_get_wtime();
+   find(&strings, "a list");
+   find(&strings, "this");
+   std::cout << "* benchmark: " << omp_get_wtime() - start << "seconds" << std::endl;
+   
+   return 0;
+}
