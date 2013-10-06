74fe24b1e54235f634b3237a3befa123eb0a7184
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-06 14:48:29 +0000

    Updating archive.

---
+++ trunk/Archive2/32/a56df48bc1debe/main.cpp
@@ -0,0 +1,59 @@
+
+template<int ...N> struct list { using type = list<N...>; };
+
+template<int N, typename IntList> 
+struct prepend;
+
+template<int N, int ... ints> 
+struct prepend<N, list<ints...>>  : list<N, ints...> {};
+
+namespace detail
+{
+    template<int i, typename IntList> struct insert;
+    
+    template<int i, int ...ints> 
+    struct insert_impl : list<i> {};
+    
+    template<int i, int A, int ...ints> 
+    struct insert_impl<i, A, ints...>
+    {
+        const static int small = i < A ? i : A;
+        const static int big = i + A - small;
+        using type = typename prepend<small, typename insert_impl<big, ints...>::type>::type;
+    };
+    
+    template<int i, int ...ints> 
+    struct insert<i, list<ints...>> : insert_impl<i, ints...> {};
+}
+
+template<typename IntList> 
+struct sort : list<> {};
+
+template<int A, int ...N> 
+struct sort<list<A,N...>> : detail::insert<A, typename sort<list<N...>>::type> {}; 
+
+///////////////////////////USAGE////////////////////////////////
+template<int...N>
+void f() 
+{
+    //this line is just to generate compilation error so that
+    //you can see the sorted ints in the error message
+    list<N...> generate_error = 0;
+}
+
+template<int...N>
+void invoke_f_with(list<N...>) 
+{
+    f<N...>();
+}
+ 
+template<int...N>
+void g()
+{
+  invoke_f_with(typename sort<list<N...>>::type{});
+}
+
+int main()
+{
+    g<4,7,2,9,3,7>();
+}
