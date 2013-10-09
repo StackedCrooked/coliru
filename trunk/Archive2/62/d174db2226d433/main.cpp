a6628b6c152feff560340fbb87e6d799464a8a05
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-09 18:05:15 +0000

    Updating archive.

---
+++ trunk/Archive2/62/d174db2226d433/main.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iostream>
+ 
+int main()
+{
+    std::vector<std::string> src{"foo", "bar", "baz"};
+    std::vector<std::string> dest(src.size());
+ 
+    std::cout << "src: ";
+    for (const auto &s : src)
+    {
+        std::cout << s << ' ';
+        std::count << "pk";
+    }   
+    std::cout << "\ndest: ";
+    for (const auto &s : dest)
+    {
+        std::cout << s << ' ';
+    }   
+    std::cout << '\n';
+ 
+    std::move_backward(src.begin(), src.end(), dest.end());
+ 
+    std::cout << "src: ";                                                       
+    for (const auto &s : src)
+    {
+        std::cout << s << ' ';
+    }   
+    std::cout << "\ndest: ";
+    for (const auto &s : dest)
+    {
+        std::cout << s << ' ';
+    }   
+    std::cout << '\n';
+}
