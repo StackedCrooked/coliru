115f5c6dd405849f80a33f7e32def0debd0dabd8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-15 11:35:07 +0000

    Updating archive.

---
+++ trunk/Archive2/1e/5b9e7632407b7e/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <utility>
+
+template<typename F, typename... Fs>
+class composition {
+private:
+    F f;
+    composition<Fs...> fs;
+
+public:
+    explicit composition(F f_, Fs... fs_) : f(std::move(f_)), fs(std::move(fs_)...) { }
+
+    template<typename... Args>
+    auto operator()(Args&&... args) -> decltype(f(fs(std::forward<Args>(args)...))) {
+        return f(fs(std::forward<Args>(args)...));
+    }
+};
+
+template<typename F>
+class composition<F> {
+private:
+    F f;
+
+public:
+    explicit composition(F f_) : f(std::move(f_)) { }
+
+    template<typename... Args>
+    auto operator()(Args&&... args) -> decltype(f(std::forward<Args>(args)...)) {
+        return f(std::forward<Args>(args)...);
+    }
+};
+
+template<typename... Fs>
+composition<Fs...> compose(Fs... fs) {
+    return composition<Fs...>(std::move(fs)...);
+}
+
+int main() {
+    auto f = compose([] (int x) { return x * 2; }, [] (int x) { return x + 2; }, [] (int x) { return x / 2; });
+
+    std::cout << f(42) << '\n';
+    return 0;
+}
