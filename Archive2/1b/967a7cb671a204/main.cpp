ab7790818a1c104cfce984dd612c7ca7f1ed0b33
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-15 13:38:25 +0000

    Updating archive.

---
+++ Archive2/1b/967a7cb671a204/main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int
+main (int argc, char **argv)
+{
+    (void)argc;
+  char buf1[1000],buf2[1000],buf3[1000];
+
+  char *p1 = stpcpy (buf1, (const char *) argv[0]); //this doesn't work
+  printf ("buf1 = %s, p1 = %s\n", buf1, p1);
+
+  stpcpy (buf2, (const char *) argv[0]); // this works
+  printf ("buf2 = %s\n", buf2);
+
+  char *p3 = stpcpy (buf3, (const char *) "123"); // this works too
+  printf ("buf3 = %s, p3 = %s\n", buf3, p3);
+
+  return 0;
+}
