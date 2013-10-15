a06649c6b711b71ec14ad13b9d09390f131d764c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-15 08:34:02 +0000

    Updating archive.

---
+++ Archive2/44/bd49ebd5be946e/main.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <forward_list>
+#include <iostream>
+#include <iterator>
+
+template<class FwdIt, class T>
+FwdIt find_before_first(FwdIt first, FwdIt last, T const& value)
+{
+     return std::adjacent_find(first, last, [&](int const&, int const& R) { return R == value; });
+}
+
+int main() 
+{
+     auto mylist = std::forward_list<int>{ 1, 2, 3, 4, 3, 5 };
+     mylist.erase_after(find_before_first(mylist.before_begin(), end(mylist), 3));
+     std::copy(begin(mylist), end(mylist), std::ostream_iterator<int>(std::cout, ","));
+}
