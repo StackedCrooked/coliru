514a968f2e0a2c22bc00790fb02093ad9ff5121c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-15 18:40:49 +0000

    Updating archive.

---
+++ Archive2/5f/a086f65ecd8d37/main.cpp
@@ -0,0 +1,45 @@
+struct test {
+public:
+    
+    template <unsigned int n>
+    test ( const char (&x)[n] ) {
+        
+    }
+};
+
+template <typename T>
+struct testa {
+public:
+    
+    template <unsigned int n>
+    testa ( T (&x)[n] ) {
+        
+    }
+};
+
+struct aggregate {
+public:
+    testa<const char> arf;
+    
+};
+
+int main () {
+    test t1 = test( "arf" );
+    test t2( "arf" );
+    test t3 = test{ "arf" };
+    test t4 = { "arf" };
+    test t5{ "arf" };
+    
+    testa<const char> ta1 = testa<const char>( "arf" );
+    testa<const char> ta2( "arf" );
+    testa<const char> ta3 = testa<const char>{ "arf" };
+    testa<const char> ta4 = { "arf" };
+    testa<const char> ta5{ "arf" };
+    
+    aggregate agg1{"test"};
+    aggregate agg2 = {"test"};
+    
+    /*aggregate aggregatetest[] = {
+       { "arf" }
+    };*/
+}
