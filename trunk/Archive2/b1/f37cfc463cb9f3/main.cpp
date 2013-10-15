fc9306b102568ff5effa1ad09d690735285bf6d6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-15 14:39:21 +0000

    Updating archive.

---
+++ trunk/Archive2/b1/f37cfc463cb9f3/main.cpp
@@ -0,0 +1,8 @@
+#include <string>
+#include <iostream>
+
+int main() {
+  std::wstring s0(L"foo");
+  std::wstring s1(L"bar");
+  std::cout << "The two strings are " << (s0 == s1 ? "equal" : "not equal") << std::endl;
+}
