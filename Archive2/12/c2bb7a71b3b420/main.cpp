29ac724a1e027b8d2ca7588824993df2777bc499
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-15 20:41:35 +0000

    Updating archive.

---
+++ Archive2/12/c2bb7a71b3b420/main.cpp
@@ -0,0 +1,36 @@
+#include <boost/phoenix/phoenix.hpp>
+#include <boost/phoenix/function/adapt_function.hpp>
+#include <map>
+#include <string>
+
+namespace detail
+{
+    template <typename Pair, typename First = typename std::remove_reference<Pair>::type::first_type> 
+    struct first_type {
+        typedef First type;
+    };
+    
+    template <typename Pair>
+    typename first_type<Pair>::type first_impl(Pair const& p){
+        return p.first;
+    }
+}
+
+BOOST_PHOENIX_ADAPT_FUNCTION(typename detail::first_type<A0>::type, first, detail::first_impl, 1)
+
+#include <algorithm>
+#include <iostream>
+
+int main()
+{
+    using boost::phoenix::arg_names::arg1;
+    
+    std::map<std::string, int> const m { 
+        { "one",   1 },
+        { "two",   2 },
+        { "three", 3 },
+        { "four",  4 }
+    };
+
+    std::for_each(begin(m), end(m), std::cout << first(arg1) << "\n");
+}
