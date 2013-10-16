d14a29d982da7d762618d5fd73033a09faad05a1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-16 18:58:44 +0000

    Updating archive.

---
+++ trunk/Archive2/0e/13586e119415eb/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int x;
+int a;
+int main()
+{
+    for( a = 1; a <= 20; x =- 3.5 *( a++ ) - 12 )
+    for( int z; a > 2 && a <= 20; z = - 3.5 *( a++ ) - 12 )
+         cout << "Suma wyrazow ciagu=" << z << "\t" << "Wyraz ciagu=" << x << endl;
+}
