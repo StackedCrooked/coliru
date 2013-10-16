96012a2406d285a956f969611c93d94909ef5bbb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-16 22:02:22 +0000

    Updating archive.

---
+++ Archive2/ed/54772531f25af9/main.cpp
@@ -0,0 +1,17 @@
+#include <set>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+int main()
+{
+    string str("Hello");
+    
+    str.insert(3, "-");
+    
+    cout << str;
+
+}
