db5d97c7f388a2fb96b3c04beda47d8de52b83a2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-17 19:17:01 +0000

    Updating archive.

---
+++ trunk/Archive2/28/ecd761687a162b/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+    if (argc != 2) return 1;
+	std::string str(argv[1]);
+
+	tm tm1;
+
+	sscanf(str.c_str(), "%4d%2d%2d-%2d%2d%2d", &tm1.tm_year, &tm1.tm_mon,
+           &tm1.tm_mday, &tm1.tm_hour, &tm1.tm_min, &tm1.tm_sec);
+    tm1.tm_year -= 1900; // Years since 1900
+    tm1.tm_mon--; // 0-aligned
+
+	std::cout << tm1.tm_year << std::endl << tm1.tm_mon << std::endl << tm1.tm_mday << std::endl;
+	std::cout << tm1.tm_hour << std::endl << tm1.tm_min << std::endl << tm1.tm_sec  << std::endl;
+    
+    char outchar[14];
+    strftime(outchar, sizeof(outchar), "%Y%m%d-%H%M", &tm1);
+    std::cout << outchar << std::endl;
+}
