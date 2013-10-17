b84a0ce5e43617c1e383d406f0c6826c502d73d9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-17 01:04:42 +0000

    Updating archive.

---
+++ Archive2/68/687bdcfe5f6690/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdint.h>
+
+uint64_t factorial(uint64_t n) {
+    uint64_t i = 2;
+    uint64_t v = 1;
+    for (; i <= n; i++) {
+        v *= i;
+    }
+
+    return v;
+}
+
+int main() {
+    uint64_t i = 1;
+    for (; i < 24; i++) {
+        printf("%+4lu: %lu\n", i, factorial(i));
+    }
+
+    return 0;
+}
