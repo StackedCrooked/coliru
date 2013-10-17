ca1887705fc96c3da85c0069e85c2278db7b9b9c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-17 19:17:13 +0000

    Updating archive.

---
+++ trunk/Archive2/f0/60f8756da31056/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	std::string str = "20101127 090600";
+
+	tm *tm1 = new tm();
+
+	sscanf(str.c_str(), "%4d%2d%2d %2d%2d%2d", &tm1->tm_year, &tm1->tm_mon,
+           &tm1->tm_mday, &tm1->tm_hour, &tm1->tm_min, &tm1->tm_sec);
+
+	std::cout << tm1->tm_year << std::endl << tm1->tm_mon << std::endl << tm1->tm_mday << std::endl;
+	std::cout << tm1->tm_hour << std::endl << tm1->tm_min << std::endl << tm1->tm_sec  << std::endl;
+    
+    char outchar[14];
+    strftime(outchar, sizeof(outchar), "%Y%m%d-%H%M", tm1);
+    std::cout << outchar << std::endl;
+    delete tm1;
+}
