c7b0817a9aca3716f51f4e9fec34233850dc5691
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-17 00:03:05 +0000

    Updating archive.

---
+++ trunk/Archive2/3e/857f03ff619756/cmd.sh
@@ -0,0 +1,5 @@
+set -x ; g++-4.8 -std=c++11 -O2 -Wall -pedantic -pthread main.cpp && ./a.out <<HERE
+5 1 1 2 2 3 3 4 4 5 5
+7 1 1 2 2 3 3 4 4 4 7 7 4 7 7
+8 1 1 2 4 3 9 4 16 5 25 6 36 7 49 8 64
+HERE
