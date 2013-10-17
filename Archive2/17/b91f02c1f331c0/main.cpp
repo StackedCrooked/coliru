7bc1208a081bf877a3e807b50552f45e117eb7f7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-17 00:02:45 +0000

    Updating archive.

---
+++ Archive2/17/b91f02c1f331c0/main.cpp
@@ -0,0 +1,28 @@
+#include <map>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+int main()
+{
+    map<string, int> dict;  // <KeyType, ValueType>
+    
+    dict["Mary"] = 10;
+    
+    
+    pair<string, int> entry("Sandy",30);
+    dict.insert(entry);
+    
+    dict.insert(make_pair("John",20));
+
+    if( dict.insert(make_pair("John",40)).second ) {
+        cout << "John inserted";
+    } else {
+        cout << "John already there";
+    }
+
+
+}
