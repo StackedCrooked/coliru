c79d9cad72356c2e9a1ffb3fe2689efa1dc1cccc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-17 16:12:41 +0000

    Updating archive.

---
+++ Archive2/d3/3d438d5d51353a/main.cpp
@@ -0,0 +1,34 @@
+//  Convert this program to C++
+//   change to C++ io
+//   change to one line comments
+//   change defines of constants to const
+//   change array to vector<>
+//  inline any short function
+
+#include <iostream>      // for std::cout
+#include <vector>          // for std::vector
+#include <numeric>       // for std::accumulate
+#include <algorithm>     // for std::generate
+
+const int N=40;
+
+using namespace std;
+
+// returns the sum of the contents of a vector<int>
+inline int sum(vector<int> d) { return accumulate(d.begin(), d.end(), 0); }
+
+int main()
+{
+   // initialize array data (as an array we define STL vector<int>)
+   vector<int> data(N);
+  
+   // STL generic function generate and lambda expression initilizes
+   // vector data with initial values 0, 1, 2, 3, 4, ...
+   int i(0);
+   generate(data.begin(), data.end(), [&](){ return i++; });
+
+   int accum = sum(data);
+   cout << "sum is " << accum << endl;
+
+return 0;
+}
