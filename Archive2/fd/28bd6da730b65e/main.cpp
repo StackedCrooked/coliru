d1aa2a704cac97340043e12a3662a202aed72d9b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-18 01:18:23 +0000

    Updating archive.

---
+++ Archive2/fd/28bd6da730b65e/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <getopt.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    enum {
+        total, unique, individual
+    } mode = total;
+
+    for (int c; (c = getopt(argc, argv, "tu")) != -1;) {
+        switch (c) {
+            case 't': mode = total;
+                break;
+            case 'u': mode = unique;
+                break;
+            case 'i': mode = individual;
+                break;
+        }
+    }
+
+    argc -= optind;
+    argv += optind;
+
+    using WordIndex = map<string, unsigned>;
+    using WordInfo  = WordIndex::value_type;
+    WordIndex tally;
+
+    for_each(
+            istream_iterator<string>(cin), {}, 
+            [&tally] (std::string const& word) mutable { tally[word]++; }
+        );
+
+    switch (mode) {
+        case total: 
+            {
+                auto total = std::accumulate(begin(tally), end(tally), 0u, [](unsigned accum, WordInfo const& el) { return accum + el.second; });
+                cout << "Total " << total << endl;
+            }
+            break;
+        case unique: 
+            cout << "Unique " << tally.size() << endl;
+            break;
+        case individual:
+            for (auto const& entry: tally)
+                cout << entry.first << "\t" << entry.second << "\n";
+            break;
+    }
+}
