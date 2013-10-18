3ed18c69e238722cdd26847918dd4f6b5a7b9112
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-18 10:25:14 +0000

    Updating archive.

---
+++ trunk/Archive2/e3/4ac14f1c64fe83/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+
+template<typename F, typename S>
+std::ostream& operator<<(std::ostream& os, const std::pair<F, S>& t) {
+    return os << '(' << t.first << ',' << t.second << ')';
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
+    for (auto& el : vec) {
+        os << el << ' ';
+    }
+    return os;
+}
+
+int main()
+{
+    constexpr unsigned int width = 6;
+    constexpr unsigned int height = 3;
+    const unsigned int array[height][width] = {
+        {1,0,0,0,1,0},
+        {0,1,0,0,1,1},
+        {1,0,0,1,0,0},
+    };
+    typedef std::vector<std::pair<unsigned int, unsigned int>> colonies;
+    colonies left;
+    colonies right;
+    for (unsigned int y = 0; y < height; ++y) {
+        colonies * vector = &left;
+        for (unsigned int x = 0; x < width; ++x) {
+            const unsigned int value = array[y][x];
+            if (value == 0) {
+                vector = &right;
+            } else {
+                vector->push_back(std::make_pair(y, x));
+            }
+        }
+    }
+    std::cout << "Right: " << std::endl << right << std::endl;
+    std::cout << "Left: " << std::endl << left << std::endl;
+}
