f0d9167dd76d0d67ea7c18aba148757f432ad99b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-18 02:18:40 +0000

    Updating archive.

---
+++ Archive2/e2/a99f7d7d4dc0a0/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <getopt.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    enum {
+        total, unique, individual
+    } mode = total;
+
+    for (int c; (c = getopt(argc, argv, "tui")) != -1;) {
+        switch (c) {
+            case 't': mode = total;
+                break;
+            case 'u': mode = unique;
+                break;
+            case 'i': mode = individual;
+                break;
+        }
+    }
+
+    argc -= optind;
+    argv += optind;
+
+    map<string, unsigned> tally;
+
+    unsigned long total_count = 0;
+    for_each(
+            istream_iterator<string>(cin), {}, 
+            [&] (std::string const& word) mutable { total_count++; tally[word]++; }
+        );
+
+    switch (mode) {
+        case total: 
+            cout << "Total " << total_count << endl;
+            break;
+        case unique: 
+            cout << "Unique " << tally.size() << endl;
+            break;
+        case individual:
+            for (auto const& entry: tally)
+                cout << entry.first << "\t" << entry.second << "\n";
+            break;
+    }
+}
