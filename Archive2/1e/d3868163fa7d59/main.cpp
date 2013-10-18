6f55a3349c1c8aa5ecf4d3358c7172d30c3d22bb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-18 20:30:17 +0000

    Updating archive.

---
+++ Archive2/1e/d3868163fa7d59/main.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+
+
+namespace Detail {
+
+// Overload resolution:
+// (1) decltype removes invalid candidates (SFINAE)
+// (2) int/long parameter resolves remaining ambiguity
+
+template<typename Container, typename T>
+auto find(Container&& container, T&& val, int) -> decltype(container.find(val))
+{
+    return container.find(val);
+}
+
+template<typename Container, typename T>
+auto find(Container&& container, T&& val, long) -> decltype(std::find(container.begin(), container.end(), val))
+{
+    return std::find(container.begin(), container.end(), val);
+}
+
+} // namespace Detail
+
+
+// Finds an item in a container.
+// Uses `container.find(...)` if available, otherwise `std::find(...)` is used.
+template<typename Container, typename T>
+auto find(Container&& container, T&& val)
+{
+    return Detail::find(container, val, int{}); // prefer int overload in case of ambiguity
+}
+
+
+int main()
+{    
+    std::set<std::string> wordSet = { "abc", "test", "123" };
+    std::cout << *find(wordSet, "test") << std::endl;
+        
+    std::vector<std::string> wordVector = { "abc", "test", "123" };
+    std::cout << *find(wordVector, "test") << std::endl;
+}
