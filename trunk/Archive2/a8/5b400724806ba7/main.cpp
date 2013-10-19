4a7a60b49beffff65f0713bd99e3572a24a7080a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-19 14:41:13 +0000

    Updating archive.

---
+++ trunk/Archive2/a8/5b400724806ba7/main.cpp
@@ -0,0 +1,38 @@
+#include <map>
+#include <vector>
+#include <list>
+#include <unordered_map>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+using namespace std::placeholders;
+
+struct accumulate_multiply {
+    int *sum;
+	int *product;
+	accumulate_multiply(int *s,int *p)	: sum(s), product(p) 
+    {
+    }
+    
+    void operator() (int a_number) { 
+        *sum += a_number;  
+        *product *= a_number; 
+    }
+};
+
+
+
+
+
+int main()
+{
+    int arr[] = { 1, 2, 3, 4, 5 };
+    vector<int> numbers(arr,arr+5);
+    int total = 0, product =1 ;
+    std::for_each(numbers.begin(), numbers.end(), 
+                accumulate_multiply( &total, &product )  );
+    cout << total << " " << product;
+}
