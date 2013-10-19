dbc6a668c38413f6ee721db3ccfaab67de5d9d12
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-19 10:39:33 +0000

    Updating archive.

---
+++ trunk/Archive2/0d/b181f75261e657/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main()
+{
+    int x;
+    int y;
+    int sum;
+    cout<<"Enter first number:";
+    cin>>x;
+    cout<<"Enter second number:";
+    cin>>y;
+    sum=x+y;
+    cout<<"Sum is : "<<sum;
+    
+}
