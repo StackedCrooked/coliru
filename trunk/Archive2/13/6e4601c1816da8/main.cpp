2a4383680e859671caa6753c3f8567a3fc5bc6dd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-19 13:40:37 +0000

    Updating archive.

---
+++ trunk/Archive2/13/6e4601c1816da8/main.cpp
@@ -0,0 +1,34 @@
+#include <map>
+#include <vector>
+#include <list>
+#include <unordered_map>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+using namespace std::placeholders;
+
+template<class T, class InputIterator> 
+T sum( InputIterator first, InputIterator last )
+{
+    T tot = 0;
+    for( ; first != last; ++first )
+    {
+        tot += *first;
+    }
+    return tot;
+}
+
+
+
+int main()
+{
+    int arr[] = { 1, 2, 3, 4, 5 };
+    vector<int> numbers(arr,arr+5);
+
+    cout << sum<int>(numbers.begin(), numbers.end());
+    
+    cout << accumulate(numbers.begin(), numbers.end(), 0);
+}
