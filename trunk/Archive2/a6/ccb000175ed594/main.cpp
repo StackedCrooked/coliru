4bae4c548c8f35da2212d9f2d4c3917c0b9e8e79
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-19 11:39:49 +0000

    Updating archive.

---
+++ trunk/Archive2/a6/ccb000175ed594/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <cctype>
+
+int main()
+{
+    std::ifstream file( __FILE__ ) ; // this source file
+
+    int upper = 0 ;
+    int lower = 0 ;
+    int digit = 0 ;
+    int whitespace = 0 ;
+    int punct = 0 ;
+    int other = 0 ;
+
+    char c ;
+    while( file.get(c) ) // reads every byte including white spaces
+    {
+        if( std::isupper(c) ) ++upper ; 
+        else if( std::islower(c) ) ++lower ;
+        else if( std::isdigit(c) ) ++digit ;
+        else if( std::isspace(c) ) ++whitespace ;
+        else if( std::ispunct(c) ) ++punct ;
+        else ++other ;
+    }
+
+    std::cout << "upper: " << upper << '\n' // 'A' 'B' ...
+              << "lower: " << lower << '\n' // 'a' 'b' ...
+              << "digit: " << digit << '\n' // '0' '1' ...
+              << "space: " << whitespace << '\n' // ' ' '\n' ...
+              << "punct: " << punct << '\n' // '?' '.' ...
+              << "other: " << other << '\n' ; 
+}
