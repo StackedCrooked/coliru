51ca71792ba1b751432ea56b8246979c2cfeb501
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-19 12:40:25 +0000

    Updating archive.

---
+++ trunk/Archive2/e1/796add6593906e/main.cpp
@@ -0,0 +1,30 @@
+#include <map>
+#include <vector>
+#include <list>
+#include <unordered_map>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+using namespace std::placeholders;
+
+
+int sum( list<int> v )
+{
+int tot = 0;
+for( list<int>::iterator it = v.begin(); it != v.end(); ++it )
+{
+tot += *it;
+}
+return tot;
+}
+
+int main()
+{
+    int arr[] = { 1, 2, 3, 4, 5 };
+    list<int> numbers(arr,arr+5);
+    cout << sum(numbers);
+
+}
