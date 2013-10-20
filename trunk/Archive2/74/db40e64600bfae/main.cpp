3ed5f31bd421e20fe3afe573e8dfe0166c1a157a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-20 13:51:09 +0000

    Updating archive.

---
+++ trunk/Archive2/74/db40e64600bfae/main.cpp
@@ -0,0 +1,34 @@
+#include <string>
+
+using namespace std;
+
+//NOTE: this class is just an example to demonstrate the problem
+class String {
+    public:
+        //default constructor to prevent ambiguity
+        String() {}
+
+    private:
+        //construct from wstring
+        template<typename... Args, class = typename enable_if<is_constructible<wstring, Args...>::value, void>::type>
+        String(int, Args&&... args) : ws(forward<Args>(args)...) {}
+
+        //construct from string
+        template<typename... Args, class = typename enable_if<is_constructible< string, Args...>::value, void>::type>
+        String(int&&, Args&&... args) : s(forward<Args>(args)...) {}
+        
+    public:
+        template<typename... Args>
+        String(Args&&... args) : String(0, forward<Args>(args)...) {}
+        
+    private:
+        string s;
+        wstring ws;
+};
+
+int main()
+{
+    String a(L"123");
+    String b("123");
+    return 0;
+}
