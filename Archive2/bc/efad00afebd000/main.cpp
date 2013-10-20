11cb4553a5cc06a69d8174fe3cff1c897fb4372a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-20 08:49:36 +0000

    Updating archive.

---
+++ Archive2/bc/efad00afebd000/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 1:
+	int m = [](int x)
+	{
+		return [](int y)
+			{
+				return y*2;
+			}(x)+3;
+	}(5);
+	cout<<m<<endl;
+
+	// 2:
+	auto ff=[](int x,int y)
+	{
+		return x+y;
+	};
+	cout<<ff(21,12)<<endl;
+}
