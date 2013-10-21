54528d2309f18403e79964679fc8093e7d38fd97
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-21 14:05:30 +0000

    Updating archive.

---
+++ trunk/Archive2/24/253c0565a633d4/cmd.sh
@@ -0,0 +1,1 @@
+g++-4.8 -std=c++11 -Wall -pedantic -DVAR3 main.cpp && ./a.out
---
+++ trunk/Archive2/24/253c0565a633d4/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+int main()
+{
+    std::vector<std::string> words = {
+        "Hello", "from", "GCC", __VERSION__, "!"
+    };
+    
+    std::cout << "..................................." << std::endl;
+    
+#if !defined(VAR1) && !defined(VAR2)
+    std::cout << "VAR1 and VAR2 not defined" << std::endl;
+#else
+    std::cout << "VAR1 or VAR2 defined" << std::endl;   
+#endif
+    
+    
+    std::cout << "..................................." << std::endl;
+}
