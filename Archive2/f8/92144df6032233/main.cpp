883635144492d996f469a0515f267bab9be9bda6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-21 11:03:35 +0000

    Updating archive.

---
+++ Archive2/f8/92144df6032233/main.cpp
@@ -0,0 +1,23 @@
+#include <sstream>
+#include <iostream>
+
+void print_to_console() {
+    std::cout << "Hello from print_to_console()" << std::endl;
+}
+
+void foo(){
+  std::cout<<"hello world"<<std::endl; 
+  print_to_console(); // this could be printed from anything
+}
+int main()
+{
+    std::stringstream ss;
+    
+    auto old_buf = std::cout.rdbuf(ss.rdbuf()); //change the underlying buffer
+    
+    foo(); //all the std::cout goes to ss
+    
+    std::cout.rdbuf(old_buf); //reset
+    
+    std::cout << "Start output from ss\n" << ss.str() <<  "End" << std::endl;
+}
