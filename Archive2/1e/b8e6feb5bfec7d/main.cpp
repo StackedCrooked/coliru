7a57c94ec741e9c2c3d3e00166196646dee28e14
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-21 21:09:29 +0000

    Updating archive.

---
+++ Archive2/1e/b8e6feb5bfec7d/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void foo( const std::string& path_to_file )
+{
+   std::ifstream file(path_to_file) ;
+   std::streamoff cnt = 0 ;
+   char ch ;
+   while( file.get(ch) ) if( ch == '\n' ) ++cnt ;
+   std::cout << "*** " << cnt << " ***\n" ;
+}
+
+int main()
+{
+    std::string path_to_file = __FILE__ ;
+    std::ifstream file(path_to_file) ;
+    std::string line ;
+
+    for( int i = 0 ; i < 10 && std::getline(file,line) ; ++i )
+        std::cout << line << '\n' ;
+
+    std::cout << "\n-----------------------------\n" ;
+    foo(path_to_file) ;
+    std::cout << "\n-----------------------------\n" ;
+
+    while( std::getline(file,line) ) std::cout << line << '\n' ;
+}
