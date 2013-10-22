7f5ea7c25671455abc0ffd25738e498086ecd5a6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-22 18:18:04 +0000

    Updating archive.

---
+++ trunk/Archive2/f3/2014f75e5e7d8f/main.cpp
@@ -0,0 +1,24 @@
+#include <boost/mpl/for_each.hpp>
+#include <boost/mpl/vector.hpp>
+#include <typeinfo>
+#include <iostream>
+
+struct PrintType
+{
+    template<typename T> void operator()(T) const
+    {
+        std::cout << typeid(T).name() << std::endl;
+    }
+};
+
+template<typename ...Ts>
+void foo(Ts...)
+{
+    using namespace boost::mpl;
+    for_each<vector<Ts...>>(PrintType());
+}
+
+int main()
+{
+    foo(1, 1., '1', "1");
+}
