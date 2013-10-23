244a3ac5473cb9e8c72ff71153ac611540e6beac
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-23 12:26:03 +0000

    Updating archive.

---
+++ Archive2/26/7279405be9289d/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <iterator>
+#include <boost/generator_iterator.hpp>
+
+int main()
+{
+  const std::string data = "hello";
+  auto curr = data.end();
+  
+  std::function<char()> gen = [curr,data]() mutable -> char
+  { 
+      if (curr==data.end())
+          curr = data.begin();
+      return *curr++;
+  };
+
+  auto it = boost::make_generator_iterator(gen);
+  std::copy_n(it, 35, std::ostream_iterator<char>(std::cout, ";"));
+}
