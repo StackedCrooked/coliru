4de489dcabbea5d9e3a486bb6f6d72add256c175
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-23 18:29:22 +0000

    Updating archive.

---
+++ trunk/Archive2/0e/898c59f8b1ef12/main.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <iostream>
+
+int add()
+{
+ return 0;
+}
+
+template<typename T>
+int add(T a) {
+    return a;
+}
+
+template <typename A, typename B, typename... Args>
+int add(A a, B b, Args... args)
+{
+ return add(a + b, args...);
+}
+
+int main()
+{
+    std::cout << add() << "\n";
+    std::cout << add(1, 2, 3, 4, 5);
+}
