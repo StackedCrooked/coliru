fa1224a55c49bdf46b9b1da2e0a9d33b9fb3f3c0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-23 18:29:44 +0000

    Updating archive.

---
+++ trunk/Archive2/28/97f6b2c0479050/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <memory>
+using namespace std;
+
+
+
+int main()
+{
+  const std::shared_ptr<std::string> first(new std::string("first"));  
+  cout << "first ref count: " << first.use_count();
+  std::shared_ptr<std::string> second(first);
+  cout << "\n\nfirst ref count: " << first.use_count();
+  cout << "\nsecond ref count: " << second.use_count();
+}
