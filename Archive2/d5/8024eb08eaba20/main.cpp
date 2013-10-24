9389abb7ead2f0a96c5619d84c562910c1e5a234
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-24 06:33:57 +0000

    Updating archive.

---
+++ Archive2/d5/8024eb08eaba20/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <array>
+#include <algorithm>
+
+template< char... Cs >
+bool char_match( char c )
+{
+    constexpr const std::array< char, sizeof...( Cs ) > a {{ Cs... }};
+    return std::find_if(std::begin(a), std::end(a),[c](char x){return x==c;}) != std::end(a);
+}
+
+int main()
+{
+    std::cout << char_match< 'a', 'b', 'c' >( 'a' ) << std::endl;
+    std::cout << char_match< 'a', 'b', 'c' >( 'b' ) << std::endl;
+    std::cout << char_match< 'a', 'b', 'c' >( 'c' ) << std::endl;
+    std::cout << char_match< 'a', 'b', 'c' >( 'd' ) << std::endl;
+}
