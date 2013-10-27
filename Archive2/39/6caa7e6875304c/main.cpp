d7341f53643beb67b8baae67f032197cecd03811
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-27 21:22:39 +0000

    Updating archive.

---
+++ Archive2/39/6caa7e6875304c/main.cpp
@@ -0,0 +1,36 @@
+#include <map>
+#include <set>
+#include <vector>
+#include <list>
+#include <unordered_map>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+using namespace std::placeholders;
+
+
+
+
+
+
+int main()
+{
+     
+    string arr[] = { "Jess", "Anne", "Sandra", "Hugh", "Nick" };
+    
+    set<string> s(arr, arr+5);
+
+    set<string> v;
+    
+    
+    
+    std::copy(s.begin(), s.end(), inserter(v, v.begin()) );  // prepend
+    
+
+    for(auto n : v )
+        cout << n << " ";
+    cout << endl;
+}
