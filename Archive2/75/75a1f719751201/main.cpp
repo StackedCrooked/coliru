be034c0c7cb028b7ae3ab536b2d97ff670eee1dd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-27 08:17:38 +0000

    Updating archive.

---
+++ Archive2/75/75a1f719751201/main.cpp
@@ -0,0 +1,17 @@
+#include <utility>
+#include <iostream>
+
+template<typename First, typename ... Rest>
+void Output(First&& first, Rest&&... rest) {
+    std::cout << std::forward<First>(first);
+    int sink[]{(std::cout<<" "<<std::forward<Rest>(rest),0)... };
+    (void)sink; // silence "unused variable" warning
+    std::cout << std::endl;
+}
+
+int main() {
+    Output("I","am","a","sentence");
+    Output("Let's","try",1,"or",2,"digits");
+
+    // Output();
+}
