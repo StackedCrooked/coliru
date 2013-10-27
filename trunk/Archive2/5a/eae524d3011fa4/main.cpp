a0047a55d8ae4f84adc72cf7d791a54da3aec3b5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-27 18:19:17 +0000

    Updating archive.

---
+++ trunk/Archive2/5a/eae524d3011fa4/main.cpp
@@ -0,0 +1,16 @@
+// Eric Dunaway   9/29/2013 
+// Draw  out C++
+
+#include <iostream> 
+using namespace std;
+int main( )
+{
+     char pause;
+	cout << "CCCCCCCC           ++                 ++      " << endl;
+	cout << "CC                 ++                 ++      " << endl;
+	cout << "CC           ++++++++++++++    +++++++++++++++" << endl;
+	cout << "CC           ++++++++++++++    +++++++++++++++" << endl;
+	cout << "CC                 ++                 ++      " << endl;
+	cout << "CCCCCCCC           ++                 ++      " << endl;
+	cin >> pause
+}
