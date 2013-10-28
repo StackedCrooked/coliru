1b90e6170d5de80c2a8e6c929b681343b2721265
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-28 11:28:30 +0000

    Updating archive.

---
+++ trunk/Archive2/c3/4f8a9a8793c7ac/main.cpp
@@ -0,0 +1,46 @@
+#define BOOST_SPIRIT_USE_PHOENIX_V3
+#define BOOST_RESULT_OF_USE_DECLTYPE
+#include <boost/phoenix/phoenix.hpp>
+#include <boost/phoenix/statement/try_catch.hpp>
+#include <boost/spirit/include/phoenix.hpp>
+#include <boost/spirit/include/qi.hpp>
+
+namespace qi = boost::spirit::qi;
+namespace phx = boost::phoenix;
+
+class test
+{
+    public:
+        static test from_string(const std::string &str) { throw "hula"; }
+};
+
+template <typename Iterator = std::string::const_iterator>
+struct test_parser : qi::grammar<Iterator, test(), qi::blank_type>
+{
+    test_parser() : test_parser::base_type(query)
+    {
+        query = id[
+                phx::try_ [
+                    qi::_val = phx::bind(&test::from_string, qi::_1)
+                ].catch_all [ 
+                    qi::_pass = false 
+                ],
+                qi::_pass = qi::_pass // to appease the spirit expression compilation gods
+            ];
+        id = qi::lexeme[*qi::char_("a-zA-Z_0-9")];
+    }
+    qi::rule<Iterator, test(), qi::blank_type> query;
+    qi::rule<Iterator, std::string(), qi::blank_type> id;
+};
+
+int main()
+{
+    std::string const input = "";
+    auto f(begin(input)), l(end(input));
+
+    test_parser<> p;
+    test data;
+    bool ok = phrase_parse(f,l,p,qi::blank,data);
+
+    return ok? 0 : 255;
+}
