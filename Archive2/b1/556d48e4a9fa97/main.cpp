7297eaabb0c1822d3a5105a56d9430e8a7936677
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-30 12:55:19 +0000

    Updating archive.

---
+++ Archive2/b1/556d48e4a9fa97/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+template<class T> void bar(T t) { foo(t); }
+
+struct F {};
+
+int main() {
+    F f;
+    bar(f);
+    return 0;
+}
+
+void foo(F) { cout << "foo(F)"; }
