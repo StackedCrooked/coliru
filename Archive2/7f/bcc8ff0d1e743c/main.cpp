27b2c60eca3dc08f783aa25761488f9020509099
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-31 19:04:44 +0000

    Updating archive.

---
+++ Archive2/7f/bcc8ff0d1e743c/main.cpp
@@ -0,0 +1,7 @@
+template <typename Pred>
+inline std::unique_lock<std::mutex> lock_if(std::mutex& mutex, Pred&& predicate)
+{
+    std::unique_lock<std::mutex> lock(mutex, std::defer_lock);
+	if (predicate()) lock.lock();
+	return lock;
+}
