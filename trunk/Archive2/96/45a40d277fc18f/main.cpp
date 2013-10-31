ebe54e465394970c9e61738ca6b453153101c12a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-31 13:03:29 +0000

    Updating archive.

---
+++ trunk/Archive2/96/45a40d277fc18f/main.cpp
@@ -0,0 +1,14 @@
+// Using the STL generic reverse algorithm with a string and an array
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+  cout << "tada";
+
+  
+  return 0;
+}
