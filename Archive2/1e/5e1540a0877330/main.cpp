989d910844a67b079ea647a398acb346cd0a29fe
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-10-31 13:03:19 +0000

    Updating archive.

---
+++ Archive2/1e/5e1540a0877330/main.cpp
@@ -0,0 +1,19 @@
+// Using the STL generic reverse algorithm with a string and an array
+#include <iostream>
+#include <string>
+#include <cassert> 
+#include <algorithm> // For reverse algorithm
+using namespace std;
+
+#include <thread>
+#include <mutex>
+
+std::once_flag of;
+
+int main()
+{
+  
+
+  
+  return 0;
+}
