a03964e931bc679d8d55b91dd0dbfe473ebda4cf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-09 13:27:27 +0000

    Updating archive.

---
+++ trunk/Archive2/7a/581e2fb6542c93/main.cpp
@@ -0,0 +1,32 @@
+#include <initializer_list>
+#include <iostream>
+
+struct string
+{
+    string() = default;
+    string& operator=(string const&)
+    {
+        std::cout << "copy-assignment\n";
+        return *this;
+    }
+    string& operator=(char)
+    {
+        std::cout << "assignment from a char\n";
+        return *this;
+    }
+    string& operator=(char const*)
+    {
+        std::cout << "assignment from a char const*\n";
+        return *this;
+    }
+    string& operator=(std::initializer_list<char>)
+    {
+        std::cout << "assignment from an initializer_list<char>\n";
+    }
+};
+
+int main()
+{
+    string str;
+    str = {};
+}
