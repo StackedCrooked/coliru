4acf4b66d2969d6a9730a69aecba881698e45738
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-09 14:27:51 +0000

    Updating archive.

---
+++ Archive2/85/31d8b8a1b8bda5/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class Thing
+{
+    
+};
+
+int Thing()
+{
+    return 42;   
+}
+
+void function(class Thing thing)
+{
+    std::cout << "Class";
+}
+
+void function(int number)
+{
+    std::cout << "Number";   
+}
+
+
+int main()
+{
+    function(Thing());
+}
