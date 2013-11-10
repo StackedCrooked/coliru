664139c53e0c7209ef13f3f0f2191f827827a9d7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-10 12:35:10 +0000

    Updating archive.

---
+++ Archive2/b2/3b032e50096f64/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::ostream & operator<<(std::ostream & out, std::vector<std::string> const & items)
+{
+    for(auto const & item : items ) 
+        out << item << " ";
+    return out;
+}
+
+int main() 
+{
+    std::vector<std::string> items {"default"};
+    
+    auto add = [=](std::string item) mutable { items.push_back(item); return items; } ;
+    
+    std::cout << add("one") << std::endl;
+    std::cout << add("two") << std::endl;
+    std::cout << add("three") << std::endl;
+}
