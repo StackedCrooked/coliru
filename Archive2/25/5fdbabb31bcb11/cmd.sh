0a8c26b7e7bae182408e67d063618a713f156603
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-11 05:40:19 +0000

    Updating archive.

---
+++ Archive2/25/5fdbabb31bcb11/cmd.sh
@@ -0,0 +1,3 @@
+set -x ; g++  -std=c++11 -O0  -pedantic-errors -Wall -pthread main.cpp && ./a.out
+##set -x ; gcc -x c  -O0  -pedantic main.cpp && ./a.out
+##set -x ; clang -x c  -O0  -pedantic main.cpp && ./a.out
