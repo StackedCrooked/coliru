85624352f02f960fa6a3f6aef28593d1dec56e84
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-12 04:55:03 +0000

    Updating archive.

---
+++ Archive2/51/f2dde12995c7cb/main.cpp
@@ -0,0 +1,6 @@
+int main() {
+    static const char* my_regex = 
+        "a regex"               // This parses that
+        "another regex"         // Another one
+        "yet another regex"     // That
+}
