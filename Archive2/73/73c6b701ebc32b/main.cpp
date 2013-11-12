f913c38062f38ec7752a5d5770c944362f4bf39f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-12 04:55:07 +0000

    Updating archive.

---
+++ Archive2/73/73c6b701ebc32b/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+class CGuessNumber
+{
+public:
+    
+    int GenerateNumber()
+    {
+        return rand() % 100 + 1;
+    }
+
+void checkNumber(int guess, int answer, int &attempts)
+{
+
+    if (guess < answer)
+    {
+        std::cout << "TOO LOW, TRY AGAIN"  << "\n" << "TRYS LEFT: " << attempts  << "\n";
+        attempts--;
+    }else if(guess > answer)
+    {
+        std::cout << "TOO HIGH, TRY AGAIN"  << "\n" << "TRYS LEFT: " << attempts  << "\n";
+        attempts--;
+    }else if(guess ==  answer)
+    {
+        std::cout << "YOU WON!"  << "\n" << "TRYS LEFT: " << attempts  << "\n";
+    }
+
+    if (attempts <= 0)
+    {
+        std::cout << "YOU LOST!"  << "\n" << "TRYS LEFT: " << attempts  << "\n";
+    }
+}
+};
+
+class CGAME
+{
+public:
+
+    CGAME(CGuessNumber* pNumber)
+    {
+        m_number = pNumber;
+    }
+    void init(int &answer, int &attempts)
+    {
+        answer = m_number->GenerateNumber();
+        attempts = 5;
+    };
+
+    void newGame()
+    {
+        srand (time(NULL));
+        int intAnswer, playerGuess, trys;
+
+        init(intAnswer, trys);
+
+        while(intAnswer != playerGuess and trys > 0)
+        {
+            std::cin >> playerGuess;
+
+            m_number->checkNumber(playerGuess, intAnswer, trys);
+        }
+    };
+    
+    private:
+        CGuessNumber* m_number;
+};
+
+int main()
+{
+    CGuessNumber* pGnum = new CGuessNumber();
+    CGAME* ONewGame = new CGAME(pGnum);
+    ONewGame->newGame();
+    return 0;
+}
