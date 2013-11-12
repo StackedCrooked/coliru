0a05342f65a6d8e3c89eae471cb163a868ca4b82
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-12 21:42:15 +0000

    Cache is max 5000 entries.

--- trunk/Web/cleanup-cache.sh
+++ trunk/Web/cleanup-cache.sh
@@ -16,10 +16,10 @@ max=5000
 
 while true ; do
     [ "$(($(cache_size) > $max))" == "1" ] && {
-        echo "$(cache_size) > $max => delete $(random_cache_entry)"
+        echo "$0: $(cache_size) > $max => delete $(random_cache_entry)"
         rm -rf ../CompileArchive/$(random_cache_entry)
     } || {
-        echo "$(cache_size) <= $max. Ok allow cache to grow."
+        echo "$0: $(cache_size) <= $max. Ok allow cache to grow."
     }
     sleep 1
 done
