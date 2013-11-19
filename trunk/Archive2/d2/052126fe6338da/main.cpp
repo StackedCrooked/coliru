b9e423cc15ae64fa425a013bcd56d48ea9bda59c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-19 21:31:59 +0000

    Updating archive.

---
+++ trunk/Archive2/d2/052126fe6338da/main.cpp
@@ -0,0 +1,28 @@
+#define BOOST_SPIRIT_USE_PHOENIX_V3
+#include <boost/fusion/adapted.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/karma.hpp>
+// #include <boost/spirit/include/phoenix.hpp>
+
+namespace qi    = boost::spirit::qi;
+namespace karma = boost::spirit::karma;
+typedef std::string::const_iterator It;
+
+int main()
+{
+    std::string const input ("1 2 3 4");
+    It f(begin(input)), l(end(input));
+
+    std::string data;
+
+    bool ok = qi::phrase_parse(f, l, qi::lexeme [+qi::char_], qi::space, data);
+    if (ok)   
+    {
+        std::cout << "parse success\n";
+        std::cout << "data: " << karma::format_delimited(karma::auto_, ' ', data) << "\n";
+    }
+    else std::cerr << "parse failed: '" << std::string(f,l) << "'\n";
+
+    if (f!=l) std::cerr << "trailing unparsed: '" << std::string(f,l) << "'\n";
+    return ok? 0 : 255;
+}
