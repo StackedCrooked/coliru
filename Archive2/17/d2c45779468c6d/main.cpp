fe1a787b7fc6f7c72d09bed8651b5be976ddd8bb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-20 10:41:47 +0000

    Updating archive.

---
+++ Archive2/17/d2c45779468c6d/main.cpp
@@ -0,0 +1,30 @@
+#include <cassert>
+#include <string>
+#include <vector>
+
+struct SkeletalAnimationChannel {
+    
+    std::string name;
+    double duration;
+    double currenttime, lasttime;
+	
+    SkeletalAnimationChannel() {}
+    SkeletalAnimationChannel ( SkeletalAnimationChannel&& ) = default;
+    SkeletalAnimationChannel& operator=(SkeletalAnimationChannel&&) noexcept = default;
+
+};
+
+struct Container {
+    
+    std::vector<SkeletalAnimationChannel> arf;
+    
+    Container() {}
+    Container( Container&& ) = default;
+    Container& operator=( Container&& ) noexcept = default;
+    
+};
+
+int main () {
+    Container no_copy_move_elision{};
+    Container c = std::move( no_copy_move_elision );
+}
