e9e5cfbe47c8d1cb77fe08933dc1089674be75c9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-22 18:59:53 +0000

    Improve the pgid killer.

--- trunk/Web/pgid_killer.sh
+++ trunk/Web/pgid_killer.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -x
+source logger.source
+
 rm -f .pgid_killer
 mkfifo .pgid_killer
 chmod a+w .pgid_killer
-while true ; do read line <.pgid_killer ; kill -9 "$line"  ; done 
+while true ; do
+    read line <.pgid_killer
+    [ "$line" != "" ] && {
+        echo "Killing sandbox process: $line"
+        kill -9 "$line" 
+    } || {
+        echo "Nothing to kill."
+    }
+done 
