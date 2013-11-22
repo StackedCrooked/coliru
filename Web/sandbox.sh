4fdfb35ebeb26c220fcee541bc06db8b4358db47
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-22 18:59:53 +0000

    Improve the pgid killer.

--- Web/sandbox.sh
+++ Web/sandbox.sh
@@ -32,7 +32,7 @@ pathifiedId="$(./pathify-id.sh ${id})"
 mkdir -p ${COLIRU_COMPILE_ARCHIVE}/${id}
 
 # Kill any remaining sandbox processes
-ps -eopgid,uid | grep 2002 | awk '{print $1}' >.pgid_killer
+ps -eouid,pgid | sed -E 's,^[[:space:]]+,,' | grep ^2002 | awk '{print $2}' >.pgid_killer
 
 bash -c "exec > >(tee ${COLIRU_COMPILE_ARCHIVE}/${id}/output)
 exec 2>&1
