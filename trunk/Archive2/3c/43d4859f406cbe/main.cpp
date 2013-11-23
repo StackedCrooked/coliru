48470e2e265fcd88aeaf0234d8734271f0cf93bd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-23 19:58:58 +0000

    Updating archive.

---
+++ trunk/Archive2/3c/43d4859f406cbe/main.cpp
@@ -0,0 +1,16 @@
+struct example
+{
+    int a;
+    long b;
+    int c;
+};
+
+#include <iostream>
+#include <cstddef>
+
+int main()
+{
+    std::cout << offsetof(example, a) << '\n';
+    std::cout << offsetof(example, b) << '\n';
+    std::cout << offsetof(example, c) << '\n';
+}
