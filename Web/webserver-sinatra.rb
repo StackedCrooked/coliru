c8158f556bc559b4998d0887cc06c74914f15c7a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-23 16:42:05 +0000

    Improve the log function.

--- Web/webserver-sinatra.rb
+++ Web/webserver-sinatra.rb
@@ -228,18 +228,13 @@ end
 
 
 get '/log' do
-    count = params[:count] || 200
+    count = params[:count] || 1000
     grep = params[:grep] || nil
-    ungrep = params[:ungrep] || nil
     content_type :txt
-    p = IO.popen("tail -n#{count} /var/log/syslog 2>&1")
+    cmd = grep ? "tail -n#{count} /var/log/syslog 2>&1 | grep -i '#{grep}'" : "tail -n#{count} /var/log/syslog 2>&1"
     stream do |out|
-        until p.eof
-            line = p.readline
-            if (!grep || line =~ /#{grep}/) && (!ungrep || line !~ /#{ungrep}/)
-                out << line
-            end
-        end
+        out << "Log command: " << cmd << "\n"
+        out << IO.popen(cmd).read
     end
 end
 
