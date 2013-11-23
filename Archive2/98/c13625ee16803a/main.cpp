d33289bd82b5c7f54d4ee7bcfe4cf354b58845e1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-23 22:59:31 +0000

    Updating archive.

---
+++ Archive2/98/c13625ee16803a/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <assert.h>
+#include <stdint.h>
+
+
+template<typename T>
+class Atomic
+{
+public:
+    Atomic(T t = T()) : t(t)
+    {
+        assert(__atomic_is_lock_free(sizeof(T), nullptr));
+    }
+    
+    // disable copy construction and copy-assigment
+    Atomic(const Atomic&) = delete;
+    Atomic& operator=(const Atomic&) = delete;
+    
+    // get the current value
+    T load() const
+    {
+        return __atomic_load_n(&t, __ATOMIC_ACQUIRE);
+    }   
+    
+    // set a new value
+    void store(T new_value)
+    {
+        __atomic_store_n(&t, new_value, __ATOMIC_RELEASE);
+    }
+    
+    void operator=(T new_value)
+    {
+        store(new_value);
+    } 
+    
+    operator T() const
+    {
+        return load();
+    }
+    
+    // pre-increment
+    T operator++()
+    {
+        return __atomic_add_fetch(&t, 1, __ATOMIC_ACQ_REL);
+    }
+    
+    // post-increment
+    T operator++(int)
+    {
+        return __atomic_fetch_add(&t, 1, __ATOMIC_ACQ_REL);
+    }
+    
+private:
+    mutable T t;
+};
+
+
+int main()
+{
+    Atomic<int> n;
+    std::cout << "n = " << n << std::endl;
+    std::cout << "++n = " << ++n << std::endl;
+    std::cout << "n++ = " << n++ << std::endl;
+}
