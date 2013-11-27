8a25c76085fb2ca04e575859dbc0bde02cd9ab88
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-27 19:59:01 +0000

    Updating archive.

---
+++ Archive2/89/060d65fb12d99a/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Card
+{
+public:
+    //Card();
+    Card() {   
+    }
+private:
+    int rank;
+    int suit;
+};
+
+/* 
+Card::Card() {
+    
+}
+*/
+
+int main() {
+    Card card1;
+    return 0;
+}
