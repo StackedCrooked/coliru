a8538d48daf1a397434bfad8c55e0cedefce2a1f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-27 01:00:15 +0000

    Updating archive.

---
+++ Archive2/fa/a8be0dd31bb016/cmd.sh
@@ -0,0 +1,1 @@
+gcc-4.8 -std=c++1y -O2 -Wall -pedantic main.cpp && ./a.out
---
+++ Archive2/fa/a8be0dd31bb016/main.cpp
@@ -0,0 +1,48 @@
+#include <cstdlib>
+
+template<class ValueType>
+class stAlignedContainer
+{
+public:
+    stAlignedContainer()
+		: m_data(0)
+		, m_dataSize(0)
+	{ };
+
+	stAlignedContainer(const void* data, size_t dataSize)
+	{
+		m_data = static_cast<const unsigned char *>(data);	
+		m_dataSize = dataSize;
+	};
+
+	inline const ValueType& operator[] (unsigned short n) const
+	{
+		return ((ValueType*)m_data)[n];
+	};
+
+protected:
+	const unsigned char * m_data;
+	size_t m_dataSize;
+};
+//-----------------------------------------------------------------------------
+template< class ValueType >
+class stUnalignedContainer : public stAlignedContainer<ValueType>
+{
+public:
+    typedef stAlignedContainer<ValueType> parent_type;
+
+	stUnalignedContainer()
+	{ };
+
+	stUnalignedContainer(const void* data, size_t dataSize) : parent_type(data, dataSize)
+	{ };
+
+	inline const ValueType& operator[] (unsigned short n) const
+	{
+		return *((const ValueType*) &m_data[ n * parent_type::m_dataSize ]);
+	};
+};
+
+int main () {
+    
+}
