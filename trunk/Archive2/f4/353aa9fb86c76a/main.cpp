0dc8a9b50efcb77e076a5839c1b8b5cb8065d659
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-27 04:19:22 +0000

    Updating archive.

---
+++ trunk/Archive2/f4/353aa9fb86c76a/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+class x {
+public:
+    x() {}
+    x(const x&) { std::cout << "Copy ctor\n"; }
+    x(x&&) { std::cout << "Move\n"; }
+};
+
+int main() {
+    x base;
+    std::vector<x> vec;
+    vec.emplace_back(base);
+    std::cout << '\n';
+    vec.emplace_back(std::move(base));
+}
