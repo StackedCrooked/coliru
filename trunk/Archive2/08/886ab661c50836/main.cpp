8a5c5d70710b957d1c40240544c2bf91c6f3d96c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-27 00:59:52 +0000

    Updating archive.

---
+++ trunk/Archive2/08/886ab661c50836/main.cpp
@@ -0,0 +1,42 @@
+#include <cstdlib>
+
+template<class ValueType>
+class stAlignedContainer
+{
+public:
+    stAlignedContainer()
+		: m_data(0)
+		, m_dataSize(0)
+	{ };
+
+	stAlignedContainer(const void* data, size_t dataSize)
+	{
+		m_data = static_cast<const unsigned char *>(data);	
+		m_dataSize = dataSize;
+	};
+
+	inline const ValueType& operator[] (unsigned short n) const
+	{
+		return ((ValueType*)m_data)[n];
+	};
+
+protected:
+	const unsigned char * m_data;
+	size_t m_dataSize;
+};
+//-----------------------------------------------------------------------------
+template< class ValueType >
+class stUnalignedContainer : public stAlignedContainer<ValueType>
+{
+public:
+	stUnalignedContainer()
+	{ };
+
+	stUnalignedContainer(const void* data, size_t dataSize) : stAlignedContainer(data, dataSize)
+	{ };
+
+	inline const ValueType& operator[] (unsigned short n) const
+	{
+		return *((const ValueType*) &m_data[ n * m_dataSize ]);
+	};
+};
