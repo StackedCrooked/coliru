9ccb9fe6ca6ef48379450922869adb25770dc110
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-29 04:58:33 +0000

    Updating archive.

---
+++ Archive2/a9/a98de5f17d618c/cmd.sh
@@ -0,0 +1,1 @@
+clang++ -x c -std=c99 -O3 -Wall -Wextra -pedantic-errors -pthread main.cpp && ./a.out
---
+++ Archive2/a9/a98de5f17d618c/main.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct tod {
+    int h;
+    int m;
+};
+
+struct event {
+    struct tod s;
+    struct tod e;
+};
+
+int eventCmp(const void *a, const void *b) {
+    const struct event *e1 = a;
+    const struct event *e2 = b;
+    
+    if (e1->s.h != e2->s.h) return e1->s.h - e2->s.h;
+    return e1->s.m - e2->s.m;
+}
+
+void eventPrint(struct event e) {
+    printf("%02d:%02d - %02d:%02d\n", e.s.h, e.s.m, e.e.h, e.e.m);   
+}
+
+int main(void) {
+    struct event events[] = {
+        {{15,  0}, {16, 30}},
+        {{ 9,  0}, { 9, 15}},
+        {{13,  0}, {14, 20}},
+        {{17, 15}, {18,  0}},
+    };
+    
+    qsort(events, 4, sizeof *events, eventCmp);
+    
+    for (int i = 0; i < 4; ++i) {
+        eventPrint(events[i]);
+    }
+}
