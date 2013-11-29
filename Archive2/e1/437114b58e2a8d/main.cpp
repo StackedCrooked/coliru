e371490905894d3ce6a261a15f73cd67e061940c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-29 06:22:21 +0000

    Updating archive.

---
+++ Archive2/e1/437114b58e2a8d/main.cpp
@@ -0,0 +1,35 @@
+struct Coord3{
+    float x, y, z;
+
+    Coord3() {x=0;y=0;z=0;};///Is this Correct?
+};
+
+struct Stat{
+    int Str,Dex,Int;
+
+    Stat(){Str=0;Dex=0;Int=0;};
+};
+
+struct Item{
+    Stat myStats;
+
+    Item(){...};
+}
+
+class SimpleChar{
+    Coord3 pos;
+    Stat myStats;
+    int level;
+    float health;
+    Item inventory[20];
+
+    SimpleChar(){
+    level=0;
+    health=100;
+    };///What happens with 'pos', 'inventory' and 'myStats' in this class's constructor?
+};
+
+int main(){
+    SimpleChar myChar;
+    if(myChar.inventory[0].get()!=NULL){...}///this caused the most recent SEGFAULT as mentioned below. Why wouldn't this work?
+}
