0de35a8042fc0224cc6d7a6a7de3273a405dfbdf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-11-29 08:09:25 +0000

    Updating archive.

---
+++ trunk/Archive2/5d/0ad7b77db02034/main.cpp
@@ -0,0 +1,18 @@
+#include <type_traits>
+
+using IntType = unsigned long long;
+
+template <IntType N> struct Int {};
+
+template <class T>
+struct is_int : std::false_type {};
+
+template <IntType N>
+//        ^^^^^^^^         
+struct is_int<Int<N>> : std::true_type {};
+
+int main()
+{
+    static_assert (is_int<Int<0>>::value, "");
+    return 0;
+}
