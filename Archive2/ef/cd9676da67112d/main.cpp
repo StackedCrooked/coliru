dd840919173569c61004543744539d7c443c7f5f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-01 17:00:52 +0000

    Updating archive.

---
+++ Archive2/ef/cd9676da67112d/main.cpp
@@ -0,0 +1,38 @@
+#include <cstddef>
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+namespace experimental
+{
+  // C++14's std::make_unique as per N3656 / N3797
+  using namespace std;
+
+  template< typename T, typename... Args >
+  typename enable_if< !is_array< T >::value, unique_ptr< T > >::type
+  make_unique( Args&&... args )
+  {
+    return unique_ptr< T >( new T( forward< Args >( args )... ) );
+  }
+
+  template< typename T >
+  typename enable_if< is_array< T >::value && extent< T >::value == 0, unique_ptr< T > >::type
+  make_unique( const size_t n )
+  {
+    using U = typename remove_extent< T >::type;
+    return unique_ptr< T >( new U[ n ]() );
+  }
+
+  // removing this makes to real difference, in fact the error messages becomes shorter!
+  template< typename T, typename... Args >
+  typename enable_if< extent< T >::value != 0 >::type
+  make_unique( Args&&... args )
+  {
+      static_assert( !( extent< T >::value != 0 ),"array with known bounds not allowed for std::make_shared");
+  }
+}
+
+int main()
+{
+    experimental::make_unique<int[1]>(42);
+}
