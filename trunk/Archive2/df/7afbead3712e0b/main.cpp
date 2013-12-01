22084724a5e31dc4146c1302d8ae64e4ab357959
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-01 02:03:05 +0000

    Updating archive.

---
+++ trunk/Archive2/df/7afbead3712e0b/main.cpp
@@ -0,0 +1,68 @@
+#include <functional>
+#include <iostream>
+#include <boost/container/stable_vector.hpp>
+
+template <typename... Params>
+class Delegate {
+private:
+    typedef std::function<void(Params...)> Handler;
+    typedef boost::container::stable_vector<Handler> Vector;
+    Vector m_functions;
+public:
+    typedef typename Vector::const_iterator cookie;
+
+    cookie connect(Handler&& func) {
+        m_functions.push_back(std::move(func));
+        return m_functions.begin() + m_functions.size() - 1;
+    }
+
+    template <typename... BindArgs, typename Sfinae = typename std::enable_if<(sizeof...(BindArgs)>1), void>::type>
+    cookie connect(BindArgs&&... args) {
+        return connect(Handler(std::bind(std::forward<BindArgs>(args)...)));
+    }
+
+    void disconnect(cookie which) {
+        m_functions.erase(which);
+    }
+
+    template <typename ... Args> void operator()(Args...args) {
+        for(auto const& handler : m_functions) 
+            handler(args...);
+    }
+};
+
+//////////////////////////////////
+// demonstration
+#define CV volatile const
+struct BaseClass {
+    virtual void print(const std::string& str) CV = 0;
+};
+
+struct A : BaseClass {
+    A() {}
+    void print(const std::string& str) CV { std::cout << " Class A : Print [" << str << "]\n"; }
+};
+
+struct B : BaseClass {
+    B() {}
+    void print(const std::string& str) CV { std::cout << " Class B : Print [" << str << "]\n"; }
+};
+
+using namespace std::placeholders;
+
+int main() {
+    Delegate <const std::string&> delegate;
+    A CV a;
+    B CV b;
+    auto acookie = delegate.connect(&A::print, std::ref(a), _1);
+    auto bcookie = delegate.connect(&B::print, std::ref(b), _1);
+
+    delegate("hello");
+    delegate("world");
+
+    delegate.disconnect(acookie);
+    delegate("bye world");
+
+    delegate.disconnect(bcookie);
+    delegate("nobody there.");  // should not print anything
+}
