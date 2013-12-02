0a5aedfbd5475be92f1a9d63003bdec78d651ad6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-02 21:25:39 +0000

    Updating archive.

---
+++ trunk/Archive2/a4/04512462fba616/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+struct Test {
+    static constexpr double FLUX_CAPACITOR_OUTPUT() { return 1.21; }
+};
+
+template<typename T> void nike(const T &val) {
+	cout << val << endl;
+}
+
+int main() {
+	nike(Test::FLUX_CAPACITOR_OUTPUT());
+}
