76a455596af321bde0ebc5d8bf825daeb6b233ae
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-05 18:58:22 +0000

    Updating archive.

---
+++ trunk/Archive2/fc/6076672c38be76/main.cpp
@@ -0,0 +1,13 @@
+template <typename TAs, typename T, ulword n>
+inlinealways typename std::conditional<std::is_const<T>::value, const TAs, TAs>::type ( &array_cast( T( &array )[ n ] ) )[ ( n * sizeof( T ) ) / sizeof( TAs ) ] {
+    typedef typename std::conditional<std::is_const<T>::value, const TAs, TAs>::type cast[ ( n * sizeof( T ) ) / sizeof( TAs ) ];
+	typedef typename std::conditional<std::is_const<T>::value, const void, void>::type pointer;
+	return *static_cast<cast*>( static_cast<pointer*>( &array ) );
+}
+        
+int main( ) {
+ 
+    
+
+}
+
