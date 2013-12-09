09ffa57480449ecd76db3af944a0fa5ecb79c39d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-09 01:59:51 +0000

    Updating archive.

---
+++ trunk/Archive2/3c/b6fd7505bb958b/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <type_traits>
+
+template <typename TFx, TFx fx, typename T, typename... Args>
+auto call_member_fn ( T& object, Args&&... args )
+-> decltype( (object.*fx)( std::forward<Args>( args )... ) ){
+     return (object.*fx)( std::forward<Args>( args )... );
+}
+ 
+struct A {
+    int arf () {
+        return 0;
+    }
+};
+ 
+int main() {
+    A a;
+   std::cout << call_member_fn<decltype( &A::arf ), &A::arf, A>( a ) << std::endl;
+    
+}
