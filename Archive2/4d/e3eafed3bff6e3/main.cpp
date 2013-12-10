506130aa926f403ae078ac30fc4ac43cd32f68b0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-10 12:05:46 +0000

    Updating archive.

---
+++ Archive2/4d/e3eafed3bff6e3/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cassert>
+using namespace std;
+bool ends_with(const string& s, const string& ending)
+{
+ return (s.size() >= ending.size()) && std::equal(ending.rbegin(), ending.rend(), s.rbegin());
+
+}
+
+int main()
+{
+    string one  = "ima.txtfile.txt";
+    string two  = "imnota.txt.file";
+    assert( ends_with(one,".txt"));
+    assert(!ends_with(two,".txt"));
+    assert(!ends_with("",".txt"));
+    cout << "bye" ;
+
+}
