55350637e040551a1223b2c2d4eafcf419c5f64d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-10 11:45:21 +0000

    Updating archive.

---
+++ Archive2/ac/8b186bb449b6e5/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#define GETTIN_STARTED(val) *val
+#define GOING_DEEP(val) **val
+#define SUB_ZERO(val) ***val
+#define MIGHTY_WARRIOR(val) ****val
+#define LUDACRIS(val) *****val
+#define GODLIKE(val) ******val
+#define DEEPER(val) &val
+#define FOR(val) for(int i = 0; i < val; i++)
+
+#define GET_ME_ONE_OF_THOSE(val) new int(val)
+
+#define PRINT(val) std::cout << val << std::endl;
+
+#define LVL_1 *
+#define LVL_2 LVL_1 LVL_1
+//#define LVL_4 LVL_2 LVL_2
+//#define LVL_8 LVL_4 LVL_4
+//#define LVL_16 LVL_8 LVL_8
+//#define LVL_32 LVL_16 LVL_16
+//#define LVL_64 LVL_32 LVL_32
+//#define LVL_128 LVL_64 LVL_64
+
+
+auto main() -> int {
+    auto increment = [&](int GODLIKE(j)) { (GODLIKE(j))++; };
+    auto large_increment = [&](int LUDACRIS(j)) { FOR(5) increment(DEEPER(j)); };
+    auto very_large_increment = [&](int MIGHTY_WARRIOR(j)) { FOR(5) large_increment(DEEPER(j)); };
+    auto very_very_large_increment = [&](int SUB_ZERO(j)) { FOR(5) very_large_increment(DEEPER(j)); };
+    auto very_very_very_large_increment = [&](int GOING_DEEP(j)) { FOR(5) very_very_large_increment(DEEPER(j)); };
+    
+    auto GETTIN_STARTED(i) = GET_ME_ONE_OF_THOSE(5);
+    very_very_very_large_increment(DEEPER(i));
+    
+    PRINT(LVL_1(i))
+    PRINT(LVL_2(DEEPER(i)))
+}
