43667f6e9d49f77ee3a9939f322d0c3b67120245
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-11 00:15:08 +0000

    Updating archive.

---
+++ Archive2/79/8a3199a6b44cd2/main.cpp
@@ -0,0 +1,24 @@
+//
+// QUIZ: Can you predict which samples will be accepted
+//        and which will be rejected by the compiler?
+
+
+template<typename T>
+void test(T&) { }
+
+
+int main()
+{   
+    // 1
+    int a = 68;
+    test(a); 
+    
+    
+    // 2
+    const int b = 200;
+    test(b); 
+
+
+    // 3
+    test(42); 
+}
