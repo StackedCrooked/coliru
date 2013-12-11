9d616bac0c83c80bc44243c58e1431d8597ccf09
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-11 20:24:04 +0000

    Updating archive.

---
+++ Archive2/74/9c1b4b2cf8403b/main.cpp
@@ -0,0 +1,15 @@
+#include <utility>
+#include <stdio.h>
+
+template<typename T> void _() { puts(__PRETTY_FUNCTION__); }
+#define _(statement, x)  statement;  printf(#statement); printf("; // %s is ", #x); _<decltype(x)>();
+
+
+int main()
+{   
+    _( int a = 1         , a );
+    _( int& b = a        , b );
+    _( const int& c = 3  , c );
+    _( int&& d = 4       , d );
+    _( int e = 5         , std::move(e) );
+}
