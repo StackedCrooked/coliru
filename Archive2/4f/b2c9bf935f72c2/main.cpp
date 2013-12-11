f41c7aee493434a5b7bfb92020c235433878ec26
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-11 20:23:43 +0000

    Updating archive.

---
+++ Archive2/4f/b2c9bf935f72c2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+template<typename T>
+void print_type()
+{
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+}
+
+
+#define _(expr, x) expr ; std::cout << #expr << "; // " << #x << "  is "; print_type<decltype((x))>();
+
+
+int main()
+{   
+    _( int a = 1         , a );
+    _( int& b = a        , b );
+    _( const int& c = 3  , c );
+    _( int&& d = 4       , d );
+}
