adab706005289b41ebc1aa39aaf2561851f79829
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-16 18:38:59 +0000

    Updating archive.

---
+++ Archive2/d9/7064797d885977/main.cpp
@@ -0,0 +1,11 @@
+#include <cstring>
+#include <iostream>
+ 
+int main()
+{
+    int i(0);
+    std::cout << "Hallo Welt!" << std::endl;
+    std::cin >> i;
+    std::cout << "i * 2" << i * 2 << std::endl;
+    std::cout << 15;
+}
