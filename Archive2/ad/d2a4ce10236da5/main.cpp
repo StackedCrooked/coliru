0a26036a04c3c148f6c7f8d2f11baf1f0b3aff0b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-16 18:35:27 +0000

    Updating archive.

---
+++ Archive2/ad/d2a4ce10236da5/main.cpp
@@ -0,0 +1,47 @@
+//#include <iostream>
+#include <type_traits>
+#include <new>
+#include <utility>
+
+
+struct Functor // only supports signature `void()`
+{
+    template<typename F>
+    Functor(F&& f, typename std::enable_if<!std::is_same<typename std::remove_reference<typename std::remove_const<F>::type>::type,Functor>::value>::type* = 0)
+    {        
+        static_assert(sizeof(f) <= sizeof(buf), "");
+        static_assert(std::alignment_of<F>::value <= std::alignment_of<SmallBuffer>::value, "");
+        new (&buf) F(std::forward<F>(f));
+        fptr = &Functor::call<F>;
+    }
+    
+    void operator()()
+    {
+        fptr(&buf);        
+    }
+    
+private:
+    template<typename F>
+    static void call(void* f)
+    {
+        (*static_cast<F*>(f))();
+    }
+    
+    struct alignas(128) SmallBuffer
+    {
+        char data[128];
+    };
+    
+    SmallBuffer buf;    
+    typedef void(*FPTR)(void*);
+    FPTR fptr;
+};
+
+int main(int argc, char**)
+{
+    int a = argc;
+    Functor f([&]{ ++a; });
+    return a;
+    
+}
+
