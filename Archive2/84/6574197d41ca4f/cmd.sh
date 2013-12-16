f50514202fd3eb9828a792d22feb93a8c9ebf5a6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-16 18:32:23 +0000

    Updating archive.

---
+++ Archive2/84/6574197d41ca4f/cmd.sh
@@ -0,0 +1,1 @@
+set -x ; clang++ -std=c++11 -O3 -Wall -Wextra -pedantic -pthread main.cpp && ./a.out
---
+++ Archive2/84/df1208c3d97e9f/main.cpp
@@ -0,0 +1,22 @@
+#include <map>
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+std::map<std::string, std::map<std::string, int>> grandMap =
+    { { "1", { { "hello", 42 }, { "hi", 1337 } } },
+      { "2", { { "hello", 42 }, { "hello", 1729 } } }};
+          
+auto count(std::string const& term)
+{
+     return std::count_if(std::begin(grandMap), std::end(grandMap),
+        [&] (auto const& entry)
+    {
+        return (entry.second.find("hello") != std::end(entry.second));
+    });
+}
+
+int main()
+{
+    std::cout << count("hello");
+}
