2252c2219e8a3d2ae7d0ade8c225addea063e2d6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-16 18:23:22 +0000

    Updating archive.

---
+++ trunk/Archive2/0a/b48c7f883de997/main.cpp
@@ -0,0 +1,13 @@
+#include <boost/spirit/include/karma.hpp>
+#include <boost/container/vector.hpp>
+
+namespace karma = boost::spirit::karma;
+
+int main()
+{
+    boost::variant<boost::container::vector<int>, std::string> tmp = "test";
+    std::string use;
+    bool r = karma::generate(std::back_inserter(use), +karma::int_ | karma::string, tmp);
+
+    std::cout << std::boolalpha << r << "\t" << use << "\n";
+}
