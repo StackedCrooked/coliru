a5a9a7009a1e61082b521915475a5a022658c359
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-16 18:23:14 +0000

    Updating archive.

---
+++ trunk/Archive2/07/a9458bda5a3e3a/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+int main()
+{
+ cout<<"File\'s"<<"\n"<<"computer";
+    cout<<"a"<<"\n\v"<<"\v\t"<<"d";
+}
