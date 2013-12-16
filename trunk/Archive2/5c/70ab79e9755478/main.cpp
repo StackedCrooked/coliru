9d47814a94abee37501dc3797675e6c570d73a3e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-16 18:29:32 +0000

    Updating archive.

---
+++ trunk/Archive2/5c/70ab79e9755478/main.cpp
@@ -0,0 +1,8 @@
+#include <type_traits>
+struct wat {};
+int main() {
+    using x = int (wat::*)(int);
+    using y = x (int);
+    using is_it_int = std::result_of<y>::type;
+    is_it_int foo = 5;
+}
