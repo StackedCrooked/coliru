bfe48490b0edd9152743cfcdac3072b283287614
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-16 18:42:25 +0000

    Updating archive.

---
+++ Archive2/f9/4c856dd5f3800b/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <memory>
+#include <tuple>
+
+struct Token {
+    std::string lexeme;
+    Token(std::string lex) : lexeme{std::move(lex)} {}
+};
+
+struct Node {
+Node ( Token * token ){ }
+};
+
+int main() {
+    std::tuple<std::string,std::string,std::string> tuple{"foo","bar","baz"};
+    
+    // Code from the question:
+    std::string type,rel,pred;
+    std::tie( type, rel, pred ) = tuple;
+    auto supertype = std::make_shared<Node>( new Token( type ) ); // This
+    auto predicate = std::make_shared<Node>( new Token( pred ) ); // and this
+}
