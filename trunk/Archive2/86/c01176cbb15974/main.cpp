c7b207b8737124f15b248e3e1e7067d43335102e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-16 18:32:30 +0000

    Updating archive.

---
+++ trunk/Archive2/86/c01176cbb15974/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <cstdint>
+#include <sstream>
+#include <cstdio>
+
+int main()
+{
+    double d = 7.8 ;
+    double* pointer = &d ;
+
+    // convert the address to a number and store the number
+    std::uintptr_t value = std::uintptr_t(pointer) ;
+
+    // convert the address to a readable sequence of characters and store it
+    std::ostringstream stm ;
+    stm << pointer ;
+    std::string pointer_str = stm.str() ;
+
+    char pointer_cstr[128] ;
+    std::sprintf( pointer_cstr, "%p", (void*)pointer ) ;
+
+    std::cout << pointer << ' ' << value << ' '
+              << pointer_str << ' ' << pointer_cstr << '\n' ;
+}
