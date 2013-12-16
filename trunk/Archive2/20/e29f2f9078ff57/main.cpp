114d73d69f71d41c58060c41fb77d954e12b0f8d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-16 18:25:01 +0000

    Updating archive.

---
+++ trunk/Archive2/20/e29f2f9078ff57/main.cpp
@@ -0,0 +1,39 @@
+#include <omp.h>
+#include <stdio.h>
+//#include <stdlib.h>
+
+#define RNG_MOD 741025
+
+int rng_int(int state) {
+    return (state = (1366 * state + 150889) % RNG_MOD);
+}
+
+double rng_doub(double range, int state) {
+    return ((double)state) / (double) ((RNG_MOD - 1)/range);
+}
+
+int main() {
+    int i, numIn, n;
+    int state;
+    double x, y, pi;
+
+    n = 100000000;
+    numIn = 0;
+
+    #pragma omp parallel private(state, x, y) reduction(+:numIn) 
+    {
+        
+        state = 25234 + 17 * omp_get_thread_num();
+        #pragma omp for
+        for (i = 0; i <= n; i++) {
+            state = rng_int(state);
+            x = (double)rng_doub(1.0, state);
+            state = rng_int(state);
+            y = (double)rng_doub(1.0, state);
+            if (x*x + y*y <= 1) numIn++;
+        }
+    }
+    pi = 4.*numIn / n;
+    printf("asdf pi %f\n", pi);
+    return 0;
+}
