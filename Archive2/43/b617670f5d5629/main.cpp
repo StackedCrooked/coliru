f55eaf52f5e0c9c13b0ec3e4c78a04e453031efd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-16 18:27:47 +0000

    Updating archive.

---
+++ Archive2/43/b617670f5d5629/main.cpp
@@ -0,0 +1,78 @@
+#define BOOST_RESULT_OF_USE_DECLTYPE
+#include <boost/phoenix/phoenix.hpp>
+#include <boost/phoenix/core.hpp>
+#include <boost/phoenix/operator.hpp>
+#include <boost/phoenix/function.hpp>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <utility>
+
+
+namespace {
+
+
+// a, b and c represent the equation arguments: ax2 + bx + c
+boost::phoenix::expression::argument<1>::type a;
+boost::phoenix::expression::argument<2>::type b;
+boost::phoenix::expression::argument<3>::type c;
+
+
+auto D = b * b - 4 * a * c;
+
+
+struct sqrt_impl
+{
+    template<typename Arg>
+    struct result { typedef double type; };
+
+    template<typename Arg>
+    typename result<Arg>::type operator()(Arg n)
+    {
+        return std::sqrt(static_cast<typename result<Arg>::type>(n));
+    }
+};
+
+
+boost::phoenix::function<sqrt_impl> sq;
+
+
+/**
+ * @brief get_x1 generates the first result of the equation.
+ * @return lazy return value.
+ * @example
+ *     auto value = get_x1(-1, 2, -1); // get the lazy result
+ *     double x1 = value(); // get the actual result
+ */
+auto get_x1 = (-b + sq(D)) / (2 * a);
+
+/**
+ * @brief get_x1 generates the second result of the equation.
+ * @return lazy return value.
+ */
+auto get_x2 = (-b - sq(D)) / (2 * a);
+
+
+struct solve_impl
+{
+    template<typename...> struct result { typedef result_type type; };
+
+    template<typename A, typename B, typename C>
+    result_type operator()(A a, B b, C c) const
+    {
+        return std::make_pair(get_x1(a, b, c), get_x2(a, b, c));
+    }
+};
+
+
+boost::phoenix::function<solve_impl> solve_quadratic_equation;
+
+
+} // anonymous namespace
+
+
+int main()
+{
+    auto p = solve_quadratic_equation(1, -1, -2)();
+    std::cout << p.first << ", " << p.second << std::endl;
+}
