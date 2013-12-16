4c9c623c93778a0af1752e6f31e95e142122d174
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-16 18:32:13 +0000

    Updating archive.

---
+++ Archive2/82/ad4d7e94c6716a/main.cpp
@@ -0,0 +1,56 @@
+#include <ctime>
+#include <chrono>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int total = 1000000;
+
+struct BaseClass {
+    float m[16];
+	int id;
+
+	BaseClass(int _id){
+		id = _id;
+	}
+};
+
+int main() {
+
+  std::vector< BaseClass > ar;
+
+  {
+    ar.reserve(total);
+	auto t_start = std::chrono::high_resolution_clock::now();
+    for (int var = 0; var < total; ++var) {
+      ar.emplace_back(var);
+    }
+	auto t_end = std::chrono::high_resolution_clock::now();
+	std::cout<< std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_start).count()<<"\n";
+    ar.clear();
+  }
+
+  {
+    ar.reserve(total);
+    auto t_start = std::chrono::high_resolution_clock::now();    
+    for (int var = 0; var < total; ++var) {
+      ar.emplace_back(var);
+    }
+    auto t_end = std::chrono::high_resolution_clock::now();
+	std::cout<< std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_start).count()<<"\n";
+    ar.clear();
+  }
+  
+  {
+    ar.reserve(total);
+    auto t_start = std::chrono::high_resolution_clock::now();    
+    for (int var = 0; var < total; ++var) {
+      ar.emplace_back(var);
+    }
+    auto t_end = std::chrono::high_resolution_clock::now();
+    std::cout<< std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_start).count()<<"\n";
+    ar.clear();
+  }  
+	
+	return 0;
+}
