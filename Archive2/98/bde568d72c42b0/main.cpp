04b094c0c01e1fd292fcfc658602b6b4f772bda1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-16 18:34:02 +0000

    Updating archive.

---
+++ Archive2/98/bde568d72c42b0/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+constexpr int f(int x) {
+    std::cout << x;
+}
+
+int main() {}
---
+++ Archive2/98/c5ca295ed08a50/main.cpp
@@ -0,0 +1,124 @@
+#include <typeindex>
+ 
+template <typename T>
+struct in_place_of {
+    typedef T type;
+};
+ 
+class runtime_type {
+private:
+	typedef void( *throw_t )( );
+	
+	template <typename T>
+	struct typed_throw {
+		static void check( ) {
+			throw (T*)nullptr;
+		}
+	};
+
+	std::type_index index;
+	throw_t check;
+
+public:
+	template <typename T>
+	runtime_type( in_place_of<T> ) : index( typeid( T ) ), check( typed_throw<T>::check ) {
+
+	}
+
+	std::size_t hash_code( ) const {
+		return index.hash_code( );
+	}
+
+	template <typename T>
+	bool search ( ) const {
+		try {
+			check( );
+		}
+		catch (T* e) {
+			return true;
+		}
+		catch (...) {
+		}
+		return false;
+	}
+
+	const char* name( ) const {
+		return index.name( );
+	}
+
+	bool operator== ( const runtime_type& other ) const {
+		return index == other.index;
+	}
+
+    bool operator!= ( const runtime_type& other ) const {
+		return index != other.index;
+	}
+
+	bool operator>= ( const runtime_type& other ) const {
+		return index >= other.index;
+	}
+
+	bool operator> ( const runtime_type& other ) const {
+		return index > other.index;
+	}
+
+	bool operator<= ( const runtime_type& other ) const {
+		return index <= other.index;
+	}
+
+	bool operator< ( const runtime_type& other ) const {
+		return index < other.index;
+	}
+
+};
+
+template <typename T>
+runtime_type typeof( ) {
+	typedef in_place_of<T> identity;
+	return runtime_type( identity() );
+}
+
+namespace std {
+
+	template <>
+	struct hash<runtime_type> {
+		size_t operator() ( const runtime_type& hashee ) {
+			return hashee.hash_code( );
+		}
+	};
+
+}
+ 
+#include <iostream>
+ 
+struct A {
+    virtual ~A(){};
+};
+
+struct X {}; struct Y : A {};
+
+
+ 
+int main() {
+    
+    runtime_type a = typeof<A>();
+    runtime_type x = typeof<X>();
+    runtime_type y = typeof<Y>();
+    
+    std::cout << std::boolalpha;
+    std::cout << a.search<A>() << std::endl;
+    std::cout << a.search<X>() << std::endl;
+    std::cout << a.search<Y>() << std::endl;
+    std::cout << std::endl;
+    
+    std::cout << x.search<A>() << std::endl;
+    std::cout << x.search<X>() << std::endl;
+    std::cout << x.search<Y>() << std::endl;
+    std::cout << std::endl;
+    
+    std::cout << y.search<A>() << std::endl;
+    std::cout << y.search<X>() << std::endl;
+    std::cout << y.search<Y>() << std::endl;
+    std::cout << std::endl;
+    
+}
