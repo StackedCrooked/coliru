db9daf8dcdef605338f885c2046a8d9546a6d3d5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-16 18:39:21 +0000

    Updating archive.

---
+++ trunk/Archive2/db/f62b0c49c32b28/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <iterator>
+int main()
+{
+    std::set<int> v1{1,2,3,4,5,6,7,8};
+    std::set<int> v2{        5,  7,  9,10};
+ 
+    std::set<int> v_intersection;
+ 
+    std::set_intersection(v1.begin(), v1.end(),
+                          v2.begin(), v2.end(),
+                          std::inserter(v_intersection, v_intersection.end()));
+    for(int n : v_intersection)
+        std::cout << n << ' ';
+}
