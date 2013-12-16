d7453a2ea9a9bbebaf32f5b9748b835717d3efb9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-16 18:38:04 +0000

    Updating archive.

---
+++ Archive2/d0/7bd1b8e02f26a1/main.cpp
@@ -0,0 +1,10 @@
+
+parse (partial) success
+s1 - Student - [int 23][string Bryan]
+s2 - Student - [int 45][string Tyler]
+s3 - Student - [int 4][string Stephen]
+s4 - Student - [int 7][string Mary]
+s5 - Student - [int 8][string Stephane]
+r1 [s1][s2][s3]
+r2 [s4][s2][s5]
+r3 [s1][s5]
