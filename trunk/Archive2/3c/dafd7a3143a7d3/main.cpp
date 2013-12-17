9eb21d6115c54364f3044730b6e4d49b1602d1f5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-17 23:20:26 +0000

    Updating archive.

---
+++ trunk/Archive2/3c/dafd7a3143a7d3/main.cpp
@@ -0,0 +1,53 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct Employees {
+    virtual void printStats(std::ostream& out) const=0;
+};
+
+struct Manager: public Employees {
+    virtual void printStats(std::ostream& out) const;
+    string mFirstName;
+    string mLastName;
+    int mSalary;
+    int mNumberOfMeetingsPerWeek;
+    int mNumberOfVacationDaysPerYear;    
+};
+
+
+void Manager::printStats(std::ostream& out) const //function doesn't modify, so const
+{
+    out << "\nManager." << endl;
+    out << "First name: " << mFirstName << endl; // data member of base class
+    out << "Last name: " << mLastName << endl; // data member of base class
+    out << "Salary: " << mSalary << endl; // data member of base class
+    out << "Meetings per week: " << mNumberOfMeetingsPerWeek << endl; // data member of derived class
+    out << "Vacation Days per year: " << mNumberOfVacationDaysPerYear << endl; //data member of derived class
+    out << endl;
+}
+std::ostream& operator<<(std::ostream& out, const Employees& employee) 
+{
+   employee.printStats(out);
+   return out; //return ostreams by reference when done
+}
+std::ostream& operator<<(std::ostream& out, const vector<Employees*>& midzer) 
+{
+   for (vector<Employees*>::const_iterator i = midzer.begin(); i != midzer.end(); ++i)
+   {
+       out << *(*i);
+   }
+   //or if you have a newer compiler:
+   //for (Employees* i : midzer) out << *i;
+   return out; //return ostreams by reference when done
+}
+int main() {
+    vector<Employees*> midzer;
+    ofstream outfile;
+    outfile.open("/users/mundt/desktop/EmployeeDatabase.txt");
+    outfile << midzer; //well that's easy
+    outfile.close();
+    cout << "success";
+}
