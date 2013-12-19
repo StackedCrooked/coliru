a8b72419ad38260569939a05a72190a17d508ddb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-19 20:33:34 +0000

    Updating archive.

---
+++ trunk/Archive2/62/6e8ecd82b97513/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+struct node {
+    node * next{nullptr};
+    int digit;
+    node(int digit) : digit{digit} {}
+    ~node() {
+        delete next;
+    }
+};
+
+void appendList(node * & list, int newDigit) {
+    if (list == nullptr) {//create first element if not present
+        list = new node{newDigit};
+    } else {//walk through to the end of the list and append
+        appendList(list->next, newDigit);
+    }
+}
+
+void reverseList(node * & current) {
+    if (current != nullptr) {
+        node * previous = nullptr;//last link shall be null
+        while (current->next != nullptr) {//valid link
+            node * const nextElement = current->next;//backup link
+            current->next = previous;//link backwards
+            previous = current;//backup current element pointer
+            current = nextElement;//move forward in list
+        }
+        current->next = previous;//link backwards for last element/new start
+    }
+}
+
+void print(const node * list) {
+    if (list == nullptr) {
+        std::cout << "empty list" << std::endl;
+    } else {
+        std::cout << "{" << list->digit << " ";
+        while (list->next != nullptr) {
+            list = list->next;
+            std::cout << list->digit << " ";
+        }
+        std::cout << "}" << std::endl;
+    }
+}
+
+void linkedListTest() {
+    node * list = nullptr;
+    //reverseList(list);
+    print(list);
+    for (const auto & elem : {5,1,3,2,4,8,6,9,7}) {
+        appendList(list, elem);
+        print(list);
+        std::cout << "↔";
+        reverseList(list);
+        print(list);
+    }
+}
+
+int main() {
+    linkedListTest();
+}
