0685e230d4377421e82e732b74bbcc5f773886dd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-22 02:37:39 +0000

    Updating archive.

---
+++ Archive2/b4/313b573874c0b1/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+void purr ( char a ) {
+    
+}
+
+void purr ( const char* a ) {
+    
+}
+
+template <typename T>
+struct increase_conversion_aversion {
+    T item;
+    
+    increase_conversion_aversion( T item ) : item(item) {
+    
+    }
+    
+    operator T () {
+        return item;
+    }
+};
+
+struct implicit {
+    
+    operator const char* () {
+        return "meow";
+    }
+    
+    operator increase_conversion_aversion<int> () {
+        return increase_conversion_aversion<int>( 42 );
+    }
+    
+};
+
+int main() {
+    const char* meow = "meow";
+    purr(meow);
+    purr( implicit() );
+    int convert = implicit{};
+}
