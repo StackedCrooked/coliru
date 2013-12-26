c8dc464510faa2f1c3d1f92e3ffde23d4928a8a8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-26 20:41:00 +0000

    Update f9/261ef6f75901a7

---
+++ Archive2/f9/261ef6f75901a7/main.cpp
@@ -0,0 +1,15 @@
+int main()
+{
+    void * ptr_void;
+    void ** ptr_2void;
+    ptr_void = ptr_2void;
+    return 0;
+}
+
+int main()
+{
+    int * ptr_int;
+    void * ptr_2void;
+    ptr_int = ptr_2void;
+    return 0;
+}
