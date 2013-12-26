36ae72b9d8e55670f30369127fae6f27878a9f45
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-26 20:47:58 +0000

    Update 8c/8ac392dc11e2ae

---
+++ trunk/Archive2/8c/8ac392dc11e2ae/main.cpp
@@ -0,0 +1,32 @@
+#include <cassert>
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+class Timer {
+    typedef std::chrono::high_resolution_clock Time;
+    typedef std::chrono::duration<float> duration;
+
+    Time::time_point m_timestamp;
+    duration currentElapsed;
+
+public:
+    Timer() : m_timestamp(Time::now()) {}
+
+    duration getTimeElapsed() const {
+        return currentElapsed;
+    }
+    
+    void Tick() {
+        auto now = Time::now();
+        currentElapsed = now - m_timestamp;
+        m_timestamp = now;
+    }
+};
+
+int main() {
+    Timer t;
+    std::this_thread::sleep_for(std::chrono::milliseconds{250});
+    t.Tick();
+    std::cout << t.getTimeElapsed().count() << '\n';
+}
