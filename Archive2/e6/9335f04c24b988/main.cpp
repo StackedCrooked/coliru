787c8bc47e6a04c2a9049e2871501945cd6a74f8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-26 20:37:15 +0000

    Update e6/9335f04c24b988

---
+++ Archive2/e6/9335f04c24b988/main.cpp
@@ -0,0 +1,11 @@
+/* Always remember #include <filename>
+int main() and std::cout with this "<<" being important before making a "HEllo world" then ending it with \n;
+return 0;
+*/
+#include <iostream>
+ 
+int main() /* This comment spans part of a line */
+{
+    std::cout << "Hello World!\n"; // till the end of the line
+    return 0;
+}
