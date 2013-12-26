425bae2c6ea925b3b98803b5164d8ba4d585fb62
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-26 20:30:05 +0000

    Update 4e/ced1d8ce986829

---
+++ Archive2/4e/ced1d8ce986829/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+
+#include <boost/iostreams/filtering_streambuf.hpp>
+#include <boost/iostreams/copy.hpp>
+#include <boost/iostreams/device/file.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
+
+using namespace std;
+
+int main(void)
+{
+    cout << boolalpha;
+
+    ifstream ifs("a.out", ios::binary);
+    boost::iostreams::file_sink ofile("a.out.boost.gz");
+
+    boost::iostreams::filtering_streambuf<boost::iostreams::output> out;
+    out.set_auto_close(true);
+
+    // out.push(boost::iostreams::gzip_compressor()); zlib not compiled in on coliru
+    out.push(ofile);
+
+    cout << "out.is_complete(): " << out.is_complete() << endl;
+    cout << "ifs.is_open()? "     << ifs.is_open()     << endl;
+    cout << "ofile.is_open()? "   << ofile.is_open()   << endl;
+
+    boost::iostreams::copy(ifs, out);
+
+    cout << "out.is_complete(): " << out.is_complete() << endl;
+    cout << "ifs.is_open()? "     << ifs.is_open()     << endl;
+    cout << "ofile.is_open()? "   << ofile.is_open()   << endl;
+}
