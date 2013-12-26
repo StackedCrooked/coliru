39c3d7fe8cb9cb57b5dd64c773f975ce9a504b42
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-26 20:50:21 +0000

    Update 13/f121e82c7bc79e

---
+++ trunk/Archive2/13/f121e82c7bc79e/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <functional>
+#include <algorithm>
+#include <future>
+#include <numeric>
+using namespace std;
+
+template<class InputIterator>
+int parallel_sum( InputIterator beg, InputIterator end )
+{
+    typename InputIterator::difference_type len = end - beg;
+    if ( len < 1000 )
+        return std::accumulate( beg, end, 0 );
+
+    InputIterator mid = beg + len / 2;
+    auto handle = std::async( [=]
+    {
+        int partial_sum = parallel_sum( mid, end );
+        cout << "partial_sum=" << partial_sum << endl;
+        return partial_sum;
+    } );
+    int sum = parallel_sum( beg, mid );
+    return sum + handle.get();
+}
+
+int main()
+{
+    std::vector<int> v( 10000, 1 );
+    std::cout << "The sum is " << parallel_sum( v.begin(), v.end() ) << '\n';
+
+    return 0;
+}
