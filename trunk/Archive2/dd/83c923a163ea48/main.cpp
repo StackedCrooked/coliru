4e160fefac90eeb5078eddad2a351a41f45c2d6b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-26 20:38:20 +0000

    Update dd/83c923a163ea48

---
+++ trunk/Archive2/dd/83c923a163ea48/main.cpp
@@ -0,0 +1,34 @@
+// regex_match example
+#include <iostream>
+#include <string>
+#include <regex>
+
+int main () {
+  try {
+    if(std::regex_match ("3", std::regex("[0-9]", std::regex_constants::basic) ))
+      std::cout << "string literal matched\n";
+    else
+      std::cout << "string literal unmatched";
+      
+  } catch (std::regex_error& e) {
+     using namespace std::regex_constants;
+     switch(e.code()) {
+        case error_badrepeat:
+          std::cerr << "Repeat was not preceded by a valid regular expression.\n";
+          break;
+          
+        case error_brack:
+          std::cerr << "The expression contained mismatched brackets ([ and ]).\n";
+          break;
+        
+        case error_paren:
+          std::cerr << "The expression contained mismatched parentheses (( and )).\n";
+          break;
+          
+        default:
+          std::cerr << "Some other regex exception happened.\n";
+          break;
+     }
+  }
+  return 0;
+}
