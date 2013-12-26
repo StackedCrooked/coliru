b03612c8804d2faf5ccffd0d860ff9e86aa3fa89
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-26 20:47:32 +0000

    Update 99/b9ca3db3efee4b

---
+++ trunk/Archive2/99/b9ca3db3efee4b/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class MyClass
+{
+public:
+    MyClass(initializer_list<double> il)
+    {}
+    
+    MyClass(int, string){}
+
+};
+
+int main()
+{
+    MyClass m{10, 10.1};
+
+    return 0;
+}
