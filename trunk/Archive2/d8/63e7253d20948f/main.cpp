fbaa32dad58c231d69b929250676a9aed4f3e430
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-26 20:31:27 +0000

    Update d8/63e7253d20948f

---
+++ trunk/Archive2/d8/63e7253d20948f/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+void purr ( char a ) {
+    
+}
+
+void purr ( const char* a ) {
+    
+}
+
+int main() {
+    const char* meow = "meow";
+    purr(meow);
+}
