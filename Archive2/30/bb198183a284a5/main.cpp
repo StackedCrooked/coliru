1f39e942eccedf406b0b01d4213183cb44b3c211
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-26 20:58:29 +0000

    Update 30/bb198183a284a5

---
+++ Archive2/30/bb198183a284a5/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+void purr ( char a ) {
+    std::cout << "char" << std::endl;
+    std::cout << a << std::endl;
+}
+
+void purr ( std::string a ) {
+    std::cout << "std::string selected" << std::endl;
+    std::cout << a << std::endl;
+}
+
+struct implicit {
+    
+    operator std::string () {
+        return "purr";
+    }
+  
+    template <typename T>
+    operator T () {
+        return T();
+    } 
+};
+
+int main() {
+    const char* meow = "meow";
+    purr(meow);
+    purr( implicit() );
+    
+    // int conversion still works properly
+    int convert = implicit{};
+    std::cout << "implicit conversion to int:" << std::endl;
+    std::cout << convert << std::endl;
+    
+    std::cout << "implicit conversion to string:" << std::endl;
+    std::string arf;
+    arf = meow;
+    std::cout << arf << std::endl;
+    arf = implicit{};
+    std::cout << arf << std::endl;
+}
