9d4748bf03accaef0d0056d409323b44a5025887
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-26 20:25:56 +0000

    Update 51/4cd041833ded36

---
+++ Archive2/51/4cd041833ded36/cmd.sh
@@ -0,0 +1,1 @@
+g++ -pipe -std=c++11 -W -Wall -pedantic main.cpp && ./a.out
---
+++ Archive2/51/4cd041833ded36/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+struct S {
+    S() { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+    S(const S&) { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+    ~S() { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+};
+
+S make_S() {
+    S tmp;
+    return tmp;
+}
+
+int main() {
+    S obj = make_S();
+}
