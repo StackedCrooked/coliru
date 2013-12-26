e71315e4472e3bb14a530d698c9eb07425488913
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-26 20:26:09 +0000

    Update bd/4f8b9ae9ded2f2

---
+++ trunk/Archive2/bd/4f8b9ae9ded2f2/main.cpp
@@ -0,0 +1,36 @@
+
+
+template<typename T>
+struct ThreadLocal
+{
+    ThreadLocal() : t(Get())
+    {
+        if (!t)
+        {
+            t = new T;
+        }
+    }
+    
+    T& get()
+    {
+        return *t;
+    }
+    
+    ~ThreadLocal()
+    {
+        delete t;
+        t = nullptr; // unset the static instance
+    }
+    
+private:    
+    T*& Get() // return by ref
+    {
+        static __thread T* t = nullptr;
+        return t;
+    }
+    
+    T*& t; // store as ref
+};
+
+
+int main() {}
