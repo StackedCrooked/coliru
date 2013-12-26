1ca9031b2ed6c78fb46d266c2e77d2233babf16e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-26 20:27:40 +0000

    Update 55/35e51d210d59ec

---
+++ Archive2/55/35e51d210d59ec/main.cpp
@@ -0,0 +1,87 @@
+// Heterogeneous vector (unordered) over specified sequence of types
+
+#include <boost/fusion/algorithm/transformation/transform.hpp>
+#include <boost/fusion/algorithm/iteration/for_each.hpp>
+#include <boost/fusion/sequence/intrinsic/at_key.hpp>
+#include <boost/fusion/container/map.hpp>
+
+#include <boost/mpl/transform.hpp>
+#include <boost/mpl/vector.hpp>
+
+#include <boost/phoenix/function/adapt_callable.hpp>
+#include <boost/phoenix/stl/algorithm.hpp>
+#include <boost/phoenix.hpp>
+
+#include <algorithm>
+#include <iostream>
+#include <ostream>
+#include <vector>
+
+using namespace boost;
+using namespace std;
+using phoenix::arg_names::arg1;
+
+// ___________________________________________________________________ //
+// syntax sugar
+struct get_fusion_pair_value
+{
+    template<typename FusionPair>
+    auto operator()(FusionPair &fp) const -> decltype(fp.second)
+    {
+        return fp.second;
+    }
+};
+BOOST_PHOENIX_ADAPT_CALLABLE(fp_second, get_fusion_pair_value, 1)
+// ___________________________________________________________________ //
+
+template<typename ...Ts>
+struct poly_sequence
+{
+    typename fusion::result_of::as_map
+    <
+        typename mpl::transform
+        <
+            mpl::vector<Ts...>,
+            fusion::pair< mpl::_1, std::vector<mpl::_1> >
+        >::type 
+    >::type vectors;
+};
+
+template<typename ...Ts, typename T>
+void push_back(poly_sequence<Ts...> &seq, const T &value)
+{
+    fusion::at_key<T>(seq.vectors).push_back(value);
+}
+
+template<typename ...Ts, typename PolymorhicUnaryFunctor>
+void for_each(poly_sequence<Ts...> &seq, PolymorhicUnaryFunctor f)
+{
+    fusion::for_each
+    (
+        seq.vectors,
+        phoenix::for_each(fp_second(arg1), f)
+    );
+}
+// ___________________________________________________________________ //
+
+struct Print
+{
+    template<typename T>
+    void operator()(const T &x)
+    {
+        cout << x << endl;
+    }
+};
+
+int main()
+{
+    poly_sequence<int, double> seq;
+
+    for(int i=0; i!=3; ++i)
+    {
+        push_back(seq, i);
+        push_back(seq, i+0.1);
+    }
+
+    for_each(seq, Print{});
+}
