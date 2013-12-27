6076adb9d569e63da7cd6228f36d423123593f76
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-27 07:04:49 +0000

    Updating archive.

---
+++ Archive2/c8/ad91f711161565/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#define let auto
+#define in ;
+#define ocaml int main() {
+#define print_string std::cout <<
+#define yolo }
+
+ocaml
+
+let z = "zar" in
+let d = "doz" in
+print_string z;
+print_string d;;
+
+yolo
