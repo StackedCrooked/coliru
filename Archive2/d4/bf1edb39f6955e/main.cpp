4b3f57159eaa508793cffe9faf325cba666db6f3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-28 00:12:37 +0000

    Updating archive.

---
+++ Archive2/d4/bf1edb39f6955e/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdint>
+
+constexpr std::uint32_t from_bytes(char b1, char b2, char b3, char b4)
+{
+    return b4 + 
+        (static_cast<std::uint32_t>(b3) << 8) + 
+        (static_cast<std::uint32_t>(b2) << 16) + 
+        (static_cast<std::uint32_t>(b1) << 24);
+}
+
+const std::uint32_t ELF_MAGIC_NUMBER = from_bytes(0x7F, 'E', 'L', 'F');
+
+int main()
+{
+    static_assert(ELF_MAGIC_NUMBER == 0x7F454c46, "!");
+}
