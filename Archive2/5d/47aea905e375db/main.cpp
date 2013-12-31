08e389a7a110fe7cdb060821587d1059679b5437
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-31 09:31:15 +0000

    Updating archive.

---
+++ Archive2/5d/47aea905e375db/main.cpp
@@ -0,0 +1,23 @@
+#include <cstddef>
+#include <iostream>
+#include <list>
+#include <numeric>
+
+bool predicate(int x) {
+    return x < 5;
+}
+
+int main() {
+    std::list<int> integers;
+    for (int i = 0; i < 10; ++i) {
+        // I want std::iota. :<
+        for (int j = 0; j < 10; ++j) {
+            integers.push_back(j);
+        }
+    }
+    integers.remove_if(&predicate);
+    for (std::list<int>::iterator it = integers.begin(); it != integers.end(); ++it) {
+        std::cout << *it << '\n';
+    }
+    return 0;
+}
