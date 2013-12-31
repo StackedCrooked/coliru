bba643b8c813c8e094c7190a7f8aa87373f3719c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2013-12-31 02:20:14 +0000

    Updating archive.

---
+++ trunk/Archive2/9b/475010206c3ef3/main.cpp
@@ -0,0 +1,23 @@
+#include <type_traits>
+
+template<
+  typename T,
+  int(*)[ std::is_enum<T>::value ] = nullptr,
+  typename U = typename std::underlying_type<T>::type
+>
+void func () { }
+
+template<
+  typename T, 
+  int(*)[ std::is_enum<T>::value == false ] = nullptr
+>
+void func () { }
+
+int
+main (int argc, char *argv[])
+{
+  enum E { };
+
+  func<int> ();
+  func<E  > ();
+}
