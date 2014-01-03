90bb32a922e38c0e044df57dc3c4c64e5de152dd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-03 13:20:13 +0000

    Updating archive.

---
+++ Archive2/8f/d6d243dfa6e832/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <boost/serialization/variant.hpp>
+
+namespace boost {
+
+    std::ostream& operator<<(std::ostream& out, const std::vector<int>& v) {
+        for(int i = 0; i < v.size(); ++i)
+            out << " " << v[i];
+        return out;
+    }
+
+}
+
+int main() {
+    boost::variant<int, std::string > a(3);
+    std::cout << a << '\n'; // OK
+    
+    using namespace boost;
+    std::vector<int> b(3, 1);
+    std::cout << b << '\n'; // OK
+
+    boost::variant<int, std::vector<int> > c(3);
+    std::cout << c << '\n'; // ERROR
+}
