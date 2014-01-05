5217e31e8fb06cdc0c78875984c64ca183aa2c18
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-05 16:32:02 +0000

    Updating archive.

---
+++ Archive2/d7/1a2fbbb749e073/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include </Archive2/30/a0085fa18ba8fa/main.cpp>
+
+int
+main (int argc, char *argv[])
+{
+  MAGIC(E, A = 42, B = 1, C = 123, D);
+
+  std::cout << MAGIC_STR(E, 0) << std::endl;
+  std::cout << MAGIC_STR(E, 1) << std::endl;
+  std::cout << MAGIC_STR(E, 2) << std::endl;
+  std::cout << MAGIC_STR(E, 3) << std::endl;
+}
