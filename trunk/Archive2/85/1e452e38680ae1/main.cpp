bf214621e35e5ced277d3bc0d803842324759ec6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-06 19:16:43 +0000

    Updating archive.

---
+++ trunk/Archive2/85/1e452e38680ae1/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+
+struct Data {
+    int n;
+    char c;
+};
+
+
+//template<typename T>
+//std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+//{
+//    for (auto& el : vec)
+//    {
+//        os << el << ' ';
+//    }
+//    return os;
+//}
+
+int main () {
+//    std::vector<std::string> words = {
+//        "Hello", "from", "GCC", __VERSION__, "!"
+//    };
+//    std::cout << words << std::endl;
+
+    
+}
