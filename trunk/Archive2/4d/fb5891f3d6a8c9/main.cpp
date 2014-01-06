6840f1f1954a7d016b11f3c247020aae2d7f1297
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-06 21:32:55 +0000

    Add 4d/fb5891f3d6a8c9

---
+++ trunk/Archive2/4d/fb5891f3d6a8c9/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <chrono>
+
+int main()
+{
+    std::cout << sizeof( std::time_t ) << std::endl;
+    
+    std::tm epochStart = {};
+    epochStart.tm_sec = 0;
+    epochStart.tm_min = 0;
+    epochStart.tm_hour = 0;
+    epochStart.tm_mday = 0;
+    epochStart.tm_mon = 0;
+    epochStart.tm_year = -1900;
+    epochStart.tm_wday = 0;
+    epochStart.tm_yday = 0;
+    epochStart.tm_isdst = -1;
+
+    std::time_t base = std::mktime(&epochStart);
+
+    std::chrono::system_clock::time_point baseTp=
+    std::chrono::system_clock::from_time_t(base);
+    std::time_t btp = std::chrono::system_clock::to_time_t(baseTp);
+    std::cout << "time: " << std::ctime(&btp);
+}
