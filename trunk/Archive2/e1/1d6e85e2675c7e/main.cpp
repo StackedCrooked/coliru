c9492276a13e13df6d08fe39f243d6b2b82d770b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-06 11:33:07 +0000

    Updating archive.

---
+++ trunk/Archive2/e1/1d6e85e2675c7e/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+int main ()
+{
+    const int N = 3 ;
+    int a[N][4] = { {0,1,2,3}, {4,5,0,1}, {2,3,4,5} };
+
+    for( const auto& row : a )
+    {
+        for( int v : row ) std::cout << v << ' ' ;
+        std::cout << '\n' ;
+    }
+    std::cout << '\n' ;
+
+    std::sort( std::begin( a[0] ), std::end( a[N-1] ) ) ;
+    for( const auto& row : a )
+    {
+        for( int v : row ) std::cout << v << ' ' ;
+        std::cout << '\n' ;
+    }
+    std::cout << '\n' ;
+}
