71fe2abc4494645179b1666d13cf302445fe990c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-06 21:21:20 +0000

    Add d3/39d1ccd96d5004

---
+++ Archive2/d3/39d1ccd96d5004/main.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+template <typename Iter, typename SomeFunction>                         
+void wrap(Iter first, Iter last, SomeFunction someFunction)
+{
+    someFunction(first, last);
+}
+
+template <typename Iter>
+void fill5(Iter first, Iter last)
+{
+    fill(first, last, 5); 
+}
+
+#define WRAP(f) \
+    [&] (auto&&... args) { f (std::forward<decltype(args)>(args)...); }
+
+int main()
+{
+    std::vector<int> v(100, -1);
+    wrap(v.begin(), v.end(), WRAP(fill5));
+}
