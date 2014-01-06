c9953c7f2cc02ca404d938f8a2e764288fc8d853
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-06 04:25:41 +0000

    Updating archive.

---
+++ Archive2/c4/c8ea1aad3fb5b6/cmd.sh
@@ -0,0 +1,5 @@
+##set -x ; clang++ -std=c++11 -O2 -W -Wall -pedantic -pthread main.cpp  && ./a.out
+##set -x ; g++ -std=c++11 -O2 -W -Wall  -Wconversion -pedantic -pthread main.cpp  && ./a.out
+##set -x ; g++ -std=c++11 -O2 -W -Wall  -Wconversion -pedantic -pthread main.cpp -S  && cat main.s
+##set -x ; gcc -x c -std=gnu99 -O2 -W -Wall -Wextra -pedantic  -pthread main.cpp  && ./a.out
+set -x ; clang -x c -std=c99 -O3 -W -Wall -pedantic  -pthread main.cpp  && ./a.out
