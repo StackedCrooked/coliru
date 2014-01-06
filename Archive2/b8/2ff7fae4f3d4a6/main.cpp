e7be1b7d1965aac2ea0cf295d5f5fbdd282bb952
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-06 21:17:27 +0000

    Add b8/2ff7fae4f3d4a6

---
+++ Archive2/b8/2ff7fae4f3d4a6/main.cpp
@@ -0,0 +1,31 @@
+#include <utility>
+#include <iostream>
+
+class Mutex
+{
+public:
+    Mutex() {}
+private:
+    Mutex(const Mutex&);
+    const Mutex& operator=(const Mutex&);
+};
+
+class Test
+{
+public:
+    Test() {}
+
+private:
+    Test(const Test&);
+//    const Test& operator=(const Test&);
+
+private:
+    std::pair<bool, Mutex> p;
+};
+        
+int main()
+{
+    std::pair<bool, Mutex> p;
+    Test t;
+    std::cout << "OK\n";
+}
