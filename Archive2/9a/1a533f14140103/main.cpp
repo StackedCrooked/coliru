1391a5988a5ccbefb75a0e5dc6e16bd4f940594e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-06 21:28:05 +0000

    Add 9a/1a533f14140103

---
+++ Archive2/9a/1a533f14140103/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+ 
+// "busy sleep" while suggesting that other threads run 
+// for a small amount of time
+void little_sleep(std::chrono::microseconds us)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    auto end = start + us;
+    do {
+        std::this_thread::yield();
+    } while (std::chrono::high_resolution_clock::now() < end);
+}
+ 
+int main()
+{
+    auto start = std::chrono::high_resolution_clock::now();
+ 
+    little_sleep(std::chrono::microseconds(100));
+ 
+    auto elapsed = std::chrono::high_resolution_clock::now() - start;
+    std::cout << "waited for "
+              << std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count()
+              << " microseconds\n";
+}
