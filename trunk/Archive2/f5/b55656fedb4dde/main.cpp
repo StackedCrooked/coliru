9e7c2ace1eb29f04e9b12354b88aafe54cd6f1e1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-06 21:21:55 +0000

    Add f5/b55656fedb4dde

---
+++ trunk/Archive2/f5/b55656fedb4dde/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+int main ()
+{
+    std::string s = "This is an example";
+    std::cout << s << '\n';
+ 
+    s.erase(0, 5); // Erase "This "
+    std::cout << s << '\n';
+ 
+    s.erase(std::find(s.begin(), s.end(), ' ')); // Erase ' '
+    std::cout << s << '\n';
+ 
+    s.erase(s.find(' ')); // Trim from ' ' to the end of the string
+    std::cout << s << '\n';
+}
