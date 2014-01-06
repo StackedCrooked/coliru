08e7478cdb297ffc8d6579fb7cf9d11a3717d323
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-06 21:26:09 +0000

    Add 08/68a99b31dd181b

---
+++ trunk/Archive2/08/68a99b31dd181b/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <bitset>
+#include <tuple>
+#include <map>
+#include <array>
+
+int main() {
+    auto stackoverflow = []()->void{ (*this)(); };
+    stackoverflow();
+}
