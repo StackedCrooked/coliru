939c0462e270f0984cdc078d4d9e89c380571265
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-06 21:23:30 +0000

    Add 09/b116da28e0a6d1

---
+++ trunk/Archive2/09/b116da28e0a6d1/main.cpp
@@ -0,0 +1,27 @@
+// ************* header.hpp ****************
+#include <functional>
+
+template < typename FN, typename... ARGS >
+void mylibfun( int a, int b, FN&& fn, ARGS&&... args )
+{ if( a<b ) std::bind( std::forward<FN>(fn), std::forward<ARGS>(args)... )() ; }
+
+// ...
+
+// ************* temptest.cpp **************
+// #include "header.hpp"
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+int main()
+{
+    std::string str = "abcdefgh" ;
+    mylibfun( 0, 5, &std::string::push_back, std::ref(str), '*' ) ;
+    mylibfun( 0, 5, &std::ostream::write,  std::ref(std::cout), str.c_str(), str.size() ) ;
+    mylibfun( 0, 5, &std::ostream::put, std::ref(std::cout), '\n' ) ;
+
+    std::string other = "123456" ;
+    mylibfun( 0, 5, &std::string::swap, std::ref(str), std::ref(other) ) ;
+    mylibfun( 0, 5, &std::printf, "str: %s\n", str.c_str() ) ;
+    std::cout << "other: " << other << '\n' ;
+}
