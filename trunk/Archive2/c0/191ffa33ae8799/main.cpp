36cfc5328cacc720cee7fa9a05a9def714eafe55
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-06 21:25:24 +0000

    Add c0/191ffa33ae8799

---
+++ trunk/Archive2/c0/191ffa33ae8799/main.cpp
@@ -0,0 +1,52 @@
+#include <boost/variant.hpp>
+#include <map>
+#include <string>
+
+
+using namespace std;
+using namespace boost;
+
+
+struct Node
+{    
+    template<typename T>
+    void set(string key, const T& value)
+    {
+        children_[key] = value;
+    }
+    
+    template<typename T>
+    T get(string key) const
+    {
+        auto it = children_.find(key);
+        if (it == children_.end())
+        {
+            return T(); // or throw
+        }
+        
+        return boost::get<T>(it->second); // will throw if type is wrong
+    }
+    
+private:
+    using Union = variant<int, bool, double, string, recursive_variant_>;
+    map<string, Union> children_;
+};
+
+
+void Set(Node& node, string string, int value)
+{
+    node.set(string, value);
+}
+
+void Set(Node& node, string key, bool b)
+{
+    node.set
+}
+void Set(Node& node, string key, string text);
+void Set(Node& node, string key, const Node& child);
+
+void Get(const Node&, string, Node& out);
+void Get(const Node&, string, bool& out);
+void Get(const Node&, string, int& out);
+void Get(const Node&, string, string& out);
+
