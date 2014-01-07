2c47bdb6efe6fa094561c4dc016febae47492bfb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-07 16:42:05 +0000

    Updating archive.

---
+++ Archive2/f2/c47df7b1811fab/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main()
+{
+    int const x = 1;
+    std::cout << "before modification: " << x << "\n";
+    const_cast<int&>(x) = 42;
+    std::cout << "after modification: " << x << "\n";
+}
