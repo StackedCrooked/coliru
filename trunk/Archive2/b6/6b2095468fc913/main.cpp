6befcf775b6d06a703b3e1fefb5558abe48abece
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-13 11:38:52 +0000

    Updating archive.

---
+++ trunk/Archive2/b6/6b2095468fc913/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+struct ListElement {
+    int digit;
+	ListElement *next;
+};
+
+void joinLast(ListElement *&l1, ListElement *&l2) {
+    //getting last element of l1
+    ListElement *l1tmp = new ListElement;
+    l1tmp = l1->next;
+    while(l1tmp->next) {
+       l1tmp = l1tmp->next;
+    }
+    
+    ListElement *l2tmp = new ListElement;
+    l2tmp->digit = l1tmp->digit;
+    l2tmp->next = l2;
+    l2 = l2tmp;
+    l1->next = NULL;
+}
+
+int main() {
+    ListElement *l1a = new ListElement;
+    l1a->next = new ListElement;
+    l1a->digit = 1;
+    l1a->next->digit = 2;
+    
+    ListElement *l2a = new ListElement;
+    l2a->digit = 3;
+    
+    joinLast(l1a,l2a);
+    
+    std::cout << l1a->digit << " " << l1a->next << std::endl;
+    std::cout << l2a->digit << " " << l2a->next->digit << std::endl;
+}
