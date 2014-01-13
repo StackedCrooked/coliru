05f0c3f275760b870a4cb6dca2ed582b53555517
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-13 10:38:18 +0000

    Updating archive.

---
+++ Archive2/2e/8fa75be767ce79/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+struct compare_cols
+{
+    constexpr compare_cols( std::size_t c ) : col(c) {}
+
+    using row_type = std::vector<std::string> ;
+
+    // binary predicate for sort
+    bool operator() ( const row_type& a, const row_type& b ) const
+    {
+        if( a.size() < col ) return b.size() >= col ;
+        else if( b.size() < col ) return false ;
+        else return a[col] < b[col] ;
+    }
+
+    // binary predicate for string < vector<string>
+    bool operator() ( const std::string& str, const row_type& row ) const
+    {
+        if( row.size() < col ) return false ;
+        else return str < row[col] ;
+    }
+
+    // binary predicate for vector<string> < string
+    bool operator() ( const row_type& row, const std::string& str ) const
+    {
+        if( row.size() < col ) return true ;
+        else return row[col] < str ;
+    }
+
+    const std::size_t col ;
+};
+
+int main()
+{
+    std::vector< std::vector<std::string> > v2d
+    {
+       { "zero", "ten", "twenty", "thirty" },
+       { "one", "fifteen", "twentythree", "thirtyeight" },
+       { "five", "thirteen", "twentynine", "thirtytwo" },
+       { "seven", "seventeen", "twentyseven", "thirtyseven" }
+    };
+
+    compare_cols comp2(2) ; // compare strings in col two
+
+    std::sort( std::begin(v2d), std::end(v2d), comp2 ) ; // sort on col two
+    
+    // binary search for string in col2 on sequence sorted on col 2
+    bool found = std::binary_search( std::begin(v2d), std::end(v2d), "twentythree", comp2 ) ;
+    std::cout << "found? " << std::boolalpha << found << '\n' ;
+}
