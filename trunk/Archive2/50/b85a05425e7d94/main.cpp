e4dce87b2c854920e9a79e8ebaf6c1f996943a7f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-13 07:36:35 +0000

    Updating archive.

---
+++ trunk/Archive2/50/b85a05425e7d94/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <type_traits>
+ 
+int main() {
+    typedef std::remove_cv<const int>::type type1;
+    typedef std::remove_cv<volatile int>::type type2;
+    typedef std::remove_cv<const volatile int>::type type3;
+    typedef std::remove_cv<const volatile int*>::type type4;
+    typedef std::remove_cv<int * const volatile>::type type5;
+ 
+    std::cout << "test1 " << (std::is_same<int, type1>::value
+        ? "passed" : "failed") << '\n';
+    std::cout << "test2 " << (std::is_same<int, type2>::value
+        ? "passed" : "failed") << '\n';
+    std::cout << "test3 " << (std::is_same<int, type3>::value
+        ? "passed" : "failed") << '\n';
+    std::cout << "test4 " << (std::is_same<const volatile int*, type4>::value
+        ? "passed" : "failed") << '\n';
+    std::cout << "test5 " << (std::is_same<int*, type5>::value
+        ? "passed" : "failed") << '\n';
+}
