1c9681d9fd1ca5d23405f4c20d467cf4582c9c9d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-13 07:36:55 +0000

    Updating archive.

---
+++ Archive2/88/8c356492ebe357/main.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <array>
+#include <bitset>
+
+int main() {
+    constexpr int M = 3;
+    constexpr int N = 3;
+    std::array<int, M * N> array = {{
+        1, 2, 3,
+        0, 4, 0,
+        4, 2, 3
+    }};
+    
+    std::bitset<N> zeroed_cols; 
+    for (int i = 0; i < M; ++i) {
+        for (int j = 0; j < N; ++j) {
+            int elem = array[i * N + j];
+            if (elem == 0 && !zeroed_cols[j]) {
+                for (int a = 0; a < M; ++a)
+                    array[a * N + j] = 0;
+                for (int b = 0; b < N; ++b)
+                    array[i * N + b] = 0;
+                zeroed_cols[j] = true;
+                break;
+            }
+        }
+    }
+    
+    for (int i = 0; i < M * N; ++i) {
+        std::cout << array[i];
+        if ((i + 1) % 3 == 0)
+            std::cout << '\n';
+    }
+
+    return 0; 
+}
