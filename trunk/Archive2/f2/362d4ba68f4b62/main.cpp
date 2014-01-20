dff3c422251e65824882170896941f94d06e02e4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:56:17 +0000

    Add f2/362d4ba68f4b62 .

---
+++ trunk/Archive2/f2/362d4ba68f4b62/main.cpp
@@ -0,0 +1,33 @@
+#include <cstdlib>
+#include <iostream>
+#include <mutex>
+
+struct my_type {
+    my_type() {std::cout << "Construct: " << this << "\n";}
+    ~my_type() {std::cout << "Destroy: " << this << "\n";}
+};
+
+my_type const& instantiate() {
+    union storage_t {
+        char dummy;
+        my_type instance;
+        storage_t() {}
+        ~storage_t() {}
+    };
+    
+    static std::once_flag flag;
+    static storage_t storage;
+
+    std::call_once(flag, []{
+        ::new (&storage.instance) my_type;
+        std::atexit([]{ storage.instance.~my_type(); }); 
+    });
+
+    return storage.instance;
+}
+
+int main() {
+    std::cout << "Enter main\n";
+    instantiate();
+    std::cout << "Leave main\n";
+}
