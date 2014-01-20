54586edd1492cd9c82a11140bfc4a3ba20a0a787
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:30:52 +0000

    Add 35/96c2009d6793a8 .

---
+++ trunk/Archive2/35/96c2009d6793a8/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+template <typename type_t, typename func_t, func_t f>
+struct T
+{
+    T() { f(); }
+    
+    type_t a;   /**< Irrelevant here, but left in to give your `type_t` something to do */
+};
+
+void foo() { std::cout << "A\n"; }
+int  bar() { std::cout << "B\n"; return 0; }
+
+int main()
+{
+    T<int, void(*)(), &foo> t1;
+    T<int, int (*)(),  &bar> t2;
+}
