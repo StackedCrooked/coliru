9aa475c42ae2778ac81f8711ba2cafc221f31a94
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:44:13 +0000

    Add d2/7c231e20718f51 .

---
+++ Archive2/d2/7c231e20718f51/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+template<typename T>
+void processPointer(T* ptr) { std::cout << "Hello!\n"; }
+
+void processPointer(void*) = delete;
+
+int main () {
+    processPointer<void>(nullptr);
+}
