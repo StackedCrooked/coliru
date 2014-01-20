1c18cc1ee35d48b96267536aec5fd5fb8ecc1412
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:34:59 +0000

    Add b9/959e7869c2b99c .

---
+++ Archive2/b9/959e7869c2b99c/main.cpp
@@ -0,0 +1,13 @@
+#include <boost/multiprecision/cpp_int.hpp>
+#include <iostream>
+
+using namespace boost::multiprecision;
+
+int main() {
+    cpp_rational a("4561231231235/123123123123");
+    cpp_int v = numerator(a)/denominator(a);
+    std::cout << "convert: " << v.convert_to<int>() << "\n";
+
+    a = cpp_rational("456/123");
+    std::cout << "convert: " << int(numerator(a)/denominator(a)) << "\n";
+}
