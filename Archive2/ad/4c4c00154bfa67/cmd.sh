40a2d16c41851d8de8b363929e9833ce71e53a55
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:35:58 +0000

    Add ad/4c4c00154bfa67 .

---
+++ Archive2/ad/4c4c00154bfa67/cmd.sh
@@ -0,0 +1,7 @@
+set -x ; g++ -std=c++11 -Wall -Wextra -Wconversion -pedantic  -O0  -pedantic  -pthread main.cpp && ./a.out
+##g++ --version
+##set -x ; g++ -std=c++11 -Wall -Wextra -Wconversion -pedantic  -O0  -pedantic  -pthread -S main.cpp && cat main.s
+##set -x ; clang++ -std=c++03 -Wall -Wextra -Wconversion -pedantic  -O0  -pedantic -Wall -pthread main.cpp && ./a.out
+##set -x ; gcc -x c -std=c99 -O0 -W -Wall -Wextra -Wconversion -pedantic main.cpp && ./a.out
+##set -x ; gcc -x c -std=c99 -O0 -W -Wall -Wextra -Wconversion -pedantic -S main.cpp && cat main.s
+##set -x ; clang -x c -std=c99 -Wall -Wextra -Weverything -O0  -pedantic main.cpp && ./a.out
