0785037b8283dcee1bd5a04f3dcd4d691aba9fb6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:40:57 +0000

    Add ba/ce5bac19397de5 .

---
+++ trunk/Archive2/ba/ce5bac19397de5/main.cpp
@@ -0,0 +1,15 @@
+#ifndef BIRTHDAY_H
+#define BIRTHDAY_H
+
+#include <iostream>
+
+class Birthday
+{
+public:
+    Birthday(int y, int m, int d) : y(y), m(m), d(d) {}
+    friend std::ostream& operator<<(std::ostream& os, const Birthday& bd);
+private:
+    int y, m, d;
+};
+
+#endif
