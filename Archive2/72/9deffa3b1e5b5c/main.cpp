d30b479b68d3a565fcde8a14130c349f6c9fef11
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:53:18 +0000

    Add 72/9deffa3b1e5b5c .

---
+++ Archive2/72/9deffa3b1e5b5c/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello " << name << "!" << endl;
+    return 0;
+}
