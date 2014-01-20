4850fb4173aae1bd2e738f34e9884e21f16e69ab
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:21:55 +0000

    Add 09/32152ffc4d34b0 .

---
+++ trunk/Archive2/09/32152ffc4d34b0/cmd.sh
@@ -0,0 +1,10 @@
+echo '**** not taking its address, and not defining it ****'
+g++-4.8 -std=c++11 -O2 -Wall -Wextra -pedantic-errors main.cpp && ./a.out && echo 'success!!!'
+echo
+echo '**** taking its address, but not defining it ****'
+g++-4.8 -DTAKE_ITS_ADDRESS -std=c++11 -O2 -Wall -Wextra -pedantic-errors main.cpp && ./a.out && echo 'success!!!'
+echo
+echo '**** taking its address, and also defining it ****'
+g++-4.8 -DTAKE_ITS_ADDRESS -DDEFINE_IT -std=c++11 -O2 -Wall -Wextra -pedantic-errors main.cpp && ./a.out && echo 'success!!!'
+echo
+echo
