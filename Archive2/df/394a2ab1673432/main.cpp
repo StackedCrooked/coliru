ec72217d2d8bbdaed812fdccae881358ab2c1d01
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:53:42 +0000

    Add df/394a2ab1673432 .

---
+++ Archive2/df/394a2ab1673432/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+enum class Types
+{
+    X;
+    Y;
+    Z;
+};
+
+int typeMeta[number of types] = {
+    0, // meta for X
+    1, // meta for Y
+    10, // meta for Z
+}
+    
+int main (int argc, char *argv [])
+{
+    return 0; 
+}
