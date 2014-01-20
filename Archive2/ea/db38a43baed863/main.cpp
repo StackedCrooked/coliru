041ebae4ae25215a082150c1bee8015fd03e5efd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:58:44 +0000

    Add ea/db38a43baed863 .

---
+++ Archive2/ea/db38a43baed863/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+    
+    struct Functor
+    {
+       void operator()() {
+          std::cout << "lol\n";
+       }
+    };
+    
+    int main()
+    {
+    
+    Functor f;
+    f();  // output: "lol"
+    }
