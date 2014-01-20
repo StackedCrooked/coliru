8b6514589d3cb1cd344a1c3d8f1a7aa83aa9dfb8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:35:09 +0000

    Add b9/acee77ba5a7536 .

---
+++ trunk/Archive2/b9/acee77ba5a7536/main.cpp
@@ -0,0 +1,25 @@
+signed long int volatile arr[1];
+
+struct X {
+   operator unsigned const short&() {
+      static unsigned const short a = 0;
+      return a;
+   }
+
+   int f() {
+      if (false) {
+      } else do
+           return throw compl
+                        sizeof true
+                        xor not this
+                        and new signed long int volatile
+                        or false
+                        bitor nullptr
+                        not_eq operator unsigned const short bitand()
+                        +arr, 0;
+      while (false);
+      return 42;
+   }
+};
+
+int main() {}
