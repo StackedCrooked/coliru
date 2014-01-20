097724b288d723fad1437e6d15e3c10d63043476
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:31:25 +0000

    Add 37/caa03c240cced2 .

---
+++ Archive2/37/caa03c240cced2/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <type_traits>
+
+typedef int T;
+
+struct A {
+  A(T) {}
+};
+
+struct B {
+  B(T) {}
+};
+
+struct C {
+  C(A const&) { std::cout << "C(A)\n"; }
+  C(B const&) { std::cout << "C(B)\n"; }
+  template <typename U, typename=typename std::enable_if<
+    !std::is_base_of<A,typename std::decay<U>::type>::value &&
+    std::is_convertible<U&&, A>::value>::type
+  > C(U&& u) : C(A(std::forward<U>(u))) {}
+};
+
+int main() {
+  {
+    std::cout << "rvalue A: ";
+    C c(A(42));
+  }
+  {
+    std::cout << "rvalue B: ";
+    C c(B(42));
+  }
+  {
+    std::cout << "lvalue A: ";
+    A a(13);
+    C c(a);
+  }
+  {
+    std::cout << "lvalue B: ";
+    B b(13);
+    C c(b);
+  }
+  {
+    std::cout << "rvalue T: ";
+    C c(42);
+  }
+  {
+    std::cout << "lvalue T: ";
+    T t{};
+    C c(t);
+  }
+}
