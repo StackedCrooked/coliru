821e5e502f0cd9b31a17b5f5399c22365afada81
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:23:44 +0000

    Add 16/27e74cd26e0b09 .

---
+++ Archive2/16/27e74cd26e0b09/main.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+ 
+int main()
+{
+   int a = 5, b = 3;
+ 
+   // before
+   std::cout << a << ' ' << b << '\n';
+ 
+   std::swap(a,b);
+ 
+   // after
+   std::cout << a << ' ' << b << '\n';
+}
