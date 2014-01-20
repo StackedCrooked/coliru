b67b620c61272edaa1e0741eb411e8a1abd10cc5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:37:43 +0000

    Add 49/4c97c6c2300b84 .

---
+++ trunk/Archive2/49/4c97c6c2300b84/main.cpp
@@ -0,0 +1,26 @@
+#include <memory>
+#include <vector>
+#include <iostream>
+
+struct Product{
+};
+
+struct Book : public Product{
+};
+
+struct Magazine: public Product{
+};
+
+int main(){
+    std::vector<std::shared_ptr<Product> > products;
+    //C++11
+    products.emplace_back(new Book());
+    products.emplace_back(new Magazine());
+
+    //C++03TR1
+    // we don't have emplace_back here so we need to 
+    // construct both the shared_ptr and the book
+    products.push_back(std::shared_ptr<Product>(new Book()));
+    std::cout<< products.size() << std::endl;
+    
+}
