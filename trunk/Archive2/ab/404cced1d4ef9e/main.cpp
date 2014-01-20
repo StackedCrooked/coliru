a96752e0290f6740bd05d09a06f44513b7660f8d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:35:21 +0000

    Add ab/404cced1d4ef9e .

---
+++ trunk/Archive2/ab/404cced1d4ef9e/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+class Vector{
+    double* elem;
+    int sz;
+public:
+    Vector(int s):elem{new double[s]}, sz{s} { for(int i = 0; i != sz; ++i) elem[i]= 0; }
+    ~Vector() { delete[] elem; }
+    double& operator[](int i) { return elem[i]; }
+    int size() const { return sz; }
+};
+
+class Container{
+public:
+    virtual double& operator[](int i) = 0;
+    virtual int size() const = 0;
+    virtual ~Container() {}
+};
+
+class Vector_container:public Container{
+    Vector v;
+public:
+    Vector_container(int s): v{s}{}
+    ~Vector_container() {}
+    double& operator[](int i) { return v[i]; }
+    int size() const {return v.size(); }
+};
+
+void use(Container& c)
+{
+    const int sz = c.size();
+    for(int i = 0; i != sz; i++) std::cout << c[i] << '\n';
+}
+
+int main()
+{
+    Vector_container vc{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    use(vc);
+}
+    
+
