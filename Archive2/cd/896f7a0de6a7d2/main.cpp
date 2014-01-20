91d7e4678f9d5260b4ef285c75e042dc390f73b3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:47:10 +0000

    Add cd/896f7a0de6a7d2 .

---
+++ Archive2/cd/896f7a0de6a7d2/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+struct Base {
+    int i;
+
+    Base(int i):i(i) {}
+    Base():i(10) {}
+};
+
+struct Derived : Base {
+    int j;
+
+    Derived(int j):Base(j), j(j) {}
+    Derived()=default;
+};
+
+int main() {
+    std::cout << __VERSION__ << '\n';
+    Derived d{42};
+    ::new (&d) Derived{};
+    std::cout << "d.i = " << d.i << "  " << "d.j = " << d.j << '\n';
+}
