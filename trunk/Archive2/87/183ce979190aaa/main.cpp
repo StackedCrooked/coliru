edfea1f760390a8bdb6ff3de3166ee6b11bc2153
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 19:01:21 +0000

    Add 87/183ce979190aaa .

---
+++ trunk/Archive2/87/183ce979190aaa/main.cpp
@@ -0,0 +1,13 @@
+struct S {
+    operator unsigned const short &() const {static const unsigned short s{}; return s;}
+    
+    int foo() {
+        int arr[1];
+        return throw compl sizeof true xor not this and new signed long int volatile or false bitor nullptr not_eq operator unsigned const short bitand() +arr, 0;
+    }   
+};
+
+int main() {
+    S s;
+    s.foo();
+}
