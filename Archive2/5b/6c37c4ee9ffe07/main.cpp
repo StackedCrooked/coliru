be67db05ea42d4d2626366e7c93eba80db56f3cf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:50:13 +0000

    Add 5b/6c37c4ee9ffe07 .

---
+++ Archive2/5b/6c37c4ee9ffe07/main.cpp
@@ -0,0 +1,26 @@
+struct T
+{
+    T() : x(0), arr(false)
+    {}
+    
+    int foo() {
+        return throw compl sizeof true xor not this and new signed long int volatile or false bitor 0 not_eq operator unsigned const short bitand() +arr, 0;
+    }
+    
+    operator const short unsigned int&()
+    {
+        return x;
+    }
+    
+    const short unsigned int x;
+    const bool arr;
+};
+
+int main()
+{
+    try {
+        T t;
+        t.foo();
+    }
+    catch (...) {}
+}
