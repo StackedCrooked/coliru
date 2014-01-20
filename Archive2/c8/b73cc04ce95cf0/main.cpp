0288cd8b368979f3afc657ca44f41b4347349c40
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:40:25 +0000

    Add c8/b73cc04ce95cf0 .

---
+++ Archive2/c8/b73cc04ce95cf0/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <initializer_list>
+
+
+struct test {
+  test ()  { std::cout << "test ()" << std::endl ; };
+  test (int)  { std::cout << "test (int)" << std::endl ; };
+  test ( std::initializer_list<int>) { std::cout << "test (initializer_list<int>)" << std::endl ; } ;
+};
+
+void func( test const &s = {} )
+{
+}
+
+int main()
+{
+    test s = {} ;
+    func() ;
+}
