4426088d187e0e90ee6875f607119418a50e18a6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:52:01 +0000

    Add 5f/cdd673ebff5ca2 .

---
+++ trunk/Archive2/5f/cdd673ebff5ca2/main.cpp
@@ -0,0 +1,16 @@
+#include <functional>
+#include <iostream>
+#include <thread>
+
+class A {
+public:
+    void foo(int n) { std::cout << n << std::endl; }
+};
+
+int main()
+{
+    A a;
+
+    std::thread t1(&A::foo, std::ref(a), 100);
+    t1.join();
+}
