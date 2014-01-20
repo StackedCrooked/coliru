2c32b4e24fa7a5d9dc7c0fcbe4812e8cda548821
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:33:15 +0000

    Add b3/e2da1bb9f5178b .

---
+++ Archive2/b3/e2da1bb9f5178b/main.cpp
@@ -0,0 +1,17 @@
+ #include <stdio.h>
+     #include <math.h>
+    int main ()
+    {
+    {
+    float x,y ;
+    y = x * 1.30822 ;
+    printf("Entrez une somme numerique en euros : ");
+    scanf("%f", &x);
+  
+    scanf("y = x * 1.30822 ");
+    printf(" La valeur en dollar est: ") ;
+    scanf("%f", &y);
+    getchar ();
+    return 0 ;
+    }
+    }
