3bfb105a073ed5591a768b4a9cb98d69f952f862
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:25:21 +0000

    Add 22/b701d663b8ca3e .

---
+++ trunk/Archive2/22/b701d663b8ca3e/main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+
+struct ListElement {
+    ListElement * next;
+};
+
+void joinLast1(ListElement * & l1, ListElement * & l2) {
+    if (l1->next == nullptr) {
+        l2 = l1;
+        l1 = nullptr;
+    } else {
+        joinLast1(l1->next, l2);
+    }
+}
+
+void joinLast2(ListElement * & l1, ListElement * & l2) {
+    if (l1->next == nullptr) {
+        l2 = l1;
+        l1 = nullptr;
+    } else {
+        ListElement * help = l1;
+        while (help->next->next != nullptr) {
+            help = help->next;
+        }
+        l2 = help->next;
+        help->next = nullptr;
+    }
+}
+
+void joinLast3(ListElement * & l1, ListElement * & l2) {
+    ListElement * * help = &l1;
+    while ((*help)->next != nullptr) {
+        help = &(*help)->next;
+    }
+    l2 = *help;
+    *help = nullptr;
+}
+
+#include <functional>
+
+void joinLast4(ListElement * & l1, ListElement * & l2) {
+    std::reference_wrapper<ListElement *> help = l1;
+    while (help.get()->next != nullptr) {
+        help = help.get()->next;
+    }
+    l2 = help.get();
+    help.get() = nullptr;
+}
+
+int main() {
+    float aFloat = .5;
+    int* anInt = (int*)&aFloat; // is equivalent to
+    int* anInt2 = reinterpret_cast<int*>(&aFloat); 
+    std::cout << aFloat << ' ' << *anInt << ' ' << *anInt2 << '\n';
+    {
+        ListElement help2{nullptr};
+        ListElement help{&help2};
+        ListElement * l1{&help};
+        ListElement * l2{nullptr};
+        std::cout << l1 << " " << l1->next << " : " << l2 << std::endl;
+        joinLast1(l1, l2);
+        std::cout << l1 << " " << l1->next << " : " << l2 << " " << l2->next << std::endl;
+    }
+    {
+        ListElement help2{nullptr};
+        ListElement help{&help2};
+        ListElement * l1{&help};
+        ListElement * l2{nullptr};
+        std::cout << l1 << " " << l1->next << " : " << l2 << std::endl;
+        joinLast2(l1, l2);
+        std::cout << l1 << " " << l1->next << " : " << l2 << " " << l2->next << std::endl;
+    }
+    {
+        ListElement help2{nullptr};
+        ListElement help{&help2};
+        ListElement * l1{&help};
+        ListElement * l2{nullptr};
+        std::cout << l1 << " " << l1->next << " : " << l2 << std::endl;
+        joinLast3(l1, l2);
+        std::cout << l1 << " " << l1->next << " : " << l2 << " " << l2->next << std::endl;
+    }
+    {
+        ListElement help{nullptr};
+        ListElement * l1{&help};
+        ListElement * l2{nullptr};
+        std::cout << l1 << " " << l1->next << " : " << l2 << std::endl;
+        joinLast3(l1, l2);
+        std::cout << l1 << " : " << l2 << " " << l2->next << std::endl;
+    }
+}
