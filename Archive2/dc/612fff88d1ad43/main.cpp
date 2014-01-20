f2151e4340e08b4d17a456ae1fef5f56fcdfd941
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:52:49 +0000

    Add dc/612fff88d1ad43 .

---
+++ Archive2/dc/612fff88d1ad43/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <functional>
+
+struct finder_base
+{
+    virtual bool operator()(int i) const
+    {
+        std::cout << i << ' ';
+        return true;
+    }
+};
+
+struct finder : finder_base
+{
+    virtual bool operator()(int i) const override
+    {
+        std::cout << i << ' ';
+        return false;
+    }
+};
+
+void foo(std::vector<int> const& v, finder_base const& f)
+{
+    std::cout << __PRETTY_FUNCTION__ << '\n';
+    std::find_if(v.begin(), v.end(), f);
+}
+
+void bar(std::vector<int> const& v, finder const& f)
+{
+    std::cout << __PRETTY_FUNCTION__ << '\n';
+    std::find_if(v.begin(), v.end(), f);
+}
+
+void baz(std::vector<int> const& v, finder_base const& f)
+{
+    std::cout << __PRETTY_FUNCTION__ << '\n';
+    std::find_if(v.begin(), v.end(), std::ref(f));
+}
+
+int main()
+{
+    std::vector<int> v{1,2,3,4};
+    finder f;
+    
+    foo(v, f);
+    std::cout << "\n----\n";
+    bar(v, f);
+    std::cout << "\n----\n";
+    baz(v, f);
+    std::cout << "\n----\n";
+}
