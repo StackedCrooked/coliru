3f918829105cc871eb9fe3a8b8b2302d83c704a6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 18:28:01 +0000

    Add 1d/f0127869e2e7ff .

---
+++ trunk/Archive2/1d/f0127869e2e7ff/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+
+int main() {
+    int arr[3][4] = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}};
+    std::cout << sizeof(arr);
+}
