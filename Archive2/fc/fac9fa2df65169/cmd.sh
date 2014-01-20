0f1f4c1c1adb9ea426537c58565b1af5445dba66
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 19:03:43 +0000

    Add fc/fac9fa2df65169 .

---
+++ Archive2/fc/fac9fa2df65169/cmd.sh
@@ -0,0 +1,12 @@
+g++ main.cpp -Wall -pedantic -Wextra -Weffc++; echo 7889 > shop-account.txt; ./a.out <<ENDOFINPUT
+0
+1 10000
+invalid menu choice
+1 three dollars
+1 -90
+1 7880
+0
+1 30
+0
+1
+ENDOFINPUT
