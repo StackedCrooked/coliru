0dac9e8a0d1836c7dc44c9d30e2b5ced23a16afe
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-20 19:03:47 +0000

    Add fc/d768b36774841e .

---
+++ Archive2/fc/d768b36774841e/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+
+int main()
+{char str1[10],str2[10];
+ cout<<"Enter a string";
+ cin.getline(str1,10);
+ cout<<"Enter a string";
+ cin.getline(str2,10);
+ int x1=strlen(str1);
+ int x2=strlen(str2);
+ if(x1==x2)
+ cout<<"sjsgz";
+ else
+ cout<<"aaaaa";
+}
