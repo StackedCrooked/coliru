4188c970d5011d9183ee64a335d202dcbba85a02
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-28 12:23:34 +0000

    Update archive.

---
+++ Archive2/4c/d19dde1a079811/main.cpp
@@ -0,0 +1,21 @@
+#include <string.h>  //memcpy
+#include <string>    //std::string
+#include <iostream>  //std::cout
+
+void copy (char dst[20], const std::string& src)
+{
+    if (src.size() < sizeof(dst)) {
+        memcpy (dst, src.c_str(), src.size()+1);
+    } else {
+        memcpy (dst, src.data(), sizeof(dst)-1);
+        dst[sizeof(dst)-1] = 0;
+    }
+}
+
+int main()
+{
+    const std::string src("content");
+    char              dst[20];
+    copy (dst, src);
+    std::cout << dst << std::endl;
+}
