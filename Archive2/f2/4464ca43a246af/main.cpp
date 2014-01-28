1b90e54c6e61317dbe79d25786f96f00577a9512
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-28 12:35:45 +0000

    Update archive.

---
+++ Archive2/f2/4464ca43a246af/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <functional>
+
+class Sensor{
+public:
+    std::function<void()> begin;
+    std::function<void()> end;
+};
+
+int main()
+{
+   
+   Sensor* sensor = new Sensor;
+   sensor->begin = [](){std::cout << "Started \n";};
+   sensor->end = [](){std::cout << "Ended \n";};
+   
+   sensor->begin();
+   sensor->end();
+   
+   
+   delete sensor;
+   return 0;
+}
