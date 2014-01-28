6bf1d22958fcef9491ab8bb34cb82af08edcd96d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-28 12:20:48 +0000

    Update archive.

---
+++ trunk/Archive2/34/55665a2f48aa36/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+constexpr const char * a = "toto";
+constexpr const char * b = "totvvv";
+
+constexpr bool equal( const char * a, const char * b ) {
+    if (!a || !b)
+        return false;
+    while ( *a && *b && *a == *b ) {
+        ++a; ++b;
+    }
+    return !*a && !*b;
+}
+constexpr auto c = equal(a,b);
+constexpr auto d = equal(a,a);
+
+int main() {
+  std::cout << c << std::endl;
+  std::cout << d << std::endl;
+}
