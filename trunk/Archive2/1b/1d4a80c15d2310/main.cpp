04140627822d1e45bc844f7c966c9803fd9dba42
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-28 12:18:43 +0000

    Update archive.

---
+++ trunk/Archive2/1b/1d4a80c15d2310/main.cpp
@@ -0,0 +1,6 @@
+#include <cstdio>
+
+int main() {
+    printf("%.3f\n", 0.95);
+    printf("%.3f\n", 9.5);
+}
---
+++ trunk/Archive2/1b/ad032f7db27ae0/main.cpp
@@ -0,0 +1,7 @@
+#include <map>
+
+int main()
+{
+    std::map<int, int> m;
+    m.insert(1,1); 
+}
