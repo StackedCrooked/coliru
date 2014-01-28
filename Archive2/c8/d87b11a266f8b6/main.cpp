672d49e6d94e6d189c9b389ee2834c98c818fc9e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-28 12:32:44 +0000

    Update archive.

---
+++ Archive2/c8/d87b11a266f8b6/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace std;
+
+typedef void function;
+
+auto dizi={1.0,2.0,3.0,4.0,5.0};
+auto name="James";
+auto age =27;
+auto weight=135000;
+auto Alive=false;
+
+auto size=256;
+auto doit=(size)*(size);
+auto numtriangles=doit*2;
+
+int Data[]={1,2,3,4,5,6,7,8,9,10};
+
+int main()
+{
+    for(auto &x : Data)
+    {
+       cout<<x<<" ";
+    }
+    cout<<name<<"\n";
+    cout<<age<<"\n";
+    cout<<weight<<"\n";
+    cout<<Alive<<"\n";
+    cout<<size<<"\n";
+    cout<<numtriangles<<"\n";
+
+}
