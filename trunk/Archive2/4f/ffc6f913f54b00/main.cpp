bcde3d3923ef9169c279f605f3a7e50f627681d4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-28 12:23:43 +0000

    Update archive.

---
+++ trunk/Archive2/4f/ffc6f913f54b00/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void foo(std::size_t)
+{
+    std::cout << "foo(std::size_t)" << std::endl;
+}
+
+template<typename T>
+void foo(T)
+{
+    std::cout << "foo(T)" << std::endl;
+}
+
+int main()
+{
+    foo(42);
+}
