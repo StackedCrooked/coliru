190abb5237f4fcbc4c1f2a1988047ad523972397
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-28 12:26:19 +0000

    Update archive.

---
+++ Archive2/72/52820c65c9a4c0/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <functional>
+#include <map>
+#include <array>
+#include <algorithm>
+
+int main() {
+    std::vector<int> x {1, 2, 3};
+    std::array<int, 3> y;
+    std::copy(x.begin(), x.end(), y.begin());
+    
+    for (auto i : y) std::cout << i;
+}
