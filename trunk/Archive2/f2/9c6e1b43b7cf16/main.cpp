5fbb0c9c77230010c63027a1a032062be0af1a95
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-28 12:35:45 +0000

    Update archive.

---
+++ trunk/Archive2/f2/9c6e1b43b7cf16/main.cpp
@@ -0,0 +1,8 @@
+int main()
+{
+    int* array[3];// = { 1, 2, 3 } ;
+
+    //int* pointer = array ;
+
+    array = pointer ;
+}
---
+++ trunk/Archive2/f2/b6aeef69315e3e/cmd.sh
@@ -0,0 +1,2 @@
+clang++ -std=c++11 -Wall -Wextra -Wno-trigraphs -pedantic-errors -O3 main.cpp 
+cat main.cpp | ./a.out
