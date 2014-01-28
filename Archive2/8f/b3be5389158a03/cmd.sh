679ccc3915ab26ce2e50b0c132278028659a283c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-28 12:28:43 +0000

    Update archive.

---
+++ Archive2/8f/b3be5389158a03/cmd.sh
@@ -0,0 +1,3 @@
+ln -s main.cpp main.c
+echo c89 && gcc -std=c89 -Wall -Wextra -pedantic-errors -c main.c && echo -e 'done!\n\n'
+echo c99 && gcc -std=c99 -Wall -Wextra -pedantic-errors -c main.c && echo done!
