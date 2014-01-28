c68c7032c31aff24c6496671717f971e5ea0447d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-28 12:26:34 +0000

    Update archive.

---
+++ trunk/Archive2/75/14771ccc6626e4/main.cpp
@@ -0,0 +1,18 @@
+#include <array>
+#include <iostream>
+
+using namespace std;
+
+typedef struct{
+ int i;
+ char a;
+}mystruct;
+
+int main()
+{
+    std::array<mystruct, 2> structure = {};
+    structure[0].i = 69;
+    cout << structure[0].i << endl; // Should print 69
+    structure = {};
+    cout << structure[0].i << endl; //Should print 0
+}
