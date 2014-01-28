485efa17852b82f1f89eee45196cdee920bc2f60
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-28 12:34:25 +0000

    Update archive.

---
+++ trunk/Archive2/de/e7d70435a13aff/main.cpp
@@ -0,0 +1,42 @@
+#include "json.hpp"
+#include <vector>
+
+struct X {
+    static X from_json(JSON::Value const& v);
+
+    std::string var1;
+    double      var2;
+};
+
+int main()
+{
+    auto doc = as_object(JSON::parse(
+        "{\n"
+        "    // some values\n"
+        "    \"mykey\": [\n"
+        "        {\n"
+        "            \"var1\": \"value1_str\",\n"
+        "            \"var2\" : 3.14\n"
+        "        }\n"
+        "    ]\n"
+        "    // some other values\n"
+        "}\n"
+        ));
+
+    if (doc.has_key("mykey"))
+    {
+        X data = X::from_json(doc["mykey"]);
+        std::cout << "X.var1: " << data.var1 << "\n";
+        std::cout << "X.var2: " << data.var2 << "\n";
+    }
+}
+
+X X::from_json(JSON::Value const& v)
+{
+    X result;
+    auto& o = as_object(as_array(v)[0]);
+    result.var1 = as_string(o["var1"]);
+    result.var2 = as_double(o["var2"]);
+
+    return result;
+}
