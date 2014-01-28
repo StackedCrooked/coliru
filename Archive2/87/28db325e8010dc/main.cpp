9327e9218cf757882c0249e556ea1663bb489d33
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-28 12:28:10 +0000

    Update archive.

---
+++ Archive2/87/28db325e8010dc/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+template <typename T>
+T Add (T t1, T t2)
+{
+    std::cout << "<typename T>" << std::endl ;
+    return t1 + t2 ;
+}
+
+template <int>
+int Add (int n1, int n2)
+{
+    std::cout << "<int>" << std::endl ;
+    return n1 + n2 ;
+}
+
+template <>
+int Add (int n1, int n2)
+{
+    std::cout << "<>" << std::endl ;
+    return n1 + n2 ;
+}
+
+int main (void) 
+{
+    Add (5, 4) ;
+    Add <int> (5, 4) ;
+    Add <> (5, 4) ;
+    Add <42> (5, 4) ;
+
+    return 0 ;
+}
