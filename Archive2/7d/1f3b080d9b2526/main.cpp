fbccfa8bcc18d677f884ac43b61c7d7fd5c2d0b3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-01-28 12:27:27 +0000

    Update archive.

---
+++ Archive2/7d/1f3b080d9b2526/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <functional>
+using namespace std;
+
+class VmapPlayer
+{
+public:
+    void startPlayback();
+    void playAdBreak(int adBreak, void (VmapPlayer::*callback)());
+    void playSingleAd(int ad, std::function<void (VmapPlayer&)>);
+};
+
+void VmapPlayer::playSingleAd(int, std::function<void (VmapPlayer&)> callback)
+{
+    cout << "i am here" << endl;
+    callback(*this);
+}
+
+void VmapPlayer::playAdBreak(int adBreak, void (VmapPlayer::*callback)())
+{
+    using namespace std::placeholders;
+    auto cb = std::bind(std::mem_fn(&VmapPlayer::playAdBreak), _1, adBreak, callback);
+    playSingleAd(123, cb);
+}
+
+void VmapPlayer::startPlayback()
+{
+    playAdBreak(456, &VmapPlayer::startPlayback);
+}
+
+int main()
+{
+    VmapPlayer p;
+    // p.startPlayback(); // <--- INFINITE RECURSION!!
+}
