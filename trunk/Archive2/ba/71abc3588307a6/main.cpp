0b56c79b9dc4a6596230f07930b4c7da20768fa7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-03 05:57:59 +0000

    Updating archive.

---
+++ trunk/Archive2/ba/71abc3588307a6/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sstream>
+#include <utility>
+#include <map>
+
+std::stringstream in("1 2 2 5 3 3 5
+                      9 8 5 1 4 2 9 ");
+ 
+int main()
+{
+    std::map<std::pair<int, int>, int> pairToCount;
+    int a, b;
+
+    while (in >> a >> b)
+    {
+        auto it = pairToCount.find(std::make_pair(b, a));
+        
+        ++pairToCount[
+            (it != pairToCount.end()) ? std::make_pair(b, a)
+                                      : std::make_pair(a, b)];
+    }
+    
+    for (const auto& p : pairToCount)
+    {
+        std::cout << "(" << p.first.first << " " << p.first.second << ") = "
+                                                 << p.second << '\n';
+    }
+}
