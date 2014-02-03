dc172924997e60150f25dade0762e3b1fd04ba2c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-03 17:17:07 +0000

    Updating archive.

---
+++ trunk/Archive2/2b/cea2f8abb9a029/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <queue>
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <functional>
+    
+struct ThreadPool {
+    
+    struct Task {
+        virtual void Run() const;
+        virtual ~Task() {};
+    };   
+
+    template < typename task_, typename... args_ >
+    struct RealTask : public Task {
+        RealTask( task_&& task, args_&&... args ) : fun_( std::bind( std::forward<task_>(task), std::forward<args_>(args)... ) ) {}
+        void Run() const override {
+            fun_();
+        }
+    private:
+        decltype( std::bind(std::declval<task_>(), std::declval<args_>()... ) ) fun_;
+    };
+    
+    template < typename task_, typename... args_ >
+    void AddTask( task_&& task, args_&&... args ) {
+        auto lock = std::unique_lock<std::mutex>{mtx_};
+        using FinalTask = RealTask<task_, args_... >;
+        q_.push( std::unique_ptr<Task>( new FinalTask( std::forward<task_>(task), std::forward<args_>(args)... ) ) );
+    }
+    
+    ThreadPool() {
+        for( auto & t : pool_ )
+            t = std::thread( [=] {
+                while ( true ) {
+                    std::unique_ptr<Task> task;
+                    {
+                        auto lock = std::unique_lock<std::mutex>{mtx_};
+                        if ( q_.empty() && stop_ ) 
+                            break;
+                        if ( q_.empty() )
+                            continue;
+                        task = std::move(q_.front());
+                        q_.pop();
+                    }
+                    if (task)
+                        task->Run();
+                }
+            } );
+    }
+    
+    ~ThreadPool() {
+        stop_ = true;
+        for( auto & t : pool_ )
+            t.join();
+    }
+    
+private:
+    std::queue<std::unique_ptr<Task>> q_;
+    std::thread pool_[8]; 
+    std::mutex mtx_;
+    volatile bool stop_ {};
+};
+
+void foo( int a, int b ) {
+    std::cout << a << "." << b;
+}
+
+ThreadPool pool;
+
+int main() {
+    for( int i{}; i!=314; ++i )
+        pool.AddTask( foo, 3, 14 );    
+}
