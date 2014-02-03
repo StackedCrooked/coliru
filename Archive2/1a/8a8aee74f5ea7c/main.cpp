4c638b69c4886ccf8d2f2c91551b53b7deb200cf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-03 05:28:17 +0000

    Updating archive.

---
+++ Archive2/1a/8a8aee74f5ea7c/main.cpp
@@ -0,0 +1,29 @@
+    #include <iostream>
+     
+    template<template<typename...> class C, typename... T>
+    struct is_valid_specialization {
+    typedef struct { char _; } yes;
+    typedef struct { yes _[2]; } no;
+     
+    template<template<typename...> class D, typename... U>
+    static yes test(D<U...>*);
+    /*template<template<typename...> class D, typename... U>
+    static no test(...);*/
+     
+    constexpr static bool value = (sizeof(test<C, T...>(0)) == sizeof(yes));
+    };
+     
+    template<typename T>
+    struct Test1 { };
+     
+    template<typename T1, typename T2>
+    struct Test2 { };
+     
+    template<typename...>
+    struct TestV { };
+     
+    int main() {
+    //std::cout << "Test1<T>: " << is_valid_specialization<Test1, int>::value << std::endl;
+    std::cout << "Test2<T>: " << is_valid_specialization<Test2, int, int>::value << std::endl;
+    //std::cout << "TestV<T>: " << is_valid_specialization<TestV, int>::value << std::endl;
+    }
