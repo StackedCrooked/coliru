8d44a9e0d53aeb8b091959e0ac6c4f56fd29d8c3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-03 05:53:18 +0000

    Updating archive.

---
+++ trunk/Archive2/74/28ade88cc3b0b7/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+    int arr[4] = {1,2,3,4};
+    int arr2[4] = {5,6,7,8};
+    int **arrPtr = new int*[2];
+
+    arrPtr[0] = arr;
+    arrPtr[1] = arr2;
+    typedef int* (&arrPtrRealType)[2];
+    typedef int (&arrRealType)[4];
+
+    for (int *a : (arrPtrRealType)(arrPtr)){
+        for (int i : (arrRealType)(a)){
+            cout << i << endl;
+        }
+    }
+}
