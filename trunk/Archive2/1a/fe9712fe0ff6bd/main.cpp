af0c65ee2cdb971784563fa6a2e694a9300fcea0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-03 05:28:17 +0000

    Updating archive.

---
+++ trunk/Archive2/1a/fe9712fe0ff6bd/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::ostream;
+#include <string>
+using std::string;
+
+namespace ProjectIV
+{
+  class Monster
+  {
+  friend ostream &operator<< (ostream &out, const Monster &monster);
+  public:
+    Monster(int con);
+  private:
+    int con;
+  };
+} /* namespace ProjectIV */
+
+ostream& operator<< (ostream &out, const ProjectIV::Monster &monster)
+{
+  cout << "Constitution: " << monster.con << endl;
+
+  return out;
+}
+
+ProjectIV::Monster::Monster(int con): con(con)
+{}
+
+using namespace ProjectIV;
+
+int main()
+{
+  Monster Gojira(140);
+  cout << Gojira << endl;
+  return 0;
+}
