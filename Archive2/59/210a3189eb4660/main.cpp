cd55e2aa52dd38840854183c070fa9abeed336e2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-03 19:39:41 +0000

    Updating archive.

---
+++ Archive2/59/210a3189eb4660/main.cpp
@@ -0,0 +1,31 @@
+#include <cstdint>
+#include <cassert>
+#include <iostream>
+int f() { return 42; }
+int main()
+{
+    int i = 7;
+ 
+    // pointer to integer and back
+    uintptr_t v1 = reinterpret_cast<uintptr_t>(&i); // static_cast is an error
+    std::cout << "The value of &i is 0x" << std::hex << v1 << '\n';
+    int* p1 = reinterpret_cast<int*>(v1);
+    assert(p1 == &i);
+ 
+    // pointer to function to another and back
+    void(*fp1)() = reinterpret_cast<void(*)()>(f);
+    // fp1(); undefined behavior
+    int(*fp2)() = reinterpret_cast<int(*)()>(fp1);
+    std::cout << std::dec << fp2() << '\n'; // safe
+ 
+    // type aliasing through pointer
+    char* p2 = reinterpret_cast<char*>(&i);
+    if(p2[0] == '\x7')
+        std::cout << "This system is little-endian\n";
+    else
+        std::cout << "This system is big-endian\n";
+ 
+    // type aliasing through reference
+    reinterpret_cast<unsigned int&>(i) = 42;
+    std::cout << i << '\n';
+}
