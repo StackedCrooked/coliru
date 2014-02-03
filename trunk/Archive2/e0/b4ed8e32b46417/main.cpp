e98ac85ccc9cb840cb0c00a06b413d1fc88ca95b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-03 07:10:10 +0000

    Updating archive.

---
+++ trunk/Archive2/e0/b4ed8e32b46417/main.cpp
@@ -0,0 +1,99 @@
+
+#include <iostream>
+
+
+struct A;
+struct B;
+struct C;
+
+
+void Dispatch(const B*, const B*);
+void Dispatch(const C*, const C*);
+void Dispatch(const B*, const C*);
+void Dispatch(const C*, const B*);
+void Dispatch(const A*, const A*);
+
+
+struct IDispatcher
+{
+    virtual ~IDispatcher() {}
+    virtual void Dispatch(const B* b) const = 0;
+    virtual void Dispatch(const C* c) const = 0;
+};
+
+template<class T>
+struct Dispatcher : public IDispatcher
+{
+private:
+    const T* t;
+
+public:
+    Dispatcher(const T* _t) : t(_t) {}
+
+    void Dispatch(const B* b) const {
+        return ::Dispatch(t, b);
+    }
+
+    void Dispatch(const C* c) const {
+        return ::Dispatch(t, c);
+    }
+};
+
+
+struct A
+{
+    virtual ~A() {}
+    virtual void Dispatch(const A*) const = 0;
+    virtual void Dispatch(const IDispatcher*) const = 0;
+};
+
+struct B : public A
+{
+    void Dispatch(const A* other) const {
+        Dispatcher<B> disp(this);
+        return other->Dispatch(&disp);
+    }
+    void Dispatch(const IDispatcher* disp) const {
+        return disp->Dispatch(this);
+    }
+};
+
+struct C : public A
+{
+    void Dispatch(const A* other) const {
+        Dispatcher<C> disp(this);
+        return other->Dispatch(&disp);
+    }
+    void Dispatch(const IDispatcher* disp) const {
+        disp->Dispatch(this);
+    }
+};
+
+void Dispatch(const B*, const B*) {
+    std::cout << "B,B\n";
+}
+
+void Dispatch(const C*, const C*) {
+    std::cout << "C,C\n";
+}
+
+void Dispatch(const B*, const C*) {
+    std::cout << "B,C\n";
+}
+
+void Dispatch(const C* c, const B* b) {
+    return Dispatch(b, c);
+}
+
+void Dispatch(const A* a1, const A* a2) {
+    return a1->Dispatch(a2);
+}
+
+
+int main()
+{
+    B b;
+    C c;
+    Dispatch( (A*)&b, (A*)&c );
+    return 0;
+}
