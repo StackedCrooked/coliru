07800d987deca7f7f3b54c434421f2db8311080f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-03 05:55:41 +0000

    Updating archive.

---
+++ trunk/Archive2/9b/08e9ebb9ecfe4d/main.cpp
@@ -0,0 +1,32 @@
+#include <type_traits>
+#include <utility>
+
+struct X
+{
+    constexpr void fun() {}             // OK
+    constexpr auto gun() {}             // OK
+              auto hun() {}             // OK
+    constexpr auto iun() { return 0; }  // OK
+};
+
+template<class T>
+struct Y
+{
+    constexpr void fun() {}             // OK
+    //constexpr auto gun() {}             // ERROR, why?
+              auto hun() {}             // OK
+    constexpr auto iun() { return 0; }  // OK
+};
+
+int main() 
+{
+    static_assert(std::is_same<void, decltype(std::declval<X>().fun())>::value, "");    
+    static_assert(std::is_same<void, decltype(std::declval<X>().gun())>::value, "");    
+    static_assert(std::is_same<void, decltype(std::declval<X>().hun())>::value, "");    
+    static_assert(std::is_same<int , decltype(std::declval<X>().iun())>::value, "");    
+
+    static_assert(std::is_same<void, decltype(std::declval<Y<X>>().fun())>::value, "");    
+    //static_assert(std::is_same<void, decltype(std::declval<Y<X>>().gun())>::value, "");    
+    static_assert(std::is_same<void, decltype(std::declval<Y<X>>().hun())>::value, "");    
+    static_assert(std::is_same<int , decltype(std::declval<Y<X>>().iun())>::value, "");    
+}
