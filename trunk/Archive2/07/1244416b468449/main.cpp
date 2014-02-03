7d479c070455936bc408ff03a1395fcaaacaba90
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-03 05:27:11 +0000

    Updating archive.

---
+++ trunk/Archive2/07/1244416b468449/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+
+int main()
+{
+    std::istringstream iss(R"(<?xml version="1.0" encoding="utf-8"?>
+<tokens title="issues"></tokens>)");
+
+    using boost::property_tree::ptree;
+    ptree pt;
+    read_xml(iss,pt);
+    
+    ptree& token_attrs = pt.get_child("tokens.<xmlattr>");
+    std::cout << token_attrs.erase("title") << std::endl;
+    
+    //ptree& token = pt.get_child("tokens");
+    //std::cout << token.erase("<xmlattr>") << std::endl;
+    
+    write_xml(std::cout,pt);
+
+    return 0;
+}
+
+
---
+++ trunk/Archive2/07/2061908c978a7e/cmd.sh
@@ -0,0 +1,1 @@
+clang++ -std=c++11 -Ofast -march=native -Wall -Wextra -pedantic -pthread main.cpp && ./a.out
