9c363c1fc875cb492aa41508e362085faaa4a060
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-03 05:55:23 +0000

    Updating archive.

---
+++ Archive2/97/87506d7de896f1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+int main()
+{
+    unsigned long long line_cnt = 0 ;
+    unsigned long long word_cnt = 0 ;
+    unsigned long long byte_cnt = 0 ;
+    
+    std::string line ;
+    while( std::getline( std::cin, line ) )
+    {
+        ++line_cnt ;
+        byte_cnt += line.size() + 1 ;
+        std::istringstream stm(line) ;
+        std::string word ;
+        while( stm >> word ) ++word_cnt ;
+    }
+    
+    int width = 8 + ( byte_cnt > 9999999 ? 8 : 0 ) ;
+    std::cout << line_cnt << std::setw(width)  << word_cnt << std::setw(width) << byte_cnt << '\n' ;
+}
