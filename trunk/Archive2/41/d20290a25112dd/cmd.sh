2688954d8c03a2f35389e135f55a1e88f8f6941f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-03 07:04:47 +0000

    Updating archive.

---
+++ trunk/Archive2/41/d20290a25112dd/cmd.sh
@@ -0,0 +1,10 @@
+echo ..........................
+echo -e '\n\nstandard c++ (-std=c++11)\n------------------------------'
+g++-4.8 -v -std=c++11 -Wall -Wextra -pedantic-errors main.cpp && echo 'g++ ok'
+clang++ -std=c++11 -stdlib=libc++ -Wall -Wextra -pedantic-errors main.cpp -lsupc++ && echo 'clang++ ok.'
+echo -e '\nlegacy standard c++ (-std=c++98)\n------------------------------'
+g++-4.8 -std=c++98 -Wall -Wextra -pedantic-errors main.cpp && echo 'g++ ok'
+clang++ -std=c++98 -stdlib=libc++ -Wall -Wextra -pedantic-errors main.cpp -lsupc++ && echo 'clang++ ok.'
+echo -e '\nnon-standard (g++/clang++ default)\n------------------------------'
+g++-4.8 -Wall -Wextra -pedantic-errors main.cpp && echo 'g++ ok'
+clang++ -stdlib=libc++ -Wall -Wextra -pedantic-errors main.cpp -lsupc++ && echo 'clang++ ok.'
