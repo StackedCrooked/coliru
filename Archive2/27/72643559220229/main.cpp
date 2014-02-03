d5876326c5b32bfbb53a7dbbcdce2542f8093391
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-03 20:44:13 +0000

    Updating archive.

---
+++ Archive2/27/72643559220229/main.cpp
@@ -0,0 +1,31 @@
+
+
+
+
+#include <atomic>
+#include <thread>
+
+
+class spinlock_mutex {
+  std::atomic_flag flag;
+
+public:
+  spinlock_mutex() : flag(ATOMIC_FLAG_INIT) {}
+
+  void lock() {
+    while (flag.test_and_set(std::memory_order_acq_rel)) ;
+  }
+
+  void unlock() {
+   flag.clear(std::memory_order_release);
+  }
+};
+
+
+spinlock_mutex mtx;
+
+
+int main()
+{
+    mtx.lock();
+}
