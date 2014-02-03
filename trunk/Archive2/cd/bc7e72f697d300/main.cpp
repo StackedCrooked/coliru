a61dd55fa2e8a35add79cbcc288fa02012f503d4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-03 07:08:40 +0000

    Updating archive.

---
+++ trunk/Archive2/cd/bc7e72f697d300/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template<typename T>
+struct tuple {
+    template<std::size_t N>
+    int get() const {
+        return N;
+    }
+};
+
+template<typename T>
+int test(tuple<T>& x) {
+    return x.get<1>();
+}
+
+int main() {
+    tuple<int> x;
+    test(x);
+}
