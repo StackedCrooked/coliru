b7f5bbe506739f35eeb2cb2b051368be0d355f1e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-03 05:58:11 +0000

    Updating archive.

---
+++ Archive2/bc/4af87525057a00/cmd.sh
@@ -0,0 +1,2 @@
+clang++ -std=c++11 -stdlib=libc++ -Wall -Wextra -pedantic-errors main.cpp -lsupc++ && ./a.out
+clang++ -DERRORS -std=c++11 -stdlib=libc++ -Wall -Wextra -pedantic-errors main.cpp -lsupc++ && ./a.out
