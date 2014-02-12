1fba3b099ad6f36821d7f3a02092e617c06a2c00
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-12 21:02:36 +0000

    Update 92

---
+++ Archive2/92/2bf72a8225c05b/main.cpp
@@ -0,0 +1,29 @@
+#include <cstddef>
+#include <iostream>
+#include <string>
+
+void test ( std::string binary, std::string expected, uint32_t boy ) {
+    union {
+        uint32_t u;
+        float cast;
+    } cow{boy};
+    std::cout << binary << " == " << expected << " == " << cow.cast << std::endl;
+}
+
+int main() {
+    test( "11101.1010", 
+    "29.625", 
+    0b01000001111011010000000000000000 );
+    test( "-11101.1010", 
+    "-22.75", 
+    0b11000001101101100000000000000000 );
+    test( "0.001", 
+    "0.125", 
+    0b00111110000000000000000000000000 );
+    test( "100011", 
+    "35", 
+    0b01000010000011000000000000000000 );
+    test( "-1.0011", 
+    "-1.1875", 
+    0b10111111100110000000000000000000 );
+}
