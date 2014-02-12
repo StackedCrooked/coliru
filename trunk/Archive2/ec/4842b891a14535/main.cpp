cbcaceb4d402589aa8686d70ed8f3a1f02460ddf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-12 21:23:29 +0000

    Update ec

---
+++ trunk/Archive2/ec/4842b891a14535/main.cpp
@@ -0,0 +1,37 @@
+#include <functional>
+#include <iostream>
+#include <string>
+ 
+struct Employee {
+  std::string name;
+  unsigned int ID;
+};
+ 
+namespace std {
+template <>
+class hash<Employee> {
+ public:
+  size_t operator()(const Employee &employee) 
+  {
+    // computes the hash of an employee using a variant 
+    // of the Fowler-Noll-Vo hash function
+    size_t result = 2166136261;
+ 
+    for (size_t i = 0, ie = employee.name.size(); i != ie; ++i) {
+      result = (result * 16777619) ^ employee.name[i];
+    }
+ 
+    return result ^ (employee.ID << 1);
+  }
+};
+}
+ 
+int main()
+{
+  Employee employee;
+  employee.name = "Zaphod Beeblebrox";
+  employee.ID = 42;
+ 
+  std::hash<Employee> const hash_fn;
+  std::cout << hash_fn(employee) << '\n';
+}
