d40d38a5914b4e7d094baebe4f1da1700d5c2858
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-12 20:48:41 +0000

    Update 57

---
+++ trunk/Archive2/57/b7d5e003b80192/main.cpp
@@ -0,0 +1,59 @@
+#include <vector>
+#include <iostream>
+
+class B
+{
+public:
+    B() : x(0) {}
+    B(const B &r) : x(r.x) { std::cout << "Copied" << std::endl; }
+    B(B&& r) : x(r.x) { std::cout << "Moved" << std::endl; r.x = 0; }
+    B& operator =(B r) { x = std::move(r.x); return *this; }
+
+    explicit B(int x) : x(x) {}
+    int get() const { return x; }
+    
+private:
+    int x;
+};
+
+class C
+{
+public:
+    template<typename ...T>
+    C(T&& ...x)
+    {
+        add(std::forward<T>(x)...);
+    }
+    
+    template<typename T>
+    C(std::vector<T>& x)
+    {
+        std::cout << "Vector! " << x.size() << std::endl;
+    }
+    
+    template<typename T>
+    C(std::vector<T>&& x)
+    {
+        std::cout << "RValue Vector! " << x.size() << std::endl;
+    }
+    
+    void add() {}
+    
+    template<typename T>
+    void add(T r) { std::cout << r.get() << std::endl; }
+    
+    template<typename T, typename ...Rest>
+    void add(T&& r, Rest ...s) {
+        std::cout << r.get() << std::endl;
+        add(std::forward<Rest>(s)...);
+    }
+};
+
+int main()
+{
+    std::vector<int> v;
+    
+    C a(B(1), B(2), B(3));
+    C b(v);
+    C c(std::vector<int>(10));
+}
