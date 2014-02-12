467991147754c2f7dca7653ce45b4221dfb9484e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-12 21:11:36 +0000

    Update b8

---
+++ trunk/Archive2/b8/6276e05bc97b66/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    string filename;
+    cout << "Enter the filename: ";
+    cin >> filename;
+    ifstream myfile(filename);
+}
---
+++ trunk/Archive2/b8/d947c3ef0e5bef/cmd.sh
@@ -0,0 +1,1 @@
+clang++ -std=c++1y -O3 -Wall -Wextra -pedantic-errors -pthread main.cpp && (echo "1 1 1"; echo "0 0 0") | ./a.out
