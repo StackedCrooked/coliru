86da4c2ea1d73ca05b14045b1fd0a60dedddfa83
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-12 21:03:19 +0000

    Update 95

---
+++ trunk/Archive2/95/685bb86b5b81e7/cmd.sh
@@ -0,0 +1,3 @@
+g++-4.8 --version
+g++-4.8 -std=c++11 -Wall -Wextra -pedantic -fno-strict-aliasing -fwrapv  main.cpp && ./a.out
+##clang++ -std=c++11 -O2 -Wall -Wextra -pedantic  main.cpp && ./a.out
