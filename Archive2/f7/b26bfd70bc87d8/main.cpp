f357a0747bb409e8916014ebb21980506d0bb0cf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-12 21:26:03 +0000

    Update f7

---
+++ Archive2/f7/b26bfd70bc87d8/main.cpp
@@ -0,0 +1,30 @@
+/*
+ *          File: manager.h
+ *        Author: Robert I. Pitts <rip@cs.bu.edu>
+ * Last Modified: April 8, 2000
+ *         Topic: Introduction to Inheritance in C++
+ * ----------------------------------------------------------------
+ *
+ * Manager class definition.
+ */
+
+#ifndef _MANAGER_H
+#define _MANAGER_H
+
+#include "employee.h"
+
+class Manager : public Employee {
+public:
+  Manager(string theName,
+          float thePayRate,
+          bool isSalaried);
+
+  bool getSalaried() const;
+
+  float pay(float hoursWorked) const;
+
+protected:
+  bool salaried;
+};
+
+#endif /* not defined _MANAGER_H */
