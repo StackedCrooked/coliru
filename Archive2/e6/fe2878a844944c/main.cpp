36d6d55404dc32568298277faca31e526479f189
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-12 21:22:16 +0000

    Update e6

---
+++ Archive2/e6/fe2878a844944c/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
+
+void inc( int&& x )
+{
+    std::cout << "on entry to inc() -            address: "
+               << std::addressof(x) << " value: " << x << '\n' ;
+    ++x ;
+    std::cout << "just before exiti from inc() - address: "
+               << std::addressof(x) << " value: " << x << '\n' ;
+}
+
+int main()
+{
+    int&& ri = 23 ;
+    std::string str = "abc" ;
+    std::string&& rs = str + "efgh" ;
+
+    std::cout << "address: " << std::addressof(ri) << " value: " << ri << '\n'
+               << "address: " << std::addressof(rs) << " value: " << rs << '\n' ;
+
+    ri += 65 ;
+    rs += "ijklmnop" ;
+
+    std::cout << "address: " << std::addressof(ri) << " value: " << ri << '\n'
+               << "address: " << std::addressof(rs) << " value: " << rs << '\n' ;
+
+    inc(99) ;
+}
