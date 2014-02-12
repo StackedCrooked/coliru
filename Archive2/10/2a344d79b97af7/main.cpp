ac08f8dc2cf3579d54f2efbeafd87b614b5f417e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-12 20:32:48 +0000

    Update 10

---
+++ Archive2/10/2a344d79b97af7/main.cpp
@@ -0,0 +1,7 @@
+#include <map>
+
+enum class A { a, b, c};
+
+std::map<A, int> test = {{A::a,1},{A::b,2},{A::c,3}};
+
+int main(){}
---
+++ Archive2/10/db3c63de0a324b/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+struct aClass { std::string a, b ; double d ; };
+
+int main()
+{
+    std::map< std::string, std::map< std::string, aClass > > myMap = 
+    {
+        {
+            "abc",
+            {
+                { "defg", { "one", "two", 1.2 } },
+                { "hijk", { "three", "four", 3.4 } },
+                { "lmno", { "five", "six", 5.6 } }
+            }
+        },
+        
+        {
+            "pqr",
+            {
+                { "stuv", { "seven", "eight", 7.8 } },
+                { "wxyz", { "nine", "zero", 9.0 } }
+            }
+        }
+    };
+    
+    // iterate using range-based loop
+    {
+        for( const auto& sm_pair : myMap )
+        {
+            std::cout << sm_pair.first << '\n' ;
+            for( const auto& sc_pair : sm_pair.second )
+            {
+                std::cout << "     " << sc_pair.first << '{' << sc_pair.second.a << ',' 
+                          << sc_pair.second.b << ',' << sc_pair.second.d << "}\n" ;    
+            }
+        }
+    }
+    
+    std::cout << "\n----------------------\n\n" ;
+    
+    // iterate C++98 style
+    {
+        typedef std::map< std::string, std::map< std::string, aClass > >::iterator outer_iterator ;
+        typedef std::map< std::string, aClass >::iterator inner_iterator ;
+        
+        for( outer_iterator outer = myMap.begin() ; outer != myMap.end() ; ++outer )
+        {
+            std::cout << outer->first << '\n' ;
+            for( inner_iterator inner = outer->second.begin() ; inner != outer->second.end() ; ++inner )
+            {
+                std::cout << "     " << inner->first << '{' << inner->second.a << ',' 
+                          << inner->second.b << ',' << inner->second.d << "}\n" ;    
+            }
+        }
+    }
+}
+
