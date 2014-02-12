374fa1d5a8a8fbee98f7c27a35c5a8d83c979985
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-12 21:23:17 +0000

    Update eb

---
+++ Archive2/eb/ba809343287fda/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+ 
+int main()
+{
+    std::vector<int> v { 3, 1, 4, 1, 5, 9 };
+ 
+    std::cout << "initially, v: ";
+    for (auto i : v) std::cout << i << ' ';
+    std::cout << '\n';
+ 
+    std::make_heap(v.begin(), v.end());
+ 
+    std::cout << "after make_heap, v: ";
+    for (auto i : v) std::cout << i << ' ';
+    std::cout << '\n';
+ 
+    std::pop_heap(v.begin(), v.end());
+    auto largest = v.back();
+    v.pop_back();
+    std::cout << "largest element: " << largest << '\n';
+ 
+    std::cout << "after removing the largest element, v: ";
+    for (auto i : v) std::cout << i << ' ';
+    std::cout << '\n';
+}
