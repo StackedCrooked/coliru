ea67067e64e0201fa3f93c74abc49e799509df37
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-12 20:52:24 +0000

    Update 67

---
+++ Archive2/67/df03b1a763f2f1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+
+
+int someFunctionIWantToTest(){ 
+  return 1;
+}
+
+int main()
+{
+std::cout << someFunctionIWantToTest() << std::endl;  
+}
