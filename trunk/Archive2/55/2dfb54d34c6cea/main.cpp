f477bb6eee4d5627d8c097d48131160ddd1fe4bf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-12 20:48:09 +0000

    Update 55

---
+++ trunk/Archive2/55/2dfb54d34c6cea/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    double d = 0.00000095;
+    double e = 0.95;
+    std::cout << std::setprecision(3);
+    std::cout.unsetf(std::ios::floatfield);
+    std::cout << "d = " << (100. * d) << "%\n";
+    std::cout << "e = " << (100. * e) << "%\n";
+    std::cout << std::fixed;
+    std::cout << "d = " << (100. * d) << "%\n";
+    std::cout << "e = " << (100. * e) << "%\n";
+    std::cout << std::scientific;
+    std::cout << "d = " << (100. * d) << "%\n";
+    std::cout << "e = " << (100. * e) << "%\n";
+}
