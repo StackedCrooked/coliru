3d95b96c7a5b862461f497712cce12fa945405cb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-17 19:28:08 +0000

    Updating archive.

---
+++ trunk/Archive2/a2/063255b85fb961/main.cpp
@@ -0,0 +1,60 @@
+// Outer/Inner block scopes examples + simplified declarations and vector addition.
+// Author: !ixAlastor2 ‮❤✨~oht omoh on srS~✨❤‮‭‭
+// Derived from: http://coliru.stacked-crooked.com/a/92c40af6be155c73
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std; //used the namespace thing so that I don't have to declare allll thooooseee vaaarrrsss
+
+int main() //vital for the entire thing to work~ 
+{
+    int x{10}, y{20};
+    int A{x*y};
+    cout << "\nOriginal outer scope values: \n" << "x: " << x << "\ny: " << y << "\nA: " << A << endl;
+    //So in this one I decided to expirement with shortening my declarations
+    //Turns out you can easily declare all the variables ya need with only one line! 
+    //So I figured why not exploit such a time/line saver :3c
+    //>tfw no more 20 lines of declaring integer variables.
+    {
+        int x{42}, y{77}; 
+        int A{x*y};
+        cout << "\nInner block scope values: \n" << "x: " << x << "\ny: " << y << "\nA: " << A << endl;
+        //same exact thing as the former, however with x and y being in the inner block
+        //to remain unaffected by the declarations in lines 12-14
+        {
+            // I decided to mess around with it even further
+            // and go back to some of the stuff I originally learned
+            vector<string> K{"\nC++ ", "also ", "how "}, 
+            Y{"is ", "I ", "to "}, 
+            P{"getting ", "finally ", "stream "}, 
+            L{"pretty ", "figured ", "and"}, 
+            S{"easy.", "out", " such \n"}, 
+            Kden(3);
+            /*Turns out just like you can simplify your declarations with similar type variables
+            so in this case, they were all strings so I decided to only use on declaration to create
+            a complex statement*/
+            for(int idk = 0; idk < 3; ++idk){
+                Kden[idk] = K[idk] +Y[idk] +P[idk] +L[idk] +S[idk];
+                cout << Kden[idk] << endl;
+            }
+        }
+    }//I decided instead of doing a final outer scope thing, to go back to integer array addition
+    // declaring sz and datsumdoe was kind of tricky at first because it wasn't something I looked at with depth
+    // originally, however I kind of get it, I must work on it further
+    vector<int> v1{1, 2, 3, 4, 5, 1, 2};
+    vector<int> v2{5, 4, 3, 2, 1};
+    vector<int> vBig{0};
+    if(v1.size() > v2.size())
+        vBig = v1;
+    else 
+        vBig = v2;
+    const size_t sz{vBig.size()};
+    //ya, ya, I know, I was determining the size based only on v1, which meant I could change
+    //the length of v2 and it would not even recognize it. oh well~
+    //I just don't really know who ask for v1&&v1 or eh, v1||v2 even.
+    vector<int> datsumdoe(sz);
+    for(size_t idy = 0; idy < sz; ++idy){
+        datsumdoe[idy] = v1[idy] + v2[idy];
+        cout << datsumdoe[idy] << endl;
+    }
+}
