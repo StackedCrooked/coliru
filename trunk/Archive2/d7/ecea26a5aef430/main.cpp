fcb4d22090660d41e038b5d50ac5464400c084cb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-17 19:32:28 +0000

    Updating archive.

---
+++ trunk/Archive2/d7/ecea26a5aef430/main.cpp
@@ -0,0 +1,35 @@
+#include <list>
+#include <map>
+#include <typeindex>
+#include <type_traits>
+#include <iostream>
+#include <iomanip>
+
+struct UndeadMonster { virtual ~UndeadMonster() = default ; /* ... */ };
+struct Skeleton : UndeadMonster { /* ... */ } ;
+struct Zombie : UndeadMonster { /* ... */ } ;
+struct Ghoul : UndeadMonster { /* ... */ } ;
+
+std::map< std::type_index, std::list<UndeadMonster*> > lists ;
+
+void foo( UndeadMonster* monster )
+{ lists[ std::type_index( typeid(*monster) ) ].push_back(monster) ; }
+
+int main()
+{
+    UndeadMonster* monsters[] = 
+    { 
+        new Zombie, new Skeleton, new Ghoul, new Skeleton, new Zombie, new Ghoul, 
+        new Zombie, new Ghoul, new Zombie, new Skeleton, new Ghoul, new Zombie 
+    } ;
+    for( auto ptr : monsters ) foo(ptr) ;
+    
+    // iteratoe through the lists
+    std::cout << "lists\n--------------\n" ;
+    for( const auto& pair : lists )
+    {
+        std::cout << std::setw(12) << pair.first.name() << "  -  " ;
+        for( auto p : pair.second ) std::cout << p << "  " ;
+        std::cout << '\n' ;
+    }
+}
