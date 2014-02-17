4dd80083d8002156f6ea636e6bedbedc173f9586
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-17 19:33:18 +0000

    Updating archive.

---
+++ trunk/Archive2/e0/97f29d459c1d7f/main.cpp
@@ -0,0 +1,34 @@
+// Outer/Inner block scopes example.
+
+// Derived from: http://www.cplusplus.com/doc/tutorial/namespaces/
+
+#include <iostream>
+using namespace std;
+
+int main () 
+{
+  // Initialize two int variables in this outer scope of main().
+  int x{10};
+  int y{20};
+  
+  cout << "\n Original outer scope values; \n";
+  cout << "x: " << x << endl;
+  cout << "y: " << y << endl;
+  
+  { // Inner block's scope begins here.
+      
+    int x{50}; // Another (different) 'x' variable's initialization is ok, 
+               //   since we're inside main()'s inner block here.
+    y = 50;    // Alos, assign a new value to outer scope's 'y'.
+    
+    cout << "\n Inner scope values; \n";
+    cout << "x: " << x << endl;
+    cout << "y: " << y << endl;
+    
+  } // Inner block's scope ends here, so does the lifetime of inner 'x'.
+  
+  cout << "\n Final outer scope values; \n";
+  cout << "x: " << x << endl;
+  cout << "y: " << y << endl;
+  
+}
