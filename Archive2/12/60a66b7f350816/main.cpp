5643037deef6130cb2f23692decbfb635c9d53a2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-17 19:15:30 +0000

    Updating archive.

---
+++ Archive2/12/60a66b7f350816/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <unordered_map>
+
+struct Point
+{
+  Point(int px, int py) : x(px), y(py) {}
+  int x,y;
+
+  bool operator==(const Point &other) const
+  { return (x == other.x
+            && y == other.y);
+  }
+};
+
+namespace std {
+  template <>
+  struct hash<Point>
+  {
+    std::size_t operator()(const Point& k) const
+    {
+      return (k.x + k.y);
+    }
+  };
+}
+int main()
+{
+   std::unordered_map<Point,std::string> mmap = {
+    { {0,1}, "example"},
+    { {1,0}, "another"}
+   };   
+   std::cout << mmap.size() << std::endl;   // outputs 2
+   
+   std::unordered_map<Point,std::string> mmap2 = {
+    { {0,1}, "example"},
+    { {0,1}, "another"}
+   };   
+   std::cout << mmap2.size() << std::endl;   // outputs 1
+   return 0;
+}
