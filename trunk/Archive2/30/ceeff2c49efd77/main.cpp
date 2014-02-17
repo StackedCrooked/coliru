6d14cf09877470307666926a4d0790e9fae0aa36
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-17 19:18:08 +0000

    Updating archive.

---
+++ trunk/Archive2/30/ceeff2c49efd77/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+void grow( std::vector<int>& vec, std::size_t n )
+{
+    std::cout << "initial size: " << std::setw(2) << vec.size() 
+              << " capacity: " << std::setw(2) << vec.capacity() << '\n' ;
+              
+    auto buffer = vec.data() ;
+    
+    for( std::size_t i = 0 ; i < n ; ++i )
+    {
+        vec.push_back(i) ;
+        
+        if( buffer != vec.data() )
+        {
+            std::cout << "memory was reallocated.  size: " << std::setw(2) << vec.size() 
+                      << " capacity: " << std::setw(2) << vec.capacity() << '\n' ;
+                      
+            buffer = vec.data() ;
+        }
+    }
+
+    std::cout << " final size: " << std::setw(2) << vec.size() 
+              << " capacity: " << std::setw(2) << vec.capacity() << '\n' ;
+}
+
+int main()
+{
+    {
+        std::vector<int> vec ;
+        grow( vec, 100 ) ;
+    }
+    
+    std::cout << "\n-------- with reserve --------\n" ;
+    {
+        std::vector<int> vec ;
+        vec.reserve(100) ; // reserve space for up to 100 elements 
+        grow( vec, 100 ) ;
+    }
+}
