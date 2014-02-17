8eafddf148b769a2bfff6269f6be6df74308e59f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-17 19:19:20 +0000

    Updating archive.

---
+++ trunk/Archive2/3b/386f11434e50ba/main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N1 100
+#define N2 100
+int main() {
+    int img[N1][N2];
+    int i, histogram[10];
+    for(int i=0; i<N1*N2; i++) img[i/N2][i%N2]=rand()%10;
+    for(i=0; i<10; i++) histogram[i] = 0;
+    #pragma omp parallel
+    {
+        int i, j, histogram_private[10];
+        for(i=0; i<10; i++) histogram_private[i] = 0;
+        #pragma omp for nowait
+        for(i=0; i<N1; i++) {
+           for(j=0; j<N2; j++) {    
+               histogram_private[img[i][j]]++;
+           }
+        }      
+        #pragma omp critical 
+        {
+            for(i=0; i<10; i++) histogram[i] += histogram_private[i];
+        }
+    }
+    for(int i=0; i<10; i++) printf("%d\n", histogram[i]);
+}
