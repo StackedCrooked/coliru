5338842b27280aa178970085429d10a32e830272
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-17 19:31:35 +0000

    Updating archive.

---
+++ trunk/Archive2/cd/8b42f923776db8/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <type_traits>
+#include <utility>
+
+
+std::string
+concat()
+{
+    return "";
+}
+
+template<typename Head, typename... Tail>
+decltype( std::to_string(std::declval<Head>()) )
+concat(Head&& h, Tail&&... t);
+
+template<typename Head, typename... Tail>
+typename std::enable_if<
+    std::is_same< std::string, decltype(std::string() + std::declval<Head>()) >::value, std::string
+    >::type
+concat(Head&& h, Tail&&... t)
+{
+    return std::forward<Head>(h) + concat(std::forward<Tail>(t)...);
+}
+
+
+template<typename Head, typename... Tail>
+decltype( std::to_string(std::declval<Head>()) )
+concat(Head&& h, Tail&&... t)
+{
+    return std::to_string(std::forward<Head>(h)) + concat(std::forward<Tail>(t)...);
+}
+
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+    cout << concat("abc") << endl;
+    cout << concat("abc", "def") << endl;
+    cout << concat(std::string("abc"), "def") << endl;
+    cout << concat(3) << endl;
+    cout << concat("abc", 3) << endl;
+    cout << concat(2, "abc", 4) << endl;
+    cout << concat(2, "abc", 4, std::string("def")) << endl;
+}
