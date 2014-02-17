35db693ae1a3f8b204c504bbb8599a1d88486b53
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-17 19:29:33 +0000

    Updating archive.

---
+++ trunk/Archive2/b3/696c95d5632896/main.cpp
@@ -0,0 +1,28 @@
+// vector::push_back
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main (){
+    int myint;
+    vector<int> v1{1, 2, 3, 4, 5, 6, 7, 8};
+    vector<int> v2{1, 2, 3, 4, 5};
+    const size_t s1{v1.size()};
+    const size_t s2{v2.size()};
+    if(s1 > s1){
+        myint = s1 - s2;
+        for(int idk = 0; idk < myint; idk++)
+            v1.push_back(0);
+    }
+    else {
+        myint = s1 - s2;
+        for(int idk = 0; idk < myint; idk++)
+            v2.push_back(0); 
+    } 
+    cout << "V1 equals " << v1.size() << "\nV2 Equals " << v2.size() << "\nmyint equals  " << myint << endl;
+    const size_t sz{v1.size()};
+    vector<int> datsumdoe(sz);
+    for(size_t idy = 0; idy < sz; ++idy){
+        datsumdoe[idy] = v1[idy] + v2[idy];
+        cout << datsumdoe[idy] << endl;} 
+}
