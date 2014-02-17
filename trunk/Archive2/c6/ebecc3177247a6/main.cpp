1a1436702955c50fcebc490dbed956f449577e40
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-17 19:30:58 +0000

    Updating archive.

---
+++ trunk/Archive2/c6/ebecc3177247a6/main.cpp
@@ -0,0 +1,19 @@
+#include <regex>
+#include <string>
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+string str = "aaabxxxaab";
+regex rx("ab");
+
+vector<int> index_matches; // results saved here 
+                           // (should be {2, 8}, but always get only {2})
+   
+for(auto it = std::sregex_iterator(str.begin(), str.end(), rx); it != std::sregex_iterator(); ++it)
+    index_matches.push_back(it->position());
+
+ 
+}
