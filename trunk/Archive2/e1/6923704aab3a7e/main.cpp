a58dab65b504f35c306a9107629a0a1f15cfe0be
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-17 19:33:24 +0000

    Updating archive.

---
+++ trunk/Archive2/e1/6923704aab3a7e/main.cpp
@@ -0,0 +1,29 @@
+#include <cstddef>
+
+
+template <typename T, size_t s>
+struct a
+{  
+     T x[s] ;
+} ;
+
+template <typename T>
+constexpr T func() {
+    a<T,3> a1 ;
+  
+  a1.x[0] = 10 ;
+  return a1.x[0];
+}
+
+constexpr int func2() {
+    a<int,3> a1 ;
+  
+  a1.x[0] = 10 ;
+  return a1.x[0];
+}
+
+
+int main()
+{
+    constexpr int x = func<int>();
+}
