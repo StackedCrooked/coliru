7b95222514d1597616389fbc1c9704694fb73747
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-17 19:28:51 +0000

    Updating archive.

---
+++ trunk/Archive2/ab/5e52653b24b15d/main.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+double f(double x)
+{
+    return std::sqrt(x);
+}
+
+int main()
+{
+	using std::begin;
+	using std::end;
+
+	std::vector<double> squares;//{1., 4., 9., 16., 25.};
+	std::vector<double> numbers;
+	std::transform(begin(squares), end(squares), std::back_inserter(numbers), f);
+	for (auto n : numbers) std::cout << n << ' ';
+	endl(std::cout);
+}
