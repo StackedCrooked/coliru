4ad1e3eed1480d99aa23cd3bc96cb1b85ba8178e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-17 19:29:17 +0000

    Updating archive.

---
+++ Archive2/b0/a5cb1ffaca2a39/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned long long int a = 174467440737095;
+    signed long long int b = -30000000003;
+    signed int c;
+    c = (signed long long int)a/b;
+    cout << "\n\n\n" << c << endl;
+}
