01b74665b48e43e2fc8399a1b290905230a62c75
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-18 09:33:04 +0000

    Updating archive.

---
+++ trunk/Archive2/d9/b5806c14df9055/main.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+template<typename T>
+void call_destructor(T* x) {
+    x->~T();
+}
+
+int main() {
+    auto buffer = new char[sizeof(std::vector<int>::iterator)];
+    
+    auto iterator = new((void*)buffer) std::vector<int>::iterator();
+    //iterator->std::vector<int>::iterator::~iterator();
+    call_destructor(iterator);
+    
+    delete[] buffer;
+}
