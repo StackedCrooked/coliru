c55a8149cbe2c3ae78bd2ea9b1560a45b7990289
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-18 00:09:51 +0000

    Updating archive.

---
+++ Archive2/d6/b5939b8067e608/main.cpp
@@ -0,0 +1,47 @@
+#include <functional>
+
+
+
+template<size_t I, size_t N, typename T, class F, class R>
+R reduce_impl(const std::array<T, N>&, F&&, R&& r, typename std::enable_if<(I==N),void>::type* =0)
+{return std::forward<R>(r);}
+
+template<size_t I, typename T, std::size_t N, class F, class R>
+auto reduce_impl(const std::array<T, N>& a, F&& f, R&& r, typename std::enable_if<(I<N),void>::type* =0)
+-> decltype(reduce_impl<I+1>(a, std::forward<F>(f), f(std::forward<R>(r), a[I])))
+{
+    auto&& p = f(std::forward<R>(r), a[I]);
+    return reduce_impl<I+1>(a, std::forward<F>(f), std::move(p));
+}
+
+template<typename T, std::size_t N, class F>
+auto reduce(const std::array<T, N>& a, F&& f) 
+-> decltype(reduce_impl<1>(a, std::forward<F>(f), a[0]))
+{return reduce_impl<1>(a, std::forward<F>(f), a[0]);}
+
+
+
+struct plus { //duplicate of C++0y std::plus<void>
+    template<class T, class U>
+    auto operator()(T&& t, U&& u) const
+    ->decltype(std::forward<T>(t)+std::forward<U>(u))
+    {return std::forward<T>(t)+std::forward<U>(u);}
+};
+template<int I>
+struct wierd_adder {
+    template<int J>
+    wierd_adder<I+J> operator+(wierd_adder<J>) const
+    {return {};}
+    operator int() {return I;}
+};
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::cout << reduce(std::array<int, 3>{{5,7,11}}, plus()) << '\n'; //3 integers
+    std::cout << reduce(std::array<std::string, 2>{{"HELLO","WORLD"}}, plus()) << '\n'; //2 strings
+    std::cout << reduce(std::array<std::vector<int>, 1>{{{3}}}, plus()).front() << '\n'; //1 object that doesn't even work
+    std::cout << reduce(std::array<wierd_adder<1>, 2>{{{},{}}}, plus()) << '\n'; //objects that return completely unrelated types
+}
+
+    
