2ddf462a204712deba4b8b67524e8b4a9b853c9e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-18 15:06:51 +0000

    Updating archive.

---
+++ Archive2/c8/f1f366c7580ceb/main.cpp
@@ -0,0 +1,22 @@
+#ifndef MY_ITEM_HPP
+#define MY_ITEM_HPP
+#include <string>
+
+class Item {
+public:
+    Item();
+    Item(const std::string& name);
+    Item(const std::string& name, int value);
+
+    std::string name() const;
+    void set_name(const std::string& name);
+
+    int value() const;
+    void set_value(int value);
+
+private:
+    std::string item_name{};
+    int itam_value{};
+};
+
+#endif // MY_ITEM_HPP
