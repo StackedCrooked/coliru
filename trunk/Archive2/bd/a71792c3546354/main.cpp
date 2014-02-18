0d2b85a5bd04e446e9137018a585d2bfcb0637ec
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-18 09:32:18 +0000

    Updating archive.

---
+++ trunk/Archive2/bd/a71792c3546354/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+int main() {
+    auto buffer = new char[sizeof(std::vector<int>::iterator)];
+    
+    auto iterator = new((void*)buffer) std::vector<int>::iterator();
+    iterator->std::vector<int>::iterator::~iterator();
+    
+    delete[] buffer;
+}
