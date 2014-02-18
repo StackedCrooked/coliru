b9e3666e4fd2e41acca8ae31aa35dbda4c55ee0f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-18 15:04:08 +0000

    Updating archive.

---
+++ Archive2/64/bff0056ff956cc/main.cpp
@@ -0,0 +1,11 @@
+#include <string.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    if(strcmp("aString", "anotherString")!=0)
+    {
+        cout << "ci sono passato" << endl;
+    }
+}
