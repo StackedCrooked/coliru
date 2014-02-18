70c68d8057e977c10a7c7c313fb07247787cdbc7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-18 16:12:39 +0000

    Updating archive.

---
+++ trunk/Archive2/50/c23f1ba5e11501/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "Sales_item.h"
+ 
+struct Sales_data
+{
+    std::string bookNo;
+	unsigned units_sold;
+	double revenue;
+};
+ 
+int main()
+{
+	////////////////////////////////////////////////////////////
+	// Exercise 7.1 (Books Version)
+	////////////////////////////////////////////////////////////
+ 
+	Sales_item total;
+	if (std::cin >> total)
+	{
+		Sales_item trans;
+		while (std::cin >> trans)
+		{
+			if (total.isbn() == trans.isbn())
+				total += trans;
+			else
+			{
+				std::cout << total << std::endl;
+				total = trans;
+			}
+		}
+		std::cout << total << std::endl;
+	}
+	else
+	{
+		std::cerr << "No data?" << std::endl;
+		return -1;
+	}
+ 
+	////////////////////////////////////////////////////////////
+	// Exercise 7.1 (My Version)
+	////////////////////////////////////////////////////////////
+ 
+	//Sales_data total;
+}
