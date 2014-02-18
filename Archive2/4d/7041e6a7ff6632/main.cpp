11e941acfd903a128de2ca4cd01f557a6f563941
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-18 16:12:30 +0000

    Updating archive.

---
+++ Archive2/4d/7041e6a7ff6632/main.cpp
@@ -0,0 +1,26 @@
+#ifndef MY_ITEM_HPP
+#define MY_ITEM_HPP
+#include <string>
+#include <iosfwd>
+
+class Item {
+public:
+    Item();
+    Item(const std::string& name);
+    Item(const std::string& name, int value);
+
+    std::string name() const;
+    void set_name(const std::string& name);
+
+    int value() const;
+    void set_value(int value);
+
+private:
+    std::string item_name{};
+    int item_value{};
+};
+
+std::istream& operator>>(std::istream& stream, Item& item);
+std::ostream& operator<<(std::ostream& stream, const Item& item);
+
+#endif // MY_ITEM_HPP
