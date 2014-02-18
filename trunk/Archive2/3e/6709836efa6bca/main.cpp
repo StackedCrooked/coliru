e4222c92fb0418a90b0395eccad3a148f691b17a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-18 16:11:56 +0000

    Updating archive.

---
+++ trunk/Archive2/3e/6709836efa6bca/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "/Archive2/21/801190a50b6029/main.cpp"
+
+using namespace std;
+
+int main()
+{
+    MyClass temp;
+	temp.myInt = 42;
+
+	std::cout << "myInt: " << temp.myInt << std::endl;
+
+	int x = 0;
+	cin >> x;
+
+	return 0;
+}
