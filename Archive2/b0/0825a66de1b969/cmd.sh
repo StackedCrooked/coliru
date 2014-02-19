615ca6b81036b89b61cbc16be262a3dd55037076
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-19 09:45:23 +0000

    Update archive b0

---
+++ Archive2/b0/0825a66de1b969/cmd.sh
@@ -0,0 +1,2 @@
+clang++ -std=c++11 -O0 -Wall -Wextra -Wconversion -pedantic -Werror=return-type -pthread main.cpp && ./a.out
+##clang -x c -std=c99 -O2 -Wall -Wextra -Wconversion -pedantic -pthread main.cpp && ./a.out
