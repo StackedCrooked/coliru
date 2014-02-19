bfa0037198951466f6b72431e732a3fd30e7a79a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-19 08:22:31 +0000

    Update archive 88

---
+++ trunk/Archive2/88/5ff143eaae57da/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+template < typename T, std::size_t N > struct checked_array
+{
+    T a[N] {} ;
+
+    T& operator[] ( std::size_t pos )
+    {
+        if( pos >= N ) throw "std::out_of_range" ;
+        return a[pos] ;
+    }
+
+    const T& operator[] ( std::size_t pos ) const
+    {
+        if( pos >= N ) throw "std::out_of_range" ;
+        return a[pos] ;
+    }
+};
+
+template < std::size_t FROM, std::size_t TILL, typename FN >
+struct unroll_loop
+{
+    unroll_loop( FN fn = FN() ) { fn(FROM) ; unroll_loop< FROM+1, TILL, FN >{fn} ; }
+};
+
+template < std::size_t N, typename FN >
+struct unroll_loop< N, N, FN >
+{
+    unroll_loop( FN ) {}
+} ;
+
+struct foo
+{
+    void operator() ( int i ) const { std::cout << "foo(" << i << ")\n" ; }
+};
+
+int main()
+{
+    unroll_loop<1,6,foo>() ;
+    
+    checked_array<int,10> a ;
+    
+    try
+    {
+        std::cout << "a[5] == " << a[5] << " fine\n" ;
+        std::cout << "a[25] == " << a[25] << " not fine\n" ;
+    }
+    catch( const char* what ) { std::cerr << what << '\n' ; }
+}
