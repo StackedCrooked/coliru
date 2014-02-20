7309bf1fd3e3cfbddc9cb1861d47751cd7c0df8f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-20 10:12:41 +0000

    Update archive 58

---
+++ Archive2/58/d56c5ed7098413/main.cpp
@@ -0,0 +1,21 @@
+//
+// Author: LLD!5DeRpYLOvE
+#include <iostream>
+#include <vector>
+#include <string>
+#include <regex>
+
+using std::string;
+
+int main()
+{
+    string hellothere{"Hello from planet Earth"};
+    string conscious{"To be conscious that you are ignorant is a great step to knowledge"};
+    string autocorrect{"I think you had one of those autocorrect problems sir"};
+    string leapstep{"That's one small step for man, one giant leap for Mankind"};
+    string ourselves{"It is not in the stars to hold our destiny but in ourselves."};
+    string onelife{"One life, you're Thomas A. Anderson, program writer for a respectable software company"};
+    string makesure{"Before you say you can't, make sure you've tried"};    
+    
+
+}
