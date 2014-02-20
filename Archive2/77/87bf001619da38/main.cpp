1141dc5eb25c1f9dc154ff74a227895042fd7662
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-20 12:49:45 +0000

    Update archive 77

---
+++ Archive2/77/87bf001619da38/main.cpp
@@ -0,0 +1,46 @@
+// Strings, Chars, and Regex.
+// Author: LLD!5DeRpYLOvE
+
+#include <string>
+#include <iostream>
+#include <cctype>
+//#include <iterator>
+//#include <vector>
+//#include <regex>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::toupper;
+//using std::begin;
+//using std::end;
+
+int main()
+{
+    // Some source strings and stuff
+    string hellothere{"Hello from planet Earth."};
+    string conscious{"To be conscious that you are ignorant is a great step to knowledge."};
+    string autocorrect{"I think YOU had one of those autocorrect problems sir."};
+    string leapstep{"That's one small step for man, one giant leap for Mankind."};
+    string ourselves{"It is not in the stars to hold our destiny but in ourselves."};
+    string onelife{"One life, you're Thomas A. Anderson, program writer for a respectable software company."};
+    string makesure{"Before you say you can't, make sure you've tried."};
+    
+    cout << hellothere << endl;
+    string a{hellothere.substr(11, hellothere.size())};
+    cout << a << endl;
+    a[0] = toupper(a[0]);
+    cout << a << " Hello." << endl << endl;
+    
+    cout << "before: " << autocorrect << endl;
+    autocorrect.replace(8, 3, "you");
+    cout << "after:  " << autocorrect << endl << endl;
+    
+    // With replace(), it's unnecessary that the replacement text be the same number of chars as that being replaced.
+    cout << onelife << endl;
+    string otherlife{"The other life is lived in computers, where you go by the hacker alias 'Neo' and are guilty of virtually every computer crime we have a law for."};
+    //onelife.replace(0, onelife.size(), otherlife);
+    onelife.replace(onelife.begin(), onelife.end(), otherlife);
+    cout << onelife << endl << endl;
+        
+}
