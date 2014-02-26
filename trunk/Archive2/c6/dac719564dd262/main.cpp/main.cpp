4a9e5837c40171e7c7667cccd07a930a2f70ec5b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-26 20:13:32 +0000

    update c6

---
+++ trunk/Archive2/c6/dac719564dd262/main.cpp/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class Log
+{
+public:    
+    enum logModifiers
+    {
+            lol,
+            wat
+    };
+
+    template<class T> const Log& operator<<(const T& t) const {
+            return *this;
+    }
+}
+ 
+const Log& Log::operator<< <Log::logModifiers>(const Log::logModifiers& t) const;
+ 
+const Log& Log::operator<< <Log::logModifiers> (const Log::logModifiers& t) const 
+{
+    //specialized cosax
+}
+
+int main(int argc, char *argv[])
+{
+    Log log;
+    log << (Log::logModifiers) Log::lol;
+}
