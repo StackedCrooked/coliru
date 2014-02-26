c2e7c7c4e7495f2f50e9b4b33e5f73cadc4f6120
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-02-26 20:10:48 +0000

    update a6

---
+++ trunk/Archive2/a6/3b8c74958fdbbf/main.cpp/main.cpp
@@ -0,0 +1,22 @@
+#include <deque>
+#include <iostream>
+#include <iomanip>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+#define DBG(x) { cout << left << setw(40) << setfill(' ') << #x << (x) << endl; }
+
+
+int main()
+{
+
+    int i = 0;
+
+	if(i = 1){
+		cout << "i = 1!" << endl;
+	}
+	return 0;
+}
+
