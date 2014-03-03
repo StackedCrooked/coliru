24a42fe5a0f3fc7073f2da9ef9221857cf2d55b4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-03 18:31:17 +0000

    Update archive 86

---
+++ Archive2/86/447d780ce3370a/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+constexpr unsigned int str2int(const char* str, int h = 0) {
+    return !str[h] ? 5381 : (str2int(str, h+1)*33) ^ str[h];
+}
+
+int main() {
+    const std::string n = "Floris";
+    
+    switch(str2int(n.c_str())) {
+    case str2int("Floris"):
+        std::cout << "Oh hai" << std::endl;
+        break;
+    case str2int("Zilaz"):
+        std::cout << "Zilaaaz" << std::endl;
+        break;
+    default:
+        break;
+    }
+}
