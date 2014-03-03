89484362036b4345b5405b29106af2e0351eab2b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-03 18:32:04 +0000

    Update archive d4

---
+++ trunk/Archive2/d4/cdf791f5984472/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+struct X {
+    int data;
+};
+
+int main()
+{
+    X x1; // declare a X object
+    x1.data = 500; // access the member --
+                   // since x1 is not a pointer, we need to use dot . operator
+                   // to access the member of x1
+                   
+    X* xPointer = &x1; // point xPointer to x1
+    xPointer->data = 100; // access the member --
+                          // since xPointer is a pointer, we need to use -> arrow
+                          // operator to access the member data
+                          
+    std::cout << "x1.data : " << x1.data << '\n';
+    std::cout << "xPointer->data : " << xPointer->data << '\n';
+    
+    // delete xPointer; is not needed since we did not dynamically allocate
+}
