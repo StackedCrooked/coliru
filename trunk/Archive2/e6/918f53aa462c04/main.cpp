8c9cd4272339fd61555420a352d0dca1ffe9dc62
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-03 18:35:42 +0000

    Update archive e6

---
+++ trunk/Archive2/e6/918f53aa462c04/main.cpp
@@ -0,0 +1,32 @@
+#include <stdbool.h>
+// .. other codes
+
+int main()
+{
+    bool b = true;
+    
+    while( b )
+    {
+        int choice;
+        
+        clrscr();
+        printf("This is the main!\n");
+        printf("\n\n\n\n[0] - Map");
+        printf("\n[1] - Region Choice");
+        gotoxy(1,9);
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 0:
+                map();
+                break;
+            case 1:
+                rc();
+                break;
+            default:
+                printf("Invalid choice!");
+        }
+    }
+    //getch();
+    return 0;
+}
