50ae0339ca65ae363686a6c8f1b782dba8dda2e3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-03 18:42:58 +0000

    Update archive 15

---
+++ Archive2/15/bbcf2bc1371ff4/main.cpp
@@ -0,0 +1,23 @@
+#include <regex>
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const string text = R"(
+XXXXXXXXXXXXXXXXXXXXXXXXXXXX"string1":"string2"XXXXXXXXXXXXXXXXXXXXXXXXXX"string3":"string4" XXXXXXXXXXXXXXXXXXXXXXXXXXXX...
+XXXXXXXXXXXXXXXXXXXXXXXXXXXX"string5":"string6"XXXXXXXXXXXXXXXXXXXXXXXXXX"string7":"string8" XXXXXXXXXXXXXXXXXXXXXXXXXXXX...
+)";
+
+int main() {
+    const regex pattern{R"~("([^"]*)":"([^"]*)")~"};
+    auto left_it = sregex_token_iterator(begin(text), end(text), pattern, 1);
+    auto right_it = sregex_token_iterator(begin(text), end(text), pattern, 2);
+    const auto end = sregex_token_iterator();
+
+    while (left_it != end && right_it != end) {
+        cout << "First: " << (*left_it++).str()
+             << ", Second: " << (*right_it++).str() << endl;
+    }
+}
