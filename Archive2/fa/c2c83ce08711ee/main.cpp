c39d5c54a0957968f00f52feb559e34ebb2a7f62
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-03 18:50:43 +0000

    Update archive fa

---
+++ Archive2/fa/c2c83ce08711ee/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+struct A ; int bar( A ) ; // bar declared at global namespace scope
+
+struct A
+{
+    // int foo(A) is defined as a part of the friend declaration
+    // 1. there is no previous declaration of int foo(A)
+    // 2. the function int foo(a) has namespace scope
+    // 3. the function is implicitly inline
+    // 4. however, it is within the lexical scope of class A
+    // 5. therefore, it can only be found through ADL (Koenig lookup)
+    friend int foo( A a ) { return a.v * 2 ; }
+    
+    // int bar(A) is defined as a part of the friend declaration
+    // 1. there is a previous declaration of int bar(A) at global namespace scope
+    // 2. the previously declared function is being defined as part of the friend declaration
+    // 3. the function is implicitly inline
+    friend int bar( A a ) { return a.v * 2 ; }
+
+    int v = 10 ;
+};
+
+int main()
+{
+    A a ;
+    
+    std::cout << foo(a) << '\n' ; // fine, int ::foo(A) found via ADL
+    std::cout << bar(a) << '\n' ; // fine, int ::bar(A) declared at global namespace scope
+    
+    // std::cout << ::foo(a) << '\n' ; // *** error *** name ::foo not found
+    std::cout << ::bar(a) << '\n' ; // fine, ::bar has been declared
+    
+    // std::cout << A::foo(a) << '\n' ; // *** error *** A does not have a member named foo
+}
---
+++ Archive2/fa/e990bf1c3df581/main.cpp
@@ -0,0 +1,62 @@
+#include <map>
+#include <boost/fusion/adapted.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/karma.hpp>
+
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/serialization/serialization.hpp>
+
+namespace qi = boost::spirit::qi;
+namespace karma = boost::spirit::karma;
+
+struct Demo
+{
+    int i;
+    std::string truth;
+
+    using Vars = std::map<std::string, double>;
+    Vars vars;
+};
+
+BOOST_FUSION_ADAPT_STRUCT(Demo, (int,i)(std::string,truth)(Demo::Vars, vars))
+
+static std::string serialize(Demo const& data)
+{
+    std::ostringstream oss;
+    oss << karma::format(
+            "Demo{" << karma::int_ << ';' << karma::string << ';' 
+                << '{' 
+                        << ('{' << karma::string << ": " << karma::double_ << '}') % ", " 
+                << '}'
+            << '}', data);
+
+    return oss.str();
+}
+
+static Demo deserialize(std::string const& text)
+{
+    auto f(text.begin()), l(text.end());
+    Demo parsed;
+    if (qi::parse(f, l,
+            "Demo{" >> qi::int_ >> ';' >> +~qi::char_(';') >> ';' 
+            >> '{' 
+                >> ('{' >> +~qi::char_(':') >> ": " >> qi::double_ >> '}') % ", " >> '}'
+            >> '}', parsed))
+    {
+        return parsed;
+    }
+    throw std::runtime_error("Parse failed at '" + std::string(f,l) + "'");
+}
+
+int main()
+{
+    Demo a { 42, "LtUaE", { { "PI", 3.1415926 }, { "e", std::exp(1.0) } } };
+    std::cout << "Quick serialization: " << karma::format_delimited(karma::auto_, ';', a) << "\n";
+
+    std::string const serialized = serialize(a);
+    std::cout << "Serious serialization: " << serialized << "\n";
+
+    // to parse back:
+    Demo roundtrip = deserialize(serialized);
+    std::cout << "Parsed back: " << karma::format_delimited(karma::auto_, ';', roundtrip) << "\n";
+}
