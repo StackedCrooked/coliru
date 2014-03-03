0536e13ad236bdec9d402b004b167df115ccff68
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-03 18:45:47 +0000

    Update archive 24

---
+++ trunk/Archive2/24/5deaa3aa2470c0/main.cpp
@@ -0,0 +1,23 @@
+#include <cstddef>
+#include <iostream>
+#include <array>
+
+const static double maximum_piage = 3.141592653589793238462643383279502884197169399375105820974944592307816406286;
+
+template <typename T>
+struct pi_t {
+    
+    operator T () {
+        return static_cast<T>( maximum_piage );
+    }
+};
+
+template <typename T>
+using pi = pi_t<float>();
+
+int main () {
+    
+    float f = pi<float>;
+    
+}
+
