08ebe20e3e94e2a78320ee6f6aecccaf3d22eab1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-03 18:28:14 +0000

    Update archive cc

---
+++ trunk/Archive2/cc/cc08dba54735f1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <boost/range/combine.hpp>
+
+int main()
+{
+    std::vector<int> v1{ 1, 2, 3 }, v2{ 6, 4, 2, 42 };
+
+    for (auto&& t : boost::combine(v1, v2))
+            std::cout << t.get<0>() << " " << t.get<1>() << "\n";    
+}
