fd0247c8fca636b779527cb08609d61d9e4ff114
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-03 18:27:44 +0000

    Update archive 04

---
+++ trunk/Archive2/04/5d9b9217cd8e2c/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+namespace detail
+{
+    template<class T, class = decltype(T::check())>
+    std::true_type has_check_impl(void*);
+
+    template<class T>
+    std::false_type check_impl(...);
+}
+
+template<class T>
+struct has_check : decltype(detail::has_check_impl<T>(nullptr)) { };
+
+template<class T, class...>
+using first = T;
+
+template<class... Mixins>
+struct Checker
+{
+public:
+    template<class =
+             first<void,
+                   typename std::enable_if<has_check<Mixins>::value>::type...>>
+    static void go()
+    {
+        auto x{ (Mixins::check(), 0)... };
+        (void)x;
+    }
+
+    static void go()
+    {
+        auto x{ (Mixins::handle(), 0)... };
+        (void)x;
+    }
+};
+
+struct A { static void check() { std::cout << "check() "; } };
+struct B { static void check() { std::cout << "check() "; } };
+
+int main()
+{
+    Checker<A, B>::go();
+}
---
+++ trunk/Archive2/04/85be0539b7715e/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <boost/iterator/zip_iterator.hpp>
+#include <boost/range/iterator_range.hpp>
+
+int main()
+{
+    std::vector<int> v1{ 1, 2, 3 }, v2{ 6, 4, 2 };
+
+    auto rng = boost::make_iterator_range(
+        boost::make_zip_iterator(boost::make_tuple(begin(v1), begin(v2))),
+        boost::make_zip_iterator(boost::make_tuple(end(v1), end(v2)))        
+    );
+
+    for (auto&& t : rng)
+            std::cout << t.get<0>() << " " << t.get<1>() << "\n";    
+}
