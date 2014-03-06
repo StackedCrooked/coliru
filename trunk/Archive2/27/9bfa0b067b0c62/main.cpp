1bc16b307e647a8cceb1e103ffb4832d4d0b518a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-06 00:37:46 +0000

    Update archive 27

---
+++ trunk/Archive2/27/9bfa0b067b0c62/main.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <iostream>
+
+class Way {
+private:
+    std::string id;
+	std::string name;
+
+public:
+	Way() = default;
+	Way(const Way& other) = default;
+
+	void SetName(std::string name) { this->name = name; }
+	std::string const & GetName() const { return name; }
+
+	void SetId(std::string id) { id = id; }
+	std::string const & GetId() const { return id; }
+};
+
+class AreaDataRepository {
+private:
+	AreaDataRepository() = default;
+	AreaDataRepository(const AreaDataRepository& orig) = default;
+	virtual ~AreaDataRepository() = default;
+
+	Way onGoingWay;
+public:
+	static AreaDataRepository& Instance()
+	{
+		static AreaDataRepository singleton;
+		return singleton;
+	}
+
+	void SetOnGoingWay(Way onGoingWay)	{ this->onGoingWay = onGoingWay;  }
+	Way const & GetOnGoingWay() const { return onGoingWay; }
+};
+
+int main()
+{
+	Way wayNode;
+	wayNode.SetId("123");
+	wayNode.SetName("jan");
+	AreaDataRepository::Instance().SetOnGoingWay(wayNode);
+
+	std::cout << AreaDataRepository::Instance().GetOnGoingWay().GetId();
+
+	std::cin.get();
+	return 0;
+}
