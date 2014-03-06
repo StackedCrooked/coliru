e5ffdc2f7a709c5815e9235254f1acde17123c0c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-06 00:41:01 +0000

    Update archive 91

---
+++ Archive2/91/9accf2090c4d90/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+namespace detail
+{
+    template<class T, class = decltype(T::check())>
+    std::true_type has_check_impl(void*);
+
+    template<class T>
+    std::false_type check_impl(...);
+}
+
+template<class T>
+struct has_check : decltype(detail::has_check_impl<T>(nullptr)) { };
+
+template<class T, class...>
+using first = T;
+
+template<class... Mixins>
+struct Checker
+{
+public:
+    template<class =
+             first<void,
+                   typename std::enable_if<has_check<Mixins>::value>::type...>>
+    static void foo(int)
+    {
+        auto x{ (Mixins::check(), 0)... };
+        (void)x;
+    }
+
+    static void foo(...)
+    {
+        auto x{ (Mixins::handle(), 0)... };
+        (void)x;
+    }
+    
+    static void go() {
+        derp(0);
+    }
+};
+
+struct A { static void check() { std::cout << "check() "; } };
+struct B { static void check() { std::cout << "check() "; } };
+
+int main()
+{
+    Checker<A, B>::go();
+}
