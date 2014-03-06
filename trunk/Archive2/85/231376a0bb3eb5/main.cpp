99fb50079d3ed004d6a8ee0d5a8b11388b190f19
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-06 00:45:52 +0000

    Update archive 85

---
+++ trunk/Archive2/85/231376a0bb3eb5/main.cpp
@@ -0,0 +1,12 @@
+    #include <iostream>
+    
+    int main()
+    {
+        int *ptr1 = NULL ;
+        
+        if( NULL = ptr1 )
+        {
+                std::cout << "It is NULL" << std::endl ;
+        }
+        
+    }
