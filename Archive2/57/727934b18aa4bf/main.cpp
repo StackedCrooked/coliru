81f242e09e928d24941f3d6fe306c23ae6fa146f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-06 00:37:56 +0000

    Update archive 57

---
+++ Archive2/57/727934b18aa4bf/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class base{public:~Base() { }};
+class a : public base{};
+
+void Funktion(base& variable)
+{
+    cout << "base" << endl;
+}
+void Funktion(a& variable)
+{
+    cout << "a" << endl;
+}
+
+int main()
+{
+    vector<base*> list;
+    list.push_back(new a());
+    list.push_back(new base());
+
+    for (const auto& e : list)
+    {
+        Funktion(*e);
+    }
+}
