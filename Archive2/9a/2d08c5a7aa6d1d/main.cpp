f7ba8a4bbcef445a3d704d76407ae01b5cf8d813
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-06 00:39:50 +0000

    Update archive 9a

---
+++ Archive2/9a/2d08c5a7aa6d1d/main.cpp
@@ -0,0 +1,89 @@
+#include <type_traits>
+#include <iterator>
+#include <iostream>
+
+template<typename Var, typename Cont>
+struct WithExisting {
+public:
+    typedef typename std::remove_reference<Cont>::type cont_noref;
+    
+private: // note that we have to pass "cont_noref&" to declval, because std::begin doesn't work on rvalue arrays
+    typedef decltype(std::begin(std::declval<cont_noref&>())) cont_iterator;
+    typedef cont_iterator const_cont_iterator;
+  
+    struct Iterator {
+        typedef Var &reference;
+        typedef Var value_type;
+        
+    public:
+        Iterator(Var &var, cont_iterator contIterator):var(var), contIterator(contIterator) { }
+        
+    public:
+        Iterator operator++(int) {
+            return Iterator(var, contIterator++);
+        }
+        
+        Iterator &operator++() {
+            ++contIterator;
+            return *this;
+        }
+        
+        Iterator operator--(int) {
+            return Iterator(var, contIterator++);   
+        }
+        
+        Iterator &operator--() {
+            --contIterator;   
+            return *this;
+        }
+        
+        Var &operator*() const {
+            var = *contIterator;
+            return var;
+        }
+        
+        friend bool operator==(Iterator a, Iterator b) {
+            return a.contIterator == b.contIterator;   
+        }
+        friend bool operator!=(Iterator a, Iterator b) {
+            return !(a == b);   
+        }  
+        
+    private:
+        Var &var;
+        cont_iterator contIterator;
+    };
+    
+    typedef Iterator iterator;
+    typedef Iterator const_iterator;
+    
+public:
+    WithExisting(Var &var, Cont &&cont):var(var), cont(std::forward<Cont>(cont)) { }
+
+    iterator begin() {
+        return iterator(var, std::begin(cont));   
+    }
+
+    iterator end() {
+        return iterator(var, std::end(cont));
+    }
+    
+public:
+    Var &var;
+    Cont &&cont;
+};
+
+template<typename Var, typename Cont>
+WithExisting<Var, Cont> with_existing(Var &var, Cont &&cont) {
+    return with_existing(var, std::forward<Cont>(cont));
+}
+
+int main() {
+    int n;
+    int a[] = { 1, 2, 3, 4, 5 };
+    for(auto &&x : with_existing(n, a)) {
+        std::cout << "n is " << n << std::endl;
+        x++;   
+    }
+    std::cout << "n is " << n; // must output "n is 6"
+}
