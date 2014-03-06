722d6957e3d029d20710edeb210f42f43c72a866
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-06 00:50:12 +0000

    Update archive 55

---
+++ Archive2/55/cc297c99a2c58f/main.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <initializer_list>
+#include <iostream>
+#include <type_traits>
+#include <vector>
+
+template<typename T>
+class any {
+public:
+    explicit any(std::initializer_list<T> const& list)
+        : data(list) { }
+
+    template<typename InputIt>
+    explicit any(InputIt begin, InputIt end)
+        : data(begin, end) { }
+
+    template<class U = T>
+    explicit operator typename std::enable_if<std::is_same<U, bool>::value, bool>::type() {
+        return std::any_of(data.begin(), data.end(), [] (bool x) { return x; });
+    }
+
+private:
+    std::vector<T> data;
+
+    template<typename U>
+    friend any<bool> operator==(U const& other, any const& self) {
+        std::vector<bool> results(self.data.size());
+        std::transform(self.data.begin(), self.data.end(), results.begin(),
+                       [&] (T const& x) { return x == other; });
+        return any<bool>(results.begin(), results.end());
+    }
+};
+
+int main() {
+    auto x = 42;
+    std::cout << static_cast<bool>(x == any<int>{3, 42, 90}) << '\n';
+    std::cout << static_cast<bool>(x == any<int>{3, 41, 90}) << '\n';
+    return 0;
+}
