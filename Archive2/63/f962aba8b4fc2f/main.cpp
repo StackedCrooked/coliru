66949cec704e0b42938d9cdbb4c046d3aff559cf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-11 07:31:52 +0000

    Update archive 63

---
+++ Archive2/63/f962aba8b4fc2f/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <cassert>
+
+struct monster {
+    virtual ~monster() = default ;
+
+    virtual int attacks() const = 0 ;
+	// etc.
+};
+
+template < typename DERIVED, int NATTACKS > struct monster_type : monster {
+    static int numberOfAttacks ;
+    virtual int attacks() const override { return numberOfAttacks ; }
+    // etc.
+};
+
+template < typename DERIVED, int NATTACKS >
+int monster_type<DERIVED,NATTACKS>::numberOfAttacks = NATTACKS ;
+
+struct goblin: monster_type<goblin,5> {};
+struct dragon: monster_type<dragon,20> { dragon(double,int) {} };
+struct troll: monster_type<troll,8> { explicit troll(int) {} };
+
+namespace cpp14
+{
+    template< class T, typename... ARGS >
+    std::unique_ptr<T> make_unique( ARGS&&... args )
+    { return std::unique_ptr<T>( new T( std::forward<ARGS>(args)...) ) ; }
+}
+
+int main()
+{
+    using cpp14::make_unique ;
+    std::unique_ptr<monster> monsters[] = {
+        make_unique<goblin>(), make_unique<dragon>( 3.4, 7 ), make_unique<troll>(24) } ;
+
+   for( const auto& pointer : monsters ) std::cout << pointer->attacks() << ' ' ;
+   assert( dragon::numberOfAttacks == 20 && "something is wrong" ) ;
+}
