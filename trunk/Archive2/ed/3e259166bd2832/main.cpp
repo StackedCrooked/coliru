d64b72ded1be8ec6f09cdffda1f116272d2e1579
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-11 07:14:03 +0000

    Update archive ed

---
+++ trunk/Archive2/ed/3e259166bd2832/main.cpp
@@ -0,0 +1,26 @@
+#include <boost/config/warning_disable.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/qi_real.hpp>
+#include <boost/tuple/tuple_io.hpp>
+
+namespace qi = boost::spirit::qi;
+namespace ascii = boost::spirit::ascii;
+
+int main()
+{
+    using qi::double_;
+	using qi::int_;
+	using qi::lit;
+
+	std::string const path = "42,43";
+	{
+		boost::tuple<int, int> result;
+
+		std::string::const_iterator iter = path.begin();
+		std::string::const_iterator end = path.end();
+		bool r = boost::spirit::qi::parse(iter, end, int_ >> lit(',') >> int_, result);
+        std::cout << "r = " << r << std::endl;
+        std::cout << "iter == end = " << (iter == end) << std::endl;
+		std::cout << "Result: " << result.get<0>() << " , " << result.get<1>() << std::endl;
+	}
+}
