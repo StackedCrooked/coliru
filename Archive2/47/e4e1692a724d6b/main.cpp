d635890bd95059c8f277097ffb7856c47920a055
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-11 07:26:47 +0000

    Update archive 47

---
+++ Archive2/47/e4e1692a724d6b/main.cpp
@@ -0,0 +1,27 @@
+class A {};        
+class B {};
+
+class Base
+{
+};    
+
+template <class T>    
+class Derived : public Base
+{   
+    public:
+        Derived(T2 startVal):var(startVal){}
+    private:
+        T2 var;    
+};
+
+template <typename T>
+Derived<T> MakeDerived(T initValue)
+{
+    return Derived<T>(initValue);
+}
+
+
+int main() {
+  Derived<A> da;
+  Derived<B> db;
+}
