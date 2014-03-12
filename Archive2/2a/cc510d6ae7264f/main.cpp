1b3fad6376066bd33d811220280be7e0d5518f39
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-12 07:35:44 +0000

    Update archive 2a

---
+++ Archive2/2a/cc510d6ae7264f/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <locale>
+#include <codecvt>
+#include <vector>
+#include <sstream>
+ 
+int main()
+{
+    const std::u16string utf16 = u"Πυθαγόρας ὁ Σάμιος" ;
+    const std::u16string another_utf16 = u"(Pythagoras of Samos)" ;
+    
+    // http://en.cppreference.com/w/cpp/locale/wstring_convert
+    std::wstring_convert<std::codecvt_utf8_utf16<char16_t>,char16_t> codecvt ;
+    
+    std::ostringstream ograph ; 
+    ograph << "the two UTF-16 strings contain: '" << codecvt.to_bytes(utf16) 
+           << "' and '" << codecvt.to_bytes(another_utf16) << "'" ;
+           
+    std::cout << ograph.str() << '\n' ;
+}
