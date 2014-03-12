0c93ca40fc15ab2f12cc5ac4f467606a1c66b139
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-12 19:31:23 +0000

    Update archive 2f

---
+++ Archive2/2f/fe5c6bbfbc89d2/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <utility>
+#include <map>
+
+template <typename K, typename V>
+static void
+addMapping(const K& id,
+           const V& index,
+           std::map<K, V>& mapset
+) {
+    std::pair<typename std::map<K, V>::iterator, bool> ret;
+    // ...
+}
+int main (){
+    
+    return 0;
+}
