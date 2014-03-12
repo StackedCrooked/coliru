3d8d76a9bacab5acb0cd43a8a1371473f16ab49f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-12 19:30:51 +0000

    Update archive ae

---
+++ Archive2/ae/7f87ae82ccd9eb/main.cpp
@@ -0,0 +1,20 @@
+#include <boost/spirit/include/qi.hpp>
+
+namespace qi = boost::spirit::qi;
+
+int main(int argc, char** argv)
+{
+    typedef std::string::const_iterator It;
+    std::string const input = "cout<<hello;my \"name is\" Hassan";
+
+    qi::rule<It, std::string()> delimiter = qi::char_("; ") | qi::string("<<");
+    qi::rule<It, std::string()> quoted    = '"' >> *~qi::char_('"') > '"';
+    qi::rule<It, std::string()> word      = +((quoted | qi::char_) - delimiter);
+
+    std::vector<std::string> tokens;
+    if (qi::parse(input.begin(), input.end(), *(word >> delimiter), tokens))
+    {
+        for(auto& token : tokens)
+            std::cout << "'" << token <<  "'\n";
+    }
+}
