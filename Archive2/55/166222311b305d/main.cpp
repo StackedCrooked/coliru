8169af7fe105fc271c06171d03c0beb2bcaa570c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-12 19:23:46 +0000

    Update archive 55

---
+++ Archive2/55/166222311b305d/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <locale>
+#include <codecvt>
+ 
+int main()
+{
+    const std::u16string utf16 = u"Πυθαγόρας ὁ Σάμιος (Pythagoras of Samos)" ;
+    
+    // http://en.cppreference.com/w/cpp/locale/wstring_convert
+    std::wstring_convert<std::codecvt_utf8_utf16<char16_t>,char16_t> codecvt ;
+    
+    std::cout << codecvt.to_bytes(utf16) << '\n' ;
+}
