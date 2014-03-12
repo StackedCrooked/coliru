d5c4d760cbef30b755874df2eeb65e46723cdd3d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-12 07:35:15 +0000

    Update archive a6

---
+++ Archive2/a6/b5a2cb80faaaa2/cmd.sh
@@ -0,0 +1,6 @@
+g++-4.8 -std=c++11 -Wall -Wextra -O3 -pedantic-errors main.cpp && ./a.out
+With maximum compiler optimization
+g++-4.8 -std=c++11 -Wall -Wextra -O2 -pedantic-errors main.cpp && ./a.out
+With some compiler optimization
+g++-4.8 -std=c++11 -Wall -Wextra -pedantic-errors main.cpp && ./a.out
+With no compiler optimization
