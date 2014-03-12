ce965070c9c4c0534a6859c6b0f11eff317dc559
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-12 07:26:55 +0000

    Update archive 84

---
+++ Archive2/84/1225a08f2f6c00/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+
+// Imagine this is provided by some lib
+namespace lib
+{
+    struct type {};
+    
+    template<class T>
+    struct specialization
+    {
+        static void call()
+        {
+            std::cout << "specialization-default\n";
+        }
+    };
+    
+    template<>
+    struct specialization<type>
+    {
+        static void call()
+        {
+            std::cout << "specialization(type)\n";
+        }
+    };
+    
+    template<class T>
+    void overload(T)
+    {
+        std::cout << "overload-default\n";
+    }
+    
+    void overload(type)
+    {
+        std::cout << "overload(type)\n";
+    }
+
+    template<class T>
+    void call_overload()
+    {
+        overload(T());
+    }
+    
+    template<class T>
+    void call_specialization()
+    {
+        specialization<T>::call();
+    }
+}
+
+// Imagine this is your custom namespace
+namespace custom
+{
+    struct type {};
+}
+
+// Imagine now you want to extend the lib with your custom type
+namespace lib
+{    
+    void overload(custom::type)
+    {
+        std::cout << "overload(custom::type)\n";
+    }
+    
+    template<>
+    struct specialization<custom::type>
+    {
+        static void call()
+        {
+            std::cout << "specialization(custom::type)\n";
+        }
+    };
+}
+
+int main(int argc, char** argv) {
+    // now see what will happen...
+    lib::call_overload<custom::type>();
+    lib::call_specialization<custom::type>();
+    return 0;
+}
