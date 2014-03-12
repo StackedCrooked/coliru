17c3bdf1226f8acc4b11693c0b5a1b348009333c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-12 07:29:23 +0000

    Update archive 7b

---
+++ Archive2/7b/ad432df71f8e40/main.cpp
@@ -0,0 +1,25 @@
+typedef struct A {
+    int x;
+    int y;
+} A;
+
+typedef struct B {
+    A a;
+    int d;
+} B;
+
+extern void ModifyX(A *a, B *b)
+{
+    b->a.x = a->x + 1;
+
+    if(b->a.x != a->x)
+    {
+        b->a.x--;
+    }
+}
+
+extern A* GetSuper(B *b)
+{
+    return (A *)(b);
+}
+
