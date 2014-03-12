7ef10897c481c061a6bd733f029d4f318558906d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-12 19:38:10 +0000

    Update archive 42

---
+++ trunk/Archive2/42/86f80bfadd5ded/cmd.sh
@@ -0,0 +1,2 @@
+clang++ -std=c++11 -stdlib=libc++ -O2 -Wall -pedantic-errors main.cpp -lsupc++ 
+./a.out <<< 'The Lao alphabet has thirty-eight vowel symbols, representing twenty-four vowel sounds.'
