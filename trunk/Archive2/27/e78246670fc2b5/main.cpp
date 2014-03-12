ba2d5214be3838108bebce9fcc2c5d9aff94cd18
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-12 07:30:25 +0000

    Update archive 27

---
+++ trunk/Archive2/27/e78246670fc2b5/main.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#define p printf
+#define s scan f
+int main()
+{
+    char a,b;
+    p("Enter: ");
+    s("%s",&a);
+    
+    if (a=='r')
+    p("red");
+    else if (a==''b)
+    p("blue");
+    else 
+    p("XD");
+    getch();
+}
