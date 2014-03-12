b98f09141d9dc49584840e98e36fed754a6844d5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-12 19:33:50 +0000

    Update archive b0

---
+++ Archive2/b0/db61aac4687f41/main.cpp
@@ -0,0 +1,15 @@
+#include <type_traits>
+#include <utility>
+
+int test(double x, double y);
+double test(int x, int y);
+char test(char x, int y);
+
+template<typename... Ts>
+using TestType = auto(Ts...) -> decltype(test(std::declval<Ts>()...));
+
+int main() {
+    static_assert(std::is_same<TestType<double, double>, int(double, double)>::value, "1");
+    static_assert(std::is_same<TestType<int, int>, double(int, int)>::value, "2");
+    static_assert(std::is_same<TestType<char, int>, int(char, int)>::value, "3");
+}
