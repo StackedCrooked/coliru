66049fae7dfc82f9640b35fd4b1541ec77d72115
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-13 07:28:39 +0000

    Update archive 61

---
+++ Archive2/61/d357c0fe036d20/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <cstdint>
+#include <sstream>
+#include <vector>
+
+int main()
+{
+    const char* const typical_response = "0E1D" "052B" "FBB7" "11C1" "002C" "0042"
+                "007A" "014D" "FE44" "022B" "270F" "7FFF" "8000" "0000" "00g0" "0000" ;
+
+    constexpr std::size_t RESP_SZ = 64 ;
+    char response[RESP_SZ] ;
+    std::memcpy( response, typical_response, RESP_SZ ) ;
+
+    std::vector<int> numbers ;
+
+    // every 4 characters above are a signed decimal value
+    constexpr std::size_t NCHARS = 4 ;
+    for( std::size_t i = 0 ; i < RESP_SZ ; i += NCHARS ) // in 4 char chunks
+    {
+        std::istringstream stm( { response+i, response+i+NCHARS } ) ;
+
+        using uint_type = std::uint16_t ;
+        using sint_type = std::int16_t ;
+        uint_type value ; // read in as unsigned [ 0,  0xffff ]
+        if( stm >> std::hex >> value && stm.eof() ) 
+            numbers.push_back( sint_type(value) ) ; // push_back as signed [ -32768, +32767 ]
+        else 
+        {
+            std::cerr << "inalid hex number starting at position " << i << ": '" << stm.str() << "'\n" ;
+            numbers.push_back(0) ;
+        }
+    }
+
+    for( int n : numbers ) std::cout << n << ' ' ;
+    std::cout << '\n' ;
+}
