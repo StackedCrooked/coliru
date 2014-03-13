8338357a6a6447bf8f3fd04abd8573833beb9e48
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-13 19:36:41 +0000

    Update archive 04

---
+++ Archive2/04/d94d6443a5b208/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstddef>
+#include <type_traits>
+
+template<std::size_t V>
+struct sum
+{
+    static constexpr std::size_t value=sum<V-1>::value+V;
+};
+
+template<>
+struct sum<0>
+{
+    static constexpr std::size_t value=0;
+};
+
+
+int main() {
+    std::cout << sum<300>::value << std::endl;
+}
