3804705813f1f956ff789ede5dd9a308a109d10b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-13 07:34:24 +0000

    Update archive a6

---
+++ trunk/Archive2/a6/42496b155fed35/main.cpp
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <iostream>
+#include <math.h>
+#include <time.h>
+
+using namespace std;
+
+
+int main()
+{
+    srand (time(NULL)); // semilla
+	int begin = -2, end = 4; // principio y fin del rango
+	int n = 0;
+	cout << "¿Cuantos numeros quieres generar? ";
+	cin >> n;
+
+	for (int i=0; i<n; i++)
+	{
+		double y = (rand() % (end-begin)) + begin + 1; // número aleatorio entre begin y end. Sumo 1 para que el rango sea [-2, 4] y no [-2,4)
+		cout << y << "\t"<< exp (-pow(y-1, 2.0)) << endl;
+	}
+	return 0;
+}
