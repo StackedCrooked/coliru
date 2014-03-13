6cf6d306e6a564c8069244ff9cf65fd2999a7a55
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-13 19:30:53 +0000

    Update archive be

---
+++ Archive2/be/8dcf3255eaca1b/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <boost/noncopyable.hpp>
+#include <type_traits>
+
+template <class Enum>
+auto foo(Enum& v) -> typename std::enable_if<std::is_enum<Enum>::value>::type
+{
+    printf("foo(enum)\n");
+}
+
+template <class Digit>
+auto foo(Digit& v) -> typename std::enable_if<std::is_integral<Digit>::value>::type
+{
+    printf("foo(int)\n");
+}
+
+/*
+template <class Enum>
+void goo(Enum& v)
+{
+    static_assert(std::is_enum<Enum>::value, "");
+    printf("goo(enum)\n");
+}
+
+template <class Digit>
+void goo(Digit& v)
+{
+    static_assert(std::is_integral<Digit>::value, "");
+    printf("goo(int)\n");
+}
+*/
+
+enum class En {
+    val
+};    
+
+int main()
+{
+    En v = En::val;
+    foo(v);
+    
+    int i = 1;
+    foo(i);
+    
+    //goo(v);
+    
+    return 0;
+}
