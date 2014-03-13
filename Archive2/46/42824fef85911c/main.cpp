ee9c3f1db76968a9ec08a72f6570102416d1f554
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-13 19:34:54 +0000

    Update archive 46

---
+++ Archive2/46/42824fef85911c/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+using _true  = integral_constant <bool, true>;
+using _false = integral_constant <bool, false>;
+
+template <typename T> using pass = _true;
+
+template <template <typename...> class F>
+struct temp { };
+
+template <typename... A, template <typename...> class F>
+F <A...> subs_fun(temp <F>);
+
+template <template <typename...> class F, typename... A>
+using subs = decltype(subs_fun <A...>(temp <F>()));
+
+template <template <typename...> class F>
+struct test
+{
+    template <typename... A> static _false                 _(...);
+    template <typename... A> static pass <subs <F, A...> > _(int);
+};
+
+template <template <typename...> class F, typename... A>
+using sfinae = decltype(test <F>::template _<A...>(0));
+
+template <typename T> using type_of  = typename T::type;
+template <typename T> using has_type = sfinae <type_of, T>;
+
+struct A { using type = double; };
+
+int main()
+{
+    cout << has_type <int>() << ", ";
+    cout << has_type <A>()   << endl;
+}
