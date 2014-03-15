5c728682cc69a5cd4bd187bc8471cd1fd1e0c95a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-15 07:36:13 +0000

    Update archive e4

---
+++ Archive2/e4/55de747d5ea5f4/main.cpp
@@ -0,0 +1,11 @@
+#include <regex>
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    
+    regex partialCommReg(R"((^/\*[\S\s]*$))");
+    if (regex_match ("/* ", partialCommReg) )
+      cout<<"ok";
+}
