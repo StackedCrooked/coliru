443227c93f9842964be2c39ea2057c201d37fe0b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-15 07:43:12 +0000

    Update archive 25

---
+++ trunk/Archive2/25/fc1931372ba356/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void print (void){}
+//template <typename... Tail>
+//void print (bool b, Tail... t);
+template <typename Head, typename... Tail>
+void print (Head h, Tail... t);
+
+
+template <typename Head, typename... Tail>
+void print (Head h, Tail... t) {
+  std::cout << h << " ";
+  print(t...);
+}
+
+template <typename... Tail>
+void print (bool , Tail... t) {
+  std::cout <<"bool" << " ";
+  print(t...);
+}
+
+int main(int argc, char *argv[]) {
+  print("string", 42, true, 3.43, true, 'c');
+  return 0;
+}
