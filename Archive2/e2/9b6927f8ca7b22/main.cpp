94036553d91d666a0072d40dc14707ad6af16517
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-17 08:02:36 +0000

    Update archive e2

---
+++ Archive2/e2/9b6927f8ca7b22/main.cpp
@@ -0,0 +1,19 @@
+template <typename T>
+struct foo {
+    template <typename U>
+    foo& operator=(foo<U> const&);
+};
+template <typename T, typename U> void f(foo<T>, foo<U>);
+
+template <typename T>
+template <typename U>
+foo<T>& foo<T>::operator=(foo<U> const& that) {
+    f(*this, that);
+}
+
+int main() {
+    
+    foo<int> arf;
+    arf = foo<unsigned>();
+    
+}
