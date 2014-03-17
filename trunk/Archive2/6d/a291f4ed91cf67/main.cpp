608b8758965e435c0d0178aed4154bac4e99b086
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-17 08:04:44 +0000

    Update archive 6d

---
+++ trunk/Archive2/6d/a291f4ed91cf67/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    setuid( 0 );   // you can set it at run time also
+    system( "/tmp/test" );
+    return 0;
+ }
