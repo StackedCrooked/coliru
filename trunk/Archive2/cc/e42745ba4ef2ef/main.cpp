29dcdd39cc766558a6a7175346eb0ae3477efec5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-17 19:57:09 +0000

    Update archive cc

---
+++ trunk/Archive2/cc/e42745ba4ef2ef/main.cpp
@@ -0,0 +1,16 @@
+
+void swap(int *num1, int *num2)
+{
+    int tmp = *num2;
+    *num2 = *num1;
+    *num1 = tmp;
+}
+
+int main()
+{
+    int num1 = 5, num2 = 7 ;
+
+    swap( &num1, &num2 ); // fine
+    
+    swap( num1, num2 ); // *** error ****
+}
