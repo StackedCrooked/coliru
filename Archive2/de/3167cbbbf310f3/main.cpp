b9913cfb3a18555b93fdd4efcad0ad557541052e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-18 20:08:37 +0000

    Update archive de

---
+++ Archive2/de/3167cbbbf310f3/main.cpp
@@ -0,0 +1,15 @@
+#include <type_traits>
+
+class Client {};
+class Derived : public Client {};
+class NonDerived {};
+
+template<class T, class Enable = void>
+class Server;
+
+template <typename CLIENT>
+class Server<CLIENT, typename std::enable_if<std::is_base_of<Client, CLIENT>::value>::type> {};
+
+int main(int argc, char* argv[]) {
+    Server<NonDerived> x;
+}
