8d0c1fac058d61e10c7056ff1dd2b49ecc787d60
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-19 19:57:22 +0000

    Update archive f2

---
+++ trunk/Archive2/f2/9aa8aa798a1519/cmd.sh
@@ -0,0 +1,23 @@
+cat > ramp.bnd <<RAMP.BND; clang++ -Os -std=c++11 -Wall -pedantic -Wextra main.cpp && ./a.out 
+      1   12   13   0   0   1   0   INLE
+      2    9    5   0   0   1   0   NOPE
+      3   17    4   0   0   1   0   INLE
+      4    8   11   0   0   1   0   NOPE
+      5    5    6   0   0   1   0   INLE
+      6    6   11   0   0   1   0   NOPE
+      7    0    2   0   0   1   0   NOPE
+      8   13    6   0   0   1   0   NOPE
+      9    1    9   0   0   1   0   NOPE
+     10   16   16   0   0   1   0   NOPE
+     11   10   13   0   0   1   0   NOPE
+     12   19    6   0   0   1   0   NOPE
+     13    5   14   0   0   1   0   NOPE
+     14    5   11   0   0   1   0   INLE
+     15   16    5   0   0   1   0   NOPE
+     16    1   18   0   0   1   0   INLE
+     17    0    1   0   0   1   0   NOPE
+     18    5    8   0   0   1   0   INLE
+     19   13    2   0   0   1   0   INLE
+     20   19   18   0   0   1   0   INLE
+     21    5    6   0   0   1   0   NOPE
+RAMP.BND
