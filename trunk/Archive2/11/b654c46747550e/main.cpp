62e626b9bf6b5f8a7708c6f18e3826f2aeecd8ba
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-19 08:03:21 +0000

    Update archive 11

---
+++ trunk/Archive2/11/b654c46747550e/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+struct node {
+    int data;
+    node *left, *right;
+};
+
+void copy(node *& dest, node * root)
+{
+  if(!root)
+  {
+      dest = NULL;
+      return;
+  }
+  else
+  {
+      dest = new node;
+      dest -> data = root -> data;
+      copy(dest -> left, root -> left);
+      copy(dest -> right, root -> right);
+  }
+}
+
+std::ostream &operator << (std::ostream &oss, const node *tree) {
+    if (tree != nullptr) {
+        oss << tree->left << tree->data << " " << tree->right;
+    }
+    return oss;
+}
+
+int main()
+{
+    node *tree = new node{3, new node{2, new node {1, nullptr, nullptr}, nullptr}, new node{4, nullptr, nullptr}};
+    node *cp;
+    std::cout << tree << std::endl;
+    copy(cp, tree);
+    std::cout << tree << std::endl;
+    std::cout << cp << std::endl;
+    return 0;
+}
