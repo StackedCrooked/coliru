ed6320995a65d7b069bd1d1ddd0e3b2d1c39e192
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-19 19:56:30 +0000

    Update archive 0c

---
+++ trunk/Archive2/0c/68fac3bab9b949/main.cpp
@@ -0,0 +1,21 @@
+#include <map>
+#include <functional>
+
+struct A {
+    
+    void func1 (int) {};
+	void func2 (int) {};
+    
+	void mapping() {
+		// std::map<int, std::function<void(int)>> myMap = { {1, func1}, {2, func2} };
+        
+        using std::placeholders::_1 ;
+        std::map< int, std::function<void(int)> > myMap = { { 1, std::bind( &A::func1, this, _1 ) }, 
+                                                            { 2, std::bind( &A::func2, this, _1 ) } };
+        
+        std::map<int, std::function< void( A*, int )> > myMap2 = { { 1, &A::func1 }, { 2, &A::func2 } };
+		// do whatever
+	}
+};
+
+int main() {}
