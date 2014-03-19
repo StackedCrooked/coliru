11257df19a5af3e6c72ddbd0a864cce60c9437e1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-19 07:57:28 +0000

    Update archive 78

---
+++ trunk/Archive2/78/f1735974103fd1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class Base
+{
+public:
+    void testy() { test(); }
+    virtual void test() {std::cout << "Base" << std::endl; }
+};
+
+class Derived : public Base
+{
+public:
+    void test() { std::cout << "Derived" << std::endl; }
+};
+
+int main()
+{
+    Derived d;
+    Base& b = d;
+    d.testy();
+    b.testy();
+}
