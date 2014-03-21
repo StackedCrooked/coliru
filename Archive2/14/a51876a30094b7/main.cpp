1743e214819a89a000832ce371a5aba86caf90fe
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-21 12:38:34 +0000

    Update archive 14

---
+++ Archive2/14/a51876a30094b7/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    double v = 10.0/3.;
+    std::cout << v << '\n';
+    std::cout << std::setw(5) << std::setprecision(2) << v << '\n';
+    std::cout << std::setw(5) << std::setprecision(2) << std::fixed << v << '\n';
+}
