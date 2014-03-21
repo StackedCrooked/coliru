4f97d541c7b3dfba172424f6192a992a39a36c92
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-21 00:32:56 +0000

    Update archive be

---
+++ trunk/Archive2/be/bfe6464b324e2b/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <thread>
+#include <future>
+#include <cstdint>
+
+using namespace std;
+
+std::intmax_t parallel_sum(vector<int> const& V) {
+    auto const mid = begin(V) + V.size() / 2;
+    
+    auto sum1 = std::async(std::launch::async, [&V, mid](){
+      return std::accumulate(begin(V), mid, std::intmax_t(0));
+    });
+    
+    auto sum2 = std::accumulate(mid, end(V), std::intmax_t(0));
+    return sum1.get() + sum2;
+}
+
+int main() {
+    std::vector<int> V (10000000);
+    std::generate(begin(V), end(V), &rand);
+    cout << parallel_sum(V) << endl;
+    cout << std::accumulate(begin(V), end(V), std::intmax_t(0)) << endl;
+}
