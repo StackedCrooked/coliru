839ba9bc67fefca4cc45358f341d7349b9861fbf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-21 12:29:09 +0000

    Update archive d2

---
+++ Archive2/d2/6288c826582dad/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+template<typename T>
+class Tree{
+    using value_type = T;                   // member alias
+    enum Policy { rb, splay, treeps };      // member enum
+    class Node{                             // member class
+        Node* right;
+        Node* left;
+        value_type value;
+    public:
+        void f(Tree*);
+    };
+    Node* top;
+    
+public:
+    void g(Node*);
+};
+
+template<typename T>
+void Tree<T>::Node::f(Tree* p)
+{
+  //top = right;                            // error: no object of type tree specified
+    p->top = right;                         // OK
+    value_type v = left->value;             // OK: value_type is not associated with an object
+}
+
+template<typename T>
+void Tree<T>::g(Tree::Node* p)
+{
+  //value_type val = right->value;          // error: no object of type Tree::Node
+    value_type v = p->right->value;         // error: Node::right is private
+    p->f(this);                             // OK
+}    
+
+int main()
+{   
+    Tree<int>().g(nullptr);
+}
