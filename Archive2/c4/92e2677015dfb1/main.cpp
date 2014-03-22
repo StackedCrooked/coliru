b629d05941b3b92f8f2ef9192174fbcdf1716ee8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-22 00:29:27 +0000

    Update archive c4

---
+++ Archive2/c4/92e2677015dfb1/main.cpp
@@ -0,0 +1,17 @@
+
+
+// Quiz: how to define a macro that can "remember" the name passed to the previous call? (Using void for the first.)
+#define REGISTER_TYPE(Name) struct Name { /* previous_type */ };
+
+
+REGISTER_TYPE(X) // should generate: struct X { typedef void previous_type; };
+REGISTER_TYPE(Y) // should generate: struct Y { typedef X previous_type; };
+REGISTER_TYPE(Z) // should generate: struct Z { typedef Y previous_type; };
+
+
+// these tests must pass
+static_assert(std::is_same<X::previous_type, void>::value, "");
+static_assert(std::is_same<Y::previous_type, X>::value, "");
+static_assert(std::is_same<Z::previous_type, Y>::value, "");
+
+
