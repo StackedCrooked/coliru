a61b9d66cabf8a2bf34be1aaf1320cfeb92e13d2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-24 12:41:33 +0000

    Update archive 2d

---
+++ trunk/Archive2/2d/8a4bafe380b78b/main.cpp
@@ -0,0 +1,16 @@
+#include <boost/range/adaptor/strided.hpp>
+#include <boost/range/adaptor/reversed.hpp>
+#include <iostream>
+#include <vector>
+
+int main()
+{
+  std::vector<int> input = {1,2,3,4,5,6,7,9,9,10};
+
+  for(const auto& element : boost::adaptors::reverse(input))
+    std::cout << element << '\n';
+  std::cout << '\n';
+
+  for(const auto& element : boost::adaptors::stride(input,2))
+    std::cout << element << '\n';
+}
