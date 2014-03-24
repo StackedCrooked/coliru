2077cd6e4b1320aa56027c27f0a3f09c4f0eba8f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-24 12:33:31 +0000

    Update archive b4

---
+++ Archive2/b4/e1e2a0a0ba51b8/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+
+enum X
+{
+    CL_DEVICE_TYPE_GPU,
+    CL_DEVICE_NAME
+};
+
+namespace cl
+{
+    struct Device
+    {
+        template<X x>
+        char const* getInfo()
+        {
+            return "hello world";
+        }
+    };
+    
+    struct Platform
+    {
+        static void get(std::vector<Platform>* p)
+        {
+            static std::vector<Platform> platforms;
+            *p = platforms;
+        }
+        
+        void getDevices(X, std::vector<Device>* p)
+        {
+            static std::vector<Device> devices;
+            *p = devices;
+        }
+    };
+}
+
+template <typename T>
+class Foo {
+    public:
+    std::vector<cl::Platform> platforms;
+    std::vector<cl::Device> devices;    
+    Foo();   
+    void bar()
+    {
+        // Fails to compile:  error: expected expression
+        //std::cout << devices[0].getInfo<CL_DEVICE_NAME>() << std::endl;
+        // Works
+        std::cout << this->devices[0].getInfo<CL_DEVICE_NAME>() << std::endl;
+        // Also works
+        cl::Device &mydevice = devices[0];
+        std::cout << mydevice.getInfo<CL_DEVICE_NAME>() << std::endl;
+    }
+};
+
+template<typename T>
+Foo<T>::Foo() {      
+    cl::Platform::get(&platforms);
+    platforms[0].getDevices(CL_DEVICE_TYPE_GPU, &devices);
+}
+
+/*template<typename T>
+void Foo<T>::bar() {
+    // Fails to compile:  error: expected expression
+    //std::cout << devices[0].getInfo<CL_DEVICE_NAME>() << std::endl;
+    // Works
+    std::cout << devices[0].getInfo<CL_DEVICE_NAME>() << std::endl;
+    // Also works
+    cl::Device &mydevice = devices[0];
+    std::cout << mydevice.getInfo<CL_DEVICE_NAME>() << std::endl;
+
+}*/
+
+int main() {
+    Foo<double> foo = Foo<double>();    
+    foo.bar();    
+    return 0;
+}
