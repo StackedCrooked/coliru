c3402c0b0a4687978f225a0b0dcbf4fdf2633e8d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-25 12:43:35 +0000

    Update archive 7a

---
+++ trunk/Archive2/7a/3f75e5dad8418b/main.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>                                                                                                            
+#include<string.h>
+
+int main()
+{
+    int str;
+    int input;
+    scanf(input, "Test", str);
+    printf("Hello world!");
+}
