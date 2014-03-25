e599fe9820a59e81146461c9d716690503318862
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-25 18:46:18 +0000

    Update archive 20

---
+++ trunk/Archive2/20/4b3e2329d6efd0/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#define VIRTUAL
+
+struct Data {
+    int x;
+};
+
+struct Object {
+    std::unique_ptr<Data> data;
+    
+    Object(std::unique_ptr<Data> input) : data(std::move(input)) { }
+    Object() : Object(std::unique_ptr<Data>{}) { }
+        
+//    virtual ~Object() { }
+
+    /*virtual*/ void describe()
+    { }
+};
+
+int main(int, char *[])
+{
+    std::vector<Object> objects;
+    objects.push_back(Object{});
+    return 0;
+}
