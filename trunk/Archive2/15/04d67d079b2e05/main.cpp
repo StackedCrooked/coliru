1a294feb517d52c321e7fe7d1ed4a0801af25282
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-25 12:52:15 +0000

    Update archive 15

---
+++ trunk/Archive2/15/04d67d079b2e05/main.cpp
@@ -0,0 +1,41 @@
+#define BOOST_SPIRIT_USE_PHOENIX_V3
+#include <boost/fusion/adapted/boost_tuple.hpp>
+#include <boost/spirit/include/qi.hpp>
+
+namespace qi = boost::spirit::qi;
+namespace phx = boost::phoenix;
+
+template <typename Int>
+void do_test()
+{
+    for (Int const testcase : { 
+            std::numeric_limits<Int>::min(),
+            Int(),
+            std::numeric_limits<Int>::max()
+            })
+    {
+        auto const input = std::to_string(testcase);
+
+        auto f(input.begin()), l(input.end()); 
+
+        Int parsed = 0;
+        bool ok = qi::parse(f, l, qi::auto_, parsed);
+
+        if (!ok || f!=l)
+            throw std::runtime_error("parse error");
+
+        std::cout << std::boolalpha << (testcase==parsed) << "\t" << std::to_string(testcase) << " -> " << std::to_string(parsed) << "\n";
+    }
+}
+
+int main()
+{
+    do_test<int16_t>();
+    do_test<uint16_t>();
+    do_test<int32_t>();
+    do_test<uint32_t>();
+    do_test<int64_t>();
+    do_test<uint64_t>();
+    do_test<intmax_t>();
+    do_test<uintmax_t>();
+}
