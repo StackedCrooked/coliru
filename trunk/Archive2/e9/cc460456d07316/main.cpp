d2d2babceb94befa95d93657d6bf50ef3f179e72
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-26 06:58:35 +0000

    Update archive e9

---
+++ trunk/Archive2/e9/cc460456d07316/main.cpp
@@ -0,0 +1,20 @@
+template<class> struct some_trait;       // primary template
+
+template <class T>
+struct A : some_trait<T> {               // (1)
+  C a;
+};
+
+template<> struct some_trait<struct B> { // (2)
+  class C { };
+};
+
+struct B : A<B> {                        // (3)
+  C b;
+};
+
+int
+main (int argc, char *argv[])
+{
+  B a; // legal
+}
