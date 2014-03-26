42bac44b202a68affb9e91a58f78d5ff3ee572b9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-26 12:43:33 +0000

    Update archive 20

---
+++ Archive2/20/b9d1c48a3a100a/main.cpp
@@ -0,0 +1,27 @@
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/repository/include/qi_advance.hpp>
+#include <fstream>
+
+namespace qi   = boost::spirit::qi;
+namespace repo = boost::spirit::repository::qi;
+
+int main()
+{
+    std::ifstream ifs("test.cpp");
+    ifs >> std::noskipws;
+
+    boost::spirit::istream_iterator f(ifs), l;
+
+    std::string parsed;
+    bool ok = qi::parse(
+            f, l, 
+               qi::omit [ *(qi::char_ - (qi::eol >> "int main")) >> qi::eol ]
+            >> +(qi::char_ - qi::eol),
+            parsed
+        );
+
+    if (ok)
+        std::cout << "Found: '" << parsed << "'\n";
+    else
+        std::cout << "Fail at: '" << std::string(f,l) << "'\n";
+}
