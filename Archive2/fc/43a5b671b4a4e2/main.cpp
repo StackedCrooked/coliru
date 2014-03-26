30d974da2976d5e61af3f21dfc572259969c18c5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-26 06:46:32 +0000

    Update archive fc

---
+++ Archive2/fc/43a5b671b4a4e2/main.cpp
@@ -0,0 +1,10 @@
+#include "boost/date_time/posix_time/time_formatters_limited.hpp"
+#include <string>
+
+using namespace boost::posix_time;
+
+int main()
+{
+    ptime now = second_clock::local_time();
+    std::cout << to_simple_string(now) << std::endl;
+}
