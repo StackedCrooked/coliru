91fc817aa5fcec3e9b5ebb8573e775c4c1a8d195
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-26 19:00:09 +0000

    Update archive f9

---
+++ trunk/Archive2/f9/2eaa8eb327a97d/main.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(void)
+{
+  int i;
+  int n;
+  int k;
+  int sum1;
+  int sum2;
+  
+  do
+  {
+    printf("Zahl eingeben (0 fuer Ende): ");
+    scanf("%i\n",&k);
+    for(i>0; i=0; i++)
+      sum1=+i;
+    {
+      for(n<0; n=0; n++)
+    sum2=+n;
+	
+    }
+  }while(k!=0);
+  printf("Anzahl positiver Zahlen: %i \n",i);
+  printf("Anzahl ngativer Zahlen: %i \n",n);
+  
+  return 0;
+}
