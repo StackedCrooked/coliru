87772d82942bff38c6d3e472bf0004b0f591f3c6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-27 19:03:48 +0000

    Update archive 95

---
+++ trunk/Archive2/95/4eb7e4a9f11cd3/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <typeinfo>
+
+void show(const int (&array)[10]) {
+    for (int& i : array)
+        i = 0;
+}
+
+int main() {
+    int x[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    show(x);
+}
