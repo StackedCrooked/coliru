bb47e23a585488146f97c5b59f5a3cbac93ffada
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-28 01:01:52 +0000

    Update archive 4e

---
+++ Archive2/4e/084e46e589425b/main.cpp
@@ -0,0 +1,56 @@
+#include <typeinfo>
+#include <iostream>
+#include <functional>
+using namespace std;
+
+using FuncPtr = void(*)(int);
+
+template<typename T>
+void Fun(T t);
+
+
+auto Glambda = [](auto a_){ cout << a_ << endl;};
+
+template<>
+void Fun<>(function<void(int)> function_)
+{
+   function_(10);
+}
+
+template<>
+void Fun<>(FuncPtr funptr_)
+{
+   funptr_(10);
+}
+
+template<>
+void Fun<>(decltype(Glambda) a_)
+{
+   a_(19);
+   a_(11.2);
+   a_("it's fun! Can't help loving c++");
+}
+
+
+void test()
+{
+   int local;
+   // Fun([]{}); // New type
+   Fun(Glambda);
+
+   // FuncPtr fptr = [local](auto a){cout << a << endl;}; //Not a ptr to func
+   FuncPtr fptr = [](auto a){cout << a << endl;};
+   Fun(fptr);
+
+   function<void(int)> funcTion = [](auto a){ cout << a << endl;};
+   function<void(int)> funcTion2 = [local](auto a){ cout << a << endl;};
+   Fun(funcTion);
+   Fun(funcTion2);
+
+
+}
+
+
+int main(){
+   test();
+}
