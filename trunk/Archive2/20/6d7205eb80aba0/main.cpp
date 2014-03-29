46d0a5cff1f538a3f92a7058b05ed66c9933df23
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-29 00:49:17 +0000

    Update archive 20

---
+++ trunk/Archive2/20/6d7205eb80aba0/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct myType{int a; int b; int c;
+};
+std::vector<myType> myVector(100);
+bool myBool = true;
+
+void myFunction(){
+    for(unsigned i = 0; i < myVector.size(); ++i) {
+        myVector[i].a = 5;
+    }
+    if(myBool) {
+        std::cout << "bar" << std::endl;
+    }
+    return;
+}
+
+int main() {
+    myFunction();
+}
