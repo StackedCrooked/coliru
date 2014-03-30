d8549c361bb27ff6784cc82ed16625c18239ce74
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-30 19:02:46 +0000

    Update archive f7

---
+++ trunk/Archive2/f7/a27b9202d52c59/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+struct S
+{
+    S() { std::cout << i++ << std::endl; }
+	static int i;
+//private:
+	S( const S& ) { std::cout << "copy" << std::endl; }
+};
+
+int S::i = 0;
+
+int main() {
+	
+	std::vector<S> s(10);
+	return 0;
+}
