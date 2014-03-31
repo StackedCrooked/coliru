12b858072ba2cda8af83f9b622191d55c4d22171
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-03-31 01:00:38 +0000

    Update archive 80

---
+++ trunk/Archive2/80/b9c15b9d449ed3/main.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+
+template<class... P>
+void g(P&&...) {
+    std::cout << "generic\n";
+}
+
+template<class T>
+void f(T&& value) {
+    g(value);
+}
+
+// Remove the alias here and comment out namespace B
+// for g(E&) to be called
+namespace B {
+    enum class E : unsigned {};
+}
+using E = B::E;
+
+void g(E&) {
+    std::cout << "B::E\n";
+}
+
+signed main() {
+    f(E{});
+    return 0;
+}
