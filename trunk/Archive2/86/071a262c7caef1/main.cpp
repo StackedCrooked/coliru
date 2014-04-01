e2cf1ffeb69b99cd8edec07143cfaad36c19bec6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-01 07:11:04 +0000

    Update archive 86

---
+++ trunk/Archive2/86/071a262c7caef1/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+class Base
+{
+public:
+    virtual void Foo()
+    {
+        std::cout << __PRETTY_FUNCTION__ << '\n';
+    }
+    virtual void Bar()
+    {
+        std::cout << __PRETTY_FUNCTION__ << '\n';
+    }
+};
+
+class Derived1 : public Base
+{
+public:
+    void Foo()
+    {
+        std::cout << __PRETTY_FUNCTION__ << '\n';
+    }
+    
+    void Bar()
+    {
+        std::cout << __PRETTY_FUNCTION__ << '\n';
+    }
+};
+        
+class Derived2 : public Base
+{
+public:
+    void Foo()
+    {
+        std::cout << __PRETTY_FUNCTION__ << '\n';
+    }
+    
+    void Bar()
+    {
+        std::cout << __PRETTY_FUNCTION__ << '\n';
+    }
+};
+
+class OtherClass
+{
+public:
+    Base* obj;
+    void (Base::*method)();
+    
+    void Add( Base* _obj, void (Base::*_method)() )
+    {
+        obj = _obj;
+        method = _method;
+    }
+    
+    void Run()
+    {
+        (obj->*method)();
+    }
+};
+
+int main()
+{
+    Derived1 d1;
+    Derived2 d2;
+    
+    OtherClass other;
+    
+    other.Add( &d1, &Base::Foo );
+    other.Run();
+    
+    other.Add( &d2, &Base::Bar);
+    other.Run();
+}
