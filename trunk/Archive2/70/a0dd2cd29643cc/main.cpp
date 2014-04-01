791e362923dbccb265ebdee113c3d0a52d33a3f1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-01 13:17:49 +0000

    Update archive 70

---
+++ trunk/Archive2/70/a0dd2cd29643cc/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+enum MyEnum1 {ZERO = 0, ONE = 1, TWO = 2};
+enum MyEnum2 {THREE = 3, FOUR = 4, FIVE = 5};
+
+int blah( int one ){ int i = one; return i; }
+int guff( int two ){ int i = two; return i; }
+
+int main()
+{   
+    int (*pFunc[2]) ( int );
+    
+    pFunc[0] = blah;
+    pFunc[1] = guff;
+    
+    std::cout << (*pFunc)(ONE) << std::endl;
+}
