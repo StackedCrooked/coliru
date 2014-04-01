2d87155b7dfcb081c3a4388f6106b42455f13011
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-01 07:18:35 +0000

    Update archive 12

---
+++ Archive2/12/678424382a3e35/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <cassert>
+ 
+#define ASSERT_EQ(a, b) assert_eq(a, b, __FILE__, __LINE__)
+
+template <typename T1, typename T2>
+void assert_eq(const T1 & a, const T2 & b, const char * file, int line) {
+    if (a != b) {
+        std::cout << "assertion failed at " << file << ":" << line << ": " << a << " != " << b << "\n";
+    	std::abort();
+    }
+}
+
+int main() {
+	int x = 42;
+	unsigned long long y = 123;
+    assert(x == 42);
+    assert(y == 123);
+	ASSERT_EQ(x, 42);
+	ASSERT_EQ(y, 123);
+}
