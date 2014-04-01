e8a996d05c3d33749a5d351e4ae135df2975e329
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-01 13:09:00 +0000

    Update archive a1

---
+++ Archive2/a1/eec1ef7554e4a4/main.cpp
@@ -0,0 +1,22 @@
+#include <memory>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+int main() {
+    std::vector<std::unique_ptr<int>> vec;
+    vec.emplace_back(new int(7));
+    vec.emplace_back(new int(8));
+    vec.emplace_back(new int(9));
+    vec.emplace_back(new int(10));
+    vec.emplace_back(new int(11));
+    vec.emplace_back(new int(12));
+    
+    bool remove_10 = true;
+    vec.erase(std::remove_if(std::begin(vec), std::end(vec), [&](const std::unique_ptr<int>& x) {
+        return remove_10 && x && *x == 10;
+    }), std::end(vec));
+    for(auto&& i : vec) {
+        std::cout << *i << '\n';
+    }
+}
