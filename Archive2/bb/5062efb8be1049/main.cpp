0126c21215490647d533923a81d1df02d4d828b9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-02 19:18:22 +0000

    Update archive bb

---
+++ Archive2/bb/5062efb8be1049/main.cpp
@@ -0,0 +1,14 @@
+template<typename T, typename U>
+struct OuterInner<T, U> {};
+
+template<typename T>
+struct Outer {
+    template<typename U>
+    using Inner = OuterInner<T, U>;
+};
+
+template<typename T, typename U>
+struct OuterInner<T, int> {};
+
+int main() {
+}
