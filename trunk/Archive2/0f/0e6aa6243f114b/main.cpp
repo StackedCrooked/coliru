2977add2cd3170e08da3a808f41524491ebf7eff
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-04 09:52:28 +0000

    Update archive 0f

---
+++ trunk/Archive2/0f/0e6aa6243f114b/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+    int i1 = -3;
+    int i2 = 0;
+    int i3 = 3;
+    
+    std::cout << std::abs(i1) << std::endl;
+    std::cout << std::abs(i2) << std::endl;
+    std::cout << std::abs(i3) << std::endl;
+}
