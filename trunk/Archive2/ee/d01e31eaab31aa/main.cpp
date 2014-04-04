1bbe1335ef0f2e12f87e50f8ddd8198532d02fa4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-04 09:58:51 +0000

    Update archive ee

---
+++ trunk/Archive2/ee/d01e31eaab31aa/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename ValueType, ValueType value_>
+struct generic_value
+{ static ValueType value; };
+
+template <typename ValueType, ValueType value_>
+ValueType generic_value<ValueType,value_>::value = value_; // initialize static template-class member
+
+//////////
+
+int f() { return 1111; }
+float g() { return 2.22; }
+
+
+
+template <typename T>
+struct B
+{};
+
+template <>
+struct B<float>
+{
+   static auto constexpr funptr = &g; // this only works withh gcc, not with any VS version
+   typedef generic_value<decltype(&g),&g> func_ptr_type;
+};
+
+template <>
+struct B<int>
+{
+   static auto constexpr funptr = &f;
+   typedef generic_value<decltype(&f),&f> func_ptr_type;
+};
+
+
+
+int main()
+{
+   std::cout << B<float>::func_ptr_type::value() << std::endl;
+   std::cout << B<int>::func_ptr_type::value() << std::endl;
+
+   std::cout << B<float>::funptr() << std::endl;
+   std::cout << B<int>::funptr() << std::endl;
+
+   return 0;
+}
