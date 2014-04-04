686a273a450a092f4136696ec8242e4790cb785e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-04 09:52:06 +0000

    Update archive 18

---
+++ Archive2/18/19b72e8c56cb16/main.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <iostream>
+#include <utility>
+
+struct A {
+    std::string s;
+    
+    A() {}
+    
+    A(A const& other) : s(other.s) {
+        std::cout << "copy construct" << std::endl;
+    }
+    
+    A(A&& other) : s(std::move(other.s)) {
+        std::cout << "move construct" << std::endl;
+    }
+};
+
+struct B {
+    A a;
+    
+    explicit B(A a) : a(std::move(a)) {}
+};
+ 
+int main()
+{
+    A a;
+    B b1{ a };
+    A b2{ A() };
+}
