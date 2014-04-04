e2db5b7bce072aa1d03d87383c9eead3d806e6a8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-04 15:43:34 +0000

    Update archive 52

---
+++ trunk/Archive2/52/30d95dafc5baba/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <regex>
+
+int main()
+{
+    std::string str{"not it, it\x88\x89 and not it\x90\x09. Pitch. \n And \"hidden\" it\xfe\xff\n"};
+    std::regex pat{"it[\\x80-\\xff]{2,2}"};
+    for (std::sregex_iterator p(str.begin(), str.end(), pat); p!=std::sregex_iterator{}; ++p) {
+        std::string mymatch = (*p)[0];
+        std::cout << "0x" << std::hex <<  static_cast<unsigned>(0xff & mymatch[2])  << std::endl;
+    }
+    return 0;
+}
