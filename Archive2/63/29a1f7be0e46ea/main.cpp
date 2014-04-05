d54883a645b7d9281b1237040785e677a7f358f6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-05 13:07:04 +0000

    Update archive 63

---
+++ Archive2/63/29a1f7be0e46ea/main.cpp
@@ -0,0 +1,28 @@
+#include <boost/thread.hpp>
+#include <cstdio>
+
+struct StreamX
+{
+    StreamX() { puts(__FUNCTION__); }
+   ~StreamX() { puts(__FUNCTION__); }
+};
+
+static boost::thread_specific_ptr<StreamX> StreamThreadSpecificPtr;
+
+void thread_proc() {
+    StreamX* stream = NULL;
+    stream = StreamThreadSpecificPtr.get();
+    if (NULL == stream) {
+        stream = new StreamX();
+        StreamThreadSpecificPtr.reset(stream);
+    }
+    printf("%p\n", stream);
+}
+
+int main() {
+    boost::thread_group threads;
+    for(int i = 0; i < 5; i ++)
+        threads.create_thread(&thread_proc);
+
+    threads.join_all();
+}
