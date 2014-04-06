12eef6e3b3a82e680e09179b0d5b19222e5cc475
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-06 19:17:32 +0000

    Update archive a5

---
+++ trunk/Archive2/a5/f9b8928382ae52/main.cpp
@@ -0,0 +1,66 @@
+#include <memory>
+#include <queue>
+#include <boost/shared_ptr.hpp>
+
+
+struct message
+{
+    virtual ~message() {}
+    virtual message* clone() const = 0;
+};
+
+struct myMessage : message
+{
+    myMessage(int,int,int) {};
+    myMessage* clone() const { return new myMessage(*this); }
+};
+
+#ifdef HAS_CPLUSPLUS11
+    typedef std::unique_ptr<message> msg_pointer_t;
+#else
+    typedef boost::shared_ptr<message> msg_pointer_t;
+#endif
+
+std::queue<msg_pointer_t> messages;
+
+
+
+void sendMessageA(const message& msg)
+{
+    msg_pointer_t p(msg.clone());
+    messages.push(p);
+}
+
+void sendMessageB(const msg_pointer_t& p)
+{
+    messages.push(p);
+}
+
+#ifdef HAS_CPLUSPLUS11
+    template <typename T, typename... Args>
+    void sendMessageC(Args... args)
+    {
+        messages.emplace(new T(args...));
+    }
+}
+#endif
+
+int main()
+{
+    // Current usage.
+    // 
+    // Wasteful due to the need for clone, but that's
+    // the way the API is.
+    sendMessageA(myMessage(3,4,5));
+    
+    // Better, C++03-compliant usage which doesn't need
+    // copies, though there's still a refcount increment
+    // and I don't like the verbosity at the call site.
+    sendMessageB(msg_pointer_t(new myMessage(3,4,5)));
+    
+    #ifdef HAS_CPLUSPLUS11
+        // Ideal, right? May be able to sell this if I'm
+        // migrating to C++11 anyway.
+        sendMessageC<myMessage>(3,4,5);
+    #endif
+}
