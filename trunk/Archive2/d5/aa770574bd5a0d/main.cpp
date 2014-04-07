f9ef433bd45e5dd248e6c9a92c68339fa2811e55
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-07 19:29:54 +0000

    Update archive d5

---
+++ trunk/Archive2/d5/aa770574bd5a0d/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template <typename T>
+using stringpair = std::pair<std::string, T>;
+
+int main (int argc, char* argv[]) {
+    return 0;
+}
