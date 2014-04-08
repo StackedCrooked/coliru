9d16f34d815c38f8286188a4ae25a0ee31958a95
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-08 01:26:24 +0000

    Update archive 23

---
+++ trunk/Archive2/23/54307e9787c4c4/main.cpp
@@ -0,0 +1,95 @@
+#footer-logo, #footer-legal, #room-ad, #toggle-favorite, .ignored {
+    display: none;
+}
+
+#sidebar {
+    color: #eee;
+}
+
+.button, .tag {
+    background-color: #455667;
+    border: none;
+    border-radius: 0;
+    box-shadow: none;
+    color: #eee;
+    font-family: inherit;
+}
+
+.catchup-marker {
+    border-top: none;
+}
+
+.messages {
+    background: none;
+    margin-right: 0;
+    color: #eee;
+}
+
+.mine {
+    background-color: #444;
+}
+
+.monologue {
+    border-radius: 0;
+    color: #eee;
+    margin-top: -3px;
+    padding-top: 0;
+}
+
+.signature .avatar {
+    position: relative;
+    top: 2px;
+}
+
+.username {
+    font-size: 10px;
+}
+
+a, .more {
+    color: #7fbbe2;
+}
+
+a:hover, .more:hover {
+    color: #eee;
+}
+
+body {
+    font-size: 12px;
+    color: #eee;
+}
+
+body, #input-area {
+    background: #333;
+}
+
+#sidebar #info #roomtitle {
+    text-shadow: 0px 1px 0px #333;
+}
+
+.ob-post {
+    color: #eee;
+    background-color: #444;
+}
+
+div.message.reply-parent, div.message.reply-child {
+    background-color: #555;
+}
+
+#input {
+    background-color: #444;
+    color: #eee;
+}
+
+.ob-tweet, .ob-twitteruser {
+    background-color: #444;
+    color: #eee;
+}
+
+.ob-tweet .ob-status-text {
+    color: #eee;
+}
+
+.onebox, .ob-message {
+    color: #eee;
+    background-color: #444;
+}
