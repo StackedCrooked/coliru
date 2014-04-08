6d06777cda89dc125816280636d21e09086dbbe4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-08 13:28:44 +0000

    Update archive 84

---
+++ trunk/Archive2/84/8a529540ee5b5d/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <stdint.h>
+
+using namespace std;
+
+static bool my_isnan(double val) {
+    union { double f; uint64_t x; } u = { val };
+	return (u.x << 1) > 0x7ff0000000000000u;
+}
+
+int main() {
+	cout << std::isinf(std::log(0.0)) << endl;
+	cout << std::isnan(std::sqrt(-1.0)) << endl;
+	cout << my_isnan(std::sqrt(-1.0)) << endl;
+    cout << __isnan(std::sqrt(-1.0)) << endl;
+
+	return 0;
+}
+
