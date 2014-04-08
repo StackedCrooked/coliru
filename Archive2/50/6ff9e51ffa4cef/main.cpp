2b9e9802a42fccb069f55ec5d1d4c7860c98c0cb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-08 07:25:56 +0000

    Update archive 50

---
+++ Archive2/50/6ff9e51ffa4cef/main.cpp
@@ -0,0 +1,25 @@
+#include <type_traits>
+
+template<typename FUN, typename... ARGS>
+struct eval_impl;
+
+template<template<typename...> class F , typename... PLACEHOLDERS , typename... ARGS>
+struct eval_impl<F<PLACEHOLDERS...>,ARGS...> : public F<ARGS...> {};
+
+template<typename EXPRESSION, typename... ARGS>
+using eval = typename eval_impl<EXPRESSION, ARGS...>::type;
+
+struct _1 {};
+struct _2 {};
+struct _3 {};
+struct _4 {};
+
+template<typename ARG, typename... ARGS>
+struct first_of { using type = ARG; };
+
+using call = eval<first_of<_1,_2,_3,_4>, bool, float, char, int>;
+
+int main ()
+{
+    static_assert(std::is_same<call, bool>{}, "Oops!");
+}
