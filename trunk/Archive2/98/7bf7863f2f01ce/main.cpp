ca590b01037eb007449b1817d3882a0123bf4da2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-09 07:29:47 +0000

    Update archive 98

---
+++ trunk/Archive2/98/7bf7863f2f01ce/main.cpp
@@ -0,0 +1,13 @@
+int main() {
+    int count = Convert.ToInt(cond1) + Convert.ToInt(cond2) + ... + Convert.ToInt(cond5);
+    switch(count) 
+    {
+    case 1: 
+        //code 1
+        break;
+    case 2: 
+        //code 2
+        break;
+    .... and so on...
+    }
+}
