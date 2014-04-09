0864fb70340f94844464929a86075594c534e231
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-09 14:39:03 +0000

    Update archive 61

---
+++ trunk/Archive2/61/f0969261ea10a9/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <boost/preprocessor.hpp>
+
+#define PRINT_ONE(elem) BOOST_PP_STRINGIZE(elem) " = " << elem
+#define PRINT_ONE_COMMA(r, data, elem) BOOST_PP_STRINGIZE(elem) " = " << elem << ", " <<
+
+#define LAST(seq) BOOST_PP_SEQ_ELEM(BOOST_PP_DEC(BOOST_PP_SEQ_SIZE(seq)), seq)
+#define REST(seq) BOOST_PP_SEQ_SUBSEQ(seq, 0, BOOST_PP_DEC(BOOST_PP_SEQ_SIZE(seq)))
+#define PRINT(seq) std::cout << BOOST_PP_SEQ_FOR_EACH(PRINT_ONE_COMMA, _ ,REST(seq)) PRINT_ONE(LAST(seq)) << '\n';
+
+int main() {
+    int a = 5, b = 3, c = 7;
+    double d=3.5;
+    PRINT((a)(b)(c)(d));
+}
