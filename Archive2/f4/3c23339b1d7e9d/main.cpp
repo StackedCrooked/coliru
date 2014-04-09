d348c2b86cb00da3fd9580b234d8f209c17dde79
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-09 20:56:50 +0000

    Update archive f4

---
+++ Archive2/f4/3c23339b1d7e9d/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+  int JESSICA,QT;
+  printf("OLA JESSICA\n");
+  JESSICA;
+printf("SABEMOS DE SEU SEGREDO!\n");
+printf("QUANTAS VEZES VOCE VAI AO BANHEIRO POR DIA:\? \n" ,QT);
+scanf("%d",&QT);
+  QT=QT;
+  printf("NOSSA, VOCE EH UMA CAGALHONA HAHA\n");
+  printf("CAGA %d VEZES POR DIA" ,QT);
+  system("pause");
+}
