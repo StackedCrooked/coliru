20e0efd3d15228f4b9219fbe48bbcfe849edd9db
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-10 20:14:18 +0000

    Update archive d9

---
+++ trunk/Archive2/d9/a9e799791361a7/main.cpp
@@ -0,0 +1,14 @@
+#include <functional>
+#include <iostream>
+using namespace std;
+
+using cmdf = function<void()>;
+template <typename T> using alias = T;
+
+int main()
+{
+    struct _ { cmdf undo, redo; } static const cmd { []{cout<<"pain";}, []{cout<<"relief";} };
+
+    for (int i=1; i<100000; i*=42)
+        (cmd.*alias<cmdf _::*[]> { &_::undo, &_::redo }[rand()%2])();
+}
