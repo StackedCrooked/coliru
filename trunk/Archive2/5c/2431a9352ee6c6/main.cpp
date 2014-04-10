758dc242ba10237773f8fc505e6cd9a68986ed9b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-10 14:42:37 +0000

    Update archive 5c

---
+++ trunk/Archive2/5c/2431a9352ee6c6/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <locale>
+#include <string>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time/posix_time/posix_time_io.hpp>
+
+int main()
+{
+  const boost::posix_time::ptime time = 
+      boost::posix_time::time_from_string("1981-08-20 08:05:00");
+
+  // ptime to string.
+  const std::string str_time = to_simple_string(time);
+  std::cout << str_time << std::endl;
+
+  // ptime to stringstream to string.
+  std::stringstream stream;
+  stream << time;
+  std::cout << stream.str() << std::endl;
+  stream.str("");
+
+  // Using a facet for a custom format to only display hour and minute.
+  boost::posix_time::time_facet* facet = new boost::posix_time::time_facet();
+  facet->format("%H:%M");
+  stream.imbue(std::locale(std::locale::classic(), facet));
+  stream << time;
+  std::cout << stream.str() << std::endl;
+}
