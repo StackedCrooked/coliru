7a857d7339f544ff39cdbcba086ea8a3791b906d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-11 20:01:04 +0000

    Update archive a3

---
+++ Archive2/a3/6e6c6cdcd0cdf2/main.cpp
@@ -0,0 +1,30 @@
+#include <thread>
+#include <atomic>
+#include <cassert>
+#include <string>
+ 
+std::atomic<std::string*> ptr;
+int data;
+ 
+void producer()
+{
+    std::string* p  = new std::string("Hello");
+    data = 42;
+    ptr.store(p, std::memory_order_release);
+}
+ 
+void consumer()
+{
+    std::string* p2;
+    while (!(p2 = ptr.load(std::memory_order_acquire)))
+        ;
+    assert(*p2 == "Hello"); // never fires
+    assert(data == 42); // never fires
+}
+ 
+int main()
+{
+    std::thread t1(producer);
+    std::thread t2(consumer);
+    t1.join(); t2.join();
+}
