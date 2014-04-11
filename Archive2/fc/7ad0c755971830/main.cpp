53bf2b103598bf3466f7dad9876e3cecaa19fa3e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-11 02:12:49 +0000

    Update archive fc

---
+++ Archive2/fc/7ad0c755971830/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+template <size_t... Ns> struct int_seq{ };
+template <size_t... Ns> struct make_int_seq;
+template <size_t N, size_t... Ns> struct make_int_seq<N, Ns...>
+                                              : make_int_seq<N - 1, N, Ns...>{ };
+template <size_t... Ns> struct make_int_seq<0, Ns...>
+{ using type = int_seq<0, Ns...>; };
+
+template <size_t... Ns>
+void show(int_seq<Ns...> const &seq)
+{ std::vector<int>{ (std::cout << Ns << " ", 0)... }; }
+
+int main() 
+{
+    auto seq(make_int_seq<10>::type{});
+    show(seq);
+}
