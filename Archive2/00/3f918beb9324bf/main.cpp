0b8ead0e072205bad9157a8370543cee9d4b552f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-11 14:04:46 +0000

    Update archive 00

---
+++ Archive2/00/3f918beb9324bf/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+class B
+{
+  public:
+    B() : m(17)
+    {
+        std::cout << "B()" << std::endl;
+    }
+
+    B(const B & o) : m(o.m)
+    {
+        std::cout << "B(const B& o)" << std::endl;
+    }
+    B & operator=(const B & o)
+    {
+        std::cout << "B & operator=(const B & o)" << std::endl;
+        m = o.m;
+        return *this;
+    }
+    int m;
+};
+
+int main()
+{
+    B b  = b;
+    int i = i;
+    
+    std::cout << b.m << " " << i << std::endl;
+}
