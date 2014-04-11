3d867cf61bfc6c34e9a36effa7ddedd1e91fbac8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-11 08:06:13 +0000

    Update archive 32

---
+++ Archive2/32/115fd327ca4c88/cmd.sh
@@ -0,0 +1,3 @@
+set -x;
+g++-4.8 -O0 -Wall -pedantic -Wextra -pthread -std=c++11 main.cpp && ./a.out
+clang++ -O0 -Wall -pedantic -Wextra -pthread -std=c++11 -stdlib=libc++ main.cpp -lsupc++ && ./a.out
