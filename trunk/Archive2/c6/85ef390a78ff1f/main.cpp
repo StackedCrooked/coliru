bbacf7daa024601b6d66e61258455b67ddc365b2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-11 08:03:18 +0000

    Update archive c6

---
+++ trunk/Archive2/c6/85ef390a78ff1f/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+template <typename T>
+inline T const& max (T const& a, T const& b)
+{
+    std::cout << "template" << '\n';
+    return a < b ? b : a;
+}
+
+template <typename T>
+inline T const& max (T const& a, T const& b, T const& c)
+{
+    return max (max(a,b), c);
+}
+
+inline int const& max (int const& a, int const& b)
+{
+    std::cout << "non-template" << '\n';
+    return a <b ? b : a;
+}
+
+int main()
+{
+    std::cout << max(3, 5, 7) << '\n';   
+}
