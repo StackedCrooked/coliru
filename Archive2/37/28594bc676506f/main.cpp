6dac79a8c4c13b82cb65c2797ee3ad15f8e6ef98
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-12 14:08:24 +0000

    Update archive 37

---
+++ Archive2/37/28594bc676506f/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+
+class Report
+{
+private:
+std::string str;
+public:
+Report(const std::string s) : str(s)
+{std::cout << "Object created!\n"; }
+~Report() { std::cout << "Object deleted!\n"; }
+void comment() const { std::cout << str << "\n";}
+};
+
+
+int main()
+{
+{
+std::auto_ptr<Report> ps (new Report("using auto_ptr"));
+ps -> comment();
+}
+{
+std::shared_ptr<Report> ps (new Report("using shared_ptr"));
+ps -> comment();
+}
+{
+std::unique_ptr<Report> ps (new Report("using unique_ptr"));
+ps -> comment();
+}
+system("pause");
+return 0;
+}
