8c5bd71ca50cdd524a3fd2dd9a3fd43701fc6aec
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-12 08:15:00 +0000

    Update archive b3

---
+++ Archive2/b3/4d8ea772cd22b0/cmd.sh
@@ -0,0 +1,3 @@
+clang++ -std=c++11 -stdlib=libc++ -O2 -Wall -Wextra -pedantic-errors main.cpp -lsupc++ && ./a.out <<< "fred flinstone"
+clang++ -std=c++11 -stdlib=libc++ -O2 -Wall -Wextra -pedantic-errors main.cpp -lsupc++ && ./a.out <<< "iron man"
+clang++ -std=c++11 -stdlib=libc++ -O2 -Wall -Wextra -pedantic-errors main.cpp -lsupc++ && ./a.out <<< "captain america"
