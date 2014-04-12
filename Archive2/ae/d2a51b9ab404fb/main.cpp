4486daaa92768b70a7b9cde5d8ff001d3838d757
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-12 08:10:12 +0000

    Update archive ae

---
+++ Archive2/ae/d2a51b9ab404fb/main.cpp
@@ -0,0 +1,22 @@
+class __attribute__((deprecated)) old_style_throw_specification;
+class old_style_throw_specification {};
+
+#define throw(...) throw(old_style_throw_specification, ##__VA_ARGS__)
+
+void f()
+{
+    throw 42;
+}
+
+void g() throw(int)
+{
+    throw 42;
+}
+
+void h() throw()
+{
+    throw 42;
+}
+
+int main()
+{}
