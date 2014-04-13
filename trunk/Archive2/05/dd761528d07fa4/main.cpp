ddf1c019461b15c105b977cca64c00983017eb89
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-13 08:09:16 +0000

    Update archive 05

---
+++ trunk/Archive2/05/dd761528d07fa4/main.cpp
@@ -0,0 +1,12 @@
+# Include 
+# include
+ 
+int principal ( ) 
+{ 
+    const  de char str [ ]  =  "Quantos personagens contém essa corda?" ;
+ 
+    printf ( "sem caráter nulo:% d \ n " , strlen ( str ) ) ; 
+    printf ( "com caractere nulo:% d \ n " , sizeof ( str ) ) ;
+ 
+    retornar  0 ; 
+}
