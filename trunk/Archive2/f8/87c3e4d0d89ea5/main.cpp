f050bef59b0ca268cc45debd9c6dbf55b41a86bd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-13 14:21:32 +0000

    Update archive f8

---
+++ trunk/Archive2/f8/87c3e4d0d89ea5/main.cpp
@@ -0,0 +1,40 @@
+template<typename T>
+struct has_x {
+    typedef char(&yes)[1];
+    typedef char(&no)[2];
+    
+    struct Fallback { char x; };
+    struct Derived : T, Fallback { };
+    
+    template<typename U, U>
+    struct Check;
+    
+    template<typename U>
+    static no test(Check<char Fallback::*, &U::x>*);
+    
+    
+    template<typename U>
+    static yes test(...);
+
+    static const bool value = sizeof(test<Derived>(0)) == sizeof(yes);
+};
+
+#include <iostream>
+
+struct A { int x; };
+struct B { const char x; };
+struct C { void x() volatile ; };
+struct D : A { };
+struct E {};
+struct F : A, B {};
+
+int main()
+{
+    std::cout
+        << has_x<A>::value
+        << has_x<const B>::value
+        << has_x<volatile C>::value
+        << has_x<const volatile D>::value
+        << has_x<E>::value
+        << has_x<F>::value;
+}
