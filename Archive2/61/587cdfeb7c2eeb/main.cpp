9fec1df395fa1f38f1340a8e6013f2d2e8f6ea99
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-13 14:17:33 +0000

    Update archive 61

---
+++ Archive2/61/587cdfeb7c2eeb/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+
+int main( int argc, char* argv[] )
+{
+    if( argc != 2 ) return 1;
+    
+    const int size = atoi( argv[1] );
+    
+    int arr[ size ] = { 100  };
+    
+    for( int i = 0; i < size; ++i )
+        std::cout << arr[i] << " ";
+}
