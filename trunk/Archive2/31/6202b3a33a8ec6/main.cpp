3f267ee1b975e95855bf67d57d13d0e6c620ef53
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-15 17:16:17 +0000

    Update archive 31

---
+++ trunk/Archive2/31/6202b3a33a8ec6/main.cpp
@@ -0,0 +1,31 @@
+    #include <iostream>
+    #include <vector>
+    #include <algorithm>
+    
+    class Comparators
+    {
+    public:
+        bool operator()(int first, int second)
+        {
+            return first < second;
+        }
+    };
+    
+    int main()
+    {
+        class ComparatorsInner : public Comparators{};
+    
+        std::vector<int> compares ;
+        compares.push_back(20) ;
+        compares.push_back(10) ;
+        compares.push_back(30) ;
+        
+        ComparatorsInner comparatorInner;
+        std::sort(compares.begin(), compares.end(), comparatorInner);
+    
+        std::vector<int>::iterator it;
+        for(it = compares.begin(); it != compares.end(); ++it)
+        {
+            std::cout << (*it) << std::endl;
+        }
+    }
