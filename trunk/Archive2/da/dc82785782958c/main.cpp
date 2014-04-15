c49d7b878a02c9a693f30e87e739daa76b1997f7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-15 23:14:38 +0000

    Update archive da

---
+++ trunk/Archive2/da/dc82785782958c/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <array>
+
+int main() {
+    std::array<std::array<int, 100>, 100> matrix;
+    auto ar = matrix[0];
+    matrix[0][0] = 1;
+    ar[0] = 2;
+    std::cout << matrix[0][0] << '\n';
+    std::cout << ar[0] << '\n';
+}
