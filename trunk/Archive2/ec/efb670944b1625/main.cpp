788fbfa8b078e9bd8b039725d74b7d3fc0ce1f60
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-15 17:15:04 +0000

    Update archive ec

---
+++ trunk/Archive2/ec/efb670944b1625/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+void funkcja()
+{
+    cout << "template" << endl;
+}
+
+int main()
+{
+    // []()->void { cout << 132 << endl;}();
+
+    funkcja();
+}
