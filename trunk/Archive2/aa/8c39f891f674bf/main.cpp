3dbccdbe221f35f4fba3cd6e71666e4d55c0a90d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-15 23:16:58 +0000

    Update archive aa

---
+++ trunk/Archive2/aa/8c39f891f674bf/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cctype>
+
+int main()
+{
+    // string num1="";
+    std::string num1 ;
+
+    std::cout << "enter a number: ";
+    std::cin >> num1 ;
+
+    // int a[10];
+    std::vector<int> a ; // http://www.mochima.com/tutorials/vectors.html
+
+    // for (int i=0; i<10; i++)
+    for( char c : num1 ) // http://www.stroustrup.com/C++11FAQ.html#for
+    {
+        // a[i]=stoi(num1.substr(i,i));
+
+        // http://en.cppreference.com/w/cpp/string/byte/isdigit
+        if( std::isdigit(c) ) a.push_back( c - '0' ) ;
+        else std::cerr << "invalid character '" << c << "\n" ;
+    }
+    
+    std::cout << "\ndigits in " << num1 << " are: " ;
+    // for(int i=0;i<10;i++)
+    for( int digit : a )
+    {
+        // cout<<a[i];
+        std::cout << digit << ' ' ;
+    }
+
+    // system("pause");
+    std::cout << "\npress enter to quit: " ;
+    std::cin.ignore( 1000, '\n' ) ;
+    std::cin.get() ;
+
+    // return 0;
+}
