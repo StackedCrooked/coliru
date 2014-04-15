7c2830fc1f31396c7eb5bfa318f221320d45862f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-15 02:35:02 +0000

    Update archive 3d

---
+++ trunk/Archive2/3d/71ac757899f013/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+template<typename ...T>
+void f(T ...t) {
+   struct E { E(...) {} } 
+   e{ ([&]{ 
+       int x = t + 1; 
+       std::cout << x << std::endl;  }(), 0)... };
+}
+
+int main() {
+   f(1, 2, 3, 4);   
+}
