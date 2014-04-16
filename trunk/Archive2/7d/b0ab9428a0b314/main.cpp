7e69e8b500155172c2b6e5bc68f4e2195a1dca9f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-16 05:20:10 +0000

    Update archive 7d

---
+++ trunk/Archive2/7d/b0ab9428a0b314/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 3;
+    int y = 7;
+    int z = x + y;
+    int w = x * y;
+    int a = y / x;
+    int b = x - y;
+    int c = y % x;
+    
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+    cout << "z = " << z << endl;
+    cout << "w = " << w << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+    
+    return 0;
+}
