2876971b09c6dec5c38aeef9f30314dbca81e070
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-16 17:19:36 +0000

    Update archive 27

---
+++ Archive2/27/77624eb3fd8558/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+template<class T>
+struct has
+{
+  typedef char yes;
+  typedef yes (&no)[2];
+
+  template<int>
+  struct foo;
+
+  template<class U>
+  static yes test(foo<U::bar>*);
+  
+  template<class U>
+  static no  test(...);
+
+  static bool const value = sizeof(test<T>(0)) == sizeof(yes);
+};
+
+enum foo
+{
+    bar
+};
+
+
+int main()
+{
+    std::cout << (has<foo>::value ? "yes" : "no") << std::endl;
+}
