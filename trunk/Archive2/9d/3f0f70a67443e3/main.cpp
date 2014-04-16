e6ce0209087acf83335cfdc69dda99a25662234c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-16 11:13:15 +0000

    Update archive 9d

---
+++ trunk/Archive2/9d/3f0f70a67443e3/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+void h(int);
+
+template <class T> void f2(T);  
+
+namespace A {
+    struct X {
+        friend void f(X); // A::f(X) is a friend
+        class Y {
+            friend void g(); // A::g is a friend
+            friend void h(int); // A::h is a friend
+            // ::h not considered
+            friend void f2<>(int); // ::f2<>(int) is a friend
+            static const int i = 1000;
+        };
+    };
+    // A::f, A::g and A::h are not visible here
+    X x;
+    void g() { f(x); } // definition of A::g
+    void f(X) { /* ... */} // definition of A::f
+    void h(int) { /* ... */ } // definition of A::h}
+    // A::f, A::g and A::h are visible here and known to be friends
+}
+    template <class T> void f2(T t) { std::cout << A::X::Y::i << '\n'; }
+using A::x;
+void h() {
+    A::f(x);
+    //   A::X::f(x); // error: f is not a member of A::X
+    //    A::X::Y::g(); // error: g is not a member of A::X::Y
+}
+
+
+
+int main()
+{
+    h();
+    f2(1000);
+}
