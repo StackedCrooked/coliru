d45136020113e50df5ac2ef2df69a8bdd10643fe
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-17 05:23:18 +0000

    Update archive 5a

---
+++ Archive2/5a/e08c5109349384/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    bool a;
+	if( a ) {
+		std::cout << "True" << std::endl;
+	} else {
+		std::cout << "False" << std::endl;
+	}
+	
+	int b;
+	std::cout << b << std::endl;
+}
