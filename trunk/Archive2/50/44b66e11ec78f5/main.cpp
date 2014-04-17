962880d14bf79c9a0e3405b7bdba29ef791c9bb0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-17 11:31:27 +0000

    Update archive 50

---
+++ trunk/Archive2/50/44b66e11ec78f5/main.cpp
@@ -0,0 +1,40 @@
+    #include <vector>
+    #include <list>
+    #include <boost/fusion/include/vector.hpp>
+    #include <boost/fusion/mpl.hpp>
+    #include <boost/mpl/transform.hpp>
+    #include <iostream>
+
+    namespace container
+    {
+        struct vector {
+            template<typename T> struct apply {
+                typedef std::vector<T> type;
+            };
+        };
+        struct list   {
+            template <typename T> struct apply {
+                typedef std::list<T> type;
+            };
+        };
+    }
+
+    template<typename T, typename ContainerMaker>
+    struct wrap_into_container
+    {
+        typedef typename ContainerMaker::template apply<T>::type type;   
+    };
+
+    int main()
+    {
+        namespace fusion = boost::fusion;
+        namespace mpl = boost::mpl;
+    
+        typedef fusion::vector<int, float, int> vec_type;
+        typedef mpl::transform< vec_type, wrap_into_container<mpl::_1, container::vector> >::type wrapped_vec_type;
+    
+        wrapped_vec_type w;
+        std::cout << size(w) << "\n";
+        return size(w);
+
+    }
