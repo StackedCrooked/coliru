217e4a3aed01175d7ca5be3eb55ed3524cdea649
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-17 11:22:48 +0000

    Update archive e3

---
+++ Archive2/e3/ce83d4dc2e58be/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+int main()
+{
+int x[5][5],i,j;
+for(i=1;i<=4;i++)
+for(j=1;j<=4;j++)
+x[i][j]=((pow(j,2)+(3*j))/2)+(i-1)*j;
+cout<<"\n\n";
+for(i=1;i<=4;i++)
+{
+for(j=1;j<=4;j++)
+cout<<x[i][j]<<"\t";
+cout<<"\n";
+}
+}
