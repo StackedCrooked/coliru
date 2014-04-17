4476071d01307de5df02e4252fccdf07f9f8cbbc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-17 17:29:55 +0000

    Update archive 3a

---
+++ trunk/Archive2/3a/9768812b936ae3/main.cpp
@@ -0,0 +1,16 @@
+struct D
+{
+  template <class T> D (T);
+};
+int operator~(const D &);
+
+template <typename T> T &make ();
+
+template <typename Rhs> struct H
+{
+  static const int value = sizeof operator~(make<Rhs>());
+};
+
+enum class E;
+
+int main () { return H<E>::value; }
