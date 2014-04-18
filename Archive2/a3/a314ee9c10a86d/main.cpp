ae3d61a4edbbaf18ed2d008ea6e3bf0a9f488914
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-18 23:31:35 +0000

    Update archive a3

---
+++ Archive2/a3/a314ee9c10a86d/main.cpp
@@ -0,0 +1,48 @@
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/random_access_index.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <iostream>
+
+struct Data
+{
+    int i;
+    std::string index1;
+
+    friend std::ostream& operator<<(std::ostream& os, Data const& v) {
+        return os << "{ " << v.i << ", '" << v.index1 << "' }";
+    }
+
+};
+
+namespace bmi = boost::multi_index;
+
+using Table = bmi::multi_index_container<
+    Data,
+    bmi::indexed_by<
+        bmi::random_access<>,
+        bmi::ordered_unique<bmi::member<Data, std::string, &Data::index1> >
+    > >;
+
+static std::ostream& operator<<(std::ostream& os, Table const& table) {
+    os << "insertion order: ";  for(auto const& element : table) os << element << "; ";
+    os << "\nsecondary index: "; for(auto const& element : table.get<1>()) os << element << "; ";
+    return os << "\n";
+}
+
+int main()
+{
+    Table table {
+        { 42, "aap" },
+        { 43, "noot" },
+        { 44, "mies" } 
+    };
+
+    std::cout << "Before:\n" << table << "\n";
+
+    table.modify(table.begin(),  [](Data& v) { v.i *= v.i; });
+    std::cout << "Edit 1:\n" << table << "\n";
+
+    table.modify(table.begin()+2, [](Data& v) { v.index1 = "touched"; });
+    std::cout << "Edit 2:\n" << table << "\n";
+}
