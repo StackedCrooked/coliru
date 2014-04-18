ba457eed7d0fedf39c0f165754abfd116b0b28d6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-18 17:36:08 +0000

    Update archive 8a

---
+++ Archive2/8a/d26c75891a26fd/main.cpp
@@ -0,0 +1,39 @@
+#include <cmath>
+#include <limits>
+#include <iostream>
+
+template<typename T>
+struct foo
+{
+    T val;
+
+	foo(T v = T()) : val(v) { sync(v); }
+	foo& operator=(T v) { val = v; sync(v); return *this; }
+
+	operator T&() { return val; }
+	operator const T&() const { return val; }
+
+	static int& min() { static int e = std::numeric_limits<int>::max(); return e; }
+	static int& max() { static int e = std::numeric_limits<int>::min(); return e; }
+
+	static void sync_min(T e) { e = std::floor(e); if (e < min()) min() = int(e); }
+	static void sync_max(T e) { e = std::ceil(e);  if (e > max()) max() = int(e); }
+
+	static void sync(T e)
+	{
+		e = std::abs(e);
+    	e = e == 0 ? T(1) : std::log10(e);
+		sync_min(e); sync_max(e);
+	}
+};
+
+int main ()
+{
+	using T = foo<float>;
+	T x;
+    
+	for (T e = -10; e <= 30; ++e)
+		x = std::pow(10, e);
+        
+	std::cout << T::min() << " " << T::max() << std::endl;
+}
