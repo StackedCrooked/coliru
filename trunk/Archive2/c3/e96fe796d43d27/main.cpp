22f9c68aec7ee6265072489ed5406c8ba2bce48a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-18 17:21:07 +0000

    Update archive c3

---
+++ trunk/Archive2/c3/e96fe796d43d27/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <boost/function.hpp>
+using namespace std;
+
+
+struct CallCounter
+{
+    CallCounter() : count( 0 ) {}
+    void operator()()
+    {
+        // do stuff
+        cout << "count is at " << count << endl;
+        ++count;
+    }
+
+    int count;
+};
+//However, when I try to access count after passing it to another function, count is still at 0.
+
+void callNTimes( int n, boost::function<void()> func )
+{
+    for ( int i = 0; i < n; ++i )
+        func();
+}
+
+int main()
+{
+    CallCounter counter;
+    callNTimes( 20, counter );
+
+    cout << counter.count << endl; // prints 0
+    return 0;
+}
