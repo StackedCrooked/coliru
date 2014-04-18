7b82ac70806f345825f5f0186f8e6dc5a30c5c85
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-18 23:26:22 +0000

    Update archive ec

---
+++ Archive2/ec/0faedca3eb9ce1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdint>
+#include <vector>
+#include <typeinfo>
+
+int main()
+{
+    std::vector<std::uint8_t> items = { 0, 1, 2, 3 };
+    for (auto x : items)
+    {
+        std::cout << std::boolalpha << (typeid(x) == typeid(std::uint8_t)) << std::endl;
+    }
+    
+}
