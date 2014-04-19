4dd5a85f1cad8f619503d045c0537aaaf9ea6794
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-19 05:31:29 +0000

    Update archive 6a

---
+++ Archive2/6a/13a4b173f2b9f1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <type_traits>
+
+class Foo {
+public:
+    template<typename Type>
+    typename std::enable_if<
+        std::is_base_of<Foo, Type>::value,
+    const bool>::type
+    operator==(const Type&) const { return false; }
+
+    template<typename Type>
+    typename std::enable_if<
+        !std::is_base_of<Foo, Type>::value,
+    const bool>::type
+    operator==(const Type&) const { return true; }
+};
+
+struct Bar : public Foo {};
+
+int main() {
+    Foo foo;
+    Bar bar;
+    std::cout << (foo == bar);
+}
