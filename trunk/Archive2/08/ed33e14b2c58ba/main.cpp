3393a5793ff501d2eaf28990f474c2673055f4b0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-20 11:49:03 +0000

    Update archive 08

---
+++ trunk/Archive2/08/ed33e14b2c58ba/main.cpp
@@ -0,0 +1,20 @@
+#include <type_traits>
+#include <boost/tti/has_template.hpp>
+
+BOOST_TTI_HAS_TEMPLATE(Template1)
+BOOST_TTI_HAS_TEMPLATE(Template2)
+BOOST_TTI_HAS_TEMPLATE(Template3)
+
+struct Top
+{
+  template <class X> struct Template1 { };
+  template <typename A,typename B,typename C> class Template2 { };
+  template <typename A,typename B,typename C,int D> class Template3 { };
+};
+  
+int main()
+{
+    static_assert( has_template_Template1<Top>::value, ""); // true
+    static_assert( has_template_Template2<Top>::value, ""); // true
+    static_assert(!has_template_Template3<Top>::value, ""); // false, not all typename/class template parameters
+}
