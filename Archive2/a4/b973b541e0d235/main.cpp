15f831a3a46340ead298fb59fe8452fbd442c410
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-21 05:46:41 +0000

    Update archive a4

---
+++ Archive2/a4/b973b541e0d235/main.cpp
@@ -0,0 +1,31 @@
+#include <memory>
+#include <iostream>
+ 
+struct Good: std::enable_shared_from_this<Good>
+{
+    std::shared_ptr<Good> getptr() {
+        return shared_from_this();
+    }
+     ~Good() { std::cout << "Good::~Good() called\n"; }
+};
+ 
+struct Bad: std::enable_shared_from_this<Bad>
+{
+    std::shared_ptr<Bad> getptr() {
+        return shared_from_this();
+    }
+    ~Bad() { std::cout << "Bad::~Bad() called\n"; }
+};
+ 
+int main()
+{
+    // Good: the two shared_ptr's share the same object
+    std::shared_ptr<Good> gp1(new Good);
+    std::shared_ptr<Good> gp2 = gp1->getptr();
+    std::cout << "gp2.use_count() = " << gp2.use_count() << '\n';
+ 
+    // Bad, each shared_ptr thinks it's the only owner of the object
+    std::shared_ptr<Bad> bp1(new Bad);
+    std::shared_ptr<Bad> bp2 = bp1->getptr();
+    std::cout << "bp2.use_count() = " << bp2.use_count() << '\n';
+} // UB: double-delete of Bad
