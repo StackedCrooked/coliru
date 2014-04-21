477e6aab7826a4fe2b1df3691cf87aac97bd7127
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-21 11:50:19 +0000

    Update archive 69

---
+++ trunk/Archive2/69/be21be4787770c/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+typedef  string *string2d[2];
+
+string2d flowFile() {
+    static string flow[23][2];
+   return flow;
+ }
+
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+int main()
+{
+    std::vector<std::string> words = {
+        "Hello", "from", "GCC", __VERSION__, "!"
+    };
+    std::cout << words << std::endl;
+}
