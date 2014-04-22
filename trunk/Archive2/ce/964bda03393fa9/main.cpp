9417e3d3f8ffedcb02bbd5b1af728c3209111839
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-22 14:09:48 +0000

    Update archive ce

---
+++ trunk/Archive2/ce/964bda03393fa9/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+template <typename T, typename Fn>
+size_t partition(T arr[], size_t size, Fn&& fun)
+{
+    std::cout << __PRETTY_FUNCTION__ << '\n';
+    
+    size_t index = 0;
+    for(size_t i = 0; i < size; i++)
+    {
+        if (fun(arr[i]))
+        {
+            T tmp = arr[index];
+            arr[index] = arr[i];
+            arr[i] = tmp;
+            index = i;
+        }
+        else
+        {
+            if(index == 0)
+                index = i;
+        }
+    }
+    return index;
+}
+
+template <typename T>
+void printTable(T const arr[], size_t size)
+{
+    cout << "[ ";
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "]\n";
+}
+
+// for fixed arrays
+template<typename T, size_t N>
+void printTable(T const (&arr)[N])
+{
+    printTable(arr, N);
+}
+
+
+template <typename T>
+bool less10(T a)
+{
+    return a < 10;
+}
+
+int main()
+{
+    int arri[] = {1,20,3,50,6,7};
+    size_t sizi = sizeof(arri)/sizeof(arri[0]);
+    printTable(arri);
+    
+    size_t fi = partition(arri,sizi, less10<int>);
+    printTable(arri);
+    cout << "index: " << fi << endl << endl;
+    
+    double arrd[] = {1,20,3,50,6,7};
+    size_t sizd = sizeof(arrd)/sizeof(arrd[0]);
+    printTable(arrd);
+    
+    size_t fd = partition(arrd,sizd, [] (double x) -> bool {return x > 10;});
+    printTable(arrd);
+    cout << "index: " << fd << endl;
+}
