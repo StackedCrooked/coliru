888ed9b2741c24aad0ce077841a581af4856218b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-22 05:51:23 +0000

    Update archive 4a

---
+++ Archive2/4a/b2e5fcaed3e7f3/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+int search(T items[], int first, int last, T searchItem){
+    
+}
+
+template<typename T>
+int insert(T items[], int& size, T insertItem){
+    
+}
+
+template<typename T>
+class TreeNode<T>{
+    
+};
+
+template<typename T>
+TreeNode<T>* search(TreeNode<T>*& root, T searchItem){
+    
+}
+
+template<T>* insert(TreeNode<T>*& root, T searchItem){
+    
+}
+
+//TEST CODE FOR ABOVE IMPLEMENTATIONS
+
+int main(){
+    
+    return 0;
+}
