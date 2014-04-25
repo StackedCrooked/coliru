6dd8c7e8881ec06c72966a0e9461e7aa9d2e5e75
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-25 06:03:12 +0000

    Update archive ea

---
+++ Archive2/ea/055e7dba39ed59/cmd.sh
@@ -0,0 +1,8 @@
+cat <<"JSON">json.txt; g++-4.8 -std=c++11 -O2 -Wall main.cpp -lboost_system && ./a.out
+{
+    "assetType": "font",
+    "assetName": "default font",
+    "assetFile": "dat/assets/dfont.png",
+    "charMap": "`1234567890-=~!@#$%^&*()_+qwertyuiop[]\\QWERTYUIOP{}|asdfghjkl;'ASDFGHJKL:\"zxcvbnm,./ZXCVBNM<>? "
+}
+JSON
