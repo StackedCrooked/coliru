7f309ddd477afb144c79888d3438c7de0d8059e4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-25 06:17:15 +0000

    Update archive 2c

---
+++ Archive2/2c/e76d742c2cb82e/main.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <iostream>
+
+struct X { 
+    X()  { std::cout << __PRETTY_FUNCTION__ << "\n"; }
+    ~X() { std::cout << __PRETTY_FUNCTION__ << "\n"; }
+    std::string x = "abc"; 
+};
+
+int main()
+{
+l1:
+       std::string s = "hellow world";
+       static int i = 0, j = 0, k = 0;
+       if (i++)
+           goto l2;
+       else
+           goto l1;
+
+       X x;
+l2:
+       
+       if (j++) goto more;
+       goto l2;
+
+more:
+       if (k++<3)
+           goto l1;
+
+}
