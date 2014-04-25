c8f96ffd3328b272685d13cbd32fd27898642d21
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-25 05:59:27 +0000

    Update archive e3

---
+++ Archive2/e3/ebd83be1894fca/main.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+
+struct Base;
+
+struct BaseVTableLayout {
+    int (*virtualCall)(Base*);
+};
+
+struct Base { BaseVTableLayout const* vptr; };
+struct Derived: Base {};
+
+int Derived_virtualCall(Base*) { return 42; }
+
+BaseVTableLayout const BaseVTable = { 0 };
+BaseVTableLayout const DerivedVTable = { &Derived_virtualCall };
+
+int add(Base** array, size_t size) {
+    int total = 0;
+    for (size_t i = 0; i != size; ++i) {
+        Base* b = array[i];
+        total += b->vptr->virtualCall(b);
+    }
+    return total;
+}
+
+int func() {
+    Derived d0;
+    d0.vptr = &DerivedVTable;
+    Derived d1;
+    d1.vptr = &DerivedVTable;
+    Derived d2;
+    d2.vptr = &DerivedVTable;
+    
+    Base* b[3] = { &d0, &d1, &d2 };
+
+    return add(b, 3);
+}
