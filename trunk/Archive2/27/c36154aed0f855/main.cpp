75505c95e104a3cfc86a9a7f0d903f3148f41932
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-27 17:57:04 +0000

    Update archive 27

---
+++ trunk/Archive2/27/c36154aed0f855/main.cpp
@@ -0,0 +1,17 @@
+#include <boost/spirit/include/karma.hpp>
+#include <boost/iterator/function_input_iterator.hpp>
+#include <boost/random.hpp>
+
+boost::random::variate_generator<boost::mt19937, boost::uniform_int<int> > rdngen(
+        boost::mt19937(), boost::uniform_int<int>(1, 1000));
+
+int main() {
+    auto f = boost::make_function_input_iterator(rdngen, 1),
+         l = boost::make_function_input_iterator(rdngen, 100);
+
+    auto range = boost::make_iterator_range(f, l);
+
+    using namespace boost::spirit::karma;
+    std::cout << format_delimited(columns(5) [ right_align(10) [ *auto_ ] ], "\t", range) << "\n";
+
+}
