f74d98ff8ec6f2ea009766b970bed69b1ec8b488
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-27 18:02:55 +0000

    Update archive fa

---
+++ Archive2/fa/b3c1e6734eb3c6/cmd.sh
@@ -0,0 +1,11 @@
+cat <<TRADE>trade.txt; cat <<NBBO>nbbo.txt; g++ -std=c++11 -Wall -pedantic main.cpp && ./a.out
+MSFT 100 57.2 13:00
+MSFT 100 58.2 13:10
+GOOG 200 300.2 13:01
+GOOG 300 300.2 14:30
+TRADE
+GOOG 100 300.2 100 300.3 13:03
+GOOG 100 300.3 100 300.4 13:04
+MSFT 100 57.2 100 57.3 12:09
+MSFT 100 57.3 100 58.4 13:05
+NBBO
