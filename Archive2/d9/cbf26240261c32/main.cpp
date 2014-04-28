7f5feb34fc7066981eb9f98a315dcdf66bc466f9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-28 16:37:22 +0000

    Update archive d9

---
+++ Archive2/d9/cbf26240261c32/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <memory>
+#include <type_traits>
+
+struct base {
+    virtual ~base() = default;
+    virtual int test(int) = 0;
+};
+
+template<typename T>
+struct derived : public base  {
+    template<typename U = T>
+    typename std::enable_if<std::is_void<U>::value, int>::type test(int) override { return 0; }
+    template<typename U = T>
+    typename std::enable_if<!std::is_void<U>::value, int>::type test(int) override { return 1; }
+};
+
+template<typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args) {
+    return { new T(std::forward<Args>(args)...) };
+}
+
+int main() {
+    std::unique_ptr<base> x = make_unique<derived<int>>();
+}
