ec3b015c9ee9f296fcab772b5ff2796d83d663a1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-28 16:55:34 +0000

    Update archive 16

---
+++ trunk/Archive2/16/71536646eb1a3d/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+struct string // belongs to the type category literal type
+{
+    constexpr explicit string( int v = 0 ) : value(v) {} 
+    
+    constexpr int size() const { return value ; } // size() is constexpr
+    
+    const int value ;    
+};
+
+constexpr bool isShorter( const string& s1, const string& s2 )
+{
+    return s1.size() < s2.size();
+}
+
+int main()
+{
+    constexpr char test [ isShorter( string(20), string(30) ) ? 100 : 200 ] = {0} ;
+    std::cout << sizeof(test) << '\n' ;
+}
+
