9653b0bb00a3e7d064ae72c3fbd8bd1daa63ee32
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-28 16:58:26 +0000

    Update archive 73

---
+++ Archive2/73/cac8d5e7387a7c/main.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+
+template<typename It, typename Elem>
+bool in_ranges(It first, It last, It first2, const Elem &val) {
+    return std::any_of(first, last, [&first2, val](const Elem &e) {
+        return e <= val && val <= *first2++;
+    });
+}
+
+int main() {
+    int starts[] = {11, 31, 51, 71, 91};
+    int ends[] = {20, 40, 60, 80, 100};
+    
+    const int num = 15;
+    
+    std::cout << in_ranges(std::begin(starts), std::end(starts), std::begin(ends), num);
+}
