39894cacc9fa34c252c238b39090118831da5989
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-28 22:55:21 +0000

    Update archive bf

---
+++ trunk/Archive2/bf/8e58cb72f5d8e9/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+struct S {
+private:
+    short a;
+	char b;
+public:
+        void foo(){ std::cout << " a:"<< (long) &a <<" b:" << (long) &b << std::endl;}
+
+};
+
+
+class C {
+	short a;
+	char b;
+public:
+	void foo(){ std::cout << " a:"<< (long) &a <<" b:" << (long) &b << std::endl;}
+
+};
+
+class EC : public C {
+	char c;
+public:
+	void foo(){ C::foo(); std::cout << " &c" << (long) &c << std::endl;}
+};
+
+
+struct ES : S {
+	char c;
+	void foo(){ S::foo();std::cout << " c:" <<(long) &c << std::endl;}
+
+};
+
+int main() {
+static_assert(sizeof(short) == 2,"");
+static_assert(sizeof(S) == 4,"");
+std::cout << sizeof(C);
+
+std::cout << sizeof(EC);
+std::cout << sizeof(ES);
+
+EC ec;
+ES es;
+ec.foo();
+es.foo();
+
+}
