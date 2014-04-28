828a0787f37501bd0eb18dcc9fbe4f1d0dd0dac2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-28 17:01:05 +0000

    Update archive 74

---
+++ trunk/Archive2/74/af4eeea67dd0ca/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <memory>
+#include <type_traits>
+
+struct base {
+    virtual ~base() = default;
+    virtual int test(int) = 0;
+};
+
+template<typename T>
+struct derived : public base  {
+    template<typename U = T>
+    typename std::enable_if<std::is_void<U>::value, int>::type test(int) { return 0; }
+    template<typename U = T>
+    typename std::enable_if<!std::is_void<U>::value, int>::type test(int) { return 1; }
+    
+    int test(int) { return test<>(0); }
+};
+
+int main() {
+    std::shared_ptr<base> x = std::make_shared<derived<int>>();
+    std::cout << x->test(0) << '\n';
+    x = std::make_shared<derived<void>>();
+    std::cout << x->test(0) << '\n';
+}
