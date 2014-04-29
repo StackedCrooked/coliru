ab274259a1f55bbe73a5510b1e16502b45334958
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-29 16:52:32 +0000

    Update archive 4d

---
+++ trunk/Archive2/4d/54bb29131b88d8/main.cpp
@@ -0,0 +1,11 @@
+# Include <cmath> 
+# include <iostream>
+ 
+int main ( ) 
+{ 
+    std:: cout  <<  std:: fixed ; 
+    std:: cout  << std :: cbrt ( 27.0 )  <<  ' \n ' ; 
+    std:: cout  << std :: cbrt ( 1.0 )  <<  ' \n ' ; 
+    std:: cout  << std :: cbrt ( 125.0 )  <<  ' \n ' ; 
+    std:: cout  << std :: cbrt ( - 0.125 )  <<  ' \n ' ; 
+}
