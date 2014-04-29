567a1505c8f316f3d19723e890084a6951c4f684
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-29 16:46:29 +0000

    Update archive 1e

---
+++ Archive2/1e/7c3c748ecb0ba9/main.cpp
@@ -0,0 +1,38 @@
+#include <type_traits>
+#include <iostream>
+#include <string>
+
+struct foo {
+    template<typename T, typename=
+      typename std::enable_if<
+        std::is_pointer<typename std::remove_reference<T>::type>{} &&
+        std::is_same<typename std::decay<decltype(*std::declval<T>())>::type, char>{}
+      >::type>
+    foo(T&&) { std::cout << "pointer\n"; }
+    template<int N>
+    foo(char const (&)[N]) { std::cout << "array\n"; }
+    foo(std::string) { std::cout << "string\n"; }
+};
+
+int main()
+{
+    // arrays
+    char a[12]{};
+    foo{a};
+    const char ca[10]{};
+    foo{ca};
+    foo{"hello"};
+
+    // pointers
+    char* p{};
+    foo{p};
+    const char* cp{};
+    foo{cp};
+    char* const pc{};
+    foo{pc};
+    foo{&a[0]};
+    foo{ca+4};
+
+    // strings (Verify SFINAE)
+    foo{std::string("foo")};
+}
