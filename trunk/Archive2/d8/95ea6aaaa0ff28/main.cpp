2bd1b464e7e4d33c842e87c994beb0c8e8775567
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-29 17:02:05 +0000

    Update archive d8

---
+++ trunk/Archive2/d8/95ea6aaaa0ff28/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <set>
+#include <chrono>
+#include <cmath>
+#include <unordered_map>
+#include <climits>
+
+
+int main()
+{
+    size_t val = UINT_MAX;
+    int i1{ val };
+}
