3ef71d2ed307b750f76f639d7688acae8cd9097b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-29 16:41:17 +0000

    Update archive 43

---
+++ trunk/Archive2/43/88afd79255f76f/main.cpp
@@ -0,0 +1,27 @@
+#include <boost/property_tree/detail/rapidxml.hpp>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+using namespace boost::property_tree::detail::rapidxml;
+
+static void xmlTreePresent(xml_node<> const* node, int depth = 0) {
+    if(node) {
+        std::cout << std::setw(depth*2) << "" << node->name() << "\n";
+        for(auto child = node->first_node(); child; child = child->next_sibling())
+            if(child->name() && *child->name()) 
+                xmlTreePresent(child, depth + 1);
+    } else std::cout << std::setw(depth*2) << "" << "NULL NODE\n";
+}
+
+int main(int argc, char* argv[]) {
+    std::string xml(std::istreambuf_iterator<char>(std::ifstream(argc>1? argv[1] : "input.txt").rdbuf()), {});
+
+    try {
+        xml_document<> doc;
+        doc.parse<0>(&xml[0]);
+        xmlTreePresent(doc.first_node());
+    } catch(std::exception const& e) { 
+        std::cerr << "ERROR: " << e.what() << "\n";
+        return 1;
+    } 
+}
