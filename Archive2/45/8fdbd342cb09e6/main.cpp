e13ad29c4dea3392e5a1045f11c0722c31c8a439
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-29 16:58:55 +0000

    Update archive 45

---
+++ Archive2/45/8fdbd342cb09e6/main.cpp
@@ -0,0 +1,54 @@
+//a user defined type
+class Widget{};
+class Frobnicator{};
+
+/////////////////////////////////////////////////////
+// copied and reduced from boost/strong_typedef.hpp
+#define CUSTOM_STRONG_TYPEDEF(T, D)                                 \
+struct D                                                            \
+    /*: boost::totally_ordered1< D           */                     \
+    /*, boost::totally_ordered2< D, T        */                     \
+    /*> >                                    */                     \
+{                                                                   \
+    T t;                                                            \
+    explicit D(const T t_) : t(t_) {};                              \
+    D(){};                                                          \
+    D(const D & t_) : t(t_.t){}                                     \
+    D & operator=(const D & rhs) { t = rhs.t; return *this;}        \
+    D & operator=(const T & rhs) { t = rhs; return *this;}          \
+    explicit operator const T & () const {return t; }               \
+    explicit operator T & () { return t; }                          \
+    /*bool operator==(const D & rhs) const { return t == rhs.t; } */\
+    /*bool operator<(const D & rhs) const { return t < rhs.t; }   */\
+};
+
+CUSTOM_STRONG_TYPEDEF(Widget, Gadget)
+CUSTOM_STRONG_TYPEDEF(Frobnicator, Normalcy)
+
+void acceptWidget(Widget){}
+void acceptGadget(Gadget){}
+void acceptFrobnicator(Frobnicator){}
+void acceptNormalcy(Normalcy){}
+
+int main(){
+
+    //make the two different types
+    Widget w;
+    Gadget g;
+
+    //call a function that should ONLY accept Widgets
+    acceptWidget(w); //works (good)
+    acceptGadget(g);
+
+    //acceptWidget(g); // Error
+    //acceptGadget(w); // Error
+    // but we can enjoy conversions if we summon them
+    acceptWidget(static_cast<Widget&>(g));
+
+    //more, unrelated, types with the very same macro
+    Frobnicator f;
+    Normalcy n;
+    acceptFrobnicator(f);
+    acceptNormalcy(n);
+
+}
