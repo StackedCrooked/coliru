a220669929ccf675be0f5301233e2c6e40e13890
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-29 16:50:17 +0000

    Update archive ce

---
+++ trunk/Archive2/ce/188ce38259e9ed/main.cpp
@@ -0,0 +1,17 @@
+/ * Comentários de estilo C pode conter
+várias linhas * / 
+/ * ou apenas uma * /
+ 
+Comentários / / C + estilo + pode comentar uma linha
+ 
+/ / Ou, eles podem 
+/ / ser amarrados juntos
+ 
+int principal ( ) 
+{ 
+  / / O código abaixo não será executado 
+  / / return 1;
+ 
+  / / O código abaixo será executado 
+  retorno  0 ; 
+}
