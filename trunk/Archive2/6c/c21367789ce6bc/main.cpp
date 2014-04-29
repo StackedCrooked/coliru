7248bf3b41c783c5a132ff01a52adf671fcd276b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-29 04:42:53 +0000

    Update archive 6c

---
+++ trunk/Archive2/6c/c21367789ce6bc/main.cpp
@@ -0,0 +1,23 @@
+#include <stdexcept>
+#include <iostream>
+#include <string>
+ 
+int main()
+{
+    std::string s("message"); // for capacity
+ 
+    s = "abc";
+    s.at(2) = 'x'; // ok
+    std::cout << s << '\n';
+ 
+    std::cout << "string size = " << s.size() << '\n';
+    std::cout << "string capacity = " << s.capacity() << '\n';
+ 
+    try {
+        // throw, even if capacity allowed to access element
+        s.at(3) = 'x';
+    }
+    catch (std::out_of_range& exc) {
+        std::cout << exc.what() << '\n';
+    }
+}
