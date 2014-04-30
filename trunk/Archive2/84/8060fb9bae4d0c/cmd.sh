41e00475b8db6c1e1dcbc21faccb8906ebf68f3d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-30 04:48:43 +0000

    Update archive 84

---
+++ trunk/Archive2/84/8060fb9bae4d0c/cmd.sh
@@ -0,0 +1,1 @@
+clang++ -std=c++1y -Ofast -Wall -Wextra -pedantic -pthread main.cpp -o variant && ./variant.out
---
+++ trunk/Archive2/84/8060fb9bae4d0c/main.cpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "variant/traits.hpp"
+#include "variant/variant_fwd.hpp"
+#include "variant/recursive_wrapper.hpp"
+#include "variant/variant.hpp"
+#include "variant/visitor.hpp"
+#include "variant/variant_relational_ops.hpp"
