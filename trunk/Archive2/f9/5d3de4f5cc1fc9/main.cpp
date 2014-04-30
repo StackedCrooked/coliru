5b052da4c4211f9083ce0d9850427f4de4153f88
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-30 21:11:40 +0000

    Update archive f9

---
+++ trunk/Archive2/f9/5d3de4f5cc1fc9/main.cpp
@@ -0,0 +1,7 @@
+    #include <iostream>
+    
+    int main()
+    {
+    double number = 6666666.77777;
+    printf("double number %f can be presented as: %p, %x, %e, %a or as optimal %g", number, number, number, number,number, number);
+    }
