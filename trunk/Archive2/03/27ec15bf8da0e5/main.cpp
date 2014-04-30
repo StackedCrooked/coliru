585023e28fe156df1afd7b7b95e1e171b1aca071
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-30 04:55:57 +0000

    Update archive 03

---
+++ trunk/Archive2/03/27ec15bf8da0e5/main.cpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "traits.hpp"
+#include "variant_fwd.hpp"
+#include "recursive_wrapper.hpp"
+#include "variant.hpp"
+#include "visitor.hpp"
+#include "variant_relational_ops.hpp"
