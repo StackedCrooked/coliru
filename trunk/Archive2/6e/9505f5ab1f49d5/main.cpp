7d84c854393558e7c2f9ecdfa4292b35670d3430
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-30 04:59:33 +0000

    Update archive 6e

---
+++ trunk/Archive2/6e/9505f5ab1f49d5/main.cpp
@@ -0,0 +1,25 @@
+#include <type_traits>
+
+template <class T>
+class Base 
+{
+public:
+    ~Base() {
+        static_assert(std::is_same<decltype(T::foo(1)), int>::value, "ERROR STRING");
+    }
+};
+
+class Good : public Base<Good>
+{
+public:
+    static int foo(int) { return 42; };
+};
+
+class Bad : public Base<Bad>
+{
+public:
+    static double foo(int) { return 42; };
+};
+
+Good g;
+Bad b;
