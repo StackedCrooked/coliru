154339fd5c331af82950bf20a80a3b9cab915e6e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-04-30 21:12:34 +0000

    Update archive 67

---
+++ trunk/Archive2/67/9e06c1d7609ea2/main.cpp
@@ -0,0 +1,7 @@
+    #include <iostream>
+    
+    int main()
+    {
+        const char * UserName = "Vasia";
+        printf("C String %s can be located by pointer %p", UserName, UserName);
+    }
