3a1da0e7ade73e36003a20e2017d231c54e64882
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-01 20:51:51 +0000

    Update archive 00

---
+++ Archive2/00/a817cf80d7396c/cmd.sh
@@ -0,0 +1,1 @@
+clang++ -std=c++1y -Wall -Wextra -pedantic main.cpp -O0 && ./a.out
---
+++ Archive2/00/a817cf80d7396c/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <tuple>
+#include <string>
+using namespace std;
+ 
+ 
+// basically C++14's integer sequence
+template<int ...>
+struct seq { };
+ 
+template<int N, int ...S>
+struct gens : gens<N-1, N-1, S...> { };
+ 
+template<int ...S>
+struct gens<0, S...> {
+typedef seq<S...> type;
+};
+ 
+template<typename Func, typename ...B, int ...S> auto
+map_1(Func&& f, std::tuple<B...> &&t, seq<S...>)
+-> decltype(std::make_tuple(f(std::get<S>(t))...))
+{
+return std::make_tuple(f(std::get<S>(t))...);
+}
+ 
+template<typename Func, typename ...B> auto
+map(Func&& f, std::tuple<B...> &&t)
+-> decltype(map_1(std::forward<Func>(f), std::forward<std::tuple<B...>>(t), typename gens<sizeof...(B)>::type()))
+{
+return map_1(std::forward<Func>(f), std::forward<std::tuple<B...>>(t), typename gens<sizeof...(B)>::type());
+}
+ 
+ 
+// Some functions
+double f(double x) { return x; }
+size_t f(std::string s) { return s.size(); }
+ 
+template<typename T>
+void printFoo(const T& )
+{
+   std::cout << __PRETTY_FUNCTION__ << std::endl;   
+}
+ 
+#define BINDOVERLOADED(X) [](auto t) { return X(t); }
+int main() {
+ 
+auto x = map(BINDOVERLOADED(f), make_tuple(3.14, "a string"));
+printFoo(x);
+return 0;
+}
