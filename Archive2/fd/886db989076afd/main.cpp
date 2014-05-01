dd6add27979eaf72656987435ae0b2a14d5f2dab
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-01 15:00:40 +0000

    Update archive fd

---
+++ Archive2/fd/886db989076afd/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <memory>
+
+class cStudent
+{
+public:
+    bool shouldBeGraduated;
+	int studentAge;
+
+	cStudent( int age = last_objects_age )
+	{
+		last_objects_age = studentAge = age;
+		shouldBeGraduated = studentAge > 18 ;
+	}
+
+	static int last_objects_age  ;
+};
+
+int cStudent::last_objects_age = 0 ;
+
+int main()
+{
+    cStudent array[8] { 25 } ;
+    for( const cStudent& s : array ) std::cout << s.studentAge << ' ';
+    std::cout << '\n' ;
+
+    const int n = 10 ;
+    std::unique_ptr< cStudent[] > array2( new cStudent[n] { 17 } ) ;
+    for( int i = 0 ; i <n ; ++i ) std::cout << array2[i].studentAge << ' ';
+    std::cout << '\n' ;
+
+}
