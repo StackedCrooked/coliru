60e3980680c73794e0e979d797e4c011830b42ec
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-01 21:05:51 +0000

    Update archive f9

---
+++ trunk/Archive2/f9/c9d559a352d91c/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+
+template <typename... Ts>
+struct Set {};
+
+template <typename T, typename TS>
+struct MemberOf;
+
+template <typename T, typename... Ts>
+struct MemberOf<T, Set<T, Ts...>> {
+    static constexpr bool value = true;
+};
+
+template <typename T>
+struct MemberOf<T, Set<>> {
+    static constexpr bool value = false;
+};
+
+template <typename T, typename U, typename... Ts>
+struct MemberOf<T, Set<U, Ts...>> {
+    static constexpr bool value = false || MemberOf<T, Set<Ts...>>::value;
+};
+
+int main()
+{
+    Set<int, double, char, short> s1;
+    std::cout << std::boolalpha << MemberOf<float, decltype(s1)>::value << '\n';
+    std::cout << std::boolalpha << MemberOf<short, decltype(s1)>::value << '\n';
+    std::cout << std::boolalpha << MemberOf<double, decltype(s1)>::value << '\n';
+    std::cout << std::boolalpha << MemberOf<long, decltype(s1)>::value << '\n';
+    return EXIT_SUCCESS;
+}
