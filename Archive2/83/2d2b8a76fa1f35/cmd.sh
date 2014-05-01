556b7eb4acb6004590b94b9048abc1746f7be405
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-01 15:14:38 +0000

    Update archive 83

---
+++ Archive2/83/2d2b8a76fa1f35/cmd.sh
@@ -0,0 +1,1 @@
+clang++ -std=c++11 -stdlib=libstdc++ -O2 -Wall -Wextra -pedantic -pthread main.cpp && ./a.out
---
+++ Archive2/83/2d2b8a76fa1f35/main.cpp
@@ -0,0 +1,6 @@
+#include <fstream>
+
+int main() {
+    std::ifstream f;
+    f = std::ifstream(__FILE__);
+}
