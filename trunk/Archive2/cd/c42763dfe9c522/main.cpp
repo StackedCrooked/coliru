d105ec3174b3e2f609a9dba39379696d39fabf9b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-01 08:53:22 +0000

    Update archive cd

---
+++ trunk/Archive2/cd/c42763dfe9c522/main.cpp
@@ -0,0 +1,43 @@
+#include <memory>
+#include <set>
+
+struct Collection;
+
+struct Item
+{
+};
+
+struct Collection
+{    
+    Item& add()
+    {
+        return **items.emplace().first;
+    }
+    
+    void remove(Item& item)
+    {
+        items.erase(items.find(std::unique_ptr<Item, maybe_delete>(&item, maybe_delete(false))));
+    }
+    
+private:
+    struct maybe_delete
+    {
+        maybe_delete(bool b = true) : b(b) {}
+        
+        void operator()(Item* item) {
+            if (b) delete item;
+        }
+        bool b;
+    };
+    std::set<std::unique_ptr<Item, maybe_delete>> items;
+};
+
+
+int main()
+{
+    Collection collection;
+    Item& item1 = collection.add();
+    Item& item2 = collection.add();
+    
+    collection.remove(item1);
+}
