1e0d35214bfb1129b53e0a69bf8557e8512c044b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-01 09:07:43 +0000

    Update archive fc

---
+++ Archive2/fc/3e04da3233b76d/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <typeinfo>
+
+class A { public: virtual ~A() = default ; };
+class B : public A{};
+class C : public A{};
+
+void func( A* a )
+{
+    if( a != nullptr )
+    {
+        std::cout << "dynamic type of the object pointed to is: " ;
+        
+        const std::type_info& type_info = typeid(*a) ; // typeid on reference to polymorphic type
+
+        if( type_info == typeid(A) ) std::cout << "A\n" ;
+        else if( type_info == typeid(B) ) std::cout << "B\n" ;
+        else if( type_info == typeid(C) ) std::cout << "C\n" ;
+        else std::cout << "an unknown class derrived from A\n" ;
+    }
+}
+
+int main()
+{
+    A a ; A* pa = &a ; func(pa) ; // A
+    B b ; pa = &b ; func(pa) ; // B
+    C c ; pa = &c ; func(pa) ; // C
+    struct D : A {} d ; pa = &d ; func(pa) ; // an unknown class derrived from A
+}
