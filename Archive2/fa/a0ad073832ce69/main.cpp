81b8e2fad6b3ca990774f78e91da13ba9e9fe5e2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-01 14:52:54 +0000

    Update archive fa

---
+++ Archive2/fa/a0ad073832ce69/main.cpp
@@ -0,0 +1,16 @@
+
+#include <studio.h>
+
+int main()
+{
+       int a, b, c;
+       printf("Entrez la valeur de a : ");
+       scanf("%d" , &b);
+       printf("Entrez la valeur de b : ");
+       scanf("%d" , &b);
+       c = a + b;
+       printf ("%d + %d = %d\n" , a, b, c);
+       return 0;
+}
+
+
