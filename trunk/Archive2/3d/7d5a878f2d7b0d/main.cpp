3f1762c3b10fea43441e972775b354355b6cbe12
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-01 02:49:48 +0000

    Update archive 3d

---
+++ trunk/Archive2/3d/7d5a878f2d7b0d/main.cpp
@@ -0,0 +1,38 @@
+#include <memory>
+#include <set>
+#include <iostream>
+
+struct Collection;
+
+struct Item
+{
+    ~Item(){ std::cout << "hi\n"; }
+};
+
+struct Collection
+{    
+    Item& add()
+    {
+        return **items.emplace().first;
+    }
+    
+    void remove(Item& item)
+    {
+        std::unique_ptr<Item> p(&item);
+        items.erase(p);
+        //p.release();
+    }
+    
+private:
+    std::set<std::unique_ptr<Item>> items;
+};
+
+
+int main()
+{
+    Collection collection;
+    Item& item1 = collection.add();
+    Item& item2 = collection.add();
+    
+    collection.remove(item1);
+}
