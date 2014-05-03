6eaf3dcf00b2f0d26dd1e2c1b94ba0e6aeb2d9f4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-03 03:10:20 +0000

    Update archive 2c

---
+++ trunk/Archive2/2c/6bb1e76263c56e/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+struct A {
+    void func() {
+        std::cerr << "A::func()" << std::endl;
+    }
+};
+struct B {
+    int func() {
+        std::cerr << "B::func()" << std::endl;
+        return 1;
+    }
+};
+
+// type_sink takes a type, and discards it.  type_sink_t is a C++1y style using alias for it
+template<typename T> struct type_sink { typedef void type; };
+template<typename T> using type_sink_t = typename type_sink<T>::type;
+
+// has_func is a traits class that inherits from `true_type` iff the expression t.func()
+// is a valid one.  `std::true_type` has `::value=true`, and is a good canonical way to
+// represent a compile-time `bool`ean value.
+template<typename T,typename=void> struct has_func : std::false_type {};
+template<typename T> struct has_func<
+  T,
+  type_sink_t< decltype( std::declval<T&>().func() ) >
+> : std::true_type {};
+
+// helpers for tag dispatching.
+namespace helper_ns {
+  template<typename T> void anotherFunc( T&& t, std::false_type /* has_func */ ) {}
+  template<typename T> void anotherFunc( T&& t, std::true_type /* has_func */ ) {
+    std::forward<T>(t).func();
+  }
+}
+// take the type T, determine if it has a .func() method.  Then tag dispatch
+// to the correct implementation:
+template<typename T> void anotherFunc(T t) {
+  helper_ns::anotherFunc( std::forward<T>(t), has_func<T>() );
+}
+
+int main() {
+    A a;
+    B b;
+    anotherFunc(a);
+    anotherFunc(b);
+}
