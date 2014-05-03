5f40cbd6f2156b66447be6e9032799cb357fce45
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-03 09:13:07 +0000

    Update archive f7

---
+++ trunk/Archive2/f7/b07a95620e7866/main.cpp
@@ -0,0 +1,14 @@
+// simple_example_2.cpp
+#include<iostream>
+#include<boost/tokenizer.hpp>
+#include<string>
+
+int main(){
+   using namespace std;
+   using namespace boost;
+   string s = "Field 1, Field 2 with embedded \\,Field 3";
+   tokenizer<escaped_list_separator<char> > tok(s);
+   for(tokenizer<escaped_list_separator<char> >::iterator beg=tok.begin(); beg!=tok.end();++beg){
+       cout << *beg << "\n";
+   }
+}
