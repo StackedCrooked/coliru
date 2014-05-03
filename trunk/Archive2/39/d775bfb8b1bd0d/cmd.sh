f97ecba6588b8e31469ab22db1d6ed4aa54465bc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-03 21:05:05 +0000

    Update archive 39

---
+++ trunk/Archive2/39/d775bfb8b1bd0d/cmd.sh
@@ -0,0 +1,25 @@
+cat<<INPUT>input.txt; clang++ -std=c++03 -Os -Wall -pedantic main.cpp && ./a.out
+[0]
+total=0
+
+[10000]
+total=3
+-593 427 683
+-976 703 701
+-974 307 688
+
+[20000]
+total=0
+
+[30000]
+total=1
+197 -83 153
+
+[30001]
+total=1
+77 49 244
+
+[40000]
+total=0
+
+INPUT
