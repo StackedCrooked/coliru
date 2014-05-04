b88e2d170aabcb05462d66c805cc6cfbb31c3e2b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-04 15:17:05 +0000

    Update archive c1

---
+++ trunk/Archive2/c1/9726f0bbe8931e/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "This code is for wrooks to look at" << std::endl;
+    std::cout << "And i love PONIES!" << std::endl;
+    std::wcout << L"AND CANDY!" << std::endl;
+    
+    return 0;
+}
