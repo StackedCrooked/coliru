6ec837b7c69c0f264e9b00a17a7b5662d99be039
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-04 21:26:26 +0000

    Update archive 76

---
+++ Archive2/76/245f4469d2ea40/main.cpp
@@ -0,0 +1,40 @@
+#include <cstdlib>
+#include <ctime>
+#include <array>
+
+struct T
+{
+    void setFlag(bool x) { this->x = x; }
+private:
+    bool x;
+};
+
+int main()
+{
+    std::srand(std::time(0));
+    const unsigned int N = 1000000;
+    
+    T obj;
+    std::array<int, N> A;
+    
+    for (unsigned int i = 0; i < N; i++)
+        A[i] = std::rand();
+    
+    #ifdef LOOP_A
+        for (unsigned int i = 0; i < N; i++)
+        {
+            if(A[i] > 10)
+                obj.setFlag(true);
+            else
+                obj.setFlag(false);
+        }
+    #endif
+    
+    #ifdef LOOP_B
+        for (unsigned int i = 0; i < N; i++)
+        {
+            obj.setFlag(A[i] > 10);
+        } 
+        
+    #endif
+}
