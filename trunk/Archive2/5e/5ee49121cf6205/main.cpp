c36af3a5bb684424816eda2d2df9053bd77ba18d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-04 03:12:38 +0000

    Update archive 5e

---
+++ trunk/Archive2/5e/5ee49121cf6205/main.cpp
@@ -0,0 +1,46 @@
+#include <cstdint>
+
+constexpr const std::uint8_t major = 1;
+constexpr const std::uint8_t minor = 10;
+constexpr const std::uint8_t bugfix = 0;
+
+constexpr class VersionStrHolder
+{
+public:
+    constexpr VersionStrHolder()
+        : ofs{sizeof(value) - 1}, value{}
+    {
+        predend_int(bugfix);
+        push_front('.');
+        predend_int(minor);
+        push_front('.');
+        predend_int(major);
+    }
+
+    constexpr const char* c_str() const { return value + ofs; }
+
+private:
+    constexpr void push_front(char c) { value[--ofs] = c; }
+
+    constexpr void predend_int(int x)
+    {
+        do
+        {
+            auto digit = x % 10;
+            x = x / 10;
+            push_front(digit + '0');
+        }
+        while (x);
+    }
+
+    int ofs;
+    char value[16]; // big enough to hold an 'xxx.xxx.xxx' string
+} versionStrHolder{};
+
+constexpr const char* version_str = versionStrHolder.c_str();
+
+#include <iostream>
+int main()
+{
+    std::cout << version_str << '\n';
+}
