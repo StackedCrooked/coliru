0ff67c68edc6ff715926e8712eecf2203882b1a2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-04 21:23:12 +0000

    Update archive ae

---
+++ trunk/Archive2/ae/f8ef7ea8047c7c/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "This code is for wrooks to look at" << std::endl;
+    std::cout << "And i love PONIES!" << std::endl;
+    
+    return 0;
+}
