16037d19cf7e8c916e6c2dfe7d5dbb0e01395cde
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-04 03:18:04 +0000

    Update archive 67

---
+++ Archive2/67/c28b751ba4f158/main.cpp
@@ -0,0 +1,16 @@
+struct delta
+{
+    template<typename I>
+	constexpr void inc(I& i) { ++i; }
+    
+    template<typename I>
+    constexpr I next(I i) { inc(i); return i; }
+};
+
+template<int I>
+struct A {};
+
+int main ()
+{
+	A<delta{}.next(4)>{};
+}
