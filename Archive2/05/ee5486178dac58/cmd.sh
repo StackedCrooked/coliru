e582a87dd009e318445cb581aba06c138a66c49f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-05 21:29:11 +0000

    Update archive 05

---
+++ Archive2/05/ee5486178dac58/cmd.sh
@@ -0,0 +1,1 @@
+set -x; ln -s /Archive2/f6/63cd838c7b8ec2/main.cpp nonius.hpp; g++ -std=c++11 -O2 -Wall -Wextra -pedantic main.cpp  && ./a.out
---
+++ Archive2/05/ee5486178dac58/main.cpp
@@ -0,0 +1,105 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
+#include <chrono>
+#include <iostream>
+#include <random>
+#include <vector>
+
+#include <math.h>
+
+#define NONIUS_RUNNER
+#include "nonius.hpp"
+
+using Clock = std::chrono::high_resolution_clock;
+using milliseconds = std::chrono::milliseconds;
+
+template<typename T>
+T abs_template(T t)
+{
+  return t>0 ? t : -t;
+}
+
+float abs_ugly(float f)
+{
+  (*reinterpret_cast<std::uint32_t*>(&f)) &= 0x7fffffff;
+  return f;
+}
+NONIUS_BENCHMARK("construct small", [](nonius::chronometer meter)
+{
+    std::vector<nonius::storage_for<std::string>> storage(meter.runs());
+    meter.measure([&](int i) { storage[i].construct("small"); });
+})
+
+
+
+NONIUS_BENCHMARK("naive template abs", [](nonius::chronometer meter) {
+  std::random_device rd;
+  std::mt19937 mersenne(rd());
+  std::uniform_real_distribution<> dist(-std::numeric_limits<float>::lowest(), std::numeric_limits<float>::max());
+  std::vector<float> v(meter.runs());
+  std::generate(std::begin(v), std::end(v), [&dist, &mersenne]() { return dist(mersenne); });
+
+  meter.measure([&](int i) {
+      volatile float temp;
+      temp = abs_template(v[i]);
+  });
+})
+#if 0
+int main()
+{
+
+
+  Clock::time_point t0 = Clock::now();
+
+
+  Clock::time_point trand = Clock::now();
+
+  volatile float temp;
+  for (float f : v)
+    temp = abs_template(f);
+
+  Clock::time_point ttemplate = Clock::now();
+
+  for (float f : v)
+    temp = abs_ugly(f);
+
+  Clock::time_point tugly = Clock::now();
+
+  for (float f : v)
+    temp = std::abs(f);
+
+  Clock::time_point tstd = Clock::now();
+
+  for (float f : v)
+    temp = ::fabsf(f);
+
+  Clock::time_point tfabsf = Clock::now();
+
+  for (float f : v)
+    temp = abs_template(f);
+
+  Clock::time_point ttemplate2 = Clock::now();
+
+  for (float f : v)
+    temp = abs_template(f);
+
+  Clock::time_point ttemplate3 = Clock::now();
+
+  milliseconds random_time = std::chrono::duration_cast<milliseconds>(trand - t0);
+  milliseconds template_time = std::chrono::duration_cast<milliseconds>(ttemplate - trand);
+  milliseconds ugly_time = std::chrono::duration_cast<milliseconds>(tugly - ttemplate);
+  milliseconds std_time = std::chrono::duration_cast<milliseconds>(tstd - tugly);
+  milliseconds c_time = std::chrono::duration_cast<milliseconds>(tfabsf - tstd);
+  milliseconds template_time2 = std::chrono::duration_cast<milliseconds>(ttemplate2 - tstd);
+  milliseconds template_time3 = std::chrono::duration_cast<milliseconds>(ttemplate3 - ttemplate2);
+  std::cout << "random number generation: " << random_time.count() << " ms\n"
+    << "naive template abs: " << template_time.count() << " ms\n"
+    << "ugly bitfiddling abs: " << ugly_time.count() << " ms\n"
+    << "std::abs: " << std_time.count() << " ms\n"
+    << "::fabsf: " << c_time.count() << " ms\n"
+    << "naive template abs: " << template_time2.count() << " ms\n"
+    << "naive template abs: " << template_time3.count() << " ms\n";
+}
+#endif
