a90a58c8a6e609938d1c028ced613d86249e732e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-05 15:26:22 +0000

    Update archive 92

---
+++ trunk/Archive2/92/55707d8fd381a1/main.cpp
@@ -0,0 +1,36 @@
+#include <boost/mpl/eval_if.hpp>
+#include <boost/mpl/identity.hpp>
+#include <boost/tti/has_type.hpp>
+#include <type_traits>
+
+#define BOOST_TTI_BOX_TYPE(U)           \
+template<class T>                       \
+struct box_type_ ## U                   \
+{                                       \
+        using type = typename T::U;     \
+};
+
+BOOST_TTI_HAS_TYPE(X)
+BOOST_TTI_BOX_TYPE(X)
+
+template<class T, class U = int>
+struct X_trait
+:
+        boost::mpl::eval_if<
+                has_type_X<T>,
+                box_type_X<T>,
+                boost::mpl::identity<U>
+        >
+{};
+
+template<class T>
+using X_trait_t = typename X_trait<T>::type;
+
+struct T1 { /* X defaults to int */ };
+struct T2 { using X = char; };
+
+int main()
+{
+    static_assert(std::is_same<X_trait_t<T1>, int >::value, "");
+    static_assert(std::is_same<X_trait_t<T2>, char>::value, "");
+}
