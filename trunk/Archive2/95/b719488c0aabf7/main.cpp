6873994c36105365ea9d3bc836cc7e5d1edefeab
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-06 09:11:55 +0000

    Update archive 95

---
+++ trunk/Archive2/95/b719488c0aabf7/main.cpp
@@ -0,0 +1,13 @@
+#include <cstddef>
+#include <iostream>
+
+int main()
+{
+    const std::size_t N = 100;
+    int* a = new int[N];
+ 
+    for (std::size_t n = 0; n < N; ++n)
+        a[n] = n;
+ 
+    delete[] a;
+}
