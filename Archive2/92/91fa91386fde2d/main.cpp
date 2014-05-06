6b3492c4e1e650e2c1b0c73ab736a7116fa43524
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-06 09:16:14 +0000

    Update archive 92

---
+++ Archive2/92/91fa91386fde2d/main.cpp
@@ -0,0 +1,23 @@
+#include <functional>
+#include <cstdio>
+
+
+void foo(std::function<void()> f)
+{
+    f();
+}
+
+
+int main()
+{
+    long a = 1;
+    long b = 2;
+    long c = 3;
+    long d = 4;
+    printf("start of program\n");
+    foo([]{ printf(" ^- for zero args\n"); });
+    foo([a]{ printf(" ^- for 1 arg (%lu)\n", a); });
+    foo([a, b]{ printf(" ^- for 2 args (%lu %lu)\n", a, b); });
+    foo([a, b, c]{ printf(" ^- for 3 args (%lu %lu %lu)\n", a, b, c); });
+    foo([a, b, c, d]{ printf(" ^- for 4 args (%lu %lu %lu %lu)\n", a, b, c, d); });
+}
