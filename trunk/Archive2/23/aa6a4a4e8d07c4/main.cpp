17d4e6a598ac34f371012376174e35a3352073ee
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-06 09:12:59 +0000

    Update archive 23

---
+++ trunk/Archive2/23/aa6a4a4e8d07c4/main.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+
+template <class T>
+struct Foo
+{
+    std::vector<T> vec;
+
+    std::vector<T> getVector() && {
+        // fill vector if empty
+        // and some other work
+        return std::move(vec);
+    }
+
+    std::vector<T> getVectorAndMore() &&
+    {
+        // do some more work
+        return getVector(); // not compile
+       // return std::move(*this).getVector(); // seems wrong to me
+    }
+};
+
+int main()
+{
+    Foo<int> foo;
+
+    auto vec = std::move(foo).getVectorAndMore();
+}
