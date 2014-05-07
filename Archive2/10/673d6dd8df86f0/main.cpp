028ccc8918c40c4803dffa44b05b14943910b366
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-07 15:27:23 +0000

    Update archive 10

---
+++ Archive2/10/673d6dd8df86f0/main.cpp
@@ -0,0 +1,13 @@
+    #include <stdio.h>
+    #include <time.h>
+    #include <mutex>
+    #include <iostream>
+    #include <vector>
+    #include <bitset>
+
+
+int main(){
+ double test = 5/2;
+ std::cout << test;
+ 
+}
