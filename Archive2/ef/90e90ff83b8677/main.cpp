32a3674a3d338b10ed2b7486479e3e63fd6a39ef
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-08 03:19:14 +0000

    Update archive ef

---
+++ Archive2/ef/90e90ff83b8677/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class A {
+    public:
+        A() : is_default_constructed(true) { cout << "A default constructor" << endl; }
+        A( int x, int y) : is_default_constructed(false) { cout << "A constructor " << x << " " << y << endl; }
+        ~A() { cout << "A destructor, default_constructed: " << (is_default_constructed ? "true" : "false") << endl; }
+    private:
+        bool is_default_constructed;
+};
+
+class B : public A {
+    public:
+        B() : is_default_constructed(true)  { A(); cout << "B default constructor" << endl; }
+        B( int x, int y ) : is_default_constructed(false) { A(x, y); cout << "B constructor " << x << " " << y << endl; }
+        ~B() { cout << "B destructor, default_constructed: " << (is_default_constructed ? "true" : "false") << endl; }
+    private:
+        bool is_default_constructed;
+};
+
+int main(void) {
+    B b(2, 4);
+    cout << "In main, after construction" << endl;
+    return 0;
+}
