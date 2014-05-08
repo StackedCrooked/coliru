de8f409dc6608669757544e78c34da070c42f629
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-08 03:24:23 +0000

    Update archive 7f

---
+++ Archive2/7f/d4f903cf5428e3/main.cpp
@@ -0,0 +1,70 @@
+#include <atomic>
+#include <iostream>
+#include <mutex>
+#include <vector>
+#include <thread>
+
+std::mutex mutexes[3];
+std::mutex print_mutex;
+
+
+
+
+struct DeferLock : std::unique_lock<std::mutex>
+{
+    DeferLock(int index) : std::unique_lock<std::mutex>(mutexes[index], std::defer_lock), retries() {}
+    
+    bool try_lock()
+    {
+        auto b = unique_lock::try_lock();
+        retries += int(!b);
+        return b;
+    }
+    
+    int retries;
+};
+
+
+const auto num_threads = 6;
+static std::atomic<int> num_started{0};
+
+auto spin_wait = []{
+    num_started++;
+    while (num_started < num_threads); // spin wait
+};
+    
+    
+
+auto lock = [](int i, int j, int k)
+{   
+    spin_wait();
+    
+    DeferLock a(i), b(j), c(k);
+    
+    std::lock(a, b, c);    
+    
+    a.unlock(), b.unlock(), c.unlock();
+    
+    
+    std::lock_guard<std::mutex> lock_print_mutex(print_mutex);
+    std::cout << "permutation: " << i <<  j << k
+              << " retries: " << a.retries << ',' << b.retries << ',' << c.retries
+              << std::endl;
+};
+    
+int main()
+{
+    
+    typedef std::thread t;
+    
+    std::thread threads[] = {
+        t([]{ lock(0, 1, 2); }),
+        t([]{ lock(0, 2, 1); }),
+        t([]{ lock(1, 0, 2); }),
+        t([]{ lock(1, 2, 0); }),
+        t([]{ lock(2, 0, 1); }),
+        t([]{ lock(2, 1, 0); })
+    };
+    
+    for (auto&& t : threads) t.join();
+}
