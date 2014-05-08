fd13228db0d45a59e1805b04c754a2a060da44d6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-08 09:17:12 +0000

    Update archive 21

---
+++ Archive2/21/12968e208b0db4/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+// Loop between 1 and 5
+int main()
+int iii=1;
+while (iii<=5)
+{
+    // loop between 1 and iii
+    int jjj = 1;
+    while (jjj <= iii)
+        cout << jjj++;
+ 
+    // print a newline at the end of each row
+    cout << endl;
+    iii++;
+}
