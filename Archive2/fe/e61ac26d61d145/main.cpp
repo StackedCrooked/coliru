3cd0e556baba4afe663aa9da7a09484b56539db9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-08 21:23:31 +0000

    Update archive fe

---
+++ Archive2/fe/e61ac26d61d145/main.cpp
@@ -0,0 +1,41 @@
+#include <utility>
+#include <cstddef>
+
+template<typename T, std::size_t First, std::size_t Second, class Enable = void>
+class other_class;
+
+template<typename T, std::size_t Size, class Enable = void>
+class my_class;                        // I have my reasons
+
+template<typename T, std::size_t Size>
+class my_class<T, Size, typename std::enable_if<Size >= 0>::type>{
+    public:
+        template<
+                std::size_t S = Size,
+                typename = typename std::enable_if<S >= 0>::type
+        >
+        operator other_class<T, 1, Size>();
+};
+
+template<typename T, std::size_t First, std::size_t Second>
+class other_class<T, First, Second, typename std::enable_if<First >= 0>::type>{
+    public:
+        other_class(){
+            // ...
+        }
+
+        my_class<T, First> data[Second];
+};
+
+template<typename T, std::size_t Size>
+template<std::size_t S, typename>
+my_class<T, Size, typename std::enable_if<Size >= 0>::type>::
+operator other_class<T, 1, Size>(){
+    return other_class<T, 1, Size>();
+}
+
+int main()
+{
+    my_class<int, 5>();
+    other_class<int, 0, 4>();
+}
