4eb83deb9e5269bbdb253ed74ae684d470cb3bd8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-08 15:31:30 +0000

    Update archive 54

---
+++ Archive2/54/5bccdf5c0fa1af/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <map>
+#include <iterator>
+
+template <typename Iter>
+class map_iterator : public std::iterator<std::bidirectional_iterator_tag,typename Iter::value_type::second_type>
+{
+public:
+    using type =  std::iterator<std::bidirectional_iterator_tag,typename Iter::value_type::second_type>;
+
+    map_iterator() {}
+    map_iterator(Iter j) : i(j) {}
+    map_iterator& operator++()
+    {
+        ++i;
+        return *this;
+    }
+    map_iterator operator++(int)
+    {
+        auto tmp = *this;
+        ++(*this);
+        return tmp;
+    }
+    map_iterator& operator--()
+    {
+        --i;
+        return *this;
+    }
+    map_iterator operator--(int)
+    {
+        auto tmp = *this;
+        --(*this);
+        return tmp;
+    }
+    bool operator==(map_iterator j) const {
+        return i == j.i;
+    }
+    bool operator!=(map_iterator j) const {
+        return !(*this == j);
+    }
+    typename type::reference operator*() {
+        return i->second;
+    }
+    typename type::pointer operator->() {
+        return &i->second;
+    }
+protected:
+    Iter i;
+};
+
+template <typename Iter>
+inline map_iterator<Iter> make_map_iterator(Iter j) {
+    return map_iterator<Iter>(j);
+}
+
+int main()
+{
+    std::map<int,std::string> m {{1, "Hi"},{2, "Bye"}};
+
+    for (auto i = make_map_iterator(m.begin()); i != make_map_iterator(m.end()); i++)
+    {
+        std::cout << *i << std::endl;
+    }
+}
