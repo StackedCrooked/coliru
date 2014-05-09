5fb0e968c1d962f2e956afdbce1e956b27c802b0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-09 21:06:36 +0000

    Update archive 1d

---
+++ Archive2/1d/eb28dec47eaf50/main.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+template<typename Type>
+class Predicate{
+public:
+    Predicate() {};
+    virtual ~Predicate(){};
+    virtual bool operator()(const Type & value) = 0;
+    virtual bool operator()(const Type * value){ //<-- this is the operator thats not working
+        return (*this)(*value);
+    };
+};
+
+template<typename Type>
+class Always : public Predicate<Type>{
+public:
+    using Predicate<Type>::operator();
+    bool operator()(const Type & value){return true;}
+    ~Always(){};
+};
+
+int main(){
+    Always<int> a;
+    const int i = 1000;
+    a(&i);
+}
