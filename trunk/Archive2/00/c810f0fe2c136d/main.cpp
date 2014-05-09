91bca9fb14b817414a586bf8b5e0cab82570111e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-09 21:05:18 +0000

    Update archive 00

---
+++ trunk/Archive2/00/c810f0fe2c136d/main.cpp
@@ -0,0 +1,34 @@
+#include <map>
+#include <vector>
+#include <iostream>
+
+template<class T>
+std::map<T, int> count_series(const std::vector<T>& seq)
+{
+    std::map<T, int> series;
+    if (!seq.empty())
+    {
+        auto current = seq[0];
+        series[current] = 1;
+        std::size_t i = 1;
+        while (i < seq.size())
+        {
+            if (current != seq[i])
+            {
+                current = seq[i];
+                series[current] += 1;
+            }
+        
+            ++i;
+        }
+    }
+    
+    return series;
+}
+
+int main()
+{
+    std::vector<int> seq = {1,1,1,2,1,2,1,1,2,2,2,2,1,1};
+    for (const auto& s : count_series(seq))
+        std::cout << s.first << ": " << s.second << '\n';
+}
