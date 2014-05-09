f213d18580f2af164d6929e4e0be1d31fd496844
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-09 15:32:10 +0000

    Update archive 52

---
+++ Archive2/52/2f926efd3d26a9/cmd.sh
@@ -0,0 +1,1 @@
+clang++ -std=c++1y -Wall -Wextra -pedantic-errors -O3 -pthread main.cpp && echo "1 5 3 " | ./a.out
---
+++ Archive2/52/2f926efd3d26a9/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    while (!std::cin.eof()) {
+        int i = 594;
+        std::cin >> i;
+        std::cout << "\"" << i << "\"\n";
+    }
+}
