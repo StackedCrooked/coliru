cbf4bd8f1569cf320065982a56bce5f7963d46e5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-09 21:06:04 +0000

    Update archive b1

---
+++ trunk/Archive2/b1/3a5f50ce14a559/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Please enter a number: ";
+    int number = 0;
+    std::cin >> number;
+    
+    std::cout << "The number entered was: " << number << std::endl;
+    
+    return 0;
+}
