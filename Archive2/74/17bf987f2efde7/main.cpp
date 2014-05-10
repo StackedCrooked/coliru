0434d163c2fdab6611197d4a6ca6d6f04989987c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-10 01:31:36 +0000

    Update archive 74

---
+++ Archive2/74/17bf987f2efde7/main.cpp
@@ -0,0 +1,42 @@
+#include <tuple>
+#include <utility>
+#include <iostream>
+
+template<size_t... I> struct sizes { using type = sizes<I...>; };
+
+template<size_t N, size_t K = 0, size_t... I>
+struct idx_t : idx_t<N, K+1, I..., K> {};
+
+template<size_t N, size_t... I>
+struct idx_t<N, N, I...> : sizes<I...> {};
+
+template<size_t N>
+using idx = typename idx_t<N>::type;
+
+struct A {
+    A(int) { std::cout << "This is int\n"; }
+	A(unsigned, double) { std::cout << "This is unsigned, double\n"; }
+};
+
+struct B {
+	B(char, int) { std::cout << "This is char, int\n"; }
+};
+
+template <typename T>
+class C : public T
+{
+    template<typename... A, size_t... I>
+    C(std::tuple<A...>&& a, sizes<I...>, double x) :
+    	T(std::get<I>(std::move(a))...) { std::cout << "This is double\n"; }
+
+public:
+    template<typename... A>
+    C(std::tuple<A...>&& a, double x) :
+		C(std::move(a), idx<sizeof...(A)>{}, x) { }
+};
+
+int main() {
+	C<A> p(std::forward_as_tuple(1),      1.0);
+	C<A> r(std::forward_as_tuple(1u, 2.), 1.0);
+	C<B> q(std::forward_as_tuple('c', 0), 1.0);
+}
