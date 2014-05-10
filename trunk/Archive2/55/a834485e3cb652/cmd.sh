acd4986404e28e0da7edeee834818ba254022d9a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-10 01:53:32 +0000

    Update archive 55

---
+++ trunk/Archive2/55/a834485e3cb652/cmd.sh
@@ -0,0 +1,1 @@
+clang++ -S -emit-llvm -std=c++11 -march=corei7-avx -O3 -Wall -pedantic -pthread main.cpp -o main.ll && cat main.ll && clang++ -S -std=c++11 -march=corei7-avx -O3 -Wall -pedantic -pthread main.cpp -o main.asm && cat main.asm
