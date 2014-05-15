a0344f06612b325c349b7561ec47d78b1acaaeac
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-15 20:16:53 +0000

    Update 40

---
+++ Archive2/40/7ac2bd0e07268d/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <tuple>
+
+template<typename F> 
+struct arity;
+
+template<typename R, typename ...Args> 
+struct arity<R (*)(Args...)>
+{
+    static const std::size_t value = sizeof ... (Args);
+};
+
+template<int ... N>
+struct seq
+{
+	using type = seq<N...>;
+
+	template<int I>
+	struct push_back : seq<N..., I> {};
+};
+
+template<int N>
+struct genseq : genseq<N-1>::type::template push_back<N-1> {};
+
+template<>
+struct genseq<0> : seq<> {};
+
+template<int N>
+using genseq_t = typename genseq<N>::type;
+        
+template<typename F, typename ArgEvaluator, int ...N>
+void invoke(seq<N...>, F f, ArgEvaluator arg_evaluator)
+{
+    using arg_type = decltype(arg_evaluator());
+    
+    constexpr std::size_t arity = sizeof ... (N);
+    
+    arg_type args[] { (N, arg_evaluator()) ... };
+    
+    f( args[N] ... );
+}
+
+
+int foo(int i, int j) {  std::cout << "foo(" << i << "," << j << ")\n"; return 0; }
+int bar(int i, int j, int k) {  std::cout << "bar(" << i << "," << j << "," << k << ")\n"; return 0; }
+
+
+template<typename F>
+void test_func(F f)
+{
+    int i=0;
+    auto arg_evaluator = [i]() mutable { return ++i; };
+    invoke(genseq_t<arity<F>::value>(), f, arg_evaluator);
+}
+
+int main()
+{
+    test_func(foo);
+    test_func(bar);
+}
