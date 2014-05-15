e92230b4bae023c258072877c8cad1a3cdf43233
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-15 20:19:17 +0000

    Update a3

---
+++ Archive2/a3/3726a45c5447dd/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <boost/filesystem.hpp>
+
+struct StructA {
+    boost::filesystem::path mPath;
+    std::string elem1;
+    int intVal;
+};
+
+template<typename CharT, typename Traits>
+std::basic_ostream<CharT, Traits>& 
+operator<<(std::basic_ostream<CharT, Traits>& os, StructA const& sa) {
+    return os << sa.mPath << " - " << sa.elem1 << " - " << sa.intVal << std::endl;
+}
+
+
+int main()
+{
+    
+    std::vector<StructA> aStructs = {
+        {"/zoo/dir1/filename.txt", "nameA1", 1}, 
+        {"/fad/dir1/filename.txt", "nameA1", 4}, 
+        {"/tmp/dir1/filename.txt", "nameA1", 3}
+    };    
+    
+    std::cout << "Order[mPath.filename(), elem1, intVal]" << std::endl;
+    std::cout << "======================================" << std::endl;
+    std::sort(aStructs.begin(), aStructs.end(),
+        [](const StructA& lhs, const StructA& rhs){
+            return std::make_tuple(lhs.mPath.filename(), lhs.elem1, lhs.intVal) < 
+                std::make_tuple(rhs.mPath.filename(), rhs.elem1, rhs.intVal);
+        });
+        
+    // print reordered structs
+    std::copy(aStructs.begin(), aStructs.end(),
+        std::ostream_iterator<StructA>(std::cout, ""));        
+        
+    std::cout << std::endl;
+    
+    std::cout << "Order[mPath, elem1, intVal]" << std::endl;
+    std::cout << "======================================" << std::endl;
+    std::sort(aStructs.begin(), aStructs.end(),
+        [](const StructA& lhs, const StructA& rhs){
+            return std::tie(lhs.mPath, lhs.elem1, lhs.intVal) < 
+                std::tie(rhs.mPath, rhs.elem1, rhs.intVal);
+        });
+        
+    // print reordered structs
+    std::copy(aStructs.begin(), aStructs.end(),
+        std::ostream_iterator<StructA>(std::cout, ""));
+
+    std::cout << std::endl;
+    
+    std::cout << "Order[mPath.filename(), elem1, intVal]" << std::endl;
+    std::cout << "======================================" << std::endl;
+    std::sort(aStructs.begin(), aStructs.end(),
+        [](const StructA& lhs, const StructA& rhs){
+            // attempt at efficiency - but not quite right
+            // AHA, I think I figured it out - see http://en.cppreference.com/w/cpp/utility/tuple/operator_cmp
+            // return value documentation which states 
+            // (bool)(std::get<0>(lhs) < std::get<0>(rhs)) || (!(bool)(std::get<0>(rhs) < std::get<0>(lhs)) && 
+            // lhstail < rhstail), where lhstail is lhs without its first element, and rhstail is rhs 
+            // without its first element. For two empty tuples, returns false.
+            // --------------------------------------------------------
+            // edit thanks to @Praetorian for suggesting the following:
+            // --------------------------------------------------------
+            auto f1 = lhs.mPath.filename(); auto f2 = rhs.mPath.filename();            
+            return std::tie(f1, lhs.elem1, lhs.intVal) < std::tie(f2, rhs.elem1, rhs.intVal);
+        });
+        
+    // print reordered structs
+    std::copy(aStructs.begin(), aStructs.end(),
+        std::ostream_iterator<StructA>(std::cout, ""));
+}
---
+++ Archive2/a3/fef10a60bbda7c/main.cpp
@@ -0,0 +1,27 @@
+#include <boost/phoenix.hpp>
+
+#include <boost/foreach.hpp>
+#include <iostream>
+#include <set>
+
+template <typename Action>
+void work(  Action doAction ) {
+    long results[] = { 2, 5, 4 };
+    BOOST_FOREACH( long r, results )
+        doAction( r );
+}
+
+bool cond( long r ) { return r % 2 == 0 ; }
+
+
+int main() {
+    namespace phx = boost::phoenix;
+    using namespace phx::placeholders;
+    
+    std::set<long> myResults;
+    work( 
+        if_(phx::bind(cond, _1)) [ phx::insert(phx::ref(myResults), _1) ] );
+
+    BOOST_FOREACH( long r, myResults )
+        std::cout << r << "\n";
+}
