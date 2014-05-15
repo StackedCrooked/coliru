f9a40df1ef7b4e2a5f6683992c253f18ceb6d312
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-15 20:09:51 +0000

    Update c2

---
+++ trunk/Archive2/c2/2f955de4287551/main.cpp
@@ -0,0 +1,17 @@
+#include <tuple>
+#include <iostream>
+
+template<class A>
+void func(A a) {
+    std::cout << "First of last: " << std::get<0>(a) << '\n';
+}
+
+template<class A, class... Types>
+void func(A a, Types... args) {
+    std::cout << "First of element: " << std::get<0>(a) << '\n';
+    func(args...);
+}
+
+int main() {
+    func(std::make_tuple(1,2), std::make_tuple("hello"));
+}
