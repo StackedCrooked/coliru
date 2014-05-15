0bd92ac78554f3f90eba57531f9c8e64714fd414
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-15 20:14:51 +0000

    Update 5e

---
+++ trunk/Archive2/5e/56275859c6dd8e/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <type_traits>
+#include <typeinfo>
+
+template<typename T>
+using Remove_cv_ref = std::remove_cv<typename std::remove_reference<T>::type>;
+    
+template<typename T>
+using Remove_ref_cv = std::remove_reference<typename std::remove_cv<T>::type>;
+static int a = 1666;
+int main()
+{
+    //std::cout << std::is_same<typename Remove_cv_ref<const int&>::type, int>::value;
+    //std::cout << std::is_same<typename Remove_ref_cv<const int&>::type, int>::value;
+    int *ptr = 0xFFFFFFFF;
+    std::cout << (0 > ptr) << ptr << std::endl;
+}
