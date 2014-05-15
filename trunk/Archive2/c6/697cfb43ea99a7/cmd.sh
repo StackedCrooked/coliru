54ed9752b2157f4cb67e489360b280932f97317b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-15 20:22:54 +0000

    Update c6

---
+++ trunk/Archive2/c6/697cfb43ea99a7/cmd.sh
@@ -0,0 +1,2 @@
+g++-4.8 -std=c++11 -Wall -Wextra -pedantic -pthread main.cpp -O2 && ./a.out
+clang++ -std=c++11 -Wall -Wextra -pedantic -pthread main.cpp -O2 && ./a.out
---
+++ trunk/Archive2/c6/697cfb43ea99a7/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int division(int a, int b) // Calcule a divisé par b.
+{
+    return a / b;
+}
+ 
+int main()
+{
+    int a {};
+    cout << "Valeur pour a : ";
+    cin >> a;
+   
+    auto b {};
+    cout << "Valeur pour b : ";
+    cin >> b;
+ 
+    cout << a << " / " << b << " = " << division(a,b) << endl;
+   
+    return 0;
+}
