e39badca53b608098d110786779a83ead2ef23d8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-15 20:22:39 +0000

    Update 34

---
+++ Archive2/34/f8e0f91cc058c9/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+struct vertex
+{
+    char x;
+    char y;
+    char z;
+};
+
+
+struct vertexinfo_1 
+{
+    char vertex_count;
+    const vertex* vertices;
+};
+
+struct vertexinfo_2
+{
+    char vertex_count;
+    const vertex vertices[];
+};
+
+// Consists of amount then vertices.
+const char DATA[] = { 0x03, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09 };
+
+int main()
+{
+    vertexinfo_1 vinf1;
+    vinf1.vertex_count = DATA[0];
+    vinf1.vertices = (const vertex*)(DATA + 1);
+    const vertexinfo_2* vinf2 = (const vertexinfo_2*)(DATA);
+    for (int i = 0; i != vinf1.vertex_count; i++) {
+    std::cout << "vertice " << i << " in vinf1: {" << int(vinf1.vertices[i].x) << ", "  << int(vinf1.vertices[i].y) << ", "  << int(vinf1.vertices[i].z) << "}" <<  std::endl;
+    std::cout << "vertice " << i << " in vinf2: {" << int(vinf2->vertices[i].x) << ", "  << int(vinf2->vertices[i].y) << ", "  << int(vinf2->vertices[i].z) << "}" << std::endl;
+    }
+    return 0;
+}
