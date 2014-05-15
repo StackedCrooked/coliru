af4ce2774cd8907576f7af0527b08ffe85490d5a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-15 20:13:04 +0000

    Update bf

---
+++ trunk/Archive2/bf/0ecdcf97771dc4/main.cpp
@@ -0,0 +1,55 @@
+#include <utility>
+#include <iostream>
+
+template< typename Signature >
+class Delegate;
+
+template< typename Ret, typename Param >
+class Delegate< Ret(Param) >
+{
+public:
+
+   Ret operator()(Param&& p_param)
+   {
+      return m_ifunc(m_obj, std::forward< Param >(p_param));
+   }
+
+   template< typename ObjType, Ret(ObjType::*Method)(Param) >
+   static auto createDelegate(ObjType * const p_obj)
+   {
+      Delegate< Ret(Param) > del;
+      del.m_obj = p_obj;
+      del.m_ifunc = &ifunction< ObjType, Method >;
+      return del;
+   }
+
+private:
+
+   void * m_obj = nullptr;
+   Ret (*m_ifunc)(void*, Param&&) = nullptr;
+
+   template< typename ObjType, Ret(ObjType::*Method)(Param) >
+   static Ret ifunction(void * const p_obj, Param&& p_param)
+   {
+      ObjType * const obj = (ObjType * const) p_obj;
+      return (obj->*Method)(std::forward< Param >(p_param));
+   }
+};
+
+struct Test
+{
+   void test(int x)
+   {
+      std::cout << x << std::endl;
+   }
+
+};
+
+int main()
+{
+   Test t;
+
+   Delegate< void(int) > d = Delegate< void(int) >::createDelegate< Test, &Test::test >(&t);
+
+   d(5);
+}
---
+++ trunk/Archive2/bf/a3c7f2cd30a536/cmd.sh
@@ -0,0 +1,1 @@
+pwd #g++-4.8 -std=c++11 -O2 -Wall -pedantic -pthread main.cpp -lboost_system -lboost_filesystem && ./a.out
