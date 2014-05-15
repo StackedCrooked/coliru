bed5dbb8f656065c3b1ef6e6c6a6cea31973eaa1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-15 20:23:39 +0000

    Update 64

---
+++ trunk/Archive2/64/bd23273a4032b5/main.cpp
@@ -0,0 +1,14 @@
+#include <cstdint>
+
+uint8_t SoundEffects(uint8_t EffectType,uint8_t RecordedSample); 
+
+uint8_t SoundEffects(uint8_t EffectType,uint8_t RecordedSample)
+{
+    // some code
+    return 0;
+} 
+
+
+int main() {
+    SoundEffects(0,0);   
+}
