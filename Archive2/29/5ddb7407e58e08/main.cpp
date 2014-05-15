ddfc7f5f75e4dde1e983dfdbde64004850198c3b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-15 20:19:43 +0000

    Update 29

---
+++ Archive2/29/5ddb7407e58e08/main.cpp
@@ -0,0 +1,24 @@
+#include <boost/spirit/include/qi.hpp>
+
+namespace qi = boost::spirit::qi;
+
+template <typename It>
+struct DiamidentGrammar : public qi::grammar<It>
+{
+    DiamidentGrammar() : DiamidentGrammar::base_type(diameterIdentity)
+    {
+        using namespace qi;
+        diameterIdentity = realm;
+        realm            = label % '.'; 
+        label            = diameterName | diameterDname;
+        diameterName     = alpha >> *(alnum | '-');
+        diameterDname    = digit >> +(alnum | '-');
+    };
+  private:
+    qi::rule<It> diameterIdentity, realm, label, diameterName, diameterDname;
+};
+
+int main()
+{
+    DiamidentGrammar<std::string::const_iterator> p;
+}
