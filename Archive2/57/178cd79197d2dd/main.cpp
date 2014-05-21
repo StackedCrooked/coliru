51101cb52ec7c6903b4eb4dc3b51cb5313c00c29
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-21 21:02:22 +0000

    Update 57

---
+++ Archive2/57/178cd79197d2dd/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <memory>
+#include <fstream>
+#include <limits>
+#include <string>
+#include <array>
+
+class Texture
+{
+public:
+    Texture(const std::string&)
+    {
+        std::cout << "Texture::Texture() for " << this << std::endl;
+    }
+
+    Texture(const Texture& other)
+    {
+        std::cout << "Texture::Texture(const Texture&) for " << this << std::endl;
+    }
+
+    ~Texture()
+    {
+        std::cout << "Texture::~Texture() for " << this << std::endl;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    std::shared_ptr<Texture> up(std::make_shared<Texture>("textures/lakeup.png"));
+    return 0;
+}
