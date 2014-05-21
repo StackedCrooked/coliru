fc31d6ae2cc119f582171d31a3f1bcd5636f34dc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-21 20:03:51 +0000

    Update 78

---
+++ Archive2/78/3096372f411226/main.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    char arr[2];
+    sprintf(arr, "+");
+    puts(arr);
+}
---
+++ Archive2/78/41b43f0f389b09/main.cpp
@@ -0,0 +1,12 @@
+#include <cstddef>
+#include <type_traits>
+
+#include <iostream>
+#include <array>
+
+int main () {
+    
+    std::cout << std::rank<std::array<int, 1>>::value << std::endl;
+    std::cout << std::extent<std::array<int, 1>>::value << std::endl;
+    
+}
