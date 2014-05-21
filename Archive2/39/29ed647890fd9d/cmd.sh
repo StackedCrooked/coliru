a7a6de3214f62704f8345c3bdc23f0f66715fd73
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-21 20:00:49 +0000

    Update 39

---
+++ Archive2/39/29ed647890fd9d/cmd.sh
@@ -0,0 +1,12 @@
+cat>csv.txt<<HERE; clang++ -std=c++11 -Os -Wall -pedantic -pthread main.cpp -lboost_iostreams && ./a.out
+hoot,hoot's,hootch,hootch's,hooted
+famished,famishes,famishing,famous,famously
+blemish,blemish's,blemished,blemishes,blemishing
+reliability,reliability's,reliable,reliably,reliance
+reinstating,reinterpret,reinterpretation,reinterpretation's,reinterpretations
+Pharisee's,Pharisees,Phekda,Phekda's,Phelps
+pansy,pansy's,pant,pant's,pantaloons
+hosteling,hostelled,hostelling,hostelries,hostelry
+hollyhock's,hollyhocks,holocaust,holocaust's,holocausts
+caresses,caressing,caret,caret's,caretaker
+HERE
