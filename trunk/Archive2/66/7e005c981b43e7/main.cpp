1d1b240790df6433113d21b0ae2e7da9109d8f45
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-21 20:48:28 +0000

    Update 66

---
+++ trunk/Archive2/66/7e005c981b43e7/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <vector>
+int main()
+{
+    // prepare file
+    std::ofstream("test.txt") << 1 << ' ' << 2 << '\n';
+    std::FILE* f = std::fopen("test.txt", "r");
+ 
+    std::vector<char> buf(4); // char is trivally copyable
+    if ( std::fread(&buf[0], sizeof buf[0], buf.size(), f) < buf.size() ) {
+        std::cerr << "Failed to read" << std::endl;
+        exit(1);
+    }
+ 
+    for(char n : buf)
+        std::cout << n;
+ 
+    std::vector<std::string> buf2; // string is not trivially copyable
+// this would result in undefined behavior
+//    std::fread(&buf2[0], sizeof buf2[0], buf2.size(), f);
+}
