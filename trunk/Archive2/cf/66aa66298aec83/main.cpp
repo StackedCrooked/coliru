8b26da9e194db0d478488e8b15aabd2eae4b15c5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-21 20:53:03 +0000

    Update cf

---
+++ trunk/Archive2/cf/66aa66298aec83/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <locale.h>
+#include <time.h>
+#include <wchar.h>
+ 
+int main()
+{
+    setlocale(LC_ALL, "en_US.UTF-8"); // the C locale will be the UTF-8 enabled English
+    setlocale(LC_NUMERIC, "de_DE");   // decimal dot will be German
+    setlocale(LC_TIME, "ja_JP");      // date/time formatting will be Japanese
+    wchar_t str[100];
+    time_t t = time(NULL);
+    wcsftime(str, 100, L"%A %c", localtime(&t));
+    wprintf(L"номер: %.2f\nDate: %Ls\n", 3.14, str);
+}
