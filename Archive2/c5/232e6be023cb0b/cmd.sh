96d4d10b7e83409158c8e17b0f03a857b1e95156
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-21 21:00:07 +0000

    Update c5

---
+++ Archive2/c5/232e6be023cb0b/cmd.sh
@@ -0,0 +1,7 @@
+cat>config.cfg<<HERE; clang++ -std=c++11 -Os -Wall -pedantic -pthread main.cpp && ./a.out && cat ./config.cfg.ex
+"Info"
+{
+    a "If updating this file make sure to update all settings accordingly."
+    b "This program has been created by Name 'Nickname' Lastname"
+}
+HERE
