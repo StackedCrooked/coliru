fbe4ea4fa70cc269741ccba80ff880261cf4e82d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-21 21:00:34 +0000

    Update c4

---
+++ trunk/Archive2/c4/d7842c0ea234ba/main.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <string>
+
+void seven_segment_display(std::string number)
+{
+    auto row = [&](auto r)
+    {
+        for (char ch : number) std::printf("%.3s ", r + (ch - '0') * 3);
+        std::putchar('\n');
+    };
+    row(" _     _  _     _  _  _  _  _ ");
+    row("| |  | _| _||_||_ |_   ||_||_|");
+    row("|_|  ||_  _|  | _||_|  ||_| _|");
+}
+
+int main()
+{
+    std::putchar('\n');
+    seven_segment_display("1920385764");
+}
