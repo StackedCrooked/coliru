0e58a22d7f2614fe574df3d914b2cf69c4e56055
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-23 10:49:53 +0000

    Update archive 3e

---
+++ trunk/Archive2/3e/0c71de722801bb/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <tuple>
+#include <functional>
+
+struct S{
+    //typedef S ThisT;
+    
+    template<class IdsT, class DataT>
+    struct CallbackPack{
+        /*constexpr */CallbackPack(IdsT &selfIds):
+            selfIds(selfIds){}
+
+        const IdsT  &selfIds;
+    };    
+    
+    const  CallbackPack<S, S> callbackPack123{*this};
+};
+ 
+int main()
+{
+    return 0;
+}
