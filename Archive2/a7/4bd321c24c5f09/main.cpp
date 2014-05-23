a27fa0b3ff77f26b1aebdc8fe35ecb5cc24e4df1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-23 04:40:20 +0000

    Update archive a7

---
+++ Archive2/a7/4bd321c24c5f09/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+ 
+int main()
+{
+    std::vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::fill_n(std::back_inserter(v), 3, -1);
+    for (int n : v)
+        std::cout << n << ' ';
+}
