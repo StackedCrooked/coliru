09264c6fd63639beb632c3c1708487c1cc1f358d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-23 22:47:02 +0000

    Update archive b7

---
+++ trunk/Archive2/b7/7f88624a9dc9a5/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <functional>
+#include <type_traits>
+
+template <bool B, typename T = void>
+struct enable_if {
+  typedef T type;
+};
+
+template <typename T>
+struct enable_if<false, T> {};
+
+template <class T> class A
+{
+private:
+    template <class S>
+    typename enable_if<std::is_integral<S>::value>::type a_member() {
+        std::cout << "Doing something! :D";
+    }
+
+    //template <class S>
+    //typename enable_if<std::is_floating_point<S>::value>::type a_member() {
+    //    std::cout << "Doing something :(
+    //}
+
+public:
+    void another_member() {
+        a_member<T>();
+    }
+};
+
+int main() {
+    A<int> AInt;
+    AInt.another_member();
+
+    A<float> AFloat;
+    AFloat.another_member(); // still making a function call, even if it's a no-op
+}
