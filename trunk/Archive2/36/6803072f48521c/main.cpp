19515bae896063b030737fdc97c28202ee54be85
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-23 04:46:28 +0000

    Update archive 36

---
+++ trunk/Archive2/36/6803072f48521c/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <boost/regex.hpp>
+
+int main() {
+    std::string orig("abc.bc");
+    std::string::const_iterator start, end;
+    start = orig.begin();
+    end = orig.end();
+    boost::match_results<std::string::const_iterator> what;
+    boost::regex reg("\\([a-z]\\{1,\\}*\\)\\.\\1", boost::regex::basic);
+    bool found = regex_search(start, end,  what, reg, boost::match_posix);
+    std::cout << found;
+}
