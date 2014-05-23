8342858bb1df0af4cf142f5b2790f386716f75bb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-23 10:51:03 +0000

    Update archive 18

---
+++ Archive2/18/03d5fe673d6c68/main.cpp
@@ -0,0 +1,16 @@
+#include <type_traits>
+
+void f(int = 0);
+
+template<typename... T, typename = decltype(f(std::declval<T>()...))>
+std::true_type check(int &&, T &&...);
+
+std::false_type check(...);
+
+template<typename... T> using Predicate = decltype(check(0, std::declval<T>()...));
+
+static_assert(!Predicate<int, int>::value, "!!");
+static_assert( Predicate<int>::value, "!!");
+static_assert( Predicate<>::value, "!!");
+
+int main() {}
