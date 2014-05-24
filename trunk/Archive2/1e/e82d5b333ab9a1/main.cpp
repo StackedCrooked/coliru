de5c0b6cf81267dfbf9285bdf88cb3f753c604cb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-24 23:01:17 +0000

    Update archive 1e

---
+++ trunk/Archive2/1e/e82d5b333ab9a1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+using namespace std;
+
+class X
+{
+public:
+    X(){}
+    ~X() { cout << "Destructor X" << endl; }
+    void Print() { cout << "X" << endl; }
+};
+
+int main()
+{
+    unique_ptr<X> ptr;
+    ptr->Print();
+    return 0;
+}
