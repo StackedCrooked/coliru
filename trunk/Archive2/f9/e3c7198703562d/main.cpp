9d00f1d24fc77be113a7b3ebb09945a6e5f6a1f0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-24 22:44:17 +0000

    Update archive f9

---
+++ trunk/Archive2/f9/e3c7198703562d/main.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <map>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <set>
+
+using namespace std;
+
+struct mycomparer 
+{
+    bool operator () (string const& lhs, 
+                      pair<string const, int> const& rhs) 
+    {
+        return lhs < rhs.first;
+    }
+
+    bool operator () (pair<string const, int> const& lhs,
+                      string const& rhs) 
+    {
+        return lhs.first < rhs;
+    }
+};
+
+int main()
+{
+    
+    vector<string> custom_list;
+    map<string, int> master_list;
+    vector<string> target_list;
+
+    std::sort(custom_list.begin(), custom_list.end());
+    std::set_intersection(custom_list.begin(), 
+                          custom_list.end(), 
+                          master_list.begin(),
+                          master_list.end(), 
+                          back_inserter(target_list), 
+                          mycomparer());
+}
