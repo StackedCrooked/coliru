e89ee252742919220236b048e29410770869c99d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-24 22:48:49 +0000

    Update archive 5e

---
+++ Archive2/5e/07b030f8e4831c/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    const int NUM_ROWS = 6 ;
+    
+    for( int row = 1 ; row <= NUM_ROWS ; ++row ) // for each row 1 .. NUM_ROWS
+    {
+        // print numbers row, row+1 ... (row times) 
+        for( int col = 0 ; col < row ; ++col )
+        {
+            std::cout << std::setw(2) << row+col << ' ' ;
+        }
+        
+        std::cout << '\n' ; // and a new line at the end of each row
+    }
+}
