e1bcee0c9dd80f9e4d7a43fdb65c307a29a9c4df
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-26 23:00:39 +0000

    Update archive 14

---
+++ Archive2/14/94b46e5a832770/main.cpp
@@ -0,0 +1,11 @@
+template<class T> struct id{ using type = T; };
+template<class T>
+using id_t = typename id<T>::type;
+
+struct B { void f(){} };
+struct D : B {};
+template<typename T, typename R> void g(R id_t<T>::*){}
+
+int main() {
+    g<D>(&D::f);
+}
