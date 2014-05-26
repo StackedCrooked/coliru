b87fb852d8a92a23a3fa1a547f4ba2ca76afb925
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-26 04:50:24 +0000

    Update archive 0d

---
+++ trunk/Archive2/0d/1516da6b0a8a15/main.cpp
@@ -0,0 +1,23 @@
+thread_local constexpr static const unsigned long long*
+    const* const* const* const* const* const* const* const*
+    const* const* const* const* const* const* const* const*
+    const* const* const* const* const* const* const* const*
+    const* const* const* const* const* const* const* const*
+    const* const* const* const* const* const* const* const*
+    const* const* const* const* const* const* const* const*
+    const* const* const* const* const* const* const* const*
+    const* const* const* const* const* const* const* const*
+    const* const* const* const* const* const* const* const*
+    const* const* const* const* const* const* const* const*
+    const* const* const* const* const* const* const* const*
+    const* const* const* const* const* const* const* const*
+    const* const* const* const* const* const* const* const*
+    const* const* const* const* const* const* const* const*
+    const* const* const* const* const* const* const* const*
+    const* const* const* const* const* const* const* const*
+    const* const* const* const* const* const* const* const*
+    const* const* const* const* const* const* const* const* i{nullptr};
+
+int main()
+{
+}
