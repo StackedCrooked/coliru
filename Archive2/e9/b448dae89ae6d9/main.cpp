a41272d53b6495a1ec413d67e24aefd8a0335870
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-26 23:08:29 +0000

    Update archive e9

---
+++ Archive2/e9/b448dae89ae6d9/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+     double Ri = 0.0, Ti, Ui = 0.0;
+     
+     std::cout
+     << "Ri = " << Ri << "\n"
+     << "Ti = " << Ti << "\n"
+     << "Ui = " << Ui << "\n"
+     ;
+     
+
+    return 0;
+}
+
