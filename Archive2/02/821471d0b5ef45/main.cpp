a14c787cb15c57ba7ac0b5ca8c53bec67776f144
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-27 05:01:32 +0000

    Update archive 02

---
+++ Archive2/02/821471d0b5ef45/main.cpp
@@ -0,0 +1,30 @@
+#define AUTO_RETURN( ... ) -> decltype(__VA_ARGS__) { return (__VA_ARGS__); }
+
+template< typename F,
+          typename T1,
+          typename...
+           Args >
+auto invoke( F f, T1&& t1, Args&&... args )
+AUTO_RETURN(  (std::forward<T1>(t1).*f)(std::forward<Args>(args)...)  )
+
+template< typename F,
+          typename T1,
+          typename... Args >
+auto invoke( F f, T1 t1, Args&&... args )
+AUTO_RETURN(  (t1->*f)(std::forward<Args>(args)...)  )
+
+template< typename F,
+          typename T1 >
+auto invoke( F f, T1&& t1 )
+AUTO_RETURN(  std::forward<T1>(t1).*f  )
+
+template< typename F,
+          typename T1 >
+auto invoke( F f, T1 t1 )
+AUTO_RETURN(  t1->*f  )
+
+template< typename F, typename... Args >
+auto invoke( F&& f, Args&&... args )
+{
+    return std::forward<F>(f)(std::forward<Args>(args)...);
+}
