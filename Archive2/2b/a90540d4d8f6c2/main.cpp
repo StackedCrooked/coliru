b24e04f8292b942458ab9c75a6706a8ca76cf492
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-27 23:11:26 +0000

    Update archive 2b

---
+++ Archive2/2b/a90540d4d8f6c2/main.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include <type_traits>
+
+struct A
+{
+    std::string hello = "world";
+  A() = delete;
+};
+
+int main() {
+    A a{};
+    std::cout << a.hello << "\n";
+    std::cout << std::is_pod<std::string>::value << "\n";
+    std::cout << std::is_pod<A>::value << "\n";
+}
