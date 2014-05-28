0b63a45245dd6ebc59cad189ce9328bedcd64ce8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-28 04:59:13 +0000

    Update archive 8e

---
+++ Archive2/8e/04e822919cc131/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+template <typename ContainerType, typename ResultType = double>
+ResultType avg(const ContainerType c) {
+    typename ContainerType::value_type total{};
+    for (auto v : c) {
+        total += v;
+    }
+    return total / c.size();
+}
+
+
+int main() {
+    std::cout << std::fixed;
+    std::cout << "Average of some ints:    " << avg(std::vector<int>{5, 12, 136, 999, 17}) << '\n';
+    std::cout << "Average of some doubles: " << avg(std::vector<double>{1240.54124, 25231.2993, 2839928.22407, 2398.234}) << '\n';
+}
