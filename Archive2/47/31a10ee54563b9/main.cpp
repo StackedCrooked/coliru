a5508dff229d415fa3b6b7eb11eeca9cd3fc3ba0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-28 17:14:15 +0000

    Update archive 47

---
+++ Archive2/47/31a10ee54563b9/main.cpp
@@ -0,0 +1,41 @@
+#include <array>
+#include <cstdio>
+
+template <size_t N>
+struct bar {
+        template <typename T, typename ...Tn>
+        static constexpr auto apply(T v, Tn ...vs)
+        {
+                return bar<N - 1>::apply(v, v, vs...);
+        }
+};
+
+template <>
+struct bar<1> {
+        template <typename T, typename ...Tn>
+        static constexpr auto apply(T v, Tn ...vs)
+        {
+                return std::array<T, sizeof...(vs) + 1>{v, vs...};
+        }
+
+};
+
+template <typename T, size_t N>
+struct foo {
+        std::array<T, N> data;
+
+        constexpr foo(T val)
+        : data(bar<N>::apply(val))
+        {}
+};
+
+int main(int argc, char **argv)
+{
+
+        constexpr foo<int, 10> f(5);
+        for (auto d: f.data)
+                printf("%d", f.data[d]);
+
+    	printf("\n");
+        return 0;
+}
