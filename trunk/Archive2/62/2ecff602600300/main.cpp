ee6425ee221bf33d2ce09c2d0cfad83cf0defec4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-29 17:31:20 +0000

    Update archive 62

---
+++ trunk/Archive2/62/2ecff602600300/main.cpp
@@ -0,0 +1,16 @@
+Hallo, 
+
+ich habe eine Frage zu ihrem Produkt "Milbopax", Sprühlösung. In der Gebrauchsanweisung steht etwas irritierend bei "Anwendung", 1)
+
+  "Abnehmbare Bezüge bitte vor der Behandlung abziehen und nach Herstellerangaben waschen oder reinigen..."
+
+Bedeutet das, dass die Möbel bzw der Matratzenkern ohne den Bezug eingesprüht werden sollen, oder soll der abnehmbare Bezug nach dem 
+Waschen wieder aufgezogen werden, und nur dieser dann eingesprüht werden?
+
+Ich besitze ein Sofa mit relativ dicken Baumwolle/Polyester-Bezügen (80%/20%), und jetzt habe ich die Bezüge abgenommen, ausgeschüttelt 
+(da die Bezüge noch relativ neu sind habe ich sie nicht gewaschen), und dann die Kissen selbst eingesprüht, die Bezüge aber nicht.
+
+War das richtig, oder hätte ich das Kissen samt Bezug einsprühen sollen? Oder sollte man das Kissen separat und dann den Bezug noch einmal 
+für sich einsprühen?
+
+Vielen Dank für ihre Antwort,
