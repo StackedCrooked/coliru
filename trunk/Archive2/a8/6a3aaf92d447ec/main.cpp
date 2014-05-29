a0c8dfc188ffbf8fc81b43b491a08124f40ad131
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-29 17:27:57 +0000

    Update archive a8

---
+++ trunk/Archive2/a8/6a3aaf92d447ec/main.cpp
@@ -0,0 +1,46 @@
+#include <memory>
+
+using namespace std;
+
+struct HeavyResource
+{
+    int data;
+    HeavyResource(int data): data(data) {}
+};
+
+class Wrapper
+{
+    std::shared_ptr<HeavyResource> heavyResource;
+    public:
+    explicit Wrapper(std::shared_ptr<HeavyResource> heavyResource): heavyResource(heavyResource) {}
+};
+
+Wrapper returnAWrapper()
+{
+    auto heavyResource = std::make_shared<HeavyResource>(1);
+    return Wrapper(heavyResource);
+}
+
+void useWrapperTemporarily(std::shared_ptr<HeavyResource> heavyResource)
+{
+    Wrapper wrapper(heavyResource);
+    // Do stuff with wrapper...
+}
+
+void doStuffWithResource(std::shared_ptr<HeavyResource> heavyResource)
+{
+    // Do stuff with heavyResource WITHOUT constructing
+    // a Wrapper around it...
+}
+
+int main()
+{
+    // Case 1.
+    Wrapper wrapper1 = returnAWrapper();
+    
+    auto heavyResource = std::make_shared<HeavyResource>(2);
+    
+    // Case 2.
+    useWrapperTemporarily(heavyResource);
+    doStuffWithResource(heavyResource);
+}
