3f0257eda6b77e5dab98539931efb773d4bac22e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-29 17:08:17 +0000

    Update archive ee

---
+++ trunk/Archive2/ee/ffa20eafc89d44/main.cpp
@@ -0,0 +1,19 @@
+#include <type_traits>
+#include <iostream>
+
+template<typename T>
+using unqualified_one = typename std::remove_reference<typename std::remove_cv<T>::type>::type;
+
+template<typename T>
+using unqualified_two = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
+
+template<typename T, typename U = unqualified_one<T>, typename V = unqualified_two<T>>
+void stacktrace() {
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+//    static_assert(std::is_same<U, int>::value, "..."); // wtf?
+//    static_assert(std::is_same<V, int>::value, "...");
+}
+
+int main() {
+    stacktrace<const int&>();
+}
