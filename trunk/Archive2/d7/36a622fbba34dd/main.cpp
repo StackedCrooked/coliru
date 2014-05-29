0d3c00e5c280a098eb14c8d217d769e9cf47300f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-29 05:10:09 +0000

    Update archive d7

---
+++ trunk/Archive2/d7/36a622fbba34dd/main.cpp
@@ -0,0 +1,74 @@
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <typeinfo>
+#include <memory>
+
+using namespace std;
+
+class A
+{
+    weak_ptr<A> parent;
+   shared_ptr<A> ptr;
+    vector<weak_ptr<A> > children;
+public:
+    A() :  name("I'm new")
+    {
+        }
+
+    virtual ~A(){
+        cout<<name<<" died"<<endl;
+    }
+    string name;
+    virtual void setParent(shared_ptr<A> &child){
+
+            parent=child;
+
+    }
+    virtual void hello()
+    {
+
+      cout<<"i am "<<name<<endl;
+      for(auto &i : children)
+      {
+        if(auto pi=i.lock()){
+            pi->hello();
+        }
+      }
+    }
+
+};
+
+class B : public A
+{
+    public:
+    int numbers;
+    virtual ~B(){
+        cout<<name<<" died (B!)"<<endl;
+    }
+    virtual void hello(){
+        A::hello();
+         cout<<name<<" is a B!"<<endl;
+    }
+
+};
+
+
+int main(){
+    auto a1= shared_ptr<A>(new B());
+    auto a2= shared_ptr<A>(new A());
+    auto a3= shared_ptr<A>(new B());
+
+    a1->name="1";
+    a2->name="2";
+    a3->name="3";
+
+    a2->setParent(a1);
+    a3->setParent(a2);
+
+    a1->hello();
+
+cout<<"done"<<endl;
+    return 0;
+}
