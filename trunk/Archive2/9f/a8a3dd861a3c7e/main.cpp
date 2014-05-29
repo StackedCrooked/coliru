d3d81a7f19241b56729b10b5540a1de28c557c74
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-29 05:07:28 +0000

    Update archive 9f

---
+++ trunk/Archive2/9f/a8a3dd861a3c7e/main.cpp
@@ -0,0 +1,69 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <typeinfo>
+#include <memory>
+
+using namespace std;
+
+class A
+{
+    shared_ptr<A> parent;
+    vector<weak_ptr<A> > children;
+public:
+    A() : name("I'm new")
+    {}
+    
+    ~A(){
+        cout<<name<<" died"<<endl;
+    }
+    string name;
+    virtual void setParent(A *child){
+        parent=shared_ptr<A>(child);
+    }
+    virtual void hello()
+    {
+        cout<<"i am "<<name<<endl;
+      for(auto &i : children)
+      {
+        if(auto pi=i.lock()){
+            pi->hello();
+        }
+      }
+    }
+
+};
+
+class B : public A
+{
+    int numbers;
+    virtual void hello(){
+        A::hello();
+         cout<<name<<" is a B!"<<endl;       
+    }
+    
+};
+
+
+int main(){
+    auto a1= shared_ptr<A>(new B());
+    auto a2= shared_ptr<A>(new A());
+    auto a3= shared_ptr<A>(new B());
+    
+    a1->name="1";
+    a2->name="2";
+    a3->name="3";
+    
+    a2->setParent(a1.get());
+    a3->setParent(a2.get());
+      
+    
+//    shared_ptr<A*> a1=&a2;
+    
+    
+    a1->hello();
+    a2= shared_ptr<A>(new A());
+    a1->hello();
+    cout<<"done"<<endl;
+    return 0;
+}
