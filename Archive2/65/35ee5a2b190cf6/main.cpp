32e3515450bf3697f4f0f0cb4380dca3dbeda462
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-30 05:35:45 +0000

    Update archive 65

---
+++ Archive2/65/35ee5a2b190cf6/main.cpp
@@ -0,0 +1,49 @@
+#include <functional>
+#include <iostream>
+#include <thread>
+
+struct Foo
+{
+    int x, *ptr;
+    std::thread thread;
+
+    Foo(int x) :
+        x(x)
+    {
+        ptr = &this->x;
+    }
+        
+    void print(volatile int * ov=nullptr)
+    {
+        volatile int * temp = nullptr;
+
+        if (ov != nullptr)  temp = ov;
+        else                temp = this->ptr;
+
+        std::cout << *temp << std::endl;
+    }
+
+    void print_thread(volatile int * ov)
+    {
+        thread = std::thread([this, ov]() 
+                {
+                    this->print(ov);
+                });
+
+        thread.join();
+    }
+    
+    ~Foo()
+    { }
+
+};
+
+int main(void)
+{   
+    int x = 12;
+    Foo f{5};
+
+    f.print_thread(&x);         // prints 12
+    f.print_thread(nullptr);    // prints 0, should print 5 :(
+    f.print_thread(&f.x);       // prints 5
+};
