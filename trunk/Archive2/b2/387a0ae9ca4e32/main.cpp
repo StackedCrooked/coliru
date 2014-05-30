e5ff7acd7cb28ec0de14c210206c2b840b8295bd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-30 05:36:35 +0000

    Update archive b2

---
+++ trunk/Archive2/b2/387a0ae9ca4e32/main.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+using namespace std;
+
+template<class T>
+class Stack : private vector<T>
+{
+    vector<T> S;
+
+public:
+    Stack<T> push(T str);
+    Stack<T> operator += (const Stack<T> &str);
+};
+
+
+template<class T>
+Stack<T> Stack<T>::push(T str)
+{
+    S.push_back(str);
+
+}
+
+template<class T>
+Stack<T> Stack<T>::operator += (const Stack<T> &str)
+{
+    return this->push(str);
+
+}
+
+int main() {
+    Stack<int> s;
+    s += 5;
+}
