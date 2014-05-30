f48f5230eb60c466b7d28c1588d676beacb7e224
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-30 05:18:15 +0000

    Update archive d5

---
+++ trunk/Archive2/d5/d17c72f3321b27/main.cpp
@@ -0,0 +1,12 @@
+class Base { 
+protected:
+    Base(int); 
+}; 
+struct Derived : Base {
+    Derived() : Base(42) { 
+        static_assert(!noexcept(Base(42)), "Err"); 
+    } 
+};
+int main() {
+    Derived d;
+}
