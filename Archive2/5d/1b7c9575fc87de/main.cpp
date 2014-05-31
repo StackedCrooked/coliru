288ddb017e88d3f22a5fe50c96713e159da76cb9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-05-31 05:25:32 +0000

    Update archive 5d

---
+++ Archive2/5d/1b7c9575fc87de/main.cpp
@@ -0,0 +1,110 @@
+#include <functional>
+
+namespace std {
+
+    template <> struct plus < void > {
+		template <class T, class U> auto operator()( T&& t, U&& u ) const
+			-> decltype( std::forward<T>( t ) +std::forward<U>( u ) ) { 
+			return std::forward<T>( t ) +std::forward<U>( u );
+		 }
+	};
+
+	template <> struct minus < void > {
+		template <class T, class U> auto operator()( T&& t, U&& u ) const
+			-> decltype( std::forward<T>( t ) -std::forward<U>( u ) ) { 
+			return std::forward<T>( t ) -std::forward<U>( u );
+		 }
+	};
+
+	template <> struct multiplies < void > {
+		template <class T, class U> auto operator()( T&& t, U&& u ) const
+			-> decltype( std::forward<T>( t ) * std::forward<U>( u ) ) { 
+			return std::forward<T>( t ) * std::forward<U>( u );
+		 }
+	};
+
+	template <> struct divides < void > {
+		template <class T, class U> auto operator()( T&& t, U&& u ) const
+			-> decltype( std::forward<T>( t ) / std::forward<U>( u ) ) { 
+			return std::forward<T>( t ) / std::forward<U>( u );
+		 }
+	};
+
+	template <> struct modulus < void > {
+		template <class T, class U> auto operator()( T&& t, U&& u ) const
+			-> decltype( std::forward<T>( t ) % std::forward<U>( u ) ) { 
+			return std::forward<T>( t ) % std::forward<U>( u );
+		 }
+	};
+
+	template <> struct negate < void > {
+		template <class T> auto operator()( T&& t ) const
+			-> decltype( -std::forward<T>( t ) ) { 
+			return -std::forward<T>( t );
+		 }
+	};
+
+	template <> struct equal_to < void > {
+		template <class T, class U> auto operator()( T&& t, U&& u ) const
+			-> decltype( std::forward<T>( t ) == std::forward<U>( u ) ) { 
+			return std::forward<T>( t ) == std::forward<U>( u );
+		 }
+	};
+
+	template <> struct not_equal_to < void > {
+		template <class T, class U> auto operator()( T&& t, U&& u ) const
+			-> decltype( std::forward<T>( t ) != std::forward<U>( u ) ) { 
+			return std::forward<T>( t ) != std::forward<U>( u );
+		 }
+	};
+
+	template <> struct greater < void > {
+		template <class T, class U> auto operator()( T&& t, U&& u ) const
+			-> decltype( std::forward<T>( t ) > std::forward<U>( u ) ) { 
+			return std::forward<T>( t ) > std::forward<U>( u );
+		 }
+	};
+
+	template <> struct less < void > {
+		template <class T, class U> auto operator()( T&& t, U&& u ) const
+			-> decltype( std::forward<T>( t ) < std::forward<U>( u ) ) { 
+			return std::forward<T>( t ) < std::forward<U>( u );
+		 }
+	};
+
+	template <> struct greater_equal < void > {
+		template <class T, class U> auto operator()( T&& t, U&& u ) const
+			-> decltype( std::forward<T>( t ) >= std::forward<U>( u ) ) { 
+			return std::forward<T>( t ) >= std::forward<U>( u );
+		 }
+	};
+
+	template <> struct less_equal < void > {
+		template <class T, class U> auto operator()( T&& t, U&& u ) const
+			-> decltype( std::forward<T>( t ) <= std::forward<U>( u ) ) { 
+			return std::forward<T>( t ) <= std::forward<U>( u );
+		 }
+	};
+
+	template <> struct logical_and < void > {
+		template <class T, class U> auto operator()( T&& t, U&& u ) const
+			-> decltype( std::forward<T>( t ) && std::forward<U>( u ) ) { 
+			return std::forward<T>( t ) && std::forward<U>( u );
+		 }
+	};
+
+	template <> struct logical_or < void > {
+		template <class T, class U> auto operator()( T&& t, U&& u ) const
+			-> decltype( std::forward<T>( t ) || std::forward<U>( u ) ) { 
+			return std::forward<T>( t ) || std::forward<U>( u );
+		 }
+	};
+
+	template <> struct logical_not < void > {
+		template <class T> auto operator()( T&& t ) const
+			-> decltype( !std::forward<T>( t ) ) { 
+			return !std::forward<T>( t );
+		 }
+	};
+
+};
