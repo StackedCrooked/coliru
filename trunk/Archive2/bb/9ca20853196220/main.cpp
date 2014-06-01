a1977a4ee82497f0dd9189a866ee2a860cb13119
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-01 05:30:21 +0000

    Update archive bb

---
+++ trunk/Archive2/bb/9ca20853196220/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <boost/assign/std/vector.hpp>
+#include <boost/geometry.hpp>
+#include <boost/geometry/algorithms/area.hpp>
+#include <boost/geometry/algorithms/assign.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
+#include <boost/geometry/io/dsv/write.hpp>
+
+int main()
+{
+    using namespace boost::assign;
+    typedef boost::geometry::model::d2::point_xy<double> point_xy;
+
+    // Create points to represent a 5x5 closed polygon.
+    std::vector<point_xy> points;
+    points +=
+      point_xy(0,0),
+      point_xy(0,5),
+      point_xy(5,5),
+      point_xy(5,0),
+      point_xy(0,0)
+      ;
+
+    // Create a polygon object and assign the points to it.
+    boost::geometry::model::polygon<point_xy> polygon;  
+    boost::geometry::assign_points(polygon, points);
+
+    std::cout << "Polygon " << boost::geometry::dsv(polygon) << 
+      " has an area of " << boost::geometry::area(polygon) << std::endl;
+}
