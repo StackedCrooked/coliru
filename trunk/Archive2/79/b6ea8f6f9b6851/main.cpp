10c1d7e5e654ed422ba69f84cc5aeea1f2b291fe
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-01 23:36:17 +0000

    Update archive 79

---
+++ trunk/Archive2/79/b6ea8f6f9b6851/main.cpp
@@ -0,0 +1,37 @@
+#include <type_traits>
+#include <array>
+#include <iostream>
+
+typedef std::array<char, 1> sfinae_small_t;
+typedef std::array<char, 2> sfinae_big_t;
+
+
+namespace detail {
+	template<typename T>
+	struct has_get_null_function {
+		template <typename C> static sfinae_small_t test( decltype( &C::get_null ) );
+		template <typename C> static sfinae_big_t test( ... );
+
+		const static bool value = sizeof( test<T>( 0 ) ) == sizeof( sfinae_small_t );
+	};
+}
+
+template <typename T>
+struct has_get_null_function : public std::integral_constant<bool, detail::has_get_null_function<T>::value > {
+
+};
+
+
+struct f {
+    void get_null () {};
+};
+
+struct g {};
+
+
+int main () {
+   
+   std::cout << has_get_null_function<f>::value << std::endl;
+   std::cout << has_get_null_function<g>::value << std::endl;
+   
+}
