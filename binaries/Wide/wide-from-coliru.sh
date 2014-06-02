02de094d44775bad70780c7573a4bba1e91e123d
francis.rammeloo@gmail.com <francis.rammeloo@gmail.com@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-02 12:07:55 +0000

    Undo remove of LD_LIBRARY_PATH update.

--- binaries/Wide/wide-from-coliru.sh
+++ binaries/Wide/wide-from-coliru.sh
@@ -2,6 +2,8 @@
 
 # This is a helper script for invoking the Wide compiler from Coliru.
 
+# Tell the system to look inside /usr/local/lib for libboost_program_options.so.1.54.0		
+export LD_LIBRARY_PATH="/usr/local/lib:${LD_LIBRARY_PATH}"
 
 # Invoke the CLI with the user parameters.
 /usr/local/bin/Wide/CLI --stdlib="/usr/local/bin/Wide/WideLibrary" "$@"
