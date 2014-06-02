8c5c35cd5b0f0e0d44e3d156928e9d2d02d5ab04
wolfeinstein@gmail.com <wolfeinstein@gmail.com@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-02 12:10:07 +0000

    Latest update TC build 167 rev2

--- binaries/Wide/wide-from-coliru.sh
+++ binaries/Wide/wide-from-coliru.sh
@@ -2,7 +2,7 @@
 
 # This is a helper script for invoking the Wide compiler from Coliru.
 
-# Tell the system to look inside /usr/local/lib for libboost_program_options.so.1.54.0		
+# Tell the system to look inside /usr/local/lib for libboost_program_options.so.1.54.0
 export LD_LIBRARY_PATH="/usr/local/lib:${LD_LIBRARY_PATH}"
 
 # Invoke the CLI with the user parameters.
