d7629883e18082003ac95e43d2d04aad9c095db7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-02 17:35:28 +0000

    Update archive 94

---
+++ Archive2/94/2b053f33fb0790/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <type_traits>
+#include <initializer_list>
+#include <limits>
+#include <climits>
+
+using namespace std;
+
+struct Data{};
+struct Val1{};
+
+template <class T>
+struct Base{
+   template<class Val>
+   void do_it(){
+   }
+};
+
+template <class T = Base<Data> >
+struct Concrete: T{      
+  void test(){
+    typename Concrete:: template do_it<Val1>();         // I want not write "typename Concrete:: template " each time
+  }
+};
+
+
+int main()
+{
+    Concrete<> x;
+    x.test();
+}
