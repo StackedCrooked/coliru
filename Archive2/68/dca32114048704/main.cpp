43e5fad91f69e407a999c2907da4ee29123edace
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-03 05:38:31 +0000

    Update archive 68

---
+++ Archive2/68/dca32114048704/main.cpp
@@ -0,0 +1,16 @@
+#include <array>
+using namespace std; 
+
+class computer {
+public:
+    enum state {
+        on,
+        off
+    }
+    
+    void turn( enum x ) {}
+};
+
+int main() {
+    computer c;
+}
