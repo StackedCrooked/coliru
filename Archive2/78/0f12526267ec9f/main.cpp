c89e204e3bba1523c255c056c4dd0810128fb6ba
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-04 18:09:51 +0000

    Update archive 78

---
+++ Archive2/78/0f12526267ec9f/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+const int N = 1000;
+
+class MyClass{
+    // constructor here, contains 1 int and 5 floats
+};
+
+typedef array<array<MyClass,N>,N> ClassArray;
+
+int main(){
+    cout << "Beginning" << endl;
+    ClassArray A;
+    //operations
+}
