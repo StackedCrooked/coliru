a59426645852a8a280c83699bf4f76f823ce1361
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-06 18:10:38 +0000

    Update archive 79

---
+++ trunk/Archive2/79/47fa8393272cc5/main.cpp
@@ -0,0 +1,14 @@
+#include <functional>
+#include <type_traits>
+
+template <typename L> void Callback(L l){
+    using F = typename std::remove_pointer<decltype(+std::declval<L>())>::type;
+    std::function<F> f( l );
+    // store f and call later
+}
+
+int main(){
+    Callback([](int a, float b){
+        // do something
+    });
+}
