46ea9e1a08d81ffe154cbebe23a818ed5a16b81d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-06 00:11:49 +0000

    Update archive bb

---
+++ trunk/Archive2/bb/327a5e412e8c4b/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <limits>
+
+int main()
+{
+    std::cout << std::boolalpha;
+ 
+    std::cout << "char == unsigned char: " << std::is_same<char, unsigned char>::value << '\n';   // true
+    std::cout << "char == signed char: " << std::is_same<char, signed char>::value << '\n';   // false
+    std::cout << "char is signed: " << std::is_signed<char>::value << '\n'; // false
+    
+    std::cout << "char == unsigned char: " << std::is_same<int, unsigned int>::value << '\n';   // true
+    std::cout << "char == signed char: " << std::is_same<int, signed int>::value << '\n';   // false
+    std::cout << "char is signed: " << std::is_signed<int>::value << '\n'; // false   
+    
+    return 0;
+}
