573f7d208cc91b8d531a04208d4561a83a6045fa
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-06 06:23:08 +0000

    Update archive 95

---
+++ Archive2/95/f99c83bab5e5b5/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    double d = -4;
+    printf("d = %.64f\n",d);
+    
+    double d1 = -4.2;
+    printf("d1 = %.64f\n",d1);
+    
+    double d2 = d-0.2;
+    printf("d2 = %.64f\n",d2);
+}
