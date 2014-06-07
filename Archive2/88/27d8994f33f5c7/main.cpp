15cdb8c9e5748353daa8b6e397e2d403205182bc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-07 12:22:42 +0000

    Update archive 88

---
+++ Archive2/88/27d8994f33f5c7/main.cpp
@@ -0,0 +1,28 @@
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/range/iterator_range.hpp>
+#include <iostream>
+
+using boost::multi_index_container;
+using namespace boost::multi_index;
+
+struct employee {
+    int         id;
+    std::string name;
+    int         age;
+};
+
+typedef multi_index_container<
+  employee,
+  indexed_by<
+    ordered_unique<tag<struct id>      , BOOST_MULTI_INDEX_MEMBER(employee, int        , id)>   ,
+    ordered_non_unique<tag<struct name>, BOOST_MULTI_INDEX_MEMBER(employee, std::string, name)> ,
+    ordered_non_unique<tag<struct age> , BOOST_MULTI_INDEX_MEMBER(employee, int        , age)> >
+> employee_set;
+
+int main()
+{
+    employee_set es;
+    std::cout << typeid(get<age>(es)).name();
+}
