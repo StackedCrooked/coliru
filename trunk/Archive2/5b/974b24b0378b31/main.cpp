b9a47a7ce9dd135e56ba546157095ba6f36ed1f2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-08 08:12:54 +0000

    Update archive 5b

---
+++ trunk/Archive2/5b/974b24b0378b31/main.cpp
@@ -0,0 +1,49 @@
+#include <boost/icl/gregorian.hpp>
+#include <boost/icl/interval_set.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time/posix_time/posix_time_io.hpp>
+#include <iostream>
+#include <iterator>
+
+namespace icl = boost::icl;
+
+struct record {
+    std::string name;
+
+    using ptime = boost::posix_time::ptime;
+    ptime added, removed;
+
+    icl::interval<ptime>::interval_type validity() const { 
+        icl::interval<ptime> x;
+        return x.right_open(added, removed);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os,const record& r) {
+        return os << "{" << r.name << ", " << r.added << ", " << r.removed << "}";
+    }
+};
+
+int main()
+{
+    using namespace boost::gregorian;
+    using namespace boost::posix_time;
+
+    ptime const start(day_clock::local_day(), { });
+
+    std::vector<record> L {
+        { "long"     , start + hours(77) , start + days(7) }   ,
+        { "medium"   , start + hours(200), start + hours(236) },
+        { "short"    , start + hours(220), start + hours(226) },
+        { "ephemeral", start + hours(100), start + hours(101) },
+    };
+
+
+    for (auto Slot = icl::interval<ptime>::right_open(start, start + hours(6));
+         Slot.lower() <= (start + days(10)); 
+         Slot = icl::interval<ptime>::right_open(Slot.upper(), Slot.upper() + hours(6)))
+    {
+        std::cout << "\nSlot:" << Slot << "\t";
+        std::ostream_iterator<record> out(std::cout, ";");
+        std::copy_if(L.begin(), L.end(), out, [Slot](record const& r) { return icl::intersects(Slot, r.validity()); });
+    }
+}
