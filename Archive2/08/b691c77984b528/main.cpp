2edc59b14c9b8606a5548c4f309a7f85b8f03b74
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-08 14:09:56 +0000

    Update archive 08

---
+++ Archive2/08/b691c77984b528/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename U, typename T>
+vector<T> append_vec(std::initializer_list<U> lst1, const vector<T> &lst2) {
+    vector<T> inner (lst1.begin(), lst1.end());
+    inner.insert(inner.end(), lst2.begin(), lst2.end());
+    return inner;
+}
+
+int main() {
+        
+    vector<string> suffix {{"1", "2", "3"}};
+    vector<vector<string>> v;
+    v = {{
+        append_vec({"a", "b", "c"}, suffix),
+        {"aa", "bb"},
+        append_vec({"xyz", "yzx", "zxy"}, suffix),
+        {}
+    }};
+    
+    for (const auto &c : v){
+        cout << "*    ";
+        for(const auto &d : c){
+            cout << d << ' ';
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
