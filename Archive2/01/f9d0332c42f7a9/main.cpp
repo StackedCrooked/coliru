d3d9e6e9c3adafd7b3eafc21a8d70d30eb2dfd67
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-08 14:12:24 +0000

    Update archive 01

---
+++ Archive2/01/f9d0332c42f7a9/main.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+ 
+int main(void)
+{
+    printf("%g\n", atof("  -0.0000000123junk"));
+    printf("%g\n", atof("0.012"));
+    printf("%g\n", atof("15e16"));
+    printf("%g\n", atof("-0x1afp-2"));
+    printf("%g\n", atof("inF"));
+    printf("%g\n", atof("Nan"));
+ 
+    return 0;
+}
