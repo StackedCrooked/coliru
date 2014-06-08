0c2351c8a9d09cc459733e26fcf71820c9d2a6cb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-08 14:08:05 +0000

    Update archive 3e

---
+++ Archive2/3e/f95f047f5669d5/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// every object of type sse_t will be aligned to 16-byte boundary
+struct alignas(16) sse_t
+{
+  float sse_data[4];
+};
+ 
+// the array "cacheline" will be aligned to 128-byte boundary
+alignas(128) char cacheline[128];
+
+int main()
+{
+    printf("%i", alignof(sse_t));
+    return 0;
+}
