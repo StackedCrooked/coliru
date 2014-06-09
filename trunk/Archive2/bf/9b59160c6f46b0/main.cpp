502f32485038fbfe72423a8d8efe27b6bab97fac
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-09 02:14:41 +0000

    Update archive bf

---
+++ trunk/Archive2/bf/9b59160c6f46b0/main.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+typedef vector<string> Strings;
+
+void print(string const& s) {
+    printf(s.c_str());
+    printf("\n");
+}
+
+void print(Strings const& ss, int i) {
+    print("Test " + to_string(i));
+    print("Number of strings: " + to_string(ss.size()));
+    for (auto& s: ss) {
+        auto t = "length = " + to_string(s.size()) + ": " + s;
+        print(t);
+    }
+    print("\n");
+}
+
+void test() {
+    Strings a{"hello"};
+   // print(a, 1); // uncomment these for the exception
+    
+    Strings b{{"hello", "there"}};
+   // print(b, 2);   // uncomment these for the exception
+
+    Strings c{"hello", "there", "kids"};
+ //  print(c, 3);   // uncomment these for the exception
+}
+
+int main() {
+    Strings b{{"hello", "world"}};
+}
