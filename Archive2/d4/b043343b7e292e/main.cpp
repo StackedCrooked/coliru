6f90c5b3807b8f86dff354312cae43bb15f80c61
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-09 14:13:26 +0000

    Update archive d4

---
+++ Archive2/d4/b043343b7e292e/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long double i, j, ans;
+    scanf("%Lf %Lf", &i, &j);
+    ans=i*j;
+    printf("%Lf %Lf", ans, i*j);
+    return 0;
+}
