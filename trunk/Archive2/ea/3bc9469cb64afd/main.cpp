ec6c41238832f8e354edb5363bc38b8ec8df03e7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-10 14:08:38 +0000

    Update archive ea

---
+++ trunk/Archive2/ea/3bc9469cb64afd/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/multiprecision/rational_adapter.hpp>
+
+int main(){
+    using boost::multiprecision::cpp_int;
+    using boost::multiprecision::cpp_rational;
+
+    cpp_rational r(1);
+    r /= (cpp_int(1)<<127);
+
+    std::cout << "0!:\t\t" << r << std::endl;
+    std::cout << "1!:\t\t" << r << std::endl;
+
+    for(cpp_rational i=2; i<257; ++i){
+        r *= i;
+        std::cout << i << "!:\t\t" << r << std::endl;
+    }
+}
