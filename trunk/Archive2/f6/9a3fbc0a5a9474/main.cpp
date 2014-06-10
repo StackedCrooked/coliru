8fd11167765aaa50cb829c116ee2d08f5d9de0a5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-10 20:28:28 +0000

    Update archive f6

---
+++ trunk/Archive2/f6/9a3fbc0a5a9474/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+struct Foo {
+  void boo(const char*) {
+    std::cout << "call: const char*" << std::endl;
+  }
+  
+  template <class T>
+  void boo(T) {
+    std::cout << "call: template T" << std::endl;
+  }
+};
+
+int main() {
+  Foo foo;
+  
+  std::cout << "argument: char*" << std::endl;
+  char* a = nullptr;
+  foo.boo(a);
+  
+  std::cout << "argument: const char*" << std::endl;
+  const char* b = "abc";
+  foo.boo(b);
+}
