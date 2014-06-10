d327e672e51c3d8a7c1580c6e9100d9def690490
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-10 08:06:49 +0000

    Update archive 5d

---
+++ trunk/Archive2/5d/e5fb582be997bc/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+struct Foo {
+  void boo(const char*) {
+    std::cout << "call: const char*" << std::endl;
+  }
+};
+
+int main() {
+  Foo foo;
+  
+  std::cout << "argument: char*" << std::endl;
+  char* a = nullptr;
+  foo.boo(a);
+  
+  std::cout << "argument: const char*" << std::endl;
+  const char* b = "abc";
+  foo.boo(b);
+}
