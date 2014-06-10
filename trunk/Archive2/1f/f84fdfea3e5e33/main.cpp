b0b873a8fefd9b63a1b1d24da6e8c41621cc6335
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-10 02:07:59 +0000

    Update archive 1f

---
+++ trunk/Archive2/1f/f84fdfea3e5e33/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int i;    
+   cout << "Hello World" << endl; 
+   
+   for(i=0;i<10;i++) {
+   cout << i <<endl;
+   }
+   return 0;
+}
