032a9d78de93cb6fdbfa0a3dd2f5239456c6af85
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-11 08:21:26 +0000

    Update archive 1f

---
+++ trunk/Archive2/1f/c3e863af3a281c/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <random>
+int roundnew(double d)
+{
+  return floor(d + 0.5);
+}
+int main()
+{
+    std::default_random_engine generator;
+    std::normal_distribution<double> distribution(10,1);
+	
+    for (int n = 0; n < 12; ++n) {
+       printf("%d\n",roundnew(distribution(generator)));
+    }
+
+return 0;
+}
