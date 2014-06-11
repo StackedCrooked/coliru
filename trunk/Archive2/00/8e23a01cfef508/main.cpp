5bd941145e939f0861b342215b97bebe22673974
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-11 08:28:46 +0000

    Update archive 00

---
+++ trunk/Archive2/00/8e23a01cfef508/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+struct Foo {
+  template <class T>
+  void boo(T) {
+    std::cout << "call: template" << std::endl;
+  }
+
+  template <class T>
+  void boo(T*) {
+    using WithoutCV = typename std::remove_cv<T>::type;
+    static_assert(std::is_same<WithoutCV, char>::value, "");
+    std::cout << "call: char template" << std::endl;
+  }
+};
+
+int main() {
+  Foo foo;
+  
+  std::cout << "argument: char*" << std::endl;
+  char* a = nullptr;
+  foo.boo(a);
+  
+  std::cout << "argument: const char*" << std::endl;
+  const char* b = "abc";
+  foo.boo(b);
+}
