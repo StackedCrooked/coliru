06130522be616c73fa4e597c975b98a477307c0e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-11 08:28:46 +0000

    Update archive 00

---
+++ Archive2/00/e0059db838a94a/main.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+typedef vector<string> Strings;
+
+void print(string const& s) {
+    printf(s.c_str());
+    printf("\n");
+}
+
+void print(Strings const& ss, int i) {
+    print("Test " + to_string(i));
+    print("Number of strings: " + to_string(ss.size()));
+    for (auto& s: ss) {
+        auto t = "length = " + to_string(s.size()) + ": " + s;
+        print(t);
+    }
+    print("\n");
+}
+
+void test() {
+    Strings a{"hello"};
+    print(a, 1);
+
+    Strings b{{"hello", "there"}};
+    print(b, 2);
+
+    Strings c{"hello", "there", "kids"};
+    print(c, 3);
+}
+
+int main() {
+    Strings b{{"hello", "there"}};
+    //print(b, 1);
+}
