b20420557e17907e2785c84e0e831f120c075891
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-11 08:34:56 +0000

    Update archive 96

---
+++ Archive2/96/677bd195b4dbfb/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <tuple>
+
+
+auto list = std::make_tuple(1, 2, 3, 4);
+
+/// Work like a charm
+template <class T>
+auto test1(T &&brush) -> decltype(std::get<0>( std::forward<T>(brush) )) {
+    return std::get<0>( std::forward<T>(brush) );
+}
+
+
+/// And now - C++14 feature
+/// fail to compile - return value, instead of reference
+template <class T>
+auto test2(T &&brush) -> decltype(auto) {
+    return std::get<0>( std::forward<T>(brush) );
+}
+
+int main()
+{
+    auto &t1 = test1(list);
+    auto &t2 = test2(list);        
+}
+
