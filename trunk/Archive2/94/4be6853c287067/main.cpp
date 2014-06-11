749bad15c064a8d38dcd7acdeb7122d13a18729a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-11 08:25:26 +0000

    Update archive 94

---
+++ trunk/Archive2/94/4be6853c287067/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+ 
+int main() 
+{
+    std::vector<int> v = {0, 1, 2, 3, 4, 5};
+ 
+    for (int &i : v) // access by reference (const allowed)
+        std::cout << i << ' ';
+ 
+    std::cout << '\n';
+ 
+    for (auto i : v) // compiler uses type inference to determine the right type
+        std::cout << i << ' ';
+ 
+    std::cout << '\n';
+ 
+    for (int i : v) // access by value as well
+        std::cout << i << ' ';
+ 
+    std::cout << '\n';
+}
