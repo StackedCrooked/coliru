0fd4fcb477cda02ba337a7d08a45035a79e1ed2f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-11 20:14:23 +0000

    Update archive 8d

---
+++ trunk/Archive2/8d/e82d7d15ffcccb/main.cpp
@@ -0,0 +1,20 @@
+struct B
+{
+    B() = default;
+    B(B&&) = default;
+    B(const B&) = delete;
+    B& operator=(const B&) = default;
+    
+    // Successfully takes the place of the copy constructor,
+    // but please don't do this!
+    operator B() { return B(); }
+};
+
+int main()
+{
+   B b;
+   B c;
+   c = b;
+   
+   // Can't do `B c(b)` though.
+}
