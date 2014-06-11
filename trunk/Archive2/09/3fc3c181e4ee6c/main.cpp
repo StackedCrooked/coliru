ce2729d08921f7d08bcc7e6dd8f57669c11766f0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-11 14:33:36 +0000

    Update archive 09

---
+++ trunk/Archive2/09/3fc3c181e4ee6c/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+struct B{
+
+    B(){ std::cout << "    B constructor " << std::endl;}
+    B(const B&){ std::cout << "    B copy constructor " << std::endl;}
+    B(B&&){ std::cout << "    B move constructor " << std::endl;}
+    
+};
+
+struct A{
+    A(){ std::cout << "    A constructor " << std::endl;}
+    /*explicit*/  A(const A&){ std::cout << "    A copy constructor " << std::endl;}
+    /*explicit*/  A(A&&)  { std::cout << "    A move constructor " << std::endl;}
+
+    /*explicit*/  A(const B&){ std::cout << "    A conversion constructor " << std::endl;}
+    /*explicit*/  A(B&&){ std::cout <<  "    A move conversion constructor " << std::endl;}
+
+
+};
+
+int main()
+{
+   std::cout << "A a{B()}; " << std::endl;
+
+   A a1{B()};
+   
+   std::cout << "A a = B(); " << std::endl;
+
+   A a2 = B();
+
+   std::cout << "A a{A()}; " << std::endl;
+
+   A a3{A()};
+   
+   std::cout << "A a = A(); " << std::endl;
+
+   A a4 = A();
+
+
+}
