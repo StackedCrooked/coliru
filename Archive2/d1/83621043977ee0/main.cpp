060ea5ad49511b8f9c79517a692a6d0f22afb8ae
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-11 22:00:47 +0000

    Update archive d1

---
+++ Archive2/d1/83621043977ee0/main.cpp
@@ -0,0 +1,15 @@
+.hbox {
+    display: box; display: -webkit-box; display: -moz-box; position: relative; padding: 0; margin: 0; 
+    box-orient: horizontal; -webkit-box-orient: horizontal; -moz-box-orient: horizontal;
+    box-align: stretch; -webkit-box-align: stretch; -moz-box-align: stretch;
+}
+
+.vbox {
+    display: box; display: -webkit-box; display: -moz-box;
+    position: relative; padding: 0; margin: 0;
+    box-orient: vertical; -webkit-box-orient: vertical; -moz-box-orient: vertical;
+    box-align: stretch; -webkit-box-align: stretch; -moz-box-align: stretch;
+}
+
+.f0 { display: box; display: -webkit-box; display: -moz-box; position: relative; box-flex: 0.0; -webkit-box-flex: 0.0; -moz-box-flex: 0.0; }
+.f1 { display: box; display: -webkit-box; display: -moz-box; position: relative; box-flex: 1.0; -webkit-box-flex: 1.0; -moz-box-flex: 1.0; }
