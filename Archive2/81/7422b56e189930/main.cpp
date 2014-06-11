adaf95a2d486bcffb8f9ea81ada36fa5221e8025
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-11 22:18:36 +0000

    Update archive 81

---
+++ Archive2/81/7422b56e189930/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <iterator>
+
+int main()
+{
+    std::multimap<std::string, std::string> m{{"Hello", "World"}, {"Bye", "World"}, {"Foo", "Bar"}};
+    std::vector<decltype(m)::value_type> vec;
+    
+    std::copy_if(m.begin(), m.end(), std::back_inserter(vec),
+        [](decltype(m)::value_type const& kv) {
+            return std::any_of(kv.first.begin(), kv.first.end(), 
+                               [](decltype(*kv.first.begin()) c) { return c == 'e'; });
+        });
+    
+    for(auto const& v : vec) {
+        std::cout << v.first << ": " << v.second << std::endl;
+    }
+}
