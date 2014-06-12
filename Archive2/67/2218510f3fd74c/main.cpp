506a6b795bca499e08254385ebed5dc6712301ad
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-12 23:28:15 +0000

    Update archive 67

---
+++ Archive2/67/2218510f3fd74c/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
+
+struct CVector3f { float vec[3]; };
+
+void Parse(const char* vectorString, CVector3f& outputVector)
+{
+    std::vector<std::string> parts;
+    boost::split(parts, vectorString, boost::is_any_of(","));
+    parts.resize(3, "0");
+    
+    std::transform(
+        parts.begin(),
+        parts.end(),
+        outputVector.vec,
+        &boost::lexical_cast<float, std::string>);
+}
+
+int main()
+{
+    CVector3f vec;
+    Parse("1.11,2.222", vec);
+    
+    std::cout
+        << "X: " << vec.vec[0] << std::endl
+        << "Y: " << vec.vec[1] << std::endl
+        << "Z: " << vec.vec[2] << std::endl;
+}
