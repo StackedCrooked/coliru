f9ad2a9d67aaf840f0b9de2f09008b28872e581b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-12 04:02:03 +0000

    Update archive a0

---
+++ Archive2/a0/7f38c0e44931e4/main.cpp
@@ -0,0 +1,28 @@
+#include <array>
+#include <iostream>
+
+#include <sched.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+	auto stack = new std::array<char, 1024>;
+
+	int tid = clone([] (void *data) {
+        sleep(1);
+        return 123;
+    }, stack->end(), SIGCHLD, NULL);
+    
+	int status;
+	waitpid(tid, &status, 0);
+	if(WIFEXITED(status)) {
+		std::cout << "Returned " << WEXITSTATUS(status) << ".\n";
+	} else if(WIFSIGNALED(status)) {
+    	std::cout << "Killed by signal " << WTERMSIG(status) << ".\n";
+	}
+	return 0;
+}
