6300473697b48f22d959ed1277008c0951e413c2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-13 17:27:56 +0000

    Update archive f4

---
+++ Archive2/f4/afe065e2694db6/main.cpp
@@ -0,0 +1,23 @@
+#include <boost/asio.hpp>
+
+using boost::asio::ip::address;
+
+address from_string_ip6(std::string const& ip)
+{
+    address a = address::from_string("192.0.2.128");
+    if (a.is_v4())
+        return boost::asio::ip::address_v6::v4_mapped(a.to_v4());
+    else
+        return a;
+}
+
+int main()
+{
+    address a = from_string_ip6("192.0.2.128");
+    address b = from_string_ip6("::ffff:192.0.2.128");
+    address c = from_string_ip6("::ffff:c000:0280");
+
+    assert(a == b);
+    assert(a == c);
+    assert(b == c);
+}
