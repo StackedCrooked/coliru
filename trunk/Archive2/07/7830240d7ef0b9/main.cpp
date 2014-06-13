f1d79d2fb6b55631b299e23e3df380fafc29fc3d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-13 17:37:49 +0000

    Update archive 07

---
+++ trunk/Archive2/07/7830240d7ef0b9/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+double fcn_mod(const double &phit){
+    return 1.2+phit;
+}
+
+class Object{
+    public:
+        Object() : fcn_ptr(&Object::fcn_default)
+        {}
+        double (Object::*fcn_ptr)(const double &) = NULL;
+
+    private:
+        double fcn_default(const double &phit){
+            return 3.2+phit;
+        }
+};
+
+
+int main(){
+
+    Object test1,test2;
+
+    cout << (test1.*(test1.fcn_ptr))(2) << endl; // can not compile here
+
+//    test2.fcn_ptr = &fcn_mod;
+//    cout << (test2.*fcn_ptr)(2) << endl; // and here
+//
+//    cout << "test" << endl;
+}
