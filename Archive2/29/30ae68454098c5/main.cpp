a26635923e0f13b4bfb28ad2dc703647c04c792e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-14 09:01:39 +0000

    Update archive 29

---
+++ Archive2/29/30ae68454098c5/main.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+// Excercise from the book "Advanced C++ Metaprogramming" by Gennaro
+
+
+// GIVEN:
+
+template<typename T>
+void f(T) { puts("T"); }
+struct Base {};
+
+
+// TODO: write overload that works for Base or derived
+
+
+// my initial attempt (which fails for Derived)
+void f(Base)
+{
+    puts("Base or derived");
+}
+
+
+int main()
+{
+    struct Derived : Base {};
+
+    Base base;
+    Derived derived;
+
+    // Test
+    f(1);       // OK, calls template f
+    f(base);    // OK, calls f(Base)
+    f(derived); // WRONG: calls template f
+}
