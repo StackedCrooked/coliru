cc333fd43ec1cc99e9582331f16c793bf1bffdfe
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-14 05:18:35 +0000

    Update archive e4

---
+++ Archive2/e4/2629b43621efa4/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/optional.hpp>
+#include <boost/thread.hpp>
+
+class A
+{
+    using ios = boost::asio::io_service;
+
+    ios                        m_io;
+    boost::optional<ios::work> m_active;
+  public:
+    A() : m_io(), m_active(ios::work(m_io)) {}
+
+    ~A() {
+        m_active.reset();
+        m_io.run(); // to completion
+    }
+
+    void update()
+    {
+        m_io.poll();
+        //do other stuff
+    }
+
+    void postSomething()
+    {
+        while(m_active)
+        {
+            m_io.post([]{std::cout<<"something...\n";});
+            sleep(1000);
+        }
+    }
+};
+
+int main()
+{
+    boost::thread thr;
+    {
+        A a;
+
+        thr = boost::thread(boost::bind(&A::postSomething, &a));
+
+        for (int i = 0; i < 100; ++i)
+        {
+            boost::this_thread::sleep_for(boost::chrono::milliseconds(5));
+            a.update();
+        }
+    }
+
+    thr.join(); // thread will not join unless A is destructed (m_active is cleared)
+}
