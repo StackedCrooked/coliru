c89886ee203cecbc8b51d21da2ae75ad9b797855
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-16 03:14:15 +0000

    Update archive 15

---
+++ Archive2/15/e8ba5cc019adfe/cmd.sh
@@ -0,0 +1,3 @@
+set -x ;
+clang++ -std=c++11 -O0 -Wall -pedantic -Wextra -pthread -Wno-unused-variable -Wno-unused-parameter main.cpp && ./a.out
+g++-4.8 -std=c++11 -O0 -Wall -pedantic -Wextra -pthread -Wno-unused-variable -Wno-unused-parameter main.cpp && ./a.out
