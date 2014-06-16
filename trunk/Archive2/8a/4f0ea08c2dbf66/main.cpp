0491a8843f1914216323572381446344995d93a6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-16 21:28:30 +0000

    Update archive 8a

---
+++ trunk/Archive2/8a/4f0ea08c2dbf66/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <utility>
+
+class Object
+{
+public:
+    void Test()
+    {
+        std::cout << "Test" << std::endl;
+    }
+};
+
+class EmptyObject
+{
+};
+
+class Base
+{
+public:
+    virtual std::string Foo()
+    {
+        return "Base";
+    }
+};
+
+template <typename T>
+class Intermediate1 : public Base
+{
+    // common operations with m_object
+
+protected:
+    T m_object;
+};
+
+template <typename T, typename = bool>
+class Intermediate2 : public Intermediate1<T> {};
+
+template <typename T>
+class Intermediate2<T, decltype(std::declval<T>().Test(), void(), true)>
+    : public Intermediate1<T>
+{
+public:
+    virtual std::string Foo() override
+    {
+        this->m_object.Test();
+        return "Derived";
+    }
+};
+
+template <typename T>
+class Derived : public Intermediate2<T> { /* ... */ };
+
+int main()
+{
+    Derived<Object> testObject;
+    std::cout << testObject.Foo() << std::endl;
+
+    Derived<EmptyObject> testEmpty;
+    std::cout << testEmpty.Foo() << std::endl;
+}
