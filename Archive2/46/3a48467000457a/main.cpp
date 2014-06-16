3f33e71af7beb0ddfaf4583507b30cd624f66475
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-16 03:22:01 +0000

    Update archive 46

---
+++ Archive2/46/3a48467000457a/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+struct B
+{
+    virtual ~B() { cout << "Destructor B" << endl; }
+};
+
+struct D1 : B
+{
+    ~D1() { cout << "Destructor D1" << endl; }
+};
+
+struct D2 : D1
+{
+    ~D2() { cout << "Destructor D2" << endl; }
+};
+
+int main(void)
+{
+    B* b = new D2();
+    D1* d1 = new D2();
+    
+    delete b;
+    
+    cout << "PAUSE" << endl;
+    
+    delete d1;
+    // return 0;
+}
