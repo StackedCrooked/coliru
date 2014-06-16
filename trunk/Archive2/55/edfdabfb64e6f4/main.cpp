66b4fc8e28f34c600da3830888aa48fd5e4e6a34
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-16 21:37:02 +0000

    Update archive 55

---
+++ trunk/Archive2/55/edfdabfb64e6f4/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <type_traits>
+
+class Base
+{
+public:
+    virtual std::string Foo()
+    {
+        return "Base";
+    }
+};
+
+template <typename T>
+class Derived : public Base
+{
+public:
+    virtual std::string Foo() override
+    {
+        return do_Foo(std::is_same<T, float>{});
+    }
+    
+private:
+    std::string do_Foo(std::false_type)
+    {
+        return "Derived";
+    }
+    std::string do_Foo(std::true_type)
+    {
+        return Base::Foo();
+    }
+};
+
+int main()
+{
+    Derived<int> testInt;
+    std::cout << testInt.Foo() << std::endl;
+
+    Derived<float> testFloat;
+    std::cout << testFloat.Foo() << std::endl;//I would like this to print 'Base'
+}
