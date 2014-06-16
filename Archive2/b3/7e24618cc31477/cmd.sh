d1b630359ab01054503faac04fc5a32224096abd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-16 15:17:38 +0000

    Update archive b3

---
+++ Archive2/b3/7e24618cc31477/cmd.sh
@@ -0,0 +1,2 @@
+clang++ -std=c++11 -stdlib=libc++ -O2 -Wall -Wextra -pedantic-errors main.cpp -lsupc++ && ./a.out
+g++-4.8 -std=c++11 -ftemplate-depth=2048 -O2 -Wall -Wextra -pedantic-errors main.cpp && ./a.out
