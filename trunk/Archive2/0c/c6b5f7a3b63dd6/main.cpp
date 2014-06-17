c18718f8b517ab695912324377af544cce724fea
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-17 21:26:55 +0000

    Update archive 0c

---
+++ trunk/Archive2/0c/c6b5f7a3b63dd6/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct O{
+    O(int i) : i(i){};
+    int i;
+};
+
+
+class A{
+    mutable std::vector<O> data;
+public:
+    O& get(int id) const{
+        return data[id];
+    }
+    
+    void push(O &&value){
+        data.emplace_back(value);
+    }
+};
+
+int main()
+{
+    A a;
+    a.push(O(0));
+    a.get(0);
+}
