106f0578b1052a8426a6912a02463e29723dd429
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-18 15:22:25 +0000

    Update archive 42

---
+++ Archive2/42/ebd7a0785d580b/main.cpp
@@ -0,0 +1,35 @@
+#include <type_traits>
+#include <utility>
+#include <string>
+#include <vector>
+#include <iostream>
+
+template<typename F, typename... Args>
+typename std::result_of<F(Args...)>::type
+    HandledCall(F&& func, Args&&... args)
+{
+    try {
+        return std::forward<F>(func)(std::forward<Args>(args)...);
+    } catch(...) {
+        return {};
+    }
+}
+
+std::string foo(std::vector<int>) { return ""; }
+void bar() {}
+
+int fun(int input)
+{
+    return input;
+}
+
+int main()
+{
+    static_assert(std::is_same<decltype(HandledCall(foo, std::vector<int>{})), std::string>::value, "Oops1");
+    static_assert(std::is_same<decltype(HandledCall(bar)), void>::value, "Oops2");
+    static_assert(std::is_same<decltype(fun(1)), int>::value, "Oops3");
+    
+    std::cout << HandledCall(fun,1) << std::endl; // this should return 1
+    std::cout << HandledCall(fun,-1) << std::endl; // this should return 0
+    HandledCall(bar);
+}
