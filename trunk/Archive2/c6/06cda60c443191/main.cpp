400eb0c0e1cba262dcf4ddfb51993f9ca9a90f6e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-19 19:28:54 +0000

    Update archive c6

---
+++ trunk/Archive2/c6/06cda60c443191/main.cpp
@@ -0,0 +1,22 @@
+#include <boost/asio.hpp>
+#include <iostream>
+
+int main() {
+    boost::asio::io_service ios;
+
+    std::cout << "using Boost " << BOOST_LIB_VERSION << std::endl;
+    
+    ios.post([](){});   // add something
+    ios.stop();         // Subsequent calls to run(), run_one(), poll() or poll_one() will return immediately until reset() is called.
+
+    boost::system::error_code ec;
+    size_t re = ios.poll_one(ec);
+    std::cout << "poll after stop: handled " << re << ", stopped " << std::boolalpha << ios.stopped() << ", error " << ec << std::endl;
+    
+    ios.reset();
+    ios.post([](){ std::cout << "second post" << std::endl; });   // add again
+    re = ios.poll_one(ec);
+    std::cout << "poll after reset: handled " << re << ", stopped " << std::boolalpha << ios.stopped() << ", error " << ec << std::endl;
+
+    return 0;
+}
