68a8b5258296702c2183bbbd01957c450e2166e5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-19 03:18:30 +0000

    Update archive 5d

---
+++ trunk/Archive2/5d/cdd7d46d61ff31/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <boost/rational.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+
+template < typename INTEGER_TYPE = int > using rational = boost::rational<INTEGER_TYPE> ;
+
+int main()
+{
+    rational<> a = 7 ; // 7/1
+    rational<> b( 44, 14 ) ; // 22/7
+    rational<> c = a*a + b*b - 2*a*b ;
+    std::cout << a << '\n' << b << '\n' << c << '\n' ;
+    rational<> e = (b + a) % 938;
+    std::cout << e << endl;
+    using boost::multiprecision::cpp_int ;
+    rational<cpp_int> d( cpp_int("12345678987654321"), cpp_int("123456789") ) ;
+    std::cout << d << '\n' << d + 25000000 << '\n' ;
+    
+    std::cout << d / boost::rational_cast<cpp_int>(c) << '\n' ;
+}
