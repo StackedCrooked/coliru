a0d56ababef98d4ec08b3e2d4fbe5b714eadf114
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-19 03:26:39 +0000

    Update archive 91

---
+++ trunk/Archive2/91/2597d9501bd3fb/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int fun(int input)
+{
+    if (input < 0) throw "something to throw";
+    return input;
+}
+
+template <typename F, typename...Args>
+static auto HandledCall(F&& function, Args...args)
+-> decltype(std::forward<F>(function)(std::forward<Args>(args)...))
+{
+    try
+    {
+        return std::forward<F>(function)(std::forward<Args>(args)...);
+    }
+    catch(...)
+    {
+        return {};
+    }
+}
+
+int main() {
+    std::cout << HandledCall(fun,1) << std::endl; // this should return 1
+    std::cout << HandledCall(fun,-1) << std::endl; // this should return 0      
+}
