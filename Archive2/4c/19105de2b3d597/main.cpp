54ef78b921c31e221fc416d7e67f7c4137c2a4cb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-20 18:20:32 +0000

    Update archive 4c

---
+++ Archive2/4c/19105de2b3d597/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+void test(int){}
+
+template<typename T,typename...Args>
+void test(int& sum,T v,Args... args)
+{
+    sum+=v;
+    test(sum,args...);
+}
+
+auto sum= test<int, int, int>;
+int main()
+{
+    int val=0;
+    sum(val,4,7);
+    std::cout<<val;
+}
