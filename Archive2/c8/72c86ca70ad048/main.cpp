e917e3bf512b0d04a66282787dbdea6d30ff671c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-20 18:27:51 +0000

    Update archive c8

---
+++ Archive2/c8/72c86ca70ad048/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <unordered_set>
+
+auto main() -> int
+{
+  std::set<int > a{ 1, 2, 3, 4, 5};
+  std::vector<std::unordered_set<int>> b (1, std::unordered_set<int>(a.begin(), a.end()));
+  
+  for(auto i : b) {
+    for(auto j : i) std::cout << j << " ";
+    std::cout << std::endl;
+  }
+  return 0;
+}
