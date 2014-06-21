0febf8b66de5426ba7f903369787837087edd530
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-21 13:33:04 +0000

    Update archive a7

---
+++ trunk/Archive2/a7/3c6d75f6a900a6/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cassert>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    os << '[';
+    for (auto& el : vec)
+    {
+        os << "'" << el << "' ";
+    }
+    os << ']';
+    return os;
+}
+
+std::vector<std::string> parse_str(const std::string& str)
+{
+    bool quote {false};
+    std::vector<std::string> result{};
+    std::string tmp{};
+ 
+    for(const char c : str)
+    {
+        if(c == '"')
+        {
+        //" fermant
+            if(quote)
+            {
+                quote = false;
+                result.push_back(tmp);
+                tmp = "";
+            }
+            else
+            { //" ouvrant
+                quote = true;
+            }
+        }
+ 
+        //pas un ",
+       if(quote && c != '"') {
+           tmp += c;
+        }
+    }
+ 
+    assert(quote == false);
+    
+    return result;
+}
+
+int main()
+{
+    std::string chaine {"\"Ceci\", \" est une\", \"chaine!\""};
+    std::cout << parse_str(chaine);
+}
