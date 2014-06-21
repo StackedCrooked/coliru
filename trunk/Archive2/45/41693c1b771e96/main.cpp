a496562711fcf85c107268dc094123e35a5f7238
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-21 13:21:12 +0000

    Update archive 45

---
+++ trunk/Archive2/45/41693c1b771e96/main.cpp
@@ -0,0 +1,32 @@
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+template <typename T>
+class remove_pointer_ {
+    template <typename U>
+    struct wrap { using type = U; };
+
+    template <typename U>
+    using dereference =
+      typename std::remove_reference<decltype(*std::declval<U>())>::type;
+
+    template <typename U=T>
+    static auto test(int) -> wrap<dereference<U>>;
+    static auto test(...) -> wrap<T>;
+
+public:
+    using type = typename decltype(test(0))::type;
+};
+
+template <typename T>
+using remove_pointer = typename remove_pointer_<T>::type;
+
+int main() {
+    static_assert(std::is_same<int,remove_pointer<int*>>::value, "");
+    static_assert(std::is_same<int,remove_pointer<int>>::value, "");
+    static_assert(std::is_same<int,remove_pointer<std::unique_ptr<int>>>::value, "");
+    static_assert(std::is_same<int,remove_pointer<std::shared_ptr<int>>>::value, "");
+    static_assert(std::is_same<int[1],remove_pointer<int(*)[1]>>::value, "");
+    static_assert(std::is_same<int(int),remove_pointer<int(*)(int)>>::value, "");
+}
