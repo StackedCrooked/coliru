7d31cfdf08b5007795a30c1ec15359f82a03bdf0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-23 16:46:09 +0000

    Update archive 4e

---
+++ Archive2/4e/0dd427f7c82263/main.cpp
@@ -0,0 +1,16 @@
+// Live variables
+class Main {
+  void main() {
+    int a = 0;
+    if(true) {
+      entrySet = "!a@l";
+      a = 0;
+      exitSet = "a@l";
+    }
+    int b = a;
+    return;
+  }
+
+  String entrySet;
+  String exitSet;
+}
