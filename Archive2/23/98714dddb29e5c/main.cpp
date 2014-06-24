6d904a1241ed4805c2d80f820dc885efec15656f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-24 16:25:12 +0000

    Update archive 23

---
+++ Archive2/23/98714dddb29e5c/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template <typename T>
+class A
+{
+template <template <class X> class Z> friend class C;
+private:
+    void f() { std::cout << "This is A" << std::endl; }
+};
+
+template <typename T>
+class B : public A<T>
+{
+public:
+    void f() { std::cout << "This is B" << std::endl; }
+};
+
+template <template <class X> class T>
+class C
+{
+public:
+    void f() {
+        T<X> b;
+        b.A<X>::f();
+    }
+};
+
+int main()
+{
+    C<B<int>> c;
+    c.f();
+}
