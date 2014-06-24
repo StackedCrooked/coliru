7173e5f15d5061ed86ad8d92c170fb97cd7388db
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-24 10:27:15 +0000

    Update archive 48

---
+++ trunk/Archive2/48/88d2a3d5fd03b2/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class A
+{
+public:
+    int i;
+    float f;
+};
+
+class B
+{
+public:
+    B() {}
+    B(int const _i, float const _f) : i(_i), f(_f) {} 
+    int i = 0;
+    float f = 3.14;
+};
+
+
+int main()
+{
+    A a{ 1, 0.1 };
+    std::cout << a.i << " -- " << a.f << std::endl;
+    B b{ 1, 0.1 };
+    std::cout << b.i << " -- " << b.f << std::endl;
+    B b2;
+    std::cout << b2.i << " -- " << b2.f << std::endl;    
+    return 0;
+}
