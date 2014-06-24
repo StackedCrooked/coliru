3adf265f8d6b2447f85305371729ee8588c3a0ab
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-24 10:20:45 +0000

    Update archive b0

---
+++ Archive2/b0/cd59017390602c/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <random>
+
+int main()
+{
+    int times = 10;
+    std::vector<int> base { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	while(times--) {
+		std::vector<int> temp { base };
+		std::random_device rd;
+		std::mt19937 g(rd());
+		std::shuffle(std::begin(temp), std::end(temp), g);
+
+		for(auto e : temp)
+			std::cout << e << " ";
+
+		std::cout << std::endl;
+	}
+
+	return 0;
+}
