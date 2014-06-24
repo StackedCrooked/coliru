e9ae11457059b4b0d449cbbae489fbdf1b4173f0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-24 16:14:03 +0000

    Update archive 6f

---
+++ Archive2/6f/df3499700604c5/cmd.sh
@@ -0,0 +1,4 @@
+echo -e '\n'
+clang++ -std=c++11 -stdlib=libc++ -O2 -Wall -Wextra -pedantic-errors -c main.cpp && echo -e '\nclang++/libc++ compiled without errors\n'
+g++-4.9 -std=c++11 -O2 -Wall -Wextra -pedantic-errors -c main.cpp && echo -e '\ng++/libstdc++ compiled without errors\n'
+clang++ -std=c++11 -O2 -Wall -Wextra -pedantic-errors -c main.cpp && echo -e '\nclang++/libstdc++ compiled without errors\n'
