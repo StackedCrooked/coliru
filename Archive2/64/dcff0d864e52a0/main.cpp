dbce93d148b3e81307b2b4e2b69a98c9fdcb62a8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-24 16:23:42 +0000

    Update archive 64

---
+++ Archive2/64/dcff0d864e52a0/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+
+struct A
+{
+    virtual ~A() { std::cout << "A::~A()\n"; }
+};
+
+struct B : A
+{
+	~B() { std::cout << "B::~B()\n"; }
+};
+
+int main()
+{
+	std::vector<std::unique_ptr<A>> v { std::unique_ptr<A>(new B) };
+	while(auto it = std::begin(v) && it != std::end(v))
+		v.erase(it);
+	
+	return 0;
+}
