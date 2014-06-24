a5521e697b8911bb4467477c28d6f1f2dd9207d8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-24 10:24:12 +0000

    Update archive 21

---
+++ Archive2/21/593805f2c6e634/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct A{};
+struct B{};
+
+template<class T, class FieldT>
+using addRefU = typename std::conditional<
+                            false,
+                            typename std::add_rvalue_reference< FieldT >::type,
+                            typename std::conditional<
+                                true,
+                                typename std::add_rvalue_reference< FieldT >::type,
+                                typename std::add_lvalue_reference< FieldT >::type
+                            >
+                        >::type;
+
+
+
+int main()
+{
+        std::cout << std::is_rvalue_reference<
+            addRefU<A, B>
+        >::value << std::endl;
+        std::cout << std::is_lvalue_reference<
+            addRefU<A, B>
+        >::value << std::endl;
+        
+}
