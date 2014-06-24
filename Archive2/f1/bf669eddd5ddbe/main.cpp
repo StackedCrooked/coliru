a706fad40871776289231a7723ed279e01e702bc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-24 22:06:46 +0000

    Update archive f1

---
+++ Archive2/f1/bf669eddd5ddbe/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class A {
+public:
+    A(int) {
+        cout << "Base class" << endl;
+    }
+};
+
+class B : virtual public A {
+public:
+    virtual void do_something() = 0;
+};
+
+class C : public B {
+public:
+    C()
+        : A(1) {
+        cout << "C class" << endl;
+    }
+
+    virtual void do_something() {
+    }
+};
