ac9d03a80dfb366828fb98c1bb1197fb912af713
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-25 22:35:00 +0000

    Update archive bb

---
+++ Archive2/bb/354bc99b1be3e7/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <vector>
+#include <chrono>
+
+const unsigned BUFFER_SIZE = 10;
+
+std::mutex mu;
+std::condition_variable full,empty;
+std::vector<int> vec;
+const std::chrono::milliseconds slp(10);
+auto done = false;
+
+void produce()
+{
+  for (auto i = 0u; i < 2 * BUFFER_SIZE; ++i) {
+    std::unique_lock<std::mutex> locker(mu);
+    full.wait(locker, [] {return vec.size() < BUFFER_SIZE;});
+    vec.push_back(i);
+    locker.unlock();
+    
+    empty.notify_one();
+
+    std::this_thread::sleep_for(slp);
+  }
+}
+
+void consume()
+{
+  for (;;) {
+    std::unique_lock<std::mutex> locker(mu);
+    while (vec.empty()) {
+      if (done) {
+        return;
+      }
+      empty.wait(locker);
+    }
+
+    auto value = vec.back();
+    vec.pop_back();
+    locker.unlock();
+
+    full.notify_one();
+    std::cout << "Consumed: " << value << '\n';
+  }
+}
+
+int main() {
+  vec.reserve(BUFFER_SIZE*2);
+  std::thread producer(produce), consumer(consume);
+  producer.join();
+  
+  // Produce some more
+  producer = std::thread(produce);
+  producer.join();
+
+  // Tell consumer we are done producing
+  {
+    std::lock_guard<std::mutex> lock(mu);
+    done = true;
+  }
+  empty.notify_one();
+  consumer.join();
+}
