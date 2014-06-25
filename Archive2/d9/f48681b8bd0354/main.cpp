3096bcaf87c1bf6d27aacfaea0572522f2cdf426
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-25 22:09:08 +0000

    Update archive d9

---
+++ Archive2/d9/f48681b8bd0354/main.cpp
@@ -0,0 +1,9 @@
+namespace  
+{  
+   using TestString = const char* const;  
+
+   TestString STRING = "This is a string";  
+   TestString EDIT_STRING = "Try to modify string";  
+}
+
+int main() {}
