4e012aba5034012b185724411081fc07f318b703
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-25 10:08:33 +0000

    Update archive 63

---
+++ Archive2/63/34c6e2f3332b22/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <type_traits>
+
+template <class T, class U>
+auto min1(T x, U y)
+{ return x < y ? x : y; }
+
+template <class T, class U, 
+          class = std::enable_if_t<std::is_integral<T>::value &&
+                                   std::is_integral<U>::value>>
+auto min2(T x, U y)
+{ return x < y ? x : y; }
+
+struct foo {};
+
+int main()
+{
+//   min1(foo{}, foo{}); // error - invalid operands to <
+   min1(10, 20);
+   
+//   min2(foo{}, foo{}); // no matching function min2
+   min2(10, 20);
+}
