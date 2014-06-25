5cc24cda23abe203377439b020fd108278f4af6c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-25 22:31:50 +0000

    Update archive be

---
+++ Archive2/be/5121f57f650890/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <sstream>
+#include <climits>
+void get(uint bytes){
+    //--- nur Ausgabe ---//
+    std::stringstream bits;
+    std::stringstream vals;
+    //------------------//
+    char spacer = ' ';
+    uint comparator = 1;
+	for(uint i = 0; i < sizeof(bytes) * CHAR_BIT; i++, comparator <<= 1){
+        if(comparator & bytes){
+            switch(comparator){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                // ...
+                case 32768:
+                    break;
+                default:
+                    // return invalid request exception
+                    break;
+            }
+        }
+        //--- nur Ausgabe ---//
+        bits << (i<10?" ":"") << spacer << i;
+        vals << spacer << " " << !!(comparator & bytes);
+        spacer = '|';
+        //------------------//
+	}
+    std::cout << bits.str() << "\n" << vals.str();
+}
+int main()
+{
+    uint data = 16854135;
+    get(data);
+}
