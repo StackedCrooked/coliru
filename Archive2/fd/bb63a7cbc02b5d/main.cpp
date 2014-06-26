893603d2a5f8983616abebdff21564322994381e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-26 04:16:16 +0000

    Update archive fd

---
+++ Archive2/fd/bb63a7cbc02b5d/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <type_traits>
+
+using std::cout;
+using std::string;
+
+
+class Base {
+public:
+  virtual void foo(int&) { cout << "int\n"; }
+  virtual void fooForEnum(int&) { cout << "enum\n"; }
+  virtual void foo(bool&) { cout << "bool\n"; }
+  virtual void foo(string&) { cout << "string\n"; }
+  
+  template <class E>
+  void foo(E& e, typename std::enable_if< std::is_enum<E>::value >::type *ignore=0) {
+    int i = 0;
+    fooForEnum(i);
+    e = static_cast<E>(i);
+  }
+};
+
+enum Ex { A, B};
+
+int main()
+{
+  Ex ea = A;
+  Base b;
+  b.foo(ea);
+}
