6bb8ffc925220b1a3a6ca4016ddcd69d775e9da1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-26 22:20:29 +0000

    Update archive 0b

---
+++ Archive2/0b/71c6cee5c51c93/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+auto makeLambda = [](double ratio){ return [=](double value) {return value * ratio;};};
+
+int main() {
+    auto foo = makeLambda(2);
+    std::cout << foo(3);
+}
