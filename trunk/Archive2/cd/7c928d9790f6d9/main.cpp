f45ee46bc9c31ada68d751adf01e7917940a4817
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-26 16:20:48 +0000

    Update archive cd

---
+++ trunk/Archive2/cd/7c928d9790f6d9/main.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <memory>
+ 
+class A {
+public:
+    virtual ~A() = default;
+    virtual void p() const { std::cout << "A" << std::endl; }
+};
+class B : public A {
+public:
+    virtual ~B() = default;
+    virtual void p() const { std::cout << "B" << std::endl; }
+};
+ 
+int main()
+{
+    std::shared_ptr<A> pA = std::make_shared<B>();
+    pA->p();
+}
