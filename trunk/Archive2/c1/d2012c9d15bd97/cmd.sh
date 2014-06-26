e200790c3fb60d493c898751076340b211a13dde
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-26 22:36:22 +0000

    Update archive c1

---
+++ trunk/Archive2/c1/d2012c9d15bd97/cmd.sh
@@ -0,0 +1,4 @@
+g++ -Wall -Wextra -pedantic-errors -std=c++03 main.cpp && ./a.out
+g++ -Wall -Wextra -pedantic-errors -std=c++11 main.cpp && ./a.out
+clang++ -Wall -Wextra -pedantic-errors -std=c++03 main.cpp && ./a.out
+clang++ -Wall -Wextra -pedantic-errors -std=c++11 main.cpp && ./a.out
