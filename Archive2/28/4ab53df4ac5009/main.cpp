2228ba6e2417327883e35ce64cb8ab2b2c244f24
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-27 10:20:07 +0000

    Update archive 28

---
+++ Archive2/28/4ab53df4ac5009/main.cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+struct MyStr
+{
+    int key;
+    std::string strval;
+
+    MyStr(int key, const std::string& rStrVal) 
+        : key(key)
+        , strval(rStrVal) 
+    {}
+
+    // let stream operators be friend functions instead of members!
+    inline friend std::ostream& operator << (std::ostream& os, const MyStr& val) {
+        os << "key[" << val.key << "], strval['" << val.strval << "']";
+        return os;
+    }
+
+    bool operator < (const MyStr& str) const {
+        return (key > str.key);
+    }
+};
+
+struct comp
+{
+    bool operator()(int key, MyStr const& m) const { return m.key < key; }
+    bool operator()(MyStr const& m, int key) const { return key < m.key; }
+};
+
+int main()
+{
+    std::vector <MyStr> testVec = {
+        MyStr(4, "key 4"), 
+        MyStr(3, "key 3"), 
+        MyStr(3, "key 3"), 
+        MyStr(2, "key 2"), 
+        MyStr(2, "key 2"), 
+        MyStr(2, "key 2")
+    };
+
+    std::sort(testVec.begin(), testVec.end());
+    
+    // search for all elements with key=2
+    auto range = std::equal_range(testVec.begin(), testVec.end(), 2, comp());
+    
+    for(auto it = range.first; it != range.second; ++it) {
+        std::cout << "key: " << it->key << " value: " << it->strval << '\n';
+    }
+}
+
