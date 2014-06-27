4ca24a3a34acd4fe31fed2b06345871095a9a192
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-27 22:42:18 +0000

    Update archive b8

---
+++ Archive2/b8/f6d21d39acee3e/main.cpp
@@ -0,0 +1,22 @@
+#include <array>
+
+class Curve {
+private:
+    // ...
+public:
+    std::array<double, 3> value(double s);
+};
+
+template <typename Functor>
+class Algorithm {
+private:
+    Functor f;
+    decltype(f.value({})) a;
+public:
+    // ...
+};
+
+int main() {
+    Algorithm<Curve> a;
+    (void)a;
+}
