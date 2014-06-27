d046708c10ef92903b0056756bf97f399f294f82
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-27 04:25:54 +0000

    Update archive 00

---
+++ Archive2/00/9a8c9085d88bf9/main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+template <>
+class Test {
+public:
+    void print(){ cout << "Hello, World!" << endl;
+};
+
+int main(int argc, char* argv[])
+{
+    
+    Test a;
+    a.print();
+    
+	return 0;
+}
