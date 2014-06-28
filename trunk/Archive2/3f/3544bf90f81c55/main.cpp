cc6977b55c4fdd7bea91132288d20e3c9c4b4712
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-28 16:29:33 +0000

    Update archive 3f

---
+++ trunk/Archive2/3f/3544bf90f81c55/main.cpp
@@ -0,0 +1,74 @@
+#include <type_traits>
+#include <iostream>
+
+template<class datatype, class FN1, class FN2>
+auto if_else(std::true_type,datatype&& data, FN1 &&fn1, FN2 &&) 
+{return fn1(std::forward<datatype>(data));}
+
+template<class datatype, class FN1, class FN2>
+auto if_else(std::false_type,datatype&& data, FN1 &&, FN2 &&fn2)
+{return fn2(std::forward<datatype>(data));}
+
+
+// ------------------------------------------------------------------------------------------------------------
+
+
+
+struct A{
+    int id= 4;
+};
+
+struct B : A{
+    int h = 900;
+};
+
+class Test{
+    
+//template<class B>
+int fn1(B &a){
+    std::cout << "fn1 " << a.h;
+    return a.h;
+};
+
+//template<class A>
+int fn2(A &a){
+    std::cout << "fn2 " << a.id;
+    a.id = 9;
+    return a.id;
+};
+
+
+template<class Aa>
+auto do_cast(Aa &a){
+    using datatype = typename std::conditional<
+            std::is_same<typename std::decay<Aa>::type, B>::value,
+            B,
+            A>::type;
+    return static_cast<B>(a);
+}
+
+
+
+public:
+    template<bool do_it>
+    void go(){
+        std::integral_constant<bool,do_it> do_first;
+        
+        using datatype = typename std::conditional<do_it,B,A>::type;
+        datatype data;
+        std::cout << std::is_same<decltype(data), A>::value;
+        
+        std::cout << if_else(do_first,
+            *this,
+            [&](auto self){ return self.fn1(data); },
+            [&](auto self){ return self.fn2(data); }
+        ) << '\n';
+    }
+};
+
+int main(){
+    Test().template go<false>();
+    Test().template go<true>();
+    return 0;
+}
+
