8376c2ec97ab70235d839f3fe39ea9d84c71af36
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-28 22:33:16 +0000

    Update archive 88

---
+++ trunk/Archive2/88/43503e5cfa8fdd/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+struct A;
+struct B{
+    B(int){ std::cout << "B constructor\n";}
+    
+    B(const B&) { std::cout << "B copy contructor \n";}
+    B(B&&) { std::cout << "B move contructor \n";}
+    
+   // explicit B(const A&) { std::cout << "B converting contructor \n";}
+    
+};
+struct A{  
+    operator B() {std::cout <<" A conversion \n"; return 1;}; 
+};
+int main()
+{
+   //B b_direct{A{}};
+   B b_copy = A{};
+}
