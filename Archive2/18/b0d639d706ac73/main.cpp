f91764e16643097a4f1d1c5063ae353c49f9fd83
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-29 22:39:08 +0000

    Update archive 18

---
+++ Archive2/18/b0d639d706ac73/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cmath>
+#include <limits>
+
+#define N 5
+
+void distanza(bool *SOL, int k, int n, int d, int min)
+{
+
+    if (k == n)
+	{
+		for (int i = 0; i < n; i++)
+		{
+
+			if (SOL[i])
+			{
+				std::cout << (i + 1) << " ";
+			}
+
+		}
+
+		std::cout << std::endl;
+
+	}
+	else
+	{
+
+		for (int i = 0; i <= 1; i++)
+		{
+            
+            int newMin;
+            
+            if (i == 1 && (k + 1) < min)
+            {
+                newMin = k + 1;
+            }
+            else
+            {
+                newMin = min;
+            }
+
+			SOL[k] = (i ? true : false);
+            
+            if (k + 1 - min <= d || i == 0)
+            {
+                distanza(SOL, k + 1, n, d, newMin);
+            }
+
+		}
+
+	}
+
+
+}
+
+int main(int argc, char *argv[])
+{
+
+	bool SOL[N];
+	distanza(SOL, 0, N, 2, std::numeric_limits<int>::max());
+	return 0;
+
+}
