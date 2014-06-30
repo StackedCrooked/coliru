cb0c8b6ca46c20e875d9aa7981fe43bbc3fa5dd7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-30 04:51:05 +0000

    Update archive a8

---
+++ Archive2/a8/8d9489ff6ad92b/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int array[1000][1000];
+    int cases, height, width, i, j, k, max, total;
+	cin >> cases;
+	for(i=0; i<cases; ++i){
+		cin >> height>> width;
+	   for(j=0; j<height; ++j){
+		   for(k=0; k<width; ++k){
+		   	cin >> array[j][k];
+		   }
+	   }
+	   max = array[height-1][width-1];
+	   for(j=0; j<height; ++j){
+		 for(k=0; k<width; ++k){
+				 total = 0;
+				 for(int l=j; l<height; l++){
+					 for (int g=k; g<width; g++){
+						 total += array[l][g];
+					 }
+				 }
+				 if(total>max)
+				 max = total;
+		 
+		 }
+	   }
+	   cout << max;
+	   
+   }
+	return 0;
+}
