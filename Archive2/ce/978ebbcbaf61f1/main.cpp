6976793e60f8b065db26d7f464834199381e7dee
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-30 10:21:53 +0000

    Update archive ce

---
+++ Archive2/ce/978ebbcbaf61f1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    int array[2000][2000];
+	int cases, height, width, i, j, k;
+	cin>> cases>> height>> width;
+	for(i=0; i<cases; ++i){
+	   for(j=0; j<height; ++j){
+		   for(k=0; k<width; ++k){
+		   	cin>> array[j][k];
+		   }
+	   }
+   }
+	return 0;
+}
