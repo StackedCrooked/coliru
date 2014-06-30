cb744a50a2384b4caa5f0d5a33fa3473768075f1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-06-30 22:42:29 +0000

    Update archive f1

---
+++ trunk/Archive2/f1/1bf1f506deab5a/main.cpp
@@ -0,0 +1,10 @@
+struct M {
+    int operator<<(const void *) { return 0; }
+};
+
+int operator<<(M&, const char*) { return 1; }
+
+int main(int argc, char* argv[])
+{
+    return (M() << "test");
+}
