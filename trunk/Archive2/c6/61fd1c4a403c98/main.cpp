8399a3d6234c313f5f555b81dc22dce1fb9baed4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-01 10:49:09 +0000

    Update archive c6

---
+++ trunk/Archive2/c6/61fd1c4a403c98/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <functional>
+
+struct reply
+{
+    std::string msg = "message from server" ;
+	std::function< int( std::string ) > special = [](std::string) { std::cout << "do nothing\n" ; return 0; };
+
+	template < typename CALLABLE, typename... ARGS > void rebind( CALLABLE&& fn, ARGS&&... args )
+	{ special = std::bind( std::forward<CALLABLE>(fn), std::placeholders::_1, std::forward<ARGS>(args)... ) ; }
+
+	int do_callback() { return special(msg) ; }
+};
+
+int foo( std::string msg, int a, int b )
+{ std::cout << "foo(" << msg << ',' << a << ',' << b << ")\n" ; return a+b ; }
+
+struct bar
+{
+    long operator() ( const std::string& msg, const char* prefix ) const
+    { std::cout << "bar - " << prefix << " '" << msg << "'\n" ; return 23L ; }
+};
+
+int main()
+{
+    reply r ;
+    r.do_callback() ; // do nothing
+
+    const auto fn1 = [] ( std::string str ) { std::cout << str << '\n' ; return 0 ; } ;
+    r.special = fn1 ;
+    r.do_callback() ; // message from server
+
+    r.special = std::bind( foo, std::placeholders::_1, 22, 87 ) ;
+    r.do_callback() ; // foo(message from server,22,87)
+
+    r.rebind( foo, 999, 1000 ) ;
+    r.do_callback() ; // foo(message from server,999,1000)
+
+    r.rebind( bar(), "the mesage is:" ) ;
+    r.do_callback() ; // bar - the mesage is: 'message from server'
+}
