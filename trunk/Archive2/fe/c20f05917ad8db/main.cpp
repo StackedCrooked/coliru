c6d4a607cd6c86b50cf71dcd09e085ece65900b3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-01 16:44:05 +0000

    Update archive fe

---
+++ trunk/Archive2/fe/c20f05917ad8db/main.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+
+class A {
+public:
+    std::vector<int> vec;
+};
+
+template <typename T> class B : public A {  };
+
+template <typename T> class C : public B<T> {
+public:
+    using A::vec;
+    
+    int test() {
+        return vec[1];      // OK
+    }
+
+    int test2() {
+        return vec.size();  // error: 'class A' has no member named 'size'
+    }
+};
+
+int main() {
+    C<double> c;
+    return 0;
+}
