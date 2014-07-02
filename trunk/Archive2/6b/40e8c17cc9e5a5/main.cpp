2789a5e3c9888a2437783c2c04efc48ed7d69c44
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-02 16:54:59 +0000

    Update archive 6b

---
+++ trunk/Archive2/6b/40e8c17cc9e5a5/main.cpp
@@ -0,0 +1,14 @@
+#include <random>
+#include <iostream>
+ 
+int main()
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, 6);
+    dis.param(decltype(dis){dis.a(), 10}.param());
+ 
+    for (int n=0; n<10; ++n)
+        std::cout << dis(gen) << ' ';
+    std::cout << '\n';
+}
