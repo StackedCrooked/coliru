51e71a114206069b9c9843f0bba58b7fe4d1c5c1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-02 04:33:24 +0000

    Update archive 33

---
+++ trunk/Archive2/33/d306d6a90923c8/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+template <bool b1, bool b2>
+struct HeavyLoopImpl
+{
+    static int func(int arg)
+    {
+        for (int i = 0; i < 10000000; i++) {
+            arg += b1 ? 1 : 2;
+            arg += b2 ? 10 : 20;
+        }
+        return arg;
+    }
+};
+
+template <template<bool...> class Impl,bool...Bs>
+struct GenericJump
+{
+    template<typename... Args>
+    static int impl(Args&&... args)
+    {
+        return Impl<Bs...>::func(std::forward<Args>(args)...);
+    }
+
+    template<typename... Args>
+    static int impl(bool b, Args&&... args)
+    {
+        return b
+            ? GenericJump<Impl,Bs...,true >::impl(std::forward<Args>(args)...)
+            : GenericJump<Impl,Bs...,false>::impl(std::forward<Args>(args)...);
+    }
+};
+
+int HeavyLoop(bool b1, bool b2, int arg)
+{
+    return GenericJump<HeavyLoopImpl>::impl(b1,b2,arg);
+}
+
+int main()
+{
+    bool b1 = true;
+    bool b2 = false;
+    int arg = 0;
+    int res = HeavyLoop(b1, b2, arg);
+    cout << "res: "<<res<<endl;
+    return 0;
+}
