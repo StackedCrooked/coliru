753be936a15be7fb4b3173185f8eb974657f7a50
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-02 16:51:46 +0000

    Update archive d5

---
+++ Archive2/d5/bf2faadfae7101/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+namespace X {
+    template <typename T>
+    inline T const& max(T const& a, T const& b)
+    {
+        std::cout << "template" << '\n';
+        return a;
+    }
+}
+
+template <typename T>
+inline T const& max(T const& a, T const& b, T const& c)
+{
+    using ::max; // fallback if no user-defined max
+    return max(max(a, b), c);
+}
+
+namespace X {
+
+class A {};
+
+inline X::A const& max(X::A const& a, X::A const& b)
+{
+    std::cout << "non-template" << '\n';
+    return a;
+}
+
+} // namespace X
+
+int main()
+{
+    X::A a, b, c;
+    max(a, b, c);
+}
