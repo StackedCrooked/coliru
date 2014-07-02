b7baedc1d60f5063488149788a1b584250771a06
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-02 16:35:35 +0000

    Update archive d9

---
+++ Archive2/d9/ae1f555f9d8aec/main.cpp
@@ -0,0 +1,26 @@
+class Handle;
+
+class Base
+{
+protected:		
+	 virtual void load(const Handle &handle, const char * data) = 0;
+public:
+	 virtual ~Base();
+	 void operator()(const Handle &handle, const char * data)
+	 {
+		 load(handle, data);
+	 }
+};
+
+template<class Func>
+class Child
+{
+	Func loader;
+protected:
+	virtual void load(const Handle &handle, const char * data)
+	{
+		loader(handle, *reinterpret_cast<const /* type of Func's second argument */  *>(data));
+	}
+public:
+	Child(Func f) : loader(f) {}
+};
