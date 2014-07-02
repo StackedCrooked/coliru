2dc67e5296154d83a13c9ca66d5a99bef4bfc803
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-02 22:44:12 +0000

    Update archive e1

---
+++ trunk/Archive2/e1/41ee3d27213d6a/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sstream>
+#include<iomanip>
+struct mystream 
+{
+     mystream (std::ostream& os) : os(os) {}
+     std::ostream&   os;
+};
+
+template <class T> 
+mystream& operator<< (mystream& con, const T& x) 
+{ 
+     con.os << x << " "; // add extra delimeter
+     return con; 
+}
+
+
+int main() 
+{
+    std::ostringstream foo;
+    mystream c(foo) ;
+
+    c << 1 << 1.2 << "bar" ;
+    
+    std::cout << foo.str() ;
+ c<<std::setw(10)   <<std::setprecision(3)<<std::hexfloat<<3.14159;
+    std::cout << "Ends!";
+}
