8586f28ce4c42462e18f3ceeac9d69d504faf01c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-03 22:57:20 +0000

    Update archive f2

---
+++ trunk/Archive2/f2/de0ce106f8d93d/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    int tmp_available;
+    int bought = 10;
+    string available = "50";
+
+    tmp_available=stoi(available); 
+    tmp_available -= bought;
+
+    stringstream tmp;
+    tmp << tmp_available;
+    available = tmp.str();
+    
+    cout << available;
+}
