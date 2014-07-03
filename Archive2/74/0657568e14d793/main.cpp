8eba42085584b818565d38a09c08d4406fbea645
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-03 04:39:51 +0000

    Update archive 74

---
+++ Archive2/74/0657568e14d793/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace::std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+    for (int i = 0; i < 10; i++)
+        if(i % 2 == 0)
+            cout << i << endl;
+
+}
