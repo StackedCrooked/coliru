5e3eace3404a19764855556ae117f45cb9e331a2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-03 22:57:04 +0000

    Update archive b5

---
+++ trunk/Archive2/b5/fe09ed0848788d/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+struct Foo {
+    int x, a, b, c, d, e;
+};
+
+int main() {
+    Foo foo = { 1, 2, 3, 4, 5, 6 };
+    int* pfoo = &foo.x;
+    
+    int structMembers = sizeof(Foo) / sizeof(int);
+    for (int i = 0; i < structMembers; i++)
+    {
+        cout << pfoo[i] << endl;
+    }
+}
