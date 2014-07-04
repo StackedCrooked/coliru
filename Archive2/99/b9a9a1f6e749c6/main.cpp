22af2cf243db2804a4d206cf3394eb90c791e564
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-04 11:02:49 +0000

    Update archive 99

---
+++ Archive2/99/b9a9a1f6e749c6/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <boost/preprocessor.hpp>
+
+#define TUP_WITH_0(a, b) BOOST_PP_TUPLE_ELEM(2, 1, BOOST_PP_IF(BOOST_PP_EQUAL(BOOST_PP_TUPLE_ELEM(2, 0, a), 0), a, b))
+#define TUP_WITH_1(a, b) BOOST_PP_TUPLE_ELEM(2, 1, BOOST_PP_IF(BOOST_PP_EQUAL(BOOST_PP_TUPLE_ELEM(2, 0, a), 1), a, b))
+
+#define POSITIONAL2(macro, a, b) macro(TUP_WITH_0(a, b), TUP_WITH_1(a, b))
+
+#define TEST_FOO 0
+#define TEST_BAR 1
+#define TEST(foo, bar) POSITIONAL2(TEST_IMPL, foo, bar)
+
+#define TEST_IMPL(foo, bar) std::cout << (foo) << (bar);
+
+int main() {
+    TEST((TEST_FOO, "foo"), (TEST_BAR, "bar"));
+    std::cout << '\n';
+    TEST((TEST_BAR, "bar"), (TEST_FOO, "foo"));
+}
