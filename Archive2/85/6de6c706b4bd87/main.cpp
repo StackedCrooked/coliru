049feebd537587150539ba41d9346137e41bb579
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-04 05:05:17 +0000

    Update archive 85

---
+++ Archive2/85/6de6c706b4bd87/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+//move this somewhere useful later
+template<typename... Args>
+void swallow(Args&&...)
+{
+}
+
+//and this
+
+template<typename... Args>
+void dbgprint(Args &&... args)
+{
+   swallow((std::cout << args, 0)...);
+}
+
+int main() {
+    dbgprint(1, ' ', 2, " foo ", 5, '\n', "yay");   
+}
