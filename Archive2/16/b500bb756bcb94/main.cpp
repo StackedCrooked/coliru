c0f18eb9e7c527a2e16e625454f8aba7c631f2b2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-06 17:05:44 +0000

    Update archive 16

---
+++ Archive2/16/b500bb756bcb94/main.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <iostream>
+ 
+struct Noisy {
+    Noisy() {std::cout << "constructed\n"; }
+    Noisy(const Noisy&) { std::cout << "copied\n"; }
+    Noisy(Noisy&&) { std::cout << "moved\n"; }
+    ~Noisy() {std::cout << "destructed\n"; }
+};
+ 
+std::vector<Noisy> f()
+{
+    std::vector<Noisy> v = std::vector<Noisy>(3); // copy elision from temporary to v
+    return v; // NRVO from v to the nameless temporary that is returned
+}
+  
+void fn_by_val(std::vector<Noisy> arg)
+{
+    std::cout << "I'm in" << std::endl;
+    arg.emplace_back();
+    std::cout << "I'm in here" << std::endl;
+    std::cout << "arg.size() = " << arg.size() << '\n';
+    std::cout << "I'm ready to get out" << std::endl;
+}
+ 
+int main()
+{
+    //std::vector<Noisy> v = f(); // copy elision from returned temporary to v
+    fn_by_val(f());             // and from temporary to the argument of fn_by_val()
+}
