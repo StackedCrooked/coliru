219bd7a4baf832c199a15eee5d65860e3d239ec5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-06 22:46:55 +0000

    Update archive ba

---
+++ Archive2/ba/e3ec8acd8b823e/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    std::vector<std::string> myvector{"", "1", "", "2", "", "", "3", ""};
+    
+    for (vector<std::string>::iterator i=myvector.begin(); 
+              i != myvector.end(); /* do nothing here */) {
+       if (*i == "") {
+          myvector.erase(i++);
+       } else {
+         ++i;
+       }
+    }
+    
+    for (const auto &s : myvector) {
+        std::cout << s << ' ';   
+    }
+}
