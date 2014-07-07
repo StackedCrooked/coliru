1cc94d6210a51de12cfd92b9e300af68201bd75f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-07 10:55:22 +0000

    Update archive 16

---
+++ Archive2/16/499d9ab82b181f/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <boost/bimap.hpp>
+#include <boost/bimap/set_of.hpp>
+#include <boost/bimap/list_of.hpp>
+
+int main()
+{
+    using namespace std;    
+    map<string, string> myMap;
+    myMap["key1"] = "value1";
+    myMap["key1"] = "value2";
+    for (auto&& elem : myMap)
+        std::cout << "{" << elem.first << ", " << elem.second << "}, ";
+    std::cout << "\n";
+
+    using namespace boost::bimaps;
+    bimap<set_of<string>, list_of<string>> myMap2;
+    myMap2.left["key1"] = "value1";
+    myMap2.left["key1"] = "value2";
+    for (auto&& elem : myMap2.left)
+        std::cout << "{" << elem.first << ", " << elem.second << "}, ";
+    std::cout << "\n";    
+}
