f68edec8d6ce9ac129c4663b9f4c7a5fb9678af3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-07 10:53:29 +0000

    Update archive 89

---
+++ Archive2/89/6bd11719270d14/cmd.sh
@@ -0,0 +1,1 @@
+g++-4.8 -Os -Wall -pedantic -std=c++11 main.cpp &&  ./a.out
---
+++ Archive2/89/6bd11719270d14/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template<class T>
+void myFunction()
+{
+    typename basic_string<T>::size_type i = 0;
+
+}
+
+int main( )
+{
+myFunction<char>( ) ;
+    
+}
