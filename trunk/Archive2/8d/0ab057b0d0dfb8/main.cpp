68fa0dca5a4ec3551323ab527ba032b423cc874d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-08 17:13:50 +0000

    Update archive 8d

---
+++ trunk/Archive2/8d/0ab057b0d0dfb8/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <typeinfo>
+
+#include <type_traits>
+template<typename T>
+void bar(T t)
+{
+    static_assert(std::is_same<T, const int>::value, "Error!");
+    std::cout << t << '\n';
+}
+
+int main()
+{
+    //  static_assert doesn't fire because T==const int
+    bar<const int>(1);
+
+    //  But here static_assert fires bacause T==int (see the error message). Why is this?
+    //  If I replace bar by bar<const int> below the code compiles.
+
+    void(*pf)(int) = bar;
+    pf(1000);
+}
