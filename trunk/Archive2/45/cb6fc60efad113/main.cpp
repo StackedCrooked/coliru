884db4610d0bdb4a5afd7c473e7bdf0abb40bd66
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-08 23:04:29 +0000

    Update archive 45

---
+++ trunk/Archive2/45/cb6fc60efad113/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+int main()
+{
+    unsigned int i = std::numeric_limits<int>::min();
+    std::cout<<"\n" << i << std::endl;
+}
