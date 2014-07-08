fd1c79edabd8a3a7e56ec74c8f455cf6f81a8846
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-08 10:57:55 +0000

    Update archive b5

---
+++ Archive2/b5/51f1e80146f063/main.cpp
@@ -0,0 +1,10 @@
+#include <iomanip>
+#include <iostream>
+using namespace std;
+int main()
+ {
+  double a=1.000000000000020206059048177849 + 0.000000000000020206059048177849;
+  cout<<fixed<<setprecision(30)<<a;
+ system("pause");
+ return 0;
+ }
