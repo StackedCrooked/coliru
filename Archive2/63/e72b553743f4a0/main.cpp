400183943aa91abcf6863fb50d035a9ce896e127
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-08 17:02:13 +0000

    Update archive 63

---
+++ Archive2/63/e72b553743f4a0/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using RingType = int;
+
+void push(const RingType& aData) { 
+    std::cout << 'a';
+    push(std::forward<RingType>(RingType(aData)));
+}
+
+void push(RingType&& aData) {
+   std::cout << "Hello, world\n";
+}
+
+
+int main()
+{
+    const RingType a = 0;
+    push(a);
+}
