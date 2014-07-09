a9f7ec87f2a35742597fd69777e3b57dd9555d3b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-09 11:15:34 +0000

    Update archive 73

---
+++ Archive2/73/21cfdcb7295369/main.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+ 
+int main()
+{
+    // greet the user
+    std::string name;
+    std::cout << "What is your name? ";
+    std::getline(std::cin, name);
+    std::cout << "Hello " << name << ", nice to meet you.\n";
+ 
+    // read file line by line
+    std::istringstream input;
+    input.str("1\n2\n3\n4\n5\n6\n7\n");
+    int sum = 0;
+    for (std::string line; std::getline(input, line); ) {
+        sum += std::stoi(line);
+    }
+    std::cout << "\nThe sum is: " << sum << "\n";
+}
