2c8f0638a9931a7acc647c52899fe21d92e5866e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-09 23:24:48 +0000

    Update archive b7

---
+++ trunk/Archive2/b7/7d476981fa4a09/main.cpp
@@ -0,0 +1,28 @@
+#include <functional>
+#include <iostream>
+
+template<template<class> class COMP = std::less, typename T> 
+void insertionSort(T* elements, int size) {
+    T tempElement;
+    int j;
+    COMP<T> pred;
+    for (int i = 0; i < size; ++i) {
+        tempElement = elements[i];
+
+        for (j = i - 1; j >= 0 && pred(tempElement, elements[j]); --j)
+            elements[j + 1] = elements[j];
+
+        elements[j + 1] = tempElement;
+    }
+}
+
+int main(int argc, char *argv[]) {
+  int A[] = {1,3,5,6,7,4,2};
+  insertionSort<std::greater>(A, 7);
+  for(auto i : A) std::cout << i << " ";
+  std::cout << std::endl;
+  int B[] = {1,3,5,6,7,4,2};
+  insertionSort(B, 7);
+  for(auto i : B) std::cout << i << " ";
+  std::cout << std::endl;
+}
