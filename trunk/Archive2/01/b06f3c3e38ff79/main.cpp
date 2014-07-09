1554865bceaf9e042dffd7f1dd70f9957a1c0e4d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-09 23:20:38 +0000

    Update archive 01

---
+++ trunk/Archive2/01/b06f3c3e38ff79/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct person{
+    std::string name;
+	bool likes_dogs;
+	
+	friend bool dog_lover(const person & p){
+		return p.likes_dogs;
+	}
+};
+
+int main() {
+	vector<person> people = {{"Mary", true}, {"Stanley", false}};
+	
+	auto it = find_if(people.begin(), people.end(), dog_lover);
+	auto it = find_if(people.begin(), people.end(), person::dog_lover);
+	
+	return 0;
+}
