d500daa558946d4b1ff205c6c695cac694dbc6fd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-09 05:17:47 +0000

    Update archive 05

---
+++ Archive2/05/720076cb93b2c3/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+struct element {
+    element(element&&) = default;
+    element& operator=(element&&) = default;
+    
+    element(const element&) = delete;
+    element& operator=(const element&) = delete;
+};
+
+int main() {
+    std::vector<element> source(10), destination;
+    std::move(std::begin(source), std::end(source), std::back_inserter(destination)); // will it work?
+}
