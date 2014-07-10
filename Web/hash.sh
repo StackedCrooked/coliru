a055f750bcba06d1a474403d6742f9c5a59f9e4c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-10 11:36:56 +0000

    hash.sh: check for input file contents last.

--- Web/hash.sh
+++ Web/hash.sh
@@ -4,14 +4,19 @@
     exit 1
 }
 
+[ -d "$INPUT_FILES_DIR" ] || {
+    echo "INPUT_FILES_DIR=$INPUT_FILES_DIR which is not an existing directory." 1>&2
+    exit 1
+}
+
 # start a subshell in order to restore
 # the current directory when it exits
 # increment cache_invalidator to force invalidating all caches
 (
     cd ${INPUT_FILES_DIR}
-    cat *
+    cache_invalidator=1
+    /usr/local/bin/Wide/CLI --version
     type g++ && g++ --version
     type clang++ && clang++ --version
-    /usr/local/bin/Wide/CLI --version
-    cache_invalidator=1
+    cat *
 ) 2>&1 | shasum | awk '{print $1}' | cut -c -16
