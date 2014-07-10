0f096eff4879c7532e1c11fb8d5bfad7dc4959af
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-10 11:05:00 +0000

    Update archive aa

---
+++ trunk/Archive2/aa/1b6246e6fc17ad/main.cpp
@@ -0,0 +1,74 @@
+#include <algorithm>
+#include <initializer_list>
+#include <iostream>
+#include <set>
+#include <unordered_set>
+#include <type_traits>
+
+template<int I> struct priority : priority<I - 1>{};
+template<> struct priority<0>{};
+
+class foo
+{
+public:
+    int data;
+
+    template<class It>
+    bool in(It first, It last) const
+    {
+        std::cout << "iterator overload: ";
+        return std::find(first, last, data) != last;
+    }
+
+    template<class T>
+    bool in(std::initializer_list<T> il) const
+    {
+        std::cout << "initializer_list overload: ";
+        return in(begin(il), end(il));
+    }
+
+    template<class Container>
+    bool in(Container const& c) const 
+    {
+        std::cout << "associative overload: ";
+        return detail_in(priority<1>(), c);    
+    }
+    
+private:
+    template<class AssociativeContainer>
+    auto detail_in(priority<1>, AssociativeContainer const& c) const 
+    -> decltype(c.find(data), bool())
+    {
+        std::cout << "associative overload: ";
+        return c.find(data) != end(c);    
+    }
+
+    template<class SequenceContainer> 
+    bool detail_in(priority<0>, SequenceContainer const& c) const
+    {
+        std::cout << "sequence overload: ";
+        return in(begin(c), end(c));    
+    }
+};
+
+int main()
+{
+    foo f{1};
+    std::cout << f.in({1, 2, 3}) << "\n";
+    std::cout << f.in({2, 3, 4}) << "\n";
+
+    int a1[] = { 1, 2, 3};
+    int a2[] = { 2, 3, 4};
+
+    std::cout << f.in(std::begin(a1), std::end(a1)) << "\n";
+    std::cout << f.in(std::begin(a2), std::end(a2)) << "\n";
+
+    std::cout << f.in(std::vector<int>{ 1, 2, 3 }) << "\n";
+    std::cout << f.in(std::vector<int>{ 2, 3, 4 }) << "\n";
+
+    std::cout << f.in(std::set<int>{ 1, 2, 3 }) << "\n";
+    std::cout << f.in(std::set<int>{ 2, 3, 4 }) << "\n";
+    
+    std::cout << f.in(std::unordered_set<int>{ 1, 2, 3 }) << "\n";
+    std::cout << f.in(std::unordered_set<int>{ 2, 3, 4 }) << "\n";    
+}
