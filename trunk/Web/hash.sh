b3a61020eab6c1d1a0f86ca352b3d9815e964eeb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-10 11:26:00 +0000

    Added cache_invalidator to hash.sh.

--- trunk/Web/hash.sh
+++ trunk/Web/hash.sh
@@ -6,10 +6,12 @@
 
 # start a subshell in order to restore
 # the current directory when it exits
+# increment cache_invalidator to force invalidating all caches
 (
     cd ${INPUT_FILES_DIR}
     cat *
     type g++ && g++ --version
     type clang++ && clang++ --version
     /usr/local/bin/Wide/CLI --version
+    cache_invalidator=1
 ) 2>&1 | shasum | awk '{print $1}' | cut -c -16
