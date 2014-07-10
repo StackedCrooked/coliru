41d7f00962bb0b07992806cc37e4b875f6cb8f8e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-10 05:19:59 +0000

    Update archive 69

---
+++ trunk/Archive2/69/65616f13aa8ddc/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <functional>
+
+void foo() {
+    std::cout << "foo\n";
+}
+
+void bar() {
+    std::cout << "bar\n";
+}
+
+void foobar() {
+    std::cout << "foobar\n";
+}
+
+void qux(int t) {
+    std::cout << t << "\n";
+}
+
+template<class T>
+decltype(auto) operator*(T& f) {
+    return [&](auto&&... a) -> decltype(auto){return f(std::forward<decltype(a)>(a)...);};
+}
+
+int main() {
+    auto f = *foobar;
+    f();
+    auto f2 = *qux;
+    f2(300);
+    return 0;
+}
