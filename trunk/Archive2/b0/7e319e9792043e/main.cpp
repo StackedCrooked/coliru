0d24550f2f11cb179be764658535e579057577d1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-10 22:18:32 +0000

    Update archive b0

---
+++ trunk/Archive2/b0/7e319e9792043e/main.cpp
@@ -0,0 +1,56 @@
+#include <vector>
+#include <iostream>
+
+#include <boost/iterator/iterator_facade.hpp>
+
+struct const_foo_iterator : boost::iterator_facade<const_foo_iterator,
+                                                    unsigned char const,
+                                                    boost::random_access_traversal_tag>
+{
+  const_foo_iterator() = default;
+  const_foo_iterator(unsigned char const *iter) : iter(iter) {}
+private:
+    friend class boost::iterator_core_access;
+
+    void increment() { ++iter; }
+    void decrement() { --iter; }
+    void advance(std::ptrdiff_t n) { iter += n; }
+    
+    std::ptrdiff_t distance_to(const_foo_iterator const& other) const
+    { return iter - other.iter; }
+    
+    bool equal(const_foo_iterator const& other) const
+    { return this->iter == other.iter; }
+    
+    unsigned char const& dereference() const { return *iter; }
+    unsigned char const* iter = nullptr;
+};
+
+class foo {
+    unsigned char* value;
+    int length;
+
+    const_foo_iterator begin();
+    const_foo_iterator end();
+};
+
+const_foo_iterator foo::begin() {
+    return value;
+}
+
+const_foo_iterator foo::end() {
+    return value + length;
+}
+
+#include <iterator>
+#include <type_traits>
+
+int main()
+{
+    static_assert(std::is_same<std::iterator_traits<const_foo_iterator>::value_type,
+                               unsigned char>::value, "value_type");
+    static_assert(std::is_same<std::iterator_traits<const_foo_iterator>::pointer,
+                               unsigned char const *>::value, "pointer");
+    static_assert(std::is_same<std::iterator_traits<const_foo_iterator>::iterator_category,
+                               std::random_access_iterator_tag>::value, "iterator_category");
+}
