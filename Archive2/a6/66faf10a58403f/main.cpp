e50d4c0f4417531343dc300bab050bc33d65acec
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-11 16:11:46 +0000

    Update archive a6

---
+++ Archive2/a6/66faf10a58403f/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+struct foo
+{
+    float a[100];
+};
+
+struct bar
+{
+	float a[100];
+	
+	bar() : a{}
+	{}
+};
+
+struct quux
+{
+	foo f;
+	bar b;
+	
+	quux() : f{} , b{}
+	{}
+};
+
+int main() {
+	quux q;
+	
+	for(float x : q.f.a )
+		std::cout << x;
+
+	std::cout << std::endl;
+	
+	for(float x : q.b.a )
+		std::cout << x;
+	return 0;
+}
