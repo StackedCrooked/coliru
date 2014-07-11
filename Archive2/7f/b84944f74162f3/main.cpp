9d09417c63862cd3190d11f8639865b4133d2180
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-11 10:35:59 +0000

    Update archive 7f

---
+++ Archive2/7f/b84944f74162f3/main.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+template<typename T>
+T processVector(const std::vector<T>& input){
+
+    for(std::vector<T>::const_iterator it=input.begin();it!=input.end();it++){
+
+            //process vector
+    }
+
+}
