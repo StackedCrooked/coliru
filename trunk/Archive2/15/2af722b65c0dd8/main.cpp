5f64d1b1c9aa7e006680bbb1eb7620d09cbb9c76
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-11 04:25:29 +0000

    Update archive 15

---
+++ trunk/Archive2/15/2af722b65c0dd8/main.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <initializer_list>
+#include <iostream>
+#include <set>
+
+class foo
+{
+public:
+    int data;
+
+    template<class T>
+    bool in(std::initializer_list<T> numbers) const
+    {
+        std::cout << "initializer_list overload: ";
+        return std::find(begin(numbers), end(numbers), data) != end(numbers);
+    }
+    
+    template<class T>
+    bool in(std::set<T> const& numbers) const
+    {
+        std::cout << "set overload: ";
+        return numbers.find(data) != end(numbers);    
+    }
+};
+
+int main()
+{
+    foo f{1};
+    std::cout << f.in({1, 2, 3}) << "\n";
+    std::cout << f.in({2, 3, 4}) << "\n";
+    
+    std::cout << f.in(std::set<int>{ 1, 2, 3 }) << "\n";
+    std::cout << f.in(std::set<int>{ 2, 3, 4 }) << "\n";
+}
