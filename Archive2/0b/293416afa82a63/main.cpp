014c566f3015dfa7d99cf2bd720381556c02e271
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-12 04:25:10 +0000

    Update archive 0b

---
+++ Archive2/0b/293416afa82a63/main.cpp
@@ -0,0 +1,16 @@
+#include <ctime>
+#include <iostream>
+#include <locale>
+ 
+int main()
+{
+    std::locale::global(std::locale("ja_JP.utf8"));
+    std::time_t t = std::time(NULL);
+    char mbstr[100];
+    if (std::strftime(mbstr, sizeof(mbstr), "%A %c", std::localtime(&t))) {
+        std::cout << mbstr << '\n';
+    }
+    
+    int* p =0;
+    (*p) =8;
+}
