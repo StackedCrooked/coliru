c849166619fc959b1318cbdc64761c518f53e741
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-13 19:30:37 +0000

    Update archive 82

---
+++ trunk/Archive2/82/a0987bd3ab54d9/main.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+
+bool comp (int i, int j) { return (i > j); }
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vector)
+{
+    long size = vector.size();
+    os << "vector[" << size << "] ";
+    for(size_t i = 0; i < size; ++i)
+        os << vector[i] << " ";
+    return os;
+}
+
+int main()
+{
+    std::vector<int> input;
+
+    int line;
+    std::ifstream source("source.txt");
+    if (source.is_open()) {
+		while (source.good()) {
+            source >> line;
+            input.push_back(line);
+		}
+		source.close();
+	}
+	else {
+		std::cout << "Unable to open file" << std::endl;
+        return 1;
+	}
+    
+    
+    
+    return 0;
+}
