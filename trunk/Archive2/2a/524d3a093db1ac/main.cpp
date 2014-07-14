dd86149b9fc6e57ad39498162379022985739a68
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-14 23:05:33 +0000

    Update archive 2a

---
+++ trunk/Archive2/2a/524d3a093db1ac/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+void t1(int argc, char *argv[])
+{
+    const std::string kFlashLimintArgName = "-myparam";
+
+	char ** search_begin = argv + 1;
+	char ** search_end = argv + (argc - 1);
+
+	char** flash_limit = std::find(search_begin, search_end, kFlashLimintArgName);
+
+	if (flash_limit != search_end)
+	{
+		std::cout << "Found!"<<endl;
+	}
+	else
+	{
+		std::cout << "Not found!"<<endl;
+	}
+}
+
+void t2(int argc, char *argv[])
+{
+	char ** search_begin = argv + 1;
+	char ** search_end = argv + (argc - 1);
+
+	char** flash_limit = std::find(search_begin, search_end, "-myparam");
+
+	if (flash_limit != search_end)
+	{
+		std::cout<<"Found!"<<endl;
+	}
+	else
+	{
+		std::cout << "Not found!"<<endl;
+	}
+}
+
+int main(int argc, char *argv[]) 
+{
+	t1(argc,argv);
+	t2(argc,argv);
+}
