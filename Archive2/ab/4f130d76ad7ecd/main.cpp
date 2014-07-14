0c09d38c3c38d23888fe9cb8cec314c9c6af599f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-14 01:46:05 +0000

    Update archive ab

---
+++ Archive2/ab/4f130d76ad7ecd/main.cpp
@@ -0,0 +1,72 @@
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vector)
+{
+    long size = vector.size();
+    os << "vector[" << size << "] ";
+    for(size_t i = 0; i < size; ++i)
+        os << vector[i] << " ";
+    return os;
+}
+
+class alg
+{
+public:
+    template<class T>
+    static void re_group_array(std::vector<T>& data)
+    {
+        int temp;
+        size_t i = 0;
+        size_t size = data.size();
+        while (i < size) {
+            if (data[i] > 0) {
+                if (data[i]<data[i - 1]) {
+                    temp = data[i];
+                    data[i] = data[i - 1];
+                    data[i-1] = temp;
+                    if (i > 1) {
+                        i-=2;
+                    }
+                }
+            }
+            i++;
+        }
+    }
+    
+};
+
+int main()
+{
+    std::vector<int> input;
+
+    int line = 0;
+    std::ifstream source("source.txt");
+    if (source.is_open()) {
+		while (source.good()) {
+            source >> line;
+            input.push_back(line);
+		}
+		source.close();
+	}
+	else {
+		std::cout << "Unable to open file" << std::endl;
+        return 1;
+	}
+    
+    std::cout << "input: " << input << std::endl;
+    
+    int sum = std::accumulate(input.begin(), input.end(), 0);
+    
+    std::cout << "sum: " << sum << std::endl;
+    
+    auto reGroupArrayCopy(input);
+    alg::re_group_array(reGroupArrayCopy);
+    
+    std::cout << "Re group array: " << reGroupArrayCopy << std::endl;
+    
+    return 0;
+}
