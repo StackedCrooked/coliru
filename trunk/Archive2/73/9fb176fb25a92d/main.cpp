a3ab292eacb6761689f2dcf5dd1958ccf7850f30
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-15 11:28:39 +0000

    Update archive 73

---
+++ trunk/Archive2/73/9fb176fb25a92d/main.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class A {
+public:
+    A()           { cout << "A::A" << endl; }        
+    ~A()          { cout << "A::~A" << endl; }               
+    A(const A& t) { cout <<"A::A(A&)" << endl; }              
+    A(A&& t)      { cout << "A::A(A&&)" << endl; }            
+};
+
+class B {
+public:
+    vector<A> va;
+    B(const vector<A>& va) : va(va) {};
+};
+
+int main(void) {
+    auto list = { A() };
+    return 0;
+}
