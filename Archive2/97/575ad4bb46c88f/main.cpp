ff75513ffbb2999d732206089ec70ed90a193a21
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-16 12:03:06 +0000

    Update archive 97

---
+++ Archive2/97/575ad4bb46c88f/main.cpp
@@ -0,0 +1,16 @@
+#include <array>
+#include <tuple>
+#include <iostream>
+
+const unsigned int NUM = 500;
+
+void simple()
+{
+    using namespace std;
+    array<tuple<float, float, float>, NUM> vectors;
+}
+
+int main() 
+{
+    std::cout << "Hello, world!" << std::endl;
+}
