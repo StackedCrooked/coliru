04e13cac1fb982972d00d4da0556647a7b670258
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-16 18:00:25 +0000

    Update archive 66

---
+++ trunk/Archive2/66/f18080f547fb67/main.cpp
@@ -0,0 +1,33 @@
+#include <regex>
+#include <iostream>
+
+static bool ismatch(const std::string pattern,const std::string field)
+{
+
+        try
+        {
+                std::regex re(pattern);
+                if(std::regex_match(field.begin(),field.end(),re))
+                        return true;
+
+                return false;
+        }
+        catch(std::regex_error &err)
+        {
+                std::cerr<<"code:"<< err.code()<<" Error: "<<err.what()<<std::endl;
+                return false;
+        }
+
+}
+
+
+int main()
+{
+        if(ismatch("^[a-zA-Z]","on"))
+        {
+                std::cout<<"Yes I know";
+        }else
+        {
+                std::cout<<"No";
+        }
+}
