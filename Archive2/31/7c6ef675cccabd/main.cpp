b9862b3769c63e097d3c25d825805aa984bb62f3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-16 17:47:49 +0000

    Update archive 31

---
+++ Archive2/31/7c6ef675cccabd/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <array>
+#include <memory>
+#include <vector>
+
+template <typename T, typename = void>
+struct hasNullComparator : std::false_type {};
+
+template <typename T>
+struct hasNullComparator<T,
+typename std::enable_if<!std::is_void<decltype(T::compareToNull())>::value>::type>
+: std::true_type {};
+
+struct Foo {
+    static bool compareToNull() { return true; }
+};
+
+static_assert(hasNullComparator<Foo>::value, "");
+static_assert(!hasNullComparator<int>::value, "");
+
+int main() {
+	return 0;
+}
