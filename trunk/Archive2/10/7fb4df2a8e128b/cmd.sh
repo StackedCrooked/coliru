42c5b4c0bb213a6fef99ea3ecca8dc6bc05ce181
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-17 06:08:06 +0000

    Update archive 10

---
+++ trunk/Archive2/10/7fb4df2a8e128b/cmd.sh
@@ -0,0 +1,3 @@
+set -x ;
+g++ -std=c++11 -O3 -Wall -pedantic -Wextra -pthread -Wno-unused-variable -Wno-unused-parameter -Wconversion -fno-elide-constructors main.cpp && ./a.out
+# clang++ -std=c++11 -O3 -Wall -pedantic -Wextra -pthread -Wno-unused-variable -Wno-unused-parameter -Wconversion -fno-elide-constructors -stdlib=libc++ main.cpp -lsupc++ && ./a.out
