ab4c9206608b53f946ef2f70b6350ece31f551e8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-17 18:04:46 +0000

    Update archive fd

---
+++ trunk/Archive2/fd/560f1cd2c3e4df/main.cpp
@@ -0,0 +1,21 @@
+// Compile on a recent version of clang and run it:
+// clang++ -std=c++11 -O3 -Wall -fsanitize=undefined main.cpp -o stdint16
+
+#include <cinttypes>
+#include <cstdint>
+#include <cstdio>
+
+int main()
+{
+    /*
+     * http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2012/n3337.pdf
+     *
+     * 3.9.1 Fundamental types [basic.fundamental]
+     *
+     */
+    unsigned short b = UINT16_MAX; b = b * b; // Standard says this is defined, and modulo 2^16
+
+    std::printf(" %04" PRIX16 "\n", b );
+
+    return 0;
+}
