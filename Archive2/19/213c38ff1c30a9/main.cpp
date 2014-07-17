bf75b943c75f1ca155b1cf3e85f70cb61716c3b4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-17 06:06:31 +0000

    Update archive 19

---
+++ Archive2/19/213c38ff1c30a9/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <thread>
+#include <functional>
+
+class parallel {
+public:
+    parallel() : value(0) {}
+	~parallel() {}
+	int value;
+	void increment() {
+		for (int i=0;i<10;++i) {
+			value += i;
+			std::cout << value << ' ';
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+		}
+	}
+};
+
+int main() {
+	parallel test;
+	std::thread t(&parallel::increment, std::ref(test));
+	
+	for (int i=0;i<10;++i)
+		std::cout << test.value << '\n';
+
+	t.join();
+	return 0;
+}
