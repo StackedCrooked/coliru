31ab1e5cb08e233272e58c59245c07e568b82e9d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-19 12:03:38 +0000

    Update archive 90

---
+++ trunk/Archive2/90/f33e637b87b655/main.cpp
@@ -0,0 +1,11 @@
+//#include<iostream>
+#include<unordered_map>
+//#include<algorithm>
+//#include<string>
+
+int main(){
+
+    std::unordered_map<std::string,int> myMap;
+    std::string str = "test1";
+    myMap[str] = 1;
+}
