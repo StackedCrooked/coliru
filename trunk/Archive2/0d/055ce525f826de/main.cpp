73d9038efe27ab5dc7a4c5e999bee00149dfa8a9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-19 17:59:47 +0000

    Update archive 0d

---
+++ trunk/Archive2/0d/055ce525f826de/main.cpp
@@ -0,0 +1,16 @@
+if(function_zero(&myMutableObject) == FAILURE)
+{
+    status = FAILURE;
+}
+else if(function_one(&myMutableObject) == FAILURE)
+{
+    status = FAILURE;
+}
+else if(function_two(&myMutableObject) == FAILURE)
+{
+    status = FAILURE;
+}
+else if(function_three(&myMutableObject) == FAILURE)
+{
+    status = FAILURE;
+}
