e2179e5cfcf1356bb90cb07cbe9dc6335027a8e7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-19 12:04:39 +0000

    Update archive cd

---
+++ Archive2/cd/050d2dca7fc87a/main.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <string>
+
+class ClassWithConstMember
+{
+    public:
+        const int m_const = 2;
+        ClassWithConstMember(){}
+        ~ClassWithConstMember(){}
+};
+
+int main(int argc, char** argv)
+{
+    std::vector<ClassWithConstMember> list;
+    ClassWithConstMember c;
+    list.push_back(c);
+    
+    std::vector<ClassWithConstMember>::iterator clIt = list.begin();
+    while(clIt != list.end())
+    {
+        printf("%d\n", (*clIt).m_const);
+        
+        if(rand() % 2)
+        {
+            clIt = list.erase(clIt);
+        }
+        else
+        {
+            ++clIt;
+        }
+    }
+}
