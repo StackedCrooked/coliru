2ea8b30449f558015945326e8c12977f227ddbbf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-19 00:01:59 +0000

    Update archive 40

---
+++ trunk/Archive2/40/8a53ef2356d6a9/main.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <iostream>
+
+int main()
+{
+    int a = 1024;
+    char* ar = reinterpret_cast<char*>(&a);
+    int b = *((int*)ar);
+    std::cout << b;
+    return 0;
+}
