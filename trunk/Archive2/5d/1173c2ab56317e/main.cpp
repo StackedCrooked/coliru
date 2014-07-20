e0178f8907a9a3a4c9c0f7c3ff3a4b09dc7a41e5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-20 06:12:39 +0000

    Update archive 5d

---
+++ trunk/Archive2/5d/1173c2ab56317e/main.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <iostream>
+
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/spirit/include/phoenix_fusion.hpp>
+#include <boost/spirit/include/phoenix_stl.hpp>
+#include <boost/spirit/include/phoenix_object.hpp>
+#include <boost/fusion/adapted.hpp>
+
+int main()
+{
+    namespace spirit = boost::spirit;
+    namespace qi = spirit::qi;
+    namespace phoenix = boost::phoenix;
+    namespace ascii = spirit::ascii;
+
+    using qi::rule;
+    using qi::int_;
+    using qi::_1;
+    using qi::_val;
+    using qi::phrase_parse;
+    using boost::shared_ptr;
+    using boost::make_shared;
+
+    shared_ptr<int> x = make_shared<int>(123);
+
+    std::cout << *x << std::endl;
+
+    rule<
+        std::string::const_iterator
+        , shared_ptr<int>()
+        , ascii::space_type
+    > int_rule_ = int_[_val = new int(_1)];
+
+    shared_ptr<int> subject_;
+
+    std::string text_ = "12345";
+
+    auto result_ = phrase_parse(text_.cbegin(), text_.cend(), int_rule_, ascii::space, subject_);
+
+    std::cout << (result_ ? "passed" : "failed") << *subject_ << std::endl;
+}
