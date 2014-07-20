b12c112b227c2392aeccdfb972fb84d713b159a5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-20 18:17:24 +0000

    Update archive 40

---
+++ trunk/Archive2/40/ef924f9767361b/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <inttypes.h>
+ 
+int main(void)
+{
+    printf("%zu\n", sizeof(uint64_t));
+    printf("%"PRIu64"\n", UINT64_MAX);
+    printf("%zu\n", sizeof(int64_t));
+    printf("%s\n", PRId64);
+    printf("%+"PRId64"\n", INT64_MIN);
+    printf("%+"PRId64"\n", INT64_MAX);
+ 
+    int64_t n = 7;
+    printf("%+"PRId64"\n", n);
+ 
+    return 0;
+}
