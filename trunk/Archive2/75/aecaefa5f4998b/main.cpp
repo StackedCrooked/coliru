42b8243fc0c8070a2f75302400a406209cb56ea3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-22 22:09:33 +0000

    Update archive 75

---
+++ trunk/Archive2/75/aecaefa5f4998b/main.cpp
@@ -0,0 +1,60 @@
+#include <chrono>
+#include <string>
+#include <iostream>
+#include <functional>
+#include <vector>
+#include <memory>
+#include <boost/ptr_container/ptr_vector.hpp>
+
+using namespace std;
+
+
+// 1. A way to easily measure elapsed time -------------------
+template<typename TimeT = std::chrono::milliseconds>
+struct measure
+{
+    template<typename F>
+    static typename TimeT::rep execution(F const &func)
+	{
+		auto start = std::chrono::system_clock::now();
+		func();
+		auto duration = std::chrono::duration_cast< TimeT>(
+			std::chrono::system_clock::now() - start);
+		return duration.count();
+	}
+};
+// -----------------------------------------------------------
+
+
+// 2. MyObject -----------------------------------------------
+struct MyObject {
+	string mem;
+	MyObject(const char *text) : mem(text) {};
+};
+// -----------------------------------------------------------
+
+
+int main() 
+{
+	cout << "Pushback Object: " << measure<>::execution([&]()
+	{
+		vector<MyObject> VectorOfObjects;
+        VectorOfObjects.reserve(100000);
+    
+		for (int i = 0; i < 100000; i++) {
+			VectorOfObjects.emplace_back("test");
+		}
+	}) << endl;
+
+
+	cout << "Pushback Pointers: " << measure<>::execution([&]()
+	{
+    	boost::ptr_vector<MyObject> VectorOfPointers;
+        VectorOfPointers.reserve(100000);
+    
+		for (int i = 0; i < 100000; i++) 
+		    VectorOfPointers.push_back(new MyObject("test"));
+	}) << endl;
+
+	return 0;
+}
