97ab2392921e4f8df27eabe1e87f798b1c9d8bc4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-22 21:41:35 +0000

    Update archive a9

---
+++ Archive2/a9/5b7d4740cf6fd4/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+
+// Trying out template varargs.
+template<typename T, T... args>
+struct Test
+{
+    // Using constexpr
+    // I had assumed that with this I did not need to
+    // explicitly create an object `values`
+    // that the compiler will work this out at compile time.
+    static constexpr T values[] = {args...};
+};
+
+template<typename T, T... args> 
+constexpr T Test<T, args...>::values[];
+
+// Explicitly instantiate
+// the template to force it to generate the appropriate code. 
+template struct Test<int, 1, 2, 3>;
+
+
+typedef Test<int, 1, 2, 3>  TestInt;
+
+int main()
+{
+    // Silly test to see if it worked.
+    std::cout << TestInt::values[0] << "\n";
+}
