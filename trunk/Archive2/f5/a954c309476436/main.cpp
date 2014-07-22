be6c41286af5e14ec300bbbae549c638c9ebb997
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-22 12:11:56 +0000

    Update archive f5

---
+++ trunk/Archive2/f5/a954c309476436/main.cpp
@@ -0,0 +1,12 @@
+#include <type_traits>
+
+template <char... s> struct silly { using type = int; };
+template <char... s> struct silly<'1', s...> { using type = double; };
+
+template <char... s>
+typename silly<s...>::type operator"" _silly() { return 0; }
+
+int main() {
+    static_assert(std::is_same<int, decltype(4321_silly)>::value, "no luck");
+    static_assert(std::is_same<double, decltype(1234_silly)>::value, "no luck");
+}
