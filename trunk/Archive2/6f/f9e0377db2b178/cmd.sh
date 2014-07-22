b771bf5a5b4fbbac40ffa7a9aac5c7b58cea9130
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-22 22:06:19 +0000

    Update archive 6f

---
+++ trunk/Archive2/6f/f9e0377db2b178/cmd.sh
@@ -0,0 +1,4 @@
+g++ -x c++ -std=c++11 -Wall -Wextra -pedantic main.cpp && ./a.out
+g++ -x c -std=c11 -Wall -Wextra -pedantic main.cpp && ./a.out
+clang++ -x c++ -std=c++11 -Wall -Wextra -pedantic main.cpp && ./a.out
+clang++ -x c -std=c11 -Wall -Wextra -pedantic main.cpp && ./a.out
