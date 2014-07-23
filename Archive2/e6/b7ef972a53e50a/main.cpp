c0ac9dd97e647fe32b35ffaaaaba3da99ab93108
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-23 09:10:52 +0000

    Update archive e6

---
+++ Archive2/e6/b7ef972a53e50a/main.cpp
@@ -0,0 +1,29 @@
+
+template<typename T>
+struct storage {
+    const T* ptr;
+    storage(const T& x): ptr(&x) {}
+};
+
+template<typename T>
+storage<T> make_storage(const T& t) {
+    return { t };
+}
+
+#include <iostream>
+
+
+struct test {
+    test() = default;
+    test(const test&) = default;
+    
+    ~test() {
+        std::cout << "~test()" << std::endl;
+    }
+};
+
+
+int main() {
+    test t;
+    t.~test();
+}
