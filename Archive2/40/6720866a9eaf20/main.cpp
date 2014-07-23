f5244901d6fcaffcc202c8661c3440829e7f835c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-23 05:30:45 +0000

    Update archive 40

---
+++ Archive2/40/6720866a9eaf20/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void my_test_function(void)
+{
+  // cout << "my_test_function is working" << cout << endl;
+  cout << "my_test_function is working\n" << cout << endl;
+}
+
+int main()
+{
+    my_test_function();
+}
