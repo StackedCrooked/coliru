09ae17996bf5bb20fb3520ab67e113bd6975c141
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-23 19:49:00 +0000

    defunct-restarter.sh shows more feedback.

--- Web/defunct-restarter.sh
+++ Web/defunct-restarter.sh
@@ -7,6 +7,8 @@ while true ; do
     if [ $count -gt 50 ] ; then
         echo "There are $count processes. This is suspiciously high. I will reboot the webserver in 5 minutes."
         { sleep 300 ; reboot ; } & disown
+    else
+        echo "There are $count processes. This may still grow."
     fi
     sleep 120
 done
