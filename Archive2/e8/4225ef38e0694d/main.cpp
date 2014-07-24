7363549c62cc21b188c7215fda8edc82bf0b389e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-24 06:37:14 +0000

    Update archive e8

---
+++ Archive2/e8/4225ef38e0694d/main.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <thread>
+#include <unistd.h>
+
+void m ()
+{
+
+  int a(0), b(0);
+
+  auto f = [&]() 
+    {
+      while (1)
+      { 
+          __transaction_atomic
+          {
+            a++;
+            b++;
+            
+            if (a % 5 == 2) { a+= 11; b += 11; }
+          };
+      }
+    };
+
+  
+  std::thread(f).detach();
+
+  std::thread([&]{ while(1) { if ( __transaction_atomic(a-b) != 0 ) { printf("error\n"); exit(0); } } }).detach();
+  std::thread([] { sleep(1); printf("no errors\n"); exit(0); }).detach();
+  sleep(2);
+  printf("exit\n");
+  return;
+
+
+
+}
+
+int main ()
+{
+  m();
+  printf("hi\n");
+}
