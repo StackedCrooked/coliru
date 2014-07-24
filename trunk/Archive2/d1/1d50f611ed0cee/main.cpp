e8318ebbcfb3190a90c7c9f1bca0ed817994ccd6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-24 06:41:54 +0000

    Update archive d1

---
+++ trunk/Archive2/d1/1d50f611ed0cee/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+ 
+ template<class ...ArgsIn>
+struct C {
+    
+  template<class ...Args>
+  C(Args&& ... args) {std::cout << "Ctr\n";}        // elision occurs without &&
+  
+  ~C(){std::cout << "Dstr\n";}
+  //C(C&&) { std::cout << "A move was made.\n"; }
+};
+ 
+template<class ...Args> 
+auto f(Args ... args) {
+    int i = 1;
+    std::cout << "call "<<std::endl;
+  return C<>(i, i, i);
+}
+ 
+int main() {
+  std::cout << "Hello World!\n";
+  auto obj = f();
+}
