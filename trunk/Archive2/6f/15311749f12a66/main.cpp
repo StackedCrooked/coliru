002e0340671df5c420dd87016a5302c85378d213
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-24 15:35:13 +0000

    Update archive 6f

---
+++ trunk/Archive2/6f/15311749f12a66/main.cpp
@@ -0,0 +1,89 @@
+#include <array>
+#include <vector>
+#include <iostream>
+
+template<typename Type>
+class elements 
+{
+    public: 
+
+        elements(std::initializer_list<Type> list)
+            :
+                data_(list)
+        {}
+
+        auto begin() const
+        {
+            return data_.begin(); 
+        }
+
+        auto end() const
+        {
+            return data_.end(); 
+        }
+
+    private:
+
+        std::vector<Type> data_; 
+}; 
+
+template<typename Type>
+class triElement 
+{
+    public: 
+
+        triElement(std::initializer_list<Type> list)
+            :
+                data_(list)
+        {}
+
+        auto begin() const
+        {
+            return data_.begin(); 
+        }
+
+        auto end() const
+        {
+            return data_.end(); 
+        }
+
+    private:
+
+        std::array<Type, 3> data_; 
+}; 
+
+template<typename C>
+void print(C const & c)
+{
+    for (const auto& e : c)
+    {
+        std::cout << e  << " "; 
+    }
+    std::cout << "\n";
+}
+
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+    // Works. 
+    std::array<int, 3> a = {1,2,3}; 
+
+    print(a);
+
+    // Works. 
+    elements<int>  e = {1,2,3,4,5,6,7}; 
+
+    print(e);
+
+    // Fails.
+    triElement<int> t = {1,2,3};
+    
+    print(t);
+
+    return 0;
+}
+
+
+
+
