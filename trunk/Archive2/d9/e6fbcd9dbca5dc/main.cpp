beed94fe06e385f968ce2b50ad3afbcff07458ca
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-24 12:35:47 +0000

    Update archive d9

---
+++ trunk/Archive2/d9/e6fbcd9dbca5dc/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+std::vector<int> convert(int number)
+{
+    std::vector<int> result;
+
+	if (number == 0) {
+		result.push_back(0);
+	}
+
+	while (number) {
+		int temp = number % 10;
+        number /= 10;
+		//result.push_back(temp);
+        result.insert(result.begin(), temp);
+	}
+
+	//std::reverse(result.begin(), result.end());
+    
+    return result;
+}
+
+int main()
+{
+    int i = 123;
+    auto result = convert(i);
+    std::cout << i << std::endl;
+    for (auto& j : result) {
+        std::cout << j << ',';
+    }
+    std::cout << std::endl;
+}
