0634f472dc304154e4dcf8409cf046c45fcc1186
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-24 18:39:37 +0000

    Update archive 6f

---
+++ Archive2/6f/8ed73f2bb95738/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <iomanip>
+
+
+std::string string_to_hex(const std::string& input)
+{
+    static const char* const lut = "0123456789ABCDEF";
+    size_t len = input.length();
+
+    std::string output;
+    output.reserve(2 * len);
+    for (size_t i = 0; i < len; ++i)
+    {
+        const unsigned char c = input[i];
+        output.push_back(lut[c >> 4]);
+        output.push_back(lut[c & 15]);
+    	output.push_back(' ');
+		if( !((i+1) % 16) )
+		{
+			output.push_back('\n');
+		}
+    }
+    return output;
+}
+
+
+
+int main ( int argc, char *argv[] )
+{
+
+    std::string test;
+	int i = 42;
+	int j;
+    long k = 1023;
+    long l;
+	
+	test.append( reinterpret_cast<char *>(&i), sizeof(int));
+	test.append( "[0][1][2][3][4][5][6][7]" );
+    auto offset = test.size();
+    test.append( reinterpret_cast<char *>(&k), sizeof(long));
+	test.append( "[0][1][2][3][4][5][6][7]" );
+
+	//j = *((int *)( test.substr(0,sizeof(i)).data()));
+	j = *((int *)( &test[0]));
+    //l = *((long *)(test.substr(28,sizeof(k)).data()));
+	l = *((long *)( &test[offset]));
+	std::cout << string_to_hex( test ) << std::endl;
+    std::cout <<  "j = " << j << " l = " << l << std::endl;
+	return( 0 );
+}
