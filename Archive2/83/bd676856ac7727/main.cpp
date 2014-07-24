72014e2516ef9a065ba2f85436d1ccd7eb02fb0c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-24 21:53:27 +0000

    Update archive 83

---
+++ Archive2/83/bd676856ac7727/main.cpp
@@ -0,0 +1,56 @@
+#define BOOST_SPIRIT_USE_PHOENIX_V3
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix.hpp>
+
+namespace qi    = boost::spirit::qi;
+namespace ascii = boost::spirit::ascii;
+namespace phx   = boost::phoenix;
+
+struct ConditionTree {
+    void AddDaysLimitOperator(unsigned) {}
+    void AddHoursLimitOperator(unsigned) {}
+    void AddMinutesLimitOperator(unsigned) {}
+};
+
+template <typename Iterator = std::string::const_iterator, typename Skipper = ascii::space_type>
+struct Parser: qi::grammar<Iterator, ascii::space_type>
+{
+public:
+  Parser(ConditionTree& a_lTree):
+    Parser::base_type(limit_expression),
+    m_lTree(a_lTree)
+  {
+    limit_expression = limit_days_operator | limit_hours_operator | limit_minutes_operator ;
+
+    limit_days_operator    = ( "limit" >> qi::uint_ )[ phx::bind( &ConditionTree::AddDaysLimitOperator,    phx::ref(m_lTree), qi::_1) ];
+    limit_hours_operator   = ( "limit" >> qi::uint_ )[ phx::bind( &ConditionTree::AddHoursLimitOperator,   phx::ref(m_lTree), qi::_1) ];
+    limit_minutes_operator = ( "limit" >> qi::uint_ )[ phx::bind( &ConditionTree::AddMinutesLimitOperator, phx::ref(m_lTree), qi::_1) ];
+
+    BOOST_SPIRIT_DEBUG_NODE(limit_expression);
+
+    BOOST_SPIRIT_DEBUG_NODE(limit_days_operator);
+    BOOST_SPIRIT_DEBUG_NODE(limit_hours_operator);
+    BOOST_SPIRIT_DEBUG_NODE(limit_minutes_operator);
+  }
+
+  qi::rule<Iterator, Skipper> limit_expression;
+  qi::rule<Iterator, Skipper> limit_days_operator;
+  qi::rule<Iterator, Skipper> limit_hours_operator;
+  qi::rule<Iterator, Skipper> limit_minutes_operator;
+
+  ConditionTree& m_lTree;
+};
+
+int main()
+{
+    ConditionTree oTree;
+
+    Parser<> parser(oTree);
+
+    std::string strTest("limit5minutes");
+
+    std::string::const_iterator it_begin(strTest.begin());
+    std::string::const_iterator it_end(strTest.end());
+
+    bool result = phrase_parse(it_begin, it_end, parser, ascii::space);
+}
