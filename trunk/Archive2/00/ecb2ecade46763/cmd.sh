72bf1a1acd99387d84fd284d1875cac31aac949e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-25 15:40:54 +0000

    Update archive 00

---
+++ trunk/Archive2/00/ecb2ecade46763/cmd.sh
@@ -0,0 +1,2 @@
+set -x; g++ -std=c++1y -O3 -Wall -Wextra -pthread -pedantic main.cpp && ./a.out
+clang++ -stdlib=libc++ -std=c++1y -O3 -Wall -Wextra -pthread -pedantic main.cpp -lcxxrt -ldl  && ./a.out
