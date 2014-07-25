e00f2e4ef3fd4998004d85f0c0bbe81f215f2821
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-25 06:34:14 +0000

    Update archive 8b

---
+++ trunk/Archive2/8b/2da2ecdee26572/main.cpp
@@ -0,0 +1,20 @@
+template <typename typeA>
+class Parent{
+public:
+    template <typename typeB>
+    void foo(){}
+
+};
+
+template <typename typeA>
+class Child : public Parent<typeA>{
+public:
+    void bar(){
+        this->template foo<double>();
+    }
+};
+
+int main(){
+    Child<int> c;
+    c.bar();
+}
