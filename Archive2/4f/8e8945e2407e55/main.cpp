d38e9d207a58a61122694e9f9039fb9ba7432d52
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-25 15:49:23 +0000

    Update archive 4f

---
+++ Archive2/4f/8e8945e2407e55/main.cpp
@@ -0,0 +1,50 @@
+#include <array>
+#include <vector>
+#include <iostream>
+
+template<typename Geometry=void, typename Enable = void>
+struct tag 
+{
+    typedef void type;
+};
+
+struct point_tag {}; 
+struct triangle_tag {}; 
+
+template<>
+struct tag<std::vector<double>>
+{
+    typedef point_tag type; 
+};
+
+template<typename Point>
+using triangle = 
+typename std::enable_if
+<
+    std::is_base_of<typename tag<Point>::type, point_tag>::value,
+    std::array<Point,3>
+>::type;
+// This works, but there is no restriction on Point to be tagged with point_tag.
+//std::array<Point, 3>;
+
+/*template <typename Point>
+struct tag<triangle<Point>, void> : point_tag 
+{
+    typedef triangle_tag type;
+};*/
+
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+    typedef vector<double> pointType; 
+    typedef triangle<pointType> triangleType;
+
+    triangleType t = { { {0,0,0}, {1,0,0}, {0, 1,0} } }; 
+
+    return 0;
+}
+
+
+
+
