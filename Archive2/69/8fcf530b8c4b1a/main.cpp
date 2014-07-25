36b3abd8dbfaad8751014d714beb89f913c306d4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-25 18:49:26 +0000

    Update archive 69

---
+++ Archive2/69/8fcf530b8c4b1a/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+
+namespace detail
+{
+    template <size_t N>
+    size_t constexpr pack_size()
+    { return N; }
+    template <size_t N, typename T, typename... Ts>
+    size_t constexpr pack_size()
+    { return pack_size<N + 1, Ts...>(); }
+}
+
+template <typename... Ts>
+size_t constexpr pack_size()
+{ return detail::pack_size<0, Ts...>(); }
+
+int main()
+{
+    std::cout << pack_size<int, float, bool>() << std::endl;
+}
