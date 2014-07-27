df8442ab352d7ed80affffbfe4960104832b9b48
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-27 00:51:09 +0000

    Update archive ec

---
+++ Archive2/ec/044eb9970349dd/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+#include <malloc.h>
+
+using namespace std;
+
+void* operator new[](size_t t, size_t)
+{
+    cout << "allocation" << endl;
+    return malloc(t);
+}
+
+struct A
+{ 
+    void* operator new[](size_t t, size_t)
+    {
+        cout << "Member allocation" << endl;
+        return malloc(t);
+    }
+
+    void operator delete[](void *p) noexcept
+    {
+        cout << "one argument dealloaction" << endl;
+    }
+    
+    void operator delete[](void *p, size_t t)
+    {
+        cout << "two arguments deallocation" << endl;
+        free(p);
+    }
+};
+
+int main()
+{
+    float* p[5];
+    float ** t = p;
+}
