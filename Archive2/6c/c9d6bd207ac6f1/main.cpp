c8b028436467a8e6afe33a90f054cfb7e7fff2d1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-28 13:10:02 +0000

    Update archive 6c

---
+++ Archive2/6c/c9d6bd207ac6f1/main.cpp
@@ -0,0 +1,16 @@
+ #include<stdio.h>
+    void main()
+    {
+    printf("%d");
+    }
+
+
+
+
+ #include<stdio.h>
+    void main()
+    {
+    int num1 = 0x20 + 020 + 20;
+    printf("%d",sizeof(num1));
+    }
+    
