5fa4954899e9ca3fe067b1a2b99bab665b0b5580
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-28 13:14:45 +0000

    Update archive ad

---
+++ trunk/Archive2/ad/b0199e8605be34/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdarg>
+ 
+void simple_printf(const char* fmt...)
+{
+    va_list args;
+    va_start(args, fmt);
+ 
+    while (*fmt != '\0') {
+        if (*fmt == 'd') {
+            int i = va_arg(args, int);
+            std::cout << i << '\n';
+        } else if (*fmt == 'c') {
+            // note automatic conversion to integral type
+            int c = va_arg(args, int);
+            std::cout << static_cast<char>(c) << '\n';
+        } else if (*fmt == 'f') {
+            double d = va_arg(args, double);
+            std::cout << d << '\n';
+        }
+        ++fmt;
+    }
+ 
+    va_end(args);
+}
+ 
+int main()
+{
+    simple_printf("dcff", 3, 'a', 1.999, 42.5); 
+}
