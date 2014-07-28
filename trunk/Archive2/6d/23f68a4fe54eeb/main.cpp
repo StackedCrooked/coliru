0fcc99c712ec8d3b0e90ebc5f3abc55419f57d17
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-28 19:16:28 +0000

    Update archive 6d

---
+++ trunk/Archive2/6d/23f68a4fe54eeb/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <type_traits>
+
+template<bool b> struct booltype {};
+
+class A {
+public:
+    virtual void foo() = 0;
+};
+
+template <typename T, typename Enable = void>
+class B : public A {
+public:
+    void foo() override { std::cout << "A is not base of T!" << std::endl; }
+};
+
+
+template <typename T>
+class B<T, typename std::enable_if<std::is_base_of<A,T>::value>::type> : public A {
+public:
+    void foo() override { std::cout << "A is base of T!" << std::endl; }
+};
+
+template <typename T>
+class B<std::vector<T>, typename std::enable_if<!std::is_base_of<A,T>::value>::type> : public A {
+public:
+    void foo() override { std::cout << "Vector of Ts!" << std::endl; }
+};
+
+template <typename T>
+class B<std::vector<T>, typename std::enable_if<std::is_base_of<A,T>::value>::type> : public A {
+public:
+    void foo() override { std::cout << "Vector of Ts where A is a base of T!" << std::endl; }
+};
+
+class C : A {
+public:
+    void foo() { }
+};
+
+class D  { };
+
+int main() {
+    B<C> bar;
+    bar.foo();
+    B<D> baz;
+    baz.foo();
+    B<std::vector<D> > qux;
+    qux.foo();
+    B<std::vector<C> > quux;
+    quux.foo();
+}
