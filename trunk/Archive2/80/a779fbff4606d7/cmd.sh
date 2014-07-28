93e561f08421ece0db935a6a5eef9615485fbc06
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-28 19:07:06 +0000

    Update archive 80

---
+++ trunk/Archive2/80/a779fbff4606d7/cmd.sh
@@ -0,0 +1,3 @@
+set -x;
+# g++-4.9 -g -std=c++1y -O0 -Wall -Wextra -pedantic -Wno-unused-variable -Wno-unused-parameter main.cpp && ./a.out
+clang++ -g -std=c++1y -O3 -Wall -Wextra -pedantic -Wno-unused-variable -Wno-unused-parameter -stdlib=libc++ main.cpp -lsupc++ && ./a.out
---
+++ trunk/Archive2/80/a779fbff4606d7/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+template<typename T> const int a = 0;
+
+template<> const int a<float> = 5;
+template<> const int a<double> = 14;
+template<> const int a<char> = 6;
+template<> const int a<wchar_t> = 33;
+
+int main()
+{
+    std::cout << a<int> << std::endl;
+    std::cout << a<float> << std::endl;
+    std::cout << a<double> << std::endl;
+    std::cout << a<char> << std::endl;
+    std::cout << a<wchar_t> << std::endl;
+}
