eabf591fcdbd2f1979890146f929b9dc74eb111f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-28 15:58:47 +0000

    Update archive 22

---
+++ Archive2/22/1f3f1254ed6d19/main.cpp
@@ -0,0 +1,33 @@
+#include <cstddef>
+#include <type_traits>
+
+struct Default {};
+
+template<std::size_t Pos, bool UseDefault, typename... Ts>
+struct At_;
+
+template<std::size_t Pos, typename... Ts>
+struct At_<Pos, true, Ts...> {
+    using type = Default;
+};
+
+template<typename Head, typename... Tail>
+struct At_<0, false, Head, Tail...> {
+	using type = Head;
+};
+
+template<std::size_t Pos, typename Head, typename... Tail>
+struct At_<Pos, false, Head, Tail...> : At_<Pos - 1, false, Tail...> {};
+
+template<std::size_t Pos, typename... Ts>
+using At = typename At_<Pos, sizeof...(Ts) <= Pos, Ts...>::type;
+
+int main() {
+    static_assert(std::is_same<At<0, int>, int>::value, "");
+    static_assert(std::is_same<At<1, int>, Default>::value, "");
+    
+    static_assert(std::is_same<At<0>, Default>::value, "");
+    static_assert(std::is_same<At<0, int, char>, int>::value, "");
+    static_assert(std::is_same<At<1, int, char>, char>::value, "");
+    static_assert(std::is_same<At<9001, int, char>, Default>::value, "");
+}
