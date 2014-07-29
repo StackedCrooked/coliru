602a19e879de9374912158e3744821ae21984285
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-29 20:46:14 +0000

    Timeout.

--- trunk/Web/sandbox.sh
+++ trunk/Web/sandbox.sh
@@ -3,8 +3,13 @@
 set -e
 source coliru_env.source
 
+# Kill any remaining sandbox processes
+ps -eouid,pgid | sed -E 's,^[[:space:]]+,,' | grep ^2002 | awk '{print $2}' >.pgid_killer
+
+
 # check if INPUT_FILES_DIR is set.
 [ "$INPUT_FILES_DIR" == "" ] && { echo "INPUT_FILES_DIR not set." 1>&2 ; exit 1 ; }
+
 
 # get the id (a on hash of main.cpp and cmd.sh)
 id="$(./hash.sh)"
@@ -27,11 +32,10 @@ pathifiedId="$(./pathify-id.sh ${id})"
     cat "${COLIRU_ARCHIVE2}/${pathifiedId}/output" && exit
 }
 
+
 # no previous result found => compile it and print the output
 mkdir -p ${COLIRU_COMPILE_ARCHIVE}/${id}
 
-# Kill any remaining sandbox processes
-ps -eouid,pgid | sed -E 's,^[[:space:]]+,,' | grep ^2002 | awk '{print $2}' >.pgid_killer
 
 /bin/bash -c "exec > >(tee ${COLIRU_COMPILE_ARCHIVE}/${id}/output)
 exec 2>&1
