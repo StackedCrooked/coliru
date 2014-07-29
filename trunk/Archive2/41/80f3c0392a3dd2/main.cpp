d6e7ba285d3b66aca4b4a246303e7ca84f8010db
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-29 01:01:23 +0000

    Update archive 41

---
+++ trunk/Archive2/41/80f3c0392a3dd2/main.cpp
@@ -0,0 +1,41 @@
+typedef struct internal_data * Opaque;
+
+struct internal_data { int unusable; };
+
+template<typename T>
+struct Internal {
+ T data;
+};
+
+template <typename T>
+Opaque map_internal(Internal<T>* p)
+{
+  return reinterpret_cast<Opaque>(p);
+}
+
+template <typename T>
+Internal<T>* remap_internal(Opaque p)
+{
+  return reinterpret_cast<Internal<T>*>(p);
+}
+
+template<typename T>
+Opaque Initialise(T data) {
+  Internal<T>* t = new Internal<T>();
+  t->data = data;
+  return map_internal(t);
+}
+
+#include <cassert>
+
+int main(int argc, char** argv)
+{
+  Opaque o = Initialise(argc);
+  (void)o;
+
+  Internal<int>* pt = new Internal<int>();
+  Opaque o2 = map_internal(pt);
+  Internal<int>* p = remap_internal<int>(o2);
+  assert(p == pt);
+  return 0;
+}
