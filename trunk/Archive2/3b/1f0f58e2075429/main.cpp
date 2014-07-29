1eeb194a25d33bae813ab812636155db8aaa2ced
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-29 21:45:46 +0000

    Update archive 3b

---
+++ trunk/Archive2/3b/1f0f58e2075429/main.cpp
@@ -0,0 +1,11 @@
+#include <memory>
+#include <iostream>
+
+void FuckingCOM ( int** cominitializegogo ) {delete *cominitializegogo; *cominitializegogo = new int(48);}
+
+int main () {    
+    std::unique_ptr<int> meow( new int(24) );
+    auto* ptr = meow.get();
+    FuckingCOM ( &ptr );    
+    std::cout << *meow << std::endl;
+}
