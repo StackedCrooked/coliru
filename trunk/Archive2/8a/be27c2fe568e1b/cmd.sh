2a4ce6196ca93e431ddc1c36816f3e45d10dff32
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-29 21:45:16 +0000

    Update archive 8a

---
+++ trunk/Archive2/8a/be27c2fe568e1b/cmd.sh
@@ -0,0 +1,9 @@
+##clang++ --version
+##g++ --version
+##set -x ; g++-4.9 -std=c++11 -Wall -Wextra -Wconversion -pedantic -O3 -pthread  main.cpp && ./a.out | c++filt -t
+##g++ --version
+#set -x ; g++-4.9 -std=c++11 -Wall -Wextra -Wconversion -pedantic  -O3  -pedantic  -pthread -S main.cpp && cat main.s 
+##set -x ; clang++ -std=c++11 -fsanitize=undefined,signed-integer-overflow -Wall -Wextra -Wconversion  -pedantic -O0  -Wall -pthread  main.cpp && ./a.out
+##set -x ; gcc -x c -std=c99 -O0  -W -Wall -Wextra -pedantic -Wconversion  main.cpp && ./a.out
+##set -x ; gcc -x c -std=c99 -O0 -W -Wall -Wextra -Wconversion -pedantic -S main.cpp && cat main.s | grep 15
+set -x ; clang -x c -std=c11 -fsanitize=undefined -Wall -Wextra -Wconversion -Weverything  -O0  -pedantic main.cpp && ./a.out
---
+++ trunk/Archive2/8a/be27c2fe568e1b/main.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main() {
+ unsigned long x1 = 3 * 1024 * 1024 * 1024 ;
+}
