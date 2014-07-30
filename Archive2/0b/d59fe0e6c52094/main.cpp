d46201ef2020269ab1ea2264f0db6cc6b394ea86
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-30 13:53:19 +0000

    Update archive 0b

---
+++ Archive2/0b/d59fe0e6c52094/main.cpp
@@ -0,0 +1,27 @@
+#include <string>
+using namespace std;
+
+char _getch();
+
+string& Get_Filename()
+{
+    char ch;
+    string name;
+
+    while (ch = _getch() != 'CR')
+    {
+        if (ch == 'ESC')
+            throw RESTART;
+        if (ch == 'BS')
+        {
+            name.pop_back();
+            cout << 'BS';   // Also, will this correctly delete or will I need to
+        }                   // << '\b' << ' ' << '\b'?
+        else
+        {
+            name += ch;
+            cout << ch;
+        }
+    }
+    return name;
+}
