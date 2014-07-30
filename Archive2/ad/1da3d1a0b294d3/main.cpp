a756f723ce1daeca0e636f61eae6f856991155aa
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-30 10:35:27 +0000

    Update archive ad

---
+++ Archive2/ad/1da3d1a0b294d3/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+    cout << setprecision(20) << pow(1000000006, 2) << endl
+         << 1000000006LL * 1000000006LL << endl;
+}
