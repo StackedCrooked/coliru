22ba89c9cdfcf37ca6450d3385919f12645a148b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-30 22:04:42 +0000

    Update archive b8

---
+++ Archive2/b8/428aae426f9f85/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+void copy_names( std::istream& input, std::ostream& output )
+{
+    static const std::string name_tag = "name = " ;
+    static const auto name_tag_sz = name_tag.size() ;
+    static const std::string product_tag = ", product =" ;
+
+    std::string line ;
+    while( std::getline( input, line ) )
+    {
+        auto begin = line.find(name_tag) ; // try to find name_tag
+        if( begin != std::string::npos ) // if found
+        {
+            begin += name_tag_sz ; // set begin of name to char just after name_tag
+
+            const auto end = line.find( product_tag, begin  ) ; // try to find product_tag
+            // if found, name is everything up to the product_tag,
+            // else everything up to end of line
+            auto len = end != std::string::npos ? end-begin : end ;
+
+            output << line.substr( begin, len ) << '\n' ; // extract ant write the name
+        }
+    }
+}
+
+int main()
+{
+    std::istringstream file( R"(
+                                    userid = xxsfdfafa, name = vinnidrk, product = 12341,
+                                    otherfield = 1
+                                    otherfield2 = 2
+                                    otherfield3 = 4
+
+                                    userid = asdsada, name = anotherperson, product = 424123,
+                                    otherfield = 3
+                                    otherfield2 = 5
+                                    otherfield3 = 1
+
+                                    userid = vhhvjv, name = a third person, product = 999,
+                                    otherfield = 8
+                                    otherfield2 = 3
+                                    otherfield3 = 4
+                               )" ) ;
+    copy_names( file, std::cout ) ;
+}
