262e7ebc4ac1cbdce0eae2479e76e31758b17cd6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-30 20:05:01 +0000

    Update archive e4

---
+++ Archive2/e4/420f5ee80371b9/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+struct base {
+  virtual void foo(int) { std::cout << "base::foo(int)" << std::endl; }
+  virtual void foo(char) { std::cout << "base::foo(char)" << std::endl; }
+};
+
+struct derived : public base {
+  using base::foo;
+  void foo(int) override { std::cout << "derived::foo(int)" << std::endl; }
+};
+
+int main(void) {
+  derived d;
+  base b;
+  
+  b.foo('1');
+  d.foo('1');
+}
