d8e2051875b77ba6945367f09214f0e0deeb8092
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-30 21:03:36 +0000

    Update archive 79

---
+++ trunk/Archive2/79/9f1afc1b0a1bd7/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>           // std::cout
+#include <thread>             // std::thread
+#include <mutex>              // std::mutex, std::unique_lock
+#include <condition_variable> // std::condition_variable
+
+
+std::mutex mtx;
+std::condition_variable cv;
+bool started = false;
+
+void print_id(int id)
+{
+      // lock mutex
+	std::unique_lock<std::mutex> lck(mtx);
+	std::cout << id << " ready\n";
+	while( ! started ) {
+		std::cout << id << " waiting\n";
+		  // unlock mutex and wait for notification
+		cv.wait(lck);
+		  // after getting notification try to lock mutex
+	}
+	std::cout << id << " finished\n";
+	  // unlock mutex on exit (destroy unique_lock)
+}
+
+void trigger_start()
+{
+	  // lock mutex
+	std::unique_lock<std::mutex> lck(mtx);
+	std::cout << "10 threads ready to race...  Go!\n";
+	started = true;
+	  // send notification to all subscribers and...
+	cv.notify_all();  // note that sending notification doesn't unlock mutex
+	  // ...exit function unlocking mutex
+}
+
+int main()
+{
+	std::thread threads[10];
+	  // spawn 10 threads:
+	for( int i = 0 ; i < 10 ; ++i )
+		threads[i] = std::thread(print_id, i);
+
+	trigger_start();  // send notification to all threads
+
+	for( auto& th : threads )
+		th.join();
+
+	return 0;
+}
