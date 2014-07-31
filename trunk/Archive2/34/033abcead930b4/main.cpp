eba66dcbf45cd47fb41b37f7cdd5b1e61e044e2a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-31 05:54:27 +0000

    Update archive 34

---
+++ trunk/Archive2/34/033abcead930b4/main.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+
+int main()
+{
+
+// These values dont really matter, just stopping an uninitialized variable warning.
+unsigned char *m_start = static_cast<unsigned char*>(0x243);
+unsigned char *m_end = static_cast<unsigned char*>(0x24F);
+size_t pos = 10;
+if (m_start + pos >= m_end) // This gives the error
+{
+
+}
+
+}
