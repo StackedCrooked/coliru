954a70f713e3fe2ff7ec3b3368c68b8ba2a46e7d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-31 15:08:30 +0000

    Update archive be

---
+++ Archive2/be/ba2f67147b2aae/cmd.sh
@@ -0,0 +1,2 @@
+echo g++/libstdc++ && g++ -std=c++11 -O2 -Wall -pedantic-errors main.cpp && ./a.out
+echo -e '\nclang++/libc++' && clang++ -std=c++11 -stdlib=libc++ -O2 -Wall -pedantic-errors main.cpp -lsupc++ && ./a.out
