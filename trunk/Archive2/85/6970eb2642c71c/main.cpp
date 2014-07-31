70f4d77010ce696d72ba4cf6ccb880f030dc144c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-31 02:45:23 +0000

    Update archive 85

---
+++ trunk/Archive2/85/6970eb2642c71c/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <memory>
+
+namespace has {
+  namespace details {
+    struct _ { };
+    template<typename T> struct _ make_unique (...);
+  }
+
+  namespace hack {
+    using namespace details;
+    using namespace std;
+  }
+
+  template<typename = void>
+  class _make_unique {
+    private:
+      template<typename T = int>
+      static constexpr bool test (decltype ((hack::make_unique<int> (T {}) = details::_ {}), 0)) { return false; }
+
+      template<typename T = int>
+      static constexpr bool test (...) { return true; }
+
+    public:
+      static constexpr bool value = test<> (0);
+  };
+
+  using make_unique = _make_unique<>;
+}
+
+int
+main (int argc, char *argv[])
+{
+  static_assert (has::make_unique::value, "std::make_unique is not present!");
+  
+  std::cout << "SUCCESS";
+}
