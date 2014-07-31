0d8ecdf596a5cba1c5c9fb190326a1d2eb7d5cb4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-31 16:00:31 +0000

    Update archive 2c

---
+++ trunk/Archive2/2c/9c795013626a63/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <typeinfo>
+
+template <unsigned int N, template <class...> class ClassName, class ... InsideClass> struct GetNthTypeOfNested;
+template<unsigned int N, template<class...> class ClassName, class FirstOfInsideClass, class ... RestOfInsideClass>
+struct GetNthTypeOfNested<N, ClassName, FirstOfInsideClass, RestOfInsideClass...>:GetNthTypeOfNested<N - 1, ClassName, RestOfInsideClass...>{};
+
+template<template<class...> class ClassName, 
+         class FirstOfInsideClass,                              // <- change here
+         class ... InsideClass>
+struct GetNthTypeOfNested<0, 
+                          ClassName, 
+                          FirstOfInsideClass,                   // <- and here
+                          InsideClass...>
+{
+    typedef ClassName<FirstOfInsideClass, InsideClass...> type; // <- and here
+};
+
+
+template<typename ... Args> struct Typer;
+template<>
+struct Typer<>{
+
+};
+template<typename First,typename ... Args>
+struct Typer<First,Args...>:Typer<Args...>{
+    template<unsigned int N>
+    void get(){
+        std::cout << typeid(typename GetNthTypeOfNested<N, Typer, First, Args...>::type).name();
+//Typer undefined here i cant use Typer but i can use Typer outside of function
+    }
+};
+    
+    
+
+int main ()
+{
+  Typer<int, float, double> f; f.get<1>();
+}
