22556e78dc664adbc2fc17f569fec5e9cbc72795
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-07-31 19:15:12 +0000

    Update archive 16

---
+++ Archive2/16/ea319a48a88917/main.cpp
@@ -0,0 +1,10 @@
+class Foo {
+  void bar() {
+     /* BEGIN CODE */
+     /* here
+        is some significant text
+     */
+     /* END CODE */
+     String code = SourceUtils::getCode(/*what to pass here!?*/);
+  }
+}
