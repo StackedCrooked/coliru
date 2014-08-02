284bf550ea97da3214b09e974dda2e345bf9ba6c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-02 02:32:34 +0000

    Update archive 13

---
+++ Archive2/13/c6a026e0464e1a/main.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <iostream>
+ 
+void print(std::string::size_type n, std::string const &s)
+{
+    if (n == std::string::npos) {
+        std::cout << "not found\n";
+    } else {
+        std::cout << "found: \"" << s.substr(n) << "\" at " << n << '\n';
+    }
+}
+ 
+int main()
+{
+    std::string::size_type n;
+    std::string const s = "This is a string";
+ 
+    // search backwards from end of string
+    n = s.rfind("is");
+    print(n, s);
+    // search backwards from position 4
+    n = s.rfind("is", 4);
+    print(n, s);
+    // find a single character
+    n = s.rfind('s');
+    print(n, s);
+    // find a single character
+    n = s.rfind('q');
+    print(n, s);
+}
