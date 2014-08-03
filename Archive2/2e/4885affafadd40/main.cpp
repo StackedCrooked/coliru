db1bd14c0bc03f53f88fe1e91501ce2d182cebec
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-03 14:11:12 +0000

    Update archive 2e

---
+++ Archive2/2e/4885affafadd40/main.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+
+#define stringify_macro(s) stringify(s)
+#define stringify(s) #s
+
+#define FILELINE() __FILE__ "|" stringify_macro(__LINE__)
+
+#define PRAGMA(x) _Pragma(#x)
+#define PRAGMA_MESSAGE(x) PRAGMA(message(x))
+
+#define TEMPLATE_CLASS(name) PRAGMA_MESSAGE("class|" stringify(name) "|" FILELINE())
+#define TEMPLATE_FUNCTION(name) PRAGMA_MESSAGE("function|" stringify(name) "|" FILELINE())
+
+
+template <typename T>
+struct Foo {
+    TEMPLATE_CLASS(Enter_Foo)
+    template <typename U>
+    struct Bar {
+        TEMPLATE_CLASS(Enter_Bar)
+        
+        TEMPLATE_CLASS(Exit_Bar)
+    };
+   TEMPLATE_CLASS(Exit_Foo)
+};
+
+
+int main() {
+    TEMPLATE_FUNCTION(main)
+    
+    Foo<int> foo_i;
+    Foo<float> foo_f;
+
+    return 0;
+}
