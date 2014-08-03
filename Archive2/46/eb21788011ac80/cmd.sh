cb14363aa79fb7d0558aab831a6b61e6494496dc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-03 12:14:16 +0000

    Update archive 46

---
+++ Archive2/46/eb21788011ac80/cmd.sh
@@ -0,0 +1,2 @@
+set -x; g++-4.9 -std=c++1y -O0 -Wall -pedantic -pthread main.cpp && ./a.out
+clang++ -std=c++1y -O0 -Wall -pedantic -pthread main.cpp  && ./a.out
---
+++ Archive2/46/eb21788011ac80/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main()
+{
+    int a[50000000];        
+    std::cout << a[500];    
+}
