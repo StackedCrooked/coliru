e53ba5b3c1f7d9072a10ae2fb6a2d4b0e4658d38
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-04 14:47:29 +0000

    Update archive a5

---
+++ trunk/Archive2/a5/b72ed208cfdb59/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+template<int cl, int dim>
+struct g_impl
+{
+  int g(int x) { return cl + 1 * x;}    
+};
+
+template<int cl>
+struct g_impl<cl, 1>
+{
+  int g(int x) { return cl + 3 * x; }    
+};
+
+template <int cl, int dim>
+class impl_prototype 
+:
+    public g_impl<cl, dim>
+{
+public:
+  impl_prototype() {}
+
+  int f(int x) { return cl + 2 * this->g(x); } // this-> to enable lookup from dependent base
+};
+
+int main ()
+{
+  impl_prototype<0, 0> test_0;
+  impl_prototype<0, 1> test_1;
+
+
+  cout << test_0.f(5) << " " << test_0.g(5) << std::endl;
+  cout << test_1.f(5) << " " << test_1.g(5) << std::endl;
+
+
+  return 0;
+}
