f94b7e449ec63c9b9a154d0409dfd90122c3d912
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-04 04:37:43 +0000

    Update archive ba

---
+++ trunk/Archive2/ba/ded450b659b9c2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int n;
+   
+    cout<<"ingrese numero"<<endl;
+    cin>>n;
+   
+   while(n<1000){
+       
+       n=n*2;
+       cout<<n<<endl;
+       
+   }
+   
+   return 0;
+}
