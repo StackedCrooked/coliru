d41af7b6a9b0f7118dc5e24760c94a6964f6292a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-04 04:26:51 +0000

    Update archive e9

---
+++ Archive2/e9/e82be9a037ef4c/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <utility>
+
+struct comma_guard
+{
+    template<class T>
+    const comma_guard& operator,(T&&) const
+    {
+        std::cout << "111" << std::endl;
+        return *this;
+    }
+};
+
+struct foo {};
+template<class T> T operator,(T x, foo)
+{
+    std::cout << "222" << std::endl;
+    return x;
+}
+
+int main()
+{
+    (comma_guard().operator,(foo()));
+}
