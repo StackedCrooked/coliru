4fdba5bebc6d5f30e93d54224e8bde9ca250b77c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-05 19:46:17 +0000

    Update archive fb

---
+++ trunk/Archive2/fb/f5916b4fd26ac5/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <thread>
+#include <atomic>
+#include <chrono>
+
+struct foo
+{
+    void updateTimer()
+    {
+        while(running_) {
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            std::cout << "Hello" << std::endl;
+        }
+    }
+    
+    std::atomic_bool running_{true};
+    std::thread t_{&foo::updateTimer, this};
+    ~foo()
+    {
+        running_ = false;
+        t_.join();
+        std::cout << "Thread stopped\n";
+    }
+};
+
+int main()
+{
+    {
+        foo f;
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+    }
+    std::cout << "End\n";
+}
