d1348e3f24064282db139e7adebc3ed8f6bc9a53
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-06 17:30:58 +0000

    Update archive a6

---
+++ trunk/Archive2/a6/b04c6d094b3d4b/main.cpp
@@ -0,0 +1,67 @@
+#include <boost/config/warning_disable.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/spirit/include/phoenix_stl.hpp>
+#include <boost/spirit/include/phoenix.hpp>
+#include <boost/spirit/include/phoenix_statement.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/spirit/repository/include/qi_iter_pos.hpp>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+namespace client
+{
+    namespace qi = boost::spirit::qi;
+    namespace ascii = boost::spirit::ascii;
+    namespace phoenix = boost::phoenix;
+
+    struct double_w_pos
+    {
+        double_w_pos(double d, std::string::iterator p) : d(d), pos(p) {}
+        double d;
+        std::string::iterator pos;
+    };
+
+    
+    template <typename Iterator>
+    bool parse_numbers(Iterator first, Iterator last, std::vector<double_w_pos>& v)
+    {
+        using qi::double_;
+        using qi::phrase_parse;
+        using qi::_1;
+        using ascii::space;
+        using phoenix::at_c;
+        using phoenix::push_back;
+        using phoenix::begin;
+        using boost::spirit::repository::qi::iter_pos;
+        
+        bool r = phrase_parse(first, last,
+            (
+               (iter_pos >> qi::double_)[push_back(phoenix::ref(v), phoenix::construct<double_w_pos>(qi::_2,  qi::_1))] % ","
+            )
+            ,
+            space);
+
+        if (first != last)
+            return false;
+        return r;
+    }
+}
+
+int main()
+{
+    std::vector<client::double_w_pos> v;
+    std::string str = "2.0, 3.45, 5.67, 2.08";
+
+    client::parse_numbers(str.begin(), str.end(), v);
+    
+    for(auto i : v)
+        std::cout << i.d << " at " << std::distance(str.begin(), i.pos) << ", ";
+    std::cout << std::endl;
+    
+    return 0;
+}
