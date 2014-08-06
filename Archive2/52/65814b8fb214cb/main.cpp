1759eda248b530ca43193f08508c71b2664a2cfc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-06 05:16:57 +0000

    Update archive 52

---
+++ Archive2/52/65814b8fb214cb/main.cpp
@@ -0,0 +1,47 @@
+template <typename T>
+struct through {
+    typedef T type;
+};
+
+template <typename... T>
+struct id {};
+
+template<class... Args, class R>
+auto resolve( id<Args...>, R (*fun_ptr)( typename through<Args>::type... ) ) -> R( *)( typename through<Args>::type... ) {
+    return fun_ptr;
+}
+
+template<class... Args, class R>
+auto resolve( R (*fun_ptr)( Args... ) ) -> R( *)( Args... ) {
+	return fun_ptr;
+}
+
+template<class Sig>
+Sig* resolve( Sig* fun_ptr ) {
+	return fun_ptr;
+}
+
+int overloaded(int x) {
+    return 3;
+}
+
+int overloaded(int x, int y) {
+    return 7;
+}
+
+int overloaded(int x, int y, int z) {
+    return 11;
+}
+
+int main() {
+    // KABOOM
+    auto o1 = resolve( id<int>{}, overloaded);
+    // OH GOD EXPLODIES
+    //auto o2 = resolve<int, int>(overloaded);
+    
+    // Example to show how it's supposed to work: specifying 3-arguments produces 
+    // the right pointer type
+    //auto o3 = resolve<int, int, int>(overloaded);
+    
+    //struct {} _ = decltype(o3)();
+}
