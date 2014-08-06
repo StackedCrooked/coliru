5b86da61cb06468abe7679898d4d13996ca1549b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-06 15:18:54 +0000

    Update archive 61

---
+++ trunk/Archive2/61/fa43b418628213/main.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+int main() {
+    using Map = std::unordered_multimap<int, int>;
+    auto m = Map { {1, 1}, {1, 2}, {1, 3}, {2, 4}, {2, 5}, {3, 6} };
+    
+    std::vector<int> v;
+    v.reserve(m.size());
+    
+    std::transform(begin(m), end(m), std::back_inserter(v), [](auto const& elem) {
+        return elem.second;    
+    });
+
+    for (auto const& elem : v)
+        std::cout << elem << "\n";
+}
