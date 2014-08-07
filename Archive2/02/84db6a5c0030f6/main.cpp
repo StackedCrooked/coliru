eda561cc12b81010bebb44dc15ce7e38e074887a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-07 17:30:19 +0000

    Update archive 02

---
+++ Archive2/02/84db6a5c0030f6/main.cpp
@@ -0,0 +1,25 @@
+#include <cstdlib>
+#include <iostream>
+#include <new>
+
+struct S {
+    void *operator new(std::size_t) {
+        std::cout << "My new\n";
+        return ::new S;
+    }
+    
+    void operator delete(void *p, std::size_t) {
+        std::cout << "My delete\n";
+        ::delete static_cast<S *>(p);
+    }
+};
+
+int main() {
+    std::cout << "new:\n";
+    auto p1 = new S;
+    delete p1;
+    
+    std::cout << "::new:\n";
+    auto p2 = ::new S;
+    ::delete p2;
+}
