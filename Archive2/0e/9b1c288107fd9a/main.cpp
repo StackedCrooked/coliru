b8ff0fbaec8f812d01dd4c2baba775fdf10621aa
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-08 14:39:43 +0000

    Update archive 0e

---
+++ Archive2/0e/9b1c288107fd9a/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+enum {
+    VAL0,
+    VAL1
+};
+
+int main(int argc, char **argv)
+{   
+    cout << "VAL0: " << VAL0 << endl;
+    cout << "VAL1: " << VAL1 << endl;
+    
+    return 0;
+}
