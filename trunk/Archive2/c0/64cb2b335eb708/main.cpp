8e1845e8647a0de568b673602b56ef04d6bc3453
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-08 16:36:45 +0000

    Update archive c0

---
+++ trunk/Archive2/c0/64cb2b335eb708/main.cpp
@@ -0,0 +1,25 @@
+#include <stdint.h>
+
+
+typedef uint32_t sum_t;
+typedef uint8_t byte_t;
+
+
+sum_t by_val(byte_t* b, byte_t* e)
+{
+    uint32_t sum = 0;
+    for (byte_t* i = b; i != e; ++i)
+    {
+        sum += *i;
+    }
+    return sum;
+}
+
+
+void by_ref(byte_t* b, byte_t* e, sum_t& sum)
+{
+    for (byte_t* i = b; i != e; ++i)
+    {
+        sum += *i;
+    }
+}
