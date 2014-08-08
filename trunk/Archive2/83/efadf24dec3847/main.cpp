e6c2714633d8f70439005f898bb2861fbeaeb241
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-08 17:34:28 +0000

    Update archive 83

---
+++ trunk/Archive2/83/efadf24dec3847/main.cpp
@@ -0,0 +1,23 @@
+#include <stdint.h>
+
+
+typedef uint32_t sum_type;
+
+
+sum_type by_val(char* b, char* e, sum_type sum)
+{
+    for (char* i = b; i != e; ++i)
+    {
+        sum += *i;
+    }
+    return sum;
+}
+
+
+void by_ref(char* b, char* e, sum_type& sum)
+{
+    for (char* i = b; i != e; ++i)
+    {
+        sum += *i;
+    }
+}
