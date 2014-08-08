6afe31c7e09fd4d7bd3f41afc2ad69d8a2a2d493
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-08 17:04:44 +0000

    Update archive 2e

---
+++ trunk/Archive2/2e/a30dd9857ed843/main.cpp
@@ -0,0 +1,15 @@
+#include <tuple>
+#include <vector>
+#include <iostream>
+
+auto func()
+{
+    std::vector<int> a;
+    //anjam amalaiat rooye a
+    return a;
+}
+
+int main()
+{
+    auto result=func();
+}
