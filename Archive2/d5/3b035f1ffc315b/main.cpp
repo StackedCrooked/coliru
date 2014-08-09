656218642116638452734f55d3e2a2b18309668a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-09 03:42:59 +0000

    Update archive d5

---
+++ Archive2/d5/3b035f1ffc315b/main.cpp
@@ -0,0 +1,23 @@
+template<typename T> struct tuple_size {};
+template<typename T> struct tuple_size<T const>: tuple_size<T> {};
+
+template<bool B> struct bool_type {
+    static constexpr bool value = B;
+};
+using true_type  = bool_type<true>;
+using false_type = bool_type<false>;
+
+template<typename T, typename = void>
+struct query: false_type {};
+template<typename T>
+struct query<T, typename tuple_size<T>::type>
+: true_type {};
+
+int main()
+{
+    static_assert( !query<int>::value, "" );
+    static_assert( !query<int const>::value, "" );
+
+    // error: invalid use of incomplete type 'struct tuple_size<void()>'
+    static_assert( !query<void()>::value, "" );
+}
