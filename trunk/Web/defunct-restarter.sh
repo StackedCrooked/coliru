a13a145fff37bcc907eed72237ed06c2ae447349
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-09 02:54:14 +0000

    Kill sandbox.sh processes if there are 3 or more.

--- trunk/Web/defunct-restarter.sh
+++ trunk/Web/defunct-restarter.sh
@@ -2,13 +2,30 @@
 
 source logger.source
 
+
+get_sandbox_pids() {
+    ps -eopid,args | grep 'sandbox\.sh' | grep -v grep | awk '{print $1}' | uniq
+}
+
 while true ; do
+    sandbox_pids="$(get_sandbox_pids)"
+    echo "sandbox_pids: $sandbox_pids"
+    num_sandboxes="$(echo "$sandbox_pids" | wc -l)"
+    while [ "$num_sandboxes" -gt "2" ] ; do 
+        echo "There are $num_sandboxes sandbox processes. Kill the oldest."
+        pid_to_kill="$(echo "$sandbox_pids" | head -n1)"
+        [ "$pid_to_kill" == "" ] && break
+        echo "Killing $pid_to_kill"
+        kill "$pid_to_kill"
+        sandbox_pids="$(get_sandbox_pids)"
+        num_sandboxes="$(echo "$sandbox_pids" | wc -l)"
+    done
+
+    # Count number of defunct processes
     count="$(ps -ef | grep 'sandbox\|2002\|2001' | wc -l)"
-    if [ $count -gt 50 ] ; then
+    if [ $count -gt 100 ] ; then
         echo "There are $count processes. This is suspiciously high. I will reboot the webserver in 5 minutes."
         { sleep 300 ; reboot ; } & disown
-    else
-        echo "There are $count processes. This may still grow."
     fi
-    sleep 120
+    sleep 10
 done
