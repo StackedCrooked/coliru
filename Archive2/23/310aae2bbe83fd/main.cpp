063f084ca0f17e583753a3f8c64c6d640a3d6b31
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-09 23:59:03 +0000

    Update archive 23

---
+++ Archive2/23/310aae2bbe83fd/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <type_traits>
+using namespace std;
+
+class Base{};
+class Child :public Base{};
+class Child2 :public Child{};
+
+class A{};
+
+
+template<class T,
+     class DummyType=
+		 class  std::enable_if<
+		 std::is_base_of<Base,typename T>::value,std::true_type>::type>
+class MyTemplate {
+};
+int main()
+{
+	MyTemplate<Child> a;//ok
+	MyTemplate<Child2> b;//ok
+
+	//MyTemplate<int> c; compile error
+	//MyTemplate<A> d; compile error
+}
