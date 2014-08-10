36ee13dd0637d408c50d8938c8f91fa7eed7e461
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-10 19:08:18 +0000

    Update archive 39

---
+++ Archive2/39/0cf29d515e37b2/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+// Declara¸c˜ao de vari´aveis
+int a=10;
+int b=20;
+int aux=b;
+// Desenvolvimento da L´ogica da L´ogica do Programa
+b=a;
+a=aux;
+printf("O valor de C ´e: %d\n",c);
+getch();
+// Retorno da fun¸c˜ao ( inteiro)
+return 0;
+}
