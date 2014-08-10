164a77948f4d68415dcab3e17490e68869ede0e6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-10 18:59:05 +0000

    Update archive 19

---
+++ trunk/Archive2/19/9b970524897679/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template < typename A, typename B > struct wrapper_t
+{
+     const A& first ;
+     const B& second ;
+};
+
+template < typename A, typename B >
+wrapper_t<A,B> wrap( const A& first, const B& second ) { return { first, second } ; }
+
+template < typename SEQA, typename SEQB >
+std::ostream& operator<< ( std::ostream& stm, const wrapper_t<SEQA,SEQB>& wrapper )
+{
+    for( std::size_t i = 0 ; i < wrapper.first.size() ; ++i )
+        stm << wrapper.first[i] << ", " << wrapper.second.at(i) << '\n' ;
+    return stm ;
+}
+
+int main()
+{
+    const std::vector<std::string> names { "felix", "fido", "silver", "daffy", "button" } ;
+    const std::vector<double> ages { 2.3, 1.8, 3.2, 2.1, 0.7 } ;
+
+    std::cout << wrap( names, ages ) << '\n' ;
+}
