d76ea58d61c8b0344b46b48f3a5ff3b8a8cd1470
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-14 09:07:28 +0000

    Update archive 3e

---
+++ trunk/Archive2/3e/0ea812243764bd/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+std::vector< std::string > split_csv_remove_quotes( const std::string& line )
+{
+    std::vector< std::string > result ;
+
+    std::istringstream stm( line ) ;
+    std::string part ;
+    while( std::getline( stm, part, '"' ) )
+    {
+        std::stringstream sub_stm( part );
+        std::string token ;
+        while( std::getline( sub_stm, token, ',' ) ) result.push_back(token) ;
+        if( std::getline( stm, part, '"' ) ) result.push_back(part) ; // the last token
+    }
+    return result ;
+}
+
+int main()
+{
+    const std::string line = "1, 0, 3, \"Braund, Mr. Owen Harris\", male, 22, 1, 0, A / 5 21171, 7.25, , S";
+    std::cout << line << '\n' ;
+
+    for( const auto& tok : split_csv_remove_quotes(line) ) std::cout << tok << '\n' ;
+}
