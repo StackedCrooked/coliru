6d0403f4b14901ca4e5e0f755c16312dd86e7e26
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-14 06:38:13 +0000

    Update archive 6f

---
+++ Archive2/6f/2e761347bc8782/main.cpp
@@ -0,0 +1,160 @@
+#include <iostream>
+#include <string>
+
+template <class T>
+class Stack
+{
+    public:
+    Stack();
+    Stack( const Stack& );
+    ~Stack();
+    Stack& operator=( const Stack& );
+    int size() const;     // returns number of elements
+    bool empty() const;   // returns true iff this is empty
+    T& top() const;       // returns the top element
+    void push( const T& );  // inserts given element on top
+    void pop();           // removes element from top
+
+    T& bottom();
+
+
+    protected:
+    class Node
+    {
+        public:
+        Node( const T& data, Node* next = 0 ) : _data( data ), _next( next ) {}
+        T _data;
+        Node* _next;
+    };
+    Node* _top;
+    int _size;
+};
+
+template <class T>
+Stack<T>::Stack() : _top( 0 ), _size( 0 )
+{}
+
+template <class T>
+Stack<T>::Stack( const Stack& s ) : _top( 0 ), _size( s._size )
+{
+    if( _size == 0 ) return;
+    Node* pp = 0;
+    for( Node* p = s._top; p; p = p->_next )
+        if( p == s._top ) pp = _top = new Node( p->_data );
+        else pp = pp->_next = new Node( p->_data );
+}
+
+template <class T>
+Stack<T>::~Stack()
+{
+    while( _top )
+    {
+        Node* p = _top;
+        _top = _top->_next;
+        delete p;
+    }
+}
+
+template <class T>
+int Stack<T>::size() const
+{
+    return _size;
+}
+
+template <class T>
+bool Stack<T>::empty() const
+{
+    return _size == 0;
+}
+
+template <class T>
+T& Stack<T>::top() const
+{
+    return _top->_data;
+}
+
+template <class T>
+void Stack<T>::pop()
+{
+    Node* p = _top;
+    _top = _top->_next;
+    delete p;
+    --_size;
+}
+
+template <class T>
+void Stack<T>::push( const T& x )
+{
+    if( _size == 0 ) _top = new Node( x );
+    else _top = new Node( x, _top );
+    ++_size;
+}
+
+
+class stack : public Stack<char>
+{
+    public:
+    char& bottom();
+
+};
+template<class T>
+T& Stack<T>::bottom()
+{
+
+    while( Stack<T>::size() != 1 )
+    {
+        Stack<T>::top();
+        Stack<T>::pop();
+    }
+
+    return _top->_data;
+
+}
+
+#include <iostream>
+#include <string.h>
+
+
+template <class T>
+void print( Stack<T> );
+
+int main()
+{
+    Stack<std::string> s;
+
+    s.push( "first" );     print( s );
+
+    s.push( "second" )    ;     print( s );
+
+    s.push( "third" )   ;     print( s );
+
+    s.pop();     print( s );
+
+    s.pop();        print( s );
+
+    std::cout << std::endl;
+
+
+}
+
+
+template<class T>
+void print( Stack<T> s )
+{
+    std::cout << "size= " << s.size() << std::endl;
+    // std::cout<<"bottom= "<<s.bottom()<<std::endl;
+
+    while( s.size()>0 )
+    {
+        std::cout << s.top() << " (" << s.top() << ", ";
+        s.pop();
+
+        while( !s.empty() )
+        {
+            std::cout << s.top() << ", ";
+            s.pop();
+        }
+
+        std::cout << " )" << std::endl;
+    }
+}
