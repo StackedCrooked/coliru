bb1ed0142cd8c1ee5c62564dd26ec0a3e49350fd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-14 03:51:23 +0000

    Update archive 1d

---
+++ trunk/Archive2/1d/904297f47fd6a0/main.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+#include <limits>
+#include <vector>
+
+int find_max(int i) {
+    return i;
+}
+
+template <typename T>
+int find_max(const std::vector<T>& v) {
+    return std::accumulate(std::begin(v), std::end(v), std::numeric_limits<int>::min(),
+        [] (const int prev, const T& v) {
+            int m = find_max(v);
+            return (m > prev) ? m : prev;
+        });
+}
+
+int main() {
+    std::vector<int> v1{1, 2, 3};
+    std::vector<std::vector<int>> v2{{1, 2, 3}, {4, 5, 6}};
+    std::vector<std::vector<std::vector<int>>> v3{{{1, 2, 3}, {4, 5, 6}}, {{7, 8, 9}, {10, 11, 12}}};
+
+    std::cout << find_max(v1) << std::endl;
+    std::cout << find_max(v2) << std::endl;
+    std::cout << find_max(v3) << std::endl;
+}
