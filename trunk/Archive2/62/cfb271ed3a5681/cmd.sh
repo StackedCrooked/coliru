9956b7411d732b98758c854532a25a9f54920380
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-14 18:03:57 +0000

    Update archive 62

---
+++ trunk/Archive2/62/cfb271ed3a5681/cmd.sh
@@ -0,0 +1,1 @@
+g++ -g -O0 -Wall -Wextra -pedantic -Wno-unused-variable -Wno-unused-parameter -fno-elide-constructors main.cpp && ./a.out
---
+++ trunk/Archive2/62/cfb271ed3a5681/main.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+#include <bitset>
+
+int main() {
+    typedef std::vector<std::bitset<128>> BITSET_VEC;
+    BITSET_VEC v;
+    return 0;
+}
