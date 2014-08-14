60c3cf10c2df1b4654dd2d265d73ee05a2a877b6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-14 20:46:38 +0000

    Update archive d4

---
+++ Archive2/d4/72d48f40b7fa4c/main.cpp
@@ -0,0 +1,16 @@
+class Main {
+    TextIO io = new TextIO();
+
+    void main() {
+        int i = 0;
+        int j = 0;
+        for (; i < 4; i = i + 1) {
+            int t = i * i;
+            io.putInt(t);
+            x = i;
+        }
+
+        return;
+    }
+    int x;
+}
