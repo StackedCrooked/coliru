75547a60fc11d70a2df08d060d1a35b4ccb5c46a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-16 16:23:51 +0000

    Update archive 6d

---
+++ Archive2/6d/c647e14d96b5c9/cmd.sh
@@ -0,0 +1,1 @@
+clang++ -Wall -Wextra -pedantic -std=c++1y -pthread -o a.out main.cpp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 && ./a.out
---
+++ Archive2/6d/c647e14d96b5c9/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <memory>
+
+namespace xtd {
+    using std::shared_ptr;
+
+    template<class T, class... Args>
+    auto make_shared(Args&&... args) -> std::enable_if_t<std::is_constructible<T, Args...>::value, shared_ptr<T>> {
+        return shared_ptr<T>(new T(std::forward<Args>(args)...));
+    }
+
+    template<class T, class... Args>
+    auto make_shared(Args&&... args) -> std::enable_if_t<!std::is_constructible<T, Args...>::value, shared_ptr<T>> {
+        return shared_ptr<T>(new T{std::forward<Args>(args)...});
+    }
+}
+
+template<typename T>
+struct Node {
+    T data;
+    std::shared_ptr<Node> next;
+};
+
+int main() {
+    auto ptr = xtd::make_shared<Node<int>>(42, xtd::make_shared<Node<int>>(43, nullptr));
+    std::cout << ptr->data << ' ' << ptr->next->data << '\n';
+}
