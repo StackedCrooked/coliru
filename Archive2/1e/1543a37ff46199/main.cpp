0ccf1d0937d9f92d2ec15f7ddec3006a229f22f6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-16 16:24:32 +0000

    Update archive 1e

---
+++ Archive2/1e/1543a37ff46199/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int doit(int x, ...) { return x; }
+template<class X, class Y> auto doit(X x, Y y) { return doit(x) + doit(y); }
+int main() {
+    std::cout << doit(1, &std::cout) << std::endl;
+}
