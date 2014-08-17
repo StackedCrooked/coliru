bd203c848bfc4a95954d6899568bdca58abc5626
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-17 16:08:46 +0000

    Update archive 55

---
+++ trunk/Archive2/55/9d3bca554517c6/main.cpp
@@ -0,0 +1,21 @@
+#include <regex>
+#include <iostream>
+int main()
+{
+    std::smatch matches;
+    std::string s = "y=a^b,z=(y+76),k=(z|p)";
+    std::regex  expr(R"((\w+)=\((\w+)([|&^+*-])(\w+)\))");
+
+    for(auto it = std::sregex_iterator(s.begin(), s.end(), expr);
+             it != std::sregex_iterator();
+           ++it)
+    {
+         std::cout << "Found a match: " << it->str() << "\n";
+         std::smatch m = *it;
+
+         std::cout << "prefix=[" << m.prefix() << "]\n";
+         for(std::size_t n = 0; n < m.size(); ++n)
+                 std::cout << "   m[" << n << "]=[" << m[n] << "]\n";
+         std::cout << "suffix=[" << m.suffix() << "]\n";
+    }
+}
