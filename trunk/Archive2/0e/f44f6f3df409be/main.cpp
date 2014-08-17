81b7e85b29ba8d1b9eeb9f56f0430813b84cec4c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-17 09:28:41 +0000

    Update archive 0e

---
+++ trunk/Archive2/0e/f44f6f3df409be/main.cpp
@@ -0,0 +1,34 @@
+
+struct Base
+{
+    virtual ~Base() = default;
+
+    virtual Base *f() const = 0;
+};
+
+struct A
+: virtual Base
+{
+    virtual A *f() const override = 0;
+};
+struct B
+: virtual Base
+{
+    virtual B *f() const override = 0;
+};
+
+struct Middle
+: virtual A
+, virtual B
+{
+};
+
+struct Derived
+: Middle
+{
+    virtual Derived *f() const override = 0;
+};
+
+int main()
+{
+}
