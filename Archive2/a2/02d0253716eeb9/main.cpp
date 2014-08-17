4542f1446bf2416b0239ff36c438a867444bbd9f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-17 16:13:54 +0000

    Update archive a2

---
+++ Archive2/a2/02d0253716eeb9/main.cpp
@@ -0,0 +1,14 @@
+// C program to demonstrate Ternary Search Tree (TST) insert, travese 
+// and search operations
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+ 
+// Driver program to test above functions
+int main()
+{
+    unordered_map<int, int> m;
+    cout << "hello" << endl; 
+    return 0;
+}
