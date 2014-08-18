6f1daead7c474190bb8b9362c9e07130989f6eb9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-18 19:06:34 +0000

    kill-all.sh only kills children of restart.sh.

--- Web/kill-all.sh
+++ Web/kill-all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for PGID in $(ps -eopgid,uid,comm | grep '200[12]\|defunct\|committer.sh' | awk '{print $1}' | sort | uniq) ; do
-    echo "Killing PGID: ${PGID}"
-    kill -9 -${PGID}
+for pid in $(ps -eopid,uid,comm,args | grep '200[12]\|cleanup-cache.sh|run.sh\|pgid_killer.sh\|repair-permissions.sh' | grep -v grep | awk '{print $1}' | sort | uniq) ; do
+    echo "Killing pid: ${pid}"
+    echo "kill -9 ${pid}"
+    kill -9 ${pid}
 done
