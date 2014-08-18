7181da999e786239e2279c97938e2b3b3c9bf77f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-18 04:22:37 +0000

    Update archive c1

---
+++ trunk/Archive2/c1/e4c2e109bd4ca0/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+ using namespace std;
+ 
+ 
+ int void main()
+ {
+  
+    
+     
+     int hours=0;
+     double rph=0;
+     double tax1=0;
+     double tax2=0;
+     double networth=0;
+     int input=input;
+     
+     cout<<"Enter No. of Hours Worked:"<<endl;
+     cin>>hours;
+     
+     
+     cout<<"Enter Rate per Hour:"<<endl;
+     cin>>rph;
+     
+     
+     cout<<"Enter Tax Acquired:"<<endl;
+     cin>>tax1;
+     
+     input= hours
+     input= rph
+     input= tax1
+     
+     tax2 = hours * rph - tax1;
+     networth= tax2;
+     
+     
+     cout<<"Your Toatal NetWorth is:"<<endl;
+     cin>>networth;
+     
+     
+     return 0;
+     
+ 
+     
+     
+}
