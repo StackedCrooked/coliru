551ffb052b79052a5841524ac9da9323776b963b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-19 06:18:51 +0000

    Update archive 61

---
+++ Archive2/61/4a11fd1fcde9a9/main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+    struct tm timeinfo;
+    memset(&timeinfo, 0, sizeof(timeinfo));
+    
+    while(fscanf(stdin, "%d%d%d%d%d%d",
+            &timeinfo.tm_year, &timeinfo.tm_mon, &timeinfo.tm_mday,
+            &timeinfo.tm_hour, &timeinfo.tm_min, &timeinfo.tm_sec) != EOF)
+    {
+        timeinfo.tm_year -= 1900;
+        timeinfo.tm_mon -= 1;
+        
+        fprintf(stdout, "%d %d %d %d %d %d %d\n",
+            timeinfo.tm_year, timeinfo.tm_mon, timeinfo.tm_mday,
+            timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec, timeinfo.tm_isdst);
+        fprintf(stdout, "%ld\n", mktime(&timeinfo));
+        fprintf(stdout, "%d %d %d %d %d %d %d\n\n",
+            timeinfo.tm_year, timeinfo.tm_mon, timeinfo.tm_mday,
+            timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec, timeinfo.tm_isdst);
+    }
+
+    return 0;
+}
