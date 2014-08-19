c346989dce3e347c05989a2e3877204aae980221
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-19 20:16:18 +0000

    Update archive 4d

---
+++ trunk/Archive2/4d/c1489cb3470690/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+struct A{
+    operator char() const{ return 'a'; }
+    operator int() const{ return 10; }
+};
+
+struct B {
+    void operator<< (int) { }
+    void operator<< (long) { }
+};
+
+int main()
+{
+    A a;
+    B b;
+    b << a;
+}
