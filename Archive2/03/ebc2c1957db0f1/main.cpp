98629a3ee33315e040be62dda3e61edd9ae4c45b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-19 15:38:33 +0000

    Update archive 03

---
+++ Archive2/03/ebc2c1957db0f1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct A { void foo(){std::cout << "foo";} };
+
+template <class T>
+void fun (T t) {t.fuzz();}
+
+template <class T>
+void fun (T t) {t.foo();}
+
+int main () {
+    A a;
+    fun(a);
+}
