f6b2f96d6f9f76ad839714ca5c6df5ca2acf8870
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-19 20:33:59 +0000

    Update archive 8b

---
+++ Archive2/8b/a28787636b4552/main.cpp
@@ -0,0 +1,67 @@
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using std::vector;
+using std::queue;
+using std::cout;
+using std::endl;
+
+struct Node
+{
+    Node(int r, int i)
+      : row(r)
+      , id(i)
+    {}
+
+    explicit Node(int i)
+      : id(i)
+      , row(0)
+    {}
+
+    Node()
+      : id(0)
+      , row(0)
+    {}
+
+    int nextLayer(queue<int>& queue, int& curRow)
+    {
+        int nextId = queue.front();
+        queue.pop();
+
+        for (int i = 0; i < children.size(); i++) {
+            if (children[i].id == nextId) {
+               if (children[i].row == 0)
+                  return children[i].nextLayer(queue, curRow);
+               else
+                  return children[i].row - 1;
+            }
+        }
+
+        if (queue.size() == 0) {
+            curRow++;
+            children.push_back(Node(curRow, nextId));
+            return curRow - 1;
+        }
+        else {
+            children.push_back(Node(nextId));
+            return children.end()->nextLayer(queue, curRow);
+        }
+    }
+
+    int id;
+    int row;
+    vector<Node> children;
+};
+
+int main()
+{
+    queue<int> test;
+    int row = 0;
+    test.push(1);
+    test.push(2);
+    test.push(3);
+
+    Node testNode;
+    cout << testNode.nextLayer(test, row) << endl;
+}
