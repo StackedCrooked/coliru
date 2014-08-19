3fbda69473b7e6da82964d9467d82fff32d5995b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-19 06:47:07 +0000

    Update archive c5

---
+++ Archive2/c5/ed2fd7a1b267e7/main.cpp
@@ -0,0 +1,30 @@
+template<class T, T value>
+struct set_on_exit {
+    set_on_exit(T& var) :var(&var) {}
+    ~set_on_exit() {*var = value;}
+    set_on_exit(const set_on_exit&rhs) = delete;
+    set_on_exit& operator=(const set_on_exit&rhs) = delete;
+private:
+    T* var;
+}
+
+template<class Functionoid>
+void call_once_even_if_except(Functionoid&& func)
+{
+    static std::mutex mutex;
+    std::unique_lock<std::mutex> lock(mutex, std::try_to_lock);
+    
+    //if can't grab the lock, must not be first.  return
+    if (!lock)
+        return
+    
+    static bool first = true;    
+    //the first thread to get here will have first=true
+    //it will set first==false, and no subsequent threads will enter the block
+    if (first) {
+        first = false;
+        
+        //only one thread will ever make it here.  
+        func();
+    }
+}
