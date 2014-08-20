1aaefd10415f2cbe97106a97767489523651d0ea
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-20 03:14:20 +0000

    Update archive d4

---
+++ trunk/Archive2/d4/828e32e58ac788/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <tuple>
+#include <vector>
+
+
+using namespace std;
+
+
+#include "a.cpp"
+ 
+
+int main()
+{
+    Commands::register_all();
+}
