0d3483500bbee4a941f8f02db78c0c6fbbbf958c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-20 22:11:31 +0000

    Split restart.sh into restart.sh and _restart.sh

--- trunk/Web/restart.sh
+++ trunk/Web/restart.sh
@@ -1,40 +1,9 @@
 #!/bin/bash
 
-source coliru_env.source
-source logger.source
- 
-if [ "$(whoami)" != "root" ] ; then
-    echo "$(basename $0) must be run with root permissions." 1>&2
-    exit 1
-fi
+# Kill previous instance of _restart.sh (if any)
+ps -eopid,comm | grep '_restart.sh' | grep -v grep | awk '{print $1}' | while read line;  do
+     echo "Killing previous instance $line"
+     kill -9 $line
+done
 
-
-cd $(dirname $0)
-
-
-# Kill any previously running instances
-./kill-all.sh >/dev/null 2>&1
-
-
-# Setup directories and set permissions
-rm -rf /tmp/coliru ; mkdir -p /tmp/coliru ; chown -R webserver:coliru /tmp/coliru
-rm -f /tmp/cleanup ; touch /tmp/cleanup ; chown webserver:coliru /tmp/cleanup
-chown -R webserver:coliru /tmp
-
-# Check/repair permissions
-./repair-permissions.sh & disown
-
-# Start the pgid killer (for killing timed out sandbox processes)
-./pgid_killer.sh & disown
-
-# Start cache-cleanup script
-./cleanup-cache.sh & disown
-
-# Disable network access
-iptables -A OUTPUT -m owner --uid-owner 2002 -j DROP
-
-# Start the webserver
-./run.sh & disown
-
-# Schedule a future reboot (because we crash all the time)
-setsid bash -c "sleep $((1 * 3600)) ; reboot" & disown
+./_restart.sh & disown
