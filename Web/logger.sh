bd141218629ff743648bd54754d6242d296d3b46
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-20 22:14:34 +0000

    logger.sh: use only one named pipe: stdout.

--- Web/logger.sh
+++ Web/logger.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
 cd "$(dirname $0)"
-rm -f stdout ; mkfifo stdout ; chown webserver:coliru stdout
-rm -f stderr ; mkfifo stderr ; chown webserver:coliru stderr
 
-{ while true ; do while read line ; do logger "stdout: $line" ; done <stdout ; logger "Broke out of stdout loop" ; sleep 10 ; done } & disown
-{ while true ; do while read line ; do logger "stderr: $line" ; done <stderr ; logger "Broke out of stderr loop" ; sleep 10 ; done } & disown
+log() {
+    echo "$1"
+    logger "$1"
+}
+
+log "$(basename $0) started"
+
+
+make_stdout() {
+    rm -f stdout
+    mkfifo stdout
+    chown webserver:coliru stdout
+}
+
+make_stdout
+
+while true ; do
+
+    # remake the stdout file if it no longer exists for some reason
+    [ -f stdout ] || make_stdout
+    
+    while read line ; do 
+        log "stdout: $line"
+    done <stdout
+    log "Broke out of stdout loop"
+    sleep 2
+done
