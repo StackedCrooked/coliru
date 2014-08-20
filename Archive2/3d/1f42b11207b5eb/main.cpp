fbaf6429e5cc2779ee5aee85cabae06ef27c1476
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-20 01:17:31 +0000

    Update archive 3d

---
+++ Archive2/3d/1f42b11207b5eb/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+
+ 
+int main()
+{
+    std::set<int> v { 3, 1, 4, 1, 5, 9 };
+ 
+    std::cout << "initially, v: ";
+    for (auto i : v) std::cout << i << ' ';
+    std::cout << '\n';
+ 
+    //std::make_heap(v.begin(), v.end());
+ 
+    //std::cout << "after make_heap, v: ";
+    //for (auto i : v) std::cout << i << ' ';
+    //std::cout << '\n';
+ 
+    //std::pop_heap(v.begin(), v.end());
+    //auto largest = v.back();
+    //v.pop_back();
+    auto largest = *(--v.end());
+    v.erase(--v.end());
+    std::cout << "largest element: " << largest << '\n';
+ 
+    std::cout << "after removing the largest element, v: ";
+    for (auto i : v) std::cout << i << ' ';
+    std::cout << '\n';
+}
