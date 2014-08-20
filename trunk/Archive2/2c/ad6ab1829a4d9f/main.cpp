3e36d0f5f02f373cb3623017c3d158087abf88a3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-20 21:06:30 +0000

    Update archive 2c

---
+++ trunk/Archive2/2c/ad6ab1829a4d9f/main.cpp
@@ -0,0 +1,15 @@
+
+class uncopyable {
+    
+   uncopyable(uncopyable const&) = delete;
+   uncopyable & operator =(uncopyable const&) = delete;
+};
+
+
+struct type : uncopyable {
+
+   void * ptr;
+};
+
+
+int main() {}
