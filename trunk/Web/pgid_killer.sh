6ff0361105a6ae5b871b0dd423fef1a64f8c1a6b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-20 23:07:33 +0000

    pgid_killer: don't log.

--- trunk/Web/pgid_killer.sh
+++ trunk/Web/pgid_killer.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-source logger.source
+
+# FIXME: This seems to block the loop
+#source logger.source
 
 rm -f .pgid_killer
 mkfifo .pgid_killer
 chmod a+w .pgid_killer
 while true ; do
     read line <.pgid_killer
+    echo "$(basename $0): request for killing pgid $line"
     [ "$line" != "" ] && {
         kill -9 -${line}
     }
