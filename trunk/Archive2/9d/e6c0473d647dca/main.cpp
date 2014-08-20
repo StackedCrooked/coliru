475bda0be779b9258c8646a8fdb4d87a8b9a0445
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-20 23:51:42 +0000

    Update archive 9d

---
+++ trunk/Archive2/9d/e6c0473d647dca/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <set>
+#include <string>
+
+// do not touch from here
+template<typename Set>
+std::set<Set> powerset(const Set& s, size_t n)  {
+    std::set<Set> result;
+    if(n > 0) {
+        std::set<Set> ps = powerset(s, n-1);
+        for(auto&& ss : ps) {
+            for(auto&& el : s) {
+                Set subset(ss);
+                subset.insert(el);
+                result.insert(subset);
+            }
+        }
+        result.insert(ps.begin(), ps.end());
+    }
+    else {
+        result.insert(Set());
+    }
+    return result;
+}
+
+template<typename Set>
+std::set<Set> powerset(const Set& s) {
+    return powerset(s, s.size());
+}
+
+int main() {
+    // this could be modified
+    std::set<std::string> letters = {
+        "Schedule A", "Schedule C", "Schedule D",
+        "Schedule E", "Schedule F"
+    };
+
+    // do not touch from here
+    auto&& power = powerset(letters);
+
+    for(auto&& set : power) {
+        if(set.empty()) {
+            continue;
+        }
+        
+        std::cout << "\t- ";
+        char comma[] = { '\0', ' ', '\0' };
+        for(auto&& elem : set) {
+            std::cout << comma << elem;
+            comma[0] = ',';
+        }
+        std::cout << '\n';
+    }
+}
