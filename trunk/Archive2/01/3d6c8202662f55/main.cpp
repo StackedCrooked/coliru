a0ab093542a245a4e3a9f7ea354f8415f587dc28
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-20 11:49:10 +0000

    Update archive 01

---
+++ trunk/Archive2/01/3d6c8202662f55/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+struct A
+{
+    virtual void foo(){ };
+    
+    A()
+    {
+        cout << "A()" << endl;
+    }
+};
+
+struct B : A
+{
+    virtual void bar() = 0;
+    
+    B()
+    {
+        cout << "B()" << endl;
+    }
+};
+
+struct C : B
+{
+    void bar(){ };
+    C() : B()
+    {
+        cout << "C()" << endl;
+    }
+};
+
+C c;
+
+int main()
+{
+    
+}
