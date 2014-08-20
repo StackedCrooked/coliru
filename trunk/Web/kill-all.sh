67bedfafd4791045ffa1da29db9b8c1373b0f505
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-20 22:13:53 +0000

    kill-all.sh also kills webserver and sandbox processes.

--- trunk/Web/kill-all.sh
+++ trunk/Web/kill-all.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-for pid in $(ps -eopid,uid,comm,args | grep '200[12]\|cleanup-cache.sh|run.sh\|pgid_killer.sh\|repair-permissions.sh' | grep -v grep | awk '{print $1}' | sort | uniq) ; do
+
+# kill webserver process
+pkill -u 2001
+
+# kill any sandbox processes
+pkill -u 2002
+
+for pid in $(ps -eopid,uid,comm,args | grep '200[12]\|cleanup-cache.sh|run.sh\|repair-permissions.sh' | grep -v grep | awk '{print $1}' | sort | uniq) ; do
     echo "Killing pid: ${pid}"
     echo "kill -9 ${pid}"
     kill -9 ${pid}
