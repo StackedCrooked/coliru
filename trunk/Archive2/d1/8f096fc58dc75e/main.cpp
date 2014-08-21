b0a729db8d915d5b0cc9787ac53810f4c4a75a7f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-21 12:34:02 +0000

    Update archive d1

---
+++ trunk/Archive2/d1/8f096fc58dc75e/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+template<typename T>
+class Singleton
+{
+    friend T;
+    Singleton() = default;
+    virtual ~Singleton() = default;
+
+public:
+
+    static auto getInstance()
+    -> T &
+    {
+        static T t;
+        return t;
+    }
+};
+
+class Test : public Singleton<Test>
+{
+    friend Singleton<Test>;
+    Test(){}
+};
+
+int main()
+{
+    Test &t = Test::getInstance();
+}
