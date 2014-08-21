c28e5b65519b74773acbdefad31f40a6c17235e4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-21 05:04:43 +0000

    Update archive cb

---
+++ Archive2/cb/173f47fe09849f/main.cpp
@@ -0,0 +1,32 @@
+#include <functional>
+#include <iostream>
+
+template <typename T>
+struct foo {
+    std::function<void(T&, int)> f;
+    
+    template <typename K, void (K::*method)(T&, int)>
+    void
+    set(K* instance) {
+        f = [=] (T& t, int i) { 
+            (instance->*method)(t, i); 
+        };
+    }
+};
+
+struct bar {
+    void fun(double& d, int i) {
+        std::cout << d << ' ' << i << '\n';
+    }
+};
+
+int
+main() {
+    foo<double> f;
+    bar b;
+    f.set<bar, &bar::fun>(&b);
+    
+    double d{3.14};
+    f.f(d, 5);
+}
+    
