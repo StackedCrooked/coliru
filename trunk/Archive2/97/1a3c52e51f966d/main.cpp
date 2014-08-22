47b074bc401f44cddfacac356397d43d71d8cf88
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-22 14:22:44 +0000

    Update archive 97

---
+++ trunk/Archive2/97/1a3c52e51f966d/main.cpp
@@ -0,0 +1,19 @@
+class Var
+{
+public:
+
+    operator int () const
+    { return 0; }
+
+    template <typename T>
+    operator T () const
+    { return T(); }
+
+};
+
+int main()
+{
+    Var v;
+    switch (v)
+    { }
+}
