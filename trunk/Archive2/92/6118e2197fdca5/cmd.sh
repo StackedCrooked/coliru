4ea7485e6ec9b76f9cc6eac28ad372f0df8a804e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-22 23:10:02 +0000

    Update archive 92

---
+++ trunk/Archive2/92/6118e2197fdca5/cmd.sh
@@ -0,0 +1,4 @@
+false && echo g++ && g++ -x c++ -std=c++1y -Wall -Wextra -pedantic main.cpp && ./a.out
+echo gcc && gcc -x c -std=c11 -Wall -Wextra -pedantic main.cpp && ./a.out
+false && echo clang++ && clang++ -x c++ -std=c++1y -Wall -Wextra -pedantic main.cpp && ./a.out
+echo clang && clang -x c -std=c11 -Wall -Wextra -pedantic main.cpp && ./a.out
