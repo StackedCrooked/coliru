1cdc676d27e09f3953d90235c20618eba68278ca
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-22 14:18:48 +0000

    Update archive 8c

---
+++ trunk/Archive2/8c/ad5921a0e416ee/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double ans = pow(2.0, 800.0);
+    printf("%.0f\n", ans);
+    printf("%.0f\n", ans + 1);
+    printf("%.0f\n", ans * 2);
+    return 0;
+}
