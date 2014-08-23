4a67628e234508c7d53b4895aa4cf90965398f68
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-23 03:03:28 +0000

    Update archive 15

---
+++ trunk/Archive2/15/07ff40432526c2/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+const std::size_t N = 10 ;
+using array_t = int[N] ;
+
+template < int N >
+void print_it( int (&a)[N], int from_pos = 0 )
+{
+    if( from_pos == N )
+    {
+        for( int v : a ) std::cout << v << ' ' ;
+        std::cout << '\n' ;
+    }
+
+    else
+    {
+        for( int i = 0 ; i < N ; ++i )
+        {
+            a[from_pos] = i ;
+            print_it( a, from_pos+1 ) ;
+        }
+    }
+}
+
+int main()
+{
+    int array[3] {} ;
+    print_it(array) ;
+}
