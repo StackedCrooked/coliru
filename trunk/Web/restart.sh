ee96c59d34786b14f2f61572b6b82b7da92894f4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-24 13:57:47 +0000

    Improve run.sh loop.

--- trunk/Web/restart.sh
+++ trunk/Web/restart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # Kill previous instance of _restart.sh (if any)
 ps -eopid,comm | grep '_restart.sh' | grep -v grep | awk '{print $1}' | while read line;  do
      echo "Killing previous instance $line"
