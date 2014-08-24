9e6b1bc9223368fb41154e2172ad8e79aaff4ece
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-24 15:19:42 +0000

    Add boot.sh.

---
+++ trunk/Web/boot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Start the logger
+./logger.sh & disown
+
+# Start the killer helper
+./pgid_killer.sh & disown
+
+# Start cache cleanup script
+./cleanup-cache.sh & disown
+
+# Repair permissions
+./repair-permissions.sh & disown
+
+# Start the webserver
+./restart.sh & disown
+
+{ sleep 3600 ; reboot ; } & disown
