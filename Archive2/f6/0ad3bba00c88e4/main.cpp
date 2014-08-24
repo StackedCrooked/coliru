342d037ebf2855cb49614f886460b92c97512297
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-24 05:28:27 +0000

    Update archive f6

---
+++ Archive2/f6/0ad3bba00c88e4/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+
+//Why does the following call the move constructor only if copy constructor has a const in it.
+
+
+class foo
+{
+public:
+    foo()
+	{std::cout << "Regular constructor \n";}
+
+	foo(const foo& a)                            //In abscence of const keyword the copy constructor is called
+	{std::cout << "Copy constructor \n";}
+
+	foo& operator=(const foo& a) 
+	{std::cout << "Copy Assignment operator \n";}
+	
+	foo(foo&& a)
+	{
+		std::cout << "Move constructor \n";
+	}
+
+	foo& operator=(foo&& a) 
+	{std::cout << "Move Assignment operator \n";}
+
+	int a;
+};
+
+
+foo myfunction()
+{
+	foo d;
+	d.a =120;
+	return d;
+}
+
+int main()
+{
+	foo a = myfunction();
+}
