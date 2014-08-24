e98b312d9d0cfff71da31e067586e7a7433a5b56
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-24 00:47:51 +0000

    Update archive 3c

---
+++ Archive2/3c/38c4d534e3a123/main.cpp
@@ -0,0 +1,17 @@
+template <int N> struct require_constexpr { };
+struct test { 
+  constexpr test() = default;
+  //constexpr test(const test &){ }
+  //constexpr test(const test &) = default;
+};
+
+template <typename T> constexpr int foo (T ) { return 0; }
+template <typename T> constexpr int bar (T obj) { return foo(require_constexpr< foo(obj) > { }); }
+
+int main ()
+{
+  constexpr auto const ar = test();
+  constexpr auto const baz = require_constexpr< bar(ar) >();
+
+  return 0;
+}
