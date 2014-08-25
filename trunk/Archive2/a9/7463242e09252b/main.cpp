3fb9f1c4b2f86f76107b77e86fbdec463b602e9c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-25 17:36:46 +0000

    Update archive a9

---
+++ trunk/Archive2/a9/7463242e09252b/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <typeinfo>
+
+struct dummy {};
+
+struct TArg1 {};
+struct TArg2 {};
+struct TArg3 {};
+struct TArg4 {};
+
+template<typename THead/*no tail*/> void print( THead &head )
+{
+    std::cout << typeid( THead ).name( ) << std::endl;
+}
+
+template<typename THead, typename... TTail> void print( THead &head, TTail&... tail )
+{
+    std::cout << typeid( THead ).name( ) << "; ";
+    return print( tail... );
+}
+
+template<typename TReq, typename TAns = dummy, typename... TArgs> void foo( TArgs&... args )
+{
+    std::cout << "TReq: " << typeid( TReq ).name( ) << "; TAns: " << typeid( TAns ).name( ) << std::endl;
+    std::cout << "TArgs: ";
+    print( args... );
+    std::cout << std::endl;
+}
+
+int main( )
+{
+    TArg1 arg1;
+    TArg2 arg2;
+    TArg3 arg3;
+    TArg4 arg4;
+    
+    foo<TArg1, TArg2>( arg3, arg2 );
+    foo<TArg2>( arg1, arg2, arg3, arg4 );
+}
