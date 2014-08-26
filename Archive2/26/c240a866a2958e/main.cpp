529c70a114cdcab5b5e1a2620eec233ba3cb47ad
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-26 01:41:30 +0000

    Update archive 26

---
+++ Archive2/26/c240a866a2958e/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <sstream>
+
+int main()
+{
+    std::string value = "asd";
+    std::stringstream ss("");
+
+    if (ss >> value) // this will fail
+        std::cout << "value read successfully: " << value << std::endl;
+        
+    // will value still be guaranteed to be zero?
+    std::cout << "value is " << value << std::endl;
+}
