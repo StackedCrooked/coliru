cb9780835aece89efbb5c2b95e3c7be54a127eee
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-26 19:00:12 +0000

    Update archive e1

---
+++ Archive2/e1/0529077dac879e/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename FUNCTION, typename... ARGS>
+auto wrapper(FUNCTION&& func, ARGS&&... args) -> decltype(func(args...))
+{
+    cout << "WRAPPER: BEFORE" << endl;
+    auto res = func(args...);
+    cout << "WRAPPER: AFTER" << endl;
+    return res;
+}
+
+int dummy(int a, int b)
+{
+    cout << a << '+' << b << '=' << (a + b) << endl;
+    return a + b;
+}
+
+int main(void)
+{
+    dummy(3, 4);
+    cout << "WRAPPERS RES IS: " << wrapper(dummy, 3, 4) << endl;
+}
