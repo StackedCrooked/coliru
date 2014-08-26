ab396b4d8eccce890545453c937c0629eb0bba11
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-26 19:29:18 +0000

    Update archive 2a

---
+++ trunk/Archive2/2a/879c8521d47d04/cmd.sh
@@ -0,0 +1,2 @@
+echo clang++ && time clang++ -std=c++11 -stdlib=libc++ -O2 -Wall -Wextra -pedantic-errors main.cpp -lsupc++ && echo ---------------- && time ./a.out
+echo --------- && echo g++-4.9 && echo ------------ && time g++-4.9 -std=c++11 -O2 -Wall -Wextra -pedantic-errors main.cpp && echo ----------------- && time ./a.out
