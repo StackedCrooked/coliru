8d84b5fb58481d93186f542fa471d25260938d62
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-27 17:50:55 +0000

    Move to own dir.

--- trunk/Web/logger.sh
+++ trunk/Web/logger.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd "$(dirname $0)"
+
 # Kill previous instance of _logger.sh (if any)
 ps -eopid,comm | grep '_logger.sh' | grep -v grep | awk '{print $1}' | while read line;  do
      echo "Killing previous instance $line"
