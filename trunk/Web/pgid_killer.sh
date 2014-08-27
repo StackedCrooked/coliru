ad527d72c66c7d16184b3f40fc0a0081ab0e46e7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-27 17:50:21 +0000

    cleanup pgid_killer.

--- trunk/Web/pgid_killer.sh
+++ trunk/Web/pgid_killer.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# Started as /root/coliru/Web/pgid_killer.sh
+cd "$(dirname $0)"
+
 [ -p .pgid_killer ] || mkfifo .pgid_killer 
 chmod a+w .pgid_killer
 
-
-while true ; do 
+while true ; do
     ./_pgid_killer.sh
-    logger "$(basename ${0}): _pgid_killer.sh stopped. Restarting soon." 
     sleep 10
-done >/dev/null 2>&1 </dev/null & disown
+done
