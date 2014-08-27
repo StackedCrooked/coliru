2b5badaf81fe63c784a3f81bba152ad544664895
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-27 17:51:57 +0000

    launch chroot as webserver user. this enables kill.

--- trunk/Web/build_and_run.sh
+++ trunk/Web/build_and_run.sh
@@ -30,4 +30,4 @@ set +e
 $(cat ${INPUT_FILES_DIR}/cmd.sh)"
 
 # Run the command in the chroot and create a new pgid.
-setsid sudo -u sandbox chroot ${CHROOT} timeout 65 bash -c "${CMD}"
+setsid chroot ${CHROOT} timeout 65 bash -c "${CMD}"
