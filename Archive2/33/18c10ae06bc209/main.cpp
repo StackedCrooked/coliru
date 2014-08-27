f8adb2b0990fe348280b520ea99290f09cf5ffc6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-08-27 18:23:17 +0000

    Update archive 33

---
+++ Archive2/33/18c10ae06bc209/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <type_traits>
+
+struct parent
+{
+    virtual ~parent() = default ;
+
+    virtual int mem_fun_parent( int a ) { return i += a ; }
+
+    template < typename DERIVED >
+    int mem_fun2_parent( std::vector<DERIVED> children )
+    {
+        static_assert( std::is_base_of<parent,DERIVED>::value, "class must be derived from parent" ) ;
+        int s = 0 ;
+        for( DERIVED& d : children ) s += d.mem_fun_parent( parent::mem_fun_parent(10) ) ;
+        return s ;
+    }
+
+    private : int i = 5 ;
+};
+
+struct child : parent { /* ... */ };
+
+int main()
+{
+    std::vector<child> children(100) ;
+    parent p ;
+    std::cout << p.mem_fun2_parent(children) << '\n' ;
+}
