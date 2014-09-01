4a090939a7878e8809eb0468334d6e9cd3d15ee3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-01 17:19:54 +0000

    Update archive 02

---
+++ Archive2/02/50313eaef414fb/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int num[10];
+int j,i,sum=0;
+printf("enter 5 integers\n");
+for(i=0;i<5;i++)
+{
+scanf("%d",&num[i]);
+}
+for(i=0;j<5;i++);
+{
+sum+=num[j];
+printf("sum=%d\n",sum);
+}
+return 0;
+} 
