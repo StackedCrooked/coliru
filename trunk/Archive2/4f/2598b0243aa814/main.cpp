29dbe0489fb1de556679ebaac377428ef65dcd41
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-01 15:41:58 +0000

    Update archive 4f

---
+++ trunk/Archive2/4f/2598b0243aa814/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <unistd.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main( int argc, char* argv[] )
+{
+    const char* nvalue = "World" ; 
+    int tvalue = 1 ;
+    
+    int c ;
+    while( ( c = getopt (argc, argv, "n:t:") ) != -1 ) 
+    {
+        switch(c)
+        {
+            case 'n':
+                if(optarg) nvalue = optarg;
+                break;
+            case 't':
+                if(optarg) tvalue = std::atoi(optarg) ;
+                break;
+        }
+    }
+    
+    for( int i = 0 ; i < tvalue; ++i )
+       std::cout << '[' << i+1 << "] Hello " << nvalue << "!\n" ;
+    std::cout << '\n' ;   
+}
