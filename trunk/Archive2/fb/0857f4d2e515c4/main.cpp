3515898ce54f9224f45b15a8db47e122415b721f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-01 15:07:59 +0000

    Update archive fb

---
+++ trunk/Archive2/fb/0857f4d2e515c4/main.cpp
@@ -0,0 +1,49 @@
+#include <cmath>
+#include <algorithm>
+
+struct point3d {
+    double x,y,z;
+    
+    point3d()=default;
+    point3d(const point3d&rhs)=default;
+    point3d&operator=(const point3d&rhs)=default;
+    point3d(double x,double y,double z):x(x),y(y),z(z){}
+    point3d& operator+=(const point3d& rhs) {x+=rhs.x;y+=rhs.y;z+=rhs.z;return *this;}
+    point3d& operator-=(const point3d& rhs) {x-=rhs.x;y-=rhs.y;z-=rhs.z;return *this;}
+    point3d& operator*=(const point3d& rhs) {x*=rhs.x;y*=rhs.y;z*=rhs.z;return *this;}
+    point3d& operator/=(const point3d& rhs) {x/=rhs.x;y/=rhs.y;z/=rhs.z;return *this;}
+    point3d& operator*=(double rhs) {x*=rhs;y*=rhs;z*=rhs;return *this;}
+    point3d& operator/=(double rhs) {x/=rhs;y/=rhs;z/=rhs;return *this;}
+    friend point3d operator+(point3d lhs,const point3d& rhs){return lhs+=rhs;}
+    friend point3d operator-(point3d lhs,const point3d& rhs){return lhs-=rhs;}
+    friend point3d operator*(point3d lhs,const point3d& rhs){return lhs*=rhs;}
+    friend point3d operator/(point3d lhs,const point3d& rhs){return lhs/=rhs;}
+    friend point3d operator*(point3d lhs,double rhs){return lhs*=rhs;}
+    friend point3d operator/(point3d lhs,double rhs){return lhs/=rhs;}
+    friend point3d operator*(double lhs,point3d rhs){return rhs*=lhs;}
+    friend point3d operator/(double lhs,point3d rhs){return rhs/=lhs;}
+    friend point3d abs(const point3d& rhs) {return {fabs(rhs.x),fabs(rhs.y),fabs(rhs.z)};}
+};
+
+point3d get_intersection(point3d topfrontleft, point3d backbottomright, point3d source)
+{
+    //find the middle
+    point3d middle(topfrontleft/2+backbottomright/2);
+    //slide box and source as if the "middle" was at the origin
+    topfrontleft -= middle;
+    source -= middle;
+    //scale source as if the box is the unit square
+    source /= topfrontleft;
+    //find the smallest magnatude of the source's offset
+    point3d mag = abs(source);
+    auto min = std::min(mag.x,std::min(mag.y,mag.z));
+    //scale the source so that it touches the box
+    source /= min;
+    //then scale and slide that touching point back to original coordinates
+    source = source * topfrontleft + middle;
+    return source;
+}
+
+int main() {
+    
+}
