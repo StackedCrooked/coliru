d063c5b7f3c41798d6c87637bd7b36a932a45d09
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-01 15:09:58 +0000

    Update archive 38

---
+++ Archive2/38/d215c4ffbd0df7/main.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+const int n = 1024;
+int a[n][n], b[n][n], c[n][n];
+void mul()
+{
+    // 遅いほう
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            for (int k = 0; k < n; ++k)
+                c[i][j] += a[i][k] * b[k][j];
+}
+int main()
+{
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            a[i][j] = rand();
+            b[i][j] = rand();
+        }
+    }
+
+    mul();
+    printf("%d\n", a[19][19]);
+}
