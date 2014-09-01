e60109daa0c416cdd6b61e7096449e1639e13f2f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-01 15:56:46 +0000

    Update archive 3b

---
+++ Archive2/3b/99ce4f88245df5/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class student
+{
+    public:
+    char s;
+    int age;
+};
+
+ostream& operator<<(ostream &output, const student &s) 
+{
+      output << s.s << " " << s.age;
+      return output;
+
+}
+
+int main (void)
+{
+    student a;
+    a.s = 'a';
+    a.age = 1;
+    ofstream myfile;
+    myfile.open("a1.txt",ios::app);
+    myfile << a;
+    return 0;
+}
