2e5071795edba2dcc262b0dd2a2f8405898fa975
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-01 15:51:00 +0000

    Update archive 73

---
+++ Archive2/73/2339b5ffb4dee1/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <typeinfo>
+#include <cassert>
+
+using std::cout;
+using std::endl;
+
+long unsigned int hash_in_constructor;
+
+struct A
+{
+    virtual void foo()
+    {
+        cout << "A" << endl;
+    }
+    
+    A()
+    { 
+        hash_in_constructor = typeid(*this).hash_code();
+    }
+};
+
+
+int main()
+{
+    A a;
+    cout << hash_in_constructor << endl;
+    cout << typeid(A).hash_code();
+}
