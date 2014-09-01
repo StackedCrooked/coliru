e06b4c6fc2780d7cafbd090c368584d3631612c9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-01 17:22:52 +0000

    Update archive b8

---
+++ Archive2/b8/81682b716fada0/main.cpp
@@ -0,0 +1,35 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <boost/variant.hpp>
+
+#define P_(x) \
+    std::cout << __FUNCTION__ << ':' << __LINE__ << "  " \
+              << #x << " == " << (x) << std::endl
+
+int main() {
+    using namespace std;
+    using str_or_dbl = boost::variant<string, double>;
+    vector<str_or_dbl> v;
+    string w;
+    while (cin >> w) {
+        char *p;
+        const auto dval = strtod(w.c_str(), &p);
+        str_or_dbl tmp;
+        if (*p != 0)
+            tmp = w; // kein double
+        else
+            tmp = dval; // war double
+        v.push_back(tmp);
+    }
+    
+    for (auto &e : v) {
+        if (auto *p = boost::get<double>(&e)) {
+            cout << *p << endl;
+        }
+        if (auto *p = boost::get<string>(&e)) {
+            cout << *p << endl;
+        }
+    }
+
+}
