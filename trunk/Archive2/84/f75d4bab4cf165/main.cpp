7379791c2032687db8475264c6dab4d9221fad05
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-02 17:25:45 +0000

    Update archive 84

---
+++ trunk/Archive2/84/f75d4bab4cf165/main.cpp
@@ -0,0 +1,82 @@
+using namespace std;
+
+#include <string>
+#include <algorithm>
+
+
+typedef std::pair<int, std::string> Pair;
+
+struct Cmp 
+{
+    bool operator()(Pair const& a, Pair const& b) const { return a.first < b.first; };
+    bool operator()(Pair const& a, int & b) const { return a.first < b; };
+    bool operator()(int & a, Pair const& b) const { return a < b.first; };
+};
+
+struct SectorSignalPairEquals : public std::binary_function<Pair, int, bool> {
+   bool operator()(Pair const& a, int b) const
+   {
+      return (a.first == b);
+   };
+};
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::cout << "Executing... \n";    
+    
+    std::vector<Pair> sectorSignals = { 
+        { 1, "105" }, 
+        { 2, "110" }, 
+        { 3, "115" },
+        { 42, "120" }, 
+    };
+    
+    std::vector<int> sCells = { 2, 3, 8 };
+    std::vector< int > sCellsInNBest(sCells.size());
+    std::vector< int > sCellsNotInNBest(sCells.size());
+    
+    Cmp cmp;
+    std::sort(sectorSignals.begin(), sectorSignals.end(), cmp);
+    std::sort(sCells.begin(), sCells.end());
+
+    std::vector<int>::iterator resIt = std::set_intersection (sCells.begin(), sCells.end(), sectorSignals.begin(), sectorSignals.end(), sCellsInNBest.begin(), cmp);
+    //auto range = std::equal_range(vec.begin(), vec.end(), std::string("zus"), cmp);
+    sCellsInNBest.resize(resIt-sCellsInNBest.begin());    
+    if (sCellsInNBest.size() == 0)
+    {
+        std::cout << "There is no Secondary Cells in NBest Server";
+    }
+    else
+    {
+        std::cout << "Secondary Cells: ";
+    }
+    
+    std::vector< int >::iterator It = sCellsInNBest.begin();
+    for(; It!= sCellsInNBest.end(); ++It)
+    {
+       std::cout << *It << ", ";
+    }
+    std::cout << endl;
+    SectorSignalPairEquals equals;
+    std::vector<Pair>::iterator nBestIt;
+    for(size_t i=0; i<sCellsInNBest.size(); ++i)
+    {
+       nBestIt = std::find_if(sectorSignals.begin(), sectorSignals.end(), std::bind2nd(equals, sCellsInNBest[i]));
+       std::cout << "nBest.first: " << (*nBestIt).first << endl;
+       std::cout << "nBest.second: " << (*nBestIt).second << endl;
+    }
+    sCellsNotInNBest.resize(sCells.size());
+    resIt = std::set_difference(sCells.begin(), sCells.end(), sCellsInNBest.begin(), sCellsInNBest.end(), sCellsNotInNBest.begin());
+    sCellsNotInNBest.resize(resIt-sCellsNotInNBest.begin());
+    
+    // std::vector<Pair>::iterator notInNBestIt;
+    for(size_t i=0; i<sCellsNotInNBest.size(); ++i)
+    {
+       std::cout << "Sector Index: " << sCellsNotInNBest[i] << endl;
+    }
+}
+
