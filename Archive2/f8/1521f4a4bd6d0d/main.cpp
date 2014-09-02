39db3d0d7185d5d7d549028b0b67684cc48df1bb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-02 17:53:02 +0000

    Update archive f8

---
+++ Archive2/f8/1521f4a4bd6d0d/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <memory>
+
+struct Base
+{
+    virtual ~Base() = default;
+
+    struct Inner
+    {
+        Inner(Base &b)
+        : inst(b)
+        {
+        }
+        virtual ~Inner() = default;
+
+        virtual void f() = 0;
+
+        virtual Base &instance()
+        {
+            return inst;
+        }
+
+    private:
+        Base &inst;
+    };
+
+    virtual std::unique_ptr<Inner> new_inner() = 0;
+};
+
+struct Derived
+: virtual Base
+{
+    Derived(int x)
+    : v{x}
+    {
+    }
+
+    struct Inner
+    : virtual Base::Inner
+    {
+        Inner(Derived &d)
+        : Base::Inner{d}
+        , inst(d)
+        {
+        }
+
+        virtual void f()
+        {
+            std::cout << inst.v << std::endl;
+        }
+        virtual void g(int x) //just a random example
+        {
+            inst.v = x;
+        }
+
+        virtual Derived &instance() //fine
+        {
+            return inst;
+        }
+
+    private:
+        Derived &inst;
+    };
+
+    virtual std::unique_ptr<Inner> new_inner() override //problem
+    {
+        return std::unique_ptr<Inner>{new Inner{*this}};
+    }
+
+private:
+    int v;
+};
+
+int main()
+{
+    Derived d {6};
+    auto inner = d.new_inner();
+    inner->f();
+    inner->g(7);
+    inner->f();
+}
