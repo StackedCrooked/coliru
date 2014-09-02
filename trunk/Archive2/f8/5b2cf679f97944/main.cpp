74d982161d5a09cb4c4c2d7a9a4dff4ec33c1aca
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-02 17:53:02 +0000

    Update archive f8

---
+++ trunk/Archive2/f8/5b2cf679f97944/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+    
+    int A[100];
+    
+    int n = 10; 
+    
+    
+    
+    cout << "Please enter the " << n << " numbers:" ;
+    
+    
+    return 0;
+}
+    
