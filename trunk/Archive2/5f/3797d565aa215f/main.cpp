af73d18da6502ee89b9f1f7b43629d598e540802
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-02 17:44:25 +0000

    Update archive 5f

---
+++ trunk/Archive2/5f/3797d565aa215f/main.cpp
@@ -0,0 +1,23 @@
+#include <functional>
+#include <iostream>
+#include <vector>
+
+int foo(int i) { return i * 2; }
+
+double bar() { return 42.42; }
+
+int bar2(int) { std::cout << "bar2\n"; return 0; }
+
+int main() 
+{
+    std::vector<std::function<void()>> v; // vectors of functions
+    
+    v.push_back(std::bind(bar2, 42));
+    v.push_back(std::function<double(void)>(bar));
+    v.push_back([] { std::cout << "lambda"; });
+    v.push_back(std::bind(foo, 42));
+    
+    // Execute functions
+    for(auto f : v)
+        f();
+}
