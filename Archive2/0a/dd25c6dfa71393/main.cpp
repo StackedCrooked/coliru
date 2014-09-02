45b61dbe16671dc6617268ce9fd1848dc63e26fa
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-02 17:41:53 +0000

    Update archive 0a

---
+++ Archive2/0a/dd25c6dfa71393/main.cpp
@@ -0,0 +1,62 @@
+class Processor;
+
+class Data
+{
+    friend Processor;
+    void privateFunction() {}
+};
+
+class IDataOwnerCallback
+{
+public:
+    virtual void visit(Data&) = 0;
+};
+
+class DataOwner
+{
+    Data d;
+public:
+    void accept(IDataOwnerCallback* pcb) { pcb->visit(d); }
+};
+
+class Processor
+{
+    void useData(Data& data);
+public:
+    void doProcess(DataOwner&);
+};
+
+void Processor::doProcess(DataOwner& dataOwner)
+{   
+    auto l = [this](Data& d) { useData(d); };
+    
+    class DataOwnerCallback final : public IDataOwnerCallback
+    {
+        decltype(l)& l;
+    public:
+        DataOwnerCallback(decltype(l)& p)
+        : l(p)
+        {}
+
+        virtual void visit(Data& data) final override
+        {
+            l(data);
+        }
+    } callback(l);
+    
+    dataOwner.accept(&callback);
+}
+
+void Processor::useData(Data& data)
+{
+    // Use the data, accessing private functionality via friendship
+    data.privateFunction();
+    // etc
+}
+
+int main()
+{
+    DataOwner dataOwner;
+    Processor p;
+    p.doProcess(dataOwner);
+}
