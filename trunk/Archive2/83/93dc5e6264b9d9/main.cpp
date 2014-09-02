338fbc33e0434b05f86677d98986d520c8f2c9b3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-02 10:20:12 +0000

    Update archive 83

---
+++ trunk/Archive2/83/93dc5e6264b9d9/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+// There's plenty of better implementations around.
+// This is similar to libstdc++'s implementation.
+template<class T, T... Ints>
+struct integer_seq {
+    using next = integer_seq<T, Ints..., sizeof...(Ints)>;
+    static constexpr std::size_t size() { return sizeof...(Ints); }
+};
+
+template<class T, int Len>
+struct seq_builder{
+    static_assert(Len > 0, "Length must be nonnegative");
+    using type = typename seq_builder<T, Len-1>::type::next;
+};
+
+template<class T>
+struct seq_builder<T, 0>{
+    using type = integer_seq<T>;
+};
+
+template<class T, int length>
+using make_int_sequence = typename seq_builder<T, length>::type;
+
+/////////////////// Actual stuff starts here/////////////////////////////////
+
+template<int FIRST, int LAST, class = make_int_sequence<int, LAST+1-FIRST>>
+struct Foo;
+
+template<int FIRST, int LAST, int... SEQ>
+struct Foo<FIRST, LAST, integer_seq<int, SEQ...>>
+{
+    static constexpr int array[sizeof...(SEQ)] = {(FIRST+SEQ)...};
+};
+
+template<int FIRST, int LAST, int... SEQ>
+constexpr int Foo<FIRST, LAST, integer_seq<int, SEQ...>>::array[sizeof...(SEQ)];
+
+int main(){
+    for(int i : Foo<5, 10>::array) {
+        std::cout << i << '\t';
+    }
+    std::cout << std::endl;
+}
