805b1921a671dc6a4813aa79397d540509fef8c1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-03 22:05:16 +0000

    Update archive 07

---
+++ trunk/Archive2/07/921310bd285c54/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    const char* un = "Allo";
+    const char* deux = "Allo";
+    
+    if (un == deux)
+        printf("Vrai");
+    else
+        printf("Faux");
+    
+    return 0;
+}
