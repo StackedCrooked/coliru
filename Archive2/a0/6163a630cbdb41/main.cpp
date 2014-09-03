7c2c4d8a11e0bb80124ed19861b2e5481f6911f9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-03 22:02:58 +0000

    Update archive a0

---
+++ Archive2/a0/6163a630cbdb41/main.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <boost/utility.hpp>
+
+struct some_policy {
+    typedef boost::integral_constant<bool, false> condition;
+};
+
+struct other_policy {
+    typedef boost::integral_constant<bool, true> condition;
+};
+
+
+template <typename policy>
+class test {
+   void do_something(typename boost::enable_if<typename policy::condition>::type* = 0) {}
+};
+
+int main() {
+    test<other_policy> p1;
+    test<some_policy>  p2;
+}
