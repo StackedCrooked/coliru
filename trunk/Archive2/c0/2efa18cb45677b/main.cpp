e9a7a26e10d602f221432202278eca983a17fa7f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-04 19:48:02 +0000

    Update archive c0

---
+++ trunk/Archive2/c0/2efa18cb45677b/main.cpp
@@ -0,0 +1,30 @@
+template < typename DERIVED, typename... BASE > struct crtp_helper ;
+
+template < typename T > struct crtp_helper<T> { /* ... */ };
+
+struct base : crtp_helper<base>
+{
+    virtual ~base() = default ;
+    const base& get() const { return do_get() ; }
+    protected: virtual const base& do_get() const = 0 ;
+};
+
+template < typename DERIVED, typename FIRST, typename... REST >
+struct crtp_helper< DERIVED, FIRST, REST... > : virtual FIRST, virtual REST...
+{
+    const DERIVED& get() const { return dynamic_cast< const DERIVED& >( do_get() ) ; }
+    protected: virtual const base& do_get() const override = 0 ;
+    // ...
+};
+
+struct A : crtp_helper<A,base> {}; struct B : crtp_helper<B,base> {}; struct C : crtp_helper<B,base> {};
+struct ABC : crtp_helper<ABC,A,B,C> {};
+
+int main()
+{
+    struct B1 : B { protected: virtual const base& do_get() const override { return *this ; } };
+    struct ABC1 : ABC { protected: virtual const base& do_get() const override { return *this ; } };
+    
+    B1 b ; const B& rb = b.get() ;
+    ABC1 abc ; const ABC& rabc = abc.get() ;;
+}
