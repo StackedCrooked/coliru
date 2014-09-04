927411980204d4923543bb5b437014c664e6d2fc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-04 08:42:55 +0000

    Update archive 32

---
+++ Archive2/32/545ddd836b21b2/main.cpp
@@ -0,0 +1,21 @@
+#include <stdexcept>
+#include <string>
+#include <memory>
+
+class BaseException : public virtual std::exception {
+public:
+    explicit BaseException(const std::string& msg) : msg_storage(std::make_shared<std::string>(msg)) { }
+    explicit BaseException(const char* msg) : msg_storage(std::make_shared<std::string>(msg)) { }
+    
+    virtual const char* what() const noexcept { return msg_storage->c_str(); }
+    
+private:
+    std::shared_ptr<std::string> msg_storage;
+};
+
+
+int main(int argc, char **argv) {
+    throw BaseException("computation aborted with answer: 42");
+    
+    return 0;
+}
