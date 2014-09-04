53efcab418f5c6bc25f3c0e83e80aee2f8a8c98e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-04 04:47:29 +0000

    Update archive a3

---
+++ trunk/Archive2/a3/f56a7bbaf09118/main.cpp
@@ -0,0 +1,18 @@
+#include <type_traits>
+#include <iostream>
+struct foo {
+
+    template<class T>
+	foo(T , typename std::enable_if<std::is_integral<T>::value>::type * = nullptr) { std::cout << "integral" << std::endl; }
+	template<class T>
+	foo(T , typename std::enable_if<std::is_floating_point<T>::value>::type * = nullptr) { std::cout << "floating" << std::endl; }
+};
+
+struct bar : foo {
+	using foo::foo;
+};
+
+int main() {
+    bar b(1);
+    bar bb(1.0);
+}
