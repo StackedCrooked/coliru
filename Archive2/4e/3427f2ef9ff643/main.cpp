f8a546b2a15b67ece05d16955c9f0113db88a1d5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-04 12:26:12 +0000

    Update archive 4e

---
+++ Archive2/4e/3427f2ef9ff643/main.cpp
@@ -0,0 +1,28 @@
+template <class D>
+struct base_traits;
+
+
+template <class D>
+struct base
+{
+    typedef typename base_traits<D>::value_t value_t;
+};
+
+struct derived;
+
+template<>
+struct base_traits<derived>
+{
+    typedef int value_t;
+};
+
+struct derived : base<derived>
+{
+    typedef base_traits<derived>::value_t value_t;
+};
+
+
+int main(void)
+{
+    derived d;
+}
