b7047aa1c6021ce3e05a65a80d244283bbec9877
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-04 19:27:13 +0000

    Update archive 86

---
+++ trunk/Archive2/86/f330466317ab88/main.cpp
@@ -0,0 +1,33 @@
+#include <iterator>
+#include <type_traits>
+#include <vector>
+
+template <typename I>
+  struct input_iter : public std::integral_constant<bool,
+  not std::is_integral<I>::value
+  and std::is_base_of
+    <std::input_iterator_tag
+    ,typename std::iterator_traits<I>::iterator_category
+    >::value >
+  {};
+
+template<typename T>
+  struct container
+{
+  container (size_t count, const T& init);
+  template <typename InputIt,
+            typename = typename std::enable_if<input_iter<InputIt>::value>::type >
+    container (InputIt first,InputIt last);
+};
+
+typedef container<int> int_cont;
+
+void f()
+{ std::vector<int> v (5,3);
+  int_cont a1 (3u,6); // first constructor
+  int_cont a2 (3u,6); // first constructor
+  int_cont a3 (3,6); // first constructor
+  int_cont a4 (3,6); // first constructor
+  int_cont a5 (3,6); // first constructor
+  int_cont b(v.begin(),v.end()); // second constructor
+}
