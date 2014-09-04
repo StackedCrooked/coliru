130fe15ecd5a19a533208b75faf63f3459d92409
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-04 12:46:29 +0000

    Update archive e6

---
+++ trunk/Archive2/e6/acc038d4c637bf/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+struct A // base class, moveable, noncopyable
+{
+    virtual ~A() = default ;
+    A() = default ; // file not opened
+    A( std::string filename ) : log_file( filename, std::ios::app ) {}
+
+    virtual bool open_log_file( std::string filename )
+    {
+        if( log_file.is_open() ) log_file.close() ;
+        log_file.open( filename, std::ios::app ) ;
+        return log_file.is_open() ;
+    }
+
+    virtual void test() { log_file << "from A: this is a test\n" ; }
+
+    protected: std::ofstream log_file ;
+};
+
+struct B : A // derived class, moveable, noncopyable
+{
+    B() = default ; // file not opened
+    B( std::string filename ) : A(filename) {}
+
+    virtual void test() override
+    {
+        static int n = 0 ;
+        log_file << ++n << ". from B (override): this is a test\n" ;
+    }
+};
+
+int main()
+{
+    for( int i=0 ; i < 5 ; ++i )
+    {
+        B b ;
+        A& a = b ;
+
+        a.open_log_file( "1.txt" ) ;
+        a.test() ;
+        a.open_log_file( "2.txt" ) ;
+        a.test() ;
+        a.open_log_file( "1.txt" ) ;
+        a.test() ;
+    }
+
+    std::cout << "1.txt contains\n----------------\n" << std::ifstream( "1.txt" ).rdbuf() ;
+    std::cout << "\n\n2.txt contains\n---------------\n" << std::ifstream( "2.txt" ).rdbuf() ;
+}
