dbb32574aabb469093a36edc5ef102fdce23717f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-04 19:21:03 +0000

    Update archive 9b

---
+++ trunk/Archive2/9b/65299b9f8b17d0/main.cpp
@@ -0,0 +1,32 @@
+template < typename DERIVED, typename... BASE > struct crtp_helper ;
+
+template < typename T > struct crtp_helper<T> { /* ... */ };
+
+struct base : crtp_helper<base>
+{
+    virtual ~base() = default ;
+    const base& get() const { return do_get() ; }
+    protected: virtual const base& do_get() const = 0 ;
+};
+
+template < typename DERIVED, typename FIRST, typename... REST >
+struct crtp_helper< DERIVED, FIRST, REST... > : virtual FIRST, virtual REST...
+{
+    const DERIVED& get() const { return dynamic_cast< const DERIVED& >( do_get() ) ; }
+    protected: virtual const base& do_get() const override = 0 ;
+    // ...
+};
+
+struct A : crtp_helper<A,base> { protected: virtual const base& do_get() const override = 0 ; };
+struct B : crtp_helper<B,base> { protected: virtual const base& do_get() const override = 0 ; };
+struct AB : crtp_helper<AB,A,B> { protected: virtual const base& do_get() const override = 0 ; };
+
+int main()
+{
+    struct A1 : A { protected: virtual const base& do_get() const override { return *this ; } };
+    struct B1 : B { protected: virtual const base& do_get() const override { return *this ; } };
+    struct AB1 : AB { protected: virtual const base& do_get() const override { return *this ; } };
+    A1 a ; const A& ra = a.get() ;
+    B1 b ; const B& rb = b.get() ;
+    AB1 ab ; const AB& rab = ab.get() ;;
+}
