5b134e4f838663a8cdcb9db7c3c832c98f5c1f87
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-05 02:33:40 +0000

    Update archive 21

---
+++ trunk/Archive2/21/1f7c862063385c/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <chrono>
+
+
+template<std::chrono::duration D>
+    typename D difference(std::chrono::time_pont start, std::chrono::time_point stop)
+    {
+        return std::chrono::duration_cast<D>(stop-start);
+    }
+
+
+        
+int main(void) 
+{ 
+    
+    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+    // sleep
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    //
+    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+
+    // get difference
+    auto res = difference<std::chrono::seconds>(start, stop);
+    
+    // print
+    std::cout << res.count()
+              << std::endl;
+
+}
+
+
+
