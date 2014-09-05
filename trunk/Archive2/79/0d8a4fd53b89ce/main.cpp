912f25ccd59607387ca3ca5d97ce711523588918
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-05 02:42:04 +0000

    Update archive 79

---
+++ trunk/Archive2/79/0d8a4fd53b89ce/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+template<int I>
+struct Foo
+{
+    Foo() = default;
+    
+    static const int myint = I;
+    static int get();
+    
+    /* How do you define this? */
+    template<typename T>
+    static void something();
+};
+
+
+// Simple
+template<int I>
+int Foo<I>::get()
+{
+    return myint;
+}
+
+/*      Foo arg   Extra arg 
+ *         v           v   */
+template<int I>
+template<typename T>
+void Foo<I>::something()
+{
+    std::cout << "here\n";
+}
+
+
+int main(void)
+{
+    Foo<5> f{};
+    std::cout << f.get() << std::endl;
+
+    /*          Anything  */
+    f.something<float>();
+}   
