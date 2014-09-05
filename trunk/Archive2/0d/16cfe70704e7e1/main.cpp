ad7ded27ec953c1890bc653bf52c297017bd944b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-05 12:57:40 +0000

    Update archive 0d

---
+++ trunk/Archive2/0d/16cfe70704e7e1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+template <typename T>
+void bar(T t) { std::cout << t << '\n'; }
+ 
+void foo2() {}
+ 
+template <typename... Args>
+void foo2(Args&&... args)
+{
+  __attribute__((unused)) int dummy[] = { 0, ((void) bar(std::forward<Args>(args)),0)... };
+}
+ 
+int main()
+{
+  foo2 (1, 2, 3, "30");
+}
