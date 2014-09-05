da7dc174d96daed354a468489033ef6d0417ba6a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-05 16:27:52 +0000

    Update archive 4f

---
+++ Archive2/4f/04e5ee3a43fa58/cmd.sh
@@ -0,0 +1,1 @@
+set -x ; clang++ -std=gnu++11 -O2 -pthread main.cpp && ./a.out
---
+++ Archive2/4f/3702f9bd502820/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 1;
+    cout << a++ << '\t' << ++a << '\t' << a++ << endl;
+    return 0;
+}
