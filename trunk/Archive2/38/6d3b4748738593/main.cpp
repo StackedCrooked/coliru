2a0c8c9fed879d6160b17b77d5bde986b414d2d8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-05 16:35:43 +0000

    Update archive 38

---
+++ trunk/Archive2/38/6d3b4748738593/main.cpp
@@ -0,0 +1,18 @@
+#include <memory>
+#include <iostream>
+
+class X { };
+
+class Y : public X { };
+
+std::unique_ptr<X> foo()
+{
+    auto y = std::make_unique<Y>();
+    // Do something with y...
+    return std::move(y); // OK, MOVES!
+}
+
+int main()
+{
+    std::cout << "Just to print some output";
+}
