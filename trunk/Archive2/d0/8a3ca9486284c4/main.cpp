8c69665e1bb29395628b913420566ad2db150ab4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-06 13:23:59 +0000

    Update archive d0

---
+++ trunk/Archive2/d0/8a3ca9486284c4/main.cpp
@@ -0,0 +1,17 @@
+bool out = false;
+int addNumbers(int x, int y)
+{
+    int answer = x;
+    answer = 1;
+    //return x + y;
+}
+
+int main(){
+  if(!out) {
+    addNumbers(10, 20);
+    return 1;
+  }
+  else {
+    return 2;
+  }
+}
