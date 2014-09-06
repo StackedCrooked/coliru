1190f18fce846a420a3223a1a0fcc9c3cf475873
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-06 17:02:52 +0000

    Update archive 21

---
+++ trunk/Archive2/21/816a8f5124089c/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+
+#define INFINITY 99
+#define X 9
+
+
+using namespace std;
+
+struct Node{
+    
+    int weight;
+    bool visited;
+    
+};
+
+ 
+int dijkstra(queue<Node*> graph, int src)
+{
+    vector<Node*> Node_path;
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+int main()
+{
+    for(int i = 0; i < X; i++)
+    {
+        int distances[i] = INFINITY;
+        Node* previous[i] = NULL;
+    }
+ 
+   return 0;
+}
