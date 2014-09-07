11f633020cfc965bf5b5b9241234afb7ffbb147a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-07 03:23:31 +0000

    Update archive c9

---
+++ trunk/Archive2/c9/102112f08abbfd/main.cpp
@@ -0,0 +1,71 @@
+#include <pthread.h>
+
+#include <iostream>
+#include <tuple>
+#include <memory>
+#include <utility>
+
+template<class T, class... Args>
+std::unique_ptr<T> make_unique(Args&&... args)
+{
+    return std::unique_ptr<T>( new T(std::forward<Args>(args)...) );
+}
+
+
+
+class thread
+{
+    pthread_t _tid;
+
+public:
+    thread() :
+    _tid(0)
+    { }
+
+    template <typename F, typename... Args>
+    thread(F&& f, Args&&... args)
+    {
+        auto l = [=] {
+                f(args...);
+            };
+        auto dl = make_unique<decltype(l)>(std::move(l));
+        
+        auto te =
+            [](void* rp) -> void*
+            {
+                auto const p = reinterpret_cast<decltype(dl)>(rp);
+                auto mp = decltype(dl) (p);
+                (*p)();
+                
+                return nullptr;
+            };
+            
+        if(0 == pthread_create(&_tid, 0, te.get(), l))
+        {
+            dl.release();
+        }
+    }
+    
+    void join()
+    {
+        pthread_join(_tid, nullptr);
+    }
+};
+
+/** USAGE **/
+void
+test(int i, char c)
+{
+    std::cout << "test: ";
+    while (i > 0) {
+        std::cout << c;
+        i--;
+    }
+    std::cout << std::endl;
+}
+
+int main(const int argc, const char** argv)
+{
+    thread t(test, 6, 'a');
+    t.join();
+}
