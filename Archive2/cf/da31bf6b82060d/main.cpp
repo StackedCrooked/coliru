aeb97e3cd1647567c28c0adda1813caebef3f0b3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-07 03:39:09 +0000

    Update archive cf

---
+++ Archive2/cf/da31bf6b82060d/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+
+template<typename std::size_t n>
+void printTable(int table[][n], size_t r) {
+    for(std::size_t i = 0; i < r; ++i) {
+        for(std::size_t j = 0; j < n; ++j) {
+            std::cout << std::setw(5) << table[i][j] << ' ';
+        }
+        std::cout << '\n';
+    }
+    std::cout << "-----------------------------" << std::endl;
+}
+
+int main()
+{
+    constexpr std::size_t R = 5;
+    constexpr std::size_t C = 5;
+    
+    int table[R][C] { };
+    
+    printTable(table, R);
+    
+    *(*(table + 0) + 0) = 100;
+    *(*(table + 1) + 1) = 200;
+    
+    printTable(table, R);
+}
