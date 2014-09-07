2fc2ce313186bc7c2153592d033dbd698a491778
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-07 06:49:18 +0000

    Update archive d8

---
+++ Archive2/d8/573a4f791cf335/main.cpp
@@ -0,0 +1,18 @@
+    #include <iostream>
+    bool foo = false;
+    int addNumbers(int x, int y)
+    {
+        int answer = x;
+        answer = 1;
+        //return x + y;
+    }
+    
+    int main(){
+      if(!foo) {
+        addNumbers(10, 20);
+        std::cout << 1 << std::endl;
+      }
+      else {
+        std::cout << 2 << std::endl;
+      }
+    }
