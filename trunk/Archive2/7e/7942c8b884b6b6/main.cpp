1b99c3dee685f2b62c23306f45c2feabd886726f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-07 10:26:38 +0000

    Update archive 7e

---
+++ trunk/Archive2/7e/7942c8b884b6b6/main.cpp
@@ -0,0 +1,21 @@
+#include <memory>
+#include <iostream>
+#include <chrono>
+
+using namespace std::chrono;
+
+int main()
+{
+    auto start = system_clock::now();
+    
+    auto x = 0;
+    for (auto i = 0; i < 100000000; ++i) { x += i % 2; }
+    
+    auto end = system_clock::now();
+    
+    auto elapsed_ms = duration_cast<milliseconds>(end - start).count();
+    
+    std::cout << "Time elapsed: " << elapsed_ms << " ms" << std::endl;
+    
+    std::cout << "Value of x: " << x;
+}
