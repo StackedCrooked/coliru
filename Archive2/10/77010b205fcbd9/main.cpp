9455fce82d4e07cf6b587f62a87c0e24882ac089
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-08 14:26:58 +0000

    Update archive 10

---
+++ Archive2/10/77010b205fcbd9/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+int main() {
+  std::vector<std::string> letters {"ABC", "A", "BCDE"};
+  std::for_each(letters.begin(), letters.end(), [](std::string &str) { std::reverse(str.begin() + 1, str.end()); });
+  for(auto i : letters) std::cout << i << std::endl;
+}
