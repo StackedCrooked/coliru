f41d4fdb6b8929a21b0d4dafba18c8d00c03a218
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-09 13:02:41 +0000

    Update archive 18

---
+++ trunk/Archive2/18/ba67a05f724abf/main.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+constexpr double log2() { return std::log(2); }
+
+int main() {
+   
+   std::cout << log2() << std::endl;
+   
+   return 0;
+}
