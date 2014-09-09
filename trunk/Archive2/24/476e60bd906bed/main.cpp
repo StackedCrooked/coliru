5ae34175fe58cf903c0ba7d37afbc8b844295423
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-09 09:11:43 +0000

    Update archive 24

---
+++ trunk/Archive2/24/476e60bd906bed/main.cpp
@@ -0,0 +1,15 @@
+#include <type_traits>
+#include <iostream>
+
+template<class T>
+void func(T&& param) {
+    if (std::is_same<T,int>::value) 
+        std::cout << "param is an int\n";
+    else 
+        std::cout << "param is not an int\n";
+}
+
+int main() {
+    int three = 3;
+    func(three);
+}
