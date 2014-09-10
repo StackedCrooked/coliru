23fe5da29b8c9728d8a99e3709a57a2b5845d054
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-10 11:32:40 +0000

    Update archive 54

---
+++ Archive2/54/7f7ddfa5d6b538/main.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <iostream>
+#include <type_traits>
+
+template <typename T>
+class foo
+{
+    public:
+    template <typename U>
+    typename std::enable_if<std::is_same<T, U>::value>::type
+    bar(const U& t)
+    {
+        std::cout << t << "\n";
+    }
+};
+
+template <typename X, typename... Args>
+class baz : public foo<X>, public baz<Args...>
+{
+    public:
+    using foo<X>::bar;
+    using baz<Args...>::bar;
+};
+
+template <typename X>
+class baz<X> : public foo<X>
+{
+    public:
+    using foo<X>::bar;
+};
+
+int main()
+{
+    baz<double, std::string> b;
+    b.bar(1.0);
+    b.bar(std::string("Demo"));
+}
