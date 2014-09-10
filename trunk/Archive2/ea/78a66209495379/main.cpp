cb5fe983481aa43828cd82eba2048a8da8df72e8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-10 20:46:07 +0000

    Update archive ea

---
+++ trunk/Archive2/ea/78a66209495379/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class Base
+{
+    public:
+        virtual void show() = 0;
+};
+void Base::show()
+{
+    cout << "Pure virtual definition\n";
+}
+class Derived:public Base
+{
+    public:
+        void show()
+        {
+            Base::show();//you forgot to add this line
+            cout << "Implementation of Virtual Function in Derived class";
+        }
+};
+int main()
+{
+    Base *b;
+    Derived d;
+    b= &d;
+    b->show();
+}
