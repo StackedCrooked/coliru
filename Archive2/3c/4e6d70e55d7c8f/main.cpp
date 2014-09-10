bfca426e523de56eaa5b473afd500748910e9a35
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-10 09:29:18 +0000

    Update archive 3c

---
+++ Archive2/3c/4e6d70e55d7c8f/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+
+int main()
+{
+    std::time_t t = std::time(nullptr);
+    std::cout << std::put_time(std::localtime(&t), "%a %b %d %T %p") << '\n';
+}
