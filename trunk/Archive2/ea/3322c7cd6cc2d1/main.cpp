c539d5c4c4ea94931e68220e23deb8bade2cb4e2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-11 23:26:20 +0000

    Update archive ea

---
+++ trunk/Archive2/ea/3322c7cd6cc2d1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <utility>
+
+template<typename T>
+auto is_complete(int) -> decltype(sizeof(std::declval<T>()), bool{}) {
+    return true;   
+}
+
+template<typename T>
+auto is_complete(...) -> bool {
+    return false;   
+}
+
+struct S;
+
+int main() {
+    std::cout << is_complete<int>(0);  << '\n'  
+    std::cout << is_complete<S>(0);
+}
