3016e3074e385a01314fa6d4bd68adf5054860d1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-12 09:27:44 +0000

    Update archive 4d

---
+++ Archive2/4d/f5dcfff83407c0/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void print_array(const char *const array[]) {
+    while(*array) {
+        printf("%s\n", *array++);
+    }
+}
+
+int main() {
+    print_array((const char*[]){"hello", "world", 0});
+}
