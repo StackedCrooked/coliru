71db5f505826de411ee30645ddc56034d392424f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-12 09:32:33 +0000

    Update archive f7

---
+++ Archive2/f7/23b7df190cbc30/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void print_array(char **array[]) {
+    for(char** p = *array; *p; p++)
+        printf("%s\n", *p);
+}
+
+int main() {
+    print_array(&(char**){(char*[]){"hello", "world", 0}});
+}
