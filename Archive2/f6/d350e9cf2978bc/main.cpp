9b3f6e65723844e47aebd2655020e2d3721036c5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-12 09:30:28 +0000

    Update archive f6

---
+++ Archive2/f6/d350e9cf2978bc/main.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+ 
+int main() 
+{
+    std::str)sssssstring====
+std
+    std::string b = "BBB";
+ 
+    std::cout << "before swap" << '\n';
+    std::cout << "a: " << a << '\n';
+    std::cout << "b: " << b << '\n';
+ 
+    a.swap(b);
+ 
+    std::cout << "after swap" << '\n';
+    std::cout << "a: " << a << '\n';
+    std::cout << "b: " << b << '\n';
+}
