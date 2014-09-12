92d47e6fedad9f86afd431330c616f8de90a94ed
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-12 03:00:01 +0000

    Update archive 55

---
+++ trunk/Archive2/55/451aa55424684e/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+//Don't have parallels or bootcamp installed yet :(
+int main()
+{
+    cout << "\t \t \t \t Basic Byu Trivia ";
+    cout << "\n \t Questions \t \t \t \t \t Answers";
+    cout << "\n";
+    cout << "\n What was the Original name of BYU? \t \t Brigham Young Academy";
+    cout << "\n When was BYA established? \t \t \t 1875";
+    cout << "\n Who was the first \"permanent\" principal of BYA? Karl Maeser";
+    cout << "\n When did BYA become BYU? \t \t \t 1903";
+    cout << "\n To what sports conference do we belong? \t Independent (Football)";
+    cout << "\n When did BYU win the national football title? \t 1984";
+    cout << "\n Who won the Heisman Trophy in 1990? \t \t Ty Detmer";
+}
