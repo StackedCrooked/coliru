861d5f0f71b598bf8d04e52d21248c7fc6b8b19f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-12 15:52:24 +0000

    Update archive 35

---
+++ trunk/Archive2/35/00999874c8214f/cmd.sh
@@ -0,0 +1,2 @@
+title "Puppy\'s Min/Max for C++14"
+clang++ -std=c++14 -O2 -Wall -pedantic -pthread main.cpp && ./a.out
---
+++ trunk/Archive2/35/00999874c8214f/main.cpp
@@ -0,0 +1,78 @@
+#include <cassert>
+#include <utility>
+
+
+template<typename T, typename U>
+constexpr auto Min(T&& lhs, U&& rhs)
+{
+    return lhs < rhs ? std::forward<T>(lhs) : std::forward<U>(rhs);
+}
+
+template<typename T, typename U>
+constexpr decltype(auto) Min(T& lhs, U& rhs)
+{
+    return lhs < rhs ? lhs : rhs;
+}
+
+template<typename T, typename U>
+constexpr auto Max(T&& lhs, U&& rhs)
+{
+    return lhs < rhs ? std::forward<T>(rhs) : std::forward<U>(lhs);
+}
+
+template<typename T, typename U>
+constexpr decltype(auto) Max(T& lhs, U& rhs)
+{
+    return lhs < rhs ? rhs : lhs;
+}
+
+
+
+#include <iostream>
+
+struct Nocopy
+{
+    constexpr Nocopy(int a) : a(a) {}
+    Nocopy(const Nocopy&) = delete;
+    Nocopy& operator=(const Nocopy&) = delete;
+    friend constexpr bool operator<(const Nocopy& lhs, const Nocopy& rhs) { return lhs.a < rhs.a; }
+    friend constexpr bool operator==(const Nocopy& lhs, const Nocopy& rhs) { return lhs.a == rhs.a; }
+    int a;
+};
+
+
+int main()
+{
+
+    // must not return dangling reference
+    static_assert(!std::is_reference<decltype(Max(1, 2))>::value, "");
+    static_assert(!std::is_reference<decltype(Max(2, 1))>::value, "");
+
+    static_assert(Max(1, 2) == 2, "");
+    static_assert(Max(2, 1) == 2, "");
+
+    constexpr int a = 1;
+    constexpr int b = 2;
+    static_assert(std::is_lvalue_reference<decltype(Max(a, b))>::value, "");
+    static_assert(Max(a, b) == b, "");
+    static_assert(Max(a, b) == 2, "");
+
+
+    constexpr Nocopy nocopy1{1}, nocopy2{2};
+    static_assert(std::is_lvalue_reference<decltype(Max(nocopy1, nocopy2))>::value, "");
+    static_assert(Max(nocopy1, nocopy2) == nocopy2, "");
+    static_assert(Max(nocopy2, nocopy1) == nocopy2, "");
+
+
+    std::string s1 = "s1", s2 = "s2";
+    assert(Max(s1, s2) == s2);
+    assert(Max(s2, s1) == s2);
+
+    static_assert( std::is_lvalue_reference<decltype(Max(s1, s2))>::value, "");
+    static_assert(!std::is_reference<decltype(Max("", s2))>::value, "");
+    static_assert(!std::is_reference<decltype(Max(s1, ""))>::value, "");
+
+    std::cout << "OK\n";
+
+}
+
