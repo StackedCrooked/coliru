bb8a61c4338661ad58285a9d1d1ccc6012671bbc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-13 16:10:58 +0000

    Update archive 03

---
+++ trunk/Archive2/03/035de1fd53911c/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdint>
+
+struct number_tag {};
+struct hexadecimal : number_tag {};
+struct number : number_tag {};
+
+template <typename T = unsigned>
+void write(T t, hexadecimal tag)
+{
+    std::cout << "hexadecimal" << std::endl;
+}
+
+template <typename T = unsigned>
+void write(T t, number tag)
+{
+    std::cout << "number" << std::endl;}
+
+template <typename Tag, typename U = unsigned>
+void write(U num)
+{
+    write<U>(num, Tag{});
+}
+
+int main()
+{
+    write<number>(10);
+    
+    write<hexadecimal>(0xFF);
+    
+    write<number, std::uint16_t>(0xFF);
+}
