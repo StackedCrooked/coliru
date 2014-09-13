6929e9a9d70495784a169538095d40822ea9e708
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-13 19:53:50 +0000

    Update archive b7

---
+++ trunk/Archive2/b7/c766d39ee53017/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <boost/variant.hpp>
+#include <type_traits>
+#include <stdexcept>
+
+struct A
+{
+    virtual ~A(){}
+    virtual void foo() { std::cout << "A::foo()" << std::endl; }
+};
+struct B : A
+{
+    virtual void foo() { std::cout << "B::foo()" << std::endl; }
+};
+
+template <typename T>
+struct visitor : boost::static_visitor<T>
+{
+private:
+    using Base = typename std::remove_pointer<
+                        typename std::remove_cv<
+                            typename std::remove_reference<T>::type
+                        >::type
+                    >::type;
+                        
+    template <typename U>
+    T get(U& u, std::true_type) const
+    {
+        return static_cast<T>(u);
+    }
+    
+    template <typename U>
+    T get(U& u, std::false_type) const
+    {
+        throw boost::bad_get{};
+    }
+
+public:
+    template <typename U>
+    T operator()(U& u) const
+    {
+        using Derived = typename std::remove_pointer<
+                            typename std::remove_cv<
+                                typename std::remove_reference<U>::type
+                            >::type
+                        >::type;
+                        
+        using tag = std::integral_constant<bool, (std::is_base_of<Base, Derived>::value
+                                                 || std::is_same<Base, Derived>::value)
+                                                 && std::is_convertible<U, T>::value>;
+        return get(u, tag{});
+    }
+};
+
+template <typename T, typename... Args>
+T my_get(boost::variant<Args...>& var)
+{
+    return boost::apply_visitor(visitor<T>{}, var);
+}
+
+int main()
+{    
+    boost::variant<A*,B*> var = new B;
+    
+    A* a = my_get<A*>(var); // works!
+    a->foo();
+    
+    B* b = my_get<B*>(var); // works!
+    b->foo();
+}
