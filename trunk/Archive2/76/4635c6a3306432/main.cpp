626ab136e3eb81c64b7d0af2c8540022cc10d246
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-13 19:24:44 +0000

    Update archive 76

---
+++ trunk/Archive2/76/4635c6a3306432/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+template <class T>
+class MyClass
+{
+public:
+  MyClass() { }
+private:
+  void PrivateFunction() { 
+    std::cout << "Message From Private Function" << std::endl; 
+  }
+};
+
+template <class T>
+void f()
+{
+  MyClass<int> a;
+  a.PrivateFunction();
+}
+
+int main()
+{
+    
+}
