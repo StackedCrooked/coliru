ba3d66887abb413e2d55bfdafcfb36c8cf71d020
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-13 23:33:46 +0000

    Update archive 68

---
+++ Archive2/68/67b8b536b37298/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+template<class T>
+ struct s{
+
+    T val;
+
+    public:
+        s(const T &a = T()) : val(a) { }
+        
+        friend ostream& operator<<(ostream& os, const s<T>& a){
+            return os << a.val;
+        }
+ };
+
+
+int main()
+{
+    s <double> names(4) ;
+    s <int> figure(7);
+    s <string> word;
+    s <vector<int>*> array;
+    cout << names << endl;
+    cout << figure << endl;
+    cout << array << endl;
+    return 0;
+}
