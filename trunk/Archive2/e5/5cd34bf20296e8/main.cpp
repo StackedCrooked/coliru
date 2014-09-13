3dfc245af984705f02ca2ef83281fd69201c6204
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-13 09:36:42 +0000

    Update archive e5

---
+++ trunk/Archive2/e5/5cd34bf20296e8/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+    std::cout  << "Write the path of the file\n" ;
+	std::string path ;
+	std::cin >> path ;
+
+	std::ifstream file( path.c_str() ) ;
+
+	if( file.is_open() )
+	{
+		std::cout << "File '" << path << "' opened.\n" ;
+
+		std::cout << "Write the word you're searching for\n" ;
+		std::string word ;
+		std::cin >> word ;
+
+		int countwords = 0 ;
+		std::string candidate ;
+		while( file >> candidate ) // for each candidate word read from the file 
+		{
+			if( word == candidate ) ++countwords ;
+		}
+
+		std::cout << "The word '" << word << "' has been found " << countwords << " times.\n" ;
+	}
+
+	else
+	{
+		std::cerr << "Error! File not found!\n" ;
+		return 1 ;
+	}
+}
