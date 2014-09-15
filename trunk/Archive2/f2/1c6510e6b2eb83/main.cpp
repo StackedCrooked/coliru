23e5b3b9b216011711df36d6161a54caea886589
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-15 23:41:21 +0000

    Update archive f2

---
+++ trunk/Archive2/f2/1c6510e6b2eb83/main.cpp
@@ -0,0 +1,19 @@
+template <typename T>
+class A {
+public:
+    A() {}
+    ~A() {
+        static_assert(sizeof(T) > 0, "T is incomplete.");
+    }
+};
+
+class B
+{
+public:
+    B();
+    ~B();
+
+private:
+    class impl;
+    A<impl> m_impl{};
+};
