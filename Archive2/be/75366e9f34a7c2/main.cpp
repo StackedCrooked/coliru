03033ffd148e16d791faac785b4af5f1fd94ad96
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-15 13:04:36 +0000

    Update archive be

---
+++ Archive2/be/75366e9f34a7c2/main.cpp
@@ -0,0 +1,21 @@
+// change to 0 for successful compilation
+#define WRONG 1
+
+#if !WRONG
+#include <type_traits>
+#endif
+
+typedef int (*F)();
+
+class false_ {};
+class true_ {};
+
+#if WRONG
+typedef typename result_of<F()>::type MyInt;
+#else
+typedef typename std::result_of<F()>::type MyInt;
+#endif
+
+int main () { 
+
+}
