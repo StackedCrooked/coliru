291c5baac5bc6540ab83f5188f1ea56814b72fee
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-15 13:12:38 +0000

    Update archive fb

---
+++ Archive2/fb/adf83d728de197/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <type_traits>
+#include <utility>
+
+template <typename... Ts>
+struct B
+{
+    
+};
+
+template <typename... Ts>
+struct D : B<Ts...>
+{
+};
+
+namespace detail
+{
+    template <typename T>
+    struct is_derived_from_B
+    {
+        using yes = char;
+        using no = long;
+                
+        template <typename... Args>
+        static yes test(B<Args...>*);
+        
+        static no test(...);
+        
+        static constexpr bool value = sizeof(test(std::declval<T*>())) == sizeof(yes);
+    };
+}
+
+template <typename T>
+struct is_derived_from_B : std::conditional<detail::is_derived_from_B<T>::value, std::true_type, std::false_type>::type {};
+
+int main()
+{
+    std::cout << is_derived_from_B<D<int, int>>::value << std::endl;
+    std::cout << is_derived_from_B<D<int, char, float>>::value << std::endl;
+    std::cout << is_derived_from_B<int>::value << std::endl;
+}
