a963acc42dc929af9de346d8f5cea1a339860b0b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-15 19:43:59 +0000

    Update archive 65

---
+++ Archive2/65/089239b4cb51bd/main.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <memory>
+
+namespace lib
+{
+
+class Foo_impl;
+
+class Foo
+{
+    public:
+        Foo();
+        ~Foo();
+
+    private:
+        Foo(const Foo&);
+        Foo& operator=(const Foo&);
+
+        std::unique_ptr<Foo_impl> m_impl = nullptr;
+
+        friend class Foo_impl;
+};
+
+}
+
+int main() {}
