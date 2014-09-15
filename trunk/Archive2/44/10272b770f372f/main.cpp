7045e7001f369b013e916d8d6fe63d9c85d5f0bb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-15 23:40:18 +0000

    Update archive 44

---
+++ trunk/Archive2/44/10272b770f372f/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+namespace N { class C; }
+void *operator new(size_t, N::C&);
+void operator delete(void *, N::C&);
+  
+namespace N {
+
+class C {
+  void fn(bool b)
+  {
+    std::cout << b << std::endl;
+  }
+
+  friend void *::operator new(size_t, C&);
+  friend void ::operator delete(void *, C&);
+
+  public:
+  C(bool b)
+  {
+    if (b) {
+      throw std::runtime_error("");
+    }
+  }
+};
+
+}
+
+void *operator new(std::size_t size, N::C& storage)
+{
+  storage.fn(true);
+  return std::malloc(size);
+}
+
+void operator delete(void *pointer, N::C& storage)
+{
+  storage.fn(false);
+  std::free(pointer);
+}
+
+int main()
+{
+  try {
+    N::C c(false);
+    new (c) N::C(true);
+  }
+  catch (...) {
+  }
+}
