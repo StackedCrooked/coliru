b3c3424dcd6be9336c07d2c069577a45b951f5f5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-15 05:43:56 +0000

    Update archive 94

---
+++ trunk/Archive2/94/381c2e819214e8/cmd.sh
@@ -0,0 +1,2 @@
+echo gcc && gcc -O3 -x c -std=c11 -Wall -Wextra -pedantic -Wno-unused-parameter main.cpp && ./a.out
+echo clang && clang -O3 -x c -std=c11 -Wall -Wextra -pedantic -Wno-unused-parameter main.cpp && ./a.out
