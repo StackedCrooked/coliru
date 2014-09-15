22c85d3f0021349cb368a8b5cc980406ede6b331
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-15 16:12:51 +0000

    Update archive e9

---
+++ Archive2/e9/f2ed0b36207bc8/main.cpp
@@ -0,0 +1,46 @@
+
+#include <iostream>
+using namespace std;
+
+// Do not declare variables outside main() a.k.a "global variables"
+// It is considered bad habit
+
+int main()
+{
+    // always declare variables inside blocks "{ }"
+    int nQuiz;   // number of quizzes
+    int nAss;    // number of assignment
+    double quiz = 0.0;
+    double grade;
+    double exam;
+    double total;
+
+    cout << "Prelim Grade"        << "\n"
+         << "Enter How Many Quiz: ";
+    cin >> nQuiz;
+
+    cout << "Enter how many assignment: ";
+    cin >> nAss;
+
+    //
+    // Get the quizzes
+    //
+
+    // DO NOT use this for loop format: for(int i = 1; i <= N; i++) { ... }
+    // Use this instead               : for(int i = 0; i < N; ++i) { ... } ==> always start from zero
+    for(int i = 0; i < nQuiz; ++i)
+    {
+        double temp;
+
+        cout << "Enter quiz no " << i + 1 << ": ";
+        cin >> temp;
+
+        quiz += temp;
+    }
+
+    //
+    // Do the same for the quizzes, attendance, etc...
+    //
+
+
+}
