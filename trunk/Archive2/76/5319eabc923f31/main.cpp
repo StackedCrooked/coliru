b50d9ede6757b3c9a4c7edd2f0f2113af74f7b57
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-15 13:07:31 +0000

    Update archive 76

---
+++ trunk/Archive2/76/5319eabc923f31/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+using array_type = int (*)[10];
+
+typedef int (*)[10] array_type;
+//using return_type = array_type (*)();
+
+int (*x())[3];
+
+struct Foo{ };
+
+
+int main()
+{
+    Foo (*c)[7];
+}
