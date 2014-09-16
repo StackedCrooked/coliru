b043c61267a182f3098a891563554848b8889967
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-16 12:58:02 +0000

    Update archive c3

---
+++ trunk/Archive2/c3/811bb2e2d6c2be/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <type_traits>
+#include <utility>
+
+template <typename... Ts>
+struct B {};
+
+template <typename... Ts>
+struct D : B<Ts...> {};
+
+template <typename... Ts>
+struct Other {};
+
+namespace detail
+{
+    template <typename T>
+    struct is_derived_from_B
+    {
+        using yes = char;
+        using no = long;
+                
+        template <typename... Args>
+        static yes test(B<Args...>*);
+        
+        static no test(...);
+        
+        static constexpr bool value = sizeof(test(std::declval<typename std::remove_cv<T>::type*>())) == sizeof(yes);
+    };
+}
+
+template <typename T>
+struct is_derived_from_B : std::integral_constant<bool, detail::is_derived_from_B<T>::value> {};
+
+int main()
+{
+    std::cout << is_derived_from_B<D<int, int>>::value << std::endl;
+    std::cout << is_derived_from_B<D<int, char, float>>::value << std::endl;
+    std::cout << is_derived_from_B<const D<int, char, float>>::value << std::endl;
+    std::cout << is_derived_from_B<Other<int, char, float>>::value << std::endl;
+    std::cout << is_derived_from_B<int>::value << std::endl;
+}
