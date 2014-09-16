c63d519b4b2e3bcc5c3e495d2fe9cca80f578c9c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-16 23:38:11 +0000

    Update archive e1

---
+++ trunk/Archive2/e1/fa78cf4fa8d8da/main.cpp
@@ -0,0 +1,24 @@
+#include <boost/fusion/adapted/struct.hpp>
+#include <boost/spirit/include/qi.hpp>
+using namespace std; // bite me
+
+BOOST_FUSION_DEFINE_STRUCT(, User, (string,name)(bool,ismoron))
+
+int main() {
+    string const input = R"(
+        Bartek Banachewicz: false
+        Ell: false
+        Puppy: true
+        rightfold: false)" ;
+
+    vector<User> users;
+
+    using namespace boost::spirit::qi; // real hard
+    phrase_parse(begin(input), end(input), 
+            +(lexeme[+(char_ - ':')] > ':' > bool_),
+            qi::space, 
+            users);
+
+    for (auto e : users)
+        std::cout << e.name << " is" << (e.ismoron? "":" not") << " a moron\n";
+}
