cf60a00309fd243df9ae39a66c70909df691eeb0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-16 06:34:10 +0000

    Update archive 82

---
+++ Archive2/82/7774ecc6d8bc99/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <utility>
+#include <type_traits>
+
+template <typename... Ts>
+struct B {};
+
+template <typename... Ts>
+struct D : B<Ts...> {};
+
+template <typename... Ts>
+struct Other {};
+
+namespace detail
+{
+    template <typename... Args>
+    std::true_type test(B<Args...>*);
+    std::false_type test(void*);
+}
+
+template <typename T>
+using is_derived_from_B = decltype(detail::test(std::declval<typename std::remove_cv<T>::type*>()));
+
+int main()
+{
+    std::cout << is_derived_from_B<D<int, int>>::value << std::endl;
+    std::cout << is_derived_from_B<D<int, char, float>>::value << std::endl;
+    std::cout << is_derived_from_B<const D<int, char, float>>::value << std::endl;
+    std::cout << is_derived_from_B<Other<int, char, float>>::value << std::endl;
+    std::cout << is_derived_from_B<int>::value << std::endl;
+}
