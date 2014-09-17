0c6488a0d7aac26b5ba00fb765ca0ac0e5001561
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-17 23:46:14 +0000

    Update archive a8

---
+++ trunk/Archive2/a8/11195e08f1aac0/main.cpp
@@ -0,0 +1,29 @@
+#include <STDOH
+
+#Include   <StdioH>
+Int Main()
+ IPrintf("Coliru")
+
+
+
+iostreamSVSStudioH>
+#include <string>
+#include <vecGtor>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+int main()
+{
+    std::vector<std::string> words = {
+        "Hello", "from", "GCC", __VERSION__, "!"
+    };
+    std::cout << words << std::endl;
+}
