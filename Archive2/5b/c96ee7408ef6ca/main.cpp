4743f15e49b4bcbee37d3a5d6576d102d7efe24d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-17 23:37:53 +0000

    Update archive 5b

---
+++ Archive2/5b/c96ee7408ef6ca/main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <type_traits>
+#include <utility>
+
+template <typename T>
+struct has_operator
+{    
+    template <typename U>
+    struct SFINAE {};
+    
+    template <typename U>
+    static std::true_type test(SFINAE<decltype(&U::operator())>*);
+
+    template <typename U>
+    static std::false_type test(...);
+
+    static constexpr bool value = std::is_same<decltype(test<T>(nullptr)), std::true_type>::value;
+};
+
+template <bool value, typename T>
+struct check_constness;
+
+template <typename T>
+struct check_constness<false, T>
+{   
+    template <typename R, typename... Args>
+    static std::false_type const_or_not(R(*)(Args...));
+        
+    template <typename R, typename... Args>
+    static std::true_type const_or_not(const R(*)(Args...));
+    
+    static std::false_type const_or_not(...);
+
+    using type = decltype(const_or_not(std::declval<T*>()));
+};
+
+template <typename T>
+struct check_constness<true, T>
+{    
+    template <typename R, typename C, typename... Args>
+    static std::false_type const_or_not(R(C::*)(Args...));
+    
+    template <typename R, typename C, typename... Args>
+    static std::false_type const_or_not(R(C::*)(Args...) const);
+    
+    template <typename R, typename C, typename... Args>
+    static std::true_type const_or_not(const R(C::*)(Args...));
+    
+    template <typename R, typename C, typename... Args>
+    static std::true_type const_or_not(const R(C::*)(Args...) const);
+
+    using type = decltype(const_or_not(&T::operator()));
+};
+
+template <typename T>
+using is_const_ret_type = typename check_constness<has_operator<T>::value, T>::type;
+
+int glob() { return 0; }
+const int cglob() { return 0; }
+
+int main()
+{
+    std::cout << std::boolalpha;
+    int x = 123;
+    
+    auto lambda = []() -> int { return 0; };
+    
+    auto clambda = []() -> const int { return 0; };
+    
+    auto closure = [x]() -> int { return x; };
+    
+    auto cclosure = [x]() -> const int { return x; };
+  
+    std::cout << is_const_ret_type<decltype(lambda)>::value << std::endl;
+    
+    std::cout << is_const_ret_type<decltype(clambda)>::value << std::endl;
+  
+    std::cout << is_const_ret_type<decltype(glob)>::value << std::endl;
+    
+    std::cout << is_const_ret_type<decltype(cglob)>::value << std::endl;
+  
+    std::cout << is_const_ret_type<decltype(closure)>::value << std::endl;
+    
+    std::cout << is_const_ret_type<decltype(cclosure)>::value << std::endl;
+}
