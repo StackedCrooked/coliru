5eca84fa0cd74a371fd827ee86bc22b73778bc75
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-17 16:49:33 +0000

    Update archive 70

---
+++ trunk/Archive2/70/3a360951ebcc69/main.cpp
@@ -0,0 +1,13 @@
+module Player (Player, getHealth, getScore, hit) where
+
+data Player = 
+    Player
+        { getHealth     :: Int
+        , getScore      :: Int }
+    deriving (Eq)
+    
+hit :: Player -> Player
+hit (Player health score) = Player (health - 1) (score - 10)
+
+main :: IO ()
+main = putStr "Compiles, yay!"
