58ac5b7d7bf2926a99b23e19f8af71c0c9e04ebd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-17 06:19:32 +0000

    Update archive 70

---
+++ Archive2/70/758d7d8cd85948/main.cpp
@@ -0,0 +1,40 @@
+#include <type_traits>
+
+template <class T>
+struct Allocator {
+    using Type_ = T;
+};
+
+template <class T, class Alloc_ = Allocator<T>>
+class Container
+{
+public:
+	using Allocator_ = Alloc_;
+	using Type_ = T;
+};
+
+template <
+	class Container_,
+		//enable if container have allocator and type of allocator is correct
+		typename std::enable_if <
+		(std::is_same<Container_, Container<typename Container_::Type_>>::value
+		|| std::is_base_of<Allocator<typename Container_::Type_>,
+		typename Container_::Allocator_>::value) &&
+		std::is_same<typename Container_::Type_, typename  Container_::Allocator_::Type_
+		>::value,//is_same
+		int>::type/*enable_if*/ = 0
+>
+struct Graph {};
+
+
+template <class T>
+class MyAllocator :public Allocator<T>
+{
+	using Type_ = T;
+};
+
+int main(int argc, char *argv[])
+{
+	Graph<Container<int> > a;
+	Graph<Container<char,MyAllocator<char>> > b;
+}
