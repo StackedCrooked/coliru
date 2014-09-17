63af3f88c446c0f9b302952f1cb8e0d01d78b90c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-17 19:52:05 +0000

    Update archive 22

---
+++ trunk/Archive2/22/27f655c3e5bb0e/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <type_traits>
+
+template <typename R, typename... Args>
+std::false_type const_or_not(R(*)(Args...));
+
+template <typename R, typename C, typename... Args>
+std::false_type const_or_not(R(C::*)(Args...));
+
+template <typename R, typename C, typename... Args>
+std::false_type const_or_not(R(C::*)(Args...) const);
+
+template <typename R, typename... Args>
+std::true_type const_or_not(const R(*)(Args...));
+
+template <typename R, typename C, typename... Args>
+std::true_type const_or_not(const R(C::*)(Args...));
+
+template <typename R, typename C, typename... Args>
+std::true_type const_or_not(const R(C::*)(Args...) const);
+    
+template <typename T>
+using is_const_ret_type = decltype(const_or_not(&T::operator()));
+    
+int main()
+{
+    std::cout << std::boolalpha;
+    
+    auto lambda = []() -> int { return 0; };
+    
+    auto clambda = []() -> const int { return 0; };
+  
+    std::cout << is_const_ret_type<decltype(lambda)>::value << std::endl;
+    
+    std::cout << is_const_ret_type<decltype(clambda)>::value << std::endl;
+}
