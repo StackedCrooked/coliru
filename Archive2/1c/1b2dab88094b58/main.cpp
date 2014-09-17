123af8ed2962ca78aee9d7f727abad181aab3ff3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-17 23:31:50 +0000

    Update archive 1c

---
+++ Archive2/1c/1b2dab88094b58/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <chrono>
+ 
+template<typename Diff>
+void log_progress(Diff d)
+{
+    std::cout << "..("
+              << std::chrono::duration_cast<std::chrono::milliseconds>(d).count()
+              << " ms)..";
+}
+ 
+int main()
+{
+    volatile int sink = 0;
+    std::cout << std::unitbuf; // enable automatic flushing
+ 
+    auto t1 = std::chrono::high_resolution_clock::now();
+    for (int j = 0; j < 5; ++j)
+    {
+        for (int n = 0; n < 10000; ++n)
+            for (int m = 0; m < 20000; ++m)
+                sink += m * n; // do some work
+        auto now = std::chrono::high_resolution_clock::now();
+        log_progress(now - t1);
+    }
+    std::cout << '\n';
+}
