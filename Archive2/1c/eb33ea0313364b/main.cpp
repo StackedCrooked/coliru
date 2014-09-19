88e2f0d6b56b98c99dc7021bf071bfa6b101b499
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-19 13:34:04 +0000

    Update archive 1c

---
+++ Archive2/1c/eb33ea0313364b/main.cpp
@@ -0,0 +1,12 @@
+struct Foo {
+    union {
+        struct {
+            int num_Foo;
+            int num_Bar;
+            int GreenFoo;
+        };
+        int arr[3];
+    };
+};
+
+int main(){}
