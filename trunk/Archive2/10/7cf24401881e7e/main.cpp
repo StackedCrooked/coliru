c9d6b42a4d0e896c86890d8b38b5fd5dff7efe9d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-19 06:16:43 +0000

    Update archive 10

---
+++ trunk/Archive2/10/7cf24401881e7e/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+template <int N>
+class tri {
+    std::string s;
+public:
+	tri() { s = tri<N - 1>().to_string() + "\n*" + std::string(N - 1, ' ') + "*"; }
+	std::string to_string() { return s; }
+};
+
+template <>
+class tri < 0 > {
+public:
+	std::string to_string() { return "*"; }
+};
+
+int main(){
+	static const int N = 7;
+	std::cout << tri<N - 2>().to_string() + "\n" + std::string(N, '*');
+}
