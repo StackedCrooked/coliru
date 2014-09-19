3cafda578546b61b0a787f468ea4ed579649fd91
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-19 23:47:40 +0000

    Update archive 29

---
+++ Archive2/29/b4128edb77cb9c/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <utility>
+
+struct A {
+    template<typename... Args>
+    A(Args&&... args) : A({ std::forward<Args>(args)... }) { }
+
+    int a, b, c;
+};
+
+int main() {
+    A x(1, 2, 3);
+    std::cout << x.a << x.b << x.c << '\n';
+    return 0;
+}
