16ae4722fbfed547414e60887b01e897c9b6f5e7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-20 09:37:18 +0000

    Update archive 21

---
+++ trunk/Archive2/21/2f45bbd891539d/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+struct A
+{
+    int foo();
+    
+    int a = 4;
+};
+
+int A::foo()
+{
+    cout << "foo()" << endl;
+    return 1;
+}
+
+A a;
+
+int main()
+{
+    a.foo();
+    cout << a.a << endl;
+}
