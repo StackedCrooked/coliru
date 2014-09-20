a24f69ef77e869c0e9225c01a4c92848622e8867
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-20 23:43:33 +0000

    Update archive 5f

---
+++ trunk/Archive2/5f/b62f5918555ac4/cmd.sh
@@ -0,0 +1,3 @@
+g++ -std=c++14 -O2 -Wall -Wextra -pedantic-errors main.cpp && ./a.out
+echo -------- && clang++ -std=c++14 -O2 -Wall -Wextra -pedantic-errors main.cpp && ./a.out
+echo -------- && clang++ -std=c++14 -stdlib=libc++ -O2 -Wall -Wextra -pedantic-errors main.cpp -lsupc++ && ./a.out
