9bd87e233097f2b30b6cb57fdd2e71b709e00f95
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-20 09:58:06 +0000

    Update archive c2

---
+++ trunk/Archive2/c2/0b2ebb10c8e60f/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <type_traits>
+#include <initializer_list>
+
+template <int N>
+struct names
+{ static char const * value; };
+template <int N>
+char const * names<N>::value{ "" };
+
+template <int N, typename T>
+int constexpr swallow(std::initializer_list<T>)
+{ return N; }
+
+template <int N>
+int constexpr name(char const (&str)[N])
+{ return swallow<N>({ names<N>::value = str }); } /* not constexpr, of course */
+
+template <int N, int Name = 0>
+void foo()
+{ std::cout << N << " " << names<Name>::value << std::endl; }
+
+int main()
+{
+  foo<22, name("test")>();
+}
