66918e2df7d3be35097376fcbc3a53723e66aba2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-21 13:32:45 +0000

    Update archive ab

---
+++ Archive2/ab/df8b03af21d782/main.cpp
@@ -0,0 +1,14 @@
+
+    struct A; // incomplete type
+    
+    template<class T>
+    struct D; // undefined
+    
+    template <class T>
+    struct B { int * p = nullptr; };
+    
+    int main() {
+        B<D<A>> u, v;
+        u = v;
+        u.operator=(v);
+    }
