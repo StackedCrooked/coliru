67cea152d513b6566a5b6fc9a7a67812de48baa9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-21 16:51:46 +0000

    Update archive 02

---
+++ trunk/Archive2/02/6de0d3b191aa55/cmd.sh
@@ -0,0 +1,2 @@
+clang++ -std=c++14 -stdlib=libc++ -O2 -Wall -Wextra -pedantic-errors main.cpp -lsupc++
+./a.out <<< '999 45 2 34567 999 8765432 45 9321 100 0 45 2 34567 456 5000 32 1000000 0 87 45 999'
