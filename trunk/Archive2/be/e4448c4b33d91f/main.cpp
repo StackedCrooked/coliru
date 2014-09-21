c15761fa2a533f2d5132aaee562ca46fddba99ba
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-21 16:19:10 +0000

    Update archive be

---
+++ trunk/Archive2/be/e4448c4b33d91f/main.cpp
@@ -0,0 +1,13 @@
+    struct A; // incomplete type
+    
+    template<class T>
+    struct D { T d; };
+    
+    template <class T>
+    struct B { int * p = nullptr; };
+    
+    int main() {
+        B<D<A>> u, v;
+        u = v;  // doesn't compile; complain that D<A>::d has incomplete type
+        u.operator=(v); // compiles
+    }
