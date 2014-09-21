802d330398831528066da09091d94bce7c85a8b6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-21 12:59:31 +0000

    Update archive 1f

---
+++ trunk/Archive2/1f/10b35a09e5809b/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <tuple>
+#include <type_traits>
+
+template <typename T, typename U, typename... Ts>
+struct has_type : std::false_type {};
+
+template <typename T, typename U, typename... Ts>
+struct has_type<T, std::tuple<U, Ts...>> : has_type<T, std::tuple<Ts...>> {};
+
+template <typename T, typename... Ts>
+struct has_type<T, std::tuple<T, Ts...>> : std::true_type {};
+
+int main()
+{
+    using T = std::tuple<int, double, char>;
+    
+    std::cout << has_type<int, T>::value << std::endl;
+    std::cout << has_type<double, T>::value << std::endl;
+    std::cout << has_type<float, T>::value << std::endl;
+}
