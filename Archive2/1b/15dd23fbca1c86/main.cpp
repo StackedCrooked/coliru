4cc90321c46b36fb741d12d24c3818044002bf30
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-21 13:26:57 +0000

    Update archive 1b

---
+++ Archive2/1b/15dd23fbca1c86/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+#include <iterator>
+
+typedef std::vector<int> vec_i;
+typedef std::vector<double> vec_d;
+
+int main()
+{
+    std::pair<vec_i, vec_d> temp = {{2, 3, 4}, {4.3, 5.1, 6.4}};
+
+    std::map<int, double> new_map;
+
+    std::transform(std::begin(temp.first), std::end(temp.first)
+                 , std::begin(temp.second)
+                 , std::inserter(new_map, std::begin(new_map))
+                 , &std::make_pair<int&, double&>);
+
+    for (auto it = new_map.begin(); it != new_map.end(); ++it)
+    {
+        std::cout << it->first << " : " << it->second << std::endl;
+    }
+}
