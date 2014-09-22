23435cf8bccc6fb8dce76970ffa2eb86407182c9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-22 23:51:39 +0000

    Update archive 97

---
+++ Archive2/97/8e27250a5bb028/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <memory>
+//#include <conio.h>
+
+struct State;
+
+struct FlyweightStates {
+    static std::unordered_map<std::string, State*> prototypes;
+    static void insertPrototype (const std::string& tag, State* state) {
+        prototypes.emplace(tag, state);
+        std::cout << tag << " inserted in FlyweightStates::prototypes." << std::endl;
+    }
+};
+std::unordered_map<std::string, State*> FlyweightStates::prototypes;
+
+struct State {
+    virtual ~State() {std::cout << "State destructor called." << std::endl;  }
+    State (const std::string& name) {
+        FlyweightStates::insertPrototype(name, this);
+    }
+};
+
+struct Sleeping : public State {
+    static std::shared_ptr<Sleeping> prototype;
+    Sleeping() : State("Sleeping") {}
+};
+std::shared_ptr<Sleeping> Sleeping::prototype(new Sleeping());
+
+int main() {}
