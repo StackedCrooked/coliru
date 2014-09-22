038b0f265a0a43406ea06d7448bb3326676857e1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-22 09:45:55 +0000

    Update archive 14

---
+++ trunk/Archive2/14/6c9aaae8fc09ed/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<double> arr1, arr2, arr3, arr4;
+    
+    std::vector<std::vector<double>> coordinates;
+    coordinates.push_back(arr1);
+    coordinates.push_back(arr2);
+    coordinates.push_back(arr3);
+    coordinates.push_back(arr4);
+}
---
+++ trunk/Archive2/14/e57e5734b03dbf/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a, b;
+  cin >> a;
+  while (a>0) b++;
+  cout << a;
+}
