e451c873b9b00c15ef8a058e56a29335b0e8ebed
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-22 02:40:41 +0000

    Update archive 6d

---
+++ trunk/Archive2/6d/ad29dc1a72a9f2/main.cpp
@@ -0,0 +1,12 @@
+import QtQuick 2.3
+
+Rectangle {
+    width: 800
+    height: 600
+    color: "lightblue"
+    Text {
+        anchors.top: parent.top
+        anchors.horizontalCenter: parent.horizontalCenter
+        text: "hello all folks"
+    }
+}
