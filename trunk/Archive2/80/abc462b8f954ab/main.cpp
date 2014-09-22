9bec3032d60e151b92701f94d31cdc6328153aef
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-22 09:57:35 +0000

    Update archive 80

---
+++ trunk/Archive2/80/abc462b8f954ab/main.cpp
@@ -0,0 +1,350 @@
+//************************************
+// C++ BEGINNER PARSER CLASS TUTORIAL
+//************************************
+#include <iostream>
+#include <cstdlib>
+#include <cctype>
+#include <cstring>
+
+/////////////////////// added ///////////////////////////////
+#include <string>
+#include <map>
+#include <math.h>
+////////////////////////////////////////////////////////////////
+using namespace std;
+
+/////////////////////// added ///////////////////////////////
+const std::map< std::string, double(*)(double) > unary_functions =
+{ { "sin", ::sin }, { "sqrt", ::sqrt }, { "ceil", ::ceil } /* , ... */ } ;
+////////////////////////////////////////////////////////////////
+
+
+/////////////////////// modified ///////////////////////////////
+enum types { DELIMITER = 1, VARIABLE, NUMBER, UNARY_FUNCTION };    // **************
+
+class analyzer
+{
+    private:
+    char *pExpresion;    	// Points to the exp<b></b>ression.
+    char token[80];		// Holds current token.
+    char tokenType;		// Holds token's type.
+
+    void recursive1( double &culmination );
+    void recursive2( double &culmination );
+    void recursive3( double &culmination );
+    void recursive4( double &culmination );
+    void recursive5( double &culmination );
+    void bomb( double &culmination );
+    void nextToken();
+    void serror( int error );
+    int isdelim( char c );
+
+    /////////////////////// added ///////////////////////////////
+    void expect( std::string expected ) { if( token != expected ) serror(0) ; }
+    void consume( std::string expected ) { nextToken() ; expect(expected) ; }
+    //////////////////////////////////////////////////////
+
+    public:
+    analyzer();					   // analyzer constructor.
+    double sniff( char *exp );			// analyzer entry point.
+    ~analyzer();						// analyzer destructor.
+};
+
+//************************************
+// 13 analyzer constructor.
+//************************************
+analyzer::analyzer()
+{
+
+    pExpresion = NULL;
+
+}
+
+
+double analyzer::sniff( char *exp )
+{
+
+    double culmination;
+
+    pExpresion = exp;
+
+    nextToken();
+    if( !*token )
+    {
+        serror( 2 );						// No exp<b></b>ression present.
+        return 0.0;
+    }
+    recursive1( culmination );
+    if( *token ) serror( 0 );			// Last token must be null.
+    return culmination;
+}
+
+//************************************
+// 4 Add or subtract two terms.
+//************************************
+void analyzer::recursive1( double &culmination )
+{
+
+    /*register*/ char beer; // *** register is deprecated **********************************
+    double bucket;
+
+    recursive2( culmination );
+    while( ( beer = *token ) == '+' || beer == '-' )
+    {
+        nextToken();
+        recursive2( bucket );
+        switch( beer )
+        {
+            case '-':
+                culmination = culmination - bucket;
+                break;
+            case '+':
+                culmination = culmination + bucket;
+                break;
+        }
+    }
+}
+
+//************************************
+// 5 Multiply or divide two factors.
+//************************************
+void analyzer::recursive2( double &culmination )
+{
+
+    /*register*/ char beer; // *** register is deprecated **********************************
+    double bucket;
+
+    recursive3( culmination );
+    while( ( beer = *token ) == '*' || beer == '/' || beer == '%' )
+    {
+        nextToken();
+        recursive3( bucket );
+        switch( beer )
+        {
+            case '*':
+                culmination = culmination * bucket;
+                break;
+            case '/':
+                culmination = culmination / bucket;
+                break;
+            case '%':
+                culmination = ( int )culmination % ( int )bucket;
+                break;
+        }
+    }
+}
+
+
+void analyzer::recursive3( double &culmination )
+{
+
+    double bucket, ex;
+    /*register*/ int t ; // *** register is deprecated **********************************
+
+    recursive4( culmination );
+    if( *token == '^' )
+    {
+        nextToken();
+        recursive3( bucket );
+        ex = culmination;
+        if( bucket == 0.0 )
+        {
+            culmination = 1.0;
+            return;
+        }
+        for( t = ( int )bucket - 1; t>0; --t ) culmination = culmination * ( double )ex;
+    }
+}
+
+//************************************
+// 7 Evaluate a unary + or -.
+//************************************
+void analyzer::recursive4( double &culmination )
+{
+    /*register*/ char beer; // *** register is deprecated **********************************
+
+    beer = 0;
+    // if( ( tokenType == DELIMITER ) && *token == '+' || *token == '-' ) // ****
+    if( ( tokenType == DELIMITER ) && ( *token == '+' || *token == '-' ) ) // for clarity
+    {
+        beer = *token;
+        nextToken();
+    }
+    recursive5( culmination );
+    if( beer == '-' ) culmination = -culmination;
+}
+
+
+void analyzer::recursive5( double &culmination )
+{
+
+    if( *token == '(' )
+    {
+        nextToken();
+        recursive1( culmination );
+        if( *token != ')' )
+            serror( 1 );
+        nextToken();
+    }
+    else bomb( culmination );
+}
+
+void analyzer::bomb( double &culmination )
+{
+
+    switch( tokenType )
+    {
+        case NUMBER:
+            culmination = atof( token );
+            nextToken();
+            return;
+
+        case VARIABLE:
+        {
+            std::cout << "looking up value of variable '" << token << "' => assuming result == 999\n" ;
+            culmination = 999 ; // *** hard-coded *** TODO: look up symbol table, get the actual value
+            nextToken();
+            return ;
+        }
+
+        /////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////// added ///////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////
+        case UNARY_FUNCTION:
+        {
+            std::cout << "evaluating unary function '" << token << "'\n" ;
+            auto fn = unary_functions.find(token)->second ;
+
+            consume( "(" ) ;
+
+            nextToken() ;
+            double arg ;
+            recursive1(arg) ;
+
+            culmination = fn(arg) ;
+
+            expect( ")" ) ;
+
+            nextToken() ;
+            return ;
+        }
+        /////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////
+
+        default:
+            serror( 0 );
+    }
+}
+
+
+void analyzer::serror( int error )
+{
+
+    // static char *e[] = // ****
+    static const char *e[] = // literal strings are arrays of const char
+    {
+        "Syntax Error",
+        "Unbalanced Parentheses",
+        "No exp<b></b>ression Present"
+    };
+
+    cout << e[error] << endl;
+}
+
+//************************************
+// 10 Obtain the next token.
+//************************************
+void analyzer::nextToken()
+{
+
+    /*register*/ char* bucket; // *** register is deprecated **********************************
+
+    tokenType = 0;
+    bucket = token;
+    *bucket = '\0';
+
+    if( !*pExpresion ) return; // At end of exp<b></b>ression.
+
+    while( isspace( *pExpresion ) ) ++pExpresion; // Skip over white space.
+
+    if( strchr( "+-*/%^=()", *pExpresion ) )
+    {
+        tokenType = DELIMITER;
+        // Advance to next char.
+        *bucket++ = *pExpresion++;
+    }
+    else if( isalpha( *pExpresion ) )
+    {
+        while( !isdelim( *pExpresion ) ) *bucket++ = *pExpresion++;
+        tokenType = VARIABLE;
+    }
+    else if( isdigit( *pExpresion ) )
+    {
+        while( !isdelim( *pExpresion ) ) *bucket++ = *pExpresion++;
+        tokenType = NUMBER;
+    }
+
+    *bucket = '\0';
+
+    /////////////////////// added /////////////////////////////////////////////////////////
+    if( unary_functions.find(token) != unary_functions.end() ) tokenType = UNARY_FUNCTION ;
+    ///////////////////////////////////////////////////////////////////////////////////////
+}
+
+
+int analyzer::isdelim( char c )
+{
+
+    //if( strchr( " +-/*%^=()x", c ) || c == 9 || c == '\r' || c == 0 )
+    if( strchr( " +-/*%^=()", c ) || c == 9 || c == '\r' || c == 0 )
+        return 1;
+    return 0;
+}
+
+
+analyzer::~analyzer()
+{
+
+}
+
+
+int main( /*int argc, char* argv[]*/ )
+{
+    char expstr[80];
+
+    // analyzer equation; // *** moved into the loop // Instantiates equation as an object of type analyzer.
+
+    //for( ;; )	// Creates an infinite loop.
+    {
+        analyzer equation; // *** moved into the loop // Instantiates equation as an object of type analyzer.
+        /*
+        cout << endl << endl << endl;
+        cout << "  To stop the program" << endl << endl;
+        cout << "  enter a period then press return." << endl << endl;
+        cout << "  To run the program" << endl << endl;
+        cout << "  enter an algebraic express; for example, (12+5)*6 " << endl << endl;
+        cout << "  then press return ==>\n";
+        */
+
+        cin.getline( expstr, 79 );
+        std::cout << "expression: '" << expstr << "'\n\n" ;
+
+        if( *expstr == '.' )
+        {
+            // equation.~analyzer(); // *** avoid this
+            // instead, move the definition of equation into the loop
+            // a new analyser object will be created each time through the loop
+
+            // break; // ****		// Infinite loop exited by typing a period.
+            return 0 ; // since we have commented out the loop
+        }
+
+        const auto result = equation.sniff( expstr ) ;
+        cout << "\nAnswer is: " << result  << '\n' ;
+
+    };
+
+}
+
+//************************************
