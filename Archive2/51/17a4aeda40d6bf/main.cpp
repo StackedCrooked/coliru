f3fa3f722a5a982fddb9213de0398a9912d462b5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-22 20:05:04 +0000

    Update archive 51

---
+++ Archive2/51/17a4aeda40d6bf/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstddef>
+#include <utility>
+#include <string>
+#include <tuple>
+using namespace std;
+
+struct Non
+{
+    Non(int x, double y)
+    {
+        cout << x << " " << y << endl;
+    }
+    Non(const Non&) = delete;
+    Non(Non&&) = delete;
+    Non &operator=(const Non&) = delete;
+    Non &operator=(Non&&) = delete;
+};
+
+template<typename T>
+struct Item
+{
+    T item;
+    
+    template<typename Pack, size_t ...I>
+    Item(Pack x, index_sequence<I...>)
+        : item(get<I>(x)...)
+    {}
+    template<typename ...Args>
+    Item(tuple<Args...> x)
+        : Item(x, index_sequence_for<Args...>{})
+    {}
+};
+
+template<typename T, typename ...Ts>
+struct Tuple : Tuple<Ts...>, Item<T>
+{
+    using Item<T>::item;
+
+    template<typename Pack, typename ...Tail>
+    Tuple(Pack head, Tail ...tail)
+        : Tuple<Ts...>(tail...), Item<T>(head)
+    {}
+};
+
+template<typename T>
+struct Tuple<T> : Item<T>
+{
+    template<typename Pack>
+    Tuple(Pack head)
+        : Item<T>(head)
+    {}
+};
+
+int main()
+{
+    Tuple<string, Non, Non, Non> x{make_tuple(5, '*'), make_tuple(1, .1), make_tuple(2, .2), make_tuple(3, .3)};
+    cout << x.item << endl;
+    //auto y = move(x); - compile error
+    //auto z = move(x); - compile error
+}
