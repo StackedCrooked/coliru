9111fd3d12f7eca09670e93644dd5145286cd94f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-22 23:29:51 +0000

    Update archive 6f

---
+++ Archive2/6f/b55e558cbd7abc/main.cpp
@@ -0,0 +1,20 @@
+enum E
+{
+    A, B, C
+};
+
+int main()
+{
+    int x = A;
+    E y = static_cast<E>(x);
+    
+    switch(y)
+    {
+        case A: std::cout << "A" << std::endl; break;
+        case B: std::cout << "B" << std::endl; break;
+        case C: std::cout << "C" << std::endl; break;
+        default: break;
+    }
+    
+    return 0;
+}
