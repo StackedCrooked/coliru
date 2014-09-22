52bdaf1800d19b3bb9ff7c25d2e8059a4d2d5f57
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-22 16:50:00 +0000

    Update archive b6

---
+++ Archive2/b6/3408524f3319a4/main.cpp
@@ -0,0 +1,36 @@
+#include <boost/asio.hpp>
+#include <boost/chrono.hpp>
+#include <boost/bind.hpp>
+#include <boost/asio/high_resolution_timer.hpp>
+
+typedef boost::chrono::high_resolution_clock hrc;
+using boost::chrono::duration_cast;
+using boost::chrono::milliseconds;
+
+boost::asio::io_service                              io_service_;
+boost::asio::high_resolution_timer                   timer_(io_service_);
+hrc::time_point lastTime_;
+
+void case1(boost::system::error_code ec)
+{
+    hrc::time_point currentTime = hrc::now();
+
+	hrc::duration diff = currentTime - lastTime_;
+	std::cout << "run time: " << duration_cast<milliseconds>(diff).count() << std::endl;
+
+	lastTime_ = hrc::now();
+	timer_.expires_from_now(milliseconds(20));
+	timer_.async_wait(boost::bind(case1, boost::asio::placeholders::error));
+}
+
+void test()
+{
+	timer_.expires_from_now(milliseconds(0));
+	timer_.async_wait(boost::bind(case1, boost::asio::placeholders::error));
+	io_service_.run();
+}
+
+int main()
+{
+	test();
+}
