ba6f65dff843463f7317b3f2e5e9cc31e75cfadf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-22 16:49:35 +0000

    Update archive d6

---
+++ Archive2/d6/84352b0688f4e2/main.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <limits.h>
+ 
+int main(void)
+{   
+    printf("CHAR_BIT   = %d\n", CHAR_BIT);
+    printf("MB_LEN_MAX = %d\n", MB_LEN_MAX);
+    printf("\n");
+ 
+    printf("CHAR_MIN   = %+d\n", CHAR_MIN);
+    printf("CHAR_MAX   = %+d\n", CHAR_MAX);
+    printf("SCHAR_MIN  = %+d\n", SCHAR_MIN);
+    printf("SCHAR_MAX  = %+d\n", SCHAR_MAX);
+    printf("UCHAR_MAX  = %u\n",  UCHAR_MAX);
+    printf("\n");
+ 
+    printf("SHRT_MIN   = %+d\n", SHRT_MIN);
+    printf("SHRT_MAX   = %+d\n", SHRT_MAX);
+    printf("USHRT_MAX  = %u\n",  USHRT_MAX);
+    printf("\n");
+ 
+    printf("INT_MIN    = %+d\n", INT_MIN);
+    printf("INT_MAX    = %+d\n", INT_MAX);
+    printf("UINT_MAX   = %u\n",  UINT_MAX);
+    printf("\n");
+ 
+    printf("LONG_MIN   = %+ld\n", LONG_MIN);
+    printf("LONG_MAX   = %+ld\n", LONG_MAX);
+    printf("ULONG_MAX  = %lu\n",  ULONG_MAX);
+    printf("\n");
+ 
+    printf("LLONG_MIN  = %+lld\n", LLONG_MIN);
+    printf("LLONG_MAX  = %+lld\n", LLONG_MAX);
+    printf("ULLONG_MAX = %llu\n",  ULLONG_MAX);
+    printf("\n");
+}
