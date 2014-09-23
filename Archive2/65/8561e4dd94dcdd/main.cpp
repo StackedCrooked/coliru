0336998fb0a1db0ce762375b97efa41b735d331c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-23 19:55:36 +0000

    Update archive 65

---
+++ Archive2/65/8561e4dd94dcdd/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+float round(float f,float pres) {
+    return (float) (floor(f*(1.0f/pres) + 0.5)/(1.0f/pres));
+}
+
+int main() {
+    
+	cout << "17.589 -> " << round(17.589,0.05) << std::endl; // 17.59
+	cout << "11.2125 -> " << round(17.589,0.05) << std::endl; // 11.25
+	return 0;
+}
