d7b762e94821abc555bb171d12ce2e484c732477
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-23 13:55:38 +0000

    Update archive 63

---
+++ trunk/Archive2/63/2fd5a719dab067/main.cpp
@@ -0,0 +1,53 @@
+#include <type_traits>
+#include <iostream>
+
+#include <cstdlib>
+
+struct A
+{
+    
+    template< typename type >
+    bool
+    operator () (type const &&) const = delete;
+    
+    template< typename type >
+    std::enable_if_t< std::is_same< std::remove_cv_t< std::remove_reference_t< type > >, double >::value >
+    operator () (type &&) const
+    {
+        std::cout << "1" << std::endl;
+        std::cout << __PRETTY_FUNCTION__ << std::endl << std::endl;
+    }
+    
+    template< typename type >
+    std::enable_if_t< !std::is_same< std::remove_cv_t< std::remove_reference_t< type > >, double >::value >
+    operator () (type &&) const
+    {
+        std::cout << "2" << std::endl;
+        std::cout << __PRETTY_FUNCTION__ << std::endl << std::endl;
+    }
+    
+};
+
+int main() 
+{
+    A const a{};
+    { // desired
+        double d{};
+        a(std::move(d));
+        int i{};
+        a(std::move(i));
+    }
+    { // undesired (I want a hard error)
+        double const d{};
+        a(d);
+        int const i{};
+        a(i);
+    }
+    { // undesired (I want a hard error)
+        double d{};
+        a(d);
+        int i{};
+        a(i);
+    }
+    return EXIT_SUCCESS;
+}
