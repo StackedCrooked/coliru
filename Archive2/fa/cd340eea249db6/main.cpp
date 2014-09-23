5f7f857bf02acb180d1b006ca6c4ecbd0da2ce82
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-23 23:57:45 +0000

    Update archive fa

---
+++ Archive2/fa/cd340eea249db6/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <type_traits>
+
+template <typename... Args>
+struct head {};
+
+template <typename Head, typename... Args>
+struct head<Head, Args...>
+{
+    using type = Head;
+};
+
+template <typename T>
+struct A : T
+{    
+    template <typename... Args>
+    A(Args&&... params) : T(std::forward<Args>(params)...), x(0) {
+        std::cout << "Member 'x' was default constructed\n"; 
+    }
+
+    template <typename O, typename... Args, typename = typename std::enable_if<std::is_constructible<int, O>::value>::type>
+    A(const O& z, Args&&... params) : T(std::forward<Args>(params)...), x(z) {
+        std::cout << "Member 'x' was constructed from arguments\n"; 
+    }
+
+    int x;
+};
+
+struct B
+{
+    B(const char*) {}
+};
+
+int main()
+{
+    double x = 3.0;
+    A<B> yy(x, "test");    
+}
