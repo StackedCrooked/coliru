376bcdea60f0101cee9539281c7eb97f8fa3277e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-24 06:48:45 +0000

    Update archive d0

---
+++ Archive2/d0/5a049dc29f2db5/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <type_traits>
+
+class A {};
+
+class B : public A {};
+
+template <typename T>
+class Template
+{
+public:
+    Template() { std::cout << "Template default\n"; }
+    Template(const Template &a) { std::cout << "Template copy constructor\n"; }
+
+    template <typename T2>
+    Template(const Template<T2> &a)
+    {
+       std::cout << "Templated template copy constructor\n";
+    }
+};
+
+template <typename T>
+auto funcT(const Template<T>& a) -> typename std::enable_if<std::is_base_of<A, T>::value>::type
+{
+}
+
+int main()
+{
+    Template<B> bt;
+    funcT(bt);
+}
