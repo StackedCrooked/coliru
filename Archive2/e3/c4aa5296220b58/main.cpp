9805dc3b4540eb607b5f40f1535798b378d16cba
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-24 16:48:06 +0000

    Update archive e3

---
+++ Archive2/e3/c4aa5296220b58/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <regex>
+
+using namespace std;
+
+int main()
+{
+    std::string basePartOfSymbol = "%ES A!";
+    
+    const std::regex basePartRegExp("^%(\\w+)?( )?(((\\d{1,2})|(\\w+))?(!)?)?((?:\\-([^=]*))?(?:=(.*))?)?(.+)?$");
+    
+    std::smatch captures;
+    const std::string symbol_str(basePartOfSymbol);
+	std::regex_search(symbol_str, captures, basePartRegExp);
+    
+    cout << captures.size() << endl;
+    
+    int size = captures.size();
+    
+    for (int i = 0; i < size; ++i)
+    {
+        cout << i << ": " << captures.str(i) << endl;
+    }
+    
+    return 0;
+}
