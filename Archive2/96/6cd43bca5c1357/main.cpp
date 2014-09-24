bbb927021eb2f623bb9665c48d6734d4415dc054
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-24 16:51:46 +0000

    Update archive 96

---
+++ Archive2/96/6cd43bca5c1357/main.cpp
@@ -0,0 +1,46 @@
+#include <utility>
+#include <cstdio>
+#include <type_traits>
+
+template <typename T = void>
+class SuperClass
+{
+    using CRTP = typename std::conditional<std::is_same<T, void>::value, SuperClass, T>::type;
+    
+public:
+    CRTP& SetA(int a)
+    {
+        m_a = a;
+        return static_cast<CRTP&>(*this);
+    }
+    
+    virtual void Print()
+    {
+        printf("a=%i\n", m_a);
+    }
+protected:
+    int m_a;
+};
+
+class SubClass : public SuperClass<SubClass>
+{
+public:
+    SubClass& SetB(double b)
+    {
+        m_b = b;
+        return *this;
+    }
+    virtual void Print()
+    {
+        printf("a=%i, b=%f\n", m_a, m_b);
+    }
+protected:
+    double m_b;
+};
+
+int main()
+{
+    SubClass().SetB(123.4).SetA(123).Print();
+    SubClass().SetA(123).SetB(123.4).Print();
+    SuperClass<>().SetA(123).Print();
+}
