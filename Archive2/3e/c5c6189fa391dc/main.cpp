a27838079bb5cca4393bc4c4cfd9c1c8b1f062ae
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-24 20:34:16 +0000

    Update archive 3e

---
+++ Archive2/3e/c5c6189fa391dc/main.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <utility>
+#include <vector>
+using namespace std;
+
+class StrangeComparison {
+public:
+  StrangeComparison(const vector<int>& ordering) : ordering_(ordering) {}
+  bool operator()(int a, int b) const {
+    auto index_a = distance(ordering_.begin(), find(ordering_.begin(), ordering_.end(), a));
+    auto index_b = distance(ordering_.begin(), find(ordering_.begin(), ordering_.end(), b));
+    return make_pair(index_a, a) < make_pair(index_b, b);
+  }
+private:
+  const vector<int>& ordering_;
+};
+
+int main() {
+   vector<int> v{5,4,1,2};
+   vector<int> v2{2,4,3,5,1,6,8,7};
+   vector<int> v3{1,4,2,3};
+
+   sort(v2.begin(), v2.end(), StrangeComparison(v));
+   copy(v2.begin(), v2.end(), ostream_iterator<int>(cout, ", "));
+   cout << endl;
+
+   sort(v3.begin(), v3.end(), StrangeComparison(v));
+   copy(v3.begin(), v3.end(), ostream_iterator<int>(cout, ", "));
+   cout << endl;
+}
