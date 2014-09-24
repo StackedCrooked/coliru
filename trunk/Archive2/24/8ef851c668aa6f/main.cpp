155ae155202a12ecea27357630227e9e89fdbc82
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-24 23:47:29 +0000

    Update archive 24

---
+++ trunk/Archive2/24/8ef851c668aa6f/main.cpp
@@ -0,0 +1,14 @@
+#include <boost/regex.hpp>
+#include <string>
+#include <iostream>
+int main(){
+static const boost::regex e("#region\\ (.*?)\\n(.*?)#endregion\\ (.*)");
+std::string s = "#region TESTCODEREGION_FOOTER\n my user typed this #endregion TESTCODEREGION_FOOTER";
+ 
+boost::match_results<std::string::const_iterator> results;
+if (boost::regex_match(s, results, e)) {
+    std::cout << results[1] <<"\n";
+    std::cout << results[2] <<"\n";
+    std::cout << results[3];
+}
+}
