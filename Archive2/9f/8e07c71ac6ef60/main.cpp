5221ca92dd52a81e6c9027994adebb44b242d82b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-24 16:53:16 +0000

    Update archive 9f

---
+++ Archive2/9f/8e07c71ac6ef60/main.cpp
@@ -0,0 +1,16 @@
+
+// Modify this code so that it prints
+// 20 minus signs. Add or substitute one
+// character only!
+// There are 3 solutions.
+
+#include <iostream>
+
+int main()
+{
+    int n = 20;
+    for (int i = 0; ~i < n; i--)
+        std::cout << "-";
+}
+
+
