a4c146291100fe8634a6b902844525b9dd8445a0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-24 03:30:58 +0000

    Update archive 45

---
+++ Archive2/45/18951542e218f5/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <utility>
+#include <memory>
+
+template<class T>
+constexpr int foo()
+{
+    return *std::make_unique<int>(42);
+}
+
+int main() 
+{
+    foo<int>();
+}
