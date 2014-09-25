16c961e79ae77a3e960589f769cde551d6d14982
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-25 00:18:32 +0000

    Update archive 42

---
+++ trunk/Archive2/42/d69c25fdb0a486/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <memory>
+#include <type_traits>
+
+class InitableKey {
+private:
+  friend class Initable;
+  InitableKey() {}
+public:
+
+  // Movable but not copyable
+  InitableKey(InitableKey&&) = default;
+  InitableKey& operator=(InitableKey&&) = default;
+  InitableKey(const InitableKey&) = delete;
+  InitableKey& operator=(const InitableKey&) = delete;
+};
+
+class Initable {
+ private:
+   
+   static void deleter(Initable* aptr) { aptr->terminate(); delete aptr; };
+ public:
+  Initable(InitableKey&& key) { (void) key; }  // Suppressed unused parameter warning.
+  virtual ~Initable() {}
+  virtual void terminate() = 0;
+
+  template<typename T, typename... Ts>
+  static std::unique_ptr<T, decltype(&deleter)> make_initable(Ts&&... args) {
+    static_assert(std::is_base_of<Initable, T>::value,  "Can only be used to create Initable types.");
+    return std::unique_ptr<T, decltype(&deleter)>(new T(std::forward<Ts>(args)..., InitableKey{}), &deleter);
+  }
+  
+};
+
+class B : public Initable {
+ public:
+  B(InitableKey&& key) : Initable(std::move(key)) {}
+  void terminate() override { std::cout << "terminate\n"; }
+};
+
+
+int main() {
+  std::shared_ptr<B> b = Initable::make_initable<B>();
+}
