454ca158d2caf7ca70617be552fc93b36d6fc12b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-25 14:41:10 +0000

    Update archive e5

---
+++ Archive2/e5/6e7657b1f43e3f/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <functional>
+#include <list>
+ 
+std::ostream& operator<<(std::ostream& ostr, const std::list<int>& list)
+{
+    for (auto &i : list) {
+        ostr << " " << i;
+    }
+    return ostr;
+}
+ 
+int main()
+{
+    std::list<int> list = { 8,7,5,9,0,1,3,2,6,4 };
+ 
+    std::cout << "before:     " << list << "\n";
+    list.sort();
+    std::cout << "ascending:  " << list << "\n";
+    list.sort(std::greater<int>());
+    std::cout << "descending: " << list << "\n";
+}
