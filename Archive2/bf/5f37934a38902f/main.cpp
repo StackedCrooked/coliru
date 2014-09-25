a70294f5127b2aaf25cb1c1837f49f91850f24f2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-25 07:27:22 +0000

    Update archive bf

---
+++ Archive2/bf/5f37934a38902f/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#define YEAR 2014
+using namespace std;
+
+int main (){
+    
+string mystr;
+
+int a=1;
+int b=2;
+char j={'A'};
+char f={'B'};
+cout<<"enter code"<<endl;
+getline(cin,mystr);
+cout<<j<<a<<endl;
+
+return(0);
+}
+
+
+
+
