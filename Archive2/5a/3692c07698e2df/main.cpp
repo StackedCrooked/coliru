edf99b1e92db719861b70580f50c0c29af2fa76d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-25 20:31:57 +0000

    Update archive 5a

---
+++ Archive2/5a/3692c07698e2df/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std ;
+main()
+char c;
+{
+cout << "Entrez une lettre" << endl;
+} while( char c );
+if (charc) {
+cout << "Vous avez trouve la bonne lettre" << endl;
+} else { 
+cout << "Perdu "endl;
+}
