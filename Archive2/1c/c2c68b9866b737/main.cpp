80a301800a7557718babd610c2e47366dae31e27
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-25 20:17:59 +0000

    Update archive 1c

---
+++ Archive2/1c/c2c68b9866b737/main.cpp
@@ -0,0 +1,9 @@
+char=c
+do {
+cout << "Entrez une lettre" << endl;
+} while( char=c );
+if (char=c) {
+cout << "Vous avez trouve la bonne lettre" << endl;
+} else {
+cout << "Perdu "endl;
+}
---
+++ Archive2/1c/fa4117c8e92934/main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+typedef struct
+{
+    int a;
+    int b;
+} Point;
+
+typedef struct
+{
+    int     flag;
+    Point   p;
+    int     other;
+} Things;
+
+int main(void)
+{
+    Things all;
+
+    printf("%p", &all); // prints "0x41ff2728" (correct)
+    printf("%p", &all.flag); // prints "0x41ff2728" (correct)
+
+    printf("%p", &all.p); // prints "(nil)"
+    printf("%p", (&all.p)); // prints "(nil)"
+    printf("%p", all.p); // prints "(nil)"
+    printf("%p", (void *)&all.p); // prints "(nil)"
+}
