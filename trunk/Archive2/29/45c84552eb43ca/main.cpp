0bb6aa065e287a5998d704519220901f5f79ac78
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-26 16:21:30 +0000

    Update archive 29

---
+++ trunk/Archive2/29/45c84552eb43ca/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <ostream>
+
+void print(const char *s)
+{
+    using namespace std;
+    cout << s << endl;
+}
+
+struct Test
+{
+    Test()
+    {
+        print("\tdefault");
+    }
+    Test(const Test &)
+    {
+        print("\tcopy");
+    }
+    Test &operator=(const Test &);
+    void swap(Test&)
+    {
+        print("\tswap");
+    }
+};
+
+
+Test foo()
+{
+    return Test();
+}
+
+int main()
+{
+    print("\nnormal:");
+    Test x = foo();
+    
+    print("\nconst ref binding:");
+    const Test &y = foo();
+
+    print("\nswap trick:");
+    Test z;
+    foo().swap(z);
+}
