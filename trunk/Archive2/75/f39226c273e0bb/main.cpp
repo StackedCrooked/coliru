fbac3dcbda546e1627c7627286956e195df88abd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-26 16:26:11 +0000

    Update archive 75

---
+++ trunk/Archive2/75/f39226c273e0bb/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+ 
+int main()
+{
+    std::cout << "abs(+3.0) = " << std::abs(+3.0) << '\n'
+              << "abs(-3.0) = " << std::abs(-3.0) << '\n';
+    // special values
+    std::cout << "abs(-0.0) = " << std::abs(-0.0) << '\n'
+              << "abs(-Inf) = " << std::abs(-INFINITY) << '\n';
+}
