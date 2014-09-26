0eb1336a53ef76de99075cfed840f3d56bf22a15
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-26 06:34:49 +0000

    Update archive 93

---
+++ trunk/Archive2/93/c3d7553f229809/main.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+struct Tmp
+{
+    Tmp(int p)
+        : v(p)
+    {}
+    Tmp(const Tmp& other)
+        : v(other.v)
+    {
+        std::cout << "Tmp::Tmp(const Tmp& other)" << std::endl;
+    }
+    int v;
+};
+
+int main()
+{
+    std::map<std::string, Tmp> v;
+    v.insert(std::make_pair("1", Tmp(1)));
+	v.insert(std::make_pair("2", Tmp(2)));
+	v.insert(std::make_pair("3", Tmp(3)));
+	v.insert(std::make_pair("4", Tmp(4)));
+	v.insert(std::make_pair("5", Tmp(5)));
+
+    std::cout << "======" << std::endl;
+
+    auto p = std::min_element(begin(v), end(v), 
+		  [] (const std::map<std::string, Tmp>::value_type& a,
+			  const std::map<std::string, Tmp>::value_type& b)
+			{ 
+			  return a.second.v < b.second.v; 
+			});
+    std::cout << p->second.v << std::endl;
+}
