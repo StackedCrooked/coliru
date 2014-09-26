317c64a248c9447691c7b25248adf5415c81b69f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-26 10:08:18 +0000

    Update archive 61

---
+++ Archive2/61/128a056d44801b/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+void sort(int arr[], int n)
+{
+    int c = 0;
+
+    for(int i = 0; i<n-1; ++i) {
+        if (arr[i]>arr[i+1]) {
+            c=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=c;
+            i=0;
+        }
+    }
+
+}
+
+int main()
+{
+    int arr[4] = {3,2,1,4};
+    std::cout << '\n';
+    for (size_t i = 0; i < 4; i++)
+        std::cout << arr[i] << ' ';
+    std::cout << '\n';
+    
+    ::sort(&arr[0], 4);
+    
+    std::cout << '\n';
+    for (size_t i = 0; i < 4; i++)
+        std::cout << arr[i] << ' ';
+    std::cout << '\n';
+}
