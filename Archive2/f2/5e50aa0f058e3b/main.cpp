a82719840a57d6f6a65403c574f8f1c4dc4e43e8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-26 23:28:41 +0000

    Update archive f2

---
+++ Archive2/f2/5e50aa0f058e3b/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <thread>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+
+template<typename Callback>
+void increment(int n, Callback callback)
+{
+    std::cout << "increment n=" << n << " tid=" << std::this_thread::get_id() << std::endl;
+    std::thread([=] { usleep(rand() % 1000); callback(n + 1); }).detach();
+}
+
+
+int main()
+{
+    std::cout << "start" << std::endl;
+
+    auto callback = [](int n)
+    {
+        std::cout << "main callback: n=" << n
+                  << " tid=" << std::this_thread::get_id()
+                  << std::endl;
+    };
+
+    boost::asio::io_service io;
+
+    for (int i = 0; i != 10; ++i)
+    {
+        std::cout << "dispatch i=" << i << " tid=" << std::this_thread::get_id() << std::endl;
+        
+        io.dispatch([&, i] {
+            increment(i, [&](int n) {
+                // inside the callback called from worker thread
+                std::cout << "thread callback. n=" << n << " tid=" << std::this_thread::get_id() << std::endl;
+                
+                // post the callback on the main thread
+                io.post(std::bind(callback, n));
+            });
+        });
+    }
+
+    // call stop after one second
+    std::thread([&]{
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "Stopping io" << std::endl;
+        io.stop();
+    }).detach();
+
+    // block until stop is called
+    boost::asio::io_service::work w(io);
+    io.run();
+}
