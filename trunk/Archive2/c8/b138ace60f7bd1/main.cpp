b6d5ea44f8db482f02fd727b903e64e020969e0b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-27 14:45:13 +0000

    Update archive c8

---
+++ trunk/Archive2/c8/b138ace60f7bd1/main.cpp
@@ -0,0 +1,21 @@
+#include <cassert>
+
+constexpr int getClamped(int mValue, int mMin, int mMax) noexcept
+{
+    return ( mMin <= mMax ) ? 
+           ( mValue < mMin ? mMin : (mValue > mMax ? mMax : mValue) ) :
+           throw "mMin must be less than or equal to mMax";
+}
+
+int main( int argc, char** argv )
+{
+    // These two work:
+    static_assert( getClamped( 42, 0, 100 ) == 42, "CT" );
+    assert( getClamped( argc, 0, 100 ) == argc );
+
+    // Fails at compile-time:
+    // static_assert( getClamped( 42, 100, 0 ) == 42, "CT" );
+    
+    // Fails at run-time:
+    // assert( getClamped( argc, 100, 0 ) == argc );
+}
