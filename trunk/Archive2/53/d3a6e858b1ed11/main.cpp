2c7c1848859359e1f3a43692b5a92328c499957f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-28 16:26:14 +0000

    Update archive 53

---
+++ trunk/Archive2/53/d3a6e858b1ed11/main.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <codecvt>
+#include <iostream>
+
+std::u16string to_utf16( std::string str ) // utf-8 to utf16
+{ return std::wstring_convert< std::codecvt_utf8_utf16<char16_t>, char16_t >{}.from_bytes(str); }
+
+std::string to_utf8( std::u16string str16 )
+{ return std::wstring_convert< std::codecvt_utf8_utf16<char16_t>, char16_t >{}.to_bytes(str16); }
+
+std::u32string to_utf32( std::string str )
+{ return std::wstring_convert< std::codecvt_utf8<char32_t>, char32_t >{}.from_bytes(str); }
+
+std::string to_utf8( std::u32string str32 )
+{ return std::wstring_convert< std::codecvt_utf8<char32_t>, char32_t >{}.to_bytes(str32); }
+
+std::wstring to_wchar_t( std::string str )
+{ return std::wstring_convert< std::codecvt_utf8<wchar_t>, wchar_t >{}.from_bytes(str); }
+
+std::string to_utf8( std::wstring wstr )
+{ return std::wstring_convert< std::codecvt_utf8<wchar_t>, wchar_t >{}.to_bytes(wstr); }
+
+int main()
+{
+    const std::string utf8 = "hello world!\n" ;
+
+    const std::u16string utf16 = to_utf16( utf8 ) ;
+    std::cout << to_utf8(utf16) ;
+
+    const std::u32string utf32 = to_utf32( utf8 ) ;
+    std::cout << to_utf8(utf32) ;
+    
+    const std::wstring wstr = to_wchar_t(utf8) ;
+    std::wcout << wstr ;
+    std::cout << to_utf8(wstr) ;
+}
