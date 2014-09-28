bbee4aef3de49eac02a71fcb78efd28d23bd3816
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-28 10:31:12 +0000

    Update archive ac

---
+++ trunk/Archive2/ac/0e70c13e7a156f/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sstream>
+
+template <typename T>
+void MagicLog(std::ostream& o, T t)
+{
+    o << t << std::endl;
+}
+
+template<typename T, typename... Args>
+void MagicLog(std::ostream& o, T t, Args... args) // recursive variadic function
+{
+    MagicLog(o, t);
+    MagicLog(o, args...);
+}
+
+template<typename... Args>
+void LogAll(int logType, Args... args)
+{
+    std::ostringstream oss;
+    MagicLog(oss, args...);
+    std::cout << oss.str();
+}
+
+int main()
+{
+  LogAll(5, "HELLO", "WORLD", 42);
+}
