9d629039355606403c7f8e5d845193f603454d57
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-28 19:55:38 +0000

    Update archive 47

---
+++ trunk/Archive2/47/5a2ead8b748be6/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << (-1)%(-2) << std::endl;
+    std::cout << ( 1)%(-2) << std::endl;
+    std::cout << (-1)%( 2) << std::endl;
+    std::cout << ( 1)%( 2) << std::endl;
+}
