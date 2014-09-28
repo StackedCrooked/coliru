c07ad8ef7d898d1d762840299d87d0170bdf4db6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-28 13:58:20 +0000

    Update archive 20

---
+++ Archive2/20/8bcc9b209fe60b/main.cpp
@@ -0,0 +1,33 @@
+// std::iterator example
+#include <iostream>     // std::cout
+#include <iterator>     // std::iterator, std::input_iterator_tag
+#include <vector>
+
+template <typename T>
+class MyIterator : public std::iterator<std::input_iterator_tag, T>
+{
+  T* p;
+public:
+  MyIterator(T* x) :p(x) {}
+  MyIterator(const MyIterator& mit) : p(mit.p) {}
+  MyIterator& operator++() {++p;return *this;}
+  MyIterator operator++(int) {MyIterator tmp(*this); operator++(); return tmp;}
+  bool operator==(const MyIterator& rhs) {return p==rhs.p;}
+  bool operator!=(const MyIterator& rhs) {return p!=rhs.p;}
+  T& operator*() {return *p;}
+  typename std::iterator_traits<int*>::difference_type operator- (const MyIterator &rhs) const { return p - rhs.p; }
+};
+
+template <typename T>
+MyIterator<T> make_myiterator(T t)
+{
+    return MyIterator<T>(t);
+}
+
+int main () {
+  int numbers[]={10,20,30,40,50};
+
+  std::vector<int> num( make_myiterator(numbers), make_myiterator(numbers + 5) );
+  
+  return 0;
+}
