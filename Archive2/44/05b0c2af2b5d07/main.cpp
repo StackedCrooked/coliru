cd986ec4015d097acea517a3948de5f9568536c3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-29 17:49:03 +0000

    Update archive 44

---
+++ Archive2/44/05b0c2af2b5d07/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+    #define IS_DEFINED(x) IS_DEFINED2(x)
+    #define IS_DEFINED2(x) (#x[0] == 0 || #x[0] == '1')
+    #define TRACE(x, y) if (IS_DEFINED(x)) { std::cout << y << std::endl; }
+
+int main()
+{
+    #define FOO
+    TRACE(FOO, "test1");
+    #undef FOO
+    TRACE(FOO, "test2");
+    #define FOO 1
+    TRACE(FOO, "test3");
+}
