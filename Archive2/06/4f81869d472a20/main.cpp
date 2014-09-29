e564db835ed7126a3ac6dc4463ba4a8ad839bb80
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-29 13:55:33 +0000

    Update archive 06

---
+++ Archive2/06/4f81869d472a20/main.cpp
@@ -0,0 +1,12 @@
+#define STRINGIZE_PRAGMA(x) _Pragma(#x)
+
+#if defined(__clang__)
+#define PRAGMA(x) STRINGIZE_PRAGMA (clang x)
+#elif defined (__GNUC__)
+#define PRAGMA(x) STRINGIZE_PRAGMA (GCC x)
+#endif
+
+PRAGMA(diagnostic push)
+PRAGMA(diagnostic ignored "-Wwhatever")
+// some code here
+PRAGMA(diagnostic pop)
