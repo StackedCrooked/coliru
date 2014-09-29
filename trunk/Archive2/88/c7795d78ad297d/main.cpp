6725be6128faa51d630b500f03fe6698902b7504
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-29 13:59:18 +0000

    Update archive 88

---
+++ trunk/Archive2/88/c7795d78ad297d/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#define TRACE_1(y) std::cout << y;
+#define TRACE_BLA(y) 
+#define TRACE_I(x,y) TRACE_ ## x (y)
+#define TRACE(x,y) TRACE_I(x,y)
+
+int main()
+{
+    #undef BLA
+    TRACE(BLA,"abc") // "abc" won't be printed, as BLA is not defined
+    #define BLA 1
+    TRACE(BLA,"xyz") // "xyz" will be printed, as BLA is a defined symbol
+    #undef BLA
+    TRACE(BLA,"abc") // "abc" won't be printed, as BLA is not defined
+}
