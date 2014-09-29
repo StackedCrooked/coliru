784bb57d4319dd6f4a968762eb9942958d71bda2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-29 16:57:19 +0000

    Update archive 8a

---
+++ trunk/Archive2/8a/daffcf0aee87e1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+template<typename T>
+struct just_cast_it
+{
+    just_cast_it(T&& t) : _t{std::forward<T>(t)} { }
+    
+    template<typename U>
+    operator U ()
+    {
+        return static_cast<U>(std::forward<T>(_t));
+    }
+    
+    T&& _t;
+};
+
+template<typename T>
+just_cast_it<T&&> just_cast_bitch(T&& t)
+{
+    return {std::forward<T>(t)};
+}
+
+int main() 
+{
+    int x = 42;
+    char y = just_cast_bitch(x);
+}
