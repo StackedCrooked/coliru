a80cee2a8f76d6c844fdd0db0d570b3413ad45da
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-30 17:38:33 +0000

    Update archive 1f

---
+++ trunk/Archive2/1f/801eadf8f5261e/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+class Singer {
+    friend std::ostream& Christmas(std::ostream& out);
+    Singer(std::ostream& out) : out(out) {}
+    std::ostream& sing(int day, int count) {
+      return day ? count <= day ? sing(day, count + 1) << (count == 1 && day > 1 ? "and " : "")
+                                                       << gift[count - 1] << '\n'
+                                : sing(day - 1, 1) << "\nOn the "
+                                                   << day
+                                                   << "th day of Christmas, "
+                                                      "my true love gave to me:\n"
+                 : out;
+    }
+  private:
+    std::ostream& out;
+    static const char *gift[12];
+};
+const char *Singer::gift[] = {
+        "a Partridge in a Pear Tree.",
+        "2 Turtle Doves,",
+        "3 French Hens,",
+        "4 Calling Birds,",
+        "5 Gold Rings,",
+        "6 Geese-a-Laying,",
+        "7 Swans-a-Swimming,",
+        "8 Maids-a-Milking,",
+        "9 Ladies Dancing,",
+        "10 Lords-a-Leaping,",
+        "11 Pipers Piping,",
+        "12 Drummers Drumming."
+};
+
+std::ostream& Christmas(std::ostream& out) { return Singer(out).sing(12,1); }
+
+int main() {
+    std::cout << Christmas;
+    return 0;
+}
