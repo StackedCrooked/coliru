d87c10699e08a1c3db071db9dabac34b5bbd80bf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-30 04:41:07 +0000

    Update archive 74

---
+++ Archive2/74/b074f8ac7c50c3/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <type_traits>
+
+template <typename T>
+struct WrapperType
+{
+    typedef T wrapped_type;
+    typedef typename wrapped_type::value_type value_type;
+    wrapped_type wrapee_;
+
+    WrapperType() : wrapee_{}
+    {}
+};
+
+template <typename... Args>
+struct Composer : public WrapperType<Args>...
+{
+    Composer() : WrapperType<Args>{}...
+    {}
+
+    void display()
+    {
+        call<Args...>();
+    }
+
+    template <typename T, typename... Ts>
+    void call()
+    {
+        WrapperType<T>::wrapee_.display();
+        call<Ts...>();
+    }
+    
+    template <typename... Ts>
+    auto call() -> typename std::enable_if<sizeof...(Ts) == 0>::type
+    {
+    }
+};
+
+struct A { using value_type = int; void display() { std::cout << "A" << std::endl; } };
+struct B { using value_type = int; void display() { std::cout << "B" << std::endl; } };
+struct C { using value_type = int; void display() { std::cout << "C" << std::endl; } };
+
+int main()
+{
+    Composer<A, B, C> myComposer{};
+    myComposer.display(); 
+}
