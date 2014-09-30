5a74a3f4fc6620c9ba5202e809de700d7783e0aa
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-30 15:08:31 +0000

    Update archive 91

---
+++ Archive2/91/fdc43a9f3f0de2/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <utility>
+
+template<class T>
+T& foo(T* p) { std::cout << "T*\n"; return *p; }
+
+/*template<class T>
+T&& foo(T&& p) { std::cout << "T&&\n"; return std::forward<T>(p); }*/
+
+template<class T>
+T& foo(T& p) { std::cout << "T&\n"; return p; }
+
+/*template<class T>
+T const& foo(T const& p) { std::cout << "T const&\n"; return p; }*/
+
+int main()
+{
+    int i;
+    int const c = 42;
+    
+    foo(int{});
+    foo(i);
+    foo(&i);
+    
+    foo(c);
+    foo(&c);
+    
+    int* p;
+    foo(p);
+}
