a1757f87f50a4684f4e267a09bd9f0705e2e61c9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-09-30 15:15:27 +0000

    Update archive 14

---
+++ Archive2/14/87dcef6078f4cc/main.cpp
@@ -0,0 +1,9 @@
+%:include <stdio.h>
+%:include <iso646.h>
+
+int main(int argc, char *argv<::>) 
+<%
+    if (argc > 1 and argv<:1:> not_eq NULL) <%
+        printf("Hello, %s\n", argv<:1:>);
+    %>
+%>
