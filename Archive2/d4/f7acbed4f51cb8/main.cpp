41140219c11fa143b2d3c11d70cce283d5c385cc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-01 21:02:38 +0000

    Update archive d4

---
+++ Archive2/d4/f7acbed4f51cb8/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+struct non_default_constructible
+{
+    non_default_constructible() = delete;
+//    int x = 3;
+};
+
+template <class T>
+struct dummy
+{
+    T new_t()
+    {
+        return T{};
+    }
+};
+
+int main()
+{
+    dummy<non_default_constructible> d;
+    auto ndc = d.new_t();
+    
+    (void)ndc;
+}
