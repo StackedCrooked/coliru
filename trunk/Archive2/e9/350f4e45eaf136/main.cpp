e45eb9085619b42682567f2d47abb5031de1a475
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-01 18:05:21 +0000

    Update archive e9

---
+++ trunk/Archive2/e9/350f4e45eaf136/main.cpp
@@ -0,0 +1,57 @@
+#include <cassert>
+#include <iostream>
+#include <type_traits>
+template <class Condition, class T = void>
+using EnableIf = typename std::enable_if<Condition::value, T>::type;
+
+template<typename... Ts>
+struct make_void { typedef void type;};
+
+template<typename... Ts>
+using ToVoid = typename make_void<Ts...>::type;
+
+template <class T, class Enable = void>
+struct has_scalar : public std::false_type {};
+
+template <class T>
+struct has_scalar<T, ToVoid<typename T::Scalar>> : public std::true_type {};
+
+template <class T, class Enable = void>
+struct has_element : public std::false_type {};
+
+template <class T>
+struct has_element<T, ToVoid<typename T::Element>> : public std::true_type {};
+
+
+template <class T, class Enable = void>
+struct scalar_of;
+
+template <class T>
+struct scalar_of<T, EnableIf<has_element<T>>> {
+    using type = typename T::Element;
+};
+
+template <class T>
+struct scalar_of<T, EnableIf<has_scalar<T>>> {
+    using type = typename T::Scalar;
+};
+
+template <class T>
+using ScalarOf = typename scalar_of<T>::type;
+
+
+struct BoostArray {
+    using Element = double;
+};
+
+struct EigenArray {
+    using Scalar = float;
+};
+
+
+int main() {
+    using std::is_same;
+    assert((is_same<double, ScalarOf<BoostArray>>::value));
+    assert((is_same<float, ScalarOf<EigenArray>>::value));
+    std::cout << "All is fine\n";
+}
