022437b5e7fa554647b5e79bca6f9e81e0afec65
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-01 01:06:08 +0000

    Update archive b2

---
+++ trunk/Archive2/b2/5952fd498f0afd/cmd.sh
@@ -0,0 +1,1 @@
+clang++ -std=c++11 -Os -Wall -pedantic -pthread -c main.cpp
---
+++ trunk/Archive2/b2/5952fd498f0afd/main.cpp
@@ -0,0 +1,28 @@
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/pair.hpp>
+#include <boost/mpl/transform.hpp>
+#include <boost/mpl/assert.hpp>
+#include <boost/mpl/equal.hpp>
+#include <boost/mpl/equal_to.hpp>
+
+
+using Keys = boost::mpl::vector<double, bool, int, char, bool*>;
+using Types = boost::mpl::vector<char, char, int, int, int>;
+
+using ExpectedOutput =                                                   
+    boost::mpl::vector<                                                         
+        boost::mpl::pair<double, char>,                                         
+        boost::mpl::pair<bool, char>,                                           
+        boost::mpl::pair<int, int>,                                            
+        boost::mpl::pair<char, int>,                                            
+        boost::mpl::pair<bool*, int>>;
+
+// now I perform some operation which I _think_ might work
+using PairsSequence =                                                           
+    typename boost::mpl::transform<                                             
+        Keys,                                                            
+        Types,
+        boost::mpl::pair<boost::mpl::_1, boost::mpl::_2>>
+        ::type;
+
+BOOST_MPL_ASSERT(( boost::mpl::equal<PairsSequence, ExpectedOutput> ));
