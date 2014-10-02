177c08842d476e43f7a3d678065d6819e3d8fda5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-02 11:04:56 +0000

    Update archive e5

---
+++ Archive2/e5/5469a4aa2d6673/main.cpp
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+ 
+int main() { 
+    int a =0;
+    int b =0;
+    while(a<10) {
+        a=a+1;
+        b=b+a;
+        
+        }
+        printf("somma  = %d" , b);
+        }
