50780d61d31a32e23eba714462f48071e8f6cede
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-02 17:27:05 +0000

    Update archive 92

---
+++ Archive2/92/ab7c43618d9501/main.cpp
@@ -0,0 +1,26 @@
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+ 
+enum 
+{
+    BLOCK1,
+    BLOCK2,
+    BLOCK3,
+    BLOCK4,
+    BLOCK5,
+    ERROR = -1
+};
+
+int main()
+{
+    vector<vector<int8_t>> HWConfig {
+        {BLOCK1, BLOCK5, ERROR},
+        {BLOCK3, BLOCK1, ERROR},
+        {BLOCK1, BLOCK2, ERROR},
+    };
+    cout << "OK!" << endl;
+    return 0;
+}
