244a04423c1c956a65cc9ae6e52a4f13466cf14a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-02 17:52:20 +0000

    Update archive e7

---
+++ trunk/Archive2/e7/db8d418bee818b/main.cpp
@@ -0,0 +1,70 @@
+#include <chrono>
+#include <iostream>
+
+
+namespace test
+{
+    struct Int64
+    {
+        Int64() noexcept : value_() {}
+        Int64(const Int64&) noexcept = default;
+        Int64& operator=(const Int64&) noexcept = default;
+        Int64(int64_t n) noexcept : value_(n) {}
+        Int64& operator=(int64_t n) noexcept { value_ = n; return *this; }
+        operator int64_t() const noexcept { return value_; }
+        int64_t value_;
+    };
+}
+    
+
+namespace std
+{   
+    template<class T>
+    struct common_type<test::Int64, T> : std::common_type<int64_t, T> { };
+    
+    template<class T>
+    struct common_type<T, test::Int64> : std::common_type<T, int64_t> {};
+    
+    template<>
+    struct common_type<test::Int64, test::Int64> : std::common_type<test::Int64> {};
+}
+
+
+namespace test
+{
+    
+    using Nanoseconds = std::chrono::duration<Int64, std::nano>;
+    using Microseconds = std::chrono::duration<Int64, std::micro>;
+    using Milliseconds = std::chrono::duration<Int64, std::milli>;
+    
+    void test_adl(Nanoseconds) { std::cout << "ADL lookup succeeded." << std::endl; }
+    
+}
+    
+
+    
+int main()
+{
+    using namespace test;
+    
+    /** test adl lookup */
+    Nanoseconds ns;
+    Microseconds us;
+    test_adl(ns);    
+    test_adl(us); // ADL + implicit conversion to Nanoseconds
+    
+    
+    /** test interoperability with std::chrono duration types */
+    std::chrono::duration_cast<std::chrono::nanoseconds>(ns);
+    std::chrono::duration_cast<std::chrono::nanoseconds>(us);
+    
+    std::chrono::duration_cast<std::chrono::seconds>(ns);
+    std::chrono::duration_cast<std::chrono::seconds>(us);
+    
+    std::chrono::nanoseconds() = Nanoseconds();
+    std::chrono::nanoseconds() = Milliseconds();
+    
+    Nanoseconds() = std::chrono::nanoseconds();
+    Nanoseconds() = std::chrono::milliseconds();
+}
+
