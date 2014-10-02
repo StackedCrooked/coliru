5f8a6b39183aadd7d8967161771c894e52de5917
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-02 21:06:28 +0000

    Update archive 4e

---
+++ Archive2/4e/eb079e3eb32451/main.cpp
@@ -0,0 +1,23 @@
+#include <type_traits>
+#include <cstddef>
+
+template <typename T, typename... Ts>
+struct Index;
+
+template <typename T, typename... Ts>
+struct Index<T, T, Ts...> : std::integral_constant<std::size_t, 0> {};
+
+template <typename T, typename U, typename... Ts>
+struct Index<T, U, Ts...> : std::integral_constant<std::size_t, 1 + Index<T, Ts...>::value> {};
+
+template <typename T, typename... Ts>
+constexpr std::size_t Index_v = Index<T, Ts...>::value;
+
+int main()
+{
+    static_assert(Index<int, int, double>::value == 0, "!");
+    static_assert(Index<double, int, double>::value == 1, "!");
+    
+    static_assert(Index_v<int, int, double> == 0, "!");
+    static_assert(Index_v<double, int, double> == 1, "!");
+}
