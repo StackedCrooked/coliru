d063503c148954cb601491313cf6bfe6dd244079
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-02 15:07:15 +0000

    Update archive c3

---
+++ trunk/Archive2/c3/6b176abe121ef0/main.cpp
@@ -0,0 +1,16 @@
+#include <numeric>
+#include <iostream>
+#include <vector>
+#include <functional>
+int main()
+{
+    std::vector<int> a{0, 1, 2, 3, 4};
+    std::vector<int> b{5, 4, 2, 3, 1};
+ 
+    int r1 = std::inner_product(a.begin(), a.end(), b.begin(), 0);
+    std::cout << "Inner product of a and b: " << r1 << '\n';
+ 
+    int r2 = std::inner_product(a.begin(), a.end(), b.begin(), 0,
+                                std::plus<int>(), std::equal_to<int>());
+    std::cout << "Number of pairwise matches between a and b: " <<  r2 << '\n';
+}
