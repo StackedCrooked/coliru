dce32aedcf911e1bdc18732843db1af569dae35a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-03 15:26:05 +0000

    Update archive d1

---
+++ Archive2/d1/8920572ca505d2/cmd.sh
@@ -0,0 +1,9 @@
+set -x
+g++ -std=c++11 -O2 -DSTORAGE_SIZE=1 main.cpp && ./a.out 
+g++ -std=c++11 -O2 -DSTORAGE_SIZE=2 main.cpp && ./a.out 
+g++ -std=c++11 -O2 -DSTORAGE_SIZE=3 main.cpp && ./a.out 
+g++ -std=c++11 -O2 -DSTORAGE_SIZE=4 main.cpp && ./a.out 
+g++ -std=c++11 -O2 -DSTORAGE_SIZE=5 main.cpp && ./a.out 
+g++ -std=c++11 -O2 -DSTORAGE_SIZE=6 main.cpp && ./a.out 
+g++ -std=c++11 -O2 -DSTORAGE_SIZE=7 main.cpp && ./a.out 
+g++ -std=c++11 -O2 -DSTORAGE_SIZE=8 main.cpp && ./a.out
