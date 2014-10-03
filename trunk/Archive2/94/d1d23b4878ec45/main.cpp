fc7eea1e954cc6e8be6ba43609b3c91c0922ab68
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-03 08:16:05 +0000

    Update archive 94

---
+++ trunk/Archive2/94/d1d23b4878ec45/main.cpp
@@ -0,0 +1,18 @@
+class Foo {
+    // here, compiler doesn't yet know what Bar is
+    union Bar {
+        int iBar;
+        double rBar;
+        bool bBar;
+    } bar; // now it does
+public:
+    static Bar getBar(Foo); // so use it
+};
+
+   Foo::Bar Foo::getBar(Foo foo) { return foo.bar; }
+   
+int main()
+{
+    Foo f, g;
+    f.getBar(g);
+}
