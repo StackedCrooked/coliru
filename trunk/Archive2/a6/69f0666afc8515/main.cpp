bd28f82e4618f14f6ffbcf3e40473e03a8d6a585
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-03 21:07:53 +0000

    Update archive a6

---
+++ trunk/Archive2/a6/69f0666afc8515/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdint>
+#include <arpa/inet.h>
+
+
+template<int bits, int multiplier = 1>
+struct Field
+{
+    Field& operator=(uint64_t n) { value = n / multiplier; return *this; }
+    operator uint64_t() const { return value * multiplier; }
+    unsigned value:bits;
+} __attribute__((packed));
+
+
+struct Net16
+{
+    operator uint16_t() const { return htons(n_); }
+    Net16& operator=(uint16_t n) { n_ = ntohs(n); return *this; }
+    uint16_t n_;
+};
+
+
+struct IPv4Header
+{
+    Field<4> Version;
+    Field<4, 4> IHL;
+    Field<8> TOS;
+    Net16 TotalLength;
+    Net16 Identificiation;
+};
+
+
+static_assert(std::is_pod<IPv4Header>::value, "");
+//static_assert(sizeof(IPv4Header) == 20, "");
+
+int main()
+{
+    IPv4Header header;
+    header.Version = 4;
+    header.IHL = 20;
+    header.TOS = 0;
+    
+    std::cout
+        << " HEADER=" << header.Version
+        << " IHL=" << header.IHL
+        << " TOS=" << header.TOS
+        << std::endl;
+}
