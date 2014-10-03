36e72be4477b3386742d52a83582a10dce5e9474
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-03 15:13:30 +0000

    Update archive ed

---
+++ Archive2/ed/c14895d74586e9/main.cpp
@@ -0,0 +1,30 @@
+#include <functional>
+#include <iostream>
+
+template <typename Arg, typename R, typename... Args>
+std::function<R(Arg, Args...)> add_ignored_arg(std::function<R(Args...)> f)
+{
+    return [&f] (Arg /*arg*/, Args... args) { return f(args...); };
+}
+
+struct B {};
+
+void foo()
+{
+    std::cout << "foo!" << std::endl;
+}
+
+void doSomething(std::function<void(B)> callback)
+{
+    callback(B{});
+}
+
+void myDoSomething(std::function<void()> callback0)
+{
+    doSomething(add_ignored_arg<B>(callback0));
+}
+
+int main()
+{
+    myDoSomething(&foo);
+}
