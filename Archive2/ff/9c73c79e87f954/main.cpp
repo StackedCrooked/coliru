2c46acd957053540a0083afbe790825b10d38027
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-03 04:09:28 +0000

    Update archive ff

---
+++ Archive2/ff/9c73c79e87f954/main.cpp
@@ -0,0 +1,77 @@
+#include <chrono>
+#include <iostream>
+
+
+namespace test
+{
+    struct Int64
+    {
+        Int64() noexcept : value_() {}
+        Int64(const Int64&) noexcept = default;
+        Int64& operator=(const Int64&) noexcept = default;
+        Int64(int64_t n) noexcept : value_(n) {}
+        Int64& operator=(int64_t n) noexcept { value_ = n; return *this; }
+        operator int64_t() const noexcept { return value_; }
+        int64_t value_;
+    };
+}
+    
+
+namespace std
+{   
+    template<class T>
+    struct common_type<test::Int64, T> : std::common_type<int64_t, T> { };
+    
+    template<class T>
+    struct common_type<T, test::Int64> : std::common_type<T, int64_t> {};
+    
+    template<>
+    struct common_type<test::Int64, test::Int64> : std::common_type<test::Int64> {};
+}
+
+
+namespace test
+{
+    
+    using Nanoseconds = std::chrono::duration<Int64, std::nano>;
+    using Microseconds = std::chrono::duration<Int64, std::micro>;
+    using Milliseconds = std::chrono::duration<Int64, std::milli>;
+    
+    void test_adl(Nanoseconds) { std::cout << "ADL lookup succeeded." << std::endl; }
+    
+}
+    
+
+    
+int main()
+{
+    using test::Nanoseconds;
+    using test::Microseconds;
+    using test::Milliseconds;
+    
+    /** test adl lookup */
+    Nanoseconds ns;
+    Microseconds us;
+    test_adl(ns);    
+    test_adl(us); // ADL + implicit conversion to Nanoseconds
+    
+    
+    /** test interoperability with std::chrono duration types */
+    using std::chrono::nanoseconds;
+    using std::chrono::milliseconds;
+    using std::chrono::seconds;
+    using std::chrono::duration_cast;
+    
+    duration_cast<nanoseconds>(Nanoseconds());
+    duration_cast<nanoseconds>(Microseconds());
+    
+    duration_cast<seconds>(Nanoseconds());
+    duration_cast<seconds>(Microseconds());
+    
+    nanoseconds() = Nanoseconds();
+    nanoseconds() = Milliseconds();
+    
+    Nanoseconds() = nanoseconds();
+    Nanoseconds() = milliseconds();
+}
+
