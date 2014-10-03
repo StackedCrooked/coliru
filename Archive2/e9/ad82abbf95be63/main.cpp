f15c8eb056ff6e168c3bab01f9d0920532e75afc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-03 21:23:04 +0000

    Update archive e9

---
+++ Archive2/e9/ad82abbf95be63/main.cpp
@@ -0,0 +1,24 @@
+#include <chrono>
+#include <iostream>
+
+template <typename T, class Rep, class Period>
+constexpr T(*DurationCast)(const std::chrono::duration<Rep, Period>&) = &std::chrono::duration_cast<T, Rep, Period>;
+
+template <typename T>
+void foo()
+{
+    std::cout << "foo!" << (T)3.14 << std::endl;
+}
+
+template <typename T>
+constexpr void(*foo_alias)() = &foo<T>;
+
+int main()
+{
+    std::chrono::seconds s(1);
+    std::chrono::milliseconds ms = DurationCast<std::chrono::milliseconds, float, std::ratio<1>>(s);    
+    std::cout << "ms: " << ms.count() << std::endl;
+    
+    foo_alias<int>();
+    foo_alias<float>();
+}
