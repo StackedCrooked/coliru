2b773f768ea9d5a917c0b507c605adcb86148217
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-03 21:21:32 +0000

    Update archive a8

---
+++ Archive2/a8/c89e17b5efd0b8/main.cpp
@@ -0,0 +1,39 @@
+#include <boost/multiprecision/cpp_int.hpp>
+#include <string>
+#include <deque>
+#include <type_traits>
+#include <iostream>
+
+template < std::size_t BASE, 
+           typename DIGIT = typename std::conditional< BASE <= ( std::numeric_limits<unsigned char>::max() + 1 ), unsigned char, std::uint16_t >::type > 
+std::deque<DIGIT> to_base( const std::string& unsigned_decimal )
+{
+    static_assert( BASE > 1 && BASE <= ( std::numeric_limits<DIGIT>::max() + 1 ), "" ) ;
+    std::deque<DIGIT> result ;
+    
+    boost::multiprecision::cpp_int number(unsigned_decimal) ;
+    while( number != 0 ) 
+    {
+        // std::cout << number << '\n' ;
+        const boost::multiprecision::cpp_int digit = number % BASE ;
+        result.push_front( digit.convert_to<DIGIT>() ) ;
+        number /= BASE ;
+    }
+    
+    std::cout << std::dec << "\n\n" << result.size() << " digits to base " << BASE << " == " 
+              << result.size() * sizeof(DIGIT) << " bytes\n" ;
+    return result ;
+}
+
+int main()
+{
+    const std::string number =  "1234565789012345657890123456578901234565789012345657890123456578901234565789012345657890"
+                                "1234565789012345657890123456578901234565789012345657890123456578901234565789012345657890" 
+                                "1234565789012345657890123456578901234565789012345657890123456578901234565789012345657890" 
+                                "1234565789012345657890123456578901234565789012345657890123456578901234565789012345657890" 
+                                "1234565789012345657890123456578901234565789012345657890123456578901234565789012345657890" ;
+                                
+    for( auto code_point : to_base<64>(number)  ) std::cout << std::hex << int(code_point) << ' ' ;
+    for( auto code_point : to_base<256>(number)  ) std::cout << std::hex << int(code_point) << ' ' ;
+    for( auto code_point : to_base<65536>(number)  ) std::cout << std::hex << int(code_point) << ' ' ;
+}
