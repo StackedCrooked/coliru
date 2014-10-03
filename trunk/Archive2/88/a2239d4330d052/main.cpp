4b6b199922de5d665154acc3a5dfe4ff7ec2f4f4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-03 21:35:22 +0000

    Update archive 88

---
+++ trunk/Archive2/88/a2239d4330d052/main.cpp
@@ -0,0 +1,15 @@
+#include <studio.h>
+main()
+{
+      printf("     **\n");
+      printf("    ****\n");
+      printf("   ******\n");
+      printf("  ********\n");
+      printf(" **********\n");
+      printf("  *     *\n");
+      printf("  *      *\n");    
+      printf("   *    *\n");
+      printf("    ****\n");
+      
+      printf("\n\n");
+      system("pause");
