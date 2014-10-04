459907146ca36104a256eb1da5e36f138ee373fa
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-04 00:37:10 +0000

    Update archive 64

---
+++ trunk/Archive2/64/f9baa6cac2b128/main.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <string>
+#include <vector>
+
+int main()
+{
+    const int n = 5; 
+    int a[n]; 
+    constexpr int n2 = 5; 
+    int b[n];
+    
+    return 0;
+}
