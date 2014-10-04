aeaffbdb5ebdc9d079604bc80196db785d9588d7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-04 04:08:09 +0000

    Update archive b5

---
+++ trunk/Archive2/b5/002d31059169d4/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "hello/nobody here" << std::endl;
+    std::cout << "hello?/nobody here?" << std::endl;
+    std::cout << "hello??/nobody here??" << std::endl;
+}
