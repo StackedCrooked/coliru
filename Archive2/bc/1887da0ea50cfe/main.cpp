d5f9ac4989989200f3ad7dd2ef57e9d082aa359b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-04 21:23:52 +0000

    Update archive bc

---
+++ Archive2/bc/1887da0ea50cfe/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+constexpr int giveMeValue() { return 42; }
+
+struct TryMe {
+  static constexpr int arr[1] = {
+      giveMeValue()
+  };  
+};
+
+int main() {
+    int val = TryMe::arr[0];
+    std::cout << val;
+}
