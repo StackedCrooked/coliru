46008bcabe244c7b77a81abb577acecd33659601
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-05 14:58:52 +0000

    Update archive e3

---
+++ trunk/Archive2/e3/502897d20b4660/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <type_traits>
+
+namespace detail
+{
+    template<typename T, 
+             typename U, 
+             bool = std::is_signed<T>::value, 
+             bool = std::is_signed<U>::value>
+    struct less_than_helper;
+
+    template<typename T, typename U, bool SameSign> 
+    struct less_than_helper<T, U, SameSign, SameSign> 
+    {
+        static bool less_than(T x, U y) { return x < y; }
+    };
+
+    template<typename T, typename U> 
+    struct less_than_helper<T, U, true, false> 
+    {
+        static bool less_than(T x, U y) 
+        {
+            return (x < 0) || 
+                   (static_cast<typename std::make_unsigned<T>::type>(x) < y);
+        }
+    };
+
+    template<class T, typename U> 
+    struct less_than_helper<T, U, false, true> 
+    {
+        static bool less_than(T x, U y) 
+        {
+            return (y > 0) && 
+                   (x < static_cast<typename std::make_unsigned<U>::type>(y));
+        }
+    };
+}
+
+template<class T, class U>
+inline bool less_than(T x, U y) 
+{
+    return detail::less_than_helper<T, U>::less_than(x, y);
+}
+
+int main(int argc, char** argv) 
+{
+    std::cout << less_than(1, 2u) << "\n";
+    return 0;
+}
