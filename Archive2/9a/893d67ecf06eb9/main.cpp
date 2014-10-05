ca4dada8411774e074dbb8880b1a075afa8d349f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-05 21:23:10 +0000

    Update archive 9a

---
+++ Archive2/9a/893d67ecf06eb9/main.cpp
@@ -0,0 +1,17 @@
+ #include <iostream> 
+using namespace std;
+
+void main()
+{
+ char Sasha = 'a';
+  
+ printf("%c", Sasha);
+
+ //Sasha = Sasha * 100;
+ //int vobla = Sasha;
+ //cout << vobla;
+
+
+ char lol;
+ cin >> lol;
+}
