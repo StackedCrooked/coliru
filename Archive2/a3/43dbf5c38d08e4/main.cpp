88b41acafb8d97ceaec4e8cfae86d5ab93597d6e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-06 17:32:30 +0000

    Update archive a3

---
+++ Archive2/a3/43dbf5c38d08e4/main.cpp
@@ -0,0 +1,73 @@
+#include <type_traits>
+#include <initializer_list>
+#include <tuple>
+
+template<typename T, typename U, typename = void>
+struct erases_parameter : std::false_type { };
+
+template<typename T, typename U>
+struct erases_parameter<T, U, decltype(U{std::declval<T>()}, void())> 
+    : std::true_type
+{ };
+
+constexpr auto all(std::initializer_list<bool> v)
+{
+    for (auto b : v) { if (!b) { return false; } }
+    
+    return true;
+}
+
+template<typename... Ts>
+struct erases_all_parameters;
+
+template<typename... Ts, typename... Us>
+struct erases_all_parameters<std::tuple<Ts...>, std::tuple<Us...>>
+    :  std::integral_constant<bool, all({erases_parameter<Ts, Us>{}...})> 
+{ };
+
+template<typename T, typename U>
+using erases_return_type = 
+      std::integral_constant<bool, 
+                             std::is_same<T, void>{} || 
+                             erases_parameter<U, T>{}>;
+
+template<typename S1, typename S2>
+struct erases_signature;
+
+template<typename R1, typename R2, typename... Params1, typename... Params2>
+struct erases_signature<R1(Params1...), R2(Params2...)>
+    : std::integral_constant<
+          bool, 
+          erases_all_parameters<std::tuple<Params1...>, 
+                                std::tuple<Params2...>>{} &&
+          erases_return_type<R1, R2>{}>
+{ };
+
+int main()
+{    
+    static_assert(erases_parameter<int&, int>{}, "!");
+    static_assert(erases_parameter<int&, int const>{}, "!");
+    static_assert(erases_parameter<int&, int const&>{}, "!");
+    static_assert(!erases_parameter<int&, int&&>{}, "!");
+
+    static_assert(erases_parameter<int&&, int>{}, "!");
+    static_assert(erases_parameter<int&&, int const>{}, "!");
+    static_assert(erases_parameter<int&&, int const&>{}, "!");
+    static_assert(!erases_parameter<int&&, int&>{}, "!");
+    
+    static_assert(erases_all_parameters<std::tuple<int&&, int&>, 
+                                        std::tuple<int, int const&>>{}, "!");
+    
+    static_assert(erases_return_type<int&&, int>{}, "!");
+    
+    static_assert(erases_signature<void(int&), int*(const int)>{}, "!");
+    static_assert(erases_signature<void(int&&), int*(int const&)>{}, "!");
+    static_assert(erases_signature<int&&(int&), int(int const)>{}, "!");
+    
+    static_assert(!erases_signature<int&(int&), int(int)>{}, "!");
+}
+
+
+
+
+
