8c8ee565314b43dea0d5695844172817796a99a3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-06 07:40:53 +0000

    Update archive 63

---
+++ Archive2/63/8d698db1a74ea5/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+
+template<class Child>
+class Parent {
+ private: 
+  std::vector<std::unique_ptr<Child>> children_;
+ protected:
+  ~Parent() = default;
+ public:
+  const Child* getChild(size_t child_number) const { 
+    return children_.at(child_number).get(); 
+  } 
+  Child* getChild(size_t child_number) {
+    return children_.at(child_number).get(); 
+  } 
+  size_t getNumberOfChildren() const {
+    return children_.size();
+  }
+  void addChild(std::unique_ptr<Child> child) {
+    children_.emplace_back(std::move(child));
+  }
+};
+
+struct Node : Parent<Node> {
+ private:
+  std::string name_;
+ public:
+  Node(std::string name) : name_(std::move(name)) {}
+  void print() const { std::cout << "Node: " << name_  << "\n";}
+  void setName(const std::string& name) { name_ = name; }
+  void wrong() const {
+    //children_[0]->setName("Wrong"); // Not allowed
+    //getChild(0)->setName("Wrong"); // Not allowed
+  }
+};
+
+void printRecursive(const Node* node) {
+  if (node) {
+    node->print();
+    for (size_t i=0; i!=node->getNumberOfChildren(); ++i)
+      printRecursive(node->getChild(i)); 
+  }
+}
+
+int main() {
+  // Initialization
+  Node root("Root");
+  root.addChild(std::make_unique<Node>("Child 1"));
+  root.addChild(std::make_unique<Node>("Child 2"));
+  
+  // "Computation" with pointer-to-const
+  const Node* root_ptr = &root;
+  printRecursive(root_ptr);
+}
