c3bfc690362f4a0769e5689975605ab981bfa86e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-06 00:58:22 +0000

    Update archive e2

---
+++ trunk/Archive2/e2/e6d5952a986706/main.cpp
@@ -0,0 +1,59 @@
+template <typename T>
+struct return_type;
+
+template <typename R, typename... Args>
+struct return_type<R(Args...)> { using type = R; };
+
+template <typename R, typename... Args>
+struct return_type<R(*)(Args...)> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...)> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) &> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) &&> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) const> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) const&> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) const&&> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) volatile> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) volatile&> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) volatile&&> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) const volatile> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) const volatile&> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) const volatile&&> { using type = R; };
+
+template <typename T>
+using return_type_t = typename return_type<T>::type;
+
+#include <type_traits>
+
+struct A
+{
+    int some_function(int i) { return 1; }
+};
+
+int main()
+{
+    static_assert(std::is_same<return_type_t<decltype(&A::some_function)>, int>::value, "!");
+}
