8f79a48099534b8c0b5c37e90d368ee2fb1976a5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-06 17:28:28 +0000

    Update archive 50

---
+++ Archive2/50/4a7b24ac4f6fb7/main.cpp
@@ -0,0 +1,75 @@
+#include <type_traits>
+#include <initializer_list>
+#include <tuple>
+
+template<typename T, typename U, typename = void>
+struct erases_param : std::false_type 
+{ 
+};
+
+template<typename T, typename U>
+struct erases_param<T, U, decltype(U{std::declval<T>()}, void())> 
+    : std::true_type
+{
+};
+
+constexpr auto all(std::initializer_list<bool> v)
+{
+    for (auto b : v) { if (!b) { return false; } }
+    
+    return true;
+}
+
+template<typename... Ts>
+struct erases_all_params;
+
+template<typename... Ts, typename... Us>
+struct erases_all_params<std::tuple<Ts...>, std::tuple<Us...>>
+    :  std::integral_constant<bool, all({erases_param<Ts, Us>{}...})>
+{
+};
+
+template<typename T, typename U>
+using erases_return = 
+      std::integral_constant<bool, 
+                             std::is_same<T, void>{} || erases_param<U, T>{}>;
+
+template<typename S1, typename S2>
+struct erases;
+
+template<typename R1, typename R2, typename... Params1, typename... Params2>
+struct erases<R1(Params1...), R2(Params2...)>
+    : std::integral_constant<
+          bool, 
+          erases_all_params<std::tuple<Params1...>, std::tuple<Params2...>>{} &&
+          erases_return<R1, R2>{}>
+{
+};
+
+int main()
+{    
+    static_assert(erases_param<int&, int>{}, "!");
+    static_assert(erases_param<int&, int const>{}, "!");
+    static_assert(erases_param<int&, int const&>{}, "!");
+    static_assert(!erases_param<int&, int&&>{}, "!");
+
+    static_assert(erases_param<int&&, int>{}, "!");
+    static_assert(erases_param<int&&, int const>{}, "!");
+    static_assert(erases_param<int&&, int const&>{}, "!");
+    static_assert(!erases_param<int&&, int&>{}, "!");
+    
+    static_assert(erases_all_params<std::tuple<int&&, int&>, std::tuple<int, int const&>>{}, "!");
+    
+    static_assert(erases_return<int&&, int>{}, "!");
+    
+    static_assert(erases<void(int&), int*(const int)>{}, "!");
+    static_assert(erases<void(int&&), int*(int const&)>{}, "!");
+    static_assert(erases<int&&(int&), int(int const)>{}, "!");
+    
+    static_assert(!erases<int&(int&), int(int)>{}, "!");
+}
+
+
+
+
+
