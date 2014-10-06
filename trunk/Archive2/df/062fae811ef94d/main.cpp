ff419c03d5768e5a87e2ae3d963451db4eb1f872
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-06 14:39:03 +0000

    Update archive df

---
+++ trunk/Archive2/df/062fae811ef94d/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+auto add = [](auto x, auto y)
+{
+    return x + y;
+};
+auto sub = [](auto x, auto y)
+{
+    return x - y;
+};
+
+auto apply = [](auto f, auto... args)
+{
+    return f(args...);
+};
+
+auto print = [](auto x)
+{
+    cout << x << endl;
+};
+
+int main()
+{
+    
+    print(apply(apply, apply, apply, add, 1, 2));
+    print(apply(apply, apply, sub, 11, 2));
+}
