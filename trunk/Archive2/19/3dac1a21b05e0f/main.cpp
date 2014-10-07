61b1f2faaea989b0ea7bd2e18d4d06a02e79fcc1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-07 08:36:18 +0000

    Update archive 19

---
+++ trunk/Archive2/19/3dac1a21b05e0f/main.cpp
@@ -0,0 +1,9 @@
+#include “testAppendages”
+//Naill Test
+Int main()
+{
+    If ( IsHandsWorking () && IsLegsWorking && IsEyesWorking() )
+    {
+         Cout <<  “Pour it the ***fuck*** YOURSELF!!!!”  << endl;
+    }
+}
