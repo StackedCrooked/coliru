93f3e5c73995213925e3c1f6bf5a458d025c9b0f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-07 17:24:27 +0000

    Update archive 7e

---
+++ Archive2/7e/f381786a30cdc6/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void gfserialToReg(void)
+{
+
+
+}
+
+int main()
+{
+    double i = 6546;
+    double j, k = 0;
+    
+    j = i / 10;
+    
+    printf("%f", j);
+    
+    
+    return (0);
+}
