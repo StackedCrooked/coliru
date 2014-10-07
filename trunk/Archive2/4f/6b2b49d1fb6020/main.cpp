e50c6c7b5d97415adc3ed31c24d1b110f5f1fd5f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-07 17:49:11 +0000

    Update archive 4f

---
+++ trunk/Archive2/4f/6b2b49d1fb6020/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+int main()
+{
+    int i = 1 ;
+    struct A 
+    { 
+        int i ; 
+        int j ; 
+        A( int a, int b ) : i(a), j(b) {} 
+        A& operator++() { return *this ; };
+        A operator++(int) { return *this ; };
+        A operator+ (A) { return *this ; }
+    };
+    
+    ///////////  well-defined behaviour //////////////////////
+    
+    i = i + 1 ; // well defined behaviour (same as below)
+    i += 1 ; // well defined behaviour
+    
+    i = ++i ; // well defined behaviour (same as below)
+    i = ( i += 1 ) ; // well defined behaviour (same as below)
+    i = ( i = i + 1 ) ; // well defined behaviour
+
+    ++++i ; // well defined behaviour (same as below)
+    ( i += 1 ) += 1 ; // well defined behaviour  (same as below)
+    ( i = i + 1 ) += 1 ; // well defined behaviour  (same as below)
+    i = ( i = i + 1 ) + 1 ; // well defined behaviour
+    
+    ++++++i ; // well defined behaviour (same as below)
+    ( ( i += 1 ) += 1 ) += 1 ; // well defined behaviour
+    
+    int a[] = { i, ++i, --i } ; // well defined behaviour
+    
+    A a1 { ++i, ++i } ; // well defined behaviour (compare and contrast with line 42)
+    
+    A a2 = a1++ + ++a1 ; // no undefined behaviour (indeterminately sequenced != undefined behaviour)
+    
+    
+    
+    /////////// undefined behaviour //////////////////////////
+    
+    i = i++ ; // **** undefined behaviour (unsequenced modifications) ****
+    
+    i += ++i ; // **** undefined behaviour (unsequenced modification and access) ****
+    
+    std::cout << i << ++i ; // **** undefined behaviour (unsequenced modification and access) ****
+    
+    std::cout <<  ++i << ++i ; // **** undefined behaviour (unsequenced modifications) ****
+    
+    A a3( ++i, ++i ) ; // **** undefined behaviour (unsequenced modifications) ****
+    
+    void foo( int, int ) ; foo( i, ++i ) ; // **** undefined behaviour (unsequenced modification and access) ****
+}
