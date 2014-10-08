9e11d7749c9d6c53aec25bd967c11f2b8d441ba8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-08 01:23:07 +0000

    Update archive 0b

---
+++ Archive2/0b/7a1300527ef681/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+    A() { cout << "A()" << endl; }
+    ~A() { cout << "~A()" << endl; }
+};
+
+void f_noexcept() noexcept {
+    A a;
+    throw 1;
+}
+
+void f_emptythrow() throw() {
+    A a;
+    throw 1;
+}
+
+int main() {
+   f_noexcept();
+     //   f_emptythrow();
+}
