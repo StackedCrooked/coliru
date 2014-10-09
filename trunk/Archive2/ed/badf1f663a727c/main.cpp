53f1238a2586bb4a3dd1675b33cf00fdbd944b11
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-09 12:16:32 +0000

    Update archive ed

---
+++ trunk/Archive2/ed/badf1f663a727c/main.cpp
@@ -0,0 +1,28 @@
+#include <tuple>
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+struct Foo {
+    int a;
+    string b;
+    
+    operator tuple<int&,string&>() const {
+        // Hack: get a non-const reference to our const members
+        return tie(*const_cast<int*>(&a),
+                   *const_cast<string*>(&b));
+    }
+};
+
+
+int main() {
+    const Foo foo { 42, "bar" };
+    
+    int a;
+    string b;
+    
+    tie(a, b) = foo;
+    
+    cout << a << ", " << b << endl;
+}
