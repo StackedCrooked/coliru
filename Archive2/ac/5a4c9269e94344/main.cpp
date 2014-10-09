96f28c0a320533fe8bd46649a6367d5fbd4b9c75
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-09 21:44:14 +0000

    Update archive ac

---
+++ Archive2/ac/5a4c9269e94344/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <regex>
+#include <string>
+using namespace std;
+int main()
+{
+    //int year, dayOfYear, day;
+	//std::string month, leftParenthesis, rightParenthesis;
+	std::string ExampleString = "2013 336 (02 DEC) 04";
+	regex pattern("\\s*(\\d+)\\s+(\\d+)\\s*\\((\\d+)\\s+([[:alpha:]]+)\\)\\s*(\\d+)\\s*");
+
+	// Matching single string
+	std::smatch sm;
+	if (std::regex_match(ExampleString, sm, pattern)) {
+		cout << "year: " << sm[1].str() << endl;
+		cout << "dayOfYear: " << sm[2].str() << endl;
+		cout << "day: " << sm[3].str() << endl;
+		cout << "month: " << sm[4].str() << endl;
+		cout << "hour: " << sm[5].str() << endl;
+	}
+
+	cout << endl;
+	cout << endl;
+
+	// If your data contains multiple lines to parse, use this version
+	// unfortunately it will skip all lines that does not match pattern.
+	ExampleString = "2013 336 (02 DEC) 04" "\n2014 336 (02 DEC) 04" "\n2015 336 (02 DEC) 04";
+	for (sregex_iterator it(ExampleString.begin(), ExampleString.end(), pattern), end_it;
+		it != end_it; ++it)
+	{
+		cout << "year: " << (*it)[1].str() << endl;
+		cout << "dayOfYear: " << (*it)[2].str() << endl;
+		cout << "day: " << (*it)[3].str() << endl;
+		cout << "month: " << (*it)[4].str() << endl;
+		cout << "hour: " << (*it)[5].str() << endl;
+		cout << endl;
+	}
+}
