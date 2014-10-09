b3088d63347834ad06bb925a48900a082f3f016f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-09 15:22:35 +0000

    Update archive 37

---
+++ Archive2/37/eec3625ca46dd3/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <functional>
+#include <numeric>
+
+int main () {
+  int val[] = {1,2,3,5,9,11,12};
+  int result[7];
+
+  std::adjacent_difference (val, val+7, result);
+  for(auto i : result) std::cout << i << " ";
+  std::cout << std::endl;
+}
