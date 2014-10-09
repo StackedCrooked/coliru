429f781429388ad3434c36a86409c73bf743f019
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-09 00:56:52 +0000

    Update archive 09

---
+++ Archive2/09/d5051a396c4291/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <limits>
+
+template<typename A, typename B>
+bool lt(A a, B b) {
+    if (a < 0 && b >= 0)
+        return true;
+    if (a >= 0 && b < 0)
+        return false;
+    using TYPE = decltype(a + b);
+    return (TYPE)a < (TYPE)b;
+}
+
+template<typename X,typename Y,typename Z >
+void checkParameter( X val, Y lower, Z upper) {
+    if (lt(upper, val))
+        std::cout << val << " > " << upper << std::endl;
+    if (lt(val, lower))
+        std::cout << val << " < " << lower << std::endl;
+}
+
+
+int main() {
+    checkParameter( std::numeric_limits<int>::min(), 0u, std::numeric_limits< int>::max());
+    checkParameter(20u, 10, 30);
+}
