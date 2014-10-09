f50622e071e074544062303141f15a2a254a54c5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-09 12:00:33 +0000

    Update archive f4

---
+++ Archive2/f4/1d540128514838/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main() {
+ 
+    std::string array[] = {"One", "Two", "Three"};
+    int num_strings = sizeof(array)/sizeof(array[0]);
+ 
+    cout << "sizeof(array): " << sizeof(array) << endl;
+    cout << "sizeof(array[0]): " << sizeof(array[0]) << endl;
+    cout << "sizeof(array[1]): " << sizeof(array[1]) << endl;
+    cout << "sizeof(array[2]): " << sizeof(array[2]) << endl;
+ 
+    cout << num_strings << endl;
+	return 0;
+}
