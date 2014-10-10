4cc722aec3a066293e05d559ebc86d30bf690764
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-10 05:03:21 +0000

    Update archive 07

---
+++ trunk/Archive2/07/81764beb357213/main.cpp
@@ -0,0 +1,298 @@
+SUCCESS: (((((c&31)+74)+h)*311)+0)%441
+306 
+Sn)Cu&V$U#Mo#Rf#Sg&Cs#Y+FTa%Rb)S'Nd#GaK&Mg'Zr$PtPm%ReUuo#SmDy(Ac$Lu%W&CaRa(C
+f)EuB#Ds%Md$Uus*NpIn$H&YbIr*BeEs*Tc#I(FlRuC#ThSrBh/NaCoLrKr&Nb$CePb$Ne'Am)At*PdL
+a#Tl%HgMt,CrTbBk$Rh&Rn4TeZn$HfAg%Fm)Xe$AlScFePo$As'HeO#LvN&DbGe#Ho&Mn$Cd+Ni$Rg$H
+sBr$AuSi#Pr&Uup#Se*Ti#Tm$Er$Sb&PPu&Cm$GdBa'Cn&UutLiFr#Ar#Bi#NoOs%Pa4Cl
+
+
+SUCCESS: (((((c&31)+h)*311)+82)+0)%441
+306
+Sn)Cu&V$U#Mo#Rf#Sg&Cs#Y+FTa%Rb)S'Nd#GaK&Mg'Zr$PtPm%ReUuo#SmDy(Ac$Lu%W&CaRa(C
+f)EuB#Ds%Md$Uus*NpIn$H&YbIr*BeEs*Tc#I(FlRuC#ThSrBh/NaCoLrKr&Nb$CePb$Ne'Am)At*PdL
+a#Tl%HgMt,CrTbBk$Rh&Rn4TeZn$HfAg%Fm)Xe$AlScFePo$As'HeO#LvN&DbGe#Ho&Mn$Cd+Ni$Rg$H
+sBr$AuSi#Pr&Uup#Se*Ti#Tm$Er$Sb&PPu&Cm$GdBa'Cn&UutLiFr#Ar#Bi#NoOs%Pa4Cl
+
+SUCCESS: (((((c&31)+56)+h)*452)+0)%515
+320 
+#BiGd%ReKDs&Te#Ti'Tc#Se#Po)Pr(WN#Eu%Ir/Li.Rn*BeCrPm$Fm%U*No'Lu&HgCnRf%La
+%HeSn$Mn$I#Nd'Ru%Lr'GaKr$Er'HoOV'Ge'Fe)Pb'Sb+Zn$BrAc+H&PaAl&Cd#Uup%Ar$P,XeInFAuS
+'NiBh%Cf,Es%Uus$At*Tm'B%Ne#Sr&Tb#Np#Ce%Fr'Rb#CmCl&Ra)Os+Uuo(Sm&Th#Bk'Ag#CaFl#Sg#
+Tl&Lv&Co&As'Uut%Rg&Sc&Nb#Mt$Rh(Na*AmCs#Zr(Mg)Hf#Yb$Pu0Db#Si&Dy&C%BaPd'Cu&Mo*Pt%T
+a)Y#HsMd
+
+SUCCESS: (((((c&31)+h)*452)+77)+0)%515
+320 
+#BiGd%ReKDs&Te#Ti'Tc#Se#Po)Pr(WN#Eu%Ir/Li.Rn*BeCrPm$Fm%U*No'Lu&HgCnRf%La%HeS
+n$Mn$I#Nd'Ru%Lr'GaKr$Er'HoOV'Ge'Fe)Pb'Sb+Zn$BrAc+H&PaAl&Cd#Uup%Ar$P,XeInFAuS'NiB
+h%Cf,Es%Uus$At*Tm'B%Ne#Sr&Tb#Np#Ce%Fr'Rb#CmCl&Ra)Os+Uuo(Sm&Th#Bk'Ag#CaFl#Sg#Tl&L
+v&Co&As'Uut%Rg&Sc&Nb#Mt$Rh(Na*AmCs#Zr(Mg)Hf#Yb$Pu0Db#Si&Dy&C%BaPd'Cu&Mo*Pt%Ta)Y#
+HsMd
+
+SUCCESS: (((((c&31)+h)*171)+508)+0)%541
+317 
+'Sr$Cd#Pm,Fr#Rn#Ru%Cm%Os$ISPtP&Ir$Np#Bk&No%GdHeRg'Mn'Rf&As%Fe%Dy#CrHPuZr$Te2
+PdBa,Es#Y/Sc(Al(CeMt2XeU#Ds+HgCu#Mo%SmHf#Ar*Bh#Th#Tc%Pb'LaTi%NBe&Sn,Eu#Co+Ra>Po$
+InCs&Fl&Au6Sg$Uuo%Hs#Db(At#B&Pa#Nb0Rh&Tl)Lu#Cf,Lv&Uup'ZnCl(Ag%Si#PrGe%Fm'GaNa#Kr
+Bi,O$Ta/AmMdTm(Uus*V#C#Br#Cn#F$Nd%K%Re$Ho%Rb(WNiCaAcUut&Tb#Ne#Sb#Yb(Lr%Li*SeEr$M
+g
+
+
+SUCCESS: (((((c&31)+332)+h)*171)+0)%541
+317 
+'Sr$Cd#Pm,Fr#Rn#Ru%Cm%Os$ISPtP&Ir$Np#Bk&No%GdHeRg'Mn'Rf&As%Fe%Dy#CrHPuZr$Te2
+PdBa,Es#Y/Sc(Al(CeMt2XeU#Ds+HgCu#Mo%SmHf#Ar*Bh#Th#Tc%Pb'LaTi%NBe&Sn,Eu#Co+Ra>Po$
+InCs&Fl&Au6Sg$Uuo%Hs#Db(At#B&Pa#Nb0Rh&Tl)Lu#Cf,Lv&Uup'ZnCl(Ag%Si#PrGe%Fm'GaNa#Kr
+Bi,O$Ta/AmMdTm(Uus*V#C#Br#Cn#F$Nd%K%Re$Ho%Rb(WNiCaAcUut&Tb#Ne#Sb#Yb(Lr%Li*SeEr$M
+g
+
+
+SUCCESS: (((((c&31)+h)+332)*171)+0)%541
+317 
+'Sr$Cd#Pm,Fr#Rn#Ru%Cm%Os$ISPtP&Ir$Np#Bk&No%GdHeRg'Mn'Rf&As%Fe%Dy#CrHPuZr$Te2
+PdBa,Es#Y/Sc(Al(CeMt2XeU#Ds+HgCu#Mo%SmHf#Ar*Bh#Th#Tc%Pb'LaTi%NBe&Sn,Eu#Co+Ra>Po$
+InCs&Fl&Au6Sg$Uuo%Hs#Db(At#B&Pa#Nb0Rh&Tl)Lu#Cf,Lv&Uup'ZnCl(Ag%Si#PrGe%Fm'GaNa#Kr
+Bi,O$Ta/AmMdTm(Uus*V#C#Br#Cn#F$Nd%K%Re$Ho%Rb(WNiCaAcUut&Tb#Ne#Sb#Yb(Lr%Li*SeEr$M
+g
+
+SUCCESS: (((((c&31)+h)+136)*323)+0)%546
+318 #CHe*Be'P#Ga+Mn,FePoN-RfNb'LuCaDyZn$VLa'W$Kr#Ru&Y0Fl(Zr&Rg#Co)MdOs$Nd$HfCe#H
+g#Pu,Tb$Al.BrSm'U&Ne)Ba'Re&RbXe$Pa%Uus,Mo&Cm#ArCdSb$FYb)Ho#GdO$Uuo#Sr#Sn$TmCu)Fm
+$IAg#Cf#Lr*RnTi&Es%Sg'Pb#Bi(CsNi*Ds#Np&Pt.S0Bk(SiUup%Ac,Lv*Tl%CnIr&Rh%Tc#Mg-H$Pd
+#As2Eu)Cl%Ra)Li$Te$Ta$No$Th#Au%Cr#Db$Uut$Hs%Se%AmAtBMt(ScFr$Pm#Bh(Ge'Na$In8Pr$Er
+,K
+
+SUCCESS: (((((c&31)+h)*323)+248)+0)%546
+318 #CHe*Be'P#Ga+Mn,FePoN-RfNb'LuCaDyZn$VLa'W$Kr#Ru&Y0Fl(Zr&Rg#Co)MdOs$Nd$HfCe#H
+g#Pu,Tb$Al.BrSm'U&Ne)Ba'Re&RbXe$Pa%Uus,Mo&Cm#ArCdSb$FYb)Ho#GdO$Uuo#Sr#Sn$TmCu)Fm
+$IAg#Cf#Lr*RnTi&Es%Sg'Pb#Bi(CsNi*Ds#Np&Pt.S0Bk(SiUup%Ac,Lv*Tl%CnIr&Rh%Tc#Mg-H$Pd
+#As2Eu)Cl%Ra)Li$Te$Ta$No$Th#Au%Cr#Db$Uut$Hs%Se%AmAtBMt(ScFr$Pm#Bh(Ge'Na$In8Pr$Er
+,K
+
+SUCCESS: (((((c&31)+136)+h)*323)+0)%546
+318 
+#CHe*Be'P#Ga+Mn,FePoN-RfNb'LuCaDyZn$VLa'W$Kr#Ru&Y0Fl(Zr&Rg#Co)MdOs$Nd$HfCe#H
+g#Pu,Tb$Al.BrSm'U&Ne)Ba'Re&RbXe$Pa%Uus,Mo&Cm#ArCdSb$FYb)Ho#GdO$Uuo#Sr#Sn$TmCu)Fm
+$IAg#Cf#Lr*RnTi&Es%Sg'Pb#Bi(CsNi*Ds#Np&Pt.S0Bk(SiUup%Ac,Lv*Tl%CnIr&Rh%Tc#Mg-H$Pd
+#As2Eu)Cl%Ra)Li$Te$Ta$No$Th#Au%Cr#Db$Uut$Hs%Se%AmAtBMt(ScFr$Pm#Bh(Ge'Na$In8Pr$Er
+,K
+
+SUCCESS: (((((c&31)+h)+66)*143)+0)%552
+315 
++IrCr$Al$Pm(Ga.Tl#NiCe%C$AcFm$B$Sn#LvBkK)YbBa#Zr#Hs-Rn%Li'Ra$Mt&RfCfNFl#Sb-N
+d(Es$Na#Sr%Ho*In#AmAs&Sc&Po(F*DyPb#MoRg$SeGd&FeAr$W&Fr+Nb'Np'Sm&Zn&Mn$Er$U#Rb'Pu
+*Rh#ClTe$Mg$Cs&Bi$Lu?NoTm#Uup,UuoTb#O%Bh(Ag#UutTcSi0HfS1Cm&ReCo,SgPd%At)Uus'P%Pa
+$Ca&Ds.Cd$Be(I(Ru%LrNe$VHe'Br$Cu*OsPrTaMdPt%H%Th'Eu2Ti$Xe#Au$Hg)Ge*Cn.KrDb#La+Y
+
+
+SUCCESS: (((((c&31)+66)+h)*143)+0)%552
+315 
++IrCr$Al$Pm(Ga.Tl#NiCe%C$AcFm$B$Sn#LvBkK)YbBa#Zr#Hs-Rn%Li'Ra$Mt&RfCfNFl#Sb-N
+d(Es$Na#Sr%Ho*In#AmAs&Sc&Po(F*DyPb#MoRg$SeGd&FeAr$W&Fr+Nb'Np'Sm&Zn&Mn$Er$U#Rb'Pu
+*Rh#ClTe$Mg$Cs&Bi$Lu?NoTm#Uup,UuoTb#O%Bh(Ag#UutTcSi0HfS1Cm&ReCo,SgPd%At)Uus'P%Pa
+$Ca&Ds.Cd$Be(I(Ru%LrNe$VHe'Br$Cu*OsPrTaMdPt%H%Th'Eu2Ti$Xe#Au$Hg)Ge*Cn.KrDb#La+Y
+
+
+SUCCESS: (((((c&31)+h)*143)+54)+0)%552
+315 
++IrCr$Al$Pm(Ga.Tl#NiCe%C$AcFm$B$Sn#LvBkK)YbBa#Zr#Hs-Rn%Li'Ra$Mt&RfCfNFl#Sb-N
+d(Es$Na#Sr%Ho*In#AmAs&Sc&Po(F*DyPb#MoRg$SeGd&FeAr$W&Fr+Nb'Np'Sm&Zn&Mn$Er$U#Rb'Pu
+*Rh#ClTe$Mg$Cs&Bi$Lu?NoTm#Uup,UuoTb#O%Bh(Ag#UutTcSi0HfS1Cm&ReCo,SgPd%At)Uus'P%Pa
+$Ca&Ds.Cd$Be(I(Ru%LrNe$VHe'Br$Cu*OsPrTaMdPt%H%Th'Eu2Ti$Xe#Au$Hg)Ge*Cn.KrDb#La+Y
+
+SUCCESS: (((((c&31)+h)*493)+210)+0)%554
+318 
+$Cl%Tm+In#Sc4Pa#Uut#Cu#Te'Uuo$Tc#Cd(FlZn$GdTa$Lu%Dy(B&NpPt%Es%Se$Uus.Bi(P%Si
+Ni'Hf%Uup%ReIr#Ho(BkAsBa#Er#Rn(Lv'Al#Pd#Na)Ag$ThRu'DsRf,CfTbHg%Rg%Yb$Tl%YSn,SgTi
+$Zr#Cn#Pm%O$Md%Ge#Pu0CrS#C.RaPo*WAr'Ga#Sb*Ne&Pr$N+Eu*Sm4Pb,KrLaCe#Co-Sr$Ca%F$At#
+Cm%Be$RhNoBr%Db(MtMg'Xe(Rb$Ac;Fe'I%Bh&Mn#Fm+KHeU*H$Lr,Li#Hs$VAu$Nd&AmFr*MoNb$Os0
+Cs
+
+
+SUCCESS: (((((c&31)+h)+378)*493)+0)%554
+318 
+$Cl%Tm+In#Sc4Pa#Uut#Cu#Te'Uuo$Tc#Cd(FlZn$GdTa$Lu%Dy(B&NpPt%Es%Se$Uus.Bi(P%Si
+Ni'Hf%Uup%ReIr#Ho(BkAsBa#Er#Rn(Lv'Al#Pd#Na)Ag$ThRu'DsRf,CfTbHg%Rg%Yb$Tl%YSn,SgTi
+$Zr#Cn#Pm%O$Md%Ge#Pu0CrS#C.RaPo*WAr'Ga#Sb*Ne&Pr$N+Eu*Sm4Pb,KrLaCe#Co-Sr$Ca%F$At#
+Cm%Be$RhNoBr%Db(MtMg'Xe(Rb$Ac;Fe'I%Bh&Mn#Fm+KHeU*H$Lr,Li#Hs$VAu$Nd&AmFr*MoNb$Os0
+Cs
+
+
+SUCCESS: (((((c&31)+378)+h)*493)+0)%554
+318 
+$Cl%Tm+In#Sc4Pa#Uut#Cu#Te'Uuo$Tc#Cd(FlZn$GdTa$Lu%Dy(B&NpPt%Es%Se$Uus.Bi(P%Si
+Ni'Hf%Uup%ReIr#Ho(BkAsBa#Er#Rn(Lv'Al#Pd#Na)Ag$ThRu'DsRf,CfTbHg%Rg%Yb$Tl%YSn,SgTi
+$Zr#Cn#Pm%O$Md%Ge#Pu0CrS#C.RaPo*WAr'Ga#Sb*Ne&Pr$N+Eu*Sm4Pb,KrLaCe#Co-Sr$Ca%F$At#
+Cm%Be$RhNoBr%Db(MtMg'Xe(Rb$Ac;Fe'I%Bh&Mn#Fm+KHeU*H$Lr,Li#Hs$VAu$Nd&AmFr*MoNb$Os0
+Cs
+
+SUCCESS: (((((c&31)+h)*72)+414)+0)%559
+315 
++Cf$Cr#Mn$C1Y%Ho%UuoLu*Uut'Dy$Es#Fm+Sc+Rh,Bk)Fr,Ac%SrUFl$V5HgHe*Bi&Am%Zn$Gd)
+Tc*Tm#F*BrGaCd)PtMg%MdIn#PmBa+CnW(Ni%Ag$Pd$Ir&Fe&As$Nd#Au#Rn%PaLr#Rf(Ca(Kr'At$Os
+Hs'K#CuMoDb$NbIBh#Rb)PuBe#ArYbZrH&No$Ti0NEr#Ds%La$SeNa#Ne#Po)Tl%Rg%Hf8Xe(Cs%Al,C
+m)PbLv'ClP#Sn&Re'Si#Uus)TaGeB$Sm$Co(S#Uup#Eu-SbLiMtTb-Sg&Th#Te#Ru%Pr&O%Ra$Ce&Np
+
+SUCCESS: (((((c&31)+315)+h)*467)+0)%559
+320 
+)HAt$Br%Y#Ti$S#Cf#Sn&Cd&Ar#NeGd%Ce,SrHeHsGa(Hf$Au%Ho*O+Mt&K(Ru'Am(Ac$Yb%UupA
+lFe$Rf'Cu+Sb#Cn#Kr&No%CaTlPr#Eu'Os/Fl(Sg#Mg&Np#Lu%Pu$ScRa%HgDb$Zr&Na+Pb#B)Pt(Es$
+Uus#Ag#Pa*Rg&DyTb(Po)BhLi$TaIn)N(Th*Lr&Rh%FUut$W(Fm(Pd$LvEr'CoRe*TeBaC#Si#Cr(Xe5
+I$TmZn%U#Mo0DsP&Nd$Uuo'Tc$Bi)Cl)La(Cm1Ge$Pm$Sm)Rn-MdFrNb%Cs&Ir&As5BkMn$Ni$Rb#Se$
+V)Be
+
+SUCCESS: (((((c&31)+h)+315)*467)+0)%559
+320 
+)HAt$Br%Y#Ti$S#Cf#Sn&Cd&Ar#NeGd%Ce,SrHeHsGa(Hf$Au%Ho*O+Mt&K(Ru'Am(Ac$Yb%UupA
+lFe$Rf'Cu+Sb#Cn#Kr&No%CaTlPr#Eu'Os/Fl(Sg#Mg&Np#Lu%Pu$ScRa%HgDb$Zr&Na+Pb#B)Pt(Es$
+Uus#Ag#Pa*Rg&DyTb(Po)BhLi$TaIn)N(Th*Lr&Rh%FUut$W(Fm(Pd$LvEr'CoRe*TeBaC#Si#Cr(Xe5
+I$TmZn%U#Mo0DsP&Nd$Uuo'Tc$Bi)Cl)La(Cm1Ge$Pm$Sm)Rn-MdFrNb%Cs&Ir&As5BkMn$Ni$Rb#Se$
+V)Be
+
+SUCCESS: (((((c&31)+h)*368)+265)+0)%567
+319 Tm$Cf%Na#B$Th7Sg*No#Br*O'ScS%Rb&Sr,F*MoCdHo&Nd%Sm)Ca*Fe#AtAm)Yb$CeINi&Fr#PtS
+iBh(Sn'NbU0CEs%Fm*Uus%K$HUut#Ti#Md$Tl$ArIn%Cu$DsN&Po$Hs$Tb(Pa+Se%Rf$Mt$Co$Uup1Rh
+#P,Ac(Re&Ir%Gd$W#Mn#Au&DbPr$Uuo-Dy%Hg$ZnRn.Er&Eu%Zr#Xe%Sb-Bk%Cl1CmBa+Pm0Lu#Pd#Al
+#PbHe*BiPu%KrFl#BeY&V%Ag'Ra#LaRg%Lr$Ru-Ga,As%Mg&Lv.Cn'GeCr(Os#Ta&Cs$Np'Li'TeHf'N
+eTc
+
+SUCCESS: (((((c&31)+446)+h)*368)+0)%567
+319 
+Tm$Cf%Na#B$Th7Sg*No#Br*O'ScS%Rb&Sr,F*MoCdHo&Nd%Sm)Ca*Fe#AtAm)Yb$CeINi&Fr#PtS
+iBh(Sn'NbU0CEs%Fm*Uus%K$HUut#Ti#Md$Tl$ArIn%Cu$DsN&Po$Hs$Tb(Pa+Se%Rf$Mt$Co$Uup1Rh
+#P,Ac(Re&Ir%Gd$W#Mn#Au&DbPr$Uuo-Dy%Hg$ZnRn.Er&Eu%Zr#Xe%Sb-Bk%Cl1CmBa+Pm0Lu#Pd#Al
+#PbHe*BiPu%KrFl#BeY&V%Ag'Ra#LaRg%Lr$Ru-Ga,As%Mg&Lv.Cn'GeCr(Os#Ta&Cs$Np'Li'TeHf'N
+eTc
+
+SUCCESS: (((((c&31)+h)+446)*368)+0)%567
+319 
+Tm$Cf%Na#B$Th7Sg*No#Br*O'ScS%Rb&Sr,F*MoCdHo&Nd%Sm)Ca*Fe#AtAm)Yb$CeINi&Fr#PtS
+iBh(Sn'NbU0CEs%Fm*Uus%K$HUut#Ti#Md$Tl$ArIn%Cu$DsN&Po$Hs$Tb(Pa+Se%Rf$Mt$Co$Uup1Rh
+#P,Ac(Re&Ir%Gd$W#Mn#Au&DbPr$Uuo-Dy%Hg$ZnRn.Er&Eu%Zr#Xe%Sb-Bk%Cl1CmBa+Pm0Lu#Pd#Al
+#PbHe*BiPu%KrFl#BeY&V%Ag'Ra#LaRg%Lr$Ru-Ga,As%Mg&Lv.Cn'GeCr(Os#Ta&Cs$Np'Li'TeHf'N
+eTc
+
+SUCCESS: (((((c&31)+h)+9)*313)+0)%568
+313 
+*Zn#Pt%NCd%NoCl#SnCu,HeDbTe$S/VTa#Uut'Sr$Lr8UusAt&Pu*Tm#CnAu(Zr(Ra1Sg%Ne
+#K&Ag'LuEr#FmAm'Pa-Mg'Ca#Rh%Br'Rn*PdPo#P$W#Ac.Th%Cm$Kr$RbGa)IrBiNi#Mo&C$Eu&Li(Se
+)RuGd$Md#Nd)Rg'ISb0NpTb%Ce$Lv#H+Pb&SmSi$Pr$Yb'InB$Mn%Sc*Xe,U$Uup%Re&Cr#Ba$HsFl%B
+e<Dy$Ar&BkAl/O,Ds%HfTc8NaOsMtHo#Cf#Ge%Nb#FeYHg7Pm&La%Es'CoFBh%Uuo#Ti&Tl%AsFrCs(R
+f
+
+SUCCESS: (((((c&31)+h)*313)+545)+0)%568
+313 *Zn#Pt%NCd%NoCl#SnCu,HeDbTe$S/VTa#Uut'Sr$Lr8UusAt&Pu*Tm#CnAu(Zr(Ra1Sg%Ne#K&A
+g'LuEr#FmAm'Pa-Mg'Ca#Rh%Br'Rn*PdPo#P$W#Ac.Th%Cm$Kr$RbGa)IrBiNi#Mo&C$Eu&Li(Se)RuG
+d$Md#Nd)Rg'ISb0NpTb%Ce$Lv#H+Pb&SmSi$Pr$Yb'InB$Mn%Sc*Xe,U$Uup%Re&Cr#Ba$HsFl%Be<Dy
+$Ar&BkAl/O,Ds%HfTc8NaOsMtHo#Cf#Ge%Nb#FeYHg7Pm&La%Es'CoFBh%Uuo#Ti&Tl%AsFrCs(Rf
+
+SUCCESS: (((((c&31)+h)*160)+273)+0)%571
+322 $F'C(Ho$Tl%Mo(Zr$Cn$Cu&Lu1Fm-Os&Rn#Ra$KSg%Br%Na)Pa'Hg$Zn7Ag$Cd&Ru#S$NVFl%Uup
+'Th$P+NdDy*Ir+Sm%Cm)Ca&HRh0B,I%Db#Kr%At,Pd#Y#Re&O&Be%No'CoTi#Se#HePu%Uus+Pr*YbXe
+.Mg+Bh&InSn%RfHs-Mt#Gd*Li$TcBk#PtAsFr%Cr'Tb&Sr)Er%La1ArScDs*Es&Cf%Sb)AuNi$Ac*Am#
+AlCl%Np%Tm%Rb$Fe#Pb%Eu#Te(Ta&Ga&Md%Lr#Po)Uut'Rg#Hf$Ne*BiBaUuo(PmGeNb#Mn%Cs+Si%UC
+e(Lv#W
+
+SUCCESS: (((((c&31)+h)+562)*160)+0)%571
+322 $F'C(Ho$Tl%Mo(Zr$Cn$Cu&Lu1Fm-Os&Rn#Ra$KSg%Br%Na)Pa'Hg$Zn7Ag$Cd&Ru#S$NVFl%Uup
+'Th$P+NdDy*Ir+Sm%Cm)Ca&HRh0B,I%Db#Kr%At,Pd#Y#Re&O&Be%No'CoTi#Se#HePu%Uus+Pr*YbXe
+.Mg+Bh&InSn%RfHs-Mt#Gd*Li$TcBk#PtAsFr%Cr'Tb&Sr)Er%La1ArScDs*Es&Cf%Sb)AuNi$Ac*Am#
+AlCl%Np%Tm%Rb$Fe#Pb%Eu#Te(Ta&Ga&Md%Lr#Po)Uut'Rg#Hf$Ne*BiBaUuo(PmGeNb#Mn%Cs+Si%UC
+e(Lv#W
+
+SUCCESS: (((((c&31)+562)+h)*160)+0)%571
+322 $F'C(Ho$Tl%Mo(Zr$Cn$Cu&Lu1Fm-Os&Rn#Ra$KSg%Br%Na)Pa'Hg$Zn7Ag$Cd&Ru#S$NVFl%Uup
+'Th$P+NdDy*Ir+Sm%Cm)Ca&HRh0B,I%Db#Kr%At,Pd#Y#Re&O&Be%No'CoTi#Se#HePu%Uus+Pr*YbXe
+.Mg+Bh&InSn%RfHs-Mt#Gd*Li$TcBk#PtAsFr%Cr'Tb&Sr)Er%La1ArScDs*Es&Cf%Sb)AuNi$Ac*Am#
+AlCl%Np%Tm%Rb$Fe#Pb%Eu#Te(Ta&Ga&Md%Lr#Po)Uut'Rg#Hf$Ne*BiBaUuo(PmGeNb#Mn%Cs+Si%UC
+e(Lv#W
+
+SUCCESS: (((((c&31)+278)+h)*453)+0)%574
+323 Ne'Ca$Si+Ge$UupBeCl'H$Fr'FlSg+Ra'Hf.PdMg#Rn(Hg)Po%LrRf+Pm(Gd$Br$Uut%Lu2In%Er
+*Md(Cd-Os,Ag%Uus(Rh#Tm#Re&Co0N+Hs.At#SbF$P%Uuo'S#Dy'C$AuCu)Ds#NaLa&AlBi'I,RuEuFm
+(Cn%Ni#Am&Pu'CsXe*Np$Bk*IrEs-O#SeMt$Li$AsZr,Cf%Sn$B#Pt$Sr&Ba'He%Tl/W#Th)Sc#Y*NdZ
+n$PaTa#Db&Rg#Ga#Mn#Yb%Sm#Ce#NoMo-Tb&Cm#Nb)Lv#V'Pr)HoTc#Fe&Ar%K,Cr$Rb*Pb&Kr%UBh$T
+e&Ac&Ti
+
+SUCCESS: (((((c&31)+h)+278)*453)+0)%574
+323 Ne'Ca$Si+Ge$UupBeCl'H$Fr'FlSg+Ra'Hf.PdMg#Rn(Hg)Po%LrRf+Pm(Gd$Br$Uut%Lu2In%Er
+*Md(Cd-Os,Ag%Uus(Rh#Tm#Re&Co0N+Hs.At#SbF$P%Uuo'S#Dy'C$AuCu)Ds#NaLa&AlBi'I,RuEuFm
+(Cn%Ni#Am&Pu'CsXe*Np$Bk*IrEs-O#SeMt$Li$AsZr,Cf%Sn$B#Pt$Sr&Ba'He%Tl/W#Th)Sc#Y*NdZ
+n$PaTa#Db&Rg#Ga#Mn#Yb%Sm#Ce#NoMo-Tb&Cm#Nb)Lv#V'Pr)HoTc#Fe&Ar%K,Cr$Rb*Pb&Kr%UBh$T
+e&Ac&Ti
+
+SUCCESS: (((((c&31)+h)*453)+228)+0)%574
+323 Ne'Ca$Si+Ge$UupBeCl'H$Fr'FlSg+Ra'Hf.PdMg#Rn(Hg)Po%LrRf+Pm(Gd$Br$Uut%Lu2In%Er
+*Md(Cd-Os,Ag%Uus(Rh#Tm#Re&Co0N+Hs.At#SbF$P%Uuo'S#Dy'C$AuCu)Ds#NaLa&AlBi'I,RuEuFm
+(Cn%Ni#Am&Pu'CsXe*Np$Bk*IrEs-O#SeMt$Li$AsZr,Cf%Sn$B#Pt$Sr&Ba'He%Tl/W#Th)Sc#Y*NdZ
+n$PaTa#Db&Rg#Ga#Mn#Yb%Sm#Ce#NoMo-Tb&Cm#Nb)Lv#V'Pr)HoTc#Fe&Ar%K,Cr$Rb*Pb&Kr%UBh$T
+e&Ac&Ti
+
+SUCCESS: (((((c&31)+h)*235)+250)+0)%576
+320 %Ce(SeRg$Cl&Ti#Cs%Yb$Pm)Th#Ac'BrAr$Fe$Na)Te$Tc'W#Db%Tb5In&Be/SrLi#ZnCf#Ds6La
+%Ra&Co#Lv(N)YC#K&F,Ho#Cu#Pr.Hf&Pa#NdEu$Kr#Mt#As%SbRn&U,Ne%I%Cr&FmSgAt+Ge*Am.V$Tm
+(Ga-Cm#Ru,PbLu#Hg%No&Tl'MoPu%Ag&O'CaSi(He$Al'Md*Zr#Hs$UutXeP&Cn*Uuo;Fl#IrBa#Bh)H
+%Au&Pd$Er%BkNp&Mg)Uup(Lr&Ta%B'RfMn'DyCd%PtSn&NiSmEsRb,Re(Po%Gd#Fr*Bi)ScNb&Rh%OsU
+us'S
+
+SUCCESS: (((((c&31)+430)+h)*235)+0)%576
+320 %Ce(SeRg$Cl&Ti#Cs%Yb$Pm)Th#Ac'BrAr$Fe$Na)Te$Tc'W#Db%Tb5In&Be/SrLi#ZnCf#Ds6La
+%Ra&Co#Lv(N)YC#K&F,Ho#Cu#Pr.Hf&Pa#NdEu$Kr#Mt#As%SbRn&U,Ne%I%Cr&FmSgAt+Ge*Am.V$Tm
+(Ga-Cm#Ru,PbLu#Hg%No&Tl'MoPu%Ag&O'CaSi(He$Al'Md*Zr#Hs$UutXeP&Cn*Uuo;Fl#IrBa#Bh)H
+%Au&Pd$Er%BkNp&Mg)Uup(Lr&Ta%B'RfMn'DyCd%PtSn&NiSmEsRb,Re(Po%Gd#Fr*Bi)ScNb&Rh%OsU
+us'S
+
+SUCCESS: (((((c&31)+h)+430)*235)+0)%576
+320 %Ce(SeRg$Cl&Ti#Cs%Yb$Pm)Th#Ac'BrAr$Fe$Na)Te$Tc'W#Db%Tb5In&Be/SrLi#ZnCf#Ds6La
+%Ra&Co#Lv(N)YC#K&F,Ho#Cu#Pr.Hf&Pa#NdEu$Kr#Mt#As%SbRn&U,Ne%I%Cr&FmSgAt+Ge*Am.V$Tm
+(Ga-Cm#Ru,PbLu#Hg%No&Tl'MoPu%Ag&O'CaSi(He$Al'Md*Zr#Hs$UutXeP&Cn*Uuo;Fl#IrBa#Bh)H
+%Au&Pd$Er%BkNp&Mg)Uup(Lr&Ta%B'RfMn'DyCd%PtSn&NiSmEsRb,Re(Po%Gd#Fr*Bi)ScNb&Rh%OsU
+us'S
+
+SUCCESS: (((((c&31)+h)*55)+409)+0)%577
+326 *Br'Rb#He$Pa$Mn&At3Na&BkSi$EsK%Rh&Cf%Sc#Tc#La(Er$Tm'Ta/Eu/Ag%Pd%Fl%YLrCm$Db%
+TeAuPu#Am$Th#Ne(Zr%Fm%LiDs#Uut#Np#I$Ac2Md(Bi$Ar$Sn+Sg&Sr(Cd(N$OsLu+Hs&Bh%Cl$Ti#U
+us,RaIn&Ho%Cs+MoUup'UB*Mg(Nd(Sb'Lv%Ge&H'Cu#Ir,Uuo*Nb$Yb$No%Cn/Rn%Ce$Hg+Xe#Tb#DyW
+'P'Fr#Rf'Co#V*Mt#CPr%Gd'Kr$Se'Pm$Rg*Tl)PtRu(AlCr&Ga&Po*Hf&Fe*Ba-As#Be*Re$Ni(PbZn
+$CaOS&F&Sm
+
+SUCCESS: (((((c&31)+h)+511)*55)+0)%577
+326 *Br'Rb#He$Pa$Mn&At3Na&BkSi$EsK%Rh&Cf%Sc#Tc#La(Er$Tm'Ta/Eu/Ag%Pd%Fl%YLrCm$Db%
+TeAuPu#Am$Th#Ne(Zr%Fm%LiDs#Uut#Np#I$Ac2Md(Bi$Ar$Sn+Sg&Sr(Cd(N$OsLu+Hs&Bh%Cl$Ti#U
+us,RaIn&Ho%Cs+MoUup'UB*Mg(Nd(Sb'Lv%Ge&H'Cu#Ir,Uuo*Nb$Yb$No%Cn/Rn%Ce$Hg+Xe#Tb#DyW
+'P'Fr#Rf'Co#V*Mt#CPr%Gd'Kr$Se'Pm$Rg*Tl)PtRu(AlCr&Ga&Po*Hf&Fe*Ba-As#Be*Re$Ni(PbZn
+$CaOS&F&Sm
+
+SUCCESS: (((((c&31)+511)+h)*55)+0)%577
+326 *Br'Rb#He$Pa$Mn&At3Na&BkSi$EsK%Rh&Cf%Sc#Tc#La(Er$Tm'Ta/Eu/Ag%Pd%Fl%YLrCm$Db%
+TeAuPu#Am$Th#Ne(Zr%Fm%LiDs#Uut#Np#I$Ac2Md(Bi$Ar$Sn+Sg&Sr(Cd(N$OsLu+Hs&Bh%Cl$Ti#U
+us,RaIn&Ho%Cs+MoUup'UB*Mg(Nd(Sb'Lv%Ge&H'Cu#Ir,Uuo*Nb$Yb$No%Cn/Rn%Ce$Hg+Xe#Tb#DyW
+'P'Fr#Rf'Co#V*Mt#CPr%Gd'Kr$Se'Pm$Rg*Tl)PtRu(AlCr&Ga&Po*Hf&Fe*Ba-As#Be*Re$Ni(PbZn
+$CaOS&F&Sm
+
+SUCCESS: (((((c&31)+h)*137)+133)+0)%580
+320 (TaAgUut$Cu#Es$O(Re)No-DsRa$Db$Sr4Lr#Ce%Sb%Co%Mg&As+Nb&Cm)SmNd'Lv#Fm#I%Ge'Mn
+6CaMd%Fl,Bk$Uus#Pb+Si)NeRb%Fr(Te$CdPm#HeTb$Er%AuUup.Cf$Po#Lu#Tm#Xe)B%Am#Gd$Yb&Rh
+#Rf&Sn)Pd)Pu$V.Np&Hf(HgBrFe&Y,ArBh(ZnZrPt.C$Se+Ni)Cl%W%U+Be$Uuo*N'SgAt$Tl(Pr&Rg'
+AcTi#P'ThHCn&Kr#Os-TcDyAl-K&BiCr#Mo&Na$Ho%Eu&LiIn&La%HsBa#Sc3Ir%RuPa#Mt'S(Ga,Rn&
+Cs#F
+
+SUCCESS: (((((c&31)+509)+h)*137)+0)%580
+320 (TaAgUut$Cu#Es$O(Re)No-DsRa$Db$Sr4Lr#Ce%Sb%Co%Mg&As+Nb&Cm)SmNd'Lv#Fm#I%Ge'Mn
+6CaMd%Fl,Bk$Uus#Pb+Si)NeRb%Fr(Te$CdPm#HeTb$Er%AuUup.Cf$Po#Lu#Tm#Xe)B%Am#Gd$Yb&Rh
+#Rf&Sn)Pd)Pu$V.Np&Hf(HgBrFe&Y,ArBh(ZnZrPt.C$Se+Ni)Cl%W%U+Be$Uuo*N'SgAt$Tl(Pr&Rg'
+AcTi#P'ThHCn&Kr#Os-TcDyAl-K&BiCr#Mo&Na$Ho%Eu&LiIn&La%HsBa#Sc3Ir%RuPa#Mt'S(Ga,Rn&
+Cs#F
+
+SUCCESS: (((((c&31)+h)+509)*137)+0)%580
+320 (TaAgUut$Cu#Es$O(Re)No-DsRa$Db$Sr4Lr#Ce%Sb%Co%Mg&As+Nb&Cm)SmNd'Lv#Fm#I%Ge'Mn
+6CaMd%Fl,Bk$Uus#Pb+Si)NeRb%Fr(Te$CdPm#HeTb$Er%AuUup.Cf$Po#Lu#Tm#Xe)B%Am#Gd$Yb&Rh
+#Rf&Sn)Pd)Pu$V.Np&Hf(HgBrFe&Y,ArBh(ZnZrPt.C$Se+Ni)Cl%W%U+Be$Uuo*N'SgAt$Tl(Pr&Rg'
+AcTi#P'ThHCn&Kr#Os-TcDyAl-K&BiCr#Mo&Na$Ho%Eu&LiIn&La%HsBa#Sc3Ir%RuPa#Mt'S(Ga,Rn&
+Cs#F
+
+SUCCESS: (((((c&31)+h)*41)+49)+0)%584
+312 'B)V$Nd$Ag#Bh(Cf(DsNi(Pr$Ta+Ra%Te&Zr0TcDyPb5HgCSg8SmUut$Db#Am(Fm$Sb$Pt'H#IrG
+d#EsYb$Bk+PaAs'GaTi(Al)He$YMd#Zn%Ne$TmMnAt&K%ErNo$Mo$Rn#PoFe+N'Cl#Lv&Ce$Ge)Pu+Si
++Pd&RhWLa#C#Xe4Uus#Ac$FrSr)Be%HoLiIn%U5Pm%Os#Nb&Cu'RfCs*S%Uup$Ba$Rb&KrMgTlUuo.Sn
+ThOMt)Lr)LuAu&Re#HsFl%I'Ar1Cr,Hf*Rg$Co*Sc$Na%BrFCa)PNp$Cn'Tb$RuBi(SeCm$Eu*Cd
