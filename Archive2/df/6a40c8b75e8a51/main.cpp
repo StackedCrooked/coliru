49d1b16a2201b73ddeaecacc37284b6fc77435db
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-10 08:05:16 +0000

    Update archive df

---
+++ Archive2/df/6a40c8b75e8a51/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  cout << "ceil(0)  : " << ceil(0) << endl;
+  cout << "floor(0) : " << floor(0) << endl;
+  return 0;
+}
