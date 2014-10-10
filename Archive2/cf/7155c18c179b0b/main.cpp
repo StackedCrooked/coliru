5537c78c116ae95b03576d1bb07420829f465cf5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-10 04:28:52 +0000

    Update archive cf

---
+++ Archive2/cf/7155c18c179b0b/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <array>
+#include <unordered_map>
+#include <functional>
+#include <cstdint>
+
+enum class T{
+    A = 0,
+    B = 1
+};
+
+struct X
+{
+    T t : 1;
+};
+
+int main()
+{
+}
