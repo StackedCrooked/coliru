5ae055f243a8dcabcee7d8603113206b1f646d49
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-10 00:42:30 +0000

    Update archive 2a

---
+++ trunk/Archive2/2a/ef4b7274b61aa8/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <map>
+
+struct Node
+{
+    explicit Node(int i) : _i(i) {}
+    int _i;
+    
+    bool operator<(const Node& rhs) const 
+    {
+        return _i < rhs._i;
+    }
+};
+
+std::ostream& operator<<(std::ostream& os, const Node& n)
+{
+    os << n._i;
+    return os;
+}
+
+typedef std::multimap<Node,Node> Graph;
+
+int main()
+{
+    Graph g;
+    Node node1(1), node2(2), node3(3), node4(4), node5(5), node6(6);
+    
+    g.insert (Graph::value_type(node1, node3));
+    g.insert (Graph::value_type(node1, node4));
+    g.insert (Graph::value_type(node1, node5));
+    g.insert (Graph::value_type(node2, node6));
+    g.insert (Graph::value_type(node3, node6));
+    
+    Graph::iterator it = g.begin();
+    for (; it != g.end(); it = g.upper_bound(it->first))
+    {
+        std::pair<Graph::iterator, Graph::iterator> range = g.equal_range(it->first);
+        std::cout << it->first << ": ";
+        
+        for (; range.first != range.second; ++range.first)
+        {
+            std::cout << range.first->second << ", ";
+        }
+        std::cout << std::endl;
+    }
+}
