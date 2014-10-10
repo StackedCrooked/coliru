aa37bd17e338487120bbd4e286862bb404855aa4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-10 08:23:36 +0000

    Update archive 53

---
+++ Archive2/53/e090fad3ad6d2a/main.cpp
@@ -0,0 +1,14 @@
+template<typename T>
+auto test(T&& t) -> decltype( (t = { 0. }), 0 );
+
+template<typename...>
+void test(...);
+
+int main()
+{
+    using T = decltype( test(0) );
+    // initializing void
+    T val = 0;
+    // incrementing void
+    ++val;
+}
