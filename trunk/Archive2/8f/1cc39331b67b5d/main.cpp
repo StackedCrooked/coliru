da9a04a54465ba7d34b6ba02dd29ab5263d09b41
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-10 11:44:57 +0000

    Update archive 8f

---
+++ trunk/Archive2/8f/1cc39331b67b5d/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <climits>
+
+int main()
+{
+    std::cout << "INT_MIN = " << INT_MIN << std::endl
+            << "LONG_MIN = " << LONG_MIN << std::endl 
+            << "LONG_LONG_MIN = " << LONG_LONG_MIN << std::endl;
+}
