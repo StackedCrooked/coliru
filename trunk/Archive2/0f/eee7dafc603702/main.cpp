f90b7820e8ec1d4a61cd81dd5cee17158e26d80d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-11 00:40:44 +0000

    Update archive 0f

---
+++ trunk/Archive2/0f/eee7dafc603702/main.cpp
@@ -0,0 +1,26 @@
+#include <memory>
+#include <type_traits>
+
+template <typename T>
+struct Foo
+{
+    typename std::aligned_storage<sizeof(T), std::alignment_of<T>::value>::type store;
+
+    template <typename... Args>
+    Foo(Args&&... args)
+    {
+        new (&store) T { std::forward<Args>(args)... };
+    }
+
+    void Release()
+    {
+        reinterpret_cast<T*>(&store)->~T();
+    }
+};
+
+
+int main()
+{
+    Foo<int> a(2); // ok
+    Foo<int[3]> b(1, 2, 3);
+}
