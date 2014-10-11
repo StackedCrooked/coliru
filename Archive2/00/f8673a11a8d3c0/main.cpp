84f26fce3dda01d7eaf01b17c0b703eee87daa15
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-11 04:14:38 +0000

    Update archive 00

---
+++ Archive2/00/f8673a11a8d3c0/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <type_traits>
+
+struct Parent {using bar = int;}; // <-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+struct ChildA : public Parent {};//                                                                                                                                                                                      \
+struct ChildB : public Parent {};//                                                                                                                                                                                       \
+//                                                                                                                                                                                                                         \
+int main() {        //                                                                                                                                                                                                      \
+    ChildA a_foo;//                                                                                                                                                                                                          \
+    ChildB b_foo;//                                                                                                                                                                                                           \
+//                                                                                                                                                                                                                             \
+    Parent implicit_a; //                                                                                                                                                                                                       \
+    implicit_a = a_foo; // * These are implicit conversions are they not?                                                                                                                                                        \
+Parent implicit_b;  //                                                                                                                                                                                                            |
+    implicit_b = b_foo; // *                                                                                                                                                                                                      |
+    //                                                                                                                                                                                                                            |
+    std::common_type<ChildA, ChildB>::type::bar foo = 100; // so std::common_type<ChildA, ChildB>::type should be Parent because both ChildA and ChildB can be implicitly converted to Parent. So Parent::bar is int according to |
+    
+}
