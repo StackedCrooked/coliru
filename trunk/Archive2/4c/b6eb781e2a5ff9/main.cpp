ea284ae88e092fb9edea8b8ec7db4d8e4b4f4ffc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-11 01:15:41 +0000

    Update archive 4c

---
+++ trunk/Archive2/4c/b6eb781e2a5ff9/main.cpp
@@ -0,0 +1,36 @@
+#include <type_traits>
+#include <iostream>
+struct A {
+    int work(){ std::cout << "Work A" << std::endl; return 0; }
+};
+
+struct B {
+    int work() {std::cout << "Work B" << std::endl;  return 0;}
+};
+
+struct ObjectManager {
+    static void manage( A& ){std::cout << "Manage A" << std::endl; }
+    // manage not defined for class B 
+};
+
+template<class T>
+auto call_manage(T* t) -> decltype(ObjectManager::manage(*t)){
+    ObjectManager::manage(*t);
+}
+
+void call_manage(...) {}
+
+template<class T>
+void doStuff( T t ) {
+    t.work();
+    //....
+    call_manage(&t);
+}
+
+int main() {
+A a;
+B b;
+
+doStuff(a);
+doStuff(b);
+}
