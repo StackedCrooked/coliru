8a0bbae2648007578581f3d071f100021fbb3378
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-12 20:59:16 +0000

    Update archive ab

---
+++ Archive2/ab/edab7c75307a92/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <random>
+#include <cmath>
+int main()
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+ 
+    // values near the mean are the most likely
+    // standard deviation affects the dispersion of generated values from the mean
+    std::normal_distribution<> d(5,2);
+ 
+    std::map<int, int> hist;
+    for(int n=0; n<10000; ++n) {
+        ++hist[std::round(d(gen))];
+    }
+    for(auto p : hist) {
+        std::cout << std::fixed << std::setprecision(1) << std::setw(2)
+                  << p.first << ' ' << std::string(p.second/200, '*') << '\n';
+    }
+}
