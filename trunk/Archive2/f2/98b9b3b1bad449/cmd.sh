1c39efc8c931faf28371c93e31e1def8d2e9ad42
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-13 17:59:25 +0000

    Update archive f2

---
+++ trunk/Archive2/f2/98b9b3b1bad449/cmd.sh
@@ -0,0 +1,1 @@
+clang++ -std=c++1y -O3 -Wall -Wno-unused-variable -pedantic -pthread main.cpp -lboost_system -lboost_coroutine && ./a.out
---
+++ trunk/Archive2/f2/98b9b3b1bad449/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <type_traits>
+
+struct noop{ // do nothing
+    template<class F> noop else_(const F& f) const { return noop{}; }
+    template<class F> noop else_if(const F& f) const { return noop{}; }
+    template<class F> noop operator()(const F& f) const { return noop{}; }
+};
+
+struct yes{ // executing first, no dothing for others
+    template<class F> noop operator()(const F& f) const { f(0); return noop{}; }
+    template<class F> noop else_if(const F& f) const { return noop{}; }
+};
+
+struct no{
+    template<class F> auto else_(const F& f) const { f(0); }
+    yes else_if(std::true_type){ return yes{}; }
+    no  else_if(std::false_type){ return no{}; }
+    template<class F> no operator()(const F& f) const { return no{}; }
+};
+
+yes static_if(std::true_type){ return yes{}; }
+no  static_if(std::false_type){ return no{}; }
+
+using namespace std;
+
+template<typename T>
+void check(T x)
+{
+    static_if( is_same<T, string>() )([&](auto){
+        // x.size(); // COMPILER ERROR
+        [](auto z)
+        {
+            z.size(); // OK
+        }(x);
+    });
+}
+
+
+int main()
+{
+    check(1);
+    check(""s);
+}
