5e6974c0728bff0331c73a29f12bba69a360a301
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-13 11:46:17 +0000

    Update archive 15

---
+++ trunk/Archive2/15/ee37a87523e754/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdexcept>
+
+struct X
+{
+    X() { }
+    ~X() { std::cout << "X::~X()" << std::endl; }
+    X(int) : X{} { throw std::exception{}; }
+};
+
+struct Y
+{
+    Y() { throw std::exception{}; }
+    ~Y() { std::cout << "Y::~Y()" << std::endl; }
+    Y(int) : Y{} {  }
+};
+
+int main()
+{
+    try
+    {
+        X x{42};
+    }
+    catch (std::exception const&) { }
+    
+    try
+    {
+        Y y{42};
+    }
+    catch (std::exception const&) { }
+
+}
