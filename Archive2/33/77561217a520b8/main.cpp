d698b5218135b010a2956dedf81e713fff81a0f6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-13 08:00:25 +0000

    Update archive 33

---
+++ Archive2/33/77561217a520b8/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+template <class T, size_t N>
+T* asFlatArray (T (&arr)[][N])
+{
+ (void)arr;   
+}
+
+int main() {
+    
+    const size_t rows = 2, columns = 5;
+    int arr[rows][columns] = {
+      {1, 2, 3, 4, 5},
+      {6, 7, 8, 9, 0}
+    };
+    
+    asFlatArray(arr);
+}
+
