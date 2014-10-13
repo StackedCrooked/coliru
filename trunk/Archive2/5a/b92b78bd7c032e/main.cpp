a2400e0fce1aee6223ab4efcb6703708e7680726
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-13 11:07:04 +0000

    Update archive 5a

---
+++ trunk/Archive2/5a/b92b78bd7c032e/main.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include <vector>
+
+using Pos = std::pair<int, int>;
+int main()
+{
+    int rows=4,cols=4;
+
+	std::vector<std::vector<char>> Map = 
+	{ { '-', '-', '-', '-' },
+	{ '*', '*', '*', '-' },
+	{ '-', '-', '*', '-' },
+	{ '-', '-', '*', '-' }
+	};
+	Pos start(rows-1,0);
+	Pos Goal(0,cols-1);
+
+	char ch = 'a';
+	while (start != Goal)
+	{
+		int x = start.first;
+		int y = start.second;
+		Map[x][y] = ch++;
+		if (x>0 && Map[x - 1][y] == '-')
+		{//shomal
+			start = std::make_pair(x - 1,y);
+		}
+		else if (y < cols-1 && Map[x][y+1] == '-')
+		{//shargh
+			start = std::make_pair(x ,y+1);
+		}
+		else//blocked
+			break;
+		
+	}
+	if (start == Goal)
+		Map[Goal.first][Goal.second] = ch;
+	for (auto& row : Map)
+	{
+		for (auto& ch : row)
+		{
+			std::cout << ch;
+		}
+		std::cout << '\n';
+	}
+}
