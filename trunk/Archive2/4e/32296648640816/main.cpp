730ea409a2e1708418b5fdfaa5f5dd17171cef9c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-14 21:36:44 +0000

    Update archive 4e

---
+++ trunk/Archive2/4e/32296648640816/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=3,b=0,c=0;
+    cout << "Inserisci un numero" << endl;
+    cin >> a;
+    cout << "Inserisci un numero" << endl;
+    cin >> b;
+    c=a+b;
+   cout << "il risultato e'" <<c << endl; 
+   
+   return 0;
+}
