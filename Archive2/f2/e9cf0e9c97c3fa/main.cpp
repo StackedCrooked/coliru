d65cbcdc89d772b025d4c44f83fa035ad9c2360d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-14 21:27:56 +0000

    Update archive f2

---
+++ Archive2/f2/e9cf0e9c97c3fa/main.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    std::vector<int> v(10);
+    
+    float amplitude = 3.0f;
+    float frequency = 1.2f;    
+    float sampleRate = 5.0f;
+    
+    std::generate(v.begin(), v.end(), [=, i = 0] () mutable -> short
+         {
+             return amplitude * std::sin((2 * M_PI * (i++) * frequency) / sampleRate);
+         });
+    
+    for (auto e : v)
+    {
+        std::cout << e << ", ";
+    }
+}
