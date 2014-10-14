6cd5ee70047bc2dec6281880116be6e06358ec0b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-14 21:37:31 +0000

    Update archive 2d

---
+++ Archive2/2d/e667051de00ab7/main.cpp
@@ -0,0 +1,55 @@
+#include <tuple>
+#include <utility>
+#include <type_traits>
+#include <iostream>
+
+namespace std
+{
+    template <std::size_t N, typename T>
+    using tuple_element_t = typename std::tuple_element<N, T>::type;
+}
+
+struct HasAs
+{
+    int i;
+    HasAs(int i) : i(i) {}
+    template <typename T> T as() { return i; }
+};
+
+struct JVal
+{
+    HasAs operator[](int i) const { std::cout << "at(" << i << ")" << std::endl; return {i}; }
+};
+
+// START
+template <std::size_t N, typename... TS>
+void op(const JVal& mV, std::tuple<TS...>& mX)
+{
+    using Tuple = std::tuple<TS...>;
+    std::get<N>(mX) = mV[N].as<std::tuple_element_t<N, Tuple>>();
+}
+
+template <typename... TS, std::size_t... Is>
+void toTpl(const JVal& mV, std::tuple<TS...>& mX, std::index_sequence<Is...>) 
+{ 
+    int dummy[] = { 0, ((void)op<Is>(mV, mX), 0)... };
+    (void)dummy;
+}
+
+template <typename... TS> 
+void toTpl(const JVal& mV, std::tuple<TS...>& mX)
+{
+    toTpl(mV, mX, std::make_index_sequence<sizeof...(TS)>{});
+}
+// END
+
+int main()
+{
+    std::tuple<int, float, double> t;
+    JVal mV;
+    toTpl(mV, t);
+    
+    std::cout << std::get<0>(t) << std::endl;
+    std::cout << std::get<1>(t) << std::endl;
+    std::cout << std::get<2>(t) << std::endl;
+}
