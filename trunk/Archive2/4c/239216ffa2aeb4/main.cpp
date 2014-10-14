917e1dd685b80f3df08b663c3f4c11358abcfc9f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-14 01:00:52 +0000

    Update archive 4c

---
+++ trunk/Archive2/4c/239216ffa2aeb4/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+void print(double* x, int N);
+
+int main() {
+  const int N = 2;
+  double x[N];
+  for(int i=0; i<N; ++i)
+    x[i] = numeric_limits<double>::max();
+  
+  print(x, N);
+  
+  cout << numeric_limits<double>::min() << endl;
+  
+  return 0;
+}
