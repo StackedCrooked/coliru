21fc3ce16435ff7c2ed8333e944971f56f953e81
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-14 04:19:52 +0000

    Update archive b7

---
+++ Archive2/b7/5cfda2facc3485/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <type_traits>
+
+constexpr bool condition = true; // set to false to see error
+
+template <class... Opts>
+class rqueue
+{
+    using off_t = std::conditional_t<condition, int, void>;
+    
+public:
+    template <typename T = void>
+    std::enable_if_t<!std::is_same<off_t, void>::value, off_t> start()
+    {
+        return start_();
+    }
+    
+private:
+    off_t start_()
+    {
+        std::cout << "start_()" << std::endl;
+        return 1;
+    }
+};
+
+int main()
+{
+    rqueue<> q;  
+    q.start();
+}
