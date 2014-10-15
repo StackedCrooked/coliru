88207324e45c29c8ac3eb939bb14f384d77b94b1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-15 01:01:44 +0000

    Update archive d6

---
+++ trunk/Archive2/d6/93835380383fcb/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+std::string version;
+
+void setVersion(std::string&& ver)
+{
+    if (ver.size()>1)
+    {
+        version = std::move(ver);
+        std::cout << "version now " << version << std::endl;
+    }
+}
+
+std::string getVersion() {
+    return "version2";
+}
+
+int main(){
+    setVersion("version1");
+    setVersion(getVersion());
+    const std::string v3 = "version3";
+    // doesn't work: const lvalue
+    // setVersion(v3);
+}
