89c553f0b0b64d4a0086e206e4bf2fcbf8f9a837
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-16 08:27:06 +0000

    Update archive 82

---
+++ trunk/Archive2/82/7959f34e091761/cmd.sh
@@ -0,0 +1,1 @@
+g++ -std=c++11 -O2 -Wall -Wextra -pedantic-errors -g -O0 main.cpp && ./a.out
---
+++ trunk/Archive2/82/7959f34e091761/main.cpp
@@ -0,0 +1,13 @@
+struct A
+{
+    A() { foo() ; }
+    virtual ~A() = default ;
+    virtual void foo() const { bar() ; }
+    virtual void bar() const = 0 ;
+};
+
+struct B : A { virtual void bar() const override {} } ;
+
+B b ;
+
+int main() {}
