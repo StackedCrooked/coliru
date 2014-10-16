c1f58bfd9482ddfbf2f32e4382c0b80493ddf1d9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-16 20:47:45 +0000

    Update archive d1

---
+++ trunk/Archive2/d1/7f86ce5df7c745/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+
+int main()
+{
+    char const *a = "my string";
+    char *b = new char[strlen(a) + 1]{0};
+
+	char *zeroes = new char[strlen(a) + 1];
+	memset(zeroes, 0, strlen(a) + 1);
+
+	std::cout << memcmp(b, zeroes, strlen(a) + 1); // 0 expected
+    
+    delete b;
+    delete zeroes;
+}
