8764f90a81a0c410829c76a22fb1a7f9cabd6085
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-16 16:56:32 +0000

    Update archive a7

---
+++ Archive2/a7/b0694d27db70b9/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+ 
+int main()
+{
+    for (unsigned long long size = 1; size < 10000000; size *= 10) {
+ 
+        auto start = std::chrono::steady_clock::now();   // a "timepoint" representing now
+ 
+        std::vector<int> v(size, 42);               // something that takes some time to do
+ 
+        auto end = std::chrono::steady_clock::now();     // the new current "timepoint"
+ 
+        auto elapsed = end - start;                 // difference is a "duration"
+ 
+        std::cout << size << ": " << elapsed.count() << '\n';  // clock ticks (seconds)
+    }
+}
