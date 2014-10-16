401ada820f1f05ca37feeff7bb658ae9194e2498
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-16 20:52:32 +0000

    Update archive 22

---
+++ trunk/Archive2/22/568e7501014332/main.cpp
@@ -0,0 +1,26 @@
+#include <type_traits>
+#include <algorithm>
+
+struct F
+{
+    int operator()(bool a) { return true; }
+};
+
+template<
+  typename T, typename A,
+  typename Selector,
+  typename R=typename std::result_of<Selector(T)>::type
+>
+    std::vector<R> select(std::vector<T, A> const & c, Selector&& s) {
+    std::vector<R> v;
+    std::transform(begin(c), end(c), back_inserter(v), std::forward<Selector>(s));
+    return v;
+}
+
+int main()
+{    
+    std::vector<bool> a;    
+    F f;
+    const F& r = f;    
+    select(a, r);        
+}
