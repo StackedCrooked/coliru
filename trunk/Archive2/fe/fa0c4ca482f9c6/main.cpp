82bf0ff8bcb9d823e0cd5290f32ab35d5102f899
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-16 17:14:21 +0000

    Update archive fe

---
+++ trunk/Archive2/fe/fa0c4ca482f9c6/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+int main()
+{
+    char const *a = "my string";
+    char *b = new char[strlen(a) + 1]{};
+    std::copy(a, a + strlen(a), b);
+    std::cout << '-' << b << '-';
+}
