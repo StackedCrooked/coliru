ad44f90ac7bbd9a22ef5951cd8fa02c3f284ef57
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-16 17:13:17 +0000

    Update archive d3

---
+++ trunk/Archive2/d3/0e5e4e9381cdbc/main.cpp
@@ -0,0 +1,29 @@
+#include <cstdlib>
+#include <iostream>
+#include <boost/coroutine/all.hpp>
+
+
+using Pull = boost::coroutines::asymmetric_coroutine<std::string>::pull_type;
+using Push = boost::coroutines::asymmetric_coroutine<std::string>::push_type;
+
+
+Push push([&](Pull& pull)
+{
+    while (true)
+    {
+        std::cout << pull.get() << ' ';
+        pull();
+    }
+});
+
+
+int main()
+{
+    push("1");
+    push("2");
+    push("3");
+    push("a");
+    push("b");
+    push("c");
+    std::cout << "\nEnd of program.\n";
+}
