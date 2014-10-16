c16b3a58cbda9cefc5f85a6d3f07f8f1c1befedf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-16 16:56:08 +0000

    Update archive 0c

---
+++ Archive2/0c/bddcf69eadd14e/main.cpp
@@ -0,0 +1,26 @@
+#include <regex>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::regex r("<img[^>]*src=['|\"](.*?)['|\"].*?>");
+	std::smatch results;
+	std::string test_str = R"delimiter(
+        <div>
+            <br />
+            <img width="980" height="1515" id="mainImg" src="//test/123.jpg" alt="test">
+        </div>
+        <br />
+    )delimiter";
+
+	std::sregex_iterator it(test_str.begin(), test_str.end(), r);
+	std::sregex_iterator end;
+
+	while (it != end)
+	{
+		std::cout << (*it)[1].str()<<std::endl;
+		++it;
+	}
+
+	std::cin.get();
+}
