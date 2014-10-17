05867b22f9759184e4115ddb7027c62d987397a2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-17 10:38:12 +0000

    Update archive e7

---
+++ trunk/Archive2/e7/121f9c2f8ab762/main.cpp
@@ -0,0 +1,19 @@
+template <class T>
+class A {
+    int i;
+  public:
+    template <class U>
+    void copy_i_from( const A<U> & a ){
+        i = a.i;
+    }
+  template <class F>
+  template <class U>
+  friend void A<F>::copy_i_from(const A<U> & a);
+};
+
+int main(void) {
+    A<int> ai;
+    A<double> ad;
+    ai.copy_i_from(ad);
+    return 0;
+}
