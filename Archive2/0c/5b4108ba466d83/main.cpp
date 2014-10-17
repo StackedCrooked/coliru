cef5bd3b1d25cbb6295c9358435b17dc3f0dfdfc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-17 10:53:23 +0000

    Update archive 0c

---
+++ Archive2/0c/5b4108ba466d83/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+template <typename U, typename T>
+class params
+{
+    T m_value;
+
+public:
+    params(const T val)
+        : m_value(val)
+    {
+    }
+    
+    bool check(int value) const
+    {
+        return (m_value + value) % 2;
+    }
+};
+
+template <>
+bool params<float, long>::check(int value) const
+{
+    return (m_value * value) % 2;
+}
+
+int main()
+{
+    params<float, int> p1(3);
+    std::cout << p1.check(5) << std::endl;
+    
+    params<float, long> p2(3);
+    std::cout << p2.check(5) << std::endl;
+}
