97518b85dd906061ae73ead02d67925b26ad65e9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-18 12:52:55 +0000

    Update archive 92

---
+++ Archive2/92/11bdc22ef8ad55/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <utility>
+
+using namespace std;
+
+class Hoge
+{
+    void func()
+    {
+    }
+};
+
+template <typename T>
+class has_func
+{
+    template <typename U>
+    static auto type_check(U obj) -> decltype(obj.func(), true_type());
+    static auto type_check(...) -> false_type;
+
+public:
+    using type = decltype(type_check(declval<T>()));
+};
+
+int main()
+{ 
+    cout << "Hoge has " << (has_func<Hoge>::type::value ? "func" : "not func") << endl;
+}
