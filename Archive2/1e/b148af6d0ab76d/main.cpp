405218b039a22a392d302a08313aefdb21665a74
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-18 16:42:21 +0000

    Update archive 1e

---
+++ Archive2/1e/b148af6d0ab76d/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <locale>
+
+int main()
+{
+    std::locale loc = std::locale ("en_US.UTF-8");
+    std::wcout.imbue(loc);
+    std::cout << "ي" << std::endl; //OK!
+    //std::wcout << "ي" << std::endl;
+    //std::wcout << "L specifier = " << L"ي" << std::endl;
+    std::cout << "u specifier = " << u"ي" << std::endl;
+    std::cout << "u8 specifier = " << u8"ي" << std::endl;
+}
+
