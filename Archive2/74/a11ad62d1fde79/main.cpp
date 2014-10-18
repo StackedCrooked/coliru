de73e11a665473b8473f271e289d07e8c53946ff
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-18 06:50:25 +0000

    Update archive 74

---
+++ Archive2/74/a11ad62d1fde79/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <boost/date_time/gregorian/gregorian.hpp>
+#include <boost/format.hpp>
+
+int main()
+{
+    using namespace boost::gregorian;
+
+	auto const today = day_clock::local_day();
+    auto const dow_ = date(today.year(), today.month(), 1).day_of_week();
+    auto const d = gregorian_calendar::end_of_month_day(today.year(), today.month());
+
+	for (auto sw = 0, i = 1; sw < d; i++) {
+		if (i > dow_)
+			std::cout << boost::format("%2d ") % ++sw;
+		else
+			std::cout << "   ";
+	
+		if (!(i % 7) || sw == d) std::cout << '\n';
+	}
+}
