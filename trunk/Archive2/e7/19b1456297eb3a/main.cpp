0f967f01d0b62c3345ee50d75c2dc1a4b8585a3e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-19 23:55:44 +0000

    Update archive e7

---
+++ trunk/Archive2/e7/19b1456297eb3a/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <tuple>
+#include <utility>
+#include <cstddef>
+
+template <std::size_t... Is>
+struct index_sequence {};
+
+template <std::size_t N, std::size_t... Is>
+struct make_index_sequence_h : make_index_sequence_h<N - 1, N - 1, Is...> {};
+
+template <std::size_t... Is>
+struct make_index_sequence_h<0, Is...>
+{
+    using type = index_sequence<Is...>;
+};
+
+template <std::size_t N>
+using make_index_sequence = typename make_index_sequence_h<N>::type;
+
+template <int dim, class T>
+class linear_searchspace
+{
+public:
+    template <typename... Ts>
+    inline constexpr auto operator()(Ts... args) const noexcept
+        -> std::tuple<decltype(args, void(), T{})...>
+    {
+        static_assert(sizeof...(Ts) == dim, "!");
+        using Tuple = std::tuple<decltype(args, void(), T{})...>;
+        return go<Tuple>(make_index_sequence<sizeof...(Ts)>{}, args...);
+    }
+
+private:
+    template <typename Tuple, typename... Ts, std::size_t... Is>
+    inline constexpr Tuple go(index_sequence<Is...>, Ts... args) const noexcept
+    {
+        return Tuple((some_kinda_structure[args] + Is)...);
+    }
+
+    T some_kinda_structure[dim] = { 0, 1, 2, 3, 4 };
+};
+
+int main()
+{
+    linear_searchspace<5, int> lsp{};
+    auto t = lsp(0, 1, 2, 3, 4);
+    std::cout << std::get<1>(t) << std::endl;
+
+    auto t2 = lsp(4, 3, 2, 1, 0);
+    std::cout << std::get<0>(t2) << std::endl;
+}
