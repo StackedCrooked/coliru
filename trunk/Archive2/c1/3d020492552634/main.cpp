b071f750362b39ecc89fe7e3dac628c06c610188
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-19 00:05:17 +0000

    Update archive c1

---
+++ trunk/Archive2/c1/3d020492552634/main.cpp
@@ -0,0 +1,17 @@
+template <typename... Ts> struct tuple {};
+template <typename T, typename... Ts> struct tuple<T, Ts...> : tuple<Ts...> { T head; };
+template <bool b, typename T = void> struct enable {};
+template <typename T> struct enable<true, T> { using type = T; };
+template <typename T, typename U> struct same { static constexpr bool value = false; };
+template <typename T> struct same<T, T> { static constexpr bool value = true; };
+template <typename T, typename U, typename... Ts> auto get(tuple<U, Ts...>& t) -> typename enable<same<T, U>::value, T>::type& { return t.head; }
+template <typename T, typename U, typename... Ts> auto get(tuple<U, Ts...>& t) -> typename enable<!same<T, U>::value, T>::type& { return get<T>(static_cast<tuple<Ts...>&>(t)); }
+template <int N, typename T, typename... Ts, typename enable<N == 0>::type* = nullptr> auto& get(tuple<T, Ts...>& t) { return t.head; }
+template <int N, typename T, typename... Ts, typename enable<N != 0>::type* = nullptr> auto& get(tuple<T, Ts...>& t) { return get<N - 1>(static_cast<tuple<Ts...>&>(t)); }
+
+#include <iostream>
+int main()
+{
+    struct Empty {};
+    std::cout << sizeof(tuple<int, Empty>);
+}
