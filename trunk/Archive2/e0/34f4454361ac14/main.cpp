20f658e9a4303f5e8f191177e8e8af5fc49e3e3f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-19 20:06:26 +0000

    Update archive e0

---
+++ trunk/Archive2/e0/34f4454361ac14/main.cpp
@@ -0,0 +1,27 @@
+#include <type_traits>
+#include <vector>
+
+template<typename Type, typename...> struct depend_on { using type = Type; };
+template<typename Type, typename... Dependee> using depend_on_t = typename depend_on<Type, Dependee...>::type;
+
+template<typename Type>
+struct foo {
+    std::vector<Type> v;
+    
+    template<typename D = void, std::enable_if_t<!std::is_same<depend_on_t<Type, D>, double>::value, int>...>
+    std::size_t size() const { return v.size(); }
+    
+    template<typename D = void, std::enable_if_t<std::is_same<depend_on_t<Type, D>, double>::value, int>...>
+    std::size_t size() const { return v.size() * 3; }
+};
+
+#include <cassert>
+
+int main()
+{
+    foo<int> f { { 0, 1, 2, 3, 4, 5 }};
+    assert( f.size() == 6 );
+    
+    foo<double> g { { 0, 1, 2, 3, 4, 5 }};
+    assert( g.size() == 6*3 );
+}
