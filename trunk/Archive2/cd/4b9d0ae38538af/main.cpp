10c58f5006c055c77739f0e6956484b235e6303d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-19 03:39:31 +0000

    Update archive cd

---
+++ trunk/Archive2/cd/4b9d0ae38538af/main.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <type_traits>
+
+// Bigger between two types
+template<typename T1, typename T2> 
+using Bigger = std::conditional_t<sizeof(T1) >= sizeof(T2), T1, T2>;
+
+// Recursion helper
+template<typename...> 
+struct BiggestHelper;
+
+// 2 or more types
+template<typename T1, typename T2, typename... TArgs> 
+struct BiggestHelper<T1, T2, TArgs...>
+{
+    using Type = Bigger<T1, typename BiggestHelper<T2, TArgs...>::Type>;
+};
+
+// Exactly 2 types
+template<typename T1, typename T2> 
+struct BiggestHelper<T1, T2>
+{
+    using Type = Bigger<T1, T2>;
+};
+
+// Exactly one type
+template<typename T> 
+struct BiggestHelper<T>
+{
+    using Type = T;
+};
+
+template<typename... TArgs> 
+using Biggest = typename BiggestHelper<TArgs...>::Type;
+
+static_assert(sizeof(Biggest<int, char, long, std::string, long long, double>) == sizeof(std::string), "oops");
+int main() {}
