f359ee1ecdb79e2cc14890872ab5ec793ca6e778
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-20 20:29:45 +0000

    Update archive 48

---
+++ Archive2/48/ad92e855f4fddc/main.cpp
@@ -0,0 +1,15 @@
+#include <cstddef>
+#include <utility>
+#include <iostream>
+
+void print(std::initializer_list<std::size_t> il)
+{
+    for (auto&& elem : il) std::cout << elem << std::endl;
+}
+
+int main()
+{
+    auto indexed_lambda = [] <std::size_t... Is> (std::index_sequence<Is...>) { print({Is...}); };
+    
+    indexed_lambda(std::make_index_sequence<5>{});    
+}
