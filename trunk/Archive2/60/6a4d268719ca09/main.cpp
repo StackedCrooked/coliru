0c42514568aed945de0396c3f096c6843f7af338
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-20 23:53:08 +0000

    Update archive 60

---
+++ trunk/Archive2/60/6a4d268719ca09/main.cpp
@@ -0,0 +1,22 @@
+#include <iterator>
+
+using std::begin;
+using std::end;
+
+static const char data[] = "Hello world!";
+
+struct X { 
+    friend char const* begin(X const&);
+    friend char const* end  (X const& x);
+};
+
+char const* begin(X const&)   { return data; }
+char const* end  (X const& x) { return data + sizeof(data); }
+
+#include <iostream>
+
+int main()
+{
+    for (auto ch : X())
+       std::cout << ch;
+}
