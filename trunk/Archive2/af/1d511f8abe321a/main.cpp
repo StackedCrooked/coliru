42c89c96afe01e6c90b3cbff5bd011d792d7767a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-21 07:38:29 +0000

    Update archive af

---
+++ trunk/Archive2/af/1d511f8abe321a/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <boost/bind.hpp>
+
+template <typename FooType> struct Foo {
+    const FooType tmp_value;
+
+    Foo(const FooType &tmp_) : tmp_value(tmp_) {}
+
+    typedef void result_type;
+    template <typename Object> result_type operator()(Object &operand) {
+        std::cout << operand << std::endl;
+        operand += tmp_value;
+    }
+};
+
+int main() {
+
+    double t = 4.0;
+    Foo<double> e(1.0);
+    std::cout << t << std::endl;
+    e(t); // Works
+    std::cout << t << std::endl;
+
+    double d = 5.0;
+    Foo<double> p(1.0);
+    auto f1 = boost::bind(p, _1);
+
+    std::cout << d << std::endl;
+    f1(d);
+    std::cout << d << std::endl;
+}
