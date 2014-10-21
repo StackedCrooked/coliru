6c982f945d41eb13df3f5d68390e8706e462b43c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-21 17:04:14 +0000

    Update archive 59

---
+++ trunk/Archive2/59/4c2c5ffdba13a2/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+template <typename AnArray>
+void f(AnArray& array){
+    std::cout << sizeof array/sizeof array[0] << std::endl;
+    array.hiThere();
+}
+
+template <typename AnArray>
+void g(AnArray array){
+    std::cout << sizeof array/sizeof array[0] << std::endl;
+}
+
+int main(){
+    int a[] = {1,2,3,4,5};
+    f(a);
+    g(a);
+    return 0;
+}
