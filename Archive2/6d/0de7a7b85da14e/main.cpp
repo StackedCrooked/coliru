7bc6859727adb11c7dafec5de19a95dde3babbb9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-21 20:47:02 +0000

    Update archive 6d

---
+++ Archive2/6d/0de7a7b85da14e/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+template <typename AnArray>
+void f(AnArray& array){
+    std::cout << sizeof array/sizeof array[0] << std::endl;
+}
+
+int main(){
+    int a[] = {1,2,3,4,5};
+    f(a);
+    return 0;
+}
