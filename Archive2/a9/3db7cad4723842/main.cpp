306475a7ea84eb30b78cee264817b599ba090c6e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-21 10:49:11 +0000

    Update archive a9

---
+++ Archive2/a9/3db7cad4723842/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <random>
+#include <cmath>
+int main()
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+ 
+    // valuesn affects the dispersion of generated values from the mean
+    std::normal_distribution<> d(5,2);
+ 
+    std::map<int, int> hist;
+    for(int n=0; n<10000; ++n) {
+        ++hist[std::round(d(gen))];
+    }
+    for(auto p : hist) {
+        std::cout << std::fixed << std::setprecision(1) << std::setw(2)
+                  << p.first << ' ' << std::string(p.second/200, '*') << '\n';
+    }
+}
