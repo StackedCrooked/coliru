1bdde70ba93495021f7acf30223f59c469418f14
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-22 17:15:44 +0000

    Update archive 7e

---
+++ Archive2/7e/49b8691e4d3b58/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+template<typename T>
+void f(T s)
+{
+    std::cout << s << '\n';
+}
+ 
+int main()
+{
+    f<double>(1); // instantiates and calls f<double>(double)
+    f<>('a'); // instantiates and calls f<char>(char)
+    f(7); // instantiates and calls f<int>(int)
+}
