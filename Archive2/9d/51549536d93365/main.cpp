1f3153dcb51a5a008f6d7aa9781992464fc781f0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-22 20:17:05 +0000

    Update archive 9d

---
+++ Archive2/9d/51549536d93365/main.cpp
@@ -0,0 +1,29 @@
+#include <type_traits>
+#include <iostream>
+
+template <typename... Ts>
+using void_t = void;
+
+template <typename T> struct is_int {}; 
+template <> struct is_int<int> : std::true_type {};
+
+template <typename T>
+using requires_integer = typename is_int<T>::type;
+
+template <typename T, typename Enable = void>
+struct Widget
+{    
+    Widget() { std::cout << "T is something else" << std::endl; }
+};
+
+template <typename T>
+struct Widget<T, void_t<requires_integer<T>>>
+{
+    Widget() { std::cout << "T is int" << std::endl; }
+};
+
+int main()
+{
+    Widget<int>{};
+    Widget<float>{};
+}
