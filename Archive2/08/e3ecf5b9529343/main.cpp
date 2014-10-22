6a640caa164625483bd72b014d683c0e52030d2a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-22 16:53:59 +0000

    Update archive 08

---
+++ Archive2/08/e3ecf5b9529343/main.cpp
@@ -0,0 +1,26 @@
+#include <cstddef>
+#include <tuple>
+#include <utility>
+
+template <typename Tuple, typename F, std::size_t ...Indices>
+void for_each_impl(Tuple&& tuple, F&& f, std::index_sequence<Indices...>) {
+    using swallow = int[];
+    (void)swallow{1,
+        (f(std::get<Indices>(std::forward<Tuple>(tuple))), void(), int{})...
+    };
+}
+
+template <typename Tuple, typename F>
+void for_each(Tuple&& tuple, F&& f) {
+    constexpr std::size_t N = std::tuple_size<std::remove_reference_t<Tuple>>::value;
+    for_each_impl(std::forward<Tuple>(tuple), std::forward<F>(f),
+                  std::make_index_sequence<N>{});
+}
+
+#include <iostream>
+
+int main() {
+    for_each(std::make_tuple(1, '2', 3.3), [](auto x) {
+        std::cout << x << std::endl;
+    });
+}
