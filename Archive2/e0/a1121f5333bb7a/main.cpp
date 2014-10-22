ed2018ed293733f0c0559abc5a1a3e858be47e32
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-22 03:48:57 +0000

    Update archive e0

---
+++ Archive2/e0/a1121f5333bb7a/main.cpp
@@ -0,0 +1,22 @@
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/qi_match.hpp>
+#include <sstream>
+
+namespace qi = boost::spirit::qi;
+
+int main()
+{
+    std::istringstream buffer("NbProducts = 1234;");
+
+    int NbProducts;
+    if (buffer >> qi::phrase_match(
+            qi::lit("NbProducts") >> "=" >> qi::int_ >> ";", 
+            qi::space, 
+            NbProducts))
+    {
+        std::cout << "Matched: " << NbProducts << "\n";
+    } else
+    {
+        std::cout << "Not matched\n";
+    }
+}
