a9c86770a43422de1941f10510f53a4374329cfa
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-23 15:59:28 +0000

    Update archive 84

---
+++ trunk/Archive2/84/5ff1acfb1b2a3b/main.cpp
@@ -0,0 +1,24 @@
+#include <boost/lambda/lambda.hpp>
+#include <boost/lambda/bind.hpp>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+struct X
+{
+    int x;
+	std::string accessor() const { return "X[" + std::to_string(x) + "]"; } // I know, requires c++11
+};
+
+int main()
+{
+	std::vector<X> v;
+	v.push_back({ 1 });
+	v.push_back({2});
+	v.push_back({3});
+	v.push_back({4});
+	v.push_back({5});
+
+	std::for_each(v.begin(), v.end(), std::cout << boost::lambda::bind(&X::accessor, boost::lambda::_1) << "\n");
+}
