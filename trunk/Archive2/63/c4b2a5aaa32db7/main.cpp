4e3cbd9e23c4c5b49c2c3414aa8791bd6222052e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-23 04:02:14 +0000

    Update archive 63

---
+++ trunk/Archive2/63/c4b2a5aaa32db7/main.cpp
@@ -0,0 +1,23 @@
+#include <type_traits>
+#include <iostream>
+
+template <typename T, typename Enable = void>
+struct Widget;
+
+template <typename T>
+struct Widget<T, typename std::enable_if<std::is_integral<T>::value>::type>
+{
+    Widget() { std::cout << "is_integral<T>" << std::endl; }
+};
+
+template <typename T>
+struct Widget<T, typename std::enable_if<std::is_floating_point<T>::value>::type>
+{
+    Widget() { std::cout << "is_floating_point<T>" << std::endl; }
+};
+
+int main()
+{
+    Widget<int>{};
+    Widget<float>{};
+}
