03fb525e5e8bd64c40c09d6689b6542df6f70780
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-24 00:27:53 +0000

    Update archive 8b

---
+++ Archive2/8b/8eb668487b362c/main.cpp
@@ -0,0 +1,64 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <forward_list>
+#include <random>
+#include <vector>
+
+template <typename T>
+struct mallocator {
+  using value_type = T;
+
+  mallocator() = default;
+  template <class U>
+  mallocator(const mallocator<U>&) {}
+
+  std::size_t max_size() const {
+      return std::numeric_limits<std::size_t>::max() / sizeof(T);
+  }
+  
+  T* allocate(std::size_t n) {
+    std::cout << "allocate(" << n << ") = ";
+    if (n <= max_size()) {
+      auto ptr = std::malloc(n * sizeof(T));
+      if (ptr) {
+        std::cout << ptr << '\n';
+        return static_cast<T*>(ptr);
+      }
+    }
+    std::cout << "[bad_alloc]\n";
+    throw std::bad_alloc();
+  }
+  void deallocate(T* ptr, std::size_t n) {
+    std::cout << "deallocate(" << ptr << ", " << n << ")\n";
+    std::free(ptr);
+  }
+};
+
+template <typename T, typename U>
+inline bool operator == (const mallocator<T>&, const mallocator<U>&) {
+  return true;
+}
+
+template <typename T, typename U>
+inline bool operator != (const mallocator<T>& a, const mallocator<U>& b) {
+  return !(a == b);
+}
+
+int main() {
+    auto engine = std::mt19937_64{std::random_device{}()};
+    auto dist = std::uniform_int_distribution<int>{};
+    auto rng = [&]{return dist(engine);};
+
+    {
+        // vector.
+        auto c = std::vector<int, mallocator<int>>{};
+        std::generate_n(std::back_inserter(c), 1024, rng);
+    }
+
+    {
+        // forward_list
+        auto c = std::forward_list<int, mallocator<int>>{};
+        std::generate_n(std::front_inserter(c), 1024, rng);
+    }
+}
