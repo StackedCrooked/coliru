947dc344cf9847ca58281ad93a68591f8412add4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-24 00:29:20 +0000

    Update archive 7f

---
+++ trunk/Archive2/7f/76c6f0a18abb97/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void weird_print_all() { }
+
+template<typename T, typename... Ts>
+void weird_print_all(T const& x, Ts const&... ts)
+{
+    std::cout << x << " ";
+    weird_print_all((ts + ts)...);
+}
+
+int main()
+{
+    weird_print_all(1, 3.14, std::string{"Hello"});
+}
