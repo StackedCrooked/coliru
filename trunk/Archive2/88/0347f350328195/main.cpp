c34cb6859f2b735a87b5446d019308898e99b5bc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-24 00:47:34 +0000

    Update archive 88

---
+++ trunk/Archive2/88/0347f350328195/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void print_all() { }
+
+template<typename T, typename... Ts>
+void print_all(T const& x, Ts const&... ts)
+{
+    std::cout << x << " ";
+    print_all(ts...);
+}
+
+int main()
+{
+    print_all(1, 3.14, 'c', "Hello");
+    
+    std::cout << std::endl;
+    
+    print_all(1, 'c');
+}
