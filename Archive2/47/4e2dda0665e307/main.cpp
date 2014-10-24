c96d41f8ae47bd925014d818ab8cd5f9d51488ef
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-24 10:13:36 +0000

    Update archive 47

---
+++ Archive2/47/4e2dda0665e307/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+template <class T, size_t N> constexpr size_t size(const T(&array)[N]) noexcept
+{
+    return N;
+}
+
+template <class T> constexpr size_t size(const T& array) noexcept
+{
+    return 0;
+}
+
+int main(){
+    int a[0];
+    
+    std::cout << size(a);
+    
+    return 0;
+}
