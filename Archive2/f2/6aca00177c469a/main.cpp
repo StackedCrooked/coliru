5b02d8dd1f5d09fe3db509e48740b0ab84bbdf6f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-24 12:19:39 +0000

    Update archive f2

---
+++ Archive2/f2/6aca00177c469a/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <utility>
+
+struct Noisy
+{
+    Noisy() { std::cout << "Noisy()" << std::endl; }
+    Noisy(const Noisy&) { std::cout << "Noisy(const Noisy&)" << std::endl; }
+    Noisy(Noisy&&) { std::cout << "Noisy(Noisy&&)" << std::endl; }
+    ~Noisy() { std::cout << "~Noisy()" << std::endl; }
+};
+
+void target(Noisy n) { }  // real function stored in std::function
+
+void delegate3(Noisy&& n) { target(std::forward<Noisy>(n)); }  // internal delegate3
+void delegate2(Noisy&& n) { delegate3(std::forward<Noisy>(n)); }  // internal delegate2
+void delegate1(Noisy&& n) { delegate2(std::forward<Noisy>(n)); }  // internal delegate1
+
+void source(Noisy n) { delegate1(std::forward<Noisy>(n));  }  // std::function
+
+int main()
+{
+    source(Noisy{});
+}
