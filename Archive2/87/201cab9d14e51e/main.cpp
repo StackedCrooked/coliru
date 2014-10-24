812348360d67c56ea9f2523a479e46fe99679858
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-24 00:12:34 +0000

    Update archive 87

---
+++ Archive2/87/201cab9d14e51e/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <mutex>
+
+
+// Thread-safe wrapper for an object of type T.
+// Provides access via functor or pointer-to-member-function.
+template<typename T>
+struct Synchronized
+{
+    template<typename F, typename ...Args>
+    auto apply(F f, Args&& ...args) const -> decltype(std::bind(f, std::declval<const T&>(), std::forward<Args>(args)...)())
+    {
+        std::lock_guard<std::mutex> lock(mMutex);
+        return std::bind(f, std::ref(t), std::forward<Args>(args)...)();
+    }
+    
+    template<typename F, typename ...Args>
+    auto apply(F f, Args&& ...args)  -> decltype(std::bind(f, std::declval<T&>(), std::forward<Args>(args)...)())
+    {
+        std::lock_guard<std::mutex> lock(mMutex);
+        return std::bind(f, std::ref(t), std::forward<Args>(args)...)();
+    }
+    
+
+private:
+    mutable std::mutex mMutex;
+    T t;
+};
+
+
+struct Point
+{
+    void set(int x, int y) { this->x = x; this->y = y; }
+    
+    void move(int dx, int dy)
+    {
+        x += dx;
+        y += dy;
+    }
+    
+    void print() const
+    {
+        std::cout << "x=" << x << " y=" << y << '\n';
+    }
+    
+    int x, y;
+};
+                   
+int main()
+{
+    Synchronized<Point> point;
+    
+    point.apply([&](Point& p) { p.x = 1; p.y = 2; });
+    point.apply(&Point::print);    
+    
+    point.apply(&Point::set, 3, 4);    
+    point.apply(&Point::print);
+    
+    point.apply(&Point::move, 1, -1);
+    point.apply(&Point::print);
+}
