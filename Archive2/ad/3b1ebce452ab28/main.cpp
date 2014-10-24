997c140f729361c50ada78516ab389313e51b262
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-24 00:43:31 +0000

    Update archive ad

---
+++ Archive2/ad/3b1ebce452ab28/main.cpp
@@ -0,0 +1,23 @@
+#include <type_traits>
+#include <iostream>
+ 
+template<class T>
+typename std::result_of<T(int)>::type
+f(T& t)
+{
+    std::cout << "overload of f for callable T\n";
+    return t(0);
+}
+ 
+template<class T, class U>
+int f(U u)
+{
+    std::cout << "overload of f for non-callable T\n";
+    return u;
+}
+ 
+struct S {};
+ 
+int main() {
+  f<S>(1); // fails to compile in C++11, calls the non-callable overload in C++14
+}
