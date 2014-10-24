f6a41879ef531154ec294d4185331b862a0446c1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-24 20:13:53 +0000

    Update archive a1

---
+++ Archive2/a1/af94824d6fa890/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <functional>
+#include <future>
+#include <thread>
+
+class test
+{
+    int fthread(int b)
+    {
+        return a + b;
+    }
+
+    int a;
+public:
+    test(int a_) : a(a_) {}
+
+    int run(int b)
+    {
+        std::packaged_task<int(int)>
+                task(std::bind(&test::fthread, this, std::placeholders::_1));
+        std::future<int> fut = task.get_future();
+
+        std::thread a(std::move(task), b);
+        a.join();
+
+        return fut.get();
+    }
+
+    int run2(int b)
+    {
+        return std::async(std::launch::async, [this,b]{ return fthread(b); }).get();
+    }
+};
+
+int main()
+{
+    int a = 5, b = 1;
+    test t(a);
+
+    int c = t.run(b);
+
+    std::cout << std::boolalpha << (c == a + b) << '\n';
+    std::cout << (a + b == t.run2(b)) << '\n';
+}
