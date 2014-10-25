2680df7f4261f32964a8f6247afb7ba808701830
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-25 00:06:00 +0000

    Update archive fb

---
+++ trunk/Archive2/fb/a80660fb9e9f57/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <stack>
+
+int main()
+{
+  typedef std::uint64_t num;
+
+  // ys[x] is the length of the chain starting at x.
+  // xms is the set of numbers that yield the longest chains.
+  // ym is the length of the longest chain so far.
+  std::map<num, num> ys = {{1,1}};
+  std::set<num> xms = {1};
+  num ym = 1;
+
+  for (num i = 2; i < 1000000; ++i)
+  {
+    std::stack<num> xs;
+    num x = i, y = ys[x];
+
+    // Compute successive chain elements until
+    // a base element is reached whose chain
+    // length has already been memoized.
+    while (!y)
+    {
+      xs.push(x);
+      x = (x & 1) ? (3*x + 1) : (x/2);
+      y = ys[x];
+    }
+
+    // The lengths of the newly computed elements
+    // can be found by repeatedly incrementing
+    // the base element's chain length.
+    while (!xs.empty())
+    {
+      x = xs.top();
+      ys[x] = ++y;
+      xs.pop();
+    }
+
+    // Keep track of which number(s)
+    // yield the longest chain(s).
+    if (y >= ym)
+    {
+      if (y > ym)
+      {
+        xms.clear();
+        ym = y;
+      }
+      xms.insert(x);
+    }
+  }
+
+  std::cout << "Number(s) yielding the longest chain(s): ";
+  for (num xm : xms)
+    std::cout << xm << ' ';
+  std::cout << std::endl << "Length of longest chain: "
+            << ym << std::endl;
+
+  return 0;
+}
