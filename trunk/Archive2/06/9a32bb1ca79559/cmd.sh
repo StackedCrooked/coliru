20242d6f5f0332069b534bdc630bfa163b1c5afc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-25 16:11:22 +0000

    Update archive 06

---
+++ trunk/Archive2/06/9a32bb1ca79559/cmd.sh
@@ -0,0 +1,4 @@
+clang++ --version
+clang++ -std=c++14 -Wall -Wextra -pedantic-errors main.cpp && ./a.out
+g++ --version
+g++ -std=c++14 -Wall -Wextra -pedantic-errors -O2 -pthread main.cpp && ./a.out
