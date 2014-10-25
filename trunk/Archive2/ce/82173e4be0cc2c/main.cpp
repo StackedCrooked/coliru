de4bdebb3e59b6161c5a8390fa6d13b8a0ef5064
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-25 23:42:04 +0000

    Update archive ce

---
+++ trunk/Archive2/ce/82173e4be0cc2c/main.cpp
@@ -0,0 +1,33 @@
+#include <boost/array.hpp>
+#include <boost/utility/string_ref.hpp>
+#include <cassert>
+
+template <size_t N>
+struct fixed_string : boost::string_ref {
+    fixed_string() : boost::string_ref(storage_.begin(), 0) { }
+
+    template <size_t M>
+    fixed_string(char const (&s)[M]) 
+        : boost::string_ref(storage_.begin(), M-1)
+    {
+        static_assert(M<=N+1, "capacity exceeded");
+        assert(!s[M-1]);
+        std::copy(s, s+(M-1), storage_.begin());
+    }
+
+  private:
+    boost::array<char, N> storage_ = {};
+};
+
+#include <iostream>
+
+int main()
+{
+    fixed_string<32> demo;
+    demo = "hello";
+
+    std::cout << "fs: '" << demo << "'\n";
+    demo = "world!";
+    std::cout << "fs: '" << demo << "'\n";
+
+}
