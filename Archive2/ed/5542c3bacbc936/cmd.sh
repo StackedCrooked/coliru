411e8051f0947529bf7bc8e842b87869992f0158
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-25 15:56:52 +0000

    Update archive ed

---
+++ Archive2/ed/5542c3bacbc936/cmd.sh
@@ -0,0 +1,4 @@
+clang++ --version
+clang++ -std=c++03 -Wall -Wextra -pedantic-errors main.cpp && ./a.out
+g++ --version
+g++ -std=c++03 -Wall -Wextra -pedantic-errors -O2 -pthread main.cpp && ./a.out
---
+++ Archive2/ed/5542c3bacbc936/main.cpp
@@ -0,0 +1,16 @@
+namespace N
+{
+    typedef int T;
+    
+    enum E{};
+}
+
+template <typename N::T>
+struct ST{};
+
+template <typename N::E>
+struct SE{};
+
+int main()
+{
+}
