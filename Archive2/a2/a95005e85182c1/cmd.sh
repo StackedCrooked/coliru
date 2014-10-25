3852c32e2b88dbab2998a2fb221d3d213261b2ca
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-25 16:00:52 +0000

    Update archive a2

---
+++ Archive2/a2/a95005e85182c1/cmd.sh
@@ -0,0 +1,4 @@
+clang++ --version
+clang++ -std=c++11 -Wall -Wextra -pedantic-errors main.cpp && ./a.out
+g++ --version
+g++ -std=c++11 -Wall -Wextra -pedantic-errors -O2 -pthread main.cpp && ./a.out
