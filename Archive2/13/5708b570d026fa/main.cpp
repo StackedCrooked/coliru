ce1179884dd51ca5ebb3701e9325a8d876a08078
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-25 13:24:13 +0000

    Update archive 13

---
+++ Archive2/13/5708b570d026fa/main.cpp
@@ -0,0 +1,15 @@
+#include <type_traits>
+
+template <typename T, typename=void> struct is_complete : std::false_type {};
+template <typename T>
+struct is_complete<T, typename std::enable_if<sizeof(T)>::type> : std::true_type {};
+
+struct A;
+struct B {};
+using array = int[];
+
+static_assert( not is_complete<A>::value );
+static_assert(     is_complete<B>::value );
+static_assert( not is_complete<array>::value );
+
+int main () {}
