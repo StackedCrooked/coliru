f0f701891cf3a1d7a66dcd78c4bf08a29a778ccd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-26 03:58:08 +0000

    Update archive f8

---
+++ Archive2/f8/f449a34a2a19da/main.cpp
@@ -0,0 +1,19 @@
+#include <array>
+
+template<unsigned N>
+constexpr bool foo()
+{
+    std::array<char, N> arr;
+    return true;
+}
+/*
+constexpr bool foo()
+{
+    std::array<char, 10> arr; // Non-constexpr constructor 'array' cannot be used in a constant expression
+    return true;
+}
+*/
+int main()
+{
+    foo<10>();
+}
