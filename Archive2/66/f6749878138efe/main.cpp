a337ff37fd01a83bdad4bea8e9d4046a6b4af755
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-26 16:22:11 +0000

    Update archive 66

---
+++ Archive2/66/f6749878138efe/main.cpp
@@ -0,0 +1,29 @@
+#include <map>
+#include <iostream>
+
+template <typename T1, typename T2>
+class SampleClass {
+
+public:
+    SampleClass() = default;
+
+    void Add(const T1 &key, const T2 &value)
+    {
+        database[key] = value;
+    }
+
+    typename std::map<T1, T2>::const_iterator cbegin() const { return database.cbegin(); }
+    typename std::map<T1, T2>::const_iterator cend() const { return database.cend(); }
+
+
+private:
+    std::map<T1, T2> database;
+};
+
+template <typename T1, typename T2> 
+std::ostream &operator << (std::ostream &os, SampleClass<T1, T2> &rhs)
+{
+    for(auto el = rhs.cbegin(); el != rhs.cend(); el++)
+        os << el->first << " " << el->second << std::endl;
+    return os;
+}
