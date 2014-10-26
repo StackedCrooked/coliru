90928c61c969bcf75e6b84ef02abfc9e07930763
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-26 11:57:33 +0000

    Update archive 6f

---
+++ Archive2/6f/a947179815a36b/main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+printf("LONG_MAX=%ld,sizeof=%ld\n",LONG_MAX,sizeof(long));
+printf("INT_MAX=%d,sizeof=%ld\n",INT_MAX,sizeof(int));
+return 0;
+}
