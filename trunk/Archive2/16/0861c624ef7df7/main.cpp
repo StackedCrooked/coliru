15bf485d1fd2948f185dcfac975f6ba38a6172a6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-26 17:57:27 +0000

    Update archive 16

---
+++ trunk/Archive2/16/0861c624ef7df7/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+struct Y
+{
+    int i;
+};
+
+struct X
+{
+    int i;
+    
+    X(int i) : i(i) {}
+    
+    operator Y() const {return {i};}
+};
+
+int main()
+{
+    std::vector<X> argumentVector{1, 2, 3, 4, 5};
+    
+    std::vector<Y> argumentList;
+    // […]
+    argumentList.insert(argumentList.end(), argumentVector.begin(), argumentVector.end());
+    
+    for (auto y : argumentList)
+        std::cout << y.i << ", ";
+}
