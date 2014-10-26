2e0270a184b2634554dc949bb49f395300ca4d9c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-26 15:46:14 +0000

    Update archive de

---
+++ trunk/Archive2/de/a663fb69f530ed/main.cpp
@@ -0,0 +1,29 @@
+#include <cassert>
+#include <boost/asio.hpp>
+#include <boost/asio/spawn.hpp>
+
+int main()
+{
+  boost::asio::io_service io_service;
+  boost::asio::io_service::strand strand1(io_service);
+  boost::asio::io_service::strand strand2(io_service);
+  boost::asio::io_service::strand strand3(strand1);
+
+  boost::asio::spawn(strand1,
+    [&](boost::asio::yield_context yield)
+    {
+      assert(strand1.running_in_this_thread());
+      assert(!strand2.running_in_this_thread());
+      assert(strand3.running_in_this_thread());
+
+      // Access implementation details.
+      auto strand4 = yield.handler_.dispatcher_;
+      assert(strand4.running_in_this_thread());
+
+      // No way to compare strands to one another.  Although strand1, strand3,
+      // and strand4 use the same strand implementation, the strand objects
+      // are neither identifiable nor comparable.
+    });
+
+  io_service.run();
+}
