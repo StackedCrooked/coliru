5bad3387c39baf3280d6ac9bb8aa47e29076d110
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-26 18:23:12 +0000

    Update archive 50

---
+++ trunk/Archive2/50/04bd315d227c2c/main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x,y;
+  
+cout <<"Welcome \n Please Enter Start Number :";
+cin >> x;
+
+cout <<"Please Enter End Number :";
+cin >> y;
+
+for(x=x;x<=y;x++)
+
+cout << x;
+
+  return 0;
+}
