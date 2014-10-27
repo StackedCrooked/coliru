a43051a5e3d54c6073d29293ffb8fdadfc78b383
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-27 15:42:50 +0000

    Update archive 3f

---
+++ trunk/Archive2/3f/847b023a006d8d/main.cpp
@@ -0,0 +1,37 @@
+#include <windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+  int x,y;
+  
+  char oe[1];
+  char o[10];
+  char e[10];
+  
+  cout << "\n" << "Show Odd or Even? " << "o/e" << "\n";
+cin >> oe;
+  
+for(x=1;x<=50;x++)
+
+
+
+
+if("o==oe")
+if(x %2==0 )
+{
+cout << x  << '\n';
+}
+
+
+
+    	        
+	  system("pause");
+	  return 0;
+
+
+}
