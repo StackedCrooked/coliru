52ab91caf9110930442b81276052ba5f5aebea17
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-27 16:05:20 +0000

    Update archive ab

---
+++ Archive2/ab/88e60379b8193d/main.cpp
@@ -0,0 +1,51 @@
+using StateManager = int;
+using View = int;
+
+class GameState
+{
+public:
+
+    StateManager* game;
+
+    virtual void draw(const float dt) =0;
+    virtual void update(const float dt) =0;
+    virtual void handleInput()=0;
+
+};
+
+class GameStartState:public GameState
+{
+public:
+
+
+    virtual void draw(const float dt);
+    virtual void update(const float dt);
+    virtual void handleInput();
+
+    GameStartState(StateManager* game);
+
+
+private:
+
+    View view;
+};
+
+GameStartState::GameStartState(StateManager* game)
+{
+    this->game = game;
+}
+
+void GameStartState::draw(const float dt){
+
+   this->game = nullptr;
+}
+
+void GameStartState::handleInput(){
+    this->game = nullptr;
+}
+
+void GameStartState::update(const float dt) {
+    game = nullptr;
+}
+
+int main() {}
