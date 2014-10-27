73d629929d9bcbeed906898b8b726ff2ba654bde
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-27 13:27:32 +0000

    Update archive de

---
+++ Archive2/de/81d2b65508edc4/main.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+
+template <typename F>
+void fizz_buzz(int i, F const& f) {
+    if(i % 15 == 0) {
+        f("FizzBuzz");
+    } else if(i % 5 == 0) {
+        f("Buzz");
+    } else if(i % 3 == 0) {
+        f("Fizz");
+    } else {
+        f(i);
+    }
+}
+
+int main() {
+    for(int i = 1; i <= 100; ++i) fizz_buzz(i, [](auto v) { std::cout << v << '\n'; });
+}
