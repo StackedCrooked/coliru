1ab1110f36d6097440b7c5c8085fe237098dba0f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-27 14:09:04 +0000

    Update archive 1a

---
+++ Archive2/1a/94001e256d08c2/main.cpp
@@ -0,0 +1,44 @@
+#define BOOST_SPIRIT_USE_PHOENIX_V3
+#include <boost/fusion/include/std_pair.hpp>
+#include <boost/fusion/include/reverse_view.hpp>
+#include <boost/fusion/include/repetitive_view.hpp>
+#include <boost/spirit/include/karma.hpp>
+
+typedef std::pair<std::string, std::string> pair_type;
+
+typedef boost::fusion::result_of::repetitive_view<boost::fusion::result_of::reverse_view<pair_type const>::type>::type pair_as_vkvkvkvkv;
+
+namespace karma = boost::spirit::karma;
+namespace phx = boost::phoenix;
+
+template <typename OutputIterator>
+struct keys_and_values : karma::grammar<OutputIterator, std::vector<pair_type>()>
+{
+	keys_and_values() : keys_and_values::base_type(query)
+	{
+		query = *pair;
+		pair = karma::string << ' ' << karma::string << ' ' << karma::string << karma::eol;
+	}
+	karma::rule<OutputIterator, std::vector<pair_type>()> query;
+	karma::rule<OutputIterator, pair_as_vkv()> pair;
+};
+
+int main(int argc, char *argv[])
+{
+	typedef std::back_insert_iterator<std::string> sink_type;
+
+	std::vector<pair_type> v;
+	v.push_back(pair_type("key1", "value1"));
+	v.push_back(pair_type("key2", "value2"));
+	v.push_back(pair_type("key3", "value3"));
+
+	std::string generated;
+	sink_type sink(generated);
+	keys_and_values<sink_type> g;
+
+	karma::generate(sink, g, v);
+
+	std::cout << generated << std::endl;
+
+	return 0;
+}
