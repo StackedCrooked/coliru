792f4195c3242f31909edd1d50fde933c1d4830e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-27 15:53:16 +0000

    Update archive 81

---
+++ trunk/Archive2/81/475b192b43bfe2/main.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+class ID {
+    
+    private:
+    int id;
+    
+    public:
+    
+    ID(int id) {
+        this->id = id;
+    }
+    
+    bool equals(ID& other) {
+        return id == other.getID();
+    }
+    
+    int getID() {
+        return id;
+    };
+    
+};
+
+
+int main() {
+    
+    ID a(1), b(2), c(1);
+    
+    printf("a == b -> %s\na == c -> %s",
+        a.equals(b) ? "true" : "false",
+        a.equals(c) ? "true" : "false"
+    );
+}
