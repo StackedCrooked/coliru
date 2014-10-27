83b06cfd5d60a1d5ff6e55466479df0dd7f6f356
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-27 10:10:54 +0000

    Update archive 18

---
+++ Archive2/18/ee014aca88e037/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int a = 1;
+static int b = 2;
+ 
+void f (void) {printf("from function f()\n");}
+static void g (void) {printf("from function g()\n");}
+ 
+int main(void)
+{
+    f();
+    g();
+ 
+    return 0;
+}
+/* end of this translation unit, end of file scope */
