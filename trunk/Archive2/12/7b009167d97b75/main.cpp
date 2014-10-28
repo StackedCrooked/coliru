eb84669aec4ec3cf61fef00444de1da1d10b7719
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-28 00:29:10 +0000

    Update archive 12

---
+++ trunk/Archive2/12/7b009167d97b75/main.cpp
@@ -0,0 +1,38 @@
+namespace impl { template<int> struct hack { }; }
+#define RefpConcept impl::hack< __LINE__ > * = nullptr
+
+// ---------------------------------------------------------------------
+
+#include <iostream>
+#include <type_traits>
+
+
+template<class Pred, class T = void>
+using enable_if_t = typename std::enable_if<Pred::value, T>::type;
+
+
+inline namespace Concepts {
+  template<class T, class = enable_if_t<std::is_integral<T>>>
+  using Integral_t = T;
+
+  template<class T, class = enable_if_t<std::is_floating_point<T>>>
+  using Floating_t = T;
+
+
+  template<class T, RefpConcept>
+  void func (Integral_t<T> value) { 
+    std::cerr << "integral: " << value << std::endl;
+  }
+
+  template<class T, RefpConcept>
+  void func (Floating_t<T> value) {
+    std::cerr << "floating: " << value << std::endl;
+  }
+}
+
+int main () {
+  func (int    {1234});
+  func (char   { 'A'});
+  func (float  {1.2f});
+  func (double {3.14});
+}
