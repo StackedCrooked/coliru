5033923a832d042820ee8a973460c63b863150df
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-28 13:53:56 +0000

    Update archive ef

---
+++ trunk/Archive2/ef/be1e7201d6f808/main.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+int main()
+{
+    float f = 8.75;
+    int i = int (f);
+    
+    std::vector< double > vec_a (i);
+    std::vector< double > vec_b (int (f));
+    
+    // this is ok:
+    vec_a[0] = 1.;
+    // this is not:
+    vec_b[0] = 1.;
+    
+	return 0;
+}
