5c40ccf4e178cebfbd0bda2828c41ad4198b2833
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-28 12:01:52 +0000

    Update archive 12

---
+++ trunk/Archive2/12/8ef2d2cc5e1676/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <tuple>
+#include <vector>
+#include <set>
+
+template<typename T, typename... Ts>
+struct Index {};
+
+template<typename T, typename T1, typename... Ts>
+struct Index<T, T1, Ts...> {
+    static const size_t value = 1 + Index<T, Ts...>::value;
+};
+
+template<typename T, typename... Ts>
+struct Index<T, T, Ts...> {
+    static const size_t value = 0;
+};
+
+template<typename T>
+struct Index<T> {};
+
+int main() {
+    std::cout << Index<std::set<float>, double, int, std::vector<int>, std::set<float>, bool>::value;
+    return 0;
+}
+
