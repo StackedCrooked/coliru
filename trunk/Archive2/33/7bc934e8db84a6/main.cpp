20f7daa87c830d0002bfc8e19bf45d0947448a6c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-28 12:17:11 +0000

    Update archive 33

---
+++ trunk/Archive2/33/7bc934e8db84a6/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <iterator>
+
+int main()
+{
+    std::vector<char> mykeys { 'a', 'b', 'c' };
+    std::map<char, int> myMap;
+    
+    std::transform( std::begin(mykeys), std::end(mykeys),
+                    std::inserter(myMap, myMap.begin()),
+                    [] (char c) {return std::make_pair(c, 0);} );
+
+    for (auto p : myMap)
+        std::cout << p.first << ' ' << p.second << '\n';
+}
