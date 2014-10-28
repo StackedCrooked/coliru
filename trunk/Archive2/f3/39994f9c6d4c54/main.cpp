c6c754fdf8dc0dca8e4189eeaad3a3940e95f3c8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-28 20:13:06 +0000

    Update archive f3

---
+++ trunk/Archive2/f3/39994f9c6d4c54/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <type_traits>
+
+enum class Category
+{
+  One = 1,
+  Two = 2,
+  Three = 3
+};
+
+template <Category> struct CategoryDescriptor;
+
+#define DECLARE(name,...) \
+template <> \
+struct CategoryDescriptor<Category::name> { \
+  enum sub_type { __VA_ARGS__ }; \
+  constexpr operator Category() const { return Category::name; } \
+}; \
+constexpr CategoryDescriptor<Category::name> name{}
+
+DECLARE(One, A, B);
+DECLARE(Two, C, D);
+DECLARE(Three, A, C, E);
+
+#undef DECLARE
+
+std::ostream& operator << (std::ostream& os, Category c) {
+    return os << static_cast<std::underlying_type<Category>::type>(c);
+}
+
+template <Category C>
+using SubCategoryOf = typename CategoryDescriptor<C>::sub_type;
+
+void foo(Category) {}
+void bar(SubCategoryOf<Two>) {}
+
+int main() {
+    std::cout << "One = " << One << "\n"
+                 "One.A = " << One.A << "\n"
+                 "Two = " << Two << "\n"
+                 "Two.D = " << Two.D << "\n"
+                 "Three = " << Three << "\n"
+                 "Three.A = " << Three.A << '\n';
+    
+    // Category names convert to Category
+    foo(Three);
+    
+    // Subcategory names convert to SubCategoryOf<Category>
+    bar(Two.C);
+}
