4aad23caba04b56feca2041636096c2c038be4c8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-29 00:12:41 +0000

    Update archive 48

---
+++ Archive2/48/c900cc8eb1a2c1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::string GetSomeString()
+{
+    return "Some string";
+}
+
+void SomeFunc(std::string&& someString)
+{
+}
+
+int main()
+{
+    SomeFunc(GetSomeString());
+}
