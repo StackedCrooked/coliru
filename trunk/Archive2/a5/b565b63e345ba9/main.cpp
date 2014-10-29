1e3fd33173e5fb42b0cb17a0e8a272d5027f682a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-29 12:52:46 +0000

    Update archive a5

---
+++ trunk/Archive2/a5/b565b63e345ba9/main.cpp
@@ -0,0 +1,24 @@
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/range/algorithm.hpp>
+#include <boost/range/adaptors.hpp>
+#include <iostream>
+
+using namespace boost::adaptors;
+
+using namespace boost;
+typedef boost::adjacency_list<setS, vecS, directedS, std::string, char> MyGraph;
+typedef boost::graph_traits<MyGraph>::vertex_descriptor Vertex;
+typedef boost::graph_traits<MyGraph>::edge_descriptor Edge;
+
+int main() {
+    MyGraph g;
+
+    // setup
+    add_vertex(std::string("xxx"), g);
+    Vertex currentVertex = g.vertex_set()[0];
+    Vertex endVertex = add_vertex(std::string("yyy"), g);
+    add_edge(currentVertex, endVertex, 'i', g);
+
+    for (auto matching : boost::edges(g) | filtered([&g](auto const& e) { return g[e] == 'i'; }))
+        std::cout << matching << "\n";
+}
