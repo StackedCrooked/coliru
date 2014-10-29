81f5a58558ba04084689386812fb76d709362f4b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-29 22:29:39 +0000

    Update archive f9

---
+++ Archive2/f9/c160b1594392a5/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+int main()
+{
+    int var= 0x10;
+    printf("Valeur hexa : %X\n", var);
+    printf("Valeur decimal : %d\n", var);
+    return 0;
+}
