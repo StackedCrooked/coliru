24b841aef985951beab0aa332a05cc3232716b92
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-29 16:12:56 +0000

    Update archive a9

---
+++ Archive2/a9/a2ef618b734c68/main.cpp
@@ -0,0 +1,8 @@
+#include <boost/regex.hpp>
+#include <string>
+#include <iostream>
+int main() {
+  std::string regexError = R"~(^(\w+= \{\s*(?:[a-f0-9]{2}\s+)*\})\s*)~";
+  boost::regex e(regexError);
+  std::cout << "OK";
+}
