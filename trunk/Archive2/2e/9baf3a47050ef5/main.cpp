5bbc52cf40baf19147ea261c11baddfa3a1e2b60
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-29 14:11:46 +0000

    Update archive 2e

---
+++ trunk/Archive2/2e/9baf3a47050ef5/main.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+#include <float.h>
+
+typedef double num;
+ 
+int main(void)
+{
+    num numValue = 123456111111111LL;
+
+    int nExponent = 0;
+
+    frexp(
+        numValue,
+        & nExponent);
+
+    // num: 1 bit - sign, 11 bit - exp, 52 - mantissa
+    const int C_nExponentSizeInBits = 11;
+    const int C_nMantissaSizeInBits = 52;
+    const int C_nMaxExponent = 2 << C_nExponentSizeInBits;
+
+    int nShiftedExponent =
+        //GLimitWithAssert(
+            nExponent - C_nMantissaSizeInBits;
+            //-C_nMaxExponent,
+            //C_nMaxExponent);
+
+    num numError =
+        ldexp(
+            0.5,  // denormalized mode 1.^x == 0.5^(x-1)
+            nShiftedExponent);
+
+
+    double f = 123.45;
+    printf("Error: %.2f \n", numError);
+ 
+}
