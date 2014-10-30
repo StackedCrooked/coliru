43476a48882021126eaed43fc1892a07a0d8c467
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-30 21:54:40 +0000

    Update archive 6d

---
+++ trunk/Archive2/6d/073f38735b6b48/main.cpp
@@ -0,0 +1,15 @@
+template <class _T>
+void inc(int &x) {
+    x++;
+}
+
+template <class FUNC>
+void add(int &x, FUNC f) {
+    f(x);
+}
+
+int main() {
+    int x = 0;
+    add(x, inc);
+    return 0;
+}
