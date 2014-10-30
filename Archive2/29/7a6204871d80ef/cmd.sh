ef4e4b9e714690366150fe59240222ae84ffc0ae
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-30 20:01:53 +0000

    Update archive 29

---
+++ Archive2/29/7a6204871d80ef/cmd.sh
@@ -0,0 +1,2 @@
+#title input size / compressed size = compression ratio
+for i in {1..1000} ; do c="$(head -c${i} /usr/bin/vim | gzip | wc -c)" ; echo "$i / $c = $( echo "scale=2; $i/$c" | bc)" ; done | column -t
