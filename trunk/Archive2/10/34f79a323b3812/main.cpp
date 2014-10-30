d295f63ba802d949c921e7a6846a99ca66cac924
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-30 21:51:57 +0000

    Update archive 10

---
+++ trunk/Archive2/10/34f79a323b3812/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct A
+{
+    virtual void f(int) {}
+};
+
+struct B
+{
+    virtual void f(int*) {}
+};
+
+int main()
+{
+    B b;
+    int a = 10;
+
+    b.f(a);
+    
+    std::cout << words << std::endl;
+}
