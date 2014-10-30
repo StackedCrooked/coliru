4ab10f2df3235c6846a75cefe0b5d37ff4e6a526
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-30 18:34:29 +0000

    Update archive f5

---
+++ Archive2/f5/0cfb2806b6873e/main.cpp
@@ -0,0 +1,52 @@
+#include <utility>
+
+namespace detail {
+template <unsigned I>
+struct priority_tag : priority_tag<I-1> {};
+template <> struct priority_tag<0> {};
+
+// High priority overload.
+// If f is callable with zero args, return f()
+template <typename F>
+constexpr auto curry(F&& f, priority_tag<1>) -> decltype(std::forward<F>(f)()) {
+    return std::forward<F>(f)();
+}
+
+// Low priority overload.
+// Return a partial application
+template <typename F>
+constexpr auto curry(F&& f, priority_tag<0>) {
+    return [f=std::forward<F>(f)](auto&& t) {
+        return curry([f=f,t=std::forward<decltype(t)>(t)](auto&&...args) ->
+          decltype(f(t, std::forward<decltype(args)>(args)...)) {
+            return f(t, std::forward<decltype(args)>(args)...);
+        }, priority_tag<1>{});
+    };
+}
+} // namespace detail
+
+// Dispatch to the implementation overload set in namespace detail.
+template <typename F>
+constexpr auto curry(F&& f) {
+    return detail::curry(std::forward<F>(f), detail::priority_tag<1>{});
+}
+
+#include <iostream>
+#include <string>
+
+void foo(int) { std::cout << __PRETTY_FUNCTION__ << '\n'; }
+void foo(std::string, double) { std::cout << __PRETTY_FUNCTION__ << '\n'; }
+
+static struct {
+  template<class...Args>
+  auto operator()(Args&&...args)const
+  -> decltype(foo(std::forward<Args>(args)...))
+    { return (foo(std::forward<Args>(args)...));}
+} call_foo;
+
+int main() {
+    curry([]{ std::cout << __PRETTY_FUNCTION__ << '\n'; });
+    curry([](auto&& t){ std::cout << __PRETTY_FUNCTION__ << ": " << std::forward<decltype(t)>(t) << '\n'; })(42);
+    curry(call_foo)(42);
+    curry(call_foo)("foo")(12);
+}
