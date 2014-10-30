2e1c39ab7ec6e4bcf3501cff8e1c70993cc2be0f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-30 15:57:00 +0000

    Update archive 72

---
+++ Archive2/72/678e34756fc90a/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <utility>
+
+template<class T>struct voider{using type=void;};
+template<class T>using void_t=typename voider<T>::type;
+
+template<class Sig,class=void>
+struct is_invokable:std::false_type {};
+template<class F, class... Ts>
+struct is_invokable<
+    F(Ts...),
+    void_t<decltype(std::declval<F>()(std::declval<Ts>()...))>
+>:std::true_type {};
+
+template<class F>
+struct curry_t {
+    F f;
+	template<class... Args,class=typename std::enable_if<!is_invokable<F(Args...)>::value, int>::type>
+	auto operator()(Args&&... args)const
+    {
+		auto closure = [=, f=f](auto&&...ts)
+		-> decltype(f(args..., std::declval<decltype(ts)>()...))
+		{
+			return f(args..., std::forward<decltype(ts)>(ts)...);
+		};
+		return curry_t< decltype(closure) >{ std::move(closure) };
+	}
+	template<class...Args,class=typename std::enable_if<is_invokable<F(Args...)>::value, int>::type>
+	auto operator()(Args&&... args,...)const
+	{
+		return f(std::forward<Args>(args)...);
+	}
+};
+
+template<class F>
+curry_t<typename std::decay<F>::type> curry( F&& f ) { return {std::forward<F>(f)}; }
+
+static struct {
+    int operator()(double, int)const{return 3;}
+    char operator()(char, int)const{return 'a';}
+} foo;
+
+int main() {
+    
+	auto f = curry(foo);
+    std::cout << f(3.14)('a') << "\n";
+    auto x =  f('a',2);
+    std::cout << x << "\n";
+    std::cout << is_invokable<decltype(&foo)(double, int)>{} << "\n";
+    std::cout << is_invokable<decltype(&foo)(double)>{} << "\n";
+    std::cout << is_invokable<decltype(f)(double, int)>{} << "\n";
+    std::cout << is_invokable<decltype(f)(double)>{} << "\n";
+    std::cout << is_invokable<decltype(f(3.14))(int)>{} << "\n";
+    decltype(std::declval<decltype((foo))>()(std::declval<double>(), std::declval<int>())) y = 3;
+    (void)y;
+	// std::cout << << "\n";
+}
