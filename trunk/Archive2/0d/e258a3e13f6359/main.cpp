1eb7e20ff6ee33a6f3e3bc5d4e5417ac49d9a832
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-31 18:34:43 +0000

    Update archive 0d

---
+++ trunk/Archive2/0d/e258a3e13f6359/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <utility>
+
+template <typename T, typename F>
+class Accessor
+{
+    friend F;
+    
+    template <typename... Args>
+    Accessor(Args&&... args) : t{ std::forward<Args>(args)... } {}
+    
+    T t;
+    
+    Accessor& operator=(const T& rhs)
+    {
+        t = rhs;
+        return *this;
+    }
+
+public:
+    operator const T&() const { return t; }
+};
+
+class foo_t
+{
+public:
+    Accessor<bool, foo_t> enabled_m;
+    
+    void set_this()
+    {
+        enabled_m = true; // works fine
+    }
+};
+
+int main()
+{
+    foo_t foo;
+        
+    // foo.enabled_m = true; // doesn't work
+    
+    foo.set_this();
+    std::cout << foo.enabled_m; // works fine    
+}
