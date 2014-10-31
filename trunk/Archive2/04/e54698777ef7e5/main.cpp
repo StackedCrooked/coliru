d9749930ec5d70291a43c9bda160f83938ef5e8b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-31 20:54:14 +0000

    Update archive 04

---
+++ trunk/Archive2/04/e54698777ef7e5/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <utility>
+#include <tuple>
+
+struct value
+{
+    template <typename A1>
+    static constexpr decltype(auto) apply(A1&& a1) { return std::forward<A1>(a1); }
+    
+    static constexpr int arity = 1;
+};
+
+template <typename T1, T1 value>
+struct constant
+{    
+    static constexpr decltype(auto) apply() { return value; }
+    
+    static constexpr int arity = 0;
+};
+
+template <typename T1, typename T2>
+struct plus
+{
+    template <typename... Args>
+    static constexpr decltype(auto) apply(Args&&... args)
+    {
+        return _apply(std::make_index_sequence<T1::arity>{}, std::make_index_sequence<T2::arity>{}, std::forward<Args>(args)...);
+    }
+    
+    template <typename... Args, std::size_t... Arity1, std::size_t... Arity2>
+    static constexpr decltype(auto) _apply(std::index_sequence<Arity1...>, std::index_sequence<Arity2...>, Args&&... args)
+    {
+        auto t = std::forward_as_tuple(std::forward<Args>(args)...);
+        return T1::apply(static_cast<typename std::tuple_element<Arity1, decltype(t)>::type>(std::get<Arity1>(t))...)
+             + T2::apply(static_cast<typename std::tuple_element<T1::arity + Arity2, decltype(t)>::type>(std::get<T1::arity + Arity2>(t))...);
+    }
+    
+    static constexpr int arity = T1::arity + T2::arity;
+};
+
+template <typename T1, typename T2>
+struct multiply
+{
+    template <typename... Args>
+    static constexpr decltype(auto) apply(Args&&... args)
+    {
+        return _apply(std::make_index_sequence<T1::arity>{}, std::make_index_sequence<T2::arity>{}, std::forward<Args>(args)...);
+    }
+    
+    template <typename... Args, std::size_t... Arity1, std::size_t... Arity2>
+    static constexpr decltype(auto) _apply(std::index_sequence<Arity1...>, std::index_sequence<Arity2...>, Args&&... args)
+    {
+        auto t = std::forward_as_tuple(std::forward<Args>(args)...);
+        return T1::apply(static_cast<typename std::tuple_element<Arity1, decltype(t)>::type>(std::get<Arity1>(t))...)
+             * T2::apply(static_cast<typename std::tuple_element<T1::arity + Arity2, decltype(t)>::type>(std::get<T1::arity + Arity2>(t))...);
+    }
+    
+    static constexpr int arity = T1::arity + T2::arity;
+};
+
+template <typename EXPR>
+struct wrap
+{
+    template <typename... Args>
+    static constexpr decltype(auto) func(Args&&... args)
+    {
+        return EXPR::apply(std::forward<Args>(args)...);
+    }
+};
+
+int main()
+{
+    // (1 + 2) + 3 = 6
+    std::cout << wrap<plus<plus<value, value>, value>>::func(1, 2, 3) << std::endl;
+    
+    // (a + 5) + (2 * 6) = 9 + 12 = 21
+    int a = 4;
+    std::cout << wrap<plus<plus<value, value>, multiply<value, constant<int, 6>>>>::func(a, 5, 2) << std::endl;
+        
+    // (((1 * 2) * 3) * 4) = 24
+    std::cout << wrap<multiply<multiply<multiply<value, value>, value>, value>>::func(1, 2, 3, 4) << std::endl;
+}
