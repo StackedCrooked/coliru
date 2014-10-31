2d602b2e5f189f465e5a7df992ea9cd5a80a9a2c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-31 18:38:07 +0000

    Update archive a0

---
+++ trunk/Archive2/a0/b901f2af2f348a/main.cpp
@@ -0,0 +1,25 @@
+#include <memory>
+#include <new>
+
+template <typename T>
+inline void destruct(T& t) { t.~T(); }
+
+template <typename T, typename...Args>
+inline void construct(T& t, Args&&...args) {
+  ::new((void*)std::addressof(t)) T(std::forward<Args>(args)...);
+}
+
+template <typename T, typename...Args>
+inline void default_construct(T& t) {
+  ::new((void*)std::addressof(t)) T;
+}
+
+bool foo() {
+  union bar { int i; bool b; };
+  union baz { char c; bar b; };
+  auto b = baz{'x'};
+  destruct(b.c);
+  default_construct(b.b);
+  construct(b.b.b, true);
+  return b.b.b;
+}
