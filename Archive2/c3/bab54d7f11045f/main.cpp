0c57ed97f444ac1d17baf6619e88086036552c32
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-31 11:31:44 +0000

    Update archive c3

---
+++ Archive2/c3/bab54d7f11045f/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main(void)
+{
+    FILE* fp = fopen("test.txt", "r");
+    if(!fp) {
+        perror("File opening failed");
+        return EXIT_FAILURE;
+    }
+ 
+    int c; // note: int, not char, required to handle EOF
+    while ((c = fgetc(fp)) != EOF) { // typical file reading loop
+       putchar(c);
+    }
+ 
+    if (ferror(fp))
+        puts("I/O error when reading");
+    else if (feof(fp))
+        puts("End of file reached successfully");
+}
