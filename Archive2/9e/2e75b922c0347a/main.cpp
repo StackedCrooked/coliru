cdb853eaf7c366e1d362188fd3c503a6756982da
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-31 16:35:47 +0000

    Update archive 9e

---
+++ Archive2/9e/2e75b922c0347a/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+namespace N {
+    class Foo;
+}
+
+void bar(N::Foo&);
+
+namespace N
+{
+    class Foo
+    {
+        friend void ::bar( Foo& f );
+        void print(){ std::cout << "..." << std::endl; }    // private by default
+    };  
+}
+
+void bar( N::Foo& f )
+{
+    f.print();
+}
+
+int main()
+{
+}
