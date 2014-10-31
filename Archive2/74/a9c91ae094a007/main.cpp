fc9f17b5bf875e652af02d677c692a7e5633a8b9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-10-31 00:39:51 +0000

    Update archive 74

---
+++ Archive2/74/a9c91ae094a007/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+   int list1[9] {5, 10, 15, 20, 25, 30, 35, 40, 45};
+
+   int i = 0;
+
+   list1 [i];
+
+   for (i; i < 9; i++) {
+        printf("%d \n", list1[i]);
+   }
+}
