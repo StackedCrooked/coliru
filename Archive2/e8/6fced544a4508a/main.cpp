d4ff253a7fe500f7252fbbaccd96c9d32e4ee064
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-01 13:03:23 +0000

    Update archive e8

---
+++ Archive2/e8/6fced544a4508a/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <array>
+#include <vector>
+using namespace std;
+int main()
+{
+    std::vector<int> value_initialized ;
+    std::cout << "value_initialized: " ;
+    for( int i : value_initialized ) std::cout << i << ' ' ; 
+    std::cout << '\n' ;
+
+    std::vector<int> default_initialized ;
+  
+    
+    double* x1 = new double;
+    double* x2 = new double();
+    double* x3 = new double{};
+    double* x4 = new double[5];
+    cout<<endl<<*x1<<endl<<*x2<<*x3<<*x4;
+     
+
+}
