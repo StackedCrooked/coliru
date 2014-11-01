2e72244ee14a633674dfa0984bdd27ba7ed5d110
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-01 07:05:44 +0000

    Update archive 45

---
+++ Archive2/45/c2744b7b8131fa/cmd.sh
@@ -0,0 +1,1 @@
+clang++ -std=c++1z -O3 -Wall -Wextra -pedantic -fsanitize=undefined,address,integer,leak -pthread main.cpp && ./a.out
---
+++ Archive2/45/c2744b7b8131fa/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    int* arr = new int;
+    delete arr;
+    delete arr;
+}
