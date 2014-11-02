963524d35ebbd7362b73c59db49a1ad137d9dc5f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-02 06:47:10 +0000

    Update archive e5

---
+++ trunk/Archive2/e5/f4783f874034b2/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template<int channel_count>
+struct Channels {
+    static constexpr int channels = channel_count;
+};
+
+template< typename T >
+class Cable
+{
+	public:
+	const int channels = T::channels;
+	vector<float> audio[T::channels];
+	
+	Cable(){
+		for(int i = 0; i < T::channels; i++){
+			for(int s = 0; s < 5; s++){
+				audio[i].push_back(i);
+			}
+		}
+	}
+	
+};
+
+
+int main() {
+	
+	Cable<Channels<3>> c;
+	
+	cout << "num: " << c.channels << endl;
+	for(int i = 0; i < c.channels; i++){
+		cout << i << ": ";
+		for(std::size_t s = 0; s < c.audio[i].size(); s++){
+			cout << " " << c.audio[i][s];
+		}
+		cout << endl;
+	}
+}
+
