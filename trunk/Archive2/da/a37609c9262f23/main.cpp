2d1dc9146b8799b59719441f98763d501c7bb944
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-03 04:32:23 +0000

    Update archive da

---
+++ trunk/Archive2/da/a37609c9262f23/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <regex>
+
+int main()
+{
+    std::string input{ "Mon Nov 25 20:54:36 2013" };
+    
+    std::regex  r{R"(^(\w{3}) (\w{3}) (\d{2}) (\d{2}):(\d{2}):(\d{2}) (\d{4})$)"};
+    std::smatch match;
+
+    if (std::regex_match(input,match,r)) {
+        std::cout << "Found" << "\n";
+        for(auto const& m : match) {
+            std::cout << m << '\n';
+        }
+    } else {
+        std::cout << "Did Not Found" << "\n";
+    }
+}
