6b0f9b477499be9313718b190671fcd5499a4920
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-03 08:51:19 +0000

    Update archive 4c

---
+++ Archive2/4c/627ff3d74ff174/main.cpp
@@ -0,0 +1,31 @@
+struct foo_t {};
+
+struct base
+{
+  void fun(unsigned) {}
+};
+
+#include <utility> // std::forward
+struct decorator
+{
+  base b;
+
+  template <typename... Args>
+  auto
+  fun(Args&&... args)
+  {
+    return b.fun(std::forward<Args>(args)...);
+  }
+};
+
+struct decorated : decorator
+{
+  using decorator::fun;
+  void fun(const foo_t&) {}
+};
+
+int main()
+{
+  decorated d;
+  d.fun(foo_t{});
+}
