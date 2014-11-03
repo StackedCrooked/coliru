169d4639eef1940999745cdb2b22ea011b4040dd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-03 14:29:04 +0000

    Update archive 8a

---
+++ trunk/Archive2/8a/4500733b04dbd4/main.cpp
@@ -0,0 +1,16 @@
+void transpose(double (&arr)[3][3] )
+{
+    for (int counti = 0; counti < 3; counti++) {
+        for (int countj = 0; countj < 3; countj++) 
+        {
+            double i_swap = arr[0][0];
+        }
+    }
+}   
+
+int main()
+{
+    double myarray[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    transpose(myarray);
+    return 0;
+}
