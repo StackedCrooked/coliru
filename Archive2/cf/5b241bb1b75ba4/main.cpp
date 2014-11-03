44e41e7a5d9baa850192e27cc6ce1e9c865fc3c6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-03 17:58:51 +0000

    Update archive cf

---
+++ Archive2/cf/5b241bb1b75ba4/main.cpp
@@ -0,0 +1,82 @@
+char convertalphas(char s) {
+    switch (s){
+    case 'A':
+        return '0';
+        break;
+    case 'B':
+        return '1';
+        break;
+    case 'C':
+        return '2';
+        break;
+    case 'D':
+        return '3';
+        break;
+    case 'E':
+        return '4';
+        break;
+    case 'F':
+        return '5';
+        break;
+    case 'G':
+        return '6';
+        break;
+    case 'H':
+        return '7';
+        break;
+    case 'I':
+        return '8';
+        break;
+    case 'J':
+        return '9';
+        break;
+    case 'K':
+        return '10';
+        break;
+    case 'L':
+        return '11';
+        break;
+    case 'M':
+        return '12';
+        break;
+    case 'N':
+        return '13';
+        break;
+    case 'O':
+        return '14';
+        break;
+    case 'P':
+        return '15';
+        break;
+    case 'Q':
+        return '16';
+        break;
+    case 'R':
+        return '17';
+        break;
+    case 'S':
+        return '18';
+        break;
+    case 'T':
+        return '19';
+        break;
+    case 'U':
+        return '20';
+        break;
+    case 'V':
+        return '21';
+        break;
+    case 'W':
+        return '22';
+        break;
+    case 'X':
+        return '23';
+        break;
+    case 'Y':
+        return '24';
+        break;
+    case 'Z':
+        return '25';
+        break;
+    }
+}
