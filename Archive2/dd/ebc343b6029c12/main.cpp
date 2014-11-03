905e2af7e3ac0b24e06aec14ed6afee845fea5bf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-03 06:53:38 +0000

    Update archive dd

---
+++ Archive2/dd/ebc343b6029c12/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+struct A
+{
+    A(){}
+    A(const A&){ std::cout << "A(const A&)" << std::endl; }
+    A(const A&&){ std::cout << "A(const A&&)" << std::endl; }
+};
+
+void foo(A&)
+{
+    std::cout << "A&" << std::endl;
+}
+
+A bar()
+{
+    return A();
+}
+
+int main()
+{ 
+    A t = A();
+    foo(bar());
+    foo(A());
+    foo(t);
+}
+
