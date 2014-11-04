f0cae093efb93e10bd43ec24e8c2e6c567c17cd8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-04 16:15:52 +0000

    Update archive 53

---
+++ Archive2/53/0482b8e1155eb9/main.cpp
@@ -0,0 +1,47 @@
+#include <type_traits>
+
+struct CByteArray {};
+struct HLVariant {
+    HLVariant() {}
+    HLVariant(const HLVariant&) {}
+    HLVariant(const CByteArray&) {}
+
+    };
+
+template <typename> struct Serializer;
+
+template <>
+struct Serializer<HLVariant>
+{
+    static CByteArray serialize(const HLVariant& /* value */)
+    {
+        return CByteArray();
+    }
+};
+
+template <typename T>
+struct Serializer
+{
+    template <typename U = T>
+    static typename std::enable_if<std::is_pod<U>::value, CByteArray>::type
+    serialize(const U& /* value*/)
+    {
+        static_assert(std::is_pod<U>::value, "Not a POD type");
+        return CByteArray();
+    }
+
+    template <typename U = T>
+    static typename std::enable_if<!std::is_pod<U>::value, CByteArray>::type
+    serialize(const U& value)
+    {
+        return Serializer<HLVariant>::serialize(HLVariant(value));
+    }
+};
+
+int main()
+{
+    int i = 0;
+    Serializer<int>::serialize(i);
+    Serializer<CByteArray>::serialize(CByteArray());
+    Serializer<HLVariant>::serialize(HLVariant());
+}
