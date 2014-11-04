538686554b30f25e2bf2480d496fb5d2a1412758
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-04 18:57:29 +0000

    Update archive ab

---
+++ Archive2/ab/a758d0c608edfb/main.cpp
@@ -0,0 +1,33 @@
+#include <initializer_list>
+#include <utility>
+
+template <typename T, std::size_t N>
+struct array {
+    T elems[N];
+    
+    constexpr const T& operator[] (std::size_t i) const { return elems[i]; }
+    constexpr T& operator[] (std::size_t i) { return elems[i]; }
+};
+
+template <typename T, std::size_t...Is>
+constexpr T double_sum_of(std::index_sequence<Is...>) {
+    array<T, sizeof...(Is)> foo = {{ Is... }};
+    for (auto i = sizeof...(Is); i-- > 0;) {
+        foo[i] *= 2;
+    }
+    T sum{};
+    for (auto i = sizeof...(Is); i-- > 0;) {
+        sum += foo[i];
+    }
+    return sum;
+}
+
+template <typename T, std::size_t N>
+constexpr T double_sum_of() {
+    return double_sum_of<T>(std::make_index_sequence<N>{});
+}
+
+int main() {
+    constexpr int i = double_sum_of<int, 10>();
+    return i;
+}
