2fabeb03315e952cbb81a238aa3ca1d360ce99ce
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-04 22:28:02 +0000

    Update archive c4

---
+++ trunk/Archive2/c4/3a0c3af288db94/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    std::cout << (long long)234343.0     << '\n'
+              << (long long)3.41e10      << '\n'
+              << (long long)19.999999999 << '\n'
+              << (long long)-10.1        << '\n'
+              << (long long)-0.0000001   << '\n';
+}
