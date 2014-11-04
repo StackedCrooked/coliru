c688e0f38a74122fe4a017b510523d265172a7c2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-04 09:06:42 +0000

    Update archive dd

---
+++ trunk/Archive2/dd/a05d6493116ef8/main.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+std::string nameFromPathname(std::string const& pathname, char separator = '/')
+{
+    std::string::const_reverse_iterator lastSep = std::find(
+                pathname.rbegin(), pathname.rend(), separator);
+    return std::string(lastSep.base(), pathname.end());
+}
+
+void test(std::string const& s)
+{
+    std::cout << s << ": " << nameFromPathname(s) << std::endl;
+}
+
+int main()
+{
+    test("file.txt");
+    test("/file.txt");
+    test("./file.txt");
+    test("/home/doug/file.txt");
+    return 0;
+}
