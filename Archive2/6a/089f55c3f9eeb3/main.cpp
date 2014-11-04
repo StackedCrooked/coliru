8353e57ead478bc04a5efa19924be35075ba2440
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-04 16:19:25 +0000

    Update archive 6a

---
+++ Archive2/6a/089f55c3f9eeb3/main.cpp
@@ -0,0 +1,37 @@
+#include <type_traits>
+
+struct CByteArray {};
+struct HLVariant {
+    HLVariant() {}
+    HLVariant(const HLVariant&) {}
+    HLVariant(const CByteArray&) {}
+
+    };
+
+inline CByteArray serialize(const HLVariant& /* value */)
+{
+    return CByteArray();
+}
+
+template <typename T>
+inline typename std::enable_if<std::is_pod<T>::value, CByteArray>::type
+serialize(const T& /* value*/)
+{
+    static_assert(std::is_pod<T>::value, "Not a POD type");
+    return CByteArray();
+}
+
+template <typename T>
+inline typename std::enable_if<!std::is_pod<T>::value, CByteArray>::type
+serialize(const T& value)
+{
+    return serialize(HLVariant(value));
+}
+
+int main()
+{
+    int i = 0;
+    serialize(i);
+    serialize(CByteArray());
+    serialize(HLVariant());
+}
