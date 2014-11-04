9c5f89326d42587c2d938eadc08c8d870dfeaf9f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-04 18:16:40 +0000

    Update archive 07

---
+++ trunk/Archive2/07/b6f43e4e7af6cc/main.cpp
@@ -0,0 +1,66 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <functional>
+
+template<typename T1>
+class RREvent1Arg
+{
+public:
+    typedef std::function<void (T1)> Func;
+
+public:
+    void Call( T1 arg )
+    {
+        for( auto& pair : m_handlers )
+        {
+            pair.second( arg );
+        }
+    }
+
+    void operator ()(T1 arg)
+    {
+        Call( arg );
+    }
+
+    template<typename Callable>
+    RREvent1Arg& operator += ( Callable f )
+    {
+        m_handlers.emplace(std::string(typeid(Callable).name()) + std::to_string(reinterpret_cast<uintptr_t>(&f)), f);
+        std::cout << "operator+=(" << typeid(Callable).name() << reinterpret_cast<uintptr_t>(&f) << ")\n";
+        return *this;
+    }
+
+    template<typename Callable>
+    RREvent1Arg& operator -= ( Callable f )
+    {
+        m_handlers.erase(typeid(Callable).name());
+        
+        return *this;
+    }
+
+private:
+    std::unordered_map<std::string,Func> m_handlers;
+};
+
+void f(int i) {}
+
+int main() {
+        
+    int stackValue = 200;
+    RREvent1Arg<int> e;
+    e += f;
+    e += [](int i) {};
+    
+	auto lambda = [&stackValue](int i) { printf( "%d\n", i + stackValue ); };
+    e += [=](int i) { printf( "%d\n", i + stackValue ); };
+    e += [&](int i) { printf( "%d\n", i + stackValue ); stackValue += 100; };
+    e += lambda;
+    e += [stackValue](int i) { printf( "%d\n", i + stackValue ); };
+    e(100);
+	std::cout << "----------------\n";
+	e -= lambda;
+	e(100);
+	
+}
