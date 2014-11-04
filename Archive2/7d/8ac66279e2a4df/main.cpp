9abb0b9bc1f69303888168fbb6277fb0359bf4bb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-04 00:43:24 +0000

    Update archive 7d

---
+++ Archive2/7d/8ac66279e2a4df/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+
+void cm(const char (&h)[8]) 
+    {std::cout << " const (&)[N] " << '\n';}
+void cm(const char * h)
+    {std::cout << " const char * " << '\n';}
+
+int main()
+{
+    const char ccar[] = "errqweq";
+    cm(ccar);
+}
