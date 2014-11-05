5b861c380744025a1f311ee9836cfd0219bb6990
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-05 11:43:09 +0000

    Update archive 2b

---
+++ Archive2/2b/298e7692a5d9ae/main.cpp
@@ -0,0 +1,15 @@
+struct Foo {};
+
+template<typename ForwardIterator>
+struct Foo*
+Bar(ForwardIterator first, ForwardIterator last)
+{ 
+    (void)first;
+    (void)last;
+    Foo *foo(nullptr);
+    return foo;
+}
+
+int main() {
+
+}
