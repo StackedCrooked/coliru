129e10b9ffabbc21288fc077a1b96e77b406fae6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-05 22:34:16 +0000

    Update archive bd

---
+++ trunk/Archive2/bd/53557cec96a16c/main.cpp
@@ -0,0 +1,21 @@
+#include <array>
+
+template <typename T, std::size_t...Extents>
+struct build_array_type {
+    using type = T;
+};
+template <typename T, std::size_t First, std::size_t...Rest>
+struct build_array_type<T, First, Rest...> {
+    using type = std::array<typename build_array_type<T, Rest...>::type, First>;
+};
+template <typename T, std::size_t...Extents>
+using multiarray = typename build_array_type<T, Extents...>::type;
+
+auto foo = multiarray<int, 2, 3, 4>{{
+    {{ {{  0,  1,  2,  3 }},
+       {{  4,  5,  6,  7 }},
+       {{  8,  9, 10, 11 }} }},
+    {{ {{ 12, 13, 14, 15 }},
+       {{ 16, 17, 18, 19 }},
+       {{ 20, 21, 22, 23 }} }}
+}};
