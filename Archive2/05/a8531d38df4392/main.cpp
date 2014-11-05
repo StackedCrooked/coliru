295776af3344cd5f474509e747f0e0208e61165d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-05 13:28:12 +0000

    Update archive 05

---
+++ Archive2/05/a8531d38df4392/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <sstream>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+int main()
+{
+    char s[] = "C:\\\0D:\\\0E:\\\0\\0";
+    char* d = s;
+    while (*d)
+    {
+        std::cout << *d;
+        d++;
+    }
+   
+    std::istringstream ss( std::string(s, s + sizeof(s)) );
+    
+    std::cout << std::vector<std::string>( std::istream_iterator<std::string>(ss), std::istream_iterator<std::string>() );
+}
