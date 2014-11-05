77eb6a4e87e60bfbe264bd9cfe1840abf25a51ad
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-05 07:27:56 +0000

    Update archive 8a

---
+++ trunk/Archive2/8a/4452ccd22940b4/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+struct A
+{
+    A(){ std::cout << "A()" << std::endl; };
+    A(A&){ std::cout << "A(A&)" << std::endl; };
+    A(A&&){ std::cout << "A(A&&)" << std::endl; };
+};
+
+A foo(){
+    return A();
+}
+
+A t(A()){ return A(); };
+A d(foo());
+
+A (*f)() = foo;
+
+int main()
+{ 
+    t(f);
+}
+
