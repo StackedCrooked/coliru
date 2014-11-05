61d5b564b2c5e4a2e3b0f1ff3c7bd592eb65a2fe
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-05 07:04:08 +0000

    Update archive 71

---
+++ Archive2/71/109c19fd635816/main.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <type_traits>
+#include <iostream>
+
+struct Bar 
+{
+    Bar() = default;
+    Bar(int) {}
+};
+
+struct Foo
+{
+    Foo() = default;
+    template<typename T, typename U>
+    Foo(T const&, U const&) 
+    {
+        static_assert(std::is_same<T, Bar>::value &&
+                      std::is_same<U, Bar>::value, "blah");
+    }
+};
+
+void g(Foo const &) {}
+void g(std::vector<Bar> const &) {}
+
+int main()
+{
+    Bar a, b, c;
+
+//    Foo foo1{1,2};  // fails static assertion
+
+    g({1,2}); // ok
+    g({a,b,c}); // ok
+}
