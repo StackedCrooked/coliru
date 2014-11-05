290ef425bd0c460c3e204d991f556898cddb7d06
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-05 05:18:47 +0000

    Update archive 0c

---
+++ Archive2/0c/836b55430a680b/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+struct eat_char
+{
+    eat_char( char c ) : ch(c) {}
+    const char ch ;
+};
+
+std::istream& operator>> ( std::istream& stm, eat_char ec )
+{
+    char c ;
+
+    if( stm >> c )
+    {
+        if( c == ec.ch ) return stm ;
+
+        stm.putback(c) ;
+        stm.clear(std::ios::failbit) ;
+    }
+
+    return stm ;
+}
+
+int main()
+{
+    int a, b, c ;
+    while( std::cin >> a >> '/' >> b >> ':' >> c )
+        std::cout << a << ' ' << b << ' ' << c << '\n' ;
+}
