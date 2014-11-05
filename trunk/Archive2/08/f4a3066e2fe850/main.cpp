dccd82e233163d2517237821b75580cca0b3104f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-05 11:02:50 +0000

    Update archive 08

---
+++ trunk/Archive2/08/f4a3066e2fe850/main.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <string>
+
+template <typename T> struct CSizex {
+};
+
+template <typename T> struct CPointx {
+};
+
+struct Logger
+{
+    template <typename T> Logger& operator<<(const CSizex<T>& size) { return *this; }
+    template <typename T> Logger& operator<<(const CPointx<T>& point) { return *this; }
+    template <typename T> Logger& operator<<(T value)
+    {
+        static_assert(std::is_arithmetic<T>::value || std::is_integral<T>::value || std::is_enum<T>::value, "This method is only meant for arithmetic types");
+        return *this;
+    }
+
+    template <typename T>
+    Logger& operator<<(const T* p)
+    {
+        //*this << HEX << (uint64_t)p;
+        return *this;
+    }
+};
+
+int main()
+{
+    Logger() << CSizex<int>();
+}
