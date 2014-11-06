b9b75280242ea15530807bd1bb35e3cfd75d2552
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-06 16:07:28 +0000

    Update archive bf

---
+++ trunk/Archive2/bf/aa6349486d610a/main.cpp
@@ -0,0 +1,40 @@
+#include <tuple>
+#include <utility>
+#include <type_traits>
+#include <iostream>
+
+struct HasAs
+{
+    int i;
+    HasAs(int i) : i(i) {}
+    template <typename T> T as() { return i; }
+};
+
+struct JVal
+{
+    HasAs operator[](int i) const { std::cout << "at(" << i << ")" << std::endl; return {i}; }
+};
+
+// START
+template <typename... Ts, std::size_t... Is> 
+std::tuple<Ts...> as_tuple(const JVal& mV, std::index_sequence<Is...>) 
+{ 
+    return std::tuple<Ts...>{ mV[Is].as<Ts>()... };
+}
+
+template<typename... Ts> 
+std::tuple<Ts...> as_tuple(const JVal& mV)
+{
+    return as_tuple<Ts...>(mV, std::index_sequence_for<Ts...>{});
+}
+// END
+
+int main()
+{
+    JVal mV;
+    auto t = as_tuple<int, float, double>(mV);
+    
+    std::cout << std::get<0>(t) << std::endl;
+    std::cout << std::get<1>(t) << std::endl;
+    std::cout << std::get<2>(t) << std::endl;
+}
