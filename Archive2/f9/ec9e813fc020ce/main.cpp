8ce7b34dd610fe9a8d22c6a44063a229e244eed9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-06 09:16:20 +0000

    Update archive f9

---
+++ Archive2/f9/ec9e813fc020ce/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+double specialRound(double x, int precision) {
+}
+
+int main() {
+    g();
+    std::cout << "here" << std::endl;
+    f();
+}
