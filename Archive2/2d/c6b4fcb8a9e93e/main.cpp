e980678f98e9990a923071c2b110f80c2081bc2d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-07 18:59:46 +0000

    Update archive 2d

---
+++ Archive2/2d/c6b4fcb8a9e93e/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <type_traits>
+
+namespace non_std {
+template <typename T>
+inline constexpr typename std::remove_reference<T>::type &make_ref(T &&r) noexcept {
+    using U = typename std::remove_reference<T>::type;
+    return static_cast<U &>(r);
+}
+}
+
+using namespace std;
+using namespace non_std;
+
+void divs(int &a, int &b) {
+    int t = a;
+    a /= b;
+    b /= t;
+}
+
+int main() {
+    int i_care_for_this_one = 4;
+    divs(i_care_for_this_one, make_ref(2));
+    cout << i_care_for_this_one << endl;
+}
