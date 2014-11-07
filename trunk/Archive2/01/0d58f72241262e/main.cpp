0b856e2dd710fc88adde8d66eaec9e121654836e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-07 08:40:48 +0000

    Update archive 01

---
+++ trunk/Archive2/01/0d58f72241262e/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>           
+#include <vector>
+#include <thread>
+#include <new>
+#include <algorithm>
+
+using namespace std;
+
+class A {
+ public:
+ A() {
+     cout << "CTOR!\n";
+ }
+ ~A() {
+    cout << "~DTOR!\n";   
+ }
+    
+};
+
+int main ()
+{
+    shared_ptr<A> ad;
+//    ad.reset(new A);
+    ad = make_shared<A>(new A);
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
