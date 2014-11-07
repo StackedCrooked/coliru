ec0849e52fc02d2554457f4cd21f5068a2fe96f9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-07 00:34:24 +0000

    Update archive e5

---
+++ trunk/Archive2/e5/2cdd0efa2a45f9/main.cpp
@@ -0,0 +1,40 @@
+#include <cctype>
+#include <iterator>
+
+template <typename It, typename F>
+    void tokenize(It& first, It const last, F const& f)
+{
+    while (first != last)
+    {
+        auto s   = first;
+        auto len = 0ull;
+        while (first!=last && !std::isspace(static_cast<unsigned char>(*first))) ++len, ++first;
+        f(s,first,len);
+        while (first!=last && std::isspace(static_cast<unsigned char>(*first))) ++first;;
+    }
+}
+
+template <typename Range, typename F>
+    void tokenize(Range const& range, F const& f)
+{
+    using std::begin;
+    using std::end;
+    auto b(begin(range));
+    tokenize(b, end(range), f);
+}
+
+#include <string>
+#include <iostream>
+    
+int main()
+{
+    tokenize("hortus racecar musicus religiosus", [&](auto start, auto end, size_t len) {
+        if (len>=6 && len<=12) 
+        {
+            std::string s(start,end);
+            end = start + len/2;
+            if (s.end() == std::mismatch(s.begin(), s.begin(), s.rbegin()).first)
+                std::cout << len << ": " << s << "\n";
+        }
+    });
+}
