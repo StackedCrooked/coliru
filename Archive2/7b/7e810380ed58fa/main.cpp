2ef810ea8acbfa4615084ecee24f69bdf82352ab
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-07 18:01:15 +0000

    Update archive 7b

---
+++ Archive2/7b/7e810380ed58fa/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+struct Foo { Foo(int x, int y) { std::cout << "Foo(int = " << x << ", int = " << y << ")" << std::endl; } };
+
+void bar(int x, int y) { std::cout << "bar(int = " << x << ", int = " << y << ")" << std::endl; }
+
+int main()
+{
+   Foo({}, {});   // Foo(int = 0, int = 0)
+   Foo({1}, {2}); // Foo(int = 1, int = 2)
+   Foo({1, 2});   // Foo(int = 1, int = 2)
+
+   bar({}, {});   // bar(int = 0, int = 0)
+   bar({1}, {2}); // bar(int = 1, int = 2)
+   bar({1, 2});   // error: no matching function for call to 'bar'  <<< Why? <<<
+}
