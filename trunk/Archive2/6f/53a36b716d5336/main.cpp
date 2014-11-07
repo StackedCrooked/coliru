b966cf49972793adc4b5754b5a452787ead44df1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-07 23:18:08 +0000

    Update archive 6f

---
+++ trunk/Archive2/6f/53a36b716d5336/main.cpp
@@ -0,0 +1,39 @@
+#include <random>
+#include <iostream>
+#include <memory>
+#include <functional>
+ 
+template <typename T>
+struct blah {
+    
+    void Register( std::function<bool(const T&)> consumer )
+    {
+        std::cout << " std::function\n" ;
+    }
+    
+    
+    template <typename FunctionObject>
+        inline void Register(const FunctionObject& consumer)
+        {
+        std::cout << "FunctionObject\n" ;
+    
+    }
+};
+ 
+int main()
+{
+    using namespace std::placeholders;  // for _1, _2, _3...
+ 
+    blah<int> b1 ;
+ 
+    b1.Register( [] ( const int &x ) -> bool { return x < 10 ; } ) ;
+    
+    auto f = [] ( const int &x ) -> bool { return x < 10 ; } ;
+    
+    b1.Register( f ) ;
+    
+    std::function<int(int)> f2 = [] (int x ) -> int { return x + 10 ;} ;
+    
+    std::bind(f2, std::placeholders::_1); // a copy of e is stored in rnd
+    
+    }
