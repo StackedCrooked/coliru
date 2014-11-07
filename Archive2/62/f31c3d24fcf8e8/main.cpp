b3228259f4cbaec73ca9c2cdc8f7791320030984
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-07 06:36:10 +0000

    Update archive 62

---
+++ Archive2/62/f31c3d24fcf8e8/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <type_traits>
+
+template<class T> struct Specialized : std::false_type {};
+template<> struct Specialized<int> : std::true_type {};
+
+template<class T> void f(T& a) noexcept(Specialized<T>::value);
+
+template<class T, std::size_t N>
+void f(T (&a)[N]) noexcept(noexcept(f(*a)));
+
+int main() {
+    int x;
+    int y[1];
+    int z[1][1];
+    
+    std::cout << noexcept(f(x)) << "\n";
+    std::cout << noexcept(f(y)) << "\n";
+    std::cout << noexcept(f(z)) << "\n";
+}
