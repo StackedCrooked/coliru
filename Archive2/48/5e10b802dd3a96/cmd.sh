f25ce602c22e8cf3e0071ae70ed6091a64910bf2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-07 06:52:23 +0000

    Update archive 48

---
+++ Archive2/48/5e10b802dd3a96/cmd.sh
@@ -0,0 +1,3 @@
+clang++ -stdlib=libstdc++ -std=c++11 -O2 -Wall -pedantic -pthread main.cpp && ./a.out
+clang++ -stdlib=libc++ -std=c++11 -O2 -Wall -pedantic -pthread main.cpp && ./a.out
+g++ -std=c++11 -O2 -Wall -pedantic -pthread main.cpp && ./a.out
---
+++ Archive2/48/5e10b802dd3a96/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+template<class T> struct Specialized      : std::false_type {};
+template<>        struct Specialized<int> : std::true_type  {};
+
+template<class T>                void f(T& a) noexcept(Specialized<T>::value);
+template<class T, std::size_t N> void f(T (&a)[N]) noexcept(noexcept(f(*a)));
+
+int main(int argc, char** argv) {
+    int x, y[1], z[1][1];
+
+    std::cout << noexcept(f(x)) << " "
+              << noexcept(f(y)) << " "
+              << noexcept(f(z)) << "\n";
+}
