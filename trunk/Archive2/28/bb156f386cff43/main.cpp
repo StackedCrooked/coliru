6cffb3b0a2d9d695c9ede40e480252804f6135e8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-07 10:34:47 +0000

    Update archive 28

---
+++ trunk/Archive2/28/bb156f386cff43/main.cpp
@@ -0,0 +1,29 @@
+template <typename T>
+struct funky_ptr {
+    funky_ptr() : p() {}
+    funky_ptr(T* t) : p(t) {}
+    ~funky_ptr() { delete p; } // no need to check for null
+
+    T*& ref() { return p; }
+
+  private:
+    T* p;
+};
+
+#include <iostream>
+
+struct X { 
+    X()                    { std::cout << __PRETTY_FUNCTION__ << "\n"; } 
+    ~X()                   { std::cout << __PRETTY_FUNCTION__ << "\n"; } 
+    X(X const&)            { std::cout << __PRETTY_FUNCTION__ << "\n"; } 
+    X(X&&)                 { std::cout << __PRETTY_FUNCTION__ << "\n"; } 
+    X& operator=(X const&) { std::cout << __PRETTY_FUNCTION__ << "\n"; } 
+    X& operator=(X&&)      { std::cout << __PRETTY_FUNCTION__ << "\n"; } 
+};
+
+int main()
+{
+    auto ptr = funky_ptr<X>(new X);
+
+    auto b = ptr;
+}
