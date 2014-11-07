5bdf8ef2528d3f43029597efa629a7b7b9dda40a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-07 15:17:39 +0000

    Update archive 40

---
+++ trunk/Archive2/40/eb8aee50d88812/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <list>
+
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
+
+
+int main()
+{
+    typedef boost::geometry::model::d2::point_xy<double> point_type;
+    typedef boost::geometry::model::polygon<point_type> polygon_type;
+
+    polygon_type poly;
+    boost::geometry::read_wkt(
+        "POLYGON((2 1.3,2.4 1.7,2.8 1.8,3.4 1.2,3.7 1.6,3.4 2,4.1 3,5.3 2.6,5.4 1.2,4.9 0.8,2.9 0.7,2 1.3)"
+            "(4.0 2.0, 4.2 1.4, 4.8 1.9, 4.4 2.2, 4.0 2.0))", poly);
+
+    point_type p(4, 1);
+
+    std::cout << "within: " << (boost::geometry::within(p, poly) ? "yes" : "no") << std::endl;
+
+    return 0;
+}
