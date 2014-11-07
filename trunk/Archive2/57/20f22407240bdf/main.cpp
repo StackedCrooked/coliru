5b0bc3f865450bf41f58de066e5e9039f34c43cd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-07 02:56:23 +0000

    Update archive 57

---
+++ trunk/Archive2/57/20f22407240bdf/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <type_traits>
+
+template<typename T>
+void f() {
+    // you're some library author bob and you want to accept template types that are arrays
+    static_assert(std::extent<T>::value <= 5, "...");
+    static_assert(std::is_array<T>::value, "...");
+    // now these assertions hold and you can make your array
+    T x = { 1, 2, 3, 4, 5 };
+    for(auto&& i : x) {
+        std::cout << i << ' ';
+    }
+    std::cout << '\n' << __PRETTY_FUNCTION__ << '\n';
+    // good thing the standard isn't an asshole about this
+}
+
+int main() {
+    int x[5] = {};
+    using y = int[];
+    f<decltype(x)>();
+    f<y>();
+}
