69ab24099b1fd9eb9d233e9ee2b075e67f2c2e2f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-07 23:15:55 +0000

    Update archive ca

---
+++ trunk/Archive2/ca/5de92af231d35f/cmd.sh
@@ -0,0 +1,9 @@
+clang++ -std=c++11 -Os -Wall -pedantic main.cpp && ./a.out <<INPUT
+  [SOFT] AQUA+
+  [FORWARD_SPEED]   0.00
+  [some other key] string value
+  [PERIODS_NUMBER]  6
+  [HEADINGS_NUMBER] 13
+  [LOWEST_HEADINGS]   0.00
+  [HIGHEST_HEADINGS] 180.00
+INPUT
