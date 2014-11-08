dfe0ce24de1d415437d5f53adeaef5a719a403a9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-08 01:01:39 +0000

    Update archive 73

---
+++ trunk/Archive2/73/c9904894069c05/main.cpp
@@ -0,0 +1,18 @@
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <iostream>
+
+using namespace boost::property_tree;
+
+int main() {
+    ptree pt;
+
+    auto& list = pt.add_child("list", ptree{});
+
+    for (auto data : { 12, 44, 23 })
+        list.add("value", data)
+            .add_child("<xmlattr>", ptree{})
+            .add("active", true);
+
+    xml_parser::write_xml(std::cout, pt);
+}
