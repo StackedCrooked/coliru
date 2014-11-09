ce107b4e152022392523fd1340792fff80428197
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-09 16:49:49 +0000

    Update archive a2

---
+++ Archive2/a2/8429e5f736bda0/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int d = 4;
+    a - b, c = d;
+    
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+    std::cout << d << std::endl;
+    
+    return 0;
+}
