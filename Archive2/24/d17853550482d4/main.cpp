9d03ca32661b476f57c8ced17052719bfab9115c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-11 20:30:51 +0000

    Update archive 24

---
+++ Archive2/24/d17853550482d4/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<math.h>
+void main ()
+{ 
+    int a,b,c ;
+    printf("donner a");
+    scanf("%d",&a);
+    printf("donner b");
+    scanf("%d",&b);
+    c=(a+b)*(a+b);
+    printf ("la solution est c");
+   
+}
