bed9ae2c6879e89f71e594eac1f6f9247e404a95
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-11 21:15:15 +0000

    Update archive 94

---
+++ trunk/Archive2/94/a3ec430ed758c3/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <iterator>
+#include <cstring>
+#include <algorithm>
+#include <stdexcept>
+
+template <std::size_t N,
+          std::size_t N2>
+std::string extractQualifiedName(char const(&str)[N], char const(&name)[N2])
+{
+    using namespace std;
+	static_assert(N >= N2, "");
+
+	auto cond = [] (unsigned char c) {return !isalnum(c) && c != '_';};
+
+	char const* iter = str;
+	for (;;++iter)
+	{
+		iter = search( iter, end(str),
+		               begin(name), end(name)-1 );
+
+		if (iter == end(str))
+			throw invalid_argument("");
+
+		if ((iter == begin(str)      || cond(iter[-1]))
+		 && (iter ==   end(str) - N2 || cond(iter[N2-1])))
+			break;
+	}
+
+	return string(find_if(reverse_iterator<char const*>(iter),
+	                      reverse_iterator<char const*>(str),
+	                      [=] (char c) {return cond(c) && !strchr(":(){} ", c);}).base(),
+	              iter + N2-1);
+}
+
+
+namespace foo {
+namespace {
+
+template<int i>
+int (*bar(int (*arg)(int *)))(int *)
+{
+	std::cout << extractQualifiedName(__PRETTY_FUNCTION__, __func__);
+	return arg;
+}
+
+} // anonymous namespace
+} // namespace foo
+
+int main()
+{
+	foo::bar<1337>(nullptr);
+}
