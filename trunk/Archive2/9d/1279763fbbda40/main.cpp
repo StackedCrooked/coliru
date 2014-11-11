35f1eeb3d5a4a04232e14447eab2c2cf99f0d1d3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-11 04:42:00 +0000

    Update archive 9d

---
+++ trunk/Archive2/9d/1279763fbbda40/main.cpp
@@ -0,0 +1,49 @@
+#include <tuple>
+
+template <int n>
+struct A
+{   
+    void baz() {}
+};
+
+template <>
+struct A<3>
+{
+    // no baz!
+};
+
+template<class...> struct make_void { using type = void; };
+template<class... Ts> using void_t = typename make_void<Ts...>::type;
+
+
+template <class, class = void>
+struct Foo; // undefined
+
+template <class... Ts, class T>
+struct Foo<std::tuple<Ts...>, T> {
+    // no bar
+};
+
+template <class... Ts>
+struct Foo<std::tuple<Ts...>, void_t<decltype(std::declval<Ts&>().baz())...>>
+{
+    static constexpr auto I = 2;
+    virtual void bar()
+    {
+        auto x = std::get<I>(std::make_tuple(Ts()...));
+        x.baz();
+    }
+};
+
+template<class... Ts>
+using Foo_Alias = Foo<std::tuple<Ts...>>;
+
+template struct Foo<std::tuple<A<1>, A<2>, A<3>>>;
+template struct Foo<std::tuple<A<1>, A<2>, A<2>>>;
+
+int main()
+{
+    Foo_Alias<A<1>, A<2>, A<3>> f1;
+    Foo_Alias<A<1>, A<2>, A<2>> f2;
+    f2.bar();
+}
