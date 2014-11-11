1b48551427f7e7aedd65c9d669b924bc106bf8de
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-11 02:13:23 +0000

    Update archive d5

---
+++ Archive2/d5/27ff5d506f691c/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <type_traits>
+
+template<class T>
+struct RefOps {
+    static constexpr bool specialized = false;
+};
+
+template<class Tag>
+struct Ref {
+    private:
+        void* something;
+        void* another_thing;
+        
+    public:
+        template<class T>
+        typename std::conditional<RefOps<T>::specialized, RefOps<T>, Ref<T>>::type
+        operator[](std::string name) {
+           //magic->getMember(something, name.c_str); 
+           return {};
+        }
+};
+
+struct java {
+    struct BeanProxyFactory {};
+    struct lang {
+        struct String{};
+    };
+};
+
+template <>
+class RefOps<java::lang::String> {
+    public:
+        static constexpr bool specialized = true;
+        
+        operator std::string() const {
+            return "Testing everyone";
+        }
+};
+
+int main() {
+    auto some_instance = Ref<java::BeanProxyFactory>();
+    
+    std::string bean_string = some_instance.operator[]<java::lang::String>("createMagicBeanString");
+    
+    std::cout << bean_string << "\n";
+}
