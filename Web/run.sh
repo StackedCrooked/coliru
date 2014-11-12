5f16c54e22ec7282ffdf79c56a8563ea8f23ef1e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-12 19:43:39 +0000

    Update feedback.

--- Web/run.sh
+++ Web/run.sh
@@ -2,4 +2,8 @@
 source coliru_env.source
 source logger.source
 
-sudo -u webserver -E ruby webserver-sinatra.rb -o "${COLIRU_URL}"
+while true ; do 
+    sudo -u webserver -E ruby webserver-sinatra.rb -o "${COLIRU_URL}"
+    echo "RESTARTING WEBSERVER SOON"
+    sleep 5
+done
