336facd977bc0df611ed19570273c0d0a2e80564
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-12 20:08:32 +0000

    Update archive 7d

---
+++ Archive2/7d/587b77f2cf4585/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <utility>
+
+int main() {
+    int x{5};
+    std::cout << "\nInitial value of x: " << x << '\n';
+    
+    int result = std::exchange(x, 9);
+    
+    std::cout << "Result of exchange(): " << result << '\n';
+    std::cout << "New value of x: " << x << '\n';
+}
