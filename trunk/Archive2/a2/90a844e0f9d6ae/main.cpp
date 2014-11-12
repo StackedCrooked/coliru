1ae9364d4349de70eb8fed048474c62c755b75f4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-12 13:29:14 +0000

    Update archive a2

---
+++ trunk/Archive2/a2/90a844e0f9d6ae/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <thread>
+#include <future>
+#include <functional>
+#include <vector>
+
+#include <boost/assign/std/vector.hpp>
+using namespace boost::assign;
+
+
+
+
+
+int main()
+{
+
+std::vector<int> v;
+v += 1, 2, 3, 4;
+}
+
+
