b413e4ca78dcc0761a9584ad02070f6daeca7d98
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-12 23:03:18 +0000

    Update archive 6a

---
+++ Archive2/6a/92105f5f85b943/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <tuple>
+
+class Widget {
+public:
+  Widget(){std::cout << "Widget ctor  " << std::endl; }
+  Widget(int h) : h(h){std::cout << "Widget ctor param " << std::endl; }
+
+  Widget(const Widget&) { std::cout << "Widget copy ctor  " << std::endl;  }
+  Widget(Widget&&) { std::cout << "Widget move ctor  " << std::endl;  }           // added this
+
+  template<typename T>
+  Widget(const T&) { std::cout << "Generalized Widget copy ctor  " << std::endl;  }
+
+  template<typename T>
+  Widget(T&&) { std::cout << "Universal Widget ctor  " << std::endl;  }
+
+  int h;
+};
+
+
+
+template<typename T>
+struct RvalueTest{
+    RvalueTest(T&& value) : value( std::forward<T>(value) ){}
+    T&& value;
+};
+
+
+
+int main() {
+    //auto t = std::forward_as_tuple(Widget(12));
+    RvalueTest<Widget> r(Widget(12));
+    std::cout << r.value.h;
+}
