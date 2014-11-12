f11789b1bd206e24407ce4c855d526a013215d59
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-12 18:27:04 +0000

    Update archive 7d

---
+++ Archive2/7d/b47dfe9e9d42b2/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+  struct birthyear {
+      int birth;
+  };
+
+  struct person {
+      int age;
+      char forename[5];
+      struct birthyear pbirthyear;
+  };
+
+  struct person p1;
+  int age = 45;
+
+  printf("%zu", sizeof(p1.pbirthyear));
+  printf(" %zu", sizeof(age));
+  return 0;
+}
