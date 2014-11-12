2ece338617a5867abdb3235e4a9e8afef803eb53
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-12 23:31:49 +0000

    Update archive c0

---
+++ trunk/Archive2/c0/bb133b0be578b0/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+int main() 
+{ 
+    const std::string input = "asdf 1.320 1 234.43 5.00000 3" ;
+    
+    // create an input string stream to read from the string
+    std::istringstream stm(input) ;
+    
+    // formattewd input from the string stream
+    std::string str ;
+    double d1, d2, d3 ;
+    int i1, i2 ;
+    if( stm >> str >> d1 >> i1 >> d2 >> d3 >> i2 )
+        std::cout << std::fixed << str << '\n' << d1 << '\n' << i1 << '\n' 
+                  << d2 << '\n' << d3 << '\n' << i2 << '\n' ;
+}
