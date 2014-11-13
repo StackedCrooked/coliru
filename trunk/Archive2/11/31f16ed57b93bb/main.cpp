3f7656dec2ae981f1f7ff5e8ce1acfee469933ee
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-13 22:45:30 +0000

    Update archive 11

---
+++ trunk/Archive2/11/31f16ed57b93bb/main.cpp
@@ -0,0 +1,33 @@
+class A{ // Interface
+      public:
+             virtual void example(void)=0;    
+             void thisWorks(void) {}
+};
+
+
+class B:public A{ // Derived From Interface
+      public:
+             void example(void) {}
+};
+
+class C{ // Other Random Class
+
+};
+
+template <class T>
+class D: public C{ // Derived From Other Random Class with Template for Interface A derived Classes
+       T* Object;
+       public:
+              D(T * Object){ // implemented in source file
+                   this->Object=Object;
+              }
+              void otherExample(void){
+                   this->Object->thisWorks();
+                   this->Object->example();
+              };
+};
+
+int main() {
+  D<B>* TestD=new D<B>(new B());
+  TestD->otherExample(); 
+}
