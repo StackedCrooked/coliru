94fcc206ac7fe646d259b3e418314845a20ab690
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-13 20:20:12 +0000

    Update archive 38

---
+++ trunk/Archive2/38/a699947c6ba67d/main.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <iostream>
+
+class PilotClass
+{
+    public :
+    static std::string GetTableName() { return "Pilot"+ GetName(); }
+    static std::string GetName() { return ""; }
+};
+
+class AI : public PilotClass
+{
+    public :
+    static std::string GetName() { return "AI"; }
+};
+
+class NavMesh : public PilotClass
+{
+    public :
+    static std::string GetName() { return "NavMesh"; }
+};
+
+int main()
+{
+    std::cout << AI::GetTableName();
+    std::cout << NavMesh::GetTableName();
+    
+    return 0;
+};
