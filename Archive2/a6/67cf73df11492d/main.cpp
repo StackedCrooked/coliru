295ef40bcc62475b59b5bb9a1fc64be4668af7d2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-13 00:41:16 +0000

    Update archive a6

---
+++ Archive2/a6/67cf73df11492d/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+#include <string>
+ 
+int principal ( ) 
+{ 
+    std :: string str1 =  "45" ; 
+    std :: string str2 =  "3.14159" ; 
+    std :: string str3 =  "31337" con palabras ; 
+    std :: string Str4 =  "palabras y 2" ;
+ 
+    int myint1 = std :: Stoi ( cadena1 ) ; 
+    int myint2 = std :: Stoi ( str2 ) ; 
+    int myint3 = std :: Stoi ( str3 ) ; 
+    // error: "std :: invalid_argument ' 
+    // int myint4 = std: : Stoi (STR4);
+ 
+    std :: cout  <<  "std :: Stoi ( \ " "  << cad1 <<  " \ " ) es "  << myint1 <<  ' \ n ' ; 
+    std :: cout  <<  "std :: Stoi ( \ " "  << cad2 <<  " \ " ) es "  << myint2 <<  ' \ n ' ; 
+    std :: cout  <<  "std :: Stoi ( \ " "  << str3 <<  " \ " ) es "  << myint3 <<  ' \ n ' ; 
+    // std :: cout << "std :: Stoi (\" "<< STR4 <<" \ ") es" << myint4 << "\ n"; 
+}
