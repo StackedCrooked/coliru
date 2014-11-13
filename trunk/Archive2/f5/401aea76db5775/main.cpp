0fb46c89a7c839f550239643ea3be07549b50c1f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-13 20:54:23 +0000

    Update archive f5

---
+++ trunk/Archive2/f5/401aea76db5775/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <functional>
+using namespace std;
+
+class A
+{
+public:
+    A(int inI, int inJ) : _i(inI), _j(inJ)
+    {
+        create_functor();
+    }
+
+    A(const A& rtSide) : _i(rtSide._i),_j(rtSide._j)
+    {
+        create_functor();
+    }
+
+    A(A&& rtSide) : _i(rtSide._i), _j(rtSide._j)
+    {
+        rtSide.f = nullptr;
+        create_functor();
+    }
+
+    A& operator=(A&& rtSide)
+    {
+
+        _i = rtSide._i;
+        _j = rtSide._j;
+        create_functor();
+
+        rtSide.f = nullptr;
+
+        return *this;
+    }
+
+    A& operator=(A& rtSide)
+    {
+        if (this == &rtSide)
+            return *this;
+
+        _i = rtSide._i;
+        _j = rtSide._j;
+        create_functor();
+
+        rtSide.f = nullptr;
+
+        return *this;
+    }
+
+    ~A() {}
+
+    void reset_i(const int& newI) { _i = newI; }
+
+    function<double(const double&)> f;
+
+private:
+    void create_functor()
+    {
+        auto r = std::ref(_i);
+        f = [r](const double& inX) -> double {return inX * static_cast<double>(r.get()); };
+    }
+
+    int _i;
+    int _j;
+};
+
+int main()
+{
+
+    A _A1(2,0);
+    A _A2(1,0);
+
+    _A2 = _A1;
+
+    cout << _A2.f(2) << endl;
+
+    _A2.reset_i(4);
+
+    cout << _A2.f(2) << endl;
+
+    return 0;
+}
