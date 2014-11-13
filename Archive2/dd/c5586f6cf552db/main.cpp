acb8429dcd636d80c6c30d249d3667013710f20d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-13 02:23:06 +0000

    Update archive dd

---
+++ Archive2/dd/c5586f6cf552db/main.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <algorithm>
+
+namespace Namespace
+{
+    struct Bar
+    {
+    };
+
+    bool barComparator(const Bar& bar1 , const Bar& bar2 )
+    {
+        return true;
+    }
+
+    struct Foo
+    {   
+        void doStuff()
+        {
+            std::vector<Bar> barVec;
+            std::sort( barVec.begin() , barVec.end() , barComparator );
+        }
+    };
+}   
+
+int main()
+{
+}
