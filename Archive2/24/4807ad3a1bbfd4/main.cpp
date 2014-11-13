9ae03a6ec1ed2c201f6fc5f3d9bb997a3b220921
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-13 14:55:31 +0000

    Update archive 24

---
+++ Archive2/24/4807ad3a1bbfd4/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+
+int main()
+{
+    const std::string przyszedl="przyszedł";
+    const std::string przeszedl="przeszedł";
+    std::cout << (przyszedl == przeszedl? "ja" : "nie") << std::endl;
+}
