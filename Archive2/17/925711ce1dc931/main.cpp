5268db549432a7a2ab510742e2880799230b8e11
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-13 22:16:37 +0000

    Update archive 17

---
+++ Archive2/17/925711ce1dc931/main.cpp
@@ -0,0 +1,43 @@
+#ifndef __MYVECTOR_CLASS__
+#define __MYVECTOR_CLASS__
+
+template<class Type>
+class MyVector{
+public:
+    //....
+    MyVector& operator=(const MyVector& source); //works
+    friend MyVector<Type> operator+(MyVector<Type> lhs, const MyVector<Type> &rhs); //doesnt
+    //....
+};
+
+template<class Type>
+MyVector<Type>& MyVector<Type>::operator=(const MyVector &v){
+    /*if (_d != v._d){
+        _d = v._d;
+        _deleteArray();
+        _createArray(_d);
+    }
+    _assignValues(v._vector, (v._vector + _d));*/
+    return *this;
+}
+
+
+template<class Type>
+MyVector<Type> operator+(MyVector<Type> lhs, const MyVector<Type> &rhs){
+    /*if (lhs._d == rhs._d){
+        for (int index = 0; index < lhs._d; index++){
+            lhs._vector[index] += rhs._vector[index];
+        }
+    }*/
+    return lhs;
+}
+
+#endif // __MYVECTOR_CLASS__
+
+int main()
+{
+	MyVector<float> a;
+    MyVector<float> b;
+    a = b; // works alone
+    a = a + b; //breaks
+}
