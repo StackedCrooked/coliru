2257caabe0fdfbfa569a2191dbfa519b0c82d374
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-14 21:00:27 +0000

    Update archive 62

---
+++ trunk/Archive2/62/18125521832c8b/main.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <tuple>
+
+template <typename BidirIt, typename... Predicates>
+void trivial_mul_part( BidirIt first, BidirIt last, Predicates... preds )
+{
+    std::sort( first, last,
+	  [=] (decltype((*first)) lhs, decltype((*first)) rhs)
+	  {
+	      return std::make_tuple(preds(lhs)...) > std::make_tuple(preds(rhs)...);
+	  } );
+}
+
+template <typename BidirIt, typename Out>
+void multi_partition( BidirIt, BidirIt, Out  ) {}
+
+template <typename BidirIt, typename OutputIterator,
+          typename Pred, typename... Predicates>
+void multi_partition( BidirIt first, BidirIt last,OutputIterator out,
+                      Pred pred, Predicates... preds )
+{
+	auto iter = std::partition(first, last, pred);
+	*out++ = iter;
+	multi_partition<BidirIt>(iter, last, out, preds...);
+}
+
+#include <iostream>
+
+int main()
+{
+	{
+		int arr[] {0, 1, 0, 1, 0, 2, 1, 2, 2};
+		trivial_mul_part(arr, std::end(arr), [] (int i) {return i == 2;},
+		                                     [] (int i) {return i == 1;});
+		for (auto i : arr)
+			std::cout << i << ", ";
+	}
+	std::cout << '\n';
+	{
+		int arr[] {0, 1, 0, 1, 0, 2, 1, 2, 2};
+		std::vector<int*> iters;
+		multi_partition(arr, std::end(arr), std::back_inserter(iters),
+		                 [] (int i) {return i == 2;},
+		                 [] (int i) {return i == 1;});
+		for (auto i : arr)
+			std::cout << i << ", ";
+		std::cout << '\n';
+		for (auto it : iters)
+			std::cout << "Split at " << it - arr << '\n';
+	}
+}
