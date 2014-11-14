5557055234b89c1096e339b328a3dea872a1059d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-14 23:37:00 +0000

    Update archive e6

---
+++ trunk/Archive2/e6/facaa9f18e252f/main.cpp
@@ -0,0 +1,16 @@
+    #include <iostream>
+    #include <string>
+    #include <cassert>
+    
+    void f2()
+    {
+        std::string s = "but I have heard it works even if you don't believe in it" ;
+        s.replace(0, 4, "" ).replace(s.find( "even" ), 4, "only" ).replace(s.find( " don't" ), 6, "" );
+        
+        assert( s == "I have heard it works only if you believe in it" ) ;
+    }
+    
+    int main()
+    {
+       f2() ;
+    }
