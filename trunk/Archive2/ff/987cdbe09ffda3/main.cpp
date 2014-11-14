12a2c0714ceeca4eb94e41331150825db6acc0e7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-14 15:51:37 +0000

    Update archive ff

---
+++ trunk/Archive2/ff/987cdbe09ffda3/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+    clrscr();
+    int a,
+    cout<<"enter a number"<<endl;
+    cin>>a;
+    s=a*a*a;
+    cout<<"cube root of "<<a<<"is"
+}
