3a1dc7c0ea56dfa7121a7ead5f2c5e91704f0087
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-14 10:44:11 +0000

    Update archive 5b

---
+++ Archive2/5b/fa06145ef14250/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <cfloat>
+ 
+int main()
+{
+    std::cout << std::boolalpha
+              << "isfinite(NaN) = " << std::isfinite(NAN) << '\n'
+              << "isfinite(Inf) = " << std::isfinite(INFINITY) << '\n'
+              << "isfinite(0.0) = " << std::isfinite(0.0) << '\n'
+              << "isfinite(exp(800)) = " << std::isfinite(std::exp(800)) << '\n'
+              << "isfinite(DBL_MIN/2.0) = " << std::isfinite(DBL_MIN/2.0) << '\n';
+}
