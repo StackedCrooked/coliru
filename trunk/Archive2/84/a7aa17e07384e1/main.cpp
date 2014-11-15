6f2ceb2467237287a15e5f5d895cd239f34bc1ff
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-15 01:36:43 +0000

    Update archive 84

---
+++ trunk/Archive2/84/a7aa17e07384e1/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+void Move(int arr[5][5])
+{
+    printf("hello");
+ for (int x=0; x>0; x++) {
+    for (int y=0; y>0; y++) {
+        if (arr[x][y]=1)
+         {
+             
+         }
+    ;
+  }}
+    
+return;
+}
+
+
+
+int main()
+{    std::vector<std::string> words = {
+        "Hello", "from", "GCC", __VERSION__, "!"
+    };
+    std::cout << words << std::endl;
+
+int x;
+int y;
+int table[5][5];
+ for (x=0; x>0; x++) {
+     for (y=0; y>0; y++) {
+    table[x][y]=1;
+  }}
+  
+table[1][1] = 0;
+Move(table);
+
+}
+
