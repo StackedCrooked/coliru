e1ca9ae7c1cf6c5cc16d1a68f71061f7b9451613
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-15 18:14:31 +0000

    Update archive 90

---
+++ Archive2/90/a2547792acb457/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <mutex>
+
+template<typename T>
+struct Base {
+    Base() {
+        std::call_once(flag_, []{T::register_all();} );
+    }
+
+    using F = void(T::*)();
+
+    template<F f>
+    static void register_method(const char* str) {
+        // ...
+        std::cout << "registered " << str << '\n';
+    }
+public:
+    static std::once_flag flag_;
+};
+
+struct Derived : Base<Derived> {
+public:
+    static void register_all() {
+        register_method<&Derived::foo>("foo");
+        register_method<&Derived::bar>("bar");
+    }
+public:
+    void foo() {}
+    void bar() {}
+};
+
+// have to initialise static-s outside class...
+template<>
+std::once_flag Base<Derived>::flag_{};
+
+int main() {
+    Derived x;
+    Derived y;
+}
