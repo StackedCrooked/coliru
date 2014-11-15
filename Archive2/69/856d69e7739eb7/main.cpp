09592c73762d79e788c2d2d4a1e84493ebd0dfd7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-15 18:59:49 +0000

    Update archive 69

---
+++ Archive2/69/856d69e7739eb7/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <mutex>
+
+template<typename T>
+struct Base {
+    Base() {
+        std::call_once(flag_, T::register_all );
+    }
+
+    using F = void(T::*)();
+
+    template<F f>
+    static void register_method(const char* str) {
+        // ...
+        std::cout << "registered " << str << '\n';
+    }
+public:
+    static std::once_flag flag_;
+};
+
+struct Derived1 : Base<Derived1> {
+public:
+    static void register_all() {
+        std::cout << "D1" << '\n';
+        register_method<&Derived1::foo>("foo");
+        register_method<&Derived1::bar>("bar");
+    }
+public:
+    void foo() {}
+    void bar() {}
+};
+struct Derived2 : Base<Derived2> {
+public:
+    static void register_all() {
+        std::cout << "D2" << '\n';
+        register_method<&Derived2::woot>("woot");
+    }
+public:
+    void woot() {}
+};
+
+// have to initialise static-s outside class...
+template<>
+std::once_flag Base<Derived1>::flag_{};
+// have to initialise static-s outside class...
+
+template<>
+std::once_flag Base<Derived2>::flag_{};
+
+int main() {
+    Derived1 x;
+    Derived1 y;
+    
+    Derived2 z;
+}
