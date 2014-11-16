7d71e2a7c92155cd3bd9dc14705f714b68d56037
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-16 13:47:11 +0000

    Dont restart webserver on crash.

--- trunk/Web/run.sh
+++ trunk/Web/run.sh
@@ -2,8 +2,4 @@
 source coliru_env.source
 source logger.source
 
-while true ; do 
-    sudo -u webserver -E ruby webserver-sinatra.rb -o "${COLIRU_URL}"
-    echo "RESTARTING WEBSERVER SOON"
-    sleep 5
-done
+sudo -u webserver -E ruby webserver-sinatra.rb -o "${COLIRU_URL}"
