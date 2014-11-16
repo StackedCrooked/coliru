ada31b203948d655b8ff2b0e9a7a06110531d18f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-16 02:24:17 +0000

    Update archive 38

---
+++ trunk/Archive2/38/a38ef80b3c857c/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+    constexpr auto upper_bound = 1'000'000'000'000 ; // C++14 literal
+    // constexpr auto upper_bound = 1000000000000 ; // C++11
+    
+    constexpr auto lower_bound = -1'000'000'000'000 ;
+
+    // integer_type is an alias for a signed integer type
+    // which can hold integers with 13 decimal digits
+    // http://www.stroustrup.com/C++11FAQ.html#decltype
+    using integer_type = decltype(9'999'999'999'999) ;
+
+    integer_type x = 0 ;
+    std::cin >> x ; // if input fails, x would be set to zero
+    integer_type y = 0 ;
+    std::cin >> y ; // do nothing if std::cin is in a failed state
+
+    if( x > lower_bound && y < upper_bound )
+    {
+        char relation = '=' ;
+        if( x < y ) relation = '<' ;
+        else if( y < x ) relation = '>' ;
+        std::cout << x << ' ' << relation << ' ' << y << '\n' ;
+    }
+}
