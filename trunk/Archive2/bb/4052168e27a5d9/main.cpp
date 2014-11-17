fbe89694d704da065a3ee81ef3b5757c808a80cc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-17 20:09:01 +0000

    Update archive bb

---
+++ trunk/Archive2/bb/4052168e27a5d9/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+
+template <typename T>
+class ArProg
+{
+    T val;
+	T inc;
+
+public:
+
+	ArProg(T val, T inc) : val(val), inc(inc) {}
+
+	ArProg& operator++() noexcept(noexcept(val += inc))
+	{
+		val += inc;
+		return *this;
+	}
+
+	T operator()() noexcept(noexcept(val += inc))
+	{
+		auto tmp = val;
+		val += inc;
+		return tmp;
+	}
+
+	operator T() const noexcept {return val;}
+};
+
+template <typename T, typename U>
+ArProg<typename std::common_type<T, U>::type>
+makeArProg( T val, U inc )
+{
+	return {val, inc};
+}
+
+int main()
+{
+	std::vector<float> vec;
+	std::generate_n( std::back_inserter(vec), 5, makeArProg(2.0f, 0.5f) );
+
+	for (auto f : vec)
+		std::cout << f << ", ";
+}
