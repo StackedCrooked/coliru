35a7bdb8efde20a012544ebdf51995b411b6ab2a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-17 13:53:45 +0000

    Update archive 56

---
+++ Archive2/56/48a6354a7bb716/cmd.sh
@@ -0,0 +1,2 @@
+clang++ -std=c++14 -Wall -Wextra -pedantic main.cpp && ./a.out
+g++ -std=c++14 -Wall -Wextra -pedantic main.cpp && ./a.out
---
+++ Archive2/56/48a6354a7bb716/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << __builtin_popcountll(0b1011001110) << '\n';
+}
