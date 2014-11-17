034d6eae0b144cae36ed785678818f8507115793
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-17 17:46:33 +0000

    Update archive 3c

---
+++ Archive2/3c/5f114aaf5b1d36/main.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+
+int main()
+{
+    std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    
+    int evens = 0;
+    struct not_a_lambda {
+        int& evens;
+        
+        void operator()(int n) const
+        {
+            if (n % 2 == 0) {
+                ++evens;
+            }
+        }
+    };
+    std::for_each(v.begin(), v.end(), not_a_lambda { evens });
+
+    std::cout << "There are " << evens
+        << " even numbers in the vector.\n";
+
+    evens = 0;
+    std::for_each(v.begin(), v.end(),
+            [&evens](int n)
+            {
+                if (n % 2 == 0) {
+                    ++evens;
+                }
+            }
+    );
+
+    std::cout << "There are " << evens
+        << " even numbers in the vector.\n";
+}
