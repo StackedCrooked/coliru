061511715dd52d14f2500c34b4c23dc85a31bd45
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-17 17:15:26 +0000

    Update archive fe

---
+++ trunk/Archive2/fe/f6ca310973fad1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <utility>
+
+// invariant: numbers.second != 0
+bool rem_is_9( std::pair<int,int> numbers ) { return ( numbers.first % numbers.second ) == 9 ; }
+
+// invariant: str contains decimal digits, str.size() == 9
+std::pair<int,int> split54( std::string str ) 
+{ return { std::stoi( str.substr(0,5) ), std::stoi( str.substr(5) ) }; }
+
+int main()
+{
+    std::string digits = "123456789" ;
+    int cnt = 0 ;
+    
+    do if( rem_is_9( split54(digits) ) ) ++cnt ; 
+    while( std::next_permutation( std::begin(digits), std::end(digits) ) ) ;
+
+    std::cout << cnt << '\n' ;
+}
