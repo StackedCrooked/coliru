1ae3f34d41fe16b1eed294e064dc762e28cbbd97
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-17 03:14:30 +0000

    Update archive 9a

---
+++ trunk/Archive2/9a/33f34ba620c8c4/main.cpp
@@ -0,0 +1,9 @@
+#include <ctime>
+#include <iostream>
+ 
+int main()
+{
+    std::time_t result = std::time(nullptr);
+    std::cout << std::asctime(std::localtime(&result))
+              << result << " seconds since the Epoch\n";
+}
