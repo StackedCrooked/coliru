063d8b1dd013ca5bc210f37ca502dbbf040e3ed4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-18 00:49:01 +0000

    Update archive ab

---
+++ trunk/Archive2/ab/9f4034c23384da/main.cpp
@@ -0,0 +1,53 @@
+#include<algorithm>
+#include<numeric>
+#include<iostream>
+#include<iomanip>
+#include<vector>
+
+template<class InputIt>
+InputIt repetition_find(InputIt first, InputIt last, size_t repetitions)
+{
+    std::vector<int> temp;
+    std::adjacent_difference(first, last, std::back_inserter(temp));
+    temp.front()=0;
+    
+    int count=0;
+    std::for_each(temp.rbegin(), temp.rend(), [&](auto& a) { if(a==0) a = ++count; else {a=++count; count=0;}} );
+    
+    auto diff =  std::find(temp.begin(), temp.end(), repetitions) - temp.begin();
+    return std::next(first, diff);
+}
+
+
+int main()
+{
+    std::vector<int> values = { 9, 9, 8, 8, 8, 8, 7, 7, 6, 6, 6 };
+
+    for(auto v : values)
+    {
+        std::cout<<std::setw(3)<<v<<" ";
+    }
+    std::cout<<std::endl;
+
+
+    std::vector<int> temp;
+    std::adjacent_difference(values.begin(), values.end(),std::back_inserter(temp));
+    temp.front()=0;
+
+    for(auto v : temp)
+    {
+        std::cout<<std::setw(3)<<v<<" ";
+    }
+    std::cout<<std::endl;
+
+    int count=0;
+    std::for_each(temp.rbegin(), temp.rend(), [&](auto& a) { if(a==0) a = ++count; else {a=++count; count=0;}} );
+
+    for(auto v : temp)
+    {
+        std::cout<<std::setw(3)<<v<<" ";
+    }
+    std::cout<<std::endl;
+
+    std::cout<<*repetition_find(values.begin(), values.end(), 4)<<std::endl;
+}
