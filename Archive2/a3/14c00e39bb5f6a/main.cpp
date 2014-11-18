98e7d595d4bfcf9ead5495128f02d2e066cd6d2c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-18 11:07:03 +0000

    Update archive a3

---
+++ Archive2/a3/14c00e39bb5f6a/main.cpp
@@ -0,0 +1,39 @@
+template <typename T>
+class Delegate;
+
+template <typename T>
+struct Base;
+
+template <typename R, typename... Args>
+struct Base<Delegate<R(Args...)>>
+{
+    R operator()(Args...)
+    {
+        Delegate<R(Args...)>* that = static_cast<Delegate<R(Args...)>*>(this);
+        return {};
+    }
+};
+
+template <typename R>
+struct Base<Delegate<R()>>
+{
+    R operator()()
+    {
+        Delegate<R()>* that = static_cast<Delegate<R()>*>(this);
+        return {};
+    }
+};
+
+template <typename R, typename... Args>
+class Delegate<R(Args...)> : public Base<Delegate<R(Args...)>>
+{
+    friend class Base<Delegate<R(Args...)>>;
+};
+
+int main()
+{
+    Delegate<int(int)> a;
+    Delegate<int()> b;
+    a(1);
+    b();
+}
