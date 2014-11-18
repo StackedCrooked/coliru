d5a2b1630b7f6792d2ff55efdcc4c3b54f985d87
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-18 10:29:29 +0000

    Update archive 55

---
+++ trunk/Archive2/55/b3297dd7e65bb3/main.cpp
@@ -0,0 +1,36 @@
+#include <utility>
+
+template <typename T>
+struct Impl;
+
+template <typename R, typename... Args>
+struct Impl<R(Args...)>
+{
+    static R call(Args&&...) { return {}; }
+};
+
+template <typename R>
+struct Impl<R()>
+{
+    static R call() { return {}; }
+};
+
+template <typename T>
+struct Delegate;
+
+template <typename R, typename... Args>
+struct Delegate<R(Args...)>
+{
+    R operator()(Args... args)
+    {
+        return Impl<R(Args...)>::call(std::forward<Args>(args)...);
+    }
+};
+
+int main()
+{
+    Delegate<int(int)> a;
+    Delegate<int()> b;
+    a(1);
+    b();
+}
