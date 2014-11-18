14d2bd002c732191f7336d7dee5afa882202a226
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-18 09:21:13 +0000

    Update archive fb

---
+++ Archive2/fb/917f465f9d1b51/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <iterator>
+#include <thread>
+#include <chrono>
+ 
+void f(int n)
+{
+    std::this_thread::sleep_for(std::chrono::seconds(n));
+    std::cout << "thread " << n << " ended" << '\n';
+}
+ 
+std::vector<std::thread> move_test()
+{
+    std::vector<std::thread> v;
+    v.emplace_back(f, 1);
+    v.emplace_back(f, 2);
+    v.emplace_back(f, 3);
+        
+    // auto t = std::thread(f,4);
+    // v.emplace_back(t);    
+    return v;
+}
+
+int main() 
+{
+    auto v = move_test(); 
+    // auto v1 = v;
+    std::list<std::thread> l;
+    
+    // copy() would not compile, because std::thread is noncopyable
+   
+    std::move(v.begin(), v.end(), std::back_inserter(l)); 
+    for (auto& t : l) t.join();
+}
