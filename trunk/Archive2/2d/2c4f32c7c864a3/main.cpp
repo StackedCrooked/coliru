8fc9b8988dca01ec891be05abaffefdb1e4503eb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-18 22:03:58 +0000

    Update archive 2d

---
+++ trunk/Archive2/2d/2c4f32c7c864a3/main.cpp
@@ -0,0 +1,19 @@
+class Cool
+{
+    public:
+        Cool();
+        int getVar();
+};
+
+#include <iostream>
+
+Cool::Cool()
+{
+    //ctor
+}
+
+int getVar()
+{
+    int pop = 22;
+    return pop;
+}
