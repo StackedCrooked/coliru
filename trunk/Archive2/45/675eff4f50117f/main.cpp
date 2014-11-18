e1ba0c2df81062179c0c61b175d67475ba425cab
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-18 10:59:12 +0000

    Update archive 45

---
+++ trunk/Archive2/45/675eff4f50117f/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <boost/lexical_cast.hpp>
+
+namespace mandala
+{
+    struct vec2_t {
+        float x,y;
+    };    
+}
+
+namespace mandala
+{
+    std::istream& operator>>(std::istream& istream, vec2_t& v) {
+        return istream >> std::skipws >> v.x >> v.y;
+    }
+}
+
+int main()
+{
+    auto v = boost::lexical_cast<mandala::vec2_t>("1231 152");
+    std::cout << "Parsed: " << v.x << ", " << v.y << "\n";
+}
