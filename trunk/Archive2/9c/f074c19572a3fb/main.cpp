fd6b5f8a931429c264a5eae91fac78638d0b1cdf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-18 05:04:11 +0000

    Update archive 9c

---
+++ trunk/Archive2/9c/f074c19572a3fb/main.cpp
@@ -0,0 +1,13 @@
+    #include <iostream>
+    
+    struct A 
+    {
+        A(){ };
+        ~A() = delete; //{ std::cout << "~A::A()" << std::endl; };
+    };
+    
+    struct B: A { };
+    
+    B *b = new B;
+    
+    int main(){ }
