2f20c0aab7fdb21e8938e9c3de0735b3ada5edb4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-18 16:40:30 +0000

    Update archive 6a

---
+++ Archive2/6a/213cc2a14f1d2e/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+int main()
+{
+    std::vector<std::string> words = {
+        "Hello", "from", "GCC", __VERSION__, "!"
+    };
+    std::cout << words << std::endl;
+}
+#include<iosteam.h>
+
+void main(void)
+
+{
+  cout<<"Учимся программировать на языке C++!";
+} 
