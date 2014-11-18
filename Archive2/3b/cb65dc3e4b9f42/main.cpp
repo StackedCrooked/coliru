00ba7c89373bd832d538f70903109f5192d9128d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-18 02:31:53 +0000

    Update archive 3b

---
+++ Archive2/3b/cb65dc3e4b9f42/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template <class Iter>
+Iter consecutive_find(Iter first, Iter last, std::size_t n)
+{
+    Iter marker(first), lead(first);
+    
+    while (lead != last)
+    {
+        std::size_t count(1);
+        
+        lead = std::next(marker);
+        
+        while ((lead != last) && (*marker == *lead))
+        {
+            ++count;
+            ++lead;
+        }
+        
+        if (count == n)
+        {
+            if ((lead == last) || (*lead != *marker))
+                    return lead - count;
+            
+            ++lead;
+        }
+        marker = lead;
+    }
+    
+    return last;
+}
+
+int main()
+{
+    std::vector<int> values = { 9, 9, 8, 8, 8, 8, 7, 7, 6, 6, 6 };
+    auto it = consecutive_find(values.begin(), values.end(), 4);
+    
+    while (it != values.end())
+    {
+        std::cout << *it << " ";
+        it++;
+    }
+}
