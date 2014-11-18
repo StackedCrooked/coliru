be2b05d4dad7c7274abe41671964dcab34f66adf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-18 15:15:27 +0000

    Update archive 29

---
+++ Archive2/29/43cb6c376dee11/cmd.sh
@@ -0,0 +1,2 @@
+clang++ -std=c++11 -Os -Wall -pedantic -pthread main.cpp
+./a.out -a 8 -m 7 -a 32
---
+++ Archive2/29/43cb6c376dee11/main.cpp
@@ -0,0 +1,65 @@
+#define BOOST_SPIRIT_DEBUG
+#include <boost/fusion/adapted/struct.hpp>
+#include <boost/fusion/include/io.hpp>
+#include <boost/spirit/include/qi.hpp>
+
+namespace qi  = boost::spirit::qi;
+
+struct Operation {
+    enum Kind { add, multiply } kind;
+    double operand;
+
+    friend std::ostream& operator<<(std::ostream& os, Kind k) {
+        switch (k) {
+            case add:      return os << "add";
+            case multiply: return os << "multiply";
+        };
+        return os << "??";
+    }
+};
+
+BOOST_FUSION_ADAPT_STRUCT(Operation, (Operation::Kind,kind)(double,operand))
+
+template <typename It, typename Skipper = qi::space_type> 
+   struct expression_grammar : qi::grammar<It, std::vector<Operation>(), Skipper> {
+       expression_grammar() : expression_grammar::base_type(start) {
+           using namespace qi;
+
+           opkinds.add
+               ("add",      Operation::add)
+               ("multiply", Operation::multiply)
+               ;
+
+           option = opkinds >> double_;
+
+           start  = *(option >> ';');
+
+           BOOST_SPIRIT_DEBUG_NODES((start)(option))
+       }
+     private:
+       qi::symbols<char, Operation::Kind> opkinds;
+       qi::rule<It, Operation(), Skipper> option;
+       qi::rule<It, std::vector<Operation>(), Skipper> start;
+   };
+
+int main() {
+    std::string const expression("add 8; multiply 7; add 32;");
+
+    typedef std::string::const_iterator It;
+    expression_grammar<It> grammar;
+
+    It first(expression.begin()), last(expression.end());
+    std::vector<Operation> operations;
+    bool ok = qi::phrase_parse(first, last, grammar, qi::space, operations);
+
+    if (ok)
+    {
+        std::cout << "Parse success\n";
+        for (auto const& op : operations)
+            std::cout << boost::fusion::as_vector(op) << "\n";
+    } else
+        std::cout << "Parse failed\n";
+
+    if (first!=last)
+       std::cout << "Remaining input: '" << std::string(first,last) << "'\n";
+}
