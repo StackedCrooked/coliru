e795a4640456d5256047438cb113d1575aa0ee94
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-19 11:39:07 +0000

    Update archive eb

---
+++ trunk/Archive2/eb/019d8ad8a8a30d/main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+template <typename ... Args>
+struct Convert;
+
+template <typename T>
+struct Convert<T,T> {
+    static int param() { return 0; }
+};
+
+template <typename T, typename U>
+struct Convert<T,U> {
+    static int param() { return -Convert<U,T>::param(); }
+};
+
+struct A{};
+struct B{};
+struct C{};
+
+template <>
+struct Convert<A,B> {
+    static int param() { return 42; }
+};
+
+
+int main()
+{
+    std::cout<<Convert<A,B>::param()<<std::endl;
+    std::cout<<Convert<B,A>::param()<<std::endl;
+    
+    
+    std::cout<<Convert<A,A>::param()<<std::endl;
+}
