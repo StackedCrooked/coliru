d47ba43835a1ed8459cab6b8c9bdc7009a1af36e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-19 22:21:24 +0000

    Update archive cb

---
+++ Archive2/cb/eefdcc879c9c1f/cmd.sh
@@ -0,0 +1,7 @@
+g++ -std=c++11 -Os -Werror -Wall main.cpp && ./a.out
+2 asdjaklsdjalkjsdflkjsdflkjsdf wlerlkjlksdfjk
+-5 asdfhjsksdfl weuriiodfus dwerh
+1 qasdasdaj asdlkjal sdas
+5 sdafjkdsfu welrkj sdjk lksdfljk 'werkjklsdflkjwer
+3 sdfjklsdfj siuweroiuiosdj dfjslkdjjsdf/
+INPUT
