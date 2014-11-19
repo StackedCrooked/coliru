0464e81b1cbb26252090a78004d7a7e39a44804a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-19 21:41:55 +0000

    Update archive 01

---
+++ trunk/Archive2/01/63ade1172ab515/main.cpp
@@ -0,0 +1,57 @@
+#include <tuple>
+
+template <typename ReturnType, typename... Args>
+struct function_traits_defs
+{
+    static constexpr size_t arity = sizeof...(Args);
+    
+    using result_type = ReturnType;
+    
+    template <size_t i>
+    struct arg
+    {
+        using type = typename std::tuple_element<i, std::tuple<Args...>>::type;
+    };
+};
+
+template <typename T>
+struct function_traits_impl;
+
+template <typename ClassType, typename ReturnType, typename... Args>
+struct function_traits_impl<ReturnType(ClassType::*)(Args...)>
+    : function_traits_defs<ReturnType, Args...> {};
+
+template <typename ClassType, typename ReturnType, typename... Args>
+struct function_traits_impl<ReturnType(ClassType::*)(Args...) const> // + other cv-ref-variations
+    : function_traits_defs<ReturnType, Args...> {};
+
+template <typename ReturnType, typename... Args>
+struct function_traits_impl<ReturnType(Args...)>
+    : function_traits_defs<ReturnType, Args...> {};
+
+template <typename ReturnType, typename... Args>
+struct function_traits_impl<ReturnType(*)(Args...)>
+    : function_traits_defs<ReturnType, Args...> {};
+
+template <typename T, typename V = void>
+struct function_traits
+    : function_traits_impl<T> {};
+
+template <typename T>
+struct function_traits<T, decltype((void)&T::operator())>
+    : function_traits_impl<decltype(&T::operator())> {};
+
+int main()
+{
+    static_assert(function_traits<void()>::arity == 0, "!");
+    
+    static_assert(function_traits<void(int)>::arity == 1, "!");
+    
+    static_assert(function_traits<void(*)(int, float)>::arity == 2, "!");  
+    
+    auto lambda = [] (int, float, char) {};
+    static_assert(function_traits<decltype(lambda)>::arity == 3, "!");
+    
+    auto mutable_lambda = [] (int, float, char, double) mutable {};
+    static_assert(function_traits<decltype(mutable_lambda)>::arity == 4, "!");
+}
