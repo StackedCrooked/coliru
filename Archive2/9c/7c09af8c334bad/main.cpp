04565133743d57f98aeffc24128cd3ae14b693ee
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-19 06:08:00 +0000

    Update archive 9c

---
+++ Archive2/9c/7c09af8c334bad/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+void vigenere_encrypt(std::string& s, std::string key) ;
+void vigenere_decrypt(std::string& s, std::string key) ;
+
+int main( int argc, /*const char** argv*/ char* argv[] )
+{
+        if (argc < 3 )
+        {
+    usage:
+            std::cerr << "Usage: " << argv[0] << " -e <text to encrypt>\n"
+                      << "       " << argv[0] << " -d <text to decrypt>\n" ;
+            return 1 ;
+        }
+
+        const std::string option = argv[1];
+
+        std::string text = argv[2];
+        // cat the remaining cmd line args
+        for( int i = 3 ; i < argc ; ++i ) { text += ' ' ; text += argv[i] ; }
+
+        const std::string key("Passkey");
+
+        if ( option== "-e" )
+            std::cout << "Encrypt: '" << text << "'\n" ;
+
+        else if ( option == "-d" )
+            std::cout << "Decrypt: '" << text << "'\n" ;
+
+        else
+        {
+            std::cout << "Unrecognised command line option '" << option << "'\n";
+            goto usage ;
+        }
+}
