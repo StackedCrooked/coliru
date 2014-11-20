c7cf1223af2b1359c0a320ca5f17a93c6bffc460
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-20 21:18:13 +0000

    Update archive 68

---
+++ Archive2/68/9442f785770c02/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+// Door Karel De Smet 20/11/2014
+
+int main()
+{
+    for (float x = -1; x <=2; x+= 0.2 )
+	{
+		else
+		cout << "X = "<<x << "           X*X = "<<x*x << endl;
+		cin.get();
+	}
+	return 0;
+}
