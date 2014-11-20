76d6275978a89b89de68a5467f91616c6e4a3e69
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-20 15:13:50 +0000

    Update archive 05

---
+++ trunk/Archive2/05/c5e97e3521122e/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+#include <cstdlib>
+ 
+int principal ( ) 
+{ 
+    const  char  * cadena1 =  "42" ; 
+    const  char  * cadena2 =  "3.14159" ; 
+    const  char  * str3 =  "31337" con palabras ; 
+    const  de char  * Str4 =  "palabras y 2" ;
+ 
+    int num1 = std :: atoi ( cadena1 ) ; 
+    int num2 = std :: atoi ( str2 ) ; 
+    int num3 = std :: atoi ( str3 ) ; 
+    int num4 = std :: atoi ( STR4 ) ;
+ 
+    std :: cout  <<  "std :: atoi ( \ " "  << cad1 <<  " \ " ) es "  << num1 <<  " \ n " ; 
+    std :: cout  <<  "std :: atoi ( \ " "  << cad2 <<  " \ " ) es "  << num2 <<  ' \ n ' ; 
+    std :: cout  <<  "std :: atoi ( \ " "  << str3 <<  " \ " ) es "  << num3 <<  ' \ n ' ; 
+    std :: cout  <<  "std :: atoi ( \ " "  << STR4 <<  " \ " ) es "  << num4 <<  " \ n " ; 
+}
