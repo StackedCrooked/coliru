a0b4382a09c343f679c6a08385d907eb84eb9ab3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-20 00:21:16 +0000

    Update archive 53

---
+++ trunk/Archive2/53/bc2a1802337526/cmd.sh
@@ -0,0 +1,3 @@
+g++-4.7 -std=c++11 -Wall -pedantic -pthread main.cpp && ./a.out
+g++-4.8 -std=c++11 -Wall -pedantic -pthread main.cpp && ./a.out
+g++-4.9 -std=c++11 -Wall -pedantic -pthread main.cpp && ./a.out
