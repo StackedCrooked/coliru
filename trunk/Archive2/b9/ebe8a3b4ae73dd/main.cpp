09d3a0a8e2f34cccefb91ffc751bf92fdcd48a4f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-20 13:17:17 +0000

    Update archive b9

---
+++ trunk/Archive2/b9/ebe8a3b4ae73dd/main.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+struct A {
+int a;
+int b;
+};
+ 
+void* p;
+ 
+int* availableStackFrameAddr() {
+    int a;
+    return &a;
+}
+
+__attribute__((noinline)) void foo() {
+    A a;
+    p = &a;
+    printf("%p\n", availableStackFrameAddr());
+}
+
+__attribute__((noinline)) void bar() {
+    A a;
+    p = &a;
+    a.a = 1;
+    printf("%p %d\n", availableStackFrameAddr(), a.a);
+}
+
+__attribute__((noinline)) void baz() {
+    A a;
+    p = &a;
+    a.b = 2;
+    printf("%p %d\n", availableStackFrameAddr(), a.b);
+}
+
+__attribute__((noinline)) void bazz() {
+    A a;
+    p = &a;
+    a.a = 11;
+    a.b = 22;
+    printf("%p %d %d\n", availableStackFrameAddr(), a.a, a.b);
+}
+
+int main() {
+    printf("%p\n", availableStackFrameAddr());
+    foo();
+    bar();
+    baz();
+    bazz();
+    return 0;
+}
