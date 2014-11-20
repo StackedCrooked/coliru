fa4532714d20e7cdd219e2e758ab467827e9b5d8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-20 21:33:17 +0000

    Update archive 79

---
+++ trunk/Archive2/79/33876a584d3f69/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+
+    cout<< "Hello, World." ;
+    cout<< "\n This is pretty nice" ;
+    
+    return 0;
+}
