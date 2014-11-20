c35678e5995074e942fdbf03f67fbe6c2d191c38
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-20 17:39:47 +0000

    Update archive 14

---
+++ Archive2/14/27a0fd0d85061b/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <functional>
+
+class Sorting {
+public:
+    std::vector<std::function<void(int*,int)>> functions;
+    
+private:
+    void bubbleSort(int* a, int n){
+        std::cout << "In bubble sort" << std::endl;
+    }
+    
+public:
+    Sorting(){
+        std::function<void(int*, int)> myMemberFunction = [=](int* a, int n) {
+            this->bubbleSort(a,n);
+        };
+        functions.push_back(myMemberFunction);
+    }
+};
+
+int main()
+{
+    Sorting s;
+    int a[10] = {0,1,2,3,4,5,6,7,8,9};
+    int b = 10;
+    s.functions[0](a,b);
+}
