063ccebd1bd3aea632f017cfc30caa1044b7615d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-20 00:01:49 +0000

    Update archive f2

---
+++ Archive2/f2/8f8421946a75b4/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+int main()
+{
+    int A[9] = {2, 4, 4334, 642, 54, 26, 76, 356, 35}, B[] = {5, 6, 2, 33, 2, 7723, 1, 5, -2};
+    std::sort(A, A+9);std::sort(B,B+9);
+}
