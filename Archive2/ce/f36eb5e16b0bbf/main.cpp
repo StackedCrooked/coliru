88b5b035962297e7c93299608e73dabab80f82f8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-20 15:24:05 +0000

    Update archive ce

---
+++ Archive2/ce/f36eb5e16b0bbf/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+void fun(std::string a="", int b=0, std::string c="", int d=0)
+{
+ //parse used arguments somehow
+}
+
+int main()
+{
+    fun(0, 10);
+    return 0;
+}
