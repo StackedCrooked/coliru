d97a580c46156835e10f0611c75ff7660397d27c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-21 17:19:29 +0000

    Update archive 70

---
+++ Archive2/70/4377b5d3f1ef69/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+class HtmlRover
+{
+public:
+    HtmlRover(const std::string::const_iterator &, const std::string::const_iterator &);
+    ~HtmlRover();
+    // ...
+private:
+    /...
+    std::string::const_iterator _curIter;
+    const std::string::const_iterator _offend;
+    /...
+};
+
+HtmlRover::HtmlRover(const std::string::const_iterator & it1, const std::string::const_iterator & it2):
+    _curIter(it1), _offend(it2)
+{
+}
+
+int main()
+{
+    std::string foo("bar");
+    HtmlRover f(foo.begin(), foo.end());
+    
+    return 0;
+}
