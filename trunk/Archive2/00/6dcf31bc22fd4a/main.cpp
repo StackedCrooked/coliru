d51d44f1a0e533bd08c2fb7d6ae5f59a5b5897e1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-21 08:47:36 +0000

    Update archive 00

---
+++ trunk/Archive2/00/6dcf31bc22fd4a/main.cpp
@@ -0,0 +1,32 @@
+#include <utility>
+#include <tuple>
+#include <iostream>
+
+template <int N, typename T = std::make_index_sequence<N>>
+struct Q;
+
+template <int N, std::size_t... Is>
+struct Q<N, std::index_sequence<Is...>>
+{
+    struct X
+    {
+        virtual int v() = 0;
+    };
+    
+    template<int i>
+    struct Z : X
+    {
+        virtual int v() { return i; }
+    };
+    
+    std::tuple<Z<Is>...> z;
+
+    X * x[N] = { &std::get<Is>(z)... };
+};
+
+int main()
+{
+    Q<4> q;
+    std::cout << q.x[0]->v() << std::endl;
+    std::cout << q.x[1]->v() << std::endl;
+}
