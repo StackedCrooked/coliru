7e1caeb225acb7e6651a6061206adca582d9a8d7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-21 19:12:46 +0000

    Update archive 9b

---
+++ Archive2/9b/3e9a2825c96415/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <set>
+#include <map>
+#include <utility>
+
+int main()
+{
+    std::map< std::string, std::set<std::string> > authors =
+    {
+        { "Charlie Wilson", { "Gone Far", "Clocks Gone", "Fargone Clocks" } },
+        { "James Wilson", { "The Water", "Zebra", "Aardvark" } }
+    };
+
+    authors[ "Charlie Wilson" ].insert( "Clocks Gone Far" ) ;
+    authors[ "Borges" ].insert( "The Garden of Forking Paths" ) ;
+
+    for( const auto& pair : authors )
+    {
+        std::cout << "\nauthor: '" << pair.first << "'\n" ;
+        for( const auto& str : pair.second ) std::cout << "\tbook: '" << str << "'\n" ;
+    }
+}
