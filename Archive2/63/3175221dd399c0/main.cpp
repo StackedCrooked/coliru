211f5629c86e738b8bd352671ce899c40b1fa525
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-21 17:16:42 +0000

    Update archive 63

---
+++ Archive2/63/3175221dd399c0/main.cpp
@@ -0,0 +1,34 @@
+    #include <iostream>
+    #include <string>
+    
+    std::string::size_type my_find( std::string s, const char *cs )
+    {
+        std::string::size_type pos = s.find( cs ) ;
+        std::cout << "position " << cs << " found in complete expression: "
+            << pos << std::endl ;
+        
+        return pos ;
+    }
+    
+    int main()
+    {
+       std::string s = "but I have heard it works even if you don't believe in it" ;
+       std::string copy_s = s ;
+       
+       std::cout << "position of even before s.replace(0, 4, "" ): " 
+             << s.find( "even" ) << std::endl ;
+       std::cout << "position of don't before s.replace(0, 4, "" ): " 
+             << s.find( " don't" ) << std::endl << std::endl;
+       
+       copy_s.replace(0, 4, "" ) ;
+       
+       std::cout << "position of even after s.replace(0, 4, "" ): " 
+             << copy_s.find( "even" ) << std::endl ;
+       std::cout << "position of don't after s.replace(0, 4, "" ): "
+             << copy_s.find( " don't" ) << std::endl << std::endl;
+      
+       s.replace(0, 4, "" ).replace( my_find( s, "even" ) , 4, "only" )
+            .replace( my_find( s, " don't" ), 6, "" );
+       
+       std::cout << "Result: " << s << std::endl ;
+    }
