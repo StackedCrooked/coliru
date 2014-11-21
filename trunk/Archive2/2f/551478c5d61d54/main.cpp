1b6aa63c50f494967a571d35a0434d3cac6aea57
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-21 18:01:11 +0000

    Update archive 2f

---
+++ trunk/Archive2/2f/551478c5d61d54/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <tuple>
+ 
+template <class T>
+void test(T t)
+{
+    int a[std::tuple_size<T>::value]; // can be used at compile time
+    std::cout << static_cast<size_t>(std::tuple_size<T>()) << std::endl;
+ 
+    std::cout << std::tuple_size<T>::value << '\n'; // or at run time
+}
+ 
+int main()
+{
+    test(std::make_tuple(1, 2, 3.14));
+}
