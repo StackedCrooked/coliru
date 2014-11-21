066b9149db6dad51e68a34aeaf4579f91749aa55
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-21 02:29:51 +0000

    Update archive 04

---
+++ Archive2/04/697db827b44808/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+struct Object
+{
+    int a[10]; // change to 1 and the warning disappears
+    Object() {}
+    ~Object() {}
+};
+
+
+void Deconstruct(Object* objects, size_t num_objects)
+{
+    while(num_objects) objects[--num_objects].~Object();
+}
+
+void foo(Object* o)
+{
+    Deconstruct(o, 0x1FFFFFFFFFFFFFFFll);
+}
+
+int main(int argc, char* argv[]) 
+{
+    std::cout << "gcc version: " << __VERSION__ << "\nfinished\n";
+}
