3596fa76c7282465bfc33379a4ddcb2c72cb683c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-21 00:08:38 +0000

    Update archive fd

---
+++ Archive2/fd/31b0761727674c/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#define VARGS_(_10, _9, _8, _7, _6, _5, _4, _3, _2, _1, N, ...) N 
+#define VARGS(...) VARGS_(__VA_ARGS__, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+
+#define CONCAT_(a, b) a##b
+#define CONCAT(a, b) CONCAT_(a, b)
+    
+#define MACRO_2(a, b) std::cout << a << ' ' << b;
+
+#define MACRO_1(a) MACRO_2(a, "test") // Supply default argument
+
+#define MACRO(...) CONCAT(MACRO_, __VA_ARGS__)(__VA_ARGS__)
+
+int main()
+{
+    MACRO(1);
+}
