42a7f0a87ed969ffb31984e90132a859a0be0c47
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-22 14:06:35 +0000

    Update archive 94

---
+++ trunk/Archive2/94/675fc557f6b0c8/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <type_traits>
+
+template<typename Iterator, typename T> 
+bool contains (Iterator begin, Iterator end, T value) {
+    
+    static_assert(std::is_same<typename std::iterator_traits<Iterator>::value_type, T>::value, "Wrong Type");
+    
+     while(begin != end)
+       if(*begin++ == value)
+         return true;
+     return false;
+}
+
+int main(){
+    std::vector<int> v { 41, 42 };
+    std::cout << contains(std::begin(v), std::end(v), 42);   // Good type
+    std::cout << contains(std::begin(v), std::end(v), 43.0); // Wrong type
+};
