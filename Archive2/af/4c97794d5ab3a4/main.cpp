ef249fe09992f88e1b4afc3c47614ef06d5f2e06
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-22 00:17:57 +0000

    Update archive af

---
+++ Archive2/af/4c97794d5ab3a4/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include <boost/any.hpp>
+namespace boost { namespace program_options {
+    template<class charT>
+        void validate(boost::any& v, 
+                const std::vector< std::basic_string<charT> >& xs, 
+                unsigned int* p, int)
+        {
+            std::cout << "validate is redefined" << std::endl;
+            // do something else
+        }
+}}
+
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
+int main()
+{
+    po::options_description cmdLineOptions;
+    po::variables_map vm;
+
+    unsigned int v;
+    const char* args[] = {"tst", "-k", "10"};
+
+    cmdLineOptions.add_options()
+        ("key,k", po::value<unsigned int>(&v)->required())
+      ;
+
+    po::store(po::command_line_parser(sizeof(args) / sizeof(args[0]), args).options(cmdLineOptions).run(), vm);
+    po::notify(vm);
+
+    std::cout << v << '\n';
+
+    return 0;
+}
