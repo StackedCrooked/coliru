c9fca5873c19459ae1044b7503d470479de2e96a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-22 22:46:55 +0000

    Update archive 99

---
+++ trunk/Archive2/99/ca178825e136be/main.cpp
@@ -0,0 +1,32 @@
+#include<functional>
+
+template <typename T>
+struct sList
+{
+    sList() = default;
+    
+    template<typename FunctionType>
+    void DoForEachList(FunctionType pFunc)
+    {
+        for(auto p = this; p; p = p->pNext)
+            pFunc(p);
+    }
+
+    template<typename FunctionType>
+    void DoForEachList(FunctionType pFunc) const
+    {
+        for(auto p = this; p; p = p->pNext)
+            pFunc(p);
+    }
+
+    sList *pNext = nullptr;
+};
+
+int main()
+{
+    const sList<unsigned char> cvobj;
+    cvobj.DoForEachList([] (auto const pCurr) {/* Do something */});
+
+    sList<unsigned char> obj;
+    obj.DoForEachList([] (auto pCurr) {/* Do something */});
+}
