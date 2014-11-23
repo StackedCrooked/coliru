20cd7c5bd1cbcc82c4f9f8972c4c1b96bed4f05b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-23 11:27:48 +0000

    Update archive 68

---
+++ Archive2/68/2a00c83f4cbf74/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector <int> a;
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        a.push_back(x);
+    }
+    return 0;
+}
+
