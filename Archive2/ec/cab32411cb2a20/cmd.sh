180c066e1d8ab291f445dac43367c3e1f510c781
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-23 11:37:37 +0000

    Update archive ec

---
+++ Archive2/ec/cab32411cb2a20/cmd.sh
@@ -0,0 +1,1 @@
+g++ -std=c++11 -Os -pthread main.cpp && ./a.out
---
+++ Archive2/ec/cab32411cb2a20/main.cpp
@@ -0,0 +1,53 @@
+#include <boost/intrusive/slist.hpp>
+
+typedef boost::intrusive::slist_base_hook<> InputBufferSglHook;
+
+class InputBufferSglNode;
+
+class InputBufferSglNode : public InputBufferSglHook
+{
+public :
+
+    InputBufferSglNode(const void* buffer, size_t size); 
+
+    ~InputBufferSglNode() {};
+
+    // list
+    typedef boost::intrusive::slist<InputBufferSglNode, boost::intrusive::cache_last<true>> InputBufferSglList;
+    typedef InputBufferSglList::iterator InputBufferSglIterator;
+
+    // copy 'size_t' bytes from list to target, if list doesn't have enough space, method shall return false
+    static bool copy_buffer_from_list(InputBufferSglList &list, InputBufferSglIterator &iter, size_t &offset_in_node, const InputBufferSglIterator& end, uint8_t *target, size_t size);
+
+    void reset();
+
+public:
+    const void *m_buffer;
+    size_t m_size;
+};
+
+
+// empty list
+const InputBufferSglNode::InputBufferSglList s_EMPTY_INPUT_BUFF_SGL = InputBufferSglNode::InputBufferSglList();
+
+InputBufferSglNode::InputBufferSglNode(const void* buffer, size_t size) :
+    InputBufferSglHook(),
+    m_buffer(buffer),
+    m_size(size)
+{
+    // empty
+}
+
+void InputBufferSglNode::reset()
+{
+    m_buffer = nullptr;
+    m_size = 0;
+}
+
+bool InputBufferSglNode::copy_buffer_from_list(InputBufferSglList &list, InputBufferSglIterator &iter, size_t &offset_in_node, const InputBufferSglIterator& end, uint8_t *target, size_t size)
+{
+    // implementation
+}
+
+int main() {
+}
