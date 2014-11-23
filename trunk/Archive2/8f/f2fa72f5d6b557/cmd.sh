f0c04e39a449b1209e54672e42f66a641d80c895
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-23 18:55:13 +0000

    Update archive 8f

---
+++ trunk/Archive2/8f/f2fa72f5d6b557/cmd.sh
@@ -0,0 +1,1 @@
+g++ --version && g++ -std=c++1y main.cpp -Wall -Wextra -pedantic -Werror
---
+++ trunk/Archive2/8f/f2fa72f5d6b557/main.cpp
@@ -0,0 +1,6 @@
+#include <experimental/optional>
+
+int main()
+{
+    std::experimental::optional<int> foo;
+}
