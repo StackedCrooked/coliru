8976d126bfa4312b436fe1d563e86b57f64725e5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-24 23:34:52 +0000

    Update archive e7

---
+++ Archive2/e7/7af87bd5119715/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+  int i,x,y,k ;
+  cin>>x;
+  cin>>y;
+  cin>>k;
+  for(i=0; i<=k*i; i=i+k)
+    {if(i<y);
+        if (i>x);
+        cout<<i<<endl;
+    }
+system ("pause");
+}
