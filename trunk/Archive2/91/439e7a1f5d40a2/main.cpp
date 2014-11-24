6b96815191343a3ab1f3a536ffb41c8d9bf80186
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-24 22:04:42 +0000

    Update archive 91

---
+++ trunk/Archive2/91/439e7a1f5d40a2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+  int i,x,y,k ;
+  cin>>x;
+  cin>>y;
+  cin>>k;
+  for(i=1; i<=k*i; i=i+k)
+    {if(i<y);
+        if (i>x);
+        cout<<i<<endl;
+    }
+}
