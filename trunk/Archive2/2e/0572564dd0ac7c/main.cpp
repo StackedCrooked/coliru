02f558ecd95d39a3d027a8db8fc9ce49e13fb913
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-25 05:38:28 +0000

    Update archive 2e

---
+++ trunk/Archive2/2e/0572564dd0ac7c/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+struct C{ };
+
+struct B
+{ 
+    B(){ }
+    B(const C&){ }
+};
+
+struct A
+{
+    A(const C&, const C&){ }
+    A(const B&){ }
+};
+
+B b;
+C c;
+A a{c, b};
+
+
+int main(){ }
