519181c053f1567affa72849699da43eb771ab69
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-25 12:56:59 +0000

    Update archive 79

---
+++ trunk/Archive2/79/9b443cb4f1092b/main.cpp
@@ -0,0 +1,31 @@
+// #define BOOST_SPIRIT_USE_PHOENIX_V3
+#define BOOST_RESULT_OF_USE_TR1_WITH_DECLTYPE_FALLBACK
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix.hpp>
+#include <boost/spirit/repository/include/qi_kwd.hpp>
+#include <boost/spirit/repository/include/qi_keywords.hpp>
+#include <iostream>
+#include <string>
+
+namespace qi  = boost::spirit::qi;
+namespace phx = boost::phoenix;
+namespace rqi = boost::spirit::repository::qi;
+
+template <typename Iterator>
+bool difference(Iterator first, Iterator last, double& n) {
+    bool r = boost::spirit::qi::phrase_parse(first, last,
+             //the grammar
+             (rqi::kwd("A") [qi::double_ [phx::ref(n) += qi::_1]] / 
+              rqi::kwd("B") [qi::double_ [phx::ref(n) -= qi::_1]]), boost::spirit::ascii::space);
+
+    if (first != last) return false;
+    return r;
+}
+
+int main() {
+    std::string str("A 1 B 2");
+    double n=0;
+    std::cout << "Full parse: " << difference(str.begin(), str.end(), n) << std::endl;  
+    std::cout << "A - B = " << n <<std::endl;
+    return 0;
+}
