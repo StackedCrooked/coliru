c16223b8f21c2b552950ba5ef7bb1e372084761e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-25 17:53:03 +0000

    Update archive 33

---
+++ Archive2/33/cd9e7a942bc579/main.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <string>
+#include <iostream>
+ 
+struct President
+{
+    std::string name;
+    std::string country;
+    int year;
+ 
+    President(std::string p_name, std::string p_country, int p_year)
+        : name(std::move(p_name)), country(std::move(p_country)), year(p_year)
+    {
+        std::cout << "I am being constructed.\n";
+    }
+    President(President&& other)
+        : name(std::move(other.name)), country(std::move(other.country)), year(other.year)
+    {
+        std::cout << "I am being moved.\n";
+    }
+    President& operator=(const President& other) = default;
+};
+ 
+int main()
+{
+    std::vector<President> elections;
+    std::cout << "emplace_back:\n";
+    elections.emplace_back("Nelson Mandela", "South Africa", 1994);    
+    elections.push_back(President("Melson Nandela", "Aouth Sfrica", 1994));
+ 
+    President& p = elections[0];
+}
