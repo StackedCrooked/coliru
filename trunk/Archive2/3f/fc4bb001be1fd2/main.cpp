2c98c03e8635335ef24151f16d18eef6adec902a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-25 14:47:25 +0000

    Update archive 3f

---
+++ trunk/Archive2/3f/fc4bb001be1fd2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class a
+{
+public:
+    enum b
+    {
+        c,
+        d
+    }
+}
+
+int main()
+{
+    a::b lol;
+    
+    lol = a
+}
