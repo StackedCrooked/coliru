b392386b29e37e1f79c750aa1b56f435b94d9521
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-25 19:40:33 +0000

    Update archive d9

---
+++ trunk/Archive2/d9/89cbc2b1af4109/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+string subfunction(string word)
+{
+    cout << word << endl;
+}
+
+int main()
+{
+    cout << "Enter a number: "; // ask user for a number
+    int x;
+    cin >> x; // read number from console and store it in x
+    cout << "You entered " << x << endl;
+    cout << "\n" << "Enter 'a': ";
+    string y;
+    cin >> y;
+    if (y=="a"){
+       cout << "You typed 'a'!!!";
+       int z;
+       cin >> z;
+       return 0;
+    }
+    else {
+         cout << "You didn't type 'a'!!!" << endl;
+    }
+    string word;
+    subfunction("abc");
+    
+    return main();
+}
