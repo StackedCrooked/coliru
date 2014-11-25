8cdfb5541f2c187ee93b0b34387bc989ff625602
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-25 21:43:57 +0000

    Update archive c7

---
+++ trunk/Archive2/c7/b58523909d0fea/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+struct B;
+struct A
+{
+    A(B);
+    A(A const&) {std::cout << "COPY" << std::endl;}
+    A() = default;
+};
+
+struct B { operator A() {std::cerr << "operator A" << std::endl; return {};} };
+
+A::A(B) {std::cerr << "A::A(B)" << std::endl;}
+
+int main()
+{
+    A(B{});
+	(A)B{};
+}
