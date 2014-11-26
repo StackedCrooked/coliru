21945d6a9038d159efda2ea2262749418e959950
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-26 14:30:58 +0000

    Update archive 65

---
+++ trunk/Archive2/65/85ba82e78381be/main.cpp
@@ -0,0 +1,22 @@
+#include <regex>
+#include <string>
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+string str = "    1     10      0       11        3       11";
+regex rx("\\@d");
+
+vector<int> index_matches; // results saved here 
+                           // (should be {2, 8}, but always get only {2})
+   
+for(auto it = std::sregex_iterator(str.begin(), str.end(), rx); it != std::sregex_iterator(); ++it)
+    index_matches.push_back(it->position());
+
+for(int n: index_matches)
+{
+    std::cout << n << '\n';
+}
+}
