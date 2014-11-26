8b0773190df1807737e968006b971e4d3d09f398
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-26 19:51:13 +0000

    Update archive 8e

---
+++ Archive2/8e/fd4793f8d0ebe5/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+struct str
+{
+    ~str()
+    {
+        cout << "str::~str()" << endl;
+    } 
+};
+
+str && Funcf(str arg)
+{
+    return (str&&)arg;
+}
+
+int main()
+{
+    
+    std::cout << "Current Expression_0" << endl << (Funcf(str()), "Current Expression, comma operator") << endl << "Current Expression_1" << std::endl;
+}
