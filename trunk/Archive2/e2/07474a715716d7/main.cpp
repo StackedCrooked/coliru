8071c2bb42ad8f35a83bed75765899842609e9e3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-27 02:39:55 +0000

    Update archive e2

---
+++ trunk/Archive2/e2/07474a715716d7/main.cpp
@@ -0,0 +1,48 @@
+struct encoder_t {
+    
+} const encoder = {};
+
+struct validator_t {
+    
+} const validator = {};
+
+struct test {
+// none of these work
+#if 0
+    // This says it can't deduce the types, so it's understandable
+    // that it doesn't work (albeit I wish it could deduce back into the type signature)
+    template <typename Range, typename Encoding, typename Validation>
+    test ( Range&& range, Encoding&& encoding = encoder, Validation&& validation = validator ) {
+        
+    }
+#elif 0
+    // I can default the type now, since that was the error from the above,
+    // but now it complains that the type provided doesn't match
+    // the type for the encoder
+    template <typename Range, typename Encoding = encoder_t, typename Validation = validator_t>
+    test ( Range&& range, Encoding&& encoding = encoder, Validation&& validation = validator ) {
+        
+    }
+#elif 0
+    // okay, so let's try decltype...
+    // but now it doesn't work because it can't bind an lvalue to the actual type here! ( const type&& <-- wat )
+    template <typename Range, typename Encoding = decltype( encoder ), typename Validation = decltype( validator )>
+    test ( Range&& range, Encoding&& encoding = encoder, Validation&& validation = validator ) {
+        
+    }
+#else
+    // Works! But the syntax is strange... potential ramifications/deduction mishaps?
+    // Is this the "proper" way to default these arguments?
+    template <typename Range, typename Encoding = encoder_t, typename Validation = validator_t>
+    test ( Range&& range, Encoding&& encoding = Encoding(), Validation&& validation = Validation() ) {
+        
+    }
+#endif
+
+};
+
+int main() {
+    
+    test( "woof" );
+    
+}
