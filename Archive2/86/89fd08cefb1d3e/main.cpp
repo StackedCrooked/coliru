ea439894b4f5e318991cf1caed10f6b07445b40f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-27 23:31:29 +0000

    Update archive 86

---
+++ Archive2/86/89fd08cefb1d3e/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <memory>
+ 
+struct Foo
+{
+    Foo()      { std::cout << "Foo::Foo\n";  }
+    ~Foo()     { std::cout << "Foo::~Foo\n"; }
+    void bar() { std::cout << "Foo::bar\n";  }
+};
+ 
+void f(const Foo &)
+{
+    std::cout << "f(const Foo&)\n";
+}
+ 
+int main()
+{
+    std::unique_ptr<Foo> p1(new Foo);  // p1 owns Foo
+    if (p1) p1->bar();
+ 
+    {
+        std::unique_ptr<Foo> p2(std::move(p1));  // now p2 owns Foo
+        f(*p2);
+ 
+        p1 = std::move(p2);  // ownership returns to p1
+        std::cout << "destroying p2...\n";
+    }
+ 
+    if (p1) p1->bar();
+ 
+    // Foo instance is destroyed when p1 goes out of scope
+}
