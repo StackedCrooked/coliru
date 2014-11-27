f45336b7343966a473bfb2933aa1ad88a796b711
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-27 08:49:26 +0000

    Update archive 64

---
+++ Archive2/64/6cdd0771665ca3/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+template <typename T>
+decltype(auto) ok(T i) {
+    auto x = i + 1;
+    return x;
+}
+
+template <typename T>
+decltype(auto) undefined_behavior(T i) {
+    auto x = i + 1;
+    return (x);
+}
+
+int main()
+{
+    std::cout<<ok(41)<<std::endl;
+    std::cout<<undefined_behavior(32)<<std::endl;
+}
