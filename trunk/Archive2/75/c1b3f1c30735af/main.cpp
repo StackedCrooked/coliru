73625b2d2731f8607752def5538cf271b85f3436
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-28 06:20:32 +0000

    Update archive 75

---
+++ trunk/Archive2/75/c1b3f1c30735af/main.cpp
@@ -0,0 +1,12 @@
+    #include <iostream>
+ 
+    struct B{ };
+    
+    struct A
+    {
+        A(int, B){ std::cout << "A(int, B())" << std::endl; }
+    };
+    
+    A a({4,B()});
+    
+    int main(){ }
