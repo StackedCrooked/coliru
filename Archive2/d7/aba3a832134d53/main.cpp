d69eebdf80678cef50f216475cb755c26c6b3177
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-28 21:26:53 +0000

    Update archive d7

---
+++ Archive2/d7/aba3a832134d53/main.cpp
@@ -0,0 +1,68 @@
+#include <iomanip>
+#include <iostream>
+#include <type_traits>
+
+
+struct Flagger
+{
+    Flagger(std::ostream& os) : os(os), flags(os.flags()) {}
+    Flagger(const Flagger&) = delete;    
+    Flagger& operator=(const Flagger&) = delete;
+    ~Flagger() { os.flags(flags); }
+    std::ostream& os;
+    std::ios_base::fmtflags flags;
+};
+
+
+struct Byte
+{
+    Byte() = default;
+    Byte(unsigned char value) : value_(value) {}
+    
+    friend std::ostream& operator<<(std::ostream& os, Byte b)
+    {
+        return os << static_cast<int>(b.value_);
+    }
+    
+    unsigned char value_;
+};
+
+
+struct MAC
+{
+    friend std::ostream& operator<<(std::ostream& os, MAC mac)
+    {   
+        Flagger flagger(os);
+        
+        for (auto& b : mac.bytes)
+        {
+            os << (&b == mac.bytes ? "" : ":") << std::hex << std::setw(2) << std::setfill('0') << b;
+        }
+        return os;
+    }
+    
+    Byte bytes[6];
+};
+
+
+struct IP
+{
+    friend std::ostream& operator<<(std::ostream& os, IP ip)
+    {
+        for (auto& b : ip.bytes)
+        {
+            os << (&b == ip.bytes ? "" : ".") << b;
+        }
+        return os;
+    }
+    
+    Byte bytes[4];
+};
+
+
+int main()
+{
+    std::cout << MAC{ 0x00, 0xff, 0x23, 0x00, 0x00, 0x01 } << std::endl;
+    std::cout << IP{ 10, 0, 0, 1 } << std::endl;
+}
+
