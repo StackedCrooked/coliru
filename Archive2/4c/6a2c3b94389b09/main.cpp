58db4f4bdc0fc94ec09dc0ecbd59506450c60658
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-29 01:48:14 +0000

    Update archive 4c

---
+++ Archive2/4c/6a2c3b94389b09/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+struct Literal
+{
+    int i;
+	constexpr Literal(int i) : i(i) {}
+	~Literal() {std::cout << i << std::endl;}
+};
+
+struct NonLiteral
+{
+	NonLiteral() {std::cout << "NonLiteral()" << std::endl;}
+	~NonLiteral() {std::cout << "~NonLiteral" << std::endl;}
+};
+
+Literal l1(1);
+NonLiteral nonliteral1;
+Literal l2(2);
+NonLiteral nonliteral2;
+Literal l3(3);
+
+
+int main()
+{
+}
