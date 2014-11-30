b83b0f2347c17c2f3be42d4f8b6665572b505363
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-30 19:11:34 +0000

    Update archive 55

---
+++ Archive2/55/8135c969adb308/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+template <typename CharT,
+          typename Traits = std::char_traits<CharT>>
+class SplittingStreambuf : public std::basic_streambuf<CharT, Traits>
+{
+    using buffer_type = std::basic_streambuf<CharT, Traits>;
+	std::vector<buffer_type*> others;
+
+protected:
+
+	typename Traits::int_type overflow(typename Traits::int_type c)
+	{
+		if (Traits::eq_int_type(Traits::eof(), c))
+			return Traits::not_eof(c);
+
+		bool good = true;
+		for (auto buf : others)
+			good = good && not Traits::eq_int_type(buf->sputc(c), Traits::eof());
+
+		return good? Traits::not_eof(c) : Traits::eof();
+	}
+
+public:
+
+	template <typename InputIterator>
+	SplittingStreambuf( InputIterator first, InputIterator last ) :
+		others(first, last) {}
+
+	SplittingStreambuf( std::initializer_list<buffer_type*> init ) :
+		others(init) {}
+
+	SplittingStreambuf() = default;
+	SplittingStreambuf(SplittingStreambuf&&) = default;
+	SplittingStreambuf& operator=(SplittingStreambuf&&) = default;
+};
+
+int main()
+{
+	SplittingStreambuf<char> buf{std::cout.rdbuf(), std::cout.rdbuf()};
+	auto original = std::cout.rdbuf(&buf);
+
+	std::cout << "Hello world!";
+
+	std::cout.rdbuf(original); // Restore old streambuffer.
+}
