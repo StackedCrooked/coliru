545cb801d6c7bf947b57976f9b11493f0640ea64
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-30 19:12:59 +0000

    Update archive 34

---
+++ Archive2/34/716611e3685004/main.cpp
@@ -0,0 +1,18 @@
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/qi_match.hpp>
+
+int main() {
+    std::vector<std::string> three_wordses;
+    {
+        using namespace boost::spirit::qi;
+
+        rule<boost::spirit::istream_iterator, std::string()> wordses =
+            repeat(1,3)[+graph >> *(*omit[eol] >> space)];
+
+        std::cin >> std::noskipws >> phrase_match(+wordses, space, three_wordses);
+        std::cout << "three_wordses: " << three_wordses.size() << "\n";
+    }
+
+    for(auto s : three_wordses)
+        std::cout << s << "\n";
+}
