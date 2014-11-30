219c441b1ec1e556b5d478ed64b67151f20e19bf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-30 23:09:29 +0000

    Update archive cc

---
+++ trunk/Archive2/cc/8077441420c501/cmd.sh
@@ -0,0 +1,1 @@
+java main.cpp && javac Ideone.class
---
+++ trunk/Archive2/cc/8077441420c501/main.cpp
@@ -0,0 +1,15 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		String x;
+		if (x == null) System.out.println("Test");
+	}
+}
