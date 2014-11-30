72b25652d308a3c3182891604ac238601544435a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-11-30 19:10:27 +0000

    Update archive 7e

---
+++ trunk/Archive2/7e/9b448c4a10c86f/cmd.sh
@@ -0,0 +1,5 @@
+clang++ -std=c++0x -Os -Werror -Wall main.cpp && ./a.out <<INPUT
+4 3 1
+hello hai moo
+(i need to be able to enter3 more lines of similar words,but I cant the program terminates)
+INPUT
