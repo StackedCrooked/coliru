72ce15c6d720254236fbeaa4063664c143d0e2bf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-01 21:55:28 +0000

    Update archive 97

---
+++ Archive2/97/25178e225c4011/main.cpp
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+#include <boost/logic/tribool.hpp>
+
+struct B
+{
+  boost::tribool boo;
+
+  void bug ()
+  {
+    bool tmp = indeterminate (boo);
+    std::cout << "tmp = " << std::boolalpha << tmp << "\n";
+    if (tmp && false == (boo = should_not_be_called ())) {;}
+  }
+
+  bool should_not_be_called () const
+  {
+    std::cout << "BUG, wrong call\n";
+    abort ();
+  }
+};
+
+int main ()
+{
+  B a;
+  a.bug ();
+}
