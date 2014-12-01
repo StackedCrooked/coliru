29fbf3c35796d1a55796fcc1060f400bf654c7e1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-01 05:42:32 +0000

    Update archive 32

---
+++ trunk/Archive2/32/ffafa6b20de72a/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <initializer_list>
+
+struct A
+{
+    bool operator+(A a);
+};
+
+bool operator+(A&, A)
+{
+    return false;
+}
+
+int main()
+{ 
+    A a, b;
+    std::cout << a + b << std::endl;
+}
