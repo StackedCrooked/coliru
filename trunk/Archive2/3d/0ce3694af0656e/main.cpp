39a93bbb75b9ace63f1ebf97f66eb7ddd58ae517
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-01 23:31:22 +0000

    Update archive 3d

---
+++ trunk/Archive2/3d/0ce3694af0656e/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <map>
+#include <boost/current_function.hpp>
+#include <boost/foreach.hpp>
+
+class A{
+    public:
+    void myfunc1(int a, int b)
+    {
+        calls[BOOST_CURRENT_FUNCTION]++;
+    }
+    void myfunc2()
+    {
+        calls[BOOST_CURRENT_FUNCTION]++;
+    }
+    std::string report_calls()
+    {
+        std::stringstream sstr;
+        typedef std::pair<std::string,int> map_entry;
+        BOOST_FOREACH( map_entry entry, calls)
+        {
+            sstr << entry.first << "=" << entry.second << std::endl;
+        }
+        return sstr.str();
+    }
+    std::map<std::string,int> calls;
+};
+
+int main()
+{
+    A obj;
+    obj.myfunc1(0,0);
+    obj.myfunc1(0,0);
+    obj.myfunc2();
+    std::cout<<obj.report_calls();
+}
