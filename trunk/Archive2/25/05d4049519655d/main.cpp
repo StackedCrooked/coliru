e5bf2c967bdd6cce2cf5eb2f9b302470c85525f9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-01 16:45:52 +0000

    Update archive 25

---
+++ trunk/Archive2/25/05d4049519655d/main.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <type_traits>
+
+template<typename E>
+typename std::enable_if<std::is_enum<E>::value,typename std::underlying_type<E>::type>::type
+to_integral(E e) {
+  return static_cast<typename std::underlying_type<E>::type>(std::move(e));
+}
+template<typename E>
+typename std::enable_if<!std::is_enum<E>::value&&std::is_integral<E>::value,E>::type
+to_integral(E e) {
+  return std::move(e);
+}
+
+enum class MyEnum : int { A = 5 };
+
+int main()
+{
+  auto a = to_integral(42);
+  auto b = to_integral(MyEnum::A);
+  printf("%d\n", a);
+  printf("%d\n", b);
+}
