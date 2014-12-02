8097f34f9c600c6b05a75335ff6e2db880264fae
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-02 16:48:48 +0000

    Update archive de

---
+++ Archive2/de/c02c893213236c/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <functional>
+#include <memory>
+#include <map>
+
+struct A {};
+struct B {};
+struct C {
+  std::shared_ptr<A> calc(std::shared_ptr<B> b) { return nullptr; }
+};
+
+int main() {
+  C c;
+  std::function<std::shared_ptr<A>(std::shared_ptr<B>)> x = std::bind(std::mem_fn(&C::calc), &c, std::placeholders::_1);
+}
