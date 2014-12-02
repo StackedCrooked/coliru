2f6b6b27240b751fca994ceec0121c365c6069b6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-02 06:01:10 +0000

    Update archive 56

---
+++ Archive2/56/bb0c352f000c0c/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <boost/shared_ptr.hpp>
+
+struct A
+{
+};
+
+template <typename T>
+void F1( boost::shared_ptr< T >& arg1 )
+{
+    std::cout << "f1 called" << std::endl ;
+}
+ 
+int main() {
+    boost::shared_ptr< A > a1( new A() ) ;
+	boost::shared_ptr< const A > a2( new A() ) ;
+	boost::shared_ptr< const A > a3 = a1 ;
+ 
+	F1( a2 ) ;
+	F1( a1 ) ;
+    F1( a3 ) ;
+	return 0;
+}
