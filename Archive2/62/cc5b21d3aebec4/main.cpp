286e87530555b520314afd299cebe5dddedac78d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-02 14:46:20 +0000

    Update archive 62

---
+++ Archive2/62/cc5b21d3aebec4/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <random>
+#include <cstdint>
+#include <ctime>
+
+struct mt19937_16 : private std::mt19937
+{
+    using result_type = std::uint16_t ;
+    using std::mt19937::mt19937 ;
+    using std::mt19937::seed ;
+    using std::mt19937::discard ;
+    // more using ... (if required)
+
+    static constexpr result_type min() { return 0 ; }
+    static constexpr result_type max() { return 0xffff ; }
+
+    result_type operator()() { return narrow( base() ) ; }
+
+    private:
+        std::mt19937& base() { return *this ; }
+        const std::mt19937& base() const { return *this ; }
+
+        static std::uniform_int_distribution<std::uint16_t> narrow ;
+
+    friend bool operator== ( const mt19937_16& a, const mt19937_16& b )
+    { return a.base() == b ; }
+    friend bool operator!= ( const mt19937_16& a, const mt19937_16& b )
+    { return !( a == b ) ; }
+
+    template< typename C, typename T >
+    friend std::basic_ostream<C,T>& operator<< ( std::basic_ostream<C,T>& stm, const mt19937_16& mt16 )
+    { return  stm << mt16.base() ; }
+
+    template< typename C, typename T >
+    friend std::basic_istream<C,T>& operator>> ( std::basic_istream<C,T>& stm, mt19937_16& mt16 )
+    { return  stm >> mt16.base() ; }
+};
+
+std::uniform_int_distribution<std::uint16_t> mt19937_16::narrow ; // in the implementation file (cpp)
+
+int main()
+{
+    mt19937_16 twister16 ;
+
+    for( int i = 0 ; i < 10 ; ++i ) std::cout << twister16() << '\n' ;
+
+    std::normal_distribution<double> normal( 100.0, 25.0 ) ;
+    for( int i = 0 ; i < 10 ; ++i ) std::cout << normal(twister16) << '\n' ;
+}
