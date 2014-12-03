48fe522ff243cbb6f1978d6fd905537f2afca05c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-03 19:06:15 +0000

    Update archive 36

---
+++ Archive2/36/17502cb7c678ec/main.cpp
@@ -0,0 +1,13 @@
+int main (){
+    
+	int f[10] = {0, -1, -2, -3, -4, -5, -6, -7, -8, -9};
+	int values[2] = {7,3};
+	int *pos;
+	int x = 0;
+	while (x <10){
+		pos = f + x;
+		x+=2;
+		*pos = (int)(&x);
+	
+			}
+}
