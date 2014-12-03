46994f46a41fddf59d26fe22a2e7d17935dd65f4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-03 13:06:09 +0000

    Update archive 07

---
+++ trunk/Archive2/07/35f521ffa8d214/main.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+
+int volatile dummy;
+
+class A {
+public:
+    void yell() { dummy++; }
+};
+class B : virtual public A {};
+class C : virtual public A {};
+class D : public B, public C {};
+
+int bar()
+{
+    auto hack = new D;
+    hack->yell();
+    return 1;
+}
+
