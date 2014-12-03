e62fccfde772efac1458aed05bfacff9cbee7e42
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-03 09:09:27 +0000

    Update archive a6

---
+++ trunk/Archive2/a6/b178afdbe0248b/main.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+
+void print(std::vector<std::vector<float>> const& vec)
+{
+    for(auto const& row : vec) {
+        for(auto const& elem : row) {
+            std::cout << elem << ' ';
+        }
+        std::cout << '\n';
+    }
+}
+
+int main()
+{
+    std::vector<std::vector<float>> vec{{1,2,3},
+                                        {4,5,6},
+                                        {7,8,9}};
+                                        
+    print(vec);
+    
+    // delete column 1
+    std::for_each(vec.begin(), vec.end(), 
+        [](std::vector<float>& row) {
+            row.erase(std::next(row.begin()));
+        });
+        
+    print(vec);
+}
