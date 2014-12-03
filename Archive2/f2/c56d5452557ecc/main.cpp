44fe2c95d6e20bf3de9ffa5468bb4d8c70a09e7f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-03 19:58:54 +0000

    Update archive f2

---
+++ Archive2/f2/c56d5452557ecc/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <type_traits>
+ 
+class A {};
+ 
+int main() 
+{
+    std::cout << std::boolalpha;
+    std::cout << std::is_floating_point<A>::value << '\n';
+    std::cout << std::is_floating_point<float>::value << '\n';
+    std::cout << std::is_floating_point<int>::value << '\n';
+}
