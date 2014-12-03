af178206b20b7a80283c861208aee71ae24af1e2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-03 19:49:43 +0000

    Update archive e7

---
+++ trunk/Archive2/e7/9a3dbc6d30b6fe/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    
+	int f[1];
+    
+    std::cout << f << std::endl;
+    std::cout << &f[0] << std::endl;
+    std::cout << f+1 << std::endl;
+    std::cout << &f[1] << std::endl;
+	
+}
