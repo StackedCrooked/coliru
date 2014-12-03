3f9d166cd5adaf9edb2478b8010ac4bc6b0b0b5a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-03 09:12:25 +0000

    Update archive dc

---
+++ trunk/Archive2/dc/99225a670bbef3/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <random>
+int main()
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+ 
+    // if an event occurs 4 times a minute on average
+    // how often is it that it occurs n times in one minute?
+    std::poisson_distribution<> d(4);
+ 
+    std::map<int, int> hist;
+    for(int n=0; n<10000; ++n) {
+        ++hist[d(gen)];
+    }
+    for(auto p : hist) {
+        std::cout << p.first <<
+                ' ' << std::string(p.second/100, '*') << '\n';
+    }
+}
