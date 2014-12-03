3f86b2d1f9ccd093efe71d944eb01814afef3c4f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-03 19:33:47 +0000

    Update archive 3a

---
+++ Archive2/3a/9b1ec9a72b076a/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Empty {};
+struct Bit {unsigned bit:1; };
+
+int main()
+{
+    Empty e;
+    int q[10];
+    vector <int> v = {1,2,3,4,5,6,7,8,9,10};
+    
+    cout << "size of empty class: "     << sizeof e        << '\n'
+              << "size of pointer : "        << sizeof &e       << '\n'
+              << "sizeof int[10]: " << sizeof(int[10]) << '\n'
+              << "sizeof q: " << sizeof(q) << endl
+              << "sizeof v: " << sizeof(v) << endl    
+              << "sizeof v: " << sizeof(v[1])*v.size() << endl    
+              
+              << "alignof q: " << alignof(vector <int>) << endl            
+              << "alignof v: " << alignof(int [10]) << endl            
+           
+              ;
+}
