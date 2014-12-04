a776265058ae926dfae3a46d1aadaac35aee48c3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-04 18:58:51 +0000

    Update archive a6

---
+++ trunk/Archive2/a6/ff0d9e2ec87ffc/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+
+int main() {
+    std::string text("abcdef");
+    
+    std::cout << text.find("", 0) << std::endl;
+    std::cout << text.find("", 1) << std::endl;
+    std::cout << text.find("", -1) << std::endl;
+    
+    text.clear();
+    
+    std::cout << text.find("", 0) << std::endl;
+    std::cout << text.find("", 1) << std::endl;
+    std::cout << text.find("", -1) << std::endl;    
+}
