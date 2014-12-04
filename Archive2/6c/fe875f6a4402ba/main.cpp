04956c8a3db4622ac683eb936048cd6cbf96f542
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-04 13:06:27 +0000

    Update archive 6c

---
+++ Archive2/6c/fe875f6a4402ba/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <memory>
+#include <vector>
+#include <functional>
+#include <exception>
+
+
+class foo {
+public:
+
+    foo() : x_(0) { std::cout << "foo constructor\n"; }
+    foo(foo& c) = delete; //: x_(c.x_) { std::cout << "foo copy- constructor\n"; }
+    
+    void duh() {
+        std::cout << "duhded " << x_ << "\n";
+    }
+    
+    foo& operator=(foo const& c) {
+        std::cout << "foo operator=\n";
+        x_ = c.x_;
+        return *this;
+    }
+    
+protected:
+    int x_;
+};
+
+class bar : public foo {
+public:
+    
+    bar(int x) { foo::x_=x; std::cout << "bar constructor\n"; }
+    bar(bar& c) { std::cout << "bar copy- constructor\n"; }
+    
+    bar& operator=(bar const& c) {
+        foo::operator=(c);
+        std::cout << "bar operator=\n";
+        return *this;
+    }
+};
+
+int main() {    
+    
+    foo f;
+    bar b(123);
+    
+    f.duh();
+    b.duh();
+    
+    f = *static_cast<foo*>(&b);
+    f.duh();
+}
