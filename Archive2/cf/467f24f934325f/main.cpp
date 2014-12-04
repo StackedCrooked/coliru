048366fd67acd827bf3b6ca63dcacae39dc5153d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-04 09:23:12 +0000

    Update archive cf

---
+++ Archive2/cf/467f24f934325f/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int x;
+    int y;
+    int z;
+	cout<<"Welcome to A Basic Division Program by Chris"<<endl;
+	cout<<"Please enter the dividend:";
+	cin>>x;
+	cout<<endl<<"Please enter the Divisor";
+	cin>>y;
+	z=x/y;
+	cout<<endl<<x<<" divided by "<<y<<" equals "<<z;
+	system("pause");
+	return 0;
+}
