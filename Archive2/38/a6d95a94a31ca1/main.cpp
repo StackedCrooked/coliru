3a5a987c2cc5aa20a69a610f69759f5d297647c0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-04 04:44:56 +0000

    Update archive 38

---
+++ Archive2/38/a6d95a94a31ca1/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+
+#define TRACE() std::cout << "  " << __PRETTY_FUNCTION__ << std::endl
+
+
+struct Item
+{
+    Item() { TRACE(); }
+    ~Item() { TRACE(); }
+
+    Item(const Item&) { TRACE(); }
+    Item& operator=(const Item&) { TRACE();  return *this; }
+    
+    Item(Item&&) { TRACE(); }
+    Item& operator=(Item&&) { TRACE(); return *this; }
+};
+
+
+Item make_item() { return {}; }
+
+
+void func(Item item) { }
+
+
+int main()
+{
+    std::cout << "\nimplicit: " << std::endl;
+    func(Item());
+    
+    std::cout << "\nwith std::move: " << std::endl;
+    func(std::move(Item()));
+}
+
