426ea42d7e4605dd6e72560d939e8ab7d93faee7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-04 21:06:00 +0000

    Update archive 13

---
+++ trunk/Archive2/13/bd9c6f44a338cb/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <future>
+#include <thread>
+#include <chrono>
+
+int main()
+{
+    std::cout << "Main thread id: " << std::this_thread::get_id() << std::endl;
+
+    std::vector<std::future<void>> futures;
+    for (int i = 0; i < 10; ++i)
+    {
+        auto fut = std::async([i]
+        {
+            std::this_thread::sleep_for(std::chrono::seconds(2));
+            std::cout << std::this_thread::get_id() << " ";
+        });
+        futures.push_back(std::move(fut));
+    }
+} 
