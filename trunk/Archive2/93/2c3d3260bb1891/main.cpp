3d718e73961e4ced90c0d314d75977997f9355a7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-04 21:59:21 +0000

    Update archive 93

---
+++ trunk/Archive2/93/2c3d3260bb1891/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <future>
+#include <thread>
+#include <chrono>
+#include <vector>
+
+int main()
+{
+    std::cout << "Main thread id: " << std::this_thread::get_id() << std::endl;
+    std::cout << "Hardware concurrency: " << std::thread::hardware_concurrency() << std::endl;
+
+    std::vector<std::future<void>> futures;
+    for (int i = 0; i < 10; ++i)
+    {
+        auto fut = std::async([i]
+        {
+            std::this_thread::sleep_for(std::chrono::seconds(2));
+            std::cout << std::this_thread::get_id() << " ";
+        });
+        futures.push_back(std::move(fut));
+    }
+}
