9e6217edcf482650de4bc4408bef50fff51de6f2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-04 22:04:33 +0000

    Update archive 30

---
+++ Archive2/30/f5a14df66dd615/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+template <typename T>
+int P(T num) {
+    return num == std::string::npos ? -1 : num;
+}
+
+
+int main() {
+    std::string text("abcdef");
+    
+    std::cout << P(text.rfind("", -1)) << " " << P(text.find_last_of("", -1)) << std::endl;
+    std::cout << P(text.rfind("", 1)) << " " << P(text.find_last_of("", 1)) << std::endl;
+    std::cout << P(text.rfind("", 0)) << " " << P(text.find_last_of("", 0)) << std::endl;
+    
+    text.clear();
+    
+    std::cout << P(text.rfind("", -1)) << " " << P(text.find_last_of("", -1)) << std::endl;
+    std::cout << P(text.rfind("", 1)) << " " << P(text.find_last_of("", 1)) << std::endl;
+    std::cout << P(text.rfind("", 0)) << " " << P(text.find_last_of("", 0)) << std::endl;
+    
+    std::cout << P(text.rfind("a", -1)) << " " << P(text.find_last_of("", -1)) << std::endl;
+}
