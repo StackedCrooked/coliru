a81b036a429a8c83fe12be87b8b63963d8acbe43
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-04 21:50:57 +0000

    Update archive e2

---
+++ trunk/Archive2/e2/a2c1fd9385b32d/main.cpp
@@ -0,0 +1,48 @@
+#include <functional>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+// Iterative DFS for a binary tree.
+// BinaryTree must be a (smart) pointer to a struct with 'value', 'left', and 'right'
+// members. 
+template <typename BinaryTree> void dfs(BinaryTree t, function<void(decltype(t->value))> fVisit)
+{
+    stack<BinaryTree> todo;
+
+    for ( ; ; )
+    {
+        while (t != nullptr)
+        {
+            todo.push(t);
+            t = t->left;
+        }
+
+        if (todo.empty()) break;
+
+        fVisit(todo.top()->value);
+        t = todo.top()->right;
+        todo.pop();
+    }
+}
+
+struct Node
+{
+    Node(char v, Node *l = nullptr, Node *r = nullptr) : value(v), left(l), right(r) {}
+
+    char value;
+    Node *left;
+    Node *right;
+};
+
+int main()
+{
+    dfs(static_cast<Node*>(nullptr), [](char c){ cout << c; });
+    cout << endl;
+
+    dfs(new Node('x'), [](char c){ cout << c; });
+    cout << endl;
+
+    dfs(new Node('x', new Node('a', new Node('1'), new Node('2')), new Node('b', new Node('3'), new Node('4'))), [](char c){ cout << c; });
+}
