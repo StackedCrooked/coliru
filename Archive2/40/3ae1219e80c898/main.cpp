61ff669700f277969499ad63761b10dbb0fc0771
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-04 19:08:36 +0000

    Update archive 40

---
+++ Archive2/40/3ae1219e80c898/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+
+int main() {
+    std::string text("abcdef");
+    
+    std::cout << text.rfind("", -1) << std::endl;
+    std::cout << text.rfind("", 1) << std::endl;
+    std::cout << text.rfind("", 0) << std::endl;
+    
+    text.clear();
+    
+    std::cout << text.rfind("", -1) << std::endl;
+    std::cout << text.rfind("", 1) << std::endl;
+    std::cout << text.rfind("", 0) << std::endl;
+    
+    std::cout << text.rfind("a", -1) << std::endl;
+}
