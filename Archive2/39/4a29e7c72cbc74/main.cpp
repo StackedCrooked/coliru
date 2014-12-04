74ab93fd533938c2debb908f46927c39fbcd2d97
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-04 21:35:09 +0000

    Update archive 39

---
+++ Archive2/39/4a29e7c72cbc74/main.cpp
@@ -0,0 +1,12 @@
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+printTree :: (Show a) => Tree a -> String
+printTree (Node a left right) = printTreeAux (Node a left right) 0
+
+printTreeAux :: (Show a) => Tree a -> Int -> String
+printTreeAux EmptyTree _ = ""
+printTreeAux (Node a left right) depth = (replicate (6 * depth) ' ') ++ show a ++ "\n" ++ (printTreeAux left (depth + 1)) ++ "\n" ++ (printTreeAux right (depth + 1))
+
+main :: IO ()
+main = do
+    putStrLn "OK"
