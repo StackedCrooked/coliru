0cefbeb012f7aa444ecae9b69e030e9700b9a7d0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-04 04:03:11 +0000

    Update archive 5c

---
+++ trunk/Archive2/5c/5bf982e95f82c3/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+
+#define TRACE() std::cout << "  " << __PRETTY_FUNCTION__ << std::endl
+
+
+struct Item
+{
+    Item() { TRACE(); }
+    ~Item() { TRACE(); }
+
+    Item(const Item&) { TRACE(); }
+    Item& operator=(const Item&) { TRACE();  return *this; }
+    
+    Item(Item&&) { TRACE(); }
+    Item& operator=(Item&&) { TRACE(); return *this; }
+};
+
+
+Item make_item() { return {}; }
+
+
+void func(Item item) { }
+
+
+int main()
+{
+    std::cout << "implicit: " << std::endl;
+    func(Item());
+    
+    std::cout << "\nwith std::move: " << std::endl;
+    func(std::move(Item()));
+    
+    // => Don't help the compiler!
+    // (See also http://channel9.msdn.com/Events/GoingNative/2013/Don-t-Help-the-Compiler)
+}
+
