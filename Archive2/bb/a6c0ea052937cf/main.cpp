deaca679247c13441bcd6721dc6276b3b498313f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-05 20:20:02 +0000

    Update archive bb

---
+++ Archive2/bb/a6c0ea052937cf/main.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <iostream>
+
+template <typename Container, typename OutputIt, typename UnaryPredicate>
+auto remove_and_copy_if(Container& c, OutputIt d_first, UnaryPredicate pred)
+    -> decltype(c.begin())
+{
+    auto it = std::begin(c);
+    if (it != std::end(c))
+    {
+        for (it = std::begin(c); ++it != std::end(c); )
+        {
+            while (pred(*it))
+            {
+                d_first++ = *it;
+                it = c.erase(it);
+            }
+        }
+    }
+    return it;
+}
+
+template <typename Container, typename OutputIt, typename T>
+auto remove_and_copy(Container& c, OutputIt d_first, T const& value)
+    -> decltype(c.begin())
+{
+    return remove_and_copy_if(c, d_first, 
+    [&] (T const& t) { return t == value; });
+}
+
+int main()
+{
+    std::string str = "Text with some   spaces";
+    std::string output;
+    std::cout << "Before: " << str << '\n';
+    
+    remove_and_copy(str, std::back_inserter(output), ' ');
+    
+    std::cout << "After: " << str << '\n';
+    std::cout << "Characters removed: " << output << '\n';
+}
