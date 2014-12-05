98b365a2bf83c4db4eb05871b3f04dece2501d91
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-05 19:29:03 +0000

    Update archive 30

---
+++ trunk/Archive2/30/6ef329b068f01f/cmd.sh
@@ -0,0 +1,1 @@
+g++ -std=c++11 main.cpp -S -masm=intel -o - | c++filt
---
+++ trunk/Archive2/30/6ef329b068f01f/main.cpp
@@ -0,0 +1,19 @@
+
+
+
+template<typename ...T>
+int sum(T...);
+
+int main()
+{
+    return sum(1)                         // edi
+         + sum(1, 2)                      // edi, esi
+         + sum(1, 2, 3)                   // edi, esi, edx
+         + sum(1, 2, 3, 4)                // edi, esi, edx, ecx
+         + sum(1, 2, 3, 4, 5)             // edi, esi, edx, ecx, r8d
+         + sum(1, 2, 3, 4, 5, 6)          // edi, esi, edx, ecx, r8d, r9d
+         + sum(1, 2, 3, 4, 5, 6, 7)       // edi, esi, edx, ecx, r8d, r9d, push
+         + sum(1, 2, 3, 4, 5, 6, 7, 8)    // edi, esi, edx, ecx, r8d, r9d, push, push
+         + sum(1, 2, 3, 4, 5, 6, 7, 8, 9) // edi, esi, edx, ecx, r8d, r9d, push, push, push
+         ;
+}
