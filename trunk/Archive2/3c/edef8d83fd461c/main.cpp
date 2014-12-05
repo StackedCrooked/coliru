61078c528b8dc6d3b46e8a8d1ce2390e46f11a8e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-05 22:20:10 +0000

    Update archive 3c

---
+++ trunk/Archive2/3c/edef8d83fd461c/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_SIZE = 10;
+
+void swapItems(int& first, int& second) // Swap two values by reference
+{
+    int tempVal;
+
+    tempVal = first;
+    first = second;
+    second = tempVal;
+}
+
+void quickSort(int myArray[], int first, int last)
+{
+
+    int pivotIdx = (first + last) / 2; // middle of array
+
+    int leftIdx = first;
+    int rightIdx = last;
+
+    while (leftIdx <= rightIdx)
+    {
+        if (myArray[leftIdx] > myArray[pivotIdx])
+        {
+            swapItems(myArray[leftIdx], myArray[pivotIdx]);
+        }
+        if (myArray[rightIdx] < myArray[pivotIdx])
+        {
+            swapItems(myArray[rightIdx], myArray[pivotIdx ]);
+        }
+        leftIdx++;
+        rightIdx--;
+    }
+
+    for (int i = 0; i < MAX_SIZE; ++i)
+        cout << myArray[i] << ' ';
+
+    cout << endl;
+}
+
+int main() {
+    { int myArray[MAX_SIZE] = {0, 7, 4, 1, 9, 6, 3, 2, 5, 8}; quickSort(myArray, 0, 9);}
+    return 0;
+}
