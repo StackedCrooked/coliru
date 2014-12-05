a02b781bb4c0930681fbe33c0e38b9de9fd7d2b5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-05 13:08:26 +0000

    Update archive 20

---
+++ Archive2/20/d62c820d4276f2/main.cpp
@@ -0,0 +1,31 @@
+struct M
+{
+    virtual ~M() = default;
+};
+
+struct M1 : M
+{
+    static M1* foo() { return nullptr; }
+};
+
+struct A
+{
+    struct register_type_base
+    {
+        virtual ~register_type_base() {}
+        virtual M* foo() const = 0;
+    };
+    
+    template <typename Mn>
+    struct register_type : register_type_base
+    {
+        Mn* foo() const
+        {
+            return Mn::foo();
+        }
+    };
+};
+
+const A::register_type<M1> REG;
+
+int main() {}
