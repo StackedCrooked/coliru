2f6da9d9b0d3d6209d355b3e29e4b9c5e50298a9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-05 11:17:28 +0000

    Update archive f9

---
+++ Archive2/f9/9c9dbdd7514b5e/main.cpp
@@ -0,0 +1,29 @@
+#include <array>
+#include <cstdint>
+#include <iostream>
+
+
+namespace networking
+{
+    using IP = std::array<std::uint8_t, 4>;
+    
+   
+}
+
+ std::ostream& operator<<(std::ostream& os, networking::IP ip)
+    {
+        return os << static_cast<int>(ip[0]) << "."
+                  << static_cast<int>(ip[1]) << "."
+                  << static_cast<int>(ip[2]) << "."
+                  << static_cast<int>(ip[3]);
+    }
+
+
+int main()
+{
+    using networking::IP;
+    
+    IP ip = { 1, 2, 3, 4 };
+    
+    std::cout << ip << std::endl;
+}
