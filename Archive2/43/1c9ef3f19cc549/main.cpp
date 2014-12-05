b55bacc50ba59ff0d7b3939164d36516da8dcc6e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-05 20:22:41 +0000

    Update archive 43

---
+++ Archive2/43/1c9ef3f19cc549/main.cpp
@@ -0,0 +1,76 @@
+#include <vector>
+#include <iostream>
+
+template<typename T>
+struct MapTtoT { typedef T (type)(const T); };
+
+template<typename T, typename F>
+auto map_vec(F&& fnc, const std::vector<T>& source) -> decltype(fnc(std::declval<T>()), std::vector<T>())
+{
+    std::vector<T> dest;
+    dest.reserve(source.size());
+    for (const auto i : source)
+    {
+        dest.emplace_back(fnc(i));
+    }
+    return dest;
+}
+
+template<typename T>
+struct MapTandVectoT { typedef T (type)(const T, const std::vector<T>&); };
+
+template<typename T, typename F>
+auto map_vec(F&& fnc, const std::vector<T>& source) -> decltype(fnc(std::declval<T>(), source), std::vector<T>())
+{
+    std::vector<T> dest;
+    dest.reserve(source.size());
+    for (const auto i : source)
+    {
+        dest.emplace_back(fnc(i, source));
+    }
+    return dest;
+}
+
+class AddNum
+{
+public:
+    AddNum(const int num) : num_(num) {}
+
+    int operator()(const int x) const
+    {
+        return x + num_;
+    }
+
+private:
+    const int num_;
+};
+
+class AddNumMulSize
+{
+public:
+    AddNumMulSize(const int num) : num_(num) {}
+
+    int operator()(const int x, const std::vector<int>& v) const
+    {
+        return (x + num_) * v.size();
+    }
+
+private:
+    const int num_;
+};
+
+int foo(const int) {return 0;}
+int bar(const int, const std::vector<int>&) {return 0;}
+
+int main()
+{
+    std::vector<int> v {1, 2, 3};
+    const auto a = map_vec(foo, v);
+    const auto b = map_vec(bar, v);
+    
+    const AddNum add2(2);
+    const auto c = map_vec(add2, v);
+    
+    const AddNumMulSize add2mulsz(2);
+    const auto d = map_vec(add2mulsz, v);
+}
