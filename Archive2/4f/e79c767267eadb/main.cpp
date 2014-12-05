c80db99f68fc7516517186727758c61268c2e13d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-05 19:21:16 +0000

    Update archive 4f

---
+++ Archive2/4f/e79c767267eadb/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+    std::string str1 = "Hallo%",
+                str2 = "Hallo World!";
+    
+    auto iters = std::mismatch( str1.begin(), str1.end(), str2.begin(), str2.end(), 
+                                [] (char lhs, char rhs) {return lhs != '%' && lhs == rhs;});
+                                
+    if (iters.first == str1.end() || *iters.first == '%') // Success
+        std::cout << "HURRAY!";
+}
