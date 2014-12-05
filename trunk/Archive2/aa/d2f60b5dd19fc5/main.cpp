821c7306c678be12646fcf16dbc753cb17322511
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-05 01:41:01 +0000

    Update archive aa

---
+++ trunk/Archive2/aa/d2f60b5dd19fc5/main.cpp
@@ -0,0 +1,48 @@
+bool lock = true;
+
+
+#include <map>
+#include <iostream>
+#include <stdio.h>
+    
+    
+std::map<void*, const char*> map;
+
+
+extern "C" {   
+    void __cyg_profile_func_enter(void*, void* call_site) __attribute__((no_instrument_function));
+    void __cyg_profile_func_exit(void*, void* call_site) __attribute__((no_instrument_function));
+}
+
+
+int B() { }
+int A() { B(); }
+
+
+int main(int argc, char** argv)
+{
+    // map function pointer to name
+    map[(void*)&A] = "A";
+    map[(void*)&B] = "B";
+    
+
+    lock = false;
+    A();
+    lock = true;
+}
+
+
+void __cyg_profile_func_enter(void* this_fn, void* call_site)
+{
+    if (lock) return; lock = true;
+    std::cout << map[this_fn];
+    lock = false;
+}
+
+
+void __cyg_profile_func_exit(void* this_fn, void* call_site)
+{
+    if (lock) return; lock = true;
+    std::cout << map[this_fn];
+    lock = false;
+}
