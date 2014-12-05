7f417765e526cb079364c30aa450bc363fbb32ce
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-05 02:06:38 +0000

    Update archive c6

---
+++ trunk/Archive2/c6/0a0fd7d845a95d/main.cpp
@@ -0,0 +1,55 @@
+    #include <iostream>
+     
+    using namespace std;
+     
+    int main()
+    {
+    char a[21], b[21];
+    char yn='n';
+    int ap = 0;
+    int bp = 0;
+    int set = 1;
+    int tp = 0;
+    char point;
+     
+     
+    do
+    {
+    cout << "\t\t Beach VolleyBall Game Tracker" << endl << endl;
+     
+    cout << "Enter the name of Team A: ";
+    cin >> a;
+    cout << "Enter the name of Team B: ";
+    cin >> b;
+     
+    cout << "Let the Games begin! Best 2 of 3 sets wins!" << endl << endl;
+    //Above this point is looking good. Wont need to be repeated at all anymore.
+     
+    for(int num = 7; num > tp;)
+    {
+    cout << " Current Status \t Set " << set << endl;\
+    cout << " Team A\t " << a << " | " << "Team B\t " << b << endl;
+    cout << "\t " << ap << " | "<< " \t " << bp << endl;
+    cout << "What team scored a point (A or B)? ";
+    cout << point;
+     
+    if(point == 'a')
+    {
+    ap++;
+    tp++;
+    }
+    else if(point == 'b')
+    {
+    bp++;
+    tp++;
+    }
+    }
+     
+    cout << "Would you like to play again? (Y/N)? ";
+    cin >> yn;
+     
+    }while(yn !='n' && yn !='N');
+     
+    return 0;
+    }
+    
