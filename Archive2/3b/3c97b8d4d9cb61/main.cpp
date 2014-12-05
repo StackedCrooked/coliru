73a227833a04e238d5a1496a45e7b2f1bddd4b8c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-05 19:53:45 +0000

    Update archive 3b

---
+++ Archive2/3b/3c97b8d4d9cb61/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+
+struct A
+{
+    std::string str() {return "asdf ";}
+};
+
+int main()
+{
+	std::vector<A> vec(7);
+	std::transform(vec.begin(), vec.end(), std::ostream_iterator<std::string>(std::cout), std::mem_fn(&A::str));
+}
