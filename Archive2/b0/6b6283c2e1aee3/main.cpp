c5da31104a1190a14c32183ca2dcd7d247c57846
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-06 00:19:21 +0000

    Update archive b0

---
+++ Archive2/b0/6b6283c2e1aee3/main.cpp
@@ -0,0 +1,27 @@
+
+
+
+template<typename T>
+struct Foo
+{
+    template<typename ...Args>
+    Foo(Args&& ...args) : inner_object_(std::forward<Args>(args)...) {}
+    
+    T inner_object_;
+};
+
+
+struct Bar
+{
+    Bar(int n, float f) : n_(n), f_(f) {}
+    
+    int n_;
+    float f_;
+};
+
+
+int main()
+{
+    Foo<Bar> f(1, 2.3);
+}
+
