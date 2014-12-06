148442a40bd6884c2ea107ca3048008204b0ddb0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-06 18:38:32 +0000

    Update archive 4f

---
+++ Archive2/4f/1eaa33ba8291d8/main.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+#include <set>
+
+int main()
+{
+  std::set<int> one = {1, 2, 3, 4, 5};
+  std::set<int> two = {2, 4, 6, 8, 10};
+  
+  one.insert(std::begin(two), std::end(two));
+  
+  for(int i : one)
+    std::cout << i << '\n';
+}
