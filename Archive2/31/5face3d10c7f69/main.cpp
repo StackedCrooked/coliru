e92d7b24252a53365988729329ca3b82775931d7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-06 20:45:51 +0000

    Update archive 31

---
+++ Archive2/31/5face3d10c7f69/main.cpp
@@ -0,0 +1,20 @@
+class Base
+{
+public:
+   Base(void) {}
+   virtual ~Base(void) {}
+};
+
+class Derived : public Base
+{
+public:
+   Derived(void) {}
+   virtual ~Derived(void) {}
+};
+
+
+int main(int argc, char **argv)
+{
+    Base * A;
+    A = new Derived();
+}
