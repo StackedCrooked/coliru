203fddcde9bbdd554f38df0ec2ed6fd64a51830f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-07 16:34:46 +0000

    Update archive db

---
+++ Archive2/db/eb9fb44035a121/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+
+class Test{
+
+
+public:
+        void cool()
+        {
+
+            cout << "test" << endl;
+        }
+
+};
+
+
+int main()
+{
+
+    Test object;
+    object.cool();
+
+
+    return 0;
+}
