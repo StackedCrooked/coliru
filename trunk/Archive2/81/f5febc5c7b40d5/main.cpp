5afd96d558087103c1e8cd532b4c0a4702046efd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-07 18:35:16 +0000

    Update archive 81

---
+++ trunk/Archive2/81/f5febc5c7b40d5/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::string test;
+    test = std::string("ciao");
+    cout << test << endl;
+    test.clear();
+    test.append("mandi");
+    cout << test << endl;
+   cout << "Questo e' il nostro compilatore online!" << endl;
+}
