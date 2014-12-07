6adfc6e2eb2ffa34c583b9b6297a0c2fe63b68a9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-07 04:05:27 +0000

    Update archive 79

---
+++ trunk/Archive2/79/357b53eb42c108/main.cpp
@@ -0,0 +1,51 @@
+#include <type_traits>
+
+// lightweight type list
+template <class...> struct type_list {};
+
+template <class T> using eval = typename T::type;
+
+template <class T> struct identity {using type = T;};
+
+template <typename T, typename... E>
+class best_conversion
+{
+    template <typename...> struct overloads {};
+
+	template <typename U, typename... Rest>
+	struct overloads<U, Rest...> :
+		overloads<Rest...>
+	{
+		using overloads<Rest...>::call;
+
+		static identity<U> call(U);
+	};
+
+	template <typename U>
+	struct overloads<U>
+	{
+		static identity<U> call(U);
+	};
+
+	template <typename... E_>
+	static identity<eval<decltype(overloads<E_...>::call(std::declval<T>()))>>
+	best_conv(int);
+
+	template <typename...>
+	static identity<void> best_conv(...);
+
+public:
+
+	using type = eval<decltype(best_conv<E...>(0))>;
+};
+
+template <typename... T>
+using best_conversion_t = eval<best_conversion<T...>>;
+
+#include <string>
+
+static_assert( std::is_same<best_conversion_t<int, int, int>, int>{}, "" );
+static_assert( std::is_same<best_conversion_t<int, long, std::string>, long>{}, "" );
+static_assert( std::is_same<best_conversion_t<int>, void>{}, "" );
+
+int main() {}
