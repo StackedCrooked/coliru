2c7eee3810778bdbea1b43dd97c7cdbecb8519b1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-07 13:27:17 +0000

    Update archive 7d

---
+++ Archive2/7d/7ac4ea04e9be2d/cmd.sh
@@ -0,0 +1,2 @@
+clang++ -std=c++11 -stdlib=libc++ -O2 -Wall -Wextra -pedantic-errors -c -S -fomit-frame-pointer main.cpp && cat main.s
+echo ----------- && g++ -std=c++11 -O2 -Wall -Wextra -pedantic-errors -c -S -fomit-frame-pointer main.cpp && cat main.s
---
+++ Archive2/7d/7ac4ea04e9be2d/main.cpp
@@ -0,0 +1,5 @@
+void busy_idle()
+{
+    for( volatile int i = 0 ; i<1000000 ; ++i ) 
+        ;
+}
