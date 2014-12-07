dc93a5a55ce0812d22c12d47711e055e1bf22f39
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-07 01:46:54 +0000

    Update archive 2c

---
+++ trunk/Archive2/2c/72dc054554713e/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <functional>
+#include <vector>
+
+class NoCopy
+{
+public:
+    NoCopy() {}
+    int operator()() const { return 42; }
+    
+private:
+    NoCopy(const NoCopy& r) = delete;
+};
+
+int main()
+{
+    std::function<int()> fn(NoCopy());
+    
+    std::cout << fn(0) << std::endl;
+}
