889014e6248703adca4b96f1b75dac364aa2eecb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-07 18:51:22 +0000

    Update archive c8

---
+++ trunk/Archive2/c8/5cbdee014ed103/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int answer[5];
+    int guess[5];
+    
+    for (i=0; i<n; i++){
+        guess[i]=0;
+        answer[i]=-1;
+    }//for
+    cout << guess[0] << answer[0];
+    return 0;
+}
