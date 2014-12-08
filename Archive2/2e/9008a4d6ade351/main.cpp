0c6048b31d0ef986989ccf3c8e8143dbd2a901b5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-08 14:18:53 +0000

    Update archive 2e

---
+++ Archive2/2e/9008a4d6ade351/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout<< "dammi a:";
+cin>> a;
+cout<< "dammi b:";
+cin >>b;
+std::swap(a,b);
+cout<< a;
+cout<< b;
+
+
+return 0;
+}
+
