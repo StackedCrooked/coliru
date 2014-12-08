84115e0d85aeac73007ae0515d6f958159233ee2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-08 17:44:42 +0000

    Update archive 12

---
+++ trunk/Archive2/12/321c0a2866f105/cmd.sh
@@ -0,0 +1,2 @@
+echo '--- clang++ ---' && clang++ -std=c++11 -stdlib=libc++ -O3 -Wall -Wextra -pedantic-errors main.cpp -lsupc++ && ./a.out && echo ok
+echo '--- g++ ---' && g++-4.9 -std=c++11 -O3 -Wall -Wextra -pedantic-errors main.cpp && ./a.out && echo ok
