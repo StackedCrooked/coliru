154617940ba7e99c5acf99daaa577e261f54aa77
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-09 23:21:31 +0000

    Update archive 70

---
+++ Archive2/70/566400ba29bac4/main.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+struct S
+{
+    S () { pSomeMemory = new char[250], cout << "S::S()" << endl; }
+
+    S (const S &arg) : S() { memcpy(pSomeMemory, arg.pSomeMemory, sizeof(char) * 250), cout << "S::S(const S &arg)" << endl; }
+
+    S (S &&arg) : S() { pSomeMemory = arg.pSomeMemory, arg.pSomeMemory = nullptr, cout << "S::S(S &&arg)" << endl; }
+
+    ~S () { delete[] pSomeMemory; }
+
+    char *pSomeMemory;
+};
+
+
+S FunctionReturningS(bool b)
+{
+    //the above 2 temporaries must be constructed
+
+    S temp;
+
+    S temp1;
+
+    //store some data at 'temp' and 'temp1'
+
+    temp.pSomeMemory[0] = '\0';
+
+    temp1.pSomeMemory[5] = '6';
+
+    return (b ? temp : temp1); // return either 'temp' or 'temp1'
+}
+
+int main()
+{
+    FunctionReturningS(true);
+
+    FunctionReturningS(false);
+
+    return 0;
+}
