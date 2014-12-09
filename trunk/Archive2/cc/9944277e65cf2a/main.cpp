b71098631c2ec40b68229fc27d7bd4879a6a5d0b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-09 14:25:20 +0000

    Update archive cc

---
+++ trunk/Archive2/cc/9944277e65cf2a/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class gg{
+public:
+    gg(int x ){
+
+    }
+    int a;
+    int b;
+    int c; 
+};
+
+int main()
+{
+    std::cout << sizeof(gg());
+}
