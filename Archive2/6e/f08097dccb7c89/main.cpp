1002e55a32c5dc2e8779add535f6d3b5f030efcd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-09 22:00:29 +0000

    Update archive 6e

---
+++ Archive2/6e/f08097dccb7c89/main.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+
+int main()
+{
+    std::stringbuf sb("hellohellohello");
+
+    for (int i = 0; i < 3; ++i) {
+        std::string hello;
+        
+        std::copy_n(std::istreambuf_iterator<char>(&sb),
+                    5,
+                    std::back_inserter(hello));
+                    
+        std::cout << hello << std::endl;
+    }
+}
