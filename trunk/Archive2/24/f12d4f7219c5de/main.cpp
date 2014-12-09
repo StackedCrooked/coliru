610fd17c8423b80eea45e8303574ea7c5c6de7c6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-09 00:38:02 +0000

    Update archive 24

---
+++ trunk/Archive2/24/f12d4f7219c5de/main.cpp
@@ -0,0 +1,114 @@
+#include <boost/interprocess/managed_shared_memory.hpp>
+#include <boost/interprocess/allocators/allocator.hpp>
+#include <boost/interprocess/containers/map.hpp>
+#include <boost/interprocess/containers/vector.hpp>
+#include <boost/interprocess/containers/string.hpp>
+
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/member.hpp>
+
+namespace emulation {
+    template <typename T1,typename T2,typename Alloc>
+        struct mutable_pair
+        {
+            typedef T1 first_type;
+            typedef T2 second_type;
+
+            mutable_pair(Alloc alloc):first(T1(alloc)),second(T2(alloc)){}
+            mutable_pair(const T1& f,const T2& s):first(f),second(s){}
+            mutable_pair(const std::pair<T1,T2>& p):first(p.first),second(p.second){}
+
+            T1         first;
+            mutable T2 second;
+        };
+
+    using namespace boost::multi_index;
+
+    template <typename Key, typename T, typename Compare, typename Allocator, typename Element = mutable_pair<Key, T, Allocator> >
+    struct map_gen {
+        typedef multi_index_container<
+            Element,
+            indexed_by<
+                ordered_unique<member<Element,Key,&Element::first>,Compare>
+            >,
+            typename Allocator::template rebind<Element>::other
+        > type;
+    };
+
+  template <typename Key, typename T, typename Compare, typename Allocator, typename Element = mutable_pair<Key, T, Allocator> >
+  struct multimap_gen {
+    typedef multi_index_container<
+        Element,
+        indexed_by<
+            ordered_non_unique<member<Element,Key,&Element::first>,Compare>
+        >,
+        typename Allocator::template rebind<Element>::other
+    > type;
+  };
+
+}
+
+// Typedefs of allocators and containers
+namespace Shared {
+    typedef boost::interprocess::managed_shared_memory Segment;
+    typedef boost::interprocess::managed_shared_memory::segment_manager SegmentManager;
+    typedef boost::interprocess::allocator<void, SegmentManager> Allocator;
+    typedef boost::interprocess::allocator<char, SegmentManager> CharAllocator;
+    typedef boost::interprocess::basic_string<char, std::char_traits<char>, CharAllocator> String;
+
+    struct MyLess {
+        template <typename T, typename U> bool operator()(const T &t, const U &u) const { return t < u; }
+    };
+    typedef MyLess StringComparator;
+
+
+    typedef boost::interprocess::allocator<char, SegmentManager> StringAlloc;
+    typedef emulation::mutable_pair<const String, String, StringAlloc> MapItem;
+    typedef boost::interprocess::allocator<MapItem, SegmentManager> MapItemAllocator;
+    typedef emulation::map_gen<String, String, StringComparator, MapItemAllocator>::type Map;
+}
+
+int main(void) {
+    struct shm_remove {
+        shm_remove() { boost::interprocess::shared_memory_object::remove("MySharedMemory"); }
+        ~shm_remove() { boost::interprocess::shared_memory_object::remove("MySharedMemory"); }
+    } remover;
+
+    // Create shared memory
+    Shared::Segment seg(boost::interprocess::create_only, "MySharedMemory", 65536);
+    Shared::Allocator alloc(seg.get_segment_manager());
+
+    // Construct the shared memory map
+    Shared::Map *myMapPtr = seg.construct<Shared::Map>("myMap")(alloc);
+
+    myMapPtr->emplace(Shared::String("foo", alloc), Shared::String("bar", alloc));
+    myMapPtr->emplace(Shared::String("goo", alloc), Shared::String("car", alloc));
+    myMapPtr->emplace(Shared::String("hoo", alloc), Shared::String("dar", alloc));
+
+    // Here's the problem:
+
+    // This the only version I can get to work.  But it forces you to create a
+    // copy of the key you are searching for, in the managed segment.
+    Shared::String key("foo", alloc);
+
+    // This is the point of the exercise:
+    Shared::Map::const_iterator it = myMapPtr->find(key);
+
+    if (it!=myMapPtr->end())
+        std::cout << "Found: '" << it->first << "' -> '" << it->second << "'\n";
+
+    // this is now okay too
+    char szkey[] = "foo";
+    it = myMapPtr->find(szkey);
+    if (it!=myMapPtr->end())
+        std::cout << "Found: '" << it->first << "' -> '" << it->second << "'\n";
+
+    // this is now okay too
+    std::string skey("foo");
+    it = myMapPtr->find(skey.c_str());
+    if (it!=myMapPtr->end())
+        std::cout << "Found: '" << it->first << "' -> '" << it->second << "'\n";
+
+    return 0;
+}
