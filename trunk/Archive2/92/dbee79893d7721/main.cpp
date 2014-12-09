4f3ee672d40d24a312655f7cb569eff5a29de802
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-09 11:48:48 +0000

    Update archive 92

---
+++ trunk/Archive2/92/dbee79893d7721/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <utility>
+
+template <typename Arg, typename... Args>
+void doPrint(std::ostream& out, Arg&& arg, Args&&... args)
+{
+    out << std::forward<Arg>(arg);
+    using expander = int[];
+    (void)expander{0, (void(out << ',' << std::forward<Args>(args)),0)...};
+}
+
+int main()
+{
+    doPrint(std::cout, 34, "bla", 15);
+}
