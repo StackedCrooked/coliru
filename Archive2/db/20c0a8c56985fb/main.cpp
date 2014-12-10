f12e5788c356b9a99c9db5025d868bf1af1c9044
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-10 20:52:47 +0000

    Update archive db

---
+++ Archive2/db/20c0a8c56985fb/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <inttypes.h>
+ 
+int main(void)
+{
+    printf("%zu\n", sizeof(int64_t));
+    printf("%s\n", PRId64);
+    printf("%+"PRId64"\n", INT64_MIN);
+    printf("%+"PRId64"\n", INT64_MAX);
+ 
+    int64_t n = 7;
+    printf("%+"PRId64"\n", n);
+ 
+    return 0;
+}
