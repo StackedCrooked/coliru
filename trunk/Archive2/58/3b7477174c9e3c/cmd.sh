dc71541f95fe8d8890d8c5e0172dba21cdb67249
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-10 07:07:44 +0000

    Update archive 58

---
+++ trunk/Archive2/58/3b7477174c9e3c/cmd.sh
@@ -0,0 +1,1 @@
+g++ -std=c++11 -O2 -Wall -pedantic -pthread main.cpp -S -o test.asm && cat test.asm
---
+++ trunk/Archive2/58/3b7477174c9e3c/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    const int BigNumber = 10000;
+    for (int i = 0; i != BigNumber; ++i){
+        std::cout << i;
+    }
+}
