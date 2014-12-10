f55c853f2c5c4240fcbc805a1e2758443ab87096
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-10 13:06:09 +0000

    Update archive db

---
+++ trunk/Archive2/db/461b6c22b3930d/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    bool x = true;
+    std::cout << "x=" << x << std::endl;
+
+    int y = 2;
+    x |= y;
+    std::cout << "x=" << x << std::endl;
+
+    x &= 0x01;
+    std::cout << "x=" << x << std::endl;
+    
+    x &= y;
+    std::cout << "x=" << x << std::endl;
+}
