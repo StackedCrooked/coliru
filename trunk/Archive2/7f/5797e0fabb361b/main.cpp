baefd9106067269cf82a09563a3f1473b1e54c6f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-10 11:16:20 +0000

    Update archive 7f

---
+++ trunk/Archive2/7f/5797e0fabb361b/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+
+int main()
+{
+    std::vector<std::string> words = {
+        "Hello", "from", "GCC", __VERSION__, "!"
+    };
+
+    std::copy(words.begin(), words.end(),
+        std::ostream_iterator<std::string>(std::cout, " "));
+
+    return 0;
+}
