3e04cef95c19a957a370f848ecbf39e92f83fa33
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-10 22:47:43 +0000

    Update archive d1

---
+++ trunk/Archive2/d1/ceb08c6424d889/main.cpp
@@ -0,0 +1,397 @@
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+
+struct SegmentPool
+{
+    SegmentPool(std::size_t inSegmentSize) : mSegments(), mSegmentSize(inSegmentSize)
+    {
+        if (inSegmentSize < 10000)
+        {
+            mSegments.reserve(10);
+        }
+        else
+        {
+            mSegments.reserve(1);
+        }
+
+        while (mSegments.size() != mSegments.capacity())
+        {
+            mSegments.push_back(malloc(inSegmentSize));
+        }
+    }
+
+    SegmentPool(const SegmentPool&) = delete;
+    SegmentPool& operator=(const SegmentPool&) = delete;
+
+    ~SegmentPool()
+    {
+        while (!mSegments.empty())
+        {
+            std::free(mSegments.back());
+            mSegments.pop_back();
+        }
+    }
+
+    void* allocate()
+    {
+        if (mSegments.empty())
+        {
+            auto cap = mSegments.capacity();
+            mSegments.reserve(2 * cap);
+            while (mSegments.size() != mSegments.capacity())
+            {
+                mSegments.push_back(malloc(mSegmentSize));
+            }
+        }
+        auto back = mSegments.back();
+        mSegments.pop_back();
+        return back;
+    }
+
+    void deallocate(void* data)
+    {
+        mSegments.push_back(data);
+    }
+
+    std::size_t getSegmentSize() const { return mSegmentSize; }
+
+private:
+    std::vector<void*> mSegments;
+    std::size_t mSegmentSize;
+};
+
+
+struct MemoryPool
+{
+    MemoryPool() = default;
+
+    MemoryPool(MemoryPool&&) noexcept = default;
+
+    MemoryPool& operator=(MemoryPool&&) noexcept = default;
+
+    ~MemoryPool() = default;
+
+    void* allocate(std::size_t inRequestedSize)
+    {
+        std::size_t actual_size = nextpow2(inRequestedSize);
+        std::size_t index = log2(inRequestedSize);
+
+        if (mSegmentPools.size() <= index)
+        {
+            mSegmentPools.resize(index + 1);
+        }
+
+        auto& pool = mSegmentPools[index];
+        if (!pool)
+        {
+            auto np = new SegmentPool(actual_size);
+            pool.reset(np);
+        }
+
+        return pool->allocate();
+    }
+
+    void deallocate(void* data, std::size_t inRequestedSize)
+    {
+        std::size_t index = log2(inRequestedSize);
+        mSegmentPools[index]->deallocate(data);
+    }
+
+private:
+    static int nextpow2(int x)
+    {
+        if (x < 0)
+        {
+            return 0;
+        }
+        --x;
+        x |= x >> 1;
+        x |= x >> 2;
+        x |= x >> 4;
+        x |= x >> 8;
+        x |= x >> 16;
+        return x+1;
+    }
+
+
+    static int log2(int x)
+    {
+        int y;
+        asm ("\tbsr %1, %0\n"
+             : "=r"(y)
+             : "r" (x)
+        );
+        return y;
+    }
+
+    typedef std::vector<std::unique_ptr<SegmentPool>> SegmentPools;
+    SegmentPools mSegmentPools;
+};
+
+
+
+
+
+template <typename T>
+struct MemoryPoolAllocator
+{
+    typedef T * pointer;
+    typedef const T * const_pointer;
+    typedef T& reference;
+    typedef const T& const_reference;
+    typedef T value_type;
+    typedef std::size_t size_type;
+    typedef std::ptrdiff_t difference_type;
+
+    template<typename U>
+    struct rebind
+    {
+        typedef MemoryPoolAllocator<U> other;
+    };
+
+    MemoryPoolAllocator(MemoryPool* inMemoryPool) : mMemoryPool(inMemoryPool) { }
+
+    MemoryPoolAllocator(const MemoryPoolAllocator& rhs) : mMemoryPool(rhs.mMemoryPool) { }
+
+    template <typename U>
+    MemoryPoolAllocator(const MemoryPoolAllocator<U>& rhs) : mMemoryPool(rhs.mMemoryPool) { }
+
+    ~MemoryPoolAllocator() { }
+
+
+    const T * address(const T& s) const { return &s; }
+    T * address(T& r) const { return &r; }
+
+    size_t max_size() const { return (static_cast<size_t>(0) - static_cast<size_t>(1)) / sizeof(T); }
+
+    bool operator==(const MemoryPoolAllocator&) const { return true; }
+    bool operator!=(const MemoryPoolAllocator& other) const { return !(*this == other); }
+
+    void construct(T * const p, const T& t) const
+    {
+        void * const pv = static_cast<void *>(p);
+        new (pv) T(t);
+    }
+
+    void destroy(T * const p) const { p->~T(); }
+
+    // The following will be different for each allocator.
+    T * allocate(size_t n) const
+    {
+        if (n == 0) return nullptr;
+        return static_cast<T*>(mMemoryPool->allocate(n * sizeof(T)));
+    }
+
+    void deallocate(T * const p, const size_t n) const
+    { mMemoryPool->deallocate(p, n * sizeof(T)); }
+
+    template <typename U>
+    T * allocate(const size_t n, const U*) const
+    { return this->allocate(n); }
+
+private:
+    MemoryPoolAllocator& operator=(const MemoryPoolAllocator&) = delete;
+    MemoryPool* mMemoryPool;
+};
+
+
+#include <array>
+
+
+struct Packet
+{
+    Packet(const char* b, const char* e) : Packet(std::allocator_arg, std::allocator<Packet>(), b, e) {}
+
+    Packet(uint16_t inSize) : Packet(std::allocator_arg, std::allocator<Packet>(), inSize) {}
+
+    template<typename Alloc>
+    Packet(std::allocator_arg_t, Alloc alloc, const char* b, const char* e)
+    {
+        using Other = typename Alloc::template rebind<char>::other;
+        static_assert(sizeof(Impl<Other>) <= sizeof(mStorage), "");
+        new (storage()) Impl<Other>(Other(alloc), b, e);
+    }
+
+    template<class Alloc>
+    Packet(std::allocator_arg_t, Alloc alloc, uint16_t inSize)
+    {
+        using Other = typename Alloc::template rebind<char>::other;
+        static_assert(sizeof(Impl<Other>) <= sizeof(mStorage), "");
+        new (storage()) Impl<Other>(Other(alloc), inSize);
+    }
+
+    Packet(Packet&& rhs)
+    {
+        rhs.base().move_construct(storage());
+    }
+
+    Packet(const Packet& rhs)
+    {
+        rhs.base().copy_construct(storage());
+    }
+
+    Packet& operator=(Packet rhs)
+    {
+        swap(rhs);
+        return *this;
+    }
+
+    ~Packet() { base().~Base(); }
+
+    void swap(Packet& rhs)
+    {
+        std::swap(mStorage[0], rhs.mStorage[0]);
+        std::swap(mStorage[1], rhs.mStorage[1]);
+        std::swap(mStorage[2], rhs.mStorage[2]);
+    }
+
+    const char* begin() const { return base().begin(); }
+    char* begin() { return base().begin(); }
+
+    const char* end() const { return base().end(); }
+    char* end() { return base().end(); }
+
+    const char* data() const { return begin(); }
+    char* data() { return begin(); }
+
+    std::size_t size() const { return base().size(); }
+
+private:
+    friend bool operator==(const Packet& lhs, const Packet& rhs)
+    {
+        return lhs.size() == rhs.size() && !memcmp(lhs.data(), rhs.data(), lhs.size());
+    }
+    
+    friend bool operator!=(const Packet& lhs, const Packet& rhs)
+    {
+        return !(lhs == rhs);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Packet& p)
+    {
+        for (char c : p) os << c;
+        return os;
+    }
+
+    struct Base
+    {
+        Base(char* inData, uint16_t inSize) : mData(inData), mSize(inSize) { }
+
+        Base(const Base&) = delete;
+        Base& operator=(const Base&) = delete;
+
+        virtual ~Base() { } 
+
+        virtual void copy_construct(void* where) const = 0;
+        virtual void move_construct(void* where) = 0;
+
+        char* begin() { return mData; }
+        char* end() { return begin() + size(); }
+
+        const char* begin() const { return mData; }
+        const char* end() const { return begin() + size(); }
+        std::size_t size() const { return mSize; }
+
+        char* mData;
+        uint16_t mSize;
+        uint16_t mExtra;
+        uint32_t mExtra2;
+    };
+
+    template<class Alloc>
+    struct Impl : Alloc, Base
+    {
+        Impl(Alloc alloc, const char* b, const char* e) : Alloc(alloc), Base(allocate(e - b), e - b)
+        { memcpy(mData, b, size()); }
+
+        Impl(Alloc alloc, uint16_t inSize) : Alloc(alloc), Base(allocate(inSize), inSize) { }
+
+        Impl(const Impl& rhs) : Alloc(rhs), Base(allocate(rhs.mSize), rhs.mSize)
+        { memcpy(mData, rhs.mData, rhs.mSize); }
+
+        Impl(Impl&& rhs) : Alloc(std::move(rhs.getAlloc())), Base(rhs.mData, rhs.mSize)
+        { rhs.mData = nullptr; rhs.mSize = 0; }
+
+        Impl& operator=(Impl rhs)
+        { rhs.swap(*this); return *this; }
+
+        ~Impl() { if (mData) deallocate(mData, size()); }
+
+        Alloc& getAlloc() { return *this; }
+
+        void swap(Impl& rhs)
+        {
+            std::swap(getAlloc(), rhs.getAlloc());
+            std::swap(mData, rhs.mData);
+            std::swap(mSize, rhs.mSize);
+        }
+
+        virtual void copy_construct(void* where) const override final
+        { new (where) Impl<Alloc>(*this); }
+
+        virtual void move_construct(void* where) override final
+        { new (where) Impl<Alloc>(std::move(*this)); }
+
+        using Alloc::allocate;
+        using Alloc::deallocate;
+
+        const char* begin() const { return mData; }
+        std::size_t size() const { return mSize; }
+    };
+
+    const Base& base() const { return *static_cast<const Base*>(storage()); }
+    Base& base() { return *static_cast<Base*>(storage()); }
+
+    const void* storage() const { return mStorage.data(); }
+    void* storage() { return mStorage.data(); }
+
+    std::array<void*, 4> mStorage;
+};
+
+
+#include <algorithm>
+#include <iostream>
+#include <cassert>
+
+
+int main()
+{
+    MemoryPool storage;
+    MemoryPoolAllocator<char> alloc(&storage);
+
+    for (int i = 0; i != 10; ++i)
+    {
+        std::string data = "1234567890";
+    
+        Packet packet1(std::allocator_arg, alloc, &*data.begin(), &*data.end());
+    
+        std::reverse(data.begin(), data.end());
+    
+        Packet packet2(std::allocator_arg, alloc, &*data.begin(), &*data.end());
+    
+        std::cout << "packet1=" << packet1 << std::endl;
+        std::cout << "packet2=" << packet2 << std::endl;
+    
+        std::reverse(packet2.begin(), packet2.end());
+    
+        std::cout << "packet1.data=" << packet1 << std::endl;
+        std::cout << "packet1.size=" << packet1.size() << std::endl;
+        
+        std::cout << "packet2.data=" << packet2 << std::endl;    
+        std::cout << "packet2.size=" << packet2.size() << std::endl;
+    
+        assert(packet1 == packet2);
+        packet2 = packet1;
+        assert(packet1 == packet2);
+        packet1 = std::move(packet2);
+        assert(packet1 != packet2);
+        std::cout << packet1.size() << '-' << packet2.size() << std::endl << std::endl;
+    }
+}
+
+
+
