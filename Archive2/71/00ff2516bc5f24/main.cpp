e7086755e0c5abd639576bec1c6aaa7a90767b60
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-10 04:31:04 +0000

    Update archive 71

---
+++ Archive2/71/00ff2516bc5f24/main.cpp
@@ -0,0 +1,13 @@
+const double pi = 3.14159565359;
+const double e = 2.718182;
+
+double CircleArea(const double radius);
+
+int main() {
+	return 0;
+}
+
+double CircleArea(const double radius) {
+
+	return (pi*(radius*radius));
+}
