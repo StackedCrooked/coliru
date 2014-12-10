c9d3a1c5a1f3dcf92738ee8d262372693c3bd4e3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-10 20:35:22 +0000

    Update archive b6

---
+++ Archive2/b6/c936655c21fce1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iterator>
+
+template< class InputIt, class Size, class OutputIt>
+OutputIt copy_n(InputIt first, Size count, OutputIt result)
+    // Copied from http://en.cppreference.com/w/cpp/algorithm/copy_n
+{
+    if (count > 0) {
+        *result++ = *first;
+        for (Size i = 1; i < count; ++i) {
+            *result++ = *++first;
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    const char str[]  = "hello world";
+    const char oops[] = "zzzzzzzzzzz";
+
+    copy_n(str, sizeof(str)/sizeof(str[0]), std::ostream_iterator<char>(std::cout));
+}
