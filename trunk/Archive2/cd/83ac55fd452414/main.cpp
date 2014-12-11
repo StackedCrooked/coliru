c63672cecfaffaa52e0c061dda6d41444e34fb26
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-11 22:15:20 +0000

    Update archive cd

---
+++ trunk/Archive2/cd/83ac55fd452414/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+int copied = 0;
+int MAX_COPIES = 1000;
+
+struct A {
+    A() {
+        static int c = 0;
+        x = c++;
+    }
+    
+    A(const A& other) {
+        x = other.x;
+        if (copied++ > MAX_COPIES) throw std::runtime_error("copied too often");
+    }
+    
+    A& operator=(const A& other) {
+        x = other.x;
+        if (copied++ > MAX_COPIES) throw std::runtime_error("copied too often");
+        return *this;
+    }
+    
+    int x;
+};
+
+int main() {
+    std::vector<A> v(10);
+    v.reserve(100);
+    
+    for (auto& e : v) std::cout << e.x << " ";
+    std::cout << "\n";
+    
+    MAX_COPIES = 10;
+    try { v.insert(v.begin() + 5, A()); } catch (...) {
+        std::cout << "error\n";
+    }
+    
+    for (auto& e : v) std::cout << e.x << " ";
+    std::cout << "\n";
+    
+    return 0;
+}
