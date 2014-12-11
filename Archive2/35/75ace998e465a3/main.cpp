9a382e133b3a8b12d1f695532c93684dc8cf11f9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-11 15:42:51 +0000

    Update archive 35

---
+++ Archive2/35/75ace998e465a3/main.cpp
@@ -0,0 +1,58 @@
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/xml_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/xml_oarchive.hpp>
+
+int data = 42;
+
+template <typename Ar>
+void some_output(std::ostream& os)
+{
+    std::cout << "Writing archive at " << os.tellp() << "\n";
+    Ar ar(os);
+    ar << BOOST_SERIALIZATION_NVP(data);
+}
+
+template <typename Ar>
+void roundtrip(std::istream& is)
+{
+    data = -1;
+    std::cout << "Reading archive at " << is.tellg() << "\n";
+    Ar ar(is);
+    ar >> BOOST_SERIALIZATION_NVP(data);
+    assert(data == 42);
+}
+
+#include <sstream>
+
+int main()
+{
+    std::stringstream ss;
+
+    //some_output<boost::archive::text_oarchive>(ss); // this derails the binary archive that follows
+    some_output<boost::archive::binary_oarchive>(ss);
+    some_output<boost::archive::xml_oarchive>(ss);
+    some_output<boost::archive::text_oarchive>(ss);
+
+    //roundtrip<boost::archive::text_iarchive>(ss);
+    roundtrip<boost::archive::binary_iarchive>(ss);
+    roundtrip<boost::archive::xml_iarchive>(ss);
+    roundtrip<boost::archive::text_iarchive>(ss);
+
+    // just to prove that there's remaining whitespace
+    std::cout << "remaining: ";
+    char ch;
+    while (ss>>std::noskipws>>ch)
+        std::cout << " " << std::showbase << std::hex << ((int)(ch));
+    std::cout << "\n";
+
+    // of course, anything else will fail:
+    try {
+        roundtrip<boost::archive::text_iarchive>(ss);
+    } catch(boost::archive::archive_exception const& e)
+    {
+        std::cout << "Can't deserialize from a stream a EOF: " << e.what();
+    }
+}
