63ce441db6f698e3ee9a7ea90348b44b59ba481a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-11 01:19:58 +0000

    Update archive 18

---
+++ Archive2/18/01a9e6051cad0d/main.cpp
@@ -0,0 +1,24 @@
+#include <regex>
+#include <string>
+#include <iostream>
+#include <vector>
+ 
+int main()
+{
+    const std::wstring str = L"We bought the grocer oil worth 2,48 drachmas. "
+                              "soap worth 94 drs. and rice worth 11,75 drachmas."
+                              "How much change will take from 7/8 of 50 drachmas?"; 
+                              
+    std::wregex integer( L"\\s[0-9]+\\s" ); // whitespace, one or more dcimal digits, whitespace
+    
+    auto iter = std::wsregex_iterator( str.begin(), str.end(), integer );
+    const auto end = std::wsregex_iterator();
+    
+    int sum = 0 ;
+    for( ; iter != end; ++iter) 
+    {
+        std::wcout << iter->str() ;
+        sum += std::stoi( iter->str() ) ; 
+    }
+    std::wcout << L"(sum:" << sum << L")\n" ;
+}
