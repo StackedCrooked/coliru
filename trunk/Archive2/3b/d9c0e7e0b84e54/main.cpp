207e8c68bea40b4dfdb5744dd5472b3dc3badcfe
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-11 19:37:55 +0000

    Update archive 3b

---
+++ trunk/Archive2/3b/d9c0e7e0b84e54/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+ 
+int n = 0;
+struct C {
+  explicit C(int) {}
+  C(const C&) { ++n; } // the copy constructor has a visible side effect
+};                     // it modifies an object with static storage duration
+ 
+C putamerda()
+{
+    C c(10);
+    std::cout << "putamerda" << std::endl;
+    return c;
+}
+ 
+int main() {
+  C c1(42); // direct-initialization, calls C::C(42)
+  C c2 = C(42); // copy-initialization, calls C::C( C(42) )
+  C c3 = putamerda();
+ 
+  std::cout << n << std::endl; // prints 0 if the copy was elided, 1 otherwise
+  return 0;
+}
