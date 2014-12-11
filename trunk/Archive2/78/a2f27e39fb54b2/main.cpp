fb5687af818f6a905d41a6efa83b0545b0274394
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-11 15:17:42 +0000

    Update archive 78

---
+++ trunk/Archive2/78/a2f27e39fb54b2/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main()
+{
+    char*name;
+    char*surname;
+    
+    scanf_s("%s %s", name,surname); //_s bec of microsoft visual stduio. two seperate scanf_s work
+    printf("%s",strcat(name,surname));
+}
+
