bbcb1d3b700579c19c962d9f2c7cea49db8f49a6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-11 13:27:36 +0000

    Update archive b7

---
+++ Archive2/b7/4e90dc3c931361/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
+#include <cassert>
+#include <iterator>
+#include <cstdlib>
+
+int main()
+{
+    std::vector <int> v1 { 7, 3, 8, 0, 5, 1, 2, 4 };
+    std::vector <int>::iterator i = std::unique (v1.begin (), v1.end (), [] (int a, int b) { 
+        return (a % 2) == (b % 2); 
+    });
+    
+    v1.erase (i, v1.end ());    
+    assert ((v1 == std::vector <int> { 7, 8, 5, 2 }));
+}
