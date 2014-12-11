2bbd85e6378581e31d26dab36071ceaf3cfc9b3f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-11 19:22:38 +0000

    Update archive ce

---
+++ Archive2/ce/bc914877ab3863/main.cpp
@@ -0,0 +1,22 @@
+#include <type_traits>
+#include <iostream>
+
+struct base
+{
+    template<typename T, typename = typename std::enable_if< std::is_integral<T>::value, int >::type>
+    base(T) { std::cout << "integral\n"; }
+    
+    template<typename T, typename = typename std::enable_if< std::is_floating_point<T>::value, int >::type>
+    base(T,...) { std::cout << "floating point\n"; }
+};
+
+struct derived : base
+{
+    using base::base;
+};
+
+int main()
+{
+    derived{42};
+    derived{4.2};
+}
