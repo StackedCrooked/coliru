08c5b3c2933d6a04080fa54b90635f45fd8b15c0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-12 20:00:41 +0000

    Update archive cd

---
+++ Archive2/cd/f0eb94c85bc55d/main.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <memory>
+
+template<class B, class... Ds>
+std::vector<std::shared_ptr<B>>& instances()
+{
+    static std::vector<std::shared_ptr<B>> a = { std::shared_ptr<B>(new Ds)... };
+    return a;
+}
+
+struct B {};
+
+struct D1 : B {};
+struct D2 : B {};
+
+int main()
+{
+    auto&& i = instances<B, D1, D2>();
+    (void)i;
+}
