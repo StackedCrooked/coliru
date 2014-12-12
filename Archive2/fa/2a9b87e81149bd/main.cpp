8cb8381c6cd21d931261246d1fb6dc1936c4996c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-12 12:40:22 +0000

    Update archive fa

---
+++ Archive2/fa/2a9b87e81149bd/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <valarray>
+
+int main()
+{
+    std::valarray<std::valarray<double>> A(std::valarray<double>(6), 2);
+    std::valarray<double> B(0.5, 3);
+    A[0][std::slice(0,B.size(),2)] = B;
+    A[1][std::slice(1,B.size(),2)] = B;
+    
+    for (auto& v : A)
+    {
+        for (auto& e : v)
+        {
+            std::cout << e << "\t";
+        }
+        std::cout << std::endl;
+    }
+}
