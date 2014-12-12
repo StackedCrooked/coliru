ee324e1f82e95b1227319a62015cf44a377dfa6f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-12 10:44:58 +0000

    Update archive c3

---
+++ Archive2/c3/9da7ec692b794e/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <future>
+#include <stdexcept>
+
+
+template<typename T, typename Exception>
+inline auto MakeExceptionalFuture(Exception e) -> std::future<T>
+{
+    std::cout << "MakeExceptionalFuture(Exception e)" << std::endl;
+    std::promise<T> prom;
+    try { throw e; } catch (...) { prom.set_exception(std::current_exception()); }
+    return prom.get_future();
+}
+
+
+template<typename T, typename Exception>
+inline auto MakeExceptionalFuture(std::exception_ptr eptr) -> std::future<T>
+{
+    std::cout << "MakeExceptionalFuture(std::exception_ptr eptr)" << std::endl;
+    std::promise<T> prom;
+    prom.set_exception(eptr);
+    return prom.get_future();
+}
+
+
+int main()
+{
+    try
+    {
+        throw std::runtime_error("Test");
+    } 
+    catch (...)
+    {
+        std::future<int> f = MakeExceptionalFuture<int>(std::current_exception());
+        f.get();
+    }
+}
