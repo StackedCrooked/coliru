e5d3eece65d216355ba8ebe556b8b065faff0779
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-12 17:50:46 +0000

    Update archive 61

---
+++ Archive2/61/8f187abb9705ea/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+int main()
+{
+    std::vector<std::string> words = {
+        "WE ARE ALIENS WE ARE TRYING TO COMMUNICATE"
+    };
+    while(1)
+    std::cout << words << std::endl;
+}
