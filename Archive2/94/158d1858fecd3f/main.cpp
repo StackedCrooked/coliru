439ea3c98a2fc3090494b0e06a8989473d4ed06f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-12 02:46:57 +0000

    Update archive 94

---
+++ Archive2/94/158d1858fecd3f/main.cpp
@@ -0,0 +1,23 @@
+ #include <iostream>
+ #include <string>
+
+
+ class A {
+ public:
+   explicit A(int a = 1, int b = 2) :
+     a_(a),
+     b_(b) {}
+
+   A(std::string s)
+     : A() {
+       std::cout << s;
+   }   
+
+   int a_;
+   int b_;
+ };
+
+ int main() {
+   A a("!");
+   std::cout << a.a_;
+ }
