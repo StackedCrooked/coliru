cf3adb140e1b8861ad091de5fcc85b7b6fe7bce6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-13 17:30:07 +0000

    Update archive fa

---
+++ Archive2/fa/30ccc8ec3fadd9/main.cpp
@@ -0,0 +1,23 @@
+
+   
+  int arr={.....}
+  sort(arr) //nlogn
+  
+  int k=0;
+  int elem;
+  int counter=0;
+  for(i=1 to arr.size)//n
+  {
+      if(arr[i] == arr[i-1])
+         counter++;
+      else if(counter>k)
+      {
+         k=counter  
+         elem =arr[i-1];
+         counter=0;
+      }
+  }
+  return k,elem;
+  
+  (n+nlogn)=O(nlogn)
+   
