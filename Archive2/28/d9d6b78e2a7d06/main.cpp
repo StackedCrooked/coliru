f0e93c1d42e41fc151ef5bcbe3559c5e88545f7c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-13 08:15:17 +0000

    Update archive 28

---
+++ Archive2/28/d9d6b78e2a7d06/main.cpp
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+	struct node* next;
+};
+void push(struct node** head, int data)
+{
+	struct node* temp=*head;
+	struct node* r = (struct node*)malloc(sizeof(struct ndoe*));
+	r->data = data;
+	r->next = NULL;
+	if(*head == NULL || (*head)->data > data)
+    {
+        *head = r;
+        (*head)->next = temp;
+    }
+    else
+    {
+        while(temp->next!=NULL)
+        {
+            if(temp->data<data &&((temp->next->data > data )||temp->next == NULL))
+            {
+                    r->next = temp->next;
+                    temp->next = r;
+                    return;
+            }
+            temp = temp->next;
+        }
+        temp->next = r;
+    }
+}
+void print(struct node* head)
+{
+	while(head!=NULL)
+	{
+		printf("%d  ",head->data);
+		head = head->next;
+	}
+}
+int main()
+{
+	struct node* head = NULL;
+	push(&head,1);
+	push(&head,7);
+	push(&head,5);
+	push(&head,15);
+	push(&head,10);
+	printf("Linked list before swapping:\n");
+	print(head);
+}
+
+
+
+
+
+
+
+
