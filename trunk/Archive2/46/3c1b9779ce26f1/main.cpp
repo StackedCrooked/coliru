eeeb0dcac716cff4524247565edaf4e565d221df
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-13 21:13:03 +0000

    Update archive 46

---
+++ trunk/Archive2/46/3c1b9779ce26f1/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <typeinfo>
+
+
+class A{
+protected:
+    virtual void printClassInfo(const std::type_info & dInfo) = 0;
+};
+
+class B: public A{
+protected:
+    void printClassInfo ( const std::type_info& dInfo ){
+        std::cout<<"Got "<<dInfo.name()<<std::endl;
+    }
+};
+
+template<typename type> class TypedA: public A{
+public:
+    inline void print(){
+        printClassInfo(typeid(type));
+    }
+};
+
+int main(int argc, char* argv[]){
+
+    A * a=new B;
+    TypedA<int> * test=static_cast<TypedA<int> *>(a);
+
+    test->print();
+
+    return 0;
+}
