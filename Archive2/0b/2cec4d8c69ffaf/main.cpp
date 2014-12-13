3e02132c317e88b27c3df1681ba1caf0a1254315
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-13 21:11:53 +0000

    Update archive 0b

---
+++ Archive2/0b/2cec4d8c69ffaf/main.cpp
@@ -0,0 +1,37 @@
+#include <iterator>
+#include <algorithm>
+#include <forward_list>
+#include <iostream>
+ 
+template <class ForwardIt>
+ ForwardIt choose_pivot(ForwardIt first, ForwardIt last)
+ {
+   return std::next(first, std::distance(first,last)/2);
+ }
+ 
+template <class ForwardIt>
+ void quicksort(ForwardIt first, ForwardIt last)
+ {
+    if(first == last) return;
+ 
+    auto pivot = *choose_pivot(first,last);
+ 
+    ForwardIt middle1 = std::partition(first, last, 
+                         [pivot](const auto& em){ return em < pivot; });
+    ForwardIt middle2 = std::partition(middle1, last, 
+                         [pivot](const auto& em){ return !(pivot < em); });
+ 
+    quicksort(first, middle1);
+    quicksort(middle2, last);
+ }
+ 
+ 
+int main()
+{
+   std::forward_list<int> fl = {1, 30, -4, 3, 5, -4, 1, 6, -8, 2, -5, 64, 1, 92};
+ 
+   quicksort(std::begin(fl), std::end(fl));
+ 
+   for(int fi : fl) std::cout << fi << ' ';
+   std::cout << '\n';
+}
