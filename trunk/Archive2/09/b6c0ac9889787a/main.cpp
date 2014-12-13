7b8538c09376eec0cdc133bd4396c2f43980e51c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-13 23:27:41 +0000

    Update archive 09

---
+++ trunk/Archive2/09/b6c0ac9889787a/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+
+const int BYTE_STRING_LEN = 13;
+typedef unsigned char byte;
+
+struct bytestring
+{
+    bytestring(const byte* val)
+    {
+        memcpy(value, val, sizeof(value));
+    }
+    
+    // For testing only here
+    bytestring(const char* val)
+    {
+        memcpy(value, val, sizeof(value));
+    }
+
+    bool operator==(const bytestring& other) const
+    {
+        return memcmp(value, other.value, sizeof(value)) == 0;
+    }
+
+    byte value[BYTE_STRING_LEN];
+};
+
+void printNewSolution(bytestring b)
+{
+    // check against known solutions to see if it's unique
+    static std::vector<bytestring> KnownSolutions;
+    if (std::find(KnownSolutions.begin(), KnownSolutions.end(), b) == KnownSolutions.end())
+    {
+        std::cout << "new" << std::endl;
+        KnownSolutions.push_back(b);
+    }
+    else
+        std::cout << "old" << std::endl;
+}
+
+int main()
+{
+    // Forcing 13 characters for this purpose.  I assume you have some way of reading these in.
+    printNewSolution("hello there!!");
+    printNewSolution("hello there!!");
+}
