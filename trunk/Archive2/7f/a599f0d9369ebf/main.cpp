a61ebf44539212b4cd6d71982ae10658c0090933
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-13 14:44:27 +0000

    Update archive 7f

---
+++ trunk/Archive2/7f/a599f0d9369ebf/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <functional>
+
+int main(int argc, char **argv) {
+  std::string test = "fdsa";
+
+  auto fn = std::bind(static_cast<std::string&(std::string::*)(const std::string&)>(&std::string::append), test, std::string("test"));
+  fn();
+  std::cout << test << '\n';
+}
