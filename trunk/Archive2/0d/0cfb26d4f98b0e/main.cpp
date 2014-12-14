03260bef7262f013304641108b3a527551968521
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-14 17:11:23 +0000

    Update archive 0d

---
+++ trunk/Archive2/0d/0cfb26d4f98b0e/main.cpp
@@ -0,0 +1,45 @@
+#ifndef MQTT_V1_BUFFER_SEQUENCE_HPP_
+#define MQTT_V1_BUFFER_SEQUENCE_HPP_
+#include <boost/array.hpp>
+#include <boost/asio/buffer.hpp>
+#include <boost/fusion/include/as_vector.hpp>
+#include <boost/fusion/include/is_sequence.hpp>
+#include <boost/fusion/functional/invocation/invoke.hpp>
+
+namespace mqtt {
+inline namespace v1 {
+struct make_buffer_sequence_impl
+{
+    template<typename T, typename... As>
+    auto operator()(As&&... as) -> boost::array<T, sizeof...(As)> const
+    {
+        return boost::array<T, sizeof...(As)>{boost::asio::buffer(as)...};
+    }
+};
+
+template<typename Sequence>
+typename boost::fusion::result_of::invoke<
+    make_buffer_sequence_impl
+  , Sequence
+>::type make_buffer_sequence(Sequence& sequence)
+{
+    static_assert(boost::fusion::traits::is_sequence<Sequence>::value,
+                  "Not a Boost.Fusion sequence");
+    return boost::fusion::invoke(
+        make_buffer_sequence_impl<boost::asio::mutable_buffer>(), sequence);
+}
+
+template<typename Sequence>
+typename boost::fusion::result_of::invoke<
+    make_buffer_sequence_impl
+  , Sequence
+>::type make_buffer_sequence(const Sequence& sequence)
+{
+    static_assert(boost::fusion::traits::is_sequence<Sequence>::value,
+                  "Not a Boost.Fusion sequence");
+    return boost::fusion::invoke(
+        make_buffer_sequence_impl<boost::asio::const_buffer>(), sequence);
+}
+} // v1
+} // mqtt
+#endif
