41513944c4c6c6a21f3ffb9061322ca965944778
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-14 16:32:46 +0000

    Update archive 93

---
+++ trunk/Archive2/93/52f3349f48bff4/main.cpp
@@ -0,0 +1,19 @@
+#include <type_traits>
+#include <cstdint>
+#include <iostream>
+
+template <int N>
+struct int_type {
+    using type = std::conditional_t<N <= 8, std::uint8_t,
+                 std::conditional_t<N <= 16, std::uint16_t,
+                 std::conditional_t<N <= 32, std::uint32_t,
+                 std::conditional_t<N <= 64, std::uint64_t,
+                 std::uintmax_t>>>>;
+};
+
+int main()
+{
+    int_type<10>::type t; // std::uint16_t
+    t = 12;
+    std::cout << t << std::endl;
+}
