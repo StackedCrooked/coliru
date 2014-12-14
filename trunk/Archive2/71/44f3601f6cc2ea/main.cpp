9b5587ee9f896889a02652294b1fac5f00051476
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-14 19:13:54 +0000

    Update archive 71

---
+++ trunk/Archive2/71/44f3601f6cc2ea/main.cpp
@@ -0,0 +1,14 @@
+main :: IO ()
+main = result ["a", "b"] ["a"]
+
+compareList :: (Eq a) => [a] -> [a] -> Bool
+compareList [] _ = False
+compareList (x:xs) ys 
+    | elem x ys = True
+    | otherwise  = compareList xs ys
+
+result :: (Eq a) => [a] -> [a] -> IO ()
+result list1 list2 = 
+    if (compareList list1 list2)
+        then putStrLn ("They share elements")
+        else putStrLn ("They don't share elements")
