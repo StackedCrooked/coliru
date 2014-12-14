ce52d85132b35a48511e73e79a48ea5a7e591651
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-14 01:49:51 +0000

    Update archive e9

---
+++ trunk/Archive2/e9/daf91969c09b1c/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <sstream>
+
+std::stringstream istreams("Map: something\nPlayer:    Max");
+
+std::istream& eatwhites(std::istream& stream)
+{
+    std::string skip = " \t";
+    while (std::string::npos != skip.find(stream.peek()))
+        stream.ignore();
+    return stream;
+}
+
+int main()
+{
+    char characters[50];
+    char killMap[50];
+    if (!(istreams.getline(characters, 50, ':') >> eatwhites) || !istreams.get(killMap, 50) || !istreams.gcount())
+    {
+        std::cout << "Error!";
+        return 0;
+    }
+    
+    if (!(istreams.getline(characters, 50, ':') >> eatwhites) || !istreams.get(characters, 50) || !istreams.gcount())
+    {
+        std::cout << "Error!";
+        return 0;
+    }
+    
+    if (istreams && istreams.gcount())
+        std::cout << killMap << '\n' << characters;
+}
