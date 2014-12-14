0b93e01df22e83fb48add2a6f2ac0e76efffb762
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-14 17:07:55 +0000

    Update archive 3f

---
+++ Archive2/3f/5ea6ecfcc17f5e/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+struct expand_type {
+  template<typename... T>
+  expand_type(T&&...) {}
+};
+template<typename... ArgTypes>
+void print(ArgTypes... args)
+{ 
+  expand_type{ 0, (std::cout << args, 0)... };
+}
+template<> void print() {} // end recursion
+
+int main()
+{
+  print("Random ", 'c', "ollection of ", 5, " arguments.\n");
+}
