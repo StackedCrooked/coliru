035b3d19b459cdec92613ade4f98f15c4e0a9b40
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-15 11:17:06 +0000

    Update archive 6e

---
+++ trunk/Archive2/6e/25ce2467394ad9/main.cpp
@@ -0,0 +1,20 @@
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time/posix_time/posix_time_duration.hpp>
+#include <iostream>
+using DATE = double;
+
+boost::posix_time::ptime pTime_from_DATE(double date)
+{
+    static const boost::posix_time::ptime::date_type base_date(1899, boost::gregorian::Dec, 30);
+    return boost::posix_time::ptime(
+            base_date, 
+            boost::posix_time::milliseconds(date * 1000 * 60 * 60 * 24));
+}
+
+int main() {
+    boost::posix_time::time_duration duration(1007645, 15, 0);
+
+    DATE date = duration.total_milliseconds() / 1000.0 / 60 / 60 / 24;
+
+    std::cout << pTime_from_DATE(date);
+}
