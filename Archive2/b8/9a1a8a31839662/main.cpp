1ca24403e4ee152e087fcc89ce96990943df78ae
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-15 04:10:58 +0000

    Update archive b8

---
+++ Archive2/b8/9a1a8a31839662/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <tuple>
+
+template <std::size_t I, std::size_t N, typename F, typename T>
+struct transform_tuple_helper {
+    template <typename... Accum>
+    static auto
+    do_(F f, T t, Accum... accum)
+        -> decltype(transform_tuple_helper<I + 1, N, F, T>::do_(f, t, accum..., f(std::get<I>(t))))
+    {
+        return transform_tuple_helper<I + 1, N, F, T>::do_(f, t, accum..., f(std::get<I>(t)));
+    }
+};
+
+template <std::size_t N, typename F, typename T>
+struct transform_tuple_helper<N, N, F, T> {
+    template <typename... Accum>
+    static auto
+    do_(F, T, Accum... accum) 
+        -> decltype(std::make_tuple(accum...))
+    {
+        return std::make_tuple(accum...);
+    }
+};
+
+template <typename F, typename T>
+auto
+transform_tuple(T t, F f) 
+    -> decltype(transform_tuple_helper<0, std::tuple_size<T>::value, F, T>::do_(f, t))
+{
+    return transform_tuple_helper<0, std::tuple_size<T>::value, F, T>::do_(f, t);
+}
+
+
+struct doubler {
+    template <typename T>
+    double
+    operator () (T x) {
+        return 2 * x + 0.1;
+    }
+};
+
+template< class... Types >
+class my_tuple : public std::tuple<Types...> {
+public:
+    using base = std::tuple<Types...>;
+    using base::tuple;
+
+};
+
+int
+main() {
+    
+    my_tuple<int, double> a{5, 7.2};
+    std::tuple<double, double> b = transform_tuple(a, doubler{});
+    std::cout << std::get<0>(b) << ", " << std::get<1>(b) << '\n';
+}
