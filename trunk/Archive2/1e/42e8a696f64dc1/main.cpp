08daefe296384fe0459e5c01b58f4ce26dda9395
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-15 12:38:30 +0000

    Update archive 1e

---
+++ trunk/Archive2/1e/42e8a696f64dc1/main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main()
+{
+    cout << "std::numeric_limits<std::streampos>::max(): " <<
+        std::numeric_limits<std::streampos>::max()  << endl;
+
+    return 0;
+}
