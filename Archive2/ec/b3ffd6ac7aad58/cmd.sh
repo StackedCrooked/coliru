d6c57954cbf973681a10588e7a5d9e7caf24f113
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-15 15:40:15 +0000

    Update archive ec

---
+++ Archive2/ec/b3ffd6ac7aad58/cmd.sh
@@ -0,0 +1,4 @@
+clang++ -std=c++11 -Wall -Wextra -pedantic main.cpp && ./a.out
+g++ -std=c++11 -Wall -Wextra -pedantic main.cpp && ./a.out
+clang++ -std=c++14 -Wall -Wextra -pedantic main.cpp && ./a.out
+g++ -std=c++14 -Wall -Wextra -pedantic main.cpp && ./a.out
