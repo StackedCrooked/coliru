1335b9c7ab2b2ff128abe55830f060c80b49e26f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-15 22:30:29 +0000

    Update archive e1

---
+++ Archive2/e1/382895e9a81154/cmd.sh
@@ -0,0 +1,3 @@
+clang++ -std=c++1y -Wall -Wextra -pedantic main.cpp -stdlib=libc++ && ./a.out
+echo -e "\n=======================================================\n"
+g++ -std=c++1y -Wall -Wextra -pedantic main.cpp && ./a.out
