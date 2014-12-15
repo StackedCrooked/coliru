dfc0ab2ad2c08f37a4021995a9d134119e8071a0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-15 08:06:55 +0000

    Update archive b6

---
+++ Archive2/b6/8696d1e6ad8d28/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+struct A
+{
+private:
+    int a = 5;
+    friend int foo(A a)
+    {
+        return a.a;
+    }
+};
+
+int a = foo(A());
+
+int main(){ std::cout << a << std::endl; }
