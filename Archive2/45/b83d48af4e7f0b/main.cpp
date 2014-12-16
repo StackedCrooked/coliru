55a1a7626df1984ad593de7d65e180829229f3ce
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-16 21:14:55 +0000

    Update archive 45

---
+++ Archive2/45/b83d48af4e7f0b/main.cpp
@@ -0,0 +1,6 @@
+A first chance exception of type 'System.Deployment.Application.InvalidDeploymentException' occurred in System.Deployment.dll
+A first chance exception of type 'System.IO.DirectoryNotFoundException' occurred in mscorlib.dll
+A first chance exception of type 'System.IO.DirectoryNotFoundException' occurred in PresentationCore.dll
+A first chance exception of type 'System.Xaml.XamlObjectWriterException' occurred in System.Xaml.dll
+A first chance exception of type 'System.Windows.Markup.XamlParseException' occurred in PresentationFramework.dll
+Additional information: Provide value on 'System.Windows.Baml2006.TypeConverterMarkupExtension' threw an exception.
