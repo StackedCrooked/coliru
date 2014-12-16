eea84e7b7a3a9c42569311e5815ede21951bb362
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-16 11:06:45 +0000

    Update archive 58

---
+++ Archive2/58/4a4a56d2b774fa/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <sstream>
+
+std::ostream & operator<<( std::ostream & os, const std::vector<int> & vec )
+{
+  for ( auto && e : vec)
+    os << e << " ";
+  return os;
+}
+
+int main()
+{
+  std::stringstream ss1, ss2;
+  ss1 << 42;
+  ss2 << 42;
+
+  std::istream_iterator<int> just_iter( ss1 );
+  std::vector<int> vec1( just_iter, std::istream_iterator<int>() );
+  std::vector<int> vec2( std::istream_iterator<int>{ ss2 }, std::istream_iterator<int>() );
+
+  std::cout << vec1 << std::endl;
+  std::cout << vec2 << std::endl;
+}
