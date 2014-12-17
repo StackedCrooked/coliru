e6f621c0df40393332fc06ea3077659bc10c3add
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-17 15:15:12 +0000

    Update archive bf

---
+++ Archive2/bf/18eab4edec34e3/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+#include <boost/variant.hpp>
+
+using namespace std;
+
+using FuncInt = std::function<int()>;
+using VaryInt = boost::variant<int, FuncInt>;
+
+struct VaryIntVisitor : public boost::static_visitor<int> {
+  int operator()(int i) const { return i; }
+  int operator()(FuncInt f) const { return f(); }
+};
+
+template <bool>
+struct Log;
+
+template <>
+struct Log<true> {
+  static void print_int(VaryInt f) {
+    cout << boost::apply_visitor(VaryIntVisitor(), f) << endl;
+  };
+};
+
+template <>
+struct Log<false> {
+  static void print_int(VaryInt) {};
+};
+
+#ifdef LOG_DEBUG
+using LOG = Log<true>;
+#else
+using LOG = Log<false>;
+#endif
+
+int int_generator() {
+    cout << "I'm being called" << endl;
+    return 42;
+}
+
+int main(void) {
+  LOG::print_int([] { int i = 0; /* do expensive computation */ i = int_generator(); return i; });
+  LOG::print_int(52);
+  LOG::print_int(int_generator());
+  return 0;
+};
