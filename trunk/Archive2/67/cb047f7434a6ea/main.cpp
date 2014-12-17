469f45aad9a2d3ff32fb2f1fa83d9fafe6608b45
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-17 07:42:45 +0000

    Update archive 67

---
+++ trunk/Archive2/67/cb047f7434a6ea/main.cpp
@@ -0,0 +1,23 @@
+#include <stdexcept>
+#include <iostream>
+struct X {
+    X() { throw std::runtime_error(""); }
+    // custom placement new
+    static void* operator new(std::size_t sz, bool b) {
+        std::cout << "custom placement new called, b = " << b << '\n';
+        std::cout << "sz = " << sz << '\n';
+        return ::operator new(sz);
+    }
+    // custom placement delete
+    static void operator delete(void* ptr, bool b)
+    {
+        std::cout << "custom placement delete called, b = " << b << '\n';
+        ::operator delete(ptr);
+    }
+    int a[2];
+};
+int main() {
+   try {
+     X* p1 = new (true) X;
+   } catch(const std::exception&) { }
+}
