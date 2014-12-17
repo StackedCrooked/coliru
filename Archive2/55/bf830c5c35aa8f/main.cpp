e68e2c0cefdb5d32183ff308551609b5520b92f5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-17 10:19:02 +0000

    Update archive 55

---
+++ Archive2/55/bf830c5c35aa8f/main.cpp
@@ -0,0 +1,68 @@
+#include <cstddef>
+
+// Get ref to any type, used in expression SFINAE
+template<typename T>
+T& declval();
+
+// Expression SFINAE tool, first parameter is an expression used for
+// SFINAE, second parameter is passed through as a typedef
+template<std::size_t, typename type_>
+struct expr_check
+{
+    typedef type_ type;
+};
+
+// Return type deduction
+template<class T>
+struct func_ptr_result{};
+
+template<class R>
+struct func_ptr_result<R (*)()>
+{
+    typedef R type;
+};
+
+template<class R, class Arg1>
+struct func_ptr_result<R (*)(Arg1)> {
+    typedef R type;
+};
+
+
+// Functions that will be passed as arguments
+// Commenting out foo() allows the portion at the bottom to compile.
+int foo() { return 0; }
+int foo(int i) { return 0; }
+
+
+// Function template overloads
+// Try to overload based on signature of function, F.
+template<typename F>
+typename expr_check<sizeof(declval<F>()()), typename func_ptr_result<F>::type>::type
+call(const F& f) {
+    return f();
+}
+
+template<typename F, typename A>
+typename expr_check<sizeof(declval<F>()(declval<A>())), typename func_ptr_result<F>::type>::type
+call(F f, A a) {
+    return f(a);
+}
+
+
+// Positive control: explicitly specify the type of the pointer to foo.
+int (*func_takes_void)() = foo;
+int a = call(func_takes_void);
+
+int (*func_takes_int)(int) = foo;
+int b = call(func_takes_int, 0);
+
+
+// Only provide function name.  Try to deduce type of pointer.
+// Won't compile.  clang and gcc: "no matching function"
+// Either one alone is enough for the error.
+int c = call(foo);
+int d = call(foo, 0);
+// If foo(void) definition is commmented out then the line above compiles.
+
+// Is there any way to get `call(foo)` and `call(foo, 0)` to compile without
+// explicitly providing the types?
