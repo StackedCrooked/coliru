294f3513a3e1d6a3e99eefa8ac12704e38a5939a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-17 15:21:06 +0000

    Update archive 61

---
+++ Archive2/61/1bf395e4181aa0/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+struct TS {};
+
+class TMy {
+public:
+    TMy() {}
+    explicit operator bool() const {
+        return true;
+    }
+    operator const TS*() const {
+        return nullptr;
+    }
+};
+
+void func(bool i) {
+    cout << "func: " << i << endl;
+}
+
+int main() {
+    TMy m;
+    func(m);
+    func(bool(m));
+    return 0;
+}
