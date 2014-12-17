4c136fe69e38ea1199e2f511387ebd7e1d9f2c1a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-17 17:24:14 +0000

    Update archive 56

---
+++ Archive2/56/92ee8d6fc6a28f/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+template<typename... Args>
+void bar(Args...) {
+    std::cout << sizeof...(Args);
+}
+
+template <typename... Args>
+void foo(Args&&... args) {
+#if TEST == 1
+    bar(std::forward<Args>(args...));      // variant 1
+#elif TEST == 2
+    bar(std::forward<Args...>(args...));   // variant 2
+#else
+    bar(std::forward<Args>(args)...);
+#endif
+}
+
+int main() {
+    std::cout << TEST;
+    foo();
+    foo(1);
+    foo(1,2);
+    std::cout << '\n';
+}
