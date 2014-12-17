4a12710252368dba67ae99fab21d0046c426f688
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-17 15:51:29 +0000

    Update archive 45

---
+++ Archive2/45/170b047bd4071f/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <functional>
+
+using namespace std;
+
+static const bool LOG_DEBUG = true;
+
+template< class T, bool dbg = LOG_DEBUG, std::enable_if_t<dbg>* = nullptr >
+inline void print(T && t) 
+   { cout << "EXPENSIVE PRINT " << std::forward<T>(t) << std::endl; }
+  
+template< class T, bool dbg = LOG_DEBUG, std::enable_if_t<dbg>* = nullptr >
+inline void print_function(T && t) 
+   { cout << "CHEAP PRINT " << std::forward<T>(t)() << std::endl; }
+
+template< class T, bool dbg = LOG_DEBUG, std::enable_if_t<!dbg>* = nullptr >
+inline void print(T && ) {}
+  
+template< class T, bool dbg = LOG_DEBUG, std::enable_if_t<!dbg>* = nullptr >
+inline void print_function(T && ) {}
+
+int int_generator() {
+    return 42;
+}
+
+int main(void) {
+  print_function([] { int i = 0; /* do expensive computation */ i = int_generator(); return i; });
+  print(52);
+  print(int_generator());
+  return 0;
+}
