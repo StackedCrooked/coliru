f4c734ed2d12b8c7efe40403a3d4f85916b6e42c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-17 14:38:42 +0000

    Update archive f5

---
+++ Archive2/f5/88d4717801d755/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main()
+{
+    char a[][30] = { "hello", "world", };
+    
+    std::cout << a[0] << " " << a[1] << std::endl;
+    
+    return 0;
+}
