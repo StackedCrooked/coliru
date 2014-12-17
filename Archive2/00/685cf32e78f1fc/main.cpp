993d264864620bc9c0300b0b04507c28b2ed856c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-17 15:24:55 +0000

    Update archive 00

---
+++ Archive2/00/685cf32e78f1fc/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <functional>
+
+using namespace std;
+
+static const bool LOG_DEBUG = false;
+
+
+int int_generator() {
+    cout << "I was called!" << endl;
+    return 42;
+}
+
+template <bool dbg = LOG_DEBUG, std::enable_if_t<dbg>* = nullptr>
+inline void call(std::function<void(void)> f) { f(); }
+template <bool dbg = LOG_DEBUG, std::enable_if_t<!dbg>* = nullptr>
+inline void call(std::function<void(void)>) {}
+
+int main(void) {
+  printf("%d\n", int_generator());
+  call([] { printf("%d\n", int_generator()); });
+  return 0;
+}
