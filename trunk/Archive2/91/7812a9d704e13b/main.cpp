6c6c58364e2bfdd7395dff9c698ddfa861310f51
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-17 15:35:27 +0000

    Update archive 91

---
+++ trunk/Archive2/91/7812a9d704e13b/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <functional>
+
+template<typename F, typename T>
+auto opby(F func, T arg)
+{
+    return [=](auto val) {
+        return func(val, arg); 
+    };
+}
+
+
+int main()
+{
+ auto mu = opby(std::multiplies<int>(), 2); 
+ std::cout << mu(3) << std::endl;
+}
