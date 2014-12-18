c193cbb91b7a4da97dea9a7ea2e2fe2c3d67ed54
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-18 20:00:56 +0000

    Update archive 38

---
+++ trunk/Archive2/38/0db3e42806b65e/cmd.sh
@@ -0,0 +1,3 @@
+echo stdlibc++ > stdlibc++ ; g++     -std=c++11                main.cpp && ./a.out >>stdlibc++
+echo libc++    > libc++    ; clang++ -std=c++11 -stdlib=libc++ main.cpp && ./a.out >>libc++
+echo "Results:" ; diff -y -t stdlibc++ libc++
