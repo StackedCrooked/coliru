46e68ec5467e0e712cee529761d95d1a24e7fff9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-18 21:14:15 +0000

    Update archive 59

---
+++ Archive2/59/9453432bbbc7b4/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+struct X
+{
+    X()  { std::cout << "CREATED   : " << this << std::endl; }
+    ~X() { std::cout << "DESTROYED : " << this << std::endl; }
+    X* operator&() { return this; }
+};
+
+int main()
+{
+    void *address = &X();
+    std::cout << "ADDRESS   : " << address << "  of temporary (destroyed already) " << std::endl;
+}
