f36f558d0e374d58b85fd336cee843416845526a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-18 15:28:24 +0000

    Update archive d4

---
+++ trunk/Archive2/d4/26522ef110ca7d/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main() {
+    
+    
+ int t[]={4, 3, 2, 1};
+ int x
+ 
+ for( i = 0; i < 3 ; ++i)   
+    t[i]++;
+    
+  if(t[3] = 2)  x = 0;
+  else x = 1; 
+  
+  printf("la valeur de x est : %d .\n");
+   
+}
