ec4e35d7ef5dd20ae0bf1f65d0e95d12eb197c59
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-18 19:55:24 +0000

    Update archive 9c

---
+++ trunk/Archive2/9c/d2f36bd1d99b43/main.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+int main()
+{
+    std::vector<std::string> argv_storage;
+    for (std::size_t i = 0; i < 10; ++i)
+    {
+        // I copy output of generate_rand_str() because I will need it
+        // later in the code.
+        argv_storage.push_back(std::to_string(i));
+    }
+    
+    std::vector<char *> argv( argv_storage.size() + 1 );    // one extra for NULL
+    for (std::size_t i = 0; i != argv_storage.size(); ++i)
+    {
+        argv[i] = &argv_storage[i][0];
+    }
+    
+    //argv.back() = NULL;
+    
+    char** test = argv.data();
+    while (*test != NULL) {
+        std::cout << *(test++) << '\n' << std::flush;
+    }
+}
