966adb38cd2cefe1ef7de7c75e70049df88af409
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-18 04:29:09 +0000

    Update archive 83

---
+++ trunk/Archive2/83/97389b5db13abc/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using score_board_type = std::map< std::string, int > ;
+typedef std::map< std::string, int > score_board_type ; // alternative syntax
+
+int main()
+{
+    score_board_type score_board ;
+
+    // insert a new entry / update existing score of a player
+    std::string name = "Daffy Duck" ;
+    int score = 38 ;
+    score_board[name] = score ;
+
+    // increment the score of a player
+    int delta = 9 ;
+    score_board[name] += delta ;
+
+    // check if an entry for a player exists
+    const auto iter = score_board.find(name) ;
+    if( iter != score_board.end() )
+    {
+        std::cout << "found player '" << name << "' score: " << iter->second << '\n' ;
+    }
+
+    // remove the entry for a playert
+    score_board.erase(name) ;
+
+    score_board[ "Bugs Bunny" ] = 63 ;
+    score_board[ "Speedy Gonzales" ] = 84 ;
+    score_board[ "Wile E. Coyote" ] = 72 ;
+
+    // display player scores
+    for( const auto& pair : score_board ) std::cout << pair.first << " : " << pair.second << '\n' ;
+}
