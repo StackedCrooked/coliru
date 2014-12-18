e5ef19ca0024032a27bf5287a14f9b855b0f5819
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-18 16:59:45 +0000

    Update archive 9c

---
+++ trunk/Archive2/9c/53d621b3c10391/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+struct B {
+    virtual void f(){ std::cout << "B" << std::endl;}
+}; 
+
+struct D : B 
+{
+    void f(){ std::cout << "D" << std::endl;} /* must override, we are the final overrider*/
+}; 
+
+struct J :  B
+{
+    void f(){ std::cout <<"J" << std::endl;}        
+};
+struct E : J, D {
+    
+    //void f(){ std::cout << "E"<< std::endl;}
+    
+}; 
+
+int main()
+{
+   J* foo = new E;
+   foo->f();
+    
+   D* bar = new E; 
+   bar->f();
+}
