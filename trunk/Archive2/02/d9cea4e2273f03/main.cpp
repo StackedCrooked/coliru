09b97788f213f93f6f97c67e9fc65cafb48f7ec9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-18 11:02:05 +0000

    Update archive 02

---
+++ trunk/Archive2/02/d9cea4e2273f03/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+#include <thread>
+#include <future>
+#include <functional>
+#include <vector>
+#include <stdint.h>
+#include <iostream>
+#include <sstream>
+
+
+class Foo{
+    
+    private:
+        int i = 6;
+    public:
+    
+        friend std::ostream& operator<< (std::ostream& stream, const Foo& x);
+
+};
+
+
+std::ostream& operator<<(std::ostream& stream, const Foo& x)
+{
+    stream << x.i;
+    return stream;
+}
+
+int main()
+{
+    Foo f;
+    std::stringstream ss;
+    ss << f;
+    std::cout << ss.str();
+}
