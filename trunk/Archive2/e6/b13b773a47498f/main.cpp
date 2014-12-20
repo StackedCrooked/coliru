9f94f52f19c640e1d15e5ac6d641ccf65eb62eff
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-20 21:37:16 +0000

    Update archive e6

---
+++ trunk/Archive2/e6/b13b773a47498f/main.cpp
@@ -0,0 +1,22 @@
+/*
+template <typename Key>
+void DoublyLinkedList<Key>::clear()
+{
+    Node* victim = Head_;
+    while (victim && victim->Next_ != Tail_)
+    {
+        if (victim == Head_)
+            victim = victim->Next_;
+            
+        Node* temp = victim;
+        victim = victim->Next_;
+        delete temp;
+    }
+    Size_ = 0;
+}
+*/
+
+int main()
+{
+    
+}
