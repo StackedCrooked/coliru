3e2d00bac099b70782c3d5fedc8fd211d41cbc50
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-20 20:55:12 +0000

    Update archive 30

---
+++ trunk/Archive2/30/5a90009b022201/main.cpp
@@ -0,0 +1,18 @@
+/*
+template <typename Key>
+void DoublyLinkedList<Key>::clear()
+{
+    while (Head_)
+    {
+        Node* victim = Head_;
+        Head_ = Head_->next;
+        delete victim;
+    }
+    Size_ = 0;
+}
+*/
+
+int main()
+{
+    
+}
