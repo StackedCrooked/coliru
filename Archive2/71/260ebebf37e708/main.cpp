0f40597ceac9a3d77d15b4090117b0fd9d772029
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-20 00:30:31 +0000

    Update archive 71

---
+++ Archive2/71/260ebebf37e708/main.cpp
@@ -0,0 +1,18 @@
+template< bool... Bs >
+void func();
+
+template< bool... Bs, typename... Ts >
+void func( bool b, Ts... ts )
+{
+    if( b ) {
+        func< Bs..., true >( ts... );
+    }
+    else {
+        func< Bs..., false >( ts... );
+    }
+}
+
+int main()
+{
+    func( true, false );
+}
