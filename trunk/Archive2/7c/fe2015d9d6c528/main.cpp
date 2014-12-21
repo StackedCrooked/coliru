40b1ea2c8d3e088bcbc27577d2463cf19f3557d7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-21 18:56:25 +0000

    Update archive 7c

---
+++ trunk/Archive2/7c/fe2015d9d6c528/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+constexpr int do_something(int x)
+{
+  static_assert(x > 0, "x must be > 0");
+  return x + 5;
+}
+
+int do_something(int x)
+{
+  if(x > 0) { cout << "x must be > 0" << endl; exit(-1); }
+  return x + 5;
+}
+
+int main(int argc, char* argv[])
+{
+    std::cout << do_something(7) << std::endl;
+    std::cout << do_somathing(argc) << std::endl;
+    
+    return 0;
+}
