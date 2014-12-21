e5de88c38ad1c25b0235724b118beb3ff0761132
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-21 14:55:38 +0000

    Update archive bb

---
+++ trunk/Archive2/bb/702d1df1e62799/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+#include <cfenv>
+
+long long ipow( int base, unsigned int exp )
+{
+    if( exp == 0 ) return 1 ;
+    else if( exp == 1 ) return base ;
+    else return ipow( base, exp/2 ) * ipow( base, (exp+1)/2 ) ;
+}
+
+int main()
+{
+    unsigned long long sum = 0 ;
+    const unsigned long long upper_limit = 211111111 ;
+
+    {
+        for( int i = 0 ; i < 9 ; ++i )
+        {
+            sum += std::llround( std::pow( 10, i ) ) ;
+
+            // this is just to whet your curiosity; you may ignore this for the present
+            if( std::fetestexcept(FE_INVALID) ) { std::cerr << "*** error: out of representable range\n" ; return 1 ; }
+        }
+        std::cout << sum << ' ' << upper_limit - 3 << '\n' ;
+    }
+
+    {
+        sum = 0 ;
+        for( int i = 0 ; i < 9 ; ++i ) sum += ipow( 10, i ) ;
+        std::cout << sum << ' ' << upper_limit - 3 << '\n' ;
+    }
+}
