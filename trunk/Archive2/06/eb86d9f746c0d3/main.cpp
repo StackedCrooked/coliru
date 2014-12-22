579a01f77f4582c58b66c231d707ca5a65bd5b4f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-22 12:25:13 +0000

    Update archive 06

---
+++ trunk/Archive2/06/eb86d9f746c0d3/main.cpp
@@ -0,0 +1,41 @@
+#include <cstddef>
+#include <utility>
+#include <type_traits>
+template<bool...> struct foo {};
+
+template <std::size_t N>
+struct matrix
+{
+  const bool data_[N*N];
+
+  template<typename... Values>
+  constexpr matrix(Values... values) noexcept
+    : data_{static_cast<bool>(values)...}
+  {}
+
+  constexpr bool operator [](std::size_t index) const noexcept
+  {
+    return data_[index];
+  }
+};
+
+struct matrix_holder {
+    static constexpr matrix<2> mat = {true, false, true, false};
+};
+
+constexpr matrix<2> matrix_holder::mat;
+
+    template<std::size_t N, const matrix<N> &Mat, class> 
+    struct ty1_helper;
+
+    template<std::size_t N, const matrix<N> &Mat, std::size_t... Is>
+    struct ty1_helper<N, Mat, std::index_sequence<Is...>> { 
+        using type = foo<Mat[Is]...>; 
+    };
+    
+    template<std::size_t N, const matrix<N> &Mat>
+    using ty1 = typename ty1_helper<N, Mat, std::make_index_sequence<N*N>>::type;
+
+int main(){
+    static_assert(std::is_same<ty1<2, matrix_holder::mat>, foo<true, false, true, false>>::value, "Oops");
+}
