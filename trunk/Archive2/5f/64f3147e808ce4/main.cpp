28a27077196073448c38dfaace846f081b52a0a1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-22 06:31:53 +0000

    Update archive 5f

---
+++ trunk/Archive2/5f/64f3147e808ce4/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+ 
+struct Printer { // generic functor
+    std::ostream& os;
+    Printer(std::ostream& os) : os(os) {}
+    template<typename T>
+    void operator()(const T& obj) { os << obj << ' '; } // member template
+};
+ 
+int main()
+{
+    std::vector<int> v = {1,2,3};
+    std::for_each(v.begin(), v.end(), Printer(std::cout));
+    std::string s = "abc";
+    std::for_each(s.begin(), s.end(), Printer(std::cout));
+}
