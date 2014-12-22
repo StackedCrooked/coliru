6719d67707c2b8164c1ba8b011b875ce1b0fd4b3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-22 02:22:31 +0000

    Update archive ed

---
+++ trunk/Archive2/ed/b8c2cf231409c5/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+//  Works
+//template <typename type> using r_ptr = type *;
+
+//  Fails
+template <typename type> using r_ptr = type *__restrict__;
+
+
+template <void (*fp)(r_ptr<int> ptr)>
+void foo()
+{
+    int x = 123;
+    fp(&x);
+}
+
+void fp(r_ptr<int> ptr)
+{
+    cout << ptr[0] << endl;
+}
+
+int main(int argc, char* argv[])
+{
+    foo<fp>();
+}
