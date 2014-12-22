57e8dfb09dddf0c6f937c35630af87a345e31b70
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-22 08:56:18 +0000

    Update archive f3

---
+++ trunk/Archive2/f3/359cc2a1da9937/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+// using namespace std; // **** avoid
+
+const int SIZE = 50;
+
+void reverse_case(char *);
+void lower(char *);
+void upper(char *);
+
+int len( const char* cstr ) // *** added: find the length of a string
+{
+    /*
+    int n = 0 ;
+    while( cstr[n] != 0 ) ++n ;
+    return n ;
+    */
+    return std::strlen(cstr) ;
+}
+
+int main()
+{
+    char cstr[SIZE];
+	std::cout << "Enter a sentence.\n";
+
+	std::cin.getline(cstr, SIZE);
+
+	reverse_case(cstr);
+	std::cout << cstr << '\n' ;
+
+	lower(cstr);
+	std::cout << cstr << '\n' ;
+
+	upper(cstr);
+	std::cout << cstr << '\n' ;
+
+	// return 0; // **** implicit return 0 ;
+}
+
+void reverse_case(char *str)
+{
+    const int sz = len(str) ;
+	for (int count = 0; count < sz ; ++count )
+	{
+	    if( std::isupper( str[count] ) ) str[count] = std::tolower( str[count] ) ;
+	    else str[count] = std::toupper( str[count] ) ;
+	}
+	//std::cout << str << endl;
+}
+
+void lower(char *str)
+{
+    const int sz = len(str) ;
+	//for (int count = 0; count < SIZE; ++count )
+	for (int count = 0; count < sz ; ++count )
+		str[count] = tolower(str[count]);
+	//std::cout << str << endl;
+}
+
+void upper(char *str)
+{
+    const int sz = len(str) ;
+	//for (int count = 0; count < SIZE; ++count )
+	for (int count = 0; count < sz ; ++count )
+		str[count] = toupper(str[count]);
+	//std::cout << str << endl;
+}
