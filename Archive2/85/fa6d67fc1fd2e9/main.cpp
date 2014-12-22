d06938cdb9e43f09e9c0be8e8a7b66c427cdb1b5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-22 04:24:38 +0000

    Update archive 85

---
+++ Archive2/85/fa6d67fc1fd2e9/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+//  Works
+//template <typename type> using r_ptr = type *;
+
+//  Fails
+template <typename type> using r_ptr = type *__restrict__;
+
+
+template <int x>
+class MyObj{
+public:
+    void foo(r_ptr<int> ptr) const;
+    
+};
+
+
+template <>
+void MyObj<3>::foo(r_ptr<int> ptr) const{
+    cout << "asdf" << endl;
+}
+
+
+int main(int argc, char* argv[]) {
+
+    MyObj<3> x;
+    x.foo(nullptr);
+
+
+}
