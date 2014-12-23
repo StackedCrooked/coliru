3c78d502488c95620a88574fd9c31b22a0d9a193
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-23 14:44:35 +0000

    Update archive 23

---
+++ Archive2/23/ea5a662bdd7f27/main.cpp
@@ -0,0 +1,28 @@
+
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+template<class T=void>
+class Setting
+{
+    public:
+    Setting(Setting* instance){((T*)instance)->write();};
+    Setting(){write();};
+    virtual void write(){cout<<"in parent\n";}
+};
+
+class child:public Setting<child>
+{
+  public:
+   child():Setting(this){};
+  virtual void write(){cout<<"in child\n";}
+};
+int main()
+{
+    child ch;   
+}
