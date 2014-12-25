b9e56f46c7876b017e5234bae232fad91175b389
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-25 10:34:48 +0000

    Update archive 6c

---
+++ trunk/Archive2/6c/3497033d0690f8/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <stdint.h>
+#include <errno.h>
+#include <cstring>
+
+using namespace std;
+
+
+int main()
+{
+
+    cout << "hi";
+
+	char buffer[100];
+
+	fstream myfile;
+	myfile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	myfile.open("C:\\Users\\Amaze SUCH WOW\\Desktop\\Recording_2014-12-24_20-44-34_0002.ts", ios::in | ios::out | ios::binary);
+	if (!myfile) {
+		cout << "shit: " << strerror(errno);
+	}
+	myfile.read(buffer, 100);
+
+	cout << " " << uint64_t(buffer[23]);
+	myfile.close();
+
+	cin.get();
+	return 0;
+}
