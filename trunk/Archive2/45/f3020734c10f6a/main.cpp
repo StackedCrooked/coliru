05522a9894b1f575d9ff0eb03a7d51810f3faff4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-25 07:26:02 +0000

    Update archive 45

---
+++ trunk/Archive2/45/f3020734c10f6a/main.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iterator>
+#include <iostream>
+#include <algorithm>
+#include <array>
+ 
+int main()
+{
+    int a = 0;
+    int n = rand() % a;
+    std::cout << n << ' ';
+}
