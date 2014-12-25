ab9bca7d7d16ca24278d68febec094303f012114
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-25 11:09:59 +0000

    Update archive 39

---
+++ trunk/Archive2/39/56af8051013ee7/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <stdint.h>
+
+using namespace std;
+
+
+int main()
+{
+    cout << "hi";
+
+	char buffer[100];
+
+	fstream myfile;
+
+	myfile.open("C:\\Users\\Amaze SUCH WOW\\Desktop\\Recording_2014-12-24_20-44-34_0002.ts", ios::in | ios::out | ios::binary);
+	myfile.read(buffer, 100);
+	if (!myfile) {
+		cout << "Error";
+	}
+	if (!myfile.read(buffer, 100)) {
+		cout << "Error2";
+	}
+	
+	cout << " " << int64_t(buffer[44]);
+	myfile.close();
+
+	cin.get();
+	return 0;
+}
