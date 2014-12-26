f2dfc6bda0dafb5fdce2f0a76d8652d3b2f1ee8a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-26 00:14:35 +0000

    Update archive b7

---
+++ Archive2/b7/a3482f2af47bf2/main.cpp
@@ -0,0 +1,73 @@
+#include <boost/serialization/serialization.hpp>
+#include <boost/serialization/type_info_implementation.hpp>
+#include <boost/serialization/shared_ptr.hpp>
+#include <boost/serialization/base_object.hpp>
+#include <boost/serialization/export.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/make_shared.hpp>
+#include <sstream>
+
+class Interface {
+    public:
+        virtual void aVirtual() = 0;
+        virtual ~Interface() {}
+    private:
+        friend class boost::serialization::access;
+        template<class Archive> void serialize(Archive&, unsigned) { }
+};
+
+BOOST_SERIALIZATION_ASSUME_ABSTRACT(Interface)
+
+template<class T>
+class Derived : public Interface {
+    public:
+        Derived(T in = 0) : m_data(in) {}
+        virtual void aVirtual() { /*Do something*/ }
+        T const& getData() const { return m_data; }
+    private:
+        T m_data;
+        friend class boost::serialization::access;
+        template<class Archive>
+            void serialize(Archive& ar, unsigned)
+            {
+                ar & boost::serialization::base_object<Interface>(*this);
+                //// alternatively, if you don't want to make the abstract base serializable:
+                // boost::serialization::void_cast_register<Derived, Interface>();
+
+                ar & m_data;
+            }
+};
+
+BOOST_CLASS_EXPORT(Derived<std::string>)
+BOOST_CLASS_EXPORT(Derived<double>)
+BOOST_CLASS_EXPORT(Derived<int>) // include all subtypes we can expect in an input archive
+
+int main()
+{
+    std::stringstream ss;
+
+    {
+        boost::archive::text_oarchive oa(ss);
+        Interface* o = new Derived<int>(42);
+
+        oa << o; // you can also use a raw `Interface*` but I hate new/delete
+
+        delete o;
+    }
+
+    std::cout << "Serialized: '" << ss.str() << "'\n";
+
+    {
+        boost::archive::text_iarchive ia(ss);
+        //boost::shared_ptr<Interface> o = boost::make_shared<Derived<int> >(42);
+        Interface* o = nullptr;
+
+        ia >> o; // you can also use a raw `Interface*` but I hate new/delete
+
+        if (auto p = dynamic_cast<Derived<int>*>(o))
+            std::cout << "Deserialized into Derived<int> with data: " << p->getData() << "\n";
+
+        delete o;
+    }
+}
