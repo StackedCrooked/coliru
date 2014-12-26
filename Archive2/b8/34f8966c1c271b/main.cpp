bb2db98f1ce81b24a49dfca5355e075768be5e01
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-26 21:26:33 +0000

    Update archive b8

---
+++ Archive2/b8/34f8966c1c271b/main.cpp
@@ -0,0 +1,115 @@
+// Copyright (C) 1999 Lucent Technologies 
+// Excerpted from 'The Practice of Programming' 
+// by Brian W. Kernighan, Rob Pike and StackedCrooked
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int match(char*, char*);
+int matchstar(int, char*, char*);
+
+
+// matchhere searches for a regexp at beginning of text 
+int matchhere(char* regexp, char* text)
+{
+    if (regexp[0] == '\0')
+        return 1;
+        
+    if (regexp[1] == '*')
+        return matchstar(regexp[0], regexp+2, text);
+        
+    if (regexp[0] == '$' && regexp[1] == '\0')
+        return *text == '\0';
+        
+    if (*text!='\0' && (regexp[0]=='.' || regexp[0]==*text))
+        return matchhere(regexp+1, text+1);
+        
+    return 0;
+}
+
+// match searches for regexp anywhere in text 
+int match(char* regexp, char* text)
+{
+    if (regexp[0] == '^')
+        return matchhere(regexp+1, text);
+
+    // must look even if string is empty 
+    do {
+        if (matchhere(regexp, text)) return 1;
+    }
+    while (*text++ != '\0');
+    return 0;
+}
+
+
+// matchstar searches for `regexp` at beginning of text 
+int matchstar(int c, char* regexp, char* text)
+{
+    // a '*' matches zero or more instances 
+    do {
+        if (matchhere(regexp, text)) return 1;
+    }
+    while (*text != '\0' && (*text++ == c || c == '.'));
+    return 0;
+}
+
+
+// grep searches for `regexp` in file 
+int grep(char* regexp, FILE* f, char* name)
+{
+    int n, nmatch;
+    char buf[BUFSIZ];
+
+    nmatch = 0;
+    while (fgets(buf, sizeof buf, f) != NULL)
+    {
+        n = strlen(buf);
+        if (n > 0 && buf[n-1] == '\n')
+            buf[n-1] = '\0';
+        if (match(regexp, buf))
+        {
+            nmatch++;
+            if (name != NULL)
+                printf("%s:", name);
+            printf("%s\n", buf);
+        }
+    }
+    return nmatch;
+}
+
+
+int main(int argc, char* argv[])
+{
+    int i, nmatch;
+    FILE* f;
+
+    if (argc < 2)
+    {
+        puts("usage: grep regexp [file ...]");
+    }
+    
+    nmatch = 0;
+    
+    if (argc == 2)
+    {
+        if (grep(argv[1], stdin, NULL)) nmatch++;
+    }
+    else
+    {
+        for (i = 2; i < argc; i++)
+        {
+            f = fopen(argv[i], "r");
+            if (f == NULL)
+            {
+                printf("can't open %s:", argv[i]);
+                continue;
+            }
+            if (grep(argv[1], f, argc>3 ? argv[i] : NULL) > 0)
+                nmatch++;
+            fclose(f);
+        }
+    }
+    return nmatch == 0;
+}
