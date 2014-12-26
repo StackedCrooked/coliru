2875a3c0ffc42dc6d0383499e3524dc901d13503
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-26 15:41:09 +0000

    Update archive 4f

---
+++ trunk/Archive2/4f/6f033293b291c1/main.cpp
@@ -0,0 +1,30 @@
+#include <cstdint>
+#include <cstdio>
+
+
+struct Byte
+{
+    template<typename T>
+    Byte& operator=(T t)
+    {
+        value_ = t;
+        return *this;
+    }
+
+    template<typename T>
+    operator T() const
+    {
+        return value_;
+    }
+
+    std::uint8_t value_;
+};
+
+
+int main()
+{
+    Byte a { 0x0a };
+    Byte b { 0x0b };
+
+    a = b; // Which method or method sequence is invoked?
+}
