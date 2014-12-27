18dc142ea054b76cb3ed366d728062246626343c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-27 00:19:19 +0000

    Update archive cd

---
+++ trunk/Archive2/cd/0e2ad6b2523fb0/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+    int x;
+public:
+    A() {
+        cout<<"Default Constructor\n";
+    }
+
+    A(A&& a) : x(a.x){
+        cout<<"Move Constructor\n";
+        a.x=0;
+    }
+
+    A(const A& a){
+        x=a.x;
+        cout<<"Copy Constructor\n";
+    }
+
+    A fun(A a){
+        return a;
+    }
+};
+
+int main()
+{
+    A a;
+    A b;
+    A c;
+    c = a.fun(b);
+}
