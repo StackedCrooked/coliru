907c16a07f45db9e609aae7c3f9a9c823424e923
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-27 15:11:25 +0000

    Update archive d7

---
+++ Archive2/d7/331bab7cea7218/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <type_traits>
+#include <utility>
+
+template <typename Function, typename... Args>
+auto call_function(Function&& f, Args&&... args)
+    -> typename std::result_of<Function&&(Args&&...)>::type
+{
+    return std::forward<Function>(f)(std::forward<Args>(args)...);
+}
+
+template <typename Function, typename Class, typename... Args>
+auto call_member_function(Function&& f, Class* instance, Args&&... args)
+    -> typename std::result_of<Function&&(Class*, Args&&...)>::type
+{
+    return (instance->*std::forward<Function>(f))(std::forward<Args>(args)...);
+}
+
+struct F
+{
+    int f(int a, char c) { std::cout << "F::f(int, char)" << std::endl; return 0; }
+};
+
+int f() { std::cout << "f()" << std::endl; return 0; }
+
+int main()
+{
+    F instance;
+    call_function(f);
+    call_member_function(&F::f, &instance, 123, 'c'); 
+}
