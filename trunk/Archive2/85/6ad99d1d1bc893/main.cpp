8c7e7733490a44c27b501fe95c5a066285b37c53
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-27 23:56:54 +0000

    Update archive 85

---
+++ trunk/Archive2/85/6ad99d1d1bc893/main.cpp
@@ -0,0 +1,20 @@
+-- Some int wrapper
+newtype X = X Int deriving (Eq, Show)
+
+-- With some `Num` instance
+instance Num X where
+    (X a) + (X b) = X (a + b)
+    (X a) * (X b) = X (a * b)
+    abs (X a) = X (abs a)
+    signum (X a) = X (signum a)
+    fromInteger a = X (fromInteger a)
+    negate (X a) = X (negate a)
+
+-- Given a function that accepts `X` ...
+func :: X -> Int
+func (X i) = i
+
+-- ... we can pass any `Num` object to it.
+-- Similarly to how `IsString` works with `OverloadedStrings`.
+main :: IO ()
+main = print $ func 5
