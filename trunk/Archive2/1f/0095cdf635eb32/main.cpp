6918aab53a7ed0cbf09ac63b0812db28f951e344
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-27 17:10:34 +0000

    Update archive 1f

---
+++ trunk/Archive2/1f/0095cdf635eb32/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <utility>
+#include <type_traits>
+
+template <typename F>
+struct return_type;
+    
+template <typename R, typename... Args>
+struct return_type<R(Args...)> { using type = R; };
+
+template <typename R, typename... Args>
+struct return_type<R(*)(Args...)> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...)> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) &> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) &&> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) const> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) const&> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) const&&> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) volatile> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) volatile&> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) volatile&&> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) const volatile> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) const volatile&> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type<R(C::*)(Args...) const volatile&&> { using type = R; };
+
+template <typename Function, typename... Args>
+auto call_function(Function&& f, Args&&... args)
+    -> typename return_type<typename std::remove_reference<Function>::type>::type
+{
+    return std::forward<Function>(f)(std::forward<Args>(args)...);
+}
+    
+template <typename Function, typename Class, typename... Args>
+auto call_member_function(Function&& f, Class* instance, Args&&... args)
+    -> typename return_type<typename std::remove_reference<Function>::type>::type
+{
+    return (instance->*std::forward<Function>(f))(std::forward<Args>(args)...);
+}
+
+struct F
+{
+    int f() { std::cout << "F::f()" << std::endl; return 0; }
+};
+
+int f() { std::cout << "f()" << std::endl; return 0; }
+
+int main()
+{
+    F instance;
+    static_assert(std::is_same<decltype(call_function(f)), int>{}, "!");
+    static_assert(std::is_same<decltype(call_member_function(&F::f, &instance)), int>{}, "!"); 
+}
