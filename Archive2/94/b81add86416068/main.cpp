e7f3505501e353540c46f7bc69b0007c0af6d357
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-28 04:15:26 +0000

    Update archive 94

---
+++ Archive2/94/b81add86416068/main.cpp
@@ -0,0 +1,72 @@
+#include <boost/asio.hpp>
+
+struct Client
+{
+    boost::asio::io_service& io_service;
+    boost::asio::ip::tcp::socket socket;
+
+    Client(boost::asio::io_service& svc, std::string const& host, std::string const& port) 
+        : io_service(svc), socket(io_service) 
+    {
+        boost::asio::ip::tcp::resolver resolver(io_service);
+        boost::asio::ip::tcp::resolver::iterator endpoint = resolver.resolve(boost::asio::ip::tcp::resolver::query(host, port));
+        boost::asio::connect(this->socket, endpoint);
+    };
+
+    void send(std::string const& message) {
+        socket.send(boost::asio::buffer(message));
+    }
+};
+
+
+#include <iostream>
+
+static const int PORT = 52275;
+
+void client_thread() {
+    boost::asio::io_service svc;
+    Client client(svc, "127.0.0.1", std::to_string(PORT));
+
+    client.send("hello world\n");
+    client.send("bye world\n");
+}
+
+void server_thread() {
+    try
+    {
+        boost::asio::io_service io_service;
+        boost::asio::ip::tcp::acceptor acceptor(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), PORT));
+
+        {
+            boost::asio::ip::tcp::socket socket(io_service);
+            acceptor.accept(socket);
+
+            boost::asio::streambuf sb;
+            boost::system::error_code ec;
+            while (boost::asio::read(socket, sb, ec)) {
+                std::cout << "received: '" << &sb << "'\n";
+
+                if (ec) {
+                    std::cout << "status: " << ec.message() << "\n";
+                    break;
+                }
+            }
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+}
+
+#include <boost/thread.hpp>
+
+int main() {
+    boost::thread_group tg;
+    tg.create_thread(server_thread);
+
+    boost::this_thread::sleep_for(boost::chrono::milliseconds(100));
+    tg.create_thread(client_thread);
+
+    tg.join_all();
+}
