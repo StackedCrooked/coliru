c72ffdfb3211fdfebad7a74c36f2e141095201b1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-28 14:35:21 +0000

    Update archive ea

---
+++ Archive2/ea/b5fc91e12f73cd/main.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int equals( const char* a, const char* b )
+{
+    if( a == NULL || b == NULL ) return a == NULL && b == NULL ; // equal if both are NULL
+
+    // invariant a and b are both non-NULL
+    for( ; *a && *b ; ++a, ++b ) if( *a != *b ) return 0 ; // not equal if there is a mismatch
+
+    // invariant: all characters up to the end of either one of the two strings are equal
+    return *a == 0 && *b == 0 ; // equal if we have reached the end of both strings
+}
+
+int main()
+{
+    const char expected_username[] = "jane1234" ;
+
+    enum { MAX_USERNAME_SZ = 99 } ; // avoid magic numbers
+    char username[ MAX_USERNAME_SZ + 1 ] = "" ; // +1 for the terminating null character
+
+    char format[32] = "" ; // avoid magic numbers in the format string too
+    sprintf( format, "%%%us", MAX_USERNAME_SZ ) ; // writing maintainable code in C is hard 
+                                                  // especially when i/o is involved
+
+    int fields_read = scanf( format, username ) ; // **** not &user_name ****
+
+    if( fields_read == 1 ) 
+    {
+        printf( "%s: ", username ) ;
+        if( equals( username, expected_username ) ) puts( "correct" ) ;
+        else puts( "incorrect" ) ;
+    }
+}
