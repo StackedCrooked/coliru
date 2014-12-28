312f73b233007726b2006df49031e855e1dd48c0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-28 00:10:23 +0000

    Update archive b2

---
+++ trunk/Archive2/b2/50742b388b2874/main.cpp
@@ -0,0 +1,21 @@
+import Control.Applicative
+
+data Vec2 a = Vec2 a a deriving (Eq, Show)
+
+instance Functor Vec2 where
+    fmap fn (Vec2 a b) = Vec2 (fn a) (fn b)
+
+instance Applicative Vec2 where
+    pure a = Vec2 a a
+    (Vec2 fa fb) <*> (Vec2 a b) = Vec2 (fa a) (fb b)
+
+instance (Num a) => Num (Vec2 a) where
+    (+)           = liftA2 (+) 
+    (*)           = liftA2 (*)
+    abs           = fmap abs
+    signum        = fmap signum
+    fromInteger x = pure (fromInteger x)
+    negate        = fmap negate
+    
+main :: IO ()
+main = print $ 9 * (Vec2 2 0)
