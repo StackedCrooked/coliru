049f8c005a8a821b79e3acdfcbb13224de464020
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-29 00:21:49 +0000

    Update archive bf

---
+++ trunk/Archive2/bf/690a678c39bf47/main.cpp
@@ -0,0 +1,134 @@
+#include <string>
+
+#include <boost/numeric/ublas/storage.hpp>
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/vector_expression.hpp>
+
+#include <boost/numeric/ublas/vector_proxy.hpp>
+#include <boost/numeric/ublas/io.hpp>
+
+namespace model
+{
+    using std::string;
+
+    namespace ublas = boost::numeric::ublas;
+
+    class Vector2D
+    {
+        friend Vector2D operator *( const Vector2D& lhs, const float rhs );
+        
+        public:
+            Vector2D( float x, float y );
+            Vector2D( const Vector2D& rhs );
+            Vector2D& operator=( const Vector2D& rhs );
+            virtual ~Vector2D();
+
+            /// Operators:
+            Vector2D operator-();
+ 
+            /// Other methods:
+            string toString(); 
+        protected:
+            ublas::vector<float> underwater_vector;
+        private:
+    };
+}
+
+#include <boost/numeric/ublas/blas.hpp> 
+#include <sstream>
+
+namespace model
+{
+    Vector2D::Vector2D( float x, float y ) : underwater_vector( 2 )
+    {
+        this->underwater_vector.insert_element( 0, x );
+        this->underwater_vector.insert_element( 1, y );
+    }
+
+    Vector2D::Vector2D( const Vector2D& rhs )
+    {
+        ublas::blas_1::copy( this->underwater_vector, rhs.underwater_vector );
+    }
+
+    Vector2D& Vector2D::operator=( const Vector2D& rhs )
+    {
+        if( this != &rhs )
+        {
+            ublas::blas_1::copy( this->underwater_vector, rhs.underwater_vector );
+        }
+        return *this;
+    }
+
+    Vector2D::~Vector2D()
+    {
+    }
+
+    /** @brief operator*
+      *
+      */
+    Vector2D operator*( const Vector2D& lhs, const float rhs )
+    {
+        float x = lhs.underwater_vector[0] * rhs;
+        float y = lhs.underwater_vector[1] * rhs;
+
+        Vector2D multiplied( x, y );
+        return multiplied;
+    }
+
+    /** @brief operator-
+      *
+      */
+    Vector2D Vector2D::operator-()
+    {
+        Vector2D lhs = *this;
+        lhs.underwater_vector[0] *= -1;
+        lhs.underwater_vector[1] *= -1;
+        return lhs;
+    }
+
+    /** @brief toString
+      *
+      */
+    string Vector2D::toString()
+    {
+        std::stringstream ss;
+        ss << this->underwater_vector;
+        return ss.str();
+    }
+}
+
+#include <iostream>
+#include <exception>
+
+int main( int argc, char* argv[] )
+{
+    using std::cout;
+    using std::cin;
+    using std::cerr;
+    using std::endl;
+    using std::string;
+    using std::exception;
+ 
+    using model::Vector2D;
+
+    Vector2D* vectorA = nullptr;
+
+    try
+    {
+        vectorA = new Vector2D( 10.45f, 3.3f ); 
+
+        cout << vectorA->toString() << endl; 
+
+        Vector2D vectorB = *vectorA * 2.0f;
+
+        cout << vectorB.toString() << endl; 
+    }
+    catch( exception& ex )
+    {
+        cout << "exception:" << endl;
+        cout << ex.what() << endl;
+        cin.get();
+    }
+
+    return EXIT_SUCCESS;
+}
