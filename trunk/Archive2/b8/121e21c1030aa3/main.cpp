d21b1090f00424f5eb0ab5c1609dceb9ad614b19
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-29 10:23:06 +0000

    Update archive b8

---
+++ trunk/Archive2/b8/121e21c1030aa3/main.cpp
@@ -0,0 +1,25 @@
+#include <boost/variant.hpp>
+#include <boost/mpl/vector.hpp>
+#include <iostream>
+
+template <typename T> struct Column { 
+    T v; 
+    Column(T const& v) : v(v) {}
+    friend std::ostream& operator<<(std::ostream& os, Column<T> const& cv) {
+        return os << cv.v;
+    }
+};
+
+template <typename... Ts>
+using make_column_variant = typename boost::make_variant_over<
+    typename boost::mpl::transform<
+            boost::mpl::vector<Ts...>, 
+            Column<boost::mpl::_1>,
+            boost::mpl::back_inserter<boost::mpl::vector<> > 
+        >::type
+    >::type;
+
+int main() {
+    make_column_variant<int, float, std::string> v(std::string("hello world"));
+    std::cout << v;
+}
