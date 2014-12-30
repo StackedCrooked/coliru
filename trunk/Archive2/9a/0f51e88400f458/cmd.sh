871f1c6695cd93fa84c75c06392d8b520980de62
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-30 09:08:50 +0000

    Update archive 9a

---
+++ trunk/Archive2/9a/0f51e88400f458/cmd.sh
@@ -0,0 +1,2 @@
+clang++ -Wall -Wextra -pedantic -std=c++14 main.cpp
+g++     -Wall -Wextra -pedantic -std=c++14 main.cpp
---
+++ trunk/Archive2/9a/0f51e88400f458/main.cpp
@@ -0,0 +1,13 @@
+struct A
+{
+    A() = default;
+    A(A&&) = default;
+    
+    A(A const&) = delete;
+};
+
+int main()
+{
+    A a;
+    throw a;
+}
