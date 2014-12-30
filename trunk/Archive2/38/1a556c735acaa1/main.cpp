eb173f27c85dd841d955fbe44ab38ed39dcc23af
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-30 08:42:15 +0000

    Update archive 38

---
+++ trunk/Archive2/38/1a556c735acaa1/main.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+using PyObject = int;
+
+PyObject* PyObject_GetItem( PyObject* pyob, PyObject* key )                  { cout << "PyObject_GetItem \n"; return nullptr; }
+void      PyObject_SetItem( PyObject* pyob, PyObject* key, PyObject* value ) { cout << "PyObject_SetItem \n"; }
+
+class Object {
+public:
+    PyObject* p;
+    //Object& memberOb;
+    
+    Object(PyObject* _p) : p{_p} { cout << "Constructing " << *p << " \n"; }
+    
+    void someMethod() { cout << "In someMethod() \n"; }
+    //PyObject* ptr() { cout << "In ptr() \n"; return p; }
+    
+    const Object operator[] (const Object& key)  const { 
+        cout << "const Object operator[] \n";
+        return Object{ PyObject_GetItem( p, key.p ) }; 
+    }
+
+    const Object& operator= (const Object& rhs) {
+        cout << "const Object& operator= \n";
+        p = rhs.p;
+        return *this;
+    }
+
+    class Proxy {
+    private:
+        const Object& container;
+        const Object& key;
+        
+        //Object value{};
+    
+    public:
+        // at this moment we don't know whether it is 'c[k] = x' or 'x = c[k]'
+        Proxy( const Object& c, const Object& k ) : container{c}, key{k} { 
+            cout << "Proxy::constructor \n"; 
+        }
+    
+        // Rvalue
+        // e.g. cout << myList[5] hits 'const Object operator[]'
+        operator Object() const {
+            cout << "Proxy::operator Object() const \n"; 
+            return container[key];
+        }
+    
+        // Lvalue
+        // e.g. (something = ) myList[5] = foo
+        const Proxy&  operator= (const Object& rhs_ob) {
+            cout << "Proxy::const Proxy&  operator= \n"; 
+            PyObject_SetItem( container.p, key.p, rhs_ob.p );
+            return *this; // allow daisy-chaining a = b = c etc, that's why we return const Object&
+        }
+    
+        Object operator->() { 
+            cout << "Proxy::Object operator->() \n"; 
+            return container[key]; 
+        }
+    };
+    
+    Proxy operator[] (const Object& key) {
+        cout << "Object::Proxy operator[] \n"; 
+        return Proxy{ *this, key };
+    }
+    
+
+    const Object* operator -> () const { 
+        cout << "Object::const Object* operator -> \n"; 
+        return this; 
+    }
+    
+    Object* operator -> () { 
+        cout << "Object::Object* operator -> \n"; 
+        return this; 
+    }        
+};
+
+int main()
+{
+    PyObject a=1,b=2,c=3;
+    Object ob=Object(&a), idx=Object(&b), targ=Object(&c);
+    
+    //#define DO(x) cout << "- - - - - - - - - - - - - - - - \n" #x "\n"; x;
+    
+    //DO( ob[idx] = targ )
+
+    PyObject d=4;
+    Object idy=Object(&d);
+
+    //DO( ob[idx] = ob[idy] )
+    ob[idx] = ob[idy]
+
+    /*
+    Object ob(), idx(), idx2(), target();
+    ob[idx] = ob[idx2] = target;
+    ob[idx].someMethod();
+    //ob[idx].memberOb = Object{};
+    PyObject* w = ob[idx]->p;
+    */
+} 
