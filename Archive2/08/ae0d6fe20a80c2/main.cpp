4794f36c82af66ba6ece1abcf35114557cb561f1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-30 08:44:52 +0000

    Update archive 08

---
+++ Archive2/08/ae0d6fe20a80c2/main.cpp
@@ -0,0 +1,67 @@
+#include <string>
+#include <cstdint>
+#include <boost/spirit/include/qi.hpp>
+
+struct record_struct {
+    std::string Name;
+    uint8_t Distance;
+    uint8_t TravelDistance;
+    std::string Comment;
+};
+
+BOOST_FUSION_ADAPT_STRUCT(
+    record_struct,
+    (std::string, Name)
+    (uint8_t, Distance)
+    (uint8_t, TravelDistance)
+    (std::string, Comment)
+)
+
+std::ostream &operator<<(std::ostream &o, const record_struct &s) {
+    o << s.Name << " (" << +s.Distance << ":" << +s.TravelDistance << ") " << s.Comment;
+    return o;
+}
+
+bool test(std::string const& s) {
+    std::string::const_iterator iter = s.begin();
+    std::string::const_iterator end  = s.end();
+    record_struct result;
+
+    bool parsed = false;
+    {
+        namespace qi = boost::spirit::qi;
+        using namespace qi;
+
+        std::string              name;
+        uint8_t                  distance;
+        boost::optional<uint8_t> travelDistance;
+        std::string              comment;
+
+        parsed = phrase_parse(iter, end, (
+                       lexeme[+(alnum | '_')]
+                    >> uint_ >> -uint_
+                    >> -("//" >> lexeme[+char_])
+                ), space, name, distance, travelDistance, comment);
+
+        result = { name, distance, travelDistance? *travelDistance : distance, comment };
+    }
+
+    if (parsed) {
+        std::cout << "Parsed: " << result << "\n";
+    } else {
+        std::cout << "Failed\n";
+    }
+
+    if (iter!=end) {
+        std::cout << "Unparsed: '" << std::string(iter, end) << "'\n";
+    }
+
+    return parsed && (iter == end);
+}
+
+int main() {
+    if (!test("Milan 20 22"))                 return 1;
+    if (!test("Paris 8 9 // comment"))        return 1;
+    if (!test("London 5"))                    return 1;
+    if (!test("Rome 1 //not a real comment")) return 1;
+}
