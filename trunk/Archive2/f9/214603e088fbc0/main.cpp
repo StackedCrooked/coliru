dfe81abb9c6d30998c7999e2c9127145ad987600
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-31 14:56:43 +0000

    Update archive f9

---
+++ trunk/Archive2/f9/214603e088fbc0/main.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <iostream>
+
+int main()
+{
+    auto ptr1 = &std::vector<void *>::size;
+    auto ptr2 = &std::vector<int *>::size;
+
+    void *p1 = *(void **)&ptr1;
+    void *p2 = *(void **)&ptr2;
+
+    std::cout << (p1 == p2) << std::endl;
+}
