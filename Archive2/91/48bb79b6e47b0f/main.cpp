9932b7841a8ff277c72318b4cba004fbcad8ba49
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2014-12-31 02:45:58 +0000

    Update archive 91

---
+++ Archive2/91/48bb79b6e47b0f/main.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <iterator>
+
+class MyClass {
+    std::vector<int> lst { 1, 3, 77, 42 };
+  public:
+    template <typename Base>
+    struct const_forward_iterator : public Base {
+        using base_type         = Base;
+        using iterator_type     = const_forward_iterator;
+        using iterator_category = std::forward_iterator_tag;
+
+        const_forward_iterator& operator+=(typename Base::difference_type) = delete;
+        const_forward_iterator& operator-=(typename Base::difference_type) = delete;
+        const_forward_iterator  operator+ (typename Base::difference_type) = delete;
+        const_forward_iterator  operator- (typename Base::difference_type) = delete;
+
+        const_forward_iterator(base_type it) : base_type(it) {}
+    };
+
+    template <typename it>
+    struct range : std::pair<it,it> {
+        range(it b, it e) : std::pair<it,it>(b,e) {}
+
+        it begin() const { return this->first; }
+        it end()   const { return this->second; }
+    };
+
+    using iterator_type = const_forward_iterator<std::vector<int>::const_iterator>;
+    range<iterator_type> getNumbers() const { return { lst.cbegin(), lst.cend() }; }
+};
+
+#include <iostream>
+
+int main()
+{
+    MyClass o;
+
+    for (auto i : o.getNumbers())
+        std::cout << i << " ";
+
+    auto numbers = o.getNumbers();
+    //numbers.first += 2; error: use of deleted function
+}
