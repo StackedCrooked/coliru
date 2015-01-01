751a16ea90b0bf7d901178e1b0d36b13944d189a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-01 17:24:02 +0000

    Update archive 66

---
+++ trunk/Archive2/66/1b1a3820396455/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <functional>
+
+template <typename T>
+struct combinator_traits : combinator_traits<decltype(&T::template operator()<T>)> {};
+
+template <typename C, typename R, typename F, typename... Args>
+struct combinator_traits<R(C::*)(F, Args...)>
+{
+    using type = std::function<R(Args...)>;
+};
+
+template <typename C, typename R, typename F, typename... Args>
+struct combinator_traits<R(C::*)(F, Args... )const>
+    : combinator_traits<R(C::*)(F, Args...)> {};
+
+// Tests:
+template <typename T>
+typename combinator_traits<T>::type Y(T t)
+{
+    return {};
+}
+
+template <typename T> void TD() { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+
+int main()
+{    
+    auto fib = [](auto myself, int x)
+    {
+        if(x < 2)
+            return 1;
+        return myself(myself, x - 1) + myself(myself, x - 2);
+    };
+    
+    TD<decltype(Y(fib))>();
+    
+    std::function<int(int)> f = Y(fib);
+}
