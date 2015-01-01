989d758fd942f004ba25372b7df1f92e4e74f139
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-01 17:23:26 +0000

    Update archive 6e

---
+++ Archive2/6e/935cca8c4417ef/main.cpp
@@ -0,0 +1,23 @@
+#include <type_traits>
+
+namespace asdf {
+    template <typename T>
+	std::enable_if_t<std::is_class<T>{}, T> declval();
+}
+
+template <class T>
+struct A
+{
+	using type = int;
+	using foo = typename decltype(asdf::declval<A>())::type;
+};
+
+namespace std
+{
+	template <>
+	class is_class<A<void>> : public std::false_type {};
+}
+
+int main() {
+	A<void> a;
+}
