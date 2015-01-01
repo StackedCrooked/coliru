a34dddc9c74d203af4c2b834d3c232b0c0cc3c36
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-01 06:43:55 +0000

    Update archive d1

---
+++ trunk/Archive2/d1/5c310983e2d330/cmd.sh
@@ -0,0 +1,2 @@
+echo libc++ && clang++ -std=c++11 -stdlib=libc++ -O2 -Wall -Wextra -pedantic-errors main.cpp -lsupc++ && ./a.out
+echo -e '\nlibstdc++' && g++ --version | grep GCC && g++ -std=c++11 -O2 -Wall -Wextra -pedantic-errors main.cpp && ./a.out
