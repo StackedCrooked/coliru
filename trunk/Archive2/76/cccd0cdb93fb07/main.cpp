9b79fa0582d3d69243e3b313e5497edf311a4366
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-01 20:17:35 +0000

    Update archive 76

---
+++ trunk/Archive2/76/cccd0cdb93fb07/main.cpp
@@ -0,0 +1,49 @@
+#include <functional>
+#include <tuple>
+#include <utility>
+
+template <typename R, typename... Args, std::size_t... Is>
+std::function<R(typename std::tuple_element<Is, std::tuple<Args...>>::type...)> pop_last(std::index_sequence<Is...>);
+
+template <typename R, typename... Args>
+auto pop_last() -> decltype(pop_last<R, Args...>(std::make_index_sequence<sizeof...(Args) - 1>{}));
+
+template <typename T>
+struct combinator_traits : combinator_traits<decltype(&T::template operator()<T>)> {};
+
+template <typename C, typename R, typename... Args>
+struct combinator_traits<R(C::*)(Args...)>
+{
+    using result_type = decltype(pop_last<R, Args...>());
+};
+
+template <typename C, typename R, typename... Args>
+struct combinator_traits<R(C::*)(Args...)const>
+    : combinator_traits<R(C::*)(Args...)> {};
+
+template <typename... Ts>
+void dummy(Ts...);
+
+// Tests:
+#include <type_traits>
+#include <iostream>
+
+int foo(int x)
+{
+    if (x < 2)
+        return 1;
+    return foo(x - 1) + foo(x - 2);
+}
+
+int main()
+{
+    auto fib = [](int x, auto t = dummy) -> int
+    {
+        if (x < 2)
+            return 1;
+        return t(x - 1) + t(x - 2);
+    };
+    
+    static_assert(std::is_same<combinator_traits<decltype(fib)>::result_type, std::function<int(int)>>::value, "!");
+    std::cout << fib(5, foo) << std::endl; // 8
+}
