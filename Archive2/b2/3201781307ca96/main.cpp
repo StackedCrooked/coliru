a2b4e2a87161fe8aa94289fe32c2a94d851129a0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-01 21:18:52 +0000

    Update archive b2

---
+++ Archive2/b2/3201781307ca96/main.cpp
@@ -0,0 +1,34 @@
+#include <functional>
+#include <type_traits>
+
+struct fake_anything {};
+
+auto fib = [](auto myself, int x) -> int{
+  if(x < 2)
+    return 1;
+  return myself(x - 1) + myself(x - 2);
+};
+
+    template<class T>
+    struct extract_signature;
+    
+    template<class T, class R, class FA, class...Args>
+    struct extract_signature<R (T::*)(FA, Args...)> {
+        //static_assert(std::is_same<fake_anything, std::decay_t<FA>>::value, "Unexpected signature");
+        using type = std::function<R(Args...)>;
+    };
+    
+    template<class T, class R, class FA, class...Args>
+    struct extract_signature<R (T::*)(FA, Args...) const> {
+        //static_assert(std::is_same<fake_anything, std::decay_t<FA>>::value, "Unexpected signature");
+        using type = std::function<R(Args...)>;
+    };
+// other cv- and ref-qualifier versions omitted - not relevant to lambdas
+// possibly other variations on the type of the first parameter, e.g., fake_anything &
+
+template<class T>
+using signature_t = typename extract_signature<decltype(&T::template operator()<fake_anything>)>::type;
+
+int main() {
+    static_assert(std::is_same<signature_t<decltype(fib)>, std::function<int(int)>>::value, "Oops");
+}
