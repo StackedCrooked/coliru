efefaee243773fa432dec5b7f45a1c8cc2400ae7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-01 17:59:28 +0000

    Update archive 7f

---
+++ trunk/Archive2/7f/a32c0c012db4aa/main.cpp
@@ -0,0 +1,52 @@
+#include <functional>
+#include <type_traits>
+
+struct fake_anything {
+  fake_anything(fake_anything const&);
+  fake_anything();
+  fake_anything&operator=(fake_anything const&);
+  template<class T>operator T&() const;
+  template<class T>operator T&&() const;
+  template<class T>operator T const&() const;
+  template<class T>operator T const&&() const;
+  fake_anything operator*() const;
+  fake_anything operator++() const;
+  fake_anything operator++(int) const;
+  fake_anything operator->() const;
+  template<class... Ts> fake_anything operator()(Ts&&...) const;
+  template<class T>fake_anything(T&&);
+};
+fake_anything operator+(fake_anything, fake_anything);
+fake_anything operator-(fake_anything, fake_anything);
+fake_anything operator*(fake_anything, fake_anything);
+fake_anything operator/(fake_anything, fake_anything);
+// etc for every operator
+
+auto fib = [](auto myself, int x) -> int{
+  if(x < 2)
+    return 1;
+  return myself(x - 1) + myself(x - 2);
+};
+
+
+template<class T>
+struct extract_signature;
+
+template<class T, class R, class...Args>
+struct extract_signature<R (T::*)(fake_anything, Args...)> {
+    using type = std::function<R(Args...)>;
+};
+
+template<class T, class R, class...Args>
+struct extract_signature<R (T::*)(fake_anything, Args...) const> {
+    using type = std::function<R(Args...)>;
+};
+// other cv- and ref-qualifier versions omitted - not relevant to lambdas
+// possibly other variations on the type of the first parameter, e.g., fake_anything &
+
+template<class T>
+using signature_t = typename extract_signature<decltype(&T::template operator()<fake_anything>)>::type;
+
+int main() {
+    static_assert(std::is_same<signature_t<decltype(fib)>, std::function<int(int)>>::value, "Oops");
+}
