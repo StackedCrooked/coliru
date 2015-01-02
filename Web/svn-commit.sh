af9cf4474578e04733fe98fbdc986133661be559
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-02 16:38:00 +0000

    Improve.

--- Web/svn-commit.sh
+++ Web/svn-commit.sh
@@ -2,17 +2,22 @@
 rp() { chown -R webserver:coliru . ; }
 
 {
+    set -x
     cd $(dirname ${0})
     while true ; do
         for d in $(ls Archive2 | sort -R) ; do (
             set -x;
             cd Archive2/$d 
-            svn add *
             [ "$(svn st | wc -l)" == "0" ] || {
-                { echo "I have stuff to commit!" && sleep 5 && svn ci -m "Update archive $d" && rp; } ||
-                { echo "Commit failed!" && svn cleanup && sleep 5 && svn up ; rp; }
+                echo "Found stuff. Try to svn add it."
+                svn add *
+                [ "$(svn st | wc -l)" == "0" ] || {
+                    { echo "I have stuff to commit!" && sleep 5 && svn ci -m "Update archive $d" && rp; } ||
+                    { echo "Commit failed!" && svn cleanup && sleep 5 && svn up ; rp; }
+                }
             }
-            sleep $((10 + $(($RANDOM % 60))))
+            sleep 1
+            #sleep $((10 + $(($RANDOM % 50))))
         )
         done 2>&1 | tee -a _svn-commit.log
         echo "sleeping for 3600 seconds"
