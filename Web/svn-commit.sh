c0cfc7e47a8d42d5fcea1fde8c2286f1e738c142
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-02 15:54:49 +0000

    Cleanup permissions after maintenance.

--- Web/svn-commit.sh
+++ Web/svn-commit.sh
@@ -3,7 +3,7 @@
     cd $(dirname ${0})
     while true ; do
         for d in $(ls Archive2 | sort -R) ; do
-            (set -x; cd Archive2/$d ; { svn add * 2>/dev/null && { { sleep 5 && svn ci -m "Update archive $d" ; } || { svn cleanup && sleep 5 && svn up ; } ; } ; })
+            (set -x; cd Archive2/$d ; { svn add * 2>/dev/null && { { sleep 5 && svn ci -m "Update archive $d" ; } || { svn cleanup && sleep 5 && svn up ; } ; } ; }; chown -R webserver:coliru .)
             sleep 10
         done 2>&1 | tee -a _svn-commit.log
         echo "sleeping for 3600 seconds"
