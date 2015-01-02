fff75f4a4ba01b0473b205031240788cccf9a16f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-02 16:18:32 +0000

    Improve commit script.

--- trunk/Web/svn-commit.sh
+++ trunk/Web/svn-commit.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+rp() { chown -R webserver:coliru . ; }
+
 {
     cd $(dirname ${0})
     while true ; do
-        for d in $(ls Archive2 | sort -R) ; do
-            (set -x; cd Archive2/$d ; { svn add * 2>/dev/null && { { sleep 5 && svn ci -m "Update archive $d" ; } || { svn cleanup && sleep 5 && svn up ; } ; } ; }; chown -R webserver:coliru .)
-            sleep 10
+        for d in $(ls Archive2 | sort -R) ; do (
+            set -x;
+            cd Archive2/$d 
+            svn add * ; [ "$(svn st | wc -l)" == "0" ] || {
+                { echo "I have stuff to commit!" && sleep 5 && svn ci -m "Update archive $d" && rp; } ||
+                { echo "Commit failed!" && svn cleanup && sleep 5 && svn up ; rp; }
+            }
+        )
+        sleep 10
         done 2>&1 | tee -a _svn-commit.log
         echo "sleeping for 3600 seconds"
         sleep 3600
     done
-} & disown
+}
 
