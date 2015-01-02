6cc8f901527e461274eb622bf6c981062dff8208
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-02 17:26:03 +0000

    Update archive 90

---
+++ Archive2/90/ff1e002dcc9a53/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+// assumption: zero is repeated zero times (ie. ignored)
+std::string repeat_digits( unsigned long long n )
+{ return n==0 ? "" : repeat_digits(n/10) + std::string( n%10, n%10 + '0' ) ; }
+
+int main()
+{
+    unsigned long long n ;
+    std::cin >> n ;
+    std::cout << n << " => " << repeat_digits(n) << '\n' ;
+}
