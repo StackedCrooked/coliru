e21d56f33ca0473fcae6b012a7fdb7b72c4ce409
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-02 14:12:35 +0000

    Update archive 46

---
+++ Archive2/46/d301d24623b765/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <locale>
+ 
+void try_with(wchar_t c, const char* loc)
+{
+    std::wcout << "isblank('" << c << "', locale(\"" << loc << "\")) returned " << std::boolalpha
+               << std::isblank(c, std::locale(loc)) << '\n';
+}
+int main()
+{
+    const wchar_t IDEO_SPACE = L'\u3000'; // Unicode character 'IDEOGRAPHIC SPACE'
+    try_with(IDEO_SPACE, "C");
+    try_with(IDEO_SPACE, "en_US.UTF-8");
+}
