cb67d75969591ea07d2f748ea15acda5a69ef35b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-02 23:25:15 +0000

    Update archive 85

---
+++ trunk/Archive2/85/eb874df86ed055/main.cpp
@@ -0,0 +1,25 @@
+// system_clock::now
+#include <iostream>
+#include <ctime>
+#include <ratio>
+#include <chrono>
+
+int main ()
+{
+  using namespace std::chrono;
+
+  duration<int,std::ratio<60*60*24> > one_day (1);
+
+  system_clock::time_point today = system_clock::now();
+  system_clock::time_point tomorrow = today + one_day;
+
+  time_t tt;
+
+  tt = system_clock::to_time_t ( today );
+  std::cout << "today is: " << ctime(&tt);
+
+  tt = system_clock::to_time_t ( tomorrow );
+  std::cout << "tomorrow will be: " << ctime(&tt);
+
+  return 0;
+}
