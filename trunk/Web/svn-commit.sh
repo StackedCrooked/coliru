4bce5f6b81f3fceb1ce37a31eb2a9ea76a6b5379
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-02 15:41:51 +0000

    Add svn-commit.sh.

---
+++ trunk/Web/svn-commit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+{
+    cd $(dirname ${0})
+    while true ; do
+        for d in $(ls Archive2 | sort -R) ; do
+            (set -x; cd Archive2/$d ; { svn add * 2>/dev/null && { { sleep 5 && svn ci -m "Update archive $d" ; } || { svn cleanup && sleep 5 && svn up ; } ; } ; })
+            sleep 10
+        done 2>&1 | tee -a _svn-commit.log
+        echo "sleeping for 3600 seconds"
+        sleep 3600
+    done
+} & disown
+
