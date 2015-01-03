415b7a8eab567ef6fa1d09f5762ad7b48f752b81
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-03 02:25:12 +0000

    Update archive c4

---
+++ Archive2/c4/208d3a92a4a3cb/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <sstream>
+using std::cout;
+using std::string;
+using std::endl;
+
+std::stringstream dummy("david\n4\njohn\n35");
+
+typedef struct
+{
+    int age;
+    string name;
+} people;
+
+people* register_people( people* data, int n);
+
+int main(void)
+{
+    people data[2];
+    people* info[2];
+
+    info[0] = register_people(data, 2);
+
+    cout << "Name: " << info[0]->name
+         << "\tAge: " << info[0]->age;
+    cout << "Name: " << info[1]->name
+         << "\tAge: " << info[1]->age;
+
+    return 0;
+}
+
+people* register_people( people* data, int n)
+{
+    int i;
+
+    for ( i = 0; i < n; i++ )
+    {
+        cout << "People" << i + 1 << ":" << endl;
+        cout << "   " << "name: ";
+        getline(dummy, data[i].name);
+
+        cout << "   " << "age: ";
+        dummy >> data[i].age;
+
+        dummy.ignore();
+    }
+    return data;
+}
