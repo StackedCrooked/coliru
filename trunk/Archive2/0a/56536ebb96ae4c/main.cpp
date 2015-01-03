55c5846af4032afb73897fd79431765c8d1c53bc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-03 13:03:04 +0000

    Update archive 0a

---
+++ trunk/Archive2/0a/56536ebb96ae4c/main.cpp
@@ -0,0 +1,9 @@
+
+typedef unsigned long long uint;
+constexpr uint loop(uint x, uint k) {
+    return k <= 1?x + k:loop(loop(x, k/2), k/2 + k%2);
+}
+
+template<int x>
+class X {};
+X<loop(0, -1ull)> x;
