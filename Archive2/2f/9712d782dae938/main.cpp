21608dfc995f1a97a973cba51ded4e2c5aa216af
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-03 15:24:14 +0000

    Update archive 2f

---
+++ Archive2/2f/9712d782dae938/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>     // std::cout
+#include <functional>   // std::bind
+#include <map>          // std::map
+#include <iostream>
+#include <utility>
+
+template <typename T>
+struct Bindable {   
+    void bindFunction (int x, void(T::*newFn)()) {
+        mFns.insert(std::make_pair(x,newFn));
+    }
+    void invokeFunction (int key) {
+        (static_cast<T*>(this)->*mFns.at(key))();
+    }
+
+protected:
+    std::map<int, void(T::*)()> mFns;
+};
+
+struct A : Bindable<A> {
+    void funAone (void) {
+        std::cout << "called funAone" <<std::endl;
+    }
+    void funAtwo (void) {
+        std::cout << "called funAtwo" <<std::endl;
+    }
+};
+
+struct B : Bindable<B> {
+    void funBone (void) {
+        std::cout << "called funBone" <<std::endl;
+    }
+    void funBtwo (void) {
+        std::cout << "called funBtwo" <<std::endl;
+    }
+};
+
+int main() {
+    A a;
+    B b;
+
+    a.bindFunction(1, &A::funAone);
+    a.bindFunction(2, &A::funAtwo);
+    b.bindFunction(1, &B::funBone);
+    b.bindFunction(2, &B::funBtwo);
+
+    a.invokeFunction(1);
+    a.invokeFunction(2);
+    b.invokeFunction(1);
+    b.invokeFunction(2);
+}
