dd75a0448ce870d5210e25f29852a1e7f4a95a73
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-04 00:01:58 +0000

    Update archive aa

---
+++ Archive2/aa/20c93fda064c89/main.cpp
@@ -0,0 +1,31 @@
+class ET
+{
+public:
+    template< class Type >
+    struct Component {};
+};
+
+template<class EntityManager>
+class System {
+public:
+  virtual void Update(EntityManager& entity_manager, double dt) = 0;
+protected:
+  template<typename T> using Component = typename EntityManager::template Component<T>;
+};
+
+template< class EntityManager >
+class MovementSystem : public System<EntityManager> {
+public:
+#ifdef FIXIT
+  template< class T > using Component = typename EntityManager::template Component< T >;
+#endif
+
+  virtual void Update(EntityManager& entity_manager, double dt) {
+    Component<int> position_component; // I'd like to use Component<T> here.
+  }
+};
+
+auto main() -> int
+{
+    MovementSystem< ET > ms;
+}
