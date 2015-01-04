f77076471cf48aa1f23752ecdc062974961f5738
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-04 21:12:16 +0000

    Update archive 3d

---
+++ trunk/Archive2/3d/e3582db6538808/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+class A {};
+
+void foo(const A&  a) { std::cout << "Reference" << std::endl; }
+
+void foo(      A&& a) { std::cout << "Move" << std::endl; }
+
+template <typename T, typename = decltype(foo(std::forward<T>(std::declval<T&>())))>
+void bar(T&& a)
+{
+    //Other things, which treat "a" as a const reference
+    foo(std::forward<T>(a));
+}
+
+void bar(std::string a) 
+{
+    std::cout << a << std::endl;
+}
+
+int main() {
+    A a;
+    bar(a);
+    bar(A());
+    bar("abc"); // not ambiguous
+}
