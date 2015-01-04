1a7b7f174368e90db9ef76d111e5b78ac740cf2c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-04 21:14:23 +0000

    Update archive 9f

---
+++ Archive2/9f/b93e3ca2ea42ef/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct NODE *P_NODE;
+typedef struct NODE{
+    char name[21];
+	int num;
+	P_NODE link;
+};
+
+int main()
+{
+	NODE c;
+	return 0;
+}
