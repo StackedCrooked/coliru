bc45e623c0303a390a2595e5809365e351b52247
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-04 00:00:04 +0000

    Update archive f2

---
+++ Archive2/f2/5cc0fc48f27d0d/main.cpp
@@ -0,0 +1,31 @@
+#include <utility>
+#include <thread>
+#include <iostream>
+
+typedef struct foo{
+    foo() = default;
+    void operator()(int i) const {}
+//    ~foo(){std::cout<<"dtor\n";}
+} foo;
+
+typedef struct bar{
+    bar()           {std::cout<<"bar      " << this << '\n';}
+    bar(const bar&) {std::cout<<"bar copy " << this << '\n';}
+    bar(bar&&)      {std::cout<<"bar move " << this << '\n';}
+    void operator()(const foo& f, int k) const {f(k);}
+    ~bar()          {std::cout<<"~bar     " << this << '\n';}
+} bar;
+
+int main(){
+    foo f_1, f_2;
+    bar b_1, b_2;
+    int i(0), j(0);
+    while(i++!=1){
+            std::cout << "---- 1 ----\n";
+            std::thread t(b_1, std::cref(f_1), i);
+            std::cout << "---- 2 ----\n";
+            b_2(f_2, j);
+            t.join();
+            std::cout << "---- 2 ----\n";
+    }
+}
