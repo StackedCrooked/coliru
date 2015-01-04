f2b522bb9bd75a04547a7c7d7422b889f9a139e7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-04 19:50:07 +0000

    Update archive 81

---
+++ trunk/Archive2/81/72c93b0af351eb/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <type_traits>
+
+class A {};
+
+void foo(const A&  ) { std::cout << "Reference" << std::endl; }
+
+void foo(      A&& ) { std::cout << "Move" << std::endl; }
+
+template <typename T, typename U, typename R=void>
+using enable_if_compatible = typename std::enable_if<std::is_same<U,
+                       typename std::remove_cv<typename std::remove_reference<T>::type>::type>::value, R>::type;
+    
+
+template <typename T>
+enable_if_compatible<T, A> bar(T&& a)
+{
+    //Other things, which treat "a" as a const reference
+    foo(std::forward<T>(a));
+}
+
+int main() {
+    A a;
+    bar(a);
+    bar(A());
+}
