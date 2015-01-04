96ac001b2af84c290e9e053aeae88c28331d28b2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-04 01:52:41 +0000

    Update archive 22

---
+++ Archive2/22/d25f9a93befadb/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+template<typename T>
+class Parent
+{
+    protected:
+        virtual int Show()
+        {
+            std::cout<<"Parent::Show Called\n";
+            return 0;
+        }
+};
+
+class Child : Parent<Child>
+{
+    public:
+        virtual int Show() final override
+        {
+            std::cout<<"Child::Show Called\n";
+
+            //supposed to be in a thread in my original code.
+            auto call_parent = [&] {
+                //Parent<Child>::Create(); //call other parent functions..
+                return Parent<Child>::Show();
+            };
+
+            return call_parent();
+        }
+};
+
+int main() {
+    Child().Show();
+}
