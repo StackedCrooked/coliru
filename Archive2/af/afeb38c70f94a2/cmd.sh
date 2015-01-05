51ad6ab062181f7bf55420aa347622c5df0ebd79
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-05 01:03:39 +0000

    Update archive af

---
+++ Archive2/af/afeb38c70f94a2/cmd.sh
@@ -0,0 +1,2 @@
+set -x; clang++ -std=c++1y -O2 -Wall -pedantic -pthread main.cpp && ./a.out
+g++ -std=c++1y -O2 -Wall -pedantic -pthread main.cpp && ./a.out
---
+++ Archive2/af/afeb38c70f94a2/main.cpp
@@ -0,0 +1,27 @@
+namespace Items
+{
+        class Item;
+        class HealingItem;
+        class Weapon;
+}
+//some code cut out for obvious reasons, i'll just show the basic class constructors for the classes
+class Items::Item
+{
+public:
+        Item();
+//...
+};
+class Items::HealingItem: public Items::Item
+{
+//...
+public:
+        HealingItem();
+};
+class Items::Weapon: public Items::Item
+{
+public:
+        Weapon();
+//...other properties and get/setters
+};
+
+int main() {}
