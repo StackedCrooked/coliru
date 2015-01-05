26905f8280462785fdb60acb27ba16a43ae0c978
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-05 16:56:00 +0000

    Update archive 20

---
+++ trunk/Archive2/20/dba6d2f3b0b2ef/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <thread>
+#include <list>
+#include <functional>
+#include <chrono>
+using namespace std;
+
+void outputToSomeContainer(int val){
+    //make it's implementation by youself
+	//temporary implementation:
+	cout<<val<<endl;
+}
+
+class async{
+	list<thread> a;
+public:
+	async(){}
+	async(async&& a): a(move(a.a))
+	{}
+	void addTask(function<void()>&&f){
+		a.emplace_back(move(f));
+	}
+	void wait(){
+		for(auto&& i: a)
+			i.join();
+	}
+};
+
+async async_O_n_Sort(const list<char>& unsorted, function<void(int)> outputToContainer){
+	async a;
+	for(int i: unsorted)//O(n)
+		a.addTask([i, &outputToContainer](){this_thread::sleep_for(chrono::milliseconds(5+i*10));outputToContainer(i);});
+	return a;
+}
+
+int main() {
+	list<char> unsorted {1, 0, 3, 6, 3, 4};
+	auto a = async_O_n_Sort(unsorted, outputToSomeContainer);
+	cout<<"А мы веселые пельменья, мы похоже на варенья"<<endl;
+	a.wait();
+	return 0;
+}
