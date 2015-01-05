0a32be1a951d11a6a8d5f460a8c4d52b0b8a2163
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-05 04:08:11 +0000

    Update archive 57

---
+++ trunk/Archive2/57/8be8dfdeece43e/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#if 0
+template<class T>
+T Connect(typename std::enable_if<std::is_same<bool, bool>::value, T>::type func)
+{
+    return func;
+}
+
+#else
+
+template<class T>
+typename std::enable_if<std::is_same<bool, bool>::value, T>::type Connect(T func)
+{
+    return func;
+}
+
+#endif
+
+int main()
+{
+    Connect([]{});
+}
