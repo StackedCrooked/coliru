7524b71871f8581bc525e7bbb69e4c3eef0cd641
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-05 11:53:39 +0000

    Update archive ec

---
+++ Archive2/ec/8b42c073a7c963/main.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <unordered_set>
+#include <numeric>
+#include <type_traits>
+
+template <typename Iterator1, typename Iterator2>
+typename std::enable_if<
+   std::is_same<typename std::iterator_traits<Iterator2>::iterator_category, std::random_access_iterator_tag>::value
+   >::type
+sorty(Iterator1 first1, Iterator1 last1, Iterator2 first2)
+{
+    // O(N*lgN) complexity sorting
+}
+
+template <typename T> struct AlwaysFalse : std::false_type {};
+
+template <typename Iterator1, typename Iterator2>
+typename std::enable_if<
+    !std::is_same<typename std::iterator_traits<Iterator2>::iterator_category, std::random_access_iterator_tag>::value
+    >::type
+sorty(Iterator1 first1, Iterator1 last1, Iterator2 first2)
+{
+    // other sorting algorithm or print out a user-friendly error
+    static_assert(AlwaysFalse<Iterator2>{}, "Iterator2 must be random-access iterator!");
+}
+
+int main()
+{
+    std::vector<int> toOrder(10);
+    std::iota(toOrder.begin(), toOrder.end(), 0);
+
+    std::vector<double> orderBy{0.2, 9.8,  4.0,  0.01,  15.1,
+                                3.3, 9.01, 9.11, 100.1, 2.03};
+
+    std::unordered_set<double> orderBy_s(orderBy.begin(), orderBy.end());
+    
+    sorty(toOrder.begin(), toOrder.end(), orderBy.begin());
+        
+    sorty(toOrder.begin(), toOrder.end(), orderBy_s.begin());
+}
