b9e432d825ec2e63b9970d55935fd3a58ca567b3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-05 23:46:26 +0000

    Update archive ac

---
+++ trunk/Archive2/ac/90b80c775fabc2/main.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <initializer_list>
+
+class example
+{
+private:
+    int m_sum;
+public:
+    example(std::initializer_list<int> parameters)
+    {
+        m_sum = std::accumulate(std::begin(parameters), std::end(parameters), 0);
+    }
+
+    int get_sum() const
+    {
+        return m_sum;
+    }
+};
+
+int main() {
+
+    example e = { 1, 2, 3, 4};
+    std::cout << e.get_sum();
+    return 0;
+}
