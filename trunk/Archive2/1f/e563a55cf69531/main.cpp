780dbcb0d7f771372db5e4ca0bbf9aae87468f22
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-05 17:57:56 +0000

    Update archive 1f

---
+++ trunk/Archive2/1f/e563a55cf69531/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <thread>
+#include <list>
+#include <functional>
+using namespace std;
+
+void outputToSomeContainer(int val){
+    //make it's implementation by youself
+	//temporary implementation:
+	cout<< val<<endl;
+}
+
+class async{
+	list<thread> a;
+public:
+	async(){}
+	async(async&& a): a(move(a.a))
+	{}
+	void addTask(function<void()>&&f){
+		a.emplace_back(move(f));
+	}
+	void wait(){
+		for(auto&& i: a)
+			i.join();
+	}
+};
+
+async async_O_n_Sort(const list<char>& unsorted, function<void(int)> outputToContainer){
+	async a;
+	for(int i: unsorted)
+		a.addTask([i, &outputToContainer](){outputToContainer(i);});
+	return a;
+}
+
+int main() {
+	list<char> unsorted {1, 0, 3, 6, 3, 4};
+	auto a = async_O_n_Sort(unsorted, outputToSomeContainer);
+	cout<<"А мы веселые пельменья, мы похоже на варенья"<<endl;
+	a.wait();
+	return 0;
+}
