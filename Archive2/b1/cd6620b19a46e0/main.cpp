714a48bcb09106b071d93f8fde3ebcc2cdd1b234
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-06 13:41:29 +0000

    Update archive b1

---
+++ Archive2/b1/cd6620b19a46e0/main.cpp
@@ -0,0 +1,37 @@
+#define BOOST_SPIRIT_USE_PHOENIX_V3
+
+#include <boost/range.hpp>
+#include <boost/range/adaptors.hpp>
+#include <boost/range/algorithm.hpp>
+
+using namespace boost::adaptors;
+
+#include <iostream>
+
+enum crdType { CARTESIAN =0, SPHERICAL, CUSTOM1, CUSTOM2 };
+
+typedef std::pair<int,int> coordinate;
+
+struct customCRD {
+    std::vector<coordinate> Corners;
+    crdType type;
+
+    struct havingCorner {
+        coordinate _a;
+        havingCorner(coordinate a) : _a(a) {}
+        bool operator()(customCRD const& cc) const { return cc.Corners.end()!=std::find(cc.Corners.begin(), cc.Corners.end(), _a); }
+    };
+};
+
+int main() {
+    std::vector<customCRD> const v;
+    //{ 
+        //customCRD { {  {1,  2},  {3,  4},  {5,  6} }, CARTESIAN },
+        //customCRD { {  {7,  8},  {9, 10}, {11, 12} }, SPHERICAL },
+        //customCRD { { {13, 14}, {15, 16}, {17, 18} },   CUSTOM2 },
+    //};
+
+    std::cout << "{9,10} found: " << std::boolalpha << !boost::empty(v | filtered(customCRD::havingCorner(coordinate(9,10)))) << "\n";
+    std::cout << "{3,5}  found: " << std::boolalpha << !boost::empty(v | filtered(customCRD::havingCorner(coordinate(3, 5)))) << "\n";
+}
+
