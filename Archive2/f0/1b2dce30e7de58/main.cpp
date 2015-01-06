bfaeb0aee88ac2c0a4ef431f9268afe4b1516bad
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-06 23:12:22 +0000

    Update archive f0

---
+++ Archive2/f0/1b2dce30e7de58/main.cpp
@@ -0,0 +1,105 @@
+#include <boost/bind.hpp>
+#include <boost/icl/interval_set.hpp>
+#include <boost/tuple/tuple_comparison.hpp>
+#include <iomanip>
+#include <iostream>
+#include <map>
+
+namespace icl = boost::icl;
+
+template<typename T> using rw = boost::reference_wrapper<T>;
+
+using Section = std::string;
+using Page    = int;
+using Chapter = int;
+using Pages   = icl::interval_set<Page>::type;
+
+struct Module {
+    Section section;
+    Chapter chapter;
+
+    bool operator<(Module const& o) const { return boost::tie(section,chapter) < boost::tie(o.section,o.chapter); }
+};
+
+using Table = std::map<Module, Pages>;
+
+static inline std::ostream& operator<<(std::ostream& os, Table::value_type const& p) {
+    return os << p.first.section << "\t" << p.first.chapter << "\t" << p.second;
+}
+
+int main()
+{
+    std::cout << std::unitbuf;
+    Table table;
+
+    {
+        struct Fill { Section s; Chapter c; Page p; };
+        for (auto& tup : std::vector<Fill> { 
+            { "Jugement",    18 , 2 },
+            { "Conclusions", 11 , 1 },
+            { "Assignation", 43 , 1 },
+            { "Assignation", 43 , 2 },
+            { "Conclusions", 10 , 1 },
+            { "Jugement",    17 , 3 },
+            { "Assignation", 45 , 1 },
+            { "Jugement",    17 , 1 },
+            { "Conclusions", 9  , 1 },
+            { "Jugement",    17 , 2 },
+            { "Jugement",    18 , 1 },
+        })
+        {
+            table[{tup.s, tup.c}] += tup.p; // add page to (existing) range
+        }
+    }
+
+    std::cout << "------------- table: \n";
+    for (auto& r:table)
+        std::cout << r << "\n";
+    
+    {
+        using rv = rw<Table::value_type>;
+        std::vector<rv> vw(begin(table), end(table));
+
+        // blind shuffle
+        srand(time(0));
+        std::random_shuffle(vw.begin(), vw.end());
+
+        // try to avoid subsequent modules from equal sections (dup)
+        auto dup     = [](rv a, rv b) { return a.get().first.section == b.get().first.section; };
+        auto it      = vw.begin();
+        auto const e = vw.end();
+
+        while(it != e) // bit redundant, could be while(true)
+        {
+            std::cout << "------------- STATE: \n";
+            for (auto& rv:vw)
+                std::cout << rv.get() << (it->get_pointer() == rv.get_pointer()? "*\n":"\n");
+
+            it = std::adjacent_find(it, e, dup);
+            if (it == e) 
+                break;
+            
+            std::cout << "------------- dupes: \n";
+            std::cout << "\t" << (it+0)->get() << "\n";
+            std::cout << "\t" << (it+1)->get() << "\n";
+
+            auto m = std::find_if(it+1, e, [&] (rv r) { return r.get().first.section != it->get().first.section; });
+
+            if (m == e)
+            {
+                it = m;
+            } else
+            {
+                std::cout << "------------- rotating to: \n";
+                std::cout << "\t" << m->get() << "\n";
+
+                std::rotate(it+1, m, e);
+
+                it = std::adjacent_find(it, e, dup);
+            }
+        }
+        std::cout << "------------- selection: \n";
+        for (auto& r : vw)
+            std::cout << r.get() << "\n";
+    }
+}
