e9d5b111b3bd8aaa7fcb09f7ca4e6f4550cc560c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-06 01:16:10 +0000

    Update archive 04

---
+++ Archive2/04/f01a2b9e70fa8b/main.cpp
@@ -0,0 +1,15 @@
+int check()
+{
+    extern int a[3][3][3];
+    for(int i=0; i<3; i++) {
+       for(int j=0; j<3; j++) {
+           for(int k=0; k<3; k++) {
+              int v=a[i][j][k];
+              if ( v == 0 ) return 0;
+              if ( v == 1 ) goto skip;
+           }
+       }
+       skip: ;
+    }
+    return 1;
+}
