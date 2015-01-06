31f3b0a3b3ab0a25148173f9f863c1945c243f50
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-06 13:28:17 +0000

    Update archive cf

---
+++ Archive2/cf/3d084020ab7e8a/main.cpp
@@ -0,0 +1,30 @@
+#include <memory>
+
+template<class T>
+struct shared_stack_node {
+    T head;
+    std::shared_ptr<shared_stack_node> tail;
+};
+
+template<class T>
+class shared_stack {
+private:
+    std::shared_ptr<shared_stack_node> data_;
+    
+public:
+    bool empty() const { return !data_; }
+    const T& top() const { return data_->head; }
+    void pop() { data_ = data_->tail; }
+    void push(const T& value) {
+        data_ = std::make_shared<shared_stack_node>(value, data_);
+    }
+};
+
+...
+class tree_iterator {
+    ...
+    shared_stack<tree_node*> stack;
+    ...
+};
+
+
