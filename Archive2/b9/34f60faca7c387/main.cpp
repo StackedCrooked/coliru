3542a840b92ce034caef2e9835a28de852e698b9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-07 19:37:07 +0000

    Update archive b9

---
+++ Archive2/b9/34f60faca7c387/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+template<typename R, class...A> 
+R (*trap_gen(R(*f)(A...)))(A...)
+{
+    static auto g = f;
+
+    return [](A... a) 
+    {
+        try {
+            return g(a...);
+        } catch (...) {
+            return std::is_integral<R>::value ? static_cast<R>(42)
+                                              : static_cast<R>(3.14); 
+        }
+    };
+}
+
+int add(int a, int b)
+{
+    return a+b;
+}
+
+int mul(int a, int b)
+{
+    return a*b;
+}
+
+int main() {
+    int(*f)(int, int) = trap_gen<>(add);
+    int(*f2)(int, int) = trap_gen<>(add);
+    std::cout << f(2, 3) << std::endl;
+    std::cout << f2(2, 3) << std::endl;
+
+    return 0;
+}
