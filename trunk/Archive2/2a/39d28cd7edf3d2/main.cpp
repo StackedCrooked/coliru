537886ecd4db62cff245e821666775baffe3aa0f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-07 16:17:43 +0000

    Update archive 2a

---
+++ trunk/Archive2/2a/39d28cd7edf3d2/main.cpp
@@ -0,0 +1,70 @@
+#include <type_traits>
+
+template <typename F>
+struct return_type_impl;
+    
+template <typename R, typename... Args>
+struct return_type_impl<R(Args...)> { using type = R; };
+
+template <typename R, typename... Args>
+struct return_type_impl<R(*)(Args...)> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type_impl<R(C::*)(Args...)> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type_impl<R(C::*)(Args...) &> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type_impl<R(C::*)(Args...) &&> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type_impl<R(C::*)(Args...) const> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type_impl<R(C::*)(Args...) const&> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type_impl<R(C::*)(Args...) const&&> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type_impl<R(C::*)(Args...) volatile> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type_impl<R(C::*)(Args...) volatile&> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type_impl<R(C::*)(Args...) volatile&&> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type_impl<R(C::*)(Args...) const volatile> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type_impl<R(C::*)(Args...) const volatile&> { using type = R; };
+
+template <typename R, typename C, typename... Args>
+struct return_type_impl<R(C::*)(Args...) const volatile&&> { using type = R; };
+
+template <typename T, typename = void>
+struct return_type : return_type_impl<typename std::remove_reference<T>::type> {};
+
+template <typename T>
+struct return_type<T, decltype(void(&T::operator()))> : return_type_impl<decltype(&T::operator())> {};
+
+
+template <typename F>
+void test(F h)
+{
+    typename return_type<F>::type i = 1; (void)i;
+    static_assert(std::is_same<typename return_type<F>::type, int>{}, "!");
+}
+
+int a_function(int i)
+{ 
+    return 2*i; 
+}
+
+int main()
+{
+    test(a_function);
+}
