b121bf2e8090fe71af87ba891dd11d228475c6cb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-07 05:06:58 +0000

    Update archive 13

---
+++ trunk/Archive2/13/55f01c100b14d7/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+template <typename T, T nontype>
+struct CL {
+    void call() { nontype(123); }
+};
+
+void f(int n) {
+    std::cout << n << std::endl;
+}
+CL<void(*)(int), f> cl7;
+
+using df = decltype(&f);
+CL<df, f> cl8; // << error
+
+int main() {
+    cl8.call();
+}
