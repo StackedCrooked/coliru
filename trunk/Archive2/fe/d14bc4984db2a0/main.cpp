8aebfa8b586f7d85c9ee21c408ec68cd8092aad7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-07 09:20:49 +0000

    Update archive fe

---
+++ trunk/Archive2/fe/d14bc4984db2a0/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+struct Singleton
+{
+public:   
+    static Singleton& getInstance();
+	void showMessage() const ; // ****
+	int i;
+
+private: 
+	Singleton(){i = 7; };
+	
+	// Singleton(const Singleton &) = delete;  
+	// Singleton(Singleton&&) = delete; 
+    Singleton(const Singleton &) ; // declared private, not defined
+	
+	
+	// Singleton& operator=(const Singleton&) = delete;
+	// Singleton& operator=(Singleton&&) = delete;
+	Singleton& operator=(const Singleton&) ; // declared private, not defined
+};
+
+const int bg = Singleton::getInstance().i ;
+
+Singleton & Singleton::getInstance()
+{
+  // caveat: legacy c++ is not concurrency aware
+  // there could be a race-condition if multiple threads are involved
+  static Singleton  Instance; 
+  return Instance;
+}
+
+void Singleton::showMessage() const // ****
+{
+   std::cout << "Hello World, this is a Meyer Singleton Design Pattern\n" ; // << std::endl;
+}
+
+int g = Singleton::getInstance().i;
+
+int main()
+{
+
+	std::cout << bg << ' ' << g << '\n' ; // std::endl;
+    Singleton::getInstance().showMessage() ;
+
+	// system("Pause");
+    // return 0;
+}
