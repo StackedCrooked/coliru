97bb5748a385e682c81fd4e12283902f472c8821
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-07 01:30:08 +0000

    Update archive b1

---
+++ Archive2/b1/4050a81f3f07a8/main.cpp
@@ -0,0 +1,29 @@
+    protected:
+        // overloads for a new-style class (notice that each method gets its own handler)
+        template <F0 f> static void add_method( const char* name, const char* doc="" )  { add_method( name, f, PCF(&handler<f>), doc ); }
+        template <F1 f> static void add_method( const char* name, const char* doc="" )  { add_method( name, f, PCF(&handler<f>), doc ); }
+        template <F2 f> static void add_method( const char* name, const char* doc="" )  { add_method( name, f, PCF(&handler<f>), doc ); }
+
+    private:
+
+        static FinalClass* final(PyObject* o) { return cxxObject_for<FinalClass>(o); }
+
+        static PyObject* handlerX( int i, std::function<Object()> lambda )
+        {
+            try
+            {
+                COUT( i );
+                return charge( *lambda() ); // feed charged ref back to Python
+            }
+            catch( Exception & )
+            {
+                COUT( "! ! !  Exception Python calling new-style-class handler ! ! !" );
+                return 0;
+            }
+        }
+
+        #define P PyObject*
+        template< F0 f > static P handler( P o, P   )      { return handlerX( 0, [&] ()->Object { return (final(o) ->* f)(                         ); }  ); }
+        template< F1 f > static P handler( P o, P a )      { return handlerX( 1, [&] ()->Object { return (final(o) ->* f)( to_tuple(a)             ); }  ); }
+        template< F2 f > static P handler( P o, P a, P k ) { return handlerX( 2, [&] ()->Object { return (final(o) ->* f)( to_tuple(a), to_dict(k) ); }  ); }
+        #undef P
