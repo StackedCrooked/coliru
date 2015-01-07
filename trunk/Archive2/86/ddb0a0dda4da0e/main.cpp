86ae3a5a4fda93f2ad3cb87d321eda537ecf01f4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-07 16:28:36 +0000

    Update archive 86

---
+++ trunk/Archive2/86/ddb0a0dda4da0e/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <functional>
+#include <type_traits>
+
+struct Base
+{
+    int f53(short, char) { return 123; }
+    int f54(short, char, double) { throw 1; }
+} base;
+
+Base* get_base(int) { return &base; }
+
+template <typename T, T t>
+struct trap
+{
+    template <typename... Args>
+    auto operator()(int s, Args&&... args)
+        -> decltype((get_base(s)->*t)(std::forward<Args>(args)...))
+    {
+        using R = decltype((get_base(s)->*t)(std::forward<Args>(args)...));
+        try
+        {
+            return (get_base(s)->*t)(std::forward<Args>(args)...);
+        }
+        catch (...)
+        {
+            return std::is_integral<R>::value ? static_cast<R>(42) : static_cast<R>(3.14); 
+        }
+    }
+};
+
+struct Foo
+{
+    struct Table
+    {
+        std::function<int(int, short, char)> fp_53;
+        std::function<int(int, short, char, double)> fp_54;
+    }* table = new Table;
+    
+    void load53()
+    {
+        table->fp_53 = trap<decltype(&Base::f53), &Base::f53>{};
+    }
+    
+    void load54()
+    { 
+        table->fp_54 = trap<decltype(&Base::f54), &Base::f54>{};
+    }
+};
+
+int main()
+{
+    Foo f{};
+    f.load53();
+    f.load54();
+        
+    std::cout << f.table->fp_53(1, 2, 'a') << std::endl;
+    std::cout << f.table->fp_54(3, 4, 'b', 2.72) << std::endl;
+}
