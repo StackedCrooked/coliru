020ef1422da6565d322fd9e2ffe72098ba0fc91d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-08 20:33:00 +0000

    Update archive 65

---
+++ Archive2/65/a111fa84edf388/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <initializer_list>
+
+template <typename T>
+void func(T t) 
+{
+    std::cout << t << std::endl ;
+}
+
+template<typename T, typename... Args>
+void func(T t, Args... args) // recursive variadic function
+{
+    std::cout << t <<std::endl ;
+
+    func(args...) ;
+}
+
+template <class T>
+void func2( std::initializer_list<T> list )
+{
+    for( auto elem : list )
+    {
+        std::cout << elem << std::endl ;
+    }
+}
+
+int main()
+{
+    std::string
+        str1( "Hello" ),
+        str2( "world" );
+
+    func(1,2.5,'a',str1);
+
+    func2( {10, 20, 30, 40 }) ;
+    func2( {str1, str2 } ) ;
+}
