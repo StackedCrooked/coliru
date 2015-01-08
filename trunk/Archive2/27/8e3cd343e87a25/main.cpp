f92b472e03d15c374c14ce16d72991f1c86ddbec
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-08 20:35:15 +0000

    Update archive 27

---
+++ trunk/Archive2/27/8e3cd343e87a25/main.cpp
@@ -0,0 +1,17 @@
+#include <map>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+    std::map<int, std::string> myMap = { { 2, "foo" }, { 3, "bar" } };
+    
+    std::vector<std::string> vec(myMap.size());
+    std::transform(myMap.begin(), myMap.end(), std::back_inserter(vec), [](const std::pair<int, std::string>& entry) { return entry.second; });
+    
+    for (auto& s : vec) { std::cout << s << " "; }
+    
+    return 0;
+}
