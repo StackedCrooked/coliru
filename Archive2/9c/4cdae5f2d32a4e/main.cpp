46f16e6779fed4a4266b6423f2fc09bbb000743a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-08 16:05:05 +0000

    Update archive 9c

---
+++ Archive2/9c/4cdae5f2d32a4e/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::ostream& operator<<(std::ostream& os, const std::vector<int>& vec)
+{
+    std::vector<int>::const_iterator iter = vec.begin();
+    for (; iter != vec.end(); ++iter)
+    {
+        os << *iter << ' ';
+    }
+    return os;
+}
+
+class obj
+{
+    public:
+    obj(int x): mX(x){ std::cout << "create " << mX <<std::endl;}
+    obj(const obj& other) { mX = other.mX; std::cout << "copy " << mX << std::endl;}
+    int mX;
+};
+
+std::vector<obj> getAllData()
+{
+    std::vector<obj> data;
+    data.push_back(9);
+    data.push_back(33);
+    data.push_back(12);
+    data.push_back(54);
+    std::cout << "returning" << std::endl;
+    return data;
+}
+
+int main()
+{
+    std::vector<obj> proba;
+    proba = getAllData();
+}
