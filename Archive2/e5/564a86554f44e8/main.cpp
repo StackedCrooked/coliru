8515f4c44cffba2d19badce790371e127ce816db
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-08 14:32:12 +0000

    Update archive e5

---
+++ Archive2/e5/564a86554f44e8/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+std::vector<int> getAllData()
+{
+    std::vector<int> data{9, 33, 12, 54};
+    return data;
+}
+
+int main()
+{
+    std::vector<std::string> words = {
+        "Hello", "from", "GCC", __VERSION__, "!"
+    };
+    std::cout << words << std::endl;
+    
+    std::vector<int> proba = getAllData();
+    
+    std::cout << proba << std::endl;
+}
