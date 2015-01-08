a9d714ce9b14a7befdb2d24cea1d01ae5d029bee
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-08 15:21:37 +0000

    Update archive 4a

---
+++ Archive2/4a/4dcaa443195748/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cctype>
+#include <map>
+
+void freq( const char* cstr ) // *** [b]const[/b]
+{
+    if(cstr)
+    {
+        std::map<char,int> freq ;
+        for( ; *cstr ; ++cstr ) if( std::isalpha(*cstr) ) ++freq[ std::tolower(*cstr) ] ;
+        for( auto pair : freq ) std::cout << pair.first << ' ' << pair.second << '\n' ;
+    }
+}
+
+int main()
+{
+    freq( "We accept reality so readily, perhaps because we sense that nothing is real." ) ;
+}
