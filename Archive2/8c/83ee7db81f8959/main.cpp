90cd5f35b02cfaeeba46acbe6d7c032f938d910b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-08 23:54:43 +0000

    Update archive 8c

---
+++ Archive2/8c/83ee7db81f8959/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+template <typename Base>
+struct VariantMap;
+
+struct FruitMap;
+
+template <typename Base>
+struct VariantMap
+{
+    template <typename TValue>
+    struct Key {};
+    
+    template <typename TValue>
+    TValue& get(typename Base::template Key<TValue> base_key) {
+        // TODO: impl
+        return TValue();
+    }
+};
+
+struct FruitMap : VariantMap<FruitMap> {
+    template <typename T>
+    struct Key : VariantMap<FruitMap>::Key<T> {};
+    
+    Key<int> Apple;
+    Key<double> Orange;
+};
+
+
+int main() {
+  FruitMap f;
+  
+  int apple_value = f.get(FruitMap.Apple);
+  
+  return 0;
+}
