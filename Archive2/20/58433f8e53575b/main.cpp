7184ad57a516fce8e6bb2c34299f5c65f25abca8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-09 15:31:35 +0000

    Update archive 20

---
+++ Archive2/20/58433f8e53575b/main.cpp
@@ -0,0 +1,21 @@
+#include <type_traits>
+#include <limits>
+
+template <typename T>
+struct inferred_integer
+{
+    typedef T data_t;
+};
+
+template<
+    typename Integer,
+    std::enable_if_t<
+        (  std::numeric_limits<Integer>::is_integer
+        && std::is_convertible<
+            Integer, typename inferred_integer<Integer>::data_t // <-- typename
+        >::value )
+    , int> = 0
+>
+inferred_integer<Integer> from_int(const Integer& i) {
+    return inferred_integer<Integer>(i);
+}
