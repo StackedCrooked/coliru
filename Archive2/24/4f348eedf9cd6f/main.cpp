43c051941b9c1df2b645001c3a42251267dc384a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-09 23:33:33 +0000

    Update archive 24

---
+++ Archive2/24/4f348eedf9cd6f/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> foo(10);
+    int idx = -1;
+    int start_idx = -1;
+    if (idx >= start_idx + foo.size()) {
+        std::cout << "You gotta be kidding me!\n";
+    }    
+}
