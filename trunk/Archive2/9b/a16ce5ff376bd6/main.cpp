014b9c337d49553104a08733ab4a7193f4f67433
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-09 01:43:58 +0000

    Update archive 9b

---
+++ trunk/Archive2/9b/a16ce5ff376bd6/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <future>
+#include <boost/thread.hpp>
+#include <vector>
+#include <functional>
+
+namespace NS = boost;
+
+static NS::mutex mtx;
+NS::unique_lock<NS::mutex> lock() {
+    return NS::unique_lock<NS::mutex>{mtx};
+}
+
+int main()
+{
+    std::vector<std::function<void()>> messages;
+    NS::atomic<bool> running{true};
+
+    NS::thread thread([&]{
+        while (running) {
+            NS::this_thread::yield();
+            std::vector<std::function<void()>> own;
+            {
+                //auto lk = lock();
+                NS::lock_guard<NS::mutex> lk(mtx);
+                own.swap(messages);
+            }
+            std::cout << "[T] locked with " << own.size() << " messages." << std::endl;
+            for (auto& fn: own) {
+                fn();
+            }   
+        }   
+    }); 
+
+    for (int i = 0; i < 1000000; ++i) {
+        NS::packaged_task<int> task([=]{
+            std::cout << "[T] returning " << i << std::endl;
+            return i;
+        }); 
+
+        {   
+            //auto lk = lock();
+            NS::lock_guard<NS::mutex> lk(mtx);
+            messages.emplace_back(std::bind(std::ref(task)));
+        }   
+
+        task.get_future().get();
+    }   
+
+    running = false;
+    thread.join();
+}
