d8530fd33a7246266b5e49746c1d4fedea2de403
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-09 09:37:05 +0000

    Update archive 48

---
+++ trunk/Archive2/48/1fafcc022d7587/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <iterator>
+
+using namespace std;
+
+int main()
+{   
+    int v[]{1,7,4,2,9,7};
+    
+    sort(v.begin(),v.end());
+    
+     
+    return 0;
+}
