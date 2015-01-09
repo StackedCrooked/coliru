022137d1e1ac215a86c879b5aac1c3c73c917d7e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-09 15:25:59 +0000

    Update archive ea

---
+++ Archive2/ea/067403cf56b001/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int x = 1;
+    int y = 2;
+    
+    // :> ??! ??/
+    if(x == 1 and y != 1)
+    {
+        std::cout << "hi" << std::endl;   
+    }
+}
