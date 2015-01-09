db81dc0b11628c4ae08ccc085e1f5d517dccde91
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-09 02:13:07 +0000

    Update archive 47

---
+++ trunk/Archive2/47/a6d4df564c6c8b/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <exception>
+#include <string>
+#include <stdexcept>
+
+template<class Ret>
+class return_type
+{
+    union
+    {
+        Ret ret_value;
+        std::exception_ptr eptr;
+    };
+    bool value_tag;
+public:
+    return_type(Ret&& value) : ret_value(std::forward<Ret>(value)), value_tag(true) {}
+    return_type(std::exception_ptr e) : eptr(e), value_tag(false) {}
+    return_type(const return_type& other) : value_tag(other.value_tag) { if(value_tag) new(&ret_value) Ret(other.ret_value); else eptr = other.eptr; }
+    ~return_type() { if(value_tag) ret_value.~Ret(); }
+    
+    auto get() const -> decltype(ret_value) { if(value_tag) return ret_value; else std::rethrow_exception(eptr); }
+    auto get() -> decltype(ret_value) { if(value_tag) return ret_value; else std::rethrow_exception(eptr); }
+    
+    bool has_value() const { return value_tag; }
+};
+
+template<class Function, class ...Args>
+auto safe_call(Function fn, Args&&... args) -> return_type<decltype(fn(std::forward<Args>(args)...))>
+try
+{
+    return fn(std::forward<Args>(args)...);
+}
+catch(...)
+{
+    return std::current_exception();
+}
+
+std::string good(const std::string& s) { return s; }
+
+std::string throwing(const std::string& s) { if(s != "Life is good") throw std::runtime_error("bad vibes"); return s; }
+
+
+int main() try
+{
+    std::cerr << safe_call(good, "Hello, World!\n").get();
+    
+    auto result = safe_call(throwing, "Life is good");
+    std::cerr << "It didn't throw: " << result.get() << "\n";
+    
+    auto bad_result = safe_call(throwing, "Puke!\n");
+    
+    if(!bad_result.has_value())
+    {
+        std::cerr << "result has no value, and if you try to access it ... \n";
+        // It's going to throw now!
+        std::cerr << bad_result.get();
+    }
+}
+catch(std::exception& s)
+{
+    std::cerr << "\nCaught exception: " << s.what() << "\n";
+}
