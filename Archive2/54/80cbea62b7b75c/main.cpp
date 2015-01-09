9316b8f31e58f89fe2929b6ff4f57c87498acc14
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-09 15:37:55 +0000

    Update archive 54

---
+++ Archive2/54/80cbea62b7b75c/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <functional>
+#include <algorithm>
+
+std::pair<int, int> get_traffic_configuration(int mps)
+{
+  std::vector<std::pair<int, int>> ts;
+  for (int i = 2; i < 1000; ++i)
+  {
+    int period = std::max(mps / i, 10);
+    int tick_per_min = 60000 / period;
+    int mpt = std::max(mps * 60 / tick_per_min, 1);
+
+    ts.emplace_back(std::make_pair(mpt, period));
+  }
+  
+  for (int i = 10; i < 1000; ++i)
+  {
+    int mpt = std::max(mps / i, 1);
+    int period = i;
+
+    ts.emplace_back(std::make_pair(mpt, period));
+  }
+
+  auto p = min_element(begin(ts), end(ts), [=] (std::pair<int, int> a, std::pair<int, int> b) {
+      return std::abs(mps * 60 - a.first * (int)(60000 / a.second)) < std::abs(mps * 60 - b.first * (int)(60000 / b.second));
+  });
+
+  return *p;
+}
+
+int main()
+{
+  auto f = [] (int i)
+  {
+    auto v1 = get_traffic_configuration(i);
+    std::cout << "(" << v1.first << ", " << v1.second << ") :" << std::abs(i - v1.first * (int)(1000 / v1.second))  << std::endl;
+  };
+
+  f(162);
+  f(340);
+  f(299);
+  f(7);
+  f(28);
+}
