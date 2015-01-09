4ddadb9eb83b20014108fcfc6740aed322f39df9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-09 20:54:28 +0000

    Update archive b2

---
+++ Archive2/b2/1d82dcf077fa48/cmd.sh
@@ -0,0 +1,2 @@
+// Quiz: rock, paper and scissors
+g++-4.8 -std=c++11 -O2 -pthread main.cpp && ./a.out ; echo $?
---
+++ Archive2/b2/1d82dcf077fa48/main.cpp
@@ -0,0 +1,10 @@
+// Quiz: what's the exit code of this program? (and do you understand why)
+
+auto rock = 1;
+auto paper = 2;
+auto scissors = 3;
+
+int main()
+{
+    return rock, paper and scissors;
+}
