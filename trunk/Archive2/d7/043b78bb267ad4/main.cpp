d55827f929f6ab7ca23080212401289578667a40
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-09 02:34:36 +0000

    Update archive d7

---
+++ trunk/Archive2/d7/043b78bb267ad4/main.cpp
@@ -0,0 +1,29 @@
+#include <functional>
+#include <boost/optional.hpp>
+
+template <typename T, typename Factory>
+struct Lazy {
+    template <typename F> Lazy(F&& f) : _factory(std::forward<F>(f)) { }
+
+    T& get()             { return *(_value? _value : _value = _factory()); }
+    T const& get() const { return *(_value? _value : _value = _factory()); }
+
+    operator bool() const { return _value; }
+
+  private:
+    Factory _factory;
+    boost::optional<T> _value;
+};
+
+
+
+template <typename T, typename F>
+    Lazy<T,F> make_lazy(F&& f) { return Lazy<T,F>(std::forward<F>(f)); }
+
+#include <string>
+#include <iostream>
+
+int main() {
+    const auto ls = make_lazy<std::string>([] { return "Hello world"; });
+    std::cout << ls.get() << "\n";
+}
