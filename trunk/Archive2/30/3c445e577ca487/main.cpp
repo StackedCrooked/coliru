1135310abc1320e02afd80108a097bd882cd0c85
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-09 21:36:49 +0000

    Update archive 30

---
+++ trunk/Archive2/30/3c445e577ca487/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+
+struct Item
+{   
+    Item& operator=(const Item&) &
+    {
+        std::cout << "4";
+        return *this;
+    }
+    
+    Item& operator=(const Item&) &&
+    {
+        std::cout << "3";
+        return *this;
+    }
+    
+    Item& operator=(Item&&) &
+    {
+        std::cout << "2";
+        return *this;
+    }
+    
+    Item& operator=(Item&&) &&
+    {
+        std::cout << "1";
+        return *this;
+    }
+};
+
+
+int main()
+{
+         Item() =      Item();
+         Item() = *new Item();
+    *new Item() =      Item();
+    *new Item() = *new Item();
+}
