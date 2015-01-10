b8c4f5e286dfc0364262103e863a346698c1b679
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-10 20:58:33 +0000

    Update archive fe

---
+++ trunk/Archive2/fe/9168d7f1eb3886/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <cstdint>
+#include <iomanip>
+
+int main( int argc, char* argv[] )
+{
+    const std::size_t N = 16 ;
+    const std::size_t NHEXDIGITS = N * 2 ;
+
+    if( argc != 2 )
+    {
+        std::cerr << "usage: " << argv[0] << " <" << N*8 << "-bit value as " << NHEXDIGITS << " hex digits>\n" ;
+        return 1 ;
+    }
+
+    std::string hex = argv[1] ;
+    if( hex.size() > NHEXDIGITS )
+    {
+        std::cerr << "too many hex digits\n" ;
+        return 1 ;
+    }
+
+    std::cout << "command line argument: " << hex << '\n' ;
+    // if less than N digits, pad with zeroes on the left
+    if( hex.size() < NHEXDIGITS ) hex = std::string( NHEXDIGITS - hex.size(), '0' ) + hex ;
+
+    std::uint8_t octets[N]{} ;
+
+    for( std::size_t i = 0 ; i < N ; ++i )
+    {
+        try
+        {
+            // convert each set of two characters to an integer (octet) 
+            // http://en.cppreference.com/w/cpp/string/basic_string/stoul
+            octets[i] = std::stoul( hex.substr( i*2, 2 ), nullptr, 16 ) ;
+        }
+        catch( const std::exception& )
+        {
+            std::cerr << "command line parse error\n" ;
+            return 1 ;
+        }
+    }
+
+    std::cout << "octets: " << std::hex ;
+    for( unsigned int v : octets ) std::cout << std::setw(2) << std::setfill('0') << v << ' ' ;
+    std::cout << '\n' ;
+}
