8d05073dc858c6903f063655d560f6c600d387e1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-10 23:18:21 +0000

    Update archive e9

---
+++ trunk/Archive2/e9/dd7411c71a0f18/cmd.sh
@@ -0,0 +1,1 @@
+set -x; mv main.cpp main.c ; gcc -std=c89 -O0 -Wall -Wextra -pedantic -pthread main.c && ./a.out abc
---
+++ trunk/Archive2/e9/dd7411c71a0f18/main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main(void) {
+	const int  STR_NAME = 5;
+    char strName[STR_NAME] = "abc";
+    printf("%s", strName);
+	return 0;
+}
