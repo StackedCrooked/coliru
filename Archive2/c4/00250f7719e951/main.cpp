b9f6feff371f48c05690d8e78ec4477c1e2fd85e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-10 17:44:18 +0000

    Update archive c4

---
+++ Archive2/c4/00250f7719e951/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+//array<array<int,1>,5> f() { return {array<int,1>{1},array<int,1>{2},array<int,1>{3},array<int,1>{4},array<int,1>{5}}; }
+array<array<int,1>,5> f() { return { {{ { 1 } },{ { 2 } },{ { 3 } },{ { 4 } },{ { 5 } }} }; }
+
+int main()
+{
+    const auto& v = f()[3];
+    cout << v[0] << endl;
+}
