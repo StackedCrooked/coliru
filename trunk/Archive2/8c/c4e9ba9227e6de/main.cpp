f9c41f0ae461d1dc1c4467788158292cac6b538d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-10 00:08:58 +0000

    Update archive 8c

---
+++ trunk/Archive2/8c/c4e9ba9227e6de/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// Quiz: why is the output distorted?
+
+
+int main()
+{
+    std::cout << "Hello/nobody here." << std::endl;
+    std::cout << "Hello?/nobody here!" << std::endl;
+    std::cout << "Hello??/nobody here!!" << std::endl;
+}
