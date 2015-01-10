02511de3d574a8ef980c1d86b8efe0856ec87f7f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-10 15:32:37 +0000

    Update archive 20

---
+++ Archive2/20/742e89ad4120e0/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <functional>
+#include <utility>
+
+struct Noisy
+{
+    Noisy() { std::cout << "Noisy()" << std::endl; }
+    Noisy(const Noisy&) { std::cout << "Noisy(const Noisy&)" << std::endl; }
+    Noisy(Noisy&&) { std::cout << "Noisy(Noisy&&)" << std::endl; }
+    ~Noisy() { std::cout << "~Noisy()" << std::endl; }
+    Noisy& operator=(const Noisy&) { std::cout << "operator=(const Noisy&)" << std::endl; return *this; }
+    Noisy& operator=(Noisy&&) { std::cout << "operator=(Noisy&&)" << std::endl; return *this; }
+};
+
+struct Foo {};
+struct Bar
+{
+    explicit Bar(const Foo& f) { std::cout << "Bar::Bar(Foo)" << std::endl; }
+};
+
+struct Base
+{
+    void target(Bar, Noisy)
+    {
+        std::cout << "Base::target" << std::endl;
+    }
+};
+
+Base* get_base() { static Base b; return &b; }
+
+template<typename T>
+T&& process(T&& t) {            // transparent function, great
+    std::cout << "process(T&&)" << std::endl;
+    return std::forward<T>(t);  
+}
+
+Bar process(Foo x) {            // overload for specific type of arguments
+    std::cout << "process(Foo)" << std::endl;
+    return Bar{x}; 
+}
+
+template <typename... Args> // simplified
+struct trap
+{    
+    static void call(Args... args)
+    {
+        get_base()->target(process(args)...);
+    }
+};
+
+int main()
+{
+    auto f = &trap<Foo, Noisy>::call;
+    Foo a;
+    Noisy b;
+    f(a, b);
+}
