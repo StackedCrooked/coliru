a25cb15641fb901e378073517b4fc394d83cc031
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-10 17:22:29 +0000

    Update archive 64

---
+++ Archive2/64/bd745301e07788/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+array<array<int,1>,5> f() { return {array<int,1>{1},array<int,1>{2},array<int,1>{3},array<int,1>{4},array<int,1>{5}}; }
+
+int main()
+{
+    const auto& v = f()[3];
+    cout << v[0] << endl;
+}
