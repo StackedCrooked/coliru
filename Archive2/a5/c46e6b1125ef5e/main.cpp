5c1b130f361b19d279d5120aee6570f9139bf3d7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-10 18:34:45 +0000

    Update archive a5

---
+++ Archive2/a5/c46e6b1125ef5e/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <functional>
+#include <utility>
+
+struct Noisy
+{
+    Noisy() { std::cout << "Noisy()" << std::endl; }
+    Noisy(const Noisy&) { std::cout << "Noisy(const Noisy&)" << std::endl; }
+    Noisy(Noisy&&) { std::cout << "Noisy(Noisy&&)" << std::endl; }
+    ~Noisy() { std::cout << "~Noisy()" << std::endl; }
+    Noisy& operator=(const Noisy&) { std::cout << "operator=(const Noisy&)" << std::endl; return *this; }
+    Noisy& operator=(Noisy&&) { std::cout << "operator=(Noisy&&)" << std::endl; return *this; }
+};
+
+struct Base
+{
+    int target(Noisy& a, Noisy b)
+    {
+        std::cout << "Base::target" << std::endl;
+        return 0;
+    }
+};
+
+Base* get_base() { static Base b; return &b; }
+
+template <typename T, T t>
+struct trap;
+
+template <typename R, typename... Args, R(Base::*t)(Args...)>
+struct trap<R(Base::*)(Args...), t>
+{    
+    static R call(Args... args)
+    {
+        return (get_base()->*t)(std::move(args)...); // WRONG!
+    }
+};
+
+int main()
+{
+    auto f = &trap<decltype(&Base::target), &Base::target>::call;
+    Noisy a, b;
+    f(a, b);
+}
