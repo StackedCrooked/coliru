7511924264a100620857d7d6f406f5639e564cb0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-12 12:39:20 +0000

    Update archive d5

---
+++ Archive2/d5/48f25a4e8351d2/main.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+std::pair<int, int> find_equal_sum(const std::vector<int>& arr,const int x)
+{
+    int max = arr[arr.size()-1];//O(1)
+    std::vector<int> direct_index_hash(max+1, 0);//O(n)
+	for (size_t i = 0; i < arr.size(); i++)//O(n)
+		direct_index_hash[arr[i]] ++;
+	for (size_t i = 0; i < arr.size(); i++)//O(n)
+	{
+		int diff = x - arr[i];
+		if (diff <0) 
+			continue;
+		int diff_count = direct_index_hash[diff];
+		if (diff == arr[i]){
+			diff_count--;
+		}
+		if (diff_count >0)
+			return std::make_pair(arr[i], diff);
+	}
+	return std::make_pair(-1, -1);//does not exist
+}
+int main()
+{
+	const int x = 10;
+	std::vector<int> arr{ 5 };
+	auto res=find_equal_sum(arr, x);
+	std::cout << res.first <<" "<< res.second;
+	return 0;
+}
