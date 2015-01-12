5547ec3e9be6f7c027ed47b2cc5aa269d1287cf2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-12 17:41:06 +0000

    Update archive 6a

---
+++ Archive2/6a/963dad87cd1503/main.cpp
@@ -0,0 +1,25 @@
+class A
+{
+public:
+  A(int i, double d) : i(i), d(d) {}
+private:
+  int i;
+  double d;
+};
+
+class B
+{
+public:
+  template<typename... ArgTypes>
+  B(ArgTypes... list_of_args) : a(list_of_args...)
+  {
+    // do something here
+  }
+private:
+  A a;
+};
+
+int main()
+{
+  B b(1, 3.14);
+}
