c30717b854a5dfc0a394cfb6051c802d8b3f302a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-12 03:35:05 +0000

    Update archive 2b

---
+++ trunk/Archive2/2b/2827d84ce33f00/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+struct X1
+{
+    template<typename... Args>
+    X1(Args&&... args)
+             : t(std::forward<Args>(args)...) {} // ?
+
+    T t;
+};
+
+template<typename T>
+struct X2
+{
+    template<typename... Args>
+    X2(Args&&... args)
+     : t{std::forward<Args>(args)...} {} // ?
+
+    T t;
+};
+
+int main() {
+    auto x1 = X1<std::vector<int>> { 42, 2 };
+    auto x2 = X2<std::vector<int>> { 42, 2 };
+    
+    std::cout << "size of X1.t : " << x1.t.size()
+              << "\nsize of X2.t : " << x2.t.size();
+}
