3c2b556d4593e221ab9d8c2bde53cb4c10295f94
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-12 23:55:17 +0000

    Update archive c6

---
+++ trunk/Archive2/c6/197bdd12b13cea/main.cpp
@@ -0,0 +1,77 @@
+#include <array>
+#include <iostream>
+#include <string>
+#include <functional>
+#include <boost/mpl/identity.hpp>
+#include <boost/mpl/transform.hpp>
+#include <boost/mpl/size.hpp>
+#include <boost/mpl/fold.hpp>
+#include <boost/mpl/set.hpp>
+#include <boost/mpl/insert.hpp>
+#include <boost/fusion/functional/invocation/invoke.hpp>
+#include <boost/fusion/container/vector.hpp>
+#include <boost/fusion/adapted/mpl.hpp>
+#include <boost/fusion/include/mpl.hpp>
+#include <boost/variant.hpp>
+#include <boost/variant/apply_visitor.hpp>
+#include <boost/mpl/lambda.hpp>
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/push_back.hpp>
+
+template<typename T>
+struct fit : std::remove_volatile<T>
+{};
+
+namespace result_of
+{
+template<typename S>
+struct dynamic_at_c : boost::make_variant_over<
+    typename boost::mpl::fold<
+//        typename boost::mpl::transform<
+//            S
+//          , boost::mpl::lambda<fit<boost::mpl::_>>
+//        >::type
+        boost::mpl::vector<int, std::string>
+      , boost::mpl::set0<>
+      , boost::mpl::push_back<boost::mpl::_1, boost::mpl::_2>
+    >::type
+> {};
+} // result_of
+
+template<typename T>
+struct construct_array
+{
+    template<typename... Ts>
+    auto operator()(Ts&&... ts) -> std::array<T, sizeof...(Ts)>
+    {
+        return std::array<T, sizeof...(Ts)>{ts...};
+    }
+};
+
+template<typename S>
+auto dynamic_at_c(const S& seq, std::size_t idx)
+    -> typename result_of::dynamic_at_c<S>::type
+{
+    return boost::fusion::invoke(construct_array<typename result_of::dynamic_at_c<S>::type>(), seq).at(idx);
+}
+
+struct printer : boost::static_visitor<void>
+{
+    template<typename T>
+    void operator()(const T& val) const
+    {
+        std::cout << val << std::endl;
+    }
+
+};
+
+int main(int argc, char* argv[])
+{
+    boost::fusion::vector<int, std::string> vec(1, "one");
+    auto idx0 = dynamic_at_c(vec, 0);
+    auto idx1 = dynamic_at_c(vec, 1);
+    boost::apply_visitor(printer(), idx0);
+    boost::apply_visitor(printer(), idx1);
+
+    return 0;
+}
