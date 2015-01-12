4e502e4e54add23afc722b157d1c0033ae1a1d9f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-12 01:25:50 +0000

    Update archive 57

---
+++ trunk/Archive2/57/994fb74a69b508/main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+char str[20];
+ 
+int main(void)
+{
+    FILE *f;
+    char ch;
+ 
+    f = fopen("file.txt", "w");
+    for (ch = '0'; ch <= '9'; ch++) {
+        fputc(ch, f);
+    }
+    fclose(f);
+ 
+    f = fopen("file.txt", "r");
+    fread(str, 1, 10, f);
+    puts(str);
+ 
+    rewind(f);
+    fread(str, 1, 10, f);
+    puts(str);
+    fclose(f);
+ 
+    return 0;
+}
