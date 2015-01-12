ad2b43d26657f89e3c9e8c6d56971c7c2713a34e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-12 21:24:08 +0000

    Update archive 2d

---
+++ trunk/Archive2/2d/a7c65df19dee67/main.cpp
@@ -0,0 +1,21 @@
+template<typename BoolFlagsType>
+struct AdvancedDijkstra
+{
+    template <int direction, typename F, typename G, typename H>
+	static void run(int source, F onSettle, G shouldExpandTo, H onEnqueue);
+};
+
+template<typename BoolFlagsType>
+template <int direction, typename F, typename G, typename H>
+void AdvancedDijkstra<BoolFlagsType>::run(int source, F onSettle, G shouldExpandTo, H onEnqueue)
+{
+	(void)source;
+	onSettle();
+	shouldExpandTo(4);
+	onEnqueue(1.0);
+}
+
+int main()
+{
+	AdvancedDijkstra<int>::run<5>(3, [](){}, [](int){}, [](double){} );
+}
