5389dca4091f72ebf158c90a429c92867026d8cc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-12 10:39:07 +0000

    Update archive a4

---
+++ Archive2/a4/269543fcdb93df/main.cpp
@@ -0,0 +1,19 @@
+#include <boost/range/adaptors.hpp>
+#include <boost/range/algorithm.hpp>
+#include <set>
+#include <iostream>
+
+struct MixingParts{
+    int intPart;
+    double doublePart;
+};
+
+using boost::adaptors::transformed;
+
+int main() {
+
+    std::vector<MixingParts> v { { 1,0.1 }, {2, 0.2}, {3,0.3} };
+
+    boost::copy(v | transformed(std::mem_fn(&MixingParts::intPart)),
+            std::ostream_iterator<int>(std::cout, " "));
+}
