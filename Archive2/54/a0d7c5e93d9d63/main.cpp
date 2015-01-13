aa7a47b0ad0acdc8c3d388475807d4acc9115d84
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-13 23:55:58 +0000

    Update archive 54

---
+++ Archive2/54/a0d7c5e93d9d63/main.cpp
@@ -0,0 +1,38 @@
+//http://stackoverflow.com/questions/27933178/is-there-a-portable-way-to-find-out-what-alignments-are-supported-by-an-c11-impl
+
+#include <stdlib.h>
+#include <assert.h>
+
+void* seriously_aligned_malloc(size_t alignment, size_t size) {
+    assert(alignment<=255);
+    char* allocated = (char*)malloc(size + alignment);
+    unsigned char extraspace = (unsigned char)(alignment - (intptr_t)(allocated+alignment) % alignment);
+    char* ptr = allocated + extraspace;
+    unsigned char* extraspaceptr = (unsigned char*)(ptr - 1);
+    *extraspaceptr = extraspace;
+    return ptr;
+}
+void seriously_aligned_free(void* p) {
+    char* ptr = (char*)p;
+    unsigned char* extraspaceptr = (unsigned char*)(ptr - 1);
+    unsigned char extraspace = *extraspaceptr;
+    char* allocated = ptr - extraspace;
+    free(allocated);
+}
+
+#include <stdio.h>
+
+int main() {
+    for(unsigned i=0; i<20; ++i) {
+        char* p = (char*)seriously_aligned_malloc(7, 5);
+        printf("%d", (size_t)p%7);
+        seriously_aligned_free(p);
+        p = (char*)seriously_aligned_malloc(7, 7);
+        printf("%d", (size_t)p%7);
+        seriously_aligned_free(p);
+        p = (char*)seriously_aligned_malloc(7, 9);
+        printf("%d", (size_t)p%7);
+        seriously_aligned_free(p);
+    }
+    return 0;
+}
