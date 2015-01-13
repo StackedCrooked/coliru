53e05c0c7795a44a60cb7d184a0ce15159f61eb0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-13 16:09:29 +0000

    Update archive 44

---
+++ trunk/Archive2/44/0e0f336124da0a/main.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+int main()
+{
+    std::string s = "abababababababa";
+    while (!s.empty()) {
+        char c = s[0];
+        s.erase(std::remove_if(s.begin(), s.end(), c), s.end());
+        std::cout << s << std::endl;
+    }
+    return 0;
+}
