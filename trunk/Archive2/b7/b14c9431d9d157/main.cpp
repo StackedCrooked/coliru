cb2760d71daf1c0d5cf2ee320804095bd6562d44
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-13 12:28:32 +0000

    Update archive b7

---
+++ trunk/Archive2/b7/b14c9431d9d157/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int wybur;
+ 
+int main()
+{
+   cout << "Hello World" << endl; 
+   cout << " [1] Dodawanie" << endl;
+   cout << " [2] Odejmowanie" << endl;
+   cout << "Twuj wybur to: " << endl;
+   cin >> wybur;
+return 0;
+}
