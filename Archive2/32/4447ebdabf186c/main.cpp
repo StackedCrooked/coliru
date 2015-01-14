e666c22c540571a57ed5447cfc2f09925f27018f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-14 22:34:24 +0000

    Update archive 32

---
+++ Archive2/32/4447ebdabf186c/main.cpp
@@ -0,0 +1,31 @@
+#include <utility>
+#include <iostream>
+
+template<typename T>
+struct MyClass
+{
+    explicit MyClass(T a) : impl(std::move(a)) { }
+
+    T const& getImpl() const { return impl; }
+
+private:
+    T impl;
+
+public:
+    template<typename T0, typename T1>
+    friend auto operator +(MyClass<T0> const& a, MyClass<T1> const& b);
+};
+
+template<typename T0, typename T1>
+auto operator +(MyClass<T0> const& a, MyClass<T1> const& b)
+{
+    return MyClass<decltype(a.impl + b.impl)>(a.impl + b.impl);
+}
+
+int main()
+{
+    MyClass<int> x(3);
+    MyClass<long> y(5);
+
+    std::cout << (x + y).getImpl() << '\n';
+}
