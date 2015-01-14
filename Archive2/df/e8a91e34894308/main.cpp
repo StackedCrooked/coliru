04d29d772b1970e15e5ec089d82f719d3568dff6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-14 23:46:16 +0000

    Update archive df

---
+++ Archive2/df/e8a91e34894308/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <memory>
+#include <functional>
+using namespace std;
+
+class Thing
+{
+public:
+    Thing() : x(0)
+    {
+
+    }
+
+    Thing(Thing const &other)
+    {
+        this->x = other.x;
+        std::cout << "Copy constructed Thing!\n";
+    }
+
+    Thing(Thing &&other)
+    {
+        this->x = other.x;
+        std::cout << "Move constructed Thing!\n";
+    }
+
+    Thing & operator = (Thing const &other)
+    {
+        this->x = other.x;
+        std::cout << "Copied Thing!\n";
+        return (*this);
+    }
+
+    Thing & operator = (Thing && other)
+    {
+        this->x = other.x;
+        std::cout << "Moved Thing!\n";
+        return (*this);
+    }
+
+    int x;
+};
+
+class Event
+{
+public:
+    Event() { }
+	Event(function<void()> && f) : m_f(std::move(f)) { }
+	void SetF(function<void()> && f) { m_f = std::move(f); }
+	
+private:
+	function<void()> m_f;
+};
+
+int main() {
+	
+	auto lambda = [](Thing &thing) { std::cout << thing.x << "\n"; };
+	
+	Thing thing;
+	std::cout << "without make_unique: \n";
+	Event ev(std::bind(lambda,thing));
+	
+	std::cout << "with make_unique: \n";
+	auto ev_ptr = make_unique<Event>(std::bind(lambda,thing));
+	
+	std::cout << "with SetF: \n";
+	ev_ptr.reset(nullptr);
+	ev_ptr = make_unique<Event>();
+	ev_ptr->SetF(std::bind(lambda,thing));
+	
+	return 0;
+}
