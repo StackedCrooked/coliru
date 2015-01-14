e732b62f5cdeb560fa4ab427229096a427cbbdc7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-14 20:20:06 +0000

    Update archive b0

---
+++ Archive2/b0/51f1297aa71112/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    int num1;
+    int num2;
+    int somma;
+    
+    cout << "Inserire il Numero: ";
+    cin >> num1;
+    cout << endl <<"inserire il secondo numero: ";
+    cin >> num2;
+    Somma = num1 + num2
+    cout << "il risultato e:" <<somma;
+    return 0;
+
+    
+}
