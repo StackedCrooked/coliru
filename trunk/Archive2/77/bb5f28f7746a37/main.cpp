fe939730306253b1b9b076e281df7341d31db178
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-14 12:58:08 +0000

    Update archive 77

---
+++ trunk/Archive2/77/bb5f28f7746a37/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <algorithm>
+
+template<class T>
+struct Null_Function{
+    const T& operator()(const T& t)const{
+		return t;
+	}
+};
+template <typename IT>
+using is_const_iterator =
+	std::is_const<typename std::remove_reference<typename std::iterator_traits<IT>::reference>::type>;
+
+
+template <typename Iterator, typename Func = Null_Function<typename std::iterator_traits<Iterator>::value_type>>
+typename std::enable_if<
+	 	   !is_const_iterator<Iterator>::value,
+		   typename std::iterator_traits<Iterator>::value_type>::type
+	 Median(Iterator src_begin, Iterator src_end, Func func = Func()) {
+		using value_type = typename std::iterator_traits<Iterator>::value_type;
+		size_t count = std::distance(src_begin, src_end);
+		std::nth_element(src_begin, src_begin + (count / 2), src_end,
+			[&](value_type a, value_type b) { return func(a) < func(b); });
+		return count > 0 ? func(*(src_begin + (count / 2))) : 0;
+}
+
+template <typename ConstIterator, typename Func = Null_Function<typename std::iterator_traits<ConstIterator>::value_type>>
+typename std::enable_if<
+		 is_const_iterator<ConstIterator>::value,
+		 typename std::iterator_traits<ConstIterator>::value_type>::type
+	 Median(ConstIterator src_begin, ConstIterator src_end, Func func = Func()) {
+		using value_type = typename std::iterator_traits<ConstIterator>::value_type;
+		std::vector<value_type> src_cpy(src_begin, src_end);
+		auto src_cpy_begin = src_cpy.begin();
+		auto src_cpy_end = src_cpy.end();
+
+		size_t count = std::distance(src_cpy_begin, src_cpy_end);
+		std::nth_element(src_cpy_begin, src_cpy_begin + (count / 2), src_cpy_end,
+			[&](value_type a, value_type b) { return func(a) < func(b); });
+		return count >0 ? func(*(src_cpy_begin + (count / 2))) : 0;
+}
+
+int main()
+{
+	const std::vector<double> arr = { 5, 2, 32, 11, 7, 8, 9 };
+	std::cout << Median(arr.begin(), arr.end());//use second overload slower
+
+	std::vector<double> arr2 = { 5, 2, 32, 11, 7, 8, 9 };
+	std::cout << Median(arr2.begin(), arr2.end());//use first overload (find median inplace ) faster
+}
