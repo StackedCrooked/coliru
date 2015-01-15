f5a4c59767bc8c1a5256a4a695c9e243183d09d8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-15 13:03:01 +0000

    Update archive a4

---
+++ Archive2/a4/d6efab0321cc63/main.cpp
@@ -0,0 +1,26 @@
+#include <utility>
+#include <iostream>
+#include <stdio.h>
+#include <cstring>
+
+
+int main()
+{
+    std::cout << "basladi" << std::endl;
+    int i = 0;
+    int z = 0;
+    for(i = 0; i <= 10; i++)
+    {
+        std::cout << "1.for i: " << i << std::endl;
+        for(i = 0; i <= 10; i++) {
+            std::cout << "2.for i: " << i << std::endl;
+            for(i = 0; i <= 10; i++) {
+              std::cout << "3.for i: " << i << std::endl;
+               z++;
+            }
+        }
+    }
+             
+    std::cout << "z= " << z << std::endl;
+    std::cout << "bitti" << std::endl;
+}
