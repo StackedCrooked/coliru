01648d16a9c1014f56c1609237e9bb3de4a78b12
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-15 20:08:09 +0000

    Update archive 5b

---
+++ Archive2/5b/96796d7aa10605/main.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+
+// utility for getting an array's size
+// usage: array_size(m_vec)
+// returns: the number of elements in the array
+template<typename T, unsigned long N>
+unsigned long array_size(T (&)[N]) { return N; }
+
+
+struct T {
+    double m_vec[3];
+
+    T() {
+        std::fill(m_vec, m_vec+array_size(m_vec), 0.0);
+    }
+};
+
+#include <iterator>
+#include <iostream>
+
+int main() {
+    T t;
+    std::cout << "T: ";
+    std::copy(t.m_vec, t.m_vec + array_size(t.m_vec),
+              std::ostream_iterator<int>(std::cout, ", "));
+    
+}
