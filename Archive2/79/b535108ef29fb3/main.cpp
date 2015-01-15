deac892fb4f4ece81bf3bf530d3e053dea8cc0de
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-15 16:19:14 +0000

    Update archive 79

---
+++ Archive2/79/b535108ef29fb3/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+ 
+int main()
+{
+    std::string str = "Hello, world";
+    std::istringstream in(s;
+    in >> word;
+    std::cout << "After reading the word \"" << word
+              << "\" tellg() returns " << in.tellg() << '\n';
+}
