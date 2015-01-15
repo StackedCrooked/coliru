aac24f81cbf11cea051c4c88ed3fa4e020dee788
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-15 14:51:17 +0000

    Update archive f8

---
+++ trunk/Archive2/f8/d1793b9b236574/main.cpp
@@ -0,0 +1,40 @@
+#include <functional>
+#include <list>
+#include <iostream>
+
+typedef std::function<bool(int)> callback_t;
+
+std::list<callback_t> callbacks;
+
+void register_callback(callback_t callback) {
+    callbacks.push_back(callback);
+}
+
+void register_callback(std::function<bool()> callback) {
+    // just use a lambda with an unused int param
+    // capture callback by value - capturing by reference here
+    // is undefined behavior since callback is now a stack-allocated
+    // local about to go out of scope
+    callbacks.push_back([callback](int) {
+        return callback();
+    });
+}
+
+int main() {
+    register_callback([](int x) {
+        return x%2 == 0;
+    });
+
+    for (int i = 0; i < 5; ++i) {
+    register_callback([]() {
+        return false;
+    });
+    }
+    
+    
+    for (auto & c : callbacks) {
+        std::cout << std::boolalpha << c(2) << std::endl;   
+    }
+
+    return 0;
+}
