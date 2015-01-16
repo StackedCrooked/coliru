36271013a851e2efaf53733b21b59e0fdfe983cb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-16 16:01:47 +0000

    Update archive 92

---
+++ Archive2/92/046f7ed520308f/cmd.sh
@@ -0,0 +1,2 @@
+clang++ -std=c++14 -stdlib=libc++ -O2 -Wall -Wextra -pedantic-errors -Wno-unused main.cpp -lsupc++ && ./a.out
+g++ -std=c++14 -O2 -Wall -Wextra -pedantic-errors -Wno-unused main.cpp && ./a.out
