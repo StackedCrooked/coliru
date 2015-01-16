c55b037aef089cc5a65f7eddc08046a8ec78fe5d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-16 13:25:58 +0000

    Update archive a8

---
+++ trunk/Archive2/a8/40c65bf910a21e/main.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+int main(void) 
+{
+    char *str = malloc(20);
+    char *tok = NULL;
+    int len = 0;
+ 
+    strcpy(str, "This is a string");
+    len = strlen(str);
+ 
+    printf("string before strtok(): %s\n", str);
+    tok = strtok(str, " ");
+    while (tok) {
+        printf("Token: %s\n", tok);
+        tok = strtok(NULL, " ");
+    }
+ 
+    printf("Print mangled string after strtok()\n");
+    for (int i = 0; i < len; i++) {
+        if (str[i] == '\0') {
+            printf("'\\0'");
+        } else {
+            printf("%c", str[i]);
+        }
+    }
+    printf("\n");
+    free(str);
+ 
+    return 0;
+}
