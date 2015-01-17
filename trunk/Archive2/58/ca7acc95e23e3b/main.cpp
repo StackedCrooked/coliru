caccb3f0715ef935ad219e17a581d1b843479c08
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-17 23:55:13 +0000

    Update archive 58

---
+++ trunk/Archive2/58/ca7acc95e23e3b/main.cpp
@@ -0,0 +1,26 @@
+#include <boost/filesystem.hpp>
+#include <boost/range.hpp>
+#include <iostream>
+
+namespace fs = boost::filesystem;
+
+int main() {
+    fs::path latest;
+    std::time_t latest_tm {};
+
+    for (auto&& entry : boost::make_iterator_range(fs::directory_iterator("."), {})) {
+        if (is_regular_file(entry.path()) && entry.path().extension() == ".grb2") 
+        {
+            std::time_t timestamp = fs::last_write_time(entry.path());
+            if (timestamp > latest_tm) {
+                latest = entry.path();
+                latest_tm = timestamp;
+            }
+        }
+    }
+
+    if (latest.empty())
+        std::cout << "Nothing found\n";
+    else
+        std::cout << "Last modified: " << latest << "\n";
+}
