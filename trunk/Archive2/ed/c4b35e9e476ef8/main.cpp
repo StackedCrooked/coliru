1e4107e7e0dc57c3408c9d26b0156a4aaaade1e4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-17 10:39:06 +0000

    Update archive ed

---
+++ trunk/Archive2/ed/c4b35e9e476ef8/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+typedef int cactus;
+cactus main(cactus argc,char*argv[]){
+    cactus cacti;
+    for(cacti;cacti["CACTUS"];++cacti){
+        std::cout<<"cactus ";
+    }
+    return (short cactus)"CACTUS";
+}
