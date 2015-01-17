e2e3fd327ce93bbbe5d05f1d8582f86e70c3ed7c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-17 04:21:49 +0000

    Update archive b8

---
+++ trunk/Archive2/b8/3cfdcdffeb8bde/main.cpp
@@ -0,0 +1,6 @@
+#include <cstdio>
+ 
+int main()
+{
+    std::printf("                                Basic BYU Trivia\n\n                Questions                                   Answers\n\nWhat was the original name of BYU?                Brigham Young Academy\ntest\n\nShe said, \"How are you?\"\n");
+}
