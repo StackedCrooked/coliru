b19f45884804f4028ecc7e55ab63f749cf9a2d85
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-17 00:06:22 +0000

    Update archive 62

---
+++ Archive2/62/0cc4b1f1338031/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <type_traits>
+#include <utility>
+
+#include <boost/type_index.hpp>
+
+template<typename Func, typename ... Args>
+void func(Func f, Args... args) {
+    using boost::typeindex::type_id_with_cvr;
+  
+    {
+        using result_type = decltype(std::declval<Func>()(std::declval<Args>()...));
+        std::cout << "Result type = " 
+                << type_id_with_cvr<result_type>().pretty_name()
+                << '\n';
+    }
+    {
+        using result_type = typename std::result_of<Func(Args...)>::type;
+        std::cout << "Result type = " 
+                << type_id_with_cvr<result_type>().pretty_name()
+                << '\n';
+    }
+    
+    return f(args...);
+}
+
+template<typename T>
+void add(const T& l, const T& r) {
+  (void)(l + r);
+}
+
+int main() {
+  double a = 4.5, b = 5.5;
+  func(add<double>, a, b);
+  return 0;
+}
