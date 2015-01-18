b33d158978c493cfa6427d94f343f214f9ac0015
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-18 23:16:13 +0000

    Update archive b4

---
+++ Archive2/b4/f1dd578570e40c/main.cpp
@@ -0,0 +1,14 @@
+template <class T> struct identity {using type = T;};
+
+template <typename T>
+struct FunctionAnalyzer
+    : identity<decltype(&T::operator())> {};
+
+int main()
+{
+    []
+    {
+        auto a = []{};
+        using b = FunctionAnalyzer<decltype(a)>::type;
+    }();
+}
