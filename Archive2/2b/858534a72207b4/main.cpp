7031cf7c0d8925cb259f15db0ea1d967579043a2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-18 07:58:32 +0000

    Update archive 2b

---
+++ Archive2/2b/858534a72207b4/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void foo(int){ cout << "foo(int)" << endl; }
+void foo(int, int){ cout << "foo(int, int)" << endl; }
+
+void (*baz)(int, int);
+
+
+int main(){ baz = foo; baz(); }
