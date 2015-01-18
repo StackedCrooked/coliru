5e5201d867e942e228a4730d2177c2b5dc566fa6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-18 04:22:12 +0000

    Update archive 57

---
+++ trunk/Archive2/57/7cb8f90fc49890/main.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+static const int NOT_FOUND = -1;
+
+template <class sequence, class T>
+int binarySearch(sequence& seq, int low, int high, T& item)
+{
+    return 0;
+}
+
+template <class sequence, class T>
+int binarySearch(sequence& seq, T& item)
+{
+    if (seq.size() == 0)
+        return NOT_FOUND;
+    return binarySearch(seq, 0, seq.size() - 1, item);
+}
+
+int main()
+{
+    vector<int> t1 = {0, 3 ,45, 94};
+    int i = 0;
+    cout << binarySearch(t1, 0);
+    //binarySearch(t1, 0, t1.size() - 1, 45);
+    return 0;
+}
