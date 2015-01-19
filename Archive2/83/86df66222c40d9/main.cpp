ecca0963c6d81f22ae69eb2d5b9ab2f9721b52ca
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-19 02:41:48 +0000

    Update archive 83

---
+++ Archive2/83/86df66222c40d9/main.cpp
@@ -0,0 +1,31 @@
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix.hpp>
+
+struct Point {
+    float x,y;
+    constexpr Point(float x,float y) : x(x), y(y) {}
+};
+
+template <typename OI>
+static inline void parse_points(char const* szInput, OI out) {
+    namespace qi  = boost::spirit::qi;
+    namespace phx = boost::phoenix;
+
+    if (!qi::phrase_parse(
+                szInput, std::numeric_limits<char const*>::max(), // just wait for NUL char
+                (qi::double_ >> ',' >> qi::double_) 
+                    [ *phx::ref(out) = phx::construct<Point>(qi::_1,qi::_2) ]
+                >> &qi::lit('\0'),
+                qi::space))
+    {
+        throw "parse failure";
+    }
+}
+
+#include <boost/function_output_iterator.hpp>
+
+int main() {
+    int count = 0;
+    parse_points( " 10,9 2.5, 3   4 ,150.32    ", boost::make_function_output_iterator([&](Point const&){count++;}));
+    std::cout << "elements in sample: " << count << "\n";
+}
