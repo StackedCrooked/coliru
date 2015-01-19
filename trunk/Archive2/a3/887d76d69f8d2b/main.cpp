0ddcc63f008bfde41b696cfcd93f6ff46058d391
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-19 02:47:59 +0000

    Update archive a3

---
+++ trunk/Archive2/a3/887d76d69f8d2b/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double height;
+    double weight;
+    double bmi;
+    double height_meters;
+    double weight_Kg;
+    cout<<"Please enter your height(inches): "<<endl;
+    cin>>height;
+    cout<<"Please enter your weight(lbs): "<<endl;
+    cin>>weight;
+    height_meters=height/39.3701;
+    weight_Kg=weight/2.20462;
+    bmi=weight_Kg/(height_meters*height_meters);
+    cout<<"Your BMI is: "<<bmi<<endl;
+    cout<<"Your height in Meters is: "<<height_meters<< endl;
+    cout<<"Your weight in Kilograms is: "<<weight_Kg<<endl;
+    system("pause");
+    return 0;
+}
