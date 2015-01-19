4ec81821e10da417d8ce500f6a7b7dbdda0acc6b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-19 22:59:41 +0000

    Update archive b1

---
+++ trunk/Archive2/b1/f558545f3aad49/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Hallgato {
+    string nev;
+};
+
+string monogram (Hallgato h3) {
+    string mono;
+    for (int i = 0; i < h3.nev.size(); i++) {
+        if('a' > h3.nev[i] && h3.nev[i] != ' ')
+            mono = mono + h3.nev[i];
+    }
+    
+    return mono;
+}
+
+int main()
+{
+    Hallgato h { "Goretity Arpad;" };
+    std::cout << monogram(h) << std::endl;
+	return 0;
+}
