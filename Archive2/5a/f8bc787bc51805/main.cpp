55d00aa672ebab3022606f10db67aabdbea23ef7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-19 02:03:38 +0000

    Update archive 5a

---
+++ Archive2/5a/f8bc787bc51805/main.cpp
@@ -0,0 +1,36 @@
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix.hpp>
+
+struct Point {
+    float x,y;
+    constexpr Point(float x,float y) : x(x), y(y) {}
+};
+
+template <typename OI>
+static inline void parse_points(OI out, char const* it, char const* last = std::numeric_limits<char const*>::max()) {
+    namespace qi  = boost::spirit::qi;
+    namespace phx = boost::phoenix;
+
+    bool ok = qi::phrase_parse(it, last,
+            *(qi::double_ >> ',' >> qi::double_) [ *phx::ref(out) = phx::construct<Point>(qi::_1, qi::_2) ],
+            qi::space);
+
+    if (!ok || !(it == last || *it == '\0')) {
+        throw it; // TODO proper error reporting?
+    }
+}
+
+int main(){
+    char const* const sample = " 10,9 2.5, 3   4 ,150.32    ";
+
+    std::vector<Point> data;
+    data.reserve(100); // optional; may reduce reallocs
+
+    try {
+        parse_points(back_inserter(data), sample);
+        parse_points(back_inserter(data), sample);
+        parse_points(back_inserter(data), sample);
+    } catch (char const* sz) { std::cerr << "Parse failed at '" << sz << "'\n"; }
+
+    std::cout << "data.size(): " << data.size() << " elements\n";
+}
