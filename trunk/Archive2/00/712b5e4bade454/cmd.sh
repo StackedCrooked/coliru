4519ae7d02764e16750f6ee5d41d93b650976e48
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-20 05:19:07 +0000

    Update archive 00

---
+++ trunk/Archive2/00/712b5e4bade454/cmd.sh
@@ -0,0 +1,1 @@
+g++ -std=c++14 -O3 -Wall -Wextra -pedantic-errors main.cpp -lsupc++ && ./a.out
---
+++ trunk/Archive2/00/712b5e4bade454/main.cpp
@@ -0,0 +1,39 @@
+#include <utility>
+#include <typeindex>
+
+template < typename T > struct result : std::pair< T, std::type_index >
+{ result( T v ) : std::pair< T, std::type_index >( v, typeid(v) ) {} };
+
+template < typename... > struct type_selector ;
+
+template < typename T > struct type_selector<T>
+{ static constexpr result<T> select( T v ) { return v ; } };
+
+template < typename FIRST, typename... REST >
+struct type_selector<FIRST, REST...> : type_selector<FIRST>, type_selector<REST...>
+{
+    static constexpr result<FIRST> select( FIRST v )
+    { return type_selector<FIRST>::select(v) ; }
+
+    template < typename T > static constexpr result<T> select( T v )
+    { return type_selector<REST...>::select(v) ; }
+};
+
+#include <string>
+#include <complex>
+#include <iostream>
+#include <cassert>
+
+int main()
+{
+    using selector = type_selector< char, int, double, std::string, std::complex<double> > ;
+
+    const auto c = selector::select('!').first ;
+    const auto i = selector::select(100).first ;
+    const auto d = selector::select(99.99).first ;
+    const auto str = selector::select( std::string("hello") ).first ;
+    const auto cmpx = selector::select( std::complex<double>(1,2) ).first ;
+
+    std::cout << c << ' ' << i << ' ' << d << ' ' << str << ' ' << cmpx << '\n' ;
+    assert( selector::select(5).second == typeid(int) ) ;
+}
