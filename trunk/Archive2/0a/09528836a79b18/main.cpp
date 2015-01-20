896242a21b7ca4adfc35cc67fb5a9ce4f40073a7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-20 04:26:40 +0000

    Update archive 0a

---
+++ trunk/Archive2/0a/09528836a79b18/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <memory>
+
+class Model {
+    friend class Window;
+public:
+    int i = 1337;
+private:
+    Model(){}
+};
+
+class Window {
+public:
+    std::shared_ptr<Model> get_model() {
+        struct foo : public Model{};
+        return std::make_shared<foo>();
+    }
+};
+
+
+int main() {
+    Window win;
+    auto model = win.get_model();
+    std::cout << "model i: " << model->i << std::endl;
+}
