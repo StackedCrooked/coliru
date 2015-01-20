5fb5cd4a714c88ec0b0cce540a90088e03d43dab
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-20 23:08:15 +0000

    Update archive 0b

---
+++ trunk/Archive2/0b/dfe7dfe5dbd3e9/main.cpp
@@ -0,0 +1,17 @@
+struct target {
+    int somevalue;
+    target() = default;
+    constexpr target(float f) : somevalue(static_cast<int>(1024*f)) {}
+};
+
+#include <type_traits>
+#include <boost/type_traits.hpp>
+static_assert(std::is_pod<target>::value,   "expected to be POD");
+static_assert(boost::is_pod<target>::value, "expected to be POD");
+
+#include <iostream>
+
+int main() {
+    target t = 3.14f;
+    std::cout << t.somevalue << "\n";
+}
