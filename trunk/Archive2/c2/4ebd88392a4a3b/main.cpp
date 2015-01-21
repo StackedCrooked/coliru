b1696215f56816629a470978368cbdc200a2f6c5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-21 17:37:34 +0000

    Update archive c2

---
+++ trunk/Archive2/c2/4ebd88392a4a3b/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+namespace eco
+{
+    int x = 5;    
+}
+
+class Alumno
+{
+public:
+    int id;
+    string name;
+};
+
+struct Ficha
+{
+    int t1;
+    float t2;
+};
+
+void func(int *ar)
+{
+    cout << ar[1] << endl;
+}
+
+char* func2(char *ar)
+{
+    ar[0] = 't';
+    return ar;
+}
+
+int main()
+{
+   Alumno al1; 
+   vector<Alumno> al(5);
+   al[0].id = 1;
+   al[1].id = 2;
+   
+   Alumno *alu;
+   alu = new Alumno[5];
+   alu->id = 3;
+   alu->name = "hola";
+   
+   struct Ficha fich;
+   fich.t1 = 5;
+   
+   string trama = "caracola";
+   
+   bool var = true;
+   var = ~true;
+   
+   int array[3] = {2,5,6};
+   char name[] = "hola";
+   
+   cout << *array << endl;
+   func(array);
+   cout << func2(name) << endl;
+   
+   cout << eco::x << endl;
+   
+   int t;
+   cout << sizeof(t) << endl;
+   unsigned char byte = 255;
+   cout << int(byte) << endl;
+   return 0;
+}
