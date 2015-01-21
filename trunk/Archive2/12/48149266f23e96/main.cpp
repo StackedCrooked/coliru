3e7d98c1dfb421dc2dc8c4632af97e2e3cf7ab84
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-21 14:59:52 +0000

    Update archive 12

---
+++ trunk/Archive2/12/48149266f23e96/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+
+	cout << "inserire CARDINALITà matrice"<< endl;
+	cin >> n;
+	float mat [n][n];
+	for (int i=0;i<n;i++)
+		for (int j=0;j<n;j++) {
+			cout << "inserire elementi matrice"<< endl;
+			cin >>mat[i][j];
+
+						}
+	
+
+	return 0;
+
+
+}
