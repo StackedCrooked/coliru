a5c14d64ee91b683d46745ee81ca3b83ba81f153
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-21 09:18:43 +0000

    Update archive 72

---
+++ trunk/Archive2/72/0576bb749e8e03/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+#include <iterator>
+#include <algorithm>
+
+int main() {
+    auto isPerfect = [](int64_t maybeSquare) {
+        int64_t r = sqrt(maybeSquare);
+        return (r * r == maybeSquare);
+    };
+
+    int discard;
+    if (std::cin>>discard) {
+        std::transform(
+                std::istream_iterator<int64_t>(std::cin), {},
+                std::ostream_iterator<int64_t>(std::cout, "\n"),
+                [isPerfect](int64_t value) { 
+                    auto sqr = 5 * pow(value, 2);
+                    return (isPerfect(sqr + 4) || isPerfect(sqr - 4))? "IsFibo" : "IsNotFibo";
+                }
+            );
+    }
+}
