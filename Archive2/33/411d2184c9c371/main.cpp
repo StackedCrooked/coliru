b67509f52b19c959e9732c575ecdf091cff13ba7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-21 23:23:01 +0000

    Update archive 33

---
+++ Archive2/33/411d2184c9c371/main.cpp
@@ -0,0 +1,150 @@
+#include <atomic>
+#include <cassert>
+#include <array>
+#include <iostream>
+#include <future>
+#include <functional>
+#include <cstdio>
+#include <mutex>
+#include <thread>
+#include <vector>
+#include <sstream>
+#include <tbb/concurrent_queue.h>
+
+
+std::mutex m;
+
+
+struct LogItem
+{
+    ~LogItem()
+    {
+        std::lock_guard<std::mutex> lock(m);
+        std::cout << ss.str() << std::endl;
+
+    }
+    std::stringstream ss;
+};
+
+
+#define LOG() LogItem().ss
+
+
+
+using Task = std::function<void()>;
+
+
+std::atomic<std::uint64_t> gNumExecutors{0};
+
+struct Executor
+{
+    Executor() : mImpl(std::make_shared<Impl>(this))
+    {
+    }
+
+    Executor(const Executor&) = delete;
+    Executor& operator=(const Executor&) = delete;
+
+    // may run sync or async
+    template<typename F>
+    static void dispatch(F&& f)
+    {
+        Executor* executor = nullptr;
+        GetExecutors().try_pop(executor);
+
+        if (executor)
+        {
+            executor->mImpl->mTasks.push(f);
+        }
+        else if (gNumExecutors < hardware_concurrency())
+        {
+            (new Executor)->mImpl->mTasks.push(f);
+        }
+        else
+        {
+            // run synchronously 
+            f();
+        }
+    }
+
+private:
+    using Executors = tbb::concurrent_queue<Executor*>;
+    
+    static unsigned int hardware_concurrency() { return 8; } // fake for coliru
+
+    static Executors& GetExecutors()
+    {
+        static Executors fExecutors;
+        return fExecutors; 
+    }
+
+    struct Impl
+    {
+        Impl(Executor* exec) : mThread([=]{ this->work(exec); })
+        {
+            LOG() << "Impl: gNumExecutors=" << ++gNumExecutors;
+            mTasks.set_capacity(100);
+        }
+
+        ~Impl()
+        {
+            mTasks.push(Task()); // send stop flag
+            mThread.join();
+            LOG() << "Impl: gNumExecutors=" << --gNumExecutors;
+        }
+
+        void work(Executor* exec)
+        {
+            for (;;)
+            {
+                Task task;
+                mTasks.pop(task);
+
+                do
+                {
+                    // Allow scheduling a new task before we start running the current task.
+                    GetExecutors().push(exec);
+
+                    // stop flag
+                    if (!task)
+                    {
+                        LOG() << "Executor received DEATH FLAG!" << std::endl;
+                        return;
+                    }
+
+                    try
+                    {
+                        task();
+                    }
+                    catch (const std::exception& e)
+                    {
+                        LOG() << "Executor::Impl::work: Caught: " << e.what();
+                    }
+                }
+                while (mTasks.try_pop(task));
+            }
+        }
+
+        tbb::concurrent_bounded_queue<Task> mTasks;
+        std::thread mThread;
+    };
+
+
+    std::shared_ptr<Impl> mImpl;
+};
+
+
+int main()
+{
+    std::promise<void> keep_alive;
+
+    for (int i = 1; i <= 50; ++i)
+    {
+        Executor::dispatch([=] { LOG() << "Task " << i; });
+    }
+
+    Executor::dispatch([&] { keep_alive.set_value(); });
+    
+    
+    keep_alive.get_future().get();
+}
