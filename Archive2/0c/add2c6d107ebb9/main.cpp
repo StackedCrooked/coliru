114aafb6d8254f194a48976fb3ad6691a8c56560
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-21 20:19:58 +0000

    Update archive 0c

---
+++ Archive2/0c/add2c6d107ebb9/main.cpp
@@ -0,0 +1,36 @@
+#include <functional>
+#include <iostream>
+#include <type_traits>
+
+#define REM_CTOR(...) __VA_ARGS__
+
+template <typename T, typename R>
+using helper_ = std::conditional_t<std::is_rvalue_reference<R>{}, T&&, T&>;
+
+#define SPEC(var, cv, ref) \
+template <typename R, typename C, typename... Args> \
+struct get_std_function<R(C::*)(Args... REM_CTOR var) cv ref > \
+{using type = std::function<R(helper_<C cv, int ref>, Args... REM_CTOR var)>;};
+
+#define REF(var, cv) SPEC(var, cv,) SPEC(var, cv, &) SPEC(var, cv, &&)
+
+#define CV(var) REF(var,) REF(var, const) \
+                REF(var, volatile) REF(var, const volatile)
+
+template <typename> struct get_std_function;
+
+CV(()) CV((,...))
+
+template <typename T>
+void print() {std::cout << __PRETTY_FUNCTION__ << '\n';}
+
+int main()
+{
+    struct Test1 { void f(int) const&; };
+    struct Test2 { void f(int, ...) const volatile; };
+    struct Test3 { void f(int) volatile&&; };
+    
+    print<get_std_function<decltype(&Test1::f)>::type>();
+    print<get_std_function<decltype(&Test2::f)>::type>();
+    print<get_std_function<decltype(&Test3::f)>::type>();
+}
