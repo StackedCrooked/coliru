61365a9d88879537e5cf530ea494a732c75b1660
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-21 09:38:30 +0000

    Update archive 7c

---
+++ Archive2/7c/2460140628a3e0/main.cpp
@@ -0,0 +1,38 @@
+// http://stackoverflow.com/questions/28056551/how-to-convert-a-long-long-to-a-binary-character-string
+// See also:
+// http://stackoverflow.com/questions/15859649/why-is-data-sent-across-the-network-converted-to-network-byte-order
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using std::string;
+
+template< typename T >
+void serial(T val, string & str, size_t N = sizeof(T)) {
+    str[sizeof(T)-N] = char(val>>(8*(N-1)));
+    if (--N) serial(val,str,N);
+}
+
+template< typename T >
+string serialize( T val ) {
+    string str( sizeof(T), 0 );
+    serial( val, str );
+    return str;
+}
+
+template< typename T >
+T deserialize( const string & data, size_t N=sizeof(T)-1 ) {
+    T top = N ? deserialize<T>(data,N-1) : 0;
+    return (top << 8) | (unsigned char)(data[N]);
+}
+
+int main(int argc, const char * argv[]) {
+    
+    for(int i=1; i<argc; ++i) {
+        char *end;
+        long long val = strtoull(argv[i], &end, 10);
+        std::cout << " deserialize<long long>( serialize(" << val << ") ) = "
+            << deserialize<long long>(serialize(val))
+            << std::endl;
+    }
+}
