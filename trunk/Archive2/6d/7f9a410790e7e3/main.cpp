779c3ff11d610f1610267588e1a99a4ebad94362
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-22 06:32:39 +0000

    Update archive 6d

---
+++ trunk/Archive2/6d/7f9a410790e7e3/main.cpp
@@ -0,0 +1,24 @@
+#define ABCD 2
+#include <iostream>
+ 
+int main()
+{
+ 
+#ifdef ABCD
+    std::cout << "1: yes\n";
+#else
+    std::cout << "1: no\n";
+#endif
+ 
+#ifndef ABCD
+    std::cout << "2: no1\n";
+#elif ABCD == 2
+    std::cout << "2: yes\n";
+#else
+    std::cout << "2: no2\n";
+#endif
+ 
+#if !defined(DCBA) && (ABCD < 2*4-3)
+    std::cout << "3: yes\n";
+#endif
+}
