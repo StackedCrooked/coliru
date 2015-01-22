44a48dd96ae6544a6b8e6edc1f889ddd27a1e5ab
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-22 15:26:43 +0000

    Update archive a5

---
+++ Archive2/a5/0839cbdce43a50/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+class c{  int a;
+public : c() {} 
+ c(const c&); 
+ c& operator =(c&); };  
+c& c::operator=(c &c){ cout << endl << "copiere cu egal"; return c;} 
+c::c(const c &c) { cout << endl << "Constructor de copiere"; } 
+int main() 
+{ c x; c y=x;
+ c b=x;
+ x=y; 
+}
