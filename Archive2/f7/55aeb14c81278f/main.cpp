b51a4cd24e0d713381be1e7793f33e5e1b6bf7ba
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-22 18:36:45 +0000

    Update archive f7

---
+++ Archive2/f7/55aeb14c81278f/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 3;
+    int b = 5;
+    int c = 2;
+    
+    int d = 0;
+    
+    d = (a + b)/c + a/c – a+b/c * b;
+    
+    cout << d << endl;
+    
+    return 0;
+}
