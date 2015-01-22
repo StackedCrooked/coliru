d53bc83cf5b19d2bb804854e48f2ebc02c132e67
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-22 12:11:55 +0000

    Update archive d5

---
+++ Archive2/d5/c68c8b14fc653e/main.cpp
@@ -0,0 +1,24 @@
+#include <type_traits>
+
+template <typename T, typename... Args>
+class is_only_constructible
+{
+    template <typename U, typename=void>
+    struct test : std::false_type {};
+    template <typename U>
+    struct test<U, decltype(void(new U(std::declval<Args>()...)))>
+        : std::true_type {};
+        
+public:
+    static constexpr bool value = test<T>::value;
+};
+
+int main()
+{
+    class A { ~A(); public: A(int); };
+    
+    static_assert( !std::is_constructible<A, int>::value, "" );  
+    static_assert( is_only_constructible<A, int>::value, "" );    
+}
+
+
