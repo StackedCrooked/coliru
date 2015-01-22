05fde998ff209a9cd311eea8ed834c940ac6644c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-22 12:11:55 +0000

    Update archive d5

---
+++ trunk/Archive2/d5/c68c8b14fc653e/cmd.sh
@@ -0,0 +1,3 @@
+clang++ -std=c++11 -stdlib=libc++ -Wall -pedantic -pthread main.cpp
+clang++ -std=c++11 -stdlib=libstdc++ -Wall -pedantic -pthread main.cpp
+g++ -std=c++11 -Wall -pedantic -pthread main.cpp
