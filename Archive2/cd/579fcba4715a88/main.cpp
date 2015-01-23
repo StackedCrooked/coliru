e32ac83f7fea1172c76a7f076654b931b4836f75
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-23 12:52:29 +0000

    Update archive cd

---
+++ Archive2/cd/579fcba4715a88/main.cpp
@@ -0,0 +1,37 @@
+#include <emmintrin.h>
+#include <cstdio>
+
+
+union ssereg
+{
+    unsigned char c[16];
+	__m128i v;
+
+	ssereg(int in = 0) : v(_mm_set1_epi32(in)) {}
+	ssereg(__m128i in) : v(in) {}
+
+	operator __m128i const&() { return v; }
+
+	void print()
+	{
+		for(auto byte : c) printf("%02x ", byte);
+		puts("");
+	}
+};
+
+
+
+
+int main()
+{
+	ssereg a(0xffffffff);
+	ssereg b(0x00000000);
+
+	ssereg c = _mm_cmplt_epi8(a, b);
+
+	a.print();
+	b.print();
+	c.print();
+
+    return 0;
+}
