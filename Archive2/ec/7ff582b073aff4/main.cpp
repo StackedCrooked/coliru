c4eff52f9f0572a9136867fe87d508a87a613cfc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-23 20:40:15 +0000

    Update archive ec

---
+++ Archive2/ec/7ff582b073aff4/main.cpp
@@ -0,0 +1,28 @@
+#include <boost/function_output_iterator.hpp>
+#include <boost/range.hpp>
+#include <boost/range/algorithm.hpp>
+#include <set>
+
+struct TIPAddressDescription {
+    int i;
+    bool operator<(TIPAddressDescription const& other) const { return i < other.i; }
+};
+
+struct THNNetIface { 
+    THNNetIface(TIPAddressDescription const& other) : i(other.i) {}
+    int i;
+};
+
+struct HNInfoComparator {
+    template <typename T, typename U> bool operator()(T const&, U const& u) const { return false; /*TODO*/ }
+};
+
+int main() {
+    std::set<TIPAddressDescription> iface, ip;
+
+    std::vector<THNNetIface> delIfaces;
+    auto output = boost::make_function_output_iterator(
+            [&](TIPAddressDescription const& ip) { return THNNetIface(ip); });
+
+    boost::set_difference(iface, ip, output, HNInfoComparator());
+}
