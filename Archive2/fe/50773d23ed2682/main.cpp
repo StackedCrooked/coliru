50d7ed7addf0f011d21e01095ba435dea2db0e82
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-24 20:12:20 +0000

    Update archive fe

---
+++ Archive2/fe/50773d23ed2682/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+int main()
+{
+    int a,b,sum;
+    cout<<"Scrivi due valori"<<endl;
+    cin>>a;
+    cin>>b;
+    sum=a+b;
+    cout<<sum<<endl;
+    
+    
+    system ("pause");
+    return 0;
+}
