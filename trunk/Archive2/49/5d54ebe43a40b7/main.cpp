87076c7450784a09a7aa66a3fae5147b395fd4d0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-24 02:43:04 +0000

    Update archive 49

---
+++ trunk/Archive2/49/5d54ebe43a40b7/main.cpp
@@ -0,0 +1,7 @@
+template <typename T> void Foo(T&&) {std::cout << "A";}
+void Foo(const char*) {std::cout << "B";}
+
+int main() {
+    const char* thing = nullptr;
+    Foo(thing); //prints A, because it matches `Foo<const char*&>`
+}
