071f5c05a6de6c23b1c8d4f2bdc92825504d8c91
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-24 00:16:58 +0000

    Update archive 70

---
+++ trunk/Archive2/70/87dcf9463cf98b/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    auto lambda = []
+    {
+        return __func__;
+    };
+    
+    auto lambda2 = []
+    {
+        return __func__;
+    };
+    
+    std::cout << (lambda() == lambda2());
+}
