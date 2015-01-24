0a794bc5235deb0729624d968270d1ee8ba480d9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-24 04:37:43 +0000

    Update archive 62

---
+++ trunk/Archive2/62/d52b5979c189ac/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+template <class F, class... Args>
+void for_each_argument(F f, Args&&... args) 
+{
+    [](...){}((f(std::forward<Args>(args)), 0)...);  // ,0 used when lambda return void, returning void is a valid expression so the ',' return a 0 instead.
+}
+
+template <class F, class... Args>
+void for_each_argument_mine(F f, Args&&... args) 
+{
+    [](...){}((f(std::forward<Args>(args)))...);  // not needed if you guarantee your lambda returns non-void.
+}
+
+int main()
+{
+    auto lam_mine = [](char a) -> int { cout << a; return 0; };
+    for_each_argument_mine(lam_mine, 'H','e','l','l','o');
+    
+    auto lam = [](char a) { cout << a; };
+    for_each_argument(lam, 'H','e','l','l','o');
+}
