7bf63cc2f9a81eb8bfbcfcbfedc5cee820711154
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-25 15:37:48 +0000

    Update archive 34

---
+++ trunk/Archive2/34/0b7313111a651a/main.cpp
@@ -0,0 +1,38 @@
+#include <algorithm> 
+
+namespace test_helpers {
+
+    template <typename T> 
+        T& clone_to(T const& s, T& t) 
+    { 
+        return (t = s); 
+    }
+
+    template <typename T, std::size_t N> 
+        auto clone_to(T const (&s)[N], T (&t)[N]) -> decltype(t)& 
+    {
+        std::copy(s, s+N, t);
+        return t;
+    }
+}
+
+#include <vector>
+#include <cassert>
+
+int main()
+{
+    using namespace test_helpers;
+    {
+        std::vector<int> a(10, 11), b;
+        assert(a == clone_to(a, b));
+        assert(a == b);
+    }
+
+    {
+        int a[10] = {11,11,11,11,11,11,11,11,11,11};
+        int b[10];
+        assert(a == clone_to(a, b));
+        assert(a != b);
+        // boost::range::equal would do the trick here, not gonna copy it out
+    }
+}
