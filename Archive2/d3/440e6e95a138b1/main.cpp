675f576f84ce72fcbbab6571186cb3df0343849a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-25 07:12:31 +0000

    Update archive d3

---
+++ Archive2/d3/440e6e95a138b1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+struct A
+{
+    A(A&&){ std::cout << "A(A&&)" << std::endl; }
+    A(A&){ std::cout << "A(A&)" << std::endl; }
+    operator int(){ return 42; }
+};
+
+struct B
+{
+    operator A(){ return A(); }
+};
+
+B b;
+int a = A(b);
+
+int main(){ std::cout << a << std::endl; }
