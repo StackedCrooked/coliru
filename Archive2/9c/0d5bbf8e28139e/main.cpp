4970c6724b7941d3d6fcc65cdcfb883f3177a807
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-26 18:58:01 +0000

    Update archive 9c

---
+++ Archive2/9c/0d5bbf8e28139e/main.cpp
@@ -0,0 +1,14 @@
+#include <thread>
+
+template <typename T>
+struct f {
+    void operator() (T& result) { result = 1;}
+};
+
+int main() {
+    auto g = [](auto& result) { result = 1; };
+    int x = 0;
+
+    std::thread(f<int>{}, std::ref(x)).join();  // COMPILES
+    std::thread(std::bind(g, std::ref(x))).join();         // FAILS TO COMPILE
+}
