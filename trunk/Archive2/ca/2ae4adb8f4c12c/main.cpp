852f85d01c9d374deae4cb5dafa69dd7c72d3c5f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-26 18:22:02 +0000

    Update archive ca

---
+++ trunk/Archive2/ca/2ae4adb8f4c12c/main.cpp
@@ -0,0 +1,17 @@
+#include <utility>
+#include <thread>
+
+template <typename T>
+struct f {
+    void operator() (T& result) { result = 1;}
+};
+
+int main() {
+    int x = 0;
+    auto g = [](auto& result) { result = 1; };
+
+    std::thread(f<int>(), std::ref(x));  // COMPILES
+    //std::thread(g, std::ref(x));         // FAILS TO COMPILE
+    
+    g(std::ref(x));
+}
