cc53a4e1b4b29cf628745bb2c083d58e453fd7f9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-27 02:37:19 +0000

    Update archive cf

---
+++ Archive2/cf/988e58a7123d49/main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <functional>
+#include <forward_list>
+#include <memory> 
+#include <chrono> 
+
+
+template<typename... Arguments>
+    class delegate{
+
+	public:
+
+		void operator()(Arguments... args){
+
+			for(auto& callback : callbacks){
+				callback(args...);
+			}
+		}
+
+
+		void operator+=(std::function<void(Arguments... args)> function){
+	        auto before_end = callbacks.before_begin();
+	        for (auto& _ : callbacks)
+	            ++ before_end;
+
+	        callbacks.insert_after(before_end, function); //TODO need to replace this and check performance.
+		}
+
+	/* //TODO
+
+		void operator-=(std::function<void(Arguments... args)> function){
+			callbacks.erase(function);
+		}
+	*/
+
+	private:
+	    std::forward_list<std::function<void(Arguments... args)> > callbacks;
+
+	};	
+    
+    
+int main()
+{
+
+    auto first_time = std::chrono::high_resolution_clock::now();
+
+
+    delegate<int> printer;
+    
+    printer += [](int number){
+       std::cout<<"A printer "<< number<<std::endl;   
+    };
+    
+    printer += [](){
+        class sum_class{
+            public:
+            int sum(int number){ return ++number; }
+        };
+        
+        std::shared_ptr<sum_class> sum_object(new sum_class());
+        
+        return [sum_object](int number){ 
+            
+            std::cout<<"B printer "<<sum_object->sum(number)<<std::endl;
+
+        };
+
+    }();
+    
+    
+    printer(20);
+    
+    auto second_time = std::chrono::high_resolution_clock::now();
+
+
+    auto time_diference = std::chrono::duration_cast<std::chrono::nanoseconds>(second_time - first_time);
+
+    double delta = time_diference.count();
+
+    delta /= 1000000000;
+
+    std::cout<<"Took: "<<delta<<std::endl;
+}
