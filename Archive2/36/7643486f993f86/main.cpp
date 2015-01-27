940fe8a5e3aa6b5a399159ef9bb565eb590d548c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-27 14:58:11 +0000

    Update archive 36

---
+++ Archive2/36/7643486f993f86/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using name std;
+int main()
+{
+double d= 30;
+double pl= 3.14; 
+
+double r =d/2;
+double s =pl*r*r;
+double p=2*pl*r;
+
+ cout<< "r="<<r<<endl;
+ cout<< "s="<<s<<endl;
+cout<< "p="<<p<<endl;
+
+}
