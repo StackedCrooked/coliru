912199a8f6f6d676d948908bcd6e82be5f8efa1d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-27 14:15:46 +0000

    Update archive 53

---
+++ Archive2/53/bbf50574bacddd/main.cpp
@@ -0,0 +1,46 @@
+int main(void){
+    
+    int a,b,c,d,e,f,g;
+    
+    for(a=0;a<=5;a++){
+        
+        if(a=0){
+            for(b=0:b<=23;b++){
+                printf("%c",'#x87');}}
+        
+        if(a=1){
+            for(b=0;b<=23;b++){
+                if(0||23){
+                    printf("%c",'#x87');}}}
+        
+        if(a=2){
+            for(b=0;b<=23;b++){
+                if(0||23){
+                    printf("%c",'#x87');}
+                if(b=7){
+                    printf("A");}
+                if(b=8){
+                    printf("L");}
+                if(b=9){
+                    printf("L");}
+                if(b=10){
+                    printf(" ");}
+                if(b=10){
+                    printf("R");}
+                if(b=10){
+                    printf("E");}
+                if(b=10){
+                    printf("S");}
+                if(b=10){
+                    printf("E");}
+                if(b=10){
+                    printf("T");}}}
+        
+        if(a=3){
+            for(b=0;b<=23;b++){
+                if(0||23){
+                    printf("%c",'#x87');}}}
+        
+        if(a=4){
+            for(b=0;b<=23;b++){
+                    printf("%c",'#x87');}}
