9b42bcce545cf3582c71b9f7d772e0f20153bb77
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-27 21:18:23 +0000

    Update archive e2

---
+++ trunk/Archive2/e2/21aeca2b8c4aaa/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class Test {
+ public:
+  Test() {
+    std::cout << "Construct Test instance..." << std::endl;
+  }
+  
+  std::string GetValue() const {
+    return "a value";
+  }
+  
+ private:
+   // Disallow copy and assign.
+   Test(const Test& rhs);
+   Test& operator= (const Test& other);
+};
+
+// Return a newly created Test instance.
+auto_ptr<Test> GetPtr() {
+  return auto_ptr<Test>(new Test());
+}
+
+// Use a test instance.
+std::string Use(const Test& test) {
+  return test.GetValue();
+}
+
+int main() {
+   auto_ptr<Test> result = GetPtr();
+   std::cout << "Test instance returns '" << Use(*result) << "'" << std::endl;
+  
+    // your code goes here
+	return 0;
+}
