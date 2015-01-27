c427a3f9491eae673ee3db4de452e9fb85ba6a9a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-27 18:36:42 +0000

    Update archive 3e

---
+++ trunk/Archive2/3e/a6a18322f80f8a/main.cpp
@@ -0,0 +1,49 @@
+#include <type_traits>
+#include <utility>
+#include <iostream>
+
+template <typename T>
+class Obj;
+
+template <typename T>
+Obj<T> make_obj(T t);
+
+template <typename T>
+struct ret_obj_helper {
+    template <typename RetVal>
+    RetVal ret_obj(T t) {return RetVal(make_obj(t).t);}
+};
+
+// I guess RetVal, having to be explicitly specified, better goes first (?)
+template <typename RetVal, typename T>
+RetVal ret_obj(T&& t)
+{
+    // Overcomplicated for the sake of perfect forwarding
+    return ret_obj_helper<typename std::remove_reference<T>::type>{}.
+      template ret_obj<RetVal>(std::forward<T>(t));
+}
+
+template <typename T>
+class Obj {
+private:
+    T t;
+    Obj (T t) : t(t) { }
+    Obj() = delete;
+
+    friend Obj make_obj<T>(T t);
+
+    // Make all specializations of the member function template 
+    // of ret_obj_helper<T> a friend, regardless of the return type
+    template <typename RetVal>
+    friend RetVal ret_obj_helper<T>::ret_obj(T t);
+};
+
+template <typename T>
+Obj<T> make_obj(T t) { 
+    return Obj<T>(t);
+}
+
+int main()
+{
+    std::cout << ret_obj<float>( 4 );
+}
