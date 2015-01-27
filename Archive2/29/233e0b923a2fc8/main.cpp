c6df871d6b0853a4924b51d7b2939e7aea2adc66
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-27 18:14:33 +0000

    Update archive 29

---
+++ Archive2/29/233e0b923a2fc8/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <cstdlib> // std::exit
+
+bool jump( const std::string& to )
+{
+    std::cout << "jump to loop " << to << "? " ;
+    char c = 'n';
+    if( std::cin >> c )
+    {
+        std::cout << "answer: '" << c << "'\n" ;
+        return c == 'y' || c == 'Y' ;
+    }
+
+    else std::exit(0) ; // maintenance hazard: avoid except in case of catastrophic failures
+                        // present here to mimic the example behavior
+                        // note: destructors of local objects are *not* called
+}
+
+int main()
+{
+    one:
+        std::cout << "loop one begin \n" ;
+        std::cout << "first part of loop one code\n" ;
+
+        if( jump("two") ) goto two ;
+
+        std::cout << "second part of loop one code\n" ;
+        std::cout << "loop one end \n" ;
+        goto one ;
+
+    two:
+        std::cout << "loop two begin \n" ;
+        std::cout << "first part of loop two code\n" ;
+
+        if( jump("one") ) goto one ;
+
+        std::cout << "second part of loop two code\n" ;
+        std::cout << "loop two end \n" ;
+        goto one ;
+}
