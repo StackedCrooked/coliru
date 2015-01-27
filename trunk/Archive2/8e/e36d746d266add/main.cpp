ea8472af1801c37dd602e7a4e041715fad1990dd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-27 19:02:14 +0000

    Update archive 8e

---
+++ trunk/Archive2/8e/e36d746d266add/main.cpp
@@ -0,0 +1,19 @@
+#include <utility>
+
+template <typename func>
+class Entry{
+public:
+    void* Address = (void*)(void(*)())[]{};
+
+    template <typename... Args>
+    auto operator()(Args&&... args)
+     -> decltype( ((typename std::remove_pointer<func>::type*) Address)(std::forward<Args>(args)...) ) {
+        return    ((typename std::remove_pointer<func>::type*) Address)(std::forward<Args>(args)...);
+    }
+};
+
+int main()
+{
+    Entry<void(*)()> en;
+    en();
+}
