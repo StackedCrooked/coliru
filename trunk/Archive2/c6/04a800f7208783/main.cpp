e32be15362c2a4db32d81121e84db64ff4259f73
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-27 23:39:08 +0000

    Update archive c6

---
+++ trunk/Archive2/c6/04a800f7208783/main.cpp
@@ -0,0 +1,62 @@
+#include<stdio.h>
+
+int main(){
+    
+	char a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, x, z, ab, bb, cc;
+	a = 39;  /* ' */
+	b = 126; /* ~ */
+	c = 96;  /* ` */
+	d = 40;  /* ( */
+	e = 111; /* o */
+	f = 41;  /* ) */
+	g = 43;  /* + */
+	h = 45;  /* - */
+	i = 46;  /* . */
+	j = 48;  /* 0 */
+	k = 95;  /* _ */
+	l = 124; /* | */
+	m = 69;  /* E */
+	n = 85;  /* U */
+	o = 67;  /* C */
+	p = 82;  /* R */
+	q = 84;  /* T */
+	r = 79;  /* O */
+	s = 76;  /* L */                                            
+	t = 73;  /* I */                                      
+	u = 78;  /* N */                                
+	v = 71;  /* G */                                  
+	x = 65;  /* A */								 
+	z = 77;  /* M */
+	ab = 33; /* ! */
+	bb = 92; /* \ */ 
+	cc = 47; /* / */
+	
+	
+/*
+	                     ''~``
+                        ( o o )
++------------------.oooO--(_)--Oooo.------------------+
+|                                                     |
+|               EU CURTO LINGUAGEM C!!!               |
+|                                                     |
+|                    .oooO                            |
+|                    (   )   Oooo.                    |
++---------------------\ (----(   )--------------------+
+                       \_)    ) /
+                             (_/
+*/
+
+	printf("                          %c %c%c%c\n", a, a, b, c, c);
+	printf("                         %c %c %c %c\n", d, e, e, f);
+	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", g, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, i, e, e, e, j, h, h, d, k, f, h, h, j, e, e, e, i, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, g);
+	printf("%c                                                       %c\n", l, l);
+	printf("%c                %c%c %c%c%c%c%c %c%c%c%c%c%c%c%c%c %c%c%c%c                %c\n", l, m, n, o, n, p, q, r, s, t, u, v, n, x, v, m, z, o, ab, ab, ab, l );
+	printf("%c                                                       %c\n", l, l);
+	printf("%c                    %c%c%c%c%c                              %c\n", l, i, e, e, e, j, l);
+	printf("%c                    %c   %c     %c%c%c%c%c                    %c\n", l, d, f, j, e, e, e, i, l);
+	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c  %c%c%c%c   %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", g, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, bb, d, h, h, h, h, d, f, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, h, g);
+	printf("%c                        %c%c%c    %c %c                      %c\n", bb, k, f, f, cc);
+	printf("%c                             %c%c%c                       %c\n", d, k, cc);
+	
+	return 0;
+}
