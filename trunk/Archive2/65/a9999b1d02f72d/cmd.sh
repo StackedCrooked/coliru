1e92ccd95e4db888ba4941fd3254bf73fcfbe593
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-28 15:19:14 +0000

    Update archive 65

---
+++ trunk/Archive2/65/a9999b1d02f72d/cmd.sh
@@ -0,0 +1,9 @@
+##clang++ --version
+##g++-4.8 --version2set -x ;
+##g++-4.9 -std=c++11 -Wall -Wextra -Wconversion -pedantic-errors -fstrict-aliasing -O0 -pthread  main.cpp && ./a.out 
+##g++ --version
+##set -x ; g++-4.9 -std=c++11 -Wall -Wextra -Wconversion -pedantic  -O3  -pedantic  -pthread -S main.cpp && cat main.s 
+##set -x ; clang++ -std=c++11 -fsanitize=undefined,signed-integer-overflow -O0 -Wall -Wimplicit-fallthrough -Wextra -Wconversion -Wformat=2 -Wall  -pthread -pedantic main.cpp && ./a.out 
+set -x ; gcc -x c -Wall  main.cpp && ./a.out
+##set -x ; gcc -x c -std=c99 -O0 -W -Wall -Wextra -Wconversion -pedantic -S main.cpp && cat main.s 
+##set -x ; clang -x c -std=c11 -fsanitize=undefined -Wall -Wextra -Wconversion  -O0  -pedantic main.cpp && ./a.out
