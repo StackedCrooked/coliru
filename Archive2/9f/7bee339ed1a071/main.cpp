d22be5297d2e46bb965c64a6e4f70a3b69ec58b9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-28 20:09:20 +0000

    Update archive 9f

---
+++ Archive2/9f/7bee339ed1a071/main.cpp
@@ -0,0 +1,20 @@
+#include <tuple>
+
+template<typename... Ts>
+std::tuple<Ts...> foo(Ts&&... v) {
+    return std::tuple<Ts...>(std::forward<Ts>(v)...); 
+}
+
+struct X { int i; };
+
+#include <iostream>
+#include <typeinfo>
+
+int main() {
+    X x;
+    X const y { 56 };
+    double splurg[10];
+
+    std::cout << typeid(foo(X{42}, x, y, splurg)).name();
+}
+
