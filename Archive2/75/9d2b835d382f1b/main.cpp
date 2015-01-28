b82c67ff8203d2137b6033b2c8385e57ecb6380e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-28 02:30:47 +0000

    Update archive 75

---
+++ Archive2/75/9d2b835d382f1b/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <utility>
+#include <tuple>
+
+template <class T, class...> struct interlace_ {using type = T;};
+template <class... R,  template<class...> class T, class f, class... t, class... P>
+struct interlace_<std::tuple<R...>, T<f, t...>, P...>
+    : interlace_<std::tuple<R..., f>, P..., std::tuple<t...>> {};
+template <class... R, template<class...> class T, class f, class... P>
+struct interlace_<std::tuple<R...>, T<f>, P...>
+    : interlace_<std::tuple<R..., f>, P...> {};
+
+template <class... packs>
+using interlace = interlace_<std::tuple<>, packs...>;
+
+/// TEST:
+    
+template <typename T> void print () {std::cout << __PRETTY_FUNCTION__;}
+
+template <typename...> struct Pack {};
+template <typename...> struct Group {};
+template <typename...> struct Wrap {};
+struct Object {};  struct Blob {};
+
+using TestPack1 = Pack<int, double, Object>;  // 3 types
+using TestPack2 = Group<double, char, int, short, long>;  // 5 types
+using TestPack3 = Wrap<char, short, Blob, char>;  // 4 types
+
+int main()
+{
+    print<interlace<TestPack1, TestPack2, TestPack3>::type>();
+}
