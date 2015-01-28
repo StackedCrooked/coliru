175fe26683b328af48cffde7aa70f5351a864e0f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-28 05:47:49 +0000

    Update archive c5

---
+++ Archive2/c5/cbc6cc97411c4e/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+struct Base1 {
+    Base1() : a(1) {}
+    int a;
+};
+
+struct Base2 {
+    Base2() : b(2) {}
+    int b;
+};
+
+struct Derived : Base2, Base1 {
+    Derived() : c(3) {}
+    int c;
+    void f(int n) { std::cout << n << ", " << a << ", " << b << ", " << c << ", " << '\n'; }
+};
+int main()
+{
+    void (Derived::* pd)(int) = &Derived::f;
+    void (Base1::* pb)(int) = static_cast<void (Base1::*)(int)>(pd);
+    void (Base2::* pc)(int) = static_cast<void (Base2::*)(int)>(pd);
+    // It is a tragedy this cannot be done automatically
+    void (Base2::* px)(int) = pd;
+    // Gives error: cannot convert 'void (Derived::*)(int) to 'void (Base2::*)(int)' in initialization
+ 
+    Derived d;
+    (d.*pb)(0);
+    (d.*pc)(0);
+    
+    Base1 b;
+    (b.*pb)(4); // Undefined behavior
+    
+    Base2 c;
+    (c.*pc)(5); // Undefined behavior
+}
+
