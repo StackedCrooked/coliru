e198b5adf53c6e897a3ddd82340c2197024aeddb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-28 21:37:31 +0000

    Update archive 30

---
+++ trunk/Archive2/30/7ca9a47b883e3c/main.cpp
@@ -0,0 +1,17 @@
+#include <boost/asio.hpp>
+#include <iostream>
+#include <thread>
+
+int main() {
+    boost::asio::io_service svc;
+    boost::asio::deadline_timer timer(svc, boost::posix_time::seconds(2));
+    timer.async_wait([](boost::system::error_code ec) { std::cout << ec.message() << '\n'; });
+
+    std::thread([&] { svc.run(); }).detach();
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "first\n";
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    std::cout << "bye\n";
+}
+
