abc826e99ff20d384aa388b284af81014c3ef1bc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-28 14:11:53 +0000

    Update archive db

---
+++ trunk/Archive2/db/615a6d17a7945f/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <sstream>
+#include <boost/date_time.hpp>
+
+int main()
+{
+    boost::posix_time::ptime time;
+    try 
+    {
+        std::stringstream ss("2014-12-19T15:53:14.533+01:00");
+        boost::local_time::local_time_input_facet* ifc= new boost::local_time::local_time_input_facet();
+        ifc->set_iso_extended_format();
+        ss.imbue(std::locale(ss.getloc(), ifc));
+        boost::local_time::local_date_time zonetime(boost::local_time::not_a_date_time);
+        if( ss >> zonetime ) {
+            time = zonetime.utc_time();
+        }
+
+        std::cout << "Remaining: '" << ss.rdbuf() << "'\n";
+    }
+    catch( std::exception const& e )
+    {
+        std::cout << "ERROR:" << e.what() <<std::endl;
+    }
+    
+    std::cout << "UTC time: " << time;
+    
+    return 0;  
+}
