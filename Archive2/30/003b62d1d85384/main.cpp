25a8c0caad911fcf33d488feb8368b055177897d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-29 23:59:07 +0000

    Update archive 30

---
+++ Archive2/30/003b62d1d85384/main.cpp
@@ -0,0 +1,17 @@
+#include <boost/icl/interval_set.hpp>
+#include <iostream>
+
+int main() {
+    typedef boost::icl::interval_set<unsigned int> set_t;
+    typedef set_t::interval_type ival;
+    set_t outages;
+
+    outages.insert(ival::closed(1,1));
+    outages.insert(ival::open(7,10));
+    outages.insert(ival::open(8,11));
+    outages.insert(ival::open(90,120));
+
+    for(set_t::iterator it = outages.begin(); it != outages.end(); it++){
+        std::cout << it->lower() << ", " << it->upper() << "\n";
+    }
+}
