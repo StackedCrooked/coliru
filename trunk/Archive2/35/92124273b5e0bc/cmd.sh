a6607aa0ec33b44c76e26d66a5b09dda5d6f177c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-29 14:49:06 +0000

    Update archive 35

---
+++ trunk/Archive2/35/92124273b5e0bc/cmd.sh
@@ -0,0 +1,2 @@
+clang++ -std=c++14 -stdlib=libc++ -O3 -Wall -Wextra -pedantic-errors main.cpp -lsupc++ && echo ok
+g++ -std=c++14 -O3 -Wall -Wextra -pedantic-errors main.cpp && echo ok
---
+++ trunk/Archive2/35/92124273b5e0bc/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <type_traits>
+
+int main()
+{
+    static_assert( std::is_same< decltype( 'a' ), char >::value, "type of 'a' is char" ) ;
+    static_assert( std::is_same< decltype( 'a' + 'C' ), int >::value, "type of 'a' + 'C' is int" ) ;
+    static_assert( std::is_same< decltype( +'a' ), int >::value, "type of +'a' is int" ) ;
+}
