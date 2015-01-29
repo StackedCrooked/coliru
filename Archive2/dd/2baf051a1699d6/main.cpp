6b1e8251ff3e7e21cb6b3a723eedb97113a7796d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-29 23:27:21 +0000

    Update archive dd

---
+++ Archive2/dd/2baf051a1699d6/main.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <iostream>
+#include <array>
+#include <vector>
+#include <algorithm>
+#include <memory>
+
+int main()
+{
+    std::cout << "SHIFT TO RIGHT: ";
+    
+    {
+        std::vector<int> v { 1, 2, 3, 4, 5, 6 };
+        auto it = v.begin()+3; // number 4
+        
+        std::rotate(it, it+1, v.end());
+        
+        std::for_each(v.begin(), v.end(), [](int i) {
+            std::cout << i << ", ";
+        });
+    }
+    
+    std::cout << std::endl << std::endl << "SHIFT TO LEFT: ";
+    
+    {
+        std::vector<int> v { 1, 2, 3, 4, 5, 6 };
+        auto it = v.begin()+3; // number 4
+        
+        //std::reverse_iterator<std::vector<int>::iterator> rit(it+1);
+        
+        std::rotate(v.begin(), it, it+1);
+        
+        std::for_each(v.begin(), v.end(), [](int i) {
+            std::cout << i << ", ";
+        });
+    }
+    
+    std::cout << std::endl;
+}
