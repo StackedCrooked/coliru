d3d5bf5123eb97d53258201d1609b5339e17bda3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-29 21:42:10 +0000

    Update archive 26

---
+++ trunk/Archive2/26/9f221d5a8efab8/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+struct Params { };
+
+template <class T>
+struct Base
+{
+    int data() const { return 42; }
+};
+
+template <template <class> class D, class P>
+struct Middle : private D<P> // private
+{
+    using D<P>::data;
+};
+
+struct Final : public Middle<Base,Params>
+{
+    using Base<Params>::data;
+};
+
+
+int main() {
+
+    Final f;
+
+    std::cout << f.data() << std::endl;
+
+    return 0;
+}
