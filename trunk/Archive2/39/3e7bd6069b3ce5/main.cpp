b743460dd2716e09bf73777dfa507ee3b7a1abfc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-29 18:16:18 +0000

    Update archive 39

---
+++ trunk/Archive2/39/3e7bd6069b3ce5/main.cpp
@@ -0,0 +1,24 @@
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/iostreams/device/file_descriptor.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <boost/filesystem.hpp>
+#include <iostream>
+#include <fstream>
+
+namespace pt = boost::property_tree;
+namespace io = boost::iostreams;
+namespace fs = boost::filesystem;
+
+int main()
+{
+    boost::filesystem::path::imbue(std::locale("en_US.utf8"));
+    fs::wpath const fname = L"test撧.xml";
+    io::file_descriptor_source fs(fname);
+    io::stream<io::file_descriptor_source> fsstream(fs);
+
+    pt::ptree xml;
+    pt::read_xml(fsstream, xml);
+
+    for (auto const& node : xml.get_child("root"))
+        std::cout << node.first << ": " << node.second.get_value<std::string>() << "\n";
+}
