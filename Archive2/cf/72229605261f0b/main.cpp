af6b0e0c543f51948b6339e2786030e524fb3617
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-29 15:07:35 +0000

    Update archive cf

---
+++ Archive2/cf/72229605261f0b/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int var1;
+    int var2;
+    cin>>var>>var2
+    
+    cout<< "Your input is"
+    << var<<""<<var2<<endl;
+    
+    system("pause");
+}
+
