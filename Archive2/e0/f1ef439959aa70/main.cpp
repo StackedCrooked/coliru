1e44ed8351ef4f7646a4e27b117c48529a6c4d74
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-29 05:25:06 +0000

    Update archive e0

---
+++ Archive2/e0/f1ef439959aa70/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <functional>
+
+class LeakedMemory
+{
+public:
+    int v;
+
+    LeakedMemory()
+    {
+		std::cout << "Nasci\n";
+	}
+    
+    LeakedMemory(const LeakedMemory& lm) : v(lm.v)
+    {
+    	std::cout << "Nasci da copia\n";
+	}
+    
+    LeakedMemory(LeakedMemory&& lm) : v(lm.v)
+    {
+        std::cout << "Nasci do move\n";
+	}
+    
+	~LeakedMemory()
+	{
+		std::cout << "Destroyed\n";
+	}
+};
+
+// Non funciona
+//void (*lambda)(void) ToLeakOrNotToLeak()
+//{
+//	LeakedMemory lm{ 5 };
+//
+//	return []() { std::cout << "Aborto da natureza\n"; };
+//}
+
+std::function<void(void)> ToLeakOrNotToLeak()
+{
+	LeakedMemory lm;
+
+	lm.v = 5;
+
+	return [=]() // Don't you dare &
+	{ 
+		std::cout << "Aborto da natureza\n";
+		std::cout << lm.v << std::endl;
+		
+	};
+}
+
+int main(int argc, char* argv [])
+{
+	auto p = ToLeakOrNotToLeak();
+
+	p();
+
+	return 0;
+}
