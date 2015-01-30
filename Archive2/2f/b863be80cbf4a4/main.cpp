afa4f1863102ea65cc2fef5c96e050ed3b2fcb10
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-30 13:16:05 +0000

    Update archive 2f

---
+++ Archive2/2f/b863be80cbf4a4/main.cpp
@@ -0,0 +1,13 @@
+struct Bar
+{
+   void Foo(bool = false, int = 10, int = 20) {}
+};
+
+int main()
+{
+   int x = 40, y = 50;
+   Bar b;
+   b.Foo();            // correct usage
+   b.Foo(true, x, y);  // correct usage
+   b.Foo(x, y);        // problem:  compiles but won't do what the caller expects
+}
