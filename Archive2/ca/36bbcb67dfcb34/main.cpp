166cd10ec1b260b891ccc6ba6fea555bdf86b22d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-30 21:07:01 +0000

    Update archive ca

---
+++ Archive2/ca/36bbcb67dfcb34/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+template <size_t N>
+void foo(const char (&s)[N]) {
+    std::cout << "array, size=" << N-1 << std::endl;
+}
+
+template <typename T>
+typename std::enable_if<std::is_same<T, char const*>{}>::type foo(T s) {
+    std::cout << "raw, size=" << strlen(s) << std::endl;
+}
+int main()
+{
+    foo("hello"); // prints array, size=5
+}
+
