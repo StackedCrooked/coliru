2f016337235879ca9787d817ae9fadada896a1b2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-30 02:24:29 +0000

    Update archive d8

---
+++ trunk/Archive2/d8/bd7e2189a2fe8d/cmd.sh
@@ -0,0 +1,4 @@
+od -Anone -w8 -t f8 /dev/urandom | head -10 | sort -g > floats;
+od -Anone -w4 -t u4 /dev/urandom | head -10 > uints;
+paste floats uints > input.txt;
+g++ -std=c++11 -O3 -march=native -Wall -pedantic -pthread main.cpp -lboost_system -lboost_iostreams -lrt && ./a.out
