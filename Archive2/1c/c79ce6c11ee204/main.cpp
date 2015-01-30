4953cf7d6b6cc2dffcff5596fb8109c4bd04e31f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-30 10:31:51 +0000

    Update archive 1c

---
+++ Archive2/1c/c79ce6c11ee204/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include <type_traits>
+
+struct S1 {
+    void f() { std::cout << "S1::f\n"; }
+};
+
+struct S2 {
+    void f() { std::cout << "S2::f\n"; }
+    void g() { std::cout << "S2::g\n"; }
+};
+
+template <class T, class V = void>
+struct D : private T {
+    using T::f;
+    //using T::g; // need this only if T provides g() function
+};
+
+template<class T, class V = void>
+struct has_g : std::false_type { };
+
+template <class T>
+struct D<T, decltype(void(&T::g))> : private T {
+    using T::f;
+    using T::g; // need this only if T provides g() function
+};
+
+int main() {
+    D<S1>().f(); // ok. Prints 'S1::f'
+    D<S2>().f(); // ok. Prints 'S2::f' 
+    D<S2>().g(); // fail. But wants to be ok and prints 'S2::g'
+    return 0;
+}
