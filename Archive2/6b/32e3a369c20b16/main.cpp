3ef2d146d1e7f42872178dca1020f1bca81842cd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-30 18:51:14 +0000

    Update archive 6b

---
+++ Archive2/6b/32e3a369c20b16/main.cpp
@@ -0,0 +1,24 @@
+#include <tuple>
+#include <iostream>
+#include <utility>
+
+template <typename T, std::size_t... indices>
+auto invert( T&& tuple, std::index_sequence<indices...> )
+{
+    using tuple_t = std::remove_reference_t<T>;
+	auto constexpr tuple_size = std::tuple_size<tuple_t>{};
+	return std::tuple<std::tuple_element_t<tuple_size - indices - 1, tuple_t>...>( std::get<tuple_size - indices - 1>(std::forward<T>(tuple))... );
+}
+
+template <typename T>
+auto invert( T&& tuple )
+{
+	return invert( std::forward<T>(tuple), std::make_index_sequence<std::tuple_size<std::remove_reference_t<T>>{}>() );
+}
+
+int main()
+{
+	std::tuple<char, int, std::string> tup('4', 2, "asdf");
+	auto result = invert(tup);
+	std::cout << std::get<0>(result) << ' ' << std::get<1>(result) << ' ' << std::get<2>(result);
+}
