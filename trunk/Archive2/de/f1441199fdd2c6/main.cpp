641c6a0bbeb1bd405f96226c693fc7723abd0f77
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-30 22:10:03 +0000

    Update archive de

---
+++ trunk/Archive2/de/f1441199fdd2c6/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <tuple>
+using namespace std;
+ 
+template <typename... Args>
+struct pack
+{
+};
+ 
+template <template <typename... Args> class ENCAP, typename... Args>
+struct encapsulate_arguments
+{
+    typedef pack<ENCAP<Args>...> type;
+};
+ 
+template <template <typename... Args> class ENCAP, typename... Args>
+struct encapsulate_arguments<ENCAP, pack<Args...>>
+{
+	typedef pack<ENCAP<Args>...> type;
+};
+ 
+template <typename L>	
+struct Master
+{
+	template <typename T>
+	struct Slave
+	{
+		typedef T type;	
+	};
+};
+
+template <typename L>   
+struct Other
+{
+  typedef typename encapsulate_arguments<Master<L>::template Slave, pack<double, int>>::type EmbeddedType;
+};
+ 
+int main() {
+	encapsulate_arguments<Master<float>::Slave, pack<double, int>>::type foo;
+	encapsulate_arguments<Master<float>::Slave, double, int>::type bar;
+	return 0;
+}
