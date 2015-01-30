1399f6d9e800d3928ba7ed5806d5d1d33d880af8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-30 08:30:54 +0000

    Update archive 8a

---
+++ Archive2/8a/b0195030ec5273/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    fixed(std::cout) ; // fine: function call, koenig lookup applies
+    
+    std::cout << fixed << 2.7 << '\n' ; // *** error: fixed is not not a function call 
+    std::cout << setprecision(2) << 2.7 << '\n' ; // *** error: setprecision(int), int is not a member of std
+}
