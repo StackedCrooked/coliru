a9426339c339d3ac5c614909e6105c19d3273b9a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-31 23:47:07 +0000

    Update archive 47

---
+++ trunk/Archive2/47/c44b64e574bff8/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+using namespace std;
+
+int main()
+{
+    std::vector<std::string> words = {
+        "Hello", "from", "GCC", __VERSION__, "!"
+    };
+    
+    
+    std::cout << words << std::endl;
+    
+    
+    vector<int> nums;
+    nums.push_back(1);
+    std::cout << "how many elements? --> " << nums.size() << std::endl;
+    
+    return 0;
+}
