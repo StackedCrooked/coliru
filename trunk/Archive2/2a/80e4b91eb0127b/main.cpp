f10be583ff540824af190abea12569b7e71365bd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-31 12:59:23 +0000

    Update archive 2a

---
+++ trunk/Archive2/2a/80e4b91eb0127b/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <type_traits>
+
+template <size_t N>
+void foo(const char (&s)[N]) {
+    std::cout << "array, size=" << N-1 << std::endl;
+}
+
+template <typename T>
+typename std::enable_if<std::is_convertible<T, char const*>{}>::type foo(T s) {
+    std::cout << "raw, size=" << strlen(s) << std::endl;
+}
+int main()
+{
+    foo("hello"); // prints array, size=5
+}
+
