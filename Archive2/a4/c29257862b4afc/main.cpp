076542ed8dffa0a5e39724f9eb9818690e1d3966
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-01-31 11:49:33 +0000

    Update archive a4

---
+++ Archive2/a4/c29257862b4afc/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+class A{
+public:
+    A(int){
+        std::cout<<"Constructor\n";   
+    }
+    A(const A&){
+        std::cout<<"Copy Constructor\n";   
+    }
+    A(A&&){
+        std::cout<<"Move Constructor\n";   
+    }
+    void operator=(const A&){
+        std::cout<<"operator= \n";   
+    }
+    void operator=(A&&){
+        std::cout<<"operator= (rvalue) \n";   
+    }
+
+};
+
+int main(){
+	std::vector<A> vec;
+    vec.push_back(A(5));
+    std::cout<<"____\n";
+    vec.emplace_back(5);
+}
