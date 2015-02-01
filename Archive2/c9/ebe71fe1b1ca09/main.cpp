29ab47a44e23407b36a1f73797e710fac339c5e5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-01 08:06:41 +0000

    Update archive c9

---
+++ Archive2/c9/ebe71fe1b1ca09/main.cpp
@@ -0,0 +1,21 @@
+struct B
+{
+    void foo(float a){ }  
+};
+
+struct C
+{
+    void foo(int a){ }
+};
+
+struct A : B, C
+{
+    using B::foo;
+    using C::foo;
+} a;
+
+int main()
+{
+    a.foo(5);
+    a.foo(3.14f);
+}
