948035e3244f6c336cd00cd432764ad6c8e0d0f9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-01 21:35:06 +0000

    Update archive 7b

---
+++ trunk/Archive2/7b/45fe5ef552010b/main.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <string>
+#include <iostream>
+using namespace std;
+using std::placeholders::_1;
+
+int main() {
+    std::string v1 = "foo";
+	std::string v2 = "foo";
+	
+    {
+    	bool r = 
+    	    std::bind(
+    	        static_cast<bool(*)(const std::string&, const std::string&)>(operator==<char, std::char_traits<char>, std::allocator<char>>),
+    	        std::placeholders::_1,
+    	        std::cref(v1))(v2);
+    	std::cout << r << '\n';
+    }
+    {
+        bool r = 
+    	    std::bind(
+    	        std::equal_to<>(),
+    	        std::placeholders::_1,
+    	        std::cref(v1))(v2);
+    	std::cout << r << '\n';
+    }
+}
