182c24682a24ef84672ce080d9ffa16d0fd5d6b4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-02 18:28:57 +0000

    Update archive 01

---
+++ Archive2/01/7e844bb222d752/main.cpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <utility>
+
+    namespace named_operator {
+      template<class D>struct make_operator{};
+    
+      template<class T, class O> struct half_apply { T&& lhs; };
+    
+      template<class Lhs, class Op>
+      half_apply<Lhs, Op> operator*( Lhs&& lhs, make_operator<Op> ) {
+          return {std::forward<Lhs>(lhs)};
+      }
+    
+      template<class Lhs, class Op, class Rhs>
+      auto operator*( half_apply<Lhs, Op>&& lhs, Rhs&& rhs )
+      -> decltype( invoke( std::forward<Lhs>(lhs.lhs), Op{}, std::forward<Rhs>(rhs) ) )
+      {
+          return invoke( std::forward<Lhs>(lhs.lhs), Op{}, std::forward<Rhs>(rhs) );
+      }
+    }
+    
+namespace vector_ops {
+    struct append_tag {};
+    static named_operator::make_operator<append_tag> append;
+
+    template<class T, class A1, class A2>
+    std::vector<T,A1> invoke( std::vector<T,A1> lhs, append_tag, std::vector<T,A2> const& rhs ) {
+        lhs.insert(lhs.end(), rhs.begin(), rhs.end());
+        return std::move(lhs); // RVO blocked
+    }
+}
+
+using vector_ops::append;
+
+int main()
+{
+    std::vector<std::string> hello = {"hello", " "};
+    std::vector<std::string> world = {"world", "!", "\n"};
+    for(auto&& s : hello *append* world) {
+        std::cout << s;
+    }
+}
