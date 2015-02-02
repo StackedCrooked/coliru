a01fba1265c0bc04a92f8c184f26439b0135b1b7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-02 10:30:34 +0000

    Update archive 35

---
+++ Archive2/35/0d9a9080f380df/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+ 
+void independentThread() 
+{
+    std::cout << "Starting concurrent thread.\n";
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    std::cout << "Exiting concurrent thread.\n";
+}
+ 
+void threadCaller() 
+{
+    std::cout << "Starting thread caller.\n";
+    std::thread *ptr = new std::thread(independentThread);
+    ptr->detach();
+    delete ptr;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "Exiting thread caller.\n";
+}
+ 
+int main() 
+{
+    threadCaller();
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    std::cout << "Exiting main thread.\n";
+}
