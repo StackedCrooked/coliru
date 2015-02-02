84c2c11ffdd11c85a5df017c54e16cbdc7f15cfa
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-02 23:38:24 +0000

    Update archive 55

---
+++ Archive2/55/ef2315d4bb0359/main.cpp
@@ -0,0 +1,17 @@
+template <typename T, int n>
+class SmallVector {
+private:
+    T* begin_;
+    T* end_;
+    T* capacity_;
+    alignas(T) char data_small_[n * sizeof(T)];
+public:
+    SmallVector()
+    : begin_(reinterpret_cast<T*>(data_small_))
+    , end_(reinterpret_cast<T*>(data_small_))
+    , capacity_(reinterpret_cast<T*>(data_small_) + n)
+    {}
+};
+
+int main()
+{}
