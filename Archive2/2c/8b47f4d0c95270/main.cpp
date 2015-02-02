f0a92449bc40d909769b72f60e37cd5dd8d754ac
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-02 10:44:33 +0000

    Update archive 2c

---
+++ Archive2/2c/8b47f4d0c95270/main.cpp
@@ -0,0 +1,33 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+struct City {
+    string name;
+    City *nextCity;
+    string message;
+};
+
+City buildNetwork() {
+    City *cityList;
+    City *newCity;
+    City *head;
+
+    newCity = new City;
+    head = newCity;
+
+    for(int8_t n=0; n<10; n++) {
+        City *nxc = new City;
+        nxc->name = "Los Angeles";
+        nxc->nextCity=cityList;
+        //newCity->nextCity=nxc;
+        cityList = nxc;
+    }
+    return cityList;
+}
+
+int main() {
+    City *builtList = new City;
+    builtList = buildNetwork();
+}
