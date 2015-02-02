c82629ec578cfc9f33186903dbbef8a4321f946f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-02 13:35:42 +0000

    Update archive 3a

---
+++ Archive2/3a/e003ea2284eefa/main.cpp
@@ -0,0 +1,38 @@
+#include <type_traits>
+
+enum class Color {Red, Green, Blue};
+enum class ShowAxes {False, True};
+enum class ShowLabels {False, True};
+
+    template<class Result, class...>
+    struct extract;
+    
+    template<class Result, Result Value, class... Tail>
+    struct extract<Result, std::integral_constant<Result, Value>, Tail...> : std::integral_constant<Result, Value> {};
+    
+    template<class Result, class Head, class... Tail>
+    struct extract<Result, Head, Tail...> : extract<Result, Tail...> {};
+
+template<class X, class Y, class Z>
+class A { 
+public:
+    static constexpr Color col = extract<Color, X, Y, Z>::value;
+    static constexpr ShowAxes axes = extract<ShowAxes, X, Y, Z>::value;
+    static constexpr ShowLabels labels = extract<ShowLabels, X, Y, Z>::value;
+};
+
+#define AS_IC(Value) std::integral_constant<decltype(Value), Value>
+
+int main(){
+
+    A<AS_IC(Color::Red), AS_IC(ShowAxes::True), AS_IC(ShowLabels::True)> a;
+    A<AS_IC(Color::Red), AS_IC(ShowLabels::True), AS_IC(ShowAxes::True)> b;
+    static_assert(a.col == Color::Red, "Oops");
+    static_assert(a.axes == ShowAxes::True, "Oops");
+    static_assert(a.labels == ShowLabels::True, "Oops");
+    static_assert(b.col == Color::Red, "Oops");
+    static_assert(b.axes == ShowAxes::True, "Oops");
+    static_assert(b.labels == ShowLabels::True, "Oops");
+    
+    return 0;
+ }
