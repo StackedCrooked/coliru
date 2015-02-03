a9972e270a49834bb7221ddc31b27de068b6a386
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-03 18:16:14 +0000

    Update archive 7b

---
+++ trunk/Archive2/7b/1f93663bc0047a/main.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+    ifstream foo("foo.txt");
+
+    while (foo.good()){
+        string bar;
+        getline(foo, bar);
+        cout << bar << endl;
+    }
+    cout << "^^^^^ Or does it? ^^^^^";
+}
