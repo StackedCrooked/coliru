ad8d4c84c875a2d1de03e718057e537ca187a469
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-03 15:28:36 +0000

    Update archive 98

---
+++ Archive2/98/754c238727e406/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <boost/chrono.hpp>
+#include <boost/chrono/process_cpu_clocks.hpp>
+
+using namespace boost::chrono;
+
+template <typename R, typename P>
+    static std::ostream& operator<<(std::ostream& os, duration<R,P> const& d) {
+        return os << boost::chrono::duration_cast<boost::chrono::milliseconds>(d).count() << "ms";
+    }
+
+template <typename Clock = boost::chrono::process_cpu_clock, typename F, typename... Args>
+nanoseconds bench(F&& f, Args&&... args) {
+    auto start = Clock::now();
+
+    volatile auto force = std::forward<F>(f)(std::forward<Args>(args)...);
+    (void) force;
+
+    return Clock::now() - start;
+}
+
+int main() {
+    srand(time(NULL));
+
+    auto ll = [](int runs) {
+        long long result = 0;
+        for (int i = 0; i < runs; i++) {
+            int x = rand() % 100 + 1;
+            int y = rand() % 100 + 1;
+            auto dummy = x * y;
+            result = (result + dummy) % 50;
+        }
+    
+        return result;
+    };
+
+    std::cout << "1st method: " << bench(ll, 1e7)   << "\n";
+    std::cout << "2nd method: " << bench(ll, 3e7) << "\n";
+}
