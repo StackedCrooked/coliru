434c5df0b75c6533be6ac36f96e7d0dcc005d96e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-03 05:52:15 +0000

    Update archive 40

---
+++ Archive2/40/7eaaf612339bee/main.cpp
@@ -0,0 +1,11 @@
+int main()
+{
+    //1. Get all words in "file.txt" and place it into a container
+    //2. Count all words in the container
+    //3. Is there a word "STL" in that file?
+    //4. What is the last word?
+    //5. What is the middle word?
+    //6. How many words are there that has length greater than 3?
+    //7. Remove all words whose length is less than 4
+    //8. Print content of container
+}
