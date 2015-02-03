374a4ac3c76590e3538e9db034628b30f8671f62
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-03 21:51:45 +0000

    Update archive 44

---
+++ Archive2/44/0e38eb9a76ea6f/main.cpp
@@ -0,0 +1,29 @@
+#include <boost/filesystem.hpp>
+#include <boost/range/iterator_range.hpp>
+#include <boost/range/algorithm.hpp>
+#include <vector>
+#include <iostream>
+#include <map>
+
+int main() {
+    using boost::filesystem::path;
+    using boost::filesystem::directory_iterator;
+    using boost::make_iterator_range;
+
+    path p("."); // or "C:\\Customer\\AppleFruit\\"
+
+    std::map<std::string, std::vector<path> > bins {
+        { "In_",  {} },
+        { "Out_", {} },
+      //{ "",     {} }, // remaining files
+    };
+
+    for(auto&& de: make_iterator_range(directory_iterator(p), {}))
+        for(auto& bin: bins)
+            if (de.path().filename().native().find(bin.first) == 0)
+                bin.second.push_back(std::move(de));
+
+    for(auto& bin: bins)
+        for(auto& f : bin.second)
+            std::cout << "Prefix '" << bin.first << "': " << f << "\n";
+}
