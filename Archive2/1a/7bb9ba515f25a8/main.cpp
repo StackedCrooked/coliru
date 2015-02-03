54c77cc74fe15ed6bca5ddb1f5c263e5494b8dfb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-03 21:11:39 +0000

    Update archive 1a

---
+++ Archive2/1a/7bb9ba515f25a8/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+namespace detail {
+    template<class...>
+    struct make_void { using type = void; };
+    
+    template<class... Ts>
+    using void_t = typename make_void<Ts...>::type;
+    
+    template<class T, class = void>
+    struct has_size_member : std::false_type { };
+    
+    template<class T>
+    struct has_size_member<T, decltype(std::declval<T&>().size(), void())>
+        : std::true_type { };
+}
+
+template <class T>
+struct MyHelper
+{
+    size_t size() const { return sizeof(T); }
+    void print(std::ostream& out) const { out << "(" << get_size(detail::has_size_member<T>()) << ")"; }
+private:
+    std::size_t get_size(std::true_type) const
+    { return static_cast<T const&>(*this).size(); }
+    
+    std::size_t get_size(std::false_type) const
+    { return sizeof(T); }
+};
+
+struct X : MyHelper<X>
+{
+    size_t size() const { return 24; }
+};
+
+int main()
+{
+    MyHelper<X> v;
+    MyHelper<int> x;
+    v.print(std::cout);
+    x.print(std::cout);
+}
