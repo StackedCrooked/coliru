9a15228a9aaa9c18b6daccc9c332f67490cf2642
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-04 12:16:18 +0000

    Update archive ea

---
+++ trunk/Archive2/ea/75a5e3f953a035/main.cpp
@@ -0,0 +1,31 @@
+#include <atomic>
+#include <cstdint>
+#include <type_traits>
+
+extern void* aglujUnit;
+
+struct Closure {
+    void* type;
+    void** freeVariables;
+    std::uint32_t freeVariableCount;
+    void* (*implementation)(Closure*, void* parameter);
+
+    void* operator()(void* parameter) {
+        return this->implementation(this, parameter);
+    }
+};
+
+struct Lazy {
+    void* type;
+    void* value;
+    Closure initializer;
+};
+
+void* force(Lazy* lazy) {
+    auto value = __sync_fetch_and_add(&lazy->value, 0);
+    if (value == nullptr) {
+        value = lazy->initializer(aglujUnit);
+        __sync_lock_test_and_set(&lazy->value, value);
+    }
+    return value;
+}
