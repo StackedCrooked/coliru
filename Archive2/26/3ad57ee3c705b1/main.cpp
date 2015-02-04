a376dc1cda6d3fcc2fccfb4bea8cdb9b2bf71430
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-04 20:27:45 +0000

    Update archive 26

---
+++ Archive2/26/3ad57ee3c705b1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main()
+{
+  std::cout << 123.456e-67 << '\n'
+            << .1E4f       << '\n'
+            << 58.         << '\n'
+            << 4e2         << '\n';
+}
