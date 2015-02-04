263190084e0971b9e5ea77c55001a62163b2a7af
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-04 18:16:55 +0000

    Update archive da

---
+++ trunk/Archive2/da/e33c6b6402a5b7/cmd.sh
@@ -0,0 +1,2 @@
+set -x; clang++ -O3 -std=c++1y -stdlib=libc++ -Wall -Wextra -pedantic main.cpp  -lcxxrt -ldl -o a.out && ./a.out
+g++ -O3 -std=c++1y -Wall -Wextra -pedantic main.cpp -o a.out && ./a.out
