5a59b224f04726b42edc5f9fd71f953e45fc5961
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-04 14:12:11 +0000

    Update archive 20

---
+++ trunk/Archive2/20/b28524f5bb78d4/main.cpp
@@ -0,0 +1,55 @@
+#include <atomic>
+#include <cstddef>
+#include <cstdint>
+#include <type_traits>
+#include <utility>
+ 
+#define AGLUJ_CHECK_CXX_TYPE(Type) \
+    static_assert(std::is_standard_layout<Type>() && \
+                  std::is_same<decltype(std::declval<Type>().type), void*>() && \
+                  offsetof(Type, type) == 0, \
+                  #Type " is fucked-up!")
+ 
+namespace agluj {
+    struct Closure {
+        void* type;
+        void** freeVariables;
+        std::uint64_t freeVariableCount;
+        void* (*implementation)(Closure const*, void* parameter);
+    
+        void* operator()(void* parameter) const {
+            return implementation(this, parameter);
+        }
+    };
+    AGLUJ_CHECK_CXX_TYPE(Closure);
+ 
+    struct Lazy {
+        void* type;
+        void* value;
+        Closure initializer;
+    };
+    AGLUJ_CHECK_CXX_TYPE(Lazy);
+ 
+    void* force(Lazy* lazy, void* unit) {
+        auto value = __sync_fetch_and_add(&lazy->value, 0);
+        if (__builtin_expect(value == nullptr, false)) {
+            value = lazy->initializer(unit);
+            __sync_lock_test_and_set(&lazy->value, value);
+        }
+        return value;
+    }
+ 
+    struct Ref {
+        void* type;
+        void* value;
+    };
+    AGLUJ_CHECK_CXX_TYPE(Ref);
+ 
+    void* get(Ref* ref) {
+        return __sync_fetch_and_add(&ref->value, 0);
+    }
+ 
+    void* set(Ref* ref, void* value) {
+        return __sync_lock_test_and_set(&ref->value, value);
+    }
+}
