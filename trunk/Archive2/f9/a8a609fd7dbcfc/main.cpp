4617d9a2a06d31ab5307d83337edb58806308825
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-05 08:42:40 +0000

    Update archive f9

---
+++ trunk/Archive2/f9/a8a609fd7dbcfc/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+public:
+void eat() { std::cout << "I'm eating generic food." << endl; }
+//virtual void eat() { std::cout << "I'm eating generic food." << endl; }
+};
+
+class Cat : public Animal
+{
+public:
+void eat() { std::cout << "I'm eating a rat." << endl; }
+};
+
+void func(Animal *xyz) { xyz->eat(); }
+//void funcv(Animal *xyz) { xyz->veat(); }
+
+int main (){
+    Animal *animal = new Animal;
+    Cat *cat = new Cat;
+    
+    animal->eat(); // outputs: "I'm eating generic food."
+    cat->eat();    // outputs: "I'm eating a rat."
+    
+    cout << endl;
+    
+    func(animal); // outputs: "I'm eating generic food."
+    func(cat);    // outputs: 
+    
+}
