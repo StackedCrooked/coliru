ad7478c425e3d4a11aabafac1491497df9129997
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-05 07:33:56 +0000

    Update archive 19

---
+++ trunk/Archive2/19/22cb9cbb26fd93/main.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+#include <cstring>
+int main()
+{
+    char str2[30] = "\052";
+	char str3[30] = {'\0','5','2'};
+
+	printf("Length of str %d\n", strlen(str2));
+	printf("Length of str %d\n", strlen(str3));   
+}
