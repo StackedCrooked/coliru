80ad1f639b6094bf0a664c1aedd8aa5bc704a94c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-05 02:56:10 +0000

    Update archive ab

---
+++ trunk/Archive2/ab/adff80e3a2a573/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <typeinfo>
+
+class Base
+{
+public:
+ virtual void Teste()
+ {
+  std::cout << typeid(*this).name() << std::endl;
+  (*this).Print();
+  this->Print();
+ }
+ 
+ virtual void Print()
+ {
+     std::cout << "Hello from Base" << std::endl;
+ }
+};
+
+class Derived : public Base
+{
+public:
+ virtual void Teste() override
+ {
+  std::cout << typeid(*this).name() << std::endl;
+  Base::Teste();
+ }
+ 
+ virtual void Print() override
+ {
+     std::cout << "Hello from Derived" << std::endl;
+ }
+};
+
+int main(int argc, char* argv [])
+{
+ Derived d;
+
+ d.Teste();
+
+ return 0;
+}
