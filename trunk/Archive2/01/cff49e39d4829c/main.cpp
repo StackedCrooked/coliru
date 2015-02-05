9915dcac2c4d9b698de1a400ea031836469c15ad
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-05 05:49:59 +0000

    Update archive 01

---
+++ trunk/Archive2/01/cff49e39d4829c/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>       
+#include <thread>         
+
+
+void foo(int& x)
+{
+  std::cout << x << std::endl;
+}
+
+int main() 
+{
+    int value = 10;
+    foo(value);
+
+    std::thread th (foo, value);     
+
+    th.join();
+}
