47a0c5c79a599cc39277436afff7e5a34e890ddd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-05 14:42:20 +0000

    Update archive 87

---
+++ Archive2/87/520c5e0ced135a/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <tuple>
+#include <utility>
+using namespace std;
+ 
+    template<class Func, class Tuple, size_t...Is>
+    void for_each_in_tuple(Func f, Tuple&& tuple, std::index_sequence<Is...>){
+        using expander = int[];
+        (void)expander { 0, ((void)f(std::get<Is>(std::forward<Tuple>(tuple))), 0)... };
+    }
+    
+    template<class Func, class Tuple>
+    void for_each_in_tuple(Func f, Tuple&& tuple){
+        for_each_in_tuple(f, std::forward<Tuple>(tuple), std::make_index_sequence<std::tuple_size<std::decay_t<Tuple>>::value>());
+    }
+ 
+class Lambda{
+public: 
+   template<class T>
+   void operator()(T arg){ std::cout << arg << "; "; }
+};
+ 
+int main() {
+    for_each_in_tuple(Lambda{}, std::make_tuple(1, 2.0f));
+}
+ 
