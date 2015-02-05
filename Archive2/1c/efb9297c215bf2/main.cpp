4248a7fe6648da1e802a7363df3e9f86ee214ba9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-05 00:09:27 +0000

    Update archive 1c

---
+++ Archive2/1c/efb9297c215bf2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+enum  { ONE = 1 , << 1 = " << ( ull << 1 ) << ' \ n ' << "0x123 << 63 ​​=" << ( ull << 63 ​​) << ' \ n ' // estouro em não assinado << " 0x10 << 10 = " << ( c << 10 ) << ' \ n ' ; // char é promovido para int longo tempo ll = - 1,000 ; std :: cout << std :: dezembro << "-1000 >> 1 = " << ( ll >> ONE ) << ' \ n ' ; }
+
+
+        
+     
+       
+           
+                     
+                       
+      
+            
