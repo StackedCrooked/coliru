60d4c4052875e77e14a4f56ee0bc9d771adafcde
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-05 02:59:38 +0000

    Update archive 79

---
+++ trunk/Archive2/79/124156f39b18c9/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class container{
+    public:
+    static void method_a(){
+        
+    }
+};
+
+void method_a() {
+    
+}
+
+void method_b(){
+    
+}
+
+int main()
+{
+    std::cout << "Hello world" << std::endl;
+}
