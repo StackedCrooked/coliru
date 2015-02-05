a48c1b7e1b3e935780a249c4db465acce5620ac1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-05 13:19:40 +0000

    Update archive 37

---
+++ trunk/Archive2/37/03bdfbfa047700/main.cpp
@@ -0,0 +1,36 @@
+//Layout.h
+#ifndef LAYOUT_H
+#define LAYOUT_H
+
+#ifndef SPEAKER_H
+#define SPEAKER_H
+namespace LSS
+{
+    struct Speaker
+    {
+        Speaker(int val1, float val2)
+        :some_int(val1)
+        , some_float(val2)
+        {
+        }
+
+        ~Speaker()    
+        {
+            //do other stuff
+        }
+
+         int some_int;
+         float some_float;
+
+    };//struct Speaker
+}
+#endif
+
+class Layout
+{
+    public:
+        //unrelated stuff
+    private:
+        LSS::Speaker temp;
+};
+#endif
