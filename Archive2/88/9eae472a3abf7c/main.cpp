f4c8c3dcaddb7491c837b0d2951412730a611384
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-05 18:40:07 +0000

    Update archive 88

---
+++ Archive2/88/9eae472a3abf7c/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cmath>
+
+struct Point { double x,y; };
+
+struct Shape {
+    virtual ~Shape() {}
+    virtual double area() const = 0;
+  protected:
+    virtual void print(std::ostream& os) const = 0;
+
+    friend std::ostream& operator<<(std::ostream& os, Shape const& s) {
+        s.print(os);
+        return os;
+    }
+};
+
+struct Circle : Shape {
+    Circle(Point c, double r) : centre(c), radius(r) {}
+
+    Point centre;
+    double radius;
+
+    virtual double area() const override {
+        return (radius*radius) * M_PI;
+    }
+
+    virtual ~Circle() { }
+
+  protected:
+    virtual void print(std::ostream& os) const override {
+        os << "Circle";
+    }
+};
+
+struct Rectangle : Shape {
+    Rectangle(Point tl, Point br) : topleft(tl), bottomright(br) {}
+
+    Point topleft, bottomright;
+
+    virtual double area() const override {
+        return std::abs(bottomright.x - topleft.x) * 
+               std::abs(bottomright.y - topleft.y);
+    }
+
+    virtual ~Rectangle() { }
+  protected:
+    virtual void print(std::ostream& os) const override {
+        os << "Rectangle";
+    }
+
+};
+
+int main() {
+    Rectangle r { { 1,1 }, { 5,5 } };
+
+    {
+        Shape const& shape = r;
+        std::cout << shape << ": " << shape.area() << "\n";
+    }
+
+    Circle    c { { 10, 10 },  5 };
+    {
+        Shape const& shape = c;
+        std::cout << shape << ": " << shape.area() << "\n";
+    }
+
+}
+
+
