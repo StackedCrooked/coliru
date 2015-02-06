20f1647949c3d62c56af11fef5d4e052faaeee44
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-06 23:43:49 +0000

    Update archive a3

---
+++ trunk/Archive2/a3/4e5c2c15fa3162/cmd.sh
@@ -0,0 +1,5 @@
+cat>random.txt<<HERE; g++ -std=c++11 -O2 -Wall -pedantic -pthread main.cpp && ./a.out 
+333331234567,blah,"this is nonsense"
+222221234567,word,"yadda yadda words"
+111111234567,thing,"total gibberish stuff"
+HERE
