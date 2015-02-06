20fa266601a17c07399a2c3f7459211f5f67cc95
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-06 00:18:17 +0000

    Update archive ce

---
+++ trunk/Archive2/ce/92e6ab30b940e8/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <sstream>
+
+int main()
+{
+    std::stringstream ss(" P");
+    char c;
+    ss >> std::noskipws >> c;
+    std::cout << std::boolalpha << "c == ' '" << (c == ' ');
+}
