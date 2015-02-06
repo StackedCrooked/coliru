aa211af6076869b2368dd721a33b1d0c4a60f9dd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-06 15:34:01 +0000

    Update archive 25

---
+++ Archive2/25/81657301b75cb7/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <functional>
+#include <utility>
+#include <unordered_set>
+
+template<typename T>
+void
+hash_combine(std::size_t &seed, T const &key) {
+  std::hash<T> hasher;
+  seed ^= hasher(key) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+namespace std {
+  template<typename T1, typename T2>
+  struct hash<std::pair<T1, T2>> {
+    std::size_t operator()(std::pair<T1, T2> const &p) const {
+      std::size_t seed1(0);
+      ::hash_combine(seed1, p.first);
+      ::hash_combine(seed1, p.second);
+
+      std::size_t seed2(0);
+      ::hash_combine(seed2, p.second);
+      ::hash_combine(seed2, p.first);
+
+      return std::min(seed1, seed2);
+    }
+  };
+}
+
+struct UnorderedEqual
+{
+    template <class T1, class T2>
+    bool operator() (const std::pair<T1, T2> &lhs, const std::pair<T1, T2> &rhs) const
+    {
+        return lhs == rhs || (
+            lhs.second == rhs.first && rhs.second == lhs.first
+        );
+    }
+};
+
+int main() {
+    std::unordered_set<std::pair<int, int>, std::hash<std::pair<int, int>>, UnorderedEqual> s;
+    std::pair<int, int> a = std::make_pair(4, 3);
+    s.insert(a);
+    std::pair<int, int> b = std::make_pair(2, 1);
+    s.insert(b);
+    std::pair<int, int> c = std::make_pair(5, 6);
+    s.insert(c);
+    std::pair<int, int> d = std::make_pair(1, 2);
+    s.insert(d);
+    for(auto e : s) std::cout << e.first << ", " << e.second << std::endl; 
+    
+    return 0;
+}
