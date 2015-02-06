fa5d22d8f56a3ba97c2906dda9968327ccfb6065
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-06 07:52:08 +0000

    Update archive 82

---
+++ trunk/Archive2/82/48fd368dd4598d/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    const double v = 12.34567 ;
+
+    std::cout << std::fixed << std::setprecision(3)
+    
+              << std::setfill('0') << std::setw(10) << v << '\n' // 000012.346
+              
+              << v << '\n' // 12.346 // default width; setw(10) is no longer in efffect
+              
+              << std::setw(12) << v << '\n' // 00000012.346 // setfill('0') is still in effect
+              
+              << std::setw(5) << v << '\n' ; // 12.346 // width of 5 is too small; setw(5) is ignored
+}
