51aa9832289f5e6be49cd28f9772219c1280f101
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-06 23:26:41 +0000

    Update archive 7a

---
+++ trunk/Archive2/7a/d9e9e33a336dac/cmd.sh
@@ -0,0 +1,5 @@
+g++ -std=c++11 -O2 -Wall -pedantic -pthread main.cpp && ./a.out <<HERE
+333331234567,blah,"this is nonsense"
+222221234567,word,"yadda yadda words"
+111111234567,thing,"total gibberish stuff"
+HERE
