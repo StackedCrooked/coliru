bd683d0c11a2ba4d7b2628c30d2b22e06555605e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-07 21:01:52 +0000

    Update archive f2

---
+++ trunk/Archive2/f2/eb7212f8a54c6b/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i=0;    
+    
+   cout << "Questo e' il nostro compilatore online!" << endl;
+   cout<<"scegli 1 o 2: ";
+   cin>>i;
+   
+   if(i==1)
+   cout<<"ciao"<<endl;
+   
+   else if(i==2)
+   cout<<"arrivederci"<<endl;
+   
+   cout<<"premi un tasto per uscire..."<<endl;
+   return 0;
+   
+}
