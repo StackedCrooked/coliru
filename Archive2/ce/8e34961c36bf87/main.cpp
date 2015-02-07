da150e3cce74bb3f936e497a6be7cefd70210fb0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-07 17:31:58 +0000

    Update archive ce

---
+++ Archive2/ce/8e34961c36bf87/main.cpp
@@ -0,0 +1,32 @@
+#include <array>
+#include <iostream>
+#include <utility>
+
+void print(std::array<int, 5>& array)
+{
+  for(int i: array)
+    std::cout << i << " ";
+  std::cout << std::endl;
+}
+
+int main()
+{
+  std::array<int, 5> seq = {5, 1, 4, 2, 8};
+  std::cout << "Start: ";
+  print(seq);
+
+  for (std::size_t i=0; i < seq.size(); ++i)
+  {
+    for (std::size_t j=i; j < seq.size(); ++j)
+    {
+      if (seq[i] > seq[j])
+        std::swap(seq[i], seq[j]);
+    
+      std::cout << "(" << i << "," << j << ") ";
+      print(seq);
+    }
+    std::cout << "-- next iteration --" << std::endl;
+  }
+  std::cout << "End: ";
+  print(seq);
+}
