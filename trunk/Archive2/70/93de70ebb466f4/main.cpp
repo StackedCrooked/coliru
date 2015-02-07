0619177e1e12cdb402c34360568e889e70cf7cbe
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-07 18:32:48 +0000

    Update archive 70

---
+++ trunk/Archive2/70/93de70ebb466f4/main.cpp
@@ -0,0 +1,17 @@
+ # include <iostream>
+
+struct X {
+    int operator++(int) const { return 0; }
+};
+
+// the following two function declarations are equivalent
+auto incr(auto x) { return x++; }
+
+template <typename T>
+auto incr2(T x) { return x++; }
+
+int main()
+  {
+      incr(X());
+      incr2(X());
+  }
