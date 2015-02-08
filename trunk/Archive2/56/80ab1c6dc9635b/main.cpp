18589bcb052a3d22079a9c8be87c618a4fd76184
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-08 23:47:30 +0000

    Update archive 56

---
+++ trunk/Archive2/56/80ab1c6dc9635b/main.cpp
@@ -0,0 +1,15 @@
+#include <boost/optional.hpp>
+#include <boost/utility/in_place_factory.hpp>
+
+struct Foo
+{
+    Foo() {}
+    int bar = 5;  
+};
+
+int
+main()
+{
+    boost::optional<Foo> foo;
+    foo = boost::in_place();
+}
