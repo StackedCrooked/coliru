cd791e265701065fdeee1d9b45ea0fc555547426
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-08 21:27:29 +0000

    Update archive 63

---
+++ trunk/Archive2/63/214a453f777b31/main.cpp
@@ -0,0 +1,44 @@
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/serialization/serialization.hpp>
+
+#include <boost/iostreams/device/array.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <sstream>
+
+namespace bar = boost::archive;
+namespace bio = boost::iostreams;
+
+struct Packet {
+    int i;
+    template <typename Ar> void serialize(Ar& ar, unsigned) { ar & i; }
+};
+
+Packet deserialize(std::istream& is) {
+    is.seekg(0);
+
+    bar::text_iarchive ia(is);
+    Packet result;
+    ia >> result;
+    return result;
+}
+
+void serialize(Packet const& data, std::ostream& os)
+{
+    bar::text_oarchive ar(os);
+    ar << data;
+}
+
+int main() {
+    std::stringstream ss;
+
+    for (int i = 0; i < 100; ++i) {
+        ss.seekp(0);
+        ss.clear();
+        serialize(Packet { i }, ss);
+
+        Packet roundtrip = deserialize(ss);
+        assert(roundtrip.i == i);
+    }
+    std::cout << "Done\n";
+}
