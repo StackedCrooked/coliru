2ad2be87cfe231dffb64784e22595ee112508ae0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-09 02:46:29 +0000

    Update archive da

---
+++ Archive2/da/a5cf3eb0615d89/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <iterator>
+
+template<class T>
+struct Insert {
+  template<class C>
+  void into (C& c) && {
+    auto pos = std::lower_bound (std::begin (c), std::end (c), _data);
+    c.emplace (pos, std::forward<T> (_data));
+  }
+
+  T&& _data;
+};
+
+template<class T>
+Insert<T> insert (T&& e) { return { e }; }
+
+// ---------------------------------------------
+
+int main () {
+  std::vector<int> vec;
+
+  for (auto e : {1,9,4,8,5,0,-1,100,5})
+    insert (e).into (vec);
+
+  {
+    std::copy (
+      std::begin (vec), std::end (vec),
+      std::ostream_iterator<int> (std::cout, " ")
+    );
+
+    std::cout << "\n";
+  }
+}
