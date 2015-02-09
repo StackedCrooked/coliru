d2a09b6afebfccc3561bb347429e0ea9169e0ac3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-09 13:44:34 +0000

    Update archive a0

---
+++ trunk/Archive2/a0/c835414f93426b/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <boost/asio.hpp>
+
+using namespace std;
+using namespace boost::asio;
+
+istream& parseDocument( istream& input, int &data ) {
+    input >> data;
+    string extra;
+    getline( input, extra );
+    if(!extra.empty() || !input.eof() ) throw std::runtime_error("extra charaters after end of document");
+    return input;
+}
+
+template< typename MutableBuffers >
+size_t bad_case_of_read_until( MutableBuffers const& outbuf )
+{
+    return buffer_copy( outbuf, buffer("12345\n\0some bad next frame\0") );
+}
+
+int main()
+{
+    boost::asio::streambuf strbuf;
+    {
+        auto buffers= strbuf.prepare( 1024 );
+        size_t read= bad_case_of_read_until( buffers );
+        strbuf.commit( read );
+    }
+
+    int data;
+    istream stream(&strbuf);
+
+    parseDocument( stream, data ); // will fail here
+
+    return 0;
+}
