a3bb8674431d84ec46665f044f3266d18b5b054d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-09 18:32:22 +0000

    Update archive eb

---
+++ Archive2/eb/7050ea48321f5c/main.cpp
@@ -0,0 +1,41 @@
+#define BOOST_SPIRIT_USE_PHOENIX_V3
+#include <iostream>
+#include <vector>
+#include <string>
+#include <boost/spirit/include/karma.hpp>
+#include <boost/spirit/include/phoenix.hpp>
+
+namespace karma = boost::spirit::karma;
+namespace phx = boost::phoenix;
+
+struct foo {
+    std::string f() const { return "bar"; }
+};
+
+int main()
+{
+    typedef std::back_insert_iterator<std::string> iterator;
+
+    // this works:
+    {
+        std::string output;
+        iterator it(output);
+
+        std::vector<std::string> svec = { "foo", "bar", "baz" };
+        karma::rule<iterator, std::vector<std::string>()> svec_rule = karma::string % ',';
+        karma::generate(it, svec_rule, svec);
+        std::cerr << output << '\n';
+    }
+
+    {
+        std::string output;
+        iterator it(output);
+
+        // but this won't compile
+        std::vector<foo> vfoo(3);
+        karma::rule<iterator, foo&()> foo_rule = karma::string [ karma::_1 = phx::bind(&foo::f, karma::_val) ];
+        //karma::rule<iterator, std::vector<foo> &()> vfoo_rule = karma::string  [ karma::_1 = phx::bind(&foo::f, karma::_val) ] % ',';
+        karma::generate(it, foo_rule % ',', vfoo);
+        std::cerr << output << '\n';
+    }
+}
