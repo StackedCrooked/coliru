bb05567b44ecfb6cd423dd241c221b7841e3fcb1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-09 20:53:18 +0000

    Update archive 7f

---
+++ Archive2/7f/dc295d5dc4ae35/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+struct T
+{
+    int a;
+    constexpr T(): a(31){ }
+};
+
+T y;
+
+const T t = y;
+
+const T tt = T();
+
+template <class T, const T& t>
+int var = t.a;
+
+template int var<T, t>;
+
+int main()
+{ 
+    std::cout << "var <T, t> = " << var<T, t> << std::endl; 
+    std::cout << "y.a = " << y.a << std::endl; 
+    std::cout <<"var <T, tt> = " << var<T, tt> << std::endl; 
+}
