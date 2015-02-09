4811e910a1227feb83e84a60932b5869bd37ad67
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-09 19:06:45 +0000

    Update archive 92

---
+++ Archive2/92/b523338e3e8723/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class Foo {
+public:
+    virtual int get() = 0;
+    template <typename T> int get(int i) { return 4 + i; }
+};
+
+class Bar : public Foo {
+public:
+    using Foo::get;
+    virtual int get() { return 3; }
+};
+
+int main(int argv, char **argc) {
+    Bar b;
+    Foo& f = b;
+    std::cout << f.get<char>(7) << std::endl;
+    return 0;
+}
