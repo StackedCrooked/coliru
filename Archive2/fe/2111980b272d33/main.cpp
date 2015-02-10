d8ab8269e9b7aa79c2e41a081fd8da9864d11a20
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-10 13:04:58 +0000

    Update archive fe

---
+++ Archive2/fe/2111980b272d33/main.cpp
@@ -0,0 +1,28 @@
+#include <functional>
+#include <queue>
+#include <vector>
+#include <iostream>
+ 
+template<typename T> void print_queue(T& q) {
+    while(!q.empty()) {
+        std::cout << q.top() << " ";
+        q.pop();
+    }
+    std::cout << '\n';
+}
+ 
+int main() {
+    std::priority_queue<int> q;
+ 
+    for(int n : {1,8,5,6,3,4,0,9,3,2})
+        q.push(n);
+ 
+    print_queue(q);
+ 
+    std::priority_queue<int, std::vector<int>, std::greater<int> > q2;
+ 
+    for(int n : {1,8,5,6,3,4,0,9,3,2})
+        q2.push(n);
+ 
+    print_queue(q2);
+}
