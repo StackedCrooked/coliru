396e9cc355e12626e8c15c7028ae02a197e71402
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-10 14:30:32 +0000

    Update archive e1

---
+++ trunk/Archive2/e1/5fff872bced29b/main.cpp
@@ -0,0 +1,12 @@
+template <typename> struct custom_copy_ctor {
+  custom_copy_ctor() = default;
+  custom_copy_ctor(custom_copy_ctor const &) {}
+  // custom_copy_ctor(custom_copy_ctor const&) = default; // works fine
+};
+
+int main() {
+  custom_copy_ctor<void> cc;
+  [cc](auto) {}(1);
+  
+  return 0;
+}
