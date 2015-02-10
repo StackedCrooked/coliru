20ad5525654127448975b6d6bb19ff59120ba5c0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-10 16:04:17 +0000

    Update archive 22

---
+++ Archive2/22/1648a1dc830d45/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+ 
+int main()
+{
+    std::ifstream f("doesn't exist");
+    try {
+        f.exceptions(f.failbit);
+    } catch (const std::ios_base::failure& e) {
+        std::cout << "Caught an ios_base::failure.\n"
+                  << "Error code: " << e.code().value() 
+                  << " (" << e.code().message() << ")\n"
+                  << "Error category: " << e.code().category().name() << '\n';
+ 
+    }
+}
