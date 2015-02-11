45d6a6d2583eb999d7e4278086f005fb0449ea28
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-11 21:53:20 +0000

    Update archive 4c

---
+++ trunk/Archive2/4c/c08fde78bbb99d/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <boost/format.hpp>
+
+namespace logg {
+    struct output {
+        output() : device_(std::cout) {}
+        std::ostream& device_;
+    };
+
+    template<typename T>
+    output operator << (output trc, T&& t) {
+        trc.device_ << std::forward<T>(t);
+        return trc;
+    }
+    
+    struct debug {
+        debug(const std::string& fmt) : fmt_str_("[DEBUG]: " + fmt) {}        
+        std::string fmt_str_;
+    };
+    
+    template<typename T>
+    std::string operator % (const debug& dbg, const T& t) {
+        std::stringstream ss;
+        ss << boost::format(dbg.fmt_str_) % t;
+        return ss.str();
+    }
+}
+
+#define log_output() if(!debug_level) (void)0; else logg::output()
+
+std::string some_heavy_func() {
+    std::cout << "in some heavy func\n";
+    return "abcd";
+}
+
+int main() {
+    int debug_level = 1;
+    log_output() << logg::debug("this is log message: %1%") % some_heavy_func();
+    
+    debug_level = 0;
+    log_output() << logg::debug("this is log message: %1%") % some_heavy_func();
+}
