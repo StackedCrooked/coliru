5ebb3103afe2d9fec82a1cbf1efaaa1625c9d743
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-11 00:05:37 +0000

    Update archive 77

---
+++ trunk/Archive2/77/972fa43a259c5f/main.cpp
@@ -0,0 +1,38 @@
+#include <map>
+#include <vector>
+
+struct B { virtual ~B() = default; };
+template <typename T> struct D : B { };
+
+B* makeD() {
+    switch(rand()%4) {
+        case 0: return new D<int>;
+        case 1: return new D<double>;
+        case 2: return new D<std::string>;
+        case 3: return new D<D<int> >;
+    }
+    return nullptr;
+}
+
+#include <typeinfo>
+#include <string>
+
+std::map<std::string, std::vector<B*> > split(std::vector<B*> const& bs) {
+    std::map<std::string, std::vector<B*> > m;
+    for(auto b : bs)
+        m[typeid(*b).name()].push_back(b);
+    return m;
+}
+
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+
+int main() {
+    std::vector<B*> v;
+    std::generate_n(back_inserter(v), 100, makeD);
+
+    for (auto const& e : split(v))
+        std::cout << "Type: " << e.first << ", " << e.second.size() << " entries\n";
+}
+
