50b1af9e6cd27dfbe277c069f0ab95b2edbdc603
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-12 17:59:20 +0000

    Update archive 7e

---
+++ trunk/Archive2/7e/e6a16299bd1e09/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <regex>
+
+namespace tr1{}
+namespace std{using namespace tr1;}
+
+using namespace std;
+using namespace std::regex_constants;
+
+int main()
+{
+    std::string basePartOfSymbol = "CN H2012";
+
+	const std::regex basePartRegExp("^(\\w+) (?:((?:[FGHJKMNQUVXZ]\\d(?:\\d|\\d{3})?)|(?:(?:\\d{4}|\\d{2}|\\d)(?:[FGHJKMNQUVXZ])))(\\d{0,2})|(\\d+W\\d+))(\\-\\w+)?(=\\w)?$");
+
+	std::smatch captures;
+	const std::string symbol_str(basePartOfSymbol);
+	std::regex_search(symbol_str, captures, basePartRegExp);
+
+	cout << captures.size() << endl;
+
+	int size = captures.size();
+
+	for (int i = 0; i < size; ++i)
+	{
+		cout << i << ": " << captures.str(i) << endl;
+	}
+
+	return 0;
+}
