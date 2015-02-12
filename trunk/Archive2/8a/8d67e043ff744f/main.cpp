5033a2682759e929ecd89940a91439bd9e931b99
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-12 16:06:55 +0000

    Update archive 8a

---
+++ trunk/Archive2/8a/8d67e043ff744f/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <utility>
+#include <thread>
+#include <chrono>
+#include <functional>
+#include <atomic>
+ 
+void f1(int n)
+{
+    for(int i=0; i<5; ++i) {
+        std::cout << "Thread " << n << " executing\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+}
+ 
+void f2(int& n)
+{
+    for(int i=0; i<5; ++i) {
+        std::cout << "Thread 2 executing\n";
+        ++n;
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+}
+ 
+int main()
+{
+    int n = 0;
+    std::thread t1; // t1 is not a thread
+    t1.join();
+
+    std::cout << "Final value of n is " << n << '\n';
+}
