75e83c04ee1e4bd172a131e79bc41e995723fb9c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-12 16:08:59 +0000

    Update archive 7f

---
+++ trunk/Archive2/7f/f3f96f404fd375/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+namespace detail {
+    template <typename T>
+    struct Test : T { Test(Test&&) = default; };
+}
+
+// TODO: Implement unions
+template <typename T>
+using is_moveable = std::integral_constant<bool, 
+    std::conditional_t<std::is_class<T>{}, 
+       std::is_move_constructible<detail::Test<T>>, std::is_arithmetic<T>>::value>;
+
+class Moveable {};
+class NotMoveable {NotMoveable(NotMoveable const&); }; // No move constructor
+
+static_assert( is_moveable<Moveable>::value, "" );
+static_assert( !is_moveable<NotMoveable>::value, "" );
+static_assert( is_moveable<int>::value, "" );
+static_assert( !is_moveable<void>::value, "" );
+
+int main() {}
