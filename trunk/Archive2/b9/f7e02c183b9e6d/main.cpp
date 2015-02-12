479ff5600161fd00483041b38efd972ef9f1b2f1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-12 23:52:13 +0000

    Update archive b9

---
+++ trunk/Archive2/b9/f7e02c183b9e6d/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+void foo() {}
+
+int main()
+{
+    double alpha, beta = 20, gamma = 2;
+    alpha = beta / gamma * 1,8;
+    std::cout << alpha <<  std::endl;
+    return 0;
+}
