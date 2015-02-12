240513ef433f87da6569789b6cd28cfca20b114a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-12 23:41:24 +0000

    Update archive cd

---
+++ Archive2/cd/11cd65526be1da/main.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string MessageToChiper()
+{
+    cout<<"Entrer le message a criper d'après le codage de césar"<<endl;
+    string phrase;
+    getline(cin, phrase);
+    //cin.ignore('\n', 256);
+    //std::cin >> var;
+    //std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    return phrase;
+}
+
+string MessageToDechiper()
+{
+    cout<<"Entrer le message a decriper d'après le codage de césar"<<endl;
+    string phrase;
+    getline(cin, phrase);
+    return phrase;
+}
+
+int KeyChiper()
+{
+    int K;
+    cout<<"Entrer la cle"<<endl;
+    cin>>K;
+    return K;
+}
+
+int KeyDechiper()
+{
+    int K;
+    cout<<"Entrer la cle"<<endl;
+    cin>>K;
+    return K;
+}
+
+string Eraseshits(string phrase)
+{
+    size_t ph;
+    do
+    {
+        ph=phrase.find_first_of(" ,./^%*µ$€°~#|`!\()=+-_:?;'<>&[]{}"); //delete every weird character
+        if(ph!=std::string::npos) // or -1
+        {
+            phrase.erase(phrase.begin()+ph);
+        }
+    }while(ph!=std::string::npos); // or -1
+    cout<<phrase<<endl;
+    return phrase;
+}
+
+string Chiper(string phrase,int K)
+{
+    K = K % 26;
+    for(size_t i=0;i<phrase.length();i++)
+    {
+        if(phrase[i]>='A'&& phrase[i]<='Z')
+        {
+            phrase[i]=phrase[i]+K;
+            if(phrase[i]>'Z')
+            {
+                phrase[i]=phrase[i]-26;
+            }
+            if(phrase[i]<'A')
+            {
+                phrase[i]=phrase[i]+26;
+            }
+        }//Maj case
+
+        if(phrase[i]>='a'&& phrase[i]<='z')
+        {
+            phrase[i]=phrase[i]+K;
+            if(phrase[i]>'z')
+            {
+                phrase[i]=phrase[i]-26;
+            }
+            if(phrase[i]<'a')
+            {
+                phrase[i]=phrase[i]+26;
+            }
+        }//Min case
+
+        if(phrase[i]>='0' && phrase[i]<='9')
+        {
+            phrase[i]=phrase[i]+K;
+            if(phrase[i]>'9')
+            {
+                phrase[i]=phrase[i]-10;
+            }
+            if(phrase[i]<'0')
+            {
+                phrase[i]=phrase[i]+10;
+            }//number case
+            //no change for weird carac not deleted
+        }
+    }
+    cout<<phrase<<endl;
+    return phrase;
+}
+
+string Dechiper(string phrase,int K)
+{
+    K = K % 26;
+    K = -K;
+    for(size_t i=0;i<phrase.length();i++)
+    {
+        if(phrase[i]>='A'&& phrase[i]<='Z')
+        {
+            phrase[i]=phrase[i]+K;
+            if(phrase[i]>'Z')
+            {
+                phrase[i]=phrase[i]-26;
+            }
+            if(phrase[i]<'A')
+            {
+                phrase[i]=phrase[i]+26;
+            }
+        }//Maj case
+
+        if(phrase[i]>='a'&& phrase[i]<='z')
+        {
+            phrase[i]=phrase[i]+K;
+            if(phrase[i]>'z')
+            {
+                phrase[i]=phrase[i]-26;
+            }
+            if(phrase[i]<'a')
+            {
+                phrase[i]=phrase[i]+26;
+            }
+        }//Min case
+
+        if(phrase[i]>='0' && phrase[i]<='9')
+        {
+            phrase[i]=phrase[i]+K;
+            if(phrase[i]>'9')
+            {
+                phrase[i]=phrase[i]-10;
+            }
+            if(phrase[i]<'0')
+            {
+                phrase[i]=phrase[i]+10;
+            }//number case
+            //no change for weird carac not deleted
+        }
+    }
+    cout<<phrase<<endl;
+    return phrase;
+
+}
+
+int main()
+{
+    string Phrase;
+    int a;
+    int cle;
+    cout<<"Chiper : 1"<<endl<<"Dechiper : 2"<<endl;
+    cin>>a;
+    if(a==1)
+    {
+    //cout<<"phase codage"<<endl;
+    Phrase=MessageToChiper();
+    cle=KeyChiper();
+    Phrase=Eraseshits(Phrase);
+    Phrase=Chiper(Phrase,cle);
+    }
+    if(a==2)
+    {
+    //cout<<"phase decodage"<<endl;
+    Phrase=MessageToDechiper();
+    cle=KeyDechiper();
+    Phrase=Dechiper(Phrase,cle);
+    }
+
+    return 0;
+}
