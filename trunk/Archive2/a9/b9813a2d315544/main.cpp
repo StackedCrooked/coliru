924b513a4307bf5337ce07cbcc9d0e191da30abd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-12 22:00:08 +0000

    Update archive a9

---
+++ trunk/Archive2/a9/b9813a2d315544/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <strings.h>
+
+using namespace std;
+
+string MessageACryprer()
+{
+    cout<<"Entrer le message a cryper d'après le codage de césar"<<endl;
+    string phrase;
+    getline(cin, phrase);
+    return phrase;
+}
+
+string Eraseshits(string phrase)
+{
+    size_t ph;
+    do
+    {
+        if(ph!=std::string::npos)
+        {
+            ph=phrase.find_first_of(" ,./!()=+-_:?;'");
+            phrase.erase(phrase.begin()+ph);
+        }
+    }while(ph!=std::string::npos);
+    cout<<phrase;
+    return phrase;
+}
+int main()
+{
+    string Phrase;
+    Phrase="Blabla, zrgu; a/(). azurh";
+    Phrase=Eraseshits(Phrase);
+    return 0;
+}
