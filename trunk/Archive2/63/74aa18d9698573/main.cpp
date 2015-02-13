ce198083fa40fbe74c5e921d86ef833d845b849e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-13 21:02:46 +0000

    Update archive 63

---
+++ trunk/Archive2/63/74aa18d9698573/main.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <string>
+#include <iostream>
+ 
+
+ 
+int main() 
+{
+    // c++11 initializer list syntax:
+    std::vector<std::string> words1 {"Will", "Casa", "Teste", "CASASSS", "usando"};
+    std::cout << "words1: " << words1.size() << '\n';
+
+}
