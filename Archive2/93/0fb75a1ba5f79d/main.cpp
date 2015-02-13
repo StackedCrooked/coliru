2ad7f1c5936edea91cb29863d3471239e291371d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-13 21:12:07 +0000

    Update archive 93

---
+++ Archive2/93/0fb75a1ba5f79d/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+
+int main()
+{
+    bool test1 = false;
+    bool test2 = true;
+    bool test3 = true;
+    
+    if ((test1 && test2) || !test3)
+        std::cout << 'test1';
+}
