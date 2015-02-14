095a57741b68eacb1126d9ad71439dbfd53ca83a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-14 05:17:11 +0000

    Update archive 44

---
+++ trunk/Archive2/44/d6ab5ed1cf4510/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class Vector2
+{
+    public:
+		static Vector2 invalid;
+
+		Vector2(bool isValid);
+		Vector2(const Vector2& vec);
+};
+
+Vector2 Vector2::invalid = Vector2(false);
+
+Vector2::Vector2(bool isValid)
+{
+    std::cout << "isValid in contructor" + isValid << std::boolalpha << std::endl;
+	
+}
+
+Vector2::Vector2(const Vector2& vec)
+{
+	std::cout << "asdf" << std::boolalpha << std::endl;
+}
+
+int main()
+{
+    Vector2(false);
+}
