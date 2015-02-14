ca213676b9ca3be1a92901f5f10062675a4aec2c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-14 01:53:37 +0000

    Update archive 7e

---
+++ trunk/Archive2/7e/77597bd757e726/main.cpp
@@ -0,0 +1,25 @@
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <sstream>
+#include <iostream>
+
+int main() {
+    std::stringstream ss;
+    int numbers1[10], numbers2[10];
+    for (int i = 0; i < 10; i++) {
+        numbers1[i] = i;
+    }
+
+    {
+        boost::archive::text_oarchive oar(ss);
+        oar << numbers1;
+    }
+
+    {
+        boost::archive::text_iarchive iar(ss);
+        iar >> numbers2;
+    }
+    
+    for (auto i : numbers2)
+        std::cout << i << " ";
+}
