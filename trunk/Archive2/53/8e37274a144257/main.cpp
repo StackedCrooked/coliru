7e289575b04bd7b8c698149ce7eb88030c14c309
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-14 08:40:27 +0000

    Update archive 53

---
+++ trunk/Archive2/53/8e37274a144257/main.cpp
@@ -0,0 +1,21 @@
+
+template < typename T > struct foo
+{
+    T bar( T a ) { return a + 2 ; }
+
+    template < int N, typename U > U baz( U a ) { return a % N ; }
+};
+
+// explicit instantiation of class foo<double>
+template struct foo<double> ; // double foo<double>::bar(double) is instantiated
+                              // U foo<double>::baz<U>(U) is not instantiated for any U
+
+// explicit instantiation of long foo<int>::baz<8,long>(long)
+template long foo<int>::baz<8>(long) ; // fine: U == long is deduced
+
+int main()
+{
+}
+
+// explicit instantiation of double foo<int>::baz<22,double>(double)
+template double foo<int>::baz<22>(double) ; // **** error: invalid type double in a % 22
