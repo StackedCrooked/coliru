17c7b3221193f44430fcd3381743605d2b2f4102
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-15 03:46:33 +0000

    Update archive f2

---
+++ trunk/Archive2/f2/a9a99401397ada/main.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main (void)
+{
+    // y= resultado.a= dividendo.b= mutiplica x
+  float a=0;
+  float b=0;
+  float x=0;
+  float y=0;
+  printf("ingresar a");
+  scanf("%F",&a);
+  printf("ingresar b");
+  scanf("%f",&b)
+  printf("ingresar x");
+  scanf("%F",&x);
+  float y=b*x/a;
+  printf ("y es: %f" ,y_resultado_de_regla de tres);
+	return 0;
+}
+   
+#include <stdio.h>
+int main (void)
+{
+  float perimetro=0;
+  float radio=0;
+  float pi=3.141592;
+  printf("ingresar radio");
+  scanf("%f",&radio);
+  float perimetro_de_radio=radio+radio+radio+radio;
+  float perimetro_de_circulo = pi*2*radio;
+  printf("elperimetro del circulo es:%f y el radio de  es%f",perimetro_de_circulo,perimetro_de_radio);
+
+  return 0;
+}
