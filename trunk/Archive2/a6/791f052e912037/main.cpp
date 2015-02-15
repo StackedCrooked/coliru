e3967457ba3051a9f95dee770a5b7986983a0273
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-15 04:59:54 +0000

    Update archive a6

---
+++ trunk/Archive2/a6/791f052e912037/main.cpp
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+template = 'Hello {person}, welcome to {city}.'
+
+print(template.format(person='Danny', city='New York'))
+print(template.format(person='Alex', city='Vancouver'))
