1f8e39161a3cd00dd17b0af86b076e86c1532a16
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-15 02:26:35 +0000

    Update archive 21

---
+++ trunk/Archive2/21/e0565bcb49b381/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (void)
+{
+    // y= resultado.a= dividendo.b= mutiplica x
+  float a =0;
+  float b =0;
+  float x =0;
+  float y =0;
+  print f("ingresar a");
+  scan f("%F",&a);
+  print f("ingresar b");
+  print f("ingresar x")
+  scan f("%F",x);
+  float y=b*x/a;
+  printf ("y es: %f"y);
+	return 0;
+}
+   
