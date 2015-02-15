87df9764879204e31220f9b338d4098cf8620f6d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-15 23:05:30 +0000

    Update archive 62

---
+++ trunk/Archive2/62/23f32d9a049b92/cmd.sh
@@ -0,0 +1,1 @@
+mv main.cpp 1t.c && gcc -c -o 1t.o 1t.c
---
+++ trunk/Archive2/62/23f32d9a049b92/main.cpp
@@ -0,0 +1,9 @@
+
+
+typedef void (*dthreads_funct_t)(void);
+
+extern int dthreads_libinit(dthreads_funct_t func, void *arg);
+
+int dthreads_libinit(dthreads_funct_t func, void* arg) {
+//Do something here...
+}
