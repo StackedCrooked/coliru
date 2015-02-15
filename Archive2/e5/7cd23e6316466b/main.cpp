8bd3208dcfe19a08504eb241a85fa75b2a9dd54c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-15 18:39:36 +0000

    Update archive e5

---
+++ Archive2/e5/7cd23e6316466b/main.cpp
@@ -0,0 +1,28 @@
+template <int A, int B, int C> struct StructA
+{
+    static constexpr int a = A;
+    // ...
+};
+
+template <int A, int B> struct StructB
+{
+    static constexpr int a = A;
+    // ...
+};
+
+template <typename S, typename T> struct StructC
+{
+    StructC() = default;
+    template <typename S1, typename T1> StructC(const StructC<S1, T1>& s) {};
+};
+
+void Func(StructC<StructB<4, 5>, double> x)
+{
+    // do something with x
+}
+
+int main()
+{
+    StructC<StructA<1, 2, 3>, float> a;
+    Func(a);
+}
