ff1f093e142de932c686fd7f2bedb1b44a34381f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-15 18:16:08 +0000

    Update archive 0c

---
+++ trunk/Archive2/0c/3c0acd7715361a/main.cpp
@@ -0,0 +1,37 @@
+#include <memory>
+#include <iostream>
+
+struct A final
+{
+    ~A() { std::cout << "A::destructor\n" ; /* .. */ }; // destructor is public
+
+    // static factory functions to create dynamically allocated objects
+    template < typename... ARGS > static std::unique_ptr<A> make_unique( ARGS&&... args )
+    { return std::unique_ptr<A>( make_new( std::forward<ARGS>(args)...  ) ) ; }
+
+    template < typename... ARGS > static std::shared_ptr<A> make_shared( ARGS&&... args )
+    { return std::shared_ptr<A>( make_new( std::forward<ARGS>(args)... ) ) ; }
+
+    // ...
+
+    private:
+        // all constructors are private
+        A( /* ... */ ) { /* ... */ }
+        A( int /* ... */ ) { /* ... */ } // another constructor
+        A( const A& ) { /* ... */ }
+        A( A&& ) noexcept = default ;
+
+        // ...
+
+        template < typename... ARGS > static A* make_new( ARGS&&... args )
+        { return new A( std::forward<ARGS>(args)... ) ; }
+
+        // ....
+};
+
+int main()
+{
+    auto p = A::make_unique( 200 /* ... */ ) ;
+    auto p1 = A::make_shared(*p) ;
+    // etc.
+}
