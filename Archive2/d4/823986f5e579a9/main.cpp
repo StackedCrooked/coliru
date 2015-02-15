0405f78d8bf2914d76bc2e70a08e9e2203dbf8bf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-15 00:35:13 +0000

    Update archive d4

---
+++ Archive2/d4/823986f5e579a9/main.cpp
@@ -0,0 +1,43 @@
+#include <cstddef>
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+
+struct field {
+    std::size_t size;
+    std::size_t alignment;
+};
+
+int main() {
+    std::vector<field> fields{
+        { 2, 2 },
+        { 4, 4 },
+        { 2, 2 },
+        { 4, 8 },
+    };
+
+    std::size_t offset = 0;
+    std::unordered_set<std::size_t> computed;
+    while (computed.size() < fields.size()) {
+        std::size_t max_alignment = 0;
+        for (std::size_t i = 0; i < fields.size(); ++i) {
+            if (computed.count(i)) {
+                continue;
+            }
+            if (offset % fields[i].alignment == 0) {
+                std::cout << "field i" << i << ": s" << fields[i].size << ": a" << fields[i].alignment << ": o" << offset << '\n';
+                offset += fields[i].size;
+                computed.insert(i);
+                break;
+            }
+            if (fields[i].alignment > max_alignment) {
+                max_alignment = fields[i].alignment;
+            }
+        }
+        if (max_alignment != 0) {
+            offset += max_alignment - offset % max_alignment;
+        }
+    }
+
+    return 0;
+}
