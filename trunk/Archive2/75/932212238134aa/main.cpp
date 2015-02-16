40c446336afad64f3302961383d1f03189be98e0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-16 00:43:11 +0000

    Update archive 75

---
+++ trunk/Archive2/75/932212238134aa/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <functional>
+
+template <typename T>
+size_t getSize()
+{
+    return sizeof(T); 
+}
+
+// terminating case for T=double
+template<>
+size_t getSize<double>()
+{
+    return 100;
+}
+
+// terminating case for T=float
+template<>
+size_t getSize<float>()
+{
+    return 50;
+}
+
+// recursive case
+template<class T, class U, class ...S>
+size_t getSize()
+{
+    return getSize<T>() + getSize<U, S...>();
+}
+
+template <class ...T>
+void print_function_arg_custom_size(std::function<void(T...)> f)
+{
+    (void)f;
+    size_t totalSize = 0;
+    // get size recursively
+    totalSize = getSize<T...>(); 
+
+    std::cout << "totalSize: " << totalSize << std::endl;
+}
+
+void foo(uint8_t, uint16_t, uint32_t, double)
+{
+    // noop
+}
+
+int main()
+{
+    std::function<void(uint8_t, uint16_t, uint32_t, double)> f = foo;
+    print_function_arg_custom_size<uint8_t, uint16_t, uint32_t, double>(f);
+
+    return 0;
+}
