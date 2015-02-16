8a868719d1b573fc3fa4ad31487fcfa76fe6c8e4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-16 23:32:35 +0000

    Update archive 49

---
+++ Archive2/49/6e88122947f5e9/main.cpp
@@ -0,0 +1,25 @@
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix.hpp>
+
+namespace qi = boost::spirit::qi;
+
+int main() { 
+    using namespace qi;
+
+    std::string const s("{A,B,C:D,E}");
+
+    std::vector<char> parsed;
+
+    bool ok = qi::parse(begin(s), end(s),
+            '{' >> 
+                (
+                      "C:" >> qi::char_("A-Z") [ qi::_1 = 'Z' - (qi::_1 - 'A') ]
+                    | qi::char_("A-Z")
+                ) % ','
+            >> '}', 
+            parsed);
+
+    if (ok) {
+        std::copy(begin(parsed), end(parsed), std::ostream_iterator<char>(std::cout, "; "));
+    }
+}
