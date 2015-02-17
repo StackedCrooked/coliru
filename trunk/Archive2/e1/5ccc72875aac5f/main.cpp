a90c76d77c5f811c87211d424b6f63092478060c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-17 19:05:28 +0000

    Update archive e1

---
+++ trunk/Archive2/e1/5ccc72875aac5f/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    
+    std::cout << 2 + 3.14 << std::endl;
+    
+    std::cout << 5 / 2 << std::endl;
+    
+    std::cout << 5 / 2.0 << std::endl;
+    
+    std::cout << 'a' + 1 << std::endl;
+    
+    return 0;
+}
