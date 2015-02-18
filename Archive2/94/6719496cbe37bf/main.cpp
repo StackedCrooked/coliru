5a57759269b019c9009f12dbfc38a9d6530bcd75
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-18 23:30:55 +0000

    Update archive 94

---
+++ Archive2/94/6719496cbe37bf/main.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+       int i;
+
+       i = 0
+
+       while (i <= 5);
+
+             {
+
+              printf ( "i = %d" );
+
+              i += 1;
+
+        }
+
+}
