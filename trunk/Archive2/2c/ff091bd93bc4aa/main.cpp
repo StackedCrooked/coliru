7c8cb712517a590b36b1465d69d663b60e0889f3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-18 10:58:04 +0000

    Update archive 2c

---
+++ trunk/Archive2/2c/ff091bd93bc4aa/main.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void myrev(char str[])
+ {
+     printf("%s",str);
+     }
+int main()
+{
+    char str[25];
+printf("Enter a string: ");
+scanf("%s",str);
+ myrev(str);
+ 
+ }
+ 
