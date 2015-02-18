0179399616bcec6066f1f6b122d6ef3fd39ea481
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-18 03:40:34 +0000

    Update archive 9d

---
+++ Archive2/9d/17a87cec86885f/main.cpp
@@ -0,0 +1,66 @@
+#include <tuple>
+#include <type_traits>
+
+namespace detail {
+  using namespace std;
+
+  template<class T>
+  struct magic {
+    using value_type     = decay_t<T>;
+    using reference_type = T&&;
+
+    reference_type src;
+
+    template<size_t N, class Target>
+    auto value_or_t (reference_type x, ...) {
+      return std::get<N> (forward<reference_type> (x));
+    }
+
+    template<
+      size_t N,
+      class Target,
+      class Ret = enable_if_t<(N >= tuple_size<value_type> {}), tuple_element_t<N, Target>>
+    >
+    auto value_or_t (reference_type, int) -> Ret {
+      return {};
+    }
+
+    template<class Target, size_t... Ns>
+    auto worker (index_sequence<Ns...>) {
+      return Target { value_or_t<Ns, Target> (forward<reference_type> (src), 0)... };
+    }
+
+    template<class Tuple, class index_seq = make_index_sequence<tuple_size<Tuple> {}>>
+    operator Tuple () {
+      return worker<Tuple> (index_seq {});
+    }
+  };
+}
+
+template<class T>
+detail::magic<T&&>
+make_expandable (T&& src) {
+  return { std::forward<T> (src) };
+}
+
+// -----------------------------------------------------------
+
+#include <iostream>
+#include <memory>
+#include <tuple>
+
+int main () {
+  {
+    std::tuple<int,int>          x { 1, 2 };
+    std::tuple<float,float,bool> y = make_expandable (x);
+
+    std::cout << std::get<0> (y) << " " << std::get<1> (y) << " " << std::get<2> (y) << std::endl;
+  }
+  
+  {
+    std::tuple<std::unique_ptr<int>>      a { std::make_unique<int> (123) };
+    std::tuple<std::unique_ptr<int>, int> b = make_expandable (std::move (a));
+
+    std::cout << *std::get<0> (b) << std::endl;
+  } 
+}
