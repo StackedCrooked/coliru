3f999fbed60ece81a801c4ed3d803bc3464186ad
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-18 14:24:17 +0000

    Update archive ce

---
+++ trunk/Archive2/ce/8f96d9329f94a1/main.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <iostream>   
+#include <cstdlib>
+#include <time.h>
+
+int main()
+{
+#if 1
+    int a = a + 3;
+#else
+    auto a = int{a + 3};
+#endif
+    return 0;
+}
+
+
