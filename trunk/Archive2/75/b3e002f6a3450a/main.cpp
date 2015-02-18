c126fcb17fa82b1074ca7271758719bef789b407
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-18 23:27:53 +0000

    Update archive 75

---
+++ trunk/Archive2/75/b3e002f6a3450a/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iterator>
+
+struct loud
+{
+    int i = 12;
+    loud() { std::cout << "loud() at " << (void*)this << "\n"; }
+    loud(loud const&) { std::cout << "loud(loud const&) at " << (void*)this << "\n"; }
+    ~loud() { std::cout << "~loud() at " << (void*)this << "\n"; }
+};
+
+class my_iterator : public std::iterator<std::bidirectional_iterator_tag, int> {
+  loud d_val;
+public:
+  my_iterator  operator--(int) { std::cout << "decrement--\n"; return my_iterator(); }
+  my_iterator &operator--()    { std::cout << "--decrement(" << d_val.i << ")\n"; return *this; }
+  my_iterator  operator++(int) { std::cout << "increment++\n"; return my_iterator(); }
+  my_iterator &operator++()    { std::cout << "++increment\n"; return *this; }
+
+  int &operator*() { std::cout << "*dereference(" << d_val.i << ")\n"; return d_val.i; }
+
+  bool operator==(my_iterator const  &o) { return false; }
+  bool operator!=(my_iterator const  &o) { return true ; }
+};
+
+
+int main() {
+  auto it = std::reverse_iterator<my_iterator>();
+  std::cout << "between construction of reverse iterator and dereference\n";
+  int &i = *it;
+  std::cout << i << '\n';
+}
