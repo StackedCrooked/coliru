2f9a715f600d8ea71c1e1bbc66470ebb25e47bc2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-18 00:16:36 +0000

    Update archive 5b

---
+++ Archive2/5b/b648a13cbe8f61/main.cpp
@@ -0,0 +1,44 @@
+#include <tuple>
+
+namespace detail {
+  using namespace std;
+
+  template<class... Ts> 
+  struct magic_impl {
+    template<class T>
+    struct hack {
+      typedef T type;
+
+      hack () = default;
+
+      template<class U>
+      hack (U&&) { } 
+
+      operator type () { return {}; }
+    };  
+
+    template<class... Us> 
+    operator tuple<Us...> () const && {
+      return [this] (hack<Ts>..., auto... xs) {
+        return tuple_cat<tuple<Ts const&...>, tuple<decltype(xs)...>> (src, {});
+      } ( hack<Us> {}... );
+    }   
+
+    tuple<Ts...> const& src;
+  };  
+}
+
+template<class... Ts> 
+detail::magic_impl<Ts...>
+make_expandable (std::tuple<Ts...> const& src) {
+  return { src };
+}
+
+#include <iostream>
+
+int main () {
+  std::tuple<int,int>          x { 1, 2 };
+  std::tuple<float,float,bool> y = make_expandable (x);
+
+  std::cout << std::get<0> (y) << " " << std::get<1> (y) << " " << std::get<2> (y);
+}
