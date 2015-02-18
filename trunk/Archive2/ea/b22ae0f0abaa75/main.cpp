b6cf0cbd4a816690621a9dc48bba038ea254644d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-18 14:15:32 +0000

    Update archive ea

---
+++ trunk/Archive2/ea/b22ae0f0abaa75/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+class Base {
+protected:
+    string type;
+    int a;
+public:
+    Base(const Base &) = delete;
+
+    void operator=(Base const &) = delete;
+
+    Base() {
+        type = "Base";   
+    }
+    
+    Base(int _a) {
+        type = "Base";
+        a = _a;
+    }
+    
+    bool operator<(const Base &b) {
+        return a < b.a;   
+    }
+
+    string getType() {
+        lol();
+        return type;   
+    }
+    
+private:
+    virtual void lol() {
+              cout << "base\n";
+    }
+};
+
+class Derived: public Base {
+  
+  private:
+  virtual void lol() override {
+      cout << "derv\n";
+  }
+};
+
+
+
+int main()
+{
+    Derived *d = new Derived();
+    d->getType();
+   return 0; 
+}
