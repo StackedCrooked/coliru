b320f3585fe5ed4caa87a561d0277406310359e3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-18 22:02:57 +0000

    Update archive ba

---
+++ trunk/Archive2/ba/74dc83a98bde77/cmd.sh
@@ -0,0 +1,1 @@
+g++ -std=c++11 -O2 -Wall -pedantic -msse2 -ftree-vectorize -pthread main.cpp && ./a.out
---
+++ trunk/Archive2/ba/74dc83a98bde77/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <random>
+#include <vector>
+#include <chrono>
+#include <functional>
+#include <cassert>
+#include <cmath>
+
+
+int main()
+{
+    std::random_device rd;
+    const size_t test_size = 10000000;
+    // Choose a random mean between 1 and 6
+    std::mt19937_64 e1(rd());
+    std::uniform_real_distribution<double> uniform_dist(-1000.0, 1000.0);
+    std::vector<double> d1;
+    d1.reserve(test_size);
+    std::vector<double> d2;
+    d1.reserve(test_size);
+    auto random = std::bind(uniform_dist, e1);
+    for(size_t i = 0; i < test_size; ++i)
+    {
+        auto r = random();
+        d1.emplace_back(r);
+        d2.emplace_back(r);
+    }
+    
+    auto start = std::chrono::high_resolution_clock::now();
+    for(size_t i = 0; i < test_size; ++i)
+    {
+        d1[i] *= 0.5;
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(end -start);
+    std::cout << ms.count() << std::endl;
+    start = std::chrono::high_resolution_clock::now();
+    for(size_t i = 0; i < test_size; ++i)
+    {
+        d2[i] /= 2.0;
+    }
+    end = std::chrono::high_resolution_clock::now();
+    ms = std::chrono::duration_cast<std::chrono::milliseconds>(end -start);
+    std::cout << ms.count() << std::endl;
+    
+    for(size_t i = 0; i < test_size; ++i)
+    {
+        assert(std::abs(d1[i] - d2[i]) < 0.01);
+    }
+}
