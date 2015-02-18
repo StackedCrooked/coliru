74716f161ead273fe714b60a27fe7e49996726f0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-18 23:20:55 +0000

    Update archive 0d

---
+++ Archive2/0d/ff72133dbf9d1f/main.cpp
@@ -0,0 +1,15 @@
+#include <memory>
+#include <iostream>
+
+struct CAT
+{
+   CAT(){}
+   CAT(const CAT&) {std::cout << "COPY";}
+   CAT(CAT&&) {std::cout << "MOVE";}
+};
+
+int main() {
+    const CAT cat;
+    CAT cat2 = std::move(cat);
+    (void)cat2;
+}
