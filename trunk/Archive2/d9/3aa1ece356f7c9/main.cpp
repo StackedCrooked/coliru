9e77b51eb0f241967780674c541694e73c89af53
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-19 00:23:52 +0000

    Update archive d9

---
+++ trunk/Archive2/d9/3aa1ece356f7c9/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+int main() {
+    vector <char> slova = {'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'};
+    for ( auto c : slova ) cout << c;
+    cout << endl;
+}
