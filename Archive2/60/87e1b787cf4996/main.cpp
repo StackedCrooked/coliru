c78117770221a7b9f018bdfb4ad06001c3c19ae1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-19 04:55:24 +0000

    Update archive 60

---
+++ Archive2/60/87e1b787cf4996/main.cpp
@@ -0,0 +1,10 @@
+#include <thread.h>
+#include <time.h>
+#include <stdio.h>
+ 
+int main(void)
+{
+    printf("Time: %s", ctime(&(time_t){time(NULL)}));
+    thrd_sleep(&(struct timespec){.tv_sec=1}, NULL); // sleep 1 sec
+    printf("Time: %s", ctime(&(time_t){time(NULL)}));
+}
