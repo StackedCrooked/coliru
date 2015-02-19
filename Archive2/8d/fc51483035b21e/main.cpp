430dfc39e845c3df67a2af75d45e0ce5eb8b2231
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-19 16:09:26 +0000

    Update archive 8d

---
+++ Archive2/8d/fc51483035b21e/main.cpp
@@ -0,0 +1,22 @@
+#include <exception>
+#include <iostream>
+
+static int i = 555;
+void f() {
+  try {
+    throw 12;
+  } catch (void *) {
+    i = 666;
+  }
+}
+
+void term_handler() {
+  std::cout << i;
+  std::abort();
+}
+
+int main() {
+  std::set_terminate(term_handler);
+  f();
+  return 0;
+}
