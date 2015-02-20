1aa216ea1105af8b9c0bbdb08e1d7d4aefb1c0d2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-20 02:32:38 +0000

    Update archive 42

---
+++ trunk/Archive2/42/71fb4fec535a6b/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <functional>
+
+struct Bar {
+    Bar(const Bar &) { std::cout << "const Bar & " << this << std::endl; }
+    Bar() { std::cout << "Bar " << this << std::endl; }
+    ~Bar() { std::cout << "~Bar " << this << std::endl; }
+    };
+
+std::function<void()> foo(const Bar & b)
+{
+    return [b]() { std::cout << "captured b=" << &b << std::endl; };
+}
+
+int main()
+{
+    std::vector<std::function<void()>> callbacks;
+    {
+        Bar b;
+        callbacks.emplace_back(foo(b));
+        std::cout << "b=" << &b << std::endl;
+    }
+    callbacks.front()();
+}
