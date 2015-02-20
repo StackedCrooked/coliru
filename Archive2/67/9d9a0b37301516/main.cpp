ecd0e1c63238c45a8f57717fa12a2eb51ef27924
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-20 00:19:25 +0000

    Update archive 67

---
+++ Archive2/67/9d9a0b37301516/main.cpp
@@ -0,0 +1,30 @@
+#include <boost/function.hpp>
+#include <boost/phoenix/phoenix.hpp>
+#include <iostream>
+
+int main()
+{
+    /*
+     * suppose we have
+     *     template<typename Thing> using pointer = Thing*;
+     * then pointer<int(int)> is int (*)(int) and function<int(int)> is a more general version
+     */
+    
+    int i = 2;
+   
+    /*
+     * can't pass local type to function template
+    struct local {
+        int operator()(int arg) { return 3 * i + 4 * arg; };
+        // name's the same, as a pun
+        int i;
+    } local_object = { i };
+     */
+    
+    namespace phx = boost::phoenix;
+    using namespace phx::placeholders;
+    using boost::proto::deep_copy;
+    boost::function<int(int)> f = deep_copy(3 * phx::ref(i) + 4 * arg1);
+   
+    std::cout << f(5) << '\n';
+}
