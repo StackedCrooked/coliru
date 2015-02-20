39c8e980582d5c269154244ac3296b876040e0bf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-20 09:38:26 +0000

    Update archive 1f

---
+++ trunk/Archive2/1f/bd46340ab9b14e/cmd.sh
@@ -0,0 +1,3 @@
+set -x; clang++ -stdlib=libc++ -std=c++14 -O3 -Wall -pedantic -pthread main.cpp && ./a.out
+g++ -std=c++14 -O3 -Wall -pedantic -pthread main.cpp && ./a.out
+g++ -std=gnu++11 -O3 -Wall -pedantic -pthread main.cpp && ./a.out
