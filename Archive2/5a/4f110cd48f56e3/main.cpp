a4fa1d4c117a8b9061be344fd3e019e9a1ffc0d7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-20 19:21:43 +0000

    Update archive 5a

---
+++ Archive2/5a/4f110cd48f56e3/main.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+
+template <std::size_t S>
+struct helper {};
+
+template <>
+struct helper<1> {
+    using type = uint8_t;
+};
+template <>
+struct helper<2> {
+    using type = uint16_t;
+};
+template <>
+struct helper<4> {
+    using type = uint32_t;
+};
+template <>
+struct helper<8> {
+    using type = uint64_t;
+};
+
+template <typename T>
+using int_type = typename helper<sizeof(T)>::type;
+
+template <typename T>
+int_type<T> caster(const T& value) {
+    int_type<T> v;
+    static_assert(sizeof(value) == sizeof(v), "");
+    std::copy_n(reinterpret_cast<const char*>(&value), sizeof(T), reinterpret_cast<char*>(&v));
+    return v;
+}
+
+int main() {
+    return caster(3.14f);
+}
