bd194fe75213069958026d4891b090bd14dee682
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-21 21:49:47 +0000

    Update archive c2

---
+++ trunk/Archive2/c2/488ae7d7d4b779/cmd.sh
@@ -0,0 +1,1 @@
+g++ -std=c++11 -O2 -Wall -pedantic -pthread main.cpp && echo '604 2.37 foo' | ./a.out
---
+++ trunk/Archive2/c2/488ae7d7d4b779/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int a;
+    double b;
+    std::string c;
+    
+    std::cin >> a >> b >> c;
+    std::cout << a << b << c;
+}
