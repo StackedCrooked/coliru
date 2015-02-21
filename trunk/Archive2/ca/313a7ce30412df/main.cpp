7d2fa25d57667fa0fbe08c1aff4cec27e6310cfb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-21 18:48:48 +0000

    Update archive ca

---
+++ trunk/Archive2/ca/313a7ce30412df/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+struct CAT
+{
+    CAT(){}
+    int age_;
+};
+
+
+void f(int&& )
+{
+    cout<<"f(int&&)"<<endl;
+}
+
+void f(int &)
+{
+
+    cout<<"f(int&)"<<endl;
+}
+
+
+template<typename T>
+void foo(T&& t)
+{
+    f(std::forward<T>(t).age_);
+}
+
+
+int main()
+{
+    CAT c;  
+    foo(c);            //c is lvalue
+    foo(std::move(c)); //move(c) is rvalue
+}
