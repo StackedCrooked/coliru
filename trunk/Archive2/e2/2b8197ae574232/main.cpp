c8d4ad402d507de89a7c55952e713c41a72662d5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-22 21:47:36 +0000

    Update archive e2

---
+++ trunk/Archive2/e2/2b8197ae574232/main.cpp
@@ -0,0 +1,24 @@
+#include <unordered_map>
+#include <string>
+#include <iostream>
+
+template <typename CharT>
+bool isUnique(const std::basic_string<CharT>& s)
+{
+    if (s.size() <= 1)
+        return true;
+    
+    std::unordered_map<CharT, bool> charSeen;
+    for (int i = 0; i < s.size(); i++) {
+        if (!charSeen[s[i]])
+            charSeen[s[i]] = true;
+        else
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    std::cout << isUnique(L"فعه") << '\n';
+}
