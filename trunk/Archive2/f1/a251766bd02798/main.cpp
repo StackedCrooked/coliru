f4a097e4b9923cc58084459fcd8cb17798bf0bb6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-22 03:03:07 +0000

    Update archive f1

---
+++ trunk/Archive2/f1/a251766bd02798/main.cpp
@@ -0,0 +1,11 @@
+#include <stdint.h>
+#include <iostream>
+ 
+int main() 
+{
+    uint64_t    v1 = 58;
+    uint64_t    v2 = 56;
+    
+    std::cout << -(v1 - v2) << std::endl;
+    std::cout << (int64_t)(-(v1 - v2)) << std::endl;
+}
