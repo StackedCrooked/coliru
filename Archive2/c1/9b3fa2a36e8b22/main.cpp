a8d14d7e872e87c6fa1657a32062546a108c088a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-22 16:32:43 +0000

    Update archive c1

---
+++ Archive2/c1/9b3fa2a36e8b22/main.cpp
@@ -0,0 +1,43 @@
+#include <deque>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <thread>
+
+class syncstream : public std::ostringstream
+{
+public:
+    using std::ostringstream::ostringstream;
+
+    syncstream& operator<<(std::ostream&  (*pf)(std::ostream&) ) { pf(*this); return *this; }
+    syncstream& operator<<(std::ios&      (*pf)(std::ios&)     ) { pf(*this); return *this; }
+    syncstream& operator<<(std::ios_base& (*pf)(std::ios_base&)) { pf(*this); return *this; }
+
+    template<typename T>
+    syncstream& operator<<(T&& token)
+    {
+        static_cast<std::ostringstream&>(*this) << std::forward<T>(token);
+        return *this;
+    }
+};
+
+inline std::ostream& operator&&(std::ostream& s, const syncstream& g) { return s << g.str(); }
+
+#define synced(stream) stream && syncstream()
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void proc(int n)
+{
+    synced(std::cout) << "Hello" << ' ' << "world" << ' ' << "from" << ' ' << "thread" << ' ' << n << '\n';
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(true);
+
+    std::deque<std::thread> threads;
+    for(int n = 0; n < 1000; ++n) threads.push_back(std::thread(std::bind(proc, n)));
+
+    for(std::thread& thread: threads) thread.join();
+    return 0;
+}
