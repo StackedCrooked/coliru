6722f20df765d7f22b46bee39172df4902d8a33a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-23 23:57:47 +0000

    Update archive f1

---
+++ trunk/Archive2/f1/30cdb263b21119/main.cpp
@@ -0,0 +1,43 @@
+namespace detail {
+  template<class... Ts>
+  constexpr int constexpr_check (Ts...) {
+    return 1;
+  }
+}
+
+#define constexpr_call(FUNC, ...) \
+  (FUNC <noexcept(detail::constexpr_check(__VA_ARGS__))>(__VA_ARGS__))
+
+
+// -----------------------------------------------------------------------------
+
+#include <iostream>
+#include <cmath>
+
+template<bool B = false>
+constexpr int pow (int base, int exp) {
+  return exp ? pow<B> (base, exp-1) * base : 1;
+}
+
+template<>
+int pow<false> (int base, int exp) {
+  std::cout << "(not constexpr) ";
+  return std::pow (base, exp);
+}
+
+int main () {
+  {
+    constexpr int e = 4;
+    constexpr int r = constexpr_call (pow, 2, e);
+    std::cout  << r << std::endl;
+  }
+  {
+    constexpr int r = constexpr_call (pow, 2, 4);
+    std::cout  << r << std::endl;
+  }
+  {
+    int e = 4;
+    int r = constexpr_call (pow, 2, e);
+    std::cout << r << std::endl;
+  }
+}
