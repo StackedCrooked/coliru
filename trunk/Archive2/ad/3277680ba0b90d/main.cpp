98baf6ff647e7583ad97d7b5089248bbc465de2d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-24 11:12:48 +0000

    Update archive ad

---
+++ trunk/Archive2/ad/3277680ba0b90d/main.cpp
@@ -0,0 +1,15 @@
+#include <functional>
+#include <iostream>
+ 
+template <typename A, typename B, typename U = std::less<int>>
+bool f(A a, B b, U u = U())
+{
+    return u(a, b);
+}
+ 
+int main() 
+{
+    std::cout << std::boolalpha;   
+    std::cout << f(5, 20) << '\n';
+    std::cout << f(100, 10) << '\n';
+}
