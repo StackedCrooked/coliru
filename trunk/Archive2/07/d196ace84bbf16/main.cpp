cdc1b9d2e306c0ac472012ad9551dc4c855cf824
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-24 12:05:06 +0000

    Update archive 07

---
+++ trunk/Archive2/07/d196ace84bbf16/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <thread>
+#include<chrono>
+using namespace std;
+
+bool finish=false;
+
+void fun(){
+    while(!finished){
+     cout<<4<<endl;
+     }
+    }
+void fun2(){
+    while(!finshed){
+        cout<<1<<endl;
+        }
+    }
+    
+    int main(){
+        using namespace std::chrono;
+        auto t1=thread(fun);
+        auto t2=thread(fun2);
+        this_thread::sleep_for(milliseconds(100));
+        finished=true;
+        t1.join();
+        t2.join();
+        return 0;
+        }
