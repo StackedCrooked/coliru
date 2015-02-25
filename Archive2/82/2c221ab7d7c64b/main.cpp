5a62a97ec113085a80da3e15a84b5e034c1017a4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-25 00:11:49 +0000

    Update archive 82

---
+++ Archive2/82/2c221ab7d7c64b/main.cpp
@@ -0,0 +1,28 @@
+// basic_string_push_back.cpp
+// compile with: /EHsc
+#include <string>
+#include <iostream>
+
+int main( ) 
+{
+   using namespace std;
+   string str1 ( "abc" );
+   basic_string <char>::iterator str_Iter, str1_Iter;
+
+   cout << "The original string str1 is: ";
+   for ( str_Iter = str1.begin( ); str_Iter != str1.end( ); str_Iter++ )
+      cout << *str_Iter;
+   cout << endl;
+
+   str1.push_back ( 'd' );
+   str1.push_back ("abc");
+   str1_Iter = str1.end ( );
+   str1_Iter--;
+   cout << "The last character-letter of the modified str1 is now: "
+        << *str1_Iter << endl;
+
+   cout << "The modified string str1 is: ";
+   for ( str_Iter = str1.begin( ); str_Iter != str1.end( ); str_Iter++ )
+      cout << *str_Iter;
+   cout << endl;
+}
