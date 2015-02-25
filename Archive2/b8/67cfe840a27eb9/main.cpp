99f84c97a3872436725123dbe6aeca738e162a0b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-25 09:05:48 +0000

    Update archive b8

---
+++ Archive2/b8/67cfe840a27eb9/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+template<int N>
+class A
+{
+    static const int a = A<N-1>::a; //OK, doesn't require implicit intantiation
+};
+
+template<int N>
+class B
+{
+    static const int a = B<1>::a; //Error, implicit instantiation of template 'B<1>' within its own definition
+};
+
+
+int main(){ }
