8b1ab8dfcbe28f1238651e35026e1e3c67783b3e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-25 21:17:56 +0000

    Update archive e1

---
+++ trunk/Archive2/e1/20087188c706f5/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void foo(const int& y)
+{
+    int& z = const_cast<int&>(y);
+    z = 4;
+}
+
+int main()
+{
+    int x = 3;
+    foo(x);
+    std::cout << x << std::endl;
+    
+    return 0;
+}
