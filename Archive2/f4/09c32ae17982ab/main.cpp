af6ed7651bbdbc515b073b675f8b54ca3e67345c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-25 09:03:12 +0000

    Update archive f4

---
+++ Archive2/f4/09c32ae17982ab/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, i;
+		//Отримати число n1 введене з калвіатури
+	cout<<"Vveditj chyslo n i natysnitj ENTER:";
+	cin>>n;
+	
+		while(n <= i);	{ 	//поки значення змінної n1 
+						//менше або рівне значенню змінної n2}
+		cout<<n<<" "; 	//вивести значення змінної n1,
+		i=i-1;			//додати 1 до значення змінної n1.			
+		
+		    
+		}
+		return 0; 
+		}
