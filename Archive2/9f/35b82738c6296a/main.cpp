a9e6fbf596d5b05a90e6e987a909e9b72634c116
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-25 00:31:49 +0000

    Update archive 9f

---
+++ Archive2/9f/35b82738c6296a/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+class Example
+{    
+     static enum Items{ desk = 0, chair, monitor };
+     enum Colors{ red = 0, blue, green };
+};
+
+int main()
+{
+  Example e;
+}
