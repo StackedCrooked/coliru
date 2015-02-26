104a4f69c352655ce3468fbad3a042c5f188c8e1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-26 03:01:54 +0000

    Update archive 1b

---
+++ trunk/Archive2/1b/20d32896c4885e/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    for (int i = 10; i < 50; i += 10) {
+        for (int j = 1; j < 6; j++)
+            std::cout << (i + j) << ' ';
+        std::cout << '\n';
+    }
+}
