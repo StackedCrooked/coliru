3b1b9f30ce44a875a7db72a335471a30a54d1b62
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-26 19:24:08 +0000

    Update archive b7

---
+++ trunk/Archive2/b7/21396b64aeb0e4/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+int main()
+{
+   int i = 0, maxPage = 4;
+    std::cout << "Page " + std::to_string(i) + " of " + std::to_string(maxPage); 
+}
