6c6c2b8065a3de88c17fe60575ae0758f94ca52f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-26 19:15:18 +0000

    Update archive 2b

---
+++ Archive2/2b/db3242c1a0202f/main.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+int main() {
+    std::string s = "abcdef";
+    
+    std::string s2 = s;
+    
+    auto begin = const_cast<std::string const &>(s2).begin();
+    auto end = const_cast<std::string const &>(s2).end();
+    
+    std::cout << end - begin << '\n';
+}
