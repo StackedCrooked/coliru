641d795458794f9da02f3ba32129502584b67d07
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-26 16:10:29 +0000

    Update archive d0

---
+++ Archive2/d0/414bd65ad151d1/main.cpp
@@ -0,0 +1,11 @@
+template <typename T, T Expected, typename U, U Actual>
+void check_eq() {
+  static_assert(Expected == Actual, "Equality check failed!");
+}
+
+#define COMPILE_CHECK_EQ(a, b) \
+  check_eq<decltype(a), a, decltype(b), b>()
+
+int main() {
+  COMPILE_CHECK_EQ(sizeof(int), 6);
+}
