b8681311c014e6f83c22f582c3c8500a46ebdf6a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-26 03:56:24 +0000

    Update archive bd

---
+++ trunk/Archive2/bd/f26ce4876f72f5/main.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    vector<unsigned char> bytes {0xFF, 0xFF, 0xFD};
+
+    for (auto const & v: bytes) {
+        cout << hex << setfill('0') << setw(2) << uppercase << static_cast<unsigned>(v) <<" ";
+    }
+    
+    cout << endl;
+}
