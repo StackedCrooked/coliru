9c6a3fc3186df887220c1a3a14a6e91752a5b534
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-26 14:38:57 +0000

    Update archive d3

---
+++ Archive2/d3/75b90c1fae84be/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+const char *arr[5] = {
+"string1", //some comment
+"string2",
+"string3"  //another comment
+"string4",
+"string5"
+};
+
+int main()
+{
+    printf("%s", arr[4]);   
+}
