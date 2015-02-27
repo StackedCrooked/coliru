b8ee80130d268e154d43a50253c4347fcc98b62c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-27 18:44:51 +0000

    Update archive 05

---
+++ trunk/Archive2/05/98e02bf470d41e/main.cpp
@@ -0,0 +1,25 @@
+class A {
+public:
+    A(int i) : x(i) { }
+    A() : x(1) {}
+    int x;
+};
+
+class B : private virtual A {
+protected:
+    B(int i) : A(i) { }
+};
+
+class C : public B, private virtual A {
+protected:
+    C(int i) : A(i), B(i) { }
+};
+
+class D : public C {
+public:
+    D() : A(1), C(3) { }
+};
+
+int main() {
+    D d;
+}
