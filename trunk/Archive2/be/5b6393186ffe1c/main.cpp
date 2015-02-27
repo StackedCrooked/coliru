11c37c17f03bbbc2c0404541c7a7fe64dcaa6aa1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-27 15:26:14 +0000

    Update archive be

---
+++ trunk/Archive2/be/5b6393186ffe1c/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    char _ = 'a' ; // can an identifier be just an underscore? yes
+    
+    float Float = 3.14f ; // fine. case-sensitive; float and Float are different
+    
+    int month = 09 ; // *** error: 9 is not an octal digit; valid octal digits are 0, 1, ... 7 
+}
