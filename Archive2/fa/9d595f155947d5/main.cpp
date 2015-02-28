8dc158fc9d1ccd0ac2c78cb565e532cec629a471
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-28 03:32:09 +0000

    Update archive fa

---
+++ Archive2/fa/9d595f155947d5/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+ 
+int main(void)
+{
+    bool a=true, b=false;
+    printf("%d\n", a&&b);
+    printf("%d\n", a||b);
+    printf("%d\n", !b);
+    
+    int n = 1;
+}
