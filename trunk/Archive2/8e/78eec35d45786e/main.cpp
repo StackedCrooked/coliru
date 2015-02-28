34b1aafd3d3ca47bdb210a0279d72e1963a5087c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-28 13:40:42 +0000

    Update archive 8e

---
+++ trunk/Archive2/8e/78eec35d45786e/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <atomic>
+#include <mutex>
+#include <memory>
+#include <map>
+#include <future>
+using namespace std;
+
+vector<shared_ptr<thread> > pool;
+
+void ClearTasksPool() {
+    for(auto & t : pool) {
+        t->join();
+    }  
+}
+
+string GetUrl(string address) {
+    this_thread::sleep_for(chrono::milliseconds(100));
+    return "data " + address;
+}
+
+int main() {
+    std::packaged_task<string()> getResource([]{
+        return GetUrl("bla-bla");
+    }); 
+    auto result = getResource.get_future();
+    
+    pool.push_back(make_shared<thread>(move(getResource)));
+    
+    cout << result.get() << endl ;
+    
+    ClearTasksPool();
+    return 0;
+}
