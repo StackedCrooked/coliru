00c37013052d29944bcc0e3fa26840647c6df6b9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-02-28 11:34:56 +0000

    Update archive 68

---
+++ trunk/Archive2/68/8618f8088ea4b0/cmd.sh
@@ -0,0 +1,3 @@
+#title Destructors and code size.
+run() { export n=$1 ; cmd="g++ -c -DA=$n -std=c++11 -O2 -Wall -pedantic -pthread main.cpp -S -o - | wc -c" ; echo "$cmd => code size is $(eval "$cmd")" ; }
+run 1 ;  run 2 ; run 3
