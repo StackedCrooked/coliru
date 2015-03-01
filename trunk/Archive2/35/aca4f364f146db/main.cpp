f818260690aca5f084b54ba4b236c98e8b6ec1c2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-01 21:25:24 +0000

    Update archive 35

---
+++ trunk/Archive2/35/aca4f364f146db/main.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <iostream>
+#include <complex>
+
+using namespace std;
+
+int main ()
+{
+    double value = 1.0;
+    std::complex<double> valueC;
+    valueC = std::complex<double>(value,0.0);
+}
