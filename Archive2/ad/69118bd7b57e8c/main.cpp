61960090c263499a742f35426efa4433554b591d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-01 14:40:27 +0000

    Update archive ad

---
+++ Archive2/ad/69118bd7b57e8c/main.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+#define offsetof2(type,member) ((std::size_t) &(((type*)0)->member))
+
+template < typename Tn, typename ...P >
+struct Vertex
+{
+    // want:
+    //   Vertex<...P>  v;
+    // unless termination (in which case int)
+    conditional< (sizeof...(P)>0), Vertex<P...>, int >  v;
+
+    // split Tn into T[n]
+    // http://en.cppreference.com/w/cpp/types/extent
+    using T = typename decay<Tn>::type;
+    static constexpr  unsigned int  n = extent<Tn>::value;
+    
+    T t[n];
+    
+    using U = Vertex<Tn, P...>;
+    
+    static constexpr unsigned int byteoffset = offsetof2( U, t );
+    static constexpr unsigned int byteoffset = offsetof( U, t );
+}
+
+int main()
+{
+    using V = Vertex< float[3],  int[2],  double[4] >;
+    cout << sizeof(V);
+}
