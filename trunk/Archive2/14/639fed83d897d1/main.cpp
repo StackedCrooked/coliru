666d5030cac79531df137b479afd7aa2ed25718c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-01 14:41:43 +0000

    Update archive 14

---
+++ trunk/Archive2/14/639fed83d897d1/main.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <functional>
+ 
+int main()
+{
+    std::vector<int> v(10, 2);
+    std::partial_sum(v.cbegin(), v.cend(), v.begin());
+    std::cout << "Among the numbers: ";
+    std::copy(v.cbegin(), v.cend(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << '\n';
+ 
+    if (std::all_of(v.cbegin(), v.cend(), [](int i){ return i % 2 == 0; })) {
+        std::cout << "All numbers are even\n";
+    }
+    if (std::none_of(v.cbegin(), v.cend(), std::bind(std::modulus<int>(), 
+                                                     std::placeholders::_1, 2))) {
+        std::cout << "None of them are odd\n";
+    }
+    struct DivisibleBy
+    {
+        const int d;
+        DivisibleBy(int n) : d(n) {}
+        bool operator()(int n) const { return n % d == 0; }
+    };
+ 
+    if (std::any_of(v.cbegin(), v.cend(), DivisibleBy(7))) {
+        std::cout << "At least one number is divisible by 7\n";
+    }
+}
