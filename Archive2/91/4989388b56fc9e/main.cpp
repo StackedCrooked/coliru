804d54523ec1a13ac060d1188b7c2168b8f636fd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-02 05:06:43 +0000

    Update archive 91

---
+++ Archive2/91/4989388b56fc9e/main.cpp
@@ -0,0 +1,5 @@
+In file included from C:\Project\CGame.h:9:0,
+                 from C:\Project\CEnemy.h:3,
+                 from C:\Project\CEnemy.cpp:1:
+C:\Project\CEnemyHealth.h:10:42: error: 'CEnemy' has not been declared
+C:\Project\CEnemyHealth.h:11:9: error: 'CEnemy' does not name a type
