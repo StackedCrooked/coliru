799a19b21ab50fe3a945044213917ff01bc3fe87
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-02 05:51:56 +0000

    Update archive 35

---
+++ trunk/Archive2/35/156f26efd1ec1b/main.cpp
@@ -0,0 +1,23 @@
+Error:
+In file included from C:\Project\CGame.h:9:0,
+                 from C:\Project\CEnemy.h:3,
+                 from C:\Project\CEnemy.cpp:1:
+C:\Project\CEnemyHealth.h:11:16: error: field 'cenemy1' has incomplete type
+
+//CEnemyHealth.h
+#ifndef CENEMYHEALTH_H
+#define CENEMYHEALTH_H
+#include "CEnemy.h"
+
+class CEnemyHealth
+{
+    public:
+        CEnemyHealth();
+        virtual ~CEnemyHealth();
+        void Set(CEnemy &passed_cenemy); 
+        CEnemy cenemy1;                  //error: "field 'cenemy1' has incomplete type"
+    protected:
+    private:
+};
+
+#endif // CENEMYHEALTH_H
