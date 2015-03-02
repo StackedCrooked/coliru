33ce89f037c6c1fbe3488fddbc34fe8757e2269c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-02 02:59:23 +0000

    Update archive e3

---
+++ trunk/Archive2/e3/d7ea02b33923d1/main.cpp
@@ -0,0 +1,33 @@
+//CEnemyHealth.h file
+#ifndef CENEMYHEALTH_H
+#define CENEMYHEALTH_H
+#include "CEnemy.h" 
+
+class CEnemyHealth
+{
+    public:
+        CEnemyHealth();
+        virtual ~CEnemyHealth();
+        void Set(CEnemy &passed_cenemy); //error: "C:\Project\CEnemyHealth.h|10|error: 'CEnemy' has not been declared|"
+        CEnemy cenemy1;                  //error: "C:\Project\CEnemyHealth.h|11|error: 'CEnemy' does not name a type|"
+    protected:
+    private:
+};
+
+#endif // CENEMYHEALTH_H
+
+//CEnemy.h Separate File
+#ifndef CENEMY_H
+#define CENEMY_H
+#include <iostream>
+
+class CEnemy
+{
+    public:
+        CEnemy();
+        virtual ~CEnemy();
+    protected:
+    private:
+};
+
+#endif // CENEMY_H
