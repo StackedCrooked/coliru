02bbc2730f505e0bc4ab6745ef7111edc88285e7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-02 19:00:06 +0000

    Update archive 35

---
+++ trunk/Archive2/35/c815eabb741ca9/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    char nome;
+    printf("digite seu nome E NÚMERO DE TELEFONE\n");
+    scanf("%d", " %c", &nome, &num);
+
+    printf( num, nome);
+
+    return 0;
+}
