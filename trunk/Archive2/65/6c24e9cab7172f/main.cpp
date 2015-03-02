509158f714afe64196023204e0a1ddd6e9ba52b9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-02 02:05:51 +0000

    Update archive 65

---
+++ trunk/Archive2/65/6c24e9cab7172f/main.cpp
@@ -0,0 +1,14 @@
+#ifndef CENEMY_H
+#define CENEMY_H
+#include <iostream>
+
+class CEnemy
+{
+    public:
+        CEnemy();
+        virtual ~CEnemy();
+    protected:
+    private:
+};
+
+#endif // CENEMY_H
