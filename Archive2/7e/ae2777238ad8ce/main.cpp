b55de4afdb2342328d350bba630ebaf42e7ebc95
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-02 10:46:10 +0000

    Update archive 7e

---
+++ Archive2/7e/ae2777238ad8ce/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+int main()
+{
+    
+    std::vector<std::string> words = {
+        "Hello", "from", "GCC", __VERSION__, "!"
+    };
+    std::cout << words << std::endl;
+    {int x = 0;
+    double sum = 0.0;
+	
+	while (sum<17)
+	{
+        x=x++;
+		sum = sum + 1 / x;
+        
+	}
+	
+    std::cout << x;
+	}
+}
