e8fd2789573381984c9b533e668179c9dc1603a8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-02 14:19:02 +0000

    Update archive d0

---
+++ trunk/Archive2/d0/4b8e737c8a5270/main.cpp
@@ -0,0 +1,163 @@
+// https://github.com/julianstorer/JUCE/blob/master/examples/Demo/Source/Demos/OpenGLDemo.cpp#L51
+
+#include <string>
+#include <iostream>
+#include <typeinfo>
+
+#define COUT(x) cout << #x << ":" << x << endl
+
+using namespace std;
+
+//template< int i, typename... P >
+//struct Offset;
+//
+//template< int i, typename P0, typename... P >
+//struct Offset;
+//{
+//    static constexpr  unsigned int  offset = sizeof(P0) + conditional< i==0, 
+
+
+// * * *  AttributeRecursor  * * *
+
+// Fills out for k'th attribute of P...
+// Use like this:
+//      AttributeRecursor<k, 0, P...>::recursor
+// (always start with bytecount = 0)
+
+template < int k, typename ...P >
+struct AttributeRecursor;
+
+template < int k, typename Tn, typename ...P >
+struct AttributeRecursor<k, Tn, P...> 
+{
+private:
+    static_assert( 0 <= k  &&  k <= sizeof...(P),  "k out of bounds" );
+    
+    using This = AttributeRecursor< k  , Tn, P... >;
+    using Next = AttributeRecursor< k-1,     P... >;
+
+public:    
+    using destination = typename  conditional< k == 0, This, Next >::type;
+
+public:
+    // split Tn into Type[dim]
+    using Type = typename remove_extent<Tn>::type;
+    static constexpr  unsigned int  dim = extent<Tn>::value;
+
+    static constexpr  unsigned int  offset      = Next::count; // bytecount_in;
+    static constexpr  unsigned int  size_bytes  = sizeof(Tn);
+    //static constexpr  unsigned int  total_bytes = bytecount_in+sizeof(Tn);
+
+    static constexpr  unsigned int  count = size_bytes + Next::count; // + Next::remaining_bytes;
+
+    static void setup(int index, int total_bytes, int byteoffsets[] )
+    {
+        byteoffsets[index] = total_bytes - count;
+        
+        //glEnableVertexAttribArray( k );
+
+        //generic_glVertexAttribPointer< Type >(
+        //    k,
+        //    dim,
+        //    size_bytes,
+        //    byteoffset
+        //    );
+
+        cout << endl << "setup(" << index << ")" << endl;
+        COUT( k );
+        cout << "Type:" << typeid(Type).name() << endl;
+        COUT( dim ); 
+        COUT( size_bytes );
+        COUT( byteoffsets[index] ); 
+//        COUT( bytes_from_P0 ); 
+        
+        Next::setup(index+1, total_bytes, byteoffsets );
+    }
+};
+
+// recursion terminator
+template< typename ...P >
+struct AttributeRecursor <-1, P...> 
+{
+    //static constexpr  unsigned int  total_bytes = bytecount_in;
+    static constexpr  unsigned int  count = 0;
+
+    //using This = AttributeRecursor< -1  , /*bytecount_in,*/ P... >;
+    //using type = This;
+
+    static void setup(int index, int total_bytes, int byteoffsets[] )
+    {
+        cout << "terminator" << endl; 
+    };
+};
+
+// * * *  AttributeHandler  * * *
+
+template < typename... P >
+struct AttributeHandler
+{
+private:
+    // Number of attributes
+    static constexpr  unsigned int  N = sizeof...(P);
+    
+    // A<k> is the k'th attribute
+    //    allow e.g. `cout << A<2>::size_bytes;`
+    template <int k>
+    using AR = AttributeRecursor< k, P... >;
+    
+    // A<k> is the k'th attribute
+    //    allow e.g. `cout << A<2>::size_bytes;`
+    template <int k>
+    using D = typename AR<k>::destination;
+
+    
+    // A's range is 0 to N-1; N hits the recursion terminator
+    static constexpr  unsigned int  total_bytes = AR<N-1>::count;
+
+    unsigned char data[ total_bytes ];
+    
+    
+public:
+    template <int k>
+    typename D<k>::Type get() {
+        static_assert( k>=0  &&  k<N,  "Element is out of range!" );
+        typename D<k>::Type ret;
+        memcpy(
+                    &ret,
+                    data  +  total_bytes - D<k>::offset /*A<k>::offset*/,
+                    D<k>::size_bytes
+                    );
+        return ret;
+    }
+    
+    template <int k>
+    void set( typename D<k>::Type& ref ) {
+        static_assert( k>=0  &&  k<N,  "Element is out of range!" );
+        typename D<k>::Type ret;
+        memcpy(
+                    data + D<k>::offset,
+                    &ret,
+                    D<k>::size_bytes
+                    );
+    }
+    
+    static  int* byteoffsets() {
+        static int b[ N ];
+        return b;
+    }
+    
+    static void setup()
+    {
+        AR<N-1>::setup( 0, total_bytes, byteoffsets() );
+    }
+};
+
+int main()
+{    
+    //using V = AttributeHandler< float[3], int[1], double[4] >;
+    using V = AttributeHandler< int[1], int[2], float[3] >;
+    
+    COUT( sizeof(V) );
+    
+    V::setup();
+}
---
+++ trunk/Archive2/d0/c2d399a8b34775/main.cpp
@@ -0,0 +1,18 @@
+using DWORD = unsigned int;
+
+constexpr DWORD const_getHash(const char *str, DWORD curHash = 0) {
+    return !*str ? curHash : const_getHash(str + 1,
+        (curHash >> 13 | curHash << (32 - 13)) + (*str >= 'a' ? *str - 32 : *str));
+}
+
+template<DWORD hash>
+struct hash_holder
+{
+  enum { value = hash };
+};
+
+int main(int, char**)
+{
+  constexpr DWORD hash = const_getHash("ok");
+  static_assert(hash > 0, "woops");
+}
