ad19bd6031ac8b079c8e0b5b0fd5b386fe65fa01
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-03 06:45:20 +0000

    Update archive 5a

---
+++ Archive2/5a/5b18ba3b2a4f08/main.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <memory>
+
+class Base {
+public:
+    std::string Name;
+};
+using BaseList = std::vector<std::shared_ptr<Base>>;
+
+class Derived : Base {
+};
+using DerivedList = std::vector<std::shared_ptr<Derived>>;
+
+class BaseHandler {
+public:
+    void process( BaseList list ) {
+    }
+};
+
+int main() {
+ 
+    DerivedList list;
+ 
+    BaseHandler bh;
+    bh.process( list );
+     
+}
