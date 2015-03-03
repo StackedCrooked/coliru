520584d74588f1cf22efa6997f2d02d137b9536e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-03 22:07:05 +0000

    Update archive 00

---
+++ trunk/Archive2/00/2adb9fd371bc37/main.cpp
@@ -0,0 +1,52 @@
+
+template<typename... Ts>
+struct MetafunctionClass1
+{
+    template<typename... Args>
+    struct apply
+    {
+        using type = int;
+    };
+};
+
+template<typename... Ts>
+struct MetafunctionClass2
+{
+    template<typename... Args>
+    struct apply
+    {
+        using result = int;
+    };
+};
+
+template<typename T, typename... Args>
+using get_apply = typename T::template apply<Args...>;
+
+template<typename...>
+using void_t = void;
+
+template<typename T, typename = void>
+struct call
+{
+    using type = typename T::result;  
+};
+
+template<typename T>
+struct call<T, void_t<typename T::type>>
+{
+    using type = typename T::type;  
+};
+
+template<typename T, typename... Args>
+using call_metafunction_class = typename call<get_apply<T, Args...>>::type;
+
+struct X {}; //Just a placeholder
+
+using result1 = call_metafunction_class<MetafunctionClass1<X,X>,int,int,int>;
+using result2 = call_metafunction_class<MetafunctionClass2<X,X>,int,int,int>;
+
+int main()
+{
+    result1 a = 0;
+    result2 b = 0;
+}
