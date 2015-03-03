a32c24f3f911c691d8a5b409e845d2d0918c1355
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-03 21:49:32 +0000

    Update archive 26

---
+++ Archive2/26/2a251d4e62865c/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+using std::ostream;
+
+template<class T>
+struct trait
+{
+    static T append(const T&, const T&);
+};
+
+template<>
+struct trait<std::vector<int>>
+{
+    static std::vector<int> append(const std::vector<int>& a, const std::vector<int>& b)
+    {
+        std::vector<int> r;
+        r.insert(r.end(), a.begin(), a.end());
+        r.insert(r.end(), b.begin(), b.end());
+        return r;
+    }
+};
+
+template<>
+struct trait<string>
+{
+    static string append(const string& a, const string& b)
+    {
+        return a + b;
+    }
+};
+
+template<typename T>
+ostream& operator<< (ostream& out, const vector<T>& v)
+{
+    out << '{';
+    auto last = v.size() - 1;
+    for(size_t i = 0; i < v.size(); ++i)
+    {
+        out << v[i];
+        if (i != last) 
+            out << ", ";
+    }
+    return out << '}';
+}
+
+int main()
+{
+    using std::cout;
+    
+    cout << trait<string>::append("hello", " world") << std::endl
+         << trait<vector<int>>::append( {1,2,3}, {4,5,6} ) << std::endl;
+}
