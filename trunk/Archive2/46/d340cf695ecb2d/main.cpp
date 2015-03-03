6a28583afe3a2e90597273f0d0734f690acd55ab
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-03 19:09:27 +0000

    Update archive 46

---
+++ trunk/Archive2/46/d340cf695ecb2d/main.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdint.h>
+
+typedef struct {
+    uint8_t pageSize;//1;    //!< 0 indicates 512Bytes and 1 indicates 528Bytes
+    uint8_t protect;// 1;     //!< Tells whether or not protection is enabled
+    uint8_t density;// 4;     //!< It should just be 1011
+    uint8_t compare;// 1;     //!< Result of page to buffer comparison
+    uint8_t ready;// 1;       //!< Indicates if the device is busy or not
+} eeprom_status_register;
+
+eeprom_status_register thing;
+
+int main()
+{
+    uint8_t* thingPtr = (uint8_t*)&thing;
+    thing.pageSize = 0x1;
+    thing.protect = 0x2;
+    thing.density = 0x3;
+    thing.compare = 0x4;
+    thing.ready = 0x5;
+    
+    printf("%lu bytes\n", sizeof thing);
+    
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", *thingPtr++);
+    }
+    
+    return 0;
+}
