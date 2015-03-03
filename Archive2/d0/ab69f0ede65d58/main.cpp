00ea7c64b8629768c18080c909c1ac5f40c2058e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-03 13:32:02 +0000

    Update archive d0

---
+++ Archive2/d0/ab69f0ede65d58/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+struct A
+{
+    int i;
+    // N.B.: no default ctor
+    A(int i) : i(i)
+    {
+        cout << "A(int)" << endl;
+    }
+};
+
+struct B : virtual A
+{
+    B()
+    {
+        cout << "B()" << endl;
+    }
+};
+
+struct C : B
+{
+    void bar(){ };
+    C() : A(10), B()
+    {
+        cout << "C()" << endl;
+    }
+};
+
+C c;
+
+int main()
+{
+    std::cout << c.i << '\n';
+}
