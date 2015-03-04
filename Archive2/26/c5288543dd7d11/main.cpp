fee0b49bcf348f84ee26bdfde33e41d0a5efcd3e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-04 13:38:05 +0000

    Update archive 26

---
+++ Archive2/26/c5288543dd7d11/main.cpp
@@ -0,0 +1,10 @@
+#include <stdint.h>
+#include <iostream>
+
+int main(int argc, char** argv) {
+    int32_t x = -1;
+    uint32_t y = 0;
+    int64_t res = x + y;
+    std::cout << res << std::endl;
+    }
+
