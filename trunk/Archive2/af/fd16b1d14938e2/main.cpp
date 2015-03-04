2f7911408165d4c1f5e5bbd0f5c14d847bb18213
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-04 13:41:03 +0000

    Update archive af

---
+++ trunk/Archive2/af/fd16b1d14938e2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <iterator>
+using namespace std;
+
+float legge_oraria_moto_accelerato(float a[3]){
+    return a[2]*a[0] + 0.5*a[1]*a[0]*a[0];
+}
+int corri(float (f)(float array[3]), float (&arrays)[3][3])
+{
+    for( float (&row)[3] : arrays) cout << f(row) << '\n';
+    return 0;
+} 
+
+int main()
+{ 
+return 0;
+}
