f3afd4bf6e02d5b8d659189fe3f0a26f914e913e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-04 20:34:48 +0000

    Update archive 80

---
+++ Archive2/80/f1ed73907c6032/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+#include <cmath>
+#include <array>
+#include <random>
+
+void calculateExpectedMass(){
+
+    using RandomGenType = std::mt19937_64;
+    std::random_device rd;
+    RandomGenType gen(rd());
+
+    std::array<double,5> intervals {0.59, 0.7, 0.85, 1, 1.18};
+    std::array<double,4> weights {2*1.36815, 2*1.99139, 2*0.29116, 2*0.039562};
+    std::piecewise_constant_distribution<double>
+    distribution (intervals.begin(),intervals.end(),weights.begin());
+    
+    // add this to make PDF integrate to almost 1 --> does not change anything
+    /*for(auto & i : intervals){
+        i+= 3.18644e-6;
+    }*/
+
+    unsigned int n = 1000000;
+    double density = 2400;
+    double mass = 0;
+
+    for(unsigned int i=0;i<n;i++){
+        auto d = 2* distribution(gen)*1e-3;
+        mass += d*d*d/3.0*M_PI_2*density;
+    }
+
+    std::cout << "Mass for n: "<< n << " spheres  with density: " << density << " = " << mass << " kg"<< std::endl;
+}
+
+
+int main()
+{
+    calculateExpectedMass();
+    calculateExpectedMass();
+    calculateExpectedMass();
+    calculateExpectedMass();
+    calculateExpectedMass();
+    calculateExpectedMass();
+}
