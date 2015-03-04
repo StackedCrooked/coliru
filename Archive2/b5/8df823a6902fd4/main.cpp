e296a67d244c1d18d6901807f73e1ea6bc6b1491
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-04 15:35:53 +0000

    Update archive b5

---
+++ Archive2/b5/8df823a6902fd4/main.cpp
@@ -0,0 +1,27 @@
+// vector assign
+#include <iostream>
+#include <vector>
+
+int main ()
+{
+  std::vector<int> first;
+  std::vector<int> second;
+  std::vector<int> third;
+
+  first.assign (7,100);             // 7 ints with a value of 100
+
+  std::vector<int>::iterator it;
+  it=first.begin()+1;
+
+  second.assign (it,first.end()-1); // the 5 central values of first
+
+  int myints[] = {1776,7,4};
+  third.assign (myints,myints+3);   // assigning from array.
+
+  std::cout << "Size of first: " << int (first.size()) << '\n';
+  std::cout << "Size of second: " << int (second.size()) << '\n';
+  std::cout << "Size of third: " << int (third.size()) << '\n';
+    system("pause");
+  return 0;
+
+}
