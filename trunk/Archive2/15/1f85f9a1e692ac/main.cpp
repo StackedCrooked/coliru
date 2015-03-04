8a2282eeaa8ec4665c8e19305890d9efbffd0955
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-04 03:14:58 +0000

    Update archive 15

---
+++ trunk/Archive2/15/1f85f9a1e692ac/main.cpp
@@ -0,0 +1,55 @@
+#include <memory>
+#include <vector>
+#include <functional>
+#include <boost/pool/object_pool.hpp>
+
+#include <iostream>
+
+struct Foo {
+   Foo(int i) : _i(i) { std::cout << __PRETTY_FUNCTION__ << ' ' << _i << '\n'; }
+   static void* operator new(size_t) = delete;
+   static void* operator new(size_t count, void *p)
+   {
+       return ::operator new(count, p);
+   }
+   int _i;
+ };
+
+struct FooDeleter
+{
+    FooDeleter(boost::object_pool<Foo>& pool)
+    : pool(&pool)
+    {}
+    
+    void operator()(Foo *p)
+    {
+        pool->destroy(p);
+    }
+    boost::object_pool<Foo> *pool;
+};
+
+using FooHandle = std::unique_ptr<Foo, FooDeleter>;
+
+struct Bar {
+    FooHandle addFoo(int i) 
+    {
+      Foo* ptr = _fooPool.malloc();
+      if(ptr)
+      {
+          return FooHandle(::new (ptr) Foo(i), FooDeleter(_fooPool));
+      }
+      else
+      {
+          return FooHandle(nullptr, FooDeleter(_fooPool)); // or throw exception etc
+      }
+    }
+    boost::object_pool<Foo> _fooPool;
+};
+ 
+int main()
+{
+    Bar b;
+    b.addFoo(42);
+    
+    delete(::new Foo(10));
+}
