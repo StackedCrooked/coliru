094268aa0481765bece57daf07baabbeb9d4db52
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-05 18:45:38 +0000

    Update archive eb

---
+++ Archive2/eb/e475aa33a3ce4d/main.cpp
@@ -0,0 +1,30 @@
+#include <boost/regex.hpp>
+#include <boost/utility/string_ref.hpp>
+
+template <typename StringT, typename C = typename StringT::value_type>
+boost::basic_string_ref<C> GetFilename(StringT const& path) {
+
+    // it's quite tricky to initialze the pattern generically:
+    static auto re = [] {
+        char const pattern[] = "[^\\\\/:]*$";
+        std::basic_string<C> spattern(pattern, pattern + sizeof(pattern)-1);
+        return boost::basic_regex<C>(spattern, boost::regex_constants::optimize);
+    }();
+
+    // the rest is plain sailing:
+    boost::match_results<typename StringT::const_iterator> mr;
+    if (boost::regex_search(path.begin(), path.end(), mr, re) && mr[0].matched)
+        return { &*(mr[0].first), size_t(mr[0].length()) };
+    return path;
+}
+
+#include <iostream>
+
+int main() {
+
+    std::string fname = "/tmp/somedir/../other//./beautiful filenames.txt.0";
+    std::cout << GetFilename(fname) << "\n";
+
+    std::wstring wname = L"D:\\Program Files (64)\\Something Else Entirely\\but works.just-the-same.exe";
+    std::wcout << GetFilename(wname) << "\n";
+}
