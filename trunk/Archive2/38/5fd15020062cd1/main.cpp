8ce0060367ade3488d9fbdfbdef7c054f8948bf9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-05 18:20:39 +0000

    Update archive 38

---
+++ trunk/Archive2/38/5fd15020062cd1/main.cpp
@@ -0,0 +1,45 @@
+#include <type_traits>
+#include <vector>
+
+struct is_tbase_of_impl
+{
+    struct err {};
+
+    template <template <class...> class T>
+    static err test(...);
+
+    template <template <class...> class T, template <class...> class U, class... Args>
+    static T<Args...> test(U<Args...>*);
+};
+
+template <template <class...> class T, class U>
+using is_tbase_of
+    = typename std::is_same< decltype(is_tbase_of_impl::test<T>((U*)0)), U >::type;
+
+template <template <class...> class T>
+struct quote
+{
+    template <class... U>
+    using type = T<U...>;
+};
+
+using QVec = quote<std::vector>;
+
+template <class...> struct S {};
+template <class...> struct S2_ { using type=S2_; };
+template <class...Ts> using S2=typename S2_<Ts...>::type;
+
+template <> struct S2_<int> { using type=S<int>; };
+
+struct foo:S<int> {};
+
+
+static_assert( !is_tbase_of< std::vector, std::is_integral<int>  >::value, "" );
+static_assert(  is_tbase_of< std::vector, std::vector<int>       >::value, "" );
+static_assert(  is_tbase_of< QVec::type,  std::vector<int>       >::value, "" );
+static_assert( !is_tbase_of< std::vector, S<int, int, int>       >::value, "" );
+static_assert( !is_tbase_of< S2,          S<int>                 >::value, "S2 is not really a tbase of S<int>, but S2<int> does produce S<int>" );
+
+int main()
+{
+}
