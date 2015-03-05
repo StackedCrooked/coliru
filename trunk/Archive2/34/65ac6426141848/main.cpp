87039b6f110eda94e76c66ebd81f75e2ff1584f7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-05 00:00:30 +0000

    Update archive 34

---
+++ trunk/Archive2/34/65ac6426141848/main.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+string remove_duplicates(std::string str)
+{
+     // your code goes here
+     return ""; // replace with the right code
+}
+
+int main()
+{
+    cout << remove_duplicates("carrots");
+}
