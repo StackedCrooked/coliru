0c543a4be89d75d94f035ac34fa3ee4994afe144
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-06 18:07:54 +0000

    Update archive 1b

---
+++ trunk/Archive2/1b/9cf4d628698ee4/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+struct Base
+{
+
+    virtual const bool operator<(const Base & other) const= 0;
+    
+    virtual const bool operator>(const Base &other) const
+    {
+        return other < *this; // Pure Virtual function called!
+    }
+    virtual ~Base() {}
+
+};
+
+
+struct Derived : Base
+{
+    const bool operator<(const Base & other) const
+    {
+        const Derived& t = dynamic_cast<const Derived&>(other);
+        return t < *this;
+    }
+};
+
+
+int main()
+{
+    Derived z;
+    Base* h= new Derived();
+    delete h;
+}
