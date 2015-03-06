e67a11f515b4a15a7421ddde054afe696824b4a7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-06 04:20:14 +0000

    Update archive fc

---
+++ Archive2/fc/fee07d34fe0490/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <functional>
+
+struct Something {
+    Something() {
+        
+    }
+    
+    void setThing(
+        std::function<int()> a,
+    	std::function<int()> b,
+		std::function<int()> c
+    ) {
+        this->a = a;
+        this->b = b;
+        this->c = c;
+        
+    }
+    
+    int run() {
+        return this->a() + this->b() + this->c();   
+    };
+    
+    private:
+        std::function<int()> a;
+		std::function<int()> b;
+		std::function<int()> c;
+};
+
+int main()
+{
+    
+   Something myThing;
+   
+   myThing.run(
+        [&]() {
+        	return 1;
+        },
+        [&]() {
+            return 2;
+        },
+        [&]() {
+            return 3;
+        }
+    );
+}
