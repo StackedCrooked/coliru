0f20de2de8e429e83078a637cf09a6df86980286
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-06 01:02:50 +0000

    Update archive 89

---
+++ Archive2/89/b86bed83e5dbda/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+ 
+int main() 
+{
+    auto&& __range = expression;
+     for (auto __begin = begin-expression,
+               __end = end-expression;
+          __begin != __end;
+          ++__begin)
+     {
+       declaration = *__begin;
+       statement
+    }
+}
