de99e4a00a8ece47d01bae6be95669d6d1652cef
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-06 00:48:15 +0000

    Update archive 16

---
+++ trunk/Archive2/16/ac017e22aa1cda/main.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <boost/utility/string_ref.hpp>
+
+template <typename StringT, typename C = typename StringT::value_type>
+boost::basic_string_ref<C> GetFilename(StringT const& path) {
+    static const C separators[4] = { '\\', '/', ':', 0 };
+    return boost::basic_string_ref<C>(path).substr(path.find_last_of(separators) + 1);
+}
+
+#include <iostream>
+
+int main() {
+    for (std::string fname : {
+            "/tmp/somedir/../other//./beautiful filenames.txt.0",
+            "simple.txt",
+            "",
+        })
+    {
+        std::cout << "'" << fname << "' -> '" << GetFilename(fname) << "'\n";
+    }
+
+    for (std::wstring fname : {
+            L"D:\\Program Files (64)\\Something Else Entirely\\but works.just-the-same.exe",
+            L"\\UNCNAME\\Something Else Entirely\\network.dat",
+            L"D://testing/test123",
+            L"http://user:password@hostname:port/test123/resource?query=parameter#yo"
+            L"E:beautiful filenames.txt.0",
+            L"simple.txt",
+            L"",
+        })
+    {
+        std::wcout << "'" << fname << "' -> '" << GetFilename(fname) << "'\n";
+    }
+}
