c963cc777f1b9a4cf7d4b428ac498d2cd44bed9a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-06 05:58:00 +0000

    Update archive 01

---
+++ Archive2/01/5660099e486a80/main.cpp
@@ -0,0 +1,60 @@
+#include <utility>
+#include <memory>
+
+template<typename R, typename... Args>
+struct holder_base {
+    virtual ~holder_base() = default;
+    
+    virtual std::unique_ptr<holder_base> clone() const = 0;
+    virtual R call(Args... args) = 0;
+};
+
+template<typename Functor, typename R, typename... Args>
+struct holder: holder_base<R, Args...> {
+    explicit holder(Functor functor): functor(std::move(functor)) {}
+    
+private:
+    Functor functor;
+
+public:
+    R call(Args... args) override { return functor(std::forward<Args>(args)...); }
+    std::unique_ptr<holder_base<R, Args...>> clone() const override
+    { return std::unique_ptr<holder> { new holder(*this) }; }
+};
+
+template<typename Functor, typename... Args>
+struct holder<Functor, void, Args...>: holder_base<void, Args...> {
+    explicit holder(Functor functor): functor(std::move(functor)) {}
+    
+private:
+    Functor functor;
+
+public:
+    void call(Args... args) override { functor(std::forward<Args>(args)...); }
+    std::unique_ptr<holder_base<void, Args...>> clone() const override
+    { return std::unique_ptr<holder> { new holder(*this) }; }
+};
+
+template<typename R, typename... Args>
+struct function {
+    template<typename Functor>
+    function(Functor functor): functor(new holder<Functor, R, Args...>(std::move(functor))) {}
+    
+    function(function const& other): functor(other.functor.clone()) {}
+    function& operator=(function const& other) { functor = other.functor.clone(); return *this; }
+    
+    function(function&&) = default;
+    function& operator=(function&&) = default;
+    
+    R operator()(Args... args) { return functor->call(std::forward<Args>(args)...); }
+private:
+    std::unique_ptr<holder_base<R, Args...>> functor;
+};
+
+int main()
+{
+    int i = 0;
+    function<void, int> f = [&i](int j) { return i = j; };
+    f(3);
+    return i;
+}
