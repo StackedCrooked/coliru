3447520d3a5a287cd4e1ab80bd6e70d7b4b8c8a0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-06 18:55:06 +0000

    Update archive d5

---
+++ Archive2/d5/7b9381958ea530/main.cpp
@@ -0,0 +1,29 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+namespace {
+void one_pass(std::istream& is) {
+  int num = 0;
+  std::string lin;
+
+  while (std::getline(is, lin)) {
+    ++num;
+    std::cout << '#' << num << ':' << lin << '\n';
+  }
+}
+} // unnamed namespace
+
+int main(int argc, char**argv)
+{
+  std::ifstream inp{argc > 1 ? argv[1] : __FILE__};
+
+  std::cout << "first pass\n";
+  one_pass(inp);
+
+  inp.clear();
+  inp.seekg(0);
+
+  std::cout << "\nsecond pass\n";
+  one_pass(inp);
+}
