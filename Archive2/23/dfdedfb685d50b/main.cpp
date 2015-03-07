b2fa1b76daa2685b8ffbe18fa3272e233f009c9f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-07 03:13:59 +0000

    Update archive 23

---
+++ Archive2/23/dfdedfb685d50b/main.cpp
@@ -0,0 +1,45 @@
+float x_coord, y_coord;
+float speed;
+color col;
+
+boolean isExist = false;
+
+void setup()
+{
+  size(400, 200);
+  noStroke();
+}
+
+void draw()
+{
+  final float accel = 0.5;
+  final int radius = 10;
+
+  background(255);
+  
+  if (isExist)
+  {
+    fill(col);
+    ellipse(x_coord, y_coord, radius * 2, radius * 2);
+
+    if (y_coord + speed < height - radius)
+    {
+      y_coord += speed;
+      speed += accel;
+    }
+    else
+    {
+      y_coord = height - radius;
+      speed *= -0.5;
+    }
+  }
+}
+
+void mouseClicked()
+{
+  x_coord = mouseX;
+  y_coord = mouseY;
+  speed = 0;
+  col = int(random(0, 0xffffff)) | 0xff000000;
+  isExist = true;
+}
