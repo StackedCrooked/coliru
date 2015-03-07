97132405e33e59c81f5f69261b3ecf2c2062187d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-07 00:22:57 +0000

    Update archive 45

---
+++ trunk/Archive2/45/a8b340b9cd9135/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+int main() {
+    using namespace std;
+    ostreambuf_iterator<char> out(cout);
+    for(istreambuf_iterator<char> f(cin), l; f!=l; ++f)
+        switch(*f) {
+            case '*': return 0;
+            case 'a': case 'e': case 'i': case 'o': case 'u': continue; 
+            default: *out++ = *f;
+        }
+}
