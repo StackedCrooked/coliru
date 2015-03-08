8330b700ffeb882c50a8cff272f2c67a019bedd4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-08 21:21:03 +0000

    Update archive f3

---
+++ Archive2/f3/a109a7fa225cc3/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+std::ostream& operator<<(std::ostream& o, const wchar_t* s) {
+    while (auto c = *s++) o.put(c);
+    return o;
+}
+
+int main() {
+    std::cout << L"test\n";
+}
