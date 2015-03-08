6673ecd432a4bc0ada37f5213f05e8867b6ecc60
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-08 18:49:17 +0000

    Update archive e1

---
+++ trunk/Archive2/e1/cbe7e73bcee449/cmd.sh
@@ -0,0 +1,3 @@
+set -x; #g++ -std=c++1y -O0 -Wall -pedantic -pthread main.cpp  -fsanitize=undefined && ./a.out
+#clang++ -stdlib=libc++ -std=c++1y -O0 -Wall -pedantic -pthread main.cpp -fsanitize=undefined -lcxxrt -ldl && ./a.out
+clang++ --version && clang++ -std=c++11 -O0 -Wall -pedantic -pthread main.cpp  -fsanitize=undefined && ./a.out
---
+++ trunk/Archive2/e1/cbe7e73bcee449/main.cpp
@@ -0,0 +1,5 @@
+#include <array>
+int main(){
+    std::array<int, 0> foo;
+    foo.begin(); // or end(), etc.
+}
