a5d9a6072ca7699ee49e49f9e247b5cf3aa931d1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-08 09:03:23 +0000

    Update archive 53

---
+++ Archive2/53/2b75a751cf937c/cmd.sh
@@ -0,0 +1,3 @@
+set -x; g++ -std=c++1y -O2 -Wall -pedantic -pthread main.cpp && ./a.out
+clang++ -stdlib=libc++ -std=c++1y -O2 -Wall -pedantic -pthread main.cpp -lcxxrt -ldl && ./a.out
+clang++ -std=c++1y -O2 -Wall -pedantic -pthread main.cpp -lcxxrt -ldl && ./a.out
