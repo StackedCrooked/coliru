7f44f80d951fe2cc6644ee3ddfcbba465180e4f4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-09 01:06:42 +0000

    Update archive 98

---
+++ trunk/Archive2/98/181eeb071a0caa/main.cpp
@@ -0,0 +1,23 @@
+#include <boost/container/flat_map.hpp>
+#include <iostream>
+
+using Elements = boost::container::flat_map<std::string, std::string>;
+
+int main() {
+    Elements map;
+    map.reserve(256); // pre-allocate 256 "nodes"!
+
+    map.insert({
+            { "one",   "Eins"  },
+            { "two",   "Zwei"  },
+            { "three", "Drei"  },
+            { "four",  "Vier"  },
+            { "five",  "Fuenf" },
+        });
+
+    for (auto& e : map) {
+        std::cout << "Hash entry: " << e.first << " -> " << e.second << "\n";
+    }
+
+    std::cout << "map[\"three\"] -> " << map.find("three")->second << "\n";
+}
