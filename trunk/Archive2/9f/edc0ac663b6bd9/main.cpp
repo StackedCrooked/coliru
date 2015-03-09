172f2f06e7ebf600be3d6970b78638f389ff07c8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-09 23:43:53 +0000

    Update archive 9f

---
+++ trunk/Archive2/9f/edc0ac663b6bd9/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+int main()
+{
+    std::vector<std::string> words = {
+        "Hello", "from", "GCC", __VERSION__, "!"
+    };
+    double toto = 324234 / 123.232;
+    std::cout << words << toto << std::endl;
+    return 0;
+}
