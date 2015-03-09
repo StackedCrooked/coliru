4510fc94e8882aca20a4e3fd00f0639b29a0d2d1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-09 06:30:19 +0000

    Update archive f5

---
+++ Archive2/f5/5971cc3791ae52/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{   
+    bool tf = true;
+    char a = 'a';
+    short int b = 24;
+    int c = 30000;
+    long int d = 123000;
+    long long int e = 9999999999;
+    float f = 9.3;
+    double g = 2.3e-24;
+    
+    std::cout << tf << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+    std::cout << d << std::endl;
+    std::cout << e << std::endl;
+    std::cout << f << std::endl;
+    std::cout << g << std::endl;
+    
+    return 0;
+}
