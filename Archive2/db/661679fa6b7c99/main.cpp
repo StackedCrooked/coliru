d4756aa4179c9187364ecf80c830d85f3db429ff
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-09 00:47:31 +0000

    Update archive db

---
+++ Archive2/db/661679fa6b7c99/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv) {
+    float a;
+    float b;
+    cin >> a,b;
+    cin.ignore();
+    cout << "a= "<< a << " b=" << b;    
+    return 0;
+}
