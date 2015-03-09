b73b7063b54ab9fefb4b7a7a9017b198168d1c25
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-09 18:12:11 +0000

    Update archive 09

---
+++ trunk/Archive2/09/bc0dad95627aca/main.cpp
@@ -0,0 +1,12 @@
+//loop com FOR
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main (void){
+int x;
+for (x=1; x<=100; x++)
+/* inicialização: x começa em 1
+critério: executar enquanto x for menor ou igual a 100
+incremento (++) será somado 1 a x */
+cout << x <<"\t";    system ("pause");
+}
