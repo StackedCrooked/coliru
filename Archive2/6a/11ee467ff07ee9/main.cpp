85f27caa74713316ee2aa9f57fa5b3d51f1a8959
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-09 06:05:35 +0000

    Update archive 6a

---
+++ Archive2/6a/11ee467ff07ee9/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    unsigned int i = 1;
+    
+    while (i != 0)
+    {
+    std::cout << i;
+    i++;
+    
+    }
+    
+    std::cout << "Oops." << std::endl;
+}
