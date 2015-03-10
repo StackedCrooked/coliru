eb48ded394086a404eb0b3d80aeb62e2db0e2a05
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-10 03:06:00 +0000

    Update archive 94

---
+++ trunk/Archive2/94/03215ffa710e6a/main.cpp
@@ -0,0 +1,23 @@
+template <typename T>
+struct Base {
+    T value;
+    
+    Base() {}
+    
+    template <typename... Args>
+    void var(Args... args) {}
+};
+
+template <typename F>
+struct Derived : public Base<F> {
+    Derived() {
+        Base<F>::var<int,int,int>(1,2,3);
+    }
+};
+
+int main()
+{
+    Derived<int> d;
+
+    return 0;  
+}
