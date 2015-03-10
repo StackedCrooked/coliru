6f304eae2ab53884f695b76019ed43a98c8f0607
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-10 20:19:43 +0000

    Update archive fe

---
+++ trunk/Archive2/fe/1c84ad0f75ed37/main.cpp
@@ -0,0 +1,15 @@
+    void main ()
+{
+    float slb, desconto, sll
+    
+    printf("Digite o slb ");
+    scanf("%f",&slb);
+    
+    printf("Digite o desconto ");
+    scanf("%f",&desconto);
+    
+    sll = slb * (desconto)/100
+    
+    printf("o %f e resultado da mult. do %f pelo %f dividido por 100", sll, slb, desconto);
+    
+}
