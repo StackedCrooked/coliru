42b074392dc0e50f7ab3f1ef0724d2a89dd3fc45
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-10 21:32:47 +0000

    Update archive a4

---
+++ trunk/Archive2/a4/e4735b00037f78/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int umaVariavel=0;
+    cout << "Ola!"<< endl;
+    cout << "Informe um valor: ";
+    cin >> umaVariavel;
+    cout << "Valor informado: "<< umaVariavel<<endl;
+    return 0;
+}
