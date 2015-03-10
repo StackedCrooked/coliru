33b94553f1eb83843baa854d9acdb71f5a7af173
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-10 15:03:12 +0000

    Update archive 11

---
+++ trunk/Archive2/11/c933d3dc2f42b8/main.cpp
@@ -0,0 +1,44 @@
+#include <cmath>
+#include <iostream>   
+#include <cstdlib>
+#include <time.h>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+struct CatIdentifier{
+    std::string id;
+    size_t start;
+};
+
+int main()
+{
+#if 1
+
+    std::vector<CatIdentifier> vec;
+    CatIdentifier id{"ciao", 5u};
+    
+    if(std::end(vec) == std::find(std::begin(vec), std::end(vec), id)){
+        std::cout << "not there" << std::endl;
+    }
+    else{
+        std::cout << "there" << std::endl;
+    }
+    
+#else
+
+    auto id1 = CatIdentifier{"ciao", 5u};
+    auto id2 = CatIdentifier{"ciao", 5u};
+    
+    if(id1 == id2){
+        std::cout << "not there" << std::endl;
+    }
+    else{
+        std::cout << "there" << std::endl;
+    }
+    
+#endif
+    
+    
+    return 0;
+}
