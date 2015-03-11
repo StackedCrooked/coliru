4e6c36ef87baac64926e352d6e5f22b32428a136
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-11 05:12:19 +0000

    Update archive 74

---
+++ trunk/Archive2/74/55c67000572f5f/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
+#include <boost/assign/list_of.hpp>
+
+class CommsInterface {
+    public:
+    typedef std::vector<unsigned char> bytes;
+    typedef boost::function<int (const bytes&)> CommsReceiverFn;
+    void Write(const bytes& data)
+    {
+        _fn(data);
+    }
+    void SetReceiver( CommsReceiverFn f ){
+        if(!f){
+            std::cout<<"Cannot set to null"<<std::endl;   
+        } else{
+        std::cout<<"Setting receiver to "<<f<<std::endl;
+        _fn = f;
+        }
+    }
+    private:
+    CommsReceiverFn _fn;
+};
+
+class RDPParser {
+    public:
+    ~RDPParser(){
+        std::cout<<"destroying parser"<<std::endl;
+    }
+    int ProcessInput(const CommsInterface::bytes& buffer){
+        for(size_t i=0; i!=buffer.size();i++){
+            std::cout<<"got "<<buffer[i]<<std::endl;
+        }
+        return 0;
+    }
+};
+
+int devNULL(const CommsInterface::bytes&){
+       std::cout<<"noop stub"<<std::endl;
+        return 0; 
+}
+
+class RDP {
+    public:
+    RDP(CommsInterface &ci) : _ci(ci){}
+    ~RDP(){
+        std::cout<<"destroying RDP"<<std::endl;
+        _ci.SetReceiver(boost::bind(&devNULL,_1));
+        delete _p;
+    }
+    RDPParser* MakeParser(){
+        _p = new RDPParser();
+        _ci.SetReceiver(boost::bind(&RDPParser::ProcessInput,_p,_1));
+        return _p;
+    }
+    private:
+    CommsInterface &_ci;
+    RDPParser* _p;
+};
+
+
+int main()
+{
+    const CommsInterface::bytes buffer = boost::assign::list_of<unsigned char>('1')('2')('3')('4')('5');
+    CommsInterface ci;
+    {
+        RDP r(ci);
+        r.MakeParser();
+        ci.Write(buffer);
+    }
+    CommsInterface::CommsReceiverFn f;
+    ci.SetReceiver(f);
+    ci.Write(buffer);
+    return 0;
+}
