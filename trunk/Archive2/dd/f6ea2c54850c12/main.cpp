3bc37b3d0413277285069ad13d80193f40f929fb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-11 11:49:26 +0000

    Update archive dd

---
+++ trunk/Archive2/dd/f6ea2c54850c12/main.cpp
@@ -0,0 +1,26 @@
+#include <map>
+#include <string>
+#include <vector>
+
+template<typename T>
+class EventDispatcher {
+    public:
+        typedef void (*EventHandler)(T event);
+
+        EventDispatcher() { }
+        ~EventDispatcher() { }
+
+        void addListener(const std::string eventName, EventHandler handler) { }
+        void fireEvent(T event) {}
+
+    private:
+        typedef std::vector<EventHandler> ListenersList;
+        typedef std::map<std::string, ListenersList*> ListenersMap;
+
+        ListenersMap listeners;
+};
+
+int main()
+{
+    EventDispatcher<int> e;
+}
