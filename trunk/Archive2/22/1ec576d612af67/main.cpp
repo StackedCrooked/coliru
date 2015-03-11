93c2d6cb5b94f9ec212a1499106f7dd83ca4aa38
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-11 16:13:40 +0000

    Update archive 22

---
+++ trunk/Archive2/22/1ec576d612af67/main.cpp
@@ -0,0 +1,17 @@
+// Modifier le code suivant pour afficher les messages demandés
+
+#include <iostream>
+
+int main() {
+    // Modifier la ligne suivante pour afficher "Bienvenue tout le monde !"
+    std::cout << "Bienvenue tout le monde !" << std::endl;
+    
+    // Ajouter UNE ligne pour afficher "Bienvenue !" et "Tout le monde !" sur DEUX lignes
+    std::cout << "Bienvenue !" << std::endl << "Tout le monde !" << std::endl;
+    
+    // Ajouter DEUX lignes pour afficher "Bienvenue !" et "Tout le monde !" sur UNE ligne
+    std::cout << "Bienvenue !" << std::endl;
+    std::cout << "Tout le monde !" << std::endl;
+    
+    return 0;
+}
