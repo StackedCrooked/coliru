7d1b19ecd1ef6c6a73e0a2b1049f7d4c881a437a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-12 01:29:14 +0000

    Update archive d3

---
+++ Archive2/d3/b4039ee4d206c8/cmd.sh
@@ -0,0 +1,1 @@
+g++ -std=c++11 -m64 -Wall -pedantic main.cpp && ./a.out
---
+++ Archive2/d3/b4039ee4d206c8/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+auto components_of(int value) -> std::vector<int> {
+    auto result = std::vector<int>();
+    
+    for (auto addend = 1; value != 0; addend <<= 1, value >>= 1) {
+        if (value & 1) {
+            result.push_back(addend);
+        }
+    }
+    
+    return result;
+}
+
+int main() {
+    auto numbers = components_of(5);
+    
+    for (auto&& i : numbers) {
+        std::cout << i << " ";
+    }
+}
