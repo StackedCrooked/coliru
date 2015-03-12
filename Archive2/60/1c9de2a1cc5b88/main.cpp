45cea21f8117c520a9b4794a7782404a39c4e55d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-12 19:15:18 +0000

    Update archive 60

---
+++ Archive2/60/1c9de2a1cc5b88/main.cpp
@@ -0,0 +1,26 @@
+struct SignalBase {};
+
+struct SignalCopyingBase : public SignalBase
+{
+    SignalCopyingBase(SignalBase const& s) : SignalBase(s)
+    {}
+    
+    SignalCopyingBase() = default;
+};
+
+struct Signal1 : public SignalCopyingBase
+{
+    using SignalCopyingBase::SignalCopyingBase;
+};
+
+struct Signal2 : public SignalCopyingBase
+{
+    using SignalCopyingBase::SignalCopyingBase;
+};
+
+
+int main()
+{ 
+    Signal1 s1;
+    Signal2 s2(s1);
+}
