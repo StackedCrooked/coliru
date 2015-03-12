d6b6d582a89c64af6401771f5808cca6c7388806
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-12 13:05:24 +0000

    Update archive ef

---
+++ trunk/Archive2/ef/9a3a7916221d92/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+
+class Base
+{
+public:
+  virtual void shout() = 0;
+};
+class Child : public Base
+{
+public:
+  void shout() { std::cout << "Child!\n"; }
+};
+class Orphan : public Base
+{
+public:
+  void shout() { std::cout << "Orphan!\n"; }
+};
+
+std::unique_ptr<Base> CreateStuff()
+{
+    return std::make_unique<Child>();
+}
+
+std::vector<std::unique_ptr<Base>> CreateListOfStuff()
+{
+    std::vector<std::unique_ptr<Base>> result;
+    result.reserve(3);
+    for(int i = 0; i < 3; i++)
+    {
+        result.emplace_back(CreateStuff());
+    }
+    return result;
+}
+
+int main()
+{
+  const std::vector<std::unique_ptr<Base>> the_list = CreateListOfStuff();
+  
+  for(const auto& item : the_list)
+  {
+    item->shout(); // remember, items are pointers!
+  }
+}
