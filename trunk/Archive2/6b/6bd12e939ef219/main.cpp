3b5a1fd218527ee0208a1cd4a434d7bb43f634e2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-12 18:46:48 +0000

    Update archive 6b

---
+++ trunk/Archive2/6b/6bd12e939ef219/main.cpp
@@ -0,0 +1,54 @@
+#include <type_traits>
+#include <iostream>
+
+template<class...>
+struct voider { using type = void; };
+template<class...Ts>
+using void_t = typename voider<Ts...>::type;
+
+template <class T, class U, class BinaryOperation, class Enable = void>
+struct is_binop_able_ : std::false_type {};
+template <class T, class U, class BinOp>
+struct is_binop_able_<T, U, BinOp, void_t<
+  decltype(std::declval<BinOp>()(
+    std::declval<T>(), std::declval<U>()))>> :
+  std::true_type {};
+  
+template <class T, class U, class BinOp>
+using is_binop_able = typeame is_binop_able_::type;
+
+#define RETURNS(...) \
+  noexcept(noexcept(__VA_ARGS__)) \
+    ->decltype(__VA_ARGS__) { \
+      return (__VA_ARGS__); \
+  }
+
+struct plus {
+  template <class T, class U>
+  auto operator()(T t, U u) RETURNS(t + u)
+};
+template<class T, class U = T>
+using is_addable = is_binop_able<T, U, plus>;
+
+struct multiplies {
+  template <class T, class U>
+  auto operator()(T t, U u) RETURNS(t * u)
+};
+template<class T, class U = T>
+using is_multiplicable = is_binop_able<T, U, multiplies>;
+
+#undef RETURNS
+
+int main()
+{
+  std::cout <<"is_addable\n";
+  std::cout <<" long:   "<< is_addable<long>::value        <<'\n';
+  std::cout <<" string: "<< is_addable<std::string>::value <<'\n';
+  std::cout <<" void:   "<< is_addable<void>::value        <<'\n';
+  std::cout <<" int*, int: "<< is_addable<int*, int>::value        <<'\n';
+
+  std::cout <<"is_multiplicable\n";
+  std::cout <<" long:   "<< is_multiplicable<long>::value        <<'\n';
+  std::cout <<" string: "<< is_multiplicable<std::string>::value <<'\n';
+  std::cout <<" void:   "<< is_multiplicable<void>::value        <<'\n';
+}
