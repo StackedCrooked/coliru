1edb34815153dab9d4bf4d27e2e62c89797d415b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-12 16:48:40 +0000

    Update archive 6a

---
+++ trunk/Archive2/6a/4db5f2584c11db/cmd.sh
@@ -0,0 +1,4 @@
+echo "clang++ -std=c++11 -Wall -pedantic -Wextra -Wfatal-errors main.cpp && ./a.out"
+clang++ -std=c++11 -Wall -pedantic -Wextra -Wfatal-errors main.cpp && ./a.out;
+echo "g++ -std=c++11 -Wall -pedantic -Wextra -Wfatal-errors main.cpp && ./a.out";
+g++ -std=c++11 -Wall -pedantic -Wextra -Wfatal-errors main.cpp && ./a.out;
