a79cfd114563090eb229136dc1a889382695ba8e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-13 19:12:40 +0000

    Update archive ed

---
+++ Archive2/ed/344ddcef2af0dd/main.cpp
@@ -0,0 +1,12 @@
+type Title = String
+type Year = Int
+type Cast = [String]
+type Fan = [String]
+
+type Film = (Title, Cast, Year, Fan)
+    
+filmToString :: Film -> String
+filmToString (title, cast, year, fan) = printf "%s %s %d\n %s" title cast year fan
+
+main :: IO ()
+main = putStrLn $ filmToString ("A", 1992, [], [])
