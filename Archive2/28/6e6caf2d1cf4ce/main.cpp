647e12b8cadfb7e333d1aebd63fb5264da2f28cf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-13 02:02:45 +0000

    Update archive 28

---
+++ Archive2/28/6e6caf2d1cf4ce/main.cpp
@@ -0,0 +1,32 @@
+// class-hierarchy.hpp
+#include <iostream>
+
+struct english {
+    struct substantives {
+        struct House;
+        struct Horse;
+    };
+};
+
+// Optionally; to avoid long qualified names.
+using House = english::substantives::House;
+using Horse = english::substantives::Horse;
+
+template<typename WordTag>
+struct WordTraits;
+
+template<> struct WordTraits<House>
+{ static constexpr char const* word = "House"; };
+
+template<> struct WordTraits<Horse>
+{ static constexpr char const* word = "Horse"; };
+
+template<typename TagWord>
+constexpr char const* getString()
+{ return WordTraits<TagWord>::word; }
+
+int main()
+{
+    std::cout << getString<Horse>() << std::endl;
+    std::cout << getString<House>() << std::endl;
+}
