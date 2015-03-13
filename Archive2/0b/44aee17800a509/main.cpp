144eb4630a4e2133103afd118b853ccfb8bc33a4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-13 14:08:06 +0000

    Update archive 0b

---
+++ Archive2/0b/44aee17800a509/main.cpp
@@ -0,0 +1,29 @@
+#include <memory>
+#include <functional>
+#include <cassert>
+
+template<typename Func, typename Item>
+std::function<bool()> foo(Func func, std::shared_ptr<Item> item)
+{
+    std::weak_ptr<Item> w = item;
+    return [w, func]
+    {
+        if(auto i = w.lock()) {
+            // ideally:
+            // std::invoke(func, i);
+            func(*i);
+            return true;
+        } else {
+            return false;
+        }
+    };
+}
+
+int main()
+{
+    auto p = std::make_shared<int>();
+    assert( foo([](int) {}, p)() );
+    assert( foo([](int) {}, p)() );
+    p.reset();
+    assert( !foo([](int) {}, p)() );
+}
