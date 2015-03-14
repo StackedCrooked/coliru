28ed91aca2d589d7acf6fd4abd94a5b11f80a170
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-14 04:58:48 +0000

    Update archive 85

---
+++ trunk/Archive2/85/84089110de52b5/main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+
+template<class T>
+string type_str();
+
+template<>
+string type_str<int>() { return "int"; }
+
+template<>
+string type_str<char>() { return "char"; }
+
+string arg_types_str()
+{
+    return "";
+}
+
+template<class T>
+string arg_types_str()
+{
+    return type_str<T>();
+}
+
+template<class T, class U, class... Args>
+string arg_types_str()
+{
+    return type_str<T>() + ", " + arg_types_str<U, Args...>();
+}
+
+template<class R, class... Args>
+void print_fptr_types(R (*fptr)(Args...))
+{
+    std::cout << type_str<R>() << " (*)(" << arg_types_str<Args...>() << ")" << std::endl;
+}
+
+template<class T, class R, class... Args>
+void print_cmemfptr_types(R (T::*memfptr)(Args...))
+{
+    std::cout << type_str<R>() << " (const " << type_str<T>() << "*)(" << arg_types_str<Args...>() << ")" << std::endl;
+}
+
+template<class T, class R, class... Args>
+void print_memfptr_types(R (T::*memfptr)(Args...))
+{
+    std::cout << type_str<R>() << " (" << type_str<T>() << "*)(" << arg_types_str<Args...>() << ")" << std::endl;
+}
+
+template<class T>
+void print_fnc_obj_types(const T&)
+{
+    print_cmemfptr_types(&T::operator());
+}
+
+template<class T>
+void print_fnc_obj_types(T&)
+{
+    print_memfptr_types(&T::operator());
+}
+
+struct FncObj
+{
+    FncObj() {}
+    int operator()(int, char)
+    {
+        return 0;
+    }
+};
+
+template<>
+string type_str<FncObj>() { return "FncObj"; }
+
+int main()
+{
+    int (*fptr)(char, int);
+    print_fptr_types(fptr);
+
+    FncObj fnc_obj1;
+    print_fnc_obj_types(fnc_obj1);
+    const FncObj fnc_obj2;
+    print_fnc_obj_types(fnc_obj2);
+}
