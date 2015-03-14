29987cce4e71c72280b8810b3ba8572322267e89
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-14 10:35:23 +0000

    Update archive 07

---
+++ Archive2/07/608cbce574c490/cmd.sh
@@ -0,0 +1,4 @@
+set -x; g++ -std=c++1y -O0 -Wall -pedantic -Wextra main.cpp && ./a.out && stat ./a.out
+g++ -std=c++1y -O0 -DSECOND -Wall -pedantic -Wextra main.cpp && ./a.out && stat ./a.out
+g++ -std=c++1y -O3 -Wall -pedantic -Wextra main.cpp && ./a.out && stat ./a.out
+g++ -std=c++1y -O3 -DSECOND -Wall -pedantic -Wextra main.cpp && ./a.out && stat ./a.out
