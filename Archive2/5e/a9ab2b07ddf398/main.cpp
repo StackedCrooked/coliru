0746a4169c17fdda4735252a02042cc2ac7e5b03
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-16 17:05:31 +0000

    Update archive 5e

---
+++ Archive2/5e/a9ab2b07ddf398/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class GiganticDatabase { // Gigantic database - several GBs
+public:
+    GiganticDatabase(const GiganticDatabase&) {} // Initialize GiganticDatabase from another object
+
+    GiganticDatabase (char * file) {} // Initialize GiganticDatabase from file
+ };
+
+class Registry {
+public:    
+   GiganticDatabase graph;
+
+   Registry(char* filename) : graph(*new GiganticDatabase(filename)) // <-- Leaks several GBs FTW
+   {}
+};
+
+int main() {
+   char file[] = "C:\\whatever.db";
+   Registry stupidObject(file);
+}
