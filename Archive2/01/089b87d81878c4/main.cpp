83e60fbe75184087084b68a292885fbe4b43a8a9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-16 15:58:16 +0000

    Update archive 01

---
+++ Archive2/01/089b87d81878c4/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+template <template<typename...> class OutputContainerType, template<typename...> class ContainerType, typename ValueType>
+static OutputContainerType<ValueType> processContainer(ContainerType<ValueType> c)
+{
+    OutputContainerType<ValueType> result(c.begin(), c.end());
+    return result;
+}
+
+int main() {
+    std::vector<int> v;
+    std::set<int> s {1, 2, 3};
+    v = processContainer<std::vector, std::set, int>(s);
+    std::cout << v.size() << std::endl;
+}
