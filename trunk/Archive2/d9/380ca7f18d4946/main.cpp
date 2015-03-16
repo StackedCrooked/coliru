69fb435ef36b5c472755e09f2acba8dfe9a9e24d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-16 08:04:47 +0000

    Update archive d9

---
+++ trunk/Archive2/d9/380ca7f18d4946/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream> 
+#include <utility>
+
+template <class T>
+class Container
+{
+private:
+    T* ptr;
+    
+public:
+    //Constructor
+    Container(T&& value)
+    {
+        ptr = &value;   
+    }
+    
+    //Destructor
+    ~Container()
+    {
+        delete ptr;   
+    }
+    
+    void print()
+    {
+        std::cout << *ptr << " " << std::endl;
+    }
+};
+
+int main()
+{
+    Container<int> jar1(3);
+    Container<int> jar2(4);
+    Container<int> binks(5);
+    
+    std::cout << "Jar-Jar Binks is: ";
+    jar1.print();
+    jar2.print();
+    binks.print();
+    std::cout << std::endl;
+    
+    return 0;
+}
+    
+    
