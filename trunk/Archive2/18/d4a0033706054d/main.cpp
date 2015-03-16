035e667764fa145d73812dec827fcfc4122e7d50
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-16 02:18:13 +0000

    Update archive 18

---
+++ trunk/Archive2/18/d4a0033706054d/main.cpp
@@ -0,0 +1,14 @@
+    void sort_list(node*& header)
+    {
+        node *newList;
+        node *curSourceHead = header;
+
+        //while items are still in the list continue to sort, why doesn't this work?
+        while (list_length(curSourceHead) > 0 )
+        {
+            node::value_type theData = curSourceHead->data();
+            newList = list_add_sorted(newList, theData);  //
+            list_head_remove(curSourceHead);
+        }
+        header = newList;
+    }
