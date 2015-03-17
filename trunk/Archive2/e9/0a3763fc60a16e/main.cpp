8c8d1276fa8ca9e52426a60ffc99565270566e98
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-17 10:36:13 +0000

    Update archive e9

---
+++ trunk/Archive2/e9/0a3763fc60a16e/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+class YM2420Range {
+    uint8_t address;
+    uint8_t first_bit;
+    uint8_t range;
+    bool inverted;
+  public:
+    YM2420Range(
+        uint8_t a, uint8_t b, uint8_t c, bool d): address(a), first_bit(b),
+        range(c), inverted(d) {}
+    uint8_t get_range() {
+      return range;
+    }
+};
+
+YM2420Range multi_sample_wave_carrier    (0x00, 0, 15, false);
+
+int main()
+{
+    std::vector<std::string> words = {
+        "Hello", "from", "GCC", __VERSION__, "!"
+    };
+    std::cout << +multi_sample_wave_carrier.get_range() << std::endl;
+}
