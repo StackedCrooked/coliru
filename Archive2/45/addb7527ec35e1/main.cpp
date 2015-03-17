122d9b270013333ec0f665f1acf07f147a124b2b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-17 23:36:26 +0000

    Update archive 45

---
+++ Archive2/45/addb7527ec35e1/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+namespace TextSearch {
+class Node;
+
+class LookupTable {
+    public:
+        LookupTable ();
+        ~LookupTable ();
+        
+        void add (const char*);
+        bool isExist (const char*) const;
+    private:
+        Node* root_;
+};
+
+
+class Node {
+    public:
+        Node ();
+        Node& child (char);
+        bool isLeaf (char) const;
+        Node& childAdd (char);
+    private:
+        static const size_t size_ = ('z' - 'a' + 1)*2;
+        Node* edges_ [size_];
+};
+
+LookupTable::LookupTable (): root_ (new Node ()) {
+}
+
+LookupTable::~LookupTable () {
+    delete root_;
+}
+
+void LookupTable::add (const char* str) {
+    if (!str) { 
+        return;
+    }
+    
+    Node& current = *root_;
+    while ('\0' != *str) {
+        current = current.childAdd (*str++);    
+    }
+}
+
+bool LookupTable::isExist (const char* str) const {
+    if (!str) { 
+        return false;
+    }
+    
+    Node& current = *root_;
+    while (
+    
+    
+    return false;
+}
+
+} //namespace TextSearch
+
+int main()
+{
+  
+    std::cout << "" << std::endl;
+}
