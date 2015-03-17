a03a005f7be964377ad98bb444b2a702e461c57d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-17 21:55:04 +0000

    Update archive 8f

---
+++ trunk/Archive2/8f/c30ce0a5195c7e/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    
+    cout << "Inserisci A: ";
+    cin >> a;
+   
+    cout << "Valore di A: " << a;
+   
+    system("PAUSE");
+    return 0;
+}
