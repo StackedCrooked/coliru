b009fe06e42ee4dfd2042efa63c7f3ede49397c7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-17 22:40:01 +0000

    Update archive e9

---
+++ Archive2/e9/d3a1f12e5b10c2/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main(void)
+{
+    FILE *fp = fopen("data.txt","r");
+    if (fp == NULL)
+    {
+       fprintf(stderr,"fopen() failed in file %s at line # %d", __FILE__,__LINE__);
+       exit(EXIT_FAILURE);
+    }
+ 
+    /* Normal processing continues here. */
+    fclose(fp);
+    printf("Normal Return\n");
+ 
+    return EXIT_SUCCESS;
+}
