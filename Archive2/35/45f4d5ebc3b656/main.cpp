a8ef61bc8654d7473cded02e5f2993705d0defcc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-18 01:23:10 +0000

    Update archive 35

---
+++ Archive2/35/45f4d5ebc3b656/main.cpp
@@ -0,0 +1,83 @@
+
+#include <functional>
+#include <memory>
+
+// Old version that works, but does not take parameters in the Call function
+
+class Callback{
+private:
+	std::function<bool()> func_;
+private:
+	Callback(std::function<bool()> func){
+		func_ = func;
+	}
+public:
+	template<class Func, class Type, class... Args>
+	static Callback* Create(Func f, std::shared_ptr<Type> s_ptr, Args&&... args){
+		std::weak_ptr<Type> w_ptr = s_ptr;
+		auto bound = std::bind(f, s_ptr.get(), std::forward<Args>(args)...);
+		auto func = 
+            [w_ptr, bound]() mutable {   // added mutable
+			if (auto ptr = w_ptr.lock()){
+				bound();
+				return true;
+			}
+			else return false;
+		};
+		return new Callback{ func };
+	}
+	bool Call(){
+		return func_();
+	}
+};
+    
+    
+// New version that does not work, because it tries to accept parameters in the Call function
+    
+template <typename... Args>
+class A{
+private:
+    std::function<bool(Args&&...)> func_;
+private:
+	A(std::function<bool(Args&&...)> func)
+		: func_{ func }
+	{
+
+	}
+public:
+	
+	template<class Func, class Type, class... FArgs>
+	static A<FArgs...>* Create(Func f, std::shared_ptr<Type> s_ptr, FArgs&&... args){
+		std::weak_ptr<Type> w_ptr = s_ptr;
+		auto bound = std::bind(f, s_ptr.get(), std::forward<FArgs>(args)...);
+		return new A{
+			[w_ptr, bound](FArgs&&... as) mutable { // package
+				if (auto ptr = w_ptr.lock()){
+					bound(as...); // package
+					return true;
+				}
+				else return false;
+			}
+		};
+	}
+	bool Call(Args&&... args){
+		return func_(std::forward<Args>(args)...);
+	}
+};
+
+class B{
+private:
+    int taco_ = 0;
+public:
+	int Increment(int i){
+		return taco_ += i;
+	}
+};
+ 
+int main(){
+    
+    auto a = A<std::_Ph<1>&>::Create(&B::Increment, std::make_shared<B>(), std::placeholders::_1);
+    
+    return 0;
+}
+ 
