a076eaf67b449203728734049923f24d788e74e4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-18 23:13:42 +0000

    Update archive 42

---
+++ trunk/Archive2/42/9d346ff500a165/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+bool inc( int *indexes, int limit, int n )
+{
+    if( ++indexes[n] < limit )
+        return true;
+    if( n == 0 ) return false;
+    if( inc( indexes, limit, n-1 ) ) {
+        indexes[n] = indexes[n-1];
+        return true;
+    }
+    return false;
+
+}
+
+int main()
+{
+    const size_t N=3;
+    int indexes[N];
+    for( size_t i = 0; i < N; ++i ) indexes[i] = 1;
+
+    do {
+        for( size_t i = 0; i < N; ++i ) std::cout << indexes[i] << ' ';
+        std::cout << std::endl;
+    } while( inc( indexes, 6, N-1 ) );
+    return 0;
+}
