28907b0d186f30a7ed38c44926ac51d74496869e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-18 20:49:36 +0000

    Update archive d5

---
+++ trunk/Archive2/d5/01e06aa0db0252/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+    void advance( std::vector<size_t>& indexes, std::vector<size_t> counts ) {
+      if (indexes.size() != counts.size())
+        indexes.resize(counts.size());
+      for (size_t i = 0; i < counts.size(); ++i ) {
+        ++indexes[i];
+        if (indexes[i] < counts[i])
+          return;
+        indexes[i] = 0;
+      }
+      // past the end, don't advance:
+      indexes = counts;
+    }
+
+    void print_a_lot( std::vector<size_t> counts ) {
+      for( std::vector<size_t> v(counts.size()); v < counts; advance(v,counts)) {
+        for (size_t x : v)
+          std::cout << x;
+        std::cout << std::endl;
+      }
+    }
+    
+int main() {
+    print_a_lot( {1,2,3,4} );
+}
