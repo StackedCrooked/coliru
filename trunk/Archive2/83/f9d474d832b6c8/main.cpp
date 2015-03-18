e761c050a8ebcdd40e743726517218356aed0b5b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-18 19:40:06 +0000

    Update archive 83

---
+++ trunk/Archive2/83/f9d474d832b6c8/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdint>
+
+int main()
+{
+    const int32_t i = 5; // create a const memory
+
+    int32_t* addr = const_cast<int32_t*>(&i); // convert the const pointer to a mutable memory pointer
+    
+    *addr += 5; // sets 10 to its memory
+    
+    std::cout << *addr << '\n' << i << '\n'; // shows the glory
+    
+    // shows address
+    
+    std::cout << &*addr << '\n' << &i;
+    
+    return 0;
+}
