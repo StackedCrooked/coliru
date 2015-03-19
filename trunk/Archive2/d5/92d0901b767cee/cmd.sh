2982032187e94fdd5c4dee22b2e38266e1937f0c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-19 23:25:18 +0000

    Update archive d5

---
+++ trunk/Archive2/d5/92d0901b767cee/cmd.sh
@@ -0,0 +1,3 @@
+ln -sfv /Archive2/d4/5cc286c63c104d/main.cpp numeric_utils.hpp # you can do anything
+clang++ -std=c++11 -Os -Wall -pedantic -pthread main.cpp
+./a.out 123_456 123456 1_bh 0_010Q 1010_1010_0111_0111_b
