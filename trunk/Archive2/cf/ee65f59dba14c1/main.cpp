cd4475ddd91c560054e2d4171a13b16047124ecb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-19 08:01:08 +0000

    Update archive cf

---
+++ trunk/Archive2/cf/ee65f59dba14c1/main.cpp
@@ -0,0 +1,28 @@
+
+#include <cstdio>
+#include <vector>
+
+int main(){
+
+    std::vector<unsigned> tomatoes = { 2, 3 };
+	std::vector<unsigned> potatoes = { 10, 3, 1 };
+
+    potatoes.reserve(potatoes.size() + tomatoes.size() + 100);
+    
+	for (unsigned i : tomatoes){
+		for (auto iter = potatoes.rbegin(); iter != potatoes.rend(); ++iter){
+			if (i < *iter){
+				//printf("%u\n", *iter);
+				potatoes.insert(iter.base(), i); // problem line, causes the segfault
+			}
+		}
+	}
+
+	for (unsigned i : potatoes){
+		printf("%u, ", i);
+	}
+
+	printf("\n");
+
+	return 0;
+}
