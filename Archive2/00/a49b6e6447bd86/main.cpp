2ca44bd3322e4236a505bf5fcd355e1f45059cab
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-19 16:10:26 +0000

    Update archive 00

---
+++ Archive2/00/a49b6e6447bd86/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+struct bob
+{
+    int i =8; // how cool is this? :) If you also provide mem-initializer in the constructor, that takes precedance
+    // However, this makes it really easy to specify defaults for members, and makes it less likely you 
+    // will forget to initialize a member when you add a new constructor.
+};
+void encrypt(bob b) = delete;
+int main()
+{
+    bob b;
+    std::cout << b.i;
+   // encrypt (b); // fails to compile because the function is deleted
+}
