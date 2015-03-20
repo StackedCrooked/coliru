7a7f5561a6eb72df82d8c35643dc5632c33146c2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-20 04:25:27 +0000

    Update archive ba

---
+++ trunk/Archive2/ba/b56e544c121201/main.cpp
@@ -0,0 +1,69 @@
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <random>
+
+namespace rc4 {
+  namespace impl {
+    template<typename T>
+    struct wrap {
+      T x;
+      wrap() = default;
+      wrap(T const& x) : x{x} {}
+      operator T&()              { return x; }
+      operator T const &() const { return x; }
+      
+      template<template<typename> class U, typename V>
+      friend void swap(U<V> & x, U<V> & y) {
+        decltype(auto) t = x; x = y; y = t;
+      }
+      
+      wrap(wrap const&) = default;
+      wrap& operator=(wrap const&) = default;
+      ~wrap() = default;
+    };
+
+    using  uint8 = wrap< std::uint8_t>;
+    using uint16 = wrap<std::uint16_t>;
+    using uint32 = wrap<std::uint32_t>;
+    using uint64 = wrap<std::uint64_t>;
+  }
+
+  inline void stream(unsigned char * s, std::size_t slen, 
+                     unsigned char * k, std::size_t klen) {
+    using impl::uint8;
+    using std::swap;
+    uint8 S[256];
+    
+    for(int i = 0; i < 256; ++i) {
+      S[i] = i;
+    }
+    
+    for(int i = 0, j = 0; i < 256; ++i) {
+      j = (j + S[i] + k[i%klen])&0xFF;
+      swap(S[i], S[j]);
+    }
+    
+    for(uint8 i = 0, j = 0; slen--; ) {
+      i += 1;
+      j += S[i];
+      swap(S[i], S[j]);
+      *s++ = S[(S[i] + S[j])&0xFF];
+    }
+  }
+}
+
+int main() {
+  using namespace std;
+  unsigned char k[  32] = {0};
+  unsigned char s[8192] = {0};
+  random_device rd;
+  generate(begin(k), end(k), ref(rd));
+  rc4::stream(s, sizeof s, k, sizeof k);
+  for(size_t i = 0; i < sizeof s; ++i) {
+    printf("%02X%s", s[i], i % 8 == 7 ? "\n" : " ");
+  }
+}
