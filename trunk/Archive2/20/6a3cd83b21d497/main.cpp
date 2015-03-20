cba68174ec123af1e209af3904a11d3c482827d3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-20 20:47:46 +0000

    Update archive 20

---
+++ trunk/Archive2/20/6a3cd83b21d497/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int bucky[9];
+//enter code here
+    cout << "Element - value" << endl;
+
+
+for (int x = 0; x <= 8; x++); {
+
+    cout <<  x << endl;
+
+}
+}
