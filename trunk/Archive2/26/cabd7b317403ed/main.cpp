6e54a03b97a4384dc1ef7532ae4fdf173de3c571
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-20 00:02:33 +0000

    Update archive 26

---
+++ trunk/Archive2/26/cabd7b317403ed/main.cpp
@@ -0,0 +1,28 @@
+#include <boost/variant.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+    struct visitor_t : boost::static_visitor<> {
+        void operator()(std::string const& x) const {
+            std::cout << "got string: " << x << '\n';
+        }
+
+        void operator()(float x) const {
+            std::cout << "got float: " << x << '\n';
+        }
+    };
+
+    std::vector<boost::variant<std::string, float>> values;
+    values.emplace_back("a");
+    values.emplace_back("b");
+    values.emplace_back(3.14);
+    values.emplace_back("d");
+    values.emplace_back(2.72);
+
+    visitor_t visitor;
+    for (auto&& value : values) {
+        boost::apply_visitor(visitor, value);
+    }
+}
