5db890e5535afb9f33d02377214139123ebfa2b8
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-20 06:43:13 +0000

    Update archive 37

---
+++ trunk/Archive2/37/d8acccdad861f9/main.cpp
@@ -0,0 +1,26 @@
+#include <boost/python.hpp>
+
+// Mockup types.
+class spam {};
+class egg  {};
+
+// Factory function that returns boost::python::objects.
+boost::python::object make_object(std::string name)
+{
+  namespace python = boost::python;
+  if (name == "spam") return python::object(spam{});
+  else if (name == "egg") return python::object(egg{});
+  else return python::object();
+}
+
+BOOST_PYTHON_MODULE(example)
+{
+  namespace python = boost::python;
+
+  // Expose models.
+  python::class_<spam>("Spam", python::init<>());
+  python::class_<egg>("Egg", python::init<>());
+
+  // Expose factory function.
+  python::def("make_object", &make_object);
+}
