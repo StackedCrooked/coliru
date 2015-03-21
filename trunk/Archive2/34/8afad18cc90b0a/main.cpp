d553a8c4a25e9e9f08e174a33bb55ca5e9447592
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-21 23:57:58 +0000

    Update archive 34

---
+++ trunk/Archive2/34/8afad18cc90b0a/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <array>
+ 
+template<typename... Ts>
+constexpr auto make_array(Ts&&... ts)
+    -> std::array<std::common_type_t<Ts...>,sizeof...(ts)>
+{
+    return { std::forward<Ts>(ts)... };
+}
+ 
+int main()
+{
+    auto b = make_array(1, 2, 3);
+    std::cout << b.size() << '\n';
+    for (auto i : b)
+        std::cout << i << ' ';
+}
