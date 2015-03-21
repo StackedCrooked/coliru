0b9beb2453d226a67fde06ed926a5d6c2a5de64b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-21 21:34:00 +0000

    Update archive ca

---
+++ trunk/Archive2/ca/eb43c05f5caa2b/main.cpp
@@ -0,0 +1,18 @@
+
+class Base{
+    Base(const Base&) =default;
+};
+
+class Son;
+class Daughter: public Base{
+public:
+    Daughter(Son* s) : Base(*s){};
+};
+
+class Son: public Base{
+public:
+    Son(Daughter* d) : Base(*d){};
+};
+
+int main(){
+}
