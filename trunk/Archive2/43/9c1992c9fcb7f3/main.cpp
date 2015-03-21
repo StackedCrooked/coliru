0d998e30a443133f43ae7d4b8bdf50e0191583a2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-21 15:08:44 +0000

    Update archive 43

---
+++ trunk/Archive2/43/9c1992c9fcb7f3/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+class A
+{
+public:
+    template<class T>
+    void foo(T t){ std::cout << "foo()" << std::endl; }
+};
+
+template<> void A::foo<int>(int t);
+template<> void A::foo<A*>(A* t);
+
+int main(){ A a; a.foo(5); };
