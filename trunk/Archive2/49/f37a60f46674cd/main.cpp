6016a5c385b2c4db9b4b307650e68fc58ea3f87b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-22 15:11:37 +0000

    Update archive 49

---
+++ trunk/Archive2/49/f37a60f46674cd/main.cpp
@@ -0,0 +1,53 @@
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix.hpp>
+
+int decode(char ch) {
+    if (ch>='0' && ch<='9') return ch - '0';
+    if (ch>='a' && ch<='z') return ch - 'a' + 10;
+    if (ch>='A' && ch<='Z') return ch - 'A' + 10;
+    assert(false);
+}
+
+BOOST_PHOENIX_ADAPT_FUNCTION(int, _decode, decode, 1)
+
+namespace qi = boost::spirit::qi;
+
+template <typename Iterator>
+struct unsigned_parser : qi::grammar<Iterator, uint64_t()> {
+    unsigned_parser() : unsigned_parser::base_type(start) {
+        using namespace qi;
+
+        start = 
+              eps [_val=0] >> +(char_("0-9a-fA-F") [ _val = _val*16 + _decode(_1) ] | '_')>>  char_("hHxX") /* hexadecimal with suffix */
+            | eps [_val=0] >> +(char_("0-7")       [ _val = _val* 8 + _decode(_1) ] | '_')>>  char_("qQoO") /* octal       with suffix */
+            | eps [_val=0] >> +(char_("01")        [ _val = _val* 2 + _decode(_1) ] | '_')>>  char_("bByY") /* binary      with suffix */
+            | eps [_val=0] >> +(char_("0-9")       [ _val = _val*10 + _decode(_1) ] | '_')>> -char_("dDtT") /* decimal     with optional suffix */
+            ;
+    }
+    private:
+    qi::rule<Iterator, uint64_t()> start;
+};
+
+int main() {
+    typedef std::string::const_iterator It;
+    unsigned_parser<It> up;
+
+    for (std::string const test : {
+            "123_456",
+            "123456",
+            "1_bh",
+            "0_010Q",
+            "1010_1010_0111_0111_b"
+            })
+    {
+        It i = test.begin(), end = test.end();
+
+        uint64_t val;
+        bool rv = parse(i, end, up, val);
+
+        std::cout << (rv?"Successful":"Failed") << " parse: '" << test << "' -> " << val << "\n";
+
+        if (i != end)
+            std::cout << " ** Remaining unparsed: '" << std::string(i,end) << "'\n";
+    }
+}
