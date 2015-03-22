7f893a6cfda6f43bb4daaf772a81592c46ef8c29
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-22 02:28:10 +0000

    Update archive 22

---
+++ Archive2/22/4afb4f863bb63d/main.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <type_traits>
+
+struct pod
+{
+    int i;
+    float f;
+    double d;
+};
+
+template<typename T>
+struct vector
+{
+    std::vector<T> data;
+
+    template<typename... Args, typename = typename std::enable_if<std::is_pod<T>::value>::type>
+    void construct(T* where, Args&&... args)
+    {
+        new(where)T{ args... };
+    }
+
+    template<typename... Args>
+    void emplace_back(Args&&... args)
+    {
+        data.resize(data.size() + 1);
+        construct(&data.back(), std::forward<Args>(args)...);
+    }
+};
+
+
+int main()
+{
+    vector<pod> pods;
+
+    pods.emplace_back(1, 2.0f, 3.0);
+}
