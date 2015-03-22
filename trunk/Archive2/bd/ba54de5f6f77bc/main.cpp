f705873fea8d0a43da3d36c871853aa0cc9d5928
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-22 18:29:39 +0000

    Update archive bd

---
+++ trunk/Archive2/bd/ba54de5f6f77bc/main.cpp
@@ -0,0 +1,11 @@
+#include <type_traits>
+
+template< typename T >
+using cond = std::conditional_t< true , std::true_type , std::false_type >::value;
+
+template< typename T , typename = std::enable_if_t< cond< T > , T > >
+void func( T t ) {}
+
+int main() {
+    func( 1 );
+}
