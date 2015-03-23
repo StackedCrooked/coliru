d3b4355d921fd1f36dbbdacdea38aed7c311f1b3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-23 00:00:06 +0000

    Update archive e6

---
+++ Archive2/e6/ea838549029857/main.cpp
@@ -0,0 +1,44 @@
+#include <cstddef>
+#include <cxxabi.h>
+#include <iostream>
+#include <typeinfo>
+
+class Value {
+public:
+    virtual ~Value() = 0;
+
+private:
+    std::size_t refcount;
+
+    friend void mill_retain(void*);
+    friend void mill_release(void*);
+};
+Value::~Value() {
+    std::cout << "Value\n";
+}
+
+struct X {
+    void* vptr;
+};
+
+int main() {
+    // The virtual table contains offset-to-top and RTTI fields followed by virtual function pointers.
+    // There is one function pointer entry for each virtual function declared in the class, in declaration
+    // order, with any implicitly-defined virtual destructor pair last.
+    struct {
+        std::ptrdiff_t ott;
+        void* rtti;
+        void (* dtor)(X*);
+        void (* dtor2)(X*);
+    } vtable;
+    vtable.ott = 0;
+    vtable.rtti = new abi::__class_type_info("hahaha ur mom");
+    vtable.dtor2 = vtable.dtor2 = [] (X*) { std::cout << "X\n"; };
+
+    X* x = new X;
+    x->vptr = &vtable.dtor;
+    std::cout << typeid(*reinterpret_cast<Value*>(x)).name() << '\n';
+    delete reinterpret_cast<Value*>(x);
+
+    return 0;
+}
