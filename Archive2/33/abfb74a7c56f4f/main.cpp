ff9570fa27325b3ba8b09f49755a059128abf0aa
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-23 09:50:01 +0000

    Update archive 33

---
+++ Archive2/33/abfb74a7c56f4f/main.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <numeric>
+#include <memory>
+ 
+int main()
+{
+    std::unique_ptr<int> a;
+    if (!a)
+        std::cout << "123\n"';
+}
