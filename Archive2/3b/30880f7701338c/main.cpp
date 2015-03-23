94ef8b7697a442bb3f36ecc31dbcf1da33608b3a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-23 01:25:04 +0000

    Update archive 3b

---
+++ Archive2/3b/30880f7701338c/main.cpp
@@ -0,0 +1,39 @@
+#include <cstddef>
+#include <iostream>
+
+class Value {
+public:
+    virtual ~Value() = 0;
+
+private:
+    std::size_t refcount;
+
+    friend void mill_retain(void*);
+    friend void mill_release(void*);
+};
+Value::~Value() = default;
+
+struct X {
+    void* vptr;
+};
+
+int main() {
+    // The virtual table contains offset-to-top and RTTI fields followed by virtual function pointers.
+    // There is one function pointer entry for each virtual function declared in the class, in declaration
+    // order, with any implicitly-defined virtual destructor pair last.
+    struct {
+        std::ptrdiff_t ott;
+        void* rtti;
+        void (* dtor)(X*);
+        void (* dtor2)(X*); // ???
+    } vtable;
+    vtable.ott = 0;
+    vtable.rtti = nullptr; // TODO
+    vtable.dtor = vtable.dtor2 = [] (X*) { std::cout << "hurray!\n"; };
+
+    X* x = new X;
+    x->vptr = &vtable.dtor;
+    delete reinterpret_cast<Value*>(x);
+
+    return 0;
+}
