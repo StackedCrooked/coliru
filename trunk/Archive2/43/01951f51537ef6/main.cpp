95d935497f13f468b73bc9557ef0ec796b5cf783
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-24 20:05:58 +0000

    Update archive 43

---
+++ trunk/Archive2/43/01951f51537ef6/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <typeinfo>
+#include <iomanip>
+#include <sstream>
+
+template <class T>
+void show_size(T t) { 
+    std::cout << std::setw(15) <<  typeid(T).name() << " => " << sizeof(t) << " bytes\n";
+}
+
+template <class T, typename ...Args>
+void show_size(T t, Args...args) { 
+	show_size(t);
+	show_size(args...);
+}
+
+int main() { 
+	typedef long double ld;
+
+	show_size(char(), short(), int(), long(), float(), double(), ld());
+}
