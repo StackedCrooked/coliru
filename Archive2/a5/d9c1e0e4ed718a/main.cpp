2d3b3978b7d55ff061966db9493e8ad787863eb5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-24 07:16:30 +0000

    Update archive a5

---
+++ Archive2/a5/d9c1e0e4ed718a/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <memory>
+#include <utility>
+
+struct PimplClass {
+    PimplClass();
+    PimplClass(const PimplClass &other);
+    PimplClass(PimplClass &&);
+    ~PimplClass();
+    
+    PimplClass &operator=(PimplClass other);
+    
+private:
+    struct Impl;
+    std::unique_ptr<Impl> impl_;
+};
+
+int main() {
+    PimplClass pimpl; //impl 1 created with default constructor
+    
+    pimpl = {}; //impl 2 created with temporary; swapped with impl 1, which dies here with the temporary
+    pimpl = pimpl; //impl 3 created with copy for pass-by-value; impl 2 swapped with it, impl 2 dies
+    
+    auto pimpl2 = pimpl; //impl 4 created with copy
+    
+    //impl 3 and impl 4 die here
+}
+
+struct PimplClass::Impl {
+    Impl() : number(++counter) {std::cout << "Impl " << number << " created.\n";}
+    Impl(const Impl &other) : number(++counter) {std::cout << "Impl " << number << " created from Impl " << other.number << '\n';}
+    ~Impl() {std::cout << "Impl " << number << " destroyed\n";} 
+    
+    int number;
+    static int counter;
+};
+
+int PimplClass::Impl::counter = 0;
+
+PimplClass::PimplClass() : impl_(new Impl) {}
+
+PimplClass::PimplClass(const PimplClass &other) 
+    : impl_(new auto(*other.impl_)) {}
+    
+PimplClass::PimplClass(PimplClass &&) = default;
+
+PimplClass::~PimplClass() = default;
+    
+PimplClass &PimplClass::operator=(PimplClass other) {
+    using std::swap;
+    swap(impl_, other.impl_);
+    return *this;
+}
