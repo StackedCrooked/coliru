8d3a75657b80f739746cab8244e2191440ae9547
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-24 16:22:30 +0000

    Update archive f0

---
+++ Archive2/f0/293b8bdb449bf2/main.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define N 4
+#define M 6
+
+int main(void){
+    int i=0,j=0,*a[N] = {NULL};
+    
+    for(; i < N; ++i){
+        a[i] = malloc(sizeof(int) * M);
+        for(j=0; j < M; ++j){
+            a[i][j] = (j+1)*(i+1);
+        }
+    }
+    
+    for(i=0; i < N; ++i){
+        for(j=0; j < M; ++j){
+            printf("%3d",a[i][j]);
+        }
+        printf("\n");
+    }
+    
+    for(i=0; i < N; ++i){
+        free(a[i]);
+    }
+    
+    
+	return 0;
+}
