f629a8244631f7de51606955745a8591504e5f5a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-24 13:09:10 +0000

    Update archive a5

---
+++ trunk/Archive2/a5/d838b098495d9f/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <type_traits>
+#include <vector>
+
+template<typename C>
+struct IsIterable
+{
+    typedef char true_type; 
+    typedef long false_type; 
+    
+    template<class T> static true_type  is_beg_iterable(int, typename T::const_iterator = C().begin()); 
+    template<class T> static false_type is_beg_iterable(...); 
+    
+    enum { value = sizeof(is_beg_iterable<C>(0)) == sizeof(true_type) }; 
+};
+
+int main()
+{
+    std::cout << IsIterable<std::vector<int>>::value << std::endl;
+    //std::cout << IsIterable<std::vector<int>::value << std::endl;
+}
