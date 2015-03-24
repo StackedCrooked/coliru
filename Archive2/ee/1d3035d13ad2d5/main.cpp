2390523e82b2c27ca57c20cfd1da3250fe662817
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-24 17:28:58 +0000

    Update archive ee

---
+++ Archive2/ee/1d3035d13ad2d5/main.cpp
@@ -0,0 +1,34 @@
+#include <stdexcept>
+
+
+template <typename>
+struct B;
+
+struct A
+{
+    A();
+    ~A();
+    
+    friend B;
+    
+private:
+    void foo() { throw std::runtime_error("Works!"); }
+    
+    B<int>* b;
+    A(const A&);
+};
+
+template <typename T>
+struct B
+{
+    B(A* a) { a->foo(); }
+};
+
+
+A::A() : b(new B<int>(this)) {}
+A::~A() { delete b; }
+
+int main()
+{
+    A a;
+}
