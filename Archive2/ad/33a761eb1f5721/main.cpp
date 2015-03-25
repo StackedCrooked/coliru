0c7c57d688bca4cbf4a5a4a372e45eda4e42227e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-25 17:25:26 +0000

    Update archive ad

---
+++ Archive2/ad/33a761eb1f5721/main.cpp
@@ -0,0 +1,43 @@
+// transform algorithm example
+#include <iostream>     // std::cout
+#include <memory>
+#include <vector>
+#include <type_traits>
+
+
+template <typename T>
+auto move_if_cannot_copy(T& x) -> std::enable_if_t<std::is_copy_constructible<T>::value, T&>
+{
+    return x;
+}
+
+template <typename T>
+auto move_if_cannot_copy(T& x) -> std::enable_if_t<!std::is_copy_constructible<T>::value, T&&>
+{
+    return std::move(x);
+}
+
+class OnlyMovable {
+public:
+    OnlyMovable(){}
+    OnlyMovable(OnlyMovable const&) = delete;
+    OnlyMovable(OnlyMovable &&){std::cout << "moving OnlyMovable" << std::endl;}
+};
+
+class Copiable {
+public:
+    Copiable(){}
+    Copiable(Copiable const&){std::cout << "copying Copiable" << std::endl;}
+    Copiable(Copiable &&){std::cout << "moving Copiable" << std::endl;}
+};
+
+int main () {
+    
+     Copiable c;
+     Copiable c2{move_if_cannot_copy(c)};
+
+     OnlyMovable m;
+     OnlyMovable m2{move_if_cannot_copy(m)};   
+  
+    return 0;
+}
