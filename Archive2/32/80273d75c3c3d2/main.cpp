f5e3bd8412efe0817272f821b2119257f189f405
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-25 16:28:24 +0000

    Update archive 32

---
+++ Archive2/32/80273d75c3c3d2/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf( "sizeof( size_t ) == %llu", sizeof( size_t ) );
+    
+    return 0;
+}
---
+++ Archive2/32/ba9cb0fca96340/main.cpp
@@ -0,0 +1,90 @@
+// transform algorithm example
+#include <iostream>     // std::cout
+#include <iterator>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <memory>
+#include <boost/bind.hpp>
+#include <utility>
+#include <type_traits>
+
+template <typename T, bool IsPointer = false>
+struct add_deep_const_impl;
+
+template <typename T>
+struct add_deep_const_impl<T, false>
+{
+    typedef const T type;
+};
+
+template <typename T>
+struct add_deep_const_impl<T, true>
+{
+    typedef std::add_pointer_t<typename add_deep_const_impl<std::remove_pointer_t<T>, std::is_pointer<std::remove_pointer_t<T>>::value>::type> const type;
+};
+
+// This is the one the user uses
+template <typename T>
+struct add_deep_const : add_deep_const_impl<T, std::is_pointer<T>::value>
+{
+};
+
+
+
+template <typename T>
+class deep_constness_ptr {
+    
+public:
+    deep_constness_ptr(T* raw) : raw(raw){}
+    
+    typename add_deep_const<T*>::type get() const {return raw;}
+    T* get() {return raw;}
+    
+    operator typename add_deep_const<T*>::type() const {return raw;}
+    operator T*() {return raw;}
+    
+    typename add_deep_const<T*>::type operator ->() const {return raw;}
+    T* operator ->()  {return raw;}
+    
+    typename add_deep_const<T>::type& operator *() const {return *(const_cast<typename add_deep_const<T*>::type>(raw));}
+    T& operator *() {return *raw;}
+                
+private:
+    T* raw;    
+};
+
+
+class Bar {
+public:
+    int*** i;
+};
+
+class Foo {
+public:
+    deep_constness_ptr<int**> i;
+};
+
+int main () {
+    
+    int*** p = new int**{new int*{new int{5}}};
+    
+    //============================
+    
+    Bar bar{p};
+    ***bar.i = 7; // OK
+    
+    const Bar cbar {p};
+    ***cbar.i = 8; // OK
+    
+    //============================
+    
+    Foo foo{p};
+    ***foo.i = 7; // OK
+    
+    const Foo cfoo {p};
+    //***cfoo.i = 8; //error
+        
+  
+    return 0;
+}
