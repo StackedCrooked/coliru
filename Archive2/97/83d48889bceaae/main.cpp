5872481e1c385413ee0dde9ef8748d5d74fee7a1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-27 05:25:33 +0000

    Update archive 97

---
+++ Archive2/97/83d48889bceaae/main.cpp
@@ -0,0 +1,18 @@
+template <typename... Types>
+class TupleLite;
+
+template <typename FirstType, typename... OtherTypes>
+class TupleLite<FirstType, OtherTypes...>
+{
+  public:
+    FirstType type_;
+    TupleLite<OtherTypes...> other_types_;
+};
+
+template <>
+class TupleLite<>{};
+
+int main()
+{
+  TupleLite<int,double> mytuple;
+}
