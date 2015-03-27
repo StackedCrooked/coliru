5efe44a20853fbeda10ea7c1de38b2463dc049ff
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-27 01:49:26 +0000

    Update archive 67

---
+++ Archive2/67/d38a42ce2a4283/main.cpp
@@ -0,0 +1,6 @@
+int n(4);
+int n_carre;
+n_carre=n*n;
+cout <<"La variable n contient "<<n<<"."<<endl;
+cout <<"Le carre de  "<<n<<"est"<<n_carre<<"."<<endl;
+cout <<"Le double de n est  "<<2*n<<"."<<endl;
