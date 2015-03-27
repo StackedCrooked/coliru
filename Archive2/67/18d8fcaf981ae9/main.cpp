03954ebc17a4d93379841fdb79ace1aac80e3ed7
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-27 21:55:17 +0000

    Update archive 67

---
+++ Archive2/67/18d8fcaf981ae9/main.cpp
@@ -0,0 +1,27 @@
+// In X.hpp
+
+#include <vector>
+
+struct Y;
+
+struct X
+{
+    // ...
+    X(X const&);
+    // ...
+private:
+    std::vector<Y> v;
+};
+
+// In X.cpp
+
+// ...
+X::X(X const&) = default;
+// ...
+
+// In main.cpp
+
+int main()
+{
+    // Use X without seeing the definition of Y
+}
