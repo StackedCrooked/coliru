0834ba8e7bf61bd2369f2a67be14190edca09b87
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-27 19:01:35 +0000

    Update archive 29

---
+++ Archive2/29/e8ac0ef59fdfc6/main.cpp
@@ -0,0 +1,39 @@
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <future>
+
+
+class ThreadPool
+{
+public:
+    ThreadPool(std::size_t num_threads) : work_(ios_)
+    {
+        for (std::size_t i = 0; i != num_threads; ++i)
+        {
+            threads_.create_thread([this]{ ios_.run(); });
+        }
+    }
+
+    template<typename F>
+    void post(F&& f) { ios_.post(std::forward<F>(f)); }
+
+    template<typename F>
+    void dispatch(F&& f) { ios_.dispatch(std::forward<F>(f)); }
+
+private:
+    boost::asio::io_service ios_;
+    boost::thread_group threads_;
+    boost::asio::io_service::work work_;
+};
+
+
+int main()
+{
+    ThreadPool pool(4);
+    usleep(100);
+    for (int i = 0; i != 10; ++i)
+    {
+        pool.post([i]{ std::cout << '[' << i << ']'; });
+    }
+    usleep(100);    
+}
