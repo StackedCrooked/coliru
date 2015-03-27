c289f4fbdcbbcc66e3e0863c16829025b4399343
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-27 11:21:58 +0000

    Update archive 04

---
+++ Archive2/04/f1307f21a001fd/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int x;
+    int y = int();
+    std::vector<int> z(1);
+    
+    std::cout << x << "\n";
+    std::cout << y << "\n";
+    std::cout << z[0] << "\n";
+    
+    return 0;
+}
