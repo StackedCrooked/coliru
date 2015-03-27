6fa0e8bdfa82032abf415003124edd82854ded14
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-27 16:55:36 +0000

    Update archive 98

---
+++ trunk/Archive2/98/df48276abf1a4c/main.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+
+struct node
+{
+    int a;
+    node* b;
+};
+
+int main()
+{
+    {
+        struct node *p = (node*)malloc(sizeof(node));
+        p->a = 0;      // first  element
+        p->b = NULL;   // second element
+        free(p);
+    }
+    
+    {
+        node* p = new node {0, NULL};
+        delete p;
+    }
+}
