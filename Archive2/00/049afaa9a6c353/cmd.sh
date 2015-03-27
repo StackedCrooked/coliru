889c2c9569d337329af56818e0aeccdb994e67a1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-27 13:44:22 +0000

    Update archive 00

---
+++ Archive2/00/049afaa9a6c353/cmd.sh
@@ -0,0 +1,4 @@
+clang++ -std=c++14 -DHAVE_GET_IN_LAYOUT=0 -O2 -Wall -pedantic -pthread main.cpp && ./a.out
+clang++ -std=c++14 -DHAVE_GET_IN_LAYOUT=1 -O2 -Wall -pedantic -pthread main.cpp && ./a.out
+g++ -std=c++14 -DHAVE_GET_IN_LAYOUT=0 -O2 -Wall -pedantic -pthread main.cpp && ./a.out
+g++ -std=c++14 -DHAVE_GET_IN_LAYOUT=1 -O2 -Wall -pedantic -pthread main.cpp && ./a.out
