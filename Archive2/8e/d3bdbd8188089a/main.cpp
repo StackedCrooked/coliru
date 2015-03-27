f52998a4450f9bb0bc307be78f04068f0f46f40e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-27 22:57:33 +0000

    Update archive 8e

---
+++ Archive2/8e/d3bdbd8188089a/main.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int r;
+    int c;
+    int d;
+
+    cout<<"r="<<r<<endl<<"c="<<c<<endl<<"d="<<d;
+    return 0;
+}
