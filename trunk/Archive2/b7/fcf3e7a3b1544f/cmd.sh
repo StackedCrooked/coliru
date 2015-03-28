210344dc90b19b7141a19da75856386f95c1a5d5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-28 02:04:03 +0000

    Update archive b7

---
+++ trunk/Archive2/b7/fcf3e7a3b1544f/cmd.sh
@@ -0,0 +1,8 @@
+g++ -std=c++11 -Wall -pedantic main.cpp && ./a.out <<HERE
+11101101111000101010
+01111101100000010100
+toggle 1
+toggle both 2
+toggle from 1 through 30
+toggle from 1 through 30
+HERE
