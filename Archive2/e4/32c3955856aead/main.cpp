3445435e739320a547e25a117f678655c9b6cd6b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-28 11:29:25 +0000

    Update archive e4

---
+++ Archive2/e4/32c3955856aead/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+ 
+int main() {
+    int x = 2;
+    int y = 5;
+    
+    x ^= y ^= x ^= y;
+    //x = x ^ (y = y ^ (x = (x ^ y)));
+
+    std::cout << "x=" << x << " y=" << y << std::endl;
+}
