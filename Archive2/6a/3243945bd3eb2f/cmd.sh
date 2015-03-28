d1b1f262f7f920a0f871be650ed6fbfe19c1e582
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-28 22:39:23 +0000

    Update archive 6a

---
+++ Archive2/6a/3243945bd3eb2f/cmd.sh
@@ -0,0 +1,9 @@
+##clang++ --version
+##g++-4.8 --version
+##g++-4.9 -std=c++11 -Wall -Wextra -Wconversion -pedantic-errors  -O0 -pthread  main.cpp && ./a.out 
+##g++ --version
+##set -x ; g++-4.9 -std=c++11 -Wall -Wextra -Wconversion -pedantic  -O3  -pedantic  -pthread -S main.cpp && cat main.s 
+##set -x ; clang++ -std=c++11 -Wall -Wextra -Wconversion -pedantic -stdlib=libc++ main.cpp && ./a.out
+##set -x ; gcc -x c -std=c99 -O0 -W -Wall -Wextra -Wconversion -pedantic -S main.cpp && cat main.s 
+set -x ; gcc -x c -std=c99 -fsanitize=undefined   -Wall -Wextra -Wconversion -Wmaybe-uninitialized  -O1  -pedantic main.cpp && ./a.out
+##set -x ; clang -x c -std=c99 -fsanitize=undefined  -Weverything -Wall -Wextra -Wconversion -O0  -pedantic main.cpp && ./a.out
