0f8ad59559f7ecebe79cbab46f0262bd3413d855
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-28 13:22:59 +0000

    Update archive 11

---
+++ trunk/Archive2/11/d768ac43e5cd4b/main.cpp
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+#include<conio.c>
+#include<locale.h>
+do{
+int main(){
+    SetLocale(LC_ALL,"Portuguese");
+    textcolor(3)
+    printf("Teste de comando Do While\n");
+    printf("Se deseja continuar, digite 1");
+}while(getch()=1);
+    
