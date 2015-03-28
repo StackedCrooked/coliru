3070c92bea51f3a3b05bad8c72ad2e51f460554c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-28 13:26:02 +0000

    Update archive 85

---
+++ trunk/Archive2/85/ce59baf9f4434a/main.cpp
@@ -0,0 +1,26 @@
+#include<type_traits>
+#include<iostream>
+
+    namespace impl
+    {
+        template<typename ... Args> struct A;
+        template<typename T> struct A<T>
+        {
+            A() {std::cout<<"I'm an A"<<std::endl;}
+        };
+    
+        template<typename ... Args> struct B;
+        template<typename T, typename V> struct B<T, V>
+        {
+            B() {std::cout<<"I'm a B"<<std::endl;}
+        };
+    }
+    
+    template<typename ... Args>
+    using C = std::conditional_t<sizeof...(Args)==1, typename impl::A<Args ...>, typename impl::B<Args ...> >;
+    
+    int main()
+    {
+        C<double> a;            //prints "I'm an A"
+        C<double, int> b;       //prints "I'm a B"
+    }
