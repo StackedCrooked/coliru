dd32ecc6b220e535b37fa50f589dc9b5cfb7bacd
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-29 21:51:02 +0000

    Update archive d7

---
+++ Archive2/d7/f1fec706015584/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+std::vector<int> items { 1,2,3,4,5,6,7,8 };
+
+template<typename Runnable>
+void for_all_active_things(Runnable && func)
+{
+    std::for_each(items.begin(), items.end(),
+                  [&func](decltype(items)::value_type & item)
+                  {
+                      if ( item % 2 == 0) // some condition that determines "active"
+                          func(item);
+                  });
+    
+}
+
+int main(void)
+{
+    //                        vvvv can I avoid this decltype or make this a little nicer to call?
+    for_all_active_things([](decltype(items)::value_type & t) 
+                            { 
+                                std::cout << t << std::endl; 
+                            });
+}
