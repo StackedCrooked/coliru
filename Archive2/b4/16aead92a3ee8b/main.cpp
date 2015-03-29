ef4a41a70a1b046523a68afb6f9e40197e705789
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-29 15:03:47 +0000

    Update archive b4

---
+++ Archive2/b4/16aead92a3ee8b/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+// return the index if the value is found; -1 if value isn't found
+int find( const int array[], int array_size, int pos, int value_to_find )
+{
+    // if we have reached the end of the array and not found the value
+    if( pos >= array_size ) return -1 ;
+
+    // if the value we are looking for is at index pos
+    else if( array[pos] == value_to_find ) return pos ; // found at array[pos]
+
+    // recursively call find for positions on the right, starting at pos+1
+    else return find( array, array_size, pos+1, value_to_find ) ; // look at the rest of the array
+}
+
+int main()
+{
+    const int N = 10 ;
+    const int a[N] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    std::cout << find( a, N, 0, 0 ) << '\n' // 0
+              << find( a, N, 0, 5 ) << '\n' // 5
+              << find( a, N, 0, 9 ) << '\n' // 9
+              << find( a, N, 0, 25 ) << '\n' ; // -1
+}
