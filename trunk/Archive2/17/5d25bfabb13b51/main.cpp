50c493557f969e075e60c333d5967d36e60c90ff
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-29 13:09:55 +0000

    Update archive 17

---
+++ trunk/Archive2/17/5d25bfabb13b51/main.cpp
@@ -0,0 +1,13 @@
+
+#include <string>
+struct Spam
+{
+    Spam();
+	Spam(bool flag);
+	Spam(const std::initializer_list<std::pair<const std::string, Spam > > &  il);
+	Spam(const std::initializer_list<Spam > & il);
+};
+int main()
+{
+	Spam({ { "1", Spam() }, { "2", Spam() } });
+}
