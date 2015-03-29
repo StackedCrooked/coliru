f7accbbbf837333e30d081460ea32f5e22dfbf26
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-29 02:23:01 +0000

    Update archive 5d

---
+++ Archive2/5d/86f5df08ce8f77/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+    std::cont<<"enter two numbers:"<<std::endl;
+    int v1=0,v2=0;
+    std::cin>>v1>>v2
+    std::cout<<"the sum of"<<v1<<v2<<"is"<<v1+v2<<std::endl;
+    return 0;
+}
