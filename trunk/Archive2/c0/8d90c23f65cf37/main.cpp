8602ecfc07fba4b6a8a5bc3b02259ffa9504b4a2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-30 20:17:36 +0000

    Update archive c0

---
+++ trunk/Archive2/c0/8d90c23f65cf37/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+    {
+    int x;
+    cout << "Digite um número: " << endl;
+    cin >> x;
+    cout << "O número digitado foi " << x;
+    cout << ", seu quadrado é " << x*x;
+    cout << ", seu cubo é " << x*x*x;
+    return 0;
+    }
