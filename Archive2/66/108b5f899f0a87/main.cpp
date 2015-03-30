23f3a2003b832f24bc6e0c6a4d35a65111231537
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-30 22:55:45 +0000

    Update archive 66

---
+++ Archive2/66/108b5f899f0a87/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+int main()
+     {
+    cout << "aqui é um texto somente,não realiza operações matematicas 3+4*2" << endl;
+
+    cout << (5+3*2) << endl;
+    cout << ((5+3)*2) << endl;
+    cout << (10.21/3.2 + 2.0)*(7.85*3.6 + 0.055) << endl;
+    cout.precision(5);
+    cout << (10.21/3.2 + 2.0)*(7.85*3.6 + 0.055) << endl;
+    cout << 1.234567 << endl;
+    cout.precision(7);
+    cout << 1.234567 << endl;
+    cout << 7/2 << endl;
+    cout << 7%2 << endl;
+    cout << 7.0/2 << endl;
+    cout << 7/2.0 << endl;
+    cout << 7.0/2.0 << endl;
+    
+
+
+
+
+
+
+    return 0;
+
+    }
