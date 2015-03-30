7921f60a87e4f3e6bae08bc01aa084feab634a1c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-30 13:18:14 +0000

    Update archive a9

---
+++ Archive2/a9/d542d406eba5ea/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+    {
+    cout << "3+4*2=" << 3+4*2 << endl;
+    cout << "256*256+1024*16=" <<256*256*1024*16 <<endl;
+    cout << 2*5*5+3*5+7 << endl;
+    cout << (5+3*2) << endl;
+    cout << ((5+3)*2) << endl;
+    cout << (10.21/3.2+2.0)*(7.85*3.6+0.055) << endl;
+    cout.precision(5);
+    cout << (10.21/3.2+2.0)*(7.85*3.6+0.055) << endl;
+    cout << 1.23456 << endl;
+    cout << 7/2 << endl;
+    cout << 7%2 << endl;
+    cout << 7.0/2 << endl;
+    cout << 8%4 << endl;
+    cout << "0=par 1=impar\n";
+    cout << 7877456675%2 << endl;
+
+
+
+
+
+
+    cout << "\nDavi is love,\nDavi is life.";
+    return 0;
+    }
