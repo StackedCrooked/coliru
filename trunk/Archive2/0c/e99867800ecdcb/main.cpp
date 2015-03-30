e7be90a80751456265046c52077895e960f419a6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-30 14:49:41 +0000

    Update archive 0c

---
+++ trunk/Archive2/0c/e99867800ecdcb/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+    {
+    cout << "1A)False\n";
+    cout << "1B)True\n";
+    cout << "1C)True\n";
+    cout << "1D)False\n";
+    cout << "1E)False\n";
+    cout << "3)cout << X+Y+Z;";
+
+    cout << "\nDavi is love,\nDavi is life.";
+    return 0;
+    }
