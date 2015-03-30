2954411da739f1726866fb5c2f71dbf494e7859d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-30 08:52:46 +0000

    Update archive b7

---
+++ Archive2/b7/f21038d7dc9d21/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#define BOOST_SPIRIT_USE_PHOENIX_V3
+#include <boost/phoenix/operator.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/repository/include/qi_seek.hpp>
+
+namespace Format
+{
+    using namespace boost::spirit::qi;
+    using namespace boost::spirit::repository::qi;
+
+    using Value = int;
+    using Sequence = std::vector<Value>;
+
+    template<class It>
+    struct Grammar : grammar<It, Sequence(), blank_type>
+    {
+        Grammar() : Grammar::base_type(start)
+        {
+            start =
+                '{' >> eol >> *("#" >> seek[eol] | int_ >> eol) >> '}' >> eol
+                ;
+        }
+
+        rule<It, Sequence(), blank_type> start;
+    };
+
+    template<class It>
+    bool Parse(It begin, It end, Sequence& ret)
+    {
+        static Grammar<It> grammar;
+        return phrase_parse(begin, end, grammar, blank, ret);
+    }
+}
+
+int main()
+{
+    char const input[] =
+        R"({
+#ignore
+123
+456
+}
+)";
+
+    Format::Sequence seq;
+    if (Format::Parse(std::begin(input), std::end(input), seq))
+    {
+        for (auto&& i : seq)
+        {
+            std::cout << i << "\n";
+        }
+    }
+    else
+        std::cout << "FAIL\n";
+}
