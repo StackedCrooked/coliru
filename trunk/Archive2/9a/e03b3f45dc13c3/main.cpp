d5fb7fd9d93c588ff7e92f0a557323cd9ca08c51
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-30 04:32:15 +0000

    Update archive 9a

---
+++ trunk/Archive2/9a/e03b3f45dc13c3/main.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+ #include <cstring>
+ #include <iomanip>
+ #include <queue>
+ #include <algorithm>
+ using namespace std;
+ 
+ namespace global {
+ int y0;
+ }
+ 
+int main()
+{
+    using global::y0;
+   cin>>y0;
+    std::cout << "hello\n";
+}
