7a143189a9d233ca2765b650a4175c70210c90ce
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-30 14:36:28 +0000

    Update archive 64

---
+++ trunk/Archive2/64/ed30b7e1f9019a/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <memory>
+#include "boost/asio.hpp"
+
+struct Data
+{
+    Data()
+		:
+		socket(io)
+	{
+		socket.open(boost::asio::ip::udp::v4());
+  		socket.bind({boost::asio::ip::udp::v4(), 12345U});
+  		socket.set_option(boost::asio::socket_base::broadcast(true));
+	}
+	
+	~Data()
+    try
+    {
+        socket.close();
+    }
+    catch(std::exception &e)
+    {
+        std::cout << "unhandled exception: " << e.what() << std::endl;
+        return;
+    }
+    catch(...)
+    {
+        std::cout << "unknown unhandled exception" << std::endl;
+        return;
+    }
+	
+    boost::asio::io_service io;
+    boost::asio::ip::udp::socket socket;
+};
+
+int main()
+try
+{
+    std::unique_ptr<Data> data = std::make_unique<Data>(); // works
+    data.reset(); // no error
+    data = std::make_unique<Data>(); // exception thrown on bind
+}
+catch(std::exception &e)
+{
+    std::cout << "main: unhandled exception: " << e.what() << std::endl;
+}
+catch(...)
+{
+    std::cout << "main: unknown unhandled exception" << std::endl;
+}
