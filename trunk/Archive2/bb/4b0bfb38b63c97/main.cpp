0e5ba3bc1b5e799cca9c02c6092f02fb01e8723a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-30 21:46:53 +0000

    Update archive bb

---
+++ trunk/Archive2/bb/4b0bfb38b63c97/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+cout << (10.21/3.2 + 2.0)*(7.85*3.6 + 0.055) << endl;
+cout.precision(5);
+cout << 7/2 << endl;
+cout << 7%2 << endl;
+cout << 7.0/2 <<endl;
+cout << 7/2.0<< endl;
+cout << 7.0/2.0<< endl;
+ return 0;
+}
+
