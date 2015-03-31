47d43cec6f9c958e89b3adbd4ed8e930969acc60
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-31 14:04:02 +0000

    Update archive cf

---
+++ trunk/Archive2/cf/2be26e4e3aa9a3/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <tuple>
+
+template<class T>
+struct TupleComparable
+{
+    bool operator==(const TupleComparable& other) {
+        return static_cast<const T&>(*this).toTuple() == static_cast<const T&>(other).toTuple();
+    }
+};
+
+struct Foo : public TupleComparable<Foo>
+{
+    Foo(int a, int b) : a{a}, b{b} {}
+    
+    std::tuple<int, int> toTuple() const {
+        return std::tie(a, b);
+    }
+
+    int a;
+    int b;
+};
+
+int main(void)
+{
+    auto f = Foo{10,20};
+    auto g = Foo{10,21};
+    auto h = Foo{11,20};
+    
+    std::cout << (f == f) << std::endl;
+    std::cout << (f == g) << std::endl;
+    std::cout << (f == h) << std::endl;
+}
