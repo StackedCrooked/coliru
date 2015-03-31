fc70d69ac6c6462bce647331779dd74c33c488a4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-31 20:10:17 +0000

    Update archive 59

---
+++ trunk/Archive2/59/7f6829b333f283/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+ 
+int main() 
+{
+    std::vector<int> v = {0, 1, 2, 3, 4, 5};
+ 
+    for (const int &i : v) // access by const reference
+        std::cout << i << ' ';
+    std::cout << '\n';
+ 
+    for (auto i : v) // access by value, the type of i is int
+        std::cout << i << ' ';
+    std::cout << '\n';
+ 
+    for (auto&& i : v) // access by reference, the type of i is int&
+        std::cout << i << ' ';
+    std::cout << '\n';
+ 
+    for(int n : {0,1,2,3,4,5}) // the initializer may be a braced-init-list
+        std::cout << n << ' ';
+    std::cout << '\n';
+    
+    struct range {
+        struct iter {
+            int i;
+            bool operator!=(iter other) { return other.i != i; };
+            int operator++() { return ++i; };
+            int operator*() { return i; }
+        };
+        int start; int stop;
+        iter begin() { return iter{start}; }
+        iter end() { return iter{stop}; }
+    };
+    for (auto n : range{0,6})
+        std::cout << n << ' ';
+    std::cout << '\n';
+}
