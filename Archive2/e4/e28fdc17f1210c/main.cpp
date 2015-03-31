c9527ab319f9b98f6aedb6572ead7a58cfddaaf9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-31 13:10:06 +0000

    Update archive e4

---
+++ Archive2/e4/e28fdc17f1210c/main.cpp
@@ -0,0 +1,47 @@
+#include <map>
+#include <iostream>
+#include <string>
+#include <boost/foreach.hpp>
+#include <sstream>
+#include <cstdarg>
+
+enum JType {
+    JInt = 0,
+    JVoid = 1,
+    JString = 2,
+    JBool = 3
+};
+
+std::string JTypeToString(JType type)
+{
+    switch (type)
+    {
+        case(JInt): return "I";
+        case(JVoid): return "V";
+        case(JString): return "Ljava/lang/String;";
+        case(JBool): return "Z";
+        default: return "";
+    }
+    return "";
+}
+
+static const char* MakeSig(JType returnType, int argCount, ...)
+{
+    std::stringstream ss;
+    ss << "(";
+    va_list args;
+    va_start(args, argCount);
+    for (int i = 0; i < argCount; ++i)
+    {
+        JType arg = (JType)va_arg(args, int);
+        ss << JTypeToString(arg);
+    }
+    va_end(args);
+    ss << ")" << JTypeToString(returnType);
+    return ss.str().c_str();
+}
+
+int main()
+{
+    std::cout << MakeSig(JType::JInt, 2, JType::JString, JType::JBool);
+}
