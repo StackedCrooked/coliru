e78ac799de82da79ebd30ea9ab7917dc12eff196
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-31 16:20:09 +0000

    Update archive 71

---
+++ Archive2/71/50c17fe6e7dd2e/main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <algorithm>
+#include <tuple>
+
+#include <boost/mpl/for_each.hpp>
+#include <boost/mpl/range_c.hpp>
+
+#include <boost/operators.hpp>
+
+template<class T>
+struct TupleComparable : public boost::operators<T>
+{
+    friend bool operator==(const T& lhs, const T& rhs) {
+        return lhs.toTuple() == rhs.toTuple();
+    }
+    friend bool operator<(const T& lhs, const T& rhs) {
+        return lhs.toTuple() < rhs.toTuple();
+    }
+};
+
+template <typename F, typename Tuple>
+struct Op{
+    F& f;
+    Tuple& tuple;
+    template <typename Index>
+    void operator()(Index){
+        f(std::get<Index::value>(tuple));
+    }
+};
+
+template <typename F, typename T>
+F for_each(T& obj){
+    F f{};
+    auto tuple = obj.toTuple();
+    boost::mpl::for_each< boost::mpl::range_c<size_t,0,std::tuple_size<decltype(tuple)>::value> >( Op<F, decltype(tuple)>{f, tuple} );
+    return std::move(f);
+}
+
+struct Foo : private TupleComparable<Foo>
+{
+    Foo(int a, int b) : a{a}, b{b} {}
+
+    using Tuple = std::tuple<const int&, const int&>;
+    
+    Tuple toTuple() const {
+        return std::tie(a, b);
+    }
+
+    int a;
+    int b;
+};
+
+struct Printer{
+    template< typename U > void operator()(const U& x)
+    {
+        std::cout << x << ' ';
+    }
+};
+
+int main(void)
+{
+    auto f = Foo{10,20};
+    auto g = Foo{10,21};
+    auto h = Foo{11,20};
+    
+    std::cout << (f != f) << std::endl;
+    std::cout << (f >= g) << std::endl;
+    std::cout << (f == g) << std::endl;
+    std::cout << (f == h) << std::endl;
+    
+    for(const auto& t : {f, g, h}){
+        for_each<Printer>(t);
+        std::cout << std::endl;
+    }
+}
