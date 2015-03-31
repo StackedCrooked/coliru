0875b7bb5c06802f994b620dd1f18b8e123332ff
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-31 17:19:35 +0000

    Update archive 62

---
+++ Archive2/62/f91bbae9fa9e51/main.cpp
@@ -0,0 +1,61 @@
+#include <cstddef>
+#include <iostream>
+#include <memory>
+#include <new>
+#include <type_traits>
+#include <utility>
+
+struct placement_delete {
+  template <typename T>
+  void operator()(T* ptr) {
+    ptr->~T();
+  }
+};
+
+template <typename T>
+using placement_ptr = std::unique_ptr<T, placement_delete>;
+
+template <std::size_t N, std::size_t Align = 0>
+class buffer {
+public:
+  template <typename T, typename...Args>
+  placement_ptr<T> emplace(Args&&...args) {
+    static_assert(std::is_base_of<Base, T>(),
+                  "Only classes derived from Base can go in a buffer.");
+    static_assert(sizeof(T) <= sizeof(space_),
+                  "Type is too big for buffer.");
+    static_assert(alignof(decltype(space_)) % alignof(T) == 0,
+                  "Buffer alignment is insufficient for type.");
+    return placement_ptr<T>{::new(&space_) T(std::forward<Args>(args)...)};
+  }
+private:
+  typename std::conditional<!Align,
+    typename std::aligned_storage<N>::type,
+    typename std::aligned_storage<N, Align>::type
+  >::type space_;
+};
+
+class Base {
+  // ...
+};
+
+class Foo : Base {
+public:
+  Foo() { std::cout << "construct Foo @" << this << '\n'; }
+  ~Foo() { std::cout << "destroy Foo @" << this << '\n'; }
+};
+
+class TooBig : Base {
+  unsigned char lots_of_bytes[1024];
+};
+
+int main() {
+  constexpr std::size_t limit = 42;
+  buffer<limit> buf1;
+  auto fptr = buf1.emplace<Foo>();
+    
+#ifdef BAD_CASE
+    buffer<limit> buf2;
+    auto tbptr = buf2.emplace<TooBig>(); // static_assert fails.
+#endif
+}
