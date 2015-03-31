19396302621de1dbe72546816a6e8793f8d8c992
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-31 15:57:16 +0000

    Update archive e9

---
+++ Archive2/e9/bf15ecff277002/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <tuple>
+
+#include <boost/mpl/for_each.hpp>
+#include <boost/mpl/range_c.hpp>
+
+template<class T>
+struct TupleComparable
+{
+    bool operator==(const TupleComparable& other) const {
+        return static_cast<const T&>(*this).toTuple() == static_cast<const T&>(other).toTuple();
+    }
+};
+
+template<class T>
+struct TupleIterable{
+    using Tuple = std::tuple<const int&, const int&>;
+    
+    template <typename F>
+    struct Op{
+        F& f;
+        Tuple& tuple;
+        template <typename Index>
+        void operator()(Index){
+            f(std::get<Index::value>(tuple));
+        }
+    };
+    
+    template <typename F>
+    F for_each(){
+        F f{};
+        auto tuple = static_cast<const T&>(*this).toTuple();
+        boost::mpl::for_each< boost::mpl::range_c<size_t,0,std::tuple_size<Tuple>::value> >( Op<F>{f, tuple} );
+        return std::move(f);
+    }
+};
+
+struct Foo : public TupleComparable<Foo>, public TupleIterable<Foo>
+{
+    Foo(int a, int b) : a{a}, b{b} {}
+
+    using Tuple = std::tuple<const int&, const int&>;
+    
+    Tuple toTuple() const {
+        return std::tie(a, b);
+    }
+
+    int a;
+    int b;
+};
+
+struct Printer{
+    template< typename U > void operator()(U x)
+    {
+        std::cout << x << ' ';
+    }
+};
+
+int main(void)
+{
+    auto f = Foo{10,20};
+    auto g = Foo{10,21};
+    auto h = Foo{11,20};
+    
+    std::cout << (f == f) << std::endl;
+    std::cout << (f == g) << std::endl;
+    std::cout << (f == h) << std::endl;
+    
+    f.for_each<Printer>();
+}
