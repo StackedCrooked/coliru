f48c9adf37f6a4d03270cf9b3e57fe481c4c12db
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-03-31 13:34:57 +0000

    Update archive 86

---
+++ Archive2/86/daea033a9686a0/main.cpp
@@ -0,0 +1,95 @@
+#include <map>
+#include <iostream>
+#include <string>
+#include <boost/foreach.hpp>
+#include <sstream>
+#include <cstdarg>
+
+enum JType {
+    JInt = 0,
+    JVoid = 1,
+    JString = 2,
+    JBool = 3
+};
+
+std::string JTypeToString(JType type)
+{
+    switch (type)
+    {
+        case(JInt): return "I";
+        case(JVoid): return "V";
+        case(JString): return "Ljava/lang/String;";
+        case(JBool): return "Z";
+        default: return "";
+    }
+    return "";
+}
+
+template<int N>
+struct JTypeList;
+
+template<>
+struct JTypeList<0>
+{
+    void insert(std::stringstream& ss) const {}
+};
+
+template<>
+struct JTypeList<1> : JTypeList<0>
+{
+    JTypeList(JType v) : value(v) {};
+    
+    JTypeList(JTypeList<0> const&, JType v)
+        : value(v) {}
+        
+    void insert(std::stringstream& ss) const
+    {
+        ss << JTypeToString(value);
+    }
+        
+    JType value;
+};
+
+template<int N>
+struct JTypeList : JTypeList<N-1>
+{
+    JTypeList(JTypeList<N-1> const& init, JType last)
+        : JTypeList<N-1>(init, init.value), value(last) {}
+
+    void insert(std::stringstream& ss) const
+    {
+        JTypeList<N-1>::insert(ss);
+        ss << JTypeToString(value);
+    }
+
+    JType value;
+};
+
+JTypeList<2> operator,(JType lhs, JType rhs)
+{
+    return JTypeList<2>(JTypeList<1>(lhs), rhs);
+}
+
+template<int N>
+JTypeList<N+1> operator,(JTypeList<N> const& l, JType next)
+{
+    return JTypeList<N+1>(l, next);
+}
+
+static JTypeList<0> const NoArgs = {};
+
+template<int N>
+std::string MakeSig(JType returnType, JTypeList<N> const& args)
+{
+    std::stringstream ss;
+    ss << "(";
+    args.insert(ss);
+    ss << ")" << JTypeToString(returnType);
+    return ss.str();
+}
+
+int main()
+{
+    std::cout << MakeSig(JInt, (JString, JBool)) << std::endl;
+    std::cout << MakeSig(JInt, NoArgs);
+}
