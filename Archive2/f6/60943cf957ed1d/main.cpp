4b3faab70e23c936614e85e25c4391b683cb8fb4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-01 17:01:04 +0000

    Update archive f6

---
+++ Archive2/f6/60943cf957ed1d/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << el << ' ';
+    }
+    return os;
+}
+
+int main()
+{
+    printf( "Salut" );
+    return 0;
+}
