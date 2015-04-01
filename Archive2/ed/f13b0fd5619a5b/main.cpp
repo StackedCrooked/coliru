e800f405ea8af70159999e0548400bca31d5c0b6
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-01 13:18:24 +0000

    Update archive ed

---
+++ Archive2/ed/f13b0fd5619a5b/main.cpp
@@ -0,0 +1,10 @@
+#include <string.h> 
+#include <stdio.h>
+ 
+int principal ( vazio ) 
+{ 
+    const  de char str [ ]  =  "Quantos personagens que essa seqüência de conter?" ;
+ 
+    printf ( "sem caráter nulo:% zu \ n " , strlen ( str ) ) ; 
+    printf ( "com carácter nulo:% zu \ n " , sizeof str ) ; 
+}
