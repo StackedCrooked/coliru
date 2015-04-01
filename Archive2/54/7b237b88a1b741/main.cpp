d9305fab1845a88f3ebc7c69940358de5ef7c263
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-01 14:02:44 +0000

    Update archive 54

---
+++ Archive2/54/7b237b88a1b741/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+    {
+    //programa
+    cout << "Questão 7  Exercicio de revisão" << endl <<endl;
+    cout << "********\n ********\n********\n ********\n********\n ********\n********\n ********\n" << endl;
+    
+    return 0;
+    }
