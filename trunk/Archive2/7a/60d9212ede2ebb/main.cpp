35cd1e337b7ed942195965ff01bfa2ebafbb0a7f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-01 14:41:36 +0000

    Update archive 7a

---
+++ trunk/Archive2/7a/60d9212ede2ebb/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main()
+    {
+cout << "Aqui é somente texto, 3+4*2. Não efetua a operaçao." << endl;
+
+cout << "3+4*2= " << 3+4*2 << endl;
+
+cout << "7/2= " << 7/2 << endl;
+
+cout << "7%2= " << 7%2 << endl;
+
+cout << "10%4= " << 10%4 << endl;
+
+cout << "7.0/2= " << 7.0/2 << endl;
+
+cout << "7/2.0= " << 7/2.0 << endl;
+
+cout << "7.0/2.0= " << 7.0/2.0 << endl;
+
+cout << "10.0/2= " << 10.0/2 << endl;
+
+cout << "256*256+1024*16= " << 256*256+1024*16 << endl;
+
+cout << " 2*5*5+3*5+7= " << 2*5*5+3*5+7 << endl;
+
+cout << "(5+3*2)= " << (5+3*2) << endl;
+
+cout << "((5+3)*2= " << ((5+3)*2) << endl;
+
+
+
+
+
+return 0;
+    }
+
