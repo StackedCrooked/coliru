f4ae6c02e0f919d760c645e8520100aed65e966b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-01 14:51:23 +0000

    Update archive 53

---
+++ trunk/Archive2/53/4c55c87014ad38/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+    {
+    cout << "1A)False\n";
+    cout << "1B)True\n";
+    cout << "1C)True\n";
+    cout << "1D)False\n";
+    cout << "1E)False\n";
+    cout << "3)cout << X+Y+Z;\n";
+    cout << "4A)//\n";
+    cout << "4B)\tcout <<\"\";\n ";
+    cout << "4C)/ %\n";
+    cout << "4D)O mais interno\n";
+    cout << "6)Asteriscos em varias linhas\n";
+
+
+
+    cout << "\nDavi is love,\nDavi is life.";
+    return 0;
+    }
