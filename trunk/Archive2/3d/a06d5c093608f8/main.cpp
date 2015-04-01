36086af025bcc08b7f95511e5b921f5f1ac1f662
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-01 15:59:00 +0000

    Update archive 3d

---
+++ trunk/Archive2/3d/a06d5c093608f8/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+    {
+    //programa
+    cout << "Questão 8" <<endl <<endl;
+    cout << "123456/13 = " <<123456.0/13 ; cout << "g" ;
+    return 0;
+    }    
