664d60a6a17992047eed45694cdfd89f38a74cce
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-01 13:06:37 +0000

    Update archive 48

---
+++ trunk/Archive2/48/a7ff28e616a8f1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+    {
+    cout << "256*256+1024*16= " << 256*256+1024*16 << endl;
+    cout << "Aqui é somente texto,3+4*2.Não efetua a operação. << endl;" << endl;
+    cout << "3+4*2=" << 3+4*2 << endl;
+    cout << "7/2 =" << 7/2 << endl;
+    cout << "7%2 =" << 7%2 << endl;
+    cout << "10%4 =" << 10%4 << endl;
+    cout << "7.0/2 =" << 7.0/2 << endl;
+    cout << "7/2.0 =" << 7/2.0 << endl;
+    cout << "7.0/2.0 =" << 7.0/2.0 << endl;
+    cout << "2*5*5+3*5+7 =" << 2*5*5+3*5+7 << endl;
+    cout << "(5+3*2)= " << (5+3*2) << endl;
+    cout << "((5+3)*2)" << ((5+3)*2)  << endl;
+    return 0;
+    }
