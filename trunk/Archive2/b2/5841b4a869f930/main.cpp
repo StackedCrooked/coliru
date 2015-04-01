44277a0b0490e0080812bbc2b9d2bcd8dabff0b0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-01 23:47:02 +0000

    Update archive b2

---
+++ trunk/Archive2/b2/5841b4a869f930/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+    {
+     cout << "O texto após é impresso ?" << endl;// imprime
+     cout << "Quando usamos\n ele quebra a linha ?" << endl;
+     cout << 7%4 << endl;
+     cout << "Este é um programa em C++" << endl;
+     cout << "Este é um programa\n em C++" << endl;
+     cout << "Este\n é\n um\n programa\n em\n C++" << endl;
+     cout << 2+7+9 << endl;
+     cout << 2 << endl;
+     cout << "x=" << 2 << endl;
+     cout << "*\n**\n***\n****\n*****" << endl;
+     cout << "********\n ********\n********\n ********\n********\n ********\n********\n ********" << endl;
+     cout << ((123456.0/3)*2)/13 << endl;
+
+
+
+
+
+
+
+
+
+
+     return 0;
+     }
