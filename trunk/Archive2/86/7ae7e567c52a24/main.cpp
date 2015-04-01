46cbd22ef9f49c1766994f42c19caca7794b8600
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-01 13:47:40 +0000

    Update archive 86

---
+++ trunk/Archive2/86/7ae7e567c52a24/main.cpp
@@ -0,0 +1,43 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
+data Shape = Square { something :: Int } | Circle
+
+class Renderer a where
+    initial :: a
+    render :: (Renderer a) => a -> Shape -> IO ()
+
+data RendererI = forall a. (Renderer a) => RendererImpl a
+
+instance Renderer RendererI where
+    render (RendererImpl a) x = render a x
+    
+data RendererFoo = Foo { internalData :: Int }
+
+instance Renderer RendererFoo where
+    initial = Foo 100
+    render r (Square x) = putStr ("square: " ++ (show x) ++ "\n")
+    render r Circle = putStr ("circle, internalData: " ++ show (internalData r) ++ "\n")
+    
+data RendererBar = Bar { uglyData :: Int }
+
+instance Renderer RendererBar where
+    initial = Bar 300
+    render r (Square x) = putStr ("square: " ++ (show x) ++ "\n")
+    render r Circle = putStr ("circle, uglyData: " ++ show (uglyData r) ++ "\n")
+
+foo = RendererImpl (initial :: RendererFoo)
+bar = RendererImpl (initial :: RendererBar)
+baz = RendererImpl (initial :: RendererI)
+
+renderSpecial :: RendererI -> IO ()
+renderSpecial r = render r (Square 666)
+
+thingies :: [Shape]
+thingies = [Square 123, Circle]
+renderers :: [RendererI]
+renderers = [foo, bar]
+    
+main :: IO ()
+main = do
+    mapM_ (\x -> mapM_ (render x) thingies) renderers
+    renderSpecial foo
