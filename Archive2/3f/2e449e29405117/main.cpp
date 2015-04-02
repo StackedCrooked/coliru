b7f4f660e2cf25982badfd0a69c90451a3644f8a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-02 11:17:01 +0000

    Update archive 3f

---
+++ Archive2/3f/2e449e29405117/main.cpp
@@ -0,0 +1,56 @@
+#include <iterator>
+
+template <int i> struct rank : rank<i+1> {};
+template <> struct rank<10> {};
+constexpr rank<0> call_ranked{};
+
+#define AUTO_RETURN(...) -> decltype(__VA_ARGS__) {return (__VA_ARGS__);}
+
+namespace detail {
+    
+    template <typename T,
+              typename = typename std::enable_if<std::is_array<typename std::remove_reference<T>::type>{}>::type>
+    auto adl_begin( rank<0>, T&& t )
+      AUTO_RETURN(t + 0)
+    template <typename T,
+              typename = typename std::enable_if<std::is_array<typename std::remove_reference<T>::type>{}>::type>
+    auto adl_end  ( rank<0>, T&& t )
+      AUTO_RETURN(t + std::extent<typename std::remove_reference<T>::type>{})
+      
+    template <typename T>
+    auto adl_begin( rank<1>, T&& t )
+      AUTO_RETURN(std::forward<T>(t).begin())
+    template <typename T>
+    auto adl_end( rank<1>, T&& t )
+      AUTO_RETURN(std::forward<T>(t).end  ())
+      
+    template <typename T>
+    auto adl_begin( rank<2>, T&& t )
+      AUTO_RETURN(begin(std::forward<T>(t)))
+    template <typename T>
+    auto adl_end  ( rank<2>, T&& t )
+      AUTO_RETURN(end  (std::forward<T>(t)))
+}
+
+template <typename T>
+auto adl_begin(T&& t) AUTO_RETURN(detail::adl_begin(call_ranked, std::forward<T>(t)))
+template <typename T>
+auto adl_end  (T&& t) AUTO_RETURN(detail::adl_end  (call_ranked, std::forward<T>(t)))
+
+/// TEST:
+
+namespace A {
+    struct C{ static int arr[2]; int* begin(){return arr;} int* end(){return arr+2;}};
+    int* begin(C&) {return 0;} int* end(C&) {return 0;}
+    int C::arr[2]{1, 2};
+}
+
+int main()
+{
+    A::C c, arr[10];
+    auto begin = adl_begin(c), 
+         end   = adl_end(c);
+    auto begin_arr = adl_begin(arr), 
+         end_arr   = adl_end(arr);
+    (void)begin, (void)end;(void)begin_arr, (void)end_arr;
+}
