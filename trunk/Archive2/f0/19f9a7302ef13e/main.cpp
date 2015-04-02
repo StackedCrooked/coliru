411c076894e9648b7eea115abb0e7d15fcf94854
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-02 11:18:21 +0000

    Update archive f0

---
+++ trunk/Archive2/f0/19f9a7302ef13e/main.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <iterator>
+
+int main()
+{
+	std::istringstream s( "-0.003  79350   -0.267  147" );
+	size_t n = std::distance( std::istream_iterator<std::string>( s ) , std::istream_iterator<std::string>() );
+	std::vector<std::string> vec(n);
+	std::cout << vec.size() << std::endl;
+	
+	return EXIT_SUCCESS;
+}
+	
+	
