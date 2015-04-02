09269dfcf82d11c9f7b3fe692101c83e643b0563
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-02 22:37:57 +0000

    Update archive f5

---
+++ trunk/Archive2/f5/ca9f9429ae4ddb/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+namespace detail {
+  struct csv_tag { };
+
+  struct csv_impl {
+    struct indirection {
+      operator csv_impl const& () const {
+        return impl._stream << ',', void(), impl;
+      }
+
+      csv_impl const& impl;
+    };
+
+    template<class T>
+    friend
+    indirection operator<< (csv_impl const& impl, T&& val) {
+      return {( impl._stream << val, void(), impl )};
+    }
+
+    ~csv_impl () {
+      _stream << "\n";
+    }
+
+    std::ostream& _stream;
+  };
+
+  csv_impl operator<< (std::ostream& os, csv_tag) {
+    return { os };
+  }
+}
+
+namespace { detail::csv_tag csv; }
+
+int main () {
+  std::cout << csv << 1 << "hello" << 3.14f;
+  std::cout << csv << 0 << "world" << 4.31f;
+}
