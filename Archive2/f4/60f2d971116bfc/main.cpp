c35adc3e36f913bd0910de81d4271297b3f5caf4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-02 14:10:31 +0000

    Update archive f4

---
+++ Archive2/f4/60f2d971116bfc/main.cpp
@@ -0,0 +1,14 @@
+//Title of this code
+
+#include <iostream>
+using namespace std;
+
+int secondmain()
+{
+    cout << "Breaking all the rules!\n";
+}   
+int main()
+{
+    cout << "Let the rhythem hit em!\n";
+    secondmain();
+}
