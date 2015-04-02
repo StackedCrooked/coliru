d643289f7f14ce0d6b94ec1ef0beceb0b1040509
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-02 19:24:36 +0000

    Update archive 97

---
+++ Archive2/97/89a8b9e3e17f2a/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+template <typename T>
+class A
+{
+public:
+    static int i;
+};
+
+template <typename T>
+int A<T>::i = 0;
+
+
+int main()
+{    
+    A<float>::i = 4;
+    A<int>::i = 3;
+    
+    std::cout << A<float>::i << std::endl;
+    return 0;
+}
