2cccf4625634df98adc9158aad1bbe0dcb8dc8c2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-02 02:09:37 +0000

    Update archive 3b

---
+++ Archive2/3b/5a27d85ce60261/main.cpp
@@ -0,0 +1,130 @@
+#include <boost/graph/adjacency_list.hpp>
+#include <iostream>
+
+struct Vertex {
+    int id;
+    const char* name;
+
+    Vertex(int i = -1, const char* name = "default") : id(i), name(name) {}
+};
+
+template <typename It> boost::iterator_range<It> mir(std::pair<It, It> const& p) {
+    return boost::make_iterator_range(p.first, p.second);
+}
+template <typename It> boost::iterator_range<It> mir(It b, It e) {
+    return boost::make_iterator_range(b, e);
+}
+
+typedef typename boost::adjacency_list<
+        boost::vecS, boost::vecS,
+        boost::undirectedS,
+        Vertex,                                      // bundled properties (id, name)
+        boost::property<boost::edge_weight_t, float> // interior property
+    > Graph;
+
+Graph make() {
+    Graph graph;
+
+    auto Jeanie   = add_vertex(Vertex { 0, "Jeanie" },   graph);
+    auto Debbie   = add_vertex(Vertex { 1, "Debbie" },   graph);
+    auto Rick     = add_vertex(Vertex { 2, "Rick" },     graph);
+    auto John     = add_vertex(Vertex { 3, "John" },     graph);
+    auto Amanda   = add_vertex(Vertex { 4, "Amanda" },   graph);
+    auto Margaret = add_vertex(Vertex { 5, "Margaret" }, graph);
+    auto Benjamin = add_vertex(Vertex { 6, "Benjamin" }, graph);
+
+    add_edge(Jeanie, Debbie,   0.5f, graph);
+    add_edge(Jeanie, Rick,     0.2f, graph);
+    add_edge(Jeanie, John,     0.1f, graph);
+    add_edge(Debbie, Amanda,   0.3f, graph);
+    add_edge(Rick,   Margaret, 0.4f, graph);
+    add_edge(John,   Benjamin, 0.6f, graph);
+
+    return graph;
+}
+
+Graph reduce(Graph graph) {
+
+    /* vertex iterator */
+    using vertex_descriptor = boost::graph_traits<Graph>::vertex_descriptor;
+
+    std::cout << "# of vertices: " << num_vertices(graph) << "\n";
+    std::cout << "# of edges:    " << num_edges(graph)    << "\n";
+
+    std::set<vertex_descriptor> to_remove;
+
+    /* iterator throught the graph  */
+    for (auto self : mir(vertices(graph)))
+    {
+        std::cout << graph[self].name << (boost::empty(mir(out_edges(self, graph)))? " has no children " : " is the parent of ");
+
+        for(auto edge : mir(out_edges(self, graph))) {
+            auto weight    = boost::get(boost::edge_weight, graph, edge);
+            auto mid_point = target(edge, graph);
+
+            if (to_remove.count(mid_point)) // already elided
+                break;
+
+            if (weight > 0.3f) {
+                std::set<vertex_descriptor> traversed;
+                for (auto hop : mir(out_edges(mid_point, graph))) {
+                    auto hop_target = target(hop, graph);
+
+                    if (hop_target != self)
+                        add_edge(self, hop_target, graph);
+                    std::cout << "\n DEBUG: " << graph[self].name << "  " << graph[mid_point].name << "  " << graph[hop_target].name << " ";
+                }
+                std::cout << "\n";
+
+                clear_vertex(mid_point, graph);
+                to_remove.insert(mid_point);
+            }
+
+            std::cout << graph[mid_point].name;
+        }
+
+        std::cout << "\n\n";
+    }
+
+    for(auto vd : to_remove)
+    {
+        clear_vertex(vd, graph);
+        remove_vertex(vd, graph);
+    }
+
+    std::cout << "# of vertices: " << num_vertices(graph) << "\n";
+    std::cout << "# of edges:    " << num_edges(graph)    << "\n";
+
+    return graph;
+}
+
+void save(Graph const& g, const char* fname);
+
+int main() {
+    auto const g = make();
+
+    auto const h = reduce(g);
+
+    save(g, "before.dot");
+    save(h, "after.dot");
+}
+
+#include <boost/graph/graphviz.hpp>
+#include <boost/property_map/property_map.hpp>
+#include <boost/property_map/function_property_map.hpp>
+#include <boost/property_map/transform_value_property_map.hpp>
+#include <boost/format.hpp>
+#include <fstream>
+
+void save(Graph const& g, const char* fname) {
+    std::ofstream ofs(fname);
+    using namespace boost;
+
+    write_graphviz(
+            ofs,
+            g,
+            make_label_writer(make_function_property_map<Graph::vertex_descriptor, std::string>([&] (Graph::vertex_descriptor v){ return g[v].name; })),
+            make_label_writer(make_transform_value_property_map([](float v){return boost::format("%1.1f") % v;}, boost::get(edge_weight, g)))
+        );
+}
+
