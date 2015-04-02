dab0d6d310d661e8df1a3f011ba044f515200b5a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-02 02:19:57 +0000

    Update archive fc

---
+++ trunk/Archive2/fc/78a2a243903551/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+
+std::string foo(
+    void (*bar)(
+		void (*collectFunc)(
+			std::string message
+		)
+	)
+) {
+    std::string result = "";
+    auto collectFunc = [&](
+		std::string message
+	) {
+		result = result + message;
+	};
+    
+    bar(collectFunc);
+    
+    return result;
+}
+
+int main()
+{
+    std::string result = foo([&](void (*collect)(std::string message)) {
+        collect("asdf");
+        collect("qwer");
+    });
+    
+    std::cout << result << std::endl;
+    
+    return 0;
+}
+
