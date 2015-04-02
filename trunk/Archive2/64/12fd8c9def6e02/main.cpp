ec5336af2d7f7cbbf0c3ea2f9898b24cfe156304
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-02 23:20:01 +0000

    Update archive 64

---
+++ trunk/Archive2/64/12fd8c9def6e02/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+    using namespace std;
+    class Numbers2;
+
+    class Numbers
+    {
+    private:
+        int num;
+    public:
+        Numbers(){num = 0;};
+        friend void Numbers2::getNumbers(Numbers &);
+    };
+
+    class Numbers2
+    {
+    private:
+
+    public:
+        void getNumbers(Numbers &);
+    };
+
+    void Numbers2::getNumbers(Numbers &numbs)
+        {
+            cout << numbs.num << endl;
+        }
+
+    int main()
+    {
+        Numbers num1;
+        Numbers2 num2;
+
+        num2.getNumbers(num1);
+    }
