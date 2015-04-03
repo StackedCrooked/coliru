96981190a5db59c52ab65d52730321a3f219a7ae
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-03 07:36:38 +0000

    Update archive c2

---
+++ trunk/Archive2/c2/c218a287aa9f8b/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <functional>
+
+typedef std::function<std::string(void)> acceptedFunc;
+template<typename Func, typename = typename std::result_of<Func()>::type>
+void asdf(Func doThing) {
+    auto val = doThing();
+    std::cout << typeid(val).name() << " -- " << typeid(acceptedFunc).name() << std::endl;
+    if(typeid(val) == typeid(acceptedFunc)) {
+        std::cout << "yay" << std::endl;
+    }
+}
+
+
+int main()
+{
+    asdf([&]() {
+        return (std::string)"qwer";
+    });
+    // rejected
+    asdf([](int) {});
+    
+    
+    
+    
+    std::cout << "done" << std::endl;
+    
+    return 0;
+}
+
+
