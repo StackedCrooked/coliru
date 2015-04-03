9d7eda5ea9ba2f032382d4f22c583f1989d8aff1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-03 07:39:59 +0000

    Update archive 46

---
+++ trunk/Archive2/46/686f41ebdba6b8/main.cpp
@@ -0,0 +1,18 @@
+#include <type_traits>
+
+template<typename Func, typename = typename std::result_of<Func&()>::type>
+void asdf(Func doThing)
+{
+    auto val = doThing();
+}
+
+
+int main()
+{
+    asdf([] {
+        return 0;
+    });
+    
+    // rejected
+    asdf([](int) {});
+}
