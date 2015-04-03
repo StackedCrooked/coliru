f0cab939828d37245077c68f48fe051ba66b625c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-03 13:09:25 +0000

    Update archive 89

---
+++ trunk/Archive2/89/493d04025c4e03/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    int i;
+//    int cnt[100]{};
+    for(i=0;i<60;i+5)
+    {   
+        break;
+//      cout<<setw(15)<<i<<" - "<<i+5<<setw(15)<<i<<setw(10)<<i+5
+//      <<setw(15)<<i+2.5<<setw(15)<<cnt[i/5]<<endl;
+    }
+}
