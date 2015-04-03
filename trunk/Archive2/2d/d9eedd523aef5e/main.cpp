2433c42d2da7560abcfffac277cfd217f9c8f10a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-03 19:08:28 +0000

    Update archive 2d

---
+++ trunk/Archive2/2d/d9eedd523aef5e/main.cpp
@@ -0,0 +1,31 @@
+// import random;
+// for i in range(2,17): print "[0,{}):\t{}".format(i,random.randrange(i))
+
+#include <cstdio>
+#include <random>
+
+struct random_device_seed_seq {
+    using result_type = std::uint_least32_t;
+    
+    std::random_device random_device;
+    
+    template<typename InputIterator>
+    void generate(InputIterator rb, InputIterator re) {
+        //int i = 0;
+        for (;rb != re; ++rb) {
+            *rb = random_device();
+            // ++i;
+        }
+        //std::printf("state size: %d\n", i);
+    }
+};
+
+int main() {
+    random_device_seed_seq seed;
+    std::mt19937 eng(seed);
+    std::uniform_int_distribution<> dist;
+    using param = std::uniform_int_distribution<>::param_type;
+   
+    for (int i = 2; i < 17; ++i)
+        std::printf("[0,%d):\t%d\n", i, dist(eng, param{0, i-1}));
+}
