3577b6ad3423882deae167bf9ee9183c5753b823
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-03 14:12:47 +0000

    Update archive e4

---
+++ Archive2/e4/12a56c4db4a194/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+template <class T> class pure {
+public:
+    pure(T const& attr) {
+        this->attr = attr;
+    }
+
+    ~pure() {
+    }
+
+    T& operator=(T const& attr) {
+        return attr;
+    }
+
+    operator T() {
+        return this->attr;
+    }
+
+private:
+    T attr;
+};
+
+int main()
+{
+    pure<int> i(3);
+    std::cout << (int)i;
+    std::cout << (long)i;
+}
