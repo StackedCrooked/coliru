437485e8368633789d11a5d3e8550e5c900498bf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-03 11:10:48 +0000

    Update archive 19

---
+++ Archive2/19/9fc3048002f9eb/cmd.sh
@@ -0,0 +1,6 @@
+echo "no RVO"
+clang++ -std=c++11 -Os -Wall -pedantic -pthread main.cpp -Wno-unused-variable -fno-elide-constructors && ./a.out
+echo "C++11 RVO"
+clang++ -std=c++11 -Os -Wall -pedantic -pthread main.cpp -Wno-unused-variable && ./a.out
+echo "C++14 RVO"
+clang++ -std=c++14 -Os -Wall -pedantic -pthread main.cpp -Wno-unused-variable && ./a.out
