beb2156a93a33adf3e77ff1a8efcbffbcc2630a3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-03 04:21:27 +0000

    Update archive e6

---
+++ trunk/Archive2/e6/957b435e79441a/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <random>
+
+typedef unsigned int uint32;
+
+class Random {
+public:
+    Random() = default;
+    Random(std::mt19937::result_type seed) : eng(seed) {}
+
+    uint32 DrawNumber();
+    uint32 DrawNumber(uint32 ub);
+
+private:
+    std::mt19937 eng{std::random_device{}()};
+    std::uniform_int_distribution<uint32> uniform_dist{0, UINT32_MAX};
+};
+
+uint32 Random::DrawNumber()
+{
+    return uniform_dist(eng);
+}
+
+uint32 Random::DrawNumber(uint32 ub)
+{
+    return uniform_dist(eng, std::uniform_int_distribution<uint32>::param_type(0, ub));
+}
+
+int main()
+{
+  Random r;
+  std::cout << r.DrawNumber() << std::endl;
+  std::cout << r.DrawNumber(42) << std::endl;
+}
