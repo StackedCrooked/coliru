9a4fe573a1735f0353cf3138fbd0779225358501
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-04 18:49:03 +0000

    Update archive a5

---
+++ Archive2/a5/4fce9097c11b3b/main.cpp
@@ -0,0 +1,40 @@
+#include <cstddef>
+#include <iterator>
+#include <memory>
+#include <utility>
+
+template<typename T, typename Category>
+class any_iterator : public std::iterator<Category, T, std::ptrdiff_t, T*, T&> {
+public:
+    template<typename I>
+    any_iterator(I iterator) : ptr(new derived<I>(std::move(iterator))) { }
+
+private:
+    struct base {
+        virtual ~base() = default;
+    };
+
+    template<typename I>
+    struct derived {
+        explicit derived(I iterator) : iterator(std::move(iterator)) { }
+
+        I iterator;
+    };
+
+    std::unique_ptr<base> ptr;
+};
+
+// example:
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+int f(any_iterator<int, std::input_iterator_tag> begin, any_iterator<int, std::input_iterator_tag> end) {
+    return std::accumulate(begin, end, 0);
+}
+
+int main() {
+    std::vector<int> xs{1, 2, 3};
+    std::cout << f(xs.begin(), xs.end());
+    return 0;
+}
