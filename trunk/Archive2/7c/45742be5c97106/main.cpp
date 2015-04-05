e2109dcf9dac9b914dc65a226ca58734293f1eac
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-05 02:19:14 +0000

    Update archive 7c

---
+++ trunk/Archive2/7c/45742be5c97106/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string test = "";
+    cout<<"Enter a sentence: ";
+    getline(cin, test);
+    cout<<test<<endl;
+}
