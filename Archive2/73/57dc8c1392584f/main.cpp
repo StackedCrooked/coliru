d4ae7409636d7ffc09b1e434f6a3e13aa96eb1ca
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-05 16:17:06 +0000

    Update archive 73

---
+++ Archive2/73/57dc8c1392584f/main.cpp
@@ -0,0 +1,29 @@
+/*Substituablity: Base class pointer can be assigned with either base class object or derived class object
+-> we can achieve this by overriding.( this can be achieved using virtual function)
+-> Compiler creates one virtual table(if you make a function virtual ? )
+-> Virtual Function
+*/
+
+#include<iostream>
+class a{
+public:
+    virtual void m1()
+    {
+        std::cout << "m1 of A" <<std::endl;
+    }
+};
+class b: public a
+{
+public:
+    void m1()
+    {
+        std::cout<<"m1 of b" << std::endl;
+    }
+};
+int main()
+{
+    a *A;
+    b B;
+    A = &B;
+    A->m1();  // always points to M1() of b
+}
