0f5d62ff897a9713c4f795fdc38af7b720a198ee
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-05 22:49:25 +0000

    Update archive 79

---
+++ Archive2/79/72330390a7287a/main.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <type_traits>
+
+template<std::size_t tag = 0>//tag
+class Coord {
+public:
+    using Coordinate = Coord<>;
+    using CoordOfCube = Coord<1>;
+    using CoordInCube = Coord<2>;
+    int x;
+    int y;
+    int z;
+
+    constexpr Coord(int x = 0, int y = 0, int z = 0) : x(x), y(y), z(z) {}
+
+    constexpr bool operator==(const Coord & rhs) const {
+        return x == rhs.x && y == rhs.y && z == rhs.z;
+    }
+    
+    template<typename T>
+    constexpr Coord<tag> operator^(const T factor) const {
+        static_assert(std::is_arithmetic<T>::value, "you’re wrong");
+        return Coord<tag>(x * factor, y * factor, z * factor);
+    }
+    
+    template<typename T>
+    auto operator%(const T factor) -> typename std::remove_reference<decltype(*this)>::type const {
+        return typename std::remove_reference<decltype(*this)>::type(x * factor, y * factor, z * factor);
+    }
+
+    template<typename T>
+    auto operator*(const T factor) -> typename std::enable_if<std::is_arithmetic<T>::value, typename std::remove_reference<decltype(*this)>::type>::type const {
+        return typename std::remove_reference<decltype(*this)>::type(x * factor, y * factor, z * factor);
+    }
+};
+
+int main() {
+    Coord<0> c;
+    c * 1;
+    c * "";
+    c % 1;
+    c % "";
+    c ^ 1;
+    c ^ "";
+}
