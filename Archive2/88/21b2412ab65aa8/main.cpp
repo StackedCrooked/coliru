840d41184992aa9805f459eb0b50058fd405aa0c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-06 22:46:41 +0000

    Update archive 88

---
+++ Archive2/88/21b2412ab65aa8/main.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main()
+    {
+    double x,y;
+    x=2.2;
+    y=3.3;
+    cout.precision(4);
+    cout << x/y << endl;
+
+    return 0;
+    }
