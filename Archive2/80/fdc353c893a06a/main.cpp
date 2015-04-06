b3b837cf5a11e719881cf1e0bc19adadc297ef9f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-06 23:25:51 +0000

    Update archive 80

---
+++ Archive2/80/fdc353c893a06a/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main()
+    {
+    string biscoito;
+
+    cout << "Digite seu nome:" ;
+    cin >> biscoito;
+    cout << "\nOlá " << biscoito << endl;
+
+
+    int num1;
+    int num2;
+    int soma;
+
+    cout << "Digite um número" << endl;
+    cin >> num1;
+    cout << "Digite outro número" << endl;
+    cin >> num2;
+    soma=num1+num2;
+    cout << soma;
+    
+    int pontos;
+    
+    return 0;
+    }
