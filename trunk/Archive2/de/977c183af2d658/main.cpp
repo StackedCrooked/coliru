e94460fb816b78ec93d53a94b1e567a466e14bf4
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-06 21:13:23 +0000

    Update archive de

---
+++ trunk/Archive2/de/977c183af2d658/main.cpp
@@ -0,0 +1,31 @@
+#include  <iostream>
+#include <string>
+using namespace std;
+int main()
+     {
+     int numero;
+     int num1;
+     int num2;
+     int soma;
+     string biscoito;
+
+     cout <<"digite um número" << endl;
+     cin >> num1;
+     cout <<"digite outro número" <<endl;
+     cin >> num2;
+     soma=num1+num2;
+     cout << soma;
+
+
+
+    cout << "digite seu nome:";
+    cin >> biscoito;
+    cout <<"\n Olá " << biscoito <<endl;
+    cout <<"digite um numero" << endl;
+
+
+
+
+    return 0;
+
+     }
