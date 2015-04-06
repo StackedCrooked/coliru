c03bf8918fb47cc61963a3a941f87e2f9f8ff719
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-06 00:00:44 +0000

    Update archive 3e

---
+++ trunk/Archive2/3e/e9037278d0850f/main.cpp
@@ -0,0 +1,64 @@
+#include <boost/core/demangle.hpp>
+#include <execinfo.h>
+#include <string>
+#include <vector>
+
+class with_stack_trace {
+public:
+    std::vector<std::string> const& stack_trace() const {
+        return entries;
+    }
+
+protected:
+    with_stack_trace() {
+        std::vector<void*> buffer(4);
+        int n;
+        while ((n = backtrace(buffer.data(), buffer.size())) == static_cast<int>(buffer.size())) {
+            buffer.resize(buffer.size() * 2);
+        }
+        auto symbols = backtrace_symbols(buffer.data(), n);
+        for (int i = 0; i < n; ++i) {
+            std::string line = symbols[i];
+            auto from = line.find('(');
+            auto to = line.find('+');
+            if (from != std::string::npos && to != std::string::npos) {
+                auto mangled_name = line.substr(from + 1, to - 1 - from);
+                auto demangled_name = boost::core::demangle(mangled_name.c_str());
+                line.replace(from + 1, to - 1 - from, demangled_name.c_str(), demangled_name.size());
+            }
+            entries.emplace_back(line);
+        }
+    }
+
+private:
+    std::vector<std::string> entries;
+};
+
+// example:
+#include <iostream>
+#include <stdexcept>
+
+class my_exception : public std::runtime_error, public virtual with_stack_trace {
+public:
+    using std::runtime_error::runtime_error;
+};
+
+void g() {
+    throw my_exception("oops!");
+}
+
+void f() {
+    g();
+}
+
+int main() {
+    try {
+        f();
+    } catch (my_exception const& ex) {
+        std::cerr << ex.what() << '\n';
+        for (auto&& entry : ex.stack_trace()) {
+            std::cerr << "  " << entry << '\n';
+        }
+    }
+    return 0;
+}
