d10eb4938365dcc02055a9b96ad95d493b65c197
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-06 22:19:24 +0000

    Update archive 20

---
+++ trunk/Archive2/20/2365e7828d34d2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main()
+    {
+    int pontos;
+
+    pontos=10 ;
+    cout << pontos+2 << endl;
+
+    double a,b,c;
+    a=1000;
+    b=2000;
+    c=3000;
+    cout.precision(10);
+    cout << a*b*c << endl;
+    
+    
+    return 0;
+    }
+
