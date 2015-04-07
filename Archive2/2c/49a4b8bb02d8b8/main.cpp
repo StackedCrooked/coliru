0b7f43c41445c22829b1a85b9945087909476f5f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-07 17:16:42 +0000

    Update archive 2c

---
+++ Archive2/2c/49a4b8bb02d8b8/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+main()
+{
+    int g; 
+    int tmin; 
+    int tmax; 
+    int stmin; 
+    int stmax; 
+    int medmin;
+    int medmax; 
+    do
+    {
+   cout << "inserisci la temperatura minima:"; 
+   cin>> tmin;
+   cout<< endl;
+   cout << "inserisci la temperatura massima:";
+   cin>> tmax;
+   cout<< endl;
+     }
+   while ( g++ );
+  system ("pause");
+  return EXIT_SUCCESS;
+} //fine programma
