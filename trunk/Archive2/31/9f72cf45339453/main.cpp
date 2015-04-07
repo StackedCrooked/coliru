590d0c7a6c531a8b1f1398fa2efcfb76960574ed
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-07 20:18:08 +0000

    Update archive 31

---
+++ trunk/Archive2/31/9f72cf45339453/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <string>
+ 
+int main()
+{
+    const std::shared_ptr<std::string> p1(new std::string("asdf1234"));
+    std::shared_ptr<std::string> p2(p1);
+    std::cout << "*p1 = '" << *p1 << "'\n";
+    std::cout << "*p2 = '" << *p2 << "'\n";
+        
+    // Remove "const" to get it to compile.
+    const std::unique_ptr<std::string> x(new std::string("qwerty"));
+    std::unique_ptr<std::string> y(std::move(x));
+    std::cout << "*y = '" << *y << "'\n";
+    
+    if (x.get() == nullptr) {
+        std::cout << "x is nullptr\n";
+    } else {
+        std::cout << "x is not nullptr\n";
+    }
+}
