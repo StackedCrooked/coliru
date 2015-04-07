a940543f6360506c67e5c02318929a7f0ccf438f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-07 23:31:21 +0000

    Update archive 42

---
+++ Archive2/42/59615d93bd4057/main.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <string>
+#include <iostream>
+ 
+class A {
+    public:
+        virtual ~A() {}
+        virtual std::vector<int> Choose(double* total) const = 0;
+        
+        virtual std::vector<int> Choose() const  { 
+            std::cout << "Choose without parameter" << std::endl;
+            return Choose(nullptr);
+        }
+};
+
+class B : public A {
+    public:
+        virtual std::vector<int> Choose(double* total) const {
+            if (total) {
+                *total = 42.0;
+            }
+            
+            return std::vector<int>();
+        }
+};
+
+ 
+int main()
+{
+    double total;
+    B b;    
+    b.Choose(&total);    
+    b.A::Choose();
+    std::cout << total << std::endl;
+}
