1b1d367dbf5eb1d84c022ce47efae2666fa7d321
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-08 19:39:03 +0000

    Update archive 01

---
+++ Archive2/01/602738771c8d66/cmd.sh
@@ -0,0 +1,2 @@
+ln -s main.cpp main.c
+gcc -ansi -O2 -Wall -pedantic -pthread main.c && ./a.out
---
+++ Archive2/01/602738771c8d66/main.cpp
@@ -0,0 +1,10 @@
+int main()
+{
+    int i;
+    i = 2;
+    i += 2;
+    int x;
+    x = 3;
+    x += 3;
+    return 0;
+}
