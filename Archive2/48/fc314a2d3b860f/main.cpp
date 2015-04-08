9434d0b31baf71db24000a6baa07c5a15b10a6fc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-08 08:38:11 +0000

    Update archive 48

---
+++ Archive2/48/fc314a2d3b860f/main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : demo.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    puts("Hello World!!!");
+	return EXIT_SUCCESS;
+}
