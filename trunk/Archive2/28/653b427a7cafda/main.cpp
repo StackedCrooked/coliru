f5954337ae1114759a04972ebdd16551b12e6c7e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-08 07:40:32 +0000

    Update archive 28

---
+++ trunk/Archive2/28/653b427a7cafda/main.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <iostream>
+#include <numeric>
+#include <random>
+#include <functional>
+ 
+int main()
+{
+    std::list<int> l(10);
+    std::iota(l.begin(), l.end(), -4);
+ 
+    std::vector<std::reference_wrapper<int>> v(l.begin(), l.end());
+    // can't use shuffle on a list (requires random access), but can use it on a vector
+    std::shuffle(v.begin(), v.end(), std::mt19937{std::random_device{}()});
+ 
+    std::cout << "Contents of the list: ";
+    for (int n : l) std::cout << n << ' '; std::cout << '\n';
+ 
+    std::cout << "Contents of the list, as seen through a shuffled vector: ";
+    for (int i : v) std::cout << i << ' '; std::cout << '\n';
+ 
+    std::cout << "Doubling the values in the initial list...\n";
+    for (int& i : l) {
+        i *= 2;
+    }
+ 
+    std::cout << "Contents of the list, as seen through a shuffled vector: ";
+    for (int i : v) std::cout << i << ' '; std::cout << '\n';
+}
