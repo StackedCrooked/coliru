f0f61793360156c2fb98996246507d227b23b29b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-08 13:58:30 +0000

    Update archive 24

---
+++ Archive2/24/1ce5ab82b4a018/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+template <class T>
+class C
+{
+public:
+    typedef char c;
+    typedef double d;
+
+    int fun() {}
+
+    static c testFun( decltype(&C::fun) ) {return c();} 
+    static d testFun(...) { return d(); }
+};
+
+int main() {
+    C<int>::testFun(0); // Why no ambiguity?
+}
