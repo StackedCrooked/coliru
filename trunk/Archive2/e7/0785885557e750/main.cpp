c296df8eb69a1b3892d220050c47b581715bf718
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-08 14:06:46 +0000

    Update archive e7

---
+++ trunk/Archive2/e7/0785885557e750/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+typedef int miles;
+typedef std::basic_string<char16_t> what;
+
+namespace Jungle
+{   
+    template <typename T>
+    class Tiger
+    {
+        public:
+        void growl(T)
+        {
+            
+        }
+        
+    };
+}
+
+template class Jungle::Tiger<std::basic_string<char16_t, std::char_traits<char16_t>, std::allocator<char16_t> > >;
+template class Jungle::Tiger<std::basic_string<char, std::char_traits<char>, std::allocator<char> > >;
+template class Jungle::Tiger<std::basic_string<int> >;
+template class Jungle::Tiger<std::wstring>;
+template class Jungle::Tiger<std::u32string>;
+
+template class Jungle::Tiger<std::map<int,int>>;
+template class Jungle::Tiger<std::map<std::vector<int>,int>>;
+
+//two situations if you write:
+// 1) template class Jungle::Tiger<std::string>;
+// or
+// 2) template class Jungle::Tiger<std::basic_string<char, std::char_traits<char>, std::allocator<char> > >;
+// it will maintain the sugar of 1 and add sugar to 2 to become like 1... all other instanciations seem to work the same between clang and gcc
+
+int fly(miles distance)
+{
+   return distance + 10; 
+}
+
+
+int main()
+{
+   
+   std::cout << "Hello World" << std::endl; 
+   fly(10);
+   return 0;
+}
