af7213b37e250bffdae1fd91e5bfab6735324677
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-09 00:08:18 +0000

    Update archive f0

---
+++ trunk/Archive2/f0/bf1db4e7199f88/main.cpp
@@ -0,0 +1,18 @@
+#include  <iostream>
+#include <string>
+using namespace std;
+int main()
+    {
+
+    double n1,n2,n3;
+
+    cin >> n1>>n2>>n3;
+    cout.precision(2);
+    cout << (n1+n2+n3)/3;
+
+
+
+    int nota;
+    cin >> nota;
+    cout << nota*2;
+
