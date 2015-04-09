b8f5face6bf0de6568c421cf2833ceb5fa26dc70
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-09 12:48:30 +0000

    Update archive c1

---
+++ trunk/Archive2/c1/10462113337de2/main.cpp
@@ -0,0 +1,26 @@
+#include <boost/range/adaptors.hpp>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace boost::adaptors;
+
+class pooper {
+public:
+    bool poop() const {
+        static auto result = false;
+        return result = !result;
+    }
+};
+
+std::string to_string(pooper const&) {
+    return "💩\n";
+}
+
+int main() {
+    std::vector<pooper> poopers{pooper(), pooper(), pooper(), pooper(), pooper()};
+    for (auto&& pooper : poopers | filtered(std::mem_fn(&pooper::poop))) {
+        std::cout << to_string(pooper);
+    }
+}
