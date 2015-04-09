b519636332f76c9e60d5dc0a839fd9999eab399f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-09 04:35:23 +0000

    Update archive c1

---
+++ Archive2/c1/d8f3afc8e08d16/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class shape {
+    
+}
+
+
+int main()
+{
+    std::vector<std::string> words = {
+        "Hello", "from", "GCC", __VERSION__, "!"
+    };
+    std::cout << words << std::endl;
+}
