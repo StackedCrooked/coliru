6f3c607b4077c81a968ff5cd6d179e5e21120aa3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-10 20:25:41 +0000

    Update archive 5a

---
+++ trunk/Archive2/5a/2a3b4cd48d0dda/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+     {
+
+   int T1,T2,T3,T4;
+   cin >>T1>>T2>>T3>>T4;
+   cout << (T1+T2+T3+T4)-3;
+
+
+
+
+
+
+
+     return 0;
+     }
