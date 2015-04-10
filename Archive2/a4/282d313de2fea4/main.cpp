825d733d1394d1e7924ce60331f2404e5790f7d9
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-10 19:47:18 +0000

    Update archive a4

---
+++ Archive2/a4/282d313de2fea4/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <type_traits>
+
+struct animal { };
+struct cat : animal { };
+struct elephant : animal { };
+
+struct car { };
+struct trabant : car { };
+struct lada : car { };
+
+template <typename T>
+typename std::enable_if<std::is_base_of<animal, T>::value>::type
+foo(T) { }
+
+template <typename T>
+typename std::enable_if<std::is_base_of<car, T>::value>::type
+bar(T) { }
+
+int
+main() {
+    foo(cat{});
+    foo(elephant{});
+    foo(trabant{});
+    
+    bar(trabant{});
+    bar(lada{});
+    //bar(elephant{});
+}
