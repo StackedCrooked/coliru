0e1e79ce5d99312c1bad6f72213cd21fa8a51097
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-10 16:46:55 +0000

    Update archive 1d

---
+++ Archive2/1d/fd001bcef9bd9a/main.cpp
@@ -0,0 +1,33 @@
+#include <functional>
+#include <boost/signals2/signal.hpp>
+
+namespace bs = boost::signals2;
+
+template<class Signature, class Owner>
+class signal
+    : private bs::signal<Signature>
+{
+    friend Owner;
+    
+    using bs::signal<Signature>::signal;
+    
+public:
+    using bs::signal<Signature>::connect;
+};
+
+struct foo
+{
+    signal<void(), foo> on_stuff;
+    
+    void test() { on_stuff(); }
+};
+
+#include <iostream>
+
+int main()
+{
+    foo f;
+    f.on_stuff.connect([]{ std::cout << "whoo\n"; });
+    f.on_stuff();
+    f.test();
+}
