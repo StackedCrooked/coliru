f7114ac83b1be4a5197ebe84e61da3cbeb5f8c08
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-10 18:50:21 +0000

    Update archive 00

---
+++ Archive2/00/88f4680baa8e34/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+    {
+    int T1;
+    int T2;
+    int T3;
+    int T4;
+    cin >> T1 >> T2 >> T3 >> T4 ;
+    cout << (T1+T2+T3+T4)-3;
+    
+    return 0;
+    }
