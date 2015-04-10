c4b0cf967a083a661c72b17ae9f7c21e9fc80b92
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-10 16:09:30 +0000

    Update archive e3

---
+++ Archive2/e3/96398e099e2a4e/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+struct MyClass
+{
+    int i;
+    
+    MyClass operator+( MyClass const& m ) {
+        MyClass r = m.i + i;
+        return r;
+    }
+    
+    MyClass(int i) : i(i) {std::cout << "Ctor!\n";}
+    MyClass(MyClass const&) {std::cout << "Copy-Ctor!\n";}
+    ~MyClass() {std::cout << "* Dtor!\n";}
+};
+
+int main() {
+    MyClass c{7}, b{3},
+            a = b + c;
+}
