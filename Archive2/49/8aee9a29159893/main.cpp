382c67f5a74e0d1a65c41d055ad122f0ffe655a0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-10 14:34:02 +0000

    Update archive 49

---
+++ Archive2/49/8aee9a29159893/main.cpp
@@ -0,0 +1,25 @@
+#include <boost/spirit/include/qi.hpp>
+
+namespace qi = boost::spirit::qi;
+
+int main() {
+    std::string str;
+    while (std::getline(std::cin, str)) {
+        auto f = str.begin(), l = str.end();
+
+        std::vector<double> v, w;
+
+        bool ok = qi::phrase_parse(
+                f, l, *(qi::double_>>!qi::lit('%')) >> *(qi::double_>'%'),
+                qi::blank | '|', v, w);
+
+        if (ok && f == l) {
+            std::cout << "Parsed " << v.size() << "/" << w.size() << " elements\n";
+        } else {
+            std::istringstream iss(str);
+            if (iss >> str && (str == "q" || str == "Q"))
+                break;
+            std::cout << "Invalid input. Remaining '" << std::string(f,l) << "'\n";
+        }
+    }
+}
