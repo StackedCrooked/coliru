0817c049d0ad3c7d401762bcf92459897704a307
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-11 15:48:34 +0000

    Update archive 0d

---
+++ Archive2/0d/288438f8dfeb27/main.cpp
@@ -0,0 +1,16 @@
+//oper mat.
+#include <iostream>
+
+using namespace std;
+
+int main()
+    {
+
+  cout<< "3+4*2= " << 3+4*2 << endl;
+
+cout<<"256*256+1024*16= " << 256*256+1024*16 << endl;
+
+
+
+return 0;
+    }
