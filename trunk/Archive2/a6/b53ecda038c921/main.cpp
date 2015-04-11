f811b51368d9a20d5156c0e5f0dd0482bb4511ee
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-11 16:11:43 +0000

    Update archive a6

---
+++ trunk/Archive2/a6/b53ecda038c921/main.cpp
@@ -0,0 +1,17 @@
+//entrada e variavéis 
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+    {
+    string nome;
+    cout<< "Digite seu nome: ";
+    cin>> nome;
+    cout<< "\nOlá, " << nome << endl;
+
+
+
+
+    return 0;
+    }
+
