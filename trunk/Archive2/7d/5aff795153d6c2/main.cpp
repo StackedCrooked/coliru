b63c0583caf21b66c826bc73b6a28face7824696
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-11 18:45:31 +0000

    Update archive 7d

---
+++ trunk/Archive2/7d/5aff795153d6c2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+#include <iomanip>
+ 
+using namespace std;
+ 
+int main() {
+    // your code goes here
+	struct tm now = { 0 };
+	cout << "Please enter the 8-digit date (YYYYMMDD): " << endl;
+	cin >> std::get_time(&now, "%Y%m%d");
+    
+    cout << "You entered " << put_time(&now, "%d %B %Y") << '\n';
+}
