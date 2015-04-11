a5e6819b4b281e8c7e1681000ed86f68fe056b59
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-11 08:35:25 +0000

    Update archive d4

---
+++ Archive2/d4/02de37adac2c9e/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   double a(0), b(0); //Déclaration des variables utiles
+
+   cout << "Bienvenue dans le programme d'addition a+b !" << endl;
+
+   cout << "Donnez une valeur pour a : "; //On demande le premier nombre
+   cin >> a;
+
+   cout << "Donnez une valeur pour b : "; //On demande le deuxième nombre
+   cin >> b;
+
+   //…
+
+   return 0;
+}
