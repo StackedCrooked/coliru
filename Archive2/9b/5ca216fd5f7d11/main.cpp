b3e57c833859fa21a86c45392a8ac80bfeacc2c1
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-11 19:42:08 +0000

    Update archive 9b

---
+++ Archive2/9b/5ca216fd5f7d11/main.cpp
@@ -0,0 +1,20 @@
+#include <boost/any.hpp>
+#include <vector>
+#include <string>
+#include <iostream>
+
+template <const char *str1, const char*... str2> struct restrictedValues {};
+
+template<const char *... str>
+void validate(restrictedValues<str...>*) {
+    std::cout << "custom version\n";
+}
+
+extern char const client[] = "hello";
+extern char const server[] = "world";
+
+int main()
+{
+    restrictedValues<client, server>* p = 0;
+    validate(p);
+}
