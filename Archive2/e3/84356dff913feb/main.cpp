682880c2841055fa47c4ec27bf5d306ac99592bc
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-11 05:57:22 +0000

    Update archive e3

---
+++ Archive2/e3/84356dff913feb/main.cpp
@@ -0,0 +1,27 @@
+#include <type_traits>
+
+#define requires(condition) \
+    typename std::enable_if<condition, void>::type
+
+template<typename T>
+bool constexpr PODType() {
+    return std::is_pod<T>();
+}
+
+// example:
+#include <cstring>
+#include <string>
+
+template<typename T, typename U>
+    requires (PODType<T>() && PODType<U>())
+T memcpy_cast(U&& from) {
+    static_assert(sizeof(T) == sizeof(U));
+    T to;
+    std::memcpy(&to, &from, sizeof(to));
+    return to;
+}
+
+int main() {
+    memcpy_cast<unsigned>(42);
+    // memcpy_cast<std::string>(42); // uncomment to see compilation fail
+}
