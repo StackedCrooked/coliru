86032c38cbbacd4bf7190e2ff8f7db5abd9c9130
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-11 23:46:51 +0000

    Update archive c8

---
+++ trunk/Archive2/c8/2b4ef54ff1b34d/main.cpp
@@ -0,0 +1,27 @@
+
+#include <future>
+
+typedef std::function<void()> F;
+
+F faux_queue_slot;
+
+void runLater(F f)
+{
+    faux_queue_slot = std::move(f);
+}
+
+struct W
+{
+    std::promise<int> p;
+    
+    void operator()()
+    {
+        p->set_value(1);
+    }
+};
+
+int main()
+{
+    W w;
+    runLater(w);
+}
