d9df6b7cb6bf92f4f2a93bfb63473197b54a573e
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-11 16:52:01 +0000

    Update archive a5

---
+++ Archive2/a5/5bf7ced8b09566/main.cpp
@@ -0,0 +1,19 @@
+//entrada e variaveis 01
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+    {
+    int numero1;
+    int numero2;
+    int soma;
+    cout<< "Digite um número: " << endl;
+    cin>> numero1;
+    cout<< "Digite outro número: " << endl;
+    cin>> numero2;
+    soma=numero1+numero2;
+    cout<< "O resultado é: " <<soma << endl;
+
+    return 0;
+    }
