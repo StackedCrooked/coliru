dc7dbef4b62047e365b5951ac6b4a3c124087ec0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-11 16:10:50 +0000

    Update archive 6b

---
+++ Archive2/6b/6104658abee7fe/main.cpp
@@ -0,0 +1,25 @@
+
+//Aula do dia Onze de Abril-Sábado
+
+#include <iostream>
+using namespace std;
+int main()
+    {
+    int valor1;
+    int valor2;
+    cin>> valor1 >> valor2;
+    if (valor1>=valor2) {
+
+cout<< valor1*3;
+
+      }else{
+
+
+    cout<< valor2*3;
+      }
+
+
+
+
+    return 0;
+    }
