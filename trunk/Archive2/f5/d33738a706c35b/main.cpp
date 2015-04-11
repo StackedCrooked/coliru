65d0941a89259b388e6014479672d10a38f844ea
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-11 05:26:15 +0000

    Update archive f5

---
+++ trunk/Archive2/f5/d33738a706c35b/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    
+    int x;
+    printf("olar");
+    scanf ("%d",&x);
+    printf("o valor de x eh:%d", x);
+    
+    
+    
+    
+    return 0;
+    
+}
