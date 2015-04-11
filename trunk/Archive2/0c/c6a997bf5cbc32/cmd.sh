230d899c71a31245e8040bf2c3959c4efd52ad1b
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-11 23:15:27 +0000

    Update archive 0c

---
+++ trunk/Archive2/0c/c6a997bf5cbc32/cmd.sh
@@ -0,0 +1,7 @@
+clang++ -std=c++14 -W{all,extra,error,pedantic} main.cpp && echo 'CatMinusMinus: 41.04
+Cinch: 7.06
+TheBlob: 20.63
+APhD: 54.28
+Wrongfold: 12.59
+LadFromMoscow: 31.17
+LightningRacistOverFFXV: 63.04' > input.txt && ./a.out
