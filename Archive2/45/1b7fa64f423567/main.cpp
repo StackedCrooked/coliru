c45280ad693d372f1fb8fbc7b6856cacaa66d6da
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-11 15:54:16 +0000

    Update archive 45

---
+++ Archive2/45/1b7fa64f423567/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+    {
+    int valor1,valor2;
+    cin >> valor1 >> valor2;
+    if (valor1>valor2){
+    cout << valor1*3;
+    }else{
+    cout << valor2*3;
+    }
+    return 0;
+    }
