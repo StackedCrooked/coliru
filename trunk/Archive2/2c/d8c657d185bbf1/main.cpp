db6fc88e3eaf9c2cb8d6a1010b9c2cba35afa945
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-11 16:45:15 +0000

    Update archive 2c

---
+++ trunk/Archive2/2c/d8c657d185bbf1/main.cpp
@@ -0,0 +1,17 @@
+
+//exercicios de variaveis
+#include <iostream>
+using namespace std;
+int main()
+    {
+   int pontos=10;
+   pontos=10+2;
+
+   cout<< "resultado da expressão é: " << pontos << endl;
+
+
+
+
+    return 0;
+    }
+
