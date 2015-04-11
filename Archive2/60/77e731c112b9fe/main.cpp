31cf5fe78cc92e5014f7b31a8ef6d6ffaeaa4ed2
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-11 13:08:02 +0000

    Update archive 60

---
+++ Archive2/60/77e731c112b9fe/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+    {
+    //intrução para criar trẽs variáveis
+    double p1,p2,p3;
+    //instrução para ler trẽs números em uma linha
+    //guardando nas variáveis p1,p2,p3
+    cin>>p1>>p2>>p3;
+    double media;
+    //instrução para ler trẽs números em uma linha
+    //guardando nas variáveis p1,p2,p3
+    media=(p1+p2+p3)/3;
+    cout << media;
+    cout.precision(2);
+
+
+
+
+
+
+
+
+    return 0;
+    }
