b2857d27219fd2f78398a7c8e9cf87272ac6f274
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-12 07:19:07 +0000

    Update archive e2

---
+++ trunk/Archive2/e2/141492723f302e/main.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+ 
+int main()
+{
+    std::string filename = "example.123";
+ 
+    std::fstream fs;
+ 
+    fs.open(filename);
+ 
+    if(!fs.is_open())
+    {
+       fs.clear();
+       fs.open(filename, std::ios::out, 00400); //Create file.
+       fs.close();
+       fs.open(filename);
+    }
+ 
+    std::cout << std::boolalpha;
+    std::cout << "fs.is_open() = " << fs.is_open() << '\n';
+    std::cout << "fs.good() = " << fs.good() << '\n';
+}
