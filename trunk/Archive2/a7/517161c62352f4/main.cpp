ff0b7008cd28e4746fdb10b4fdcbdf8dd8c09a9d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-12 00:05:56 +0000

    Update archive a7

---
+++ trunk/Archive2/a7/517161c62352f4/main.cpp
@@ -0,0 +1,31 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+struct user {
+    std::string name;
+    float time;
+    
+    friend std::istream &operator>>(std::istream &is, user &r) { 
+        std::getline(is, r.name, ':');
+        is >> r.time;
+        is.ignore(100, '\n');
+        return is;
+    }
+    
+    friend std::ostream &operator<<(std::ostream &os, user const &r) { 
+        return os << r.name << ": " << (int)r.time;
+    }
+};
+
+int main () {
+    std::ifstream file("input.txt");
+
+    std::vector<user> users{std::istream_iterator<user>(file), std::istream_iterator<user>()};
+
+    std::sort(users.begin(), users.end(), [](user const &a, user const &b){ return a.time < b.time; });
+    std::copy(users.rbegin(), users.rend(), std::ostream_iterator<user>(std::cout, "\n"));
+}
