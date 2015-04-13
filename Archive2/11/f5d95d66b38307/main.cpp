5fb151db29d4864db5d2bb54b460ebd0dbd760c5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-13 07:45:36 +0000

    Update archive 11

---
+++ Archive2/11/f5d95d66b38307/main.cpp
@@ -0,0 +1,43 @@
+#include <stdint.h>
+#include <iostream>
+
+int64_t GetOldDustThreshold(size_t nSize, int64_t nRelayTxFee = 1000)
+{
+    // Based on Bitcoin Core 0.9
+    return 1 + (((nSize * nRelayTxFee * 3) - 1) / 1000);
+}
+
+int64_t GetNewDustThreshold(size_t nSize, int64_t nRelayTxFee = 1000)
+{
+    // Based on Bitcoin Core 0.19
+    return (nRelayTxFee * nSize / 1000) * 3;
+}
+
+bool isEqual(size_t nSize, int64_t nRelayTxFee = 1000)
+{
+    int64_t nDustLimitOld = GetOldDustThreshold(nSize, nRelayTxFee);
+    int64_t nDustLimitNew = GetNewDustThreshold(nSize, nRelayTxFee);
+
+    return nDustLimitOld == nDustLimitNew;
+}
+
+int main() 
+{
+    int64_t nRelayTxFee = 1000;
+    
+    for (size_t nSize = 1; nSize < 10000; ++nSize)
+    {
+        if (!isEqual(nSize, nRelayTxFee))
+        {
+            std::cout << "Error for nSize: " << nSize << std::endl
+            << "Old: " << GetOldDustThreshold(nSize, nRelayTxFee) << std::endl
+            << "New: " << GetNewDustThreshold(nSize, nRelayTxFee) << std::endl;
+
+            return 1;
+        }
+    }
+    
+    std::cout << "Success! The calculations have a similar outcome." << std::endl;
+
+    return 0;
+}
