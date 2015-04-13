b373469e9be0d4e4ca729378f0ebca228a4548e0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-13 10:34:18 +0000

    Update archive 60

---
+++ trunk/Archive2/60/b3610ea0e0c477/main.cpp
@@ -0,0 +1,14 @@
+#include <boost/interprocess/managed_mapped_file.hpp>
+
+namespace bi = boost::interprocess;
+
+int main() {
+    std::string vecFile = "vector.dat";
+    bi::managed_mapped_file file_vec(bi::open_or_create,vecFile.c_str(), 1000);
+
+    typedef bi::allocator<int, bi::managed_mapped_file::segment_manager> int_alloc;
+    typedef std::vector<int, int_alloc>  MyVec;
+
+    int_alloc alloc(file_vec.get_segment_manager());
+    MyVec * vecptr = file_vec.find_or_construct<MyVec>("myvector")(alloc);
+}
