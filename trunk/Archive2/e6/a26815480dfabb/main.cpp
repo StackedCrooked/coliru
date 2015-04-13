dba47368fae26878fe959713b836d075658e1822
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-13 19:34:08 +0000

    Update archive e6

---
+++ trunk/Archive2/e6/a26815480dfabb/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "0 1 2 3" << std::endl << "4 5 6 7" << std::endl << "8 9 10 11" << std::endl << std::endl;   
+    std::cout << "==Calcul de la colonne et de la ligne pour i=6==" << std::endl << std::endl << "Colone=" << 6/3 << std::endl << "Ligne=" << 6/4 ;
+    return 0;
+}
