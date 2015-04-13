99f0d3e2900e8b8ddb3ded39b5e355053eaeae22
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-13 19:35:18 +0000

    Update archive 6d

---
+++ Archive2/6d/fa20572626b88a/main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int compare_ints(const void* a, const void* b)
+{
+    //printf(" a = %d\n", *a);
+    int arg1 = *(const int*)a;
+    int arg2 = *(const int*)b;
+ 
+    if (arg1 < arg2) return -1;
+    if (arg1 > arg2) return 1;
+    return 0;
+}
+ 
+int main(void)
+{
+    int ints[] = { -2, 99, 0, -743, 2, 3, 4 };
+    int size = sizeof ints / sizeof *ints;
+ 
+    qsort(ints, size, sizeof(int), compare_ints);
+ 
+    for (int i = 0; i < size; i++) {
+        printf("%d ", ints[i]);
+    }
+ 
+    printf("\n");
+}
