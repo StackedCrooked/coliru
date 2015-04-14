f2a7f173dabc734ceb38cb0a9dbf3dc5fed92b29
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-14 15:49:48 +0000

    Update archive 86

---
+++ trunk/Archive2/86/961a3fb3bd3451/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <boost/make_shared.hpp>
+#include <boost/python.hpp>
+#include <boost/shared_ptr.hpp>
+
+class foo
+{
+public:
+  foo(const char* value) : value_(value) {}
+  foo(const foo&) = delete;
+  foo& operator=(const foo&) = delete;
+
+  bool operator==(const foo& rhs) 
+  {
+    std::cout << "foo::operator==()" << std::endl;
+    return value_ == rhs.value_;
+  }
+
+  bool operator!=(const foo& rhs)
+  {
+    std::cout << "foo::operator!=()" << std::endl;  
+    return !(*this == rhs);
+  }
+
+private:
+  std::string value_;
+};
+
+boost::shared_ptr<foo> make_foo(const char* value)
+{
+  return boost::make_shared<foo>(value);
+}
+
+BOOST_PYTHON_MODULE(example)
+{
+  namespace python = boost::python;
+  python::class_<foo, boost::shared_ptr<foo>, boost::noncopyable>(
+      "Foo", python::no_init)
+    .def("__init__", python::make_constructor(&make_foo))
+    .def("__eq__", &foo::operator==)
+    .def("__ne__", &foo::operator!=)
+    ;
+}
