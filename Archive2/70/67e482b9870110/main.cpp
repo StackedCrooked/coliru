a19dd818344f89518e044b924cc3097bfea537b0
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-14 19:52:02 +0000

    Update archive 70

---
+++ Archive2/70/67e482b9870110/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <functional>
+#include <boost/signals2.hpp>
+
+namespace bs2 = boost::signals2;
+
+struct erased_connection
+{
+    
+public:
+    
+    erased_connection(bs2::connection c)
+    {
+        auto sc = std::make_shared<bs2::connection>(std::move(c));
+        disconnector = [sc] { sc->disconnect(); };
+    }
+    
+    erased_connection(std::function<void()> disconnector)
+        : disconnector{disconnector}
+    {
+    }
+
+    erased_connection(erased_connection const& rhs) = delete;
+
+    erased_connection(erased_connection&& rhs)
+        : disconnector{std::move(rhs.disconnector)}
+    {
+        rhs.disconnector = nullptr;
+    }
+
+    erased_connection& operator = (erased_connection const& rhs) = delete;
+
+    erased_connection& operator = (erased_connection&& rhs)
+    {
+        disconnector = std::move(rhs.disconnector);
+        rhs.disconnector = nullptr;
+        return *this;
+    }
+    
+    void disconnect()
+    {
+        disconnector();
+    }
+    
+    ~erased_connection()
+    {
+        disconnector();
+    }
+    
+private:
+
+    std::function<void()> disconnector;
+    
+};
+
+int main()
+{  
+    bs2::signal<void()> s;
+    
+    erased_connection c = s.connect([] { std::cout << "Hello!" << std::endl; });
+    
+    s(); // Will cause printing
+    
+    c.disconnect();
+    
+    s(); // Will not cause printing
+}
