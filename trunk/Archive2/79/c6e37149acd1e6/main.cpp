8711a42e221a14e249a569828e4cf420a8366b02
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-14 23:49:48 +0000

    Update archive 79

---
+++ trunk/Archive2/79/c6e37149acd1e6/main.cpp
@@ -0,0 +1,25 @@
+// stoi example
+#include <iostream>   // std::cout
+#include <string>     // std::string, std::stoi
+
+int main ()
+{
+  std::string str_dec = "2001, A Space Odyssey";
+  std::string str_hex = "40c3";
+  std::string str_bin = "-10010110001";
+  std::string str_auto = "0x7f";
+
+  std::string::size_type sz;   // alias of size_t
+
+  int i_dec = std::stoi (str_dec,&sz);
+  int i_hex = std::stoi (str_hex,nullptr,16);
+  int i_bin = std::stoi (str_bin,nullptr,2);
+  int i_auto = std::stoi (str_auto,nullptr,0);
+
+  std::cout << str_dec << ": " << i_dec << " and [" << str_dec.substr(sz) << "]\n";
+  std::cout << str_hex << ": " << i_hex << '\n';
+  std::cout << str_bin << ": " << i_bin << '\n';
+  std::cout << str_auto << ": " << i_auto << '\n';
+
+  return 0;
+}
