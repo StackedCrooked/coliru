450bc75830f9557339c5373574f687d5ca17d77c
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-14 10:08:19 +0000

    Update archive ef

---
+++ Archive2/ef/b12c4a31562a21/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Blup {
+    Blup(int ix) : x(ix) {
+    }
+
+    int x;
+};
+
+int main()
+{
+    vector<Blup> vec;
+    vec.push_back(1);   // index 0
+    vec.push_back(2);   // index 1
+    vec.push_back(3);   // index 2
+
+    Blup tmp(4);
+    vec[4]  = tmp;    // Illegal - and won't event show up on the loop
+
+    Blup &b1 = vec[1];
+    b1.x = 7;
+
+    Blup *b2 = &vec[2];
+    b2->x = 8;
+
+    for (auto blup : vec) {
+        cout << blup.x << " ";
+    }
+    cout << endl;
+}
