5c6eb208fe23d140473130c22296555967b18e2a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-14 13:47:19 +0000

    Update archive 80

---
+++ Archive2/80/f9436cd00a9fb9/cmd.sh
@@ -0,0 +1,10 @@
+clang++ -std=c++14 -O2 -I/usr/include/python2.7 -Wall -pedantic main.cpp -lboost_python -lpython2.7 -shared -fPIC -o example.so
+python2.7 <<EOF
+import example
+foo1 = example.Foo("abc")
+foo2 = example.Foo("abc")
+foo3 = example.Foo("def")
+assert(foo1 == foo2)
+assert(foo1 != foo3)
+EOF
+echo $?
