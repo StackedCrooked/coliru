c651175f8b704abbe398e910fbe494009d6c0f8d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-14 19:28:27 +0000

    Update archive 26

---
+++ trunk/Archive2/26/b02dfd2a41705a/main.cpp
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+struct A { int a;};
+
+int main () {
+    struct A a;
+    void* p;
+    struct A* p2;
+    a.a = 1;
+    p = malloc(2*sizeof(struct A));
+    p2 = (char*)p + sizeof(struct A);
+    *p2 = a;
+    
+    double* b = (double*)p2;
+    *b = 0.;
+    
+    int c = *p2.a;
+    
+    free(p);
+}
