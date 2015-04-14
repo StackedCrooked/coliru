390eccd5a3791df4bea3f82d344f1ce06800279f
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-14 11:33:28 +0000

    Update archive b2

---
+++ Archive2/b2/8bc52144c638cb/cmd.sh
@@ -0,0 +1,3 @@
+mkdir -p haystack/{a..z}/sub/{1..10} haystack/j/sub/9/needle
+g++ -std=c++11 -O2 -Wall -pedantic -pthread main.cpp -lboost_system -lboost_filesystem -o test
+cd haystack/k/sub/4 && ../../../../test
