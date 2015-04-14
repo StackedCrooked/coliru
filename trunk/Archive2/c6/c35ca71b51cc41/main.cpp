dfb391879f4e1d90706d42891bd49771be3b4e22
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-14 20:03:19 +0000

    Update archive c6

---
+++ trunk/Archive2/c6/c35ca71b51cc41/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Pour un row-major: (l*w)+c" << std::endl; 
+    std::cout << "Pour l=2 , w=4 , c=2 :"<< std::endl ;
+    std::cout << (2*4)+2;
+    return 0;
+}
