47ddc5e3470f759b60ab9fe72728bf679aedb277
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-14 04:35:47 +0000

    Update archive 0e

---
+++ trunk/Archive2/0e/adeac9f88af037/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdint>
+
+struct f
+{
+    f(int& a)
+        : ref(&a)
+    {
+    }
+    
+    f& operator=(f const& other)
+    {
+        ref = other.ref;
+        return *this;
+    }
+    
+    f(f const& other)
+        : ref(other.ref)
+    {
+    }
+    
+    int *ref;
+};
+
+int main()
+{
+    int a = 5;
+    int b = 6;
+    f one = a;
+    f two = b;
+    
+    one = two;
+    std::cout << "a = " << a << "\n";
+    std::cout << "b = " << b << "\n";
+}
