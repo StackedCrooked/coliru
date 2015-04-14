1e352369a26b9981106a35420ab8395e12837260
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-14 11:44:16 +0000

    Update archive cf

---
+++ Archive2/cf/df923660fa4d41/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <utility>
+
+int& forward(int& i)
+{
+    std::cout << "lvalue: ";
+    return std::forward<int&>(i);   
+}
+
+int&& forward(int&& i)
+{
+    std::cout << "rvalue: ";
+    return std::forward<int&&>(i);    
+}
+
+int*&& forward(int*&& ptr_i)
+{
+    std::cout << "rvalue ptr: ";
+    return std::forward<int*&&>(ptr_i);   
+}
+
+int*& forward(int*& ptr_i)
+{
+    std::cout << "lvalue ptr: ";
+    return std::forward<int*&>(ptr_i);   
+}
+
+const int* forward(const int* const  ptr_to_const)
+{
+    std::cout << "const ptr to lvalue: ";
+    return std::forward<const int* const>(ptr_to_const);
+}
+
+int main()
+{
+    int i = 1;
+
+    const int j = 13;
+
+    int* ptr_i = &i;
+
+    const int * const  const_ptr_const_i = &j;
+
+    int * const const_ptr_i = &i;
+
+
+    std::cout << forward(0) << std::endl;
+    std::cout << forward(i) << std::endl;
+    std::cout << forward(ptr_i) << std::endl;
+    std::cout << forward(&i) << std::endl;
+
+    //These two are almost the same by nature; note the variables they point to are different.
+    std::cout << forward(const_ptr_const_i) << std::endl;
+    std::cout << forward(const_ptr_i) << std::endl;
+
+    std::cout << "Cannot have a const pointer to a const rvalue because you can't really have a const pointer to a temporary/disappearing/ephermal rvalue." << std::endl;
+
+
+    return 0;
+}
