620862316bed3865689748e0195258bc03fcc098
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-15 13:51:14 +0000

    Update archive 0e

---
+++ trunk/Archive2/0e/4ae37637fa90e1/main.cpp
@@ -0,0 +1,23 @@
+#include <type_traits>
+
+#include <iostream>
+template<typename T>
+void print_type() { std::cout << __PRETTY_FUNCTION__ << "\n"; }
+
+using namespace std;
+
+void fun(int a) {
+    cout << "void fun(int a) : " << a << endl;
+}
+
+void fun(double a) {
+    cout << "void fun(double a) : " << a << endl;
+}
+
+#define OVERLOAD_SET(F) ([](auto&&... args) -> decltype(auto) { return F(std::forward<decltype(args)>(args)...); })
+
+int main()
+{
+    auto fl = OVERLOAD_SET(fun);
+    print_type< std::result_of<decltype(fl)(int)>::type >();
+}
