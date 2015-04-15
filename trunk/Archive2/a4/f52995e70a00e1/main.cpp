bb46442dc8f97fd5d940beb08e88b6a71b233302
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-15 08:11:35 +0000

    Update archive a4

---
+++ trunk/Archive2/a4/f52995e70a00e1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+   string s="";
+   while (cin>>s)
+  {
+      if (s=="Saad")
+      cout<<"Bleep"<<endl;
+      else
+      cout<<s<<endl;
+  }
+    
+}
