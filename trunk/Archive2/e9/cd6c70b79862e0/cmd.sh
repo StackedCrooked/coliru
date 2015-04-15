c0dded3308cbae4339cdbf56e3dd5e677814205a
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-15 01:42:45 +0000

    Update archive e9

---
+++ trunk/Archive2/e9/cd6c70b79862e0/cmd.sh
@@ -0,0 +1,28 @@
+set -x;
+# Create a Python Script embed.py
+cat >embed.py <<INPUT
+from ctypes import *
+class SimulinkBus(Structure):
+    _fields_ = [ ("a", c_int), ("b", c_char), ("c", c_float), ("d", c_double), ("e", c_char_p), ("arr", c_int*4) ]
+  
+def Logic( Signal ):
+    return  Signal.a*2 + Signal.c 
+    
+def main( *args,**kwargs ):
+    #for a in args:
+    #   print (a)
+    #for k,v in kwargs.iteritems():
+    #     print ("%s = %s" % (k, v))
+    Signal = cast(c_char_p(kwargs['SimulinkBus']), POINTER(SimulinkBus )).contents
+    print (Signal.a, Signal.b, Signal.c, Signal.d, Signal.e )
+    for i in range(4):
+        print ( Signal.arr[i] )
+        Signal.arr[i] *= 2
+    Signal.c = Logic( Signal )
+    return Signal
+INPUT
+
+# Compile the C code
+gcc -o test -O2 -Wall -Wextra main.cpp -I /usr/include/python2.7 -lpython2.7
+# Run C code
+./test embed main
