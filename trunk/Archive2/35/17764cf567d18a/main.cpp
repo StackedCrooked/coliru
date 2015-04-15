b3b1fcdfa4188119d29450a3a85ddfd9698eed96
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-15 02:25:22 +0000

    Update archive 35

---
+++ trunk/Archive2/35/17764cf567d18a/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <utility>
+
+template <typename T, T... v>
+struct value_pack {
+    static constexpr T array[] {v...};
+	static constexpr std::size_t len = sizeof...(v);
+	using type = value_pack;
+};
+template <typename T, T... v>
+constexpr T value_pack<T, v...>::array[];
+
+
+template <typename T, std::size_t p1, std::size_t p2,
+          typename pack,
+          typename=std::make_index_sequence<pack::len>>
+struct Swap;
+template <typename T, std::size_t p1, std::size_t p2,
+          typename pack, std::size_t...indices>
+struct Swap<T, p1, p2, pack, std::index_sequence<indices...>> :
+	value_pack<T,  pack::array[indices==p1? p2 : indices==p2? p1 : indices]...> {};
+
+int main()
+{
+	using t = Swap<int, 1, 2, value_pack<int, 0, 1, 2, 3>>::type;
+	for (auto i : t::array)
+		std::cout << i << ", ";
+}
