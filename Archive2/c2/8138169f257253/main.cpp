21a2521d711aaaf64bf0c45f5ea6ea9d40d59a57
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-15 13:52:01 +0000

    Update archive c2

---
+++ Archive2/c2/8138169f257253/main.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+int main()
+{
+    int x = 1;
+    int y = 2;
+    
+    x <<=-- y; // x = y
+    {
+       printf("%d ", x);
+    }
+}
+
+
+
