453a2cdbe40573d793ef8bb8354a8c78f76d9e36
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-15 18:21:11 +0000

    Update archive 24

---
+++ Archive2/24/85060dba1cc564/main.cpp
@@ -0,0 +1,17 @@
+/* C-style comments can contain
+multiple lines */
+/* or just one */
+ 
+// C++-style comments can comment one line
+ 
+// or, they can
+// be strung together
+ 
+int main()
+{
+  // The below code won't be run
+  // return 1;
+ 
+  // The below code will be run
+  return 0;
+}
