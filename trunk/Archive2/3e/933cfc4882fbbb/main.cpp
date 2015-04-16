3a0eada3b7fbf7eb9889b8c5cd7f84b19bb1dd54
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-16 23:54:02 +0000

    Update archive 3e

---
+++ trunk/Archive2/3e/933cfc4882fbbb/main.cpp
@@ -0,0 +1,22 @@
+#include <type_traits>                                                             
+#include <string>
+                                                                                
+struct foo
+{
+    template<typename T>
+    foo(T&& t)
+    : t(std::forward<std::wstring>(t))
+    {
+        static_assert(std::is_convertible<T, std::wstring>::value, "");
+    }
+    
+    std::wstring t;
+};
+
+int main()
+{
+    foo f1(L"test");
+    foo f2(std::wstring{});
+    foo f3(0);  // invalid argument type
+}
+
