b69231ba535c5257b5d17fac8eef1417dcd19988
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-16 06:09:35 +0000

    Update archive 2e

---
+++ Archive2/2e/74399f9b0830f1/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <set>
+#include <string>
+#include <iterator>
+
+int main()
+{
+    std::set<std::string> set { "int", "main()", "#include", "<iterator>", "#include", "<string>", "<set>", "<iostream>" };
+
+    {
+        // range-based loop
+        for( const auto str : set ) std::cout << str << ' ' ;
+        std::cout << '\n' ;
+    }
+
+    {
+        // iterators
+        for( auto iter = std::begin(set) ; iter != std::end(set) ; ++iter ) std::cout << *iter << ' ' ;
+        std::cout << '\n' ;
+
+        // iterators, in reverse
+        for( auto iter = set.rbegin() ; iter != set.rend() ; ++iter ) std::cout << *iter << ' ' ;
+        std::cout << '\n' ;
+    }
+
+    {
+        // iterators (legacy)
+        for( std::set<std::string>::iterator iter = set.begin() ; iter != set.end() ; ++iter ) std::cout << *iter << ' ' ;
+        std::cout << '\n' ;
+
+        // iterators, in reverse (legacy)
+        for( std::set<std::string>::reverse_iterator iter = set.rbegin() ; iter != set.rend() ; ++iter ) std::cout << *iter << ' ' ;
+        std::cout << '\n' ;
+    }
+}
