95fae18a463adb5b04b6eed583d68ee25fee39ce
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-16 11:42:46 +0000

    Update archive 31

---
+++ Archive2/31/f18bac531a2fce/cmd.sh
@@ -0,0 +1,10 @@
+g++ -std=c++11 -O2 -Wall -pedantic -pthread main.cpp && ./a.out <<INPUT
+a
+4
+b
+7
+z
+-4
+t
+2
+INPUT
---
+++ Archive2/31/f18bac531a2fce/main.cpp
@@ -0,0 +1,74 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
+
+struct Person {
+    std::string fullname;
+    int age = -1;
+
+    friend std::istream& operator>>(std::istream& is, Person& v) {
+        std::cout << "Fullname: ";
+        std::getline(is, v.fullname);
+        std::cout << "Age: ";
+        is >> v.age;
+        return is.ignore(1024, '\n');
+    };
+    friend std::ostream& operator<<(std::ostream& os, Person const& v) {
+        return os << v.fullname << ", " << v.age;
+    };
+};
+
+template <typename Pred>
+void bubblesort(std::vector<Person>& v, Pred pred)
+{
+    using iterator = decltype(std::begin(v));
+
+    auto begin = std::begin(v), end = std::end(v);
+
+    while (end != begin) {
+        iterator new_end = begin;
+
+        for (iterator i = begin; std::next(i) != end; std::advance(i, 1)) {
+            iterator next = std::next(i);
+
+            if (pred(*next, *i)) {
+                std::iter_swap(i, next);
+                new_end = next;
+            }
+        }
+
+        end = new_end;
+    }
+}
+
+std::vector<Person> input();
+void output(std::string const& caption, std::vector<Person> const& table);
+
+int main()
+{
+    std::vector<Person> table = input();
+    output("before sorting", table);
+
+    bubblesort(table, [](Person const& a, Person const& b) { return a.fullname < b.fullname; });
+    output("by name", table);
+
+    bubblesort(table, [](Person const& a, Person const& b) { return a.age < b.age; });
+    output("by age", table);
+}
+
+std::vector<Person> input()
+{
+    std::vector<Person> result;
+    std::copy(std::istream_iterator<Person>(std::cin), {}, back_inserter(result));
+    return result;
+}
+
+void output(std::string const& caption, std::vector<Person> const& table)
+{
+    std::cout << caption << ":\n";
+    for (auto& p : table)
+        std::cout << "\t" << p << "\n";
+}
+
