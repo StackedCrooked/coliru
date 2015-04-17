0e6eeeb04d2a6f93bf694660bfa0b8ff5c8a6678
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-17 08:51:48 +0000

    Update archive 00

---
+++ Archive2/00/7b0f10954cc633/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdexcept>
+#include <sstream>
+#include <string>
+
+class my_error : public std::runtime_error {
+ public:
+  explicit my_error(const std::string& e) : runtime_error(e) {}
+};
+
+class format {
+ public:
+  format() {}
+  operator std::string() {
+    return stream.str();
+  }
+
+  template <class T>
+  friend format& operator<<(format& f, const T& data) {
+    f.stream << data;
+    return f;
+  }
+
+ private:
+  std::ostringstream stream;
+};
+
+int main() {
+  try {
+    const int cols = 3;
+    const int rows = 4;
+    throw my_error(
+      format() << "Number of columns in A (" << cols
+      << ") must match number of rows in B (" << rows << ")");
+  }
+  catch (const my_error &e) {
+    std::cout << e.what() << "\n";
+  }
+  return 0;
+}
