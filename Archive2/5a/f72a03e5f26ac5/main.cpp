3a3b5b76e3c6ac847fd20c38eaa7676f565568de
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-17 16:48:44 +0000

    Update archive 5a

---
+++ Archive2/5a/f72a03e5f26ac5/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+    {
+    int valor10,valor11;
+    cin >> valor10,valor11;
+    cout << 11*3;
+
+
+
+
+
+        return 0;
+    }
