18a76d2674579cf9415443f2b0dab2bfbf9443ab
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-17 01:55:52 +0000

    Update archive 17

---
+++ trunk/Archive2/17/49696b16a98945/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdint>
+#include <array>
+#include <iostream>
+
+
+std::array<uint8_t, 6> sdkljf = {{0,1,2,3,4,5}};
+
+int main()
+{
+    std::cout << sdkljf[0] << sdkljf[0] << std::endl;
+}
