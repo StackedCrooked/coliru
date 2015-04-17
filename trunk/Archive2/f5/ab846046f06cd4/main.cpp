b12679b77d8b92ce4f41fb751ba047c12334ddae
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-17 16:22:49 +0000

    Update archive f5

---
+++ trunk/Archive2/f5/ab846046f06cd4/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+    {
+    int valor10,valor11; //Declarando duas variaveis inteiras com os nomes valor10 e valor11
+    //Errada porque tem que colocar o operador de extração no lugar da virgula
+    //cin >> valor10,valor11; 
+    cin >> valor10 >> valor11; 
+   //Agora voce precisa usar o if para comparar qual é o maior numero , o que tá na variavel valor10 ou na variavel valor11
+   cout << 11*3; //Esse comando é estatico, sempre vai imprimir 33 
+ 
+
+
+
+
+        return 0;
+    }
