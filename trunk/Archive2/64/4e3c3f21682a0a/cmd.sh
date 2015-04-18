a08c9172b0d4cbd978cef94c692d68899db9c4cf
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-18 23:37:15 +0000

    Update archive 64

---
+++ trunk/Archive2/64/4e3c3f21682a0a/cmd.sh
@@ -0,0 +1,4 @@
+clang++ -std=c++1z -stdlib=libc++ -Wall -Wextra -pedantic-errors -O3 -pthread main.cpp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 && ./a.out 
+#g++-4.9 -std=c++1y -Wall -Wextra -pedantic-errors -O3 -pthread main.cpp && ./a.out
+#clang++ -std=c++1z -E -P main.cpp
+#clang -x c -std=c11 -Wall -Wextra -pedantic -O3 main.cpp && ./a.out
