6675d34b1a0305f63f661a597989da33e884d8eb
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-18 22:33:31 +0000

    Update archive 54

---
+++ trunk/Archive2/54/ccd36e613079bd/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdarg>
+
+int sum(int argnum, ...)
+{
+    va_list arguments;
+    int i;
+    int sum = 0;
+
+    va_start(arguments, argnum); /* Needs last known character to calculate
+                               the address of the other parameters */
+    for(i = 0; i < argnum; ++i)
+        sum += va_arg(arguments, int); /* use next argument */
+
+    va_end(arguments);
+
+    return sum;
+}
+    
+int main()
+{
+    std::cout << "5 + 7 + 1 + 3 - 2 = " << sum(5, 5, 7, 1, 3, -2);
+
+   return 0;
+}
