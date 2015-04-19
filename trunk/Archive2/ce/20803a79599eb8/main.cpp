9414b8ad3c96e15386dbb86d4f3f7c4b07ab3a7d
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-19 11:45:30 +0000

    Update archive ce

---
+++ trunk/Archive2/ce/20803a79599eb8/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+typedef unsigned char u_char;
+typedef unsigned short u_short;
+typedef unsigned long long u_ll;
+typedef u_ll DAYS_DTYPE;
+typedef u_char HOURS_DTYPE;
+typedef u_char MINUTES_DTYPE;
+typedef u_char SECONDS_DTYPE;
+typedef u_short MILLISECONDS_DTYPE;
+
+// used class members; max. values are handled class-internal
+DAYS_DTYPE          m_days = 1;         // max.: 18.446.744.073.709.551.615
+HOURS_DTYPE         m_hours = 2;        // max.: 23
+MINUTES_DTYPE       m_minutes = 3;      // max.: 59
+SECONDS_DTYPE       m_seconds = 4;      // max.: 59
+MILLISECONDS_DTYPE  m_milliseconds = 5; // max.: 999
+
+enum TimeUnit { Milliseconds, Seconds, Minutes, Hours, Days };
+
+// used member function
+template <TimeUnit unit = Seconds, typename T = decltype(m_seconds)>
+T getTime()
+{
+    switch (unit)
+    {
+    case Milliseconds: return m_milliseconds;
+    case Seconds: return m_seconds;
+    case Minutes: return m_minutes;
+    case Hours: return m_hours;
+    case Days: return m_days;
+    default:
+        break;
+    }
+}
+    
+int main()
+{
+    if(getTime<Milliseconds>() == 5)
+        std::cout << "it works!" << std::endl;
+
+   return 0;
+}
