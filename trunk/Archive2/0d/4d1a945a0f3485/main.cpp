b1ba7c4e107a8201877519bcd0f81e47d38b2bd3
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-19 17:25:11 +0000

    Update archive 0d

---
+++ trunk/Archive2/0d/4d1a945a0f3485/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+#include <unistd.h>
+#include <string>
+#include <iostream>
+#include <stdio.h>
+
+using std::cout; using std::cerr;
+using std::cin;  using std::string;
+using std::endl;
+
+int main(int argc,char* argv[])
+{
+    for(int x = 0; x <= 2013; x++)
+    {
+
+        cout << "\rIteration: "<< x;
+        cout << "";
+
+    }
+
+    return 0;
+}
