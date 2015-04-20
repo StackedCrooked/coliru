bdba97a8bcff1bd2a9aeb634a14d291296763b97
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-20 00:04:49 +0000

    Update archive 14

---
+++ trunk/Archive2/14/bb831c7457e321/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+struct X {
+private:
+    static int iglobal;
+    const int i;
+public:
+    X() : i(++iglobal) {
+        std::cout << "X(" << i << ")" << std::endl;
+    }
+    X(const X& x) : i(x.i) {
+        std::cout << "X&(" << i << ")" << std::endl;
+    }
+    X(X&& x) : i(std::move(x.i)) {
+        std::cout << "X&&(" << i << ")" << std::endl;
+    }
+    ~X() {
+        std::cout << "~X(" << i << ")" << std::endl;
+    }
+};
+
+int X::iglobal = 0;
+
+int main() {
+    std::vector<X> objects;
+    for (int i = 10; i != 0; --i) {
+        objects.emplace_back();
+    }
+    std::cout << "the end" << std::endl;
+}
