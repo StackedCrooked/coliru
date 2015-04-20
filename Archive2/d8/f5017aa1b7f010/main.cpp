85156fa7346564588ddf1c9f7639f9c8bff7b5f5
francis.rammeloo <francis.rammeloo@36614edc-3e3a-acb8-9062-c8ae0e4185df>
2015-04-20 13:45:56 +0000

    Update archive d8

---
+++ Archive2/d8/f5017aa1b7f010/main.cpp
@@ -0,0 +1,31 @@
+struct B {};
+struct Coords;
+struct COORDSTYPE;
+
+template <int DIM>
+class A : public B
+{
+
+    public:
+
+        template <class T>
+        T getValue(void* ptr, int posIdx);
+
+        template <class T>
+        T getValue(void* ptr, int posIdx, int valIdx);
+
+        template <class T>
+        T getValue(void* ptr, Coords& coord, Coords& size, int valIdx);
+
+        template <class T>
+        void setValue(void* ptr, int posIdx, T val);
+
+        template <class T>
+        void setValue(void* ptr, int posIdx, int valIdx, T val);
+
+        template <class T>
+        void setValue(void* ptr, COORDSTYPE& coord, COORDSTYPE& size, int valIdx, T val);
+
+};
+
+int main() {}
