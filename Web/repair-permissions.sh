837654119bd6b98fac55f3bd8cb5bb3067a8372a
StackedCrooked <francis.rammeloo@gmail.com>
2015-12-22 20:53:39 +0100

    Archive2.log is owned by webserver.

--- Web/repair-permissions.sh
+++ Web/repair-permissions.sh
@@ -29,7 +29,7 @@ chown -R webserver:coliru /var/chroot/tmp & disown
 chmod -R a+rw /var/chroot/tmp
 
 # Make certain files writeable for the webserver.
-for file in $(echo feedback.txt timeout.txt) ; do
+for file in $(echo feedback.txt timeout.txt ../Archive2.log Archive2.log) ; do
   [ -f ${file} ] || echo >${file}
   chown webserver:coliru ${file}
 done
