1c33a1eae73f44ea5243d79d657a5b83b3df0528
StackedCrooked <francis.rammeloo@gmail.com>
2015-12-22 20:54:45 +0100

    Updae.

--- Web/BackupNewArchives.sh
+++ Web/BackupNewArchives.sh
@@ -17,15 +17,14 @@
 }
 
 # empty the file without changing permissions
-echo >../Archive2.log
+printf "" >../Archive2.log
 
 
 backup() {
     # gets the 'ab' part from '/root/etc/../ab/cdefghij'
     p1="$(basename $(dirname $@))"
     #p2="$(basename $@)"
-    echo "entry=$entry"
-    (cd .. && tar -czf "tArchive2/$p1.tar.gz" "Archive2/$p1")
+    (set -x && cd .. && tar -czf "tArchive2/$p1.tar.gz" "Archive2/$p1")
 }
 
 { cat tmp_Archive2.log && rm tmp_Archive2.log ; } | while read line ; do [ "$line" != "" ] && backup "$line" ; done 
