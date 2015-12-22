42006cf112bb42be00b6f491efab13f0c7833cb1
StackedCrooked <francis.rammeloo@gmail.com>
2015-12-22 21:12:16 +0100

    U

--- Web/BackupNewArchives.sh
+++ Web/BackupNewArchives.sh
@@ -11,9 +11,9 @@
 }
 
 [ -f tmp_Archive2.log ] && {
-    sort tmp_Archive2.log ../Archive2.log -o tmp_Archive2.log 
+    sort -R tmp_Archive2.log ../Archive2.log -o tmp_Archive2.log 
 } || {
-    sort ../Archive2.log -o tmp_Archive2.log
+    sort -R ../Archive2.log -o tmp_Archive2.log
 }
 
 # empty the file without changing permissions
@@ -27,6 +27,4 @@ backup() {
     (set -x && cd .. && tar -czf "tArchive2/$p1.tar.gz" "Archive2/$p1")
 }
 
-{ cat tmp_Archive2.log && rm tmp_Archive2.log ; } | while read line ; do [ "$line" != "" ] && backup "$line" ; done 
-
-
+{ cat tmp_Archive2.log | while read line ; do [ "$line" != "" ] && backup "$line" ; done ;  } && rm tmp_Archive2.log 
